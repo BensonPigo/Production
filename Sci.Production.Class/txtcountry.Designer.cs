81c5843d7ff2358433038c9a03528216321d9a2b
YINMEI <yinmei@sportscity.com.tw>
2015-04-13 07:21:25 +0000

    add txtcountry.cs

---
+++ Sci.Production.Class/txtcountry.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Sci.Production.Class
+{
+    partial class txtcountry
+    {
+        /// <summary> 
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 元件設計工具產生的程式碼
+
+        /// <summary> 
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new Sci.Win.UI.TextBox();
+            this.displayBox1 = new Sci.Win.UI.DisplayBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1._Alias = "Country";
+            this.textBox1._Tag = "Id";
+            this.textBox1.HelpColumnWidths = "5,100";
+            this.textBox1.HelpRecordSource = "Country.Id,Alias";
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(31, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating);
+            // 
+            // displayBox1
+            // 
+            this.displayBox1.Location = new System.Drawing.Point(33, 0);
+            this.displayBox1.Name = "displayBox1";
+            this.displayBox1.Size = new System.Drawing.Size(251, 22);
+            this.displayBox1.TabIndex = 1;
+            // 
+            // txtcountry
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.displayBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "txtcountry";
+            this.Size = new System.Drawing.Size(286, 22);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.TextBox textBox1;
+        private Win.UI.DisplayBox displayBox1;
+    }
+}
