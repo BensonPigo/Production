02e1db9487fcf91ba9f09d65d346ed225e420c93
YINMEI <yinmei@sportscity.com.tw>
2015-04-13 09:31:07 +0000

    Add txtpaytermar.cs

---
+++ Sci.Production.Class/txtpaytermar.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sci.Data;
+using Sci.Win.UI;
+
+namespace Sci.Production.Class
+{
+    public partial class txtpaytermar : UserControl
+    {
+        public txtpaytermar()
+        {
+            InitializeComponent();
+        }
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public Sci.Win.UI.TextBox TextBox1
+        {
+            get { return this.textBox1; }
+        }
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public Sci.Win.UI.DisplayBox DisplayBox1
+        {
+            get { return this.displayBox1; }
+        }
+
+        [Bindable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public string TextBox1Binding
+        {
+            set { this.textBox1.Text = value; }
+            get { return textBox1.Text; }
+        }
+
+        [Bindable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public string DisplayBox1Binding
+        {
+            set { this.displayBox1.Text = value; }
+            get { return this.displayBox1.Text; }
+        }
+
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+            string str = this.textBox1.Text;
+            if (!string.IsNullOrWhiteSpace(str) && str != this.textBox1.OldValue)
+            {
+                if (!myUtility.Seek(str, "PayTermAR", "ID"))
+                {
+                    MessageBox.Show(string.Format("< Pay Term: {0} > not found!!!", str));
+                    this.textBox1.Text = "";
+                    e.Cancel = true;
+                    return;
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            this.displayBox1.Text = myUtility.Lookup("Description", this.textBox1.Text.ToString(), "PayTermAR", "ID");
+        }
+
+        private void textBox1_PopUp(object sender, TextBoxPopUpEventArgs e)
+        {
+            Sci.Win.Tools.SelectItem item = new Sci.Win.Tools.SelectItem("PayTermAR.Id,Description", "10,100", this.textBox1.Text);
+            DialogResult result = item.ShowDialog();
+            if (result == DialogResult.Cancel) { return; }
+            this.textBox1.Text = item.GetSelectedString();
+        }
+    }
+}
