bec9855f4c90d5db074167a9318a3d10dfe425fe
YINMEI <yinmei@sportscity.com.tw>
2015-04-14 02:06:13 +0000

    Add txtsupplier.cs

---
+++ Sci.Production.Class/txtsupplier.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sci.Data;
+using Sci.Win.UI;
+
+namespace Sci.Production.Class
+{
+    public partial class txtsupplier : UserControl
+    {
+        public txtsupplier()
+        {
+            InitializeComponent();
+        }
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public Sci.Win.UI.TextBox TextBox1
+        {
+            get { return this.textBox1; }
+        }
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public Sci.Win.UI.DisplayBox DisplayBox1
+        {
+            get { return this.displayBox1; }
+        }
+
+        [Bindable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public string TextBox1Binding
+        {
+            set { this.textBox1.Text = value; }
+            get { return textBox1.Text; }
+        }
+
+        [Bindable(true)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public string DisplayBox1Binding
+        {
+            set { this.displayBox1.Text = value; }
+            get { return this.displayBox1.Text; }
+        }
+
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+            string str = this.textBox1.Text;
+            if (!string.IsNullOrWhiteSpace(str) && str != this.textBox1.OldValue)
+            {
+                if (!myUtility.Seek(str, "Supp", "ID"))
+                {
+                    MessageBox.Show(string.Format("< Supplier Code: {0} > not found!!!", str));
+                    this.textBox1.Text = "";
+                    e.Cancel = true;
+                    return;
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            this.displayBox1.Text = myUtility.Lookup("AbbEN", this.textBox1.Text.ToString(), "Supp", "ID");
+        }
+
+        private void textBox1_PopUp(object sender, Win.UI.TextBoxPopUpEventArgs e)
+        {
+            Sci.Win.Tools.SelectItem item = new Sci.Win.Tools.SelectItem("Supp.ID,AbbCH, AbbEN", "8,30,30", this.textBox1.Text);
+            DialogResult result = item.ShowDialog();
+            if (result == DialogResult.Cancel) { return; }
+            this.textBox1.Text = item.GetSelectedString();
+        }
+    }
+}
