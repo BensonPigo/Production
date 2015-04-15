a8ecb34b209de692ff990fbf71ba84619c5baad4
MIKE <mike@sportscity.com.tw>
2015-04-15 14:21:50 +0000

    add txtsubprocess class object

---
+++ Sci.Production.Class/txtsubprocess.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sci.Win.UI;
+using Sci.Data;
+
+namespace Sci.Production.Class
+{
+    public partial class txtsubprocess : Sci.Win.UI.ComboBox
+    {
+        public txtsubprocess()
+        {
+            string sqlCMD = "Select ID, ArtworkTypeId From Subprocess  where junk !=1 and IsProcess=1 Order By ID";
+            Ict.DualResult cbResult;
+            DataTable SubprocessTable = new DataTable();
+            if (cbResult = DBProxy.Current.Select(null, sqlCMD, out SubprocessTable))
+            {
+                this.DataSource = SubprocessTable;
+                this.DisplayMember = "ArtworkTypeId";
+                this.ValueMember = "ID";
+                this.Size = new System.Drawing.Size(20, 80);
+            }
+        }
+    }
+}
