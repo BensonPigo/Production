fb5c0045ba545150ded8cd49045fc1ef89b1458d
MIKE <mike@sportscity.com.tw>
2015-04-16 13:01:59 +0000

    add txtwhseReason class

---
+++ Sci.Production.Class/txtwhseReason.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sci.Production.Class
+{
+    public partial class txtwhseReason : UserControl
+    {
+        public txtwhseReason()
+        {
+            InitializeComponent();
+        }
+
+        [Category("Custom Properties")]
+        public string Type { set; get ; }
+
+
+        public Sci.Win.UI.TextBox TextBox1
+        {
+            get { return this.textBox1; }
+        }
+
+         public Sci.Win.UI.DisplayBox DisplayBox1
+        {
+            get { return this.displayBox1; }
+        }
+
+        [Bindable(true)]
+        public string TextBox1Binding
+        {
+            set { this.textBox1.Text = value; }
+            get { return textBox1.Text; }
+        }
+
+        [Bindable(true)]
+        public string DisplayBox1Binding
+        {
+            set { this.displayBox1.Text = value; }
+            get { return this.displayBox1.Text; }
+        }
+
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+            string str = this.textBox1.Text;
+            if (!string.IsNullOrWhiteSpace(str) && str != this.textBox1.OldValue)
+            {
+                if (!myUtility.Seek(str, "WhseReason", "ID"))
+                {
+                    MessageBox.Show(string.Format("< Reason: {0} > not found!!!", str));
+                    this.textBox1.Text = "";
+                    e.Cancel = true;
+                    return;
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            this.displayBox1.Text = myUtility.Lookup("Description", this.textBox1.Text.ToString(), "WhseReason", "ID");
+        }
+
+        private void textBox1_PopUp(object sender, Win.UI.TextBoxPopUpEventArgs e)
+        {
+            Sci.Win.Tools.SelectItem item = new Sci.Win.Tools.SelectItem("WhseReason.Id, Description", "10,100", this.textBox1.Text);
+            DialogResult result = item.ShowDialog();
+            if (result == DialogResult.Cancel) { return; }
+            this.textBox1.Text = item.GetSelectedString();
+        }
+    }
+}
