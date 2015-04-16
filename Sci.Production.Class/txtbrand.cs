f54639cd81e1633ac91bbd021e0c336899f8bd3d
MIKE <mike@sportscity.com.tw>
2015-04-16 02:49:05 +0000

    add txtbrand class

---
+++ Sci.Production.Class/txtbrand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sci.Win.UI;
+using Sci.Data;
+using System.Web;
+
+namespace Sci.Production.Class
+{
+    public partial class txtbrand : Sci.Win.UI.TextBox
+    {
+
+        protected override void OnPopUp(TextBoxPopUpEventArgs e)
+        {
+            Sci.Win.Tools.SelectItem item = new Sci.Win.Tools.SelectItem("Brand.id,NameCH,NameEN", "10,50,50", this.Text, false, ",");
+            // SELECT Id,NameCH,NameEN FROM Brand WHERE Junk=0  ORDER BY Id
+            DialogResult result = item.ShowDialog();
+            if (result == DialogResult.Cancel) { return; }
+            this.Text = item.GetSelectedString();
+        }
+
+        protected override void OnValidating(CancelEventArgs e)
+        {
+            string str = this.Text;
+            if (!string.IsNullOrWhiteSpace(str) && str != this.OldValue)
+            {
+                if (myUtility.Seek(str, "Brand", "id") == false)
+                {
+                    MessageBox.Show(string.Format("< Brand : {0} > not found!!!", str));
+                    this.Text = "";
+                    e.Cancel = true;
+                    return;
+                }
+            }
+        }
+
+        public txtbrand()
+        {
+
+        }
+    }
+}
