64e1c55d74b81fb399c59551bbd0423440600361
YINMEI <yinmei@sportscity.com.tw>
2015-04-27 10:08:29 +0000

    Add B03

---
+++ Sci.Production.Basic/B03.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Basic
+{
+    public partial class B03 : Sci.Win.Tems.Input1
+    {
+        public B03(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnEditAfter()
+        {
+            base.OnEditAfter();
+            this.textBox1.ReadOnly = true;
+        }
+
+        protected override bool OnSaveBefore()
+        {
+            if (String.IsNullOrWhiteSpace(CurrentMaintain["ID"].ToString()))
+            {
+                MessageBox.Show("< Code > can not be empty!");
+                return false;
+            }
+
+            if (String.IsNullOrWhiteSpace(CurrentMaintain["Name"].ToString()))
+            {
+                MessageBox.Show("< Name > can not be empty!");
+                return false;
+            }
+
+            return base.OnSaveBefore();
+        }
+    }
+}
