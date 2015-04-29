7f23c8296d85bc4cf45347156ea01e72cd74f4b7
YINMEI <yinmei@sportscity.com.tw>
2015-04-29 10:54:20 +0000

    補入存檔檢察 Country按右鍵功能尚未完成

--- Sci.Production.Basic/B04_BankData.cs
+++ Sci.Production.Basic/B04_BankData.cs
@@ -16,6 +16,7 @@ namespace Sci.Production.Basic
         {
             InitializeComponent();
             DoForm = new B04_BankData_Input();
+            this.Text = "Bank data (" + this.KeyValue1.Trim() + ")";
         }
 
         protected override bool OnGridSetup()
@@ -23,20 +24,20 @@ namespace Sci.Production.Basic
             this.grid.IsEditingReadOnly = false;
             Helper.Controls.Grid.Generator(this.grid)
                 .CheckBox("IsDefault", header: "Default", width: Widths.AnsiChars(3), iseditable: true, trueValue: 1, falseValue: 0)
-                .Text("AccountNo", header: "Account No.", width: Widths.AnsiChars(30))
+                .Text("AccountNo", header: "Account No.", width: Widths.AnsiChars(20))
                 .Text("SWIFTCode", header: "SWIFT", width: Widths.AnsiChars(11))
-                .Text("AccountName", header: "Account Name", width: Widths.AnsiChars(30))
-                .Text("BankName", header: "Bank Name", width: Widths.AnsiChars(30))
+                .Text("AccountName", header: "Account Name", width: Widths.AnsiChars(20))
+                .Text("BankName", header: "Bank Name", width: Widths.AnsiChars(20))
                 .Text("CountryID", header: "Country", width: Widths.AnsiChars(2))
                 .Text("CountryName", header: "Country Name", width: Widths.AnsiChars(10),iseditable:false)
                 .Text("City", header: "City", width: Widths.AnsiChars(20))
-                .Text("MidBankName", header: "Intermediary Bank", width: Widths.AnsiChars(30))
+                .Text("MidBankName", header: "Intermediary Bank", width: Widths.AnsiChars(20))
                 .Text("MidSWIFTCode", header: "Intermediary Bank-SWIFT Code", width: Widths.AnsiChars(11))
                 .Text("Remark", header: "Remark", width: Widths.AnsiChars(10))
-                .Text("AddName", header: "Create by", width: Widths.AnsiChars(10))
-                .DateTime("AddDate", header: "Create at")
-                .Text("EditName", header: "Edit by", width: Widths.AnsiChars(10))
-                .DateTime("EditDate", header: "Edit at");
+                .Text("AddName", header: "Create by", width: Widths.AnsiChars(10), iseditable: false)
+                .DateTime("AddDate", header: "Create at", iseditable: false)
+                .Text("EditName", header: "Edit by", width: Widths.AnsiChars(10), iseditable: false)
+                .DateTime("EditDate", header: "Edit at", iseditable: false);
             return true;
         }
 
@@ -48,6 +49,52 @@ namespace Sci.Production.Basic
             {
                 gridData["CountryName"] = myUtility.Lookup("NameEN", gridData["CountryID"].ToString(), "Country", "ID");
             }
+        }
+
+        protected override bool DoSave()
+        {
+            int defaultCount = 0;
+
+            foreach (DataRow gridData in Datas)
+            {
+                if (string.IsNullOrWhiteSpace(gridData["AccountNo"].ToString()))
+                {
+                    MessageBox.Show("The field < Account No. > can not be empty!");
+                    return false;
+                }
+
+                if (string.IsNullOrWhiteSpace(gridData["AccountName"].ToString()))
+                {
+                    MessageBox.Show("The field < Account Name > can not be empty!");
+                    return false;
+                }
+
+                if (string.IsNullOrWhiteSpace(gridData["BankName"].ToString()))
+                {
+                    MessageBox.Show("The field < Bank Name > can not be empty!");
+                    return false;
+                }
+
+                if (string.IsNullOrWhiteSpace(gridData["CountryID"].ToString()))
+                {
+                    MessageBox.Show("The field < Country > can not be empty!");
+                    return false;
+                }
+
+
+                if (gridData["IsDefault"].ToString() == "True" )
+                {
+                    defaultCount = defaultCount + 1;
+                }
+            }
+
+            if (defaultCount != 1)
+            {
+                MessageBox.Show("The field < Default > can only collude and select one!");
+                return false;
+            }
+
+            return base.DoSave();
         }
     }
 }
