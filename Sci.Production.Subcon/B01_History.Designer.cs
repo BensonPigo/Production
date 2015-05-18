3490c2af01fb1b4f986824c804fe28d4cf761091
MIKE <mike@sportscity.com.tw>
2015-05-18 06:35:50 +0000

    add form

---
+++ Sci.Production.Subcon/B01_History.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Sci.Production.Subcon
+{
+    partial class B01_History
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.displayBox1 = new Sci.Win.UI.DisplayBox();
+            this.displayBox2 = new Sci.Win.UI.DisplayBox();
+            this.grid1 = new Sci.Win.UI.Grid();
+            this.label1 = new Sci.Win.UI.Label();
+            this.button1 = new Sci.Win.UI.Button();
+            this.label2 = new Sci.Win.UI.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // displayBox1
+            // 
+            this.displayBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.displayBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.displayBox1.Location = new System.Drawing.Point(87, 9);
+            this.displayBox1.Name = "displayBox1";
+            this.displayBox1.Size = new System.Drawing.Size(197, 23);
+            this.displayBox1.TabIndex = 0;
+            // 
+            // displayBox2
+            // 
+            this.displayBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.displayBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.displayBox2.Location = new System.Drawing.Point(405, 9);
+            this.displayBox2.Name = "displayBox2";
+            this.displayBox2.Size = new System.Drawing.Size(486, 23);
+            this.displayBox2.TabIndex = 1;
+            // 
+            // grid1
+            // 
+            this.grid1.AllowUserToAddRows = false;
+            this.grid1.AllowUserToDeleteRows = false;
+            this.grid1.AllowUserToResizeRows = false;
+            this.grid1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.grid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid1.EditingEnter = Ict.Win.UI.DataGridViewEditingEnter.NextCellOrNextRow;
+            this.grid1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.grid1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.grid1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(162)))), ((int)(((byte)(163)))));
+            this.grid1.Location = new System.Drawing.Point(12, 41);
+            this.grid1.Name = "grid1";
+            this.grid1.RowHeadersVisible = false;
+            this.grid1.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.grid1.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.grid1.RowTemplate.Height = 24;
+            this.grid1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grid1.Size = new System.Drawing.Size(938, 470);
+            this.grid1.TabIndex = 2;
+            this.grid1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Refno";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button1.Location = new System.Drawing.Point(870, 519);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 30);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(327, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Description";
+            // 
+            // B01_History
+            // 
+            this.ClientSize = new System.Drawing.Size(962, 561);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.grid1);
+            this.Controls.Add(this.displayBox2);
+            this.Controls.Add(this.displayBox1);
+            this.Name = "B01_History";
+            this.Text = "Payment History";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private Win.UI.DisplayBox displayBox1;
+        private Win.UI.DisplayBox displayBox2;
+        private Win.UI.Grid grid1;
+        private Win.UI.Label label1;
+        private Win.UI.Button button1;
+        private Win.UI.Label label2;
+    }
+}
---
+++ Sci.Production.Subcon/B01_History.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict;
+using Ict.Win;
+using Sci.Data;
+
+namespace Sci.Production.Subcon
+{
+    public partial class B01_History : Sci.Win.Subs.Base
+    {
+        protected DataRow motherData;
+        public B01_History(DataRow data)
+        {
+            InitializeComponent();
+            this.motherData = data;
+            this.displayBox1.Text = motherData["refno"].ToString();
+            this.displayBox2.Text = motherData["description"].ToString();
+        }
+
+        protected override void OnFormLoaded()
+        {
+            base.OnFormLoaded();
+            string selectCommand1 = string.Format("select issuedate,localap.id,qty,unitid,currencyid, price, qty*price amount"+
+                ",localsuppid,localsupp.abb from localap, localap_detail,localsupp "+
+                "where refno = '{0}' and localap.id = localap_detail.id and localsuppid = localsupp.id", this.motherData["refno"].ToString());
+            DataTable selectDataTable1;
+
+            DualResult selectResult1 = DBProxy.Current.Select(null, selectCommand1, out selectDataTable1);
+            bindingSource1.DataSource = selectDataTable1;
+
+            //設定Grid1的顯示欄位
+            this.grid1.IsEditingReadOnly = true;
+            this.grid1.DataSource = bindingSource1;
+            Helper.Controls.Grid.Generator(this.grid1)
+                 .Text("issuedate", header: "A/P Date", width: Widths.AnsiChars(10))
+                 .Text("id", header: "A/P No", width: Widths.AnsiChars(13))
+                 .Numeric("Qty", header: "Qty", width: Widths.AnsiChars(6),integer_places:6,decimal_places:2)
+                 .Text("Unitid", header: "Unit", width: Widths.AnsiChars(8))
+                 .Text("Currencyid", header: "Currency", width: Widths.AnsiChars(3))
+                 .Numeric("Price", header: "Price", width: Widths.AnsiChars(6), integer_places: 8, decimal_places: 4)
+                 .Numeric("Amount", header: "Amount", width: Widths.AnsiChars(10), integer_places: 14, decimal_places: 4)
+                 .Text("localsuppid", header: "Supplier", width: Widths.AnsiChars(8))
+                 .Text("abb", header: "Supplier Abb.", width: Widths.AnsiChars(15));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+    }
+}
