641ad24b1cff717172ac1adb0cf8ddeff78e24a6
MIKE <mike@sportscity.com.tw>
2015-05-18 09:48:09 +0000

    ADD B01 RadioPanel的valied太慢，save前沒有走完。

---
+++ Sci.Production.Planning/B01.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace Sci.Production.Planning
+{
+    partial class B01
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.label5 = new Sci.Win.UI.Label();
+            this.label6 = new Sci.Win.UI.Label();
+            this.label7 = new Sci.Win.UI.Label();
+            this.dateBox1 = new Sci.Win.UI.DateBox();
+            this.comboBox1 = new Sci.Win.UI.ComboBox();
+            this.txtsubcon1 = new Sci.Production.Class.txtsubcon();
+            this.numericBox1 = new Sci.Win.UI.NumericBox();
+            this.numericBox2 = new Sci.Win.UI.NumericBox();
+            this.radioPanel1 = new Sci.Win.UI.RadioPanel();
+            this.radioButton2 = new Sci.Win.UI.RadioButton();
+            this.radioButton1 = new Sci.Win.UI.RadioButton();
+            this.label25 = new Sci.Win.UI.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).BeginInit();
+            this.detail.SuspendLayout();
+            this.detailcont.SuspendLayout();
+            this.tabs.SuspendLayout();
+            this.radioPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // detailcont
+            // 
+            this.detailcont.Controls.Add(this.label25);
+            this.detailcont.Controls.Add(this.radioPanel1);
+            this.detailcont.Controls.Add(this.numericBox2);
+            this.detailcont.Controls.Add(this.numericBox1);
+            this.detailcont.Controls.Add(this.txtsubcon1);
+            this.detailcont.Controls.Add(this.comboBox1);
+            this.detailcont.Controls.Add(this.dateBox1);
+            this.detailcont.Controls.Add(this.label7);
+            this.detailcont.Controls.Add(this.label3);
+            this.detailcont.Controls.Add(this.label4);
+            this.detailcont.Controls.Add(this.label5);
+            this.detailcont.Controls.Add(this.label6);
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(16, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Date";
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(16, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 23);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "ArtworkType";
+            // 
+            // label5
+            // 
+            this.label5.Lines = 0;
+            this.label5.Location = new System.Drawing.Point(16, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 23);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Fty / Supp";
+            // 
+            // label6
+            // 
+            this.label6.Lines = 0;
+            this.label6.Location = new System.Drawing.Point(16, 112);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 23);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Capacity (unit)";
+            // 
+            // label7
+            // 
+            this.label7.Lines = 0;
+            this.label7.Location = new System.Drawing.Point(16, 144);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(107, 23);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "# of Heads";
+            // 
+            // dateBox1
+            // 
+            this.dateBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "issuedate", true));
+            this.dateBox1.Location = new System.Drawing.Point(126, 16);
+            this.dateBox1.Name = "dateBox1";
+            this.dateBox1.Size = new System.Drawing.Size(130, 23);
+            this.dateBox1.TabIndex = 1;
+            this.dateBox1.Validated += new System.EventHandler(this.dateBox1_Validated);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.DataBindings.Add(new System.Windows.Forms.Binding("SelectedValue", this.mtbs, "artworktypeid", true));
+            this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IsSupportUnselect = true;
+            this.comboBox1.Location = new System.Drawing.Point(126, 48);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(159, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // txtsubcon1
+            // 
+            this.txtsubcon1.DataBindings.Add(new System.Windows.Forms.Binding("TextBox1Binding", this.mtbs, "ftysupp", true));
+            // 
+            // 
+            // 
+            this.txtsubcon1.DisplayBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.txtsubcon1.DisplayBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.txtsubcon1.DisplayBox1.Location = new System.Drawing.Point(67, 0);
+            this.txtsubcon1.DisplayBox1.Name = "displayBox1";
+            this.txtsubcon1.DisplayBox1.Size = new System.Drawing.Size(90, 23);
+            this.txtsubcon1.DisplayBox1.TabIndex = 1;
+            this.txtsubcon1.IsIncludeJunk = false;
+            this.txtsubcon1.Location = new System.Drawing.Point(126, 80);
+            this.txtsubcon1.Name = "txtsubcon1";
+            this.txtsubcon1.Size = new System.Drawing.Size(159, 23);
+            this.txtsubcon1.TabIndex = 3;
+            // 
+            // 
+            // 
+            this.txtsubcon1.TextBox1.BackColor = System.Drawing.Color.White;
+            this.txtsubcon1.TextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtsubcon1.TextBox1.Location = new System.Drawing.Point(0, 0);
+            this.txtsubcon1.TextBox1.Name = "textBox1";
+            this.txtsubcon1.TextBox1.Size = new System.Drawing.Size(66, 23);
+            this.txtsubcon1.TextBox1.TabIndex = 0;
+            // 
+            // numericBox1
+            // 
+            this.numericBox1.BackColor = System.Drawing.Color.White;
+            this.numericBox1.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "capacity", true));
+            this.numericBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.numericBox1.Location = new System.Drawing.Point(126, 112);
+            this.numericBox1.Maximum = new decimal(new int[] {
+            9999999,
+            0,
+            0,
+            0});
+            this.numericBox1.MaxLength = 7;
+            this.numericBox1.Minimum = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.numericBox1.Name = "numericBox1";
+            this.numericBox1.Size = new System.Drawing.Size(100, 23);
+            this.numericBox1.TabIndex = 4;
+            // 
+            // numericBox2
+            // 
+            this.numericBox2.BackColor = System.Drawing.Color.White;
+            this.numericBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "headsno", true));
+            this.numericBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.numericBox2.Location = new System.Drawing.Point(126, 144);
+            this.numericBox2.Maximum = new decimal(new int[] {
+            99,
+            0,
+            0,
+            0});
+            this.numericBox2.MaxLength = 2;
+            this.numericBox2.Minimum = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.numericBox2.Name = "numericBox2";
+            this.numericBox2.Size = new System.Drawing.Size(100, 23);
+            this.numericBox2.TabIndex = 8;
+            // 
+            // radioPanel1
+            // 
+            this.radioPanel1.Controls.Add(this.radioButton2);
+            this.radioPanel1.Controls.Add(this.radioButton1);
+            this.radioPanel1.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "unit", true));
+            this.radioPanel1.Location = new System.Drawing.Point(247, 112);
+            this.radioPanel1.Name = "radioPanel1";
+            this.radioPanel1.Size = new System.Drawing.Size(229, 31);
+            this.radioPanel1.TabIndex = 5;
+            this.radioPanel1.Validated += new System.EventHandler(this.radioPanel1_Validated);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.radioButton2.Location = new System.Drawing.Point(77, 3);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(84, 21);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "by month";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Value = "2";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.radioButton1.Location = new System.Drawing.Point(3, 2);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(68, 21);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "by day";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Value = "1";
+            // 
+            // label25
+            // 
+            this.label25.BackColor = System.Drawing.Color.Transparent;
+            this.label25.Lines = 0;
+            this.label25.Location = new System.Drawing.Point(313, 49);
+            this.label25.Name = "label25";
+            this.label25.Size = new System.Drawing.Size(326, 23);
+            this.label25.TabIndex = 43;
+            this.label25.TextStyle.Color = System.Drawing.Color.Blue;
+            // 
+            // B01
+            // 
+            this.ClientSize = new System.Drawing.Size(908, 457);
+            this.DefaultControl = "datebox1";
+            this.DefaultControlForEdit = "datebox1";
+            this.DefaultOrder = "ISSUEDATE,ARTWORKTYPEID,FTYSUPP";
+            this.IsSupportPrint = false;
+            this.Name = "B01";
+            this.Text = "B01. Sub-process capacity";
+            this.UniqueExpress = "ISSUEDATE,ARTWORKTYPEID,FTYSUPP";
+            this.WorkAlias = "capacity";
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).EndInit();
+            this.detail.ResumeLayout(false);
+            this.detailcont.ResumeLayout(false);
+            this.detailcont.PerformLayout();
+            this.tabs.ResumeLayout(false);
+            this.radioPanel1.ResumeLayout(false);
+            this.radioPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.RadioPanel radioPanel1;
+        private Win.UI.RadioButton radioButton2;
+        private Win.UI.RadioButton radioButton1;
+        private Win.UI.NumericBox numericBox2;
+        private Win.UI.NumericBox numericBox1;
+        private Class.txtsubcon txtsubcon1;
+        private Win.UI.ComboBox comboBox1;
+        private Win.UI.DateBox dateBox1;
+        private Win.UI.Label label7;
+        private Win.UI.Label label3;
+        private Win.UI.Label label4;
+        private Win.UI.Label label5;
+        private Win.UI.Label label6;
+        private Win.UI.Label label25;
+    }
+}
---
+++ Sci.Production.Planning/B01.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Sci.Win.UI;
+
+namespace Sci.Production.Planning
+{
+    public partial class B01 : Sci.Win.Tems.Input1
+    {
+        public B01(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Value");
+
+            DataRow dr = dt.NewRow();
+            dr[0] = "Laser";
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr[0] = "Embroidery";
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr[0] = "Bonding (Machine)";
+            dt.Rows.Add(dr);
+            dr = dt.NewRow();
+            dr[0] = "Bonding (Hand)";
+            dt.Rows.Add(dr);
+
+            this.comboBox1.DataSource = dt;
+            this.comboBox1.DisplayMember = "Value";
+            this.comboBox1.ValueMember = "Value";
+        }
+        //新增資料預設
+        protected override void OnNewAfter()
+        {
+            base.OnNewAfter();
+            this.CurrentMaintain["unit"] = 1;
+        }
+        //存檔前檢查
+        protected override bool OnSaveBefore()
+        {
+            if (String.IsNullOrWhiteSpace(CurrentMaintain["issuedate"].ToString()))
+            {
+                MessageBox.Show("< Date > can not be empty!");
+                this.dateBox1.Focus();
+                return false;
+            }
+
+            if (String.IsNullOrWhiteSpace(CurrentMaintain["artworktypeid"].ToString()))
+            {
+                MessageBox.Show("< Artwork Type > can not be empty!");
+                this.comboBox1.Focus();
+                return false;
+            }
+
+            if (String.IsNullOrWhiteSpace(CurrentMaintain["ftysupp"].ToString()))
+            {
+                MessageBox.Show("< Fty / Supp > can not be empty!");
+                this.txtsubcon1.Focus();
+                return false;
+            }
+
+            if (String.IsNullOrWhiteSpace(CurrentMaintain["headsno"].ToString()) && CurrentMaintain["artworktypeid"].ToString().TrimEnd()=="Embroidery")
+            {
+                MessageBox.Show("< # of Heads > can not be empty!");
+                this.numericBox2.Focus();
+                return false;
+            }
+
+            return base.OnSaveBefore();
+        }
+
+        //combo下拉控制其它物件
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (null == this.comboBox1.SelectedValue) return;
+            switch(this.comboBox1.SelectedValue.ToString().TrimEnd())
+            {
+                case "Bonding (Hand)":
+                    this.label6.Text = "Capacity (Person):";
+                    this.label25.Text = "(Calculate by employee)";
+                    break;
+                case "Embroidery":
+                    this.label7.Visible = true;
+                    this.numericBox2.Visible = true;
+                    break;
+                default:
+                    this.label6.Text = "Capacity (Unit)";
+                    this.label25.Text = "(Calculate by machine)";
+                    this.label7.Visible = false;
+                    this.numericBox2.Visible = false;
+                    break;
+
+            }
+
+        }
+
+        //refresh
+        protected override void OnDetailEntered()
+        {
+            base.OnDetailEntered();
+            switch (CurrentMaintain["artworktypeid"].ToString().TrimEnd())
+            {
+                case "Bonding (Hand)":
+                    this.label6.Text = "Capacity (Person):";
+                    this.label25.Text = "(Calculate by employee)";
+                    break;
+                case "Embroidery":
+                    this.label7.Visible = true;
+                    this.numericBox2.Visible = true;
+                    break;
+                default:
+                    this.label6.Text = "Capacity (Unit)";
+                    this.label25.Text = "(Calculate by machine)";
+                    this.label7.Visible = false;
+                    this.numericBox2.Visible = false;
+                    break;
+
+            }
+        }
+
+        private void dateBox1_Validated(object sender, EventArgs e)
+        {
+            if (CurrentMaintain["unit"].ToString() == "2")
+            {
+                int yy = DateTime.Parse(CurrentMaintain["issuedate"].ToString()).Year;
+                int mm = DateTime.Parse(CurrentMaintain["issuedate"].ToString()).Month;
+                CurrentMaintain["issuedate"] = DateTime.Parse(yy.ToString()+"/"+mm.ToString()+"/1");
+            }
+        }
+
+        private void radioPanel1_Validated(object sender, EventArgs e)
+        {
+            Sci.Win.UI.RadioPanel rdoG = (RadioPanel)sender;
+            if (rdoG.Value == "2")
+            {
+                int yy = DateTime.Parse(CurrentMaintain["issuedate"].ToString()).Year;
+                int mm = DateTime.Parse(CurrentMaintain["issuedate"].ToString()).Month;
+                CurrentMaintain["issuedate"] = DateTime.Parse(yy.ToString() + "/" + mm.ToString() + "/1");
+            }
+        }
+
+
+
+    }
+}
