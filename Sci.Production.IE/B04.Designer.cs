0d1d3810b534e450fb5cf9b369af81e2c4951217
YINMEI <yinmei@sportscity.com.tw>
2015-05-18 06:12:12 +0000

    Add B04

---
+++ Sci.Production.IE/B04.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Sci.Production.IE
+{
+    partial class B04
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new Sci.Win.UI.CheckBox();
+            this.label4 = new Sci.Win.UI.Label();
+            this.label3 = new Sci.Win.UI.Label();
+            this.textBox2 = new Sci.Win.UI.TextBox();
+            this.textBox1 = new Sci.Win.UI.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).BeginInit();
+            this.detail.SuspendLayout();
+            this.detailcont.SuspendLayout();
+            this.tabs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // detail
+            // 
+            this.detail.Size = new System.Drawing.Size(681, 395);
+            // 
+            // detailcont
+            // 
+            this.detailcont.Controls.Add(this.checkBox1);
+            this.detailcont.Controls.Add(this.label4);
+            this.detailcont.Controls.Add(this.label3);
+            this.detailcont.Controls.Add(this.textBox2);
+            this.detailcont.Controls.Add(this.textBox1);
+            this.detailcont.Size = new System.Drawing.Size(681, 357);
+            // 
+            // detailbtm
+            // 
+            this.detailbtm.Size = new System.Drawing.Size(681, 38);
+            // 
+            // tabs
+            // 
+            this.tabs.Size = new System.Drawing.Size(689, 424);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.DataBindings.Add(new System.Windows.Forms.Binding("Checked", this.mtbs, "Junk", true));
+            this.checkBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.checkBox1.Location = new System.Drawing.Point(349, 44);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(57, 21);
+            this.checkBox1.TabIndex = 17;
+            this.checkBox1.Text = "Junk";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(70, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 23);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Problem";
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(70, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 23);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "ID";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.CharacterCasing = System.Windows.Forms.CharacterCasing.Normal;
+            this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "Description", true));
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox2.Location = new System.Drawing.Point(137, 96);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(380, 23);
+            this.textBox2.TabIndex = 16;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "ID", true));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(137, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(50, 23);
+            this.textBox1.TabIndex = 15;
+            // 
+            // B04
+            // 
+            this.ClientSize = new System.Drawing.Size(689, 457);
+            this.DefaultFilter = "Type = \'CP\'";
+            this.DefaultOrder = "ID";
+            this.IsSupportCopy = false;
+            this.IsSupportDelete = false;
+            this.IsSupportPrint = false;
+            this.Name = "B04";
+            this.Text = "B04. Changeover Problem";
+            this.UniqueExpress = "Type,ID";
+            this.WorkAlias = "IEReason";
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).EndInit();
+            this.detail.ResumeLayout(false);
+            this.detailcont.ResumeLayout(false);
+            this.detailcont.PerformLayout();
+            this.tabs.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.CheckBox checkBox1;
+        private Win.UI.Label label4;
+        private Win.UI.Label label3;
+        private Win.UI.TextBox textBox2;
+        private Win.UI.TextBox textBox1;
+    }
+}
---
+++ Sci.Production.IE/B04.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.IE
+{
+    public partial class B04 : Sci.Win.Tems.Input1
+    {
+        public B04(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNewAfter()
+        {
+            base.OnNewAfter();
+            CurrentMaintain["Type"] = "CP";
+        }
+
+        protected override void OnEditAfter()
+        {
+            base.OnEditAfter();
+            this.textBox1.ReadOnly = true;
+        }
+
+        protected override bool OnSaveBefore()
+        {
+            if (String.IsNullOrWhiteSpace(CurrentMaintain["ID"].ToString()))
+            {
+                MessageBox.Show("< ID > can not be empty!");
+                this.textBox1.Focus();
+                return false;
+            }
+
+            if (String.IsNullOrWhiteSpace(CurrentMaintain["Description"].ToString()))
+            {
+                MessageBox.Show("< Problem > can not be empty!");
+                this.textBox2.Focus();
+                return false;
+            }
+            return base.OnSaveBefore();
+        }
+    }
+}
