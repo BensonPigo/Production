1093ac2b219e94c7741a0f4b44f01bd2b625e39e
YINMEI <yinmei@sportscity.com.tw>
2015-05-18 04:17:32 +0000

    新增B02 新增存檔會出現錯誤訊息 複製存檔時會出現ID重複問題(ID欄位為identity)

---
+++ Sci.Production.IE/B02.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Sci.Production.IE
+{
+    partial class B02
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.label5 = new Sci.Win.UI.Label();
+            this.label6 = new Sci.Win.UI.Label();
+            this.dateBox1 = new Sci.Win.UI.DateBox();
+            this.numericBox1 = new Sci.Win.UI.NumericBox();
+            this.comboBox1 = new Sci.Win.UI.ComboBox();
+            this.txtfactory1 = new Sci.Production.Class.txtfactory();
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).BeginInit();
+            this.detail.SuspendLayout();
+            this.detailcont.SuspendLayout();
+            this.tabs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // detail
+            // 
+            this.detail.Size = new System.Drawing.Size(682, 395);
+            // 
+            // detailcont
+            // 
+            this.detailcont.Controls.Add(this.txtfactory1);
+            this.detailcont.Controls.Add(this.comboBox1);
+            this.detailcont.Controls.Add(this.numericBox1);
+            this.detailcont.Controls.Add(this.dateBox1);
+            this.detailcont.Controls.Add(this.label6);
+            this.detailcont.Controls.Add(this.label5);
+            this.detailcont.Controls.Add(this.label4);
+            this.detailcont.Controls.Add(this.label3);
+            this.detailcont.Size = new System.Drawing.Size(682, 357);
+            // 
+            // detailbtm
+            // 
+            this.detailbtm.Size = new System.Drawing.Size(682, 38);
+            // 
+            // browse
+            // 
+            this.browse.Size = new System.Drawing.Size(682, 395);
+            // 
+            // tabs
+            // 
+            this.tabs.Size = new System.Drawing.Size(690, 424);
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(70, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 23);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Date";
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(70, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 23);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Type";
+            // 
+            // label5
+            // 
+            this.label5.Lines = 0;
+            this.label5.Location = new System.Drawing.Point(70, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(85, 23);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Factory";
+            // 
+            // label6
+            // 
+            this.label6.Lines = 0;
+            this.label6.Location = new System.Drawing.Point(70, 180);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(85, 23);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Target";
+            // 
+            // dateBox1
+            // 
+            this.dateBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "EffectiveDate", true));
+            this.dateBox1.Location = new System.Drawing.Point(161, 37);
+            this.dateBox1.Name = "dateBox1";
+            this.dateBox1.Size = new System.Drawing.Size(130, 23);
+            this.dateBox1.TabIndex = 10;
+            // 
+            // numericBox1
+            // 
+            this.numericBox1.BackColor = System.Drawing.Color.White;
+            this.numericBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "Target", true));
+            this.numericBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.numericBox1.Location = new System.Drawing.Point(161, 180);
+            this.numericBox1.Name = "numericBox1";
+            this.numericBox1.Size = new System.Drawing.Size(100, 23);
+            this.numericBox1.TabIndex = 13;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.DataBindings.Add(new System.Windows.Forms.Binding("SelectedValue", this.mtbs, "Type", true));
+            this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IsSupportUnselect = true;
+            this.comboBox1.Location = new System.Drawing.Point(161, 84);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(188, 24);
+            this.comboBox1.TabIndex = 11;
+            this.comboBox1.Validated += new System.EventHandler(this.comboBox1_Validated);
+            // 
+            // txtfactory1
+            // 
+            this.txtfactory1.BackColor = System.Drawing.Color.White;
+            this.txtfactory1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "FactoryID", true));
+            this.txtfactory1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtfactory1.Location = new System.Drawing.Point(161, 133);
+            this.txtfactory1.Name = "txtfactory1";
+            this.txtfactory1.Size = new System.Drawing.Size(66, 23);
+            this.txtfactory1.TabIndex = 16;
+            // 
+            // B02
+            // 
+            this.ClientSize = new System.Drawing.Size(690, 457);
+            this.DefaultOrder = "EffectiveDate";
+            this.IsSupportPrint = false;
+            this.Name = "B02";
+            this.Text = "B02. Lean Measurement Target Setting";
+            this.UniqueExpress = "ID";
+            this.WorkAlias = "ChgOverTarget";
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).EndInit();
+            this.detail.ResumeLayout(false);
+            this.detailcont.ResumeLayout(false);
+            this.detailcont.PerformLayout();
+            this.tabs.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.NumericBox numericBox1;
+        private Win.UI.DateBox dateBox1;
+        private Win.UI.Label label6;
+        private Win.UI.Label label5;
+        private Win.UI.Label label4;
+        private Win.UI.Label label3;
+        private Win.UI.ComboBox comboBox1;
+        private Class.txtfactory txtfactory1;
+    }
+}
---
+++ Sci.Production.IE/B02.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.IE
+{
+    public partial class B02 : Sci.Win.Tems.Input1
+    {
+        public B02(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnFormLoaded()
+        {
+            base.OnFormLoaded();
+
+            Dictionary<String, String> comboBox1_RowSource = new Dictionary<string, string>();
+            comboBox1_RowSource.Add("LBR  ", "Line Balancing (%)");
+            comboBox1_RowSource.Add("LLER ", "Lean Line Eff. (%)");
+            comboBox1_RowSource.Add("EFF. ", "Efficiency");
+            comboBox1_RowSource.Add("COPT ", "Changeover Process Time");
+            comboBox1_RowSource.Add("COT  ", "Changeover Time");
+            comboBox1.DataSource = new BindingSource(comboBox1_RowSource, null);
+            comboBox1.ValueMember = "Key";
+            comboBox1.DisplayMember = "Value";
+        }
+
+        protected override void OnDetailEntered()
+        {
+            base.OnDetailEntered();
+            if ((CurrentMaintain["Type"].ToString() == "COPT " || CurrentMaintain["Type"].ToString() == "COT  "))
+            {
+                this.label6.Text = "Target (min)";
+            }
+            else
+            {
+                this.label6.Text = "Target (%)";
+            }
+        }
+
+        protected override void OnNewAfter()
+        {
+            base.OnNewAfter();
+            CurrentMaintain["EffectiveDate"] = DateTime.Today;
+            CurrentMaintain["FactoryID"] = Sci.Env.User.Factory;
+        }
+
+        protected override void OnEditAfter()
+        {
+            base.OnEditAfter();
+            this.dateBox1.ReadOnly = true;
+            this.comboBox1.ReadOnly = true;
+            this.txtfactory1.ReadOnly = true;
+        }
+
+        protected override bool OnSaveBefore()
+        {
+            if (String.IsNullOrWhiteSpace(CurrentMaintain["EffectiveDate"].ToString()))
+            {
+                MessageBox.Show("< Date > can not be empty!");
+                this.dateBox1.Focus();
+                return false;
+            }
+
+            if (String.IsNullOrWhiteSpace(CurrentMaintain["Type"].ToString()))
+            {
+                MessageBox.Show("< Type > can not be empty!");
+                this.comboBox1.Focus();
+                return false;
+            }
+
+            if (CurrentMaintain["Type"].ToString() != "COPT " && CurrentMaintain["Type"].ToString() != "COT  ")
+            {
+                if (String.IsNullOrWhiteSpace(CurrentMaintain["FactoryID"].ToString()))
+                {
+                    MessageBox.Show("< Factory > can not be empty!");
+                    this.txtfactory1.Focus();
+                    return false;
+                }
+            }
+
+            DateTime effectiveDate = (DateTime)CurrentMaintain["EffectiveDate"];
+            string effectiveDateToString = effectiveDate.ToShortDateString();
+            string selectCommand = string.Format("select ID from ChgOverTarget where EffectiveDate = '{0}' and FactoryID = '{1}' and Type = '{2}'", effectiveDateToString, CurrentMaintain["FactoryID"].ToString(), CurrentMaintain["Type"].ToString());
+            if (myUtility.Seek(selectCommand, null))
+            {
+                MessageBox.Show(string.Format("Data is Duplicate!!"));
+                return false;
+            }
+
+            return base.OnSaveBefore();
+        }
+
+        private void comboBox1_Validated(object sender, EventArgs e)
+        {
+            if (this.EditMode)
+            {
+                if ((CurrentMaintain["Type"].ToString() == "COPT " || CurrentMaintain["Type"].ToString() == "COT  "))
+                {
+                    this.txtfactory1.ReadOnly = true;
+                    CurrentMaintain["FactoryID"] = "";
+                    this.label6.Text = "Target (min)";
+                }
+                else
+                {
+                    this.txtfactory1.ReadOnly = false;
+                    this.label6.Text = "Target (%)";
+                }
+            }
+        }
+    }
+}
