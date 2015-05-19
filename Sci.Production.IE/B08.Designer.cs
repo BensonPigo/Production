9dfa6cb076f0e99ba3efd439910d30fa911cfb15
YINMEI <yinmei@sportscity.com.tw>
2015-05-19 06:24:48 +0000

    Add B08, Import from excel與Print功能尚未完成

---
+++ Sci.Production.IE/B08.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Sci.Production.IE
+{
+    partial class B08
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.label5 = new Sci.Win.UI.Label();
+            this.label6 = new Sci.Win.UI.Label();
+            this.label7 = new Sci.Win.UI.Label();
+            this.label8 = new Sci.Win.UI.Label();
+            this.label9 = new Sci.Win.UI.Label();
+            this.displayBox1 = new Sci.Win.UI.DisplayBox();
+            this.textBox1 = new Sci.Win.UI.TextBox();
+            this.textBox2 = new Sci.Win.UI.TextBox();
+            this.textBox3 = new Sci.Win.UI.TextBox();
+            this.dateBox1 = new Sci.Win.UI.DateBox();
+            this.dateBox2 = new Sci.Win.UI.DateBox();
+            this.button1 = new Sci.Win.UI.Button();
+            this.txtsewingline1 = new Sci.Production.Class.txtsewingline();
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).BeginInit();
+            this.detail.SuspendLayout();
+            this.detailcont.SuspendLayout();
+            this.tabs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // detail
+            // 
+            this.detail.Size = new System.Drawing.Size(714, 395);
+            // 
+            // detailcont
+            // 
+            this.detailcont.Controls.Add(this.txtsewingline1);
+            this.detailcont.Controls.Add(this.dateBox2);
+            this.detailcont.Controls.Add(this.dateBox1);
+            this.detailcont.Controls.Add(this.textBox3);
+            this.detailcont.Controls.Add(this.textBox2);
+            this.detailcont.Controls.Add(this.textBox1);
+            this.detailcont.Controls.Add(this.displayBox1);
+            this.detailcont.Controls.Add(this.label9);
+            this.detailcont.Controls.Add(this.label8);
+            this.detailcont.Controls.Add(this.label7);
+            this.detailcont.Controls.Add(this.label6);
+            this.detailcont.Controls.Add(this.label5);
+            this.detailcont.Controls.Add(this.label4);
+            this.detailcont.Controls.Add(this.label3);
+            this.detailcont.Size = new System.Drawing.Size(714, 357);
+            // 
+            // detailbtm
+            // 
+            this.detailbtm.Size = new System.Drawing.Size(714, 38);
+            // 
+            // browse
+            // 
+            this.browse.Size = new System.Drawing.Size(714, 395);
+            // 
+            // tabs
+            // 
+            this.tabs.Size = new System.Drawing.Size(722, 424);
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(53, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 23);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Factory";
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(53, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 23);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Employee#";
+            // 
+            // label5
+            // 
+            this.label5.Lines = 0;
+            this.label5.Location = new System.Drawing.Point(53, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 23);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Nick Name";
+            // 
+            // label6
+            // 
+            this.label6.Lines = 0;
+            this.label6.Location = new System.Drawing.Point(53, 182);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 23);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Skill";
+            // 
+            // label7
+            // 
+            this.label7.Lines = 0;
+            this.label7.Location = new System.Drawing.Point(400, 32);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 23);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Hired On";
+            // 
+            // label8
+            // 
+            this.label8.Lines = 0;
+            this.label8.Location = new System.Drawing.Point(400, 81);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(75, 23);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "Resigned";
+            // 
+            // label9
+            // 
+            this.label9.Lines = 0;
+            this.label9.Location = new System.Drawing.Point(400, 132);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(75, 23);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "Line";
+            // 
+            // displayBox1
+            // 
+            this.displayBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.displayBox1.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "FactoryID", true));
+            this.displayBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.displayBox1.Location = new System.Drawing.Point(132, 32);
+            this.displayBox1.Name = "displayBox1";
+            this.displayBox1.Size = new System.Drawing.Size(70, 23);
+            this.displayBox1.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "ID", true));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(132, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(80, 23);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "Name", true));
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox2.Location = new System.Drawing.Point(132, 131);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 23);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.White;
+            this.textBox3.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "Skill", true));
+            this.textBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox3.Location = new System.Drawing.Point(132, 182);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(140, 23);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.PopUp += new System.EventHandler<Sci.Win.UI.TextBoxPopUpEventArgs>(this.textBox3_PopUp);
+            // 
+            // dateBox1
+            // 
+            this.dateBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "OnBoardDate", true));
+            this.dateBox1.Location = new System.Drawing.Point(479, 32);
+            this.dateBox1.Name = "dateBox1";
+            this.dateBox1.Size = new System.Drawing.Size(130, 23);
+            this.dateBox1.TabIndex = 11;
+            // 
+            // dateBox2
+            // 
+            this.dateBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "ResignationDate", true));
+            this.dateBox2.Location = new System.Drawing.Point(479, 81);
+            this.dateBox2.Name = "dateBox2";
+            this.dateBox2.Size = new System.Drawing.Size(130, 23);
+            this.dateBox2.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.EditMode = Sci.Win.UI.AdvEditModes.DisableOnEdit;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.button1.Location = new System.Drawing.Point(578, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Import From Excel";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtsewingline1
+            // 
+            this.txtsewingline1.BackColor = System.Drawing.Color.White;
+            this.txtsewingline1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "SewingLineID", true));
+            this.txtsewingline1.factoryobjectName = this.displayBox1;
+            this.txtsewingline1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtsewingline1.Location = new System.Drawing.Point(479, 132);
+            this.txtsewingline1.Name = "txtsewingline1";
+            this.txtsewingline1.Size = new System.Drawing.Size(60, 23);
+            this.txtsewingline1.TabIndex = 13;
+            // 
+            // B08
+            // 
+            this.ClientSize = new System.Drawing.Size(722, 457);
+            this.Controls.Add(this.button1);
+            this.DefaultOrder = "ID";
+            this.IsSupportCopy = false;
+            this.Name = "B08";
+            this.Text = "B08. Employee data maintain";
+            this.UniqueExpress = "FactoryID,ID";
+            this.WorkAlias = "Employee";
+            this.Controls.SetChildIndex(this.tabs, 0);
+            this.Controls.SetChildIndex(this.button1, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).EndInit();
+            this.detail.ResumeLayout(false);
+            this.detailcont.ResumeLayout(false);
+            this.detailcont.PerformLayout();
+            this.tabs.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.DateBox dateBox2;
+        private Win.UI.DateBox dateBox1;
+        private Win.UI.TextBox textBox3;
+        private Win.UI.TextBox textBox2;
+        private Win.UI.TextBox textBox1;
+        private Win.UI.DisplayBox displayBox1;
+        private Win.UI.Label label9;
+        private Win.UI.Label label8;
+        private Win.UI.Label label7;
+        private Win.UI.Label label6;
+        private Win.UI.Label label5;
+        private Win.UI.Label label4;
+        private Win.UI.Label label3;
+        private Class.txtsewingline txtsewingline1;
+        private Win.UI.Button button1;
+    }
+}
---
+++ Sci.Production.IE/B08.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Sci.Data;
+
+namespace Sci.Production.IE
+{
+    public partial class B08 : Sci.Win.Tems.Input1
+    {
+        public B08(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            this.DefaultFilter = "FactoryID = '" + Sci.Env.User.Factory + "'";
+        }
+
+        protected override void OnNewAfter()
+        {
+            base.OnNewAfter();
+            CurrentMaintain["FactoryID"] = Sci.Env.User.Factory;
+        }
+
+        protected override void OnEditAfter()
+        {
+            base.OnEditAfter();
+            this.textBox1.ReadOnly = true;
+        }
+
+        protected override bool OnSaveBefore()
+        {
+            if (String.IsNullOrWhiteSpace(CurrentMaintain["ID"].ToString()))
+            {
+                MessageBox.Show("< Employee# > can not be empty!");
+                this.textBox1.Focus();
+                return false;
+            }
+
+            if (String.IsNullOrWhiteSpace(CurrentMaintain["Name"].ToString()))
+            {
+                MessageBox.Show("< Nick Name > can not be empty!");
+                this.textBox2.Focus();
+                return false;
+            }
+
+            if (String.IsNullOrWhiteSpace(CurrentMaintain["OnBoardDate"].ToString()))
+            {
+                MessageBox.Show("< Hired on > can not be empty!");
+                this.dateBox1.Focus();
+                return false;
+            }
+
+            if (String.IsNullOrWhiteSpace(CurrentMaintain["SewingLineID"].ToString()))
+            {
+                MessageBox.Show("< Line > can not be empty!");
+                this.txtsewingline1.Focus();
+                return false;
+            }
+
+            return base.OnSaveBefore();
+        }
+
+        private void textBox3_PopUp(object sender, Win.UI.TextBoxPopUpEventArgs e)
+        {
+            if (this.EditMode)
+            {
+                DataTable MachineGroup;
+                Ict.DualResult returnResule = DBProxy.Current.Select("Machine", "select ID,Description from MachineGroup where Junk = 0 order by ID", out MachineGroup);
+                Sci.Win.Tools.SelectItem2 item = new Sci.Win.Tools.SelectItem2(MachineGroup, "ID,Description", "Group ID,Description", "2,60");
+                DialogResult returnResult = item.ShowDialog();
+                if (returnResult == DialogResult.Cancel) { return; }
+
+                string returnData = "";
+                IList<DataRow> gridData = item.GetSelecteds();
+                foreach (DataRow currentRecord in gridData)
+                {
+                    returnData = returnData + currentRecord["ID"].ToString() + ",";
+                }
+                this.textBox3.Text = returnData;
+            }
+        }
+    }
+}
