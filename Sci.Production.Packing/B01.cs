dd46324e0d2396e201e38eb94b29b7cec3799316
YINMEI <yinmei@sportscity.com.tw>
2015-05-20 02:51:48 +0000

    Add B01

---
+++ Sci.Production.Packing/B01.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Packing
+{
+    public partial class B01 : Sci.Win.Tems.Input1
+    {
+        public B01(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNewAfter()
+        {
+            base.OnNewAfter();
+            CurrentMaintain["Type"] = "OG";
+        }
+
+        protected override void OnEditAfter()
+        {
+            base.OnEditAfter();
+            this.textBox1.ReadOnly = true;
+        }
+
+        protected override bool OnSaveBefore()
+        {
+            if (string.IsNullOrWhiteSpace(CurrentMaintain["ID"].ToString()))
+            {
+                MessageBox.Show("< ID > can not be empty!");
+                this.textBox1.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(CurrentMaintain["Description"].ToString()))
+            {
+                MessageBox.Show("< Description > can not be empty!");
+                this.textBox2.Focus();
+                return false;
+            }
+            return base.OnSaveBefore();
+        }
+    }
+}
