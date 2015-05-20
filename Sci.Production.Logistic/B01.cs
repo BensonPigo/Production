f5b30cb99e0f85f59f027cf6532dd4b1c4273906
YINMEI <yinmei@sportscity.com.tw>
2015-05-20 02:06:24 +0000

    Add B01, Print功能尚未完成

---
+++ Sci.Production.Logistic/B01.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Logistic
+{
+    public partial class B01 : Sci.Win.Tems.Input1
+    {
+        public B01(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnEditAfter()
+        {
+            base.OnEditAfter();
+            this.textBox1.ReadOnly = true;
+        }
+
+        protected override bool OnSaveBefore()
+        {
+            if (string.IsNullOrWhiteSpace(CurrentMaintain["ID"].ToString()))
+            {
+                MessageBox.Show("< Code > can not be empty!");
+                this.textBox1.Focus();
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(CurrentMaintain["Description"].ToString()))
+            {
+                MessageBox.Show("< Description > can not be empty!");
+                this.textBox2.Focus();
+                return false;
+            }
+            return base.OnSaveBefore();
+        }
+    }
+}
