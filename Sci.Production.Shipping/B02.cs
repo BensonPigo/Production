fc725d13b6d25d44c2810592ecb466cbd45cff2c
YINMEI <yinmei@sportscity.com.tw>
2015-05-22 01:27:46 +0000

    Add B02

---
+++ Sci.Production.Shipping/B02.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Shipping
+{
+    public partial class B02 : Sci.Win.Tems.Input1
+    {
+        public B02(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+        }
+    }
+}
