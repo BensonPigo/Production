625cc75e2db228e1a62ddac983e6bf3cfc8b49f5
YINMEI <yinmei@sportscity.com.tw>
2015-05-22 02:21:11 +0000

    Add B04

---
+++ Sci.Production.Shipping/B04.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Sci.Production.Shipping
+{
+    partial class B04
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.label5 = new Sci.Win.UI.Label();
+            this.label6 = new Sci.Win.UI.Label();
+            this.label7 = new Sci.Win.UI.Label();
+            this.displayBox2 = new Sci.Win.UI.DisplayBox();
+            this.displayBox3 = new Sci.Win.UI.DisplayBox();
+            this.displayBox4 = new Sci.Win.UI.DisplayBox();
+            this.comboBox1 = new Sci.Win.UI.ComboBox();
+            this.textBox1 = new Sci.Win.UI.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).BeginInit();
+            this.detail.SuspendLayout();
+            this.detailcont.SuspendLayout();
+            this.detailbtm.SuspendLayout();
+            this.tabs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // detail
+            // 
+            this.detail.Size = new System.Drawing.Size(680, 395);
+            // 
+            // detailcont
+            // 
+            this.detailcont.Controls.Add(this.textBox1);
+            this.detailcont.Controls.Add(this.comboBox1);
+            this.detailcont.Controls.Add(this.displayBox4);
+            this.detailcont.Controls.Add(this.displayBox3);
+            this.detailcont.Controls.Add(this.displayBox2);
+            this.detailcont.Controls.Add(this.label7);
+            this.detailcont.Controls.Add(this.label6);
+            this.detailcont.Controls.Add(this.label5);
+            this.detailcont.Controls.Add(this.label4);
+            this.detailcont.Controls.Add(this.label3);
+            this.detailcont.Size = new System.Drawing.Size(680, 357);
+            // 
+            // detailbtm
+            // 
+            this.detailbtm.Size = new System.Drawing.Size(680, 38);
+            // 
+            // tabs
+            // 
+            this.tabs.Size = new System.Drawing.Size(688, 424);
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(33, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 23);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Account No.";
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(33, 70);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 23);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Account Name";
+            // 
+            // label5
+            // 
+            this.label5.Lines = 0;
+            this.label5.Location = new System.Drawing.Point(33, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 23);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Expense Reason";
+            // 
+            // label6
+            // 
+            this.label6.Lines = 0;
+            this.label6.Location = new System.Drawing.Point(33, 150);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 23);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Shipping Mode";
+            // 
+            // label7
+            // 
+            this.label7.Lines = 0;
+            this.label7.Location = new System.Drawing.Point(33, 190);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(115, 23);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Share base";
+            // 
+            // displayBox2
+            // 
+            this.displayBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.displayBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.displayBox2.Location = new System.Drawing.Point(151, 70);
+            this.displayBox2.Name = "displayBox2";
+            this.displayBox2.Size = new System.Drawing.Size(200, 23);
+            this.displayBox2.TabIndex = 6;
+            // 
+            // displayBox3
+            // 
+            this.displayBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.displayBox3.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "ExpenseReason", true));
+            this.displayBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.displayBox3.Location = new System.Drawing.Point(151, 110);
+            this.displayBox3.Name = "displayBox3";
+            this.displayBox3.Size = new System.Drawing.Size(200, 23);
+            this.displayBox3.TabIndex = 7;
+            // 
+            // displayBox4
+            // 
+            this.displayBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.displayBox4.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "ShipModeID", true));
+            this.displayBox4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.displayBox4.Location = new System.Drawing.Point(151, 150);
+            this.displayBox4.Name = "displayBox4";
+            this.displayBox4.Size = new System.Drawing.Size(455, 23);
+            this.displayBox4.TabIndex = 8;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.DataBindings.Add(new System.Windows.Forms.Binding("SelectedValue", this.mtbs, "ShareBase", true));
+            this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IsSupportUnselect = true;
+            this.comboBox1.Location = new System.Drawing.Point(151, 190);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(178, 24);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "AccountNo", true));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(151, 30);
+            this.textBox1.Mask = "CCCC-CCCC";
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(70, 23);
+            this.textBox1.TabIndex = 10;
+            // 
+            // B04
+            // 
+            this.ClientSize = new System.Drawing.Size(688, 457);
+            this.Name = "B04";
+            this.Text = "B04. Share base of Share Expense";
+            this.WorkAlias = "ShareRule";
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).EndInit();
+            this.detail.ResumeLayout(false);
+            this.detailcont.ResumeLayout(false);
+            this.detailcont.PerformLayout();
+            this.detailbtm.ResumeLayout(false);
+            this.detailbtm.PerformLayout();
+            this.tabs.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.ComboBox comboBox1;
+        private Win.UI.DisplayBox displayBox4;
+        private Win.UI.DisplayBox displayBox3;
+        private Win.UI.DisplayBox displayBox2;
+        private Win.UI.Label label7;
+        private Win.UI.Label label6;
+        private Win.UI.Label label5;
+        private Win.UI.Label label4;
+        private Win.UI.Label label3;
+        private Win.UI.TextBox textBox1;
+    }
+}
---
+++ Sci.Production.Shipping/B04.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Sci.Data;
+using Ict;
+
+namespace Sci.Production.Shipping
+{
+    public partial class B04 : Sci.Win.Tems.Input1
+    {
+        public B04(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            Dictionary<String, String> comboBox1_RowSource = new Dictionary<string, string>();
+            comboBox1_RowSource.Add("C", "CBM");
+            comboBox1_RowSource.Add("G", "G.W.");
+            comboBox1_RowSource.Add(" ", "Number of Deliver Sheets");
+            comboBox1.DataSource = new BindingSource(comboBox1_RowSource, null);
+            comboBox1.ValueMember = "Key";
+            comboBox1.DisplayMember = "Value";
+        }
+
+        protected override void OnDetailEntered()
+        {
+            base.OnDetailEntered();
+            //撈Account Name資料
+            string selectCommand = string.Format("select Name from AccountNo where ID = '{0}'", CurrentMaintain["AccountNo"].ToString());
+            DataTable AccountNoTable;
+            DualResult selectResult = DBProxy.Current.Select("Finance", selectCommand, out AccountNoTable);
+            if (AccountNoTable != null && AccountNoTable.Rows.Count > 0)
+            {
+                this.displayBox2.Text = AccountNoTable.Rows[0]["Name"].ToString();
+            }
+            else
+            {
+                this.displayBox2.Text = "";
+            }
+        }
+    }
+}
