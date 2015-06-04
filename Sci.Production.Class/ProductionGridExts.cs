6d6ec227880d060ca6e22b22e8106b3140275ca4
YINMEI <yinmei@sportscity.com.tw>
2015-06-04 06:55:53 +0000

    Add txtcloglocation & ProductionGridExts

---
+++ Sci.Production.Class/ProductionGridExts.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sci.Win.UI;
+using Sci.Data;
+using Ict.Win;
+using Ict;
+using Sci;
+
+namespace Sci
+{
+    public static class ProductionGridExts
+    {
+        public static IDataGridViewGenerator CellClogLocation(this IDataGridViewGenerator gen, string propertyname, string header, IWidth width = null, DataGridViewGeneratorTextColumnSettings settings = null, bool? iseditable = null, bool? iseditingreadonly = null, DataGridViewContentAlignment? alignment = null)
+        {
+            if (settings == null) settings = new DataGridViewGeneratorTextColumnSettings();
+            settings.CharacterCasing = CharacterCasing.Upper;
+            settings.EditingMouseDown += (s, e) =>
+            {
+                Sci.Win.UI.Grid g = (Sci.Win.UI.Grid) ((DataGridViewColumn) s).DataGridView;
+                Sci.Win.Forms.Base frm = (Sci.Win.Forms.Base)g.FindForm();
+                if (frm.EditMode)
+                {
+                    if (e.Button == System.Windows.Forms.MouseButtons.Right)
+                    {
+                        if (e.RowIndex != -1)
+                        {
+                            DataRow dr = g.GetDataRow<DataRow>(e.RowIndex);
+                            Sci.Win.Tools.SelectItem item = new Sci.Win.Tools.SelectItem("select ID,Description from ClogLocation order by ID", "10,40", dr["ClogLocationId"].ToString().Trim());
+                            DialogResult returnResult = item.ShowDialog();
+                            if (returnResult == DialogResult.Cancel) { return; }
+                            dr["ClogLocationId"] = item.GetSelectedString();
+                        }
+                    }
+                }
+            };
+            settings.CellValidating += (s,e) =>
+            {
+                Sci.Win.UI.Grid g = (Sci.Win.UI.Grid)((DataGridViewColumn)s).DataGridView;
+                Sci.Win.Forms.Base frm = (Sci.Win.Forms.Base)g.FindForm();
+                if (frm.EditMode)
+                {
+                    DataRow dr = g.GetDataRow<DataRow>(e.RowIndex);
+                    if (!myUtility.Empty(e.FormattedValue.ToString()))
+                    {
+                        if (myUtility.Seek(e.FormattedValue.ToString(), "ClogLocation", "id") == false)
+                        {
+                            MessageBox.Show(string.Format("< ClogLocation : {0} > not found!!!", e.FormattedValue.ToString()));
+                            var data = g.GetDataRow(e.RowIndex);
+                            data["ClogLocationId"] = DBNull.Value;
+                            e.Cancel = true;
+                            return;
+                        }
+                    }
+                }
+            };
+            return gen.Text(propertyname, header: header, width: width, settings: settings, iseditable: iseditable, iseditingreadonly: iseditingreadonly, alignment: alignment);
+        }
+    }
+}
---
+++ Sci.Production.Class/txtcloglocation.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sci.Win.UI;
+using Sci.Data;
+using Ict.Win;
+
+namespace Sci.Production.Class
+{
+    public partial class txtcloglocation : Sci.Win.UI.TextBox
+    {
+        public txtcloglocation()
+        {
+            this.Size = new System.Drawing.Size(80, 23);
+            this.IsSupportSytsemContextMenu = false;
+            
+        }
+
+        protected override void OnPopUp(TextBoxPopUpEventArgs e)
+        {            
+            base.OnPopUp(e);
+            if (e.IsHandled) return;
+
+            if (this.ReadOnly == false)
+            {
+                string sql = "select ID,Description from ClogLocation order by ID";
+                DataTable tbClogLocation;
+                DBProxy.Current.Select("Production", sql, out tbClogLocation);
+                Sci.Win.Tools.SelectItem item = new Sci.Win.Tools.SelectItem(tbClogLocation, "ID,Description", "10,40", this.Text, "ID,Description");
+
+                DialogResult result = item.ShowDialog();
+                if (result == DialogResult.Cancel) { return; }
+                this.Text = item.GetSelectedString();
+                e.IsHandled = true;
+            }
+        }
+
+        protected override void OnValidating(CancelEventArgs e)
+        {
+            base.OnValidating(e);
+
+            string str = this.Text;
+            if (!myUtility.Empty(str) && str != this.OldValue)
+            {
+                if (myUtility.Seek(str, "ClogLocation", "id") == false)
+                {
+                    MessageBox.Show(string.Format("< ClogLocation : {0} > not found!!!", str));
+                    this.Text = "";
+                    e.Cancel = true;
+                    return;
+                }
+            }
+        }
+    }
+}
