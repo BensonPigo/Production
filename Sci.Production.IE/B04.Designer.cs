65fd011ca9c326de1516f35bd38f11b45c330b91
YINMEI <yinmei@sportscity.com.tw>
2015-06-11 02:39:59 +0000

    修改Bindingsource的設定

--- Sci.Production.IE/B04.Designer.cs
+++ Sci.Production.IE/B04.Designer.cs
@@ -37,6 +37,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.mtbs)).BeginInit();
             this.detail.SuspendLayout();
             this.detailcont.SuspendLayout();
+            this.detailbtm.SuspendLayout();
             this.tabs.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -57,6 +58,10 @@
             // 
             this.detailbtm.Size = new System.Drawing.Size(681, 38);
             // 
+            // browse
+            // 
+            this.browse.Size = new System.Drawing.Size(681, 395);
+            // 
             // tabs
             // 
             this.tabs.Size = new System.Drawing.Size(689, 424);
@@ -64,7 +69,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.DataBindings.Add(new System.Windows.Forms.Binding("Checked", this.mtbs, "Junk", true));
+            this.checkBox1.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "Junk", true));
             this.checkBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.checkBox1.Location = new System.Drawing.Point(349, 44);
             this.checkBox1.Name = "checkBox1";
@@ -129,6 +134,8 @@
             this.detail.ResumeLayout(false);
             this.detailcont.ResumeLayout(false);
             this.detailcont.PerformLayout();
+            this.detailbtm.ResumeLayout(false);
+            this.detailbtm.PerformLayout();
             this.tabs.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
