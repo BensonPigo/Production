7fe7f967a882f0b395349a4fa0ef3d5c6b8341af
MIKE <mike@sportscity.com.tw>
2015-06-18 08:49:48 +0000

    add GetItemDesc public method

---
+++ Sci.Production.PublicPrg/Subcon.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Sci.Data;
+using Sci;
+using Ict;
+using Ict.Win;
+
+namespace Sci.Production.PublicPrg
+{
+    
+    public static partial class Prgs
+    {
+        #region GetItemDesc
+        /// <summary>
+        /// GetItemDesc()
+        /// </summary>
+        /// <param name="String Category"></param>
+        /// <param name="String Refno"></param>
+        /// <returns>String Desc</returns>
+        public static string GetItemDesc(string category, string refno)
+        {
+            string desc = myUtility.Lookup(string.Format(@"
+                    select description from localitem where refno = '{0}' and category = '{1}'",refno,category));
+            string[] descs = desc.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            if (descs.Length == 0)
+                return "";
+            else
+                return descs[0];
+        }
+        #endregion
+
+        
+    }
+    
+}
