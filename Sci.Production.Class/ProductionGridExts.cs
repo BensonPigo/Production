a6ee276988926d91b13733d42bdaa7a8ffce2ce4
YINMEI <yinmei@sportscity.com.tw>
2015-06-23 03:36:03 +0000

    add CellCartonItem

--- Sci.Production.Class/ProductionGridExts.cs
+++ Sci.Production.Class/ProductionGridExts.cs
@@ -15,6 +15,7 @@ namespace Sci
 {
     public static class ProductionGridExts
     {
+        //Clog Location
         public static IDataGridViewGenerator CellClogLocation(this IDataGridViewGenerator gen, string propertyname, string header, IWidth width = null, DataGridViewGeneratorTextColumnSettings settings = null, bool? iseditable = null, bool? iseditingreadonly = null, DataGridViewContentAlignment? alignment = null)
         {
             if (settings == null) settings = new DataGridViewGeneratorTextColumnSettings();
@@ -83,6 +84,53 @@ namespace Sci
                         {
                             MessageBox.Show(string.Format("< Order Id : {0} > is not found!!!", e.FormattedValue.ToString()));
                             dr["orderid"] = DBNull.Value;
+                            e.Cancel = true;
+                            return;
+                        }
+                    }
+                }
+            };
+            return gen.Text(propertyname, header: header, width: width, settings: settings, iseditable: iseditable, iseditingreadonly: iseditingreadonly, alignment: alignment);
+        }
+
+        //LocalItem: Carton
+        public static IDataGridViewGenerator CellCartonItem(this IDataGridViewGenerator gen, string propertyname, string header, IWidth width = null, DataGridViewGeneratorTextColumnSettings settings = null, bool? iseditable = null, bool? iseditingreadonly = null, DataGridViewContentAlignment? alignment = null)
+        {
+            if (settings == null) settings = new DataGridViewGeneratorTextColumnSettings();
+            settings.CharacterCasing = CharacterCasing.Upper;
+            settings.EditingMouseDown += (s, e) =>
+            {
+                Sci.Win.UI.Grid g = (Sci.Win.UI.Grid)((DataGridViewColumn)s).DataGridView;
+                Sci.Win.Forms.Base frm = (Sci.Win.Forms.Base)g.FindForm();
+                if (frm.EditMode)
+                {
+                    if (e.Button == System.Windows.Forms.MouseButtons.Right)
+                    {
+                        if (e.RowIndex != -1)
+                        {
+                            DataRow dr = g.GetDataRow<DataRow>(e.RowIndex);
+                            Sci.Win.Tools.SelectItem item = new Sci.Win.Tools.SelectItem("select RefNo,Description,STR(CtnLength,8,4)+'*'+STR(CtnWidth,8,4)+'*'+STR(CtnHeight,8,4) as Dim from LocalItem where Category = 'CARTON' and Junk = 0 order by RefNo", "10,25,25", dr["RefNo"].ToString().Trim());
+                            DialogResult returnResult = item.ShowDialog();
+                            if (returnResult == DialogResult.Cancel) { return; }
+                            dr["RefNo"] = item.GetSelectedString();
+                        }
+                    }
+                }
+            };
+            settings.CellValidating += (s, e) =>
+            {
+                Sci.Win.UI.Grid g = (Sci.Win.UI.Grid)((DataGridViewColumn)s).DataGridView;
+                Sci.Win.Forms.Base frm = (Sci.Win.Forms.Base)g.FindForm();
+                if (frm.EditMode)
+                {
+                    DataRow dr = g.GetDataRow<DataRow>(e.RowIndex);
+                    if (!myUtility.Empty(e.FormattedValue.ToString()))
+                    {
+                        string seekSql = string.Format("select RefNo from LocalItem where Category = 'CARTON' and Junk = 0 and RefNo = '{0}'", e.FormattedValue.ToString());
+                        if (myUtility.Seek(seekSql) == false)
+                        {
+                            MessageBox.Show(string.Format("< Ref No. : {0} > not found!!!", e.FormattedValue.ToString()));
+                            dr["RefNo"] = DBNull.Value;
                             e.Cancel = true;
                             return;
                         }
