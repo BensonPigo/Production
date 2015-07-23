cc559e315c54f1478ff10e1e72773070ee94fdd4
YINMEI <yinmei@sportscity.com.tw>
2015-07-23 07:01:18 +0000

    Add P08 (To Excel功能尚未完成)

---
+++ Sci.Production.Packing/P08.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace Sci.Production.Packing
+{
+    partial class P08
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new Sci.Win.UI.Panel();
+            this.panel2 = new Sci.Win.UI.Panel();
+            this.panel3 = new Sci.Win.UI.Panel();
+            this.panel4 = new Sci.Win.UI.Panel();
+            this.listControlBindingSource1 = new Sci.Win.UI.ListControlBindingSource();
+            this.panel5 = new Sci.Win.UI.Panel();
+            this.label1 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.label3 = new Sci.Win.UI.Label();
+            this.dateRange1 = new Sci.Win.UI.DateRange();
+            this.dateRange2 = new Sci.Win.UI.DateRange();
+            this.textBox1 = new Sci.Win.UI.TextBox();
+            this.label6 = new Sci.Win.UI.Label();
+            this.textBox2 = new Sci.Win.UI.TextBox();
+            this.label4 = new Sci.Win.UI.Label();
+            this.label5 = new Sci.Win.UI.Label();
+            this.dateRange3 = new Sci.Win.UI.DateRange();
+            this.dateRange4 = new Sci.Win.UI.DateRange();
+            this.button1 = new Sci.Win.UI.Button();
+            this.button2 = new Sci.Win.UI.Button();
+            this.button3 = new Sci.Win.UI.Button();
+            this.button4 = new Sci.Win.UI.Button();
+            this.grid1 = new Sci.Win.UI.Grid();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource1)).BeginInit();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(5, 480);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(959, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(5, 480);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.dateRange4);
+            this.panel3.Controls.Add(this.dateRange3);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.textBox2);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.textBox1);
+            this.panel3.Controls.Add(this.dateRange2);
+            this.panel3.Controls.Add(this.dateRange1);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(5, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(954, 85);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button4);
+            this.panel4.Controls.Add(this.button3);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(5, 436);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(954, 44);
+            this.panel4.TabIndex = 4;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.grid1);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(5, 85);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(954, 351);
+            this.panel5.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(2, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SP#";
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(2, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SCI Delivery";
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(391, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Carton Est. Booking";
+            // 
+            // dateRange1
+            // 
+            this.dateRange1.IsRequired = false;
+            this.dateRange1.Location = new System.Drawing.Point(89, 32);
+            this.dateRange1.Name = "dateRange1";
+            this.dateRange1.Size = new System.Drawing.Size(280, 23);
+            this.dateRange1.TabIndex = 3;
+            // 
+            // dateRange2
+            // 
+            this.dateRange2.IsRequired = false;
+            this.dateRange2.Location = new System.Drawing.Point(522, 5);
+            this.dateRange2.Name = "dateRange2";
+            this.dateRange2.Size = new System.Drawing.Size(280, 23);
+            this.dateRange2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(89, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 23);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Lines = 0;
+            this.label6.Location = new System.Drawing.Point(212, 5);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 23);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "～";
+            this.label6.TextStyle.BorderColor = System.Drawing.Color.Black;
+            this.label6.TextStyle.Color = System.Drawing.Color.Black;
+            this.label6.TextStyle.ExtBorderColor = System.Drawing.Color.Black;
+            this.label6.TextStyle.GradientColor = System.Drawing.Color.Black;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox2.Location = new System.Drawing.Point(237, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(120, 23);
+            this.textBox2.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(391, 5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 23);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Sewing Inline Date";
+            // 
+            // label5
+            // 
+            this.label5.Lines = 0;
+            this.label5.Location = new System.Drawing.Point(391, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(127, 23);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Carton Est. Arrived";
+            // 
+            // dateRange3
+            // 
+            this.dateRange3.IsRequired = false;
+            this.dateRange3.Location = new System.Drawing.Point(522, 32);
+            this.dateRange3.Name = "dateRange3";
+            this.dateRange3.Size = new System.Drawing.Size(280, 23);
+            this.dateRange3.TabIndex = 16;
+            // 
+            // dateRange4
+            // 
+            this.dateRange4.IsRequired = false;
+            this.dateRange4.Location = new System.Drawing.Point(522, 58);
+            this.dateRange4.Name = "dateRange4";
+            this.dateRange4.Size = new System.Drawing.Size(280, 23);
+            this.dateRange4.TabIndex = 17;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(863, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 30);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Query";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(863, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 30);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "To Excel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(746, 7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 30);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Approve";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button4.Location = new System.Drawing.Point(848, 7);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 30);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Close";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // grid1
+            // 
+            this.grid1.AllowUserToAddRows = false;
+            this.grid1.AllowUserToDeleteRows = false;
+            this.grid1.AllowUserToResizeRows = false;
+            this.grid1.AutoGenerateColumns = false;
+            this.grid1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.grid1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.grid1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.grid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid1.DataSource = this.listControlBindingSource1;
+            this.grid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid1.EditingEnter = Ict.Win.UI.DataGridViewEditingEnter.NextCellOrNextRow;
+            this.grid1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.grid1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.grid1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(162)))), ((int)(((byte)(163)))));
+            this.grid1.Location = new System.Drawing.Point(0, 0);
+            this.grid1.Name = "grid1";
+            this.grid1.RowHeadersVisible = false;
+            this.grid1.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.grid1.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.grid1.RowTemplate.Height = 24;
+            this.grid1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grid1.Size = new System.Drawing.Size(954, 351);
+            this.grid1.TabIndex = 0;
+            this.grid1.TabStop = false;
+            // 
+            // P08
+            // 
+            this.CancelButton = this.button4;
+            this.ClientSize = new System.Drawing.Size(964, 480);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.EditMode = true;
+            this.Name = "P08";
+            this.Text = "P08. Batch Approve to Purchase Cartons";
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.Controls.SetChildIndex(this.panel2, 0);
+            this.Controls.SetChildIndex(this.panel3, 0);
+            this.Controls.SetChildIndex(this.panel4, 0);
+            this.Controls.SetChildIndex(this.panel5, 0);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource1)).EndInit();
+            this.panel5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Win.UI.Panel panel1;
+        private Win.UI.Panel panel2;
+        private Win.UI.Panel panel3;
+        private Win.UI.TextBox textBox1;
+        private Win.UI.DateRange dateRange2;
+        private Win.UI.DateRange dateRange1;
+        private Win.UI.Label label3;
+        private Win.UI.Label label2;
+        private Win.UI.Label label1;
+        private Win.UI.Panel panel4;
+        private Win.UI.ListControlBindingSource listControlBindingSource1;
+        private Win.UI.Panel panel5;
+        private Win.UI.TextBox textBox2;
+        private Win.UI.Label label6;
+        private Win.UI.Button button2;
+        private Win.UI.Button button1;
+        private Win.UI.DateRange dateRange4;
+        private Win.UI.DateRange dateRange3;
+        private Win.UI.Label label5;
+        private Win.UI.Label label4;
+        private Win.UI.Button button4;
+        private Win.UI.Button button3;
+        private Win.UI.Grid grid1;
+    }
+}
---
+++ Sci.Production.Packing/P08.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict;
+using Ict.Win;
+using Sci.Data;
+using Sci;
+using System.Transactions;
+
+namespace Sci.Production.Packing
+{
+    public partial class P08 : Sci.Win.Tems.QueryForm
+    {
+        Ict.Win.UI.DataGridViewCheckBoxColumn col_chk;
+        DualResult result;
+        string sqlCmd,updateCmd;
+        DataTable gridData;
+
+        public P08(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnFormLoaded()
+        {
+            base.OnFormLoaded();
+
+            //Grid設定
+            this.grid1.IsEditingReadOnly = false;
+            this.grid1.DataSource = listControlBindingSource1;
+
+            Helper.Controls.Grid.Generator(this.grid1)
+                .CheckBox("Selected", header: "", width: Widths.AnsiChars(3), iseditable: true, trueValue: 1, falseValue: 0).Get(out col_chk)
+                .Text("ID", header: "Packing No.", width: Widths.AnsiChars(13), iseditingreadonly: true)
+                .Text("Type", header: "Packing Type", width: Widths.AnsiChars(13), iseditingreadonly: true)
+                .Text("OrderId", header: "SP#", width: Widths.AnsiChars(13), iseditingreadonly: true)
+                .Date("SciDelivery", header: "SCI Delivery", iseditingreadonly: true)
+                .Date("SewInLine", header: "Sewing Inline Date", iseditingreadonly: true)
+                .Date("EstCTNBooking", header: "Carton Est. Booking", iseditingreadonly: true)
+                .Date("EstCTNArrive", header: "Carton Est. Arrived", iseditingreadonly: true);
+        }
+
+        //Query
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MyUtility.Check.Empty(textBox1.Text) && MyUtility.Check.Empty(textBox2.Text) && MyUtility.Check.Empty(dateRange1.Value1) && MyUtility.Check.Empty(dateRange1.Value2) && MyUtility.Check.Empty(dateRange2.Value1) && MyUtility.Check.Empty(dateRange2.Value2) && MyUtility.Check.Empty(dateRange3.Value1) && MyUtility.Check.Empty(dateRange3.Value2) && MyUtility.Check.Empty(dateRange4.Value1) && MyUtility.Check.Empty(dateRange4.Value2))
+            {
+                MyUtility.Msg.WarningBox("< SP# > or < SCI Delivery > or < Sewing Inline Date > or < Carton Est. Booking > or < Carton Est. Arrived > can not empty!");
+                textBox1.Focus();
+                return;
+            }
+
+            sqlCmd = @"select distinct 0 as Selected, pl.ID, iif(pl.Type = 'B','Bulk','Sample') as Type, pld.OrderID, o.SciDelivery, o.SewInLine, pl.EstCTNBooking, pl.EstCTNArrive
+from PackingList pl, PackingList_Detail pld, Orders o
+where pl.FactoryID = @factoryid
+and (pl.Type = 'B' or pl.Type = 'S')
+and pl.ApvToPurchase = 0
+and pl.EstCTNBooking is not null
+and pl.EstCTNArrive is not null
+and pld.ID = pl.ID
+and o.ID = pld.OrderID";
+            #region 組條件
+            if (!MyUtility.Check.Empty(this.textBox1.Text))
+            {
+                sqlCmd = sqlCmd + "\r\nand o.ID >= @orderid1";
+            }
+            if (!MyUtility.Check.Empty(this.textBox2.Text))
+            {
+                sqlCmd = sqlCmd + "\r\nand o.ID <= @orderid2";
+            }
+            if (!MyUtility.Check.Empty(dateRange1.Value1))
+            {
+                sqlCmd = sqlCmd + "\r\nand o.SciDelivery >= @scidelivery1";
+            }
+            if (!MyUtility.Check.Empty(dateRange1.Value2))
+            {
+                sqlCmd = sqlCmd + "\r\nand o.SciDelivery <= @scidelivery2";
+            }
+            if (!MyUtility.Check.Empty(dateRange2.Value1))
+            {
+                sqlCmd = sqlCmd + "\r\nand o.SewInLine >= @sewingline1";
+            }
+            if (!MyUtility.Check.Empty(dateRange2.Value2))
+            {
+                sqlCmd = sqlCmd + "\r\nand o.SewInLine <= @sewingline2";
+            }
+            if (!MyUtility.Check.Empty(dateRange3.Value1))
+            {
+                sqlCmd = sqlCmd + "\r\nand pl.EstCTNBooking >= @estbooking1";
+            }
+            if (!MyUtility.Check.Empty(dateRange3.Value2))
+            {
+                sqlCmd = sqlCmd + "\r\nand pl.EstCTNBooking <= @estbooking2";
+            }
+            if (!MyUtility.Check.Empty(dateRange4.Value1))
+            {
+                sqlCmd = sqlCmd + "\r\nand pl.EstCTNArrive >= @estarrive1";
+            }
+            if (!MyUtility.Check.Empty(dateRange4.Value2))
+            {
+                sqlCmd = sqlCmd + "\r\nand pl.EstCTNArrive <= @estarrive2";
+            }
+            #endregion
+
+            #region 準備sql參數資料
+            System.Data.SqlClient.SqlParameter sp1 = new System.Data.SqlClient.SqlParameter();
+            sp1.ParameterName = "@factoryid";
+            sp1.Value = Sci.Env.User.Factory;
+
+            System.Data.SqlClient.SqlParameter sp2 = new System.Data.SqlClient.SqlParameter();
+            sp2.ParameterName = "@orderid1";
+            sp2.Value = this.textBox1.Text;
+
+            System.Data.SqlClient.SqlParameter sp3 = new System.Data.SqlClient.SqlParameter();
+            sp3.ParameterName = "@orderid2";
+            sp3.Value = this.textBox2.Text;
+
+            System.Data.SqlClient.SqlParameter sp4 = new System.Data.SqlClient.SqlParameter();
+            sp4.ParameterName = "@scidelivery1";
+            sp4.Value = !MyUtility.Check.Empty(dateRange1.Value1) ? dateRange1.Value1 : DateTime.Now;
+
+            System.Data.SqlClient.SqlParameter sp5 = new System.Data.SqlClient.SqlParameter();
+            sp5.ParameterName = "@scidelivery2";
+            sp5.Value = !MyUtility.Check.Empty(dateRange1.Value2) ? dateRange1.Value2 : DateTime.Now;
+
+            System.Data.SqlClient.SqlParameter sp6 = new System.Data.SqlClient.SqlParameter();
+            sp6.ParameterName = "@sewingline1";
+            sp6.Value = !MyUtility.Check.Empty(dateRange2.Value1) ? dateRange2.Value1 : DateTime.Now;
+
+            System.Data.SqlClient.SqlParameter sp7 = new System.Data.SqlClient.SqlParameter();
+            sp7.ParameterName = "@sewingline2";
+            sp7.Value = !MyUtility.Check.Empty(dateRange2.Value2) ? dateRange2.Value2 : DateTime.Now;
+
+            System.Data.SqlClient.SqlParameter sp8 = new System.Data.SqlClient.SqlParameter();
+            sp8.ParameterName = "@estbooking1";
+            sp8.Value = !MyUtility.Check.Empty(dateRange3.Value1) ? dateRange3.Value1 : DateTime.Now;
+
+            System.Data.SqlClient.SqlParameter sp9 = new System.Data.SqlClient.SqlParameter();
+            sp9.ParameterName = "@estbooking2";
+            sp9.Value = !MyUtility.Check.Empty(dateRange3.Value2) ? dateRange3.Value2 : DateTime.Now;
+
+            System.Data.SqlClient.SqlParameter sp10 = new System.Data.SqlClient.SqlParameter();
+            sp10.ParameterName = "@estarrive1";
+            sp10.Value = !MyUtility.Check.Empty(dateRange4.Value1) ? dateRange4.Value1 : DateTime.Now;
+
+            System.Data.SqlClient.SqlParameter sp11 = new System.Data.SqlClient.SqlParameter();
+            sp11.ParameterName = "@estarrive2";
+            sp11.Value = !MyUtility.Check.Empty(dateRange4.Value2) ? dateRange4.Value2 : DateTime.Now;
+
+            IList<System.Data.SqlClient.SqlParameter> cmds = new List<System.Data.SqlClient.SqlParameter>();
+            cmds.Add(sp1);
+            cmds.Add(sp2);
+            cmds.Add(sp3);
+            cmds.Add(sp4);
+            cmds.Add(sp5);
+            cmds.Add(sp6);
+            cmds.Add(sp7);
+            cmds.Add(sp8);
+            cmds.Add(sp9);
+            cmds.Add(sp10);
+            cmds.Add(sp11);
+            #endregion
+            if (result = DBProxy.Current.Select(null, sqlCmd, cmds, out gridData))
+            {
+                if (gridData.Rows.Count == 0)
+                {
+                    MyUtility.Msg.WarningBox("Data not found!");
+                }
+            }
+            listControlBindingSource1.DataSource = gridData;
+        }
+
+        //To Excel
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Approve
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.grid1.ValidateControl();
+            this.grid1.EndEdit();
+            listControlBindingSource1.EndEdit();
+            DataTable detailData = (DataTable)listControlBindingSource1.DataSource;
+            DataRow[] dr = detailData.Select("Selected = 1");
+            if (dr.Length <= 0)
+            {
+                MyUtility.Msg.WarningBox("Please select at least one data!");
+                return;
+            }
+
+            #region 更新資料到PackingList.ApvToPurchase
+            using (TransactionScope transactionScope = new TransactionScope())
+            {
+                try
+                {
+                    bool lastResult = true;
+                    #region 宣告sql參數資料
+                    IList<System.Data.SqlClient.SqlParameter> cmds1 = new List<System.Data.SqlClient.SqlParameter>();
+                    System.Data.SqlClient.SqlParameter sp12 = new System.Data.SqlClient.SqlParameter();
+                    System.Data.SqlClient.SqlParameter sp13 = new System.Data.SqlClient.SqlParameter();
+                    sp12.ParameterName = "@id";
+                    sp13.ParameterName = "@date";
+                                                          
+                    cmds1.Add(sp12);
+                    cmds1.Add(sp13);
+                    #endregion
+                    foreach (DataRow currentRow in dr)
+                    {
+                        updateCmd = @"update PackingList 
+                                                   set ApvToPurchase = 1, ApvToPurchaseDate = @date 
+                                                   where ID = @id and ApvToPurchase = 0";
+                        #region 準備sql參數資料
+                        sp12.Value = currentRow["ID"].ToString();
+                        sp13.Value = DateTime.Today; 
+                        #endregion
+                        result = Sci.Data.DBProxy.Current.Execute(null, updateCmd, cmds1);
+                        if (!result)
+                        {
+                            lastResult = false;
+                        }
+                    }
+
+                    if (lastResult)
+                    {
+                        transactionScope.Complete();
+                        MyUtility.Msg.InfoBox("Approve completed!");
+                        foreach (DataRow currentRow in dr)
+                        {
+                            currentRow.Delete();
+                        }
+                    }
+                    else
+                    {
+                        MyUtility.Msg.WarningBox("Approve failed, Pleaes re-try");
+                        return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ShowErr("Commit transaction error.", ex);
+                    return;
+                }
+            }
+            #endregion
+        }
+
+        //Close
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
