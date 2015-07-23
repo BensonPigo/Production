247906273f228c9ff9eefdc8b851a4aba4eec12d
YINMEI <yinmei@sportscity.com.tw>
2015-07-23 09:07:07 +0000

    Add P11

---
+++ Sci.Production.Packing/P11.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Sci.Production.Packing
+{
+    partial class P11
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.label3 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.textBox1 = new Sci.Win.UI.TextBox();
+            this.displayBox1 = new Sci.Win.UI.DisplayBox();
+            this.displayBox2 = new Sci.Win.UI.DisplayBox();
+            this.textBox2 = new Sci.Win.UI.TextBox();
+            this.label5 = new Sci.Win.UI.Label();
+            this.label6 = new Sci.Win.UI.Label();
+            this.dateBox1 = new Sci.Win.UI.DateBox();
+            this.displayBox3 = new Sci.Win.UI.DisplayBox();
+            ((System.ComponentModel.ISupportInitialize)(this.detailgridbs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.detailgrid2bs)).BeginInit();
+            this.masterpanel.SuspendLayout();
+            this.detailpanel.SuspendLayout();
+            this.detailpanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).BeginInit();
+            this.detail.SuspendLayout();
+            this.detailcont.SuspendLayout();
+            this.detailbtm.SuspendLayout();
+            this.tabs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // masterpanel
+            // 
+            this.masterpanel.Controls.Add(this.displayBox3);
+            this.masterpanel.Controls.Add(this.label6);
+            this.masterpanel.Controls.Add(this.label5);
+            this.masterpanel.Controls.Add(this.textBox2);
+            this.masterpanel.Controls.Add(this.displayBox2);
+            this.masterpanel.Controls.Add(this.displayBox1);
+            this.masterpanel.Controls.Add(this.textBox1);
+            this.masterpanel.Controls.Add(this.label4);
+            this.masterpanel.Controls.Add(this.label3);
+            this.masterpanel.Controls.Add(this.label2);
+            this.masterpanel.Controls.Add(this.label1);
+            this.masterpanel.Controls.Add(this.dateBox1);
+            this.masterpanel.Size = new System.Drawing.Size(753, 114);
+            this.masterpanel.Controls.SetChildIndex(this.gridicon, 0);
+            this.masterpanel.Controls.SetChildIndex(this.dateBox1, 0);
+            this.masterpanel.Controls.SetChildIndex(this.label1, 0);
+            this.masterpanel.Controls.SetChildIndex(this.label2, 0);
+            this.masterpanel.Controls.SetChildIndex(this.label3, 0);
+            this.masterpanel.Controls.SetChildIndex(this.label4, 0);
+            this.masterpanel.Controls.SetChildIndex(this.textBox1, 0);
+            this.masterpanel.Controls.SetChildIndex(this.displayBox1, 0);
+            this.masterpanel.Controls.SetChildIndex(this.displayBox2, 0);
+            this.masterpanel.Controls.SetChildIndex(this.textBox2, 0);
+            this.masterpanel.Controls.SetChildIndex(this.label5, 0);
+            this.masterpanel.Controls.SetChildIndex(this.label6, 0);
+            this.masterpanel.Controls.SetChildIndex(this.displayBox3, 0);
+            // 
+            // detailpanel
+            // 
+            this.detailpanel.Location = new System.Drawing.Point(0, 114);
+            this.detailpanel.Size = new System.Drawing.Size(753, 235);
+            // 
+            // gridicon
+            // 
+            this.gridicon.Location = new System.Drawing.Point(646, 79);
+            // 
+            // refresh
+            // 
+            this.refresh.Location = new System.Drawing.Point(673, 0);
+            // 
+            // detailgridcont
+            // 
+            this.detailgridcont.Size = new System.Drawing.Size(753, 235);
+            // 
+            // detail
+            // 
+            this.detail.Size = new System.Drawing.Size(753, 387);
+            // 
+            // detailcont
+            // 
+            this.detailcont.Size = new System.Drawing.Size(753, 349);
+            // 
+            // detailbtm
+            // 
+            this.detailbtm.Size = new System.Drawing.Size(753, 38);
+            // 
+            // browse
+            // 
+            this.browse.Size = new System.Drawing.Size(753, 387);
+            // 
+            // tabs
+            // 
+            this.tabs.Size = new System.Drawing.Size(761, 416);
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(6, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SP#";
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(6, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Style";
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(6, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Season";
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(6, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 23);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Remark";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "ID", true));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(69, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 23);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating);
+            // 
+            // displayBox1
+            // 
+            this.displayBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.displayBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.displayBox1.Location = new System.Drawing.Point(69, 31);
+            this.displayBox1.Name = "displayBox1";
+            this.displayBox1.Size = new System.Drawing.Size(135, 23);
+            this.displayBox1.TabIndex = 6;
+            // 
+            // displayBox2
+            // 
+            this.displayBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.displayBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.displayBox2.Location = new System.Drawing.Point(69, 58);
+            this.displayBox2.Name = "displayBox2";
+            this.displayBox2.Size = new System.Drawing.Size(80, 23);
+            this.displayBox2.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "Remark", true));
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox2.Location = new System.Drawing.Point(70, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(384, 23);
+            this.textBox2.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.Lines = 0;
+            this.label5.Location = new System.Drawing.Point(352, 4);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 23);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Date";
+            // 
+            // label6
+            // 
+            this.label6.Lines = 0;
+            this.label6.Location = new System.Drawing.Point(352, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 23);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Factory";
+            // 
+            // dateBox1
+            // 
+            this.dateBox1.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "CloseDate", true));
+            this.dateBox1.Location = new System.Drawing.Point(413, 4);
+            this.dateBox1.Name = "dateBox1";
+            this.dateBox1.Size = new System.Drawing.Size(130, 23);
+            this.dateBox1.TabIndex = 11;
+            // 
+            // displayBox3
+            // 
+            this.displayBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.displayBox3.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "FactoryID", true));
+            this.displayBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.displayBox3.Location = new System.Drawing.Point(413, 31);
+            this.displayBox3.Name = "displayBox3";
+            this.displayBox3.Size = new System.Drawing.Size(70, 23);
+            this.displayBox3.TabIndex = 12;
+            // 
+            // P11
+            // 
+            this.ApvChkValue = "New";
+            this.ClientSize = new System.Drawing.Size(761, 449);
+            this.DefaultDetailOrder = "Article";
+            this.DefaultOrder = "ID";
+            this.GridAlias = "OverrunGMT_Detail";
+            this.GridUniqueKey = "ID,Article,SizeCode,PackingReasonID";
+            this.IsSupportConfirm = true;
+            this.IsSupportCopy = false;
+            this.IsSupportPrint = false;
+            this.IsSupportUnconfirm = true;
+            this.KeyField1 = "ID";
+            this.Name = "P11";
+            this.Text = "P11. Overrun garment record";
+            this.UnApvChkValue = "Confirmed";
+            this.UniqueExpress = "ID";
+            this.WorkAlias = "OverrunGMT";
+            this.Controls.SetChildIndex(this.tabs, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.detailgridbs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.detailgrid2bs)).EndInit();
+            this.masterpanel.ResumeLayout(false);
+            this.masterpanel.PerformLayout();
+            this.detailpanel.ResumeLayout(false);
+            this.detailpanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).EndInit();
+            this.detail.ResumeLayout(false);
+            this.detailcont.ResumeLayout(false);
+            this.detailbtm.ResumeLayout(false);
+            this.detailbtm.PerformLayout();
+            this.tabs.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.DisplayBox displayBox2;
+        private Win.UI.DisplayBox displayBox1;
+        private Win.UI.TextBox textBox1;
+        private Win.UI.Label label4;
+        private Win.UI.Label label3;
+        private Win.UI.Label label2;
+        private Win.UI.Label label1;
+        private Win.UI.DisplayBox displayBox3;
+        private Win.UI.DateBox dateBox1;
+        private Win.UI.Label label6;
+        private Win.UI.Label label5;
+        private Win.UI.TextBox textBox2;
+    }
+}
---
+++ Sci.Production.Packing/P11.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict.Win;
+using Ict;
+using Sci.Data;
+
+namespace Sci.Production.Packing
+{
+    public partial class P11 : Sci.Win.Tems.Input6
+    {
+        private string sqlCmd, masterID;
+        private DataRow dr, dr1;
+        private DialogResult buttonResult;
+        private DualResult result;
+        Ict.Win.DataGridViewGeneratorTextColumnSettings article = new Ict.Win.DataGridViewGeneratorTextColumnSettings();
+        Ict.Win.DataGridViewGeneratorTextColumnSettings size = new Ict.Win.DataGridViewGeneratorTextColumnSettings();
+        Ict.Win.DataGridViewGeneratorTextColumnSettings reason = new Ict.Win.DataGridViewGeneratorTextColumnSettings();
+
+        public P11(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            this.DefaultFilter = "FactoryID = '" + Sci.Env.User.Factory + "'";
+            detailgrid.AllowUserToOrderColumns = true;
+        }
+
+        protected override DualResult OnDetailSelectCommandPrepare(PrepareDetailSelectCommandEventArgs e)
+        {
+            masterID = (e.Master == null) ? "" : e.Master["ID"].ToString();
+
+            this.DetailSelectCommand = string.Format(@"select od.*,pr.Description
+from OverrunGMT_Detail od
+left join PackingReason pr on pr.Type = 'OG' and pr.ID = od.PackingReasonID
+where od.ID = '{0}'",masterID);
+
+            return base.OnDetailSelectCommandPrepare(e);
+        }
+
+        protected override void OnDetailEntered()
+        {
+            base.OnDetailEntered();
+
+            //帶出Orders相關欄位
+            sqlCmd = string.Format("select StyleID,SeasonID from Orders where ID = '{0}'", CurrentMaintain["ID"].ToString());
+            if (MyUtility.Check.Seek(sqlCmd, out dr))
+            {
+                displayBox1.Value = dr["StyleID"].ToString();
+                displayBox2.Value = dr["SeasonID"].ToString();
+            }
+        }
+
+        protected override void OnDetailGridSetup()
+        {
+            base.OnDetailGridSetup();
+
+            #region Article & SizeCode & Reason ID按右鍵與Validating
+            article.EditingMouseDown += (s, e) =>
+            {
+                if (this.EditMode)
+                {
+                    if (e.Button == System.Windows.Forms.MouseButtons.Right)
+                    {
+                        if (e.RowIndex != -1)
+                        {
+                            dr = this.detailgrid.GetDataRow<DataRow>(e.RowIndex);
+                            sqlCmd = string.Format("Select Article,SizeCode from Order_Qty where ID = '{0}'", CurrentMaintain["ID"].ToString());
+                            Sci.Win.Tools.SelectItem item = new Sci.Win.Tools.SelectItem(sqlCmd, "8", dr["Article"].ToString());
+                            DialogResult returnResult = item.ShowDialog();
+                            if (returnResult == DialogResult.Cancel) { return; }
+                            IList<DataRow> ildr = item.GetSelecteds();
+                            dr["Article"] = ildr[0]["Article"].ToString();
+                            dr["SizeCode"] = ildr[0]["SizeCode"].ToString();
+                            dr.EndEdit();
+                        }
+                    }
+                }
+            };
+
+            article.CellValidating += (s, e) =>
+            {
+                if (this.EditMode)
+                {
+                    dr = this.detailgrid.GetDataRow<DataRow>(e.RowIndex);
+                    if (!MyUtility.Check.Empty(e.FormattedValue) && e.FormattedValue.ToString() != dr["Article"].ToString())
+                    {
+                        if (!MyUtility.Check.Seek(string.Format("Select Article from Order_Qty where ID = '{0}' and Article = '{1}'", CurrentMaintain["OrderID"].ToString(), e.FormattedValue.ToString())))
+                        {
+                            MyUtility.Msg.WarningBox(string.Format("< Article: {0} > not found!!!", e.FormattedValue.ToString()));
+                            dr["Article"] = "";
+                        }
+                        else
+                        {
+                            dr["Article"] = e.FormattedValue.ToString();
+                        }
+                        dr["SizeCode"] = "";
+                        dr.EndEdit();
+                    }
+                }
+            };
+
+            size.EditingMouseDown += (s, e) =>
+            {
+                if (this.EditMode)
+                {
+                    if (e.Button == System.Windows.Forms.MouseButtons.Right)
+                    {
+                        if (e.RowIndex != -1)
+                        {
+                            dr = this.detailgrid.GetDataRow<DataRow>(e.RowIndex);
+                            sqlCmd = string.Format("Select distinct SizeCode from Order_Qty where ID = '{0}' and Article = '{1}'", CurrentMaintain["OrderID"].ToString(), dr["Article"].ToString());
+                            Sci.Win.Tools.SelectItem item = new Sci.Win.Tools.SelectItem(sqlCmd, "8", dr["SizeCode"].ToString());
+                            DialogResult returnResult = item.ShowDialog();
+                            if (returnResult == DialogResult.Cancel) { return; }
+                            e.EditingControl.Text = item.GetSelectedString();
+                        }
+                    }
+                }
+            };
+
+            size.CellValidating += (s, e) =>
+            {
+                if (this.EditMode)
+                {
+                    dr = this.detailgrid.GetDataRow<DataRow>(e.RowIndex);
+                    if (!MyUtility.Check.Empty(e.FormattedValue) && e.FormattedValue.ToString() != dr["SizeCode"].ToString())
+                    {
+                        if (!MyUtility.Check.Seek(string.Format("Select SizeCode from Order_Qty where ID = '{0}' and Article = '{1}' SizeCode = '{2}'", CurrentMaintain["OrderID"].ToString(), dr["Article"].ToString(), e.FormattedValue.ToString())))
+                        {
+                            MyUtility.Msg.WarningBox(string.Format("< SizeCode: {0} > not found!!!", e.FormattedValue.ToString()));
+                            dr["SizeCode"] = "";
+                            e.Cancel = true;
+                            return;
+                        }
+                    }
+                }
+            };
+
+            reason.EditingMouseDown += (s, e) =>
+            {
+                if (this.EditMode)
+                {
+                    if (e.Button == System.Windows.Forms.MouseButtons.Right)
+                    {
+                        if (e.RowIndex != -1)
+                        {
+                            dr = this.detailgrid.GetDataRow<DataRow>(e.RowIndex);
+                            Sci.Win.Tools.SelectItem item = new Sci.Win.Tools.SelectItem("Select ID, Description from PackingReason where Type = 'OG' and Junk = 0", "8,30", dr["PackingReasonID"].ToString());
+                            DialogResult returnResult = item.ShowDialog();
+                            if (returnResult == DialogResult.Cancel) { return; }
+                            e.EditingControl.Text = item.GetSelectedString();
+                        }
+                    }
+                }
+            };
+
+            reason.CellValidating += (s, e) =>
+            {
+                if (this.EditMode)
+                {
+                    dr = this.detailgrid.GetDataRow<DataRow>(e.RowIndex);
+                    if (!MyUtility.Check.Empty(e.FormattedValue) && e.FormattedValue.ToString() != dr["PackingReasonID"].ToString())
+                    {
+                        if (!MyUtility.Check.Seek(string.Format("Select ID, Description from PackingReason where Type = 'OG' and Junk = 0 and ID = '{0}'", e.FormattedValue.ToString()), out dr1))
+                        {
+                            MyUtility.Msg.WarningBox(string.Format("< Reason ID: {0} > not found!!!", e.FormattedValue.ToString()));
+                            dr["PackingReasonID"] = "";
+                            e.Cancel = true;
+                            return;
+                        }
+                        else
+                        {
+                            dr["PackingReasonID"] = e.FormattedValue.ToString();
+                            dr["Description"] = dr1["Description"].ToString();
+                            dr.EndEdit();
+                        }
+                    }
+                }
+            };
+            #endregion
+
+            Helper.Controls.Grid.Generator(this.detailgrid)
+                .Text("Article", header: "Color way", width: Widths.AnsiChars(8), settings: article)
+                .Text("SizeCode", header: "Size", width: Widths.AnsiChars(8), settings: size)
+                .Numeric("Qty", header: "Q'ty")
+                .Text("PackingReasonID", header: "Reason ID", width: Widths.AnsiChars(5), settings: reason)
+                .Text("Description", header: "Reason Description", width: Widths.AnsiChars(40),iseditingreadonly: true);
+        }
+
+        protected override void ClickNewAfter()
+        {
+            base.ClickNewAfter();
+            CurrentMaintain["FactoryID"] = Sci.Env.User.Factory;
+            CurrentMaintain["Status"] = "New";
+            CurrentMaintain["CloseDate"] = DateTime.Today;
+        }
+
+        protected override bool ClickEditBefore()
+        {
+            if (CurrentMaintain["Status"].ToString() == "Confirmed")
+            {
+                MyUtility.Msg.WarningBox("This record is < Confirmed >, can't be modified!");
+                return false;
+            }
+            return base.ClickEditBefore();
+        }
+
+        protected override void ClickEditAfter()
+        {
+            base.ClickEditAfter();
+            this.textBox1.ReadOnly = true;
+        }
+
+        protected override bool ClickSaveBefore()
+        {
+            if (MyUtility.Check.Empty(CurrentMaintain["ID"]))
+            {
+                MyUtility.Msg.WarningBox("SP# can't empty!!");
+                textBox1.Focus();
+                return false;
+            }
+            if (MyUtility.Check.Empty(CurrentMaintain["CloseDate"]))
+            {
+                MyUtility.Msg.WarningBox("Date can't empty!!");
+                dateBox1.Focus();
+                return false;
+            }
+
+            int count = 0;
+            foreach (DataRow detail in DetailDatas)
+            {
+                if (MyUtility.Check.Empty(detail["Article"]) || MyUtility.Check.Empty(detail["SizeCode"]) || MyUtility.Check.Empty(detail["Qty"]))
+                {
+                    detail.Delete();
+                    continue;
+                }
+
+                if (MyUtility.Check.Empty(detail["PackingReasonID"]))
+                {
+                    MyUtility.Msg.WarningBox("Reason ID can't be empty!");
+                    return false;
+                }
+                count = count + 1;
+            }
+            if (count == 0)
+            {
+                MyUtility.Msg.WarningBox("< Detail > can't be empty!");
+                detailgrid.Focus();
+                return false;
+            }
+            return base.ClickSaveBefore();
+        }
+
+        protected override bool ClickDeleteBefore()
+        {
+            if (CurrentMaintain["Status"].ToString() == "Confirmed")
+            {
+                MyUtility.Msg.WarningBox("This record is < Confirmed >, can't be deleted!");
+                return false;
+            }
+
+            return base.ClickDeleteBefore();
+        }
+
+        //檢查輸入的SP#是否正確
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+            if (EditMode)
+            {
+                if (textBox1.Text != textBox1.OldValue)
+                {
+                    if (!MyUtility.Check.Empty(textBox1.Text))
+                    {
+                        string sqlCmd = string.Format("select ID, StyleID, SeasonID from Orders where ID = '{0}' and FtyGroup = '{1}' and GMTClose is not null", textBox1.Text, Sci.Env.User.Factory);
+                        if (MyUtility.Check.Seek(sqlCmd, out dr))
+                        {
+                            //OrderID異動，其他相關欄位要跟著異動
+                            displayBox1.Value = dr["StyleID"].ToString();
+                            displayBox2.Value = dr["SeasonID"].ToString();
+                        }
+                        else
+                        {
+                            MyUtility.Msg.WarningBox("This SP# is not local order!");
+                            //OrderID異動，其他相關欄位要跟著異動
+                            textBox1.Text = "";
+                            displayBox1.Value = "";
+                            displayBox2.Value = "";
+                            e.Cancel = true;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        //Confirm
+        protected override void ClickConfirm()
+        {
+            base.ClickConfirm();
+            sqlCmd = string.Format("update OverrunGMT set Status = 'Confirmed', EditName = '{0}', EditDate = '{1}' where ID = '{2}'", Sci.Env.User.UserID, DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"), CurrentMaintain["ID"].ToString());
+
+            result = DBProxy.Current.Execute(null, sqlCmd);
+            if (!result)
+            {
+                MyUtility.Msg.WarningBox("Confirm failed, Pleaes re-try");
+            }
+
+            RenewData();
+            OnDetailEntered();
+            EnsureToolbarExt();
+        }
+
+        //UnConfirm
+        protected override void ClickUnconfirm()
+        {
+            base.ClickUnconfirm();
+            
+            //問是否要做Unconfirm，確定才繼續往下做
+            buttonResult = MyUtility.Msg.WarningBox("Are you sure you want to < Unconfirm > this data?", "Warning", MessageBoxButtons.YesNo);
+            if (buttonResult == System.Windows.Forms.DialogResult.No)
+            {
+                return;
+            }
+
+            sqlCmd = string.Format("update OverrunGMT set Status = 'New', EditName = '{0}', EditDate = '{1}' where ID = '{2}'", Sci.Env.User.UserID, DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"), CurrentMaintain["ID"].ToString());
+
+            result = DBProxy.Current.Execute(null, sqlCmd);
+            if (!result)
+            {
+                MyUtility.Msg.WarningBox("UnConfirm failed, Pleaes re-try");
+            }
+
+            RenewData();
+            OnDetailEntered();
+            EnsureToolbarExt();
+        }
+    }
+}
