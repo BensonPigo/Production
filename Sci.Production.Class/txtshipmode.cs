f332a29f62b0e130e8b99e6d408165fa16134df8
YINMEI <yinmei@sportscity.com.tw>
2015-07-28 03:07:54 +0000

    新增useFunction參數

--- Sci.Production.Class/txtshipmode.cs
+++ Sci.Production.Class/txtshipmode.cs
@@ -14,18 +14,40 @@ namespace Sci.Production.Class
 {
     public partial class txtshipmode : Sci.Win.UI.ComboBox
     {
+        private string useFunction;
+        [Category("Custom Properties")]
+        public string UseFunction
+        {
+            set 
+            {
+                if (!Env.DesignTime)
+                {
+                    this.useFunction = value;
+                    string sqlCMD;
+                    if (MyUtility.Check.Empty(this.useFunction))
+                    {
+                        sqlCMD = "select ID from ShipMode order by ID";
+                    }
+                    else
+                    {
+                        sqlCMD = string.Format("select ID from ShipMode where PATINDEX('%{0}%',UseFunction) <> 0", useFunction.Trim());
+                    }
+                    Ict.DualResult cbResult;
+                    DataTable ShipModeTable = new DataTable();
+                    if (cbResult = DBProxy.Current.Select(null, sqlCMD, out ShipModeTable))
+                    {
+                        this.DataSource = ShipModeTable;
+                        this.DisplayMember = "ID";
+                        this.ValueMember = "ID";
+                        this.Size = new System.Drawing.Size(80, 24);
+                    }
+                }
+            }
+            get { return this.useFunction; }
+        }
+
         public txtshipmode()
         {
-            string sqlCMD = "select ID from ShipMode order by ID";
-            Ict.DualResult cbResult;
-            DataTable ShipModeTable = new DataTable();
-            if (cbResult = DBProxy.Current.Select(null, sqlCMD, out ShipModeTable))
-            {
-                this.DataSource = ShipModeTable;
-                this.DisplayMember = "ID";
-                this.ValueMember = "ID";
-                this.Size = new System.Drawing.Size(80, 24);
-            }
         }
     }
 }
