84472c20d710280f972f0f2a94e487e8f47660c8
YINMEI <yinmei@sportscity.com.tw>
2015-08-17 07:36:50 +0000

    Add P09

---
+++ Sci.Production.Shipping/P09.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Sci.Production.Shipping
+{
+    partial class P09
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new Sci.Win.UI.Panel();
+            this.panel2 = new Sci.Win.UI.Panel();
+            this.panel3 = new Sci.Win.UI.Panel();
+            this.panel4 = new Sci.Win.UI.Panel();
+            this.listControlBindingSource1 = new Sci.Win.UI.ListControlBindingSource();
+            this.panel5 = new Sci.Win.UI.Panel();
+            this.grid1 = new Sci.Win.UI.Grid();
+            this.label1 = new Sci.Win.UI.Label();
+            this.dateRange1 = new Sci.Win.UI.DateRange();
+            this.label2 = new Sci.Win.UI.Label();
+            this.txtbrand1 = new Sci.Production.Class.txtbrand();
+            this.label3 = new Sci.Win.UI.Label();
+            this.comboBox1 = new Sci.Win.UI.ComboBox();
+            this.label4 = new Sci.Win.UI.Label();
+            this.comboBox2 = new Sci.Win.UI.ComboBox();
+            this.button1 = new Sci.Win.UI.Button();
+            this.button2 = new Sci.Win.UI.Button();
+            this.button3 = new Sci.Win.UI.Button();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource1)).BeginInit();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(5, 492);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(957, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(5, 492);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.comboBox2);
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.comboBox1);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.txtbrand1);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.dateRange1);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(5, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(952, 45);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button3);
+            this.panel4.Controls.Add(this.button2);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(5, 446);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(952, 46);
+            this.panel4.TabIndex = 4;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.grid1);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(5, 45);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(952, 401);
+            this.panel5.TabIndex = 5;
+            // 
+            // grid1
+            // 
+            this.grid1.AllowUserToAddRows = false;
+            this.grid1.AllowUserToDeleteRows = false;
+            this.grid1.AllowUserToResizeRows = false;
+            this.grid1.AutoGenerateColumns = false;
+            this.grid1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.grid1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.grid1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.grid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid1.DataSource = this.listControlBindingSource1;
+            this.grid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid1.EditingEnter = Ict.Win.UI.DataGridViewEditingEnter.NextCellOrNextRow;
+            this.grid1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.grid1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.grid1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(162)))), ((int)(((byte)(163)))));
+            this.grid1.Location = new System.Drawing.Point(0, 0);
+            this.grid1.Name = "grid1";
+            this.grid1.RowHeadersVisible = false;
+            this.grid1.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.grid1.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.grid1.RowTemplate.Height = 24;
+            this.grid1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grid1.Size = new System.Drawing.Size(952, 401);
+            this.grid1.TabIndex = 0;
+            this.grid1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(4, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Buyer Delivery";
+            // 
+            // dateRange1
+            // 
+            this.dateRange1.IsSupportEditMode = false;
+            this.dateRange1.Location = new System.Drawing.Point(102, 12);
+            this.dateRange1.Name = "dateRange1";
+            this.dateRange1.Size = new System.Drawing.Size(253, 23);
+            this.dateRange1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(377, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Brand";
+            // 
+            // txtbrand1
+            // 
+            this.txtbrand1.BackColor = System.Drawing.Color.White;
+            this.txtbrand1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtbrand1.IsSupportEditMode = false;
+            this.txtbrand1.Location = new System.Drawing.Point(427, 12);
+            this.txtbrand1.Name = "txtbrand1";
+            this.txtbrand1.Size = new System.Drawing.Size(66, 23);
+            this.txtbrand1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(513, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Factory";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.EditMode = Sci.Win.UI.AdvEditModes.None;
+            this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(573, 11);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(74, 24);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(667, 11);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 23);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Data Type";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.Color.White;
+            this.comboBox2.EditMode = Sci.Win.UI.AdvEditModes.None;
+            this.comboBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(739, 11);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(103, 24);
+            this.comboBox2.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(868, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 30);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Query";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(697, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 30);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "To Excel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button3.Location = new System.Drawing.Point(856, 7);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 30);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Close";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // P09
+            // 
+            this.CancelButton = this.button3;
+            this.ClientSize = new System.Drawing.Size(962, 492);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.EditMode = true;
+            this.Name = "P09";
+            this.Text = "P09. Delay shipment order detail";
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.Controls.SetChildIndex(this.panel2, 0);
+            this.Controls.SetChildIndex(this.panel3, 0);
+            this.Controls.SetChildIndex(this.panel4, 0);
+            this.Controls.SetChildIndex(this.panel5, 0);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource1)).EndInit();
+            this.panel5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Win.UI.Panel panel1;
+        private Win.UI.Panel panel2;
+        private Win.UI.Panel panel3;
+        private Win.UI.Panel panel4;
+        private Win.UI.ListControlBindingSource listControlBindingSource1;
+        private Win.UI.Panel panel5;
+        private Win.UI.Grid grid1;
+        private Win.UI.Label label3;
+        private Class.txtbrand txtbrand1;
+        private Win.UI.Label label2;
+        private Win.UI.DateRange dateRange1;
+        private Win.UI.Label label1;
+        private Win.UI.Button button1;
+        private Win.UI.ComboBox comboBox2;
+        private Win.UI.Label label4;
+        private Win.UI.ComboBox comboBox1;
+        private Win.UI.Button button3;
+        private Win.UI.Button button2;
+    }
+}
---
+++ Sci.Production.Shipping/P09.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict.Win;
+using Ict;
+using Sci.Data;
+
+namespace Sci.Production.Shipping
+{
+    public partial class P09 : Sci.Win.Tems.QueryForm
+    {
+        DataTable FtyGroup;
+        IList<string> comboBox2_RowSource = new List<string>();
+        BindingSource comboxbs2;
+        public P09(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnFormLoaded()
+        {
+            base.OnFormLoaded();
+            DualResult result;
+            #region Combox
+            if (result = DBProxy.Current.Select(null, "select distinct FtyGroup from Factory where Junk = 0 ", out FtyGroup))
+            {
+                this.comboBox1.DataSource = FtyGroup;
+                this.comboBox1.DisplayMember = "FtyGroup";
+                this.comboBox1.ValueMember = "FtyGroup";
+            }
+            else
+            {
+                MyUtility.Msg.ErrorBox("Query Combox fail\r\n"+result.ToString());
+            }
+            comboBox1.SelectedIndex = -1;
+
+            comboBox2_RowSource.Add("Shipped");
+            comboBox2_RowSource.Add("Not Ship");
+            comboBox2_RowSource.Add("All");
+            comboxbs2 = new BindingSource(comboBox2_RowSource, null);
+            comboBox2.DataSource = comboxbs2;
+            comboBox2.SelectedIndex = 0;
+            #endregion 
+
+            grid1.DataSource = listControlBindingSource1;
+            grid1.IsEditingReadOnly = true;
+            Helper.Controls.Grid.Generator(this.grid1)
+                .Text("ID", header: "SP#", width: Widths.AnsiChars(13))
+                .Text("Seq", header: "Seq", width: Widths.AnsiChars(2))
+                .Text("StyleID", header: "Style", width: Widths.AnsiChars(15))
+                .Text("BrandID", header: "Brand", width: Widths.AnsiChars(8))
+                .Text("FactoryID", header: "Factory", width: Widths.AnsiChars(3))
+                .Text("CustPONo", header: "PO#", width: Widths.AnsiChars(15))
+                .Text("Customize1", header: "Order#", width: Widths.AnsiChars(15))
+                .Text("CustCDID", header: "Cust#", width: Widths.AnsiChars(15))
+                .Text("Alias", header: "Dest", width: Widths.AnsiChars(13))
+                .Numeric("Qty", header: "Order Q'ty", width: Widths.AnsiChars(5))
+                .Date("BuyerDelivery", header: "Buyer Delivery")
+                .Date("PulloutDate", header: "Extended")
+                .Text("OSReason", header: "Outstanding Reason", width: Widths.AnsiChars(20))
+                .Text("OSRemark", header: "Outstanding Remark", width: Widths.AnsiChars(20));
+        }
+
+        //Query
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MyUtility.Check.Empty(dateRange1.Value1))
+            {
+                MyUtility.Msg.WarningBox("< Buyer Delivery > can not empty!");
+                dateRange1.TextBox1.Focus();
+                return;
+            }
+            StringBuilder sqlCmd = new StringBuilder();
+            sqlCmd.Append(string.Format(@"with tempData 
+as 
+(select o.ID,oq.Seq,o.StyleID,o.BrandID,o.FactoryID,o.CustPONo,o.Customize1,o.CustCDID,
+ c.Alias,oq.Qty,oq.BuyerDelivery,oq.EstPulloutDate,
+ (select max(PulloutDate) from Pullout_Detail where OrderID = o.ID and OrderShipmodeSeq = oq.Seq and ShipQty > 0) as PulloutDate,
+ oq.OutstandingReason+' - '+isnull(r.Name,'') as OSReason,o.OutstandingRemark 
+ from Orders o 
+ left join Order_QtyShip oq on oq.Id = o.ID 
+ left join Country c on c.ID = o.Dest 
+ left join Reason r on r.ReasonTypeID = 'Delivery_OutStand' and r.ID = oq.OutstandingReason 
+ where o.IsForecast = 0 
+ and o.LocalOrder = 0 
+ and oq.BuyerDelivery >= '{0}' 
+ and oq.BuyerDelivery <= '{1}' ", Convert.ToDateTime(dateRange1.Value1).ToString("d"),Convert.ToDateTime(dateRange1.Value2).ToString("d")));
+            if (!MyUtility.Check.Empty(txtbrand1.Text))
+            {
+                sqlCmd.Append(string.Format(" and o.BrandID = '{0}'",txtbrand1.Text));
+            }
+
+            if (!MyUtility.Check.Empty(comboBox1.SelectedValue))
+            {
+                sqlCmd.Append(string.Format(" and o.FtyGroup = '{0}'",comboBox1.SelectedValue.ToString().Trim()));
+            }
+            sqlCmd.Append(@") 
+select ID,Seq,StyleID,BrandID,FactoryID,CustPONo,Customize1,CustCDID,Alias,Qty,
+BuyerDelivery,iif(PulloutDate is null,EstPulloutDate,PulloutDate) as PulloutDate,
+OSReason,OutstandingRemark 
+from tempData 
+where iif(PulloutDate is null,EstPulloutDate,PulloutDate) > BuyerDelivery
+and iif(PulloutDate is null,EstPulloutDate,PulloutDate) is not null ");
+            if (comboBox2.SelectedIndex != -1)
+            {
+                if (comboBox2.SelectedValue.ToString() == "Shipped")
+                {
+                    sqlCmd.Append("and PulloutDate is not null");
+                }
+                else
+                {
+                    if (comboBox2.SelectedValue.ToString() == "Not Ship")
+                    {
+                        sqlCmd.Append("and PulloutDate is null");
+                    }
+                }
+            }
+            DataTable gridData;
+            DualResult result = DBProxy.Current.Select(null, Convert.ToString(sqlCmd), out gridData);
+            if (!result)
+            {
+                MyUtility.Msg.ErrorBox("Query fail!\r\n" + result.ToString());
+            }
+            else
+            {
+                if (gridData.Rows.Count == 0)
+                {
+                    MyUtility.Msg.WarningBox("Data not found!");
+                }
+            }
+            listControlBindingSource1.DataSource = gridData;
+        }
+
+        //To Excel
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //Close
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
