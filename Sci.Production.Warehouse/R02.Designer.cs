63fa5973f0cae5917bd6d124b14d9edffbab68a1
MIKE <mike@sportscity.com.tw>
2015-08-19 01:15:24 +0000

    Add R02

---
+++ Sci.Production.Warehouse/R02.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Sci.Production.Warehouse
+{
+    partial class R02
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateRange1 = new Sci.Win.UI.DateRange();
+            this.label1 = new Sci.Win.UI.Label();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Enabled = false;
+            this.print.Location = new System.Drawing.Point(372, 12);
+            this.print.Visible = false;
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(372, 48);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(372, 84);
+            // 
+            // dateRange1
+            // 
+            this.dateRange1.Location = new System.Drawing.Point(86, 19);
+            this.dateRange1.Name = "dateRange1";
+            this.dateRange1.Size = new System.Drawing.Size(280, 23);
+            this.dateRange1.TabIndex = 93;
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(8, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 23);
+            this.label1.TabIndex = 94;
+            this.label1.Text = "Issue Date";
+            // 
+            // R02
+            // 
+            this.ClientSize = new System.Drawing.Size(464, 205);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateRange1);
+            this.Name = "R02";
+            this.Text = "R02. Bulk To Scrap Transaction List";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.dateRange1, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Win.UI.DateRange dateRange1;
+        private Win.UI.Label label1;
+    }
+}
---
+++ Sci.Production.Warehouse/R02.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Sci;
+using Sci.Data;
+using Ict;
+using Ict.Win;
+
+namespace Sci.Production.Warehouse
+{
+    public partial class R02 : Sci.Win.Tems.PrintForm
+    {
+        DataTable dt;
+        public R02(ToolStripMenuItem menuitem)
+            :base(menuitem)
+        {
+            InitializeComponent();
+            //this.ReportResourceName = "DCLC NAME";
+            this.EditMode = true;
+        }
+
+        protected override bool ValidateInput()
+        {
+            if (MyUtility.Check.Empty(this.dateRange1.TextBox1.Value))
+            {
+                MyUtility.Msg.WarningBox("Issue date can't be empty!!");
+                return false;
+            }
+            return base.ValidateInput();
+        }
+
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            if (dt.Rows.Count == 0)
+            {
+                MyUtility.Msg.InfoBox("Data not found!!");
+                return false;
+            }
+
+            DualResult result = Result.True;
+            SaveFileDialog sfldg = new SaveFileDialog();
+            MyUtility.Excel.SaveXlsFile("Warehouse_R02_Scarp_List", out sfldg);
+            result = MyUtility.Excel.CopyToXls(dt, sfldg.FileName);
+            if (result)
+            {
+                MyUtility.Excel.XlsAutoFit(sfldg.FileName, "Warehouse_R02.xlt");
+                return true;
+            }
+            else
+                return false;
+        }
+
+        protected override DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            //return base.OnAsyncDataLoad(e);
+            DualResult result = Result.True;
+            
+            try
+            {
+                string sqlcmd =string.Format(@"select a.issuedate, b.poid, b.seq1,b.seq2,'' as description, sum(b.qty) qty, 
+(select stockunit from po_supp_detail where id= b.poid and seq1 = b.seq1 and seq2 = b.seq2) as unit
+from scrap a inner join scrap_detail b on a.id = b.id
+where a.Status = 'Approved' and a.type='A' 
+and a.issuedate between '{0}' and '{1}'
+group by a.issuedate , b.poid, b.seq1,b.Seq2
+",dateRange1.Text1,dateRange1.Text2);
+                result = DBProxy.Current.Select(null, sqlcmd, out dt);
+                if (!result) return result;
+                foreach (DataRow dr in dt.Rows)
+                {
+                    dr[4] = PublicPrg.Prgs.GetMtlDesc(dr["poid"].ToString(), dr["seq1"].ToString(), dr["seq2"].ToString(), 3);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return result;
+        }
+    }
+}
