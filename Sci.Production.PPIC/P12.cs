a1939a07f75f97e249bfd3608f118adaf3d1b7f5
YINMEI <yinmei@sportscity.com.tw>
2015-08-21 06:58:36 +0000

    Add P12

---
+++ Sci.Production.PPIC/P12.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.PPIC
+{
+    public partial class P12 : Sci.Win.Tems.Input1
+    {
+        public P12(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            this.DefaultFilter = "FTYGroup = '" + Sci.Env.User.Factory + "'";
+        }
+
+        protected override void OnDetailEntered()
+        {
+            base.OnDetailEntered();
+            numericBox3.Value = Convert.ToDecimal(MyUtility.GetValue.Lookup(string.Format("select isnull(sum(QAQty),0) as QAQty from SewingOutput_Detail where OrderId = '{0}'",CurrentMaintain["ID"].ToString())));
+        }
+    }
+}
