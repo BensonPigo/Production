9606e4c8e9a4635d10592da6cb7e3aee0badb4d2
SPIN <spin@sportscity.com.tw>
2015-09-11 02:04:40 +0000

    ADD B01,B02

---
+++ Sci.Production.Miscellaneous/B01.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Miscellaneous
+{
+    public partial class B01 : Sci.Win.Tems.Input1
+    {
+        public B01(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+        }
+        protected override bool ClickEditBefore()
+        {
+            if (CurrentMaintain["Local"].ToString() == "0")
+            {
+                MyUtility.Msg.WarningBox("Only Local Supplier can modify.");
+                return false;
+            }
+            
+            return base.ClickEditBefore();
+        }
+        protected override void ClickEditAfter()
+        {
+            base.ClickEditAfter();
+            this.textBox1.ReadOnly = true;
+        }
+        protected override void ClickNewAfter()
+        {
+            base.ClickNewAfter();
+            CurrentMaintain["Local"] = 1;
+        }
+    }
+}
