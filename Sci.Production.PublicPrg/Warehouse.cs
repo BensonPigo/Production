75c1b87df435c54a77e1ca1c6044889723d6c56d
MIKE <mike@sportscity.com.tw>
2015-09-16 06:26:40 +0000

    字串相加改成stringbuilder material description改呼叫sql function

--- Sci.Production.PublicPrg/Warehouse.cs
+++ Sci.Production.PublicPrg/Warehouse.cs
@@ -26,12 +26,12 @@ namespace Sci.Production.PublicPrg
         /// <returns>String Desc</returns>
         public static string GetMtlDesc(string Poid, string seq1, string seq2, int ReturnFormat, bool Repeat = false, System.Data.SqlClient.SqlConnection conn=null)
         {
-            string rtn="";
+            StringBuilder rtn = new StringBuilder();
             DataRow dr;
-            DataTable dt;
+            DataTable dt,dt2;
             if (null == conn)
             {
-                MyUtility.Check.Seek(string.Format(@"select StockPOID,scirefno,SuppColor,colorid,ColorDetail,sizespec
+                MyUtility.Check.Seek(string.Format(@"select StockPOID+StockSeq1+StockSeq2,scirefno,SuppColor,colorid,ColorDetail,sizespec
                                             ,special,SizeUnit,remark from po_supp_detail where id ='{0}' and seq1 = '{1}' and seq2 = '{2}'
 union all
 select '',scirefno,'',colorid,'',sizespec
@@ -41,7 +41,7 @@ select '',scirefno,'',colorid,'',sizespec
             }
             else
             {
-                DBProxy.Current.SelectByConn(conn, string.Format(@"select StockPOID,scirefno,SuppColor,colorid,ColorDetail,sizespec
+                DBProxy.Current.SelectByConn(conn, string.Format(@"select StockPOID+StockSeq1+StockSeq2,scirefno,SuppColor,colorid,ColorDetail,sizespec
                                             ,special,SizeUnit,remark 
 from po_supp_detail where id ='{0}' and seq1 = '{1}' and seq2 = '{2}'
 union all
@@ -77,7 +77,7 @@ from PO_Artwork where id ='{0}' and seq1 = '{1}' and seq2 = '{2}'"
                                         select DescDetail from fabric where SCIRefno = '{0}'", dr["scirefno"].ToString().Replace("'", "''")), out dt);
                     }
                     if (!MyUtility.Check.Empty(dt) && dt.Rows.Count > 0)
-                    { rtn += dt.Rows[0][0].ToString(); }
+                    { rtn.Append( dt.Rows[0][0].ToString()); }
                     break;
                 case 3: // 只顯示Fabric.DescDetail的第一列
                     if (!Repeat && !MyUtility.Check.Empty(dr["scirefno"]))
@@ -89,7 +89,7 @@ from PO_Artwork where id ='{0}' and seq1 = '{1}' and seq2 = '{2}'"
 
                         if (descs.Length > 0)
                         {
-                            rtn = rtn + descs[0];
+                            rtn.Append(descs[0]);
                         }
                     }
                     break;
@@ -100,41 +100,29 @@ from PO_Artwork where id ='{0}' and seq1 = '{1}' and seq2 = '{2}'"
 
             if (Repeat || ReturnFormat == 2 || ReturnFormat==4)
             {
-                rtn += dr["SuppColor"].ToString().TrimEnd();
+                rtn.Append(dr["SuppColor"].ToString().TrimEnd());
                 string colorid = dr["colorid"].ToString();
                 string[] colors = colorid.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
                 if (colors.Length > 0)
                 {
                     for (int i = 0; i < colors.Length; i++)
                     {
-//                        rtn = rtn + colors[i] + "-" + MyUtility.GetValue.Lookup(string.Format(@"select name from color,orders 
-//                                                                                                    where color.brandid = orders.brandid 
-//                                                                                                            and orders.brandid= '{0}' 
-//                                                                                                            and color.id = '{1}'", Poid, colors[i].ToString()));
                         DBProxy.Current.SelectByConn(conn, string.Format(@"select name from color,orders 
                                                                                                     where color.brandid = orders.brandid 
                                                                                                             and orders.brandid= '{0}' 
                                                                                                             and color.id = '{1}'", Poid, colors[i].ToString()), out dt);
                         if (MyUtility.Check.Empty(dt) || dt.Rows.Count == 0) continue;
-                        rtn += colors[i] + "-" + dt.Rows[0][0].ToString();
+                        rtn.Append( colors[i] + "-" + dt.Rows[0][0].ToString());
                     }
                 }
-                if (!MyUtility.Check.Empty(dr["ColorDetail"].ToString())) { rtn += dr["ColorDetail"].ToString() + Environment.NewLine; }
-                if (!MyUtility.Check.Empty(dr["sizespec"].ToString())) { rtn += dr["sizespec"].ToString() + Environment.NewLine; }
-                if (!MyUtility.Check.Empty(dr["SizeUnit"].ToString())) { rtn += dr["SizeUnit"].ToString() + Environment.NewLine; }
-                if (!MyUtility.Check.Empty(dr["Special"].ToString())) { rtn += dr["Special"].ToString() + Environment.NewLine; }
-                if (!MyUtility.Check.Empty(dr["Remark"].ToString())) { rtn += dr["Remark"].ToString() + Environment.NewLine; }
+                if (!MyUtility.Check.Empty(dr["ColorDetail"].ToString())) { rtn.Append( dr["ColorDetail"].ToString() + Environment.NewLine); }
+                if (!MyUtility.Check.Empty(dr["sizespec"].ToString())) { rtn.Append(dr["sizespec"].ToString() + Environment.NewLine); }
+                if (!MyUtility.Check.Empty(dr["SizeUnit"].ToString())) { rtn.Append( dr["SizeUnit"].ToString() + Environment.NewLine); }
+                if (!MyUtility.Check.Empty(dr["Special"].ToString())) { rtn.Append(dr["Special"].ToString() + Environment.NewLine); }
+                if (!MyUtility.Check.Empty(dr["Remark"].ToString())) { rtn.Append( dr["Remark"].ToString() + Environment.NewLine); }
 
             }
-
-            
-            //mReturn = mReturn + IIF(EMPTY(ALLTRIM(PO3.Old_Desc)), '', 'Color Detail:' + ALLTRIM(PO3.Old_Desc) + CHR(13))
-            //mReturn = mReturn + IIF(EMPTY(ALLTRIM(PO3.SizeSpec)), '', IIF(AT(PO3.ColorID, ',') = 0, ',' + ALLTRIM(PO3.SizeSpec), CHR(10) + ALLTRIM(PO3.SizeSpec)))
-            //mReturn = mReturn + IIF(EMPTY(ALLTRIM(PO3.SizeUnit)), '', ' ' + ALLTRIM(PO3.SizeUnit) + CHR(13))
-            //mReturn = mReturn + IIF(EMPTY(ALLTRIM(PO3.Special)), '', ALLTRIM(PO3.Special) + CHR(13))
-            //mReturn = mReturn + IIF(EMPTY(ALLTRIM(PO3.Spec)), '', ALLTRIM(PO3.Spec) + CHR(13))
-            //mReturn = mReturn + IIF(EMPTY(ALLTRIM(PO3.Remark)), '', ALLTRIM(PO3.Remark) + CHR(13))
-            return rtn;
+            return rtn.ToString() ;
         }
         #endregion
 
@@ -469,7 +457,7 @@ when not matched then
         public static Sci.Win.Tools.SelectItem SelePoItem(string poid, string defaultseq, string filters = null)
         {
             DataTable dt;
-            string sqlcmd = string.Format(@"select p.id poid,left(p.seq1+' ',3)+p.seq2 as seq, p.Refno, '' as Description 
+            string sqlcmd = string.Format(@"select p.id poid,left(p.seq1+' ',3)+p.seq2 as seq, p.Refno, dbo.getmtldesc(p.id,p.seq1,p.seq2,2,0) as Description 
 ,p.ColorID,p.ata,p.InQty,p.pounit,p.StockUnit,p.OutQty,p.AdjustQty
 ,p.inqty - p.OutQty + p.AdjustQty as balance
 ,p.LInvQty
@@ -486,12 +474,7 @@ where id ='{0}'", poid);
             }
 
             DBProxy.Current.Select(null, sqlcmd, out dt);
-            string tmpdesc = "";
-            foreach (DataRow dr in dt.ToList())
-            {
-                tmpdesc = PublicPrg.Prgs.GetMtlDesc(dr["poid"].ToString(), dr["seq1"].ToString(), dr["seq2"].ToString(), 3, false);
-                if (!MyUtility.Check.Empty(tmpdesc)) dr["Description"] = tmpdesc;
-            }
+            
             Sci.Win.Tools.SelectItem selepoitem = new Win.Tools.SelectItem(dt
                             , "Seq,refno,description,colorid,eta,inqty,stockunit,outqty,adjustqty,balanceqty,linvqty"
                             , "6,8,8,8,10,6,6,6,6,6,6", defaultseq, "Seq,Ref#,Description,Color,ETA,In Qty,Stock Unit,Out Qty,Adqty,Balance,Inventory Qty");
@@ -512,7 +495,7 @@ where id ='{0}'", poid);
         public static Sci.Win.Tools.SelectItem SeleShopfloorItem(string poid, string defaultseq, string filters = null)
         {
             DataTable dt;
-            string sqlcmd = string.Format(@"select p.id poid,left(p.seq1+' ',3)+p.seq2 as seq, p.Refno, '' as Description 
+            string sqlcmd = string.Format(@"select p.id poid,left(p.seq1+' ',3)+p.seq2 as seq, p.Refno, dbo.getmtldesc(p.id,.p.seq1,p.seq2,2,0) as Description 
 ,p.ColorID,p.InQty,p.pounit,p.StockUnit,p.OutQty,p.AdjustQty
 ,p.inqty - p.OutQty + p.AdjustQty as balance
 ,p.fabrictype
@@ -525,12 +508,7 @@ where id ='{0}'", poid);
                 sqlcmd += string.Format(" And {0}", filters);
             }
             DBProxy.Current.Select(null, sqlcmd, out dt);
-            string tmpdesc = "";
-            foreach (DataRow dr in dt.ToList())
-            {
-                tmpdesc = PublicPrg.Prgs.GetMtlDesc(dr["poid"].ToString(), dr["seq1"].ToString(), dr["seq2"].ToString(), 3, false);
-                if (!MyUtility.Check.Empty(tmpdesc)) dr["Description"] = tmpdesc;
-            }
+            
             Sci.Win.Tools.SelectItem selepoitem = new Win.Tools.SelectItem(dt
                             , "Seq,refno,description,colorid,inqty,stockunit,outqty,adjustqty,balanceqty"
                             , "6,15,8,8,6,6,6,6,6", defaultseq, "Seq,Ref#,Description,Color,In Qty,Stock Unit,Out Qty,Adqty,Balance");
