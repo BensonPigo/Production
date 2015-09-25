b2dfc8b1e44ee4cd852e03c4e04a814175a7feaa
YINMEI <yinmei@sportscity.com.tw>
2015-09-25 03:20:26 +0000

    調整Refresh按鈕位置

--- Sci.Production.Shipping/P02.Designer.cs
+++ Sci.Production.Shipping/P02.Designer.cs
@@ -237,7 +237,7 @@
             // 
             this.refresh.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.refresh.Dock = System.Windows.Forms.DockStyle.None;
-            this.refresh.Location = new System.Drawing.Point(962, 8);
+            this.refresh.Location = new System.Drawing.Point(909, 8);
             // 
             // detailgridcont
             // 
