122304d11d402d9b133cad912e8e7cc250b46f5f
SPIN <spin@sportscity.com.tw>
2015-10-12 09:37:19 +0000

    add threadcomb

---
+++ Database/dbo/Tables/ThreadComb.sql
@@ -0,0 +1,43 @@
+﻿CREATE TABLE [dbo].[ThreadComb] (
+    [ID]          VARCHAR (10)  CONSTRAINT [DF_ThreadCombo_ID] DEFAULT ('') NOT NULL,
+    [Description] NVARCHAR (60) CONSTRAINT [DF_ThreadCombo_Description] DEFAULT ('') NOT NULL,
+    [Junk]        BIT           CONSTRAINT [DF_ThreadCombo_Junk] DEFAULT ((0)) NULL,
+    [AddName]     VARCHAR (10)  CONSTRAINT [DF_ThreadCombo_AddName] DEFAULT ('') NULL,
+    [AddDate]     DATETIME      NULL,
+    [EditName]    VARCHAR (10)  CONSTRAINT [DF_ThreadCombo_EditName] DEFAULT ('') NULL,
+    [EditDate]    DATETIME      NULL,
+    CONSTRAINT [PK_ThreadCombo] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'最後修改時間', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ThreadComb', @level2type = N'COLUMN', @level2name = N'EditDate';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'最後修改人員', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ThreadComb', @level2type = N'COLUMN', @level2name = N'EditName';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'新增時間', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ThreadComb', @level2type = N'COLUMN', @level2name = N'AddDate';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'新增人員', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ThreadComb', @level2type = N'COLUMN', @level2name = N'AddName';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'取消', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ThreadComb', @level2type = N'COLUMN', @level2name = N'Junk';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'線種說明', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ThreadComb', @level2type = N'COLUMN', @level2name = N'Description';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'線種組合代碼', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ThreadComb', @level2type = N'COLUMN', @level2name = N'ID';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'線種組合基本檔(Thread Combo)', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'ThreadComb';
+
