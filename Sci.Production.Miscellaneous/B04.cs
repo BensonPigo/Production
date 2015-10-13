d291c226d9edf41c3790eeebd6dec7445851b26e
YINMEI <yinmei@sportscity.com.tw>
2015-10-13 09:25:22 +0000

    Add B04

---
+++ Sci.Production.Miscellaneous/B04.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Basic
+{
+    public partial class B04 : Sci.Win.Tems.Input1
+    {
+        public B04(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+        }
+
+        protected override void ClickEditAfter()
+        {
+            base.ClickEditAfter();
+            this.textBox1.ReadOnly = true;
+        }
+
+        protected override bool ClickSaveBefore()
+        {
+            if (String.IsNullOrWhiteSpace(CurrentMaintain["ID"].ToString()))
+            {
+                MyUtility.Msg.WarningBox("< Code > can not be empty!");
+                this.textBox1.Focus();
+                return false;
+            }
+
+            if (String.IsNullOrWhiteSpace(CurrentMaintain["Name"].ToString()))
+            {
+                MyUtility.Msg.WarningBox("< Name > can not be empty!");
+                this.textBox2.Focus();
+                return false;
+            }
+
+            return base.ClickSaveBefore();
+        }
+    }
+}
