5ab2d156848a4a948ce40f30215485477fec47de
YINMEI <yinmei@sportscity.com.tw>
2015-10-20 07:03:08 +0000

    Add P01_MTLImport

---
+++ Sci.Production.PPIC/P01_MTLImport.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace Sci.Production.PPIC
+{
+    partial class P01_MTLImport
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new Sci.Win.UI.Panel();
+            this.panel2 = new Sci.Win.UI.Panel();
+            this.panel3 = new Sci.Win.UI.Panel();
+            this.panel4 = new Sci.Win.UI.Panel();
+            this.panel5 = new Sci.Win.UI.Panel();
+            this.listControlBindingSource1 = new Sci.Win.UI.ListControlBindingSource();
+            this.grid1 = new Sci.Win.UI.Grid();
+            this.button1 = new Sci.Win.UI.Button();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(10, 424);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(846, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(10, 424);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(10, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(836, 10);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(10, 379);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(836, 45);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.grid1);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(10, 10);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(836, 369);
+            this.panel5.TabIndex = 4;
+            // 
+            // grid1
+            // 
+            this.grid1.AllowUserToAddRows = false;
+            this.grid1.AllowUserToDeleteRows = false;
+            this.grid1.AllowUserToResizeRows = false;
+            this.grid1.AutoGenerateColumns = false;
+            this.grid1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.grid1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.grid1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grid1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.grid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid1.DataSource = this.listControlBindingSource1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.grid1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.grid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid1.EditingEnter = Ict.Win.UI.DataGridViewEditingEnter.NextCellOrNextRow;
+            this.grid1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.grid1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.grid1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(162)))), ((int)(((byte)(163)))));
+            this.grid1.Location = new System.Drawing.Point(0, 0);
+            this.grid1.Name = "grid1";
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grid1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.grid1.RowHeadersVisible = false;
+            this.grid1.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.grid1.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.grid1.RowTemplate.Height = 24;
+            this.grid1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grid1.Size = new System.Drawing.Size(836, 369);
+            this.grid1.TabIndex = 0;
+            this.grid1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Location = new System.Drawing.Point(739, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // P01_MTLImport
+            // 
+            this.CancelButton = this.button1;
+            this.ClientSize = new System.Drawing.Size(856, 424);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
+            this.Name = "P01_MTLImport";
+            this.Text = "Material import";
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Win.UI.Panel panel1;
+        private Win.UI.Panel panel2;
+        private Win.UI.Panel panel3;
+        private Win.UI.Panel panel4;
+        private Win.UI.Button button1;
+        private Win.UI.Panel panel5;
+        private Win.UI.Grid grid1;
+        private Win.UI.ListControlBindingSource listControlBindingSource1;
+    }
+}
---
+++ Sci.Production.PPIC/P01_MTLImport.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict;
+using Ict.Win;
+using Sci.Data;
+
+namespace Sci.Production.PPIC
+{
+    public partial class P01_MTLImport : Sci.Win.Subs.Base
+    {
+        private DataRow masterData;
+        public P01_MTLImport(DataRow MasterData)
+        {
+            InitializeComponent();
+            masterData = MasterData;
+        }
+
+        protected override void OnFormLoaded()
+        {
+            base.OnFormLoaded();
+            DataTable MTLImport;
+            #region 組撈資料Sql
+            string sqlCmd = string.Format(@"declare @POID varchar(13),
+		@OrderID varchar(13)
+set @POID = '{0}';
+set @OrderID = '{1}';
+with ExportSeq1
+as 
+(select distinct ID,Seq1 from Export_Detail where PoID = @POID)
+,
+SeqSum
+as
+(select distinct ID, (select Seq1+',' from ExportSeq1 where ID = e.ID for xml path('')) as Seq from ExportSeq1 e)
+,
+ExportData
+as
+(select distinct 'Import Schedule' as Type, e.ID,e.Eta,e.WhseArrival,e.Consignee,e.ShipMark,e.Vessel,e.CYCFS
+ from Export e, Export_Detail ed
+ where e.ID = ed.ID
+ and ed.PoID = @POID)
+,
+tmpExport
+as
+(select ed.*,left(ss.Seq,len(ss.Seq)-1) as Seq
+ from ExportData ed
+ left join SeqSum ss on ed.ID = ss.ID)
+,
+ReceiveSeq1
+as 
+(select distinct ID,Seq1 from Receiving_Detail where PoID = @POID)
+,
+RecSeqSum
+as
+(select distinct ID, (select Seq1+',' from ReceiveSeq1 where ID = e.ID for xml path('')) as Seq from ReceiveSeq1 e)
+,
+ReceiveData
+as
+(select distinct 'Material Receiving' as Type, r.ID,null as ETA,r.WhseArrival,'' as Consignee,'' as ShipMark,'' as Vessel,'' as CYCFS
+ from Receiving r, Receiving_Detail rd
+ where r.ID = rd.ID
+ and rd.PoID = @POID
+ and r.Type = 'B')
+,
+tmpReceive
+as
+(select rd.*,left(rss.Seq,len(rss.Seq)-1) as Seq
+ from ReceiveData rd
+ left join RecSeqSum rss on rd.ID = rss.ID)
+,
+tmpLocalReceiving
+as
+(select 'Local Purchase  Receiving' as Type, l.Id, null as ETA, l.IssueDate as WhseArrival, '' as Consignee,'' as ShipMark,'' as Vessel,'' as CYCFS, '' as Seq
+ from LocalReceiving l, LocalReceiving_Detail ld
+ where l.Id = ld.Id
+ and ld.OrderId = @OrderID
+)
+
+select * from tmpExport
+union all
+select * from tmpReceive
+union all
+select * from tmpLocalReceiving
+order by type,ID", masterData["POID"].ToString(), masterData["ID"].ToString());
+            #endregion
+            DualResult result = DBProxy.Current.Select(null, sqlCmd, out MTLImport);
+            if (!result)
+            {
+                MyUtility.Msg.ErrorBox("Query data fail!!"+result.ToString());
+            }
+            listControlBindingSource1.DataSource = MTLImport;
+            //設定Grid的顯示欄位
+            this.grid1.IsEditingReadOnly = true;
+            this.grid1.DataSource = listControlBindingSource1;
+            Helper.Controls.Grid.Generator(this.grid1)
+                .Text("Type", header: "Type", width: Widths.AnsiChars(20))
+                .Text("Id", header: "Working No.", width: Widths.AnsiChars(15))
+                .Date("ETA", header: "ETA", width: Widths.AnsiChars(12))
+                .Date("WhseArrival", header: "Arrive W/H Date", width: Widths.AnsiChars(12))
+                .Text("Seq", header: "Main SEQ", width: Widths.AnsiChars(15))
+                .Text("Consignee", header: "Consignee", width: Widths.AnsiChars(8))
+                .Text("ShipMark", header: "Shipping Mark", width: Widths.AnsiChars(10))
+                .Text("Vessel", header: "Vessel Name", width: Widths.AnsiChars(25))
+                .Text("CYCFS", header: "Container Type", width: Widths.AnsiChars(3));
+        }
+    }
+}
