8b5ce904c02902221448c737131840a1e610188d
YINMEI <yinmei@sportscity.com.tw>
2015-10-22 03:10:30 +0000

    Add function: getMTLExport

---
+++ Database/dbo/Functions/getMTLExport.sql
@@ -0,0 +1,16 @@
+﻿
+CREATE FUNCTION [dbo].[getMTLExport](@poid varchar(13), @mtlexport varchar(2))
+RETURNS varchar(3)
+BEGIN
+	DECLARE @string varchar(3) --要回傳的字串
+	IF @mtlexport <> ''
+		BEGIN
+			SET @string = @mtlexport
+		END
+	ELSE
+		BEGIN
+			Select @string = Count(Distinct ID) from Export_Detail where POID = @poid
+		END
+
+	RETURN @string
+END
