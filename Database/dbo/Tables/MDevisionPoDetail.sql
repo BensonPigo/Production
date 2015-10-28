e7510abedc587d63b6c8c645d25db38796b8f6e7
MIKE <mike@sportscity.com.tw>
2015-10-28 02:10:40 +0000

    新增MDevision相關table

---
+++ Database/dbo/Tables/MDevisionPoDetail.sql
@@ -0,0 +1,46 @@
+﻿CREATE TABLE [dbo].[ZonePoDetail]
+(
+	[ZoneId] VARCHAR(3) NOT NULL CONSTRAINT [DF_ZonePoDetail_ZoneId] DEFAULT ('') , 
+    [POID] VARCHAR(13) NOT NULL CONSTRAINT [DF_ZonePoDetail_POID] DEFAULT (''), 
+    [Seq1] VARCHAR(3) NOT NULL CONSTRAINT [DF_ZonePoDetail_Seq1] DEFAULT (''), 
+    [Seq2] VARCHAR(2) NOT NULL CONSTRAINT [DF_ZonePoDetail_Seq2] DEFAULT (''), 
+    [TEST] BIGINT NOT NULL IDENTITY, 
+    PRIMARY KEY ([Seq2], [ZoneId], [POID], [Seq1])
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Zone',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'ZonePoDetail',
+    @level2type = N'COLUMN',
+    @level2name = N'ZoneId'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'PO#',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'ZonePoDetail',
+    @level2type = N'COLUMN',
+    @level2name = N'POID'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Seq1',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'ZonePoDetail',
+    @level2type = N'COLUMN',
+    @level2name = N'Seq1'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Seq2',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'ZonePoDetail',
+    @level2type = N'COLUMN',
+    @level2name = N'Seq2'
---
+++ Database/dbo/Tables/MDivision.sql
@@ -0,0 +1,48 @@
+﻿CREATE TABLE [dbo].[MDivision] (
+    [ID]        VARCHAR (8)  CONSTRAINT [DF_MDivision_ID] DEFAULT ('') NOT NULL,
+    [Name]      VARCHAR (10) CONSTRAINT [DF_MDivision_Name] DEFAULT ('') NULL,
+    [CountryID] VARCHAR (2)  CONSTRAINT [DF_MDivision_CountryID] DEFAULT ('') NULL,
+    [Manager]   VARCHAR (10) CONSTRAINT [DF_MDivision_Manager] DEFAULT ('') NULL,
+    [AddName]   VARCHAR (10) CONSTRAINT [DF_MDivision_AddName] DEFAULT ('') NULL,
+    [AddDate]   DATETIME     NULL,
+    [EditName]  VARCHAR (10) CONSTRAINT [DF_MDivision_EditName] DEFAULT ('') NULL,
+    [EditDate]  DATETIME     NULL,
+    CONSTRAINT [PK_MDivision] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'½s¿è®É¶¡', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'EditDate';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'½s¿èªÌ', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'EditName';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'·s¼W®É¶¡', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'AddDate';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'·s¼WªÌ', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'AddName';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'ºÞ²zªÌ', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'Manager';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'°ê§O', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'CountryID';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'¥þ¦W', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'Name';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'¥N¸¹', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'ID';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Manufacturing Division', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision';
+
