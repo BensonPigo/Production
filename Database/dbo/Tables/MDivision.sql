5dca8b8acb061ca6b248321f21f0a3fdf982b515
YINMEI <yinmei@sportscity.com.tw>
2015-10-28 09:39:50 +0000

    新增APS設定的相關欄位

--- Database/dbo/Tables/MDivision.sql
+++ Database/dbo/Tables/MDivision.sql
@@ -1,48 +1,86 @@
 ﻿CREATE TABLE [dbo].[MDivision] (
-    [ID]        VARCHAR (8)  CONSTRAINT [DF_MDivision_ID] DEFAULT ('') NOT NULL,
-    [Name]      VARCHAR (10) CONSTRAINT [DF_MDivision_Name] DEFAULT ('') NULL,
-    [CountryID] VARCHAR (2)  CONSTRAINT [DF_MDivision_CountryID] DEFAULT ('') NULL,
-    [Manager]   VARCHAR (10) CONSTRAINT [DF_MDivision_Manager] DEFAULT ('') NULL,
-    [AddName]   VARCHAR (10) CONSTRAINT [DF_MDivision_AddName] DEFAULT ('') NULL,
-    [AddDate]   DATETIME     NULL,
-    [EditName]  VARCHAR (10) CONSTRAINT [DF_MDivision_EditName] DEFAULT ('') NULL,
-    [EditDate]  DATETIME     NULL,
+    [ID]              VARCHAR (8)   CONSTRAINT [DF_MDivision_ID] DEFAULT ('') NOT NULL,
+    [Name]            VARCHAR (10)  CONSTRAINT [DF_MDivision_Name] DEFAULT ('') NULL,
+    [CountryID]       VARCHAR (2)   CONSTRAINT [DF_MDivision_CountryID] DEFAULT ('') NULL,
+    [Manager]         VARCHAR (10)  CONSTRAINT [DF_MDivision_Manager] DEFAULT ('') NULL,
+    [AddName]         VARCHAR (10)  CONSTRAINT [DF_MDivision_AddName] DEFAULT ('') NULL,
+    [AddDate]         DATETIME      NULL,
+    [EditName]        VARCHAR (10)  CONSTRAINT [DF_MDivision_EditName] DEFAULT ('') NULL,
+    [EditDate]        DATETIME      NULL,
+    [APSLoginId]      VARCHAR (15)  CONSTRAINT [DF_MDivision_APSLoginId] DEFAULT ('') NULL,
+    [APSLoginPwd]     VARCHAR (15)  CONSTRAINT [DF_MDivision_APSLoginPwd] DEFAULT ('') NULL,
+    [SQLServerName]   VARCHAR (130) CONSTRAINT [DF_MDivision_SQLServerName] DEFAULT ('') NULL,
+    [APSDatabaseName] VARCHAR (15)  CONSTRAINT [DF_MDivision_APSDatabaseName] DEFAULT ('') NULL,
     CONSTRAINT [PK_MDivision] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
 
 
-GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'½s¿è®É¶¡', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'EditDate';
 
 
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'½s¿èªÌ', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'EditName';
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'編輯時間', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'EditDate';
+
+
 
 
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'·s¼W®É¶¡', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'AddDate';
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'編輯者', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'EditName';
+
+
 
 
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'·s¼WªÌ', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'AddName';
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'新增時間', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'AddDate';
+
+
 
 
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'ºÞ²zªÌ', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'Manager';
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'新增者', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'AddName';
+
+
 
 
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'°ê§O', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'CountryID';
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'管理者', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'Manager';
+
+
 
 
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'¥þ¦W', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'Name';
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'國別', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'CountryID';
+
+
 
 
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'¥N¸¹', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'ID';
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'全名', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'Name';
+
+
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'代號', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'ID';
+
+
 
 
 GO
 EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Manufacturing Division', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'SQL Server Name', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'SQLServerName';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'APS SQL Server PassWord', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'APSLoginPwd';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'APS SQL Server Login Id', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'APSLoginId';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'APS Database Name', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MDivision', @level2type = N'COLUMN', @level2name = N'APSDatabaseName';
 
