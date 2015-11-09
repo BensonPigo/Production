4b5b610a9fd4f4f9f2655cea5848a418e3c0804d
YINMEI <yinmei@sportscity.com.tw>
2015-11-09 04:30:41 +0000

    修改寫法

--- Sci.Production.Basic/B08.cs
+++ Sci.Production.Basic/B08.cs
@@ -22,16 +22,25 @@ namespace Sci.Production.Basic
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string selectCommand = string.Format("select * from CDCode_Content where ID = '{0}'", this.CurrentMaintain["ID"].ToString());
+            //sql參數
+            System.Data.SqlClient.SqlParameter sp1 = new System.Data.SqlClient.SqlParameter();
+            sp1.ParameterName = "@id";
+            sp1.Value = CurrentMaintain["ID"].ToString();
+
+            IList<System.Data.SqlClient.SqlParameter> cmds = new List<System.Data.SqlClient.SqlParameter>();
+            cmds.Add(sp1);
+
+            //當CDCode_Content中沒有此CDCode資料時，就自動塞一筆空資料進去
+            string selectCommand = "select * from CDCode_Content where ID = @id";
             DataTable selectDataTable;
-            DualResult selectResult = DBProxy.Current.Select(null, selectCommand, out selectDataTable);
+            DualResult selectResult = DBProxy.Current.Select(null, selectCommand, cmds, out selectDataTable);
             if (selectDataTable.Rows.Count == 0)
             {
-                string insertCommand = string.Format("insert into CDCode_Content(ID) values ('{0}'); ", this.CurrentMaintain["ID"].ToString());
+                string insertCommand = "insert into CDCode_Content(ID) values (@id); ";
                 DualResult result;
-                if (result = DBProxy.Current.Execute(null, insertCommand))
+                if (result = DBProxy.Current.Execute(null, insertCommand, cmds))
                 {
-                    selectResult = DBProxy.Current.Select(null, selectCommand, out selectDataTable);
+                    selectResult = DBProxy.Current.Select(null, selectCommand, cmds, out selectDataTable);
                 }
                 else
                 {
@@ -39,7 +48,8 @@ namespace Sci.Production.Basic
                     return;
                 }
             }
-            bool hasEditauthority = true; //等Pass2結構確定後再來撈此資料
+
+            bool hasEditauthority = PublicPrg.Prgs.GetAuthority(Sci.Env.User.UserID, "B08. CD Code", "CanEdit");
             Sci.Production.Basic.B08_ProductionFabricType rm = new Sci.Production.Basic.B08_ProductionFabricType(hasEditauthority, selectDataTable.Rows[0]);
             rm.ShowDialog(this);
 
