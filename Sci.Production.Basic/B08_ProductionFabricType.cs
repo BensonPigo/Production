263aa37e8f779dd678cb8f07c8552a2caf431312
YINMEI <yinmei@sportscity.com.tw>
2015-11-09 05:53:55 +0000

    修改連接SQL語法

--- Sci.Production.Basic/B08_ProductionFabricType.cs
+++ Sci.Production.Basic/B08_ProductionFabricType.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Ict;
+using Ict.Win;
+using Sci.Data;
 
 namespace Sci.Production.Basic
 {
@@ -43,12 +46,32 @@ namespace Sci.Production.Basic
         private void textBox1_Validating(object sender, CancelEventArgs e)
         {
             Sci.Win.UI.TextBox prodTextValue = (Sci.Win.UI.TextBox)sender;
-            if (!string.IsNullOrWhiteSpace(prodTextValue.Text) && prodTextValue.Text != prodTextValue.OldValue)
+            if (!MyUtility.Check.Empty(prodTextValue.Text) && prodTextValue.Text != prodTextValue.OldValue)
             {
-                string selectCommand = string.Format("select ID from Reason where ReasonTypeID = 'Style_Apparel_Type' and Name = '{0}'", prodTextValue.Text);
-                if (!MyUtility.Check.Seek(selectCommand,null))
+                //sql參數
+                System.Data.SqlClient.SqlParameter sp1 = new System.Data.SqlClient.SqlParameter();
+                sp1.ParameterName = "@name";
+                sp1.Value = prodTextValue.Text;
+
+                IList<System.Data.SqlClient.SqlParameter> cmds = new List<System.Data.SqlClient.SqlParameter>();
+                cmds.Add(sp1);
+
+                string selectCommand = "select ID from Reason where ReasonTypeID = 'Style_Apparel_Type' and Name = @name";
+                DataTable reasonID;
+                DualResult result = DBProxy.Current.Select(null, selectCommand, cmds, out reasonID);
+                if (result)
                 {
-                    MyUtility.Msg.WarningBox(string.Format("< Prod. Type : {0} > not found!!!", prodTextValue.Text));
+                    if (reasonID.Rows.Count <= 0)
+                    {
+                        MyUtility.Msg.WarningBox(string.Format("< Prod. Type : {0} > not found!!!", prodTextValue.Text));
+                        prodTextValue.Text = "";
+                        e.Cancel = true;
+                        return;
+                    }
+                }
+                else
+                {
+                    MyUtility.Msg.WarningBox("SQL Connection fail!!\r\n" + result.ToString());
                     prodTextValue.Text = "";
                     e.Cancel = true;
                     return;
@@ -59,12 +82,32 @@ namespace Sci.Production.Basic
         private void textBox2_Validating(object sender, CancelEventArgs e)
         {
             Sci.Win.UI.TextBox fabricTextValue = (Sci.Win.UI.TextBox)sender;
-            if (!string.IsNullOrWhiteSpace(fabricTextValue.Text) && fabricTextValue.Text != fabricTextValue.OldValue)
+            if (!MyUtility.Check.Empty(fabricTextValue.Text) && fabricTextValue.Text != fabricTextValue.OldValue)
             {
-                string selectCommand = string.Format("select ID from Reason where ReasonTypeID = 'Fabric_Kind' and Name = '{0}'", fabricTextValue.Text);
-                if (!MyUtility.Check.Seek(selectCommand,null))
+                //sql參數
+                System.Data.SqlClient.SqlParameter sp1 = new System.Data.SqlClient.SqlParameter();
+                sp1.ParameterName = "@name";
+                sp1.Value = fabricTextValue.Text;
+
+                IList<System.Data.SqlClient.SqlParameter> cmds = new List<System.Data.SqlClient.SqlParameter>();
+                cmds.Add(sp1);
+
+                string selectCommand = "select ID from Reason where ReasonTypeID = 'Fabric_Kind' and Name = @name";
+                DataTable reasonID;
+                DualResult result = DBProxy.Current.Select(null, selectCommand, cmds, out reasonID);
+                if (result)
                 {
-                    MyUtility.Msg.WarningBox(string.Format("< Fabric Type : {0} > not found!!!", fabricTextValue.Text));
+                    if (reasonID.Rows.Count <= 0)
+                    {
+                        MyUtility.Msg.WarningBox(string.Format("< Fabric Type : {0} > not found!!!", fabricTextValue.Text));
+                        fabricTextValue.Text = "";
+                        e.Cancel = true;
+                        return;
+                    }
+                }
+                else
+                {
+                    MyUtility.Msg.WarningBox("SQL Connection fail!!\r\n" + result.ToString());
                     fabricTextValue.Text = "";
                     e.Cancel = true;
                     return;
