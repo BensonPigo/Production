483721e8114e1cb30f6361f8d16d9d57a8632a6e
YINMEI <yinmei@sportscity.com.tw>
2015-11-09 07:50:57 +0000

    修改ComboBox的RowSource寫法

--- Sci.Production.IE/B07.cs
+++ Sci.Production.IE/B07.cs
@@ -17,12 +17,7 @@ namespace Sci.Production.IE
             this.label5.Text = "Description\r\n(English)";
             this.label6.Text = "Description\r\n(Chinese)";
 
-            Dictionary<String, String> comboBox1_RowSource = new Dictionary<string, string>();
-            comboBox1_RowSource.Add("A", "Accoessory");
-            comboBox1_RowSource.Add("W", "Workmanship");
-            comboBox1.DataSource = new BindingSource(comboBox1_RowSource, null);
-            comboBox1.ValueMember = "Key";
-            comboBox1.DisplayMember = "Value";
+            MyUtility.Tool.SetupCombox(comboBox1, 2, 1, "A,Accoessory,W,Workmanship");
         }
     }
 }
