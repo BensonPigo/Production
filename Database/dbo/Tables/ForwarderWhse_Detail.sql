ee88574f61ffeaa86bda6801fa51e092f8fa80fc
YINMEI <yinmei@sportscity.com.tw>
2015-11-26 07:29:49 +0000

    Add Table: ForwarderWhse_Detail

---
+++ Database/dbo/Tables/ForwarderWhse_Detail.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ForwarderWhse_Detail] (
+    [ID]     BIGINT        NOT NULL,
+    [WhseNo] NVARCHAR (50) NOT NULL,
+    [UKey]   BIGINT        IDENTITY (1, 1) NOT NULL,
+    CONSTRAINT [PK_ForwarderWhse_Detail] PRIMARY KEY CLUSTERED ([ID] ASC, [WhseNo] ASC)
+);
+
