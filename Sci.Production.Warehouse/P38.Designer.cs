b7a99f1fae6c630eea4a2ddb518e252c4f85d807
MIKE <mike@sportscity.com.tw>
2016-01-13 03:54:53 +0000

    add P38

---
+++ Sci.Production.Warehouse/P38.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace Sci.Production.Warehouse
+{
+    partial class P38
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new Sci.Win.UI.Panel();
+            this.cbxStockType = new Sci.Win.UI.ComboBox();
+            this.label3 = new Sci.Win.UI.Label();
+            this.textBox1 = new Sci.Win.UI.TextBox();
+            this.label1 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.cbxStatus = new Sci.Win.UI.ComboBox();
+            this.btnQuery = new Sci.Win.UI.Button();
+            this.panel2 = new Sci.Win.UI.Panel();
+            this.grid1 = new Sci.Win.UI.Grid();
+            this.panel3 = new Sci.Win.UI.Panel();
+            this.btnLock = new Sci.Win.UI.Button();
+            this.button4 = new Sci.Win.UI.Button();
+            this.btnUnlock = new Sci.Win.UI.Button();
+            this.listControlBindingSource1 = new Sci.Win.UI.ListControlBindingSource();
+            this.btnExcel = new Sci.Win.UI.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.cbxStockType);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.cbxStatus);
+            this.panel1.Controls.Add(this.btnQuery);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1008, 45);
+            this.panel1.TabIndex = 1;
+            // 
+            // cbxStockType
+            // 
+            this.cbxStockType.BackColor = System.Drawing.Color.White;
+            this.cbxStockType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.cbxStockType.FormattingEnabled = true;
+            this.cbxStockType.IsSupportUnselect = true;
+            this.cbxStockType.Items.AddRange(new object[] {
+            "ALL",
+            "Bulk",
+            "Inventory"});
+            this.cbxStockType.Location = new System.Drawing.Point(636, 9);
+            this.cbxStockType.Name = "cbxStockType";
+            this.cbxStockType.Size = new System.Drawing.Size(100, 24);
+            this.cbxStockType.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(538, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 23);
+            this.label3.TabIndex = 38;
+            this.label3.Text = "Stock Type";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(71, 10);
+            this.textBox1.MaxLength = 13;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(9, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 23);
+            this.label1.TabIndex = 35;
+            this.label1.Text = "SP#(%)";
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(285, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 23);
+            this.label2.TabIndex = 33;
+            this.label2.Text = "Status";
+            // 
+            // cbxStatus
+            // 
+            this.cbxStatus.BackColor = System.Drawing.Color.White;
+            this.cbxStatus.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.cbxStatus.FormattingEnabled = true;
+            this.cbxStatus.IsSupportUnselect = true;
+            this.cbxStatus.Items.AddRange(new object[] {
+            "All",
+            "Locked",
+            "Unlocked"});
+            this.cbxStatus.Location = new System.Drawing.Point(362, 9);
+            this.cbxStatus.Name = "cbxStatus";
+            this.cbxStatus.Size = new System.Drawing.Size(121, 24);
+            this.cbxStatus.TabIndex = 2;
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(916, 6);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(80, 30);
+            this.btnQuery.TabIndex = 4;
+            this.btnQuery.Text = "Query";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.grid1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 45);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1008, 616);
+            this.panel2.TabIndex = 0;
+            // 
+            // grid1
+            // 
+            this.grid1.AllowUserToAddRows = false;
+            this.grid1.AllowUserToDeleteRows = false;
+            this.grid1.AllowUserToResizeRows = false;
+            this.grid1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grid1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.grid1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.grid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid1.EditingEnter = Ict.Win.UI.DataGridViewEditingEnter.NextCellOrNextRow;
+            this.grid1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.grid1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.grid1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(162)))), ((int)(((byte)(163)))));
+            this.grid1.Location = new System.Drawing.Point(3, 3);
+            this.grid1.Name = "grid1";
+            this.grid1.RowHeadersVisible = false;
+            this.grid1.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.grid1.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.grid1.RowTemplate.Height = 24;
+            this.grid1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grid1.Size = new System.Drawing.Size(1002, 547);
+            this.grid1.TabIndex = 0;
+            this.grid1.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.btnExcel);
+            this.panel3.Controls.Add(this.btnLock);
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Controls.Add(this.btnUnlock);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 601);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1008, 60);
+            this.panel3.TabIndex = 0;
+            // 
+            // btnLock
+            // 
+            this.btnLock.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLock.Location = new System.Drawing.Point(750, 15);
+            this.btnLock.Name = "btnLock";
+            this.btnLock.Size = new System.Drawing.Size(80, 30);
+            this.btnLock.TabIndex = 0;
+            this.btnLock.Text = "Lock";
+            this.btnLock.UseVisualStyleBackColor = true;
+            this.btnLock.Click += new System.EventHandler(this.btnLock_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(922, 15);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 30);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Close";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btnUnlock
+            // 
+            this.btnUnlock.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUnlock.Location = new System.Drawing.Point(836, 15);
+            this.btnUnlock.Name = "btnUnlock";
+            this.btnUnlock.Size = new System.Drawing.Size(80, 30);
+            this.btnUnlock.TabIndex = 1;
+            this.btnUnlock.Text = "Unlock";
+            this.btnUnlock.UseVisualStyleBackColor = true;
+            this.btnUnlock.Click += new System.EventHandler(this.btnUnlock_Click);
+            // 
+            // btnExcel
+            // 
+            this.btnExcel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExcel.Location = new System.Drawing.Point(664, 15);
+            this.btnExcel.Name = "btnExcel";
+            this.btnExcel.Size = new System.Drawing.Size(80, 30);
+            this.btnExcel.TabIndex = 3;
+            this.btnExcel.Text = "To Excel";
+            this.btnExcel.UseVisualStyleBackColor = true;
+            this.btnExcel.Click += new System.EventHandler(this.btnExcel_Click);
+            // 
+            // P38
+            // 
+            this.ClientSize = new System.Drawing.Size(1008, 661);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "P38";
+            this.Text = "P38. Material Lock/Unlock for All Transaction";
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.Controls.SetChildIndex(this.panel2, 0);
+            this.Controls.SetChildIndex(this.panel3, 0);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Win.UI.Panel panel1;
+        private Win.UI.Panel panel2;
+        private Win.UI.Panel panel3;
+        private Win.UI.Button btnQuery;
+        private Win.UI.Grid grid1;
+        private Win.UI.Button button4;
+        private Win.UI.Button btnUnlock;
+        private Win.UI.ListControlBindingSource listControlBindingSource1;
+        private Win.UI.Label label2;
+        private Win.UI.ComboBox cbxStatus;
+        private Win.UI.TextBox textBox1;
+        private Win.UI.Label label1;
+        private Win.UI.Label label3;
+        private Win.UI.ComboBox cbxStockType;
+        private Win.UI.Button btnLock;
+        private Win.UI.Button btnExcel;
+    }
+}
---
+++ Sci.Production.Warehouse/P38.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict.Win;
+using Ict;
+using Sci;
+using Sci.Data;
+using System.Linq;
+
+namespace Sci.Production.Warehouse
+{
+    public partial class P38 : Sci.Win.Tems.QueryForm
+    {
+        Ict.Win.UI.DataGridViewCheckBoxColumn col_chk;
+        public P38(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            this.EditMode = true;
+        }
+
+        protected override void OnFormLoaded()
+        {
+            base.OnFormLoaded();
+            cbxStatus.SelectedIndex = 0;
+            cbxStockType.SelectedIndex = 0;
+            MyUtility.Tool.SetGridFrozen(this.grid1);
+            Ict.Win.UI.DataGridViewTextBoxColumn columnStatus = new Ict.Win.UI.DataGridViewTextBoxColumn();
+            #region -- 設定Grid1的顯示欄位 --
+            this.grid1.IsEditingReadOnly = false; //必設定, 否則CheckBox會顯示圖示
+            this.grid1.DataSource = listControlBindingSource1;
+            Helper.Controls.Grid.Generator(this.grid1)
+                .CheckBox("Selected", header: "", width: Widths.AnsiChars(3), iseditable: true, trueValue: 1, falseValue: 0).Get(out col_chk)
+                 .Text("POID", header: "SP#", width: Widths.AnsiChars(13), iseditingreadonly: true)
+                 .Text("seq1", header: "Seq1", width: Widths.AnsiChars(3), iseditingreadonly: true)
+                  .Text("seq2", header: "Seq2", width: Widths.AnsiChars(2), iseditingreadonly: true)
+                  .Text("roll", header: "Roll#", width: Widths.AnsiChars(8), iseditingreadonly: true)
+                  .Text("dyelot", header: "Dyelot", width: Widths.AnsiChars(4), iseditingreadonly: true)
+                  .Text("status", header: "Status", width: Widths.AnsiChars(10), iseditingreadonly: true).Get(out columnStatus)
+                  .DateTime("lockdate", header: "Lock/Unlock" + Environment.NewLine + "Date", width: Widths.AnsiChars(10), iseditingreadonly: true)
+                  .Text("lockname", header: "Lock/Unlock" + Environment.NewLine + "Name", width: Widths.AnsiChars(8), iseditingreadonly: true)
+                  .Numeric("inqty", header: "In Qty", width: Widths.AnsiChars(10), integer_places: 8, decimal_places: 2, iseditingreadonly: true)
+                  .Numeric("outqty", header: "Out Qty", width: Widths.AnsiChars(10), integer_places: 8, decimal_places: 2, iseditingreadonly: true)
+                  .Numeric("adjustqty", header: "Adjust Qty", width: Widths.AnsiChars(10), integer_places: 8, decimal_places: 2, iseditingreadonly: true)
+                  .Numeric("balanceqty", header: "Balance Qty", width: Widths.AnsiChars(10), integer_places: 8, decimal_places: 2, iseditingreadonly: true)
+                 .Text("stocktype", header: "Stocktype", width: Widths.AnsiChars(10), iseditingreadonly: true)
+                 .Text("location", header: "location", width: Widths.AnsiChars(10), iseditingreadonly: true)
+                 .Text("description", header: "description", width: Widths.AnsiChars(20), iseditingreadonly: true)
+                 .Text("styleid", header: "Style", width: Widths.AnsiChars(13), iseditingreadonly: true)
+                 .Text("colorid", header: "Color", width: Widths.AnsiChars(10), iseditingreadonly: true)
+                  .Date("earliest_BuyerDelivery", header: "Earliest" + Environment.NewLine + "BuyerDelivery", width: Widths.AnsiChars(10), iseditingreadonly: true)
+                  .Date("earliest_SciDelivery", header: "Earliest" + Environment.NewLine + "SciDelivery", width: Widths.AnsiChars(10), iseditingreadonly: true)
+                  .Text("brandid", header: "Brand", width: Widths.AnsiChars(12), iseditingreadonly: true)
+                  .Text("factoryid", header: "Factory", width: Widths.AnsiChars(8), iseditingreadonly: true)
+                  ;
+            columnStatus.DefaultCellStyle.ForeColor = Color.Blue;
+            #endregion
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            StringBuilder strSQLCmd = new StringBuilder();
+            String sp1 = this.textBox1.Text.TrimEnd() + '%';
+
+
+            if (string.IsNullOrWhiteSpace(sp1))
+            {
+                MyUtility.Msg.WarningBox("< SP# > can't be empty!!");
+                textBox1.Focus();
+                return;
+            }
+
+            strSQLCmd.Append(string.Format(@"
+select 0 as [selected], fi.POID,fi.seq1,fi.seq2,fi.Roll,fi.Dyelot,iif(fi.Lock=0,'Unlocked','Locked') [status]
+,fi.InQty,fi.OutQty,fi.AdjustQty,fi.InQty-fi.OutQty+fi.AdjustQty as balanceqty
+,case fi.stocktype when 'B' then'Bulk' when 'I' then 'Inventory' else fi.StockType end as stocktype
+,fi.LockDate
+,(select id+'-'+name from dbo.pass1 where id=fi.LockName) LockName
+,fi.ukey
+,(select mtllocationid+',' from (select MtlLocationID from dbo.FtyInventory_Detail where ukey = fi.Ukey)t for xml path('')) as [location]
+,dbo.getMtlDesc(fi.poid,fi.seq1,fi.seq2,2,0) as [Description]
+,pd.ColorID
+,o.styleid
+,o.BrandID
+,o.FactoryID
+,x.*
+from dbo.FtyInventory fi 
+left join dbo.PO_Supp_Detail pd on pd.id = fi.POID and pd.seq1 = fi.seq1 and pd.seq2  = fi.Seq2
+left join dbo.orders o on o.id = fi.POID
+cross apply
+(
+	select min(o1.BuyerDelivery) earliest_BuyerDelivery ,min(o1.SciDelivery) earliest_SciDelivery 
+	from dbo.orders o1 where o1.POID = fi.POID and o1.Junk = 0
+) x
+where fi.MDivisionID = '{0}' and fi.POID like @poid1 
+", Sci.Env.User.Keyword));
+
+            System.Data.SqlClient.SqlParameter sp1_1 = new System.Data.SqlClient.SqlParameter();
+            sp1_1.ParameterName = "@poid1";
+
+            IList<System.Data.SqlClient.SqlParameter> cmds = new List<System.Data.SqlClient.SqlParameter>();
+            sp1_1.Value = sp1;
+            cmds.Add(sp1_1);
+
+            switch (cbxStatus.SelectedIndex)
+            {
+                case 0:
+                    break;
+                case 1:
+                    strSQLCmd.Append(@" and lock=1");
+                    break;
+                case 2:
+                    strSQLCmd.Append(@" and lock=0");
+                    break;
+            }
+
+            switch (cbxStockType.SelectedIndex)
+            {
+                case 0:
+                    strSQLCmd.Append(@" and (stocktype='B' or stocktype ='I')");
+                    break;
+                case 1:
+                    strSQLCmd.Append(@" and stocktype='B'");
+                    break;
+                case 2:
+                    strSQLCmd.Append(@" and stocktype='I'");
+                    break;
+            }
+
+            Ict.DualResult result;
+            DataTable dtData;
+            MyUtility.Msg.WaitWindows("Data Loading...");
+            if (result = DBProxy.Current.Select(null, strSQLCmd.ToString(), cmds, out dtData))
+            {
+                if (dtData.Rows.Count == 0)
+                { MyUtility.Msg.WarningBox("Data not found!!"); }
+                listControlBindingSource1.DataSource = dtData;
+                dtData.DefaultView.Sort = "poid,seq1,seq2,dyelot,roll,stocktype";
+            }
+            else
+            {
+                ShowErr(strSQLCmd.ToString(), result);
+            }
+            MyUtility.Msg.WaitClear();
+        }
+
+        private void btnLock_Click(object sender, EventArgs e)
+        {
+            LockUnlock(1);
+        }
+
+        private void btnUnlock_Click(object sender, EventArgs e)
+        {
+            LockUnlock(0);
+        }
+
+        private void LockUnlock(Byte flag)
+        {
+            bool x;
+            if (!(x = grid1.ValidateControl())) MyUtility.Msg.WarningBox("grid1.ValidateControl failed");
+            grid1.EndEdit();
+            listControlBindingSource1.EndEdit();
+            DualResult result;
+            DataTable dt = (DataTable)listControlBindingSource1.DataSource;
+            dt.AcceptChanges();
+            
+            if (dt == null || dt.Rows.Count == 0) return;
+            DataRow[] find;
+            DialogResult dResult;
+
+            string keyword = "";
+            if (flag == 1)
+            {
+                keyword = "Lock";
+            }
+            else
+            {
+                keyword = "Unlock";
+            }
+
+            find = dt.Select("Selected = 1");
+            if (find.Length == 0)
+            {
+                MyUtility.Msg.WarningBox("Please select rows first!", "Warnning");
+                return;
+            }
+
+            dResult = MyUtility.Msg.QuestionBox(string.Format("Are you sure to {0} it?", keyword), "Question", MessageBoxButtons.YesNo, MessageBoxDefaultButton.Button2);
+
+            if (dResult == DialogResult.No) return;
+
+            StringBuilder sqlcmd = new StringBuilder();
+            foreach (DataRow item in find)
+            {
+                sqlcmd.Append(string.Format(@"update dbo.ftyinventory set lock={1},lockname='{2}',lockdate=GETDATE() where ukey ={0};", item["ukey"],flag,Sci.Env.User.UserID));
+            }
+            if (!(result = Sci.Data.DBProxy.Current.Execute(null, sqlcmd.ToString())))
+            {
+                MyUtility.Msg.WarningBox(string.Format("{0} failed, Pleaes re-try", keyword));
+                return;
+            }
+            else { MyUtility.Msg.InfoBox(string.Format("{0} successful!!", keyword)); }
+        }
+
+        private void btnExcel_Click(object sender, EventArgs e)
+        {
+            DataTable dt = (DataTable)listControlBindingSource1.DataSource;
+            MyUtility.Excel.CopyToXls(dt, "");
+        }
+    }
+}
