a22402ca2dc88a0bdeeeb7969daf8eb804196c19
MIKE <mike@sportscity.com.tw>
2016-01-19 03:48:57 +0000

    add R19 (原p33)

---
+++ Sci.Production.Warehouse/R19.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Sci.Production.Warehouse
+{
+    partial class R19
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxSP = new Sci.Win.UI.TextBox();
+            this.cbxCategory = new Sci.Win.UI.ComboBox();
+            this.label7 = new Sci.Win.UI.Label();
+            this.label1 = new Sci.Win.UI.Label();
+            this.dateRange_ReturnDate = new Sci.Win.UI.DateRange();
+            this.label2 = new Sci.Win.UI.Label();
+            this.label3 = new Sci.Win.UI.Label();
+            this.tbxSeq = new Sci.Win.UI.TextBox();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Enabled = false;
+            this.print.Location = new System.Drawing.Point(484, 5);
+            this.print.TabIndex = 7;
+            this.print.Visible = false;
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(484, 41);
+            this.toexcel.TabIndex = 8;
+            this.toexcel.Click += new System.EventHandler(this.toexcel_Click);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(484, 77);
+            this.close.TabIndex = 9;
+            // 
+            // tbxSP
+            // 
+            this.tbxSP.BackColor = System.Drawing.Color.White;
+            this.tbxSP.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.tbxSP.Location = new System.Drawing.Point(127, 48);
+            this.tbxSP.MaxLength = 13;
+            this.tbxSP.Name = "tbxSP";
+            this.tbxSP.Size = new System.Drawing.Size(118, 23);
+            this.tbxSP.TabIndex = 0;
+            // 
+            // cbxCategory
+            // 
+            this.cbxCategory.BackColor = System.Drawing.Color.White;
+            this.cbxCategory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.cbxCategory.FormattingEnabled = true;
+            this.cbxCategory.IsSupportUnselect = true;
+            this.cbxCategory.Items.AddRange(new object[] {
+            "Not Yet",
+            "Yet",
+            "All"});
+            this.cbxCategory.Location = new System.Drawing.Point(127, 121);
+            this.cbxCategory.Name = "cbxCategory";
+            this.cbxCategory.Size = new System.Drawing.Size(121, 24);
+            this.cbxCategory.TabIndex = 106;
+            // 
+            // label7
+            // 
+            this.label7.Lines = 0;
+            this.label7.Location = new System.Drawing.Point(10, 121);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(114, 23);
+            this.label7.TabIndex = 107;
+            this.label7.Text = "Already Return";
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(10, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 23);
+            this.label1.TabIndex = 114;
+            this.label1.Text = "Borrow Seq#";
+            // 
+            // dateRange_ReturnDate
+            // 
+            this.dateRange_ReturnDate.Location = new System.Drawing.Point(127, 12);
+            this.dateRange_ReturnDate.Name = "dateRange_ReturnDate";
+            this.dateRange_ReturnDate.Size = new System.Drawing.Size(280, 23);
+            this.dateRange_ReturnDate.TabIndex = 115;
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(10, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 23);
+            this.label2.TabIndex = 116;
+            this.label2.Text = "Borrow SP#";
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(10, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 23);
+            this.label3.TabIndex = 117;
+            this.label3.Text = "Est. Return Date";
+            // 
+            // tbxSeq
+            // 
+            this.tbxSeq.BackColor = System.Drawing.Color.White;
+            this.tbxSeq.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.tbxSeq.Location = new System.Drawing.Point(127, 84);
+            this.tbxSeq.MaxLength = 5;
+            this.tbxSeq.Name = "tbxSeq";
+            this.tbxSeq.Size = new System.Drawing.Size(72, 23);
+            this.tbxSeq.TabIndex = 118;
+            // 
+            // R19
+            // 
+            this.ClientSize = new System.Drawing.Size(576, 240);
+            this.Controls.Add(this.tbxSeq);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateRange_ReturnDate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbxCategory);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tbxSP);
+            this.Name = "R19";
+            this.Text = "R19. Material Borrowing Query";
+            this.Load += new System.EventHandler(this.R18_Load);
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.tbxSP, 0);
+            this.Controls.SetChildIndex(this.label7, 0);
+            this.Controls.SetChildIndex(this.cbxCategory, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.dateRange_ReturnDate, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.tbxSeq, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.TextBox tbxSP;
+        private Win.UI.ComboBox cbxCategory;
+        private Win.UI.Label label7;
+        private Win.UI.Label label1;
+        private Win.UI.DateRange dateRange_ReturnDate;
+        private Win.UI.Label label2;
+        private Win.UI.Label label3;
+        private Win.UI.TextBox tbxSeq;
+    }
+}
---
+++ Sci.Production.Warehouse/R19.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Sci;
+using Sci.Data;
+using Ict;
+using Ict.Win;
+
+namespace Sci.Production.Warehouse
+{
+    public partial class R19 : Sci.Win.Tems.PrintForm
+    {
+        DataTable dt;
+
+        int selectindex = 0;
+        public R19(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            this.EditMode = true;
+            cbxCategory.SelectedIndex = 0;
+        }
+
+        protected override bool ValidateInput()
+        {
+            selectindex = cbxCategory.SelectedIndex;
+            return base.ValidateInput();
+        }
+
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            SetCount(dt.Rows.Count);
+            DualResult result = Result.True;
+            if (dt.Rows.Count == 0)
+            {
+                MyUtility.Msg.InfoBox("Data not found!!");
+                return result;
+            }
+            MyUtility.Excel.CopyToXls(dt, "", "Warehouse_R19_Material_Borrowing.xltx");
+            return false;
+
+        }
+
+        protected override DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            //return base.OnAsyncDataLoad(e);
+            String returnDate1 = dateRange_ReturnDate.Text1;
+            String returnDate2 = dateRange_ReturnDate.Text2;
+            String spno = tbxSP.Text.TrimEnd();
+            String seq = tbxSeq.Text.PadRight(5);
+
+            DualResult result = Result.True;
+            StringBuilder sqlcmd = new StringBuilder();
+            #region sql command
+            sqlcmd.Append(@"with cte 
+as
+(Select a.id, frompoid, fromseq1,FromSeq2 ,FromStockType,sum(qty) qty, issuedate, estbackdate, backdate
+from borrowback a inner join borrowback_detail b on b.id = a.id 
+Where a.type='A'
+and a.Status = 'Confirmed'");
+            if (!MyUtility.Check.Empty(dateRange_ReturnDate.Value1))
+            {
+                sqlcmd.Append(string.Format(" and a.estbackdate between '{0}' and '{1}'", returnDate1, returnDate2));
+            }
+            if (!MyUtility.Check.Empty(spno))
+            {
+                sqlcmd.Append(string.Format(" And b.frompoid = '{0}'", spno));
+            }
+            if (!MyUtility.Check.Empty(seq))
+            {
+                sqlcmd.Append(string.Format(" And b.fromseq1 = '{0}' and b.FromSeq2 ='{1}'", seq.Substring(0, 3), seq.Substring(3)));
+            }
+
+            switch (selectindex)
+            {
+                case 0:
+                    sqlcmd.Append(@" And a.BackDate is null ");
+                    break;
+                case 1:
+                    sqlcmd.Append(@" And a.BackDate is not null ");
+                    break;
+                case 2:
+                    break;
+            }
+
+            sqlcmd.Append(string.Format(@" and a.mdivisionid = '{0}'
+Group by a.id, frompoid, FromSeq1,FromSeq2 
+,FromStockType
+, a.IssueDate, estbackdate, backdate
+)
+select cte.id,cte.FromPOID,cte.FromSeq1,cte.FromSeq2
+,case cte.FromStockType when 'B' then 'Bulk' when 'I' then 'Inventory' else  cte.FromStockType end as stocktype
+, cte.qty 
+,isnull((select sum(qty) from BorrowBack x inner join BorrowBack_Detail y on y.id = x.id 
+where x.mdivisionid = '{0}' and x.Status = 'Confirmed' and x.type='B' and y.ToPOID = cte.FromPOID and y.ToSeq1 = cte.FromSeq1
+and y.ToSeq2 = cte.FromSeq2 and y.ToStockType = cte.FromStockType),0.00) as backQty
+, cte.qty - isnull((select sum(qty) from BorrowBack x inner join BorrowBack_Detail y on y.id = x.id 
+where x.mdivisionid = '{0}' and x.Status = 'Confirmed' and x.type='B' and y.ToPOID = cte.FromPOID and y.ToSeq1 = cte.FromSeq1
+and y.ToSeq2 = cte.FromSeq2 and y.ToStockType = cte.FromStockType),0.00) as balance
+,cte.IssueDate
+,cte.EstBackDate
+,cte.BackDate
+from cte", Env.User.Keyword));
+
+            #endregion
+
+            try
+            {
+                DBProxy.Current.DefaultTimeout = 600;
+                result = DBProxy.Current.Select(null, sqlcmd.ToString(), out dt);
+                DBProxy.Current.DefaultTimeout = 30;
+                if (!result) return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return result;
+        }
+
+        private void toexcel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void R18_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
