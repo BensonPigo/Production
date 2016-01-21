490a628557034787216f2fba42e7cd632e2a429d
SPIN <spin@sportscity.com.tw>
2016-01-21 06:43:49 +0000

    add colorcombination

---
+++ Sci.Production.PublicForm/ColorCombination.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Sci.Production.PublicForm
+{
+    partial class ColorCombination
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.grid1 = new Sci.Win.UI.Grid();
+            this.button1 = new Sci.Win.UI.Button();
+            this.grid2 = new Sci.Win.UI.Grid();
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grid1
+            // 
+            this.grid1.AllowUserToAddRows = false;
+            this.grid1.AllowUserToDeleteRows = false;
+            this.grid1.AllowUserToResizeRows = false;
+            this.grid1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grid1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.grid1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.grid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid1.EditingEnter = Ict.Win.UI.DataGridViewEditingEnter.NextCellOrNextRow;
+            this.grid1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.grid1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.grid1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(162)))), ((int)(((byte)(163)))));
+            this.grid1.Location = new System.Drawing.Point(3, 3);
+            this.grid1.Name = "grid1";
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grid1.RowHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.grid1.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.grid1.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.grid1.RowTemplate.Height = 24;
+            this.grid1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grid1.Size = new System.Drawing.Size(675, 416);
+            this.grid1.TabIndex = 0;
+            this.grid1.TabStop = false;
+            this.grid1.CurrentCellChanged += new System.EventHandler(this.grid1_CurrentCellChanged);
+            this.grid1.SelectionChanged += new System.EventHandler(this.grid1_SelectionChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(830, 434);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // grid2
+            // 
+            this.grid2.AllowUserToAddRows = false;
+            this.grid2.AllowUserToDeleteRows = false;
+            this.grid2.AllowUserToResizeRows = false;
+            this.grid2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grid2.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.grid2.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.grid2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid2.EditingEnter = Ict.Win.UI.DataGridViewEditingEnter.NextCellOrNextRow;
+            this.grid2.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.grid2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.grid2.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(162)))), ((int)(((byte)(163)))));
+            this.grid2.Location = new System.Drawing.Point(684, 3);
+            this.grid2.Name = "grid2";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grid2.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.grid2.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.grid2.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.grid2.RowTemplate.Height = 24;
+            this.grid2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grid2.Size = new System.Drawing.Size(226, 416);
+            this.grid2.TabIndex = 2;
+            this.grid2.TabStop = false;
+            // 
+            // ColorCombination
+            // 
+            this.ClientSize = new System.Drawing.Size(922, 468);
+            this.Controls.Add(this.grid2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.grid1);
+            this.Name = "ColorCombination";
+            this.Text = "Color Combination";
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Win.UI.Grid grid1;
+        private Win.UI.Button button1;
+        private Win.UI.Grid grid2;
+    }
+}
---
+++ Sci.Production.PublicForm/ColorCombination.cs
@@ -0,0 +1,245 @@
+﻿using Ict;
+using Ict.Win;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Sci.Data;
+
+namespace Sci.Production.PublicForm
+{
+    public partial class ColorCombination : Sci.Win.Subs.Base
+    {
+        private string cutid;
+        private DataTable colortb;
+        public ColorCombination(string cID)
+        {
+
+            InitializeComponent();
+            cutid = cID;
+            
+            color();
+            requery();
+
+        }
+        private void requery()
+        {
+           Helper.Controls.Grid.Generator(this.grid1)
+           .Text("Article", header: "Article", width: Widths.AnsiChars(8), iseditingreadonly: true);
+
+           Helper.Controls.Grid.Generator(this.grid2)
+          .Text("ID", header: "Color ID", width: Widths.AnsiChars(8), iseditingreadonly: true)
+            .Text("Name", header: "Name", width: Widths.AnsiChars(20), iseditingreadonly: true);
+
+            #region Create header
+            string createheader = "Select Article";
+            string headername;
+            //Order_Fabric
+            string headersql = string.Format("Select distinct FabricCode,PatternPanel from Order_FabricCode where id = '{0}' order by PatternPanel,FabricCode", cutid);
+            DataTable headertb;
+            DualResult sqlresult = DBProxy.Current.Select(null, headersql, out headertb);
+            if(!sqlresult)
+            {
+                ShowErr(headersql,sqlresult);
+                return;
+            }
+            foreach (DataRow dr in headertb.Rows)
+            {
+                headername = dr["PatternPanel"].ToString().Trim() + dr["FabricCode"].ToString().Substring(1,2);
+                createheader = createheader + string.Format(",case when a.PatternPAnel+right(a.FabricCode,2)='{0}' then Colorid end '{0}'",headername);
+               
+            Helper.Controls.Grid.Generator(this.grid1)
+            .Text(headername, header: headername, width: Widths.AnsiChars(8), iseditingreadonly: true);
+            }
+            //BOA
+            headersql = string.Format("Select distinct PatternPanel from Order_BOA where id = '{0}' and patternpanel!='' order by PatternPanel", cutid);
+            sqlresult = DBProxy.Current.Select(null, headersql, out headertb);
+            if(!sqlresult)
+            {
+                ShowErr(headersql,sqlresult);
+                return;
+            }
+
+            foreach (DataRow dr in headertb.Rows)
+            {
+                headername = dr["PatternPanel"].ToString().Trim();
+                createheader = createheader + string.Format(",case when a.PatternPanel='{0}' then Colorid end '{0}' ", headername );
+            Helper.Controls.Grid.Generator(this.grid1)
+            .Text(headername, header: headername, width: Widths.AnsiChars(8), iseditingreadonly: true);
+            }
+
+            string createtable = createheader + string.Format(" From Order_ColorCombo a where id ='{0}' ", cutid); //create data
+            createheader = createheader + " From Order_ColorCombo a where 1=0"; //empty table
+
+            DataTable gridtb,datatb;
+            sqlresult = DBProxy.Current.Select(null, createheader, out gridtb);
+            if (!sqlresult)
+            {
+                ShowErr(createheader, sqlresult);
+                return;
+            }
+
+            #endregion
+            #region QT
+            string qtsql = string.Format("Select * from order_FabricCode_Qt a where a.id = '{0}'", cutid);
+            DataTable qttb;
+            sqlresult = DBProxy.Current.Select(null, qtsql, out qttb);
+            DataTable qttb2 = qttb.Copy();
+            if (!sqlresult)
+            {
+                ShowErr(qtsql, sqlresult);
+                return;
+            }
+            DataRow ndr = gridtb.NewRow();
+            ndr["Article"] = "QT With";
+            gridtb.Rows.Add(ndr);
+            ndr = gridtb.NewRow();
+            ndr["Article"] = "QT Width";
+            gridtb.Rows.Add(ndr);
+            foreach (DataRow dr in qttb.Rows)
+            {
+                if (dr["seqno"] != "01")
+                {
+                    string seqno = MyUtility.Convert.NTOC((MyUtility.Convert.GetInt(dr["SEQNo"])-1),2);
+                    DataRow[] drqt = qttb2.Select(string.Format("LectraCode ='{0}' and SEQNO = '{1}'", seqno));
+                    string patternfab = drqt[0]["PatternPanel"].ToString().Trim() + drqt[0]["FabricCode"].ToString().Trim().Substring(1,2);
+                    string qtpatternfab = drqt[0]["QtPatternPanel"].ToString().Trim() + drqt[0]["QtFabricCode"].ToString().Trim().Substring(1,2);
+                    DataRow[] tbdr = gridtb.Select("Article = 'QT With'");
+                    tbdr[0][patternfab] = drqt[0][qtpatternfab];
+                    tbdr = gridtb.Select("Article = 'QT Width'");
+                }
+            }
+            #endregion
+            #region 建立Data
+           
+            sqlresult = DBProxy.Current.Select(null, createtable, out datatb);
+            if (!sqlresult)
+            {
+                ShowErr(createtable, sqlresult);
+                return;
+            }
+            foreach (DataRow dr in datatb.Rows)
+            {
+                DataRow[] dr2 = gridtb.Select(string.Format("Article = '{0}'",dr["Article"]));
+                if (dr2.Length == 0)
+                {
+                    ndr = gridtb.NewRow();
+                    ndr["Article"] = dr["article"];
+                    gridtb.Rows.Add(ndr);
+                    for (int i = 1; i < datatb.Columns.Count; i++)
+                    {
+                        if (!MyUtility.Check.Empty(dr[i]))
+                        {
+                            ndr[i] = dr[i];
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 1; i < datatb.Columns.Count; i++)
+                    {
+                        if (!MyUtility.Check.Empty(dr[i]))
+                        {
+                            dr2[0][i] = dr[i];
+                        }
+                    }
+                }
+                    
+                
+            }
+            #endregion 
+            grid1.DataSource = gridtb;
+
+            grid1.CellEnter += (s, e) =>
+            {
+                if (e.RowIndex == -1) return;
+                DataRow coldr = grid1.GetDataRow(e.RowIndex); //取得資料列
+                string str= string.Empty;
+                string propertyname = grid1.Columns[e.ColumnIndex].DataPropertyName;//取得欄位名稱
+                str = coldr[propertyname].ToString();
+                colortb.DefaultView.RowFilter = string.Format("mid = '{0}'", str);
+
+            };
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+        }
+        private void color()
+        {
+            string brandid = MyUtility.GetValue.Lookup("Brandid", cutid, "Orders", "ID");
+            #region SQL CMD 串出ColorComb 所有Color, 含在Color_Mutiple
+            string colorsql = string.Format(
+                            @"select distinct * from 
+                            (
+                                Select d.id as mid, d.id,d.name
+                                from Color d, 
+                                        (
+                                        select distinct a.colorid
+	                                        from Order_ColorCombo a 
+                                            where a.id='{0}'
+                                        ) c 
+	                            where d.id = c.colorid and d.BrandId = 'ADIDAS' 
+	                            and d.ukey not in 
+                                    (
+                                        Select ColorUkey 
+                                        from Color_Multiple 
+                                        where brandid = '{1}'
+                                    )
+	                            union all
+                                select mid,g.id,g.name
+                                from Color g,
+                                (
+	                                Select mid,f.colorid,f.brandid 
+                                    from Color_Multiple f,
+	                                (
+                                        Select  d.id as mid,d.ukey 
+                                        from Color d, 
+                                        (
+                                            select distinct a.colorid 
+                                            from Order_ColorCombo a 
+                                            where a.id='{0}' 
+                                        ) c 
+	                                    where d.id = c.colorid and d.BrandId = '{1}' 
+	                                    and d.ukey in 
+                                        (
+                                            Select ColorUkey 
+                                            from Color_Multiple 
+                                            where brandid = '{1}'
+                                        )
+                                    ) e
+                                    where e.ukey = f.ColorUkey and f.Brandid = '{1}' 
+                                ) h 
+                                where g.brandid = h.brandid and g.id = h.ColorID and g.BrandId = '{1}' 
+                            ) ord order by id ", cutid,brandid); //串出所有Colorid
+            #endregion
+            
+            DualResult sqlresult = DBProxy.Current.Select(null, colorsql, out colortb);
+            grid2.DataSource = colortb;
+        }
+
+        private void grid1_CurrentCellChanged(object sender, EventArgs e)
+        {
+            
+            //string art = grid1.CurrentCell.Value.ToString();
+            //colortb.DefaultView.RowFilter = string.Format("mid = '{0}'",art);
+        }
+
+        private void grid1_SelectionChanged(object sender, EventArgs e)
+        {
+            //DataRow dr = grid1.GetDataRow(grid1.GetSelectedRowIndex());
+            
+            //grid1.EditingControlShowing += (s,e) =>
+            //    {
+            //        e.
+            //        e.Control.Text 
+            //    }
+            //colortb.DefaultView
+        }
+        
+    }
+}
