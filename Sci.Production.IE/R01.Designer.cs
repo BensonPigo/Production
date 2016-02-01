7eeefdadb839ad9f742c215dff3a4ae73c6ec977
YINMEI <yinmei@sportscity.com.tw>
2016-02-01 12:38:14 +0000

    Add R01

---
+++ Sci.Production.IE/R01.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Sci.Production.IE
+{
+    partial class R01
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.label3 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.textBox1 = new Sci.Win.UI.TextBox();
+            this.textBox2 = new Sci.Win.UI.TextBox();
+            this.textBox3 = new Sci.Win.UI.TextBox();
+            this.comboBox1 = new Sci.Win.UI.ComboBox();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(293, 12);
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(293, 48);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(293, 84);
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 23);
+            this.label1.TabIndex = 94;
+            this.label1.Text = "Factory";
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(13, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 23);
+            this.label2.TabIndex = 95;
+            this.label2.Text = "Style#";
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(13, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 23);
+            this.label3.TabIndex = 96;
+            this.label3.Text = "Season";
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(13, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 23);
+            this.label4.TabIndex = 97;
+            this.label4.Text = "Team";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(70, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(70, 23);
+            this.textBox1.TabIndex = 99;
+            this.textBox1.PopUp += new System.EventHandler<Sci.Win.UI.TextBoxPopUpEventArgs>(this.textBox1_PopUp);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox2.Location = new System.Drawing.Point(70, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(150, 23);
+            this.textBox2.TabIndex = 100;
+            this.textBox2.PopUp += new System.EventHandler<Sci.Win.UI.TextBoxPopUpEventArgs>(this.textBox2_PopUp);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.White;
+            this.textBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox3.Location = new System.Drawing.Point(70, 84);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(80, 23);
+            this.textBox3.TabIndex = 101;
+            this.textBox3.PopUp += new System.EventHandler<Sci.Win.UI.TextBoxPopUpEventArgs>(this.textBox3_PopUp);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IsSupportUnselect = true;
+            this.comboBox1.Location = new System.Drawing.Point(70, 120);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(70, 24);
+            this.comboBox1.TabIndex = 102;
+            // 
+            // R01
+            // 
+            this.ClientSize = new System.Drawing.Size(385, 179);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.EditMode = true;
+            this.IsSupportToPrint = false;
+            this.Name = "R01";
+            this.Text = "R01. Line Mapping List";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.textBox1, 0);
+            this.Controls.SetChildIndex(this.textBox2, 0);
+            this.Controls.SetChildIndex(this.textBox3, 0);
+            this.Controls.SetChildIndex(this.comboBox1, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.Label label1;
+        private Win.UI.Label label2;
+        private Win.UI.Label label3;
+        private Win.UI.Label label4;
+        private Win.UI.TextBox textBox1;
+        private Win.UI.TextBox textBox2;
+        private Win.UI.TextBox textBox3;
+        private Win.UI.ComboBox comboBox1;
+    }
+}
---
+++ Sci.Production.IE/R01.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict.Win;
+using Ict;
+using Sci.Data;
+
+namespace Sci.Production.IE
+{
+    public partial class R01 : Sci.Win.Tems.PrintForm
+    {
+        string factory, style, season, team;
+        DataTable printData;
+        public R01(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            MyUtility.Tool.SetupCombox(comboBox1, 1, 1, ",A,B");
+        }
+
+        //Factory
+        private void textBox1_PopUp(object sender, Win.UI.TextBoxPopUpEventArgs e)
+        {
+            string sqlCmd = "select distinct FTYGroup from Factory where Junk = 0";
+
+            Sci.Win.Tools.SelectItem item = new Sci.Win.Tools.SelectItem(sqlCmd, "8", textBox1.Text, "Factory");
+            DialogResult returnResult = item.ShowDialog();
+            if (returnResult == DialogResult.Cancel) { return; }
+            textBox1.Text = item.GetSelectedString();
+        }
+
+        //Style
+        private void textBox2_PopUp(object sender, Win.UI.TextBoxPopUpEventArgs e)
+        {
+            string sqlCmd = "select distinct ID,BrandID,Description from Style where Junk = 0 order by ID";
+
+            Sci.Win.Tools.SelectItem item = new Sci.Win.Tools.SelectItem(sqlCmd, "15,8,50", textBox2.Text, "Style#,Brand,Description");
+            DialogResult returnResult = item.ShowDialog();
+            if (returnResult == DialogResult.Cancel) { return; }
+            textBox2.Text = item.GetSelectedString();
+        }
+
+        //Season
+        private void textBox3_PopUp(object sender, Win.UI.TextBoxPopUpEventArgs e)
+        {
+            string sqlCmd = "select distinct ID from Season where Junk = 0";
+
+            Sci.Win.Tools.SelectItem item = new Sci.Win.Tools.SelectItem(sqlCmd, "10", textBox3.Text, "Season");
+            DialogResult returnResult = item.ShowDialog();
+            if (returnResult == DialogResult.Cancel) { return; }
+            textBox3.Text = item.GetSelectedString();
+        }
+
+        // 驗證輸入條件
+        protected override bool ValidateInput()
+        {
+            factory = textBox1.Text;
+            style = textBox2.Text;
+            season = textBox3.Text;
+            team = comboBox1.SelectedIndex == -1 || comboBox1.SelectedIndex == 0 ? "" : comboBox1.SelectedIndex == 1 ? "A" : "B";
+
+            return base.ValidateInput();
+        }
+
+        // 非同步取資料
+        protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            StringBuilder sqlCmd = new StringBuilder();
+            sqlCmd.Append(@"select lm.FactoryID,lm.StyleID,lm.ComboType,lm.SeasonID,lm.BrandID,lm.Version,lm.SewingLineID,
+lm.Team,lm.CurrentOperators,lm.StandardOutput,lm.TaktTime,lm.TotalGSD,lm.TotalCycle,
+IIF(lm.HighestCycle = 0,0,Round(3600.0/lm.HighestCycle,2)) as EOLR,
+IIF(lm.HighestCycle*lm.CurrentOperators = 0,0,CONVERT(DECIMAL,lm.TotalGSD)/lm.HighestCycle/lm.CurrentOperators) as Eff,
+IIF(lm.HighestCycle*lm.CurrentOperators = 0,0,CONVERT(DECIMAL,lm.TotalCycle)/lm.HighestCycle/lm.CurrentOperators) as LB,
+IIF(lm.TaktTime*lm.CurrentOperators = 0,0,CONVERT(DECIMAL,lm.TotalCycle)/lm.TaktTime/lm.CurrentOperators) as LLEF,
+IIF(lm.HighestCycle * lm.CurrentOperators = 0,0,(ROUND(3600.0/lm.HighestCycle, 2) * (select isnull(CPU,0) from Style where Ukey = lm.StyleUKey)/lm.CurrentOperators)) as PPH,
+isnull((select Name from Pass1 where ID = lm.AddName),'') as CreateBy,lm.AddDate,
+isnull((select Name from Pass1 where ID = lm.EditName),'') as EditBy,lm.EditDate
+from LineMapping lm
+where 1 = 1");
+            if (!MyUtility.Check.Empty(factory))
+            {
+                sqlCmd.Append(string.Format(" and lm.FactoryID = '{0}'",factory));
+            }
+            if (!MyUtility.Check.Empty(style))
+            {
+                sqlCmd.Append(string.Format(" and lm.StyleID = '{0}'",style));
+            }
+            if (!MyUtility.Check.Empty(season))
+            {
+                sqlCmd.Append(string.Format(" and lm.SeasonID = '{0}'",season));
+            }
+            if (!MyUtility.Check.Empty(team))
+            {
+                sqlCmd.Append(string.Format(" and lm.Team = '{0}'", team));
+            }
+
+            DualResult result = DBProxy.Current.Select(null, sqlCmd.ToString(), out printData);
+            if (!result)
+            {
+                DualResult failResult = new DualResult(false, "Query data fail\r\n" + result.ToString());
+                return failResult;
+            }
+            return Result.True;
+        }
+
+        // 產生Excel
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            // 顯示筆數於PrintForm上Count欄位
+            SetCount(printData.Rows.Count);
+
+            if (printData.Rows.Count <= 0)
+            {
+                MyUtility.Msg.WarningBox("Data not found!");
+                return false;
+            }
+
+            string strXltName = Sci.Env.Cfg.XltPathDir + "IE_R01_LineMappingList.xltx";
+            Microsoft.Office.Interop.Excel.Application excel = MyUtility.Excel.ConnectExcel(strXltName);
+            if (excel == null) return false;
+            Microsoft.Office.Interop.Excel.Worksheet worksheet = excel.ActiveWorkbook.Worksheets[1];
+            //填內容值
+            int intRowsStart = 2;
+            object[,] objArray = new object[1, 22];
+            foreach (DataRow dr in printData.Rows)
+            {
+                objArray[0, 0] = dr["FactoryID"];
+                objArray[0, 1] = dr["StyleID"];
+                objArray[0, 2] = dr["ComboType"];
+                objArray[0, 3] = dr["SeasonID"];
+                objArray[0, 4] = dr["BrandID"];
+                objArray[0, 5] = dr["Version"];
+                objArray[0, 6] = dr["SewingLineID"];
+                objArray[0, 7] = dr["Team"];
+                objArray[0, 8] = dr["CurrentOperators"];
+                objArray[0, 9] = dr["StandardOutput"];
+                objArray[0, 10] = dr["TaktTime"];
+                objArray[0, 11] = dr["TotalGSD"];
+                objArray[0, 12] = dr["TotalCycle"];
+                objArray[0, 13] = dr["EOLR"];
+                objArray[0, 14] = dr["Eff"];
+                objArray[0, 15] = dr["LB"];
+                objArray[0, 16] = dr["LLEF"];
+                objArray[0, 17] = dr["PPH"];
+                objArray[0, 18] = dr["CreateBy"];
+                objArray[0, 19] = dr["AddDate"];
+                objArray[0, 20] = dr["EditBy"];
+                objArray[0, 21] = dr["EditDate"];
+
+                worksheet.Range[String.Format("A{0}:V{0}", intRowsStart)].Value2 = objArray;
+                intRowsStart++;
+            }
+
+            excel.Cells.EntireColumn.AutoFit();
+            excel.Cells.EntireRow.AutoFit();
+
+            excel.Visible = true;
+            return true;
+        }
+    }
+}
