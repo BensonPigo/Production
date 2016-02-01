5ec915e0e452d9482596edd21f1a44e5be618952
YINMEI <yinmei@sportscity.com.tw>
2016-02-01 03:14:46 +0000

    Account No補上InputMask

--- Sci.Production.Basic/B04_AccountNo.cs
+++ Sci.Production.Basic/B04_AccountNo.cs
@@ -14,6 +14,7 @@ namespace Sci.Production.Basic
 {
     public partial class B04_AccountNo : Sci.Win.Subs.Input4
     {
+        DataGridViewGeneratorMaskedTextColumnSettings accountNo = new DataGridViewGeneratorMaskedTextColumnSettings();
         public B04_AccountNo(bool canedit, string keyvalue1, string keyvalue2, string keyvalue3)
             : base(canedit, keyvalue1, keyvalue2, keyvalue3)
         {
@@ -48,10 +49,12 @@ order by a.Seq";
 
         protected override bool OnGridSetup()
         {
+            accountNo.TextMaskFormat = System.Windows.Forms.MaskFormat.ExcludePromptAndLiterals;
+
             Helper.Controls.Grid.Generator(this.grid)
                 .Text("ArtworkTypeID", header: "Type", width: Widths.AnsiChars(20),iseditable:false)
-                .Text("AccountNo", header: "Account No", width: Widths.AnsiChars(10));
-
+                .MaskedText("AccountNo", "0000-0000", header: "Account No", width: Widths.AnsiChars(8), settings: accountNo);
+            
             return true;
         }
 
