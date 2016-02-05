3c4db8ec356c6b2e5e00300e6a58a5ba77923cae
YINMEI <yinmei@sportscity.com.tw>
2016-02-05 04:27:56 +0000

    ADD Function: getInvAdjQty

---
+++ Database/dbo/Functions/getInvAdjQty.sql
@@ -0,0 +1,13 @@
+﻿
+CREATE FUNCTION [dbo].[getInvAdjQty](@orderid varchar(13), @seq varchar(2))
+RETURNS int
+BEGIN
+	DECLARE @int int --要回傳的字串
+	select @int = isnull(sum(iq.DiffQty),0)
+	from InvAdjust i, InvAdjust_Qty iq
+	where i.ID = iq.ID
+	and i.OrderID = @orderid
+	and i.OrderShipmodeSeq = @seq
+	
+	RETURN @int
+END
