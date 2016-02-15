2860fe04db1b0a4fd41452ee9f851d0c89b5da83
YINMEI <yinmei@sportscity.com.tw>
2016-02-15 04:30:22 +0000

    Add R06

---
+++ Sci.Production.Shipping/R06.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Sci.Production.Shipping
+{
+    partial class R06
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.label3 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.label5 = new Sci.Win.UI.Label();
+            this.label6 = new Sci.Win.UI.Label();
+            this.dateRange1 = new Sci.Win.UI.DateRange();
+            this.dateRange2 = new Sci.Win.UI.DateRange();
+            this.textBox1 = new Sci.Win.UI.TextBox();
+            this.textBox2 = new Sci.Win.UI.TextBox();
+            this.comboBox1 = new Sci.Win.UI.ComboBox();
+            this.txtsubcon1 = new Sci.Production.Class.txtsubcon();
+            this.comboBox2 = new Sci.Win.UI.ComboBox();
+            this.label7 = new Sci.Win.UI.Label();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(380, 12);
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(380, 48);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(380, 84);
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 23);
+            this.label1.TabIndex = 94;
+            this.label1.Text = "Date";
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(13, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 23);
+            this.label2.TabIndex = 95;
+            this.label2.Text = "Apv. Date";
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(13, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 23);
+            this.label3.TabIndex = 96;
+            this.label3.Text = "BL No.";
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(13, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 23);
+            this.label4.TabIndex = 97;
+            this.label4.Text = "M";
+            // 
+            // label5
+            // 
+            this.label5.Lines = 0;
+            this.label5.Location = new System.Drawing.Point(13, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 23);
+            this.label5.TabIndex = 98;
+            this.label5.Text = "Supplier";
+            // 
+            // label6
+            // 
+            this.label6.Lines = 0;
+            this.label6.Location = new System.Drawing.Point(13, 221);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 23);
+            this.label6.TabIndex = 99;
+            this.label6.Text = "Order by";
+            // 
+            // dateRange1
+            // 
+            this.dateRange1.IsRequired = false;
+            this.dateRange1.Location = new System.Drawing.Point(82, 12);
+            this.dateRange1.Name = "dateRange1";
+            this.dateRange1.Size = new System.Drawing.Size(280, 23);
+            this.dateRange1.TabIndex = 100;
+            // 
+            // dateRange2
+            // 
+            this.dateRange2.IsRequired = false;
+            this.dateRange2.Location = new System.Drawing.Point(82, 48);
+            this.dateRange2.Name = "dateRange2";
+            this.dateRange2.Size = new System.Drawing.Size(280, 23);
+            this.dateRange2.TabIndex = 101;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(82, 84);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 23);
+            this.textBox1.TabIndex = 102;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox2.Location = new System.Drawing.Point(82, 114);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(190, 23);
+            this.textBox2.TabIndex = 103;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IsSupportUnselect = true;
+            this.comboBox1.Location = new System.Drawing.Point(82, 148);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(73, 24);
+            this.comboBox1.TabIndex = 104;
+            // 
+            // txtsubcon1
+            // 
+            this.txtsubcon1.DisplayBox1Binding = "";
+            this.txtsubcon1.IsIncludeJunk = false;
+            this.txtsubcon1.Location = new System.Drawing.Point(82, 184);
+            this.txtsubcon1.Name = "txtsubcon1";
+            this.txtsubcon1.Size = new System.Drawing.Size(170, 23);
+            this.txtsubcon1.TabIndex = 105;
+            this.txtsubcon1.TextBox1Binding = "";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.Color.White;
+            this.comboBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.IsSupportUnselect = true;
+            this.comboBox2.Location = new System.Drawing.Point(82, 221);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(101, 24);
+            this.comboBox2.TabIndex = 106;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Lines = 0;
+            this.label7.Location = new System.Drawing.Point(279, 84);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 23);
+            this.label7.TabIndex = 107;
+            this.label7.Text = "～";
+            this.label7.TextStyle.BorderColor = System.Drawing.Color.Black;
+            this.label7.TextStyle.Color = System.Drawing.Color.Black;
+            this.label7.TextStyle.ExtBorderColor = System.Drawing.Color.Black;
+            this.label7.TextStyle.GradientColor = System.Drawing.Color.Black;
+            // 
+            // R06
+            // 
+            this.ClientSize = new System.Drawing.Size(472, 281);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.txtsubcon1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dateRange2);
+            this.Controls.Add(this.dateRange1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.EditMode = true;
+            this.IsSupportToPrint = false;
+            this.Name = "R06";
+            this.Text = "R06. Payment List - Shipping";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.label5, 0);
+            this.Controls.SetChildIndex(this.label6, 0);
+            this.Controls.SetChildIndex(this.dateRange1, 0);
+            this.Controls.SetChildIndex(this.dateRange2, 0);
+            this.Controls.SetChildIndex(this.textBox1, 0);
+            this.Controls.SetChildIndex(this.textBox2, 0);
+            this.Controls.SetChildIndex(this.comboBox1, 0);
+            this.Controls.SetChildIndex(this.txtsubcon1, 0);
+            this.Controls.SetChildIndex(this.comboBox2, 0);
+            this.Controls.SetChildIndex(this.label7, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.Label label1;
+        private Win.UI.Label label2;
+        private Win.UI.Label label3;
+        private Win.UI.Label label4;
+        private Win.UI.Label label5;
+        private Win.UI.Label label6;
+        private Win.UI.DateRange dateRange1;
+        private Win.UI.DateRange dateRange2;
+        private Win.UI.TextBox textBox1;
+        private Win.UI.TextBox textBox2;
+        private Win.UI.ComboBox comboBox1;
+        private Class.txtsubcon txtsubcon1;
+        private Win.UI.ComboBox comboBox2;
+        private Win.UI.Label label7;
+    }
+}
---
+++ Sci.Production.Shipping/R06.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict.Win;
+using Ict;
+using Sci.Data;
+
+namespace Sci.Production.Shipping
+{
+    public partial class R06 : Sci.Win.Tems.PrintForm
+    {
+        DateTime? date1, date2, apvDate1, apvDate2;
+        string blno1, blno2,supplier,mDivision;
+        int orderby;
+        DataTable printData;
+        public R06(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            dateRange1.Value1 = new DateTime(DateTime.Now.Year, 1, 1); //預設帶入登入系統當年的第一天
+            dateRange1.Value2 = DateTime.Today;
+            dateRange2.Value2 = DateTime.Today;
+            DataTable mDivision;
+            DBProxy.Current.Select(null, "select '' as ID union all select ID from MDivision", out mDivision);
+            MyUtility.Tool.SetupCombox(comboBox1, 1, mDivision);
+            comboBox1.Text = Sci.Env.User.Keyword;
+
+            MyUtility.Tool.SetupCombox(comboBox2, 1, 1, "M,B/L No.");
+            comboBox2.SelectedIndex = 0;
+        }
+
+        // 驗證輸入條件
+        protected override bool ValidateInput()
+        {
+            if (MyUtility.Check.Empty(dateRange1.Value1) && MyUtility.Check.Empty(dateRange1.Value2) && MyUtility.Check.Empty(dateRange2.Value1) && MyUtility.Check.Empty(dateRange2.Value2))
+            {
+                MyUtility.Msg.WarningBox("Date can't empty!!");
+                return false;
+            }
+
+            mDivision = comboBox1.Text;
+            date1 = dateRange1.Value1;
+            date2 = dateRange1.Value2;
+            apvDate1 = dateRange2.Value1;
+            apvDate2 = dateRange2.Value2;
+            blno1 = textBox1.Text;
+            blno2 = textBox2.Text;
+            supplier = txtsubcon1.TextBox1.Text;
+            orderby = comboBox2.SelectedIndex;
+
+            return base.ValidateInput();
+        }
+
+        // 非同步取資料
+        protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            StringBuilder sqlCmd = new StringBuilder();
+            sqlCmd.Append(@"select s.Type,s.LocalSuppID+'-'+ISNULL(l.Abb,'') as Supplier,s.ID,s.VoucherNo,
+s.CDate,s.ApvDate,s.MDivisionID,s.CurrencyID,s.Amount+s.VAT as Amt,s.BLNo,s.Remark,s.InvNo,
+isnull((select CONCAT(InvNo,'/') from (select distinct InvNo from ShareExpense where ShippingAPID = s.ID) a for xml path('')),'') as ExportInv
+from ShippingAP s
+left join LocalSupp l on s.LocalSuppID = l.ID
+where s.Status = 'Approved'");
+
+            if (!MyUtility.Check.Empty(date1))
+            {
+                sqlCmd.Append(string.Format(" and s.CDate >= '{0}'", Convert.ToDateTime(date1).ToString("d")));
+            }
+
+            if (!MyUtility.Check.Empty(date2))
+            {
+                sqlCmd.Append(string.Format(" and s.CDate <= '{0}'", Convert.ToDateTime(date2).ToString("d")));
+            }
+
+            if (!MyUtility.Check.Empty(apvDate1))
+            {
+                sqlCmd.Append(string.Format(" and s.ApvDate >= '{0}'", Convert.ToDateTime(apvDate1).ToString("d")));
+            }
+
+            if (!MyUtility.Check.Empty(apvDate2))
+            {
+                sqlCmd.Append(string.Format(" and s.ApvDate <= '{0}'", Convert.ToDateTime(apvDate2).ToString("d")));
+            }
+
+            if (!MyUtility.Check.Empty(blno1))
+            {
+                sqlCmd.Append(string.Format(" and s.BLNo >= '{0}'", blno1));
+            }
+
+            if (!MyUtility.Check.Empty(blno2))
+            {
+                sqlCmd.Append(string.Format(" and s.BLNo <= '{0}'", blno2));
+            }
+
+            if (!MyUtility.Check.Empty(mDivision))
+            {
+                sqlCmd.Append(string.Format(" and s.MDivisionID = '{0}'", mDivision));
+            }
+
+            if (!MyUtility.Check.Empty(supplier))
+            {
+                sqlCmd.Append(string.Format(" and s.LocalSuppID = '{0}'", supplier));
+            }
+
+            if (orderby == 0)
+            {
+                sqlCmd.Append(" order by s.MDivisionID");
+            }
+            else if (orderby == 1)
+            {
+                sqlCmd.Append(" order by s.BLNo");
+            }
+
+            DualResult result = DBProxy.Current.Select(null, sqlCmd.ToString(), out printData);
+            if (!result)
+            {
+                DualResult failResult = new DualResult(false, "Query data fail\r\n" + result.ToString());
+                return failResult;
+            }
+            return Result.True;
+        }
+
+        // 產生Excel
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            // 顯示筆數於PrintForm上Count欄位
+            SetCount(printData.Rows.Count);
+
+            if (printData.Rows.Count <= 0)
+            {
+                MyUtility.Msg.WarningBox("Data not found!");
+                return false;
+            }
+
+            MyUtility.Msg.WaitWindows("Starting EXCEL...");
+            string strXltName = Sci.Env.Cfg.XltPathDir + "Shipping_R06_PaymentList.xltx";
+            Microsoft.Office.Interop.Excel.Application excel = MyUtility.Excel.ConnectExcel(strXltName);
+            if (excel == null) return false;
+            Microsoft.Office.Interop.Excel.Worksheet worksheet = excel.ActiveWorkbook.Worksheets[1];
+
+            //填內容值
+            int intRowsStart = 3;
+            object[,] objArray = new object[1, 13];
+            foreach (DataRow dr in printData.Rows)
+            {
+                objArray[0, 0] = dr["Type"];
+                objArray[0, 1] = dr["Supplier"];
+                objArray[0, 2] = dr["ID"];
+                objArray[0, 3] = dr["VoucherNo"];
+                objArray[0, 4] = dr["CDate"];
+                objArray[0, 5] = dr["ApvDate"];
+                objArray[0, 6] = dr["MDivisionID"];
+                objArray[0, 7] = dr["CurrencyID"];
+                objArray[0, 8] = dr["Amt"];
+                objArray[0, 9] = dr["BLNo"];
+                objArray[0, 10] = dr["Remark"];
+                objArray[0, 11] = dr["InvNo"];
+                objArray[0, 12] = MyUtility.Check.Empty(dr["ExportInv"]) ? "" : MyUtility.Convert.GetString(dr["ExportInv"]).Substring(0, MyUtility.Convert.GetString(dr["ExportInv"]).Length - 1);
+                worksheet.Range[String.Format("A{0}:M{0}", intRowsStart)].Value2 = objArray;
+                intRowsStart++;
+            }
+
+            excel.Cells.EntireColumn.AutoFit();
+            excel.Cells.EntireRow.AutoFit();
+            MyUtility.Msg.WaitClear();
+            excel.Visible = true;
+            return true;
+        }
+    }
+}
