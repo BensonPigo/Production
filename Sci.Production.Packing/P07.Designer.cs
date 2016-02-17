f3f40e7df5d441e6e158ff9e45e1665412602a66
YINMEI <yinmei@sportscity.com.tw>
2016-02-17 06:53:25 +0000

    Add P07

---
+++ Sci.Production.Packing/P07.Designer.cs
@@ -0,0 +1,382 @@
+﻿namespace Sci.Production.Packing
+{
+    partial class P07
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle16 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle17 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle18 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new Sci.Win.UI.Panel();
+            this.panel2 = new Sci.Win.UI.Panel();
+            this.panel3 = new Sci.Win.UI.Panel();
+            this.panel4 = new Sci.Win.UI.Panel();
+            this.panel5 = new Sci.Win.UI.Panel();
+            this.listControlBindingSource1 = new Sci.Win.UI.ListControlBindingSource();
+            this.grid1 = new Sci.Win.UI.Grid();
+            this.radioGroup1 = new Sci.Win.UI.RadioGroup();
+            this.label1 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.label3 = new Sci.Win.UI.Label();
+            this.textBox1 = new Sci.Win.UI.TextBox();
+            this.textBox2 = new Sci.Win.UI.TextBox();
+            this.label4 = new Sci.Win.UI.Label();
+            this.dateRange1 = new Sci.Win.UI.DateRange();
+            this.txtbrand1 = new Sci.Production.Class.txtbrand();
+            this.button1 = new Sci.Win.UI.Button();
+            this.radioGroup2 = new Sci.Win.UI.RadioGroup();
+            this.radioPanel1 = new Sci.Win.UI.RadioPanel();
+            this.radioButton1 = new Sci.Win.UI.RadioButton();
+            this.radioButton2 = new Sci.Win.UI.RadioButton();
+            this.button2 = new Sci.Win.UI.Button();
+            this.button3 = new Sci.Win.UI.Button();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).BeginInit();
+            this.radioGroup1.SuspendLayout();
+            this.radioGroup2.SuspendLayout();
+            this.radioPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(10, 521);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(898, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(10, 521);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.radioGroup2);
+            this.panel3.Controls.Add(this.radioGroup1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(10, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(888, 150);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button3);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(10, 479);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(888, 42);
+            this.panel4.TabIndex = 4;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.grid1);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(10, 150);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(888, 329);
+            this.panel5.TabIndex = 5;
+            // 
+            // grid1
+            // 
+            this.grid1.AllowUserToAddRows = false;
+            this.grid1.AllowUserToDeleteRows = false;
+            this.grid1.AllowUserToResizeRows = false;
+            this.grid1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.grid1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            dataGridViewCellStyle16.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle16.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle16.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            dataGridViewCellStyle16.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle16.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle16.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle16.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grid1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle16;
+            this.grid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid1.DataSource = this.listControlBindingSource1;
+            dataGridViewCellStyle17.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle17.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle17.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            dataGridViewCellStyle17.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle17.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle17.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle17.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.grid1.DefaultCellStyle = dataGridViewCellStyle17;
+            this.grid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid1.EditingEnter = Ict.Win.UI.DataGridViewEditingEnter.NextCellOrNextRow;
+            this.grid1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.grid1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.grid1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(162)))), ((int)(((byte)(163)))));
+            this.grid1.Location = new System.Drawing.Point(0, 0);
+            this.grid1.Name = "grid1";
+            dataGridViewCellStyle18.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle18.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle18.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            dataGridViewCellStyle18.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle18.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle18.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle18.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grid1.RowHeadersDefaultCellStyle = dataGridViewCellStyle18;
+            this.grid1.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.grid1.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.grid1.RowTemplate.Height = 24;
+            this.grid1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grid1.Size = new System.Drawing.Size(888, 329);
+            this.grid1.TabIndex = 0;
+            this.grid1.TabStop = false;
+            // 
+            // radioGroup1
+            // 
+            this.radioGroup1.Controls.Add(this.button1);
+            this.radioGroup1.Controls.Add(this.txtbrand1);
+            this.radioGroup1.Controls.Add(this.dateRange1);
+            this.radioGroup1.Controls.Add(this.label4);
+            this.radioGroup1.Controls.Add(this.textBox2);
+            this.radioGroup1.Controls.Add(this.textBox1);
+            this.radioGroup1.Controls.Add(this.label3);
+            this.radioGroup1.Controls.Add(this.label2);
+            this.radioGroup1.Controls.Add(this.label1);
+            this.radioGroup1.Location = new System.Drawing.Point(6, 4);
+            this.radioGroup1.Name = "radioGroup1";
+            this.radioGroup1.Size = new System.Drawing.Size(501, 140);
+            this.radioGroup1.TabIndex = 0;
+            this.radioGroup1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(7, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Garment Booking#";
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(7, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "FCR Date";
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(7, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Brand";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(131, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(186, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox2.Location = new System.Drawing.Point(131, 44);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(186, 23);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(319, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 23);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "～";
+            this.label4.TextStyle.BorderColor = System.Drawing.Color.Black;
+            this.label4.TextStyle.Color = System.Drawing.Color.Black;
+            this.label4.TextStyle.ExtBorderColor = System.Drawing.Color.Black;
+            this.label4.TextStyle.GradientColor = System.Drawing.Color.Black;
+            // 
+            // dateRange1
+            // 
+            this.dateRange1.IsRequired = false;
+            this.dateRange1.Location = new System.Drawing.Point(131, 76);
+            this.dateRange1.Name = "dateRange1";
+            this.dateRange1.Size = new System.Drawing.Size(280, 23);
+            this.dateRange1.TabIndex = 6;
+            // 
+            // txtbrand1
+            // 
+            this.txtbrand1.BackColor = System.Drawing.Color.White;
+            this.txtbrand1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtbrand1.Location = new System.Drawing.Point(131, 109);
+            this.txtbrand1.Name = "txtbrand1";
+            this.txtbrand1.Size = new System.Drawing.Size(98, 23);
+            this.txtbrand1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(405, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 30);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Query";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioGroup2
+            // 
+            this.radioGroup2.Controls.Add(this.button2);
+            this.radioGroup2.Controls.Add(this.radioPanel1);
+            this.radioGroup2.Location = new System.Drawing.Point(526, 4);
+            this.radioGroup2.Name = "radioGroup2";
+            this.radioGroup2.Size = new System.Drawing.Size(357, 140);
+            this.radioGroup2.TabIndex = 1;
+            this.radioGroup2.TabStop = false;
+            // 
+            // radioPanel1
+            // 
+            this.radioPanel1.Controls.Add(this.radioButton2);
+            this.radioPanel1.Controls.Add(this.radioButton1);
+            this.radioPanel1.Location = new System.Drawing.Point(7, 15);
+            this.radioPanel1.Name = "radioPanel1";
+            this.radioPanel1.Size = new System.Drawing.Size(255, 63);
+            this.radioPanel1.TabIndex = 0;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.radioButton1.Location = new System.Drawing.Point(4, 8);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(250, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "FormA (for Adidas/UA/Saucony/NB)";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.radioButton2.Location = new System.Drawing.Point(4, 36);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(157, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "FormB (for LLL/TNF)";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(272, 15);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "To Excel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button3.Location = new System.Drawing.Point(803, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 30);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Close";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // P07
+            // 
+            this.ClientSize = new System.Drawing.Size(908, 521);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.EditMode = true;
+            this.Name = "P07";
+            this.Text = "P07. Batch Print Packing List Report (Bulk)";
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.Controls.SetChildIndex(this.panel2, 0);
+            this.Controls.SetChildIndex(this.panel3, 0);
+            this.Controls.SetChildIndex(this.panel4, 0);
+            this.Controls.SetChildIndex(this.panel5, 0);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).EndInit();
+            this.radioGroup1.ResumeLayout(false);
+            this.radioGroup1.PerformLayout();
+            this.radioGroup2.ResumeLayout(false);
+            this.radioPanel1.ResumeLayout(false);
+            this.radioPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Win.UI.Panel panel1;
+        private Win.UI.Panel panel2;
+        private Win.UI.Panel panel3;
+        private Win.UI.RadioGroup radioGroup1;
+        private Win.UI.TextBox textBox1;
+        private Win.UI.Label label3;
+        private Win.UI.Label label2;
+        private Win.UI.Label label1;
+        private Win.UI.Panel panel4;
+        private Win.UI.Panel panel5;
+        private Win.UI.Grid grid1;
+        private Win.UI.ListControlBindingSource listControlBindingSource1;
+        private Win.UI.RadioGroup radioGroup2;
+        private Win.UI.Button button2;
+        private Win.UI.RadioPanel radioPanel1;
+        private Win.UI.RadioButton radioButton2;
+        private Win.UI.RadioButton radioButton1;
+        private Win.UI.Button button1;
+        private Class.txtbrand txtbrand1;
+        private Win.UI.DateRange dateRange1;
+        private Win.UI.Label label4;
+        private Win.UI.TextBox textBox2;
+        private Win.UI.Button button3;
+    }
+}
---
+++ Sci.Production.Packing/P07.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict.Win;
+using Ict;
+using Sci.Data;
+
+namespace Sci.Production.Packing
+{
+    public partial class P07 : Sci.Win.Tems.QueryForm
+    {
+        Ict.Win.UI.DataGridViewCheckBoxColumn col_chk;
+        DataTable gridData, printData, ctnDim, qtyBDown;
+        public P07(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnFormLoaded()
+        {
+            base.OnFormLoaded();
+            radioButton1.Checked = true;
+
+            //Grid設定
+            this.grid1.IsEditingReadOnly = false;
+            this.grid1.DataSource = listControlBindingSource1;
+            Helper.Controls.Grid.Generator(this.grid1)
+               .CheckBox("Selected", header: "", width: Widths.AnsiChars(3), iseditable: true, trueValue: 1, falseValue: 0).Get(out col_chk)
+               .Text("ID", header: "Packing No.", width: Widths.AnsiChars(15), iseditingreadonly: true)
+               .Text("OrderID", header: "SP#", width: Widths.AnsiChars(15), iseditingreadonly: true)
+               .Text("InvNo", header: "Garment Booking#", width: Widths.AnsiChars(25), iseditingreadonly: true)
+               .Date("FCRDate", header: "FCR Date", iseditingreadonly: true)
+               .Text("BrandID", header: "Brand", width: Widths.AnsiChars(13), iseditingreadonly: true);
+        }
+
+        //Query
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MyUtility.Check.Empty(textBox1.Text) && MyUtility.Check.Empty(textBox2.Text) && MyUtility.Check.Empty(dateRange1.Value1) && MyUtility.Check.Empty(dateRange1.Value2))
+            {
+                MyUtility.Msg.WarningBox("< Garment Booking# > and < FCR Date > can't both empty!");
+                return;
+            }
+            StringBuilder sqlCmd = new StringBuilder();
+            sqlCmd.Append(string.Format(@"with tmpPackingData
+as (
+select distinct p.ID,pd.OrderID,p.INVNo,g.FCRDate,o.BrandID,pd.OrderShipmodeSeq,isnull(oq.Qty,0) as Qty,
+p.CustCDID,p.ShipModeID,p.Remark,p.CTNQty
+from PackingList p
+inner join PackingList_Detail pd on p.ID = pd.ID
+inner join GMTBooking g on p.INVNo = g.ID
+inner join Orders o on pd.OrderID = o.ID
+left join Order_QtyShip oq on o.ID = oq.Id and oq.Seq = pd.OrderShipmodeSeq
+where p.Type = 'B'
+and p.MDivisionID = '{0}'", Sci.Env.User.Keyword));
+            if (!MyUtility.Check.Empty(textBox1.Text))
+            {
+                sqlCmd.Append(string.Format(" and p.INVNo >= '{0}'", textBox1.Text));
+            }
+            if (!MyUtility.Check.Empty(textBox2.Text))
+            {
+                sqlCmd.Append(string.Format(" and p.INVNo <= '{0}'", textBox2.Text));
+            }
+            if (!MyUtility.Check.Empty(dateRange1.Value1))
+            {
+                sqlCmd.Append(string.Format(" and g.FCRDate >= '{0}'", Convert.ToDateTime(dateRange1.Value1).ToString("d")));
+            }
+            if (!MyUtility.Check.Empty(dateRange1.Value2))
+            {
+                sqlCmd.Append(string.Format(" and g.FCRDate <= '{0}'", Convert.ToDateTime(dateRange1.Value2).ToString("d")));
+            }
+            if (!MyUtility.Check.Empty(txtbrand1.Text))
+            {
+                sqlCmd.Append(string.Format(" and o.BrandID = '{0}'", txtbrand1.Text));
+            }
+            sqlCmd.Append(@"),
+MultipleOrder
+as (
+select ID,COUNT(ID) as cnt from tmpPackingData group by ID having COUNT(ID) > 1
+)
+select 0 as selected,* from tmpPackingData where NOT EXISTS (select 1 from MultipleOrder where ID = tmpPackingData.ID) order by ID"); //排除多SP#在同一張PL的資料
+            DualResult result = DBProxy.Current.Select(null, sqlCmd.ToString(), out gridData);
+            if (result)
+            {
+                if (gridData.Rows.Count == 0)
+                {
+                    MyUtility.Msg.WarningBox("Data not found!");
+                }
+            }
+            else
+            {
+                MyUtility.Msg.WarningBox("Sql connection fail!!\r\n" + result.ToString());
+                gridData = null;
+            }
+            listControlBindingSource1.DataSource = gridData;
+        }
+
+        //Close
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //To Excel
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (DataRow dr in ((DataTable)listControlBindingSource1.DataSource).Rows)
+            {
+                if (MyUtility.Convert.GetString(dr["selected"]) == "1")
+                {
+                    DualResult result = PublicPrg.Prgs.QueryPackingListReportData(MyUtility.Convert.GetString(dr["ID"]), radioButton1.Checked?"1":"2", out printData, out ctnDim, out qtyBDown);
+                    if (!result)
+                    {
+                        MyUtility.Msg.WarningBox("Query Data Fail --\r\n" + result.ToString());
+                        return;
+                    }
+                    PublicPrg.Prgs.PackingListToExcel_PackingListReport("Packing_P03_PackingListReport.xltx", dr, radioButton1.Checked ? "1" : "2", printData, ctnDim, qtyBDown);
+                }
+            }
+            MyUtility.Msg.InfoBox("Complete.");
+        }
+    }
+}
