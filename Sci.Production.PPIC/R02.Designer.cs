1eb5d0c887ed8284bd6388070ce47c244b1b5111
YINMEI <yinmei@sportscity.com.tw>
2016-02-17 08:52:34 +0000

    Add R02

---
+++ Sci.Production.PPIC/R02.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace Sci.Production.PPIC
+{
+    partial class R02
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.label3 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.label5 = new Sci.Win.UI.Label();
+            this.label6 = new Sci.Win.UI.Label();
+            this.label7 = new Sci.Win.UI.Label();
+            this.label8 = new Sci.Win.UI.Label();
+            this.label9 = new Sci.Win.UI.Label();
+            this.label10 = new Sci.Win.UI.Label();
+            this.label11 = new Sci.Win.UI.Label();
+            this.label12 = new Sci.Win.UI.Label();
+            this.dateRange1 = new Sci.Win.UI.DateRange();
+            this.dateRange2 = new Sci.Win.UI.DateRange();
+            this.dateRange3 = new Sci.Win.UI.DateRange();
+            this.comboBox1 = new Sci.Win.UI.ComboBox();
+            this.comboBox2 = new Sci.Win.UI.ComboBox();
+            this.txtseason1 = new Sci.Production.Class.txtseason();
+            this.txtstyle1 = new Sci.Production.Class.txtstyle();
+            this.txtbrand1 = new Sci.Production.Class.txtbrand();
+            this.txttpeuser_canedit1 = new Sci.Production.Class.txttpeuser_canedit();
+            this.txttpeuser_canedit2 = new Sci.Production.Class.txttpeuser_canedit();
+            this.txttpeuser_canedit3 = new Sci.Production.Class.txttpeuser_canedit();
+            this.txttpeuser_canedit4 = new Sci.Production.Class.txttpeuser_canedit();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(470, 12);
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(470, 48);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(470, 84);
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 23);
+            this.label1.TabIndex = 94;
+            this.label1.Text = "SCI Delivery";
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(13, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 23);
+            this.label2.TabIndex = 95;
+            this.label2.Text = "Provide Date";
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(13, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 23);
+            this.label3.TabIndex = 96;
+            this.label3.Text = "Fty MR Rcv. Date";
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(13, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 23);
+            this.label4.TabIndex = 97;
+            this.label4.Text = "Brand";
+            // 
+            // label5
+            // 
+            this.label5.Lines = 0;
+            this.label5.Location = new System.Drawing.Point(13, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 23);
+            this.label5.TabIndex = 98;
+            this.label5.Text = "Style";
+            // 
+            // label6
+            // 
+            this.label6.Lines = 0;
+            this.label6.Location = new System.Drawing.Point(13, 192);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(112, 23);
+            this.label6.TabIndex = 99;
+            this.label6.Text = "Season";
+            // 
+            // label7
+            // 
+            this.label7.Lines = 0;
+            this.label7.Location = new System.Drawing.Point(13, 228);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(112, 23);
+            this.label7.TabIndex = 100;
+            this.label7.Text = "M";
+            // 
+            // label8
+            // 
+            this.label8.Lines = 0;
+            this.label8.Location = new System.Drawing.Point(13, 264);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(112, 23);
+            this.label8.TabIndex = 101;
+            this.label8.Text = "MR";
+            // 
+            // label9
+            // 
+            this.label9.Lines = 0;
+            this.label9.Location = new System.Drawing.Point(13, 300);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(112, 23);
+            this.label9.TabIndex = 102;
+            this.label9.Text = "SMR";
+            // 
+            // label10
+            // 
+            this.label10.Lines = 0;
+            this.label10.Location = new System.Drawing.Point(13, 336);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(112, 23);
+            this.label10.TabIndex = 103;
+            this.label10.Text = "P/O Handle";
+            // 
+            // label11
+            // 
+            this.label11.Lines = 0;
+            this.label11.Location = new System.Drawing.Point(13, 372);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(112, 23);
+            this.label11.TabIndex = 104;
+            this.label11.Text = "P/O SMR";
+            // 
+            // label12
+            // 
+            this.label12.Lines = 0;
+            this.label12.Location = new System.Drawing.Point(13, 408);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(112, 23);
+            this.label12.TabIndex = 105;
+            this.label12.Text = "Print Type";
+            // 
+            // dateRange1
+            // 
+            this.dateRange1.Location = new System.Drawing.Point(129, 12);
+            this.dateRange1.Name = "dateRange1";
+            this.dateRange1.Size = new System.Drawing.Size(280, 23);
+            this.dateRange1.TabIndex = 106;
+            // 
+            // dateRange2
+            // 
+            this.dateRange2.Location = new System.Drawing.Point(129, 48);
+            this.dateRange2.Name = "dateRange2";
+            this.dateRange2.Size = new System.Drawing.Size(280, 23);
+            this.dateRange2.TabIndex = 107;
+            // 
+            // dateRange3
+            // 
+            this.dateRange3.Location = new System.Drawing.Point(129, 84);
+            this.dateRange3.Name = "dateRange3";
+            this.dateRange3.Size = new System.Drawing.Size(280, 23);
+            this.dateRange3.TabIndex = 108;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IsSupportUnselect = true;
+            this.comboBox1.Location = new System.Drawing.Point(129, 228);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(80, 24);
+            this.comboBox1.TabIndex = 112;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.Color.White;
+            this.comboBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.IsSupportUnselect = true;
+            this.comboBox2.Location = new System.Drawing.Point(129, 408);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(194, 24);
+            this.comboBox2.TabIndex = 113;
+            // 
+            // txtseason1
+            // 
+            this.txtseason1.BackColor = System.Drawing.Color.White;
+            this.txtseason1.BrandObjectName = null;
+            this.txtseason1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtseason1.Location = new System.Drawing.Point(129, 192);
+            this.txtseason1.Name = "txtseason1";
+            this.txtseason1.Size = new System.Drawing.Size(80, 23);
+            this.txtseason1.TabIndex = 111;
+            // 
+            // txtstyle1
+            // 
+            this.txtstyle1.BackColor = System.Drawing.Color.White;
+            this.txtstyle1.BrandObjectName = null;
+            this.txtstyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtstyle1.Location = new System.Drawing.Point(129, 156);
+            this.txtstyle1.Name = "txtstyle1";
+            this.txtstyle1.Size = new System.Drawing.Size(130, 23);
+            this.txtstyle1.TabIndex = 110;
+            // 
+            // txtbrand1
+            // 
+            this.txtbrand1.BackColor = System.Drawing.Color.White;
+            this.txtbrand1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtbrand1.Location = new System.Drawing.Point(129, 120);
+            this.txtbrand1.Name = "txtbrand1";
+            this.txtbrand1.Size = new System.Drawing.Size(66, 23);
+            this.txtbrand1.TabIndex = 109;
+            // 
+            // txttpeuser_canedit1
+            // 
+            this.txttpeuser_canedit1.DisplayBox1Binding = "";
+            this.txttpeuser_canedit1.Location = new System.Drawing.Point(129, 264);
+            this.txttpeuser_canedit1.Name = "txttpeuser_canedit1";
+            this.txttpeuser_canedit1.Size = new System.Drawing.Size(300, 23);
+            this.txttpeuser_canedit1.TabIndex = 114;
+            this.txttpeuser_canedit1.TextBox1Binding = "";
+            // 
+            // txttpeuser_canedit2
+            // 
+            this.txttpeuser_canedit2.DisplayBox1Binding = "";
+            this.txttpeuser_canedit2.Location = new System.Drawing.Point(129, 300);
+            this.txttpeuser_canedit2.Name = "txttpeuser_canedit2";
+            this.txttpeuser_canedit2.Size = new System.Drawing.Size(300, 23);
+            this.txttpeuser_canedit2.TabIndex = 115;
+            this.txttpeuser_canedit2.TextBox1Binding = "";
+            // 
+            // txttpeuser_canedit3
+            // 
+            this.txttpeuser_canedit3.DisplayBox1Binding = "";
+            this.txttpeuser_canedit3.Location = new System.Drawing.Point(129, 336);
+            this.txttpeuser_canedit3.Name = "txttpeuser_canedit3";
+            this.txttpeuser_canedit3.Size = new System.Drawing.Size(300, 23);
+            this.txttpeuser_canedit3.TabIndex = 116;
+            this.txttpeuser_canedit3.TextBox1Binding = "";
+            // 
+            // txttpeuser_canedit4
+            // 
+            this.txttpeuser_canedit4.DisplayBox1Binding = "";
+            this.txttpeuser_canedit4.Location = new System.Drawing.Point(129, 372);
+            this.txttpeuser_canedit4.Name = "txttpeuser_canedit4";
+            this.txttpeuser_canedit4.Size = new System.Drawing.Size(300, 23);
+            this.txttpeuser_canedit4.TabIndex = 117;
+            this.txttpeuser_canedit4.TextBox1Binding = "";
+            // 
+            // R02
+            // 
+            this.ClientSize = new System.Drawing.Size(562, 470);
+            this.Controls.Add(this.txttpeuser_canedit4);
+            this.Controls.Add(this.txttpeuser_canedit3);
+            this.Controls.Add(this.txttpeuser_canedit2);
+            this.Controls.Add(this.txttpeuser_canedit1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtseason1);
+            this.Controls.Add(this.txtstyle1);
+            this.Controls.Add(this.txtbrand1);
+            this.Controls.Add(this.dateRange3);
+            this.Controls.Add(this.dateRange2);
+            this.Controls.Add(this.dateRange1);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.EditMode = true;
+            this.IsSupportToPrint = false;
+            this.Name = "R02";
+            this.Text = "R02. Production Kits Report";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.label5, 0);
+            this.Controls.SetChildIndex(this.label6, 0);
+            this.Controls.SetChildIndex(this.label7, 0);
+            this.Controls.SetChildIndex(this.label8, 0);
+            this.Controls.SetChildIndex(this.label9, 0);
+            this.Controls.SetChildIndex(this.label10, 0);
+            this.Controls.SetChildIndex(this.label11, 0);
+            this.Controls.SetChildIndex(this.label12, 0);
+            this.Controls.SetChildIndex(this.dateRange1, 0);
+            this.Controls.SetChildIndex(this.dateRange2, 0);
+            this.Controls.SetChildIndex(this.dateRange3, 0);
+            this.Controls.SetChildIndex(this.txtbrand1, 0);
+            this.Controls.SetChildIndex(this.txtstyle1, 0);
+            this.Controls.SetChildIndex(this.txtseason1, 0);
+            this.Controls.SetChildIndex(this.comboBox1, 0);
+            this.Controls.SetChildIndex(this.comboBox2, 0);
+            this.Controls.SetChildIndex(this.txttpeuser_canedit1, 0);
+            this.Controls.SetChildIndex(this.txttpeuser_canedit2, 0);
+            this.Controls.SetChildIndex(this.txttpeuser_canedit3, 0);
+            this.Controls.SetChildIndex(this.txttpeuser_canedit4, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.Label label1;
+        private Win.UI.Label label2;
+        private Win.UI.Label label3;
+        private Win.UI.Label label4;
+        private Win.UI.Label label5;
+        private Win.UI.Label label6;
+        private Win.UI.Label label7;
+        private Win.UI.Label label8;
+        private Win.UI.Label label9;
+        private Win.UI.Label label10;
+        private Win.UI.Label label11;
+        private Win.UI.Label label12;
+        private Win.UI.DateRange dateRange1;
+        private Win.UI.DateRange dateRange2;
+        private Win.UI.DateRange dateRange3;
+        private Class.txtbrand txtbrand1;
+        private Class.txtstyle txtstyle1;
+        private Class.txtseason txtseason1;
+        private Win.UI.ComboBox comboBox1;
+        private Win.UI.ComboBox comboBox2;
+        private Class.txttpeuser_canedit txttpeuser_canedit1;
+        private Class.txttpeuser_canedit txttpeuser_canedit2;
+        private Class.txttpeuser_canedit txttpeuser_canedit3;
+        private Class.txttpeuser_canedit txttpeuser_canedit4;
+    }
+}
---
+++ Sci.Production.PPIC/R02.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict.Win;
+using Ict;
+using Sci.Data;
+
+namespace Sci.Production.PPIC
+{
+    public partial class R02 : Sci.Win.Tems.PrintForm
+    {
+        DataTable printData;
+        DateTime? sciDate1,sciDate2,prodiveDate1,prodiveDate2,rcvDate1,rcvDate2;
+        string brand, style, season, mDivision, mr, smr, pohandle, posmr;
+        int printType;
+        public R02(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            DataTable mDivision;
+            DBProxy.Current.Select(null, "select '' as ID union all select ID from MDivision", out mDivision);
+            MyUtility.Tool.SetupCombox(comboBox1, 1, mDivision);
+            comboBox1.Text = Sci.Env.User.Keyword;
+            MyUtility.Tool.SetupCombox(comboBox2, 1, 1, "ALL,MR Not Send,MR Send Not Receive,Factory Receive");
+            comboBox2.SelectedIndex = 0;
+        }
+
+        // 驗證輸入條件
+        protected override bool ValidateInput()
+        {
+            if (MyUtility.Check.Empty(dateRange1.Value1))
+            {
+                MyUtility.Msg.WarningBox("SCI Delivery can't empty!!");
+                return false;
+            }
+
+            mDivision = comboBox1.Text;
+            sciDate1 = dateRange1.Value1;
+            sciDate2 = dateRange1.Value2;
+            prodiveDate1 = dateRange2.Value1;
+            prodiveDate2 = dateRange2.Value2;
+            rcvDate1 = dateRange3.Value1;
+            rcvDate2 = dateRange3.Value2;
+            brand = txtbrand1.Text;
+            style = txtstyle1.Text;
+            season = txtseason1.Text;
+            mr = txttpeuser_canedit1.TextBox1.Text;
+            smr = txttpeuser_canedit2.TextBox1.Text;
+            pohandle = txttpeuser_canedit3.TextBox1.Text;
+            posmr = txttpeuser_canedit4.TextBox1.Text;
+            printType = comboBox2.SelectedIndex;
+
+            return base.ValidateInput();
+        }
+
+        // 非同步取資料
+        protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            StringBuilder sqlCmd = new StringBuilder();
+            sqlCmd.Append(string.Format(@"select s.BrandID,s.ID,s.SeasonID,sp.MDivisionID,sp.FactoryID,sp.DOC+'-'+isnull(r.Name,'') as Doc,
+sp.SendDate,sp.ReceiveDate,sp.SendToQA,sp.QAReceived,sp.ProvideDate,sp.OrderId,sp.SCIDelivery,
+sp.BuyerDelivery,IIF(sp.IsPF = 1,'Y','N') as PullForward,
+sp.SendName+'-'+isnull((select Name from TPEPass1 where ID = sp.SendName),'') as Handle,
+sp.MRHandle+'-'+isnull((select Name from TPEPass1 where ID = sp.MRHandle),'') as MRHandle,
+sp.SMR+'-'+isnull((select Name from TPEPass1 where ID = sp.SMR),'') as SMR,
+sp.PoHandle+'-'+isnull((select Name from TPEPass1 where ID = sp.PoHandle),'') as POHandle,
+sp.POSMR+'-'+isnull((select Name from TPEPass1 where ID = sp.POSMR),'') as POSMR,
+sp.FtyHandle+'-'+isnull((select Name from Pass1 where ID = sp.FtyHandle),'') as FtyHandle
+from Style_ProductionKits sp
+inner join Style s on s.Ukey = sp.StyleUkey
+left join Reason r on r.ReasonTypeID = 'ProductionKits' and r.ID = sp.DOC
+where sp.SCIDelivery between '{0}' and '{1}'", Convert.ToDateTime(sciDate1).ToString("d"), Convert.ToDateTime(sciDate2).ToString("d")));
+
+            if (!MyUtility.Check.Empty(prodiveDate1))
+            {
+                sqlCmd.Append(string.Format(" and sp.ProvideDate between '{0}' and '{1}'", Convert.ToDateTime(prodiveDate1).ToString("d"), Convert.ToDateTime(prodiveDate2).ToString("d")));
+            }
+
+            if (!MyUtility.Check.Empty(rcvDate1))
+            {
+                sqlCmd.Append(string.Format(" and sp.ReceiveDate between '{0}' and '{1}'", Convert.ToDateTime(rcvDate1).ToString("d"), Convert.ToDateTime(rcvDate2).ToString("d")));
+            }
+
+            if (!MyUtility.Check.Empty(brand))
+            {
+                sqlCmd.Append(string.Format(" and s.BrandID = '{0}'", brand));
+            }
+
+            if (!MyUtility.Check.Empty(style))
+            {
+                sqlCmd.Append(string.Format(" and s.ID = '{0}'", style));
+            }
+
+            if (!MyUtility.Check.Empty(season))
+            {
+                sqlCmd.Append(string.Format(" and s.SeasonID = '{0}'", season));
+            }
+
+            if (!MyUtility.Check.Empty(mDivision))
+            {
+                sqlCmd.Append(string.Format(" and sp.MDivisionID = '{0}'", mDivision));
+            }
+
+            if (!MyUtility.Check.Empty(mr))
+            {
+                sqlCmd.Append(string.Format(" and sp.MRHandle = '{0}'", mr));
+            }
+
+            if (!MyUtility.Check.Empty(smr))
+            {
+                sqlCmd.Append(string.Format(" and sp.SMR = '{0}'", smr));
+            }
+
+            if (!MyUtility.Check.Empty(pohandle))
+            {
+                sqlCmd.Append(string.Format(" and sp.PoHandle = '{0}'", pohandle));
+            }
+
+            if (!MyUtility.Check.Empty(posmr))
+            {
+                sqlCmd.Append(string.Format(" and sp.POSMR = '{0}'", posmr));
+            }
+
+            if (printType == 1)
+            {
+                sqlCmd.Append(" and sp.SendDate is null");
+            }
+            else if (printType == 2)
+            {
+                sqlCmd.Append(" and sp.SendDate is not null");
+            }
+            else if (printType == 3)
+            {
+                sqlCmd.Append(" and sp.ReceiveDate is not null");
+            }
+
+            DualResult result = DBProxy.Current.Select(null, sqlCmd.ToString(), out printData);
+            if (!result)
+            {
+                DualResult failResult = new DualResult(false, "Query data fail\r\n" + result.ToString());
+                return failResult;
+            }
+            return Result.True;
+        }
+
+        // 產生Excel
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            // 顯示筆數於PrintForm上Count欄位
+            SetCount(printData.Rows.Count);
+
+            if (printData.Rows.Count <= 0)
+            {
+                MyUtility.Msg.WarningBox("Data not found!");
+                return false;
+            }
+
+            string MyDocumentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            System.IO.DirectoryInfo dir = new System.IO.DirectoryInfo(Application.StartupPath);
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.RestoreDirectory = true;
+            dlg.InitialDirectory = MyDocumentsPath;     //指定"我的文件"路徑
+            dlg.Title = "Save as Excel File";
+            dlg.Filter = "Excel Files (*.xls)|*.xls";            // Set filter for file extension and default file extension
+
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK && dlg.FileName != null)
+            {
+                // Open document
+                bool result = MyUtility.Excel.CopyToXls(printData, dlg.FileName, xltfile: "PPIC_R02_ProductionKits.xltx", headerRow: 1);
+                if (!result) { MyUtility.Msg.WarningBox(result.ToString(), "Warning"); return false; }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            //因為速度較慢，故不使用
+            //MyUtility.Msg.WaitWindows("Starting EXCEL...");
+            //string strXltName = Sci.Env.Cfg.XltPathDir + "PPIC_R02_ProductionKits.xltx";
+            //Microsoft.Office.Interop.Excel.Application excel = MyUtility.Excel.ConnectExcel(strXltName);
+            //if (excel == null) return false;
+            //Microsoft.Office.Interop.Excel.Worksheet worksheet = excel.ActiveWorkbook.Worksheets[1];
+
+            ////填內容值
+            //int intRowsStart = 2;
+            //object[,] objArray = new object[1, 21];
+            //foreach (DataRow dr in printData.Rows)
+            //{
+            //    objArray[0, 0] = dr["BrandID"];
+            //    objArray[0, 1] = dr["ID"];
+            //    objArray[0, 2] = dr["SeasonID"];
+            //    objArray[0, 3] = dr["MDivisionID"];
+            //    objArray[0, 4] = dr["FactoryID"];
+            //    objArray[0, 5] = dr["Doc"];
+            //    objArray[0, 6] = dr["SendDate"];
+            //    objArray[0, 7] = dr["ReceiveDate"];
+            //    objArray[0, 8] = dr["SendToQA"];
+            //    objArray[0, 9] = dr["QAReceived"];
+            //    objArray[0, 10] = dr["ProvideDate"];
+            //    objArray[0, 11] = dr["OrderId"];
+            //    objArray[0, 12] = dr["SCIDelivery"];
+            //    objArray[0, 13] = dr["BuyerDelivery"];
+            //    objArray[0, 14] = dr["PullForward"];
+            //    objArray[0, 15] = dr["Handle"];
+            //    objArray[0, 16] = dr["MRHandle"];
+            //    objArray[0, 17] = dr["SMR"];
+            //    objArray[0, 18] = dr["POHandle"];
+            //    objArray[0, 19] = dr["POSMR"];
+            //    objArray[0, 20] = dr["FtyHandle"];
+            //    worksheet.Range[String.Format("A{0}:U{0}", intRowsStart)].Value2 = objArray;
+            //    intRowsStart++;
+            //}
+
+            //excel.Cells.EntireColumn.AutoFit();
+            //excel.Cells.EntireRow.AutoFit();
+            //MyUtility.Msg.WaitClear();
+            //excel.Visible = true;
+            //return true;
+        }
+    }
+}
