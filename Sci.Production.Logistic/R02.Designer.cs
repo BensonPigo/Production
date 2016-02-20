d2aed7e12f82569bbc8d5b059a77f6e4742703f8
YINMEI <yinmei@sportscity.com.tw>
2016-02-20 06:48:55 +0000

    Add R02

---
+++ Sci.Production.Logistic/R02.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace Sci.Production.Logistic
+{
+    partial class R02
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.label3 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.label5 = new Sci.Win.UI.Label();
+            this.textBox1 = new Sci.Win.UI.TextBox();
+            this.textBox2 = new Sci.Win.UI.TextBox();
+            this.textBox3 = new Sci.Win.UI.TextBox();
+            this.textBox4 = new Sci.Win.UI.TextBox();
+            this.txtbrand1 = new Sci.Production.Class.txtbrand();
+            this.comboBox1 = new Sci.Win.UI.ComboBox();
+            this.txtcloglocation1 = new Sci.Production.Class.txtcloglocation();
+            this.txtcloglocation2 = new Sci.Production.Class.txtcloglocation();
+            this.label6 = new Sci.Win.UI.Label();
+            this.label7 = new Sci.Win.UI.Label();
+            this.label8 = new Sci.Win.UI.Label();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(424, 12);
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(424, 48);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(424, 84);
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 23);
+            this.label1.TabIndex = 94;
+            this.label1.Text = "PO#";
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(13, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 23);
+            this.label2.TabIndex = 95;
+            this.label2.Text = "SP#";
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(13, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 23);
+            this.label3.TabIndex = 96;
+            this.label3.Text = "Brand";
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(13, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 23);
+            this.label4.TabIndex = 97;
+            this.label4.Text = "M";
+            // 
+            // label5
+            // 
+            this.label5.Lines = 0;
+            this.label5.Location = new System.Drawing.Point(13, 156);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 23);
+            this.label5.TabIndex = 98;
+            this.label5.Text = "Location";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(92, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(130, 23);
+            this.textBox1.TabIndex = 99;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox2.Location = new System.Drawing.Point(250, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(130, 23);
+            this.textBox2.TabIndex = 100;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.White;
+            this.textBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox3.Location = new System.Drawing.Point(92, 48);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(130, 23);
+            this.textBox3.TabIndex = 101;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.White;
+            this.textBox4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox4.Location = new System.Drawing.Point(250, 48);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(130, 23);
+            this.textBox4.TabIndex = 102;
+            // 
+            // txtbrand1
+            // 
+            this.txtbrand1.BackColor = System.Drawing.Color.White;
+            this.txtbrand1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtbrand1.Location = new System.Drawing.Point(92, 84);
+            this.txtbrand1.Name = "txtbrand1";
+            this.txtbrand1.Size = new System.Drawing.Size(100, 23);
+            this.txtbrand1.TabIndex = 103;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IsSupportUnselect = true;
+            this.comboBox1.Location = new System.Drawing.Point(92, 120);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(80, 24);
+            this.comboBox1.TabIndex = 104;
+            // 
+            // txtcloglocation1
+            // 
+            this.txtcloglocation1.BackColor = System.Drawing.Color.White;
+            this.txtcloglocation1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtcloglocation1.IsSupportSytsemContextMenu = false;
+            this.txtcloglocation1.Location = new System.Drawing.Point(92, 156);
+            this.txtcloglocation1.MDivisionObjectName = this.comboBox1;
+            this.txtcloglocation1.Name = "txtcloglocation1";
+            this.txtcloglocation1.Size = new System.Drawing.Size(80, 23);
+            this.txtcloglocation1.TabIndex = 105;
+            // 
+            // txtcloglocation2
+            // 
+            this.txtcloglocation2.BackColor = System.Drawing.Color.White;
+            this.txtcloglocation2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtcloglocation2.IsSupportSytsemContextMenu = false;
+            this.txtcloglocation2.Location = new System.Drawing.Point(196, 156);
+            this.txtcloglocation2.MDivisionObjectName = this.comboBox1;
+            this.txtcloglocation2.Name = "txtcloglocation2";
+            this.txtcloglocation2.Size = new System.Drawing.Size(80, 23);
+            this.txtcloglocation2.TabIndex = 106;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Lines = 0;
+            this.label6.Location = new System.Drawing.Point(226, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 23);
+            this.label6.TabIndex = 107;
+            this.label6.Text = "～";
+            this.label6.TextStyle.BorderColor = System.Drawing.Color.Black;
+            this.label6.TextStyle.Color = System.Drawing.Color.Black;
+            this.label6.TextStyle.ExtBorderColor = System.Drawing.Color.Black;
+            this.label6.TextStyle.GradientColor = System.Drawing.Color.Black;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Lines = 0;
+            this.label7.Location = new System.Drawing.Point(226, 48);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(19, 23);
+            this.label7.TabIndex = 108;
+            this.label7.Text = "～";
+            this.label7.TextStyle.BorderColor = System.Drawing.Color.Black;
+            this.label7.TextStyle.Color = System.Drawing.Color.Black;
+            this.label7.TextStyle.ExtBorderColor = System.Drawing.Color.Black;
+            this.label7.TextStyle.GradientColor = System.Drawing.Color.Black;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Lines = 0;
+            this.label8.Location = new System.Drawing.Point(173, 156);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(19, 23);
+            this.label8.TabIndex = 109;
+            this.label8.Text = "～";
+            this.label8.TextStyle.BorderColor = System.Drawing.Color.Black;
+            this.label8.TextStyle.Color = System.Drawing.Color.Black;
+            this.label8.TextStyle.ExtBorderColor = System.Drawing.Color.Black;
+            this.label8.TextStyle.GradientColor = System.Drawing.Color.Black;
+            // 
+            // R02
+            // 
+            this.ClientSize = new System.Drawing.Size(516, 218);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtcloglocation2);
+            this.Controls.Add(this.txtcloglocation1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtbrand1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.EditMode = true;
+            this.Name = "R02";
+            this.Text = "R02. Clog Audit List";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.label5, 0);
+            this.Controls.SetChildIndex(this.textBox1, 0);
+            this.Controls.SetChildIndex(this.textBox2, 0);
+            this.Controls.SetChildIndex(this.textBox3, 0);
+            this.Controls.SetChildIndex(this.textBox4, 0);
+            this.Controls.SetChildIndex(this.txtbrand1, 0);
+            this.Controls.SetChildIndex(this.comboBox1, 0);
+            this.Controls.SetChildIndex(this.txtcloglocation1, 0);
+            this.Controls.SetChildIndex(this.txtcloglocation2, 0);
+            this.Controls.SetChildIndex(this.label6, 0);
+            this.Controls.SetChildIndex(this.label7, 0);
+            this.Controls.SetChildIndex(this.label8, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.Label label1;
+        private Win.UI.Label label2;
+        private Win.UI.Label label3;
+        private Win.UI.Label label4;
+        private Win.UI.Label label5;
+        private Win.UI.TextBox textBox1;
+        private Win.UI.TextBox textBox2;
+        private Win.UI.TextBox textBox3;
+        private Win.UI.TextBox textBox4;
+        private Class.txtbrand txtbrand1;
+        private Win.UI.ComboBox comboBox1;
+        private Class.txtcloglocation txtcloglocation1;
+        private Class.txtcloglocation txtcloglocation2;
+        private Win.UI.Label label6;
+        private Win.UI.Label label7;
+        private Win.UI.Label label8;
+    }
+}
---
+++ Sci.Production.Logistic/R02.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict.Win;
+using Ict;
+using Sci.Data;
+
+namespace Sci.Production.Logistic
+{
+    public partial class R02 : Sci.Win.Tems.PrintForm
+    {
+        string po1, po2, sp1, sp2, brand, mDivision, location1, location2;
+        DataTable printData;
+        public R02(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            DataTable mDivision;
+            DBProxy.Current.Select(null, "select '' as ID union all select ID from MDivision", out mDivision);
+            MyUtility.Tool.SetupCombox(comboBox1, 1, mDivision);
+            comboBox1.Text = Sci.Env.User.Keyword;
+        }
+
+        // 驗證輸入條件
+        protected override bool ValidateInput()
+        {
+            po1 = textBox1.Text;
+            po2 = textBox2.Text;
+            sp1 = textBox3.Text;
+            sp2 = textBox4.Text;
+            brand = txtbrand1.Text;
+            mDivision = comboBox1.Text;
+            location1 = txtcloglocation1.Text;
+            location2 = txtcloglocation2.Text;
+
+            return base.ValidateInput();
+        }
+
+        // 非同步取資料
+        protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            StringBuilder sqlCmd = new StringBuilder();
+            sqlCmd.Append(@"select p.MDivisionID,o.FactoryID,pd.OrderID,pd.CTNStartNo,pd.ReceiveDate,o.CustPONo,pd.ClogLocationId,p.BrandID
+from PackingList p
+inner join PackingList_Detail pd on p.ID = pd.ID
+inner join Orders o on o.ID = pd.OrderID
+left join Pullout po on p.PulloutID = po.ID
+where pd.CTNQty > 0
+and pd.ReceiveDate is not null
+and (p.PulloutID = '' or po.Status = 'New')");
+
+            if (!MyUtility.Check.Empty(po1))
+            {
+                sqlCmd.Append(string.Format(" and o.CustPONo >= '{0}'", po1));
+            }
+
+            if (!MyUtility.Check.Empty(po2))
+            {
+                sqlCmd.Append(string.Format(" and o.CustPONo <= '{0}'", po2));
+            }
+
+            if (!MyUtility.Check.Empty(sp1))
+            {
+                sqlCmd.Append(string.Format(" and pd.OrderID >= '{0}'", sp1));
+            }
+
+            if (!MyUtility.Check.Empty(sp2))
+            {
+                sqlCmd.Append(string.Format(" and pd.OrderID <= '{0}'", sp2));
+            }
+
+            if (!MyUtility.Check.Empty(brand))
+            {
+                sqlCmd.Append(string.Format(" and p.BrandID = '{0}'", brand));
+            }
+
+            if (!MyUtility.Check.Empty(mDivision))
+            {
+                sqlCmd.Append(string.Format(" and p.MDivisionID = '{0}'", mDivision));
+            }
+
+            if (!MyUtility.Check.Empty(location1))
+            {
+                sqlCmd.Append(string.Format(" and pd.ClogLocationId >= '{0}'", location1));
+            }
+
+            if (!MyUtility.Check.Empty(location2))
+            {
+                sqlCmd.Append(string.Format(" and pd.ClogLocationId <= '{0}'", location2));
+            }
+
+            sqlCmd.Append(" order by pd.ClogLocationId,p.MDivisionID,o.FactoryID,pd.OrderID,pd.ID,pd.Seq");
+
+            DualResult result = DBProxy.Current.Select(null, sqlCmd.ToString(), out printData);
+            if (!result)
+            {
+                DualResult failResult = new DualResult(false, "Query data fail\r\n" + result.ToString());
+                return failResult;
+            }
+            return Result.True;
+        }
+
+        // 產生Excel
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            // 顯示筆數於PrintForm上Count欄位
+            SetCount(printData.Rows.Count);
+
+            if (printData.Rows.Count <= 0)
+            {
+                MyUtility.Msg.WarningBox("Data not found!");
+                return false;
+            }
+
+            MyUtility.Msg.WaitWindows("Starting EXCEL...");
+            string strXltName = Sci.Env.Cfg.XltPathDir + "Logistic_R02_ClogAuditList.xltx";
+            Microsoft.Office.Interop.Excel.Application excel = MyUtility.Excel.ConnectExcel(strXltName);
+            if (excel == null) return false;
+            Microsoft.Office.Interop.Excel.Worksheet worksheet = excel.ActiveWorkbook.Worksheets[1];
+
+            worksheet.Cells[2, 2] = po1 + " ~ " + po2;
+            worksheet.Cells[3, 2] = sp1 + " ~ " + sp2;
+            worksheet.Cells[4, 2] = location1 + " ~ " + location2;
+            worksheet.Cells[2, 8] = brand;
+            worksheet.Cells[3, 8] = mDivision;
+
+            //填內容值
+            int intRowsStart = 6;
+            object[,] objArray = new object[1, 8];
+            foreach (DataRow dr in printData.Rows)
+            {
+                objArray[0, 0] = dr["MDivisionID"];
+                objArray[0, 1] = dr["FactoryID"];
+                objArray[0, 2] = dr["OrderID"];
+                objArray[0, 3] = dr["CTNStartNo"];
+                objArray[0, 4] = dr["ReceiveDate"];
+                objArray[0, 5] = dr["CustPONo"];
+                objArray[0, 6] = dr["ClogLocationId"];
+                objArray[0, 7] = dr["BrandID"];
+                worksheet.Range[String.Format("A{0}:H{0}", intRowsStart)].Value2 = objArray;
+                intRowsStart++;
+            }
+
+            //excel.Cells.EntireColumn.AutoFit();
+            //excel.Cells.EntireRow.AutoFit();
+            MyUtility.Msg.WaitClear();
+            excel.Visible = true;
+            return true;
+        }
+    }
+}
