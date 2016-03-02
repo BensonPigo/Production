09bac4f073f3e4f938e5a57d09e1b356d0dd16f5
YINMEI <yinmei@sportscity.com.tw>
2016-03-02 11:46:59 +0000

    Add R11

---
+++ Sci.Production.Shipping/R11.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace Sci.Production.Shipping
+{
+    partial class R11
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.label3 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.label5 = new Sci.Win.UI.Label();
+            this.label6 = new Sci.Win.UI.Label();
+            this.label7 = new Sci.Win.UI.Label();
+            this.radioPanel1 = new Sci.Win.UI.RadioPanel();
+            this.radioButton2 = new Sci.Win.UI.RadioButton();
+            this.radioButton1 = new Sci.Win.UI.RadioButton();
+            this.dateRange1 = new Sci.Win.UI.DateRange();
+            this.txtbrand1 = new Sci.Production.Class.txtbrand();
+            this.txtcustcd1 = new Sci.Production.Class.txtcustcd();
+            this.txtcountry1 = new Sci.Production.Class.txtcountry();
+            this.txtshipmode1 = new Sci.Production.Class.txtshipmode();
+            this.displayBox1 = new Sci.Win.UI.DisplayBox();
+            this.textBox1 = new Sci.Win.UI.TextBox();
+            this.radioPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(430, 12);
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(430, 48);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(430, 84);
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 23);
+            this.label1.TabIndex = 94;
+            this.label1.Text = "Report Content";
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(13, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 23);
+            this.label2.TabIndex = 95;
+            this.label2.Text = "Pullout Date";
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(13, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 23);
+            this.label3.TabIndex = 96;
+            this.label3.Text = "Brand";
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(13, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 23);
+            this.label4.TabIndex = 97;
+            this.label4.Text = "Cust CD";
+            // 
+            // label5
+            // 
+            this.label5.Lines = 0;
+            this.label5.Location = new System.Drawing.Point(13, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 23);
+            this.label5.TabIndex = 98;
+            this.label5.Text = "Destination";
+            // 
+            // label6
+            // 
+            this.label6.Lines = 0;
+            this.label6.Location = new System.Drawing.Point(13, 216);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 23);
+            this.label6.TabIndex = 99;
+            this.label6.Text = "Ship Mode";
+            // 
+            // label7
+            // 
+            this.label7.Lines = 0;
+            this.label7.Location = new System.Drawing.Point(13, 253);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(101, 23);
+            this.label7.TabIndex = 100;
+            this.label7.Text = "Forwarder";
+            // 
+            // radioPanel1
+            // 
+            this.radioPanel1.Controls.Add(this.radioButton2);
+            this.radioPanel1.Controls.Add(this.radioButton1);
+            this.radioPanel1.Location = new System.Drawing.Point(117, 10);
+            this.radioPanel1.Name = "radioPanel1";
+            this.radioPanel1.Size = new System.Drawing.Size(112, 52);
+            this.radioPanel1.TabIndex = 101;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.radioButton2.Location = new System.Drawing.Point(3, 29);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(107, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Raw Material";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.radioButton1.Location = new System.Drawing.Point(3, 2);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(81, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Garment";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // dateRange1
+            // 
+            this.dateRange1.IsRequired = false;
+            this.dateRange1.Location = new System.Drawing.Point(120, 71);
+            this.dateRange1.Name = "dateRange1";
+            this.dateRange1.Size = new System.Drawing.Size(280, 23);
+            this.dateRange1.TabIndex = 102;
+            // 
+            // txtbrand1
+            // 
+            this.txtbrand1.BackColor = System.Drawing.Color.White;
+            this.txtbrand1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtbrand1.Location = new System.Drawing.Point(120, 107);
+            this.txtbrand1.Name = "txtbrand1";
+            this.txtbrand1.Size = new System.Drawing.Size(66, 23);
+            this.txtbrand1.TabIndex = 103;
+            // 
+            // txtcustcd1
+            // 
+            this.txtcustcd1.BackColor = System.Drawing.Color.White;
+            this.txtcustcd1.BrandObjectName = null;
+            this.txtcustcd1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtcustcd1.Location = new System.Drawing.Point(120, 143);
+            this.txtcustcd1.Name = "txtcustcd1";
+            this.txtcustcd1.Size = new System.Drawing.Size(125, 23);
+            this.txtcustcd1.TabIndex = 104;
+            // 
+            // txtcountry1
+            // 
+            this.txtcountry1.DisplayBox1Binding = "";
+            this.txtcountry1.Location = new System.Drawing.Point(120, 180);
+            this.txtcountry1.Name = "txtcountry1";
+            this.txtcountry1.Size = new System.Drawing.Size(232, 22);
+            this.txtcountry1.TabIndex = 105;
+            this.txtcountry1.TextBox1Binding = "";
+            // 
+            // txtshipmode1
+            // 
+            this.txtshipmode1.BackColor = System.Drawing.Color.White;
+            this.txtshipmode1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtshipmode1.FormattingEnabled = true;
+            this.txtshipmode1.IsSupportUnselect = true;
+            this.txtshipmode1.Location = new System.Drawing.Point(120, 216);
+            this.txtshipmode1.Name = "txtshipmode1";
+            this.txtshipmode1.Size = new System.Drawing.Size(80, 24);
+            this.txtshipmode1.TabIndex = 106;
+            this.txtshipmode1.UseFunction = null;
+            // 
+            // displayBox1
+            // 
+            this.displayBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.displayBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.displayBox1.Location = new System.Drawing.Point(183, 254);
+            this.displayBox1.Name = "displayBox1";
+            this.displayBox1.Size = new System.Drawing.Size(100, 23);
+            this.displayBox1.TabIndex = 110;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(120, 253);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(61, 23);
+            this.textBox1.TabIndex = 109;
+            this.textBox1.PopUp += new System.EventHandler<Sci.Win.UI.TextBoxPopUpEventArgs>(this.textBox1_PopUp);
+            this.textBox1.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating);
+            // 
+            // R11
+            // 
+            this.ClientSize = new System.Drawing.Size(522, 314);
+            this.Controls.Add(this.displayBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtshipmode1);
+            this.Controls.Add(this.txtcountry1);
+            this.Controls.Add(this.txtcustcd1);
+            this.Controls.Add(this.txtbrand1);
+            this.Controls.Add(this.dateRange1);
+            this.Controls.Add(this.radioPanel1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.EditMode = true;
+            this.IsSupportToPrint = false;
+            this.Name = "R11";
+            this.Text = "R11. Non Shared List";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.label5, 0);
+            this.Controls.SetChildIndex(this.label6, 0);
+            this.Controls.SetChildIndex(this.label7, 0);
+            this.Controls.SetChildIndex(this.radioPanel1, 0);
+            this.Controls.SetChildIndex(this.dateRange1, 0);
+            this.Controls.SetChildIndex(this.txtbrand1, 0);
+            this.Controls.SetChildIndex(this.txtcustcd1, 0);
+            this.Controls.SetChildIndex(this.txtcountry1, 0);
+            this.Controls.SetChildIndex(this.txtshipmode1, 0);
+            this.Controls.SetChildIndex(this.textBox1, 0);
+            this.Controls.SetChildIndex(this.displayBox1, 0);
+            this.radioPanel1.ResumeLayout(false);
+            this.radioPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.Label label1;
+        private Win.UI.Label label2;
+        private Win.UI.Label label3;
+        private Win.UI.Label label4;
+        private Win.UI.Label label5;
+        private Win.UI.Label label6;
+        private Win.UI.Label label7;
+        private Win.UI.RadioPanel radioPanel1;
+        private Win.UI.RadioButton radioButton2;
+        private Win.UI.RadioButton radioButton1;
+        private Win.UI.DateRange dateRange1;
+        private Class.txtbrand txtbrand1;
+        private Class.txtcustcd txtcustcd1;
+        private Class.txtcountry txtcountry1;
+        private Class.txtshipmode txtshipmode1;
+        private Win.UI.DisplayBox displayBox1;
+        private Win.UI.TextBox textBox1;
+    }
+}
---
+++ Sci.Production.Shipping/R11.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict.Win;
+using Ict;
+using Sci.Data;
+
+namespace Sci.Production.Shipping
+{
+    public partial class R11 : Sci.Win.Tems.PrintForm
+    {
+        DataTable printData;
+        int reportType;
+        DateTime? date1, date2;
+        string brand, custCD, dest, shipMode, forwarder;
+        public R11(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            radioButton1.Checked = true;
+        }
+
+        protected override void OnFormLoaded()
+        {
+            base.OnFormLoaded();
+            txtshipmode1.SelectedIndex = -1;
+        }
+
+        //Forwarder
+        private void textBox1_PopUp(object sender, Win.UI.TextBoxPopUpEventArgs e)
+        {
+            string selectCommand;
+            selectCommand = @"select ID,Abb from LocalSupp
+union all
+select ID,AbbEN from Supp
+order by ID";
+
+            DataTable tbSelect;
+            DBProxy.Current.Select(null, selectCommand, out tbSelect);
+            Sci.Win.Tools.SelectItem item = new Sci.Win.Tools.SelectItem(tbSelect, "ID,Abb", "9,13", this.Text, false, ",", "ID,Abb");
+            DialogResult returnResult = item.ShowDialog();
+            if (returnResult == DialogResult.Cancel) { return; }
+            IList<DataRow> selected = item.GetSelecteds();
+            this.textBox1.Text = item.GetSelectedString();
+            displayBox1.Value = MyUtility.Convert.GetString(selected[0]["Abb"]);
+        }
+
+        //Forwarder
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+            if (textBox1.OldValue != textBox1.Text)
+            {
+                if (!MyUtility.Check.Empty(textBox1.Text))
+                {
+                    DataRow inputData;
+                    string Sql = string.Format(@"select * from (
+select ID,Abb from LocalSupp
+union all
+select ID,AbbEN from Supp) a
+where a.ID = '{0}'", textBox1.Text);
+                    if (!MyUtility.Check.Seek(Sql, out inputData))
+                    {
+                        MyUtility.Msg.WarningBox(string.Format("< Forwarder: {0} > not found!!!", textBox1.Text));
+                        textBox1.Text = "";
+                        displayBox1.Value = "";
+                        e.Cancel = true;
+                        return;
+                    }
+                    else
+                    {
+                        textBox1.Text = textBox1.Text;
+                        displayBox1.Value = MyUtility.Convert.GetString(inputData["Abb"]);
+                    }
+                }
+                else
+                {
+                    textBox1.Text = "";
+                    displayBox1.Value = "";
+                }
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                label2.Text = "Pullout Date";
+                label2.Size = new System.Drawing.Size(101, 23);
+                label2.Location = new System.Drawing.Point(13, 71);
+                txtbrand1.Enabled = true;
+                txtcustcd1.Enabled = true;
+            }
+            else
+            {
+                label2.Text = "Arrive Port Date \r\n (Ship Date)";
+                label2.Size = new System.Drawing.Size(101, 36);
+                label2.Location = new System.Drawing.Point(13, 64);
+                txtbrand1.Enabled = false;
+                txtcustcd1.Enabled = false;
+            }
+        }
+
+        // 驗證輸入條件
+        protected override bool ValidateInput()
+        {
+            date1 = dateRange1.Value1;
+            date2 = dateRange1.Value2;
+            brand = txtbrand1.Text;
+            custCD = txtcustcd1.Text;
+            dest = txtcountry1.TextBox1.Text;
+            shipMode = txtshipmode1.Text;
+            forwarder = textBox1.Text;
+            reportType = radioButton1.Checked ? 1 : 2;
+            return base.ValidateInput();
+        }
+
+        // 非同步取資料
+        protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            StringBuilder sqlCmd = new StringBuilder();
+            DualResult result;
+            if (reportType == 1)
+            {
+                #region 組SQL Command
+                sqlCmd.Append(@"with GBData
+as (
+select distinct 'Export' as IE, 'GARMENT' as Type,g.ID,g.Shipper,g.BrandID,
+IIF(p.Type = 'B','Bulk',IIF(p.Type = 'S','Sample','')) as Category,
+isnull((select sum(a.Qty) from (
+select distinct oq.Id,oq.Seq,oq.Qty from PackingList_Detail pd, Order_QtyShip oq
+where pd.ID = p.ID and pd.OrderID = oq.Id and pd.OrderShipmodeSeq = oq.Seq) a),0) as OrderQty,
+g.CustCDID,g.Dest,g.ShipModeID,
+(select MAX(PulloutDate) from PackingList where INVNo = g.ID) as PulloutDate,g.TotalShipQty,
+g.TotalCTNQty,g.TotalGW,g.TotalCBM,g.Forwarder+'-'+isnull(l.Abb,'') as Forwarder,'' as BLNo
+from GMTBooking g
+inner join PackingList p on p.INVNo = g.ID
+left join LocalSupp l on l.ID = g.Forwarder
+where not exists (select 1 from ShareExpense where InvNo = g.ID)");
+                if (!MyUtility.Check.Empty(date1))
+                {
+                    sqlCmd.Append(string.Format(" and p.PulloutDate >= '{0}'", Convert.ToDateTime(date1).ToString("d")));
+                }
+                if (!MyUtility.Check.Empty(date2))
+                {
+                    sqlCmd.Append(string.Format(" and p.PulloutDate <= '{0}'", Convert.ToDateTime(date2).ToString("d")));
+                }
+                if (!MyUtility.Check.Empty(brand))
+                {
+                    sqlCmd.Append(string.Format(" and g.BrandID = '{0}'", brand));
+                }
+                if (!MyUtility.Check.Empty(custCD))
+                {
+                    sqlCmd.Append(string.Format(" and g.CustCDID = '{0}'", custCD));
+                }
+                if (!MyUtility.Check.Empty(dest))
+                {
+                    sqlCmd.Append(string.Format(" and g.Dest = '{0}'", dest));
+                }
+                if (!MyUtility.Check.Empty(shipMode))
+                {
+                    sqlCmd.Append(string.Format(" and g.ShipModeID = '{0}'", shipMode));
+                }
+                if (!MyUtility.Check.Empty(forwarder))
+                {
+                    sqlCmd.Append(string.Format(" and g.Forwarder = '{0}'", forwarder));
+                }
+                sqlCmd.Append(@"),
+PLData
+as (
+select  'Export' as IE, 'GARMENT' as Type,p.ID,p.MDivisionID,p.BrandID,
+IIF((select top 1 o.Category from Orders o, PackingList_Detail pd 
+where pd.ID = p.ID and o.ID = pd.OrderID)='B','Bulk','Sample') as Category,
+isnull((select sum(a.Qty) from (
+select distinct oq.Id,oq.Seq,oq.Qty from PackingList_Detail pd, Order_QtyShip oq
+where pd.ID = p.ID and pd.OrderID = oq.Id and pd.OrderShipmodeSeq = oq.Seq) a),0) as OrderQty,
+p.CustCDID,'' as Dest,p.ShipModeID,p.PulloutDate,p.ShipQty,p.CTNQty,p.GW,p.CBM,'' as Forwarder,
+'' as BLNo
+from PackingList p
+where (p.Type = 'F' or p.Type = 'L')
+and not exists (select 1 from ShareExpense where InvNo = p.ID)");
+                if (!MyUtility.Check.Empty(date1))
+                {
+                    sqlCmd.Append(string.Format(" and p.PulloutDate >= '{0}'", Convert.ToDateTime(date1).ToString("d")));
+                }
+                if (!MyUtility.Check.Empty(date2))
+                {
+                    sqlCmd.Append(string.Format(" and p.PulloutDate <= '{0}'", Convert.ToDateTime(date2).ToString("d")));
+                }
+                if (!MyUtility.Check.Empty(brand))
+                {
+                    sqlCmd.Append(string.Format(" and p.BrandID = '{0}'", brand));
+                }
+                if (!MyUtility.Check.Empty(custCD))
+                {
+                    sqlCmd.Append(string.Format(" and p.CustCDID = '{0}'", custCD));
+                }
+                if (!MyUtility.Check.Empty(shipMode))
+                {
+                    sqlCmd.Append(string.Format(" and p.ShipModeID = '{0}'", shipMode));
+                }
+                sqlCmd.Append(@")
+
+select * from GBData
+union all
+select * from PLData");
+                #endregion
+            }
+            else
+            {
+                #region 組SQL Command
+                sqlCmd.Append(@"with ExportData
+as (
+select 'Import' as IE,'MATERIAL' as Type,e.ID,e.ImportCountry,e.ShipModeID,e.PortArrival,e.WeightKg,
+e.Cbm,e.Forwarder+'-'+isnull(s.AbbEN,'') as Forwarder,e.Blno,
+(select top 1 ShippingAPID from ShareExpense where Blno = e.Blno) as APId1,
+(select top 1 ShippingAPID from ShareExpense where WKNo = e.ID) as APId2
+from Export e
+left join Supp s on s.ID = e.Forwarder
+where e.Junk = 0");
+                if (!MyUtility.Check.Empty(date1))
+                {
+                    sqlCmd.Append(string.Format(" and e.PortArrival >= '{0}'", Convert.ToDateTime(date1).ToString("d")));
+                }
+                if (!MyUtility.Check.Empty(date2))
+                {
+                    sqlCmd.Append(string.Format(" and e.PortArrival <= '{0}'", Convert.ToDateTime(date2).ToString("d")));
+                }
+                if (!MyUtility.Check.Empty(dest))
+                {
+                    sqlCmd.Append(string.Format(" and e.ImportCountry = '{0}'", dest));
+                }
+                if (!MyUtility.Check.Empty(shipMode))
+                {
+                    sqlCmd.Append(string.Format(" and e.ShipModeID = '{0}'", shipMode));
+                }
+                if (!MyUtility.Check.Empty(forwarder))
+                {
+                    sqlCmd.Append(string.Format(" and e.Forwarder = '{0}'", forwarder));
+                }
+                sqlCmd.Append(@"),
+FtyExportData
+as (
+select IIF(f.Type = 3,'Export','Import') as IE,
+IIF(f.Type = 1,'3rd Country',IIF(f.Type = 2,'Transfer In',IIF(f.Type = 3,'Transfer Out','Local Purchase'))) as Type,
+f.ID,f.ImportCountry,f.ShipModeID,f.PortArrival,f.WeightKg,f.Cbm,f.Forwarder+'-'+isnull(l.Abb,'') as Forwarder,
+f.Blno
+from FtyExport f
+left join LocalSupp l on l.ID = f.Forwarder
+where not exists (select 1 from ShareExpense where WKNo = f.ID)");
+                if (!MyUtility.Check.Empty(date1))
+                {
+                    sqlCmd.Append(string.Format(" and f.PortArrival >= '{0}'", Convert.ToDateTime(date1).ToString("d")));
+                }
+                if (!MyUtility.Check.Empty(date2))
+                {
+                    sqlCmd.Append(string.Format(" and f.PortArrival <= '{0}'", Convert.ToDateTime(date2).ToString("d")));
+                }
+                if (!MyUtility.Check.Empty(dest))
+                {
+                    sqlCmd.Append(string.Format(" and f.ImportCountry = '{0}'", dest));
+                }
+                if (!MyUtility.Check.Empty(shipMode))
+                {
+                    sqlCmd.Append(string.Format(" and f.ShipModeID = '{0}'", shipMode));
+                }
+                if (!MyUtility.Check.Empty(forwarder))
+                {
+                    sqlCmd.Append(string.Format(" and f.Forwarder = '{0}'", forwarder));
+                }
+                sqlCmd.Append(@")
+
+select IE,Type,ID,'' as Shipper,'' as BrandID,'' as Category,0 as OrderQty,'' as CustCDID,
+ImportCountry,ShipModeID,PortArrival,0 as ShipQty,0 as CTNQty,WeightKg,Cbm,Forwarder,Blno
+from ExportData
+where (Blno <> '' and APId1 is null) or (Blno = '' and APId2 is null)
+union all
+select IE,Type,ID,'' as Shipper,'' as BrandID,'' as Category,0 as OrderQty,'' as CustCDID,
+ImportCountry,ShipModeID,PortArrival,0 as ShipQty,0 as CTNQty,WeightKg,Cbm,Forwarder,Blno
+from FtyExportData");
+                #endregion
+            }
+
+            result = DBProxy.Current.Select(null, sqlCmd.ToString(), out printData);
+            if (!result)
+            {
+                DualResult failResult = new DualResult(false, "Query data fail\r\n" + result.ToString());
+                return failResult;
+            }
+
+            return Result.True;
+        }
+
+        // 產生Excel
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            // 顯示筆數於PrintForm上Count欄位
+            SetCount(printData.Rows.Count);
+
+            if (printData.Rows.Count <= 0)
+            {
+                MyUtility.Msg.WarningBox("Data not found!");
+                return false;
+            }
+
+            MyUtility.Msg.WaitWindows("Starting EXCEL...");
+            string excelFile = reportType == 1 ? "Shipping_R11_NonSharedListGarment.xltx" : "Shipping_R11_NonSharedListMaterial.xltx";
+            bool result = MyUtility.Excel.CopyToXls(printData, "", xltfile: excelFile, headerRow: 1);
+            if (!result) { MyUtility.Msg.WarningBox(result.ToString(), "Warning"); }
+            MyUtility.Msg.WaitClear();
+            return true;
+        }
+    }
+}
