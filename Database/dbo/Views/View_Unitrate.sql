ceaa9696731c8cc028d1bfdc61c60321e2fe644f
YINMEI <yinmei@sportscity.com.tw>
2016-03-07 12:39:01 +0000

    新增RateValue欄位

--- Database/dbo/Views/View_Unitrate.sql
+++ Database/dbo/Views/View_Unitrate.sql
@@ -1,6 +1,7 @@
 ﻿
-create view  [dbo].[View_Unitrate]
+
+CREATE view  [dbo].[View_Unitrate]
 as
-SELECT TMP.*,ISNULL(R.Rate,1) Rate 
+SELECT TMP.*,ISNULL(R.Rate,1) Rate, ISNULL(R.RateValue,1) RateValue
 FROM (SELECT A.ID FROM_U,B.ID TO_U FROM DBO.Unit A,DBO.UNIT B) TMP 
 LEFT JOIN DBO.Unit_Rate R ON TMP.FROM_U = R.UnitFrom AND TMP.TO_U = R.UnitTo
