b1ada54458cb88de78bd379b23e3c671aa5d6d0c
YINMEI <yinmei@sportscity.com.tw>
2016-03-07 12:39:35 +0000

    Add getUnitRate

---
+++ Database/dbo/Functions/getUnitRate.sql
@@ -0,0 +1,10 @@
+﻿
+CREATE FUNCTION [dbo].[getUnitRate](@fromUnit varchar(8), @toUnit varchar(8))
+RETURNS decimal
+BEGIN
+	DECLARE @rate decimal
+	SET @rate = 1
+	select @rate = isnull(RateValue,'1') from View_Unitrate where FROM_U = @fromUnit and TO_U = @toUnit
+	
+	RETURN @rate
+END
