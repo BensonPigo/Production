ec66824f568bd4221e276f4968072fef979651e1
YINMEI <yinmei@sportscity.com.tw>
2016-03-08 10:20:45 +0000

    Add R03

---
+++ Sci.Production.Planning/R03.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Sci.Production.Planning
+{
+    partial class R03
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.label3 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.label5 = new Sci.Win.UI.Label();
+            this.label6 = new Sci.Win.UI.Label();
+            this.txtstyle1 = new Sci.Production.Class.txtstyle();
+            this.txtbrand1 = new Sci.Production.Class.txtbrand();
+            this.txtseason1 = new Sci.Production.Class.txtseason();
+            this.txttpeuser_canedit1 = new Sci.Production.Class.txttpeuser_canedit();
+            this.txtsubcon1 = new Sci.Production.Class.txtsubcon();
+            this.dateRange1 = new Sci.Win.UI.DateRange();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(422, 12);
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(422, 48);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(422, 84);
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 23);
+            this.label1.TabIndex = 94;
+            this.label1.Text = "Style";
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(13, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 23);
+            this.label2.TabIndex = 95;
+            this.label2.Text = "Brand";
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(13, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 23);
+            this.label3.TabIndex = 96;
+            this.label3.Text = "Season";
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(13, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 23);
+            this.label4.TabIndex = 97;
+            this.label4.Text = "SMR";
+            // 
+            // label5
+            // 
+            this.label5.Lines = 0;
+            this.label5.Location = new System.Drawing.Point(13, 157);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 23);
+            this.label5.TabIndex = 98;
+            this.label5.Text = "Subcon";
+            // 
+            // label6
+            // 
+            this.label6.Lines = 0;
+            this.label6.Location = new System.Drawing.Point(13, 193);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(83, 23);
+            this.label6.TabIndex = 99;
+            this.label6.Text = "SCI Delivery";
+            // 
+            // txtstyle1
+            // 
+            this.txtstyle1.BackColor = System.Drawing.Color.White;
+            this.txtstyle1.BrandObjectName = null;
+            this.txtstyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtstyle1.Location = new System.Drawing.Point(100, 12);
+            this.txtstyle1.Name = "txtstyle1";
+            this.txtstyle1.Size = new System.Drawing.Size(130, 23);
+            this.txtstyle1.TabIndex = 100;
+            // 
+            // txtbrand1
+            // 
+            this.txtbrand1.BackColor = System.Drawing.Color.White;
+            this.txtbrand1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtbrand1.Location = new System.Drawing.Point(100, 48);
+            this.txtbrand1.Name = "txtbrand1";
+            this.txtbrand1.Size = new System.Drawing.Size(80, 23);
+            this.txtbrand1.TabIndex = 101;
+            // 
+            // txtseason1
+            // 
+            this.txtseason1.BackColor = System.Drawing.Color.White;
+            this.txtseason1.BrandObjectName = null;
+            this.txtseason1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtseason1.Location = new System.Drawing.Point(100, 84);
+            this.txtseason1.Name = "txtseason1";
+            this.txtseason1.Size = new System.Drawing.Size(80, 23);
+            this.txtseason1.TabIndex = 102;
+            // 
+            // txttpeuser_canedit1
+            // 
+            this.txttpeuser_canedit1.DisplayBox1Binding = "";
+            this.txttpeuser_canedit1.Location = new System.Drawing.Point(100, 121);
+            this.txttpeuser_canedit1.Name = "txttpeuser_canedit1";
+            this.txttpeuser_canedit1.Size = new System.Drawing.Size(300, 23);
+            this.txttpeuser_canedit1.TabIndex = 103;
+            this.txttpeuser_canedit1.TextBox1Binding = "";
+            // 
+            // txtsubcon1
+            // 
+            this.txtsubcon1.DisplayBox1Binding = "";
+            this.txtsubcon1.IsIncludeJunk = false;
+            this.txtsubcon1.Location = new System.Drawing.Point(100, 157);
+            this.txtsubcon1.Name = "txtsubcon1";
+            this.txtsubcon1.Size = new System.Drawing.Size(170, 23);
+            this.txtsubcon1.TabIndex = 104;
+            this.txtsubcon1.TextBox1Binding = "";
+            // 
+            // dateRange1
+            // 
+            this.dateRange1.IsRequired = false;
+            this.dateRange1.Location = new System.Drawing.Point(100, 193);
+            this.dateRange1.Name = "dateRange1";
+            this.dateRange1.Size = new System.Drawing.Size(280, 23);
+            this.dateRange1.TabIndex = 105;
+            // 
+            // R03
+            // 
+            this.ClientSize = new System.Drawing.Size(514, 252);
+            this.Controls.Add(this.dateRange1);
+            this.Controls.Add(this.txtsubcon1);
+            this.Controls.Add(this.txttpeuser_canedit1);
+            this.Controls.Add(this.txtseason1);
+            this.Controls.Add(this.txtbrand1);
+            this.Controls.Add(this.txtstyle1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.EditMode = true;
+            this.IsSupportToPrint = false;
+            this.Name = "R03";
+            this.Text = "R03. Local Quotation List";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.label5, 0);
+            this.Controls.SetChildIndex(this.label6, 0);
+            this.Controls.SetChildIndex(this.txtstyle1, 0);
+            this.Controls.SetChildIndex(this.txtbrand1, 0);
+            this.Controls.SetChildIndex(this.txtseason1, 0);
+            this.Controls.SetChildIndex(this.txttpeuser_canedit1, 0);
+            this.Controls.SetChildIndex(this.txtsubcon1, 0);
+            this.Controls.SetChildIndex(this.dateRange1, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.Label label1;
+        private Win.UI.Label label2;
+        private Win.UI.Label label3;
+        private Win.UI.Label label4;
+        private Win.UI.Label label5;
+        private Win.UI.Label label6;
+        private Class.txtstyle txtstyle1;
+        private Class.txtbrand txtbrand1;
+        private Class.txtseason txtseason1;
+        private Class.txttpeuser_canedit txttpeuser_canedit1;
+        private Class.txtsubcon txtsubcon1;
+        private Win.UI.DateRange dateRange1;
+    }
+}
---
+++ Sci.Production.Planning/R03.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict.Win;
+using Ict;
+using Sci.Data;
+
+namespace Sci.Production.Planning
+{
+    public partial class R03 : Sci.Win.Tems.PrintForm
+    {
+        DataTable printData;
+        DateTime? sciDate1, sciDate2;
+        string style, brand, season, smr, subcon;
+        public R03(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+        }
+
+        // 驗證輸入條件
+        protected override bool ValidateInput()
+        {
+            style = txtstyle1.Text;
+            brand = txtbrand1.Text;
+            season = txtseason1.Text;
+            smr = txttpeuser_canedit1.TextBox1.Text;
+            subcon = txtsubcon1.TextBox1.Text;
+            sciDate1 = dateRange1.Value1;
+            sciDate2 = dateRange1.Value2;
+
+            return base.ValidateInput();
+        }
+
+        // 非同步取資料
+        protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            StringBuilder sqlCmd = new StringBuilder();
+            sqlCmd.Append(@"select distinct s.ID,s.BrandID,s.SeasonID,sa.ArtworkTypeID,sa.Article,sa.ArtworkID,sa.ArtworkName,
+sa.PatternCode,sa.PatternDesc,sa.TMS,sa.Qty,isnull(a.ArtworkUnit,'') as ArtworkUnit,sa.Cost,
+saq.LocalSuppId+'-'+isnull(ls.Abb,'') as LocalSupp,saq.CurrencyId,saq.Price,saq.Oven,saq.Wash,saq.Mockup,saq.PriceApv
+from Orders o
+inner join Style s on o.StyleUkey = s.Ukey
+inner join Style_Artwork sa on sa.StyleUkey = o.StyleUkey and sa.StyleUkey = s.Ukey
+inner join Style_Artwork_Quot saq on sa.Ukey = saq.Ukey
+left join LocalSupp ls on ls.ID = saq.LocalSuppId
+left join ArtworkType a on a.ID = sa.ArtworkTypeID
+where 1 = 1");
+
+            if (!MyUtility.Check.Empty(style))
+            {
+                sqlCmd.Append(string.Format(@" and s.ID = '{0}'
+and o.StyleID = '{0}'", style));
+            }
+
+            if (!MyUtility.Check.Empty(brand))
+            {
+                sqlCmd.Append(string.Format(@" and s.BrandID = '{0}'
+and o.BrandID = '{0}'", brand));
+            }
+
+            if (!MyUtility.Check.Empty(season))
+            {
+                sqlCmd.Append(string.Format(@" and s.SeasonID = '{0}'
+and o.SeasonID = '{0}'", season));
+            }
+
+            if (!MyUtility.Check.Empty(smr))
+            {
+                sqlCmd.Append(string.Format(@" and ((s.Phase = 1 and s.SampleSMR = '{0}') or (s.Phase = 2 and s.BulkSMR = '{0}'))", smr));
+            }
+
+            if (!MyUtility.Check.Empty(subcon))
+            {
+                sqlCmd.Append(string.Format(" and saq.LocalSuppId = '{0}'", subcon));
+            }
+
+            if (!MyUtility.Check.Empty(sciDate1))
+            {
+                sqlCmd.Append(string.Format(" and o.SciDelivery >= '{0}'", Convert.ToDateTime(sciDate1).ToString("d")));
+            }
+
+            if (!MyUtility.Check.Empty(sciDate2))
+            {
+                sqlCmd.Append(string.Format(" and o.SciDelivery <= '{0}'", Convert.ToDateTime(sciDate2).ToString("d")));
+            }
+
+            sqlCmd.Append(" order by s.ID,s.BrandID,s.SeasonID,sa.ArtworkTypeID");
+
+            DualResult result = DBProxy.Current.Select(null, sqlCmd.ToString(), out printData);
+            if (!result)
+            {
+                DualResult failResult = new DualResult(false, "Query data fail\r\n" + result.ToString());
+                return failResult;
+            }
+            return Result.True;
+        }
+
+        // 產生Excel
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            // 顯示筆數於PrintForm上Count欄位
+            SetCount(printData.Rows.Count);
+
+            if (printData.Rows.Count <= 0)
+            {
+                MyUtility.Msg.WarningBox("Data not found!");
+                return false;
+            }
+
+            MyUtility.Msg.WaitWindows("Starting EXCEL...");
+            string strXltName = Sci.Env.Cfg.XltPathDir + "Planning_R03_LocalQuotationList.xltx";
+            Microsoft.Office.Interop.Excel.Application excel = MyUtility.Excel.ConnectExcel(strXltName);
+            if (excel == null) return false;
+            Microsoft.Office.Interop.Excel.Worksheet worksheet = excel.ActiveWorkbook.Worksheets[1];
+
+            //填內容值
+            int intRowsStart = 2;
+            object[,] objArray = new object[1, 20];
+            foreach (DataRow dr in printData.Rows)
+            {
+                objArray[0, 0] = dr["ID"];
+                objArray[0, 1] = dr["BrandID"];
+                objArray[0, 2] = dr["SeasonID"];
+                objArray[0, 3] = dr["ArtworkTypeID"];
+                objArray[0, 4] = dr["Article"];
+                objArray[0, 5] = dr["ArtworkID"];
+                objArray[0, 6] = dr["ArtworkName"];
+                objArray[0, 7] = dr["PatternCode"];
+                objArray[0, 8] = dr["PatternDesc"];
+                objArray[0, 9] = dr["TMS"];
+                objArray[0, 10] = dr["Qty"];
+                objArray[0, 11] = dr["ArtworkUnit"];
+                objArray[0, 12] = dr["Cost"];
+                objArray[0, 13] = dr["LocalSupp"];
+                objArray[0, 14] = dr["CurrencyId"];
+                objArray[0, 15] = dr["Price"];
+                objArray[0, 16] = dr["Oven"];
+                objArray[0, 17] = dr["Wash"];
+                objArray[0, 18] = dr["Mockup"];
+                objArray[0, 19] = dr["PriceApv"];
+
+                worksheet.Range[String.Format("A{0}:T{0}", intRowsStart)].Value2 = objArray;
+                intRowsStart++;
+            }
+
+            excel.Cells.EntireColumn.AutoFit();
+            excel.Cells.EntireRow.AutoFit();
+            MyUtility.Msg.WaitClear();
+            excel.Visible = true;
+            return true;
+        }
+    }
+}
