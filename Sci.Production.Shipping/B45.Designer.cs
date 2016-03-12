40cd0d1a2340c912cf0b5125d0bb26d9cc060ea9
YINMEI <yinmei@sportscity.com.tw>
2016-03-12 10:53:31 +0000

    Add B45

---
+++ Sci.Production.Shipping/B45.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Sci.Production.Shipping
+{
+    partial class B45
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.label3 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.textBox1 = new Sci.Win.UI.TextBox();
+            this.displayBox1 = new Sci.Win.UI.DisplayBox();
+            this.displayBox2 = new Sci.Win.UI.DisplayBox();
+            this.numericBox1 = new Sci.Win.UI.NumericBox();
+            this.radioPanel1 = new Sci.Win.UI.RadioPanel();
+            this.radioButton2 = new Sci.Win.UI.RadioButton();
+            this.radioButton1 = new Sci.Win.UI.RadioButton();
+            this.checkBox1 = new Sci.Win.UI.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).BeginInit();
+            this.detail.SuspendLayout();
+            this.detailcont.SuspendLayout();
+            this.detailbtm.SuspendLayout();
+            this.tabs.SuspendLayout();
+            this.radioPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // detail
+            // 
+            this.detail.Size = new System.Drawing.Size(676, 347);
+            // 
+            // detailcont
+            // 
+            this.detailcont.Controls.Add(this.checkBox1);
+            this.detailcont.Controls.Add(this.radioPanel1);
+            this.detailcont.Controls.Add(this.numericBox1);
+            this.detailcont.Controls.Add(this.displayBox2);
+            this.detailcont.Controls.Add(this.displayBox1);
+            this.detailcont.Controls.Add(this.textBox1);
+            this.detailcont.Controls.Add(this.label4);
+            this.detailcont.Controls.Add(this.label3);
+            this.detailcont.Controls.Add(this.label2);
+            this.detailcont.Controls.Add(this.label1);
+            this.detailcont.Size = new System.Drawing.Size(676, 309);
+            // 
+            // detailbtm
+            // 
+            this.detailbtm.Location = new System.Drawing.Point(0, 309);
+            this.detailbtm.Size = new System.Drawing.Size(676, 38);
+            // 
+            // browse
+            // 
+            this.browse.Size = new System.Drawing.Size(676, 347);
+            // 
+            // tabs
+            // 
+            this.tabs.Size = new System.Drawing.Size(684, 376);
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(43, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NL Code";
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(43, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "HS Code";
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(43, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Unit";
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(43, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Q\'ty";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "NLCode", true));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(108, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(60, 23);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.PopUp += new System.EventHandler<Sci.Win.UI.TextBoxPopUpEventArgs>(this.textBox1_PopUp);
+            this.textBox1.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating);
+            // 
+            // displayBox1
+            // 
+            this.displayBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.displayBox1.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "HSCode", true));
+            this.displayBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.displayBox1.Location = new System.Drawing.Point(108, 56);
+            this.displayBox1.Name = "displayBox1";
+            this.displayBox1.Size = new System.Drawing.Size(110, 23);
+            this.displayBox1.TabIndex = 5;
+            // 
+            // displayBox2
+            // 
+            this.displayBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.displayBox2.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "UnitID", true));
+            this.displayBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.displayBox2.Location = new System.Drawing.Point(108, 95);
+            this.displayBox2.Name = "displayBox2";
+            this.displayBox2.Size = new System.Drawing.Size(80, 23);
+            this.displayBox2.TabIndex = 6;
+            // 
+            // numericBox1
+            // 
+            this.numericBox1.BackColor = System.Drawing.Color.White;
+            this.numericBox1.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "Qty", true));
+            this.numericBox1.DecimalPlaces = 3;
+            this.numericBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.numericBox1.Location = new System.Drawing.Point(108, 132);
+            this.numericBox1.Name = "numericBox1";
+            this.numericBox1.NullValue = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            this.numericBox1.Size = new System.Drawing.Size(80, 23);
+            this.numericBox1.TabIndex = 7;
+            this.numericBox1.Value = new decimal(new int[] {
+            0,
+            0,
+            0,
+            0});
+            // 
+            // radioPanel1
+            // 
+            this.radioPanel1.Controls.Add(this.radioButton2);
+            this.radioPanel1.Controls.Add(this.radioButton1);
+            this.radioPanel1.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "Type", true));
+            this.radioPanel1.Location = new System.Drawing.Point(193, 129);
+            this.radioPanel1.Name = "radioPanel1";
+            this.radioPanel1.Size = new System.Drawing.Size(217, 57);
+            this.radioPanel1.TabIndex = 8;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.radioButton2.Location = new System.Drawing.Point(3, 31);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(198, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Calculate with “Qty per Ctn”";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Value = "2";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.radioButton1.Location = new System.Drawing.Point(3, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(88, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Fixed Q\'ty";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Value = "1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "TissuePaper", true));
+            this.checkBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.checkBox1.Location = new System.Drawing.Point(444, 18);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(110, 21);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Tissue paper";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // B45
+            // 
+            this.ClientSize = new System.Drawing.Size(684, 409);
+            this.DefaultOrder = "NLCode";
+            this.Name = "B45";
+            this.Text = "B45. Fixed Export Declare Data";
+            this.UniqueExpress = "NLCode";
+            this.WorkAlias = "VNFixedDeclareItem";
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).EndInit();
+            this.detail.ResumeLayout(false);
+            this.detailcont.ResumeLayout(false);
+            this.detailcont.PerformLayout();
+            this.detailbtm.ResumeLayout(false);
+            this.detailbtm.PerformLayout();
+            this.tabs.ResumeLayout(false);
+            this.radioPanel1.ResumeLayout(false);
+            this.radioPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.CheckBox checkBox1;
+        private Win.UI.RadioPanel radioPanel1;
+        private Win.UI.RadioButton radioButton2;
+        private Win.UI.RadioButton radioButton1;
+        private Win.UI.NumericBox numericBox1;
+        private Win.UI.DisplayBox displayBox2;
+        private Win.UI.DisplayBox displayBox1;
+        private Win.UI.TextBox textBox1;
+        private Win.UI.Label label4;
+        private Win.UI.Label label3;
+        private Win.UI.Label label2;
+        private Win.UI.Label label1;
+    }
+}
---
+++ Sci.Production.Shipping/B45.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Shipping
+{
+    public partial class B45 : Sci.Win.Tems.Input1
+    {
+        public B45(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+        }
+
+        protected override void ClickNewAfter()
+        {
+            base.ClickNewAfter();
+            CurrentMaintain["Type"] = 1;
+        }
+
+        protected override void ClickEditAfter()
+        {
+            base.ClickEditAfter();
+            this.textBox1.ReadOnly = true;
+        }
+
+        protected override bool ClickSaveBefore()
+        {
+            if (MyUtility.Check.Empty(CurrentMaintain["NLCode"]))
+            {
+                MyUtility.Msg.WarningBox("NL Code can't empty!!");
+                return false;
+            }
+
+            if (MyUtility.Check.Empty(CurrentMaintain["Qty"]))
+            {
+                MyUtility.Msg.WarningBox("Qty can't empty!!");
+                return false;
+            }
+
+            return base.ClickSaveBefore();
+        }
+
+        //NL Code
+        private void textBox1_PopUp(object sender, Win.UI.TextBoxPopUpEventArgs e)
+        {
+            Sci.Win.Tools.SelectItem item = new Sci.Win.Tools.SelectItem(@"select NLCode,HSCode,UnitID
+from VNContract_Detail
+where ID in (select ID from (select ID,MAX(StartDate) as MaxDate from VNContract where Encode = 1 group by ID) a)
+order by NLCode", "5,11,8", this.Text, false, ",", headercaptions: "NL Code, HSCode, Unit");
+            DialogResult result = item.ShowDialog();
+            if (result == DialogResult.Cancel) { return; }
+            IList<DataRow> selectedData = item.GetSelecteds();
+            CurrentMaintain["NLCode"] = item.GetSelectedString();
+            CurrentMaintain["HSCode"] = selectedData[0]["HSCode"];
+            CurrentMaintain["UnitID"] = selectedData[0]["UnitID"];
+
+        }
+
+        //NL Code
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+            if (EditMode && textBox1.OldValue != textBox1.Text)
+            {
+                if (MyUtility.Check.Empty(textBox1.Text))
+                {
+                    CurrentMaintain["NLCode"] = "";
+                    CurrentMaintain["HSCode"] = "";
+                    CurrentMaintain["UnitID"] = "";
+                }
+                else
+                {
+                    DataRow NLCodeDate;
+                    if (MyUtility.Check.Seek(string.Format(@"select NLCode,HSCode,UnitID
+from VNContract_Detail
+where ID in (select ID from (select ID,MAX(StartDate) as MaxDate from VNContract where Encode = 1 group by ID) a)
+and NLCode = '{0}'", textBox1.Text), out NLCodeDate))
+                    {
+                        CurrentMaintain["NLCode"] = textBox1.Text;
+                        CurrentMaintain["HSCode"] = NLCodeDate["HSCode"];
+                        CurrentMaintain["UnitID"] = NLCodeDate["UnitID"];
+                    }
+                    else
+                    {
+                        MyUtility.Msg.WarningBox("The NL Code is not in the Contract!!");
+                        CurrentMaintain["NLCode"] = "";
+                        CurrentMaintain["HSCode"] = "";
+                        CurrentMaintain["UnitID"] = "";
+                        e.Cancel = true;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
