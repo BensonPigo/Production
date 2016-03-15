f8632bf83fbcaf963bd3a2f038bdc13ed72a79b4
MIKE <mike@sportscity.com.tw>
2016-03-15 01:54:18 +0000

    ADD FINANCE EN REASON CLASS

---
+++ Sci.Production.Class/txtFinanceEnReason.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sci.Data;
+
+namespace Sci.Production.Class
+{
+    public partial class txtFinanceEnReason : Sci.Win.UI.ComboBox
+    {
+        private string type;
+        [Category("Custom Properties")]
+        public string ReasonTypeID
+        {
+            set
+            {
+                this.type = value;
+                if (!Env.DesignTime)
+                {
+                    string selectCommand = string.Format(@"select ID, rtrim(ID)+'- '+rtrim(Name) as IDName 
+                    from dbo.reason where ReasonTypeID = '{0}' order by no", this.ReasonTypeID);
+                    Ict.DualResult returnResult;
+                    DataTable dropDownListTable = new DataTable();
+                    if (returnResult = DBProxy.Current.Select("Finance", selectCommand, out dropDownListTable))
+                    {
+                        this.DataSource = dropDownListTable;
+                        this.DisplayMember = "IDName";
+                        this.ValueMember = "ID";
+                    }
+                }
+            }
+            get { return this.type; }
+        }
+
+        public txtFinanceEnReason()
+        { }
+    }
+}
