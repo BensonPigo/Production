691eb6971e4ff0c4dab791e4405af30317adb4ed
MIKE <mike@sportscity.com.tw>
2016-03-16 12:49:54 +0000

    add R14

---
+++ Sci.Production.Warehouse/Report/R14.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Sci.Production.Warehouse
+{
+    partial class R14
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateRange1 = new Sci.Win.UI.DateRange();
+            this.label4 = new Sci.Win.UI.Label();
+            this.label8 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.txtfactory1 = new Sci.Production.Class.txtfactory();
+            this.txtdropdownlist1 = new Sci.Production.Class.txtdropdownlist();
+            this.label6 = new Sci.Win.UI.Label();
+            this.cbbFabricType = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(439, 12);
+            this.print.TabIndex = 4;
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(439, 48);
+            this.toexcel.TabIndex = 5;
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(439, 84);
+            this.close.TabIndex = 6;
+            // 
+            // dateRange1
+            // 
+            this.dateRange1.Location = new System.Drawing.Point(115, 12);
+            this.dateRange1.Name = "dateRange1";
+            this.dateRange1.Size = new System.Drawing.Size(280, 23);
+            this.dateRange1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(13, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 23);
+            this.label4.TabIndex = 103;
+            this.label4.Text = "Factory";
+            // 
+            // label8
+            // 
+            this.label8.Lines = 0;
+            this.label8.Location = new System.Drawing.Point(13, 48);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(98, 23);
+            this.label8.TabIndex = 118;
+            this.label8.Text = "Fabric Type";
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(14, 12);
+            this.label2.Name = "label2";
+            this.label2.RectStyle.BorderColor = System.Drawing.Color.Black;
+            this.label2.RectStyle.BorderWidth = 1F;
+            this.label2.RectStyle.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label2.RectStyle.ExtBorderWidth = 1F;
+            this.label2.Size = new System.Drawing.Size(98, 23);
+            this.label2.TabIndex = 122;
+            this.label2.Text = "WK# ETA";
+            this.label2.TextStyle.BorderColor = System.Drawing.Color.Black;
+            this.label2.TextStyle.Color = System.Drawing.Color.Black;
+            // 
+            // txtfactory1
+            // 
+            this.txtfactory1.BackColor = System.Drawing.Color.White;
+            this.txtfactory1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtfactory1.Location = new System.Drawing.Point(115, 84);
+            this.txtfactory1.Name = "txtfactory1";
+            this.txtfactory1.Size = new System.Drawing.Size(66, 23);
+            this.txtfactory1.TabIndex = 2;
+            // 
+            // txtdropdownlist1
+            // 
+            this.txtdropdownlist1.BackColor = System.Drawing.Color.White;
+            this.txtdropdownlist1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtdropdownlist1.FormattingEnabled = true;
+            this.txtdropdownlist1.IsSupportUnselect = true;
+            this.txtdropdownlist1.Location = new System.Drawing.Point(115, 123);
+            this.txtdropdownlist1.Name = "txtdropdownlist1";
+            this.txtdropdownlist1.Size = new System.Drawing.Size(261, 24);
+            this.txtdropdownlist1.TabIndex = 3;
+            this.txtdropdownlist1.Type = "Category";
+            // 
+            // label6
+            // 
+            this.label6.Lines = 0;
+            this.label6.Location = new System.Drawing.Point(14, 123);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 23);
+            this.label6.TabIndex = 125;
+            this.label6.Text = "Order Type";
+            // 
+            // cbbFabricType
+            // 
+            this.cbbFabricType.FormattingEnabled = true;
+            this.cbbFabricType.Location = new System.Drawing.Point(115, 47);
+            this.cbbFabricType.Name = "cbbFabricType";
+            this.cbbFabricType.Size = new System.Drawing.Size(121, 24);
+            this.cbbFabricType.TabIndex = 1;
+            // 
+            // R14
+            // 
+            this.ClientSize = new System.Drawing.Size(531, 182);
+            this.Controls.Add(this.cbbFabricType);
+            this.Controls.Add(this.txtdropdownlist1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtfactory1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateRange1);
+            this.IsSupportToPrint = false;
+            this.Name = "R14";
+            this.Text = "R14. Material Outstanding / Lacking Report";
+            this.Controls.SetChildIndex(this.dateRange1, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.label8, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.txtfactory1, 0);
+            this.Controls.SetChildIndex(this.label6, 0);
+            this.Controls.SetChildIndex(this.txtdropdownlist1, 0);
+            this.Controls.SetChildIndex(this.cbbFabricType, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.DateRange dateRange1;
+        private Win.UI.Label label4;
+        private Win.UI.Label label8;
+        private Win.UI.Label label2;
+        private Class.txtfactory txtfactory1;
+        private Class.txtdropdownlist txtdropdownlist1;
+        private Win.UI.Label label6;
+        private System.Windows.Forms.ComboBox cbbFabricType;
+    }
+}
---
+++ Sci.Production.Warehouse/Report/R14.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict.Win;
+using Ict;
+using Sci.Data;
+
+namespace Sci.Production.Warehouse
+{
+    public partial class R14 : Sci.Win.Tems.PrintForm
+    {
+        string reason, factory, stocktype, fabrictype, ordertype;
+        int ordertypeindex;
+        DateTime? eta1, eta2;
+        DataTable printData;
+
+        public R14(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            txtfactory1.Text = Sci.Env.User.Keyword;
+            MyUtility.Tool.SetupCombox(cbbFabricType, 2, 1, ",ALL,F,Fabric,A,Accessory");
+            cbbFabricType.SelectedIndex = 0;
+            txtdropdownlist1.SelectedIndex = 0;
+        }
+
+        // 驗證輸入條件
+        protected override bool ValidateInput()
+        {
+            if (MyUtility.Check.Empty(dateRange1.Value1) ) 
+            {
+                MyUtility.Msg.WarningBox("< WK# ETA > can't be empty!!");
+                return false;
+            }
+
+            eta1 = dateRange1.Value1;
+            eta2 = dateRange1.Value2;
+            ordertypeindex = txtdropdownlist1.SelectedIndex;
+            fabrictype = cbbFabricType.SelectedValue.ToString();
+            factory = txtfactory1.Text;
+            switch (ordertypeindex)
+            {
+                case 0:
+                    ordertype = "('B')";
+                    break;
+                case 1:
+                    ordertype = "('S')";
+                    break;
+                case 2:
+                    ordertype = "('M')";
+                    break;
+                case 3:
+                    ordertype = "('B','S')";
+                    break;
+                case 4:
+                    ordertype = "('B','S')";
+                    break;
+                case 5:
+                    ordertype = "('B','S','M')";
+                    break;
+            }
+
+            return base.ValidateInput();
+        }
+
+        // 非同步取資料
+        protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            #region -- sql parameters declare --
+
+            System.Data.SqlClient.SqlParameter sp_factory = new System.Data.SqlClient.SqlParameter();
+            sp_factory.ParameterName = "@factory";
+
+            IList<System.Data.SqlClient.SqlParameter> cmds = new List<System.Data.SqlClient.SqlParameter>();
+            #endregion
+
+            StringBuilder sqlCmd = new StringBuilder();
+            sqlCmd.Append(string.Format(@"select 
+b.id as wkno_a, 
+b.poid as order_a, 
+b.seq1 + b.seq2 as seq_a, 
+b.refno ,
+c.ColorID,
+c.SizeSpec,
+c.StockUnit,
+(b.qty + b.foc)  * v.RateValue shipqty,
+iif ((b.qty + b.foc)  * v.RateValue > isnull(x.qty,0),'V','') over1,
+isnull(x.qty,0) received_qty,
+iif ((b.qty + b.foc)  * v.RateValue < isnull(x.qty,0),'V','') over2
+from dbo.export A INNER JOIN dbo.export_detail B ON B.ID = A.ID
+inner join dbo.PO_Supp_Detail c on c.ID = b.PoID and c.seq1 = b.seq1 and c.seq2 =  b.seq2 
+inner join dbo.View_Unitrate v on v.FROM_U = c.POUnit and v.TO_U = c.StockUnit
+outer apply ( select sum(b1.StockQty) qty from dbo.Receiving a1 inner join dbo.Receiving_Detail b1 on b1.id = a1.Id
+where a1.ExportId = a.id and b1.PoId = b.PoID and b1.seq1 = b.seq1 and b1.seq2 = b.seq2 and a1.Status = 'Confirmed') x
+inner join dbo.orders d on d.id = b.poid
+WHERE  A.Eta BETWEEN '{0}' and '{1}' and  D.Category in {2}
+", Convert.ToDateTime(eta1).ToString("d")
+ , Convert.ToDateTime(eta2).ToString("d")
+ , ordertype));
+
+            #region --- 條件組合  ---
+            if (!MyUtility.Check.Empty(factory))
+            {
+                sqlCmd.Append(" and d.factoryid = @factory");
+                sp_factory.Value = factory;
+                cmds.Add(sp_factory);
+            }
+
+            if (!MyUtility.Check.Empty(fabrictype))
+            {
+                sqlCmd.Append(string.Format(@" and c.fabrictype = '{0}'", fabrictype));
+            }
+
+            #endregion
+
+            DualResult result = DBProxy.Current.Select(null, sqlCmd.ToString(), cmds, out printData);
+            if (!result)
+            {
+                DualResult failResult = new DualResult(false, "Query data fail\r\n" + result.ToString());
+                return failResult;
+            }
+            return Result.True;
+        }
+
+        // 產生Excel
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            // 顯示筆數於PrintForm上Count欄位
+            SetCount(printData.Rows.Count);
+
+            if (printData.Rows.Count <= 0)
+            {
+                MyUtility.Msg.WarningBox("Data not found!");
+                return false;
+            }
+
+            MyUtility.Excel.CopyToXls(printData, "", "Warehouse_R14.xltx", 2);
+            return true;
+        }
+    }
+}
