ecef8b1cc85d04d624417a11c94e0edd370fbbc3
MIKE <mike@sportscity.com.tw>
2016-03-18 07:58:24 +0000

    add R09
    modify R10的條件=ETA改為 Inventory ETA，以利辨別。

--- Sci.Production.Warehouse/Report/R09.Designer.cs
+++ Sci.Production.Warehouse/Report/R09.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Sci.Production.Warehouse
 {
-    partial class R10
+    partial class R09
     {
         /// <summary>
         /// 設計工具所需的變數。
@@ -44,21 +44,23 @@
             this.label12 = new Sci.Win.UI.Label();
             this.dateRange3 = new Sci.Win.UI.DateRange();
             this.label1 = new Sci.Win.UI.Label();
+            this.cbbFilter = new Sci.Win.UI.ComboBox();
+            this.label2 = new Sci.Win.UI.Label();
             this.SuspendLayout();
             // 
             // print
             // 
-            this.print.Location = new System.Drawing.Point(430, 12);
+            this.print.Location = new System.Drawing.Point(485, 12);
             this.print.TabIndex = 12;
             // 
             // toexcel
             // 
-            this.toexcel.Location = new System.Drawing.Point(430, 48);
+            this.toexcel.Location = new System.Drawing.Point(485, 48);
             this.toexcel.TabIndex = 13;
             // 
             // close
             // 
-            this.close.Location = new System.Drawing.Point(430, 84);
+            this.close.Location = new System.Drawing.Point(485, 84);
             this.close.TabIndex = 14;
             // 
             // label3
@@ -86,7 +88,7 @@
             // label4
             // 
             this.label4.Lines = 0;
-            this.label4.Location = new System.Drawing.Point(13, 148);
+            this.label4.Location = new System.Drawing.Point(13, 152);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(98, 23);
             this.label4.TabIndex = 103;
@@ -96,14 +98,14 @@
             // 
             this.txtMdivision1.BackColor = System.Drawing.Color.White;
             this.txtMdivision1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.txtMdivision1.Location = new System.Drawing.Point(115, 148);
+            this.txtMdivision1.Location = new System.Drawing.Point(115, 153);
             this.txtMdivision1.Name = "txtMdivision1";
             this.txtMdivision1.Size = new System.Drawing.Size(66, 23);
             this.txtMdivision1.TabIndex = 6;
             // 
             // dateRange2
             // 
-            this.dateRange2.Location = new System.Drawing.Point(115, 46);
+            this.dateRange2.Location = new System.Drawing.Point(115, 47);
             this.dateRange2.Name = "dateRange2";
             this.dateRange2.Size = new System.Drawing.Size(280, 23);
             this.dateRange2.TabIndex = 1;
@@ -111,7 +113,7 @@
             // label6
             // 
             this.label6.Lines = 0;
-            this.label6.Location = new System.Drawing.Point(13, 46);
+            this.label6.Location = new System.Drawing.Point(13, 47);
             this.label6.Name = "label6";
             this.label6.RectStyle.BorderColor = System.Drawing.Color.Black;
             this.label6.RectStyle.BorderWidth = 1F;
@@ -127,7 +129,7 @@
             // 
             this.txtSpno2.BackColor = System.Drawing.Color.White;
             this.txtSpno2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.txtSpno2.Location = new System.Drawing.Point(267, 80);
+            this.txtSpno2.Location = new System.Drawing.Point(267, 82);
             this.txtSpno2.Name = "txtSpno2";
             this.txtSpno2.Size = new System.Drawing.Size(128, 23);
             this.txtSpno2.TabIndex = 3;
@@ -136,7 +138,7 @@
             // 
             this.txtSpno1.BackColor = System.Drawing.Color.White;
             this.txtSpno1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.txtSpno1.Location = new System.Drawing.Point(114, 80);
+            this.txtSpno1.Location = new System.Drawing.Point(115, 82);
             this.txtSpno1.Name = "txtSpno1";
             this.txtSpno1.Size = new System.Drawing.Size(128, 23);
             this.txtSpno1.TabIndex = 2;
@@ -144,7 +146,7 @@
             // label10
             // 
             this.label10.Lines = 0;
-            this.label10.Location = new System.Drawing.Point(245, 80);
+            this.label10.Location = new System.Drawing.Point(245, 82);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(18, 23);
             this.label10.TabIndex = 115;
@@ -153,7 +155,7 @@
             // label9
             // 
             this.label9.Lines = 0;
-            this.label9.Location = new System.Drawing.Point(13, 80);
+            this.label9.Location = new System.Drawing.Point(13, 82);
             this.label9.Name = "label9";
             this.label9.RectStyle.BorderColor = System.Drawing.Color.Black;
             this.label9.RectStyle.Color = System.Drawing.Color.SkyBlue;
@@ -166,7 +168,7 @@
             // label11
             // 
             this.label11.Lines = 0;
-            this.label11.Location = new System.Drawing.Point(13, 182);
+            this.label11.Location = new System.Drawing.Point(13, 187);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(98, 23);
             this.label11.TabIndex = 120;
@@ -176,7 +178,7 @@
             // 
             this.txtRefno2.BackColor = System.Drawing.Color.White;
             this.txtRefno2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.txtRefno2.Location = new System.Drawing.Point(303, 182);
+            this.txtRefno2.Location = new System.Drawing.Point(303, 187);
             this.txtRefno2.Name = "txtRefno2";
             this.txtRefno2.Size = new System.Drawing.Size(164, 23);
             this.txtRefno2.TabIndex = 11;
@@ -185,7 +187,7 @@
             // 
             this.txtRefno1.BackColor = System.Drawing.Color.White;
             this.txtRefno1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.txtRefno1.Location = new System.Drawing.Point(114, 182);
+            this.txtRefno1.Location = new System.Drawing.Point(114, 187);
             this.txtRefno1.Name = "txtRefno1";
             this.txtRefno1.Size = new System.Drawing.Size(164, 23);
             this.txtRefno1.TabIndex = 10;
@@ -193,7 +195,7 @@
             // label12
             // 
             this.label12.Lines = 0;
-            this.label12.Location = new System.Drawing.Point(281, 182);
+            this.label12.Location = new System.Drawing.Point(281, 187);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(18, 23);
             this.label12.TabIndex = 123;
@@ -201,7 +203,7 @@
             // 
             // dateRange3
             // 
-            this.dateRange3.Location = new System.Drawing.Point(115, 114);
+            this.dateRange3.Location = new System.Drawing.Point(115, 117);
             this.dateRange3.Name = "dateRange3";
             this.dateRange3.Size = new System.Drawing.Size(280, 23);
             this.dateRange3.TabIndex = 124;
@@ -209,7 +211,7 @@
             // label1
             // 
             this.label1.Lines = 0;
-            this.label1.Location = new System.Drawing.Point(13, 114);
+            this.label1.Location = new System.Drawing.Point(13, 117);
             this.label1.Name = "label1";
             this.label1.RectStyle.BorderColor = System.Drawing.Color.Black;
             this.label1.RectStyle.BorderWidth = 1F;
@@ -221,9 +223,31 @@
             this.label1.TextStyle.BorderColor = System.Drawing.Color.Black;
             this.label1.TextStyle.Color = System.Drawing.Color.Black;
             // 
-            // R10
+            // cbbFilter
             // 
-            this.ClientSize = new System.Drawing.Size(522, 285);
+            this.cbbFilter.BackColor = System.Drawing.Color.White;
+            this.cbbFilter.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.cbbFilter.FormattingEnabled = true;
+            this.cbbFilter.IsSupportUnselect = true;
+            this.cbbFilter.Location = new System.Drawing.Point(114, 222);
+            this.cbbFilter.Name = "cbbFilter";
+            this.cbbFilter.Size = new System.Drawing.Size(353, 24);
+            this.cbbFilter.TabIndex = 126;
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(13, 222);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 23);
+            this.label2.TabIndex = 127;
+            this.label2.Text = "Filter Condition";
+            // 
+            // R09
+            // 
+            this.ClientSize = new System.Drawing.Size(577, 285);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbbFilter);
             this.Controls.Add(this.dateRange3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtRefno2);
@@ -241,8 +265,8 @@
             this.Controls.Add(this.dateRange1);
             this.Controls.Add(this.label3);
             this.IsSupportToPrint = false;
-            this.Name = "R10";
-            this.Text = "R10. Material aging analysis for Inventory";
+            this.Name = "R09";
+            this.Text = "R09. Taipei Stock VS Factory Inventory";
             this.Controls.SetChildIndex(this.label3, 0);
             this.Controls.SetChildIndex(this.dateRange1, 0);
             this.Controls.SetChildIndex(this.txtMdivision1, 0);
@@ -262,6 +286,8 @@
             this.Controls.SetChildIndex(this.txtRefno2, 0);
             this.Controls.SetChildIndex(this.label1, 0);
             this.Controls.SetChildIndex(this.dateRange3, 0);
+            this.Controls.SetChildIndex(this.cbbFilter, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -285,5 +311,7 @@
         private Win.UI.Label label12;
         private Win.UI.DateRange dateRange3;
         private Win.UI.Label label1;
+        private Win.UI.ComboBox cbbFilter;
+        private Win.UI.Label label2;
     }
 }
---
+++ Sci.Production.Warehouse/Report/R09.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict.Win;
+using Ict;
+using Sci.Data;
+
+namespace Sci.Production.Warehouse
+{
+    public partial class R09 : Sci.Win.Tems.PrintForm
+    {
+        string    mdivision, orderby, spno1, spno2, refno1, refno2;
+        DateTime? deadline1, deadline2, buyerDelivery1, buyerDelivery2, eta1, eta2;
+        DataTable printData;
+        int filterIndex;
+
+        public R09(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            DataTable factory;
+            DBProxy.Current.Select(null, "select '' as ID union all select ID from Factory", out factory);
+            txtMdivision1.Text = Sci.Env.User.Keyword;
+            MyUtility.Tool.SetupCombox(cbbFilter, 1, 1, "Actual Inventory Qty < Taipei system,Inventory In  < Taipei InputQty,");
+            cbbFilter.SelectedIndex = 0;
+        }
+
+        // 驗證輸入條件
+        protected override bool ValidateInput()
+        {
+            if (MyUtility.Check.Empty(dateRange1.Value1) &&
+                MyUtility.Check.Empty(dateRange2.Value1) &&
+                MyUtility.Check.Empty(dateRange3.Value1) &&
+                (MyUtility.Check.Empty(txtSpno1.Text) || MyUtility.Check.Empty(txtSpno2.Text)))
+            {
+                MyUtility.Msg.WarningBox("< Dead Line > & < Buyer Delivery > & < SP# > & < ETA > can't be empty!!");
+                return false;
+            }
+
+            deadline1 = dateRange1.Value1;
+            deadline2 = dateRange1.Value2;
+            buyerDelivery1 = dateRange2.Value1;
+            buyerDelivery2 = dateRange2.Value2;
+            spno1 = txtSpno1.Text;
+            spno2 = txtSpno2.Text;
+            eta1 = dateRange3.Value1;
+            eta2 = dateRange3.Value2;
+            mdivision = txtMdivision1.Text;
+            refno1 = txtRefno1.Text;
+            refno2 = txtRefno2.Text;
+            filterIndex = cbbFilter.SelectedIndex;
+
+            return base.ValidateInput();
+        }
+
+        // 非同步取資料
+        protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            #region -- sql parameters declare --
+            System.Data.SqlClient.SqlParameter sp_spno1 = new System.Data.SqlClient.SqlParameter();
+            sp_spno1.ParameterName = "@spno1";
+
+            System.Data.SqlClient.SqlParameter sp_spno2 = new System.Data.SqlClient.SqlParameter();
+            sp_spno2.ParameterName = "@spno2";
+
+            System.Data.SqlClient.SqlParameter sp_mdivision = new System.Data.SqlClient.SqlParameter();
+            sp_mdivision.ParameterName = "@MDivision";
+
+            System.Data.SqlClient.SqlParameter sp_refno1 = new System.Data.SqlClient.SqlParameter();
+            sp_refno1.ParameterName = "@refno1";
+
+            System.Data.SqlClient.SqlParameter sp_refno2 = new System.Data.SqlClient.SqlParameter();
+            sp_refno2.ParameterName = "@refno2";
+
+
+            IList<System.Data.SqlClient.SqlParameter> cmds = new List<System.Data.SqlClient.SqlParameter>();
+            #endregion
+
+            StringBuilder sqlCmd = new StringBuilder();
+            if (!MyUtility.Check.Empty(buyerDelivery1))
+            {
+                sqlCmd.Append(string.Format(@";with cte as 
+(
+	select poid from dbo.orders o 
+	where o.BuyerDelivery between '{0}' and '{1}' group by POID
+)
+select a.POID, a.seq1, a.seq2, a.eta, A.Refno
+, iif(A.FabricType='F','Fabric',iif(a.FabricType = 'A','Accessory',a.fabrictype)) FabricType
+, B.StockUnit
+,B.ColorID, C.BLocation, (B.ShipQty+B.ShipFOC)*v.RateValue ShipQty
+,C.InQty, C.OutQty, C.AdjustQty ,B.InputQty*v.RateValue InputQty,B.OutputQty*v.RateValue OutputQty
+, (B.InputQty - B.OutputQty)*v.RateValue TaipeiBalance
+,x.InQty, x.OutQty, x.AdjustQty,x.InQty - x.OutQty + x.AdjustQty balance
+from cte
+inner join Inventory a on a.POID = cte.POID 
+inner join dbo.PO_Supp_Detail b on b.id = a.POID and b.seq1 = a.seq1 and b.seq2 = a.Seq2
+inner join MDivisionPoDetail c on c.POID = a.POID and c.seq1 = a.seq1 and c.seq2 = a.Seq2
+inner join Factory d on d.id = a.FactoryID and d.mdivisionid = c.mdivisionid
+inner join dbo.View_Unitrate v on v.FROM_U = b.POUnit and v.TO_U = b.StockUnit 
+outer apply (select isnull(sum(m.InQty),0.00) InQty,isnull(sum(m.OutQty),0.00) OutQty,isnull(sum(m.AdjustQty),0.00) AdjustQty 
+from dbo.FtyInventory m 
+            where m.POID = a.POID and m.seq1 = a.seq1 and m.seq2 = a.seq2 and StockType = 'I' and m.mdivisionid=c.mdivisionid) x
+where b.InputQty > 0 ", Convert.ToDateTime(buyerDelivery1).ToString("d"), Convert.ToDateTime(buyerDelivery2).ToString("d")));
+            }
+            else
+            {
+                sqlCmd.Append(string.Format(@"select a.POID, a.seq1, a.seq2, a.eta,A.Refno
+, iif(A.FabricType='F','Fabric',iif(a.FabricType = 'A','Accessory',a.fabrictype)) FabricType, B.StockUnit
+,B.ColorID, C.BLocation, (B.ShipQty+B.ShipFOC)*v.RateValue ShipQty
+,C.InQty, C.OutQty, C.AdjustQty ,B.InputQty*v.RateValue InputQty,B.OutputQty*v.RateValue OutputQty
+, (B.InputQty - B.OutputQty)*v.RateValue TaipeiBalance
+,x.InQty, x.OutQty, x.AdjustQty,x.InQty - x.OutQty + x.AdjustQty balance
+from Inventory a
+inner join dbo.PO_Supp_Detail b on b.id = a.POID and b.seq1 = a.seq1 and b.seq2 = a.Seq2
+inner join MDivisionPoDetail c on c.POID = a.POID and c.seq1 = a.seq1 and c.seq2 = a.Seq2 
+inner join Factory d on d.id = a.FactoryID and d.mdivisionid = c.mdivisionid
+inner join dbo.View_Unitrate v on v.FROM_U = b.POUnit and v.TO_U = b.StockUnit 
+outer apply (select isnull(sum(m.InQty),0.00) InQty,isnull(sum(m.OutQty),0.00) OutQty,isnull(sum(m.AdjustQty),0.00) AdjustQty 
+from dbo.FtyInventory m 
+            where m.POID = a.POID and m.seq1 = a.seq1 and m.seq2 = a.seq2 and StockType = 'I' and m.mdivisionid=c.mdivisionid) x
+where b.InputQty> 0"));
+
+            }
+
+            #region --- 條件組合  ---
+            if (!MyUtility.Check.Empty(deadline1))
+            {
+                sqlCmd.Append(string.Format(@" and a.deadline between '{0}' and '{1}'",
+                    Convert.ToDateTime(deadline1).ToString("d"), Convert.ToDateTime(deadline2).ToString("d")));
+            }
+            if (!MyUtility.Check.Empty(spno1))
+            {
+                sqlCmd.Append(" and A.POID >= @spno1 and A.POID <= @spno2");
+                sp_spno1.Value = spno1;
+                sp_spno2.Value = spno2;
+                cmds.Add(sp_spno1);
+                cmds.Add(sp_spno2);
+            }
+            if (!MyUtility.Check.Empty(eta1))
+            {
+                sqlCmd.Append(string.Format(@" and a.eta between '{0}' and '{1}'"
+                , Convert.ToDateTime(eta1).ToString("d"), Convert.ToDateTime(eta2).ToString("d")));
+            }
+
+            if (!MyUtility.Check.Empty(mdivision))
+            {
+                sqlCmd.Append(" and d.mdivisionid = @MDivision");
+                sp_mdivision.Value = mdivision;
+                cmds.Add(sp_mdivision);
+            }
+
+            if (!MyUtility.Check.Empty(refno1))
+            {
+                sqlCmd.Append(" and x.refno >= @refno1 and p.refno <= @refno2");
+                sp_refno1.Value = refno1;
+                sp_refno2.Value = refno2;
+                cmds.Add(sp_refno1);
+                cmds.Add(sp_refno2);
+            }
+
+            if (filterIndex == 0)
+            {
+                sqlCmd.Append(" and c.linvQty < (B.InputQty - B.OutputQty)*v.RateValue");
+            }
+
+            if (filterIndex == 1)
+            {
+                sqlCmd.Append(" and x.InQty < B.InputQty*v.RateValue");
+            }
+            #endregion
+
+            DualResult result = DBProxy.Current.Select(null, sqlCmd.ToString(), cmds, out printData);
+            if (!result)
+            {
+                DualResult failResult = new DualResult(false, "Query data fail\r\n" + result.ToString());
+                return failResult;
+            }
+            return Result.True;
+        }
+
+        // 產生Excel
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            // 顯示筆數於PrintForm上Count欄位
+            SetCount(printData.Rows.Count);
+
+            if (printData.Rows.Count <= 0)
+            {
+                MyUtility.Msg.WarningBox("Data not found!");
+                return false;
+            }
+
+            MyUtility.Excel.CopyToXls(printData, "", "Warehouse_R09.xltx", 3);
+            return true;
+        }
+    }
+}
