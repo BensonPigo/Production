32a997580f80c82cf821b50e11e7ff879fda4742
MIKE <mike@sportscity.com.tw>
2016-03-28 06:40:34 +0000

    add subcon多選的原件，用在報表條件選擇

---
+++ Sci.Production.Class/txtMultiSubcon.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sci.Production.Class
+{
+    public partial class txtMultiSubcon : Sci.Win.UI.TextBox
+    {
+        [Category("Custom Properties")]
+        [Description("串sql條件值使用，會將值用單引號先包起來。例：'G001','G002','G003'")]
+        public string Subcons { set; get; }
+
+        public txtMultiSubcon()
+        {
+            this.Size = new System.Drawing.Size(450, 23);
+            this.ReadOnly = true;
+            this.IsSupportEditMode = false;
+            //this.Text = "";
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            if (e.Button == MouseButtons.Right)
+            {
+                string sqlcmd = string.Format(@"select id,abb from dbo.LocalSupp ");
+
+                Sci.Win.Tools.SelectItem2 selectSubcons = new Win.Tools.SelectItem2(sqlcmd,
+                                "Supp ID,Supp Abb", "13,60", this.Text);
+                selectSubcons.Width = 800;
+                DialogResult result = selectSubcons.ShowDialog();
+                if (result == DialogResult.Cancel) { return; }
+                this.Text = selectSubcons.GetSelectedString();
+                Subcons = "'" + string.Join("','", selectSubcons.GetSelectedList().ToArray()) + "'";
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+    }
+}
