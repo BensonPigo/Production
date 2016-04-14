3408a678659207aef4bf1e5ee3dd1ed5825b726d
YINMEI <yinmei@sportscity.com.tw>
2016-04-14 10:43:19 +0000

    Add R42

---
+++ Sci.Production.Shipping/R42.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Sci.Production.Shipping
+{
+    partial class R42
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.label3 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.dateRange1 = new Sci.Win.UI.DateRange();
+            this.comboBox1 = new Sci.Win.UI.ComboBox();
+            this.comboBox2 = new Sci.Win.UI.ComboBox();
+            this.comboBox3 = new Sci.Win.UI.ComboBox();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(386, 12);
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(386, 48);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(386, 84);
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 23);
+            this.label1.TabIndex = 94;
+            this.label1.Text = "Buyer Delivery";
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(13, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 23);
+            this.label2.TabIndex = 95;
+            this.label2.Text = "Category";
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(13, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 23);
+            this.label3.TabIndex = 96;
+            this.label3.Text = "M";
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(13, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 23);
+            this.label4.TabIndex = 97;
+            this.label4.Text = "Factory";
+            // 
+            // dateRange1
+            // 
+            this.dateRange1.Location = new System.Drawing.Point(111, 12);
+            this.dateRange1.Name = "dateRange1";
+            this.dateRange1.Size = new System.Drawing.Size(226, 23);
+            this.dateRange1.TabIndex = 98;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IsSupportUnselect = true;
+            this.comboBox1.Location = new System.Drawing.Point(111, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(131, 24);
+            this.comboBox1.TabIndex = 99;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.Color.White;
+            this.comboBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.IsSupportUnselect = true;
+            this.comboBox2.Location = new System.Drawing.Point(111, 83);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(82, 24);
+            this.comboBox2.TabIndex = 100;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.BackColor = System.Drawing.Color.White;
+            this.comboBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.IsSupportUnselect = true;
+            this.comboBox3.Location = new System.Drawing.Point(111, 120);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(82, 24);
+            this.comboBox3.TabIndex = 101;
+            // 
+            // R42
+            // 
+            this.ClientSize = new System.Drawing.Size(478, 179);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dateRange1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.EditMode = true;
+            this.IsSupportToPrint = false;
+            this.Name = "R42";
+            this.Text = "R42. Qty b\'down by Colorway/Size";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.dateRange1, 0);
+            this.Controls.SetChildIndex(this.comboBox1, 0);
+            this.Controls.SetChildIndex(this.comboBox2, 0);
+            this.Controls.SetChildIndex(this.comboBox3, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.Label label1;
+        private Win.UI.Label label2;
+        private Win.UI.Label label3;
+        private Win.UI.Label label4;
+        private Win.UI.DateRange dateRange1;
+        private Win.UI.ComboBox comboBox1;
+        private Win.UI.ComboBox comboBox2;
+        private Win.UI.ComboBox comboBox3;
+    }
+}
---
+++ Sci.Production.Shipping/R42.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict.Win;
+using Ict;
+using Sci.Data;
+
+namespace Sci.Production.Shipping
+{
+    public partial class R42 : Sci.Win.Tems.PrintForm
+    {
+        DataTable printData;
+        DateTime? date1, date2;
+        string mDivision, factory;
+        int category;
+        public R42(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            MyUtility.Tool.SetupCombox(comboBox1, 1, 1, "Bulk,Sample,Bulk+Sample");
+            DataTable mDivision, factory;
+            DBProxy.Current.Select(null, "select '' as ID union all select ID from MDivision", out mDivision);
+            MyUtility.Tool.SetupCombox(comboBox2, 1, mDivision);
+            DBProxy.Current.Select(null, "select '' as ID union all select distinct FtyGroup from Factory", out factory);
+            MyUtility.Tool.SetupCombox(comboBox3, 1, factory);
+            comboBox1.SelectedIndex = 2;
+            comboBox2.Text = Sci.Env.User.Keyword;
+            comboBox3.SelectedIndex = -1;
+        }
+
+        // 驗證輸入條件
+        protected override bool ValidateInput()
+        {
+            if (MyUtility.Check.Empty(dateRange1.Value1))
+            {
+                MyUtility.Msg.WarningBox("Date can't empty!!");
+                dateRange1.TextBox1.Focus();
+                return false;
+            }
+
+            if (comboBox1.SelectedIndex == -1)
+            {
+                MyUtility.Msg.WarningBox("Category can't empty!!");
+                comboBox1.Focus();
+                return false;
+            }
+
+            date1 = dateRange1.Value1;
+            date2 = dateRange1.Value2;
+            category = comboBox1.SelectedIndex;
+            mDivision = comboBox2.Text;
+            factory = comboBox3.Text;
+            return base.ValidateInput();
+        }
+
+        // 非同步取資料
+        protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            StringBuilder sqlCmd = new StringBuilder();
+            sqlCmd.Append(string.Format(@"select MDivisionID,FactoryID,ID,StyleID,SeasonID,BrandID,Category,SciDelivery,BuyerDelivery,OrderTypeID,Article,SizeCode,Qty,
+IIF(CustomSP = '','',SUBSTRING(CustomSP,0,len(CustomSP)-1)) as CustomSP
+from (
+select o.MDivisionID,o.FactoryID,o.ID,o.StyleID,o.SeasonID,o.BrandID,IIF(o.Category  = 'B','Bulk','Sample') as Category,
+o.SciDelivery,oq.BuyerDelivery,o.OrderTypeID,oqd.Article,oqd.SizeCode,oqd.Qty,oa.Seq as ASeq,os.Seq as SSeq,
+isnull((select CONCAT(CustomSP, ',') from (
+select distinct v.CustomSP+'('+v.VNContractID+')' as CustomSP from VNConsumption v,VNConsumption_Article va,VNConsumption_SizeCode vs
+where v.ID = va.ID and v.ID = vs.ID and v.StyleID = o.StyleID and v.BrandID = o.BrandID and va.Article = oqd.Article and vs.SizeCode = oqd.SizeCode) a
+ORDER BY CustomSP
+FOR XML PATH('')),'') as CustomSP
+from Order_QtyShip oq
+inner join Order_QtyShip_Detail oqd on oq.Id = oqd.Id and oq.Seq = oqd.Seq
+inner join Orders o on o.ID = oq.Id
+left join Order_Article oa on o.ID = oa.ID and oa.Article = oqd.Article
+left join Order_SizeCode os on o.ID = os.ID and os.SizeCode = oqd.SizeCode
+where oq.BuyerDelivery between '{0}' and '{1}'
+and {2}
+and o.LocalOrder = 0
+and o.Junk = 0
+and oqd.Qty > 0", Convert.ToDateTime(date1).ToString("d"), Convert.ToDateTime(date2).ToString("d"), category == 0 ? "o.Category = 'B'" : category == 1 ? "o.Category = 'S'" : "(o.Category = 'B' or o.Category = 'S')"));
+            if (!MyUtility.Check.Empty(mDivision))
+            {
+                sqlCmd.Append(string.Format(" and o.MDivisionID = '{0}'", mDivision));
+            }
+            if (!MyUtility.Check.Empty(factory))
+            {
+                sqlCmd.Append(string.Format(" and o.FactoryID = '{0}'", factory));
+            }
+            sqlCmd.Append(@") a
+order by a.ID,a.BuyerDelivery,a.ASeq,a.SSeq");
+
+            DualResult result = DBProxy.Current.Select(null, sqlCmd.ToString(), out printData);
+            if (!result)
+            {
+                DualResult failResult = new DualResult(false, "Query data fail\r\n" + result.ToString());
+                return failResult;
+            }
+
+            return Result.True;
+        }
+
+        // 產生Excel
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            // 顯示筆數於PrintForm上Count欄位
+            SetCount(printData.Rows.Count);
+
+            if (printData.Rows.Count <= 0)
+            {
+                MyUtility.Msg.WarningBox("Data not found!");
+                return false;
+            }
+
+            MyUtility.Msg.WaitWindows("Starting EXCEL...");
+            bool result = MyUtility.Excel.CopyToXls(printData, "", xltfile: "Shipping_R42_QtyBDownByColorwaySize.xltx", headerRow: 1);
+            if (!result) { MyUtility.Msg.WarningBox(result.ToString(), "Warning"); }
+            
+            MyUtility.Msg.WaitClear();
+            return true;
+        }
+    }
+}
