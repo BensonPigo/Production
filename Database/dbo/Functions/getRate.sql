7be00d7a0f70a54043a374330ae6f2364bdd235c
MIKE <mike@sportscity.com.tw>
2016-04-14 02:27:26 +0000

    修改DB Name = Finance to FinanceEN

--- Database/dbo/Functions/getRate.sql
+++ Database/dbo/Functions/getRate.sql
@@ -2,11 +2,11 @@
 -- =============================================
 -- Author:		Mike
 -- Create date: 2016/03/02
--- Description:	抓取匯率別 ex: FX 
+-- Description:	依匯率別抓取工廠財務系統的匯率
 -- =============================================
 CREATE FUNCTION [dbo].[getRate]
 (
-	@rateid varchar(2),
+	@rateid varchar(2), -- ex. System.ExchangeId
 	@from_currencyid varchar(3),
 	@to_currencyid varchar(3)
 )
@@ -21,29 +21,29 @@ BEGIN
 	DECLARE @rate decimal(18,8), @tmpRate decimal(18,8)
 	
 	-- 正向取值
-	select @rate = rate from finace.dbo.rate 
+	select @rate = rate from FinanceEN.dbo.rate 
 	where RateTypeID = @rateid and OriginalCurrency = @from_currencyid and ExchangeCurrency = @to_currencyid
 	
 	if @rate is null
 	begin
 		-- 反向取值
-		select @tmpRate = rate from finace.dbo.rate
+		select @tmpRate = rate from FinanceEN.dbo.rate
 		where RateTypeID = @rateid and OriginalCurrency = @to_currencyid and ExchangeCurrency = @from_currencyid
 		if @tmpRate is null
 			begin
 				--先轉台幣再轉欲取得的幣別
-				select @tmpRate = rate from finace.dbo.rate
+				select @tmpRate = rate from FinanceEN.dbo.rate
 				where RateTypeID = @rateid and OriginalCurrency = @from_currencyid and ExchangeCurrency = 'TWD'
 				set @rate = @tmpRate
-				select @tmpRate = rate from finace.dbo.rate
+				select @tmpRate = rate from FinanceEN.dbo.rate
 				where RateTypeID = @rateid and OriginalCurrency = 'TWD' and ExchangeCurrency = @to_currencyid
 				IF @tmpRate IS NULL OR @rate IS NULL
 				begin
 					--先轉台幣再轉欲取得的幣別(反向搜尋)
-					select @tmpRate = rate from finace.dbo.rate
+					select @tmpRate = rate from FinanceEN.dbo.rate
 					where RateTypeID = @rateid and OriginalCurrency = 'TWD' and ExchangeCurrency = @from_currencyid
 					set @rate = @tmpRate
-					select @tmpRate = rate from finace.dbo.rate
+					select @tmpRate = rate from FinanceEN.dbo.rate
 					where RateTypeID = @rateid and OriginalCurrency = @to_currencyid and ExchangeCurrency = 'TWD'
 					IF @tmpRate IS NULL OR @rate IS NULL
 						set @rate = 0.0
