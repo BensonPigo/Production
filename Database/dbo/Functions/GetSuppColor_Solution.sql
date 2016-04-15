543eb0b9c163094007be1260f8a8fb0317375da3
YINMEI <yinmei@sportscity.com.tw>
2016-04-15 05:47:46 +0000

    Add Table: Color_SuppColor Add Function: GetSuppColor_Solution

---
+++ Database/dbo/Functions/GetSuppColor_Solution.sql
@@ -0,0 +1,120 @@
+﻿
+Create Function [dbo].[GetSuppColor_Solution]
+(
+	  @ColorUkey	BigInt
+	 ,@SeasonID		VarChar(10)
+	 ,@SuppID		VarChar(6)
+	 ,@BrandID		VarChar(8)
+	 ,@ProgramID	VarChar(12)
+	 ,@StyleID		VarChar(15)
+	 ,@RefNo		VarChar(20)
+)
+Returns Varchar(30)
+As
+Begin
+	--Set NoCount On;
+	Declare @SuppColor VarChar(30)
+
+	--1. 依照Season + Style + Program + RefNo
+	Select Top 1 @SuppColor = SuppColor
+	  From dbo.Color_SuppColor
+	 Where ColorUkey = @ColorUkey
+	   And SuppID = @SuppID
+	   And SeasonID = @SeasonID
+	   And StyleID = @StyleID
+	   And ProgramID = @ProgramID
+	   And RefNo = @RefNo;
+			
+	--2. 依照Season + Style + RefNo，且排除Program有值的資料
+	If IsNull(@SuppColor, '') = ''
+	Begin
+		Select Top 1 @SuppColor = SuppColor
+		  From dbo.Color_SuppColor
+		 Where ColorUkey = @ColorUkey
+		   And SuppID = @SuppID
+		   And SeasonID = @SeasonID
+		   And StyleID = @StyleID
+		   And IsNull(ProgramID,'') = ''
+		   And RefNo = @RefNo;
+	End;
+	
+	--3. 依照Season + Program + RefNo，且排除Style有值的資料
+	If IsNull(@SuppColor, '') = ''
+	Begin
+		Select Top 1 @SuppColor = SuppColor
+		  From dbo.Color_SuppColor
+		 Where ColorUkey = @ColorUkey
+		   And SuppID = @SuppID
+		   And SeasonID = @SeasonID
+		   And IsNull(StyleID,'') = ''
+		   And ProgramID = @ProgramID
+		   And RefNo = @RefNo;
+	End;
+	
+	--4. 依照Season + RefNo，且排除Style & Program有值的資料
+	If IsNull(@SuppColor, '') = ''
+	Begin
+		Select Top 1 @SuppColor = SuppColor
+		  From dbo.Color_SuppColor
+		 Where ColorUkey = @ColorUkey
+		   And SuppID = @SuppID
+		   And SeasonID = @SeasonID
+		   And IsNull(StyleID,'') = ''
+		   And IsNull(ProgramID,'') = ''
+		   And RefNo = @RefNo;
+	End;
+	
+	--5. 依照Season + Style + Program，且排除RefNo有值的資料
+	If IsNull(@SuppColor, '') = ''
+	Begin
+		Select Top 1 @SuppColor = SuppColor
+		  From dbo.Color_SuppColor
+		 Where ColorUkey = @ColorUkey
+		   And SuppID = @SuppID
+		   And SeasonID = @SeasonID
+		   And StyleID = @StyleID
+		   And ProgramID = @ProgramID
+		   And IsNull(RefNo,'') = '';
+	End;
+	
+	--6. 依照Season + Style，且排除Program & RefNo有值的資料
+	If IsNull(@SuppColor, '') = ''
+	Begin
+		Select Top 1 @SuppColor = SuppColor
+		  From dbo.Color_SuppColor
+		 Where ColorUkey = @ColorUkey
+		   And SuppID = @SuppID
+		   And SeasonID = @SeasonID
+		   And StyleID = @StyleID
+		   And IsNull(ProgramID,'') = ''
+		   And IsNull(RefNo,'') = '';
+	End;
+	
+	--7. 依照Season + Program，且排除Style & RefNo有值的資料
+	If IsNull(@SuppColor, '') = ''
+	Begin
+		Select Top 1 @SuppColor = SuppColor
+		  From dbo.Color_SuppColor
+		 Where ColorUkey = @ColorUkey
+		   And SuppID = @SuppID
+		   And SeasonID = @SeasonID
+		   And IsNull(StyleID,'') = ''
+		   And ProgramID = @ProgramID
+		   And IsNull(RefNo,'') = '';
+	End;
+	
+	--7. 依照Season，且排除Style & RefNo & Program有值的資料
+	If IsNull(@SuppColor, '') = ''
+	Begin
+		Select Top 1 @SuppColor = SuppColor
+		  From dbo.Color_SuppColor
+		 Where ColorUkey = @ColorUkey
+		   And SuppID = @SuppID
+		   And SeasonID = @SeasonID
+		   And IsNull(StyleID,'') = ''
+		   And IsNull(ProgramID,'') = ''
+		   And IsNull(RefNo,'') = '';
+	End;
+	
+	Return @SuppColor;
+End
---
+++ Database/dbo/Tables/Color_SuppColor.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[Color_SuppColor] (
+    [ID]        VARCHAR (6)   CONSTRAINT [DF_Color_SuppColor_ID] DEFAULT ('') NULL,
+    [Ukey]      BIGINT        CONSTRAINT [DF_Color_SuppColor_Ukey] DEFAULT ((0)) NOT NULL,
+    [BrandId]   VARCHAR (8)   CONSTRAINT [DF_Color_SuppColor_BrandId] DEFAULT ('') NULL,
+    [ColorUkey] BIGINT        CONSTRAINT [DF_Color_SuppColor_ColorUkey] DEFAULT ((0)) NULL,
+    [SeasonID]  VARCHAR (10)  CONSTRAINT [DF_Color_SuppColor_SeasonID] DEFAULT ('') NULL,
+    [SuppID]    VARCHAR (6)   CONSTRAINT [DF_Color_SuppColor_SuppID] DEFAULT ('') NULL,
+    [SuppColor] VARCHAR (30)  CONSTRAINT [DF_Color_SuppColor_SuppColor] DEFAULT ('') NULL,
+    [ProgramID] VARCHAR (12)  CONSTRAINT [DF_Color_SuppColor_ProgramID] DEFAULT ('') NULL,
+    [StyleID]   VARCHAR (15)  CONSTRAINT [DF_Color_SuppColor_StyleID] DEFAULT ('') NULL,
+    [Refno]     VARCHAR (20)  CONSTRAINT [DF_Color_SuppColor_Refno] DEFAULT ('') NULL,
+    [Remark]    NVARCHAR (60) CONSTRAINT [DF_Color_SuppColor_Remark] DEFAULT ('') NULL,
+    [AddName]   VARCHAR (10)  CONSTRAINT [DF_Color_SuppColor_AddName] DEFAULT ('') NULL,
+    [AddDate]   DATETIME      NULL,
+    [EditName]  VARCHAR (10)  CONSTRAINT [DF_Color_SuppColor_EditName] DEFAULT ('') NULL,
+    [EditDate]  DATETIME      NULL,
+    CONSTRAINT [PK_Color_SuppColor] PRIMARY KEY CLUSTERED ([Ukey] ASC)
+);
+
