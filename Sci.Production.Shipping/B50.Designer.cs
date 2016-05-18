de4b129e787cf16b220b66fd1f4eb2607b175d82
YINMEI <yinmei@sportscity.com.tw>
2016-05-18 06:40:32 +0000

    Add B50

---
+++ Sci.Production.Shipping/B50.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Sci.Production.Shipping
+{
+    partial class B50
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.label3 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.textBox1 = new Sci.Win.UI.TextBox();
+            this.textBox2 = new Sci.Win.UI.TextBox();
+            this.comboBox1 = new Sci.Win.UI.ComboBox();
+            this.displayBox1 = new Sci.Win.UI.DisplayBox();
+            this.checkBox1 = new Sci.Win.UI.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).BeginInit();
+            this.detail.SuspendLayout();
+            this.detailcont.SuspendLayout();
+            this.detailbtm.SuspendLayout();
+            this.tabs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // detail
+            // 
+            this.detail.Size = new System.Drawing.Size(676, 299);
+            // 
+            // detailcont
+            // 
+            this.detailcont.Controls.Add(this.checkBox1);
+            this.detailcont.Controls.Add(this.displayBox1);
+            this.detailcont.Controls.Add(this.comboBox1);
+            this.detailcont.Controls.Add(this.textBox2);
+            this.detailcont.Controls.Add(this.textBox1);
+            this.detailcont.Controls.Add(this.label4);
+            this.detailcont.Controls.Add(this.label3);
+            this.detailcont.Controls.Add(this.label2);
+            this.detailcont.Controls.Add(this.label1);
+            this.detailcont.Size = new System.Drawing.Size(676, 261);
+            // 
+            // detailbtm
+            // 
+            this.detailbtm.Location = new System.Drawing.Point(0, 261);
+            this.detailbtm.Size = new System.Drawing.Size(676, 38);
+            // 
+            // tabs
+            // 
+            this.tabs.Size = new System.Drawing.Size(684, 328);
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(16, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Description of Goods";
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(16, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "HS Code";
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(16, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Category";
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(16, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "No.";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "GoodsDescription", true));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(152, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(400, 23);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "HSCode", true));
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox2.Location = new System.Drawing.Point(152, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(80, 23);
+            this.textBox2.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "Category", true));
+            this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IsSupportUnselect = true;
+            this.comboBox1.Location = new System.Drawing.Point(152, 94);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // displayBox1
+            // 
+            this.displayBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.displayBox1.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "NLCode", true));
+            this.displayBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.displayBox1.Location = new System.Drawing.Point(152, 130);
+            this.displayBox1.Name = "displayBox1";
+            this.displayBox1.Size = new System.Drawing.Size(60, 23);
+            this.displayBox1.TabIndex = 7;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "Junk", true));
+            this.checkBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.checkBox1.Location = new System.Drawing.Point(591, 21);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(57, 21);
+            this.checkBox1.TabIndex = 8;
+            this.checkBox1.Text = "Junk";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // B50
+            // 
+            this.ClientSize = new System.Drawing.Size(684, 361);
+            this.DefaultOrder = "NLCode";
+            this.IsSupportCopy = false;
+            this.IsSupportPrint = false;
+            this.Name = "B50";
+            this.Text = "B50. CDC Goods\'s Description Basic Data";
+            this.UniqueExpress = "ID";
+            this.WorkAlias = "KHGoodsHSCode";
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).EndInit();
+            this.detail.ResumeLayout(false);
+            this.detailcont.ResumeLayout(false);
+            this.detailcont.PerformLayout();
+            this.detailbtm.ResumeLayout(false);
+            this.detailbtm.PerformLayout();
+            this.tabs.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.CheckBox checkBox1;
+        private Win.UI.DisplayBox displayBox1;
+        private Win.UI.ComboBox comboBox1;
+        private Win.UI.TextBox textBox2;
+        private Win.UI.TextBox textBox1;
+        private Win.UI.Label label4;
+        private Win.UI.Label label3;
+        private Win.UI.Label label2;
+        private Win.UI.Label label1;
+    }
+}
---
+++ Sci.Production.Shipping/B50.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Shipping
+{
+    public partial class B50 : Sci.Win.Tems.Input1
+    {
+        public B50(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+
+            MyUtility.Tool.SetupCombox(comboBox1, 1, 1, "FABRIC,ACCESSORY,CHEMICAL,MACHINERY,OTHETS");
+        }
+
+        protected override void ClickNewAfter()
+        {
+            base.ClickNewAfter();
+            comboBox1.SelectedIndex = -1;
+        }
+
+        protected override void ClickEditAfter()
+        {
+            base.ClickEditAfter();
+            textBox1.ReadOnly = true;
+        }
+
+        protected override bool ClickSaveBefore()
+        {
+            //檢查必輸欄位
+            if (MyUtility.Check.Empty(textBox1.Text))
+            {
+                MyUtility.Msg.WarningBox("Description of Goods can't empty!!");
+                textBox1.Focus();
+                return false;
+            }
+
+            if (MyUtility.Check.Empty(textBox2.Text))
+            {
+                MyUtility.Msg.WarningBox("HS Code can't empty!!");
+                textBox2.Focus();
+                return false;
+            }
+
+            if (MyUtility.Check.Empty(comboBox1.Text))
+            {
+                MyUtility.Msg.WarningBox("Category can't empty!!");
+                comboBox1.Focus();
+                return false;
+            }
+
+            //填入NL Code
+            if (IsDetailInserting)
+            {
+                string nlcode = MyUtility.GetValue.Lookup("select CONVERT(int,isnull(max(NLCode),0)) from KHGoodsHSCode");
+                CurrentMaintain["NLCode"] = (MyUtility.Convert.GetInt(nlcode)+1).ToString("00000");
+            }
+
+            return base.ClickSaveBefore();
+        }
+    }
+}
