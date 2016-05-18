6151032865d39154c788034ed9df8c68129d32f6
YINMEI <yinmei@sportscity.com.tw>
2016-05-18 10:06:45 +0000

    Add B51

---
+++ Sci.Production.Shipping/B51.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Sci.Production.Shipping
+{
+    partial class B51
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.label3 = new Sci.Win.UI.Label();
+            this.dateBox1 = new Sci.Win.UI.DateBox();
+            this.dateBox2 = new Sci.Win.UI.DateBox();
+            this.textBox1 = new Sci.Win.UI.TextBox();
+            this.label4 = new Sci.Win.UI.Label();
+            this.textBox2 = new Sci.Win.UI.TextBox();
+            this.button1 = new Sci.Win.UI.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.detailgridbs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.detailgrid2bs)).BeginInit();
+            this.masterpanel.SuspendLayout();
+            this.detailpanel.SuspendLayout();
+            this.detail2.SuspendLayout();
+            this.detailpanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).BeginInit();
+            this.detail.SuspendLayout();
+            this.detailcont.SuspendLayout();
+            this.detailbtm.SuspendLayout();
+            this.tabs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // masterpanel
+            // 
+            this.masterpanel.Controls.Add(this.button1);
+            this.masterpanel.Controls.Add(this.textBox2);
+            this.masterpanel.Controls.Add(this.label4);
+            this.masterpanel.Controls.Add(this.textBox1);
+            this.masterpanel.Controls.Add(this.dateBox2);
+            this.masterpanel.Controls.Add(this.dateBox1);
+            this.masterpanel.Controls.Add(this.label3);
+            this.masterpanel.Controls.Add(this.label2);
+            this.masterpanel.Controls.Add(this.label1);
+            this.masterpanel.Size = new System.Drawing.Size(762, 96);
+            this.masterpanel.Controls.SetChildIndex(this.label1, 0);
+            this.masterpanel.Controls.SetChildIndex(this.label2, 0);
+            this.masterpanel.Controls.SetChildIndex(this.label3, 0);
+            this.masterpanel.Controls.SetChildIndex(this.gridicon, 0);
+            this.masterpanel.Controls.SetChildIndex(this.dateBox1, 0);
+            this.masterpanel.Controls.SetChildIndex(this.dateBox2, 0);
+            this.masterpanel.Controls.SetChildIndex(this.textBox1, 0);
+            this.masterpanel.Controls.SetChildIndex(this.label4, 0);
+            this.masterpanel.Controls.SetChildIndex(this.textBox2, 0);
+            this.masterpanel.Controls.SetChildIndex(this.button1, 0);
+            // 
+            // detailpanel
+            // 
+            this.detailpanel.Location = new System.Drawing.Point(0, 96);
+            this.detailpanel.Size = new System.Drawing.Size(762, 253);
+            // 
+            // gridicon
+            // 
+            this.gridicon.Location = new System.Drawing.Point(651, 61);
+            // 
+            // detailgridcont
+            // 
+            this.detailgridcont.Size = new System.Drawing.Size(762, 253);
+            // 
+            // detail2
+            // 
+            this.detail2.Size = new System.Drawing.Size(892, 387);
+            // 
+            // detailgridcont2
+            // 
+            this.detailgridcont2.Size = new System.Drawing.Size(886, 341);
+            // 
+            // detailpanel2
+            // 
+            this.detailpanel2.Size = new System.Drawing.Size(886, 40);
+            // 
+            // detail
+            // 
+            this.detail.Size = new System.Drawing.Size(762, 387);
+            // 
+            // detailcont
+            // 
+            this.detailcont.Size = new System.Drawing.Size(762, 349);
+            // 
+            // detailbtm
+            // 
+            this.detailbtm.Location = new System.Drawing.Point(0, 349);
+            this.detailbtm.Size = new System.Drawing.Size(762, 38);
+            // 
+            // browse
+            // 
+            this.browse.Size = new System.Drawing.Size(892, 387);
+            // 
+            // tabs
+            // 
+            this.tabs.Size = new System.Drawing.Size(770, 416);
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(14, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Start Date";
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(14, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "End Date";
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(14, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "CDC No.";
+            // 
+            // dateBox1
+            // 
+            this.dateBox1.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "StartDate", true));
+            this.dateBox1.Location = new System.Drawing.Point(87, 10);
+            this.dateBox1.Name = "dateBox1";
+            this.dateBox1.Size = new System.Drawing.Size(100, 23);
+            this.dateBox1.TabIndex = 4;
+            // 
+            // dateBox2
+            // 
+            this.dateBox2.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "EndDate", true));
+            this.dateBox2.Location = new System.Drawing.Point(87, 37);
+            this.dateBox2.Name = "dateBox2";
+            this.dateBox2.Size = new System.Drawing.Size(100, 23);
+            this.dateBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "ID", true));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(87, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(130, 23);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(352, 10);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Factory";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "FactoryID", true));
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox2.Location = new System.Drawing.Point(410, 10);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(70, 23);
+            this.textBox2.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.EditMode = Sci.Win.UI.AdvEditModes.EnableOnEdit;
+            this.button1.Location = new System.Drawing.Point(633, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 30);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Import Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // B51
+            // 
+            this.ApvChkValue = "New";
+            this.ClientSize = new System.Drawing.Size(770, 449);
+            this.GridAlias = "KHContract_Detail";
+            this.GridEdit = false;
+            this.GridNew = 0;
+            this.GridUniqueKey = "ID,NLCode";
+            this.IsGridIconVisible = false;
+            this.IsSupportConfirm = true;
+            this.IsSupportCopy = false;
+            this.IsSupportPrint = false;
+            this.IsSupportUnconfirm = true;
+            this.KeyField1 = "ID";
+            this.Name = "B51";
+            this.Text = "B51. CDC";
+            this.UnApvChkValue = "Confirmed";
+            this.UniqueExpress = "ID";
+            this.WorkAlias = "KHContract";
+            this.Controls.SetChildIndex(this.tabs, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.detailgridbs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.detailgrid2bs)).EndInit();
+            this.masterpanel.ResumeLayout(false);
+            this.masterpanel.PerformLayout();
+            this.detailpanel.ResumeLayout(false);
+            this.detail2.ResumeLayout(false);
+            this.detailpanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).EndInit();
+            this.detail.ResumeLayout(false);
+            this.detailcont.ResumeLayout(false);
+            this.detailbtm.ResumeLayout(false);
+            this.detailbtm.PerformLayout();
+            this.tabs.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.TextBox textBox2;
+        private Win.UI.Label label4;
+        private Win.UI.TextBox textBox1;
+        private Win.UI.DateBox dateBox2;
+        private Win.UI.DateBox dateBox1;
+        private Win.UI.Label label3;
+        private Win.UI.Label label2;
+        private Win.UI.Label label1;
+        private Win.UI.Button button1;
+    }
+}
---
+++ Sci.Production.Shipping/B51.cs
@@ -0,0 +1,263 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict.Win;
+using Ict;
+using Sci.Data;
+
+namespace Sci.Production.Shipping
+{
+    public partial class B51 : Sci.Win.Tems.Input6
+    {
+        DataTable WrongUnitID;
+        public B51(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+        }
+
+        protected override DualResult OnDetailSelectCommandPrepare(PrepareDetailSelectCommandEventArgs e)
+        {
+            string masterID = (e.Master == null) ? "" : MyUtility.Convert.GetString(e.Master["ID"]);
+            this.DetailSelectCommand = string.Format(@"select kd.*,kd.Qty*kd.Price as TotalPrice,kh.GoodsDescription,kh.Category
+from KHContract_Detail kd
+left join KHGoodsHSCode kh on kd.NLCode = kh.NLCode
+where kd.ID = '{0}' order by CONVERT(INT,SUBSTRING(Seq,PATINDEX('%-%',Seq)+1,len(Seq)))", masterID);
+            return base.OnDetailSelectCommandPrepare(e);
+        }
+
+        protected override void OnDetailGridSetup()
+        {
+            base.OnDetailGridSetup();
+            Helper.Controls.Grid.Generator(this.detailgrid)
+                .Text("NLCode", header: "No.", width: Widths.AnsiChars(5), iseditingreadonly: true)
+                .Text("Seq", header: "Seq", width: Widths.AnsiChars(5), iseditingreadonly: true)
+                .Text("GoodsDescription", header: "Description of Goods", width: Widths.AnsiChars(50), iseditingreadonly: true)
+                .Text("Category", header: "Category", width: Widths.AnsiChars(10), iseditingreadonly: true)
+                .Text("UnitID", header: "Unit", width: Widths.AnsiChars(8), iseditingreadonly: true)
+                .Numeric("Qty", header: "Waste", decimal_places: 2, iseditingreadonly: true)
+                .Numeric("Price", header: "Unit Price", decimal_places: 2, iseditingreadonly: true)
+                .Numeric("TotalPrice", header: "Total Price", decimal_places: 2, iseditingreadonly: true);
+        }
+
+        protected override void ClickNewAfter()
+        {
+            base.ClickNewAfter();
+            CurrentMaintain["Status"] = "New";
+            CurrentMaintain["FactoryID"] = Sci.Env.User.Factory;
+        }
+
+        protected override bool ClickEditBefore()
+        {
+            if (MyUtility.Convert.GetString(CurrentMaintain["Status"]).ToUpper() == "CONFIRMED")
+            {
+                MyUtility.Msg.WarningBox("This CDC already confirmed, can't edit!!");
+                return false;
+            }
+            return base.ClickEditBefore();
+        }
+
+        protected override bool ClickDeleteBefore()
+        {
+            if (MyUtility.Convert.GetString(CurrentMaintain["Status"]).ToUpper() == "CONFIRMED")
+            {
+                MyUtility.Msg.WarningBox("This CDC already confirmed, can't delete!!");
+                return false;
+            }
+            return base.ClickDeleteBefore();
+        }
+
+        protected override bool ClickSaveBefore()
+        {
+            #region 檢查必輸欄位
+            if (MyUtility.Check.Empty(CurrentMaintain["StartDate"]))
+            {
+                MyUtility.Msg.WarningBox("Start Date can't empty!!");
+                dateBox1.Focus();
+                return false;
+            }
+            if (MyUtility.Check.Empty(CurrentMaintain["EndDate"]))
+            {
+                MyUtility.Msg.WarningBox("End Date can't empty!!");
+                dateBox2.Focus();
+                return false;
+            }
+            if (MyUtility.Check.Empty(CurrentMaintain["ID"]))
+            {
+                MyUtility.Msg.WarningBox("CDC No. can't empty!!");
+                textBox1.Focus();
+                return false;
+            }
+            if (MyUtility.Check.Empty(CurrentMaintain["FactoryID"]))
+            {
+                MyUtility.Msg.WarningBox("Factory can't empty!!");
+                textBox2.Focus();
+                return false;
+            }
+            
+            #endregion
+
+            #region 檢查日期正確性
+            //End Date：輸入日期一定不能小於Start Date
+            if (dateBox2.Value < dateBox1.Value)
+            {
+                MyUtility.Msg.WarningBox("Pls double check the end date!!");
+                dateBox2.Focus();
+                return false;
+            }
+            #endregion
+
+            #region 如果有做Import Data，檢查匯入的的Unit是否正確
+            if (WrongUnitID != null && WrongUnitID.Rows.Count > 0)
+            {
+                StringBuilder wrongunit = new StringBuilder();
+                foreach (DataRow dr in WrongUnitID.Rows)
+                {
+                    if (wrongunit.ToString().IndexOf(MyUtility.Convert.GetString(dr["UnitID"])) <= 0)
+                    {
+                        wrongunit.Append(string.Format("Unit: {0}\r\n", MyUtility.Convert.GetString(dr["UnitID"])));
+                    }
+                }
+                MyUtility.Msg.WarningBox(string.Format("Below data is 'Unit' not correct.\r\n{0}", wrongunit.ToString()));
+                return false;
+            }
+            #endregion
+
+            return base.ClickSaveBefore();
+        }
+
+        //Confirm
+        protected override void ClickConfirm()
+        {
+            base.ClickConfirm();
+            string updateCmds = string.Format("update KHContract set EditDate = GETDATE(), EditName = '{0}', Status = 'Confirmed' where ID = '{1}'",
+                Sci.Env.User.UserID, MyUtility.Convert.GetString(CurrentMaintain["ID"]));
+
+            DualResult result = DBProxy.Current.Execute(null, updateCmds);
+            if (!result)
+            {
+                MyUtility.Msg.WarningBox("Confirm fail!!\r\n" + result.ToString());
+                return;
+            }
+            RenewData();
+            OnDetailEntered();
+            EnsureToolbarExt();
+        }
+
+        //Unconfirm
+        protected override void ClickUnconfirm()
+        {
+            base.ClickUnconfirm();
+
+            string updateCmds = string.Format("update KHContract set EditDate = GETDATE(), EditName = '{0}', Status = 'New' where ID = '{1}'",
+                            Sci.Env.User.UserID, MyUtility.Convert.GetString(CurrentMaintain["ID"]));
+
+            DualResult result = DBProxy.Current.Execute(null, updateCmds);
+            if (!result)
+            {
+                MyUtility.Msg.WarningBox("Unconfirm fail!!\r\n" + result.ToString());
+                return;
+            }
+
+            RenewData();
+            OnDetailEntered();
+            EnsureToolbarExt();
+        }
+
+        //Import Data
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string excelFile = MyUtility.File.GetFile("Excel files (*.xlsx)|*.xlsx");
+            if (MyUtility.Check.Empty(excelFile))
+            {
+                return;
+            }
+
+            Microsoft.Office.Interop.Excel.Application excel = MyUtility.Excel.ConnectExcel(excelFile);
+            if (excel == null) return;
+
+            DataTable ExcelDataTable, UpdateData;
+            string sqlCmd = "select *,0.0 as TotalPrice,'' as GoodsDescription,'' as Category from KHContract_Detail where 1 = 0";
+            DualResult result = DBProxy.Current.Select(null, sqlCmd, out ExcelDataTable);
+
+            sqlCmd = "select UnitID from KHContract_Detail where 1 = 0";
+            result = DBProxy.Current.Select(null, sqlCmd, out WrongUnitID);
+
+            UpdateData = ((DataTable)gridbs.DataSource).Clone();
+
+            MyUtility.Msg.WaitWindows("Starting EXCEL...");
+            excel.Visible = false;
+            Microsoft.Office.Interop.Excel.Worksheet worksheet = excel.ActiveWorkbook.Worksheets[1];
+            int intRowsCount = worksheet.UsedRange.Rows.Count;
+            int intColumnsCount = worksheet.UsedRange.Columns.Count;
+            int intRowsStart = 2;
+            int intRowsRead = intRowsStart - 1;
+
+            Microsoft.Office.Interop.Excel.Range range;
+            object[,] objCellArray;
+            DataRow seekData;
+            StringBuilder emptyNLCode = new StringBuilder();
+
+
+            while (intRowsRead < intRowsCount)
+            {
+                intRowsRead++;
+
+                range = worksheet.Range[String.Format("A{0}:F{0}", intRowsRead)];
+                objCellArray = range.Value;
+                if (MyUtility.Check.Seek(string.Format("select * from KHGoodsHSCode where GoodsDescription = '{0}'", MyUtility.Convert.GetString(MyUtility.Excel.GetExcelCellValue(objCellArray[1, 3], "C")).ToUpper()), out seekData))
+                {
+                    DataRow newRow = ExcelDataTable.NewRow();
+                    newRow["Seq"] = MyUtility.Convert.GetString(MyUtility.Excel.GetExcelCellValue(objCellArray[1, 1], "C")) + "-" + MyUtility.Convert.GetString(MyUtility.Excel.GetExcelCellValue(objCellArray[1, 2], "C"));
+                    newRow["GoodsDescription"] = MyUtility.Excel.GetExcelCellValue(objCellArray[1, 3], "C");
+                    newRow["NLCode"] = seekData["NLCode"];
+                    newRow["Category"] = seekData["Category"];
+                    newRow["UnitID"] = MyUtility.Excel.GetExcelCellValue(objCellArray[1, 4], "C");
+                    newRow["Qty"] = MyUtility.Excel.GetExcelCellValue(objCellArray[1, 5], "N");
+                    newRow["Price"] = MyUtility.Excel.GetExcelCellValue(objCellArray[1, 6], "N");
+                    newRow["TotalPrice"] = MyUtility.Convert.GetDecimal(MyUtility.Excel.GetExcelCellValue(objCellArray[1, 5], "N")) * MyUtility.Convert.GetDecimal(MyUtility.Excel.GetExcelCellValue(objCellArray[1, 6], "N"));
+                    if (!MyUtility.Check.Seek(MyUtility.Convert.GetString(MyUtility.Excel.GetExcelCellValue(objCellArray[1, 4], "C")), "Unit", "ID"))
+                    {
+                        DataRow unitRow = WrongUnitID.NewRow();
+                        unitRow["UnitID"] = MyUtility.Excel.GetExcelCellValue(objCellArray[1, 4], "C");
+                        WrongUnitID.Rows.Add(unitRow);
+                    }
+                    ExcelDataTable.Rows.Add(newRow);
+                }
+                else
+                {
+                    emptyNLCode.Append(string.Format("{0} {1}\r\n", MyUtility.Convert.GetString(MyUtility.Excel.GetExcelCellValue(objCellArray[1, 1], "C")) + "-" + MyUtility.Convert.GetString(MyUtility.Excel.GetExcelCellValue(objCellArray[1, 2], "C")), MyUtility.Convert.GetString(MyUtility.Excel.GetExcelCellValue(objCellArray[1, 3], "C"))));
+                }
+            }
+
+            excel.Workbooks.Close();
+            excel.Quit();
+            excel = null;
+
+            //刪除表身Grid資料
+            foreach (DataRow dr in DetailDatas)
+            {
+                dr.Delete();
+            }
+
+            //將Excel寫入表身Grid
+            foreach (DataRow dr in ExcelDataTable.Rows)
+            {
+                ((DataTable)detailgridbs.DataSource).ImportRow(dr);
+            }
+            MyUtility.Msg.WaitClear();
+            if (MyUtility.Check.Empty(emptyNLCode.ToString()))
+            {
+                MyUtility.Msg.InfoBox("Import Complete!!");
+            }
+            else
+            {
+                MyUtility.Msg.WarningBox(string.Format("Below data isn't created in 'B50. CDC Goods's Description Basic Data:\r\n{0}'", emptyNLCode.ToString()));
+            }
+        }
+    }
+}
