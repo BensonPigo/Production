4e06c48833839b460a828a5103070b04582f557c
MIKE <mike@sportscity.com.tw>
2016-05-19 05:52:25 +0000

    add txtAccountNo

---
+++ Sci.Production.Class/txtAccountNo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sci.Data;
+using Sci.Win.UI;
+
+namespace Sci.Production.Class
+{
+    public partial class txtAccountNo : UserControl
+    {
+        public txtAccountNo()
+        {
+            InitializeComponent();
+        }
+
+        public Sci.Win.UI.TextBox TextBox1
+        {
+            get { return this.textBox1; }
+        }
+
+        public Sci.Win.UI.DisplayBox DisplayBox1
+        {
+            get { return this.displayBox1; }
+        }
+
+        [Bindable(true)]
+        public string TextBox1Binding
+        {
+            set { this.textBox1.Text = value; }
+            get { return textBox1.Text; }
+        }
+
+        [Bindable(true)]
+        public string DisplayBox1Binding
+        {
+            set { this.displayBox1.Text = value; }
+            get { return this.displayBox1.Text; }
+        }
+
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+            base.OnValidating(e);
+            string textValue = this.textBox1.Text;
+            if (!string.IsNullOrWhiteSpace(textValue) && textValue != this.textBox1.OldValue)
+            {
+                if (!MyUtility.Check.Seek(string.Format(@"select name from [FinanceEN].[dbo].[AccountNo] where id = '{0}'", textValue)))
+                {
+                    MyUtility.Msg.WarningBox(string.Format("< Account No: {0} > not found!!!", textValue));
+                    this.textBox1.Text = "";
+                    e.Cancel = true;
+                    return;
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            this.displayBox1.Text = MyUtility.GetValue.Lookup(string.Format(@"select name from [FinanceEN].[dbo].[AccountNo] where id = '{0}'", this.textBox1.Text));
+        }
+
+        private void textBox1_PopUp(object sender, Win.UI.TextBoxPopUpEventArgs e)
+        {
+            Sci.Win.Forms.Base myForm = (Sci.Win.Forms.Base)this.FindForm();
+            if (myForm.EditMode == false || textBox1.ReadOnly == true) return;
+            Sci.Win.Tools.SelectItem item = new Sci.Win.Tools.SelectItem("select ID,name from [FinanceEN].[dbo].[AccountNo] order by ID", "8,30", this.textBox1.Text);
+            DialogResult returnResult = item.ShowDialog();
+            if (returnResult == DialogResult.Cancel) { return; }
+            this.textBox1.Text = item.GetSelectedString();
+        }
+    }
+}
