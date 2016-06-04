993cf14d89649a28b65e8943d024c3fd692c71d2
YINMEI <yinmei@sportscity.com.tw>
2016-06-04 08:14:00 +0000

    Add P07

---
+++ Sci.Production.PPIC/P07.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Sci.Production.PPIC
+{
+    partial class P07
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new Sci.Win.UI.Label();
+            this.button1 = new Sci.Win.UI.Button();
+            this.button2 = new Sci.Win.UI.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold);
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(16, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Download Data";
+            this.label1.TextStyle.BorderColor = System.Drawing.Color.Blue;
+            this.label1.TextStyle.Color = System.Drawing.Color.Blue;
+            this.label1.TextStyle.ExtBorderColor = System.Drawing.Color.Blue;
+            this.label1.TextStyle.GradientColor = System.Drawing.Color.Blue;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(177, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 30);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // P07
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 137);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "P07";
+            this.Text = "P07. Download APS Data";
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.button1, 0);
+            this.Controls.SetChildIndex(this.button2, 0);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Win.UI.Label label1;
+        private Win.UI.Button button1;
+        private Win.UI.Button button2;
+    }
+}
---
+++ Sci.Production.PPIC/P07.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict;
+using Ict.Win;
+using Sci.Data;
+
+namespace Sci.Production.PPIC
+{
+    public partial class P07 : Sci.Win.Tems.QueryForm
+    {
+        string useAPS;
+        public P07(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            string sqlCommand = "select UseAPS from factory where ID = '" + Sci.Env.User.Factory + "'";
+            useAPS = MyUtility.GetValue.Lookup(sqlCommand, null);
+            if (useAPS.ToUpper() == "FALSE")
+            {
+                MyUtility.Msg.WarningBox("Not yet use the APS, so can't use this function!!");
+                button1.Enabled = false;
+            }
+        }
+
+        protected override void OnFormLoaded()
+        {
+            base.OnFormLoaded();
+            if (useAPS.ToUpper() == "FALSE")
+            {
+                this.Close();
+            }
+        }
+
+        //Download
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataRow dr;
+            MyUtility.Check.Seek(string.Format("select SQLServerName,APSDatabaseName from MDivision where ID = '{0}'", Sci.Env.User.Keyword), out dr);
+            if (MyUtility.Check.Empty(dr["SQLServerName"]) || MyUtility.Check.Empty(dr["APSDatabaseName"]))
+            {
+                MyUtility.Msg.WarningBox("Still not yet set APS Server data, Please contact Taipei MIS. Thank you.");
+                return;
+            }
+            string sqlCmd = string.Format("exec dbo.usp_APSDataDownLoad '{0}','{1}','{2}','{3}'", MyUtility.Convert.GetString(dr["SQLServerName"]), MyUtility.Convert.GetString(dr["APSDatabaseName"]), Sci.Env.User.Factory, Sci.Env.User.UserID);
+            DualResult Result = DBProxy.Current.Execute(null, sqlCmd);
+            if (!Result)
+            {
+                ShowErr(sqlCmd, Result);
+                return;
+            }
+        }
+
+        //Close
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
