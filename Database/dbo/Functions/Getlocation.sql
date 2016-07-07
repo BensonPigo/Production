840574abc4463e6b4863976895826598eed63828
MIKE <mike@sportscity.com.tw>
2016-07-07 02:21:00 +0000

    add getlocation function

---
+++ Database/dbo/Functions/Getlocation.sql
@@ -0,0 +1,26 @@
+﻿
+-- =============================================
+-- Author:		Mike
+-- Create date: 2016/07/07
+-- Description:	傳入FtyInventory.Ukey取得存放的儲位
+--				依逗點分隔。
+-- =============================================
+CREATE FUNCTION [dbo].[Getlocation]
+(
+	@ukey bigint
+)
+RETURNS varchar(300)
+AS
+BEGIN
+	-- Declare the return variable here
+	DECLARE @locationStr as varchar(300);
+
+	-- Add the T-SQL statements to compute the return value here
+	select  @locationStr = (select MtlLocationID+',' 
+	from (select d.MtlLocationID from dbo.FtyInventory_Detail d where ukey = @ukey) t
+	for xml path(''))
+
+	-- Return the result of the function
+	RETURN @locationStr
+
+END
