f38a58978d5897ee82db9104ff5c8ca5af35c2f2
ELENA <elena@sportscity.com.tw>
2016-07-11 08:55:32 +0000

    add file for P12 Print

--- Sci.Production.Warehouse/P12.cs
+++ Sci.Production.Warehouse/P12.cs
@@ -2,12 +2,15 @@
 using Ict.Win;
 using Sci.Data;
 using Sci.Production.PublicPrg;
+using Sci.Win;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Transactions;
 using System.Windows.Forms;
@@ -655,6 +658,113 @@ Where a.id = '{0}'", masterID);
             { MyUtility.Msg.WarningBox("Data was not found!!"); }
             else
             { detailgridbs.Position = index; }
+        }
+
+
+        protected override bool ClickPrint()
+        {
+            DataRow row = this.CurrentDataRow;
+            string id = row["ID"].ToString();
+            string Remark = row["Remark"].ToString();
+            string issuedate = ((DateTime)MyUtility.Convert.GetDate(row["issuedate"])).ToShortDateString();
+
+            List<SqlParameter> pars = new List<SqlParameter>();
+            pars.Add(new SqlParameter("@ID", id));
+            DataTable dt;
+            DualResult result = DBProxy.Current.Select("",
+            @"select    
+            b.name 
+            from dbo.Issue  a 
+            inner join dbo.mdivision  b 
+            on b.id = a.mdivisionid
+            where b.id = a.mdivisionid
+            and a.id = @ID", pars, out dt);
+            if (!result) { this.ShowErr(result); }
+            string RptTitle = dt.Rows[0]["name"].ToString();
+            ReportDefinition report = new ReportDefinition();
+            report.ReportParameters.Add(new Microsoft.Reporting.WinForms.ReportParameter("RptTitle", RptTitle));
+            report.ReportParameters.Add(new Microsoft.Reporting.WinForms.ReportParameter("ID", id));
+            report.ReportParameters.Add(new Microsoft.Reporting.WinForms.ReportParameter("Remark", Remark));
+            report.ReportParameters.Add(new Microsoft.Reporting.WinForms.ReportParameter("issuedate", issuedate));
+
+            pars = new List<SqlParameter>();
+            pars.Add(new SqlParameter("@ID", id));
+            DataTable dtt;
+            string POID, SEQ, DESC, UNIT, QTY, BULKLocation;
+            result = DBProxy.Current.Select("",
+            @"select a.POID,a.Seq1+'-'+a.seq2 as SEQ#,dbo.getMtlDesc(a.poid,a.seq1,a.Seq2,2,0) [desc1],a.Qty,dbo.Getlocation(c.ukey)[BULK Location]
+            from dbo.Issue_Detail a
+            inner join dbo.PO_Supp_Detail b
+            on 
+            b.id=a.POID and b.SEQ1=a.Seq1 and b.SEQ2=a.seq2
+            inner join dbo.FtyInventory c
+            on
+            c.poid=a.id
+            where a.id= @ID", pars, out dtt);
+            if (!result) { this.ShowErr(result); }
+            if (dtt.Rows.Count == 0)
+            {
+                POID = " ";
+                SEQ = " ";
+                DESC = " ";
+                UNIT = " ";
+                QTY = " ";
+                BULKLocation = " ";
+            }
+            else
+            {
+                POID = dtt.Rows[0]["POID"].ToString();
+                SEQ = dtt.Rows[0]["SEQ"].ToString();
+                DESC = dtt.Rows[0]["DESC"].ToString();
+                UNIT = dtt.Rows[0]["UNIT"].ToString();
+                QTY = dtt.Rows[0]["QTY"].ToString();
+                BULKLocation = dtt.Rows[0]["BULKLocation"].ToString();
+            }
+            //ReportDefinition reportt = new ReportDefinition();
+            //report.ReportParameters.Add(new Microsoft.Reporting.WinForms.ReportParameter("POID", POID));
+            //report.ReportParameters.Add(new Microsoft.Reporting.WinForms.ReportParameter("SEQ", SEQ));
+            //report.ReportParameters.Add(new Microsoft.Reporting.WinForms.ReportParameter("DESC", DESC));
+            //report.ReportParameters.Add(new Microsoft.Reporting.WinForms.ReportParameter("UNIT", UNIT));
+            //report.ReportParameters.Add(new Microsoft.Reporting.WinForms.ReportParameter("QTY", QTY));
+            //report.ReportParameters.Add(new Microsoft.Reporting.WinForms.ReportParameter("BULKLocation", BULKLocation));
+
+
+            // 傳 list 資料            
+            List<P12_PrintData> data = dtt.AsEnumerable()
+                .Select(row1 => new P12_PrintData()
+                {
+                    POID = row1["POID"].ToString(),
+                    SEQ = row1["SEQ"].ToString(),
+                    DESC = row1["DESC"].ToString(),
+                    UNIT = row1["UNIT"].ToString(),
+                    QTY = row1["QTY"].ToString(),
+                    BULKLocation = row1["BULKLocation"].ToString()
+                }).ToList();
+
+            report.ReportDataSource = data;
+
+            // 指定是哪個 RDLC
+            //DualResult result;
+            Type ReportResourceNamespace = typeof(P12_PrintData);
+            Assembly ReportResourceAssembly = ReportResourceNamespace.Assembly;
+            string ReportResourceName = "P12_Print.rdlc";
+
+            IReportResource reportresource;
+            if (!(result = ReportResources.ByEmbeddedResource(ReportResourceAssembly, ReportResourceNamespace, ReportResourceName, out reportresource)))
+            {
+                //this.ShowException(result);
+                return false;
+            }
+
+            report.ReportResource = reportresource;
+
+            // 開啟 report view
+            var frm = new Sci.Win.Subs.ReportView(report);
+            frm.MdiParent = MdiParent;
+            frm.Show();
+  
+            return true;
+
         }
     }
 }
