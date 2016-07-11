9ee5546fdfed559021e92b6f9076f1e7b663b6c9
WENNY <wenny@sportscity.com.tw>
2016-07-11 09:01:29 +0000

    add file for P13 Print

---
+++ Sci.Production.Warehouse/P13_PrintData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Sci.Production.Warehouse
+{
+    public class P13_PrintData 
+    {
+        public string ID { get; set; }
+    }
+}
