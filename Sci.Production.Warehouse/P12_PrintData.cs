e466a877d325475fd0bc91e9384c8a457f9d12fa
ELENA <elena@sportscity.com.tw>
2016-07-11 08:52:53 +0000

    ADD FILE for P12 print

---
+++ Sci.Production.Warehouse/P12_PrintData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Sci.Production.Warehouse
+{
+    public class P12_PrintData
+    {
+        public string POID { get; set; }
+        public string SEQ { get; set; }
+        public string DESC { get; set; }
+        public string UNIT { get; set; }
+        public string QTY { get; set; }
+        public string BULKLocation { get; set; }
+    }
+}
