c29ac34c588226480a812fcd92af11742a9e5f9a
ELENA <elena@sportscity.com.tw>
2016-07-11 02:55:54 +0000

    ADD P10 PRINT DATA

---
+++ Sci.Production.Warehouse/P10_PrintData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sci.Production.Warehouse
+{
+    public class P10_PrintData
+    {
+        public string Poid { get; set; }
+        public string Seq { get; set; }
+        public string Desc { get; set; }
+        public string Location { get; set; }
+        public string Unit { get; set; }
+        public string Roll { get; set; }
+        public string Dyelot { get; set; }
+        public string Qty { get; set; }
+
+    }
+}
