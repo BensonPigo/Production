1eefe310aaa6c7fbae970208532fa48c1d9430f4
WENNY <wenny@sportscity.com.tw>
2016-07-12 08:13:51 +0000

    update FOR P13 Print

--- Sci.Production.Warehouse/P13_PrintData.cs
+++ Sci.Production.Warehouse/P13_PrintData.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Sci.Production.Warehouse
 {
-    public class P13_PrintData 
+    class P13_PrintData
     {
-        public string ID { get; set; }
+        public string POID { get; set; }
+        public string SEQ { get; set; }
+        public string DESC { get; set; }
+        public string Location { get; set; }
+        public string StockUnit { get; set; }
+        public string Roll { get; set; }
+        public string DYELOT { get; set; }
+        public string QTY { get; set; }
     }
 }
