256a6f2d233bb0cf61e6cecf76b557c4345b3816
WILLY <willy@sportscity.com.tw>
2016-07-12 08:27:28 +0000

    New Create 20160712

---
+++ Sci.Production.Quality/P03_Heat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Quality
+{
+    public partial class P03_Heat : Sci.Win.Subs.Input4
+    {
+       private string loginID = Sci.Env.User.UserID;
+        private DataRow maindr;
+
+        public P03_Heat(bool CanEdit, string airID, DataRow mainDr)
+        {
+            InitializeComponent();
+        }
+    }
+}
