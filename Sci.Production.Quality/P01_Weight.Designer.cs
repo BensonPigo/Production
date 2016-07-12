e47485d2fb08dee64229bc080cd0e477b6cf0d6a
WILLY <willy@sportscity.com.tw>
2016-07-12 08:30:24 +0000

    update 20160712 create new toExcel function

--- Sci.Production.Quality/P01_Weight.Designer.cs
+++ Sci.Production.Quality/P01_Weight.Designer.cs
@@ -62,6 +62,7 @@
             this.txtuser1 = new Sci.Production.Class.txtuser();
             this.txtsupplier1 = new Sci.Production.Class.txtsupplier();
             this.result_box = new Sci.Win.UI.DisplayBox();
+            this.textID = new Sci.Win.UI.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.gridbs)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.mtbs)).BeginInit();
             this.btmcont.SuspendLayout();
@@ -71,7 +72,7 @@
             // 
             this.btmcont.Controls.Add(this.button3);
             this.btmcont.Location = new System.Drawing.Point(0, 693);
-            this.btmcont.Size = new System.Drawing.Size(896, 40);
+            this.btmcont.Size = new System.Drawing.Size(1208, 40);
             this.btmcont.Controls.SetChildIndex(this.append, 0);
             this.btmcont.Controls.SetChildIndex(this.revise, 0);
             this.btmcont.Controls.SetChildIndex(this.delete, 0);
@@ -82,15 +83,15 @@
             // gridcont
             // 
             this.gridcont.Location = new System.Drawing.Point(12, 163);
-            this.gridcont.Size = new System.Drawing.Size(872, 520);
+            this.gridcont.Size = new System.Drawing.Size(1184, 520);
             // 
             // undo
             // 
-            this.undo.Location = new System.Drawing.Point(806, 5);
+            this.undo.Location = new System.Drawing.Point(1118, 5);
             // 
             // save
             // 
-            this.save.Location = new System.Drawing.Point(726, 5);
+            this.save.Location = new System.Drawing.Point(1038, 5);
             // 
             // brand_box
             // 
@@ -403,6 +404,7 @@
             this.button3.TabIndex = 132;
             this.button3.Text = "To Excel";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // txtuser1
             // 
@@ -432,9 +434,21 @@
             this.result_box.Size = new System.Drawing.Size(78, 21);
             this.result_box.TabIndex = 133;
             // 
+            // textID
+            // 
+            this.textID.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.textID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.textID.Location = new System.Drawing.Point(1096, 128);
+            this.textID.Name = "textID";
+            this.textID.ReadOnly = true;
+            this.textID.Size = new System.Drawing.Size(100, 23);
+            this.textID.TabIndex = 134;
+            this.textID.Visible = false;
+            // 
             // P01_Weight
             // 
-            this.ClientSize = new System.Drawing.Size(896, 733);
+            this.ClientSize = new System.Drawing.Size(1208, 733);
+            this.Controls.Add(this.textID);
             this.Controls.Add(this.result_box);
             this.Controls.Add(this.txtuser1);
             this.Controls.Add(this.approve_box);
@@ -510,6 +524,7 @@
             this.Controls.SetChildIndex(this.approve_box, 0);
             this.Controls.SetChildIndex(this.txtuser1, 0);
             this.Controls.SetChildIndex(this.result_box, 0);
+            this.Controls.SetChildIndex(this.textID, 0);
             ((System.ComponentModel.ISupportInitialize)(this.gridbs)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.mtbs)).EndInit();
             this.btmcont.ResumeLayout(false);
@@ -554,5 +569,6 @@
         private Win.UI.DisplayBox approve_box;
         private Class.txtuser txtuser1;
         private Win.UI.DisplayBox result_box;
+        private Win.UI.TextBox textID;
     }
 }
