fe4197c2dc76706079af391a5ae1452cbb4dfed4
WENNY <wenny@sportscity.com.tw>
2016-07-14 08:10:02 +0000

    ADD

---
+++ Sci.Production.Warehouse/P25_PrintData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sci.Production.Warehouse
+{
+    class P25_PrintData
+    {
+        
+        public string SEQ { get; set; }
+        public string Roll { get; set; }
+        public string DYELOT { get; set; }
+        public string DESC { get; set; }
+        public string Unit { get; set; }
+        public string Type { get; set; }
+        public string ActQty { get; set; }
+        public string oLocation { get; set; }
+    }
+}
