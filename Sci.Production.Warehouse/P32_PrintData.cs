e3aff9d8d0914bf1076d654ac0cd7fcda0718618
WENNY <wenny@sportscity.com.tw>
2016-07-15 03:52:36 +0000

    ADD

---
+++ Sci.Production.Warehouse/P32_PrintData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sci.Production.Warehouse
+{
+    class P32_PrintData
+    {
+        public string StockSEQ { get; set; } 
+        public string ToSP { get; set; }
+        public string DESC { get; set; }
+        public string FromStock { get; set; }
+        public string ToStock { get; set; }
+        public string Location { get; set; }
+        public string Unit { get; set; }
+        public string Roll { get; set; }
+        public string DYELOT { get; set; }
+        public string QTY { get; set; }
+        
+    }
+}
