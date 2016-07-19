07fa3a1e7d0ae534ac98a0749182652d54ada7ee
ELENA <elena@sportscity.com.tw>
2016-07-19 03:55:44 +0000

    update

---
+++ Sci.Production.Warehouse/P50_Print.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Sci.Production.Warehouse
+{
+    partial class P50_Print
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioGroup1 = new Sci.Win.UI.RadioGroup();
+            this.radioButton2 = new Sci.Win.UI.RadioButton();
+            this.radioButton1 = new Sci.Win.UI.RadioButton();
+            this.radioGroup1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radioGroup1
+            // 
+            this.radioGroup1.Controls.Add(this.radioButton2);
+            this.radioGroup1.Controls.Add(this.radioButton1);
+            this.radioGroup1.Location = new System.Drawing.Point(33, 28);
+            this.radioGroup1.Name = "radioGroup1";
+            this.radioGroup1.Size = new System.Drawing.Size(434, 202);
+            this.radioGroup1.TabIndex = 95;
+            this.radioGroup1.TabStop = false;
+            this.radioGroup1.Text = "Report Type";
+            this.radioGroup1.Value = "Forward Stocktaking without Book Qty";
+            this.radioGroup1.ValueChanged += new System.EventHandler(this.radioGroup1_ValueChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.ForeColor = System.Drawing.Color.Black;
+            this.radioButton2.Location = new System.Drawing.Point(7, 97);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(180, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Forward Stocktaking List";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.ForeColor = System.Drawing.Color.Black;
+            this.radioButton1.Location = new System.Drawing.Point(7, 56);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(264, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Forward Stocktaking without Book Qty";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Value = "Forward Stocktaking without Book Qty";
+            // 
+            // P50_Print
+            // 
+            this.ClientSize = new System.Drawing.Size(627, 314);
+            this.Controls.Add(this.radioGroup1);
+            this.Name = "P50_Print";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.radioGroup1, 0);
+            this.radioGroup1.ResumeLayout(false);
+            this.radioGroup1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.RadioGroup radioGroup1;
+        private Win.UI.RadioButton radioButton2;
+        private Win.UI.RadioButton radioButton1;
+
+    }
+}
