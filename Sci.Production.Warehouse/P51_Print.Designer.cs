6e9c0de2fb288161a34618fd9d62d4dc43b93dae
WENNY <wenny@sportscity.com.tw>
2016-07-19 03:54:50 +0000

    ADD

---
+++ Sci.Production.Warehouse/P51_Print.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Sci.Production.Warehouse
+{
+    partial class P51_Print
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioPanel1 = new Sci.Win.UI.RadioPanel();
+            this.label1 = new Sci.Win.UI.Label();
+            this.radioButton1 = new Sci.Win.UI.RadioButton();
+            this.radioButton2 = new Sci.Win.UI.RadioButton();
+            this.radioPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Click += new System.EventHandler(this.print_Click);
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(535, 102);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(535, 138);
+            // 
+            // radioPanel1
+            // 
+            this.radioPanel1.Controls.Add(this.radioButton2);
+            this.radioPanel1.Controls.Add(this.radioButton1);
+            this.radioPanel1.Controls.Add(this.label1);
+            this.radioPanel1.Location = new System.Drawing.Point(49, 40);
+            this.radioPanel1.Name = "radioPanel1";
+            this.radioPanel1.Size = new System.Drawing.Size(425, 128);
+            this.radioPanel1.TabIndex = 94;
+            this.radioPanel1.Value = "Backward Socktaking Form";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(19, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Report Type";
+            this.label1.TextStyle.Color = System.Drawing.Color.Black;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.ForeColor = System.Drawing.Color.Black;
+            this.radioButton1.Location = new System.Drawing.Point(19, 47);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(196, 21);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Backward Socktaking Form";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Value = "Backward Socktaking Form";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.ForeColor = System.Drawing.Color.Black;
+            this.radioButton2.Location = new System.Drawing.Point(19, 74);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(175, 21);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "Backward Socktaking Li";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // P51_Print
+            // 
+            this.ClientSize = new System.Drawing.Size(627, 314);
+            this.Controls.Add(this.radioPanel1);
+            this.Name = "P51_Print";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.radioPanel1, 0);
+            this.radioPanel1.ResumeLayout(false);
+            this.radioPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.RadioPanel radioPanel1;
+        private Win.UI.RadioButton radioButton2;
+        private Win.UI.RadioButton radioButton1;
+        private Win.UI.Label label1;
+    }
+}
---
+++ Sci.Production.Warehouse/P51_Print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Warehouse
+{
+    public partial class P51_Print : Sci.Win.Tems.PrintForm
+    {
+        public P51_Print()
+        {
+            InitializeComponent();
+        }
+
+        private void print_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
