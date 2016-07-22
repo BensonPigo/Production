cf796ffc89f7ae72a4629307edb972585f4c13fe
WENNY <wenny@sportscity.com.tw>
2016-07-22 10:10:11 +0000

    add

---
+++ Sci.Production.Warehouse/P07_Sticker.cs
@@ -0,0 +1,44 @@
+﻿using Ict.Win;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Linq;
+namespace Sci.Production.Warehouse
+{
+    public partial class P07_Sticker : Sci.Win.Tems.QueryForm
+    {
+
+        BindingList<P07_Sticker_Data> Data = new BindingList<P07_Sticker_Data>();
+
+        public P07_Sticker()
+        {
+            InitializeComponent();
+            this.grid1.DataSource = this.Data;
+            this.GridSetup();
+        }
+
+        void GridSetup()
+        {
+            this.grid1.IsEditingReadOnly = false;
+            Helper.Controls.Grid.Generator(this.grid1)
+                .CheckBox("POID", header: "SP #", width: Widths.AnsiChars(14),iseditable:true)
+                .Text("PINO", header: "PI #", width: Widths.AnsiChars(16), iseditingreadonly: true)
+                .Text("Packages", header: "P'Kgs", width: Widths.AnsiChars(4), iseditingreadonly: false)
+                .Text("BoxID", header: "Box No.", width: Widths.AnsiChars(5), iseditingreadonly: false)
+                .Text("CarNo", header: "C/No", width: Widths.AnsiChars(9), iseditingreadonly: false)
+                .Text("FinishDate", header: "Finish Date", width: Widths.AnsiChars(9), iseditingreadonly: false)
+                .Text("Remark", header: "Remark", width: Widths.AnsiChars(7), iseditingreadonly: false)
+                ;
+
+        }
+
+        private void btn_Print_Click(object sender, EventArgs e)
+        {
+            List<P07_Sticker_Data> tmpData = Data.Where(data => data.selected).ToList();
+        }
+    }
+}
---
+++ Sci.Production.Warehouse/P07_Sticker_Data.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sci.Production.Warehouse
+{
+    public class P07_Sticker_Data : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        // This method is called by the Set accessor of each property.
+        // The CallerMemberName attribute that is applied to the optional propertyName
+        // parameter causes the property name of the caller to be substituted as an argument.
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        private bool sele;
+        public bool selected
+        {
+            get { return this.sele; }
+            set
+            {
+                this.sele = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public string OrderID { get; set; }
+        public string SEQ { get; set; }
+    }
+}
