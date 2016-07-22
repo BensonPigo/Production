4d5ba4faf4e94177c208bacd5c98a3328e9edd9b
ELENA <elena@sportscity.com.tw>
2016-07-22 03:00:40 +0000

    add

---
+++ Sci.Production.Warehouse/P03_Print.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Sci.Production.Warehouse
+{
+    partial class P03_Print
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioPanel1 = new Sci.Win.UI.RadioPanel();
+            this.radioButton1 = new Sci.Win.UI.RadioButton();
+            this.radioButton2 = new Sci.Win.UI.RadioButton();
+            this.radioPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radioPanel1
+            // 
+            this.radioPanel1.Controls.Add(this.radioButton2);
+            this.radioPanel1.Controls.Add(this.radioButton1);
+            this.radioPanel1.Location = new System.Drawing.Point(31, 12);
+            this.radioPanel1.Name = "radioPanel1";
+            this.radioPanel1.Size = new System.Drawing.Size(451, 197);
+            this.radioPanel1.TabIndex = 94;
+            this.radioPanel1.Value = "Material Status";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.radioButton1.Location = new System.Drawing.Point(28, 49);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(120, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Material Status";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Value = "Material Status";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.radioButton2.Location = new System.Drawing.Point(28, 116);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(112, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Purchase List";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // P03_Print
+            // 
+            this.ClientSize = new System.Drawing.Size(627, 314);
+            this.Controls.Add(this.radioPanel1);
+            this.Name = "P03_Print";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.radioPanel1, 0);
+            this.radioPanel1.ResumeLayout(false);
+            this.radioPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.RadioPanel radioPanel1;
+        private Win.UI.RadioButton radioButton2;
+        private Win.UI.RadioButton radioButton1;
+
+    }
+}
---
+++ Sci.Production.Warehouse/P03_Print.cs
@@ -0,0 +1,120 @@
+﻿using Ict;
+using Ict.Win;
+using Sci.Data;
+using Sci.Production.PublicPrg;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Transactions;
+using System.Windows.Forms;
+using System.Reflection;
+using Microsoft.Reporting.WinForms;
+using System.Data.SqlClient;
+using Sci.Win;
+using Sci;
+using Sci.Production;
+using Sci.Utility.Excel;
+
+namespace Sci.Production.Warehouse
+{
+    public partial class P03_Print : Sci.Win.Tems.PrintForm
+    {
+        public P03_Print(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+        }
+        DataTable dt;
+        string sqlcmd;
+        protected override bool ValidateInput()
+        {
+            sqlcmd = @"select  a.id[poid]
+            ,b.StyleID[style]
+            ,a.SEQ1+a.SEQ2[SEQ]
+            ,dbo.getMtlDesc(a.id,a.SEQ1,a.seq2,2,0)[Desc]
+            ,chinese_abb=d.AbbCH
+            ,case a.fabrictype
+            when 'F' then 'Fabric'
+            when 'A' THEN 'Accessory'
+            Else a.FabricType
+            end Material_Type
+            ,Hs_code=e.HsCode
+            ,supp=c.SuppID
+            ,Supp_Name=f.AbbEN
+            ,Currency=f.Currencyid
+            ,Del=substring(convert(varchar, a.cfmetd, 101),1,5)
+            ,Used_Qty=a.UsedQty
+            ,Order_Qty=a.qty
+            ,Taipei_Stock=a.InputQty
+            ,Unit=a.POUnit
+            ,TTL_Qty=a.ShipQty
+            ,FOC=a.FOC
+            ,ty=convert(numeric(5,2), iif( isnull(a.Qty,0)=0,100,a.shipqty/a.qty*100))
+            ,OK=a.Complete
+            ,Exp_Date=substring(convert(varchar,a.eta, 101),1,5)
+            ,FormA=IIF(EXISTS(SELECT * FROM DBO.Export_Detail g
+            WHERE g.PoID =a.id
+            AND g.SEQ1 = a.seq1
+            AND g.SEQ2 = a.seq2
+            AND IsFormA = 1),'Y','')
+            from dbo.PO_Supp_Detail a
+            left join dbo.orders b
+            on
+            a.id=b.id
+            left join dbo.PO_Supp c
+            on
+            a.id=c.id and a.SEQ1=c.SEQ1
+            left join dbo.Fabric_Supp d
+            on
+            d.SCIRefno=a.SCIRefno and d.SuppID=c.SuppID
+            left join dbo.Fabric_HsCode e
+            on 
+            e.SCIRefno=a.SCIRefno and e.SuppID=c.SuppID and e.year=year(a.ETA)
+            left join dbo.Supp f
+            on 
+            f.id=c.SuppID";
+            return base.ValidateInput();
+        }
+        protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            return  DBProxy.Current.Select("", sqlcmd, out dt); 
+        }
+            protected override bool OnToExcel(Win.ReportDefinition report)
+            {
+                if (dt == null || dt.Rows.Count == 0)
+                {
+                    MyUtility.Msg.ErrorBox("Data not found");
+                    return false;
+                }
+
+                var saveDialog = Sci.Utility.Excel.MyExcelPrg.GetSaveFileDialog(Sci.Utility.Excel.MyExcelPrg.filter_Excel);
+                saveDialog.ShowDialog();
+                string outpath = saveDialog.FileName;
+                if (outpath.Empty())
+                {
+                    return true;
+                }
+                if (this.radioPanel1.Value == this.radioButton1.Value) {
+                    Sci.Utility.Excel.SaveXltReportCls xl = new Utility.Excel.SaveXltReportCls("Warehouse_P03_Print.xltx");
+                    xl.dicDatas.Add("##poid", dt);
+                    xl.Save(outpath, false);
+
+                }
+                //string xlt = @"Warehouse_P03_Print.xltx";
+                //SaveXltReportCls xl = new SaveXltReportCls(xlt);
+                //SaveXltReportCls.xltRptTable xlTable = new SaveXltReportCls.xltRptTable(dt);
+                //xl.dicDatas.Add("##poid", xlTable);
+                //xl.Save(outpath, true);
+                return true;
+            }
+
+           
+        }
+    
+    }
+
+
