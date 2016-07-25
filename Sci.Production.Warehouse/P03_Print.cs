76ae77943a16f8529adf0614ab542ad0652fe6c8
ELENA <elena@sportscity.com.tw>
2016-07-25 03:59:15 +0000

    update

--- Sci.Production.Warehouse/P03_Print.cs
+++ Sci.Production.Warehouse/P03_Print.cs
@@ -82,73 +82,78 @@ namespace Sci.Production.Warehouse
             on 
             f.id=c.SuppID where a.id='12042541IIS  '";
 
-            sqlmdd = @"select a.id[sp#]
-                    ,b.StyleID[style#]
-                    ,a.SEQ1+a.SEQ2[SEQ]
-                    ,c.SuppID[Supp]
-                    ,d.NameEN[Supp Name]
-                    ,substring(convert(varchar,a.cfmetd, 101),1,5)[Sup. 1st Cfm ETA]
-                    ,substring(convert(varchar,a.RevisedETD, 101),1,5)[RevisedETD]
-                    ,a.Refno[Ref#]
-                    --,dbo.getMtlDesc(a.poid,a.SEQ1,a.SEQ2,2,0)
-                    ,e.AbbCH[Chinese Abb]
-                    ,f.HsCode[HS Code]
-                    ,case a.FabricType 
-                    when 'F' then 'Fabric'
-                    when 'A'then 'Accessory'
-                    else a.FabricType
-                    end Material_Type
-                    ,a.ColorID[Color]
-                    ,a.SizeSpec[Size]
-                    ,a.Qty[Order Qty]
-                    ,a.NETQty[Net Qty]
-                    ,a.UsedQty[Use Qty]
-                    ,a.ShipQty[Ship Qty]
-                    ,a.ShipFOC[F.O.C]
-                    ,a.ApQty[AP Qty]
-                    ,IIF(EXISTS(SELECT * FROM DBO.Export_Detail g
-                    WHERE g.PoID = a.id
-                    AND g.SEQ1 = a.seq1
-                    AND g.SEQ2 =a.seq2
-                    AND IsFormA = 1),'Y','')[FormA]
-                    ,a.InputQty[Taipei Stock Qty]
-                    ,a.POUnit[Unit]
-                    ,a.Complete[Cmplt]
-                    ,substring(convert(varchar, a.ata, 101),1,5)[Act. Eta]
-                    ,(select t.id+',' from 
-                    (select distinct id from export_detail  where poid =a.id and seq1=a.seq1 and seq2=a.seq2) t for xml path('')) [WK#]
-                    ,i.InQty[Arrived Qty]
-                    ,a.StockUnit[Unit]
-                    ,i.OutQty[Released Qty]
-                    ,i.AdjustQty[Adjust Qty]
-                    ,i.InQty-i.OutQty+i.AdjustQty[Balance]
-                    ,i.LInvQty[Stock Qty]
-                    ,i.LObQty[Scrap Qty]
-                    ,i.ALocation[Bulk Location]
-                    ,i.BLocation[Stock Location]
-
-                    from dbo.PO_Supp_Detail a
-                    left join dbo.Orders b
-                    on 
-                    a.id=b.id
-                    left join dbo.PO_Supp c
-                    on
-                    c.id=a.id and c.SEQ1=a.SEQ1
-                    left join dbo.supp d
-                    on 
-                    d.id=c.SuppID
-                    left join dbo.Fabric_Supp e
-                    on
-                    e.SCIRefno=a.SCIRefno and e.SuppID=c.SuppID
-                    left join dbo.Fabric_HsCode f
-                    on
-                    f.SCIRefno=a.SCIRefno and f.SuppID=c.SuppID and f.Year=Year(a.ETA)
-                    --inner join dbo.Export_Detail h
-                    --on 
-                    --h.poid=a.id and h.seq1=a.SEQ1 and h.seq2=a.SEQ2 and 
-                    left join dbo.MDivisionPoDetail i
-                    on
-                    i.POID=a.ID";
+                    sqlmdd = @"select a.id[sp#]
+						     ,b.StyleID[style#]
+						     ,a.SEQ1+a.SEQ2[SEQ]
+						     ,c.SuppID[Supp]
+						     ,d.NameEN[Supp Name]
+						     ,substring(convert(varchar,a.cfmetd, 101),1,5)[Sup. 1st Cfm ETA]
+						     ,substring(convert(varchar,a.RevisedETD, 101),1,5)[RevisedETD]
+						     ,a.Refno[Ref#]
+                   　	     ,dbo.getMtlDesc(a.id,a.SEQ1,a.SEQ2,2,0)[Description]
+						     ,e.AbbCH[Chinese Abb]
+						     ,f.HsCode[HS Code]
+						     ,case a.FabricType 
+						      when 'F' then 'Fabric'
+						      when 'A'then 'Accessory'
+						      else a.FabricType
+						      end Material_Type
+						      ,a.ColorID[Color]
+						      ,a.SizeSpec[Size]
+						      ,h.Currencyid[Currency]
+						      ,a.UsedQty[Qty]
+						      ,a.Qty[Order Qty]
+						      ,a.NETQty[Net Qty]
+						      ,a.NETQty+a.LossQty[Use Qty]
+						      ,a.ShipQty[Ship Qty]
+						      ,a.ShipFOC[F.O.C]
+						      ,a.ApQty[AP Qty]
+						      ,IIF(EXISTS(SELECT * FROM DBO.Export_Detail g
+						      WHERE g.PoID = a.id
+						      AND g.SEQ1 = a.seq1
+						      AND g.SEQ2 =a.seq2
+						      AND IsFormA = 1),'Y','')[FormA]
+						      ,a.InputQty[Taipei Stock Qty]
+						      ,a.POUnit[Unit]
+						      ,a.Complete[Cmplt]
+						      ,substring(convert(varchar, a.ata, 101),1,5)[Act. Eta]
+						      ,(select id+',' from 
+						      (select distinct id from export_detail  where poid =a.id and seq1=a.seq1 and seq2=a.seq2) t for xml path('')) [WK#]
+						      ,(select orderid+',' from 
+						      (select ol.orderid  from PO_Supp_Detail_OrderList ol  where id =a.id and seq1=a.seq1 and seq2=a.seq2) ol for xml path('')) [Order List]
+						      ,i.InQty[Arrived Qty]
+						      ,a.StockUnit[Unit]
+						      ,i.OutQty[Released Qty]
+						      ,i.AdjustQty[Adjust Qty]
+						      ,i.InQty-i.OutQty+i.AdjustQty[Balance]
+						      ,i.LInvQty[Stock Qty]
+						      ,i.LObQty[Scrap Qty]
+						      ,i.ALocation[Bulk Location]
+						      ,i.BLocation[Stock Location]
+						      ,(select Remark+',' from 
+						      (select r.Remark  from dbo.Receiving_Detail r where POID =a.id and seq1=a.seq1 and seq2=a.seq2 and remark !='') r for xml path('')) [Remark]
+						      from dbo.PO_Supp_Detail a
+						      left join dbo.Orders b
+						      on 
+						      a.id=b.id
+						      left join dbo.PO_Supp c
+						      on
+						      c.id=a.id and c.SEQ1=a.SEQ1
+						      left join dbo.supp d
+						      on 
+						      d.id=c.SuppID
+						      left join dbo.Fabric_Supp e
+						      on
+						      e.SCIRefno=a.SCIRefno and e.SuppID=c.SuppID
+						      left join dbo.Fabric_HsCode f
+						      on
+						      f.SCIRefno=a.SCIRefno and f.SuppID=c.SuppID and f.Year=Year(a.eta)
+						      left join dbo.supp h
+						      on
+						      h.id=c.SuppID
+						      left join dbo.MDivisionPoDetail i
+						      on
+						      i.POID=a.ID";
             return base.ValidateInput();
         }
         protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
@@ -190,6 +195,7 @@ namespace Sci.Production.Warehouse
                 }
                 return true;
             }
+            return true;
         }
 
 
