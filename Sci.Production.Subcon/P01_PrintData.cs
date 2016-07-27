fe97c47f4ad342595c94af30b4a8c2425e9fc0a7
WENNY <wenny@sportscity.com.tw>
2016-07-27 01:38:25 +0000

    ADD

---
+++ Sci.Production.Subcon/P01_PrintData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sci.Production.Subcon
+{
+    class P01_PrintData
+    {
+        public string OrderID { get; set; }
+        public string StyleID { get; set; }
+        public string poQTY { get; set; }
+        public string ArtworkID { get; set; }
+        public string PCS { get; set; }
+        public string Unitprice { get; set; }
+        public string QtyGMT { get; set; }
+        public string Amount { get; set; }
+    }
+}
