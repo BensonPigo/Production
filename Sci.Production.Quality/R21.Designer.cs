e5cdf689092ad8beda1bdb278c63bfba9252ca64
WILLY <willy@sportscity.com.tw>
2016-07-27 05:31:56 +0000

    add by willy4011 201560727

---
+++ Sci.Production.Quality/R21.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace Sci.Production.Quality
+{
+    partial class R21
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.label3 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.label5 = new Sci.Win.UI.Label();
+            this.label6 = new Sci.Win.UI.Label();
+            this.label7 = new Sci.Win.UI.Label();
+            this.label8 = new Sci.Win.UI.Label();
+            this.textBox1 = new Sci.Win.UI.TextBox();
+            this.textBox2 = new Sci.Win.UI.TextBox();
+            this.comboBox1 = new Sci.Win.UI.ComboBox();
+            this.textBox7 = new Sci.Win.UI.TextBox();
+            this.S_radioButton = new Sci.Win.UI.RadioButton();
+            this.radioGroup1 = new Sci.Win.UI.RadioGroup();
+            this.d_radioButton = new Sci.Win.UI.RadioButton();
+            this.label9 = new Sci.Win.UI.Label();
+            this.label10 = new Sci.Win.UI.Label();
+            this.label11 = new Sci.Win.UI.Label();
+            this.dateBox1 = new Sci.Win.UI.DateBox();
+            this.dateBox2 = new Sci.Win.UI.DateBox();
+            this.dateBox3 = new Sci.Win.UI.DateBox();
+            this.dateBox4 = new Sci.Win.UI.DateBox();
+            this.radioGroup1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Enabled = false;
+            this.print.Location = new System.Drawing.Point(440, 21);
+            this.print.Size = new System.Drawing.Size(112, 30);
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(440, 57);
+            this.toexcel.Size = new System.Drawing.Size(112, 30);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(440, 93);
+            this.close.Size = new System.Drawing.Size(112, 30);
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(147, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 33);
+            this.label1.TabIndex = 94;
+            this.label1.Text = "R21. CFA Inline Report";
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(27, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 23);
+            this.label2.TabIndex = 95;
+            this.label2.Text = "SP# :";
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(27, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 23);
+            this.label3.TabIndex = 96;
+            this.label3.Text = "Audit Date :";
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(27, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 23);
+            this.label4.TabIndex = 97;
+            this.label4.Text = "Buyer Delivery :";
+            // 
+            // label5
+            // 
+            this.label5.Lines = 0;
+            this.label5.Location = new System.Drawing.Point(27, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 23);
+            this.label5.TabIndex = 98;
+            this.label5.Text = "Factory :";
+            // 
+            // label6
+            // 
+            this.label6.Lines = 0;
+            this.label6.Location = new System.Drawing.Point(27, 218);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(102, 23);
+            this.label6.TabIndex = 99;
+            this.label6.Text = "Brand :";
+            // 
+            // label7
+            // 
+            this.label7.Lines = 0;
+            this.label7.Location = new System.Drawing.Point(27, 256);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(102, 23);
+            this.label7.TabIndex = 100;
+            this.label7.Text = "Format :";
+            // 
+            // label8
+            // 
+            this.label8.Lines = 0;
+            this.label8.Location = new System.Drawing.Point(447, 319);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(112, 23);
+            this.label8.TabIndex = 101;
+            this.label8.Text = "Paper Size : A4";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(147, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 102;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox2.Location = new System.Drawing.Point(279, 64);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 103;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IsSupportUnselect = true;
+            this.comboBox1.Location = new System.Drawing.Point(135, 130);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(135, 24);
+            this.comboBox1.TabIndex = 108;
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.Color.White;
+            this.textBox7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox7.Location = new System.Drawing.Point(147, 218);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 23);
+            this.textBox7.TabIndex = 109;
+            // 
+            // S_radioButton
+            // 
+            this.S_radioButton.AutoSize = true;
+            this.S_radioButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.S_radioButton.Location = new System.Drawing.Point(147, 258);
+            this.S_radioButton.Name = "S_radioButton";
+            this.S_radioButton.Size = new System.Drawing.Size(85, 21);
+            this.S_radioButton.TabIndex = 110;
+            this.S_radioButton.TabStop = true;
+            this.S_radioButton.Text = "Summary";
+            this.S_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // radioGroup1
+            // 
+            this.radioGroup1.Controls.Add(this.dateBox4);
+            this.radioGroup1.Controls.Add(this.dateBox3);
+            this.radioGroup1.Controls.Add(this.dateBox2);
+            this.radioGroup1.Controls.Add(this.dateBox1);
+            this.radioGroup1.Controls.Add(this.label11);
+            this.radioGroup1.Controls.Add(this.label10);
+            this.radioGroup1.Controls.Add(this.comboBox1);
+            this.radioGroup1.Controls.Add(this.label9);
+            this.radioGroup1.Controls.Add(this.d_radioButton);
+            this.radioGroup1.Location = new System.Drawing.Point(12, 48);
+            this.radioGroup1.Name = "radioGroup1";
+            this.radioGroup1.Size = new System.Drawing.Size(418, 308);
+            this.radioGroup1.TabIndex = 111;
+            this.radioGroup1.TabStop = false;
+            // 
+            // d_radioButton
+            // 
+            this.d_radioButton.AutoSize = true;
+            this.d_radioButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.d_radioButton.Location = new System.Drawing.Point(135, 237);
+            this.d_radioButton.Name = "d_radioButton";
+            this.d_radioButton.Size = new System.Drawing.Size(81, 21);
+            this.d_radioButton.TabIndex = 112;
+            this.d_radioButton.TabStop = true;
+            this.d_radioButton.Text = "by Detail";
+            this.d_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Lines = 0;
+            this.label9.Location = new System.Drawing.Point(238, 19);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(21, 23);
+            this.label9.TabIndex = 113;
+            this.label9.Text = "～";
+            this.label9.TextStyle.BorderColor = System.Drawing.Color.Black;
+            this.label9.TextStyle.Color = System.Drawing.Color.Black;
+            this.label9.TextStyle.ExtBorderColor = System.Drawing.Color.Black;
+            this.label9.TextStyle.GradientColor = System.Drawing.Color.Black;
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Lines = 0;
+            this.label10.Location = new System.Drawing.Point(238, 52);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(21, 23);
+            this.label10.TabIndex = 114;
+            this.label10.Text = "～";
+            this.label10.TextStyle.BorderColor = System.Drawing.Color.Black;
+            this.label10.TextStyle.Color = System.Drawing.Color.Black;
+            this.label10.TextStyle.ExtBorderColor = System.Drawing.Color.Black;
+            this.label10.TextStyle.GradientColor = System.Drawing.Color.Black;
+            // 
+            // label11
+            // 
+            this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.Lines = 0;
+            this.label11.Location = new System.Drawing.Point(238, 93);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(21, 23);
+            this.label11.TabIndex = 115;
+            this.label11.Text = "～";
+            this.label11.TextStyle.BorderColor = System.Drawing.Color.Black;
+            this.label11.TextStyle.Color = System.Drawing.Color.Black;
+            this.label11.TextStyle.ExtBorderColor = System.Drawing.Color.Black;
+            this.label11.TextStyle.GradientColor = System.Drawing.Color.Black;
+            // 
+            // dateBox1
+            // 
+            this.dateBox1.Location = new System.Drawing.Point(135, 52);
+            this.dateBox1.Name = "dateBox1";
+            this.dateBox1.Size = new System.Drawing.Size(103, 23);
+            this.dateBox1.TabIndex = 116;
+            // 
+            // dateBox2
+            // 
+            this.dateBox2.Location = new System.Drawing.Point(267, 52);
+            this.dateBox2.Name = "dateBox2";
+            this.dateBox2.Size = new System.Drawing.Size(103, 23);
+            this.dateBox2.TabIndex = 117;
+            // 
+            // dateBox3
+            // 
+            this.dateBox3.Location = new System.Drawing.Point(135, 93);
+            this.dateBox3.Name = "dateBox3";
+            this.dateBox3.Size = new System.Drawing.Size(103, 23);
+            this.dateBox3.TabIndex = 118;
+            // 
+            // dateBox4
+            // 
+            this.dateBox4.Location = new System.Drawing.Point(267, 93);
+            this.dateBox4.Name = "dateBox4";
+            this.dateBox4.Size = new System.Drawing.Size(103, 23);
+            this.dateBox4.TabIndex = 119;
+            // 
+            // R21
+            // 
+            this.ClientSize = new System.Drawing.Size(564, 395);
+            this.Controls.Add(this.S_radioButton);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioGroup1);
+            this.Name = "R21";
+            this.Text = "R21 .CFA Inline Report";
+            this.Controls.SetChildIndex(this.radioGroup1, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.label5, 0);
+            this.Controls.SetChildIndex(this.label6, 0);
+            this.Controls.SetChildIndex(this.label7, 0);
+            this.Controls.SetChildIndex(this.label8, 0);
+            this.Controls.SetChildIndex(this.textBox1, 0);
+            this.Controls.SetChildIndex(this.textBox2, 0);
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.textBox7, 0);
+            this.Controls.SetChildIndex(this.S_radioButton, 0);
+            this.radioGroup1.ResumeLayout(false);
+            this.radioGroup1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.Label label1;
+        private Win.UI.Label label2;
+        private Win.UI.Label label3;
+        private Win.UI.Label label4;
+        private Win.UI.Label label5;
+        private Win.UI.Label label6;
+        private Win.UI.Label label7;
+        private Win.UI.Label label8;
+        private Win.UI.TextBox textBox1;
+        private Win.UI.TextBox textBox2;
+        private Win.UI.ComboBox comboBox1;
+        private Win.UI.TextBox textBox7;
+        private Win.UI.RadioButton S_radioButton;
+        private Win.UI.RadioGroup radioGroup1;
+        private Win.UI.RadioButton d_radioButton;
+        private Win.UI.Label label11;
+        private Win.UI.Label label10;
+        private Win.UI.Label label9;
+        private Win.UI.DateBox dateBox4;
+        private Win.UI.DateBox dateBox3;
+        private Win.UI.DateBox dateBox2;
+        private Win.UI.DateBox dateBox1;
+    }
+}
---
+++ Sci.Production.Quality/R21.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict.Win;
+using Ict;
+using Sci.Data;
+using System.Runtime.InteropServices;
+
+namespace Sci.Production.Quality
+{
+    public partial class R21 : Sci.Win.Tems.PrintForm
+    {
+        DateTime? cdate1,cdate2,bdate1,bdate2;
+        string factory, id1, id2, brand, factoryName;
+        DataTable SummaryData,DetailData;
+
+        public R21(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            DataTable factory;
+            DBProxy.Current.Select(null, "select distinct FTYGroup from Factory order by FTYGroup", out factory);
+            MyUtility.Tool.SetupCombox(comboBox1, 1, factory);
+            comboBox1.Text = Sci.Env.User.Factory;
+           
+
+        }
+        // 驗證輸入條件
+        protected override bool ValidateInput()
+        {
+            if (S_radioButton.Checked==false && d_radioButton.Checked==false)
+            {
+                MyUtility.Msg.InfoBox("<Format> you have to choice one!  ");
+                return false;
+            }
+            id1 = textBox1.Text;
+            id2 = textBox2.Text;
+            cdate1 = dateBox1.Value;
+            cdate2 = dateBox2.Value;
+            bdate1 = dateBox3.Value;
+            bdate2 = dateBox4.Value;
+            factory = comboBox1.Text;
+            brand = textBox7.Text;
+            return base.ValidateInput();
+        }
+        // 非同步資料
+        protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            StringBuilder sqlCmd_Summary = new StringBuilder();          
+
+            #region 組撈Summary Data SQL
+            sqlCmd_Summary.Append(
+@"select 
+c.StyleID
+,a.OrderID
+,iif(c.VasShas=0,'','v') as 'VAS/SHAS'
+,c.BrandID
+,c.CustPONo
+,a.cDate
+,a.GarmentOutput
+,a.FactoryID
+,a.SewingLineID
+,case a.shift 
+when 'D' then 'night'
+when 'N' then 'NIGHT'
+when 'O' then 'SUBCON OUT'
+when 'I' then 'SUBCON IN'
+else '' end as Shift
+,a.Team
+,c.Qty
+,DBO.GETPASS1(a.CFA) as CFA
+,case a.Stage when 'I' then 'Comments/Roving'
+when 'C' then 'Change Over'
+when 'P' then 'Stagger'
+when 'R' then 'Re-Stagger'
+when 'F' then 'Final'
+when 'B' then 'Buyer'
+else ''  end as Stage
+,a.Result
+,a.InspectQty
+,a.DefectQty
+,iif(a.InspectQty=0,0,round(a.DefectQty/a.InspectQty*100,3)) [SQR]
+from dbo.Cfa a inner join dbo.Cfa_Detail b
+on b.id = a.ID 
+inner join dbo.orders c
+on c.id = a.OrderID
+where a.Status = 'Confirmed'");
+            if (!MyUtility.Check.Empty(id1))
+            {
+                sqlCmd_Summary.Append(string.Format("and a.OrderID >= '{0}'", id1));
+            }
+            if (!MyUtility.Check.Empty(id2))
+            {
+                sqlCmd_Summary.Append(string.Format(" and a.OrderID <= '{0}'", id2));
+            }
+            if (!MyUtility.Check.Empty(cdate1))
+            {
+                sqlCmd_Summary.Append(string.Format(" and a.cDate >= '{0}'", Convert.ToDateTime(cdate1).ToShortDateString()));
+            }
+            if (!MyUtility.Check.Empty(cdate2))
+            {
+                sqlCmd_Summary.Append(string.Format(" and a.cDate <= '{0}'", Convert.ToDateTime(cdate2).ToShortDateString()));
+            }
+            if (!MyUtility.Check.Empty(bdate1))
+            {
+                sqlCmd_Summary.Append(string.Format(" and c.BuyerDelivery >= '{0}'", Convert.ToDateTime(bdate1).ToShortDateString()));
+            }
+            if (!MyUtility.Check.Empty(bdate2))
+            {
+                sqlCmd_Summary.Append(string.Format(" and c.BuyerDelivery <= '{0}'", Convert.ToDateTime(bdate2).ToShortDateString()));
+            }
+            if (!MyUtility.Check.Empty(factory))
+            {
+                sqlCmd_Summary.Append(string.Format(" and a.FactoryID = '{0}'", factory));
+            }
+            if (!MyUtility.Check.Empty(brand))
+            {
+                sqlCmd_Summary.Append(string.Format(" and c.brandID = '{0}'", brand));
+            }
+        
+            DualResult result = DBProxy.Current.Select(null, sqlCmd_Summary.ToString(), out SummaryData);
+            if(!result)
+            {
+                DualResult failResult = new DualResult(false, "Query data fail\r\n" + result.ToString());
+                return failResult;
+            }
+            #endregion
+            StringBuilder sqlCmd_Detail = new StringBuilder();
+
+            #region 組撈Detail Data SQL
+            sqlCmd_Detail.Append(
+@"select 
+c.FactoryID
+,a.OrderID
+,iif(c.VasShas=0,'','v') as 'VAS/SHAS'
+,c.StyleID
+,c.BrandID
+,c.CustPONo
+,a.cDate
+,a.GarmentOutput
+,a.SewingLineID
+,case a.shift 
+when 'D' then 'night'
+when 'N' then 'NIGHT'
+when 'O' then 'SUBCON OUT'
+when 'I' then 'SUBCON IN'
+else '' end as Shift
+,a.Team
+,c.Qty
+,DBO.GETPASS1(a.CFA) as CFA
+,case a.Stage when 'I' then 'Comments/Roving'
+when 'C' then 'Change Over'
+when 'P' then 'Stagger'
+when 'R' then 'Re-Stagger'
+when 'F' then 'Final'
+when 'B' then 'Buyer'
+else ''  end as Stage
+,a.Result
+,a.InspectQty
+,a.DefectQty
+,iif(a.InspectQty=0,0,round(a.DefectQty/a.InspectQty*100,3)) [SQR]
+,'' as Area
+,d.Qty
+,d.Remark
+,d.Action
+from dbo.Cfa a inner join dbo.Cfa_Detail b
+on b.id = a.ID 
+inner join dbo.orders c
+on c.id = a.OrderID
+where a.Status = 'Confirmed'");
+            if (!MyUtility.Check.Empty(id1))
+            {
+                sqlCmd_Summary.Append(string.Format("and a.OrderID >= '{0}'", id1));
+            }
+            if (!MyUtility.Check.Empty(id2))
+            {
+                sqlCmd_Summary.Append(string.Format(" and a.OrderID <= '{0}'", id2));
+            }
+            if (!MyUtility.Check.Empty(cdate1))
+            {
+                sqlCmd_Summary.Append(string.Format(" and a.cDate >= '{0}'", Convert.ToDateTime(cdate1).ToShortDateString()));
+            }
+            if (!MyUtility.Check.Empty(cdate2))
+            {
+                sqlCmd_Summary.Append(string.Format(" and a.cDate <= '{0}'", Convert.ToDateTime(cdate2).ToShortDateString()));
+            }
+            if (!MyUtility.Check.Empty(bdate1))
+            {
+                sqlCmd_Summary.Append(string.Format(" and c.BuyerDelivery >= '{0}'", Convert.ToDateTime(bdate1).ToShortDateString()));
+            }
+            if (!MyUtility.Check.Empty(bdate2))
+            {
+                sqlCmd_Summary.Append(string.Format(" and c.BuyerDelivery <= '{0}'", Convert.ToDateTime(bdate2).ToShortDateString()));
+            }
+            if (!MyUtility.Check.Empty(factory))
+            {
+                sqlCmd_Summary.Append(string.Format(" and a.FactoryID = '{0}'", factory));
+            }
+            if (!MyUtility.Check.Empty(brand))
+            {
+                sqlCmd_Summary.Append(string.Format(" and c.brandID = '{0}'", brand));
+            }
+     
+            DualResult result1 = DBProxy.Current.Select(null, sqlCmd_Detail.ToString(), out DetailData);
+            if (!result)
+            {
+                DualResult failResult = new DualResult(false, "Query data fail\r\n" + result1.ToString());
+                return failResult;
+
+            }
+            #endregion
+            return Result.True;
+        }
+        // 產生Excel
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {           
+            
+            MyUtility.Msg.WaitWindows("Starting Excel");
+            if (S_radioButton.Checked)
+            {
+                if (SummaryData.Rows.Count <= 0)
+                {
+                    MyUtility.Msg.WarningBox("Data not found!");
+                    return false;
+                }
+
+                Microsoft.Office.Interop.Excel._Application excel = MyUtility.Excel.ConnectExcel(Sci.Env.Cfg.XltPathDir + "\\Quality_R21_CFM_InlineReport_Summary.xltx"); //預先開啟excel app
+                MyUtility.Excel.CopyToXls(SummaryData,"","Quality_R21_CFM_InlineReport_Summary.xltx",2,true,null,  excel);
+                Microsoft.Office.Interop.Excel.Worksheet excelSheets = excel.ActiveWorkbook.Worksheets[1];// 取得工作表                 
+                
+                if (excelSheets != null) Marshal.FinalReleaseComObject(excelSheets);//釋放sheet
+                if (excel != null) Marshal.FinalReleaseComObject(excel);
+            }
+            if (d_radioButton.Checked)
+            {
+                if (DetailData.Rows.Count <= 0)
+                {
+                    MyUtility.Msg.WarningBox("Data not found!");
+                    return false;
+                }
+                Microsoft.Office.Interop.Excel._Application excel = MyUtility.Excel.ConnectExcel(Sci.Env.Cfg.XltPathDir + "\\Quality_R21_CFA_InlineReport_detail.xltx"); //預先開啟excel app
+                MyUtility.Excel.CopyToXls(DetailData,"", "Quality_R21_CFA_InlineReport_detail.xltx", 2, true, null, excel);
+                 Microsoft.Office.Interop.Excel.Worksheet excelSheets = excel.ActiveWorkbook.Worksheets[1];// 取得工作表
+                 if (excelSheets != null) Marshal.FinalReleaseComObject(excelSheets);//釋放sheet
+                 if (excel != null) Marshal.FinalReleaseComObject(excel);
+            }
+
+            return true;
+        }
+    }
+    
+}
