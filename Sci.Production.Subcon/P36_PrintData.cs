587482c6d9ae6e1b30c1d043c5265adf8cfc5e24
WENNY <wenny@sportscity.com.tw>
2016-07-28 09:21:14 +0000

    add

---
+++ Sci.Production.Subcon/P36_PrintData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sci.Production.Subcon
+{
+    class P36_PrintData
+    {
+        public string Supplier { get; set; }
+        public string No { get; set; }
+        public string SP { get; set; }
+        public string Subject { get; set; }
+        public string DESC { get; set; }
+        public string Amount { get; set; }
+        public string Exchange { get; set; }
+        public string taxrate { get; set; }
+        public string Total { get; set; }
+        public string Remark { get; set; }
+        public string Purchaser { get; set; }
+    }
+}
