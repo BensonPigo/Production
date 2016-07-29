dd890b7983f97b0d3a5df8a7f90d95e4f7958e3f
WENNY <wenny@sportscity.com.tw>
2016-07-29 10:41:33 +0000

    add

---
+++ Sci.Production.Subcon/R36.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Sci.Production.Subcon
+{
+    partial class R36
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new Sci.Win.UI.Panel();
+            this.label1 = new Sci.Win.UI.Label();
+            this.comboBox1 = new Sci.Win.UI.ComboBox();
+            this.label2 = new Sci.Win.UI.Label();
+            this.dateBox1 = new Sci.Win.UI.DateBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dateBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(448, 269);
+            this.panel1.TabIndex = 94;
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(473, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 23);
+            this.label1.TabIndex = 95;
+            this.label1.Text = "Report Type:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IsSupportUnselect = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Debit Note List",
+            "Summary",
+            "Detail",
+            "Debit Schedule Detail"});
+            this.comboBox1.Location = new System.Drawing.Point(473, 190);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(143, 24);
+            this.comboBox1.TabIndex = 96;
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(16, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 23);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Debit Date:";
+            // 
+            // dateBox1
+            // 
+            this.dateBox1.Location = new System.Drawing.Point(95, 7);
+            this.dateBox1.Name = "dateBox1";
+            this.dateBox1.Size = new System.Drawing.Size(130, 23);
+            this.dateBox1.TabIndex = 1;
+            // 
+            // R36
+            // 
+            this.ClientSize = new System.Drawing.Size(627, 314);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "R36";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.comboBox1, 0);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.Panel panel1;
+        private Win.UI.Label label1;
+        private Win.UI.ComboBox comboBox1;
+        private Win.UI.DateBox dateBox1;
+        private Win.UI.Label label2;
+    }
+}
---
+++ Sci.Production.Subcon/R36.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Subcon
+{
+    public partial class R36 : Sci.Win.Tems.PrintForm
+    {
+        public R36(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            this.comboBox1.SelectedIndex = 0;
+        }
+    }
+}
