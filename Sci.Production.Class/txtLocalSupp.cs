903a899c24c15e4b5faf16fde078e90ac7abd519
WENNY <wenny@sportscity.com.tw>
2016-08-01 01:57:20 +0000

    add

---
+++ Sci.Production.Class/txtLocalSupp.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sci.Production.Class
+{
+    public partial class txtLocalSupp : UserControl
+    {
+        public txtLocalSupp()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+
+
+        public Sci.Win.UI.TextBox TextBox1
+        {
+            get { return this.textBox1; }
+            set { this.textBox1 = value; }
+        }
+
+        public Sci.Win.UI.DisplayBox DisplayBox1
+        {
+            get { return this.displayBox1; }
+            set { this.displayBox1 = value; }
+        }
+
+        [Bindable(true)]
+        public string TextBox1Binding
+        {
+            set { this.textBox1.Text = value; }
+            get { return textBox1.Text; }
+        }
+
+        [Bindable(true)]
+        public string DisplayBox1Binding
+        {
+            set { this.displayBox1.Text = value; }
+            get { return this.displayBox1.Text; }
+        }
+
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+            base.OnValidating(e);
+            string textValue = this.textBox1.Text;
+            if (!string.IsNullOrWhiteSpace(textValue) && textValue != this.textBox1.OldValue)
+            {
+                if (!MyUtility.Check.Seek(textValue, "LocalSupp", "ID"))
+                {
+                    MyUtility.Msg.WarningBox(string.Format("< LocalSupplier Code: {0} > not found!!!", textValue));
+                    this.textBox1.Text = "";
+                    e.Cancel = true;
+                    return;
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            this.displayBox1.Text = MyUtility.GetValue.Lookup("Abb", this.textBox1.Text.ToString(), "LocalSupp", "ID");
+        }
+
+        private void textBox1_PopUp(object sender, Win.UI.TextBoxPopUpEventArgs e)
+        {
+            Sci.Win.Forms.Base myForm = (Sci.Win.Forms.Base)this.FindForm();
+            if (myForm.EditMode == false || textBox1.ReadOnly == true) return;
+            Sci.Win.Tools.SelectItem item = new Sci.Win.Tools.SelectItem("select ID,Name,Abb from LocalSupp order by ID", "8,30,30", this.textBox1.Text);
+            DialogResult returnResult = item.ShowDialog();
+            if (returnResult == DialogResult.Cancel) { return; }
+            this.textBox1.Text = item.GetSelectedString();
+        }
+    }
+}
