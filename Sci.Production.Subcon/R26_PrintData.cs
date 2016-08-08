abd22df1dd8ee9ee9de02d569bde6cad35c813cc
ELENA <elena@sportscity.com.tw>
2016-08-08 02:49:10 +0000

    update

--- Sci.Production.Subcon/R26_PrintData.cs
+++ Sci.Production.Subcon/R26_PrintData.cs
@@ -8,5 +8,20 @@ namespace Sci.Production.Subcon
 {
     class R26_PrintData
     {
+        public string PO { get; set; }
+        public string Code { get; set; }
+        public string Color_Shade { get; set; }
+        public string Description { get; set; }
+        public string Quantity { get; set; }
+        public string Unit { get; set; }
+        public string Unit_Price { get; set; }
+        public string Amount { get; set; }
+        public string Total_Quantity { get; set; }
+        public string Remark { get; set; }
+        public string Total1 { get; set; }
+        public string Total2 { get; set; }
+        public string currencyid { get; set; }
+        public string vat { get; set; }
+        public string Grand_Total { get; set; }
     }
 }
