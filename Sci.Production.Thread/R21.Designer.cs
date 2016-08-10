51fc2027a222eb359f71421be7a90a0151663966
WENNY <wenny@sportscity.com.tw>
2016-08-10 05:51:26 +0000

    ADD

---
+++ Sci.Production.Thread/R21.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Sci.Production.Thread
+{
+    partial class R21
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new Sci.Win.UI.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(20, 17);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(490, 253);
+            this.panel1.TabIndex = 94;
+            // 
+            // R21
+            // 
+            this.ClientSize = new System.Drawing.Size(627, 314);
+            this.Controls.Add(this.panel1);
+            this.Name = "R21";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.Panel panel1;
+    }
+}
---
+++ Sci.Production.Thread/R21.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Thread
+{
+    public partial class R21 : Sci.Win.Tems.PrintForm
+    {
+        public R21()
+        {
+            InitializeComponent();
+        }
+    }
+}
