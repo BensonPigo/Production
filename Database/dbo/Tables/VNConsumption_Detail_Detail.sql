88eedd3bd0e5702d623a09b2d17e4e5b7a125f3e
YINMEI <yinmei@sportscity.com.tw>
2016-08-19 01:41:30 +0000

    修改PK

--- Database/dbo/Tables/VNConsumption_Detail_Detail.sql
+++ Database/dbo/Tables/VNConsumption_Detail_Detail.sql
@@ -2,9 +2,9 @@
     [ID]        VARCHAR (13)    NOT NULL,
     [NLCode]    VARCHAR (5)     CONSTRAINT [DF_VNConsumption_Detail _Detail_NLCode] DEFAULT ('') NOT NULL,
     [SCIRefno]  VARCHAR (26)    CONSTRAINT [DF_VNConsumption_Detail _Detail_SCIRefno] DEFAULT ('') NOT NULL,
-    [RefNo]     VARCHAR (20)    CONSTRAINT [DF_VNConsumption_Detail _Detail_RefNo] DEFAULT ('') NULL,
+    [RefNo]     VARCHAR (20)    CONSTRAINT [DF_VNConsumption_Detail _Detail_RefNo] DEFAULT ('') NOT NULL,
     [Qty]       NUMERIC (12, 4) CONSTRAINT [DF_VNConsumption_Detail _Detail_Qty] DEFAULT ((0)) NULL,
     [LocalItem] BIT             CONSTRAINT [DF_VNConsumption_Detail _Detail_LocalItem] DEFAULT ((0)) NULL,
-    CONSTRAINT [PK_VNConsumption_Detail _Detail] PRIMARY KEY CLUSTERED ([ID] ASC, [NLCode] ASC, [SCIRefno] ASC)
+    CONSTRAINT [PK_VNConsumption_Detail _Detail] PRIMARY KEY CLUSTERED ([RefNo], [ID], [NLCode], [SCIRefno])
 );
 
