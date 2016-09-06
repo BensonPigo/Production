7d55f0f6f82c40431ce197c185135680f489f366
WENNY <wenny@sportscity.com.tw>
2016-09-06 06:37:56 +0000

    add

---
+++ Sci.Production.Quality/R02.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace Sci.Production.Quality
+{
+    partial class R02
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new Sci.Win.UI.Panel();
+            this.label12 = new Sci.Win.UI.Label();
+            this.label11 = new Sci.Win.UI.Label();
+            this.label10 = new Sci.Win.UI.Label();
+            this.label9 = new Sci.Win.UI.Label();
+            this.label8 = new Sci.Win.UI.Label();
+            this.label7 = new Sci.Win.UI.Label();
+            this.label6 = new Sci.Win.UI.Label();
+            this.label5 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.label3 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.txtsupplier1 = new Sci.Production.Class.txtsupplier();
+            this.comboBox_Over = new Sci.Win.UI.ComboBox();
+            this.textBox_Cate = new Sci.Win.UI.ComboBox();
+            this.textBox_Ref = new Sci.Win.UI.TextBox();
+            this.txtBrand1 = new Sci.Trade.Class.TxtBrand();
+            this.txtSeason1 = new Sci.Trade.Class.TxtSeason();
+            this.label13 = new Sci.Win.UI.Label();
+            this.textBox_SP2 = new Sci.Win.UI.TextBox();
+            this.textBox_SP = new Sci.Win.UI.TextBox();
+            this.date_Est = new Sci.Win.UI.DateRange();
+            this.date_Sewing = new Sci.Win.UI.DateRange();
+            this.dateRange4 = new Sci.Win.UI.DateRange();
+            this.date_SCI = new Sci.Win.UI.DateRange();
+            this.date_Arrive = new Sci.Win.UI.DateRange();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtsupplier1);
+            this.panel1.Controls.Add(this.comboBox_Over);
+            this.panel1.Controls.Add(this.textBox_Cate);
+            this.panel1.Controls.Add(this.textBox_Ref);
+            this.panel1.Controls.Add(this.txtBrand1);
+            this.panel1.Controls.Add(this.txtSeason1);
+            this.panel1.Controls.Add(this.label13);
+            this.panel1.Controls.Add(this.textBox_SP2);
+            this.panel1.Controls.Add(this.textBox_SP);
+            this.panel1.Controls.Add(this.date_Est);
+            this.panel1.Controls.Add(this.date_Sewing);
+            this.panel1.Controls.Add(this.dateRange4);
+            this.panel1.Controls.Add(this.date_SCI);
+            this.panel1.Controls.Add(this.date_Arrive);
+            this.panel1.Controls.Add(this.label12);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(20, 9);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(472, 466);
+            this.panel1.TabIndex = 94;
+            // 
+            // label12
+            // 
+            this.label12.Lines = 0;
+            this.label12.Location = new System.Drawing.Point(9, 425);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(154, 23);
+            this.label12.TabIndex = 22;
+            this.label12.Text = "Overall Result Status:";
+            // 
+            // label11
+            // 
+            this.label11.Lines = 0;
+            this.label11.Location = new System.Drawing.Point(9, 381);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(154, 23);
+            this.label11.TabIndex = 21;
+            this.label11.Text = "Supplier..................:";
+            // 
+            // label10
+            // 
+            this.label10.Lines = 0;
+            this.label10.Location = new System.Drawing.Point(9, 339);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(154, 23);
+            this.label10.TabIndex = 20;
+            this.label10.Text = "Category.................:";
+            // 
+            // label9
+            // 
+            this.label9.Lines = 0;
+            this.label9.Location = new System.Drawing.Point(9, 298);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(154, 23);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Refno......................:";
+            // 
+            // label8
+            // 
+            this.label8.Lines = 0;
+            this.label8.Location = new System.Drawing.Point(9, 257);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(154, 23);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Brand.......................:";
+            // 
+            // label7
+            // 
+            this.label7.Lines = 0;
+            this.label7.Location = new System.Drawing.Point(9, 215);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(154, 23);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Season.....................:";
+            // 
+            // label6
+            // 
+            this.label6.Lines = 0;
+            this.label6.Location = new System.Drawing.Point(9, 170);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(154, 23);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "SP#...........................:";
+            // 
+            // label5
+            // 
+            this.label5.Lines = 0;
+            this.label5.Location = new System.Drawing.Point(9, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(154, 23);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Est. Cutting Date.......:";
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(9, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(154, 23);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Sewing in-line Date...:";
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(9, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 23);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "SCI Delivery..............:";
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(9, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 23);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Arrive W/H Date........:";
+            // 
+            // txtsupplier1
+            // 
+            this.txtsupplier1.DisplayBox1Binding = "";
+            this.txtsupplier1.Location = new System.Drawing.Point(174, 380);
+            this.txtsupplier1.Name = "txtsupplier1";
+            this.txtsupplier1.Size = new System.Drawing.Size(147, 23);
+            this.txtsupplier1.TabIndex = 41;
+            this.txtsupplier1.TextBox1Binding = "";
+            // 
+            // comboBox_Over
+            // 
+            this.comboBox_Over.BackColor = System.Drawing.Color.White;
+            this.comboBox_Over.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox_Over.FormattingEnabled = true;
+            this.comboBox_Over.IsSupportUnselect = true;
+            this.comboBox_Over.Items.AddRange(new object[] {
+            "All",
+            "Pass",
+            "Fail",
+            "Empty Result",
+            "N/A inspection & test"});
+            this.comboBox_Over.Location = new System.Drawing.Point(174, 424);
+            this.comboBox_Over.Name = "comboBox_Over";
+            this.comboBox_Over.Size = new System.Drawing.Size(161, 24);
+            this.comboBox_Over.TabIndex = 40;
+            // 
+            // textBox_Cate
+            // 
+            this.textBox_Cate.BackColor = System.Drawing.Color.White;
+            this.textBox_Cate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox_Cate.FormattingEnabled = true;
+            this.textBox_Cate.IsSupportUnselect = true;
+            this.textBox_Cate.Location = new System.Drawing.Point(174, 339);
+            this.textBox_Cate.Name = "textBox_Cate";
+            this.textBox_Cate.Size = new System.Drawing.Size(125, 24);
+            this.textBox_Cate.TabIndex = 39;
+            // 
+            // textBox_Ref
+            // 
+            this.textBox_Ref.BackColor = System.Drawing.Color.White;
+            this.textBox_Ref.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox_Ref.Location = new System.Drawing.Point(174, 298);
+            this.textBox_Ref.Name = "textBox_Ref";
+            this.textBox_Ref.Size = new System.Drawing.Size(253, 23);
+            this.textBox_Ref.TabIndex = 38;
+            // 
+            // txtBrand1
+            // 
+            this.txtBrand1.BackColor = System.Drawing.Color.White;
+            this.txtBrand1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtBrand1.Location = new System.Drawing.Point(174, 257);
+            this.txtBrand1.Name = "txtBrand1";
+            this.txtBrand1.Size = new System.Drawing.Size(114, 23);
+            this.txtBrand1.TabIndex = 37;
+            // 
+            // txtSeason1
+            // 
+            this.txtSeason1.BackColor = System.Drawing.Color.White;
+            this.txtSeason1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtSeason1.Location = new System.Drawing.Point(174, 215);
+            this.txtSeason1.myBrandName = null;
+            this.txtSeason1.Name = "txtSeason1";
+            this.txtSeason1.Size = new System.Drawing.Size(136, 23);
+            this.txtSeason1.TabIndex = 36;
+            // 
+            // label13
+            // 
+            this.label13.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label13.Lines = 0;
+            this.label13.Location = new System.Drawing.Point(302, 170);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(22, 23);
+            this.label13.TabIndex = 35;
+            this.label13.Text = "～";
+            this.label13.TextStyle.Color = System.Drawing.Color.Black;
+            // 
+            // textBox_SP2
+            // 
+            this.textBox_SP2.BackColor = System.Drawing.Color.White;
+            this.textBox_SP2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox_SP2.Location = new System.Drawing.Point(323, 170);
+            this.textBox_SP2.Name = "textBox_SP2";
+            this.textBox_SP2.Size = new System.Drawing.Size(131, 23);
+            this.textBox_SP2.TabIndex = 34;
+            // 
+            // textBox_SP
+            // 
+            this.textBox_SP.BackColor = System.Drawing.Color.White;
+            this.textBox_SP.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox_SP.Location = new System.Drawing.Point(174, 170);
+            this.textBox_SP.Name = "textBox_SP";
+            this.textBox_SP.Size = new System.Drawing.Size(125, 23);
+            this.textBox_SP.TabIndex = 33;
+            // 
+            // date_Est
+            // 
+            this.date_Est.Location = new System.Drawing.Point(174, 131);
+            this.date_Est.Name = "date_Est";
+            this.date_Est.Size = new System.Drawing.Size(280, 23);
+            this.date_Est.TabIndex = 32;
+            // 
+            // date_Sewing
+            // 
+            this.date_Sewing.Location = new System.Drawing.Point(174, 93);
+            this.date_Sewing.Name = "date_Sewing";
+            this.date_Sewing.Size = new System.Drawing.Size(280, 23);
+            this.date_Sewing.TabIndex = 31;
+            // 
+            // dateRange4
+            // 
+            this.dateRange4.Location = new System.Drawing.Point(247, 93);
+            this.dateRange4.Name = "dateRange4";
+            this.dateRange4.Size = new System.Drawing.Size(8, 23);
+            this.dateRange4.TabIndex = 30;
+            // 
+            // date_SCI
+            // 
+            this.date_SCI.Location = new System.Drawing.Point(174, 56);
+            this.date_SCI.Name = "date_SCI";
+            this.date_SCI.Size = new System.Drawing.Size(280, 23);
+            this.date_SCI.TabIndex = 29;
+            // 
+            // date_Arrive
+            // 
+            this.date_Arrive.Location = new System.Drawing.Point(174, 19);
+            this.date_Arrive.Name = "date_Arrive";
+            this.date_Arrive.Size = new System.Drawing.Size(280, 23);
+            this.date_Arrive.TabIndex = 28;
+            // 
+            // R02
+            // 
+            this.ClientSize = new System.Drawing.Size(627, 509);
+            this.Controls.Add(this.panel1);
+            this.Name = "R02";
+            this.Text = "R02.Accessory Inspection & Laboratory List Report";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.Panel panel1;
+        private Win.UI.Label label12;
+        private Win.UI.Label label11;
+        private Win.UI.Label label10;
+        private Win.UI.Label label9;
+        private Win.UI.Label label8;
+        private Win.UI.Label label7;
+        private Win.UI.Label label6;
+        private Win.UI.Label label5;
+        private Win.UI.Label label4;
+        private Win.UI.Label label3;
+        private Win.UI.Label label2;
+        private Class.txtsupplier txtsupplier1;
+        private Win.UI.ComboBox comboBox_Over;
+        private Win.UI.ComboBox textBox_Cate;
+        private Win.UI.TextBox textBox_Ref;
+        private Trade.Class.TxtBrand txtBrand1;
+        private Trade.Class.TxtSeason txtSeason1;
+        private Win.UI.Label label13;
+        private Win.UI.TextBox textBox_SP2;
+        private Win.UI.TextBox textBox_SP;
+        private Win.UI.DateRange date_Est;
+        private Win.UI.DateRange date_Sewing;
+        private Win.UI.DateRange dateRange4;
+        private Win.UI.DateRange date_SCI;
+        private Win.UI.DateRange date_Arrive;
+    }
+}
---
+++ Sci.Production.Quality/R02.cs
@@ -0,0 +1,203 @@
+﻿using Sci.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Quality
+{
+    public partial class R02 : Sci.Win.Tems.PrintForm
+    {
+        DateTime? ArrDate1; DateTime? ArrDate2;
+        DateTime? SCIDate1; DateTime? SCIDate2;
+        DateTime? SewDate1; DateTime? SewDate2;
+        DateTime? Est1; DateTime? Est2;
+        string sp1; string sp2; string Sea; string Brand; string Ref; string Cate; string Supp; string Over;
+        List<SqlParameter> lis;
+        DataTable dt; string cmd;
+        public R02(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            DataTable ORS = null;
+            string sqlm = (@" 
+                        select
+                             Category=name
+                        from  dbo.DropDownList
+                        where type = 'Category' and id != 'O'
+                        ");
+            DBProxy.Current.Select("", sqlm, out ORS);
+            ORS.Rows.Add(new string[] { "" });
+            ORS.DefaultView.Sort = "Category";
+            this.textBox_Cate.DataSource = ORS;
+            this.textBox_Cate.ValueMember = "Category";
+            this.textBox_Cate.DisplayMember = "Category";
+            this.textBox_Cate.SelectedIndex = 1;
+            this.comboBox_Over.SelectedIndex = 0;
+
+            print.Enabled = false;
+        }
+        protected override bool ValidateInput()
+        {
+            bool date_Arrive_Empty = !this.date_Arrive.HasValue, date_SCI_Empty = !this.date_SCI.HasValue, date_Sewing_Empty = !this.date_Sewing.HasValue, date_Est_Empty = !this.date_Est.HasValue,
+               textBox_SP_Empty = this.textBox_SP.Text.Empty(), textBox_SP2_Empty = this.textBox_SP2.Text.Empty(), txtSEA_Empty = this.txtSeason1.Text.Empty()
+          , txtBrand_Empty = this.txtBrand1.Text.Empty(), txtRef_Empty = this.textBox_Ref.Text.Empty(), Cate_comboBox_Empty = this.textBox_Cate.Text.Empty(), Supp_Empty = this.txtsupplier1.Text.Empty(), Over_comboBox_Empty = this.comboBox_Over.Text.Empty();
+
+            if (date_Arrive_Empty && date_SCI_Empty && date_Sewing_Empty && date_Est_Empty && textBox_SP_Empty && textBox_SP2_Empty)
+            {
+                MyUtility.Msg.ErrorBox("Please select 'Arrive W/H Date' or 'SCI Delivery' or 'Sewing in-line Date' or 'Est. Cutting Date' or 'SP#'  at least one field entry");
+
+                date_Arrive.Focus();
+
+                return false;
+            }
+
+            ArrDate1 = date_Arrive.Value1;
+            ArrDate2 = date_Arrive.Value2;
+            SCIDate1 = date_SCI.Value1;
+            SCIDate2 = date_SCI.Value2;
+            SewDate1 = date_Sewing.Value1;
+            SewDate2 = date_Sewing.Value2;
+            Est1 = date_Est.Value1;
+            Est2 = date_Est.Value2;
+            sp1 = textBox_SP.Text.ToString();
+            sp2 = textBox_SP2.Text.ToString();
+            Sea = txtSeason1.Text;
+            Brand = txtBrand1.Text;
+            Ref = textBox_Ref.Text.ToString();
+            Cate = textBox_Cate.Text;
+            Supp = txtsupplier1.Text;
+            Over = comboBox_Over.SelectedItem.ToString();
+
+            lis = new List<SqlParameter>();
+            string sqlWhere = "", RWhere = "", OWhere = "";
+            List<string> sqlWheres = new List<string>();
+            List<string> RWheres = new List<string>();
+            List<string> OWheres = new List<string>();
+            #region --組WHERE--
+
+             if (!this.date_Arrive.Value1.Empty() && !this.date_Arrive.Value2.Empty())
+            {
+                RWheres.Add("R.WhseArrival between @ArrDate1 and @ArrDate2");
+                lis.Add(new SqlParameter("@ArrDate1", ArrDate1));
+                lis.Add(new SqlParameter("@ArrDate2", ArrDate2));
+            } if (!this.date_SCI.Value1.Empty() && !this.date_SCI.Value2.Empty())
+            {
+                OWheres.Add("O.SciDelivery between @SCIDate1 and @SCIDate2");
+                lis.Add(new SqlParameter("@SCIDate1", SCIDate1));
+                lis.Add(new SqlParameter("@SCIDate2", SCIDate2));
+            } if (!this.date_Sewing.Value1.Empty() && !this.date_Sewing.Value2.Empty())
+            {
+                OWheres.Add("O.SewInLine between @SewDate1 and @SewDate2");
+                lis.Add(new SqlParameter("@SewDate1", SewDate1));
+                lis.Add(new SqlParameter("@SewDate2", SewDate2));
+            } if (!this.date_Est.Value1.Empty() && !this.date_Est.Value2.Empty())
+            {
+                OWheres.Add("O.CutInLine between @Est1 and @Est2");
+                lis.Add(new SqlParameter("@Est1", Est1));
+                lis.Add(new SqlParameter("@Est2", Est2));
+            } if (!this.textBox_SP.Text.Empty())
+            {
+                OWheres.Add("O.Id between @sp1 and @sp2");
+                lis.Add(new SqlParameter("@sp1", sp1));
+                lis.Add(new SqlParameter("@sp2", sp2));
+            } if (!this.txtSeason1.Text.Empty())
+            {
+                OWheres.Add("O.SeasonID = @Sea");
+                lis.Add(new SqlParameter("@Sea", Sea));
+            } if (!this.txtBrand1.Text.Empty())
+            {
+                OWheres.Add("O.BrandID = @Brand");
+                lis.Add(new SqlParameter("@Brand", Brand));
+            } if (!this.textBox_Ref.Text.Empty())
+            {
+                sqlWheres.Add("P.Refno = @Ref");
+                lis.Add(new SqlParameter("@Ref", Ref));
+            } if (!this.textBox_Cate.SelectedItem.ToString().Empty())
+            {
+                sqlWheres.Add("D.Category = @Cate");
+                lis.Add(new SqlParameter("@Cate", Cate));
+            } if (this.txtsupplier1.Text.Empty())
+            {
+                lis.Add(new SqlParameter("@Supp", Supp));
+
+            } if (this.comboBox_Over.Text == "Pass")
+            {
+                sqlWheres.Add("dbo.GetFirResult(F.id) = 'Pass'");
+
+            } if (this.comboBox_Over.Text == "Fail")
+            {
+                sqlWheres.Add("dbo.GetFirResult(F.id) = 'Faill'");
+
+            } if (this.comboBox_Over.Text == "Empty Result")
+            {
+                sqlWheres.Add("dbo.GetFirResult(F.id) = ' '");
+
+            } if (this.comboBox_Over.Text == "N/A inspection & test")
+            {
+                sqlWheres.Add("dbo.GetFirResult(F.id) = 'None'");
+
+            }
+
+            #endregion
+            sqlWhere = string.Join(" and ", sqlWheres);
+            OWhere = string.Join(" and ", OWheres);
+            if (!sqlWhere.Empty())
+            {
+                sqlWhere = " where " + sqlWhere;
+            }
+            if (!RWhere.Empty())
+            {
+                RWhere = " where " + RWhere;
+            }
+            if (!OWhere.Empty())
+            {
+                OWhere = " where " + OWhere;
+            }
+            #region --撈ListExcel資料--
+
+            cmd = string.Format(@"
+                        select  F.POID,(F.SEQ1+'-'+F.SEQ2)SEQ,O.factoryid,O.BrandID,O.StyleID,O.SeasonID,
+	                            t.ExportId,t.InvNo,t.WhseArrival,t.StockQty,
+	                            (SELECT MinSciDelivery FROM  DBO.GetSCI(F.Poid,O.Category))[MinSciDelivery],
+	                            (SELECT MinBuyerDelivery  FROM  DBO.GetSCI(F.Poid,O.Category))[MinBuyerDelivery],
+	                            F.Refno,P.ColorID,(SP.SuppID+'-'+s.AbbEN)Supplier,C.WeaveTypeID,
+	                            IIF(F.Nonphysical = 1,'Y',' ')[N/A Physical],F.Result,F.Physical,F.PhysicalDate,
+	                            F.TotalInspYds,F.Weight,F.WeightDate,F.ShadeBond,F.ShadeBondDate,F.Continuity,
+	                            F.ContinuityDate,L.Result,IIF(L.nonCrocking=1,'Y',' ')[N/A Crocking],L.Crocking,
+	                            L.CrockingDate,IIF(L.nonHeat=1,'Y',' ')[N/A Heat Shrinkage],L.Heat,L.HeatDate,
+	                            IIF(L.nonWash=1,'Y',' ' )[N/A Wash Shrinkage],L.Wash,L.WashDate,V.Result,CFD.Result
+                        from dbo.FIR F
+                        inner join (select distinct R.WhseArrival,R.InvNo,R.ExportId,R.Id,rd.PoId,RD.seq1,RD.seq2,RD.StockQty
+			                        from dbo.Receiving R
+			                        inner join dbo.Receiving_Detail RD on RD.Id = R.Id"
+                                    + RWhere + @" 
+			                        ) t
+                        on t.PoId = F.POID and t.Seq1 = F.SEQ1 and t.Seq2 = F.SEQ2
+                        inner join (select distinct poid,O.factoryid,O.BrandID,O.StyleID,O.SeasonID,O.Category from dbo.Orders o "
+                                    + OWhere + @"
+		                           ) O on O.poid = F.POID
+                        OUTER APPLY( select Category=name
+                                     from  dbo.DropDownList
+                                     where type = 'Category' and id != 'O')D
+                        inner join dbo.PO_Supp SP on SP.id = F.POID and SP.SEQ1 = F.SEQ1 and SP.SEQ1 = F.SEQ2
+                        inner join dbo.PO_Supp_Detail P on P.ID = F.POID and P.SEQ1 = F.SEQ1 and P.SEQ2 = F.SEQ2
+                        OUTER APPLY(SELECT * FROM  supp s WHERE s.id = SP.SuppID AND SP.id = F.Poid and SP.seq1 = F.Seq1)s
+                        OUTER APPLY(SELECT * FROM  Fabric C WHERE C.SCIRefno = F.SCIRefno)C
+                        OUTER APPLY(SELECT * FROM  FIR_Laboratory L WHERE L.CrockingEncode=1 AND L.ID = F.ID AND L.SEQ1 = F.SEQ1 AND L.SEQ1 = F.SEQ2)L
+                        OUTER APPLY(select od.Result from dbo.Oven ov 
+                        inner join dbo.Oven_Detail od on od.ID = ov.ID
+                        where ov.POID=F.POID and od.SEQ1=F.Seq1 and seq2=F.Seq2 and ov.Status='Confirmed'
+                        )V
+                        OUTER APPLY(select cd.Result from dbo.ColorFastness CF inner join dbo.ColorFastness_Detail cd on cd.ID = CF.ID
+                        where CF.Status = 'Confirmed' and CF.POID=F.POID and cd.SEQ1=F.Seq1 and cd.seq2=F.Seq2
+                        )CFD" + sqlWhere);
+            #endregion
+            return base.ValidateInput();
+        }
+    }
+}
