514c0261aa8cad62427b99a8bd968231f6227bcf
WENNY <wenny@sportscity.com.tw>
2016-09-08 06:07:17 +0000

    ADD

---
+++ Sci.Production.Quality/R03.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Sci.Production.Quality
+{
+    partial class R03
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new Sci.Win.UI.Panel();
+            this.comboFactory = new Sci.Win.UI.ComboBox();
+            this.label1 = new Sci.Win.UI.Label();
+            this.comboCategory = new Sci.Win.UI.ComboBox();
+            this.label10 = new Sci.Win.UI.Label();
+            this.label8 = new Sci.Win.UI.Label();
+            this.label7 = new Sci.Win.UI.Label();
+            this.DateEstCutting = new Sci.Win.UI.DateRange();
+            this.DateSewInLine = new Sci.Win.UI.DateRange();
+            this.dateRange4 = new Sci.Win.UI.DateRange();
+            this.DateSCIDelivery = new Sci.Win.UI.DateRange();
+            this.label5 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.label3 = new Sci.Win.UI.Label();
+            this.txtBrand = new Sci.Production.Class.txtbrand();
+            this.txtSeason = new Sci.Production.Class.txtseason();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtSeason);
+            this.panel1.Controls.Add(this.txtBrand);
+            this.panel1.Controls.Add(this.comboFactory);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.comboCategory);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.DateEstCutting);
+            this.panel1.Controls.Add(this.DateSewInLine);
+            this.panel1.Controls.Add(this.dateRange4);
+            this.panel1.Controls.Add(this.DateSCIDelivery);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(33, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(469, 294);
+            this.panel1.TabIndex = 94;
+            // 
+            // comboFactory
+            // 
+            this.comboFactory.BackColor = System.Drawing.Color.White;
+            this.comboFactory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboFactory.FormattingEnabled = true;
+            this.comboFactory.IsSupportUnselect = true;
+            this.comboFactory.Location = new System.Drawing.Point(177, 243);
+            this.comboFactory.Name = "comboFactory";
+            this.comboFactory.Size = new System.Drawing.Size(121, 24);
+            this.comboFactory.TabIndex = 47;
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(12, 243);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 23);
+            this.label1.TabIndex = 46;
+            this.label1.Text = "Factory...................:";
+            // 
+            // comboCategory
+            // 
+            this.comboCategory.BackColor = System.Drawing.Color.White;
+            this.comboCategory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboCategory.FormattingEnabled = true;
+            this.comboCategory.IsSupportUnselect = true;
+            this.comboCategory.Location = new System.Drawing.Point(177, 202);
+            this.comboCategory.Name = "comboCategory";
+            this.comboCategory.Size = new System.Drawing.Size(125, 24);
+            this.comboCategory.TabIndex = 45;
+            // 
+            // label10
+            // 
+            this.label10.Lines = 0;
+            this.label10.Location = new System.Drawing.Point(12, 202);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(154, 23);
+            this.label10.TabIndex = 44;
+            this.label10.Text = "Category.................:";
+            // 
+            // label8
+            // 
+            this.label8.Lines = 0;
+            this.label8.Location = new System.Drawing.Point(12, 163);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(154, 23);
+            this.label8.TabIndex = 41;
+            this.label8.Text = "Brand.......................:";
+            // 
+            // label7
+            // 
+            this.label7.Lines = 0;
+            this.label7.Location = new System.Drawing.Point(12, 121);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(154, 23);
+            this.label7.TabIndex = 40;
+            this.label7.Text = "Season.....................:";
+            // 
+            // DateEstCutting
+            // 
+            this.DateEstCutting.Location = new System.Drawing.Point(177, 85);
+            this.DateEstCutting.Name = "DateEstCutting";
+            this.DateEstCutting.Size = new System.Drawing.Size(280, 23);
+            this.DateEstCutting.TabIndex = 39;
+            // 
+            // DateSewInLine
+            // 
+            this.DateSewInLine.Location = new System.Drawing.Point(177, 47);
+            this.DateSewInLine.Name = "DateSewInLine";
+            this.DateSewInLine.Size = new System.Drawing.Size(280, 23);
+            this.DateSewInLine.TabIndex = 38;
+            // 
+            // dateRange4
+            // 
+            this.dateRange4.Location = new System.Drawing.Point(250, 47);
+            this.dateRange4.Name = "dateRange4";
+            this.dateRange4.Size = new System.Drawing.Size(8, 23);
+            this.dateRange4.TabIndex = 37;
+            // 
+            // DateSCIDelivery
+            // 
+            this.DateSCIDelivery.Location = new System.Drawing.Point(177, 10);
+            this.DateSCIDelivery.Name = "DateSCIDelivery";
+            this.DateSCIDelivery.Size = new System.Drawing.Size(280, 23);
+            this.DateSCIDelivery.TabIndex = 36;
+            // 
+            // label5
+            // 
+            this.label5.Lines = 0;
+            this.label5.Location = new System.Drawing.Point(12, 85);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(154, 23);
+            this.label5.TabIndex = 35;
+            this.label5.Text = "Est. Cutting Date.......:";
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(12, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(154, 23);
+            this.label4.TabIndex = 34;
+            this.label4.Text = "Sewing in-line Date...:";
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(12, 10);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 23);
+            this.label3.TabIndex = 33;
+            this.label3.Text = "SCI Delivery..............:";
+            // 
+            // txtBrand
+            // 
+            this.txtBrand.BackColor = System.Drawing.Color.White;
+            this.txtBrand.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtBrand.Location = new System.Drawing.Point(177, 163);
+            this.txtBrand.Name = "txtBrand";
+            this.txtBrand.Size = new System.Drawing.Size(107, 23);
+            this.txtBrand.TabIndex = 48;
+            // 
+            // txtSeason
+            // 
+            this.txtSeason.BackColor = System.Drawing.Color.White;
+            this.txtSeason.BrandObjectName = null;
+            this.txtSeason.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtSeason.Location = new System.Drawing.Point(177, 121);
+            this.txtSeason.Name = "txtSeason";
+            this.txtSeason.Size = new System.Drawing.Size(135, 23);
+            this.txtSeason.TabIndex = 49;
+            // 
+            // R03
+            // 
+            this.ClientSize = new System.Drawing.Size(627, 346);
+            this.Controls.Add(this.panel1);
+            this.Name = "R03";
+            this.Text = "R03.Material Inspection & Laboratory Status";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.Panel panel1;
+        private Win.UI.DateRange DateEstCutting;
+        private Win.UI.DateRange DateSewInLine;
+        private Win.UI.DateRange dateRange4;
+        private Win.UI.DateRange DateSCIDelivery;
+        private Win.UI.Label label5;
+        private Win.UI.Label label4;
+        private Win.UI.Label label3;
+        private Win.UI.Label label8;
+        private Win.UI.Label label7;
+        private Win.UI.Label label1;
+        private Win.UI.ComboBox comboCategory;
+        private Win.UI.Label label10;
+        private Win.UI.ComboBox comboFactory;
+        private Class.txtbrand txtBrand;
+        private Class.txtseason txtSeason;
+    }
+}
---
+++ Sci.Production.Quality/R03.cs
@@ -0,0 +1,197 @@
+﻿using Ict;
+using Sci.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Quality
+{
+    public partial class R03 : Sci.Win.Tems.PrintForm
+    {
+        DateTime? DateSCIStart; DateTime? DateSCIEnd;
+        DateTime? DateSewStart; DateTime? DateSewEnd;
+        DateTime? DateEstStart; DateTime? DateEstEnd;
+        string Category; string Sea; string Brand; string Factory; 
+        List<SqlParameter> lis; DualResult res;
+        DataTable dt; string cmd;
+        public R03(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            DataTable ORS = null;
+            string sqlm = (@" 
+                        select
+                             Category=name
+                        from  dbo.DropDownList
+                        where type = 'Category' and id != 'O'
+                        ");
+            DBProxy.Current.Select("", sqlm, out ORS);
+            ORS.Rows.Add(new string[] { "" });
+            ORS.DefaultView.Sort = "Category";
+            this.comboCategory.DataSource = ORS;
+            this.comboCategory.ValueMember = "Category";
+            this.comboCategory.DisplayMember = "Category";
+            this.comboCategory.SelectedIndex = 1;
+            DataTable factory;
+            DBProxy.Current.Select(null, "select distinct FTYGroup from Factory order by FTYGroup", out factory);
+            MyUtility.Tool.SetupCombox(comboFactory, 1, factory);
+            comboFactory.Text = Sci.Env.User.Factory;
+            print.Enabled = false;
+        }
+        protected override bool ValidateInput()
+        {
+            bool date_SCI_Empty = !this.DateSCIDelivery.HasValue, date_Sewing_Empty = !this.DateSewInLine.HasValue, date_Est_Empty = !this.DateEstCutting.HasValue,
+                txtSeason_Empty = this.txtSeason.Text.Empty(), txtBrand_Empty = this.txtBrand.Text.Empty() ,comboFactory_Empty = this.comboFactory.Text.Empty();
+            if (date_SCI_Empty && date_Sewing_Empty && date_Est_Empty && txtSeason_Empty && txtBrand_Empty)
+            {
+                MyUtility.Msg.ErrorBox("Please select 'SCI Delivery' or 'Sewing in-line Date' or 'Est. Cutting Date' or 'Season' or 'Brand' at least one field entry");
+
+                DateSCIDelivery.Focus();
+                return false;
+            }
+
+            DateSCIStart = DateSCIDelivery.Value1;
+            DateSCIEnd = DateSCIDelivery.Value2;
+            DateSewStart = DateSewInLine.Value1;
+            DateSewEnd = DateSewInLine.Value2;
+            DateEstStart = DateEstCutting.Value1;
+            DateEstEnd = DateEstCutting.Value2;
+            Sea = txtSeason.Text;
+            Brand = txtBrand.Text;
+            Category = comboCategory.Text;
+            Factory = comboFactory.Text;
+            lis = new List<SqlParameter>();
+            string sqlWhere = "";
+           
+            List<string> sqlWheres = new List<string>();
+            #region --組WHERE--
+            if (!this.DateSCIDelivery.Value1.Empty() && !this.DateSCIDelivery.Value2.Empty())
+            {
+                sqlWheres.Add("SciDelivery between @SCIDate1 and @SCIDate2");
+                lis.Add(new SqlParameter("@SCIDate1", DateSCIStart));
+                lis.Add(new SqlParameter("@SCIDate2", DateSCIEnd));
+            } if (!this.DateSewInLine.Value1.Empty() && !this.DateSewInLine.Value2.Empty())
+            {
+                sqlWheres.Add("SewInLine between @SewDate1 and @SewDate2");
+                lis.Add(new SqlParameter("@SewDate1", DateSewStart));
+                lis.Add(new SqlParameter("@SewDate2", DateSewEnd));
+            } if (!this.DateEstCutting.Value1.Empty() && !this.DateEstCutting.Value2.Empty())
+            {
+                sqlWheres.Add("CutInLine between @Est1 and @Est2");
+                lis.Add(new SqlParameter("@Est1", DateEstStart));
+                lis.Add(new SqlParameter("@Est2", DateEstEnd));
+            } if (!this.txtSeason.Text.Empty())
+            {
+                sqlWheres.Add("SeasonID = @Sea");
+                lis.Add(new SqlParameter("@Sea", Sea));
+            } if (!this.txtBrand.Text.Empty())
+            {
+                sqlWheres.Add("BrandID = @Brand");
+                lis.Add(new SqlParameter("@Brand", Brand));
+            } if (!this.comboCategory.SelectedItem.ToString().Empty())
+            {
+                sqlWheres.Add("Category = @Cate");
+                if (Category == "Bulk")
+                {
+                    lis.Add(new SqlParameter("@Cate", "B"));
+                }
+                if (Category == "Sample")
+                {
+                    lis.Add(new SqlParameter("@Cate", "S"));
+                }
+                if (Category == "Material")
+                {
+                    lis.Add(new SqlParameter("@Cate", "M"));
+                }
+            } if (!this.comboFactory.SelectedItem.ToString().Empty())
+                {
+                    sqlWheres.Add("Factoryid = @Factory");
+                    lis.Add(new SqlParameter("@Factory", Factory));
+                }
+              #endregion
+            sqlWhere = string.Join(" and ", sqlWheres);
+            if (!sqlWhere.Empty())
+            {
+                sqlWhere = " AND " + sqlWhere;
+            }
+            #region --撈ListExcel資料--
+
+            cmd = string.Format(@" 
+            with rawdata as 
+            (
+            select distinct poid from dbo.orders
+            where 1=1
+           " + sqlWhere + @"
+            ) 
+            ,summary as
+            (
+            select a.POID
+            ,(select count(1) total_cnt from dbo.FIR where poid = a.POID) total_cnt
+            ,(select count(1) from dbo.fir f
+            where POID = a.poid and (Result!='' or (f.Result='' and f.nonContinuity =1 and f.Nonphysical = 1 and f.nonShadebond = 1 and f.nonWeight =1))
+            ) insp_cnt
+            ,(select count(1) from dbo.FIR_Laboratory l where POID = a.POID and (l.Result!='' or (l.Result='' and l.nonCrocking=1 and l.nonHeat=1 and l.nonWash=1))
+            ) lab_cnt
+            ,(SELECT count(1) from (select distinct o.poid,q.Article from dbo.orders o inner join dbo.Order_Qty q on q.id =o.id where o.poid = a.poid) t
+            ) total_article_cnt
+            ,(SELECT count(1) from dbo.oven where poid=a.POID) oven_cnt
+            ,(select count(1) from dbo.ColorFastness where poid=a.POID) ColorFastness_cnt
+            ,(select count(1) from dbo.AIR where poid = a.POID) AIR_Total_cnt
+            ,(select count(1) from dbo.AIR where poid = a.POID and air.Status = 'Confirmed' and Result!='') AIR_Insp_Cnt
+            ,(select count(1) from dbo.AIR_Laboratory where poid = a.POID) AIR_Laboratory_Total_cnt
+            ,(select count(1) from dbo.AIR_Laboratory al where poid = a.POID and (result !='' or (al.NonOven =1 and al.NonWash = 1))) AIR_Laboratory_cnt
+             from dbo.FIR a inner join rawdata r on r.POID = a.POID
+            )
+            select distinct O.POID 
+            ,o.FactoryID
+            ,o.BrandID
+            ,o.StyleID
+            ,o.SeasonID
+            ,(select min(orders.CutInLine) from orders where poid = s.POID and orders.Junk = 0) first_cutinline
+            ,getsci.MinSciDelivery
+            ,IIF((select min(orders.CutInLine) from orders where poid = s.POID and orders.Junk = 0) > DATEADD(day,(select 0-system.MtlLeadTime from dbo.system),getsci.MinSciDelivery),DATEADD(day,(select 0-system.MtlLeadTime from dbo.system),getsci.MinSciDelivery),(select min(orders.CutInLine) from orders where poid = s.POID and orders.Junk = 0)) [Target Leadtime]
+            ,iif(s.total_cnt!=0, round(s.insp_cnt*1.0/s.total_cnt,2) * 100 ,0) [Insp %]
+            ,iif(s.total_cnt!=0, round(s.lab_cnt*1.0/s.total_cnt,2) * 100 ,0) [Lab %]
+            ,iif(s.total_article_cnt!=0, round(s.oven_cnt*1.0/s.total_article_cnt,2) * 100 ,0) [Oven %]
+            ,iif(s.total_article_cnt!=0, round(s.ColorFastness_cnt*1.0/s.total_article_cnt,2) * 100 ,0) [ColorFastness %]
+            ,iif(s.AIR_Total_cnt!=0, round(s.AIR_Insp_Cnt*1.0/s.AIR_Total_cnt,2) * 100 ,0) [AIR Insp %]
+            ,iif(s.AIR_Laboratory_Total_cnt!=0, round(s.AIR_Laboratory_cnt*1.0/s.AIR_Laboratory_Total_cnt,2) * 100 ,0) [AIR Lab %]
+            from summary s inner join orders o on o.ID = s.POID
+            cross apply dbo.GetSCI(s.poid,o.Category) getsci");
+            #endregion
+
+            return base.ValidateInput();
+        }
+        protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+         
+            res = DBProxy.Current.Select("", cmd, lis, out dt);
+            if (!res)
+            {
+                return res;
+            }
+            return res;
+        }
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            if (dt == null || dt.Rows.Count == 0)
+            {
+                MyUtility.Msg.ErrorBox("Data not found");
+                return false;
+            }
+            Microsoft.Office.Interop.Excel.Application objApp = MyUtility.Excel.ConnectExcel(Sci.Env.Cfg.XltPathDir + "\\Quality_R03.xltx"); //預先開啟excel app                         
+            MyUtility.Excel.CopyToXls(dt, "", "Quality_R03.xltx", 2, true, null, objApp);      // 將datatable copy to excel
+            Microsoft.Office.Interop.Excel.Worksheet objSheets = objApp.ActiveWorkbook.Worksheets[1];   // 取得工作表
+            if (objSheets != null) Marshal.FinalReleaseComObject(objSheets);    //釋放sheet
+            if (objApp != null) Marshal.FinalReleaseComObject(objApp);          //釋放objApp
+
+            return res;
+        }
+    }
+}
