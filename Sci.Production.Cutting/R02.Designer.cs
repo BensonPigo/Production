f09642ac7edf0e82db1993e04f9530b6d2a347b2
JEFF <jeff@sportscity.com.tw>
2016-09-12 08:25:29 +0000

    updata r02

--- Sci.Production.Cutting/R02.Designer.cs
+++ Sci.Production.Cutting/R02.Designer.cs
@@ -48,6 +48,7 @@
             // 
             // print
             // 
+            this.print.Enabled = false;
             this.print.Location = new System.Drawing.Point(528, 12);
             this.print.Size = new System.Drawing.Size(87, 30);
             // 
