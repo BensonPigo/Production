ffb029986ee285431eff63166e665f32473282be
WENNY <wenny@sportscity.com.tw>
2016-09-13 10:00:05 +0000

    UPDATA

--- Sci.Production.Quality/R05.cs
+++ Sci.Production.Quality/R05.cs
@@ -66,115 +66,147 @@ namespace Sci.Production.Quality
             }
             DateSCIStart = DateSCIDelivery.Value1;
             DateSCIEnd = DateSCIDelivery.Value2;
-            str_Category = comboCategory.Text;
-            str_Material = comboMaterialType.Text;
+            str_Category = comboCategory.Text.ToString();
+            str_Material = comboMaterialType.Text.ToString();
             ReportType = radioPanel.Value.ToString();
 
             lis = new List<SqlParameter>();
-            string sqlWhere = "";
+            string sqlWhere = ""; string sqlOrdersWhere = "";
 
             List<string> sqlWheres = new List<string>();
+            List<string> sqlOrderWheres = new List<string>();
             #region --組WHERE--
             if (!this.DateSCIDelivery.Value1.Empty() && !this.DateSCIDelivery.Value2.Empty())
             {
-                sqlWheres.Add("SciDelivery between @SCIDate1 and @SCIDate2");
+                sqlOrderWheres.Add("SciDelivery between @SCIDate1 and @SCIDate2");
                 lis.Add(new SqlParameter("@SCIDate1", DateSCIStart));
                 lis.Add(new SqlParameter("@SCIDate2", DateSCIEnd));
-            }/* if (!this.DateSewInLine.Value1.Empty() && !this.DateSewInLine.Value2.Empty())
-            {
-                sqlWheres.Add("SewInLine between @SewDate1 and @SewDate2");
-                lis.Add(new SqlParameter("@SewDate1", DateSewStart));
-                lis.Add(new SqlParameter("@SewDate2", DateSewEnd));
-            } if (!this.DateEstCutting.Value1.Empty() && !this.DateEstCutting.Value2.Empty())
-            {
-                sqlWheres.Add("CutInLine between @Est1 and @Est2");
-                lis.Add(new SqlParameter("@Est1", DateEstStart));
-                lis.Add(new SqlParameter("@Est2", DateEstEnd));
-            } if (!this.txtSeason.Text.Empty())
-            {
-                sqlWheres.Add("SeasonID = @Sea");
-                lis.Add(new SqlParameter("@Sea", Sea));
-            } if (!this.txtBrand.Text.Empty())
-            {
-                sqlWheres.Add("BrandID = @Brand");
-                lis.Add(new SqlParameter("@Brand", Brand));
             } if (!this.comboCategory.SelectedItem.ToString().Empty())
             {
-                sqlWheres.Add("Category = @Cate");
-                if (Category == "Bulk")
+                sqlOrderWheres.Add("Category = @Cate");
+                if (str_Category == "Bulk")
                 {
                     lis.Add(new SqlParameter("@Cate", "B"));
-                }
-                if (Category == "Sample")
+                }if (str_Category == "Sample")
                 {
                     lis.Add(new SqlParameter("@Cate", "S"));
                 }
-                if (Category == "Material")
-                {
-                    lis.Add(new SqlParameter("@Cate", "M"));
-                }
-            } if (!this.comboFactory.SelectedItem.ToString().Empty())
+
+            } if (!this.comboMaterialType.SelectedItem.ToString().Empty())
             {
-                sqlWheres.Add("Factoryid = @Factory");
-                lis.Add(new SqlParameter("@Factory", Factory));
-            }*/
+                sqlWheres.Add("psd.FabricType=@FabricType");
+                if (str_Material == "Fabric")
+                {
+                    lis.Add(new SqlParameter("@FabricType", "F"));
+                } if (str_Material == "Accessory")
+                {
+                    lis.Add(new SqlParameter("@FabricType", "A"));
+                }             
+            } 
             #endregion
+            sqlOrdersWhere = string.Join(" and ", sqlOrderWheres);
             sqlWhere = string.Join(" and ", sqlWheres);
             if (!sqlWhere.Empty())
             {
                 sqlWhere = " AND " + sqlWhere;
+            } if (!sqlOrdersWhere.Empty())
+            {
+                sqlOrdersWhere = " AND " + sqlOrdersWhere;
             }
-            #region --撈ListExcel資料--
+            #region --撈Fabric Detail 資料--
 
             cmd = string.Format(@" 
-            with rawdata as 
-            (
-            select distinct poid from dbo.orders
-            where 1=1
-           " + sqlWhere + @"
-            ) 
-            ,summary as
-            (
-            select a.POID
-            ,(select count(1) total_cnt from dbo.FIR where poid = a.POID) total_cnt
-            ,(select count(1) from dbo.fir f
-            where POID = a.poid and (Result!='' or (f.Result='' and f.nonContinuity =1 and f.Nonphysical = 1 and f.nonShadebond = 1 and f.nonWeight =1))
-            ) insp_cnt
-            ,(select count(1) from dbo.FIR_Laboratory l where POID = a.POID and (l.Result!='' or (l.Result='' and l.nonCrocking=1 and l.nonHeat=1 and l.nonWash=1))
-            ) lab_cnt
-            ,(SELECT count(1) from (select distinct o.poid,q.Article from dbo.orders o inner join dbo.Order_Qty q on q.id =o.id where o.poid = a.poid) t
-            ) total_article_cnt
-            ,(SELECT count(1) from dbo.oven where poid=a.POID) oven_cnt
-            ,(select count(1) from dbo.ColorFastness where poid=a.POID) ColorFastness_cnt
-            ,(select count(1) from dbo.AIR where poid = a.POID) AIR_Total_cnt
-            ,(select count(1) from dbo.AIR where poid = a.POID and air.Status = 'Confirmed' and Result!='') AIR_Insp_Cnt
-            ,(select count(1) from dbo.AIR_Laboratory where poid = a.POID) AIR_Laboratory_Total_cnt
-            ,(select count(1) from dbo.AIR_Laboratory al where poid = a.POID and (result !='' or (al.NonOven =1 and al.NonWash = 1))) AIR_Laboratory_cnt
-             from dbo.FIR a inner join rawdata r on r.POID = a.POID
-            )
-            select distinct O.POID 
-            ,o.FactoryID
-            ,o.BrandID
-            ,o.StyleID
-            ,o.SeasonID
-            ,(select min(orders.CutInLine) from orders where poid = s.POID and orders.Junk = 0) first_cutinline
-            ,getsci.MinSciDelivery
-            ,IIF((select min(orders.CutInLine) from orders where poid = s.POID and orders.Junk = 0) > DATEADD(day,(select 0-system.MtlLeadTime from dbo.system),getsci.MinSciDelivery),DATEADD(day,(select 0-system.MtlLeadTime from dbo.system),getsci.MinSciDelivery),(select min(orders.CutInLine) from orders where poid = s.POID and orders.Junk = 0)) [Target Leadtime]
-            ,iif(s.total_cnt!=0, round(s.insp_cnt*1.0/s.total_cnt,2) * 100 ,0) [Insp %]
-            ,iif(s.total_cnt!=0, round(s.lab_cnt*1.0/s.total_cnt,2) * 100 ,0) [Lab %]
-            ,iif(s.total_article_cnt!=0, round(s.oven_cnt*1.0/s.total_article_cnt,2) * 100 ,0) [Oven %]
-            ,iif(s.total_article_cnt!=0, round(s.ColorFastness_cnt*1.0/s.total_article_cnt,2) * 100 ,0) [ColorFastness %]
-            ,iif(s.AIR_Total_cnt!=0, round(s.AIR_Insp_Cnt*1.0/s.AIR_Total_cnt,2) * 100 ,0) [AIR Insp %]
-            ,iif(s.AIR_Laboratory_Total_cnt!=0, round(s.AIR_Laboratory_cnt*1.0/s.AIR_Laboratory_Total_cnt,2) * 100 ,0) [AIR Lab %]
-            from summary s inner join orders o on o.ID = s.POID
-            cross apply dbo.GetSCI(s.poid,o.Category) getsci");
+                with order_rawdata as
+                (
+	                select distinct poid from dbo.orders
+	                where Junk =0 " + sqlOrdersWhere + @"
+                )
+                select 
+                (select p.SuppID+'-'+s.AbbEN from dbo.PO_Supp p inner join dbo.Supp s on s.ID = p.SuppID
+                where p.id = psd.ID and p.seq1 = psd.SEQ1 ) [Supplier]
+                ,psd.Refno
+                ,dbo.getMtlDesc(psd.id,psd.seq1,psd.seq2,1,0) [description]
+                ,(select sum(dbo.getUnitRate(n.PoUnit,'YDS')*n.ShipQty) from dbo.Receiving m inner join dbo.Receiving_Detail n on n.Id = m.Id 
+                where m.id = f.ReceivingID and n.PoId = psd.ID and n.seq1 = psd.seq1 and n.seq2 = psd.SEQ2) [ShipQty]
+                ,(select sum(dbo.getUnitRate(n.StockUnit,'YDS')*n.StockQty) from dbo.Receiving m inner join dbo.Receiving_Detail n on n.Id = m.Id 
+                where m.id = f.ReceivingID and n.PoId = psd.ID and n.seq1 = psd.seq1 and n.seq2 = psd.SEQ2) [ArriveQty]
+                ,f.Physical
+                ,(select WhseArrival from dbo.Receiving where id = f.ReceivingID)[WhseArrival]
+                ,(select scidelivery from dbo.orders where id = a.POID)[scidelivery]
+                ,iif(@category='B',iif(DATEDIFF(day, (select WhseArrival from dbo.Receiving where id = f.ReceivingID),(select scidelivery from dbo.orders where id = a.POID))<25,'Y','')
+		                ,iif(DATEDIFF(day, (select WhseArrival from dbo.Receiving where id = f.ReceivingID),(select scidelivery from dbo.orders where id = a.POID) )<15,'Y','')
+		                ) [Delay]
+                ,psd.ID
+                ,psd.seq1+'-'+psd.seq2[SEQ#]
+                ,(select ExportId from dbo.Receiving where id = f.ReceivingID)[ExportId]
+                ,f.ReceivingID
+                ,(select count(1) from dbo.FIR_Physical x 
+	                inner join dbo.FIR_Physical_Defect y on y.FIR_PhysicalDetailUKey = x.DetailUkey 
+	                where x.ID = f.ID) * 5 [defectYDS]
+                ,f.TotalInspYds
+
+                from order_rawdata a
+                inner join dbo.PO_Supp_Detail psd on psd.ID = a.POID
+                inner join FIR f on f.POID = psd.ID and f.SEQ1 = psd.Seq1 and f.seq2 = psd.Seq2
+                where psd.SEQ1 NOT LIKE '5%'" + sqlWheres);
             #endregion
 
             return base.ValidateInput();
         }
         protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
         {
-            return base.OnAsyncDataLoad(e);
+            DualResult res;
+            res = DBProxy.Current.Select("", cmd, lis, out dt);
+            if (!res)
+            {
+                return res;
+            }
+            return res;
+        }
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            if (dt == null || dt.Rows.Count == 0)
+            {
+                MyUtility.Msg.ErrorBox("Data not found");
+                return false;
+            }
+
+               var saveDialog = Sci.Utility.Excel.MyExcelPrg.GetSaveFileDialog(Sci.Utility.Excel.MyExcelPrg.filter_Excel);
+            saveDialog.ShowDialog();
+            string outpath = saveDialog.FileName;
+            if (outpath.Empty())
+            {
+                return false;
+            }
+
+            if ("Detail".EqualString(this.radioDetail.Text))
+            {
+                Sci.Utility.Excel.SaveXltReportCls xl = new Sci.Utility.Excel.SaveXltReportCls("Quality_R05_FabricDetail.xltx");
+              
+            
+                xl.dicDatas.Add("##BODY", dt);
+                xl.Save(outpath, false);
+            }
+          /*  else if ("Detail List".EqualString(this.comboReport.Text))
+            {
+                Sci.Utility.Excel.SaveXltReportCls xl = new Sci.Utility.Excel.SaveXltReportCls("Subcon_R37_DetailList.xltx");
+                string d1 = (MyUtility.Check.Empty(DebDate1)) ? "" : Convert.ToDateTime(DebDate1).ToString("yyyy/MM/dd");
+                string d2 = (MyUtility.Check.Empty(DebDate2)) ? "" : Convert.ToDateTime(DebDate2).ToString("yyyy/MM/dd");
+                string d3 = (MyUtility.Check.Empty(ConDate1)) ? "" : Convert.ToDateTime(ConDate1).ToString("yyyy/MM/dd");
+                string d4 = (MyUtility.Check.Empty(ConDate2)) ? "" : Convert.ToDateTime(ConDate2).ToString("yyyy/MM/dd");
+                string d5 = (MyUtility.Check.Empty(SettDate1)) ? "" : Convert.ToDateTime(SettDate1).ToString("yyyy/MM/dd");
+                string d6 = (MyUtility.Check.Empty(SettDate2)) ? "" : Convert.ToDateTime(SettDate2).ToString("yyyy/MM/dd");
+                xl.dicDatas.Add("##Debitdate", d1 + "~" + d2);
+                xl.dicDatas.Add("##ConfirmDate", d3 + "~" + d4);
+                xl.dicDatas.Add("##SettledDate", d5 + "~" + d6);
+                xl.dicDatas.Add("##DebitN", DebitNo1 + "~" + DebitNo2);
+                xl.dicDatas.Add("##Handle", handle);
+                xl.dicDatas.Add("##SMR", smr);
+                xl.dicDatas.Add("##Fac", fac);
+                xl.dicDatas.Add("##Pay", Pay);
+                xl.dicDatas.Add("##Deb", dt);
+                xl.Save(outpath, false);*/
+            return true;
         }
     }
 }
