d93ef597db0dfe32b972d41ab0bf32a3d59bd635
WILLY <willy@sportscity.com.tw>
2016-10-06 08:02:26 +0000

    修復QA bug

--- Sci.Production.Quality/P01_Continuity.cs
+++ Sci.Production.Quality/P01_Continuity.cs
@@ -443,8 +443,8 @@ namespace Sci.Production.Quality
             #region Excel Grid Value
             DataTable dt;
             DualResult xresult;
-            // if (xresult = DBProxy.Current.Select("Production", string.Format("select Roll,Dyelot,Scale,Result,Inspdate,Inspector,Remark from FIR_Continuity where id='{0}'", textID.Text), out dt))
-            if (xresult = DBProxy.Current.Select("Production", "select Roll,Dyelot,Scale,Result,Inspdate,Inspector,Remark from FIR_Continuity  where id in ('489066')", out dt)) //測試用
+             if (xresult = DBProxy.Current.Select("Production", string.Format("select Roll,Dyelot,Scale,Result,Inspdate,Inspector,Remark from FIR_Continuity where id='{0}'", textID.Text), out dt))
+            //if (xresult = DBProxy.Current.Select("Production", "select Roll,Dyelot,Scale,Result,Inspdate,Inspector,Remark from FIR_Continuity  where id in ('489066')", out dt)) //測試用
             {
                 if (dt.Rows.Count <= 0)
                 {
@@ -456,10 +456,10 @@ namespace Sci.Production.Quality
             #region Excel 表頭值
             DataTable dt1;
             DualResult xresult1;
-            if (xresult1 = DBProxy.Current.Select("Production",
-                "select Roll,Dyelot,Scale,a.Result,a.Inspdate,Inspector,a.Remark,B.ContinuityEncode,C.SeasonID from FIR_Continuity a left join FIR b on a.ID=b.ID LEFT JOIN ORDERS C ON B.POID=C.ID where a.ID in ('489066')", out dt1))//測試用
-            //if (xresult1 = DBProxy.Current.Select("Production", string.Format(
-            //"select Roll,Dyelot,Scale,a.Result,a.Inspdate,Inspector,a.Remark,B.ContinuityEncode,C.SeasonID from FIR_Continuity a left join FIR b on a.ID=b.ID LEFT JOIN ORDERS C ON B.POID=C.ID where a.ID='{0}'", textID.Text), out dt1))
+            //if (xresult1 = DBProxy.Current.Select("Production",
+              //  "select Roll,Dyelot,Scale,a.Result,a.Inspdate,Inspector,a.Remark,B.ContinuityEncode,C.SeasonID from FIR_Continuity a left join FIR b on a.ID=b.ID LEFT JOIN ORDERS C ON B.POID=C.ID where a.ID in ('489066')", out dt1))//測試用
+            if (xresult1 = DBProxy.Current.Select("Production", string.Format(
+            "select Roll,Dyelot,Scale,a.Result,a.Inspdate,Inspector,a.Remark,B.ContinuityEncode,C.SeasonID from FIR_Continuity a left join FIR b on a.ID=b.ID LEFT JOIN ORDERS C ON B.POID=C.ID where a.ID='{0}'", textID.Text), out dt1))
             {
                 if (dt1.Rows.Count <= 0)
                 {
