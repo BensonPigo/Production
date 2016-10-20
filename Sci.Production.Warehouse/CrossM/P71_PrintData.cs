4fcffdd31f747bfb7f40b17861a9364864c686f3
WENNY <wenny@sportscity.com.tw>
2016-10-20 07:06:27 +0000

    add

---
+++ Sci.Production.Warehouse/CrossM/P71_PrintData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sci.Production.Warehouse.CrossM
+{
+    class P71_PrintData
+    {
+        public string SPNo { get; set; }
+        public string BulkSeq { get; set; }
+        public string DESC { get; set; }
+        public string InventorySP { get; set; }
+        public string InventorySeq { get; set; }
+        public string StockUnit { get; set; }
+        public string QTY { get; set; }
+       
+    }
+}
