2d63e519b088f5660d2d130a7e3505d9ec177b6f
JEFF <jeff@sportscity.com.tw>
2016-10-20 02:53:02 +0000

    Save 完成

--- Sci.Production.Thread/P01_Detail.cs
+++ Sci.Production.Thread/P01_Detail.cs
@@ -205,8 +205,9 @@ namespace Sci.Production.Thread
 	                        select t.ThreadColorid as {0}
 	                        from ThreadColorComb_Detail t
 	                        where Machinetypeid=MT.ID
+                            and SEQ = MT.SEQ
 	                        and Article='{0}'
-                        )TC{1}",tbArticle.Rows[i][0].ToString().Trim(),i));
+                        )TC{1}", tbArticle.Rows[i][0].ToString().Trim(),i));
             }
             sql = string.Format(@"
                     select
@@ -214,15 +215,18 @@ namespace Sci.Production.Thread
                         MT.ThreadLocation,
                         MT.UseRatioNumeric,
                         MT.Allowance,
+                        TD.id,
                         TD.Refno
                         {1}
-                    from MachineType_ThreadRatio MT
+                    from MachineType_ThreadRatio MT 
                     outer apply (
-	                    select t.Refno
+	                    select t.Refno,t.id
 	                    from ThreadColorComb_Detail t
 	                    where Machinetypeid=MT.ID
+						and SEQ = MT.SEQ
+                        group by t.Refno,t.id
                     )TD
-                    {2}
+					{2}
                     where MT.ID='{0}'", detailRow["Machinetypeid"].ToString(), art_col,op.ToString());
             dResult = DBProxy.Current.Select(null, sql, out gridTable);
             if (!dResult)
@@ -270,114 +274,154 @@ namespace Sci.Production.Thread
             else
             {
                 grid1.ValidateControl();
-                string article, seekSql, refnoSql;
-                string delSql = "", updateSql = "", insertSql = "";
-                bool msg = true; //判斷是否要Show error
-                bool linsert = true; //判斷若僅輸入Refno 就必須新增一筆Data
-                DataRow seekRow;
+                //string article, seekSql, refnoSql;
+                //string delSql = "", updateSql = "", insertSql = "";
+                //bool msg = true; //判斷是否要Show error
+                //bool linsert = true; //判斷若僅輸入Refno 就必須新增一筆Data
+                //DataRow seekRow;
                 DataTable refDT;
-                refnoSql = string.Format("Select ukey,refno from ThreadColorComb_Detail where id ='{0}' group by ukey,refno ", detailRow["ID"].ToString());
-                DualResult dResult = DBProxy.Current.Select(null, refnoSql, out refDT);
-                if (!dResult)
-                {
-                    ShowErr(refnoSql, dResult);
-                }
+                StringBuilder insertSql = new StringBuilder();
+                StringBuilder updateSql = new StringBuilder();
+                StringBuilder delSql = new StringBuilder();
+                string refnoSql;
+
                 foreach (DataRow dr in gridTable.Rows)
                 {
-                    if (!MyUtility.Check.Empty(dr["Refno"])) linsert = true; //判斷若僅輸入Refno 就必須新增一筆Data
-                    else linsert = false;
-                    msg = true;
-                    if (dr.RowState == DataRowState.Modified)
+                    //判斷Ref是否為空  以及後面的column是否全為空值
+                    if (MyUtility.Check.Empty(dr["Refno"]))
                     {
-                        foreach (DataRow drcol in tbArticle.Rows)
+                        int i;
+                        for (i = 0; i < tbArticle.Rows.Count; i++)
                         {
-                            article = drcol["article"].ToString().Trim(); 
-                            if (MyUtility.Check.Empty(dr["Refno"]) && !MyUtility.Check.Empty(dr[article]))
-                            {
-                                msg = false; //有color無Refno 不可產生
-                            }
-                            if (!MyUtility.Check.Empty(dr["Refno"]) && !MyUtility.Check.Empty(dr[article]))
-                            {
-                                linsert = false; //只要有Color 就不需要新增
-                            }
-                            if (MyUtility.Check.Empty(dr[article + "Ukey"]))
-                            {
-                                if (!MyUtility.Check.Empty(dr[article])) //Color 有值就新增空白Ukey
-                                {
-                                    insertSql = insertSql + string.Format("Insert into ThreadColorComb_Detail(id,Machinetypeid,ThreadCombid,Refno,Article,ThreadColorid,SEQ,ThreadLocationID) Values({0},'{1}','{2}','{3}','{4}','{5}','{6}','{7}');", detailRow["ID"], detailRow["Machinetypeid"], detailRow["Threadcombid"], dr["Refno"], article, dr[article], dr["SEQ"], dr["ThreadLocation"]);
-                                }
-                            }
-                            else
-                            {
-                                seekSql = string.Format("Select * from ThreadColorComb_Detail where Ukey = {0}", dr[article + "Ukey"]);
-                                if (MyUtility.Check.Seek(seekSql, out seekRow)) //存在Ukey 判斷只要有不同的Refno,Color 就update
-                                {
-                                    if (seekRow["ThreadColorid"].ToString() != dr[article].ToString() || seekRow["Refno"].ToString() != dr["Refno"].ToString())
-                                    {
-                                        if (MyUtility.Check.Empty(dr["Refno"]) && MyUtility.Check.Empty(dr[article]))
-                                        { //只要Refno 跟Color 都是空白就刪除資料 否則就更新
-                                            delSql = delSql + string.Format("Delete from ThreadColorComb_Detail where Ukey = {0}", dr[article + "Ukey"]);
-                                        }
-                                        else
-                                        {
-                                            updateSql = updateSql + string.Format("Update ThreadColorComb_Detail set threadcolorid ='{0}', Refno='{1}' where Ukey = {2};", dr[article].ToString(), dr["Refno"].ToString(), dr[article + "Ukey"]);
-                                        }
-
-                                    }
-                                }
-
-                            }
+                            if (!MyUtility.Check.Empty(dr[i + 6])) break;//只要有任一筆不為空,跳出for迴圈
+                        }
+                        if (i == tbArticle.Rows.Count)//相等表示判斷剩下的column全為空
+                        {
+                            delSql.Append(string.Format("Delete from ThreadColorComb_Detail where id = '{0}' and seq='{1}' and ThreadLocationID='{2}'", detailRow["id"].ToString(), dr["SEQ"].ToString(), dr["ThreadLocation"].ToString()));
+                            continue;
                         }
                     }
-                    if (!msg)
+
+                    refnoSql = string.Format("Select * from ThreadColorComb_Detail where id = '{0}' and seq='{1}' and ThreadLocationID='{2}'", detailRow["id"].ToString(), dr["SEQ"].ToString(), dr["ThreadLocation"].ToString());
+                    DualResult dResult = DBProxy.Current.Select(null, refnoSql, out refDT);
+                    if (!dResult)
                     {
-                        MyUtility.Msg.WarningBox("If has thread color, thread refno can't empty.");
-                        return;
+                        ShowErr(refnoSql, dResult);
                     }
-                    if (linsert)
+
+                    if (refDT.Rows.Count != 0)
                     {
-                        seekSql = string.Format(@"Select * from ThreadColorComb_Detail 
-                        where id = {0} and Machinetypeid = '{1}' and ThreadCombid='{2}' and Refno = '{3}' and 
-                        Article = '{4}' and SEQ ='{5}' and ThreadLocationID= '{6}'", 
-                        detailRow["ID"], detailRow["Machinetypeid"], detailRow["Threadcombid"], 
-                        dr["Refno"], tbArticle.Rows[0]["article"].ToString().Trim(), dr["SEQ"], dr["ThreadLocation"]);
-                        if (!MyUtility.Check.Seek(seekSql)) //存在Ukey 判斷只要有不同的Refno,Color 就update
+                        for (int i = 0; i < tbArticle.Rows.Count; i++)
                         {
-                            insertSql = insertSql + string.Format("Insert into ThreadColorComb_Detail(id,Machinetypeid,ThreadCombid,Refno,Article,SEQ,ThreadLocationID) Values({0},'{1}','{2}','{3}','{4}','{5}','{6}');", detailRow["ID"], detailRow["Machinetypeid"], detailRow["Threadcombid"], dr["Refno"], tbArticle.Rows[0]["article"].ToString().Trim(), dr["SEQ"], dr["ThreadLocation"]);
+                            updateSql.Append(string.Format("Update ThreadColorComb_Detail set Refno='{0}', threadcolorid ='{1}' where id = '{2}' and Article = '{3}' and SEQ = '{4}' and ThreadLocationID='{5}'", dr["Refno"].ToString(), dr[tbArticle.Rows[i]["article"].ToString().Trim()].ToString(), detailRow["id"].ToString(), tbArticle.Rows[i]["article"].ToString().Trim(), dr["SEQ"].ToString(), dr["ThreadLocation"].ToString()));
+                        }
+                    }
+                    if (refDT.Rows.Count == 0)
+                    {
+                        //Ref有值且無資料
+                        for (int i = 0; i < tbArticle.Rows.Count; i++)
+                        {
+                            insertSql.Append(string.Format(@"Insert into ThreadColorComb_Detail(id,Machinetypeid,ThreadCombid,Refno,Article,ThreadColorid,SEQ,ThreadLocationID) Values({0},'{1}','{2}','{3}','{4}','{5}','{6}','{7}');", detailRow["ID"], detailRow["Machinetypeid"], detailRow["Threadcombid"], dr["Refno"], tbArticle.Rows[i]["article"].ToString().Trim(), dr[tbArticle.Rows[i]["article"].ToString().Trim()], dr["SEQ"], dr["ThreadLocation"]));
                         }
                     }
                 }
+                
+//                foreach (DataRow dr in gridTable.Rows)
+//                {
+//                    if (!MyUtility.Check.Empty(dr["Refno"])) linsert = true; //判斷若僅輸入Refno 就必須新增一筆Data
+//                    else linsert = false;
+//                    msg = true;
+//                    if (dr.RowState == DataRowState.Modified)
+//                    {
+//                        foreach (DataRow drcol in tbArticle.Rows)
+//                        {
+//                            article = drcol["article"].ToString().Trim(); 
+//                            if (MyUtility.Check.Empty(dr["Refno"]) && !MyUtility.Check.Empty(dr[article]))
+//                            {
+//                                msg = false; //有color無Refno 不可產生
+//                            }
+//                            if (!MyUtility.Check.Empty(dr["Refno"]) && !MyUtility.Check.Empty(dr[article]))
+//                            {
+//                                linsert = false; //只要有Color 就不需要新增
+//                            }
+//                            if (MyUtility.Check.Empty(dr[article + "Ukey"]))
+//                            {
+//                                if (!MyUtility.Check.Empty(dr[article])) //Color 有值就新增空白Ukey
+//                                {
+//                                    insertSql = insertSql + string.Format("Insert into ThreadColorComb_Detail(id,Machinetypeid,ThreadCombid,Refno,Article,ThreadColorid,SEQ,ThreadLocationID) Values({0},'{1}','{2}','{3}','{4}','{5}','{6}','{7}');", detailRow["ID"], detailRow["Machinetypeid"], detailRow["Threadcombid"], dr["Refno"], article, dr[article], dr["SEQ"], dr["ThreadLocation"]);
+//                                }
+//                            }
+//                            else
+//                            {
+//                                seekSql = string.Format("Select * from ThreadColorComb_Detail where Ukey = {0}", dr[article + "Ukey"]);
+//                                if (MyUtility.Check.Seek(seekSql, out seekRow)) //存在Ukey 判斷只要有不同的Refno,Color 就update
+//                                {
+//                                    if (seekRow["ThreadColorid"].ToString() != dr[article].ToString() || seekRow["Refno"].ToString() != dr["Refno"].ToString())
+//                                    {
+//                                        if (MyUtility.Check.Empty(dr["Refno"]) && MyUtility.Check.Empty(dr[article]))
+//                                        { //只要Refno 跟Color 都是空白就刪除資料 否則就更新
+//                                            delSql = delSql + string.Format("Delete from ThreadColorComb_Detail where Ukey = {0}", dr[article + "Ukey"]);
+//                                        }
+//                                        else
+//                                        {
+//                                            updateSql = updateSql + string.Format("Update ThreadColorComb_Detail set threadcolorid ='{0}', Refno='{1}' where Ukey = {2};", dr[article].ToString(), dr["Refno"].ToString(), dr[article + "Ukey"]);
+//                                        }
+
+//                                    }
+//                                }
+
+//                            }
+//                        }
+//                    }
+//                    if (!msg)
+//                    {
+//                        MyUtility.Msg.WarningBox("If has thread color, thread refno can't empty.");
+//                        return;
+//                    }
+//                    if (linsert)
+//                    {
+//                        seekSql = string.Format(@"Select * from ThreadColorComb_Detail 
+//                        where id = {0} and Machinetypeid = '{1}' and ThreadCombid='{2}' and Refno = '{3}' and 
+//                        Article = '{4}' and SEQ ='{5}' and ThreadLocationID= '{6}'", 
+//                        detailRow["ID"], detailRow["Machinetypeid"], detailRow["Threadcombid"], 
+//                        dr["Refno"], tbArticle.Rows[0]["article"].ToString().Trim(), dr["SEQ"], dr["ThreadLocation"]);
+//                        if (!MyUtility.Check.Seek(seekSql)) //存在Ukey 判斷只要有不同的Refno,Color 就update
+//                        {
+//                            insertSql = insertSql + string.Format(@"Insert into ThreadColorComb_Detail(id,Machinetypeid,ThreadCombid,Refno,Article,ThreadColorid,SEQ,ThreadLocationID) Values({0},'{1}','{2}','{3}','{4}','{5}','{6}','{7}');", detailRow["ID"], detailRow["Machinetypeid"], detailRow["Threadcombid"], dr["Refno"], tbArticle.Rows[i]["article"].ToString().Trim(), dr[tbArticle.Rows[i]["article"].ToString().Trim()], dr["SEQ"], dr["ThreadLocation"]);
+//                        }
+//                    }
+//                }
                 DualResult result;
                 TransactionScope _transactionscope = new TransactionScope();
                 using (_transactionscope)
                 {
                     try
                     {
-                        if (!MyUtility.Check.Empty(delSql))
+                        if (!MyUtility.Check.Empty(delSql.ToString()))
                         {
-                            result = DBProxy.Current.Execute(null, delSql);
+                            result = DBProxy.Current.Execute(null, delSql.ToString());
                             if (!result)
                             {
-                                ShowErr(delSql, result);
+                                ShowErr(delSql.ToString(), result);
                                 return;
                             }
                         }
-                        if (!MyUtility.Check.Empty(updateSql))
+                        if (!MyUtility.Check.Empty(updateSql.ToString()))
                         {
-                            result = DBProxy.Current.Execute(null, updateSql);
+                            result = DBProxy.Current.Execute(null, updateSql.ToString());
                             if (!result)
                             {
-                                ShowErr(updateSql, result);
+                                ShowErr(updateSql.ToString(), result);
                                 return;
                             }
                         }
-                        if (!MyUtility.Check.Empty(insertSql))
+                        if (!MyUtility.Check.Empty(insertSql.ToString()))
                         {
 
-                            result = DBProxy.Current.Execute(null, insertSql);
+                            result = DBProxy.Current.Execute(null, insertSql.ToString());
                             if (!result)
                             {
-                                ShowErr(insertSql, result);
+                                ShowErr(insertSql.ToString(), result);
                                 return;
                             }
                         }
