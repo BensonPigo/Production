47ff7ed9e2a7be90d53574e36e43f0a5fa1b1d02
JEFF <jeff@sportscity.com.tw>
2016-10-27 06:18:19 +0000

    摘要0000650: THREAD_P01_Generate_Generate 說明 雙擊Thread Combination可帶出下一層ThreadColorcomb_Operation的資料，唯讀不可編輯，欄位： Operation : ThreadColorComb_Operation.OperationId Description:Operation.Description SeamLength: Operation.SeamLength (key: Operation.Id = ThreadColorComb_Operation.OperationId) 把P01從input6改成input8 新增一支繼承標單P01_Operation(input8A) (測試確認-完成)

---
+++ Sci.Production.Thread/P01_Operation.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Sci.Production.Thread
+{
+    partial class P01_Operation
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).BeginInit();
+            this.btmcont.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // append
+            // 
+            this.append.Visible = false;
+            // 
+            // save
+            // 
+            this.save.Visible = false;
+            // 
+            // revise
+            // 
+            this.revise.Visible = false;
+            // 
+            // delete
+            // 
+            this.delete.Visible = false;
+            // 
+            // P01_Operation
+            // 
+            this.ClientSize = new System.Drawing.Size(677, 395);
+            this.Name = "P01_Operation";
+            this.Text = "ThreadColorComb_Operation";
+            this.WorkAlias = "ThreadColorComb_Operation";
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).EndInit();
+            this.btmcont.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Sci.Production.Thread/P01_Operation.cs
@@ -0,0 +1,29 @@
+﻿using Ict.Win;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Thread
+{
+    public partial class P01_Operation : Sci.Win.Subs.Input8A
+    {
+        public P01_Operation()
+        {
+            InitializeComponent();
+        }
+        protected override bool OnGridSetup()
+        {
+            #region set grid
+            Helper.Controls.Grid.Generator(this.grid)
+           .Text("Operationid", header: "Operationid", width: Widths.AnsiChars(20), iseditingreadonly: true)
+           .Text("DescEN", header: "Thread Comb Desc", width: Widths.AnsiChars(15), iseditingreadonly: true)
+           .Numeric("Seamlength", header: "Seam Length", width: Widths.AnsiChars(6), integer_places: 6, decimal_places: 2, iseditingreadonly: true);
+            #endregion
+            return true;
+        }
+    }
+}
