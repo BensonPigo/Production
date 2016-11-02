75f30180f74dab83f09c9c526112ea7dfe6b01d9
ALGER <alger@sportscity.com.tw>
2016-11-02 04:03:02 +0000

    UPDATE.

--- Database/dbo/Views/View_TransactionList.sql
+++ Database/dbo/Views/View_TransactionList.sql
@@ -1,86 +1,172 @@
 ﻿
 CREATE view [dbo].[View_TransactionList]
-as
-select 'Receiving' tbname,a.Type,a.Status,a.EditDate,b.Id,b.MDivisionID,b.PoId,b.seq1,b.seq2,b.Roll,b.Dyelot
-,b.StockQty as inqty							,0.0 as outqty													,0.0 as adjustqty 
-,iif(b.StockType='B',b.StockQty,0.0) as AInqty	,0.00 AOutqty													,0.00 as AAdjustQty
-,iif(b.StockType='I',b.StockQty,0.0) as BInqty	,0.00 BOutqty													,0.00 as BAdjustQty
-,0.0 as CInqty									,0.00 COutqty													,0.00 CAdjustQty
-from dbo.Receiving a inner join dbo.Receiving_Detail b on b.id = a.id 
-union all
-select iif(a.type <'E','Issue','RequestCrossM-Issue'),a.Type,a.Status,a.EditDate,b.id,b.MDivisionID,b.POID,b.seq1,b.seq2,b.Roll,b.Dyelot
-,0.0 as inqty									,b.Qty as outqty												,0.0 as adjustqty 
-,0.0 as AInqty									,iif(b.StockType = 'B',b.Qty,0.00) as AOutQty					,0.00 AAdjustQty
-,0.0 as BInqty									,iif(b.StockType = 'I',b.Qty,0.00) as BOutQty					,0.00 BAdjustQty
-,0.0 AS CInqty									,0.00 COutQty													,0.00 CAdjustQty
-from dbo.Issue a inner join dbo.Issue_Detail b on b.id = a.id 
-union all
-select 'IssueReturn','',a.Status,a.EditDate,b.ID,b.MDivisionID,b.POID,b.Seq1,b.Seq2,b.Roll,b.Dyelot
-,0.0 as inqty								,0.0 as outqty														,iif(b.StockType!='O',b.qty,0.0) as adjustqty
-,0.0 as AInqty								,0.00 as AOutqty													,iif(b.StockType='B',b.qty,0.00) as AAdjuqtQty
-,0.0 as BInqty								,0.00 as BOutqty													,iif(b.StockType='I',b.qty,0.00) as BAdjuqtQty
-,0.0 as CInqty								,0.00 as COutqty													,iif(b.StockType='O',b.qty,0.00) as CAdjuqtQty
-from dbo.IssueReturn a inner join dbo.IssueReturn_Detail b on b.id = a.id 
-union all
-select 'SubTransfer-From',a.Type,a.Status,a.EditDate,b.Id,b.FromMDivisionID,b.FromPoId,b.Fromseq1,b.Fromseq2,b.FromRoll,b.FromDyelot 
-,iif(b.FromStockType = 'O',b.Qty,0.00) as inqty	,b.Qty as outqty												,0.0 as adjustqty 
-,0.0 as AInqty									,iif(b.FromStockType = 'B',b.Qty,0.00) as AOutQty				,0.00 AAdjustQty
-,0.0 as BInqty									,iif(b.FromStockType = 'I',b.Qty,0.00) as BOutQty				,0.00 BAdjustQty
-,0.0 AS CInqty									,iif(b.FromStockType = 'O',b.Qty,0.00) as COutQty				,0.00 CAdjustQty
-from dbo.SubTransfer a inner join dbo.SubTransfer_Detail b on b.id = a.id 
-union all
-select 'SubTransfer-To',a.Type,a.Status,a.EditDate,b.Id,b.ToMDivisionID,b.ToPoId,b.ToSeq1,b.ToSeq2,b.ToRoll,b.ToDyelot 
-,iif(b.ToStockType!='O',b.Qty,0.0) as inqty		,0.0 as outqty													,0.0 as adjustqty 
-,iif(b.ToStockType='B',b.Qty,0.0) as AInqty		,0.00 as AOutqty												,0.00 AAdjuqtQty
-,iif(b.ToStockType='I',b.Qty,0.0) as BInqty		,0.00 as BOutqty												,0.00 BAdjuqtQty
-,iif(b.ToStockType='O',b.Qty,0.0) as CInqty		,0.00 as COutqty												,0.00 CAdjuqtQty
-from dbo.SubTransfer a inner join dbo.SubTransfer_Detail b on b.id = a.id 
-union all
-select 'TransferIn','',a.Status,a.EditDate,b.id,b.MDivisionID,b.POID,b.seq1,b.seq2,b.Roll,b.Dyelot
-,iif(b.StockType!='O',b.Qty,0.0) as inqty	,0.0 as outqty														,0.0 as adjustqty 
-,iif(b.StockType='B',b.Qty,0.0) as AInqty	,0.00 as AOutqty													,0.00 as AAdjuqtQty
-,iif(b.StockType='I',b.Qty,0.0) as BInqty	,0.00 as BOutqty													,0.00 as BAdjuqtQty
-,iif(b.StockType='O',b.Qty,0.0) as CInqty	,0.00 as COutqty													,0.00 as CAdjuqtQty
-from dbo.TransferIn a inner join dbo.TransferIn_Detail b on b.ID = a.id 
-union all
-select 'TransferOut','',a.Status,a.EditDate,b.id,b.MDivisionID,b.POID,b.seq1,b.seq2,b.Roll,b.Dyelot
-,0.0 as inqty								,iif(b.stocktype!='O',b.Qty,0.0) as outqty							,0.0 as adjustqty 
-,0.0 as AInqty								,iif(b.StockType='B',b.Qty,0.00) as AOutqty							,0.00 as AAdjuqtQty
-,0.0 as BInqty								,iif(b.StockType='I',b.Qty,0.00) as BOutqty							,0.00 as BAdjuqtQty
-,0.0 as CInqty								,iif(b.StockType='O',b.Qty,0.00) as COutqty							,0.00 as CAdjuqtQty
-from dbo.TransferOut a inner join dbo.TransferOut_Detail b on b.ID = a.id 
-union all
-select 'Adjust',a.Type,a.Status,a.EditDate,b.ID,b.MDivisionID,b.POID,b.Seq1,b.Seq2,b.Roll,b.Dyelot
-,0.0 as inqty								,0.0 as outqty														,iif(b.StockType!='O',b.qtyafter - b.qtybefore,0.0) as adjustqty
-,0.0 as AInqty								,0.00 as AOutqty													,iif(b.StockType='B',b.qtyafter - b.qtybefore,0.00) as AAdjuqtQty
-,0.0 as BInqty								,0.00 as BOutqty													,iif(b.StockType='I',b.qtyafter - b.qtybefore,0.00) as BAdjuqtQty
-,0.0 as CInqty								,0.00 as COutqty													,iif(b.StockType='O',b.qtyafter - b.qtybefore,0.00) as CAdjuqtQty
-from dbo.Adjust a inner join dbo.Adjust_Detail b on b.id = a.id 
-union all
-select 'BorrowBack-From',a.Type,a.Status,a.EditDate,b.Id,b.FromMDivisionID,b.FromPoId,b.Fromseq1,b.Fromseq2,b.FromRoll,b.FromDyelot 
-,iif(b.FromStockType = 'O',b.Qty,0.00) as inqty	,b.Qty as outqty												,0.0 as adjustqty 
-,0.0 as AInqty									,iif(b.FromStockType = 'B',b.Qty,0.00) as AOutQty				,0.00 AAdjustQty
-,0.0 as BInqty									,iif(b.FromStockType = 'I',b.Qty,0.00) as BOutQty				,0.00 BAdjustQty
-,0.0 AS CInqty									,iif(b.FromStockType = 'O',b.Qty,0.00) as COutQty				,0.00 CAdjustQty
-from dbo.BorrowBack a inner join dbo.BorrowBack_Detail b on b.id = a.id 
-union all
-select 'BorrowBack-To',a.Type,a.Status,a.EditDate,b.Id,b.ToMDivisionID,b.ToPoId,b.ToSeq1,b.ToSeq2,b.ToRoll,b.ToDyelot 
-,iif(b.ToStockType!='O',b.Qty,0.0) as inqty		,0.0 as outqty													,0.0 as adjustqty 
-,iif(b.ToStockType='B',b.Qty,0.0) as AInqty		,0.00 as AOutqty												,0.00 AAdjuqtQty
-,iif(b.ToStockType='I',b.Qty,0.0) as BInqty		,0.00 as BOutqty												,0.00 BAdjuqtQty
-,iif(b.ToStockType='O',b.Qty,0.0) as CInqty		,0.00 as COutqty												,0.00 CAdjuqtQty
-from dbo.BorrowBack a inner join dbo.BorrowBack_Detail b on b.id = a.id 
-union all
-select 'IssueLack',a.Type,a.Status,a.EditDate,b.id,b.MDivisionID,b.POID,b.seq1,b.seq2,b.Roll,b.Dyelot
-,0.0 as inqty									,b.Qty as outqty												,0.0 as adjustqty 
-,0.0 as AInqty									,iif(b.StockType = 'B',b.Qty,0.00) as AOutQty					,0.00 AAdjustQty
-,0.0 as BInqty									,iif(b.StockType = 'I',b.Qty,0.00) as BOutQty					,0.00 BAdjustQty
-,0.0 AS CInqty									,0.00 COutQty													,0.00 CAdjustQty
-from dbo.IssueLack a inner join dbo.IssueLack_Detail b on b.id = a.id where a.Type='R'
-union all
-select 'RequestCrossM-Receive' tbname,a.Type,a.Status,a.EditDate,b.Id,b.MDivisionID,b.PoId,b.seq1,b.seq2,b.Roll,b.Dyelot
-,b.Qty as inqty									,0.0 as outqty													,0.0 as adjustqty 
-,iif(b.StockType='B',b.Qty,0.0) as AInqty		,0.00 AOutqty													,0.00 as AAdjuqtQty
-,iif(b.StockType='I',b.Qty,0.0) as BInqty		,0.00 BOutqty													,0.00 as BAdjuqtQty
-,0.0 as CInqty									,0.00 COutqty													,0.00 CAdjuqtQty
-from dbo.RequestCrossM a inner join dbo.RequestCrossM_Receive b on b.id = a.id
+AS
+SELECT          'Receiving' tbname, a.Type, a.Status, a.EditDate, b.Id, b.MDivisionID, b.PoId, b.seq1, b.seq2, b.Roll, b.Dyelot, 
+                            b.StockQty AS inqty, 0.0 AS outqty, 0.0 AS adjustqty, iif(b.StockType = 'B', b.StockQty, 0.0) AS AInqty, 0.00 AOutqty, 
+                            0.00 AS AAdjustQty, iif(b.StockType = 'I', b.StockQty, 0.0) AS BInqty, 0.00 BOutqty, 0.00 AS BAdjustQty, 0.0 AS CInqty, 
+                            0.00 AS COutqty, 0.00 AS CAdjustQty
+FROM              dbo.Receiving a INNER JOIN
+                            dbo.Receiving_Detail b ON b.id = a.id
+UNION ALL
+SELECT          iif(a.type < 'E', 'Issue', 'RequestCrossM-Issue'), a.Type, a.Status, a.EditDate, b.Id, b.MDivisionID, b.PoId, b.seq1, 
+                            b.seq2, b.Roll, b.Dyelot, 0.0 AS inqty, b.Qty AS outqty, 0.0 AS adjustqty, 0.0 AS AInqty, iif(b.StockType = 'B', b.Qty, 
+                            0.00) AS AOutqty, 0.00 AS AAdjustQty, 0.0 AS BInqty, iif(b.StockType = 'I', b.Qty, 0.00) AS BOutqty, 0.00 AS BAdjustQty, 
+                            0.0 AS CInqty, 0.00 AS COutqty, 0.00 AS CAdjustQty
+FROM              dbo.Issue a INNER JOIN
+                            dbo.Issue_Detail b ON b.id = a.id
+UNION ALL
+SELECT          'IssueReturn' tbname, '', a.Status, a.EditDate, b.Id, b.MDivisionID, b.PoId, b.seq1, b.seq2, b.Roll, b.Dyelot, 
+                            0.0 AS inqty, 0 - b.Qty AS outqty, 0.0 AS adjustqty, 0.0 AS AInqty, iif(b.StockType = 'B', 0 - b.Qty, 0.00) AS AOutqty, 
+                            0.00 AS AAdjustQty, 0.0 AS BInqty, iif(b.StockType = 'I', 0 - b.Qty, 0.00) AS BOutqty, 0.00 AS BAdjustQty, 0.0 AS CInqty, 
+                            0.00 AS COutqty, 0.00 AS CAdjustQty
+FROM              dbo.IssueReturn a INNER JOIN
+                            dbo.IssueReturn_Detail b ON b.id = a.id
+UNION ALL
+SELECT          'SubTransfer-From' tbname, a.Type, a.Status, a.EditDate, b.Id, b.FromMDivisionID, b.FromPoId, b.Fromseq1, 
+                            b.Fromseq2, b.FromRoll, b.FromDyelot, 0.0 AS inqty, 0.0 AS outqty, 0.0 AS adjustqty, 0.00 AS AInqty, b.Qty AS AOutqty,
+                             0.00 AS AAdjustQty, 0.00 AS BInqty, 0.00 AS BOutqty, 0.00 AS BAdjustQty, 0.00 AS CInqty, 0.00 AS COutqty, 
+                            0.00 AS CAdjustQty
+FROM              dbo.SubTransfer a INNER JOIN
+                            dbo.SubTransfer_Detail b ON b.id = a.id
+WHERE          a.Type = 'A'
+UNION ALL
+
+SELECT          'SubTransfer-From' tbname, a.Type, a.Status, a.EditDate, b.Id, b.FromMDivisionID, b.FromPoId, b.Fromseq1, 
+                            b.Fromseq2, b.FromRoll, b.FromDyelot, 
+							0.0 AS inqty, b.Qty AS outqty, 0.0 AS adjustqty, 
+							0.0 AS AInqty, 0.00 AS AOutqty, 0.00 AS AAdjustQty, 
+							0.0 AS BInqty, b.Qty AS BOutqty, 0.00 AS BAdjustQty, 
+							0.0 AS CInqty, 0.00 AS COutqty, 0.00 AS CAdjustQty
+FROM              dbo.SubTransfer a INNER JOIN
+                            dbo.SubTransfer_Detail b ON b.id = a.id
+WHERE          a.Type IN ('B', 'E')
+
+UNION ALL
+
+SELECT          'SubTransfer-From' tbname, a.Type, a.Status, a.EditDate, b.Id, b.FromMDivisionID, b.FromPoId, b.Fromseq1, 
+                            b.Fromseq2, b.FromRoll, b.FromDyelot, 
+							0.0 AS inqty, b.Qty AS outqty, 0.0 AS adjustqty, 
+							0.0 AS AInqty, b.Qty AS AOutqty, 0.00 AS AAdjustQty, 
+							0.0 AS BInqty, 0.00 AS BOutqty, 0.00 AS BAdjustQty, 
+							0.0 AS CInqty, 0.00 AS COutqty, 0.00 AS CAdjustQty
+FROM              dbo.SubTransfer a INNER JOIN
+                            dbo.SubTransfer_Detail b ON b.id = a.id
+WHERE          a.Type ='D'
+
+UNION ALL
+
+SELECT          'SubTransfer-From' tbname, a.Type, a.Status, a.EditDate, b.Id, b.FromMDivisionID, b.FromPoId, b.Fromseq1, 
+                            b.Fromseq2, b.FromRoll, b.FromDyelot, 
+							0.0 AS inqty, 0 - b.Qty AS outqty, 0.0 AS adjustqty, 
+							0.0 AS AInqty,0.00 AS AOutqty, 0.00 AS AAdjustQty, 
+							0.0 AS BInqty, 0.00 AS BOutqty, 0.00 AS BAdjustQty, 
+							0.0 AS CInqty, b.Qty AS COutqty, 0.00 AS CAdjustQty
+FROM              dbo.SubTransfer a INNER JOIN
+                            dbo.SubTransfer_Detail b ON b.id = a.id
+WHERE          a.Type = 'C'
+
+UNION ALL
+
+SELECT          'SubTransfer-To' tbname, a.Type, a.Status, a.EditDate, b.Id, b.ToMDivisionID, b.ToPoId, b.ToSeq1, b.ToSeq2, b.ToRoll, 
+                            b.ToDyelot, 
+							0.00 AS inqty, 0.00 AS outqty, 0.0 AS adjustqty, 
+							0.00 AS AInqty, 0.00 AS AOutqty, 0.00 AS AAdjustQty, 
+                            b.Qty AS BInqty, 0.00 AS BOutqty, 0.00 AS BAdjustQty, 
+							0.00 AS CInqty, 0.00 AS COutqty, 0.00 AS CAdjustQty
+FROM              dbo.SubTransfer a INNER JOIN
+                            dbo.SubTransfer_Detail b ON b.id = a.id
+WHERE          a.Type IN ('A', 'C')
+
+UNION ALL
+
+SELECT          'SubTransfer-To' tbname, a.Type, a.Status, a.EditDate, b.Id, b.ToMDivisionID, b.ToPoId, b.ToSeq1, b.ToSeq2, b.ToRoll, 
+                            b.ToDyelot, 
+							0.00 AS inqty, 0.00 AS outqty, 0.0 AS adjustqty, 
+							0.00 AS AInqty, 0.00 AS AOutqty, 0.00 AS AAdjustQty, 
+                            0.00 AS BInqty, 0.00 AS BOutqty, 0.00 AS BAdjustQty, 
+							b.Qty AS CInqty, 0.00 AS COutqty, 0.00 AS CAdjustQty
+FROM              dbo.SubTransfer a INNER JOIN
+                            dbo.SubTransfer_Detail b ON b.id = a.id
+WHERE          a.Type IN ('D', 'E')
+
+UNION ALL
+
+SELECT          'SubTransfer-To' tbname, a.Type, a.Status, a.EditDate, b.Id, b.ToMDivisionID, b.ToPoId, b.ToSeq1, b.ToSeq2, b.ToRoll, 
+                            b.ToDyelot, b.Qty AS inqty, 0.00 AS outqty, 0.0 AS adjustqty, b.Qty AS AInqty, 0.00 AS AOutqty, 0.00 AS AAdjustQty, 
+                            0.00 AS BInqty, 0.00 AS BOutqty, 0.00 AS BAdjustQty, 0.00 AS CInqty, 0.00 AS COutqty, 0.00 AS CAdjustQty
+FROM              dbo.SubTransfer a INNER JOIN
+                            dbo.SubTransfer_Detail b ON b.id = a.id
+WHERE          a.Type = 'B'
+
+UNION ALL
+SELECT          'TransferIn' tbname, '', a.Status, a.EditDate, b.Id, b.MDivisionID, b.PoId, b.seq1, b.seq2, b.Roll, b.Dyelot, 
+                            b.Qty AS inqty, 0.0 AS outqty, 0.0 AS adjustqty, iif(b.StockType = 'B', b.Qty, 0.00) AS AInqty, 0.00 AS AOutqty, 
+                            0.00 AS AAdjustQty, iif(b.StockType = 'I', b.Qty, 0.00) AS BInqty, 0.00 AS BOutqty, 0.00 AS BAdjustQty, 0.00 AS CInqty, 
+                            0.00 AS COutqty, 0.00 AS CAdjustQty
+FROM              dbo.TransferIn a INNER JOIN
+                            dbo.TransferIn_Detail b ON b.id = a.id
+UNION ALL
+SELECT          'TransferOut' tbname, '', a.Status, a.EditDate, b.Id, b.MDivisionID, b.PoId, b.seq1, b.seq2, b.Roll, b.Dyelot, 
+                            0.0 AS inqty, b.Qty AS outqty, 0.0 AS adjustqty, 0.00 AS AInqty, iif(b.StockType = 'B', b.Qty, 0.00) AS AOutqty, 
+                            0.00 AS AAdjustQty, 0.00 AS BInqty, iif(b.StockType = 'I', b.Qty, 0.00) AS BOutqty, 0.00 AS BAdjustQty, 0.00 AS CInqty, 
+                            0.00 AS COutqty, 0.00 AS CAdjustQty
+FROM              dbo.TransferOut a INNER JOIN
+                            dbo.TransferOut_Detail b ON b.id = a.id
+UNION ALL
+SELECT          'Adjust' tbname, a.Type, a.Status, a.EditDate, b.Id, b.MDivisionID, b.PoId, b.seq1, b.seq2, b.Roll, b.Dyelot, 
+                            0.0 AS inqty, 0.0 AS outqty, b.qtyafter - b.qtybefore AS adjustqty, 0.00 AS AInqty, 0.00 AS AOutqty, 
+                            b.qtyafter - b.qtybefore AS AAdjustQty, 0.00 AS BInqty, 0.00 AS BOutqty, 0.00 AS BAdjustQty, 0.00 AS CInqty, 
+                            0.00 AS COutqty, 0.00 AS CAdjustQty
+FROM              dbo.Adjust a INNER JOIN
+                            dbo.Adjust_Detail b ON b.id = a.id
+WHERE          a.Type = 'A'
+UNION ALL
+SELECT          'Adjust' tbname, a.Type, a.Status, a.EditDate, b.Id, b.MDivisionID, b.PoId, b.seq1, b.seq2, b.Roll, b.Dyelot, 
+                            0.0 AS inqty, 0.0 AS outqty, b.qtyafter - b.qtybefore AS adjustqty, 0.00 AS AInqty, 0.00 AS AOutqty, 0.00 AS AAdjustQty, 
+                            0.00 AS BInqty, 0.00 AS BOutqty, b.qtyafter - b.qtybefore AS BAdjustQty, 0.00 AS CInqty, 0.00 AS COutqty, 
+                            0.00 AS CAdjustQty
+FROM              dbo.Adjust a INNER JOIN
+                            dbo.Adjust_Detail b ON b.id = a.id
+WHERE          a.Type = 'B'
+UNION ALL
+SELECT          'BorrowBack-From' tbname, a.Type, a.Status, a.EditDate, b.Id, b.FromMDivisionID, b.FromPoId, b.Fromseq1, 
+                            b.Fromseq2, b.FromRoll, b.FromDyelot, 0.0 AS inqty, b.qty AS outqty, 0.0 AS adjustqty, 0.00 AS AInqty, 
+                            iif(b.FromStockType = 'B', b.Qty, 0.00) AS AOutqty, 0.00 AS AAdjustQty, 0.00 AS BInqty, iif(b.FromStockType = 'I', b.Qty,
+                             0.00) AS BOutqty, 0.00 AS BAdjustQty, 0.00 AS CInqty, 0.00 AS COutqty, 0.00 AS CAdjustQty
+FROM              dbo.BorrowBack a INNER JOIN
+                            dbo.BorrowBack_Detail b ON b.id = a.id
+UNION ALL
+SELECT          'BorrowBack-To' tbname, a.Type, a.Status, a.EditDate, b.Id, b.ToMDivisionID, b.ToPoId, b.ToSeq1, b.ToSeq2, b.ToRoll, 
+                            b.ToDyelot, b.Qty AS inqty, 0.0 AS outqty, 0.0 AS adjustqty, b.Qty AS AInqty, 0.00 AS AOutqty, 0.00 AS AAdjustQty, 
+                            0.00 AS BInqty, 0.00 AS BOutqty, 0.00 AS BAdjustQty, 0.00 AS CInqty, 0.00 AS COutqty, 0.00 AS CAdjustQty
+FROM              dbo.BorrowBack a INNER JOIN
+                            dbo.BorrowBack_Detail b ON b.id = a.id
+UNION ALL
+SELECT          'IssueLack' tbname, a.Type, a.Status, a.EditDate, b.Id, b.MDivisionID, b.PoId, b.seq1, b.seq2, b.Roll, b.Dyelot, 
+                            0.0 AS inqty, b.Qty AS outqty, 0.0 AS adjustqty, 0.00 AS AInqty, b.Qty AS AOutqty, 0.00 AS AAdjustQty, 0.00 AS BInqty, 
+                            0.00 AS BOutqty, 0.00 AS BAdjustQty, 0.00 AS CInqty, 0.00 AS COutqty, 0.00 AS CAdjustQty
+FROM              dbo.IssueLack a INNER JOIN
+                            dbo.IssueLack_Detail b ON b.id = a.id
+WHERE          a.Type = 'R'
+UNION ALL
+SELECT          'RequestCrossM-Receive' tbname, a.Type, a.Status, a.EditDate, b.Id, b.MDivisionID, b.PoId, b.seq1, b.seq2, b.Roll, 
+                            b.Dyelot, b.Qty AS inqty, 0.0 AS outqty, 0.0 AS adjustqty, b.Qty AS AInqty, 0.00 AS AOutqty, 0.00 AS AAdjustQty, 
+                            0.00 AS BInqty, 0.00 AS BOutqty, 0.00 AS BAdjustQty, 0.00 AS CInqty, 0.00 AS COutqty, 0.00 AS CAdjustQty
+FROM              dbo.RequestCrossM a INNER JOIN
+                            dbo.RequestCrossM_Receive b ON b.id = a.id
+WHERE          a.Type IN ('B', 'D')
+UNION ALL
+SELECT          'RequestCrossM-Receive' tbname, a.Type, a.Status, a.EditDate, b.Id, b.MDivisionID, b.PoId, b.seq1, b.seq2, b.Roll, 
+                            b.Dyelot, b.Qty AS inqty, 0.0 AS outqty, 0.0 AS adjustqty, iif(b.StockType = 'B', b.Qty, 0.00) AS AInqty, 0.00 AS AOutqty, 
+                            0.00 AS AAdjustQty, iif(b.StockType = 'I', b.Qty, 0.00) AS BInqty, 0.00 AS BOutqty, 0.00 AS BAdjustQty, 0.00 AS CInqty, 
+                            0.00 AS COutqty, 0.00 AS CAdjustQty
+FROM              dbo.RequestCrossM a INNER JOIN
+                            dbo.RequestCrossM_Receive b ON b.id = a.id
+WHERE          a.Type = 'G'
+UNION ALL
+SELECT          'ReturnReceipt' tbname, '', a.Status, a.EditDate, b.Id, b.MDivisionID, b.PoId, b.seq1, b.seq2, b.Roll, b.Dyelot, 
+                            0 - b.Qty AS inqty, 0.0 AS outqty, 0.0 AS adjustqty, iif(b.StockType = 'B', 0 - b.Qty, 0.0) AS AInqty, 0.00 AOutqty, 
+                            0.00 AS AAdjuqtQty, iif(b.StockType = 'I', 0 - b.Qty, 0.0) AS BInqty, 0.00 BOutqty, 0.00 AS BAdjuqtQty, 0.0 AS CInqty, 
+                            0.00 COutqty, 0.00 CAdjuqtQty
+FROM              dbo.ReturnReceipt a INNER JOIN
+                            dbo.ReturnReceipt_Detail b ON b.id = a.id
