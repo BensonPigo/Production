10fdd8d6c627c7364b714e642ad6d7e3e300625d
WENNY <wenny@sportscity.com.tw>
2016-11-08 10:10:46 +0000

    798: PPIC_P04_Style Management 為何Combo type皆為空白?

--- Sci.Production.PPIC/P04.Designer.cs
+++ Sci.Production.PPIC/P04.Designer.cs
@@ -231,7 +231,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(897, 507);
+            this.tabPage1.Size = new System.Drawing.Size(897, 395);
             this.tabPage1.TabIndex = 2;
             this.tabPage1.Text = "Sketch";
             // 
