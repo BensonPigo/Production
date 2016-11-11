b20e93fedcab39471c000d0e02d1d26e831f2bbf
ALGER <alger@sportscity.com.tw>
2016-11-11 10:52:29 +0000

    add table Style_ProductionKits.

--- DailyTransfer/imp_Style.sql
+++ DailyTransfer/imp_Style.sql
@@ -1176,5 +1176,130 @@ from Trade_To_Pms.dbo.Style_SimilarStyle as b
 where not exists(select 1 from Production.dbo.Style_SimilarStyle as a where a.MasterStyleUkey	= b.MasterStyleUkey AND a.ChildrenStyleUkey	= b.ChildrenStyleUkey)
 
 
+
+-----Style_ProductionKits-----2016/11/11-----
+----------------------刪除主TABLE多的資料
+Delete Production.dbo.Style_ProductionKits
+from Production.dbo.Style_ProductionKits as a 
+INNER JOIN Trade_To_Pms.dbo.Style as t on a.StyleUkey=t.Ukey
+left join Trade_To_Pms.dbo.Style_ProductionKits as b
+on a.StyleUkey= b.StyleUkey AND a.FactoryID=B.FactoryID
+where b.StyleUkey is null
+---------------------------UPDATE 主TABLE跟來源TABLE 為一樣(主TABLE多的話 記起來 ~來源TABLE多的話不理會)
+UPDATE a
+SET  
+a.StyleUkey	= b.StyleUkey
+,a.ProductionKitsGroup = b.ProductionKitsGroup
+,a.MDivisionID = c.MDivisionID
+,a.FactoryID = b.FactoryID
+,a.Article = b.Article
+,a.DOC = b.DOC
+,a.SendDate = b.SendDate
+,a.ReceiveDate = b.ReceiveDate
+,a.ProvideDate = b.ProvideDate
+,a.SendName = b.SendName
+,a.FtyHandle = b.FtyHandle
+,a.MRHandle = b.MRHandle
+,a.SMR = b.SMR
+,a.PoHandle = b.PoHandle
+,a.POSMR = b.POSMR
+,a.OrderId = b.OrderId
+,a.SCIDelivery = b.SCIDelivery
+,a.IsPF = b.IsPF
+,a.BuyerDelivery = b.BuyerDelivery
+,a.AddOrderId = b.AddOrderId
+,a.AddSCIDelivery = b.AddSCIDelivery
+,a.AddIsPF = b.AddIsPF
+,a.AddBuyerDelivery = b.AddBuyerDelivery
+,a.MRLastDate = b.MRLastDate
+,a.FtyLastDate = b.FtyLastDate
+,a.MRRemark = b.MRRemark
+,a.FtyRemark = b.FtyRemark
+,a.FtyList = b.FtyList
+,a.ReasonID = b.ReasonID
+--,a.SendToQA = b.SendToQA
+--,a.QAReceived = b.QAReceived
+,a.StyleCUkey1_Old = b.StyleCUkey1_Old
+from Production.dbo.Style_ProductionKits as a 
+inner join Trade_To_Pms.dbo.Style_ProductionKits as b ON a.StyleUkey=b.StyleUkey AND a.FactoryID=b.FactoryID
+left join Trade_To_Pms.dbo.Factory as c ON c.ID=b.FactoryID
+-------------------------- INSERT INTO 抓
+INSERT INTO Production.dbo.Style_ProductionKits(
+Ukey
+,StyleUkey
+,ProductionKitsGroup
+,MDivisionID
+,FactoryID
+,Article
+,DOC
+,SendDate
+,ReceiveDate
+,ProvideDate
+,SendName
+,FtyHandle
+,MRHandle
+,SMR
+,PoHandle
+,POSMR
+,OrderId
+,SCIDelivery
+,IsPF
+,BuyerDelivery
+,AddOrderId
+,AddSCIDelivery
+,AddIsPF
+,AddBuyerDelivery
+,MRLastDate
+,FtyLastDate
+,MRRemark
+,FtyRemark
+,FtyList
+,ReasonID
+--,SendToQA
+--,QAReceived
+,StyleCUkey1_Old)
+
+select 
+b.Ukey
+,b.StyleUkey
+,b.ProductionKitsGroup
+,c.MDivisionID
+,b.FactoryID
+,b.Article
+,b.DOC
+,b.SendDate
+,b.ReceiveDate
+,b.ProvideDate
+,b.SendName
+,b.FtyHandle
+,b.MRHandle
+,b.SMR
+,b.PoHandle
+,b.POSMR
+,b.OrderId
+,b.SCIDelivery
+,b.IsPF
+,b.BuyerDelivery
+,b.AddOrderId
+,b.AddSCIDelivery
+,b.AddIsPF
+,b.AddBuyerDelivery
+,b.MRLastDate
+,b.FtyLastDate
+,b.MRRemark
+,b.FtyRemark
+,b.FtyList
+,b.ReasonID
+--,SendToQA
+--,QAReceived
+,b.StyleCUkey1_Old
+from Trade_To_Pms.dbo.Style_ProductionKits as b
+left join Trade_To_Pms.dbo.Factory as c ON c.ID=b.FactoryID
+where not exists(select 1 from Production.dbo.Style_ProductionKits as a where a.StyleUkey=b.StyleUkey AND a.FactoryID=b.FactoryID)
+	and b.FactoryID in (select id from Production.dbo.Factory)
+
+
+
+
 END
 
