9036679b5359437e83368e2dc0bdf5eec9ba8b3e
JEFF <jeff@sportscity.com.tw>
2016-11-11 02:00:05 +0000

    add

---
+++ Sci.Production.Subcon/R43.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Sci.Production.Subcon
+{
+    partial class R43
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboFactory = new Sci.Win.UI.ComboBox();
+            this.comboSubProcess = new Sci.Win.UI.ComboBox();
+            this.label5 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.label1 = new Sci.Win.UI.Label();
+            this.dateBundleReceive = new Sci.Win.UI.DateRange();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Enabled = false;
+            this.print.Location = new System.Drawing.Point(473, 12);
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(473, 48);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(473, 84);
+            // 
+            // comboFactory
+            // 
+            this.comboFactory.BackColor = System.Drawing.Color.White;
+            this.comboFactory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboFactory.FormattingEnabled = true;
+            this.comboFactory.IsSupportUnselect = true;
+            this.comboFactory.Location = new System.Drawing.Point(169, 78);
+            this.comboFactory.Name = "comboFactory";
+            this.comboFactory.Size = new System.Drawing.Size(121, 24);
+            this.comboFactory.TabIndex = 117;
+            // 
+            // comboSubProcess
+            // 
+            this.comboSubProcess.BackColor = System.Drawing.Color.White;
+            this.comboSubProcess.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboSubProcess.FormattingEnabled = true;
+            this.comboSubProcess.IsSupportUnselect = true;
+            this.comboSubProcess.Location = new System.Drawing.Point(169, 48);
+            this.comboSubProcess.Name = "comboSubProcess";
+            this.comboSubProcess.Size = new System.Drawing.Size(121, 24);
+            this.comboSubProcess.TabIndex = 116;
+            // 
+            // label5
+            // 
+            this.label5.Lines = 0;
+            this.label5.Location = new System.Drawing.Point(32, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(134, 23);
+            this.label5.TabIndex = 115;
+            this.label5.Text = "Factory";
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(32, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 23);
+            this.label4.TabIndex = 114;
+            this.label4.Text = "Sub Process";
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(32, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 23);
+            this.label1.TabIndex = 118;
+            this.label1.Text = "Bundle Receive Date";
+            // 
+            // dateBundleReceive
+            // 
+            this.dateBundleReceive.Location = new System.Drawing.Point(169, 19);
+            this.dateBundleReceive.Name = "dateBundleReceive";
+            this.dateBundleReceive.Size = new System.Drawing.Size(280, 23);
+            this.dateBundleReceive.TabIndex = 119;
+            // 
+            // R43
+            // 
+            this.ClientSize = new System.Drawing.Size(565, 145);
+            this.Controls.Add(this.dateBundleReceive);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboFactory);
+            this.Controls.Add(this.comboSubProcess);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Name = "R43";
+            this.Text = "Sub-process BCS report (RFID)";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.label5, 0);
+            this.Controls.SetChildIndex(this.comboSubProcess, 0);
+            this.Controls.SetChildIndex(this.comboFactory, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.dateBundleReceive, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.ComboBox comboFactory;
+        private Win.UI.ComboBox comboSubProcess;
+        private Win.UI.Label label5;
+        private Win.UI.Label label4;
+        private Win.UI.Label label1;
+        private Win.UI.DateRange dateBundleReceive;
+    }
+}
---
+++ Sci.Production.Subcon/R43.cs
@@ -0,0 +1,146 @@
+﻿using Ict;
+using Sci.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Subcon
+{
+    public partial class R43 : Sci.Win.Tems.PrintForm
+    {
+        DataTable printData;
+        string SubProcess, Factory;
+        DateTime? dateBundleReceive1, dateBundleReceive2;
+
+        public R43(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            comboload();
+        }
+
+        private void comboload()
+        {
+            DataTable dtSubprocessID;
+            DualResult Result;
+            if (Result = DBProxy.Current.Select(null, "select 'ALL' as id,1 union select id,2 from Subprocess where Junk = 0 ",
+                out dtSubprocessID))
+            {
+                this.comboSubProcess.DataSource = dtSubprocessID;
+                this.comboSubProcess.DisplayMember = "ID";
+            }
+            else { ShowErr(Result); }
+
+            DataTable dtfactory;
+            if (Result = DBProxy.Current.Select(null, "select '' as id union select MDivisionID from factory", out dtfactory))
+            {
+                this.comboFactory.DataSource = dtfactory;
+                this.comboFactory.DisplayMember = "ID";
+            }
+            else { ShowErr(Result); }
+        }
+
+        #region ToExcel三步驟
+        // 驗證輸入條件
+        protected override bool ValidateInput()
+        {
+            if (MyUtility.Check.Empty(dateBundleReceive.Value1) && MyUtility.Check.Empty(dateBundleReceive.Value2))
+            {
+                MyUtility.Msg.WarningBox("Bundel receive date can't empty!!");
+                return false;
+            }
+
+            SubProcess = comboSubProcess.Text;
+            Factory = comboFactory.Text;
+            dateBundleReceive1 = dateBundleReceive.Value1;
+            dateBundleReceive2 = dateBundleReceive.Value2;
+            return base.ValidateInput();
+        }
+        //非同步讀取資料
+        protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            #region sqlcmd
+            StringBuilder sqlCmd = new StringBuilder();
+            sqlCmd.Append(@"select
+            [Factory] = b.MDivisionid,
+            [SPNo] = b.Orderid,
+            [Style] = o.StyleID,
+            [Season] = o.SeasonID,
+            [Brand] = o.BrandID,
+            [Sub-process] = bio.SubProcessId,
+            [Receive Qty] =sum(bd.qty),
+            [Release Qty] = (select sum(bd.qty)  where (bio.InComing-bio.OutGoing) <= s.BCSDate),
+            [BCS] = sum(bd.qty) /(select sum(bd.qty)  where (bio.InComing-bio.OutGoing) <= s.BCSDate)
+
+            from Bundle b
+            inner join Bundle_Detail bd on bd.Id = b.Id
+            inner join Bundle_Detail_Art bda on bda.Id = bd.Id and bda.Bundleno = bd.Bundleno
+            inner join orders o on o.Id = b.OrderId
+            left join BundleInOut bio on bio.Bundleno = bd.Bundleno
+            left join SubProcess s on s.Id = bio.SubprocessId
+
+            where 1=1
+            ");
+            #endregion
+            #region Append畫面上的條件
+            IList<SqlParameter> cmds = new List<SqlParameter>();
+            if (!MyUtility.Check.Empty(SubProcess))
+            {
+                sqlCmd.Append(@" and (bio.SubprocessId = @SubProcess or @SubProcess = 'ALL')");
+                cmds.Add(new SqlParameter("@SubProcess", SubProcess));
+            }
+            if (!MyUtility.Check.Empty(dateBundleReceive1))
+            {
+                sqlCmd.Append(@" and bio.InComing between @BundleReceive1 and @BundleReceive2 ");
+                cmds.Add(new SqlParameter("@BundleReceive1", Convert.ToDateTime(dateBundleReceive1).ToString("d")));
+                cmds.Add(new SqlParameter("@BundleReceive2", Convert.ToDateTime(dateBundleReceive2).ToString("d")));
+            }
+            if (!MyUtility.Check.Empty(Factory))
+            {
+                sqlCmd.Append(string.Format(@" and b.MDivisionid = @Factory"));
+                cmds.Add(new SqlParameter("@Factory", Factory));
+            }
+            #endregion
+            #region group order
+            sqlCmd.Append(@"
+            group by 
+            b.MDivisionid,b.Orderid,o.StyleID,o.SeasonID,o.BrandID,bio.SubProcessId,bio.InComing,bio.OutGoing,s.BCSDate
+            order by 
+            [Factory],[SPNo],[Style],[Season],[Brand],[Sub-process],[Receive Qty],[Release Qty],[BCS]");
+            #endregion
+            DualResult result = DBProxy.Current.Select(null, sqlCmd.ToString(), cmds, out printData);
+            if (!result)
+            {
+                DualResult failResult = new DualResult(false, "Query data fail\r\n" + result.ToString());
+                return failResult;
+            }
+            return Result.True;
+        }
+        // 產生Excel
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            SetCount(printData.Rows.Count);
+            if (printData.Rows.Count <= 0)
+            {
+                MyUtility.Msg.WarningBox("Data not found!");
+                return false;
+            }
+            //預先開啟excel app
+            Microsoft.Office.Interop.Excel.Application objApp
+                = MyUtility.Excel.ConnectExcel(Sci.Env.Cfg.XltPathDir + "\\Subcon_R43_Sub-process BCS report (RFID).xltx");
+            // 將datatable copy to excel
+            MyUtility.Excel.CopyToXls(printData, "", "Subcon_R43_Sub-process BCS report (RFID).xltx", 1, true, null, objApp);
+            Microsoft.Office.Interop.Excel.Worksheet objSheets = objApp.ActiveWorkbook.Worksheets[1];   // 取得工作表
+            if (objSheets != null) Marshal.FinalReleaseComObject(objSheets);    //釋放sheet
+            if (objApp != null) Marshal.FinalReleaseComObject(objApp);          //釋放objApp
+            return true;
+        }
+        #endregion
+    }
+}
