27d12ef9f30b59e25749f7e8c576e5938a398bbe
ALGER <alger@sportscity.com.tw>
2016-11-17 01:36:04 +0000

    資料全由APS匯入，不分工廠別與不可被修改，將程式復原至第一版，順便將Factory欄位移除。

--- Sci.Production.PPIC/B08.Designer.cs
+++ Sci.Production.PPIC/B08.Designer.cs
@@ -30,17 +30,12 @@
         {
             this.label3 = new Sci.Win.UI.Label();
             this.label4 = new Sci.Win.UI.Label();
+            this.displayBox1 = new Sci.Win.UI.DisplayBox();
             this.editBox1 = new Sci.Win.UI.EditBox();
-            this.textBox1 = new Sci.Win.UI.TextBox();
-            this.lbID = new Sci.Win.UI.Label();
-            this.txtID = new Sci.Win.UI.TextBox();
-            this.lbFactory = new Sci.Win.UI.Label();
-            this.txtFactory = new Sci.Win.UI.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.detailgridbs)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.detailgrid2bs)).BeginInit();
             this.masterpanel.SuspendLayout();
             this.detailpanel.SuspendLayout();
-            this.detail2.SuspendLayout();
             this.detailpanel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gridbs)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.mtbs)).BeginInit();
@@ -52,92 +47,53 @@
             // 
             // masterpanel
             // 
-            this.masterpanel.Controls.Add(this.txtFactory);
-            this.masterpanel.Controls.Add(this.lbFactory);
-            this.masterpanel.Controls.Add(this.txtID);
-            this.masterpanel.Controls.Add(this.lbID);
-            this.masterpanel.Controls.Add(this.textBox1);
             this.masterpanel.Controls.Add(this.editBox1);
+            this.masterpanel.Controls.Add(this.displayBox1);
             this.masterpanel.Controls.Add(this.label4);
             this.masterpanel.Controls.Add(this.label3);
-            this.masterpanel.Size = new System.Drawing.Size(676, 103);
+            this.masterpanel.Size = new System.Drawing.Size(757, 100);
             this.masterpanel.Controls.SetChildIndex(this.gridicon, 0);
             this.masterpanel.Controls.SetChildIndex(this.label3, 0);
             this.masterpanel.Controls.SetChildIndex(this.label4, 0);
+            this.masterpanel.Controls.SetChildIndex(this.displayBox1, 0);
             this.masterpanel.Controls.SetChildIndex(this.editBox1, 0);
-            this.masterpanel.Controls.SetChildIndex(this.textBox1, 0);
-            this.masterpanel.Controls.SetChildIndex(this.lbID, 0);
-            this.masterpanel.Controls.SetChildIndex(this.txtID, 0);
-            this.masterpanel.Controls.SetChildIndex(this.lbFactory, 0);
-            this.masterpanel.Controls.SetChildIndex(this.txtFactory, 0);
             // 
             // detailpanel
             // 
-            this.detailpanel.Location = new System.Drawing.Point(0, 103);
-            this.detailpanel.Size = new System.Drawing.Size(676, 279);
+            this.detailpanel.Size = new System.Drawing.Size(757, 249);
             // 
             // gridicon
             // 
-            this.gridicon.Location = new System.Drawing.Point(654, 68);
+            this.gridicon.Location = new System.Drawing.Point(654, 65);
             // 
             // refresh
             // 
-            this.refresh.Location = new System.Drawing.Point(586, 0);
+            this.refresh.Location = new System.Drawing.Point(677, 0);
             // 
             // detailgridcont
             // 
-            this.detailgridcont.Size = new System.Drawing.Size(676, 279);
-            // 
-            // detail2
-            // 
-            this.detail2.Size = new System.Drawing.Size(892, 387);
-            // 
-            // detailgridcont2
-            // 
-            this.detailgridcont2.Size = new System.Drawing.Size(886, 341);
-            // 
-            // detailpanel2
-            // 
-            this.detailpanel2.Size = new System.Drawing.Size(886, 40);
+            this.detailgridcont.Size = new System.Drawing.Size(757, 249);
             // 
             // detail
             // 
-            this.detail.Size = new System.Drawing.Size(676, 420);
+            this.detail.Size = new System.Drawing.Size(757, 387);
             // 
             // detailcont
             // 
-            this.detailcont.Size = new System.Drawing.Size(676, 382);
+            this.detailcont.Size = new System.Drawing.Size(757, 349);
             // 
             // detailbtm
             // 
-            this.detailbtm.Location = new System.Drawing.Point(0, 382);
-            this.detailbtm.Size = new System.Drawing.Size(676, 38);
-            // 
-            // browse
-            // 
-            this.browse.Size = new System.Drawing.Size(676, 420);
+            this.detailbtm.Size = new System.Drawing.Size(757, 38);
             // 
             // tabs
             // 
-            this.tabs.Size = new System.Drawing.Size(684, 449);
-            // 
-            // createby
-            // 
-            this.createby.Size = new System.Drawing.Size(230, 23);
-            // 
-            // editby
-            // 
-            this.editby.Location = new System.Drawing.Point(354, 7);
-            this.editby.Size = new System.Drawing.Size(230, 23);
-            // 
-            // lbleditby
-            // 
-            this.lbleditby.Location = new System.Drawing.Point(306, 13);
+            this.tabs.Size = new System.Drawing.Size(765, 416);
             // 
             // label3
             // 
             this.label3.Lines = 0;
-            this.label3.Location = new System.Drawing.Point(11, 32);
+            this.label3.Location = new System.Drawing.Point(11, 13);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(75, 23);
             this.label3.TabIndex = 1;
@@ -146,82 +102,45 @@
             // label4
             // 
             this.label4.Lines = 0;
-            this.label4.Location = new System.Drawing.Point(11, 59);
+            this.label4.Location = new System.Drawing.Point(11, 40);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(75, 23);
             this.label4.TabIndex = 2;
             this.label4.Text = "Description";
+            // 
+            // displayBox1
+            // 
+            this.displayBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.displayBox1.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "Name", true));
+            this.displayBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.displayBox1.Location = new System.Drawing.Point(90, 13);
+            this.displayBox1.Name = "displayBox1";
+            this.displayBox1.Size = new System.Drawing.Size(320, 23);
+            this.displayBox1.TabIndex = 3;
             // 
             // editBox1
             // 
             this.editBox1.BackColor = System.Drawing.Color.White;
             this.editBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "Description", true));
             this.editBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.editBox1.Location = new System.Drawing.Point(90, 59);
+            this.editBox1.Location = new System.Drawing.Point(90, 40);
             this.editBox1.Multiline = true;
             this.editBox1.Name = "editBox1";
-            this.editBox1.Size = new System.Drawing.Size(500, 40);
+            this.editBox1.Size = new System.Drawing.Size(320, 50);
             this.editBox1.TabIndex = 4;
-            // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.Color.White;
-            this.textBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "Name", true));
-            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.textBox1.Location = new System.Drawing.Point(90, 32);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(500, 23);
-            this.textBox1.TabIndex = 5;
-            // 
-            // lbID
-            // 
-            this.lbID.Lines = 0;
-            this.lbID.Location = new System.Drawing.Point(11, 6);
-            this.lbID.Name = "lbID";
-            this.lbID.Size = new System.Drawing.Size(75, 23);
-            this.lbID.TabIndex = 6;
-            this.lbID.Text = "ID";
-            // 
-            // txtID
-            // 
-            this.txtID.BackColor = System.Drawing.Color.White;
-            this.txtID.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "ID", true));
-            this.txtID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.txtID.Location = new System.Drawing.Point(90, 6);
-            this.txtID.Name = "txtID";
-            this.txtID.Size = new System.Drawing.Size(150, 23);
-            this.txtID.TabIndex = 7;
-            // 
-            // lbFactory
-            // 
-            this.lbFactory.Lines = 0;
-            this.lbFactory.Location = new System.Drawing.Point(362, 6);
-            this.lbFactory.Name = "lbFactory";
-            this.lbFactory.Size = new System.Drawing.Size(75, 23);
-            this.lbFactory.TabIndex = 8;
-            this.lbFactory.Text = "Factory";
-            // 
-            // txtFactory
-            // 
-            this.txtFactory.BackColor = System.Drawing.Color.White;
-            this.txtFactory.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "FactoryID", true));
-            this.txtFactory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.txtFactory.Location = new System.Drawing.Point(440, 6);
-            this.txtFactory.Name = "txtFactory";
-            this.txtFactory.Size = new System.Drawing.Size(150, 23);
-            this.txtFactory.TabIndex = 9;
-            this.txtFactory.PopUp += new System.EventHandler<Sci.Win.UI.TextBoxPopUpEventArgs>(this.txtFactory_PopUp);
-            this.txtFactory.Validating += new System.ComponentModel.CancelEventHandler(this.txtFactory_Validating);
             // 
             // B08
             // 
-            this.ClientSize = new System.Drawing.Size(684, 482);
+            this.ClientSize = new System.Drawing.Size(765, 449);
             this.DefaultDetailOrder = "Day";
-            this.DefaultOrder = "ID";
+            this.DefaultOrder = "Name";
             this.GridAlias = "LearnCurve_Detail";
-            this.GridNew = 0;
+            this.GridEdit = false;
             this.IsGridIconVisible = false;
             this.IsSupportCopy = false;
+            this.IsSupportDelete = false;
+            this.IsSupportEdit = false;
+            this.IsSupportNew = false;
             this.IsSupportPrint = false;
             this.KeyField1 = "ID";
             this.Name = "B08";
@@ -234,7 +153,6 @@
             this.masterpanel.ResumeLayout(false);
             this.masterpanel.PerformLayout();
             this.detailpanel.ResumeLayout(false);
-            this.detail2.ResumeLayout(false);
             this.detailpanel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.gridbs)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.mtbs)).EndInit();
@@ -251,12 +169,8 @@
         #endregion
 
         private Win.UI.EditBox editBox1;
+        private Win.UI.DisplayBox displayBox1;
         private Win.UI.Label label4;
         private Win.UI.Label label3;
-        private Win.UI.TextBox textBox1;
-        private Win.UI.Label lbFactory;
-        private Win.UI.TextBox txtID;
-        private Win.UI.Label lbID;
-        private Win.UI.TextBox txtFactory;
     }
 }
--- Sci.Production.PPIC/B08.cs
+++ Sci.Production.PPIC/B08.cs
@@ -6,22 +6,6 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using Ict.Win;
-using System.Linq;
-
-using Ict;
-//using Ict.Win;
-//using Sci.Data;
-//using Sci.Production.PublicPrg;
-//using System;
-//using System.Collections.Generic;
-//using System.ComponentModel;
-//using System.Data;
-//using System.Drawing;
-//using System.Linq;
-//using System.Text;
-//using System.Transactions;
-//using System.Windows.Forms;
-
 
 namespace Sci.Production.PPIC
 {
@@ -30,166 +14,15 @@ namespace Sci.Production.PPIC
         public B08(ToolStripMenuItem menuitem)
             : base(menuitem)
         {
-
-            string sqlCommand = "select UseAPS from factory where ID = '" + Sci.Env.User.Factory + "'";
-            string useAPS = MyUtility.GetValue.Lookup(sqlCommand, null);
-            if (useAPS.ToUpper() == "TRUE")
-            {
-                IsSupportCopy = false;
-                IsSupportDelete = false;
-                IsSupportEdit = false;
-                IsSupportNew = false;
-            }
-
             InitializeComponent();
-            this.DefaultFilter = "FactoryID = '" + Sci.Env.User.Factory + "' OR FactoryID = 'ALL' ";
-
-            //按Enter時自動跳到下一筆Record，若以已沒有資料了就新增一筆Record
-            detailgrid.StatusNotification += (s, e) =>
-            {
-                if (this.EditMode && e.Notification == Ict.Win.UI.DataGridViewStatusNotification.NoMoreRowOnEnterPress)
-                {
-                    this.OnDetailGridInsert();
-                }
-            };
-
         }
 
         protected override void OnDetailGridSetup()
         {
-            
             Helper.Controls.Grid.Generator(this.detailgrid)
-                .Numeric("Day",header:"Day", iseditingreadonly:true)
-                .Numeric("Efficiency", header: "Efficiency (%)",iseditingreadonly: false);
+                .Numeric("Day",header:"Day")
+                .Numeric("Efficiency", header: "Efficiency (%)");
         }
-
-        protected override void ClickNewAfter()
-        {
-            base.ClickNewAfter();
-            CurrentMaintain["ID"] = DBNull.Value;
-            CurrentMaintain["FactoryID"] = Sci.Env.User.Factory;
-
-            //Detail資料自動新增10筆Record，Day填入1~10
-            this.OnDetailGridInsert();
-            this.OnDetailGridInsert();
-            this.OnDetailGridInsert();
-            this.OnDetailGridInsert();
-            this.OnDetailGridInsert();
-            this.OnDetailGridInsert();
-            this.OnDetailGridInsert();
-            this.OnDetailGridInsert();
-            this.OnDetailGridInsert();
-            this.OnDetailGridInsert();
-
-        }
-
-        protected override void OnDetailGridInsert(int index = -1)
-        {
-            base.OnDetailGridInsert(index); //先給一個NewKey
-            int maxkey;
-
-            object comput = ((DataTable)detailgridbs.DataSource).Compute("Max(Day)", "");
-            if (comput == DBNull.Value) maxkey = 1;
-            else maxkey = Convert.ToInt32(comput);
-            maxkey = maxkey + 1;
-            CurrentDetailData["Day"] = maxkey;
-        }
-
-        protected override void ClickEditAfter()
-        {
-            base.ClickEditAfter();
-            this.txtID.ReadOnly = true;
-            this.txtFactory.ReadOnly = true;
-        }
-
-        protected override bool ClickSaveBefore()
-        {
-            #region 1.檢查Id跟Factory不可為空值
-            if (MyUtility.Check.Empty(CurrentMaintain["ID"].ToString()))
-            {
-                MyUtility.Msg.WarningBox("< ID > can not be empty!");
-                this.txtID.Focus();
-                return false;
-            }
-
-            if (MyUtility.Check.Empty(CurrentMaintain["FactoryID"].ToString()))
-            {
-                MyUtility.Msg.WarningBox("< Factory > can not be empty!");
-                this.txtFactory.Focus();
-                return false;
-            }
-            #endregion
-
-            #region 2.表身資料從後面開始檢查，將效率為0的自動刪除，直到有效率值才停止
-            DataTable dtDetail = (DataTable)detailgridbs.DataSource;
-            for (int i = dtDetail.Rows.Count - 1; i >= 0; i--)
-            {
-                if (Convert.ToInt16(dtDetail.Rows[i]["Efficiency"]) == 0)
-                    ((DataTable)detailgridbs.DataSource).Rows[i].Delete();
-                else
-                    break;
-            }
-            #endregion
-
-            #region 3.檢查表身資料不可以有Efficiency為0，若有則出訊息告知，且不可存檔
-            foreach (DataRow dr in dtDetail.Rows)
-            {
-                if (dr.RowState != DataRowState.Deleted)
-                {
-                    if (Convert.ToInt16(dr["Efficiency"]) == 0)
-                    {
-                        MyUtility.Msg.WarningBox("< Efficiency (%) > can not be 0 !!");
-                        return false;
-                    }
-                }
-            }
-            #endregion
-
-            #region 4.檢查表身資料不可全為空值，若有則出訊息告知，且不可存檔
-            int DetailCount = dtDetail.AsEnumerable().Where(r => r.RowState != DataRowState.Deleted).Count();
-            if (DetailCount == 0)
-            {
-                MyUtility.Msg.WarningBox("Detail can not be empty!");
-                return false;
-            }
-            #endregion
-
-            return base.ClickSaveBefore();
-        }
-
-        private void txtFactory_PopUp(object sender, Win.UI.TextBoxPopUpEventArgs e)
-        {
-            Sci.Win.Tools.SelectItem item;
-            string sqlCmd = @"SELECT 'ALL' as FTYGroup
-                            UNION
-                            select distinct FTYGroup from Factory where FTYGroup<>'' and Junk=0";
-            item = new Sci.Win.Tools.SelectItem(sqlCmd, "10", this.Text);
-            DialogResult returnResult = item.ShowDialog();
-            if (returnResult == DialogResult.Cancel) { return; }
-            txtFactory.Text = item.GetSelectedString();
-        }
-
-        private void txtFactory_Validating(object sender, CancelEventArgs e)
-        {
-            if (EditMode && txtFactory.OldValue != txtFactory.Text)
-            {
-                if (!MyUtility.Check.Empty(txtFactory.Text))
-                {
-                    string sqlCmd = @"select * from 
-                                    (SELECT 'ALL' as FTYGroup  UNION
-                                    select distinct FTYGroup from Factory where FTYGroup<>'' and Junk=0) tmp
-                                    where tmp.FTYGroup='{0}'";
-                    if (!MyUtility.Check.Seek(string.Format(sqlCmd, txtFactory.Text)))
-                    {
-                        MyUtility.Msg.WarningBox(string.Format("Factory:{0} not found !!", txtFactory.Text));
-                        CurrentMaintain["FactoryID"] = "";
-                        e.Cancel = true;
-                        return;
-                    }
-                }
-            }
-        }
-
 
     }
 }
