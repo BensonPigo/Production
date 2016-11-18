442754c1aec11ad290411884856989d3defcb356
ALGER <alger@sportscity.com.tw>
2016-11-18 06:31:54 +0000

    aps data download bug fix.

--- Sci.Production.PPIC/P07.cs
+++ Sci.Production.PPIC/P07.cs
@@ -58,6 +58,8 @@ namespace Sci.Production.PPIC
             }
 
             setcuttingdate();
+
+            MyUtility.Msg.InfoBox("Download successful !!");
         }
 
         //Close
@@ -128,7 +130,7 @@ namespace Sci.Production.PPIC
                 if (dr["offlinea"] == DBNull.Value) sewof = "";
                 else sewof = Convert.ToDateTime(dr["offlinea"]).ToShortDateString();
 
-                updsql = updsql + string.Format("insert into cutting(ID,sewInline,sewoffline,mDivisionid,FactoryID,AddName,AddDate) Values('{0}','{1}','{2}','{3}','{4}',GetDate()); ", dr["cuttingsp"], sewin, sewof, Sci.Env.User.Keyword, Sci.Env.User.Factory, Sci.Env.User.UserID);
+                updsql = updsql + string.Format("insert into cutting(ID,sewInline,sewoffline,mDivisionid,FactoryID,AddName,AddDate) Values('{0}','{1}','{2}','{3}','{4}','{5}', GetDate()); ", dr["cuttingsp"], sewin, sewof, Sci.Env.User.Keyword, Sci.Env.User.Factory, Sci.Env.User.UserID);
             }
             sqlcmd = string.Format(@"Select ord.cuttingsp,min(ord.sewinline) as inline ,max(ord.sewoffline) as offlinea 
             from orders ord,
