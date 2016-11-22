2e6d33e1dd8bf53947dddf741be8a911b296c3b7
JEFF <jeff@sportscity.com.tw>
2016-11-22 12:49:52 +0000

    add index

--- Database/dbo/Tables/Style.sql
+++ Database/dbo/Tables/Style.sql
@@ -67,6 +67,8 @@
 
 
 
+
+
 GO
 EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'款式資料基本檔', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Style';
 
@@ -326,3 +328,7 @@ EXEC sp_addextendedproperty @name = N'MS_Description',
     @level1name = N'Style',
     @level2type = N'COLUMN',
     @level2name = N'Phase'
+GO
+CREATE NONCLUSTERED INDEX [StyleUkey]
+    ON [dbo].[Style]([Ukey] ASC);
+
