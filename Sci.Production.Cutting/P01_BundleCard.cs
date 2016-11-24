42f7366e835a48dea6a97ff5bfb172a229dea1a2
ALGER <alger@sportscity.com.tw>
2016-11-24 02:07:05 +0000

    1194: CUTTING_P01_BundleCard (1) 請依Bundle No.排序 --> fix done. (2) Comb欄位資料未帶出 --> 改抓PatternPanel.

--- Sci.Production.Cutting/P01_BundleCard.cs
+++ Sci.Production.Cutting/P01_BundleCard.cs
@@ -24,14 +24,10 @@ namespace Sci.Production.Cutting
         }
         private void requery()
         {
-//            string sqlcmd = String.Format(
-//            @"Select a.id,b.bundleno,a.orderid,a.cdate,a.cutref,a.fabriccombo,a.cutno,b.sizecode,b.bundlegroup,b.Qty 
-//            from Bundle a, Bundle_Detail b 
-//            Where a.id = b.id and a.Cuttingid = '{0}'",cutid);
             string sqlcmd = String.Format(
            @"Select a.id,b.bundleno,a.orderid,a.cdate,a.cutref,a.PatternPanel,a.cutno,b.sizecode,b.bundlegroup,b.Qty 
             from Bundle a, Bundle_Detail b 
-            Where a.id = b.id and a.POID = '{0}'", cutid);
+            Where a.id = b.id and a.POID = '{0}' order by BundleNo", cutid);
             DataTable gridtb;
             DualResult dr = DBProxy.Current.Select(null, sqlcmd, out gridtb);
             grid1.DataSource = gridtb;
@@ -39,11 +35,11 @@ namespace Sci.Production.Cutting
         private void gridSetup()
         {
             Helper.Controls.Grid.Generator(this.grid1)
-                .Text("bundleno", header: "Bundle No.", width: Widths.AnsiChars(10), iseditingreadonly: true)
-                .Text("orderid", header: "SP No", width: Widths.AnsiChars(13), iseditingreadonly: true)
+                .Text("bundleno", header: "Bundle No.", width: Widths.AnsiChars(13), iseditingreadonly: true)
+                .Text("orderid", header: "SP No", width: Widths.AnsiChars(16), iseditingreadonly: true)
                 .Date("cdate", header: "Date", width: Widths.AnsiChars(10), iseditingreadonly: true)
                 .Text("cutref", header: "Cut Ref", width: Widths.AnsiChars(8), iseditingreadonly: true)
-                .Text("fabriccombo", header: "Comb", width: Widths.AnsiChars(1), iseditingreadonly: true)
+                .Text("PatternPanel", header: "Comb", width: Widths.AnsiChars(1), iseditingreadonly: true)
                 .Numeric("cutno", header: "Cut No.", width: Widths.AnsiChars(3), iseditingreadonly: true)
                 .Text("sizecode", header: "Size", width: Widths.AnsiChars(8), iseditingreadonly: true)
                 .Numeric("bundlegroup", header: "Bundle Group", width: Widths.AnsiChars(7), iseditingreadonly: true)
