d331c7cae96899fab6919b6b6d36d0cb48026fde
JEFF <jeff@sportscity.com.tw>
2016-12-03 10:07:54 +0000

    AND D.FromStockType = F.StockType

--- Sci.Production.Warehouse/P23.cs
+++ Sci.Production.Warehouse/P23.cs
@@ -225,7 +225,7 @@ namespace Sci.Production.Warehouse
             sqlcmd = string.Format(@"Select d.frompoid,d.fromseq1,d.fromseq2,d.fromRoll,d.Qty
 ,isnull(f.InQty,0)-isnull(f.OutQty,0)+isnull(f.AdjustQty,0) as balanceQty
 from dbo.SubTransfer_Detail d inner join FtyInventory f
-on d.FromMDivisionID = f.MDivisionID and d.FromPOID = f.POID 
+on d.FromMDivisionID = f.MDivisionID and d.FromPOID = f.POID  AND D.FromStockType = F.StockType
 and d.FromRoll = f.Roll and d.FromSeq1 =f.Seq1 and d.FromSeq2 = f.Seq2
 where f.lock=1 and d.Id = '{0}'", CurrentMaintain["id"]);
             if (!(result2 = DBProxy.Current.Select(null, sqlcmd, out datacheck)))
@@ -254,8 +254,8 @@ where f.lock=1 and d.Id = '{0}'", CurrentMaintain["id"]);
 ,isnull(f.InQty,0)-isnull(f.OutQty,0)+isnull(f.AdjustQty,0) as balanceQty
 from dbo.SubTransfer_Detail d left join FtyInventory f
 on d.FromMDivisionID = f.MDivisionID and d.FromPOID = f.POID 
-and d.FromRoll = f.Roll and d.FromSeq1 =f.Seq1 and d.FromSeq2 = f.Seq2
-where (isnull(f.InQty,0)-isnull(f.OutQty,0)+isnull(f.AdjustQty,0) - d.Qty < 0) and d.Id = '{0}'", CurrentMaintain["id"]);
+and d.FromRoll = f.Roll and d.FromSeq1 =f.Seq1 and d.FromSeq2 = f.Seq2 AND D.FromStockType = F.StockType
+where (isnull(f.InQty,0)-isnull(f.OutQty,0)+isnull(f.AdjustQty,0) < d.Qty) and d.Id = '{0}'", CurrentMaintain["id"]);
             if (!(result2 = DBProxy.Current.Select(null, sqlcmd, out datacheck)))
             {
                 ShowErr(sqlcmd, result2);
