6337c57ceaf1562a57b4bfe2f47298d1b212e8be
WILLY <willy@sportscity.com.tw>
2016-12-09 08:38:43 +0000

    修正從scifty貸出MDivisionID如果資料對不到要轉''

--- DailyTransfer/imp_debit.sql
+++ DailyTransfer/imp_debit.sql
@@ -25,7 +25,7 @@ insert @Sayfty select id from Production.dbo.Factory
 		t.SysDate = s.SysDate
 	when not matched by target then 	
 		insert(	 ID,  CurrencyID,  Amount,  Received,  BuyerID,  BrandID,  BankID,  LCFNO,  LCFDate,  EstPayDate,  Title,  SendFrom,  Attn,  CC,  Subject,  Handle,  SMR,  VoucherID,  BadID,  Status,  StatusRevise,  StatusReviseNm, CustPayId,   Settled,  SettleDate,  Cfm,  CfmDate,  Lock,  Lockdate,  OldAmount,  Type,  ShareFob,  VoucherFactory,  VoucherSettle,  IsSubcon,  LCLName,  LCLCurrency,  LCLAmount,  LCLRate,  AddName,  AddDate,  EditName,  EditDate,  SysDate,MDivisionID)
-		values(s.ID,s.CurrencyID,s.Amount,s.Received,s.BuyerID,s.BrandID,s.BankID,s.LCFNO,s.LCFDate,s.EstPayDate,s.Title,s.SendFrom,s.Attn,s.CC,s.Subject,s.Handle,s.SMR,s.VoucherID,s.BadID,s.Status,s.StatusRevise,s.StatusReviseNm,s.CustPayId,s.Settled,s.SettleDate,s.Cfm,s.CfmDate,s.Lock,s.Lockdate,s.OldAmount,s.Type,s.ShareFob,s.VoucherFactory,s.VoucherSettle,s.IsSubcon,s.LCLName,s.LCLCurrency,s.LCLAmount,s.LCLRate,s.AddName,s.AddDate,s.EditName,s.EditDate,s.SysDate,(SELECT MDivisionID FROM Production.dbo.scifty where id=s.BrandID))
+		values(s.ID,s.CurrencyID,s.Amount,s.Received,s.BuyerID,s.BrandID,s.BankID,s.LCFNO,s.LCFDate,s.EstPayDate,s.Title,s.SendFrom,s.Attn,s.CC,s.Subject,s.Handle,s.SMR,s.VoucherID,s.BadID,s.Status,s.StatusRevise,s.StatusReviseNm,s.CustPayId,s.Settled,s.SettleDate,s.Cfm,s.CfmDate,s.Lock,s.Lockdate,s.OldAmount,s.Type,s.ShareFob,s.VoucherFactory,s.VoucherSettle,s.IsSubcon,s.LCLName,s.LCLCurrency,s.LCLAmount,s.LCLRate,s.AddName,s.AddDate,s.EditName,s.EditDate,s.SysDate,(SELECT iif(MDivisionID is null,'',MDivisionID) FROM Production.dbo.scifty where id=s.BrandID))
 		output inserted.id,iif(deleted.id is null,1,0) into @Tdebit ;
 
 
@@ -45,7 +45,7 @@ declare @tLocalDebit table (id varchar(13),isinsert bit)
 	when not matched by target and s.IsSubcon = 1 then
 		insert(TaipeiDBC,id, FactoryID, TaipeiAMT, TaipeiCurrencyID, AddDate, AddName,[status],MDivisionID)
 		values( '1', Id, BrandID, Amount, CurrencyID, AddDate,'SCIMIS','New',
-		(SELECT  MDivisionID FROM SCIFTY WHERE ID=S.BRANDID) )
+		(SELECT  iif(MDivisionID is null,'',MDivisionID) FROM SCIFTY WHERE ID=S.BRANDID) )
 		output inserted.id,iif(deleted.id='',1,0) into @tLocalDebit;
 
 
