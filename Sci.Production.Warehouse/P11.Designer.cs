b6c2dc013fb1e6857eb67a8cab826a05d8690615
RICKY <ricky@sportscity.com.tw>
2016-12-13 01:35:21 +0000

    Fix "0001642: WAREHOUSE_P11_Issue Sewing Material by Transfer Guide" Issue 1.修正gridcon對期位置。

--- Sci.Production.Warehouse/P11.Designer.cs
+++ Sci.Production.Warehouse/P11.Designer.cs
@@ -139,8 +139,8 @@
             // 
             // gridicon
             // 
-            this.gridicon.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)));
-            this.gridicon.Location = new System.Drawing.Point(889, 127);
+            this.gridicon.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridicon.Location = new System.Drawing.Point(890, 127);
             // 
             // refresh
             // 
