e1bc4531632342fc4f74240af5f49e5bfe9d976c
JIMMY <jimmy@sportscity.com.tw>
2016-12-14 08:12:53 +0000

    fix (1).RowHeadersVisivle (2).ScrollBars

--- Sci.Production.Warehouse/P03_Transaction.Designer.cs
+++ Sci.Production.Warehouse/P03_Transaction.Designer.cs
@@ -72,7 +72,6 @@
             this.grid1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(162)))), ((int)(((byte)(163)))));
             this.grid1.Location = new System.Drawing.Point(0, 0);
             this.grid1.Name = "grid1";
-            this.grid1.RowHeadersVisible = false;
             this.grid1.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
             this.grid1.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
             this.grid1.RowTemplate.Height = 24;
