1a743312623b283350fa62d884b1d56a998880b9
JIMMY <jimmy@sportscity.com.tw>
2016-12-15 11:33:10 +0000

    改寫 CheckBox

--- Sci.Production.Warehouse/P42.Designer.cs
+++ Sci.Production.Warehouse/P42.Designer.cs
@@ -134,6 +134,7 @@
             this.checkBox2.TabIndex = 4;
             this.checkBox2.Text = "Filter empty Mtl. ETA";
             this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
             // 
             // checkBox1
             // 
@@ -145,6 +146,7 @@
             this.checkBox1.TabIndex = 3;
             this.checkBox1.Text = "Filter empty Each Cons.";
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // dateRange3
             // 
--- Sci.Production.Warehouse/P42.cs
+++ Sci.Production.Warehouse/P42.cs
@@ -160,7 +160,8 @@ namespace Sci.Production.Warehouse
             }
 
             string sqlcmd
-                = string.Format(@"select 0 as Selected,c.MdivisionId,c.POID,a.EachConsApv
+                = string.Format(@"
+select 0 as Selected,c.MdivisionId,c.POID,a.EachConsApv
 ,(select max(FinalETA) from 
 	(select po_supp_detail.FinalETA from PO_Supp_Detail 
 		WHERE PO_Supp_Detail.ID = B.ID 
@@ -195,28 +196,29 @@ AND ((B.Special NOT LIKE ('%DIE CUT%')) and B.Special is not null)", Sci.Env.Use
                 sqlcmd += string.Format(@" and a.BuyerDelivery between '{0}' and '{1}'", buyerdlv_b, buyerdlv_e);
             }
             sqlcmd += "GROUP BY c.MdivisionID,c.POID,a.EachConsApv,B.Special,B.Qty,B.SizeSpec,B.Refno,B.SEQ1,B.SEQ2,c.TapeInline,c.TapeOffline,B.ID,B.ColorID,b.SCIRefno,a.brandid,b.POUnit,b.stockunit";
-            if (eachchk && mtletachk) sqlcmd += @" having EachConsApv is not null and (SELECT MAX(FinalETA) FROM 
-	(SELECT PO_SUPP_DETAIL.FinalETA FROM PO_Supp_Detail 
-		WHERE PO_Supp_Detail.ID = B.ID 
-		AND PO_Supp_Detail.SCIRefno = B.SCIRefno AND PO_Supp_Detail.ColorID = b.ColorID 
-	UNION ALL
-	SELECT B1.FinalETA FROM PO_Supp_Detail a1, PO_Supp_Detail b1
-		WHERE a1.ID = B.ID AND a1.SCIRefno = B.SCIRefno AND a1.ColorID = b.ColorID
-		AND a1.StockPOID = b1.ID and a1.Stockseq1 = b1.SEQ1 and a1.Stockseq2 = b1.SEQ2
-	) tmp) is not null";
-            else
-            {
-                if (eachchk) sqlcmd += " having EachConsApv is not null";
-                if (mtletachk) sqlcmd += @" having (SELECT MAX(FinalETA) FROM 
-	(SELECT PO_SUPP_DETAIL.FinalETA FROM PO_Supp_Detail 
-		WHERE PO_Supp_Detail.ID = B.ID 
-		AND PO_Supp_Detail.SCIRefno = B.SCIRefno AND PO_Supp_Detail.ColorID = b.ColorID 
-	UNION ALL
-	SELECT B1.FinalETA FROM PO_Supp_Detail a1, PO_Supp_Detail b1
-		WHERE a1.ID = B.ID AND a1.SCIRefno = B.SCIRefno AND a1.ColorID = b.ColorID
-		AND a1.StockPOID = b1.ID and a1.Stockseq1 = b1.SEQ1 and a1.Stockseq2 = b1.SEQ2
-	) tmp) is not null";
-            }
+//20161215 CheckBox 選項用 checkBoxs_Status() 取代
+//            if (eachchk && mtletachk) sqlcmd += @" having EachConsApv is not null and (SELECT MAX(FinalETA) FROM 
+//	(SELECT PO_SUPP_DETAIL.FinalETA FROM PO_Supp_Detail 
+//		WHERE PO_Supp_Detail.ID = B.ID 
+//		AND PO_Supp_Detail.SCIRefno = B.SCIRefno AND PO_Supp_Detail.ColorID = b.ColorID 
+//	UNION ALL
+//	SELECT B1.FinalETA FROM PO_Supp_Detail a1, PO_Supp_Detail b1
+//		WHERE a1.ID = B.ID AND a1.SCIRefno = B.SCIRefno AND a1.ColorID = b.ColorID
+//		AND a1.StockPOID = b1.ID and a1.Stockseq1 = b1.SEQ1 and a1.Stockseq2 = b1.SEQ2
+//	) tmp) is not null";
+//            else
+//            {
+//                if (eachchk) sqlcmd += " having EachConsApv is not null";
+//                if (mtletachk) sqlcmd += @" having (SELECT MAX(FinalETA) FROM 
+//	(SELECT PO_SUPP_DETAIL.FinalETA FROM PO_Supp_Detail 
+//		WHERE PO_Supp_Detail.ID = B.ID 
+//		AND PO_Supp_Detail.SCIRefno = B.SCIRefno AND PO_Supp_Detail.ColorID = b.ColorID 
+//	UNION ALL
+//	SELECT B1.FinalETA FROM PO_Supp_Detail a1, PO_Supp_Detail b1
+//		WHERE a1.ID = B.ID AND a1.SCIRefno = B.SCIRefno AND a1.ColorID = b.ColorID
+//		AND a1.StockPOID = b1.ID and a1.Stockseq1 = b1.SEQ1 and a1.Stockseq2 = b1.SEQ2
+//	) tmp) is not null";
+//            }
             sqlcmd += @" ORDER BY c.MdivisionID,c.POID";
             Ict.DualResult result;
             if (result = DBProxy.Current.Select(null, sqlcmd, out dtData))
@@ -224,6 +226,7 @@ AND ((B.Special NOT LIKE ('%DIE CUT%')) and B.Special is not null)", Sci.Env.Use
                 if (dtData.Rows.Count == 0)
                 { MyUtility.Msg.WarningBox("Data not found!!"); }
                 listControlBindingSource1.DataSource = dtData;
+                checkBoxs_Status();
             }
             else
             {
@@ -332,6 +335,28 @@ AND ((B.Special NOT LIKE ('%DIE CUT%')) and B.Special is not null)", Sci.Env.Use
             DataTable dt = (DataTable)listControlBindingSource1.DataSource;
             Object localPrice = dt.Compute("Sum(qty)", "selected = 1");
             this.displayBox1.Value = localPrice.ToString();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBoxs_Status();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBoxs_Status();
+        }
+
+        private void checkBoxs_Status()
+        {
+            if(listControlBindingSource1.DataSource == null)
+                return;
+            string formatStr = "";
+            if (checkBox1.Checked) formatStr += "EachConsApv is not null ";
+            if (checkBox2.Checked)
+                formatStr += (formatStr.EqualString("")) ? "ETA is not null" : "and ETA is not null";
+
+            listControlBindingSource1.Filter = string.Format(formatStr);
         }
     }
 }
