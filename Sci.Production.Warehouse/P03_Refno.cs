78d107efe2473eaf5ce86d5304c9dc316ab866d6
RICKY <ricky@sportscity.com.tw>
2016-12-19 07:30:38 +0000

    Fix "WAREHOUSE_P03_Material Status" Issue 1.修正SEQ顯示方式。

--- Sci.Production.Warehouse/P03_Refno.cs
+++ Sci.Production.Warehouse/P03_Refno.cs
@@ -31,7 +31,7 @@ namespace Sci.Production.Warehouse
         {
             base.OnFormLoaded();
             string selectCommand1
-                = string.Format(@"Select md.mdivisionid ,  b.id, b.seq1+b.seq2 seq
+                = string.Format(@"Select md.mdivisionid ,  b.id,left(b.seq1+' ',3)+b.Seq2 as seq
 , b.colorid,  b.sizespec
 , c.suppid, a.sewinline
 , a.sewline, b.FinalETA
