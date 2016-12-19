b4db65bfaf36c83f6810a0f7f178ffbc48f2c836
YINMEI <yinmei@sportscity.com.tw>
2016-12-19 05:56:34 +0000

    add column - ukey, change pk

--- Database/dbo/Tables/VNConsumption_Detail_Detail.sql
+++ Database/dbo/Tables/VNConsumption_Detail_Detail.sql
@@ -1,12 +1,15 @@
 ﻿CREATE TABLE [dbo].[VNConsumption_Detail_Detail] (
-    [ID]        VARCHAR (13)    NOT NULL,
-    [NLCode]    VARCHAR (5)     CONSTRAINT [DF_VNConsumption_Detail _Detail_NLCode] DEFAULT ('') NOT NULL,
-    [SCIRefno]  VARCHAR (26)    CONSTRAINT [DF_VNConsumption_Detail _Detail_SCIRefno] DEFAULT ('') NOT NULL,
-    [RefNo]     VARCHAR (20)    CONSTRAINT [DF_VNConsumption_Detail _Detail_RefNo] DEFAULT ('') NOT NULL,
-    [Qty]       NUMERIC (12, 4) CONSTRAINT [DF_VNConsumption_Detail _Detail_Qty] DEFAULT ((0)) NOT NULL,
-    [LocalItem] BIT             CONSTRAINT [DF_VNConsumption_Detail _Detail_LocalItem] DEFAULT ((0)) NULL,
-    [FabricBrandID] VARCHAR(8) NULL DEFAULT (''), 
-    [FabricType] VARCHAR NULL DEFAULT (''), 
-    CONSTRAINT [PK_VNConsumption_Detail _Detail] PRIMARY KEY CLUSTERED ([ID], [NLCode], [SCIRefno], [RefNo], [Qty])
+    [ID]            VARCHAR (13)    NOT NULL,
+    [NLCode]        VARCHAR (5)     CONSTRAINT [DF_VNConsumption_Detail _Detail_NLCode] DEFAULT ('') NOT NULL,
+    [SCIRefno]      VARCHAR (26)    CONSTRAINT [DF_VNConsumption_Detail _Detail_SCIRefno] DEFAULT ('') NOT NULL,
+    [RefNo]         VARCHAR (20)    CONSTRAINT [DF_VNConsumption_Detail _Detail_RefNo] DEFAULT ('') NOT NULL,
+    [Qty]           NUMERIC (12, 4) CONSTRAINT [DF_VNConsumption_Detail _Detail_Qty] DEFAULT ((0)) NOT NULL,
+    [LocalItem]     BIT             CONSTRAINT [DF_VNConsumption_Detail _Detail_LocalItem] DEFAULT ((0)) NULL,
+    [FabricBrandID] VARCHAR (8)     DEFAULT ('') NULL,
+    [FabricType]    VARCHAR (1)     DEFAULT ('') NULL,
+    [UKey]          BIGINT          IDENTITY (1, 1) NOT NULL,
+    CONSTRAINT [PK_VNConsumption_Detail _Detail] PRIMARY KEY CLUSTERED ([UKey] ASC)
 );
+
+
 
