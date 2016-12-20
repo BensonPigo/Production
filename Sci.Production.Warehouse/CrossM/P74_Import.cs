a9b3041f1833eaa62797d4478725517a60bcbb5d
JIMMY <jimmy@sportscity.com.tw>
2016-12-20 12:13:38 +0000

    FIX description

--- Sci.Production.Warehouse/CrossM/P74_Import.cs
+++ Sci.Production.Warehouse/CrossM/P74_Import.cs
@@ -64,7 +64,7 @@ and seq1 = '{1}' and seq2 = '{2}'", sp, seq1, seq2, Sci.Env.User.Keyword);
                     ,a.Seq1 as FromSeq1
                     ,a.Seq2 as FromSeq2
                     ,left(a.seq1+' ',3)+a.Seq2 as fromseq
-                    --,dbo.getmtldesc(a.id,a.seq1,a.seq2,2,0) as [Description]
+                    ,dbo.getmtldesc(a.id,a.seq1,a.seq2,2,0) as [Description]
                     ,a.usedqty as poqty
                     ,a.stockunit
                     ,left(b.seq1+' ',3)+b.Seq2 as toseq
