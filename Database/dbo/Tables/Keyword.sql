3c9f1828506d36cae849a999f4eb75237b772dd0
WILLY <willy@sportscity.com.tw>
2016-12-20 11:39:32 +0000

    新增dbo.Keyword

---
+++ Database/dbo/Tables/Keyword.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Keyword] (
+    [ID]             VARCHAR (30)   NOT NULL,
+    [Description]    NVARCHAR (100) NOT NULL,
+    [Junk]           BIT            NULL,
+    [Prefix]         NVARCHAR (150) NULL,
+    [Fieldname]      NVARCHAR (100) NULL,
+    [Postfix]        NVARCHAR (150) NULL,
+    [IsSize]         BIT            NULL,
+    [IsPatternPanel] BIT            NULL,
+    [AddName]        VARCHAR (10)   NULL,
+    [AddDate]        DATETIME       NULL,
+    [EditName]       VARCHAR (10)   NULL,
+    [EditDate]       DATETIME       NULL
+);
+
