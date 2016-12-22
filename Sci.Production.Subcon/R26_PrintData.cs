10c4e1fceb62d2ae0108e21bc21eec23098bbed7
ELENA <elena@sportscity.com.tw>
2016-12-22 08:24:33 +0000

    update

--- Sci.Production.Subcon/R26_PrintData.cs
+++ Sci.Production.Subcon/R26_PrintData.cs
@@ -21,9 +21,9 @@ namespace Sci.Production.Subcon
         public string Title1 { get; set; }
         public string Issue_Date { get; set; }
         public string To { get; set; }
-        public string Group_FIDS { get { return this.Title1 + this.Issue_Date + this.To; } }
+        public string Group_FIDS { get { return this.Title1 + this.To; } }
         public string Group_PO { get { return this.PO + this.Code + this.To; } }
-        public string Group_O { get { return this.PO + this.Code + this.To; } }
+      
        
     }
 }
