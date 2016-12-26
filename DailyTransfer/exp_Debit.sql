a431d46f6eadfd3a25f333ff6d7ff44c55b1692f
WILLY <willy@sportscity.com.tw>
2016-12-26 07:21:32 +0000

    Debit_Schedule 以id,issueDate為key值,並將重複的資料Amont合併後只顯示一筆

--- DailyTransfer/exp_Debit.sql
+++ DailyTransfer/exp_Debit.sql
@@ -5,7 +5,7 @@
 -- Create date: <2016/08/17>
 -- Description:	<exp_debit>
 -- =============================================
-CREATE PROCEDURE [dbo].[exp_Debit]
+Create PROCEDURE [dbo].[exp_Debit]
 AS
 
 IF OBJECT_ID(N'dbo.Debit') IS NOT NULL
@@ -25,21 +25,23 @@ INTO Debit
 FROM Production.dbo.Debit
 WHERE EditDate 
 BETWEEN Convert(DATE,DATEADD(day,-30,GETDATE()))AND CONVERT(date,Getdate()) OR SysDate BETWEEN Convert(DATE,DATEADD(day,-30,GETDATE())) AND CONVERT(date,Getdate())
-ORDER BY ID 
+ORDER BY ID ;
 
 
-Select ds.* 
-INTO Debit5
-from Production.dbo.Debit_Schedule ds
+with amount as(
+ select id,IssueDate,sum(Amount) as Amount from Production.dbo.Debit_Schedule group by id,IssueDate
+ ),
+allDebit as(
+ Select ds.* 
+from ( select * from (
+ select RN = Row_number() over (partition by id,issuedate order by id,issuedate),* from Production.dbo.Debit_Schedule ) a
+ where a.RN=1) ds
 inner join Production.dbo.Debit d on ds.ID = d.ID
 where exists (select 1 from Production.dbo.Debit_Schedule tds 
-where tds.ID = ds.ID 
-and (tds.AddDate between Convert(DATE,DATEADD(day,-30,GETDATE())) AND  CONVERT(date,Getdate())
-or tds.EditDate between  Convert(DATE,DATEADD(day,-30,GETDATE())) AND  CONVERT(date,Getdate())
-or tds.SysDate between  Convert(DATE,DATEADD(day,-30,GETDATE())) AND  CONVERT(date,Getdate()))
-)
-
-
-
-
+where tds.ID = ds.ID )
+ )
+ select a.ID,a.IssueDate,a.Amount,b.VoucherID,b.AddName,b.AddDate,b.EditDate,b.EditName,b.SysDate 
+ INTO Debit5
+ from amount a
+ inner join allDebit b on a.id=b.id and a.IssueDate=b.IssueDate 
 
