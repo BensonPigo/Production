faca3e4e527cdcf2ae8ad067567cdc01dc5310a5
JEFF <jeff@sportscity.com.tw>
2016-12-26 06:10:40 +0000

    ADD rd1 & 2

---
+++ Sci.Production.Cutting/P01_Print_OrderList.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Sci.Production.Cutting
+{
+    partial class P01_Print_OrderList
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdCheck1 = new Sci.Win.UI.RadioButton();
+            this.rdCheck6 = new Sci.Win.UI.RadioButton();
+            this.rdCheck2 = new Sci.Win.UI.RadioButton();
+            this.rdCheck5 = new Sci.Win.UI.RadioButton();
+            this.rdCheck3 = new Sci.Win.UI.RadioButton();
+            this.rdCheck4 = new Sci.Win.UI.RadioButton();
+            this.btnCancel = new Sci.Win.UI.Button();
+            this.btnToExcel = new Sci.Win.UI.Button();
+            this.label5 = new Sci.Win.UI.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdCheck1);
+            this.groupBox1.Controls.Add(this.rdCheck6);
+            this.groupBox1.Controls.Add(this.rdCheck2);
+            this.groupBox1.Controls.Add(this.rdCheck5);
+            this.groupBox1.Controls.Add(this.rdCheck3);
+            this.groupBox1.Controls.Add(this.rdCheck4);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(368, 221);
+            this.groupBox1.TabIndex = 180;
+            this.groupBox1.TabStop = false;
+            // 
+            // rdCheck1
+            // 
+            this.rdCheck1.AutoSize = true;
+            this.rdCheck1.Checked = true;
+            this.rdCheck1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.rdCheck1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rdCheck1.Location = new System.Drawing.Point(6, 22);
+            this.rdCheck1.Name = "rdCheck1";
+            this.rdCheck1.Size = new System.Drawing.Size(282, 24);
+            this.rdCheck1.TabIndex = 173;
+            this.rdCheck1.TabStop = true;
+            this.rdCheck1.Text = "Each Consumption (Cutting Combo)";
+            this.rdCheck1.UseVisualStyleBackColor = true;
+            // 
+            // rdCheck6
+            // 
+            this.rdCheck6.AutoSize = true;
+            this.rdCheck6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.rdCheck6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rdCheck6.Location = new System.Drawing.Point(6, 157);
+            this.rdCheck6.Name = "rdCheck6";
+            this.rdCheck6.Size = new System.Drawing.Size(264, 44);
+            this.rdCheck6.TabIndex = 178;
+            this.rdCheck6.Text = "Consumption Calculate by Marker\r\nList Cons/Per pc";
+            this.rdCheck6.UseVisualStyleBackColor = true;
+            // 
+            // rdCheck2
+            // 
+            this.rdCheck2.AutoSize = true;
+            this.rdCheck2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.rdCheck2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rdCheck2.Location = new System.Drawing.Point(6, 49);
+            this.rdCheck2.Name = "rdCheck2";
+            this.rdCheck2.Size = new System.Drawing.Size(240, 24);
+            this.rdCheck2.TabIndex = 174;
+            this.rdCheck2.Text = "TTL consumption (PO Combo)";
+            this.rdCheck2.UseVisualStyleBackColor = true;
+            // 
+            // rdCheck5
+            // 
+            this.rdCheck5.AutoSize = true;
+            this.rdCheck5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.rdCheck5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rdCheck5.Location = new System.Drawing.Point(6, 130);
+            this.rdCheck5.Name = "rdCheck5";
+            this.rdCheck5.Size = new System.Drawing.Size(105, 24);
+            this.rdCheck5.TabIndex = 177;
+            this.rdCheck5.Text = "Marker List";
+            this.rdCheck5.UseVisualStyleBackColor = true;
+            // 
+            // rdCheck3
+            // 
+            this.rdCheck3.AutoSize = true;
+            this.rdCheck3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.rdCheck3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rdCheck3.Location = new System.Drawing.Point(6, 76);
+            this.rdCheck3.Name = "rdCheck3";
+            this.rdCheck3.Size = new System.Drawing.Size(249, 24);
+            this.rdCheck3.TabIndex = 175;
+            this.rdCheck3.Text = "Color & Q\'ty B\'Down (PO Combo)";
+            this.rdCheck3.UseVisualStyleBackColor = true;
+            // 
+            // rdCheck4
+            // 
+            this.rdCheck4.AutoSize = true;
+            this.rdCheck4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.rdCheck4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rdCheck4.Location = new System.Drawing.Point(6, 103);
+            this.rdCheck4.Name = "rdCheck4";
+            this.rdCheck4.Size = new System.Drawing.Size(350, 24);
+            this.rdCheck4.TabIndex = 176;
+            this.rdCheck4.Text = "Each cons. vs Order Q\'ty B\'Down (PO Combo)";
+            this.rdCheck4.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(394, 82);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(80, 30);
+            this.btnCancel.TabIndex = 184;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnToExcel
+            // 
+            this.btnToExcel.Location = new System.Drawing.Point(394, 32);
+            this.btnToExcel.Name = "btnToExcel";
+            this.btnToExcel.Size = new System.Drawing.Size(80, 30);
+            this.btnToExcel.TabIndex = 183;
+            this.btnToExcel.Text = "To Excel";
+            this.btnToExcel.UseVisualStyleBackColor = true;
+            this.btnToExcel.Click += new System.EventHandler(this.btnToExcel_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.SystemColors.Control;
+            this.label5.Lines = 0;
+            this.label5.Location = new System.Drawing.Point(383, 142);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 23);
+            this.label5.TabIndex = 182;
+            this.label5.Text = "Pager Size A4";
+            this.label5.TextStyle.BorderColor = System.Drawing.Color.Transparent;
+            this.label5.TextStyle.Color = System.Drawing.Color.Blue;
+            // 
+            // P01_Print_OrderList
+            // 
+            this.ClientSize = new System.Drawing.Size(494, 248);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnToExcel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "P01_Print_OrderList";
+            this.Controls.SetChildIndex(this.groupBox1, 0);
+            this.Controls.SetChildIndex(this.label5, 0);
+            this.Controls.SetChildIndex(this.btnToExcel, 0);
+            this.Controls.SetChildIndex(this.btnCancel, 0);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private Win.UI.RadioButton rdCheck1;
+        private Win.UI.RadioButton rdCheck6;
+        private Win.UI.RadioButton rdCheck2;
+        private Win.UI.RadioButton rdCheck5;
+        private Win.UI.RadioButton rdCheck3;
+        private Win.UI.RadioButton rdCheck4;
+        private Win.UI.Button btnCancel;
+        private Win.UI.Button btnToExcel;
+        private Win.UI.Label label5;
+    }
+}
---
+++ Sci.Production.Cutting/P01_Print_OrderList.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Sci;
+using Sci.Data;
+using Ict;
+using Ict.Win;
+using Sci.Win;
+//using Sci.Trade.Class.Commons;
+using sxrc = Sci.Utility.Excel.SaveXltReportCls;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Sci.Production.Cutting
+{
+    public partial class P01_Print_OrderList : Sci.Win.Tems.QueryForm
+    {
+        string _id;
+
+        public P01_Print_OrderList(string args)
+        {
+            this._id = args;
+            InitializeComponent();
+            EditMode = true;
+        }
+
+        private bool ToExcel()
+        {
+            if (rdCheck1.Checked)
+            {
+                #region rdCheck1
+                System.Data.DataTable[] dts;
+                DualResult res = DBProxy.Current.SelectSP("", "Cutting_P01_01", new List<SqlParameter> { new SqlParameter("@OrderID", _id) }, out dts);
+
+                if (!res) return false;
+                if (dts.Length < 2) return false;
+
+                DataRow dr = dts[0].Rows[0];
+
+                string xltPath = System.IO.Path.Combine(Env.Cfg.XltPathDir, "Cutting_P01_EachConsumptionCuttingCombo.xlt");
+                sxrc sxr = new sxrc(xltPath);
+                sxr.CopySheet.Add(1, dts.Length - 2);
+
+                for (int sgIdx = 1; sgIdx < dts.Length; sgIdx++)
+                {
+
+                    string idxStr = (sgIdx == 1) ? "" : (sgIdx - 1).ToString();
+                    string SizeGroup = dts[sgIdx].Rows[0]["SizeGroup"].ToString();
+                    string MarkerDownloadID = dts[sgIdx].Compute("MAX(MarkerDownloadID)", "").ToString();
+
+                    dts[sgIdx].Columns.RemoveAt(1);
+                    dts[sgIdx].Columns.RemoveAt(0);
+
+                    extra_P01_EachConsumptionCuttingCombo(dts[sgIdx]);
+
+                    sxr.dicDatas.Add(sxr._v + "APPLYNO" + idxStr, dr["APPLYNO"]);
+                    sxr.dicDatas.Add(sxr._v + "MARKERNO" + idxStr, dr["MARKERNO"]);
+                    sxr.dicDatas.Add(sxr._v + "CUTTINGSP" + idxStr, dr["CUTTINGSP"]);
+                    sxr.dicDatas.Add(sxr._v + "ORDERNO" + idxStr, dr["ORDERNO"]);
+                    sxr.dicDatas.Add(sxr._v + "STYLENO" + idxStr, dr["STYLENO"]);
+                    sxr.dicDatas.Add(sxr._v + "QTY" + idxStr, dr["QTY"]);
+                    sxr.dicDatas.Add(sxr._v + "FACTORY" + idxStr, dr["FACTORY"]);
+                    sxrc.xltRptTable dt = new sxrc.xltRptTable(dts[sgIdx]);
+
+                    //欄位水平對齊
+                    for (int i = 5; i <= dt.Columns.Count - 8; i++)
+                    {
+                        sxrc.xlsColumnInfo citbl = new sxrc.xlsColumnInfo(i, false, 6, Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignRight);
+                        dt.lisColumnInfo.Add(citbl);
+                    }
+                    dt.lisColumnInfo.Add(new sxrc.xlsColumnInfo(dt.Columns.Count - 7, true, 0, Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignLeft));
+                    dt.lisColumnInfo.Add(new sxrc.xlsColumnInfo(dt.Columns.Count - 6, true, 0, Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignRight));
+                    dt.lisColumnInfo.Add(new sxrc.xlsColumnInfo(dt.Columns.Count - 5, true, 0, Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignLeft));
+                    dt.lisColumnInfo.Add(new sxrc.xlsColumnInfo(dt.Columns.Count - 4, true, 0, Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignRight));
+                    dt.lisColumnInfo.Add(new sxrc.xlsColumnInfo(dt.Columns.Count - 3, true, 0, Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignRight));
+                    dt.lisColumnInfo.Add(new sxrc.xlsColumnInfo(dt.Columns.Count - 2, true, 0, Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignRight));
+                    dt.lisColumnInfo.Add(new sxrc.xlsColumnInfo(dt.Columns.Count - 1, true, 0, Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignRight));
+                    dt.lisColumnInfo.Add(new sxrc.xlsColumnInfo(dt.Columns.Count, true, 0, Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignRight));
+                    //合併儲存格
+                    dt.lisTitleMerge.Add(new Dictionary<string, string> { { "SIZE RATIO OF MARKER", string.Format("{0},{1}", 5, dt.Columns.Count - 8) } });
+
+                    dt.Borders.AllCellsBorders = true;
+
+                    sxr.dicDatas.Add(sxr._v + "tbl1" + idxStr, dt);
+                    sxr.dicDatas.Add(sxr._v + "SizeGroup" + idxStr, SizeGroup);
+                    //sxr.dicDatas.Add(sxr._v + "Now", DateTime.Now);
+                    sxr.dicDatas.Add(sxr._v + "MarkerDownloadID" + idxStr, MarkerDownloadID);
+
+                }
+
+                sxr.VarToSheetName = sxr._v + "SizeGroup";
+
+                sxr.boOpenFile = true;
+                sxr.Save();
+                //SaveExcel(sxr, xltPath);
+                #endregion
+            }
+            if (rdCheck2.Checked)
+            {
+                #region rdCheck2
+                System.Data.DataTable[] dts;
+                DualResult res = DBProxy.Current.SelectSP("", "Cutting_P01_02", new List<SqlParameter> { new SqlParameter("@OrderID", _id) }, out dts);
+
+                if (!res) return false;
+                if (dts.Length < 2) return false;
+
+                DataRow dr = dts[0].Rows[0];
+                extra_P01_Report_TTLconsumptionPOCombo(dts[1], Convert.ToInt32(dr["QTY"]));
+
+                string xltPath = System.IO.Path.Combine(Env.Cfg.XltPathDir, "Cutting_P01_TTLconsumptionPOCombo.xlt");
+                sxrc sxr = new sxrc(xltPath);
+                sxr.dicDatas.Add(sxr._v + "ORDERNO", dr["ORDERNO"]);
+                sxr.dicDatas.Add(sxr._v + "STYLENO", dr["STYLENO"]);
+
+                sxr.dicDatas.Add(sxr._v + "QTY", dr["QTY"]);
+                sxr.dicDatas.Add(sxr._v + "FTY", dr["FACTORY"]);
+
+                sxr.dicDatas.Add(sxr._v + "FABTYPE", dr["FABTYPE"]);
+                sxr.dicDatas.Add(sxr._v + "FLP", dr["FLP"].ToString());
+                sxr.dicDatas.Add(sxr._v + "MarkerDownloadID", dr["MarkerDownloadID"]);
+
+                sxr.dicDatas.Add(sxr._v + "Now", DateTime.Now);
+
+                sxrc.xltRptTable dt = new sxrc.xltRptTable(dts[1]);
+
+                //欄位水平對齊
+                for (int i = 3; i <= dt.Columns.Count; i++)
+                {
+                    sxrc.xlsColumnInfo citbl = new sxrc.xlsColumnInfo(i, true, 0, Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignRight);
+
+                    if (i == 4 | i == 6 | i == 8 | i == 7)
+                    {
+                        citbl.PointCnt = 2; //小數點兩位
+                    }
+                    else if (i == 9)
+                    {
+                        citbl.PointCnt = 0;
+                    }
+                    else if (i == 13)
+                    {
+                        citbl.PointCnt = 3;
+                    }
+                    dt.lisColumnInfo.Add(citbl);
+                }
+                dt.lisColumnInfo.Add(new sxrc.xlsColumnInfo(1, true, 0, Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignLeft));
+                dt.lisColumnInfo.Add(new sxrc.xlsColumnInfo(2, true, 0, Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignLeft));
+                //合併儲存格
+                //dt.lisTitleMerge.Add(new Dictionary<string, string> { { "Usage", string.Format("{0},{1}", 3, 4) }, { "Purchase", string.Format("{0},{1}", 5, 6) } });
+                dt.Borders.DependOnColumn.Add(1, 2);
+
+                //不顯示標題列
+                dt.ShowHeader = false;
+
+                sxr.dicDatas.Add(sxr._v + "tbl1", dt);
+
+                sxr.boOpenFile = true;
+                sxr.Save();
+                //SaveExcel(sxr, xltPath);
+                #endregion
+            }
+
+            return true;
+        }
+
+        void extra_P01_EachConsumptionCuttingCombo(DataTable dt)
+        {
+            string COMB = "";
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                DataRow dr = dt.Rows[i];
+                if (COMB != dr["COMB"].ToString().Trim())
+                {
+                    DataRow ndr = dt.NewRow();
+                    ndr["COMB"] = dr["COMB"];
+                    ndr["REMARK"] = dr["COMBdes"];
+
+                    dt.Rows.InsertAt(ndr, i);
+
+                    i += 1;
+                    COMB = dr["COMB"].ToString().Trim();
+                }
+                dr["COMB"] = "";
+            }
+
+            dt.Columns.Remove("COMBdes");
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                DataRow dr = dt.Rows[i];
+                if (dr["REMARK"].ToString().Trim() != "" & dr["COMB"].ToString().Trim() == "")
+                {
+                    DataRow ndr = dt.NewRow();
+                    ndr["REMARK"] = dr["REMARK"];
+                    dr["REMARK"] = "";
+                    dt.Rows.InsertAt(ndr, i);
+                    i += 1;
+                }
+
+            }
+        }
+
+        void extra_P01_Report_TTLconsumptionPOCombo(DataTable dt, int Qty)
+        {
+            string coltmp = "";
+            decimal totaltmp = 0;
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                string col1content = dt.Rows[i][0].ToString();
+
+                if (coltmp != col1content)
+                {
+
+                    if (coltmp == "")
+                    {
+                        coltmp = col1content;
+                    }
+                    else
+                    {
+                        coltmp = col1content;
+                        addSubTotalRow(dt, totaltmp, i, Qty);
+
+                        totaltmp = 0;
+                        i += 1;
+                    }
+                }
+                else
+                {
+                    dt.Rows[i][0] = DBNull.Value;
+                    dt.Rows[i]["M/WIDTH"] = DBNull.Value;
+                    dt.Rows[i]["M/WEIGHT"] = DBNull.Value;
+                    dt.Rows[i]["STYLE DATA CONS/PC"] = DBNull.Value;
+                }
+
+                totaltmp += decimal.Parse(dt.Rows[i]["TOTAL(Inclcut. use)"].ToString());
+            }
+
+            addSubTotalRow(dt, totaltmp, dt.Rows.Count, Qty);
+        }
+
+        void addSubTotalRow(DataTable dt, decimal tot, int idx, int Qty)
+        {
+            DataRow dr = dt.NewRow();
+            dr["TOTAL(Inclcut. use)"] = tot;
+            dr["M/WEIGHT"] = "SubTotal";
+            dr["CONS/PC"] = Qty == 0 ? 0 : Math.Round(tot / Qty, 3);
+            dt.Rows.InsertAt(dr, idx);
+        }
+
+        private void btnToExcel_Click(object sender, EventArgs e)
+        {
+            ToExcel();
+        }
+
+
+    }
+}
