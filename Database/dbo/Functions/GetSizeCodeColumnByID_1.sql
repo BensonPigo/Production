98da28420b49156bc6cc160da64603e9ce9db45f
JEFF <jeff@sportscity.com.tw>
2016-12-26 03:46:42 +0000

    add

---
+++ Database/dbo/Functions/GetSizeCodeColumnByID_1.sql
@@ -0,0 +1,64 @@
+﻿
+Create FUNCTION [dbo].[GetSizeCodeColumnByID]
+(
+	@OrderID varchar(13) = ''
+	,@ByType bit = 0 --0單張 , 1 By CustCDID , 2 By PO
+)
+RETURNS @tbl table(SizeGroup varchar(1),Seq varchar(2),SizeCode varchar(8))
+AS
+BEGIN
+	
+	declare @main table(id varchar(13),SizeGroup varchar(1),Seq varchar(2),SizeCode varchar(8))
+	--main1 取得全部SizeCode
+	insert into @main select id,SizeGroup,Seq,SizeCode from Order_SizeCode WHERE ID IN (SELECT POID FROM Orders where Id = @OrderID)	
+	
+	declare @poid varchar(13) = (select POID from Orders where ID = @OrderID)
+
+	declare @id table (id varchar(13))
+	if(@ByType = 0)
+		insert into @id select id from dbo.Orders where ID = @OrderID
+	else if(@ByType = 1)
+		insert into @id select id from dbo.Orders where ID in (select id from Trade.dbo.Orders where POID = @poid AND CustCDID = (select CustCDID from Orders where ID = @OrderID) )
+	else if(@ByType = 2)
+		insert into @id select id from dbo.Orders where ID in (select id from Trade.dbo.Orders where POID = @poid )
+		
+			
+	--main2 取得哪些有QTY
+	declare @main2 table(SizeGroup varchar(1),Seq varchar(2),SizeCode varchar(8))
+	insert into @main2
+	select a.SizeGroup,a.Seq,a.SizeCode from @main a inner join dbo.Order_Qty b on a.SizeCode = b.SizeCode
+		where b.ID in (select id from @id)
+	group by a.SizeGroup,a.Seq,a.SizeCode order by SizeGroup,a.Seq,a.SizeCode
+		
+
+	declare cs cursor for
+	select distinct SizeGroup from @main2
+	open cs
+	declare @SizeGroup varchar(1)
+	fetch next from cs into @SizeGroup
+	while @@FETCH_STATUS=0
+		begin
+		
+			declare @maxScode varchar(8) = (SELECT MAX(SizeCode) FROM @main2 WHERE SizeGroup = @SizeGroup)
+			declare @minScode varchar(8) = (SELECT MIN(SizeCode) FROM @main2 WHERE SizeGroup = @SizeGroup)
+			
+			--if(@ByType = 0)
+			--begin
+			--	insert into @tbl
+			--		select SizeGroup,Seq,SizeCode from @main where SizeGroup = @SizeGroup
+			--		group by SizeGroup,Seq,SizeCode order by Seq
+			--end
+			--else
+			--begin
+				insert into @tbl
+					select SizeGroup,Seq,SizeCode from @main where SizeGroup = @SizeGroup AND SizeCode BETWEEN @minScode AND @maxScode
+					group by SizeGroup,Seq,SizeCode order by Seq
+			--end
+
+			fetch next from cs into @SizeGroup
+		end
+	close cs
+	deallocate cs
+
+	return
+END
---
+++ Database/dbo/Stored Procedures/Cutting_P01_01.sql
@@ -0,0 +1,79 @@
+﻿
+Create PROCEDURE [dbo].[Cutting_P01_01]
+	@OrderID VARCHAR(13)
+AS
+BEGIN
+	
+	--抓取ID為POID
+	select @OrderID=POID FROM dbo.Orders where ID = @OrderID
+	
+	SELECT
+	APPLYNO=d.SMNoticeID ,MARKERNO=d.MARKERNO ,CUTTINGSP=CuttingSP ,ORDERNO=RTRIM(POID) + d.spno ,STYLENO=StyleID+'-'+SeasonID ,QTY=SUM(Qty) ,FACTORY=FactoryID
+	FROM dbo.Orders
+	OUTER APPLY(SELECT STUFF((SELECT '/'+SUBSTRING(ID,11,4) FROM Trade.dbo.Orders WHERE POID = @OrderID  order by ID FOR XML PATH(''), TYPE ).value('.', 'NVARCHAR(MAX)'),1,1,'') as spno
+	,STUFF((SELECT '/'+MarkerNo+'-'+MarkerVersion FROM dbo.Order_EachCons WHERE Id = @OrderID group by MarkerNo,MarkerVersion FOR XML PATH(''), TYPE ).value('.', 'NVARCHAR(MAX)'),1,1,'') as MarkerNo
+	,STUFF((SELECT '/'+SMNoticeID FROM dbo.Order_EachCons WHERE Id = @OrderID group by SMNoticeID FOR XML PATH(''), TYPE ).value('.', 'NVARCHAR(MAX)'),1,1,'') as SMNoticeID) d
+	WHERE POID = @OrderID
+	GROUP BY d.MarkerNo,d.SMNoticeID,CuttingSP,POID,d.spno,StyleID,SeasonID,FactoryID
+	
+	Select a.id,a.Ukey
+	,'COMB' = LectraCode
+	,'COMBdes' = e.Refno + ' ' + e.Description + '                Mark Width:' + a.Width + '  Weight(YDS):' + cast(e.Weight as nvarchar(20))
+	,MarkerName,SizeCode,MarkerLength,a.ConsPC
+	,Seq,a.REMARK,Qty
+	,ColorID,Orderqty,Layer,CutQty,Variance,c.Order_EachConsUkey,c.YDS
+	,MarkerDownloadID,Article into #tmp
+	From dbo.Order_EachCons a
+	inner join dbo.Order_EachCons_SizeQty b on a.Id = b.Id and a.Ukey = b.Order_EachConsUkey
+	inner join dbo.Order_EachCons_Color c on a.Id = c.Id and a.Ukey = c.Order_EachConsUkey
+	inner join dbo.Order_BOF d on a.Id = d.Id and a.FabricCode = d.FabricCode
+	inner join dbo.Fabric e on d.SCIRefno = e.SCIRefno
+	where a.id in (select CuttingSP from dbo.Orders where Id = @OrderID)
+	order by Seq
+
+	select * into #SizeCodes from GetSizeCodeColumnByID(@OrderID,1) a where a.SizeCode in (select SizeCode from #tmp) order by Seq
+
+	--SELECT SizeGroup FROM #SizeCodes group by SizeGroup
+
+	--For Each Size Group
+	declare scode cursor for
+		select SizeGroup from #SizeCodes group by SizeGroup order by max(Seq)
+	open scode
+
+	declare @SizeGroup varchar(5)
+	fetch next from scode into @SizeGroup
+	while @@FETCH_STATUS=0
+		begin
+			
+			declare @colStr nvarchar(max) = '' ; declare @colStr2 nvarchar(max) = '' ; declare @colStr3 nvarchar(max) = ''
+
+			select @colStr =  STUFF((SELECT ',['+SizeCode+']' from #SizeCodes a where a.SizeGroup = @SizeGroup order by Seq FOR XML PATH(''), TYPE ).value('.', 'NVARCHAR(MAX)'),1,1,'')	
+			select @colStr3= 'where ' + STUFF((SELECT ' ['+SizeCode+'] is not null or' from #SizeCodes a where a.SizeGroup = @SizeGroup order by Seq FOR XML PATH(''), TYPE ).value('.', 'NVARCHAR(MAX)'),1,1,'')
+			select @colStr3 = substring(@colStr3,1,len(@colStr3)-3)
+	
+			declare @sql varchar(max) = 'SELECT SizeGroup='''+ @SizeGroup +''',MarkerDownloadID,COMB,COMBdes,MarkerName,REMARK,Article,'+@colStr+',MarkerLength as ''MAKER LEN.+1"'',ConsPC AS ''PC(SET)'',ColorID AS COLOR,Orderqty,Layer,CutQty,Variance AS ''Var.'', YDS as ''Cons.(YDS)'' FROM (
+				select COMB,COMBdes,MarkerName,MarkerLength,ConsPC,REMARK,Article,SizeCode,c.ColorID,c.Orderqty,c.Layer,c.CutQty,c.Variance,c.YDS,Qty,Seq,MarkerDownloadID from #tmp a
+				outer apply (select 
+				ColorID=STUFF((SELECT char(10)+ColorID FROM #tmp where id = a.Id and Order_EachConsUkey = a.Ukey order by ColorID FOR XML PATH(''''), TYPE ).value(''.'',''NVARCHAR(MAX)''),1,1,'''')
+				,Orderqty=STUFF((SELECT char(10)+cast(Orderqty as varchar) FROM #tmp where id = a.Id and Order_EachConsUkey = a.Ukey order by ColorID FOR XML PATH(''''), TYPE ).value(''.'',''NVARCHAR(MAX)''),1,1,'''')
+				,Layer=STUFF((SELECT char(10)+cast(Layer as varchar) FROM #tmp where id = a.Id and Order_EachConsUkey = a.Ukey order by ColorID FOR XML PATH(''''), TYPE ).value(''.'',''NVARCHAR(MAX)''),1,1,'''')
+				,CutQty=STUFF((SELECT char(10)+cast(CutQty as varchar) FROM #tmp where id = a.Id and Order_EachConsUkey = a.Ukey order by ColorID FOR XML PATH(''''), TYPE ).value(''.'',''NVARCHAR(MAX)''),1,1,'''')
+				,Variance=STUFF((SELECT char(10)+cast(Variance as varchar) FROM #tmp where id = a.Id and Order_EachConsUkey = a.Ukey order by ColorID FOR XML PATH(''''), TYPE ).value(''.'',''NVARCHAR(MAX)''),1,1,'''')
+				,YDS=STUFF((SELECT char(10)+cast(YDS as varchar) FROM #tmp where id = a.Id and Order_EachConsUkey = a.Ukey order by ColorID FOR XML PATH(''''), TYPE ).value(''.'',''NVARCHAR(MAX)''),1,1,'''')
+				) c
+			) a pivot (max(Qty) for SizeCode in ('+@colStr+')) b '+@colStr3+'
+			order by Seq'
+
+			--print @sql
+			exec (@sql)
+			
+			fetch next from scode into @SizeGroup
+		end
+	close scode
+	deallocate scode
+
+	
+	drop table #SizeCodes
+	drop table #tmp
+	
+END
