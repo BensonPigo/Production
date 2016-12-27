e89049f279b7758699b0d829d3cc712b7aa89be1
YINMEI <yinmei@sportscity.com.tw>
2016-12-27 12:48:38 +0000

    修改RefNo欄位結構

--- Database/dbo/Tables/VNConsumption_Detail_Detail.sql
+++ Database/dbo/Tables/VNConsumption_Detail_Detail.sql
@@ -2,7 +2,7 @@
     [ID]            VARCHAR (13)    NOT NULL,
     [NLCode]        VARCHAR (5)     CONSTRAINT [DF_VNConsumption_Detail _Detail_NLCode] DEFAULT ('') NOT NULL,
     [SCIRefno]      VARCHAR (26)    CONSTRAINT [DF_VNConsumption_Detail _Detail_SCIRefno] DEFAULT ('') NOT NULL,
-    [RefNo]         VARCHAR (20)    CONSTRAINT [DF_VNConsumption_Detail _Detail_RefNo] DEFAULT ('') NOT NULL,
+    [RefNo]         VARCHAR (21)    CONSTRAINT [DF_VNConsumption_Detail _Detail_RefNo] DEFAULT ('') NOT NULL,
     [Qty]           NUMERIC (12, 4) CONSTRAINT [DF_VNConsumption_Detail _Detail_Qty] DEFAULT ((0)) NOT NULL,
     [LocalItem]     BIT             CONSTRAINT [DF_VNConsumption_Detail _Detail_LocalItem] DEFAULT ((0)) NULL,
     [FabricBrandID] VARCHAR (8)     DEFAULT ('') NULL,
