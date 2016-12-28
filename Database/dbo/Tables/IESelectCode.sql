77ab16f665fdb973157aa0eca11c39ad30269c54
YINMEI <yinmei@sportscity.com.tw>
2016-12-28 03:04:20 +0000

    Add Table: IESelectCode

---
+++ Database/dbo/Tables/IESelectCode.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[IESelectCode] (
+    [Type]     VARCHAR (5)  CONSTRAINT [DF_IESelectCode_Type] DEFAULT ('') NOT NULL,
+    [ID]       VARCHAR (10) CONSTRAINT [DF_IESelectCode_ID] DEFAULT ('') NOT NULL,
+    [Name]     VARCHAR (30) CONSTRAINT [DF_IESelectCode_Name] DEFAULT ('') NULL,
+    [AddName]  VARCHAR (10) CONSTRAINT [DF_IESelectCode_AddName] DEFAULT ('') NULL,
+    [AddDate]  DATETIME     NULL,
+    [EditName] VARCHAR (10) CONSTRAINT [DF_IESelectCode_EditName] DEFAULT ('') NULL,
+    [EditDate] DATETIME     NULL,
+    CONSTRAINT [PK_IESelectCode] PRIMARY KEY CLUSTERED ([Type] ASC, [ID] ASC)
+);
+
