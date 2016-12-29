fb5d6080d4bbaf369223ccbc3cce305fc99204d3
JIMMY <jimmy@sportscity.com.tw>
2016-12-29 05:45:47 +0000

    RowHeaders

--- Sci.Production.Warehouse/P37_AccumulatedQty.Designer.cs
+++ Sci.Production.Warehouse/P37_AccumulatedQty.Designer.cs
@@ -28,8 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new Sci.Win.UI.Button();
-            this.bindingSource1 = new Sci.Win.UI.BindingSource();
+            this.bindingSource1 = new Sci.Win.UI.BindingSource(this.components);
             this.grid1 = new Sci.Win.UI.Grid();
             this.panel1 = new Sci.Win.UI.Panel();
             this.panel2 = new Sci.Win.UI.Panel();
@@ -62,16 +63,15 @@
             this.grid1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.grid1.EditingEnter = Ict.Win.UI.DataGridViewEditingEnter.NextCellOrNextRow;
             this.grid1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
-            this.grid1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.grid1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.grid1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(162)))), ((int)(((byte)(163)))));
             this.grid1.Location = new System.Drawing.Point(0, 0);
             this.grid1.Name = "grid1";
-            this.grid1.RowHeadersVisible = false;
             this.grid1.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
             this.grid1.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
             this.grid1.RowTemplate.Height = 24;
             this.grid1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.grid1.Size = new System.Drawing.Size(784, 437);
+            this.grid1.Size = new System.Drawing.Size(784, 389);
             this.grid1.TabIndex = 1;
             this.grid1.TabStop = false;
             // 
@@ -81,7 +81,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(784, 437);
+            this.panel1.Size = new System.Drawing.Size(784, 389);
             this.panel1.TabIndex = 2;
             // 
             // panel2
@@ -93,11 +93,11 @@
             this.panel2.Size = new System.Drawing.Size(784, 48);
             this.panel2.TabIndex = 0;
             // 
-            // P16_AccumulatedQty
+            // P37_AccumulatedQty
             // 
             this.ClientSize = new System.Drawing.Size(784, 437);
-            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
             this.Name = "P37_AccumulatedQty";
             this.Text = "P37. Accumulated Qty";
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
