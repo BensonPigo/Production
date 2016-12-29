829b452e9d74400b714159906a167325d63de90f
ALGER <alger@sportscity.com.tw>
2016-12-29 06:59:56 +0000

    效能調教，XLT改美一點。

--- Sci.Production.Planning/R15.cs
+++ Sci.Production.Planning/R15.cs
@@ -99,27 +99,23 @@ namespace Sci.Production.Planning
             StringBuilder sqlCmd = new StringBuilder();
 
             sqlCmd.Append(string.Format(@"
-;with cte as
-(
-select o.MDivisionID,o.FactoryID,S.BuyerDelivery,o.SciDelivery,O.CRDDate,O.CFMDate
-,O.ID OrderID,O.Dest,O.StyleID,O.SeasonID,O.ProjectID,O.Customize1,O.BuyMonth
-,O.CustPONo,O.BrandID,O.CustCDID,O.ProgramID,O.CdCodeID,O.CPU,O.Qty,S.Qty [Qty_byShip]
-,O.FOCQty,O.PoPrice,O.CMPPrice,O.KPILETA,O.LETA,O.MTLETA,O.SewETA
-,O.PackETA,O.MTLComplete,O.SewInLine,O.SewOffLine,O.CutInLine,O.CutOffLine
-,O.Category,O.IsForecast,O.PulloutDate,O.ActPulloutDate,O.SMR,O.MRHandle
-,O.MCHandle,O.OrigBuyerDelivery,O.DoxType
-,O.TotalCTN,O.FtyCTN,O.ClogCTN
-,O.VasShas,O.TissuePaper,O.MTLExport,O.SewLine,O.ShipModeList,s.ShipmodeID,O.PlanDate
-,O.FirstProduction,O.Finished,O.FtyGroup,O.OrderTypeID,O.SpecialMark,O.GFR
-,O.SampleReason,O.InspDate,O.MnorderApv,O.FtyKPI,O.KPIChangeReason
-,O.StyleUkey
-,O.POID
- from dbo.Orders o inner join Order_QtyShip s on s.id = o.ID
- WHERE 1=1 
---S.BuyerDelivery between '2015/01/01' and '2015/01/08'
- --AND O.ID = '14050578VV006'
- --order by o.ID
-"));
+                select o.MDivisionID,o.FactoryID,S.BuyerDelivery,o.SciDelivery,O.CRDDate,O.CFMDate
+                ,O.ID OrderID,O.Dest,O.StyleID,O.SeasonID,O.ProjectID,O.Customize1,O.BuyMonth
+                ,O.CustPONo,O.BrandID,O.CustCDID,O.ProgramID,O.CdCodeID,O.CPU,O.Qty,S.Qty [Qty_byShip]
+                ,O.FOCQty,O.PoPrice,O.CMPPrice,O.KPILETA,O.LETA,O.MTLETA,O.SewETA
+                ,O.PackETA,O.MTLComplete,O.SewInLine,O.SewOffLine,O.CutInLine,O.CutOffLine
+                ,O.Category,O.IsForecast,O.PulloutDate,O.ActPulloutDate,O.SMR,O.MRHandle
+                ,O.MCHandle,O.OrigBuyerDelivery,O.DoxType
+                ,O.TotalCTN,O.FtyCTN,O.ClogCTN
+                ,O.VasShas,O.TissuePaper,O.MTLExport,O.SewLine,O.ShipModeList,s.ShipmodeID,O.PlanDate
+                ,O.FirstProduction,O.Finished,O.FtyGroup,O.OrderTypeID,O.SpecialMark,O.GFR
+                ,O.SampleReason,O.InspDate,O.MnorderApv,O.FtyKPI,O.KPIChangeReason
+                ,O.StyleUkey
+                ,O.POID
+                into #cte 
+                from dbo.Orders o inner join Order_QtyShip s on s.id = o.ID
+                WHERE 1=1 
+                "));
 
             #region --- 條件組合  ---
 
@@ -202,112 +198,112 @@ select o.MDivisionID,o.FactoryID,S.BuyerDelivery,o.SciDelivery,O.CRDDate,O.CFMDa
             #region -- 有列印Artwork --
             if (isArtwork)
             {
-                sqlCmd.Append(@")
---依取得的訂單資料取得訂單的 TMS Cost
-,rawdata_tmscost as
-(
-select aa.orderid,bb.ArtworkTypeID,iif(cc.IsTMS=1,bb.tms,bb.price) price_tms 
-from cte aa 
-inner join dbo.Order_TmsCost bb on bb.id = aa.orderid
-inner join dbo.ArtworkType cc on cc.id = bb.ArtworkTypeID
-where IsTMS =1 or IsPrice = 1
-)");
+                sqlCmd.Append(@"
+                --依取得的訂單資料取得訂單的 TMS Cost
+                select aa.orderid,bb.ArtworkTypeID,iif(cc.IsTMS=1,bb.tms,bb.price) price_tms 
+                into #rawdata_tmscost
+                from #cte aa 
+                inner join dbo.Order_TmsCost bb on bb.id = aa.orderid
+                inner join dbo.ArtworkType cc on cc.id = bb.ArtworkTypeID
+                where IsTMS =1 or IsPrice = 1
+                ");
 
                 sqlCmd.Append(string.Format(@"--將取得Tms Cost做成樞紐表
-, tmscost_pvt as 
-(
-    select * from rawdata_tmscost
-    pivot
-    (
-        sum(price_tms)
-        for artworktypeid in ( {0})
-    )as pvt ", artworktypes.ToString().Substring(0, artworktypes.ToString().Length - 1)));
+                select * 
+                into #tmscost_pvt
+                from #rawdata_tmscost
+                pivot
+                (
+                    sum(price_tms)
+                    for artworktypeid in ( {0})
+                )as pvt ", artworktypes.ToString().Substring(0, artworktypes.ToString().Length - 1)));
             }
             #endregion
 
-            sqlCmd.Append(string.Format(@")
- , cte2 as -- 依撈出來的order資料(cte)去找各製程的WIP
- (select 
- t.OrderID
- ,(SELECT SUM(CWIP.Qty) FROM DBO.CuttingOutput_WIP CWIP WHERE CWIP.OrderID = T.OrderID) cut_qty
- ,(SELECT MIN(a.cDate) from dbo.CuttingOutput a 
-	 inner join dbo.CuttingOutput_Detail b on b.id = a.id 
-	 inner join dbo.WorkOrder_Distribute c on c.WorkOrderUkey = b.WorkOrderUkey
-	 where c.OrderID = t.OrderID) first_cut_date
-,(select MIN(isnull(tt.qaqty,0)) from dbo.style_location sl left join 
-	(SELECT b.ComboType,sum(b.QAQty) qaqty FROM DBO.SewingOutput a 
-	inner join dbo.SewingOutput_Detail b on b.ID = a.ID
-	where b.OrderId = t.OrderID
-	group by ComboType ) tt on tt.ComboType = sl.Location
-	where sl.StyleUkey = t.StyleUkey) sewing_output
+            sqlCmd.Append(string.Format(@"
+                -- 依撈出來的order資料(cte)去找各製程的WIP
+                select 
+                 t.OrderID
+                 ,(SELECT SUM(CWIP.Qty) FROM DBO.CuttingOutput_WIP CWIP WHERE CWIP.OrderID = T.OrderID) cut_qty
+                 ,(SELECT MIN(a.cDate) from dbo.CuttingOutput a 
+	                 inner join dbo.CuttingOutput_Detail b on b.id = a.id 
+	                 inner join dbo.WorkOrder_Distribute c on c.WorkOrderUkey = b.WorkOrderUkey
+	                 where c.OrderID = t.OrderID) first_cut_date
+                ,(select MIN(isnull(tt.qaqty,0)) from dbo.style_location sl left join 
+	                (SELECT b.ComboType,sum(b.QAQty) qaqty FROM DBO.SewingOutput a 
+	                inner join dbo.SewingOutput_Detail b on b.ID = a.ID
+	                where b.OrderId = t.OrderID
+	                group by ComboType ) tt on tt.ComboType = sl.Location
+	                where sl.StyleUkey = t.StyleUkey) sewing_output
 
-,t.StyleUkey
-,(select min(qty) qty from (
-select sum(b.Qty) qty ,c.PatternCode,c.ArtworkID 
-from dbo.farmin a inner join dbo.FarmIn_Detail b on b.id = a.id 
-right join (select distinct v.ArtworkTypeID, v.Article,v.ArtworkID,v.PatternCode 
-from dbo.View_Order_Artworks v 
-where v.ID=t.OrderID) c on c.ArtworkTypeID = a.ArtworkTypeId 
-and c.PatternCode = b.PatternCode 
-and c.ArtworkID = b.ArtworkID
-where a.ArtworkTypeId='EMBROIDERY' 
-	and b.Orderid = t.OrderID
-	group by c.PatternCode,c.ArtworkID) x) EMBROIDERY_qty
+                ,t.StyleUkey
+                ,(select min(qty) qty from (
+                select sum(b.Qty) qty ,c.PatternCode,c.ArtworkID 
+                from dbo.farmin a inner join dbo.FarmIn_Detail b on b.id = a.id 
+                right join (select distinct v.ArtworkTypeID, v.Article,v.ArtworkID,v.PatternCode 
+                from dbo.View_Order_Artworks v 
+                where v.ID=t.OrderID) c on c.ArtworkTypeID = a.ArtworkTypeId 
+                and c.PatternCode = b.PatternCode 
+                and c.ArtworkID = b.ArtworkID
+                where a.ArtworkTypeId='EMBROIDERY' 
+	                and b.Orderid = t.OrderID
+	                group by c.PatternCode,c.ArtworkID) x) EMBROIDERY_qty
 
-,(select min(qty) qty from (
-select sum(b.Qty) qty ,c.PatternCode,c.ArtworkID 
-from dbo.farmin a inner join dbo.FarmIn_Detail b on b.id = a.id 
-right join (select distinct v.ArtworkTypeID,v.ArtworkID,v.PatternCode 
-from dbo.View_Order_Artworks v where v.ID=t.OrderID) c 
-on c.ArtworkTypeID = a.ArtworkTypeId 
-and c.PatternCode = b.PatternCode 
-and c.ArtworkID = b.ArtworkID
-where a.ArtworkTypeId='BONDING' 
-	and b.Orderid = t.OrderID
-	group by c.PatternCode,c.ArtworkID) x) BONDING_qty
+                ,(select min(qty) qty from (
+                select sum(b.Qty) qty ,c.PatternCode,c.ArtworkID 
+                from dbo.farmin a inner join dbo.FarmIn_Detail b on b.id = a.id 
+                right join (select distinct v.ArtworkTypeID,v.ArtworkID,v.PatternCode 
+                from dbo.View_Order_Artworks v where v.ID=t.OrderID) c 
+                on c.ArtworkTypeID = a.ArtworkTypeId 
+                and c.PatternCode = b.PatternCode 
+                and c.ArtworkID = b.ArtworkID
+                where a.ArtworkTypeId='BONDING' 
+	                and b.Orderid = t.OrderID
+	                group by c.PatternCode,c.ArtworkID) x) BONDING_qty
 
-,(select min(qty) qty from (
-select sum(b.Qty) qty ,c.PatternCode,c.ArtworkID 
-from dbo.farmin a inner join dbo.FarmIn_Detail b on b.id = a.id 
-right join (select distinct v.ArtworkTypeID,v.ArtworkID,v.PatternCode 
-from dbo.View_Order_Artworks v 
-where v.ID=t.OrderID) c on c.ArtworkTypeID = a.ArtworkTypeId 
-and c.PatternCode = b.PatternCode 
-and c.ArtworkID = b.ArtworkID
-where a.ArtworkTypeId='PRINTING' 
-	and b.Orderid = t.OrderID
-	group by c.PatternCode,c.ArtworkID) x) PRINTING_qty
-,SEWOUTPUT.*
- from cte t
- outer apply (SELECT min(X.OutputDate) firstSewingDate, max(X.OutputDate) lastestSewingDate
- ,sum(X.QAQty) QAQTY ,AVG(X.QAQTY) AVG_QAQTY
- from (SELECT a.OutputDate,sum(a.QAQty) QAQty FROM DBO.SewingOutput a 
-	inner join dbo.SewingOutput_Detail b on b.ID = a.ID
-	where b.OrderId = t.OrderID group by a.OutputDate ) X) SEWOUTPUT
- )
- select t.MDivisionID,t.FactoryID,t.SewLine,t.BuyerDelivery,t.SewInLine,t.SewOffLine
- ,t.BrandID,t.OrderID
- ,t.Dest,t.StyleID,t.OrderTypeID
- ,t.ShipmodeID,'' [OrderNo],t.CustPONo,t.CustCDID,t.ProgramID,t.CdCodeID,t.KPILETA
- ,t.LETA,t.MTLETA,t.SewETA,t.PackETA,t.CPU
- ,t.Qty_byShip,cte2.first_cut_date,cte2.cut_qty,cte2.EMBROIDERY_qty,cte2.BONDING_qty
- ,cte2.PRINTING_qty,cte2.sewing_output,t.qty+t.FOCQty - cte2.sewing_output [Balance]
- ,cte2.firstSewingDate,cte2.AVG_QAQTY
- ,DATEADD(DAY,iif(isnull(cte2.AVG_QAQTY,0) = 0,0,ceiling((t.qty+t.FOCQty - cte2.sewing_output)/(cte2.AVG_QAQTY*1.0))),cte2.firstSewingDate) [Est_offline]
- ,IIF(isnull(t.TotalCTN,0)=0, 0, round(t.ClogCTN / (t.TotalCTN*1.0),4) * 100 ) [pack_rate]
- ,t.TotalCTN, t.FtyCTN, t.ClogCTN, t.InspDate, t.ActPulloutDate,t.FtyKPI,t.KPIChangeReason
- ,t.PlanDate, dbo.getTPEPass1(t.SMR) [SMR], dbo.getTPEPass1(T.MRHandle) [Handle]
- ,(select dbo.getTPEPass1(p.POSMR) from dbo.PO p where p.ID =t.POID) [PO SMR]
- ,(select dbo.getTPEPass1(p.POHandle) from dbo.PO p where p.ID =t.POID) [PO Handle]
- ,(select dbo.getTPEPass1(p.McHandle) from dbo.PO p where p.ID =t.POID) [MC Handle],t.DoxType
- ,(select article+',' from (select distinct q.Article  from dbo.Order_Qty q where q.ID = t.OrderID) t for xml path('')) article_list
- , t.Customize1 [SpecMark], t.GFR, t.SampleReason
- ,(select s.StdTms * t.CPU from System s) [TMS]"));
+                ,(select min(qty) qty from (
+                select sum(b.Qty) qty ,c.PatternCode,c.ArtworkID 
+                from dbo.farmin a inner join dbo.FarmIn_Detail b on b.id = a.id 
+                right join (select distinct v.ArtworkTypeID,v.ArtworkID,v.PatternCode 
+                from dbo.View_Order_Artworks v 
+                where v.ID=t.OrderID) c on c.ArtworkTypeID = a.ArtworkTypeId 
+                and c.PatternCode = b.PatternCode 
+                and c.ArtworkID = b.ArtworkID
+                where a.ArtworkTypeId='PRINTING' 
+	                and b.Orderid = t.OrderID
+	                group by c.PatternCode,c.ArtworkID) x) PRINTING_qty
+                ,SEWOUTPUT.*
+                into #cte2
+                from #cte t
+                outer apply (SELECT min(X.OutputDate) firstSewingDate, max(X.OutputDate) lastestSewingDate
+                ,sum(X.QAQty) QAQTY ,AVG(X.QAQTY) AVG_QAQTY
+                from (SELECT a.OutputDate,sum(a.QAQty) QAQty FROM DBO.SewingOutput a 
+                inner join dbo.SewingOutput_Detail b on b.ID = a.ID
+                where b.OrderId = t.OrderID group by a.OutputDate ) X) SEWOUTPUT
+
+                select t.MDivisionID,t.FactoryID,t.SewLine,t.BuyerDelivery,t.SewInLine,t.SewOffLine
+                ,t.BrandID,t.OrderID
+                ,t.Dest,t.StyleID,t.OrderTypeID
+                ,t.ShipmodeID,'' [OrderNo],t.CustPONo,t.CustCDID,t.ProgramID,t.CdCodeID,t.KPILETA
+                ,t.LETA,t.MTLETA,t.SewETA,t.PackETA,t.CPU
+                ,t.Qty_byShip,#cte2.first_cut_date,#cte2.cut_qty,#cte2.EMBROIDERY_qty,#cte2.BONDING_qty
+                ,#cte2.PRINTING_qty,#cte2.sewing_output,t.qty+t.FOCQty - #cte2.sewing_output [Balance]
+                ,#cte2.firstSewingDate,#cte2.AVG_QAQTY
+                ,DATEADD(DAY,iif(isnull(#cte2.AVG_QAQTY,0) = 0,0,ceiling((t.qty+t.FOCQty - #cte2.sewing_output)/(#cte2.AVG_QAQTY*1.0))),#cte2.firstSewingDate) [Est_offline]
+                ,IIF(isnull(t.TotalCTN,0)=0, 0, round(t.ClogCTN / (t.TotalCTN*1.0),4) * 100 ) [pack_rate]
+                ,t.TotalCTN, t.FtyCTN, t.ClogCTN, t.InspDate, t.ActPulloutDate,t.FtyKPI,t.KPIChangeReason
+                ,t.PlanDate, dbo.getTPEPass1(t.SMR) [SMR], dbo.getTPEPass1(T.MRHandle) [Handle]
+                ,(select dbo.getTPEPass1(p.POSMR) from dbo.PO p where p.ID =t.POID) [PO SMR]
+                ,(select dbo.getTPEPass1(p.POHandle) from dbo.PO p where p.ID =t.POID) [PO Handle]
+                ,(select dbo.getTPEPass1(p.McHandle) from dbo.PO p where p.ID =t.POID) [MC Handle],t.DoxType
+                ,(select article+',' from (select distinct q.Article  from dbo.Order_Qty q where q.ID = t.OrderID) t for xml path('')) article_list
+                , t.Customize1 [SpecMark], t.GFR, t.SampleReason
+                ,(select s.StdTms * t.CPU from System s) [TMS]"));
             if (isArtwork) 
                 sqlCmd.Append(string.Format(@",{0} ",artworktypes.ToString().Substring(0, artworktypes.ToString().Length - 1)));
-            sqlCmd.Append(string.Format(@" from cte t inner join cte2 on cte2.OrderID = t.OrderID"));
+            sqlCmd.Append(string.Format(@" from #cte t inner join #cte2 on #cte2.OrderID = t.OrderID"));
             if (isArtwork) 
-                sqlCmd.Append(string.Format(@" left join tmscost_pvt on tmscost_pvt.orderid = t.orderid "));
+                sqlCmd.Append(string.Format(@" left join #tmscost_pvt on #tmscost_pvt.orderid = t.orderid "));
             sqlCmd.Append(string.Format(@" order by {0}", orderby));
 
 
