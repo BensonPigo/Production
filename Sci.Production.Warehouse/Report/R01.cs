bcdb78daa53cf8a489af7a327659795f4dddf99f
RICKY <ricky@sportscity.com.tw>
2017-01-03 01:18:59 +0000

    Fix "WAREHOUSE_R01. 3rd Country Material Status" Issue 1.修正SP#帶入參數位置。 2.新增檢查< Refno >。

--- Sci.Production.Warehouse/Report/R01.cs
+++ Sci.Production.Warehouse/Report/R01.cs
@@ -35,7 +35,8 @@ namespace Sci.Production.Warehouse
                 MyUtility.Check.Empty(dateRange2.Value1) &&
                 MyUtility.Check.Empty(dateRange4.Value1) &&
                 MyUtility.Check.Empty(dateRange3.Value1) &&
-                (MyUtility.Check.Empty(txtSpno1.Text) || MyUtility.Check.Empty(txtSpno2.Text)))
+                (MyUtility.Check.Empty(txtSpno1.Text) || MyUtility.Check.Empty(txtSpno2.Text)) &&
+                (MyUtility.Check.Empty(txtRefno1.Text) || MyUtility.Check.Empty(txtRefno2.Text)))
             {
                 MyUtility.Msg.WarningBox("< Supp Delivery > & < SCI Delivery > & < ETA > & < Final ETA >& < SP# > & < Refno > can't be empty!!");
                 return false;
@@ -108,14 +109,14 @@ where 1=1"));
                     sqlCmd.Append(string.Format(@" and o.SciDelivery between '{0}' and '{1}'",
                     Convert.ToDateTime(sciDelivery1).ToString("d"), Convert.ToDateTime(sciDelivery2).ToString("d")));
                 }
-                if (!MyUtility.Check.Empty(spno1))
-                {
-                    sqlCmd.Append(" and o.id >= @spno1 and o.id <= @spno2");
-                    sp_spno1.Value = spno1;
-                    sp_spno2.Value = spno2;
-                    cmds.Add(sp_spno1);
-                    cmds.Add(sp_spno2);
-                }
+                //if (!MyUtility.Check.Empty(spno1))
+                //{
+                //    sqlCmd.Append(" and o.id >= @spno1 and o.id <= @spno2");
+                //    sp_spno1.Value = spno1;
+                //    sp_spno2.Value = spno2;
+                //    cmds.Add(sp_spno1);
+                //    cmds.Add(sp_spno2);
+                //}
                 if (!MyUtility.Check.Empty(style))
                 {
                     sqlCmd.Append(" and o.styleid = @style");
@@ -185,6 +186,14 @@ where 1=1 and c.ThirdCountry = 1"));
             }
 
             #region --- 條件組合  ---
+            if (!MyUtility.Check.Empty(spno1))
+            {
+                sqlCmd.Append(" and a.id >= @spno1 and a.id <= @spno2");
+                sp_spno1.Value = spno1;
+                sp_spno2.Value = spno2;
+                cmds.Add(sp_spno1);
+                cmds.Add(sp_spno2);
+            }
             if (!MyUtility.Check.Empty(suppDelivery1))
             {
                 sqlCmd.Append(string.Format(@" and b.finaletd between '{0}' and '{1}'"
