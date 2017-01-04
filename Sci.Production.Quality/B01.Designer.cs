a412dcee3fdcb2dd5535bd535648d4858d81b9e4
WILLY <willy@sportscity.com.tw>
2017-01-04 07:49:40 +0000

    return

--- Sci.Production.Quality/B01.Designer.cs
+++ Sci.Production.Quality/B01.Designer.cs
@@ -35,28 +35,6 @@
             this.textBox3 = new Sci.Win.UI.TextBox();
             this.checkBox1 = new Sci.Win.UI.CheckBox();
             this.editBox1 = new Sci.Win.UI.EditBox();
-            this.textBox2 = new Sci.Win.UI.TextBox();
-            this.txtdropdownlist1 = new Sci.Production.Class.txtdropdownlist();
-            this.textBox4 = new Sci.Win.UI.TextBox();
-            this.textBox5 = new Sci.Win.UI.TextBox();
-            this.textBox6 = new Sci.Win.UI.TextBox();
-            this.textBox7 = new Sci.Win.UI.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.textBox8 = new Sci.Win.UI.TextBox();
-            this.txtdropdownlist2 = new Sci.Production.Class.txtdropdownlist();
-            this.txtdropdownlist3 = new Sci.Production.Class.txtdropdownlist();
-            this.txtdropdownlist4 = new Sci.Production.Class.txtdropdownlist();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
-            this.label12 = new System.Windows.Forms.Label();
-            this.label13 = new System.Windows.Forms.Label();
-            this.label14 = new System.Windows.Forms.Label();
-            this.txtdropdownlist5 = new Sci.Production.Class.txtdropdownlist();
             ((System.ComponentModel.ISupportInitialize)(this.gridbs)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.mtbs)).BeginInit();
             this.detail.SuspendLayout();
@@ -67,32 +45,10 @@
             // 
             // detail
             // 
-            this.detail.Size = new System.Drawing.Size(897, 546);
+            this.detail.Size = new System.Drawing.Size(813, 395);
             // 
             // detailcont
             // 
-            this.detailcont.Controls.Add(this.label14);
-            this.detailcont.Controls.Add(this.txtdropdownlist5);
-            this.detailcont.Controls.Add(this.label13);
-            this.detailcont.Controls.Add(this.label12);
-            this.detailcont.Controls.Add(this.label11);
-            this.detailcont.Controls.Add(this.label10);
-            this.detailcont.Controls.Add(this.txtdropdownlist4);
-            this.detailcont.Controls.Add(this.txtdropdownlist3);
-            this.detailcont.Controls.Add(this.txtdropdownlist2);
-            this.detailcont.Controls.Add(this.label9);
-            this.detailcont.Controls.Add(this.textBox8);
-            this.detailcont.Controls.Add(this.label8);
-            this.detailcont.Controls.Add(this.label7);
-            this.detailcont.Controls.Add(this.label6);
-            this.detailcont.Controls.Add(this.label5);
-            this.detailcont.Controls.Add(this.label4);
-            this.detailcont.Controls.Add(this.textBox7);
-            this.detailcont.Controls.Add(this.textBox6);
-            this.detailcont.Controls.Add(this.textBox5);
-            this.detailcont.Controls.Add(this.textBox4);
-            this.detailcont.Controls.Add(this.txtdropdownlist1);
-            this.detailcont.Controls.Add(this.textBox2);
             this.detailcont.Controls.Add(this.editBox1);
             this.detailcont.Controls.Add(this.checkBox1);
             this.detailcont.Controls.Add(this.textBox3);
@@ -100,15 +56,15 @@
             this.detailcont.Controls.Add(this.label1);
             this.detailcont.Controls.Add(this.label2);
             this.detailcont.Controls.Add(this.label3);
-            this.detailcont.Size = new System.Drawing.Size(897, 508);
+            this.detailcont.Size = new System.Drawing.Size(813, 357);
             // 
             // detailbtm
             // 
-            this.detailbtm.Location = new System.Drawing.Point(0, 508);
+            this.detailbtm.Size = new System.Drawing.Size(813, 38);
             // 
             // tabs
             // 
-            this.tabs.Size = new System.Drawing.Size(905, 575);
+            this.tabs.Size = new System.Drawing.Size(821, 424);
             // 
             // label1
             // 
@@ -180,235 +136,9 @@
             this.editBox1.Size = new System.Drawing.Size(292, 50);
             this.editBox1.TabIndex = 5;
             // 
-            // textBox2
-            // 
-            this.textBox2.BackColor = System.Drawing.Color.White;
-            this.textBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "ID", true));
-            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.textBox2.Location = new System.Drawing.Point(674, 22);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 23);
-            this.textBox2.TabIndex = 7;
-            // 
-            // txtdropdownlist1
-            // 
-            this.txtdropdownlist1.BackColor = System.Drawing.Color.White;
-            this.txtdropdownlist1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.txtdropdownlist1.FormattingEnabled = true;
-            this.txtdropdownlist1.Location = new System.Drawing.Point(352, 416);
-            this.txtdropdownlist1.Name = "txtdropdownlist1";
-            this.txtdropdownlist1.Size = new System.Drawing.Size(121, 24);
-            this.txtdropdownlist1.TabIndex = 8;
-            this.txtdropdownlist1.Type = null;
-            // 
-            // textBox4
-            // 
-            this.textBox4.BackColor = System.Drawing.Color.White;
-            this.textBox4.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "ID", true));
-            this.textBox4.Enabled = false;
-            this.textBox4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.textBox4.Location = new System.Drawing.Point(674, 71);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 23);
-            this.textBox4.TabIndex = 9;
-            // 
-            // textBox5
-            // 
-            this.textBox5.BackColor = System.Drawing.Color.White;
-            this.textBox5.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "ID", true));
-            this.textBox5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.textBox5.IsSupportEditMode = false;
-            this.textBox5.Location = new System.Drawing.Point(673, 132);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 23);
-            this.textBox5.TabIndex = 10;
-            // 
-            // textBox6
-            // 
-            this.textBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
-            this.textBox6.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "ID", true));
-            this.textBox6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
-            this.textBox6.IsSupportEditMode = false;
-            this.textBox6.Location = new System.Drawing.Point(674, 242);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.ReadOnly = true;
-            this.textBox6.Size = new System.Drawing.Size(100, 23);
-            this.textBox6.TabIndex = 11;
-            // 
-            // textBox7
-            // 
-            this.textBox7.BackColor = System.Drawing.Color.White;
-            this.textBox7.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "ID", true));
-            this.textBox7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.textBox7.Location = new System.Drawing.Point(674, 291);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(100, 23);
-            this.textBox7.TabIndex = 12;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(529, 28);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(139, 17);
-            this.label4.TabIndex = 13;
-            this.label4.Text = "V=T, R=F, IS=T, E=T";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(529, 77);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(138, 17);
-            this.label5.TabIndex = 14;
-            this.label5.Text = "V=T, R=F, IS=T, E=F";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(528, 135);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(138, 17);
-            this.label6.TabIndex = 15;
-            this.label6.Text = "V=T, R=F, IS=F, E=T";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(529, 242);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(140, 17);
-            this.label7.TabIndex = 16;
-            this.label7.Text = "V=T, R=T, IS=T, E=T";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(529, 294);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(139, 17);
-            this.label8.TabIndex = 17;
-            this.label8.Text = "V=T, R=F, IS=T, E=T";
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(530, 186);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(139, 17);
-            this.label9.TabIndex = 19;
-            this.label9.Text = "V=T, R=F, IS=T, E=T";
-            // 
-            // textBox8
-            // 
-            this.textBox8.BackColor = System.Drawing.Color.White;
-            this.textBox8.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "ID", true));
-            this.textBox8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.textBox8.Location = new System.Drawing.Point(675, 183);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(100, 23);
-            this.textBox8.TabIndex = 18;
-            // 
-            // txtdropdownlist2
-            // 
-            this.txtdropdownlist2.BackColor = System.Drawing.Color.White;
-            this.txtdropdownlist2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.txtdropdownlist2.FormattingEnabled = true;
-            this.txtdropdownlist2.IsSupportUnselect = true;
-            this.txtdropdownlist2.Location = new System.Drawing.Point(674, 362);
-            this.txtdropdownlist2.Name = "txtdropdownlist2";
-            this.txtdropdownlist2.Size = new System.Drawing.Size(121, 24);
-            this.txtdropdownlist2.TabIndex = 20;
-            this.txtdropdownlist2.Type = null;
-            // 
-            // txtdropdownlist3
-            // 
-            this.txtdropdownlist3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
-            this.txtdropdownlist3.EditMode = Sci.Win.UI.AdvEditModes.DisableOnEdit;
-            this.txtdropdownlist3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
-            this.txtdropdownlist3.FormattingEnabled = true;
-            this.txtdropdownlist3.IsSupportUnselect = true;
-            this.txtdropdownlist3.Location = new System.Drawing.Point(675, 416);
-            this.txtdropdownlist3.Name = "txtdropdownlist3";
-            this.txtdropdownlist3.ReadOnly = true;
-            this.txtdropdownlist3.Size = new System.Drawing.Size(121, 24);
-            this.txtdropdownlist3.TabIndex = 21;
-            this.txtdropdownlist3.Type = null;
-            // 
-            // txtdropdownlist4
-            // 
-            this.txtdropdownlist4.BackColor = System.Drawing.Color.White;
-            this.txtdropdownlist4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.txtdropdownlist4.FormattingEnabled = true;
-            this.txtdropdownlist4.IsSupportUnselect = true;
-            this.txtdropdownlist4.Location = new System.Drawing.Point(352, 362);
-            this.txtdropdownlist4.Name = "txtdropdownlist4";
-            this.txtdropdownlist4.Size = new System.Drawing.Size(121, 24);
-            this.txtdropdownlist4.TabIndex = 22;
-            this.txtdropdownlist4.Type = null;
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(188, 362);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(139, 17);
-            this.label10.TabIndex = 23;
-            this.label10.Text = "V=T, R=F, IS=T, E=T";
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(188, 416);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(138, 17);
-            this.label11.TabIndex = 24;
-            this.label11.Text = "V=T, R=F, IS=F, E=T";
-            // 
-            // label12
-            // 
-            this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(527, 369);
-            this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(139, 17);
-            this.label12.TabIndex = 25;
-            this.label12.Text = "V=T, R=F, IS=T, E=E";
-            // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(527, 423);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(140, 17);
-            this.label13.TabIndex = 26;
-            this.label13.Text = "V=T, R=F, IS=T, E=D";
-            // 
-            // label14
-            // 
-            this.label14.AutoSize = true;
-            this.label14.Location = new System.Drawing.Point(188, 313);
-            this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(141, 17);
-            this.label14.TabIndex = 28;
-            this.label14.Text = "V=T, R=T, IS=T, E=N";
-            // 
-            // txtdropdownlist5
-            // 
-            this.txtdropdownlist5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
-            this.txtdropdownlist5.EditMode = Sci.Win.UI.AdvEditModes.None;
-            this.txtdropdownlist5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
-            this.txtdropdownlist5.FormattingEnabled = true;
-            this.txtdropdownlist5.IsSupportUnselect = true;
-            this.txtdropdownlist5.Location = new System.Drawing.Point(352, 313);
-            this.txtdropdownlist5.Name = "txtdropdownlist5";
-            this.txtdropdownlist5.ReadOnly = true;
-            this.txtdropdownlist5.Size = new System.Drawing.Size(121, 24);
-            this.txtdropdownlist5.TabIndex = 27;
-            this.txtdropdownlist5.Type = null;
-            // 
             // B01
             // 
-            this.ClientSize = new System.Drawing.Size(905, 608);
+            this.ClientSize = new System.Drawing.Size(821, 457);
             this.DefaultControl = "textBox1";
             this.DefaultControlForEdit = "editBox1";
             this.IsDeleteOnBrowse = false;
@@ -441,27 +171,5 @@
         private Win.UI.Label label2;
         private Win.UI.Label label3;
         private Win.UI.EditBox editBox1;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label4;
-        private Win.UI.TextBox textBox7;
-        private Win.UI.TextBox textBox6;
-        private Win.UI.TextBox textBox5;
-        private Win.UI.TextBox textBox4;
-        private Class.txtdropdownlist txtdropdownlist1;
-        private Win.UI.TextBox textBox2;
-        private System.Windows.Forms.Label label9;
-        private Win.UI.TextBox textBox8;
-        private System.Windows.Forms.Label label13;
-        private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.Label label10;
-        private Class.txtdropdownlist txtdropdownlist4;
-        private Class.txtdropdownlist txtdropdownlist3;
-        private Class.txtdropdownlist txtdropdownlist2;
-        private System.Windows.Forms.Label label14;
-        private Class.txtdropdownlist txtdropdownlist5;
     }
 }
