08d171b29d9da720f9bf3fe22724001cc491ad4c
JEFF <jeff@sportscity.com.tw>
2017-01-04 08:51:54 +0000

    摘要	0001949: CUTTING_P01_Bundle Card 	請參考規格於最後一欄新增[Print Date]欄位。 測試確認-已解決

--- Sci.Production.Cutting/P01_BundleCard.cs
+++ Sci.Production.Cutting/P01_BundleCard.cs
@@ -25,7 +25,8 @@ namespace Sci.Production.Cutting
         private void requery()
         {
             string sqlcmd = String.Format(
-           @"Select a.id,b.bundleno,a.orderid,a.cdate,a.cutref,a.PatternPanel,a.cutno,b.sizecode,b.bundlegroup,b.Qty 
+           @"Select a.id,b.bundleno,a.orderid,a.cdate,a.cutref,a.PatternPanel,a.cutno,b.sizecode,b.bundlegroup,b.Qty
+            ,PrintDate
             from Bundle a, Bundle_Detail b 
             Where a.id = b.id and a.POID = '{0}' order by BundleNo", cutid);
             DataTable gridtb;
@@ -43,7 +44,8 @@ namespace Sci.Production.Cutting
                 .Numeric("cutno", header: "Cut No.", width: Widths.AnsiChars(3), iseditingreadonly: true)
                 .Text("sizecode", header: "Size", width: Widths.AnsiChars(8), iseditingreadonly: true)
                 .Numeric("bundlegroup", header: "Bundle Group", width: Widths.AnsiChars(7), iseditingreadonly: true)
-                .Numeric("Qty", header: "Qty", width: Widths.AnsiChars(7), iseditingreadonly: true);
+                .Numeric("Qty", header: "Qty", width: Widths.AnsiChars(7), iseditingreadonly: true)
+                .Text("PrintDate", header: "PrintDate", width: Widths.AnsiChars(8), iseditingreadonly: true);
 
         }
         private void button1_Click(object sender, EventArgs e)
