b7e9313fb67732eb32159d772b39a5834b56e477
JIMMY <jimmy@sportscity.com.tw>
2017-01-09 09:36:28 +0000

    同 Roll 不同 Dyelot 先將 Dyelot 統一後再 SUM

--- Database/dbo/Stored Procedures/usp_SingleItemRecaculate.sql
+++ Database/dbo/Stored Procedures/usp_SingleItemRecaculate.sql
@@ -35,15 +35,69 @@ BEGIN
 				EXEC sp_executesql @SQLCMD , N'@pMDivisionid varchar(8), @pPoid varchar(13) ,@pSeq1 varchar(3), @pSeq2 varchar(2)'
 				,@pMDivisionid = @MDivisionid, @pPoid = @Poid ,@pSeq1 = @Seq1, @pSeq2 = @Seq2;
 
+				---- Update Ftyinventory Bulk 倉
+				--MERGE  DBO.FTYINVENTORY AS T
+				--USING (SELECT b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,dyelot
+				--		, sum(AInqty) AInqty,sum(AOutqty) AOutqty,sum(AAdjustQty) AAdjustQty 
+				--		FROM View_TransactionList b
+				--		where b.MDivisionid = @MDivisionid and b.poid = @Poid and b.seq1 = @Seq1 and b.seq2 = @Seq2 and (AInqty > 0 or AOutqty > 0  or AAdjustQty > 0 ) AND b.status='confirmed'
+				--		group by b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,dyelot
+				--		) AS S (MDivisionid,poid,seq1,seq2,roll,dyelot,AInqty,AOutqty,AAdjustQty)
+				--ON T.MDIVISIONID = S.MDivisionid AND T.POID = S.POID AND T.SEQ1 =  S.SEQ1 AND T.SEQ2 = S.SEQ2 AND T.ROLL = S.ROLL and t.stocktype='B'
+				--WHEN MATCHED THEN UPDATE set inqty = S.AInqty, outqty = s.AOutqty , adjustQty = AAdjustqty
+				--WHEN NOT MATCHED BY TARGET THEN 
+				--INSERT (MDivisionPoDetailUkey,MdivisionID,poid,seq1,seq2,roll,stocktype,dyelot,inqty,outqty,adjustqty)
+				--values (@Ukey,S.MDivisionid,S.POID,S.SEQ1,S.SEQ2,S.ROLL,'B',S.dyelot,S.AInqty,S.AOutqty,S.AAdjustQty)
+				--;
+
+				---- Update Ftyinventory Inventory 倉
+				--MERGE  DBO.FTYINVENTORY AS T
+				--USING (SELECT b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,dyelot
+				--		, sum(BInqty) BInqty,sum(BOutqty) BOutqty,sum(BAdjustQty) BAdjustQty 
+				--		FROM View_TransactionList b
+				--		where b.MDivisionid = @MDivisionid and b.poid = @Poid and b.seq1 = @Seq1 and b.seq2 = @Seq2 and (BInqty > 0 or BOutqty > 0  or BAdjustQty > 0 ) AND b.status='confirmed'
+				--		group by b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,dyelot
+				--		) AS S (MDivisionid,poid,seq1,seq2,roll,dyelot,BInqty,BOutqty,BAdjustQty)
+				--ON T.MDIVISIONID = S.MDivisionid AND T.POID = S.POID AND T.SEQ1 =  S.SEQ1 AND T.SEQ2 = S.SEQ2 AND T.ROLL = S.ROLL and t.stocktype='I'
+				--WHEN MATCHED THEN UPDATE set inqty = S.BInqty, outqty = s.BOutqty , adjustQty = BAdjustQty
+				--WHEN NOT MATCHED BY TARGET THEN 
+				--INSERT (MDivisionPoDetailUkey,MdivisionID,poid,seq1,seq2,roll,stocktype,dyelot,inqty,outqty,adjustqty)
+				--values (@Ukey,S.MDivisionid,S.POID,S.SEQ1,S.SEQ2,S.ROLL,'I',S.dyelot,S.BInqty,S.BOutqty,S.BAdjustQty)
+				--;
+
+				---- Update Ftyinventory Scrap 倉
+				--MERGE  DBO.FTYINVENTORY AS T
+				--USING (SELECT b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,dyelot
+				--		, sum(CInqty) CInqty,sum(COutqty) COutqty,sum(CAdjustQty) CAdjustQty 
+				--		FROM View_TransactionList b
+				--		where b.MDivisionid = @MDivisionid and b.poid = @Poid and b.seq1 = @Seq1 and b.seq2 = @Seq2 and (CInqty > 0 or COutqty > 0  or CAdjustQty > 0 ) AND b.status='confirmed'
+				--		group by b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,dyelot
+				--		) AS S (MDivisionid,poid,seq1,seq2,roll,dyelot,CInqty,COutqty,CAdjustQty)
+				--ON T.MDIVISIONID = S.MDivisionid AND T.POID = S.POID AND T.SEQ1 =  S.SEQ1 AND T.SEQ2 = S.SEQ2 AND T.ROLL = S.ROLL and t.stocktype='O'
+				--WHEN MATCHED THEN UPDATE set inqty = S.CInqty, outqty = s.COutqty , adjustQty = CAdjustQty
+				--WHEN NOT MATCHED BY TARGET THEN 
+				--INSERT (MDivisionPoDetailUkey,MdivisionID,poid,seq1,seq2,roll,stocktype,dyelot,inqty,outqty,adjustqty)
+				--values (@Ukey,S.MDivisionid,S.POID,S.SEQ1,S.SEQ2,S.ROLL,'O',S.dyelot,S.CInqty,S.COutqty,S.CAdjustQty)
+				--;
+
 				-- Update Ftyinventory Bulk 倉
-				MERGE  DBO.FTYINVENTORY AS T
-				USING (SELECT b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,dyelot
-						, sum(AInqty) AInqty,sum(AOutqty) AOutqty,sum(AAdjustQty) AAdjustQty 
+				With group6key as(
+					SELECT b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,
+						sum(AInqty) AInqty,sum(AOutqty) AOutqty,sum(AAdjustQty) AAdjustQty 
 						FROM View_TransactionList b
 						where b.MDivisionid = @MDivisionid and b.poid = @Poid and b.seq1 = @Seq1 and b.seq2 = @Seq2 and (AInqty > 0 or AOutqty > 0  or AAdjustQty > 0 ) AND b.status='confirmed'
-						group by b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,dyelot
-						) AS S (MDivisionid,poid,seq1,seq2,roll,dyelot,AInqty,AOutqty,AAdjustQty)
-				ON T.MDIVISIONID = S.MDivisionid AND T.POID = S.POID AND T.SEQ1 =  S.SEQ1 AND T.SEQ2 = S.SEQ2 AND T.ROLL = S.ROLL and t.stocktype='B'
+						group by b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll
+				), groupDyelot as(
+					SELECT b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,Dyelot
+						FROM View_TransactionList b
+						where b.MDivisionid = @MDivisionid and b.poid = @Poid and b.seq1 = @Seq1 and b.seq2 = @Seq2 and (AInqty > 0 or AOutqty > 0  or AAdjustQty > 0 ) AND b.status='confirmed'
+						group by b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,Dyelot
+				)				
+				MERGE  DBO.FTYINVENTORY AS T
+				USING (SELECT g.*,
+						Dyelot = (select top 1 gd.Dyelot from groupDyelot gd where g.Roll = gd.Roll and g.seq1 = gd.seq1 and g.seq2 = gd.seq2 and g.PoId = gd.PoId and g.MDivisionID = gd.MDivisionID)
+						FROM group6key g) AS S --(MDivisionid,poid,seq1,seq2,roll,dyelot,AInqty,AOutqty,AAdjustQty)
+				ON T.MDIVISIONID = S.MDivisionid AND T.POID = S.POID AND T.SEQ1 =  S.SEQ1 AND T.SEQ2 = S.SEQ2 AND T.ROLL = S.ROLL and T.Dyelot = S.Dyelot and t.stocktype='B'
 				WHEN MATCHED THEN UPDATE set inqty = S.AInqty, outqty = s.AOutqty , adjustQty = AAdjustqty
 				WHEN NOT MATCHED BY TARGET THEN 
 				INSERT (MDivisionPoDetailUkey,MdivisionID,poid,seq1,seq2,roll,stocktype,dyelot,inqty,outqty,adjustqty)
@@ -51,14 +105,23 @@ BEGIN
 				;
 
 				-- Update Ftyinventory Inventory 倉
-				MERGE  DBO.FTYINVENTORY AS T
-				USING (SELECT b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,dyelot
-						, sum(BInqty) BInqty,sum(BOutqty) BOutqty,sum(BAdjustQty) BAdjustQty 
+				With group6key as(
+					SELECT b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,
+						sum(BInqty) BInqty,sum(BOutqty) BOutqty,sum(BAdjustQty) BAdjustQty 
 						FROM View_TransactionList b
 						where b.MDivisionid = @MDivisionid and b.poid = @Poid and b.seq1 = @Seq1 and b.seq2 = @Seq2 and (BInqty > 0 or BOutqty > 0  or BAdjustQty > 0 ) AND b.status='confirmed'
-						group by b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,dyelot
-						) AS S (MDivisionid,poid,seq1,seq2,roll,dyelot,BInqty,BOutqty,BAdjustQty)
-				ON T.MDIVISIONID = S.MDivisionid AND T.POID = S.POID AND T.SEQ1 =  S.SEQ1 AND T.SEQ2 = S.SEQ2 AND T.ROLL = S.ROLL and t.stocktype='I'
+						group by b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll
+				), groupDyelot as(
+					SELECT b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,Dyelot
+						FROM View_TransactionList b
+						where b.MDivisionid = @MDivisionid and b.poid = @Poid and b.seq1 = @Seq1 and b.seq2 = @Seq2 and (BInqty > 0 or BOutqty > 0  or BAdjustQty > 0 ) AND b.status='confirmed'
+						group by b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,Dyelot
+				)
+				MERGE  DBO.FTYINVENTORY AS T
+				USING (SELECT g.*,
+						Dyelot = (select top 1 gd.Dyelot from groupDyelot gd where g.Roll = gd.Roll and g.seq1 = gd.seq1 and g.seq2 = gd.seq2 and g.PoId = gd.PoId and g.MDivisionID = gd.MDivisionID)
+						FROM group6key g) AS S --(MDivisionid,poid,seq1,seq2,roll,dyelot,BInqty,BOutqty,BAdjustQty)
+				ON T.MDIVISIONID = S.MDivisionid AND T.POID = S.POID AND T.SEQ1 =  S.SEQ1 AND T.SEQ2 = S.SEQ2 AND T.ROLL = S.ROLL and T.Dyelot = S.Dyelot and t.stocktype='I'
 				WHEN MATCHED THEN UPDATE set inqty = S.BInqty, outqty = s.BOutqty , adjustQty = BAdjustQty
 				WHEN NOT MATCHED BY TARGET THEN 
 				INSERT (MDivisionPoDetailUkey,MdivisionID,poid,seq1,seq2,roll,stocktype,dyelot,inqty,outqty,adjustqty)
@@ -66,14 +129,23 @@ BEGIN
 				;
 
 				-- Update Ftyinventory Scrap 倉
-				MERGE  DBO.FTYINVENTORY AS T
-				USING (SELECT b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,dyelot
-						, sum(CInqty) CInqty,sum(COutqty) COutqty,sum(CAdjustQty) CAdjustQty 
+				With group6key as(
+					SELECT b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,
+						sum(CInqty) CInqty,sum(COutqty) COutqty,sum(CAdjustQty) CAdjustQty 
 						FROM View_TransactionList b
 						where b.MDivisionid = @MDivisionid and b.poid = @Poid and b.seq1 = @Seq1 and b.seq2 = @Seq2 and (CInqty > 0 or COutqty > 0  or CAdjustQty > 0 ) AND b.status='confirmed'
-						group by b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,dyelot
-						) AS S (MDivisionid,poid,seq1,seq2,roll,dyelot,CInqty,COutqty,CAdjustQty)
-				ON T.MDIVISIONID = S.MDivisionid AND T.POID = S.POID AND T.SEQ1 =  S.SEQ1 AND T.SEQ2 = S.SEQ2 AND T.ROLL = S.ROLL and t.stocktype='O'
+						group by b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll
+				), groupDyelot as(
+					SELECT b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,Dyelot
+						FROM View_TransactionList b
+						where b.MDivisionid = @MDivisionid and b.poid = @Poid and b.seq1 = @Seq1 and b.seq2 = @Seq2 and (CInqty > 0 or COutqty > 0  or CAdjustQty > 0 ) AND b.status='confirmed'
+						group by b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,Dyelot
+				)
+				MERGE  DBO.FTYINVENTORY AS T
+				USING (SELECT g.*,
+						Dyelot = (select top 1 gd.Dyelot from groupDyelot gd where g.Roll = gd.Roll and g.seq1 = gd.seq1 and g.seq2 = gd.seq2 and g.PoId = gd.PoId and g.MDivisionID = gd.MDivisionID)
+						FROM group6key g) AS S --(MDivisionid,poid,seq1,seq2,roll,dyelot,CInqty,COutqty,CAdjustQty)
+				ON T.MDIVISIONID = S.MDivisionid AND T.POID = S.POID AND T.SEQ1 =  S.SEQ1 AND T.SEQ2 = S.SEQ2 AND T.ROLL = S.ROLL and T.Dyelot = S.Dyelot and t.stocktype='O'
 				WHEN MATCHED THEN UPDATE set inqty = S.CInqty, outqty = s.COutqty , adjustQty = CAdjustQty
 				WHEN NOT MATCHED BY TARGET THEN 
 				INSERT (MDivisionPoDetailUkey,MdivisionID,poid,seq1,seq2,roll,stocktype,dyelot,inqty,outqty,adjustqty)
