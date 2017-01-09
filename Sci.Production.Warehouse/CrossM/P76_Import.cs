2514a83f1e7eefafa3c75392be0b940fb99948c6
JIMMY <jimmy@sportscity.com.tw>
2017-01-09 05:47:09 +0000

    fix Dyelot 規則

--- Sci.Production.Warehouse/CrossM/P76_Import.cs
+++ Sci.Production.Warehouse/CrossM/P76_Import.cs
@@ -110,11 +110,11 @@ namespace Sci.Production.Warehouse
 select
 	0 as selected,'' id,d.ToMDivisionID MDivisionID,d.ToPOID poid,d.ToSeq1 seq1,d.ToSeq2 seq2, left(d.toseq1+'   ',3)+d.toseq2 as seq
     , d2.Roll roll
-    , (	SELECT iif(isnull(b.Dyelot, '') = '', d2.Dyelot, b.Dyelot)
-		FROM View_TransactionList b
-		where b.MDivisionid = d.ToMDivisionID and b.poid = d.ToPOID and b.seq1 = d.ToSeq1 and b.seq2 = d.ToSeq2 and b.Roll = d2.Roll
-		group by b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,dyelot
-	) dyelot
+    , isnull((SELECT iif(b.Dyelot = '', null, b.Dyelot)
+		    FROM View_TransactionList b
+		    where b.MDivisionid = d.ToMDivisionID and b.poid = d.ToPOID and b.seq1 = d.ToSeq1 and b.seq2 = d.ToSeq2 and b.Roll = d2.Roll
+		    group by b.MDivisionid,b.poid,b.seq1,b.seq2,b.roll,dyelot)
+	    , d2.Dyelot) dyelot
     ,d2.Qty,'B' stocktype 
     , dbo.getMtlDesc(d.ToPOID,d.ToSeq1,d.ToSeq2,2,0) [description]
     , (select stockunit from dbo.po_supp_detail where id = d.topoid and seq1 = d.toseq1 and seq2 = d.toseq2) stockunit
