7ea9287e06e9551d783f22ae7e5efa49516bf3e2
JIMMY <jimmy@sportscity.com.tw>
2017-01-13 01:12:23 +0000

    add showErrMsg

--- Sci.Production.Class/txtSeq.cs
+++ Sci.Production.Class/txtSeq.cs
@@ -38,9 +38,9 @@ namespace Sci.Production.Class
             return seq1 + " " + seq2;
         }
 
-        public bool checkEmpty()
+        public bool checkEmpty(bool showErrMsg = true)
         {
-            if((MyUtility.Check.Empty(seq1) | MyUtility.Check.Empty(seq2)))
+            if ((MyUtility.Check.Empty(seq1) | MyUtility.Check.Empty(seq2)) & showErrMsg)
                 MyUtility.Msg.WarningBox("Seq' mask need enter 00-00");
 
             return MyUtility.Check.Empty(seq1) | MyUtility.Check.Empty(seq2);
