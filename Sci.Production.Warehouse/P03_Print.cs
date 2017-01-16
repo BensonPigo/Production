56d989f3415bc8cfc508aa150ae5b230585f3bce
RICKY <ricky@sportscity.com.tw>
2017-01-16 01:40:07 +0000

    P03 print btn visable=false

--- Sci.Production.Warehouse/P03_Print.cs
+++ Sci.Production.Warehouse/P03_Print.cs
@@ -31,7 +31,7 @@ namespace Sci.Production.Warehouse
             InitializeComponent();
 
             this.CurrentDataRow = row;
-            print.Enabled = false;
+            print.Visible = false;
             
             // TODO: Complete member initialization
         }
