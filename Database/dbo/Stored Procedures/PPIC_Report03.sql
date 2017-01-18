dda5ec7a4ea794cbe0326827fb57ef5acad719be
ELENA <elena@sportscity.com.tw>
2017-01-18 09:26:25 +0000

    add

---
+++ Database/dbo/Stored Procedures/PPIC_Report03.sql
@@ -0,0 +1,15 @@
+﻿
+
+CREATE PROCEDURE [dbo].[PPIC_Report03]
+	@OrderID varchar(13)
+	,@ByType int = 0 --0單張 , 1 By CustCDID , 2 By PO
+AS
+BEGIN
+
+
+exec Order_Report_QtyBreakdown @OrderID,@ByType
+exec Order_Report_FabColorCombination @OrderID,@ByType
+exec Order_Report_AccColorCombination @OrderID,@ByType
+
+
+END
