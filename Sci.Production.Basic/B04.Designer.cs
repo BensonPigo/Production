a9180e3bab9f1bec50f4d5f4109b92abf18612a4
ALGER <alger@sportscity.com.tw>
2017-01-23 08:31:31 +0000

    比照舊系統將取消的欄位變色。

--- Sci.Production.Basic/B04.Designer.cs
+++ Sci.Production.Basic/B04.Designer.cs
@@ -373,6 +373,7 @@
             // 
             this.ClientSize = new System.Drawing.Size(775, 457);
             this.DefaultOrder = "ID";
+            this.EnableGridJunkColor = true;
             this.IsSupportCopy = false;
             this.IsSupportDelete = false;
             this.IsSupportPrint = false;
