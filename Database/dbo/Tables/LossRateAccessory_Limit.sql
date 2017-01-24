2a795089c70fc4e4e752f9a95879eae9a3823a6d
JEFF <jeff@sportscity.com.tw>
2017-01-24 03:10:09 +0000

    update

---
+++ Database/dbo/Tables/LossRateAccessory_Limit.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[LossRateAccessory_Limit] (
+    [MtltypeId] VARCHAR (20)   NOT NULL,
+    [UsageUnit] VARCHAR (8)    NOT NULL,
+    [LimitUp]   NUMERIC (7, 2) NULL,
+    [AddName]   VARCHAR (10)   NULL,
+    [AddDate]   DATETIME       NULL,
+    [EditName]  VARCHAR (10)   NULL,
+    [EditDate]  DATETIME       NULL,
+    CONSTRAINT [PK_LossRateAccessory_Limit] PRIMARY KEY CLUSTERED ([MtltypeId] ASC, [UsageUnit] ASC)
+);
+
