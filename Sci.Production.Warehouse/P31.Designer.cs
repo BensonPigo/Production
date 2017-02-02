e3df7d6a91d0d3b736df9e4a6d384d4be3492871
JIMMY <jimmy@sportscity.com.tw>
2017-02-02 09:35:07 +0000

    定位順序 & Default Control

--- Sci.Production.Warehouse/P31.Designer.cs
+++ Sci.Production.Warehouse/P31.Designer.cs
@@ -81,7 +81,7 @@
             this.masterpanel.Controls.Add(this.label11);
             this.masterpanel.Controls.Add(this.label3);
             this.masterpanel.Controls.Add(this.shapeContainer1);
-            this.masterpanel.Size = new System.Drawing.Size(892, 184);
+            this.masterpanel.Size = new System.Drawing.Size(1058, 184);
             this.masterpanel.Controls.SetChildIndex(this.shapeContainer1, 0);
             this.masterpanel.Controls.SetChildIndex(this.label3, 0);
             this.masterpanel.Controls.SetChildIndex(this.label11, 0);
@@ -105,7 +105,7 @@
             // detailpanel
             // 
             this.detailpanel.Location = new System.Drawing.Point(0, 184);
-            this.detailpanel.Size = new System.Drawing.Size(892, 165);
+            this.detailpanel.Size = new System.Drawing.Size(1058, 293);
             // 
             // gridicon
             // 
@@ -120,7 +120,7 @@
             // 
             // detailgridcont
             // 
-            this.detailgridcont.Size = new System.Drawing.Size(892, 165);
+            this.detailgridcont.Size = new System.Drawing.Size(1058, 293);
             // 
             // detail2
             // 
@@ -136,16 +136,16 @@
             // 
             // detail
             // 
-            this.detail.Size = new System.Drawing.Size(892, 387);
+            this.detail.Size = new System.Drawing.Size(1058, 515);
             // 
             // detailcont
             // 
-            this.detailcont.Size = new System.Drawing.Size(892, 349);
+            this.detailcont.Size = new System.Drawing.Size(1058, 477);
             // 
             // detailbtm
             // 
-            this.detailbtm.Location = new System.Drawing.Point(0, 349);
-            this.detailbtm.Size = new System.Drawing.Size(892, 38);
+            this.detailbtm.Location = new System.Drawing.Point(0, 477);
+            this.detailbtm.Size = new System.Drawing.Size(1058, 38);
             // 
             // browse
             // 
@@ -201,7 +201,7 @@
             this.dateBox3.Location = new System.Drawing.Point(402, 13);
             this.dateBox3.Name = "dateBox3";
             this.dateBox3.Size = new System.Drawing.Size(130, 23);
-            this.dateBox3.TabIndex = 1;
+            this.dateBox3.TabIndex = 0;
             // 
             // button5
             // 
@@ -210,7 +210,7 @@
             this.button5.Location = new System.Drawing.Point(939, 43);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(89, 31);
-            this.button5.TabIndex = 4;
+            this.button5.TabIndex = 3;
             this.button5.Text = "Import";
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
@@ -222,7 +222,7 @@
             this.shapeContainer1.Name = "shapeContainer1";
             this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
             this.lineShape1});
-            this.shapeContainer1.Size = new System.Drawing.Size(892, 184);
+            this.shapeContainer1.Size = new System.Drawing.Size(1058, 184);
             this.shapeContainer1.TabIndex = 54;
             this.shapeContainer1.TabStop = false;
             // 
@@ -293,7 +293,7 @@
             this.editBox1.Multiline = true;
             this.editBox1.Name = "editBox1";
             this.editBox1.Size = new System.Drawing.Size(658, 51);
-            this.editBox1.TabIndex = 3;
+            this.editBox1.TabIndex = 2;
             // 
             // button1
             // 
@@ -302,7 +302,7 @@
             this.button1.Location = new System.Drawing.Point(883, 80);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(145, 31);
-            this.button1.TabIndex = 63;
+            this.button1.TabIndex = 4;
             this.button1.Text = "Accumulated Qty";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -313,7 +313,7 @@
             this.dateBox1.Location = new System.Drawing.Point(129, 43);
             this.dateBox1.Name = "dateBox1";
             this.dateBox1.Size = new System.Drawing.Size(130, 23);
-            this.dateBox1.TabIndex = 67;
+            this.dateBox1.TabIndex = 1;
             // 
             // label2
             // 
@@ -347,8 +347,8 @@
             // 
             this.ApvChkValue = "New";
             this.ClientSize = new System.Drawing.Size(1066, 577);
-            this.DefaultControl = "textBox3";
-            this.DefaultControlForEdit = "textBox3";
+            this.DefaultControl = "dateBox1";
+            this.DefaultControlForEdit = "dateBox1";
             this.DefaultDetailOrder = "FromMdivisionid,Frompoid,Fromseq1,Fromseq2,Fromroll,FromDyelot";
             this.DefaultOrder = "issuedate,ID";
             this.Grid2New = 0;
