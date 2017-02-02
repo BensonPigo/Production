2dbfae8aba6d8329f8d49fdb68a0774fd401b735
JIMMY <jimmy@sportscity.com.tw>
2017-02-02 09:45:46 +0000

    Default Control

--- Sci.Production.Warehouse/P34.Designer.cs
+++ Sci.Production.Warehouse/P34.Designer.cs
@@ -297,8 +297,8 @@
             // 
             this.ApvChkValue = "New";
             this.ClientSize = new System.Drawing.Size(1066, 577);
-            this.DefaultControl = "textBox3";
-            this.DefaultControlForEdit = "textBox3";
+            this.DefaultControl = "editBox1";
+            this.DefaultControlForEdit = "editBox1";
             this.DefaultOrder = "Issuedate,ID";
             this.Grid2New = 0;
             this.GridAlias = "Adjust_detail";
