f98e7db85716eec9a49ea458eb9f4f4d0efb8bc2
JIMMY <jimmy@sportscity.com.tw>
2017-02-02 09:26:55 +0000

    Default Control

--- Sci.Production.Warehouse/P22.Designer.cs
+++ Sci.Production.Warehouse/P22.Designer.cs
@@ -73,7 +73,7 @@
             this.masterpanel.Controls.Add(this.label11);
             this.masterpanel.Controls.Add(this.label3);
             this.masterpanel.Controls.Add(this.shapeContainer1);
-            this.masterpanel.Size = new System.Drawing.Size(892, 184);
+            this.masterpanel.Size = new System.Drawing.Size(1058, 184);
             this.masterpanel.Controls.SetChildIndex(this.shapeContainer1, 0);
             this.masterpanel.Controls.SetChildIndex(this.label3, 0);
             this.masterpanel.Controls.SetChildIndex(this.label11, 0);
@@ -93,7 +93,7 @@
             // detailpanel
             // 
             this.detailpanel.Location = new System.Drawing.Point(0, 184);
-            this.detailpanel.Size = new System.Drawing.Size(892, 165);
+            this.detailpanel.Size = new System.Drawing.Size(1058, 317);
             // 
             // gridicon
             // 
@@ -108,7 +108,7 @@
             // 
             // detailgridcont
             // 
-            this.detailgridcont.Size = new System.Drawing.Size(892, 165);
+            this.detailgridcont.Size = new System.Drawing.Size(1058, 317);
             // 
             // detail2
             // 
@@ -124,16 +124,16 @@
             // 
             // detail
             // 
-            this.detail.Size = new System.Drawing.Size(892, 387);
+            this.detail.Size = new System.Drawing.Size(1058, 539);
             // 
             // detailcont
             // 
-            this.detailcont.Size = new System.Drawing.Size(892, 349);
+            this.detailcont.Size = new System.Drawing.Size(1058, 501);
             // 
             // detailbtm
             // 
-            this.detailbtm.Location = new System.Drawing.Point(0, 349);
-            this.detailbtm.Size = new System.Drawing.Size(892, 38);
+            this.detailbtm.Location = new System.Drawing.Point(0, 501);
+            this.detailbtm.Size = new System.Drawing.Size(1058, 38);
             // 
             // browse
             // 
@@ -210,7 +210,7 @@
             this.shapeContainer1.Name = "shapeContainer1";
             this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
             this.lineShape1});
-            this.shapeContainer1.Size = new System.Drawing.Size(892, 184);
+            this.shapeContainer1.Size = new System.Drawing.Size(1058, 184);
             this.shapeContainer1.TabIndex = 54;
             this.shapeContainer1.TabStop = false;
             // 
@@ -299,8 +299,8 @@
             // 
             this.ApvChkValue = "New";
             this.ClientSize = new System.Drawing.Size(1066, 601);
-            this.DefaultControl = "textBox3";
-            this.DefaultControlForEdit = "textBox3";
+            this.DefaultControl = "editBox1";
+            this.DefaultControlForEdit = "editBox1";
             this.DefaultDetailOrder = "FromPoid,FromSeq1,FromSeq2,FromDyelot,FromRoll";
             this.DefaultOrder = "issuedate,ID";
             this.Grid2New = 0;
