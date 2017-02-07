1b02e9b2a85101a438a42b0aa70deef9d3287374
WENNY <wenny@sportscity.com.tw>
2017-02-07 05:34:38 +0000

    ADD

---
+++ Sci.Production.Class/VFPColors.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Sci.Production.Class
+{
+    /// <summary>
+    ///  refer colorsName  http://www.flounder.com/csharp_color_table.htm
+    ///  all colors http://www.ifreesite.com/color/
+    /// </summary>
+    public struct VFPColor
+    {
+        /// <summary>
+        /// Purple 紫色
+        /// </summary>
+        public static Color Purple_172_172_255 = Color.FromArgb(172, 172, 255);
+
+        /// <summary>
+        /// White
+        /// </summary>
+        public static Color White_255_255_255 = Color.FromArgb(255, 255, 255);
+
+        /// <summary>
+        /// Gray ( color of junk )
+        /// </summary>
+        public static Color Gray_224_224_224 = Color.FromArgb(224, 224, 224);
+
+        /// <summary>
+        /// Green
+        /// </summary>
+        public static Color Green_0_255_128 = Color.FromArgb(0, 255, 128);
+
+        /// <summary>
+        /// Green : for report title
+        /// </summary>
+        public static Color Green_204_255_204 = Color.FromArgb(204, 255, 204);
+
+        /// <summary>
+        /// little yellow 
+        /// </summary>
+        public static Color Yellow_255_255_128 = Color.FromArgb(255, 255, 128);
+        /// <summary>
+        /// little yellow -2
+        /// </summary>
+        public static Color Yellow_255_255_50 = Color.FromArgb(255, 255, 50);
+
+        /// <summary>
+        /// Red
+        /// </summary>
+        public static Color Red_255_0_0 = Color.FromArgb(255, 0, 0);
+        /// <summary>
+        /// Red
+        /// </summary>
+        public static Color Red_255_128_128 = Color.FromArgb(255, 128, 128);
+
+        /// <summary>
+        /// Pink
+        /// </summary>
+        public static Color Pink_255_0_128 = Color.FromArgb(255, 0, 128);
+
+        /// <summary>
+        /// Pink
+        /// </summary>
+        public static Color Pink_255_0_255 = Color.FromArgb(255, 0, 255);
+
+        /// <summary>
+        /// Blue
+        /// </summary>
+        public static Color Blue_0_0_255 = Color.FromArgb(0,0,255);
+        /// <summary>
+        /// Default readonly backColor
+        /// </summary>
+        public static Color Blue_183_227_255 = Color.FromArgb(183, 227, 255);
+
+        /// <summary>
+        /// Default Grid Header backColor - mark as Editable
+        /// </summary>
+        public static Color Blue_30_90_230 = Color.FromArgb(30, 90, 230);
+        /// <summary>
+        /// Blue
+        /// </summary>
+        public static Color Blue_128_255_255 = Color.FromArgb(128, 255, 255);
+        /// <summary>
+        /// Black
+        /// </summary>
+        public static Color Black_0_0_0 = Color.FromArgb(0, 0, 0);
+
+        /// <summary>
+        /// Yellow 淡黃色，PO3用
+        /// </summary>
+        public static Color Yellow_254_254_120 = Color.FromArgb(254, 254, 120);
+
+        /// <summary>
+        /// Pink 粉紅，PO3第三國Supp用
+        /// </summary>
+        public static Color Pink_255_128_129 = Color.FromArgb(255, 128, 129);
+
+        /// <summary>
+        /// Pink 淡粉紅，PO3 判斷PINO,PIDate是否有key
+        /// </summary>
+        public static Color Pink_255_228_225 = Color.FromArgb(255, 228, 225);
+    }
+}
