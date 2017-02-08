7c4e3a070f5c79f5d281d28554af97c2bd0e888f
WENNY <wenny@sportscity.com.tw>
2017-02-08 08:44:58 +0000

    ADD

---
+++ Sci.Production.PPIC/P01_MNoticePrint.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Sci.Production.PPIC
+{
+    partial class P01_MNoticePrint
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new Sci.Win.UI.GroupBox();
+            this.chkAdditional = new Sci.Win.UI.CheckBox();
+            this.radioButton_ByCustCD = new Sci.Win.UI.RadioButton();
+            this.radioButton_MNotice = new Sci.Win.UI.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Visible = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkAdditional);
+            this.groupBox1.Controls.Add(this.radioButton_ByCustCD);
+            this.groupBox1.Controls.Add(this.radioButton_MNotice);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(495, 154);
+            this.groupBox1.TabIndex = 96;
+            this.groupBox1.TabStop = false;
+            // 
+            // chkAdditional
+            // 
+            this.chkAdditional.AutoSize = true;
+            this.chkAdditional.Checked = true;
+            this.chkAdditional.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkAdditional.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.chkAdditional.Location = new System.Drawing.Point(18, 107);
+            this.chkAdditional.Name = "chkAdditional";
+            this.chkAdditional.Size = new System.Drawing.Size(321, 21);
+            this.chkAdditional.TabIndex = 8;
+            this.chkAdditional.Text = "Additionally print [Size Spec] with \"z\" beginning";
+            this.chkAdditional.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_ByCustCD
+            // 
+            this.radioButton_ByCustCD.AutoSize = true;
+            this.radioButton_ByCustCD.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.radioButton_ByCustCD.Location = new System.Drawing.Point(18, 63);
+            this.radioButton_ByCustCD.Name = "radioButton_ByCustCD";
+            this.radioButton_ByCustCD.Size = new System.Drawing.Size(213, 21);
+            this.radioButton_ByCustCD.TabIndex = 7;
+            this.radioButton_ByCustCD.TabStop = true;
+            this.radioButton_ByCustCD.Text = "M/Notice (Combo by CustCD )";
+            this.radioButton_ByCustCD.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_MNotice
+            // 
+            this.radioButton_MNotice.AutoSize = true;
+            this.radioButton_MNotice.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.radioButton_MNotice.Location = new System.Drawing.Point(18, 22);
+            this.radioButton_MNotice.Name = "radioButton_MNotice";
+            this.radioButton_MNotice.Size = new System.Drawing.Size(81, 21);
+            this.radioButton_MNotice.TabIndex = 6;
+            this.radioButton_MNotice.TabStop = true;
+            this.radioButton_MNotice.Text = "M/Notice";
+            this.radioButton_MNotice.UseVisualStyleBackColor = true;
+            // 
+            // P01_MNoticePrint
+            // 
+            this.ClientSize = new System.Drawing.Size(627, 200);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "P01_MNoticePrint";
+            this.Text = "P01.M/Notice Print";
+            this.Controls.SetChildIndex(this.groupBox1, 0);
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.GroupBox groupBox1;
+        private Win.UI.CheckBox chkAdditional;
+        private Win.UI.RadioButton radioButton_ByCustCD;
+        private Win.UI.RadioButton radioButton_MNotice;
+
+
+    }
+}
---
+++ Sci.Production.PPIC/P01_MNoticePrint.cs
@@ -0,0 +1,287 @@
+﻿using Ict;
+using Microsoft.Office.Interop.Excel;
+using Sci.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using sxrc = Sci.Utility.Excel.SaveXltReportCls;
+
+namespace Sci.Production.PPIC
+{
+    public partial class P01_MNoticePrint : Sci.Win.Tems.PrintForm
+    {
+        private string _id;
+        //private string _username;
+        //private string _userid;
+        //DualResult result;
+        //DataRow CurrentDataRow ;
+
+        public P01_MNoticePrint(ToolStripMenuItem menuitem, string args)
+            : base(menuitem)
+        {
+            Constructor(args);
+        }
+        public P01_MNoticePrint(string args)
+        {
+            Constructor(args);
+        }
+        private void Constructor(string args)
+        {
+            this._id = args;
+            InitializeComponent();
+            EditMode = true;
+
+            foreach (var control in groupBox1.Controls)
+            {
+                if (control is Sci.Win.UI.RadioButton)
+                {
+                    Sci.Win.UI.RadioButton rdb = (Sci.Win.UI.RadioButton)control;
+                    rdb.CheckedChanged += rd_CheckedChanged;
+                }
+            }
+        }
+
+        protected override DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            return new DualResult(true);
+        }
+
+
+        protected override bool ToExcel()
+        {
+           if (radioButton_MNotice.Checked == true)
+            {
+              
+              string poid =   MyUtility.GetValue.Lookup("select POID FROM dbo.Orders where ID = @ID", new List<SqlParameter> { new SqlParameter("@ID", _id) });
+
+                DataRow drvar = GetTitleDataByCustCD(poid, _id);
+
+                if (drvar == null) return true;
+
+                string xltPath = Sci.Env.Cfg.XltPathDir + "PPIC_P01_M_Notice.xltx";
+                sxrc sxr = new sxrc(xltPath);
+                sxr.dicDatas.Add(sxr._v + "PO_MAKER", drvar["MAKER"].ToString());
+                sxr.dicDatas.Add(sxr._v + "PO_STYLENO", drvar["sty"].ToString());
+                sxr.dicDatas.Add(sxr._v + "PO_QTY", drvar["QTY"]);
+                sxr.dicDatas.Add(sxr._v + "POID", poid);
+
+                System.Data.DataTable[] dts;
+                DualResult res = DBProxy.Current.SelectSP("", "PPIC_Report_SizeSpec", new List<SqlParameter> { new SqlParameter("@POID", poid), new SqlParameter("@WithZ", false), new SqlParameter("@fullsize", 1) }, out dts);
+
+                sxrc.xltRptTable xltTbl = new sxrc.xltRptTable(dts[0], 1, 0, false, 18, 2);
+
+                for (int i = 3; i <= 18; i++)
+                {
+                    sxrc.xlsColumnInfo xcinfo = new sxrc.xlsColumnInfo(i, false, 0, XlHAlign.xlHAlignLeft);
+                    xcinfo.NumberFormate = "@";
+                    xltTbl.lisColumnInfo.Add(xcinfo);
+                }
+
+                sxr.dicDatas.Add(sxr._v + "S1_Tbl1", xltTbl);
+                intSizeSpecRowCnt = dts[0].Rows.Count + 1 + 2; //起始位置加一、格線加二
+                sxrc.ReplaceAction ra = ForSizeSpec;
+                sxr.dicDatas.Add(sxr._v + "ExtraAction", ra);
+
+                System.Data.DataTable dt;
+                DualResult getIds = DBProxy.Current.Select("", "select ID, FactoryID as MAKER, StyleID+'-'+SeasonID as sty, QTY from MNorder where poid = @poid", new List<SqlParameter> { new SqlParameter("poid", poid) }, out dt);
+                if (!getIds && dt.Rows.Count <= 0)
+                {
+                    MyUtility.Msg.ErrorBox(getIds.ToString(), "error");
+                    return true;
+                }
+
+                sxr.CopySheet.Add(1, dt.Rows.Count - 1);
+                sxr.VarToSheetName = sxr._v + "SP";
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    string ID = dt.Rows[i]["ID"].ToString();
+                    string idxStr = (i == 0) ? "" : i.ToString();
+
+                    res = DBProxy.Current.SelectSP("", "PPIC_Report02", new List<SqlParameter> { new SqlParameter("@ID", ID), new SqlParameter("@WithZ", false) }, out dts);
+
+                    sxr.dicDatas.Add(sxr._v + "Now" + idxStr, DateTime.Now);
+                    sxr.dicDatas.Add(sxr._v + "SP" + idxStr, ID);
+                    sxr.dicDatas.Add(sxr._v + "MAKER" + idxStr, dt.Rows[i]["MAKER"].ToString());
+                    sxr.dicDatas.Add(sxr._v + "STYLENO" + idxStr, dt.Rows[i]["sty"].ToString());
+                    sxr.dicDatas.Add(sxr._v + "QTY" + idxStr, dt.Rows[i]["QTY"].ToString());
+
+                    sxrc.xltRptTable tbl1 = new sxrc.xltRptTable(dts[0], 1, 2, true);
+                    sxrc.xltRptTable tbl2 = new sxrc.xltRptTable(dts[1], 1, 3);
+                    sxrc.xltRptTable tbl3 = new sxrc.xltRptTable(dts[2], 1, 0);
+                    SetColumn1toText(tbl1);
+                    SetColumn1toText(tbl2);
+                    SetColumn1toText(tbl3);
+
+                    sxr.dicDatas.Add(sxr._v + "S2_Tbl1" + idxStr, tbl1);
+                    sxr.dicDatas.Add(sxr._v + "S2_Tbl2" + idxStr, tbl2);
+                    sxr.dicDatas.Add(sxr._v + "S2_Tbl3" + idxStr, tbl3);
+                    sxr.dicDatas.Add(sxr._v + "S2_Tbl4" + idxStr, dts[3]); //COLOR list
+                    sxr.dicDatas.Add(sxr._v + "S2_Tbl5" + idxStr, dts[4]); //Fabric list
+                    sxr.dicDatas.Add(sxr._v + "S2_Tbl6" + idxStr, dts[5]); //Accessories list
+                    sxr.dicDatas.Add(sxr._v + "S2SHIPINGMARK" + idxStr, new sxrc.xltImageString(dts[6].Rows[0]["shipingMark"].ToString()));
+                    sxr.dicDatas.Add(sxr._v + "S2PACKING" + idxStr, new sxrc.xltImageString(dts[7].Rows[0]["Packing"].ToString()));
+                    sxr.dicDatas.Add(sxr._v + "S2LH" + idxStr, new sxrc.xltImageString(dts[8].Rows[0]["Label"].ToString()));
+
+                }
+
+                sxr.isProtect = true; //Excel 加密
+
+                sxr.boOpenFile = true;
+                sxr.Save();
+            }
+                      
+            //M/Notict (Combo by CustCD)
+            else
+            {
+                string poid = MyUtility.GetValue.Lookup("select POID FROM dbo.Orders where ID = @ID", new List<SqlParameter> { new SqlParameter("@ID", _id) });
+
+                DataRow drvar = GetTitleDataByCustCD(poid, _id);
+
+                System.Data.DataTable[] dts;
+                DualResult res = DBProxy.Current.SelectSP("", "PPIC_Report04", new List<SqlParameter> { new SqlParameter("@ID", _id), new SqlParameter("@WithZ", chkAdditional.Checked) }, out dts);
+
+                if (drvar == null | !res) return false;
+
+                string xltPath =Sci.Env.Cfg.XltPathDir + "\\PPIC_P01_M_Notice_Combo.xlt";
+                sxrc sxr = new sxrc(xltPath);
+                sxr.dicDatas.Add(sxr._v + "Now", DateTime.Now);
+                sxr.dicDatas.Add(sxr._v + "MAKER", drvar["MAKER"].ToString());
+                sxr.dicDatas.Add(sxr._v + "STYLENO", drvar["sty"].ToString());
+                sxr.dicDatas.Add(sxr._v + "QTY", drvar["QTY"].ToString());
+                sxr.dicDatas.Add(sxr._v + "SP", drvar["SPNO"].ToString());
+                //sxr.dicDatas.Add(sxr._v + "S1_Tbl1", new sxrc.xltRptTable(dts[0], 1, 0, false, 18, 2));
+                //For SizeSpec
+                sxrc.xltRptTable xltTbl = new sxrc.xltRptTable(dts[0], 1, 0, false, 18, 2);
+                for (int i = 3; i <= 18; i++)
+                {
+                    sxrc.xlsColumnInfo xcinfo = new sxrc.xlsColumnInfo(i, false, 0, XlHAlign.xlHAlignLeft);
+                    xcinfo.NumberFormate = "@";
+                    xltTbl.lisColumnInfo.Add(xcinfo);
+                }
+
+                sxr.dicDatas.Add(sxr._v + "S1_Tbl1", xltTbl);
+                intSizeSpecRowCnt = dts[0].Rows.Count + 1 + 2; //起始位置加一、格線加二
+                sxrc.ReplaceAction ra = ForSizeSpec;
+                sxr.dicDatas.Add(sxr._v + "ExtraAction", ra);
+
+                sxrc.xltRptTable tbl1 = new sxrc.xltRptTable(dts[1], 1, 2, true);
+                sxrc.xltRptTable tbl2 = new sxrc.xltRptTable(dts[2], 1, 3);
+                sxrc.xltRptTable tbl3 = new sxrc.xltRptTable(dts[3], 1, 0);
+                SetColumn1toText(tbl1);
+                SetColumn1toText(tbl2);
+                SetColumn1toText(tbl3);
+
+                sxr.dicDatas.Add(sxr._v + "S2_Tbl1", tbl1);
+                sxr.dicDatas.Add(sxr._v + "S2_Tbl2", tbl2);
+                sxr.dicDatas.Add(sxr._v + "S2_Tbl3", tbl3); //COLOR list                
+                sxr.dicDatas.Add(sxr._v + "S2_Tbl4", dts[4]); //Fabric list                
+                sxr.dicDatas.Add(sxr._v + "S2_Tbl5", dts[5]); //Accessories list                
+                sxr.dicDatas.Add(sxr._v + "S2_Tbl6", dts[6]);
+                sxr.dicDatas.Add(sxr._v + "S2PACKING", new sxrc.xltImageString(dts[7].Rows[0]["Packing"].ToString()));
+                sxr.dicDatas.Add(sxr._v + "S2LH", new sxrc.xltImageString(dts[8].Rows[0]["Label"].ToString()));
+
+                //新增Range Repeat數
+                sxr.dicDatas.Add(sxr._v + "CR", dts[9].Rows.Count);
+
+                int idx = 0;
+                foreach (DataRow dr in dts[9].Rows)
+                {
+                    string sIdx = idx.ToString();
+                    if (idx == 0) sIdx = "";
+                    idx += 1;
+
+                    sxr.dicDatas.Add(sxr._v + "S3_SP" + sIdx, dr["ID"].ToString());
+                    sxr.dicDatas.Add(sxr._v + "S3_Style" + sIdx, dr["sty"].ToString());
+                    sxr.dicDatas.Add(sxr._v + "S3_QTY" + sIdx, dr["QTY"]);
+                    sxr.dicDatas.Add(sxr._v + "S3_CUSTCD" + sIdx, dr["CustCDID"].ToString());
+                    sxr.dicDatas.Add(sxr._v + "S3_PoNo" + sIdx, dr["CustPONO"].ToString());
+                    sxr.dicDatas.Add(sxr._v + "S3_Order" + sIdx, dr["Customize1"].ToString());
+                    sxr.dicDatas.Add(sxr._v + "S3_DELIVERY" + sIdx, dr["BuyerDelivery"]);
+                    sxr.dicDatas.Add(sxr._v + "S3_Mark" + sIdx, new sxrc.xltImageString(dr["Mark"].ToString()));
+
+                    System.Data.DataTable[] dts2;
+                    List<SqlParameter> lis2 = new List<SqlParameter>();
+                    lis2.Add(new SqlParameter("@OrderID", dr["ID"]));
+                    lis2.Add(new SqlParameter("@ByType", "0"));
+
+                    res = DBProxy.Current.SelectSP("", "Order_Report_QtyBreakdown", lis2, out dts2);
+
+                    if (res)
+                    {
+                        sxrc.xltRptTable stbl = new sxrc.xltRptTable(dts2[0], 1, 2, true);
+                        SetColumn1toText(stbl);
+                        sxr.dicDatas.Add(sxr._v + "S3_Tbl" + sIdx, stbl);
+                    }
+                }
+
+                sxr.isProtect = true; //Excel 加密
+                sxr.boOpenFile = true;
+                sxr.Save();
+            }
+            return base.ToExcel();
+        }
+
+      
+        void SetColumn1toText(sxrc.xltRptTable tbl)
+        {
+            sxrc.xlsColumnInfo c1 = new sxrc.xlsColumnInfo(1);
+            c1.NumberFormate = "@";
+            tbl.lisColumnInfo.Add(c1);
+        }
+
+        private int intSizeSpecRowCnt = 0;
+        private int intSizeSpecColumnCnt = 18;
+        void ForSizeSpec(Worksheet oSheet, int rowNo, int columnNo)
+        {
+            for (int colIdx = 3; colIdx <= intSizeSpecColumnCnt; colIdx++)
+            {
+                //oSheet.Cells[4, colIdx].Interior.Color = System.Drawing.ColorTranslator.ToOle(Color.Red);    
+                oSheet.Cells[4, colIdx].HorizontalAlignment = XlHAlign.xlHAlignLeft;
+            }
+            for (int colIdx = 3; colIdx <= intSizeSpecColumnCnt; colIdx++)
+            {
+                //oSheet.Cells[4 + intSizeSpecRowCnt, colIdx].Interior.Color = System.Drawing.ColorTranslator.ToOle(Color.Red);
+                oSheet.Cells[4, colIdx].HorizontalAlignment = XlHAlign.xlHAlignLeft;
+            }
+        }
+
+        private DataRow GetTitleDataByCustCD(string poid, string id, bool ByCustCD = true)
+        {
+            DataRow drvar;
+            string cmd = "";
+            if (ByCustCD)
+            {
+                cmd = @"
+
+SELECT MAKER=max(FactoryID),sty=max(StyleID)+'-'+max(SeasonID),QTY=sum(QTY),'SPNO'=RTRIM(POID)+b.spno FROM MNOrder a
+OUTER APPLY(SELECT STUFF((SELECT '/'+REPLACE(ID,@poid,'') FROM MNOrder WHERE POID = @poid AND CustCDID = (select CustCDID from MNOrder where ID = @ID) 
+	order by ID FOR XML PATH(''), TYPE ).value('.', 'NVARCHAR(MAX)'),1,1,'') as spno) b
+where POID = @poid and CustCDID = (select CustCDID from MNOrder where ID = @ID) group by POID,b.spno";
+            }
+            else
+            {
+                cmd = @"SELECT MAKER=max(FactoryID),sty=max(StyleID)+'-'+max(SeasonID),QTY=sum(QTY),'SPNO'=RTRIM(POID)+b.spno FROM MNOrder a
+OUTER APPLY(SELECT STUFF((SELECT '/'+REPLACE(ID,@poid,'') FROM MNOrder WHERE POID = @poid
+	order by ID FOR XML PATH(''), TYPE ).value('.', 'NVARCHAR(MAX)'),1,1,'') as spno) b
+where POID = @poid group by POID,b.spno";
+            }
+
+            bool res = MyUtility.Check.Seek(cmd, new List<SqlParameter> { new SqlParameter("@poid", poid), new SqlParameter("@ID", id) }, out drvar, null);
+            if (res)
+                return drvar;
+            else
+                return null;
+        }
+
+        void rd_CheckedChanged(object sender, EventArgs e)
+        {
+            chkAdditional.Visible = false;
+            chkAdditional.Visible = radioButton_MNotice.Checked || radioButton_ByCustCD.Checked; 
+        }
+    }
+}
