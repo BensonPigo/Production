c84dbf25ffae620f49fdf536da647d5cc054ea43
JIMMY <jimmy@sportscity.com.tw>
2017-02-09 03:08:39 +0000

    fix Excel 欄位的值可能為 null

--- Sci.Production.Warehouse/Report/R13.cs
+++ Sci.Production.Warehouse/Report/R13.cs
@@ -116,8 +116,13 @@ Where a.Status = 'Confirmed' and a.issuedate between '{0}' and '{1}' and a.type 
             Excel.Worksheet objSheets = objApp.ActiveWorkbook.Worksheets[1];   // 取得工作表
 
             this.ShowWaitMessage("Excel Processing...");
-            for (int i = 1; i <= printData.Rows.Count; i++) objSheets.Cells[i + 1, 10] = ((string)((Excel.Range)objSheets.Cells[i + 1, 10]).Value).Trim();
-            objApp.Visible = true;
+            for (int i = 1; i <= printData.Rows.Count; i++)
+            {
+                string str = objSheets.Cells[i + 1, 10].Value;
+                if(!MyUtility.Check.Empty(str))
+                    objSheets.Cells[i + 1, 10] = str.Trim();
+            }
+                objApp.Visible = true;
 
             if (objSheets != null) Marshal.FinalReleaseComObject(objSheets);    //釋放sheet
             if (objApp != null) Marshal.FinalReleaseComObject(objApp);          //釋放objApp
