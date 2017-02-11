0642d4a7681861f2ea8e901b1a4b324a615b85ef
WILLY <willy@sportscity.com.tw>
2017-02-11 07:25:15 +0000

    UPDATE

--- Database/dbo/Tables/Order_BOF.sql
+++ Database/dbo/Tables/Order_BOF.sql
@@ -1,25 +1,27 @@
 ﻿CREATE TABLE [dbo].[Order_BOF] (
-    [Id]                 VARCHAR (13)   CONSTRAINT [DF_Order_BOF_Id] DEFAULT ('') NOT NULL,
-    [FabricCode]         VARCHAR (3)    CONSTRAINT [DF_Order_BOF_FabricCode] DEFAULT ('') NOT NULL,
-    [Refno]              VARCHAR (20)   CONSTRAINT [DF_Order_BOF_Refno] DEFAULT ('') NOT NULL,
-    [SCIRefno]           VARCHAR (26)   CONSTRAINT [DF_Order_BOF_SCIRefno] DEFAULT ('') NOT NULL,
-    [SuppID]             VARCHAR (6)    CONSTRAINT [DF_Order_BOF_SuppID] DEFAULT ('') NOT NULL,
-    [ConsPC]             NUMERIC (8, 4) CONSTRAINT [DF_Order_BOF_ConsPC] DEFAULT ((0)) NULL,
-    [Seq1]               VARCHAR (3)    CONSTRAINT [DF_Order_BOF_Seq1] DEFAULT ('') NOT NULL,
-    [Kind]               VARCHAR (1)    CONSTRAINT [DF_Order_BOF_Kind] DEFAULT ('') NULL,
-    [Ukey]               BIGINT         CONSTRAINT [DF_Order_BOF_Ukey] DEFAULT ((0)) NOT NULL,
-    [Remark]             NVARCHAR (MAX) CONSTRAINT [DF_Order_BOF_Remark] DEFAULT ('') NULL,
-    [LossType]           NUMERIC (1)    CONSTRAINT [DF_Order_BOF_LossType] DEFAULT ((0)) NOT NULL,
-    [LossPercent]        NUMERIC (3, 1) CONSTRAINT [DF_Order_BOF_LossPercent] DEFAULT ((0)) NULL,
-    [RainwearTestPassed] BIT            CONSTRAINT [DF_Order_BOF_RainwearTestPassed] DEFAULT ((0)) NULL,
-    [HorizontalCutting]  BIT            CONSTRAINT [DF_Order_BOF_HorizontalCutting] DEFAULT ((0)) NULL,
-    [ColorDetail]        NVARCHAR (100) CONSTRAINT [DF_Order_BOF_ColorDetail] DEFAULT ('') NULL,
-    [AddName]            VARCHAR (10)   CONSTRAINT [DF_Order_BOF_AddName] DEFAULT ('') NULL,
-    [AddDate]            DATETIME       NULL,
-    [EditName]           VARCHAR (10)   CONSTRAINT [DF_Order_BOF_EditName] DEFAULT ('') NULL,
-    [EditDate]           DATETIME       NULL,
+    [Id]                 VARCHAR (13)    CONSTRAINT [DF_Order_BOF_Id] DEFAULT ('') NOT NULL,
+    [FabricCode]         VARCHAR (3)     CONSTRAINT [DF_Order_BOF_FabricCode] DEFAULT ('') NOT NULL,
+    [Refno]              VARCHAR (20)    CONSTRAINT [DF_Order_BOF_Refno] DEFAULT ('') NOT NULL,
+    [SCIRefno]           VARCHAR (26)    CONSTRAINT [DF_Order_BOF_SCIRefno] DEFAULT ('') NOT NULL,
+    [SuppID]             VARCHAR (6)     CONSTRAINT [DF_Order_BOF_SuppID] DEFAULT ('') NOT NULL,
+    [ConsPC]             NUMERIC (11, 4) CONSTRAINT [DF_Order_BOF_ConsPC] DEFAULT ((0)) NULL,
+    [Seq1]               VARCHAR (3)     CONSTRAINT [DF_Order_BOF_Seq1] DEFAULT ('') NOT NULL,
+    [Kind]               VARCHAR (1)     CONSTRAINT [DF_Order_BOF_Kind] DEFAULT ('') NULL,
+    [Ukey]               BIGINT          CONSTRAINT [DF_Order_BOF_Ukey] DEFAULT ((0)) NOT NULL,
+    [Remark]             NVARCHAR (MAX)  CONSTRAINT [DF_Order_BOF_Remark] DEFAULT ('') NULL,
+    [LossType]           NUMERIC (1)     CONSTRAINT [DF_Order_BOF_LossType] DEFAULT ((0)) NOT NULL,
+    [LossPercent]        NUMERIC (5, 1)  CONSTRAINT [DF_Order_BOF_LossPercent] DEFAULT ((0)) NULL,
+    [RainwearTestPassed] BIT             CONSTRAINT [DF_Order_BOF_RainwearTestPassed] DEFAULT ((0)) NULL,
+    [HorizontalCutting]  BIT             CONSTRAINT [DF_Order_BOF_HorizontalCutting] DEFAULT ((0)) NULL,
+    [ColorDetail]        NVARCHAR (100)  CONSTRAINT [DF_Order_BOF_ColorDetail] DEFAULT ('') NULL,
+    [AddName]            VARCHAR (10)    CONSTRAINT [DF_Order_BOF_AddName] DEFAULT ('') NULL,
+    [AddDate]            DATETIME        NULL,
+    [EditName]           VARCHAR (10)    CONSTRAINT [DF_Order_BOF_EditName] DEFAULT ('') NULL,
+    [EditDate]           DATETIME        NULL,
     CONSTRAINT [PK_Order_BOF] PRIMARY KEY CLUSTERED ([Ukey] ASC)
 );
+
+
 
 
 
