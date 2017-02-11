57a5add89875765dd627d206334b20a87ef393f5
ALGER <alger@sportscity.com.tw>
2017-02-11 12:09:13 +0000

    新增VIEW[GetName]。

---
+++ Database/dbo/Views/GetName.sql
@@ -0,0 +1,12 @@
+﻿
+/*
+If Object_Id ( 'GetName', 'V' ) Is Not Null
+   Drop View GetName;
+*/
+Create View [dbo].[GetName] As
+Select ID
+     , Name as NameOnly
+	 , (RTrim(Name) + ' #' + ExtNo) as NameAndExt
+	 , (RTrim(ID) + '-' + RTrim(Name) + ' #' + ExtNo) as IdAndNameAndExt
+	 , (RTrim(ID) + '-' + RTrim(Name)) as IdAndName
+  From Dbo.Pass1
