6c6ab24a9962b61200b9095689c7ecb0e7b62fea
ALGER <alger@sportscity.com.tw>
2017-02-13 09:25:48 +0000

    store procedure 調整。 1.SQL調整。 2.DESCRIPTION改為SizeDesc。

--- Database/dbo/Stored Procedures/PPIC_Report_SizeSpec.sql
+++ Database/dbo/Stored Procedures/PPIC_Report_SizeSpec.sql
@@ -6,8 +6,6 @@ CREATE PROCEDURE [dbo].[PPIC_Report_SizeSpec]
 	,@fullsize bit = 0
 AS
 BEGIN
-		
-	--select SizeCode,Seq into #tmp_Col from dbo.GetSizeCodeColumnByID(@poid,1)
 	
 	SELECT distinct a.SizeCode,Seq into #tmp_Col FROM MNOrder_SizeCode a inner join MNOrder_Qty b on a.Id = b.ID and (@fullsize = 1 or a.SizeCode = b.SizeCode)
 	where a.Id = @POID
@@ -15,10 +13,12 @@ BEGIN
 	if exists(select 1 from #tmp_Col)
 		begin
 			declare @str1 nvarchar(max), @Unit nvarchar(10)
-			select @str1=STUFF((SELECT ',['+SizeCode+']' FROM #tmp_Col order by Seq FOR XML PATH(''), TYPE ).value('.', 'NVARCHAR(MAX)'),1,1,'')
+			select @str1=STUFF((
+				SELECT ',['+SizeCode+']' FROM #tmp_Col order by Seq FOR XML PATH('')
+				),1,1,'')
 			select top 1 @Unit = SizeUnit from MNOrder_SizeItem where Id = @POID
-			declare @sql nvarchar(max) = 'select SizeItem,[Description          Unit : '+ @Unit +']=Description,'+ @str1 +' from (
-				select a.SizeItem,a.Description,b.SizeCode,b.SizeSpec from MNOrder_SizeItem a
+			declare @sql nvarchar(max) = 'select SizeItem,[SizeDesc          Unit : '+ @Unit +']=SizeDesc,'+ @str1 +' from (
+				select a.SizeItem,a.SizeDesc,b.SizeCode,b.SizeSpec from MNOrder_SizeItem a
 				left join MNOrder_SizeSpec b on a.Id = b.Id and a.SizeItem = b.SizeItem
 				where a.Id = '''+ @poid +''' AND ('+ cast(@WithZ as varchar(1)) +' = ''1'' or A.SizeItem like ''S%'')
 			) a pivot (max(SizeSpec) for SizeCode in ('+ @str1 +')) b
