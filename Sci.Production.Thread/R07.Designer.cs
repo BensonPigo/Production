8658ee0acad1d6db2465bcf8b5c57ed0a9b04746
JIMMY <jimmy@sportscity.com.tw>
2017-02-14 12:22:37 +0000

    add R07

---
+++ Sci.Production.Thread/R07.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace Sci.Production.Thread
+{
+    partial class R07
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new Sci.Win.UI.Panel();
+            this.label8 = new Sci.Win.UI.Label();
+            this.label9 = new Sci.Win.UI.Label();
+            this.textLOC2 = new Sci.Win.UI.TextBox();
+            this.textLOC1 = new Sci.Win.UI.TextBox();
+            this.textITEM = new Sci.Win.UI.TextBox();
+            this.textTYPE = new Sci.Win.UI.TextBox();
+            this.textSHA = new Sci.Win.UI.TextBox();
+            this.radioPanel1 = new Sci.Win.UI.RadioPanel();
+            this.radioButton2 = new Sci.Win.UI.RadioButton();
+            this.radioButton1 = new Sci.Win.UI.RadioButton();
+            this.textBox2 = new Sci.Win.UI.TextBox();
+            this.textBox1 = new Sci.Win.UI.TextBox();
+            this.dateRange1 = new Sci.Win.UI.DateRange();
+            this.label7 = new Sci.Win.UI.Label();
+            this.label6 = new Sci.Win.UI.Label();
+            this.label5 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.label3 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.label1 = new Sci.Win.UI.Label();
+            this.panel1.SuspendLayout();
+            this.radioPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(433, 12);
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(433, 48);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(433, 84);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.textLOC2);
+            this.panel1.Controls.Add(this.textLOC1);
+            this.panel1.Controls.Add(this.textITEM);
+            this.panel1.Controls.Add(this.textTYPE);
+            this.panel1.Controls.Add(this.textSHA);
+            this.panel1.Controls.Add(this.radioPanel1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.dateRange1);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 14);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(409, 275);
+            this.panel1.TabIndex = 94;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.SystemColors.Control;
+            this.label8.Lines = 0;
+            this.label8.Location = new System.Drawing.Point(219, 53);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(20, 30);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "~";
+            this.label8.TextStyle.Color = System.Drawing.Color.Black;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.SystemColors.Control;
+            this.label9.Lines = 0;
+            this.label9.Location = new System.Drawing.Point(236, 200);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(20, 23);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "~";
+            this.label9.TextStyle.Color = System.Drawing.Color.Black;
+            // 
+            // textLOC2
+            // 
+            this.textLOC2.BackColor = System.Drawing.Color.White;
+            this.textLOC2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textLOC2.Location = new System.Drawing.Point(256, 193);
+            this.textLOC2.Name = "textLOC2";
+            this.textLOC2.Size = new System.Drawing.Size(113, 23);
+            this.textLOC2.TabIndex = 22;
+            this.textLOC2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textLOC2_MouseDown);
+            // 
+            // textLOC1
+            // 
+            this.textLOC1.BackColor = System.Drawing.Color.White;
+            this.textLOC1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textLOC1.Location = new System.Drawing.Point(117, 193);
+            this.textLOC1.Name = "textLOC1";
+            this.textLOC1.Size = new System.Drawing.Size(113, 23);
+            this.textLOC1.TabIndex = 21;
+            this.textLOC1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textLOC1_MouseDown);
+            // 
+            // textITEM
+            // 
+            this.textITEM.BackColor = System.Drawing.Color.White;
+            this.textITEM.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textITEM.Location = new System.Drawing.Point(116, 159);
+            this.textITEM.Name = "textITEM";
+            this.textITEM.Size = new System.Drawing.Size(127, 23);
+            this.textITEM.TabIndex = 20;
+            this.textITEM.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textITEM_MouseDown);
+            // 
+            // textTYPE
+            // 
+            this.textTYPE.BackColor = System.Drawing.Color.White;
+            this.textTYPE.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textTYPE.Location = new System.Drawing.Point(117, 123);
+            this.textTYPE.Name = "textTYPE";
+            this.textTYPE.Size = new System.Drawing.Size(135, 23);
+            this.textTYPE.TabIndex = 19;
+            this.textTYPE.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textTYPE_MouseDown);
+            // 
+            // textSHA
+            // 
+            this.textSHA.BackColor = System.Drawing.Color.White;
+            this.textSHA.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textSHA.Location = new System.Drawing.Point(117, 86);
+            this.textSHA.Name = "textSHA";
+            this.textSHA.Size = new System.Drawing.Size(122, 23);
+            this.textSHA.TabIndex = 18;
+            this.textSHA.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textSHA_MouseDown);
+            // 
+            // radioPanel1
+            // 
+            this.radioPanel1.Controls.Add(this.radioButton2);
+            this.radioPanel1.Controls.Add(this.radioButton1);
+            this.radioPanel1.Location = new System.Drawing.Point(116, 227);
+            this.radioPanel1.Name = "radioPanel1";
+            this.radioPanel1.Size = new System.Drawing.Size(175, 26);
+            this.radioPanel1.TabIndex = 15;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.radioButton2.Location = new System.Drawing.Point(78, 3);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Summary";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.radioButton1.Location = new System.Drawing.Point(10, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(62, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Detail";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox2.Location = new System.Drawing.Point(240, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox2_MouseDown);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(116, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseDown);
+            // 
+            // dateRange1
+            // 
+            this.dateRange1.Location = new System.Drawing.Point(116, 17);
+            this.dateRange1.Name = "dateRange1";
+            this.dateRange1.Size = new System.Drawing.Size(280, 23);
+            this.dateRange1.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.Lines = 0;
+            this.label7.Location = new System.Drawing.Point(19, 230);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(90, 23);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Report Type";
+            // 
+            // label6
+            // 
+            this.label6.Lines = 0;
+            this.label6.Location = new System.Drawing.Point(19, 193);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 23);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Location";
+            // 
+            // label5
+            // 
+            this.label5.Lines = 0;
+            this.label5.Location = new System.Drawing.Point(19, 159);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 23);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Thread Item";
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(19, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Type";
+            // 
+            // label3
+            // 
+            this.label3.Lines = 0;
+            this.label3.Location = new System.Drawing.Point(19, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Shade";
+            // 
+            // label2
+            // 
+            this.label2.Lines = 0;
+            this.label2.Location = new System.Drawing.Point(19, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "RefNo";
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(19, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Date";
+            // 
+            // R07
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 317);
+            this.Controls.Add(this.panel1);
+            this.Name = "R07";
+            this.Text = "R07. Thread In-coming List";
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.radioPanel1.ResumeLayout(false);
+            this.radioPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.Panel panel1;
+        private Win.UI.RadioPanel radioPanel1;
+        private Win.UI.RadioButton radioButton2;
+        private Win.UI.RadioButton radioButton1;
+        private Win.UI.TextBox textBox2;
+        private Win.UI.TextBox textBox1;
+        private Win.UI.DateRange dateRange1;
+        private Win.UI.Label label7;
+        private Win.UI.Label label6;
+        private Win.UI.Label label5;
+        private Win.UI.Label label4;
+        private Win.UI.Label label3;
+        private Win.UI.Label label2;
+        private Win.UI.Label label1;
+        private Win.UI.TextBox textSHA;
+        private Win.UI.TextBox textTYPE;
+        private Win.UI.TextBox textITEM;
+        private Win.UI.TextBox textLOC1;
+        private Win.UI.TextBox textLOC2;
+        private Win.UI.Label label9;
+        private Win.UI.Label label8;
+    }
+}
---
+++ Sci.Production.Thread/R07.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Excel = Microsoft.Office.Interop.Excel;
+using Ict;
+using Sci.Data;
+using System.Runtime.InteropServices;
+
+namespace Sci.Production.Thread
+{
+    public partial class R07 : Sci.Win.Tems.PrintForm
+    {
+        string sql;
+        DataTable printData;
+        List<SqlParameter> sqlPar;
+        
+        public R07(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+        }
+
+        protected override bool ValidateInput()
+        {
+            sqlPar = new List<SqlParameter>();
+            printData = null;
+            sql = @"SELECT 
+                      Date = ti.AddDate,
+                      IncomingNo = ti.ID,
+                      RefNo = tid.Refno,
+                      Description = li.Description,
+                      Type = li.Category,
+                      item = li.ThreadTypeID,
+                      shade = tid.ThreadColorid,
+                      ColorDesc = tc.Description,
+                      NofPcs = tid.PcsUsed,
+                      TtlUsed = tid.TotalWeight,
+                      WofOneCone = li.Weight,
+                      AxleWeight = li.AxleWeight,
+                      NewCone = tid.NewCone,
+                      UsedCone = tid.UsedCone,
+                      Location = tid.ThreadLocationid
+                    FROM Threadincoming ti
+                    inner join ThreadIncoming_Detail tid on ti.ID = tid.ID
+                    left join LocalItem li on tid.Refno = li.RefNo
+                    left join ThreadColor tc on tid.ThreadColorid = tc.id";
+
+            List<string> sqlWhere = new List<string>();
+
+            if (MyUtility.Check.Empty(dateRange1.Value1.ToString()) && MyUtility.Check.Empty(dateRange1.Value2.ToString()))
+            {
+                MyUtility.Msg.ErrorBox("Date can not be empty!!");
+                return false;
+            }
+            else
+            {
+                sqlWhere.Add("(ti.AddDate between @date1 and @date2)");
+                sqlPar.Add(new SqlParameter("@date1", dateRange1.Text1.ToString()));
+                sqlPar.Add(new SqlParameter("@date2", dateRange1.Text2.ToString()));
+            }
+
+            if (!MyUtility.Check.Empty(textBox1.Text.ToString()) && !MyUtility.Check.Empty(textBox2.Text.ToString()))
+            {
+                sqlWhere.Add("(tid.Refno between @refno1 and @refno2)");
+                sqlPar.Add(new SqlParameter("@refno1", textBox1.Text.ToString()));
+                sqlPar.Add(new SqlParameter("@refno2", textBox2.Text.ToString()));
+            }
+
+            if (!MyUtility.Check.Empty(textSHA.Text.ToString()))
+            {
+                sqlWhere.Add("li.ThreadTypeID = @shade");
+                sqlPar.Add(new SqlParameter("@shade", textSHA.Text.ToString()));
+            }
+
+            if (!MyUtility.Check.Empty(textTYPE.Text.ToString()))
+            {
+                sqlWhere.Add("li.Category = @type");
+                sqlPar.Add(new SqlParameter("@type", textTYPE.Text.ToString()));
+            }
+
+            if (!MyUtility.Check.Empty(textLOC1.Text.ToString()) && !MyUtility.Check.Empty(textLOC2.Text.ToString()))
+            {
+                sqlWhere.Add("(tid.ThreadLocationid between @loc1 and @loc2)");
+                sqlPar.Add(new SqlParameter("@loc1", textLOC1.Text.ToString()));
+                sqlPar.Add(new SqlParameter("@loc2", textLOC2.Text.ToString()));
+            }
+
+            if (sqlWhere.Count > 0)
+                sql += " where " + sqlWhere.JoinToString(" and ");
+
+            return base.ValidateInput();
+        }
+
+        protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            DualResult result = DBProxy.Current.Select(null, sql, sqlPar, out printData);
+            return result;
+        }
+
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            // 顯示筆數於PrintForm上Count欄位
+            SetCount(printData.Rows.Count);
+
+            if (printData.Rows.Count <= 0)
+            {
+                MyUtility.Msg.WarningBox("Data not found!");
+                return false;
+            }
+
+            Excel.Application objApp = MyUtility.Excel.ConnectExcel(Sci.Env.Cfg.XltPathDir + "\\Thread_R07.xltx"); //預先開啟excel app
+            MyUtility.Excel.CopyToXls(printData, "", "Thread_R07.xltx", 3, showExcel: false, showSaveMsg: false, excelApp: objApp);
+
+            this.ShowWaitMessage("Excel Processing...");
+            Excel.Worksheet worksheet = objApp.Sheets[1];
+            //for (int i = 1; i <= printData.Rows.Count; i++)
+            //{
+            //    string str = worksheet.Cells[i + 1, 4].Value;
+            //    if (!MyUtility.Check.Empty(str))
+            //        worksheet.Cells[i + 1, 4] = str.Trim();
+
+            //}
+
+            worksheet.Columns.AutoFit();
+            worksheet.Rows.AutoFit();
+            objApp.Visible = true;
+
+            if (objApp != null) Marshal.FinalReleaseComObject(objApp);          //釋放objApp
+            if (worksheet != null) Marshal.FinalReleaseComObject(worksheet);    //釋放worksheet
+
+            this.HideWaitMessage();
+            return true;
+        }
+
+        private void textSHA_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                string sql = @"select   distinct
+                                        threadcolorid, 
+                                        (select tc.Description from dbo.ThreadColor tc where tc.id = ThreadStock.ThreadColorID) [Color_desc] 
+                               from ThreadStock 
+                               order by threadcolorid ";
+                Sci.Win.Tools.SelectItem item = new Win.Tools.SelectItem(sql, "13, 13", null, "Shade, Color desc");
+                DialogResult result = item.ShowDialog();
+                if (result == DialogResult.Cancel) { return; }
+                textSHA.Text = item.GetSelectedString();
+            }
+        }
+
+        private void textTYPE_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                string sql = @"select distinct 
+                                    l.Category 
+                               from dbo.ThreadStock ts
+                               inner join dbo.LocalItem l on l.refno = ts.Refno
+                               order by l.category";
+                Sci.Win.Tools.SelectItem item = new Win.Tools.SelectItem(sql, "20", null, "Type");
+                DialogResult result = item.ShowDialog();
+                if (result == DialogResult.Cancel) { return; }
+                textTYPE.Text = item.GetSelectedString();
+            }
+        }
+
+        private void textITEM_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                string sql = @"select distinct 
+                                    l.ThreadTypeID
+                               from dbo.LocalItem l
+                               inner join  dbo.ThreadStock ts on l.refno = ts.refno
+                               order by l.ThreadTypeID ";
+                Sci.Win.Tools.SelectItem item = new Win.Tools.SelectItem(sql, "35", null, "Thread Item");
+                DialogResult result = item.ShowDialog();
+                if (result == DialogResult.Cancel) { return; }
+                textITEM.Text = item.GetSelectedString();
+            }
+        }
+
+        private void textBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                string sql = @"select distinct 
+                                    Refno,
+                                    (select LocalItem.Description from dbo.LocalItem where refno= ThreadStock.Refno) [Description]
+                               from dbo.ThreadStock 
+                               order by Refno";
+                Sci.Win.Tools.SelectItem item = new Win.Tools.SelectItem(sql, "20, 40", null, "Refno, Description");
+                DialogResult result = item.ShowDialog();
+                if (result == DialogResult.Cancel) { return; }
+                textBox1.Text = item.GetSelectedString();
+            }
+        }
+
+        private void textBox2_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                string sql = @"select distinct 
+                                    Refno,
+                                    (select LocalItem.Description from dbo.LocalItem where refno= ThreadStock.Refno) [Description]
+                               from dbo.ThreadStock 
+                               order by Refno";
+                Sci.Win.Tools.SelectItem item = new Win.Tools.SelectItem(sql, "20, 40", null, "Refno, Description");
+                DialogResult result = item.ShowDialog();
+                if (result == DialogResult.Cancel) { return; }
+                textBox2.Text = item.GetSelectedString();
+            }
+        }
+
+        private void textLOC1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                string sql = @"select distinct 
+                                    ThreadlocationID,
+                                    (select distinct Description from dbo.ThreadLocation where ThreadLocation.ID = Threadincoming_Detail.ThreadLocationID) [Description]
+                               from dbo.Threadincoming_Detail 
+                               order by ThreadlocationID";
+                Sci.Win.Tools.SelectItem item = new Win.Tools.SelectItem(sql, "15, 15", null, "Location, Description");
+                DialogResult result = item.ShowDialog();
+                if (result == DialogResult.Cancel) { return; }
+                textLOC1.Text = item.GetSelectedString();
+            }
+        }
+
+        private void textLOC2_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                string sql = @"select distinct 
+                                    ThreadlocationID,
+                                    (select distinct Description from dbo.ThreadLocation where ThreadLocation.ID = Threadincoming_Detail.ThreadLocationID) [Description]
+                               from dbo.Threadincoming_Detail 
+                               order by ThreadlocationID";
+                Sci.Win.Tools.SelectItem item = new Win.Tools.SelectItem(sql, "15, 15", null, "Location, Description");
+                DialogResult result = item.ShowDialog();
+                if (result == DialogResult.Cancel) { return; }
+                textLOC2.Text = item.GetSelectedString();
+            }
+        }
+    }
+}
