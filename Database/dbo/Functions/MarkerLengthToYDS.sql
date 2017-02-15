6f2c4e7ba907c3f5c0d24f5507e3b4ffaae09aaf
ALGER <alger@sportscity.com.tw>
2017-02-15 14:02:32 +0000

    解決部分MarkerLength會有問題(例:07Y22-+1")

--- Database/dbo/Functions/MarkerLengthToYDS.sql
+++ Database/dbo/Functions/MarkerLengthToYDS.sql
@@ -33,17 +33,42 @@ Begin
 
 	Set @Yds = Cast(SubString(@MarkerLength, 1, @LocateYd - 1) as Numeric(7,4));
 	Set @Inch = Cast(SubString(@MarkerLength, @LocateYd + 1, @LocateInch - @LocateYd - 1) as Numeric(7,4));
-	Set @M1 = Cast(SubString(@MarkerLength, @LocateInch + 1, @LocateS1 - @LocateInch - 1) as Numeric(7,4));
-	If @LocateS2 = 0
+
+	--解決部分MarkerLength會有問題(例:07Y22-+1")
+	If @LocateS1 = 0
 	Begin
-		Set @M2 = Cast(SubString(@MarkerLength, @LocateS1 + 1, @LocateS3 - @LocateS1 - 1) as Numeric(7,4));
-		Set @M3 = 0;
+		Set @M1 = 0;
+		Set @M2 = 0;
+
+		If @LocateS2 = 0 
+			Begin
+				Set @M3 = 0;
+			End;
+			Else
+			Begin
+				Set @M3 = Cast(SubString(@MarkerLength, @LocateS2 + 1, @LocateS3 - @LocateS2 - 1) as Numeric(7,4));
+			End;
+
 	End;
 	Else
 	Begin
-		Set @M2 = Cast(SubString(@MarkerLength, @LocateS1 + 1, @LocateS2 - @LocateS1 - 1) as Numeric(7,4));
-		Set @M3 = Cast(SubString(@MarkerLength, @LocateS2 + 1, @LocateS3 - @LocateS2 - 1) as Numeric(7,4));
+		Set @M1 = Cast(SubString(@MarkerLength, @LocateInch + 1, @LocateS1 - @LocateInch - 1) as Numeric(7,4));
+
+		If @LocateS2 = 0 
+			Begin
+				Set @M2 = Cast(SubString(@MarkerLength, @LocateS1 + 1, @LocateS3 - @LocateS1 - 1) as Numeric(7,4));
+				Set @M3 = 0;
+			End;
+			Else
+			Begin
+				Set @M2 = Cast(SubString(@MarkerLength, @LocateS1 + 1, @LocateS2 - @LocateS1 - 1) as Numeric(7,4));
+				Set @M3 = Cast(SubString(@MarkerLength, @LocateS2 + 1, @LocateS3 - @LocateS2 - 1) as Numeric(7,4));
+			End;
+
 	End;
+
+
+
 	
 	If @M2 = 0
 	Begin
