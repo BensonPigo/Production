d81788cdae165bab1058e3c538e4da51442eabe3
JEFF <jeff@sportscity.com.tw>
2017-02-20 04:03:25 +0000

    update

--- Database/dbo/Stored Procedures/Cutting_P01_QtyBreakdown_PoCombbySPList.sql
+++ Database/dbo/Stored Procedures/Cutting_P01_QtyBreakdown_PoCombbySPList.sql
@@ -15,6 +15,9 @@ BEGIN
 	DECLARE @cols NVARCHAR(MAX)= N''
 	SELECT @cols = @cols + iif(@cols = N'',QUOTENAME(sizecode),N',' + QUOTENAME(sizecode))
 	from #tmp
+	DECLARE @cols2 NVARCHAR(MAX)= N''
+	SELECT @cols2 = @cols2 + iif(@cols2 = N'','sum('+QUOTENAME(sizecode)+')',N',sum(' + QUOTENAME(sizecode)+')')
+	from #tmp
 	drop table #tmp
 
 	DECLARE @sql NVARCHAR(MAX)
@@ -30,20 +33,28 @@ BEGIN
 			max(Qty) for sizecode in ('+@cols+N')
 		) as pt
 	)
-	SELECT
+	SELECT distinct
 		[SP#] = o.ID,
 		[Style] = o.StyleID,
 		oa.Article,
+		oc.ColorID,
 		[OrderNo] = o.Customize1,
 		[PONo] = o.CustPONo,
 		[CustCD] = o.CustCDID,
 		[TTL] = o.Qty,
 		'+@cols+N'
+	into #tmp2
 	FROM orders o
 	inner join Order_Article oa on o.ID = oa.id
 	inner join a on a.OrderID = o.ID
+	inner join Order_ColorCombo oc on oa.Article = oc.Article and oc.PatternPanel = ''FA''
 	where o.POID = '''+@OrderID+N'''
 	order by o.ID
+	select * from #tmp2
+	union all
+	select ''Total'','''','''','''','''','''','''',sum(TTL),'+@cols2+N'
+	from #tmp2
+	drop table #tmp2
 	'
 	EXEC sp_executesql @sql
 END
