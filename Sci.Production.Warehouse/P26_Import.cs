900362610bcfa69ffe927627d641471d85b80340
RICKY <ricky@sportscity.com.tw>
2017-02-20 03:47:09 +0000

    Fix P26_Import 1.修正roll dyelot 有' 會造成 import crash 問題。

--- Sci.Production.Warehouse/P26_Import.cs
+++ Sci.Production.Warehouse/P26_Import.cs
@@ -278,8 +278,11 @@ and r1.id = '{0}' ", transid, dr_master["stocktype"].ToString())); //
 
             foreach (DataRow tmp in dr2)
             {
-                DataRow[] findrow = dt_detail.Select(string.Format("poid = '{0}' and seq1 = '{1}' and seq2 = '{2}' and roll ='{3}'and dyelot='{4}'"
-                    , tmp["poid"].ToString(), tmp["seq1"].ToString(), tmp["seq2"].ToString(), tmp["roll"].ToString(), tmp["dyelot"].ToString()));
+                DataRow[] findrow = dt_detail.AsEnumerable().Where(row => row["poid"].EqualString(tmp["poid"].ToString()) && row["seq1"].EqualString(tmp["seq1"].ToString())
+                                                                          && row["seq2"].EqualString(tmp["seq2"].ToString()) && row["roll"].EqualString(tmp["roll"].ToString())
+                                                                          && row["dyelot"].EqualString(tmp["dyelot"].ToString())).ToArray();
+                //DataRow[] findrow = dt_detail.Select(string.Format("poid = '{0}' and seq1 = '{1}' and seq2 = '{2}' and roll ='{3}'and dyelot='{4}'"
+                //    , tmp["poid"].ToString(), tmp["seq1"].ToString(), tmp["seq2"].ToString(), tmp["roll"].ToString(), tmp["dyelot"].ToString()));
 
                 if (findrow.Length > 0)
                 {
