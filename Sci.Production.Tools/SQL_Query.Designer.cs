b2cebc2ebb70fde406bfb1ab597fa43fcea9db0b
JEFF <jeff@sportscity.com.tw>
2017-02-21 06:18:03 +0000

    add TOOL

---
+++ Sci.Production.Tools/SQL_Query.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Sci.Production.Tools
+{
+    partial class SQL_Query
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.grid = new Sci.Win.UI.Grid();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.Query = new Sci.Win.UI.Button();
+            this.editBox = new Sci.Win.UI.EditBox();
+            this.listControlBindingSource1 = new Sci.Win.UI.ListControlBindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.grid);
+            this.splitContainer1.Size = new System.Drawing.Size(801, 502);
+            this.splitContainer1.SplitterDistance = 99;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // grid
+            // 
+            this.grid.AllowUserToAddRows = false;
+            this.grid.AllowUserToDeleteRows = false;
+            this.grid.AllowUserToResizeRows = false;
+            this.grid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.grid.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.grid.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.DataSource = this.listControlBindingSource1;
+            this.grid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid.EditingEnter = Ict.Win.UI.DataGridViewEditingEnter.NextCellOrNextRow;
+            this.grid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.grid.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.grid.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(162)))), ((int)(((byte)(163)))));
+            this.grid.Location = new System.Drawing.Point(0, 0);
+            this.grid.Name = "grid";
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grid.RowHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.grid.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.grid.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.grid.RowTemplate.Height = 24;
+            this.grid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grid.Size = new System.Drawing.Size(801, 399);
+            this.grid.TabIndex = 1;
+            this.grid.TabStop = false;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.editBox);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.Query);
+            this.splitContainer2.Size = new System.Drawing.Size(801, 99);
+            this.splitContainer2.SplitterDistance = 723;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // Query
+            // 
+            this.Query.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Query.Location = new System.Drawing.Point(0, 0);
+            this.Query.Name = "Query";
+            this.Query.Size = new System.Drawing.Size(74, 99);
+            this.Query.TabIndex = 12;
+            this.Query.Text = "Query";
+            this.Query.UseVisualStyleBackColor = true;
+            this.Query.Click += new System.EventHandler(this.Query_Click);
+            // 
+            // editBox
+            // 
+            this.editBox.BackColor = System.Drawing.Color.White;
+            this.editBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.editBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.editBox.IsSupportEditMode = false;
+            this.editBox.Location = new System.Drawing.Point(0, 0);
+            this.editBox.Multiline = true;
+            this.editBox.Name = "editBox";
+            this.editBox.Size = new System.Drawing.Size(723, 99);
+            this.editBox.TabIndex = 11;
+            // 
+            // SQL_Query
+            // 
+            this.ClientSize = new System.Drawing.Size(801, 502);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "SQL_Query";
+            this.Text = "() ";
+            this.Controls.SetChildIndex(this.splitContainer1, 0);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel1.PerformLayout();
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private Win.UI.Grid grid;
+        private Win.UI.Button Query;
+        private Win.UI.EditBox editBox;
+        private Win.UI.ListControlBindingSource listControlBindingSource1;
+
+    }
+}
---
+++ Sci.Production.Tools/SQL_Query.cs
@@ -0,0 +1,46 @@
+﻿using Ict;
+using Sci.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Tools
+{
+    public partial class SQL_Query : Sci.Win.Tems.QueryForm
+    {
+        public SQL_Query(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            GridSetup();
+        }
+
+        string sqlcmd;
+
+        private void GridSetup()
+        {
+            this.grid.AutoGenerateColumns = true;
+        }
+
+        private void Query_Click(object sender, EventArgs e)
+        {
+            listControlBindingSource1.DataSource = null;
+            
+            sqlcmd = editBox.Text;
+            DualResult result;
+            DataTable dt;
+            result = DBProxy.Current.Select("", sqlcmd, null, out dt);
+            if (!result)
+            {
+                ShowErr(sqlcmd, result);
+                return;
+            }
+
+            listControlBindingSource1.DataSource = dt;
+        }
+    }
+}
