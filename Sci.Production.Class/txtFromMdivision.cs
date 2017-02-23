64ed350dfd4ebd79c9845ddfa970d124f647d632
JIMMY <jimmy@sportscity.com.tw>
2017-02-23 03:52:13 +0000

    add txtFromMdivision

---
+++ Sci.Production.Class/txtFromMdivision.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sci.Data;
+using Sci.Win.UI;
+
+namespace Sci.Production.Class
+{
+    public partial class txtFromMdivision : Sci.Win.UI.TextBox
+    {
+        protected override void OnPopUp(TextBoxPopUpEventArgs e)
+        {
+            base.OnPopUp(e);
+
+            Sci.Win.Tools.SelectItem item = new Sci.Win.Tools.SelectItem(string.Format("select ID from dbo.MDivision where ID <> '{0}'", Sci.Env.User.Keyword), "8,40", this.Text, false, ",");
+            DialogResult result = item.ShowDialog();
+            if (result == DialogResult.Cancel) { return; }
+            this.Text = item.GetSelectedString();
+            this.ValidateText();
+        }
+
+        protected override void OnValidating(CancelEventArgs e)
+        {
+            base.OnValidating(e);
+
+            string str = this.Text;
+            if (!string.IsNullOrWhiteSpace(str) && str != this.OldValue)
+            {
+                if (MyUtility.Check.Seek(str, "Mdivision", "id") == false)
+                {
+                    MyUtility.Msg.WarningBox(string.Format("< Mdivision ID : {0} > not found!!!", str));
+                    this.Text = "";
+                    e.Cancel = true;
+                    return;
+                }
+            }
+        }
+
+        public txtFromMdivision()
+        {
+            this.Size = new System.Drawing.Size(66, 23);
+        }
+    }
+}
