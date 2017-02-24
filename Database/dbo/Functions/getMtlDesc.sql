9a8fcff8821c2da7bf3cb1207930da18efcc2ada
JIMMY <jimmy@sportscity.com.tw>
2017-02-24 05:36:15 +0000

    add 判斷 null suppColor => colorID 換行 => char(13) + char(10)

--- Database/dbo/Functions/getMtlDesc.sql
+++ Database/dbo/Functions/getMtlDesc.sql
@@ -4,7 +4,7 @@
 CREATE FUNCTION [dbo].[getMtlDesc](@poid varchar(13),@seq1 varchar(3),@seq2 varchar(2),@type int,@repeat bit) 
 RETURNS nvarchar(max)  -- 回傳Description
 BEGIN
-	DECLARE @fabric_detaildesc nvarchar(max); -- 暫存fabric description
+DECLARE @fabric_detaildesc nvarchar(max); -- 暫存fabric description
 	DECLARE @suppcolor nvarchar(max); -- 暫存 supplier color
 	DECLARE @po_desc nvarchar(max); -- 暫存 po_supp_detail 相關欄位
     DECLARE @string nvarchar(max)  -- 最後回傳的字串
@@ -17,12 +17,12 @@ BEGIN
 
 	SELECT @scirefno=p.SCIRefno
 		, @refno = p.Refno
-		, @suppcolor = ISNULL(p.SuppColor,'')
+		, @suppcolor = ISNULL(p.ColorID,'')
 		, @StockSP = isnull(concat(p.StockPOID,' ',p.StockSeq1,' ',p.StockSeq2),'')
-		, @po_desc=@po_desc + ISNULL(p.ColorDetail,'')+ CHAR(13)--+CHAR(10)
-		, @po_desc=@po_desc + ISNULL(p.sizespec,'')+ CHAR(13)--+CHAR(10)
-		, @po_desc=@po_desc + ISNULL(p.SizeUnit,'')+ CHAR(13)--+CHAR(10)
-		, @po_desc=@po_desc + ISNULL(p.Special,'')+ CHAR(13)--+CHAR(10)
+		, @po_desc=@po_desc + ISNULL(p.ColorDetail,'')+ CHAR(13)+CHAR(10)
+		, @po_desc=@po_desc + ISNULL(p.sizespec,'')+ CHAR(13)+CHAR(10)
+		, @po_desc=@po_desc + ISNULL(p.SizeUnit,'')+ CHAR(13)+CHAR(10)
+		, @po_desc=@po_desc + ISNULL(p.Special,'')+ CHAR(13)+CHAR(10)
 		, @po_desc=@po_desc + ISNULL(p.Remark,'')
 		from dbo.po_supp_detail p WITH (NOLOCK) WHERE ID=@poid and seq1 = @seq1 and seq2=@seq2;
 
@@ -31,10 +31,10 @@ BEGIN
 		if @repeat = 0
 		BEGIN
 			select @fabric_detaildesc= ISNULL(DescDetail,'') from fabric WITH (NOLOCK) where SCIRefno = @scirefno;
-			set @string = rtrim(iif(@fabric_detaildesc='','',@fabric_detaildesc + CHAR(13)));
+			set @string = rtrim(iif(isnull(@fabric_detaildesc, '') = '','',@fabric_detaildesc + CHAR(13)+ CHAR(10)));
 		END
 		ELSE
-			set @string = rtrim(iif(@suppcolor = '','',@suppcolor + CHAR(13)))+rtrim(iif(@po_desc = '','',@po_desc+CHAR(13)));
+			set @string = rtrim(iif(isnull(@suppcolor, '') = '','',@suppcolor + CHAR(13)+ CHAR(10)))+rtrim(iif(isnull(@po_desc, '') = '','',@po_desc+CHAR(13)+ CHAR(10)));
 	END
 
 	IF @type =2
@@ -42,10 +42,15 @@ BEGIN
 		if @repeat = 0
 		BEGIN	
 			select @fabric_detaildesc= ISNULL(DescDetail,'') from fabric WITH (NOLOCK) where SCIRefno = @scirefno;
-			set @string = rtrim(iif(@fabric_detaildesc='','',@fabric_detaildesc + CHAR(13)))+ rtrim(iif(@suppcolor = '','',@suppcolor + CHAR(13)))+rtrim(iif(@po_desc = '','',@po_desc+CHAR(13)));
+			set @string = concat(rtrim(iif(isnull(@fabric_detaildesc, '') = '','',@fabric_detaildesc))
+								, CHAR(13), CHAR(10)
+								, rtrim(iif(isnull(@suppcolor, '') = '','',@suppcolor))
+								, CHAR(13), CHAR(10)
+								, rtrim(iif(isnull(@po_desc, '') = '','',@po_desc))
+								, CHAR(13), CHAR(10));
 		END
 		ELSE
-			set @string = rtrim(iif(@suppcolor = '','',@suppcolor + CHAR(13)))+rtrim(iif(@po_desc = '','',@po_desc+CHAR(13)));
+			set @string = rtrim(iif(isnull(@suppcolor, '') = '','',@suppcolor + CHAR(13)+ CHAR(10)))+rtrim(iif(isnull(@po_desc, '') = '','',@po_desc+CHAR(13)+ CHAR(10)));
 	END
 
 	IF @type =4
@@ -54,15 +59,17 @@ BEGIN
 		if @repeat = 0
 		BEGIN
 			select @fabric_detaildesc= ISNULL([Description],'') from fabric WITH (NOLOCK) where SCIRefno = @scirefno;
-			set @string = 'Ref#'+ @refno + ', ' + rtrim(iif(@fabric_detaildesc='','',@fabric_detaildesc + CHAR(13))) + rtrim(iif(@suppcolor = '','',@suppcolor + CHAR(13)))+rtrim(iif(@po_desc = '','',@po_desc+CHAR(13)));
+			set @string = 'Ref#'+ @refno + ', ' + rtrim(iif(isnull(@fabric_detaildesc, '') = '','',@fabric_detaildesc + CHAR(13)+ CHAR(10))) + rtrim(iif(isnull(@suppcolor, '') = '','',@suppcolor + CHAR(13)+ CHAR(10)))+rtrim(iif(isnull(@po_desc, '') = '','',@po_desc+CHAR(13)+ CHAR(10)));
 		End
 		ELSE
-			set @string = rtrim(iif(@suppcolor = '','',@suppcolor + CHAR(13)))+rtrim(iif(@po_desc = '','',@po_desc+CHAR(13)));
+			set @string = rtrim(iif(isnull(@suppcolor, '') = '','',@suppcolor + CHAR(13)))+rtrim(iif(isnull(@po_desc, '') = '','',@po_desc+CHAR(13)+ CHAR(10)));
 	END
 
 	IF left(@SEQ1,1) = '7'
 	BEGIN
-		SET @string = '**PLS USE STOCK FROM SP#:' + iif(@StockSP='','',@StockSP) + '**' + CHAR(13) + @string;
+		SET @string = concat('**PLS USE STOCK FROM SP#:' + iif(isnull(@StockSP, '') = '','',@StockSP) + '**' 
+							, CHAR(13) , CHAR(10)
+							, isnull(@string, ''));
 	END 
 
     RETURN rtrim(@string)
