093327851c09a6bd382a08a53a2e4aab68737458
JIMMY <jimmy@sportscity.com.tw>
2017-03-01 02:57:43 +0000

    add color.Name

--- Database/dbo/Functions/getMtlDesc.sql
+++ Database/dbo/Functions/getMtlDesc.sql
@@ -4,7 +4,7 @@
 CREATE FUNCTION [dbo].[getMtlDesc](@poid varchar(13),@seq1 varchar(3),@seq2 varchar(2),@type int,@repeat bit) 
 RETURNS nvarchar(max)  -- 回傳Description
 BEGIN
-DECLARE @fabric_detaildesc nvarchar(max); -- 暫存fabric description
+	DECLARE @fabric_detaildesc nvarchar(max); -- 暫存fabric description
 	DECLARE @suppcolor nvarchar(max); -- 暫存 supplier color
 	DECLARE @po_desc nvarchar(max); -- 暫存 po_supp_detail 相關欄位
     DECLARE @string nvarchar(max)  -- 最後回傳的字串
@@ -17,14 +17,17 @@ DECLARE @fabric_detaildesc nvarchar(max); -- 暫存fabric description
 
 	SELECT @scirefno=p.SCIRefno
 		, @refno = p.Refno
-		, @suppcolor = ISNULL(p.ColorID,'')
+		, @suppcolor = iif(ISNULL(p.ColorID,'') = '', '', p.ColorID + ' - ') + ISNULL(c.Name, '')
 		, @StockSP = isnull(concat(p.StockPOID,' ',p.StockSeq1,' ',p.StockSeq2),'')
-		, @po_desc=@po_desc + ISNULL(p.ColorDetail,'')+ CHAR(13)+CHAR(10)
-		, @po_desc=@po_desc + ISNULL(p.sizespec,'')+ CHAR(13)+CHAR(10)
-		, @po_desc=@po_desc + ISNULL(p.SizeUnit,'')+ CHAR(13)+CHAR(10)
-		, @po_desc=@po_desc + ISNULL(p.Special,'')+ CHAR(13)+CHAR(10)
+		, @po_desc=@po_desc + iif(ISNULL(p.ColorDetail,'') = '', '', p.ColorDetail + CHAR(13)+CHAR(10))
+		, @po_desc=@po_desc + iif(ISNULL(p.sizespec,'') = '', '', p.sizespec + CHAR(13)+CHAR(10))
+		, @po_desc=@po_desc + iif(ISNULL(p.SizeUnit,'') = '', '', p.SizeUnit + CHAR(13)+CHAR(10))
+		, @po_desc=@po_desc + iif(ISNULL(p.Special,'') = '', '', p.Special + CHAR(13)+CHAR(10))
 		, @po_desc=@po_desc + ISNULL(p.Remark,'')
-		from dbo.po_supp_detail p WITH (NOLOCK) WHERE ID=@poid and seq1 = @seq1 and seq2=@seq2;
+		from dbo.po_supp_detail p WITH (NOLOCK)
+		left join fabric f WITH (NOLOCK) on p.SCIRefno = f.SCIRefno
+		left join Color c WITH (NOLOCK) on f.BrandID = c.BrandId and p.ColorID = c.ID 
+		WHERE p.ID=@poid and seq1 = @seq1 and seq2=@seq2;
 
 	IF  @type = 1
 	BEGIN
@@ -42,15 +45,12 @@ DECLARE @fabric_detaildesc nvarchar(max); -- 暫存fabric description
 		if @repeat = 0
 		BEGIN	
 			select @fabric_detaildesc= ISNULL(DescDetail,'') from fabric WITH (NOLOCK) where SCIRefno = @scirefno;
-			set @string = concat(rtrim(iif(isnull(@fabric_detaildesc, '') = '','',@fabric_detaildesc))
-								, CHAR(13), CHAR(10)
-								, rtrim(iif(isnull(@suppcolor, '') = '','',@suppcolor))
-								, CHAR(13), CHAR(10)
-								, rtrim(iif(isnull(@po_desc, '') = '','',@po_desc))
-								, CHAR(13), CHAR(10));
+			set @string = concat(rtrim(iif(isnull(@fabric_detaildesc, '') = '','',@fabric_detaildesc + CHAR(13) + CHAR(10)))
+								, rtrim(iif(isnull(@suppcolor, '') = '','',@suppcolor + CHAR(13) + CHAR(10)))
+								, rtrim(iif(isnull(@po_desc, '') = '','',@po_desc + CHAR(13) + CHAR(10))));
 		END
 		ELSE
-			set @string = rtrim(iif(isnull(@suppcolor, '') = '','',@suppcolor + CHAR(13)+ CHAR(10)))+rtrim(iif(isnull(@po_desc, '') = '','',@po_desc+CHAR(13)+ CHAR(10)));
+			set @string = rtrim(iif(isnull(@suppcolor, '') = '','',@suppcolor + CHAR(13)+ CHAR(10)))+rtrim(iif(isnull(@po_desc, '') = '','',@po_desc + CHAR(13)+ CHAR(10)));
 	END
 
 	IF @type =4
@@ -62,7 +62,7 @@ DECLARE @fabric_detaildesc nvarchar(max); -- 暫存fabric description
 			set @string = 'Ref#'+ @refno + ', ' + rtrim(iif(isnull(@fabric_detaildesc, '') = '','',@fabric_detaildesc + CHAR(13)+ CHAR(10))) + rtrim(iif(isnull(@suppcolor, '') = '','',@suppcolor + CHAR(13)+ CHAR(10)))+rtrim(iif(isnull(@po_desc, '') = '','',@po_desc+CHAR(13)+ CHAR(10)));
 		End
 		ELSE
-			set @string = rtrim(iif(isnull(@suppcolor, '') = '','',@suppcolor + CHAR(13)))+rtrim(iif(isnull(@po_desc, '') = '','',@po_desc+CHAR(13)+ CHAR(10)));
+			set @string = rtrim(iif(isnull(@suppcolor, '') = '','',@suppcolor + CHAR(13) + CHAR(10)))+rtrim(iif(isnull(@po_desc, '') = '','',@po_desc+CHAR(13) + CHAR(10)));
 	END
 
 	IF left(@SEQ1,1) = '7'
@@ -72,5 +72,5 @@ DECLARE @fabric_detaildesc nvarchar(max); -- 暫存fabric description
 							, isnull(@string, ''));
 	END 
 
-    RETURN rtrim(@string)
+    RETURN ltrim(rtrim(@string))
 END
