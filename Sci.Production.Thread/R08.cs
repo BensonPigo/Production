d212475f6aa12e25c275e15ffa3cfb8d96bbf91f
JIMMY <jimmy@sportscity.com.tw>
2017-03-03 06:17:36 +0000

    add R08

---
+++ Sci.Production.Thread/R08.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sci.Production.Thread
+{
+    public partial class R08 : Sci.Win.Tems.PrintForm
+    {
+        public R08(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+        }
+
+        protected override bool ValidateInput()
+        {
+            return base.ValidateInput();
+        }
+
+        protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            return base.OnAsyncDataLoad(e);
+        }
+
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+
+            return true;
+        }
+    }
+}
