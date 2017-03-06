61e9842e541cd4abed20d2f6d75ef8418960763b
JIMMY <jimmy@sportscity.com.tw>
2017-03-06 04:19:26 +0000

    when checkBox = true & Qty = 0  then Qty = 所有庫存數 when checkBox = false           then Qty = 0 when checkBox = true & Qty != 0 then Qty 不變

--- Sci.Production.Warehouse/P61_Import.cs
+++ Sci.Production.Warehouse/P61_Import.cs
@@ -102,6 +102,10 @@ Where c.OrderID = '{0}' and c.inqty-c.outqty + c.adjustqty > 0 and c.mdivisionid
                     {
                         dr["qty"] = dr["balance"];
                     }
+                    else if (Convert.ToBoolean(dr["selected"]) == false)
+                    {
+                        dr["qty"] = 0;
+                    }
                     dr.EndEdit();
                     this.sum_checkedqty();
                 }
