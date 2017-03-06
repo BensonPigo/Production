cb53383c317c4f899d29e095f34604dad32df82b
JIMMY <jimmy@sportscity.com.tw>
2017-03-06 03:14:53 +0000

    checkBox = true, Qty 自動帶出所有庫存數

--- Sci.Production.Warehouse/P37_Import.cs
+++ Sci.Production.Warehouse/P37_Import.cs
@@ -186,6 +186,20 @@ where f.InQty - f.OutQty + f.AdjustQty > 0 and f.lock=0 and a.Status = 'Confirme
 
             this.grid1.IsEditingReadOnly = false; //必設定, 否則CheckBox會顯示圖示
             this.grid1.DataSource = listControlBindingSource1;
+
+            this.grid1.CellValueChanged += (s, e) =>
+            {
+                if (grid1.Columns[e.ColumnIndex].Name == col_chk.Name)
+                {
+                    DataRow dr = grid1.GetDataRow(e.RowIndex);
+                    if (Convert.ToBoolean(dr["selected"]) == true && Convert.ToDecimal(dr["qty"].ToString()) == 0)
+                    {
+                        dr["qty"] = dr["balance"];
+                    }
+                    dr.EndEdit();
+                }
+            };
+
             Helper.Controls.Grid.Generator(this.grid1)
                 .CheckBox("Selected", header: "", width: Widths.AnsiChars(3), iseditable: true, trueValue: 1, falseValue: 0).Get(out col_chk)   //0
                 .Text("exportid", header: "WK#", iseditingreadonly: true, width: Widths.AnsiChars(14)) //1
