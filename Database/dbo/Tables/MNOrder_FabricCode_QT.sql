bbba24f5a1dbb26c59b41b08fab7164c434a227f
JEFF <jeff@sportscity.com.tw>
2017-03-10 08:24:15 +0000

    新增資料表:MNOrder_FabricCode_Article，MNOrder_FabricCode_QT 刪除資料表:Order_FabricCode_Article，Style_FabricCode_Article

---
+++ Database/dbo/Tables/MNOrder_FabricCode_QT.sql
@@ -0,0 +1,48 @@
+﻿CREATE TABLE [dbo].[MNOrder_FabricCode_QT] (
+    [Id]                VARCHAR (13) CONSTRAINT [DF_MNOrder_FabricCode_QT_Id] DEFAULT ('') NOT NULL,
+    [FabricPanelCode]   VARCHAR (2)  CONSTRAINT [DF_MNOrder_FabricCode_QT_FabricPanelCode] DEFAULT ('') NOT NULL,
+    [SeqNO]             VARCHAR (2)  CONSTRAINT [DF_MNOrder_FabricCode_QT_SeqNO] DEFAULT ('') NOT NULL,
+    [QTFabricPanelCode] VARCHAR (2)  CONSTRAINT [DF_MNOrder_FabricCode_QT_QTFabricPanelCode] DEFAULT ('') NULL,
+    [AddName]           VARCHAR (10) CONSTRAINT [DF_MNOrder_FabricCode_QT_AddName] DEFAULT ('') NULL,
+    [AddDate]           DATETIME     NULL,
+    [EditName]          VARCHAR (10) CONSTRAINT [DF_MNOrder_FabricCode_QT_EditName] DEFAULT ('') NULL,
+    [EditDate]          DATETIME     NULL,
+    CONSTRAINT [PK_MNOrder_FabricCode_QT] PRIMARY KEY CLUSTERED ([Id] ASC, [FabricPanelCode] ASC, [SeqNO] ASC)
+);
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'最後修改時間', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MNOrder_FabricCode_QT', @level2type = N'COLUMN', @level2name = N'EditDate';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'最後修改人員', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MNOrder_FabricCode_QT', @level2type = N'COLUMN', @level2name = N'EditName';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'新增時間', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MNOrder_FabricCode_QT', @level2type = N'COLUMN', @level2name = N'AddDate';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'新增人員', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MNOrder_FabricCode_QT', @level2type = N'COLUMN', @level2name = N'AddName';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'QT 布別+布種的代碼', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MNOrder_FabricCode_QT', @level2type = N'COLUMN', @level2name = N'QTFabricPanelCode';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'編號', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MNOrder_FabricCode_QT', @level2type = N'COLUMN', @level2name = N'SeqNO';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'布別+布種的代碼', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MNOrder_FabricCode_QT', @level2type = N'COLUMN', @level2name = N'FabricPanelCode';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'訂單單號', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MNOrder_FabricCode_QT', @level2type = N'COLUMN', @level2name = N'Id';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'MnOrder : 部位別的QT組合', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MNOrder_FabricCode_QT';
+
