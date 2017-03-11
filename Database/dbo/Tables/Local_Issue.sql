ed39e5c5bcb9569095e1dd08de4eeb36fdfc140a
JIMMY <jimmy@sportscity.com.tw>
2017-03-11 03:30:32 +0000

    Add Table -> Local_Issue, Local_Issue_Detail Update FtyInventory -> Index

---
+++ Database/dbo/Tables/Local_Issue.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Local_Issue] (
+    [ID]          VARCHAR (13) NOT NULL,
+    [IssueDate]   DATE         NOT NULL,
+    [MDivisionID] VARCHAR (8)  NOT NULL,
+    [Status]      VARCHAR (15) NULL,
+    [AddName]     VARCHAR (10) NULL,
+    [AddDate]     DATETIME     NULL,
+    [EditName]    VARCHAR (10) NULL,
+    [EditDate]    DATETIME     NULL,
+    CONSTRAINT [PK_Local_Issue] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
---
+++ Database/dbo/Tables/Local_Issue_Detail.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Local_Issue_Detail] (
+    [ID]       VARCHAR (13)    NOT NULL,
+    [Poid]     VARCHAR (13)    NOT NULL,
+    [SCIRefno] VARCHAR (26)    NOT NULL,
+    [Qty]      NUMERIC (10, 2) NULL,
+    CONSTRAINT [PK_Local_Issue_Detail] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
