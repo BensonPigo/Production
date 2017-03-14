0384006e85ae4793b26787e86cb8f6db67230e74
JIMMY <jimmy@sportscity.com.tw>
2017-03-14 11:51:55 +0000

    update Local_Issue

--- Database/dbo/Tables/Local_Issue_Detail.sql
+++ Database/dbo/Tables/Local_Issue_Detail.sql
@@ -1,8 +1,11 @@
 ﻿CREATE TABLE [dbo].[Local_Issue_Detail] (
-    [ID]       VARCHAR (13)    NOT NULL,
-    [Poid]     VARCHAR (13)    NOT NULL,
-    [SCIRefno] VARCHAR (26)    NOT NULL,
-    [Qty]      NUMERIC (10, 2) NULL,
+    [ID]            VARCHAR (13)    NOT NULL,
+    [OrderID]       VARCHAR (13)    NOT NULL,
+    [Refno]         VARCHAR (21)    NOT NULL,
+    [ThreadColorID] VARCHAR (15)    NOT NULL,
+    [Qty]           NUMERIC (10, 2) NOT NULL,
     CONSTRAINT [PK_Local_Issue_Detail] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 
