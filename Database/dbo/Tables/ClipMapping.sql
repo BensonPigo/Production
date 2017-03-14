e144a5a1a2f4aa47a7639ed96f061d16b8c95372
YINMEI <yinmei@sportscity.com.tw>
2017-03-14 02:30:37 +0000

    add table: ClipMapping.sql

---
+++ Database/dbo/Tables/ClipMapping.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[ClipMapping] (
+    [NewTableName] VARCHAR (50) CONSTRAINT [DF_ClipMapping_NewTableName] DEFAULT ('') NOT NULL,
+    [OldTableName] VARCHAR (30) CONSTRAINT [DF_ClipMapping_OldTableName] DEFAULT ('') NOT NULL,
+    CONSTRAINT [PK_ClipMapping] PRIMARY KEY CLUSTERED ([NewTableName] ASC)
+);
+
