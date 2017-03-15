7e6b21eb0e4d5fa0c63b42a4c4d6798cd546816c
JIMMY <jimmy@sportscity.com.tw>
2017-03-15 09:57:34 +0000

    fix Import

--- Sci.Production.Warehouse/P13_Import.cs
+++ Sci.Production.Warehouse/P13_Import.cs
@@ -185,9 +185,10 @@ Where a.id = '{0}' and c.lock = 0 and c.inqty-c.outqty + c.adjustqty > 0
             dr2 = dtGridBS1.Select("qty <> 0 and Selected = 1");
             foreach (DataRow tmp in dr2)
             {
-                DataRow[] findrow = dt_detail.AsEnumerable().Where(row => row["poid"].EqualString(tmp["poid"].ToString()) && row["seq1"].EqualString(tmp["seq1"]) 
-                                                                          &&  row["seq2"].EqualString(tmp["seq2"].ToString()) && row["roll"].EqualString(tmp["roll"])
-                                                                          &&  row["dyelot"].EqualString(tmp["dyelot"])).ToArray();
+                DataRow[] findrow = dt_detail.AsEnumerable().Where(row => row.RowState != DataRowState.Deleted
+                                                                          && row["poid"].EqualString(tmp["poid"].ToString()) && row["seq1"].EqualString(tmp["seq1"]) 
+                                                                          && row["seq2"].EqualString(tmp["seq2"].ToString()) && row["roll"].EqualString(tmp["roll"])
+                                                                          && row["dyelot"].EqualString(tmp["dyelot"])).ToArray();
                 // .Select(string.Format("poid = '{0}' and seq1 = '{1}' and seq2 = '{2}' and roll ='{3}'and dyelot='{4}'"
                     //, tmp["poid"].ToString(), tmp["seq1"].ToString(), tmp["seq2"].ToString(), tmp["roll"].ToString(), tmp["dyelot"].ToString()));
 
