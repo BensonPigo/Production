ed5f956b2f9d17ba9e5c9039d6b25e3a1ca4fff5
WILLY <willy@sportscity.com.tw>
2017-03-16 01:13:28 +0000

    Create 2隻財務統計報表Function

---
+++ Database/dbo/Functions/GetPoPriceByArticleSize.sql
@@ -0,0 +1,35 @@
+﻿-- =============================================
+-- Author:		<Ray.Chiou>
+-- Create date: <2016/06/13>
+-- Description:	<指定訂單編號、顏色組、尺寸別取得訂單單價>
+-- =============================================
+CREATE FUNCTION [dbo].[GetPoPriceByArticleSize]
+(
+	@ID varchar(13),
+	@Article Varchar(8),
+	@SizeCode varchar(8)
+)
+RETURNS  numeric(8,3)
+AS
+BEGIN
+	-- Declare the return variable here
+	Declare @DefaultPrice  numeric(8,3) 
+Set @DefaultPrice = isnull((select Poprice from Order_UnitPrice where ID = @ID and article = '----' and SizeCode = '----'),0) 
+
+declare @Return  numeric(8,3)
+select @Return = Poprice from Order_UnitPrice where ID = @ID and Article = @Article and SizeCode = @SizeCode
+set @Return = isnull(@Return,@DefaultPrice)
+Return @Return 
+
+END
+
+
+
+
+
+
+
+
+
+GO
+
---
+++ Database/dbo/Functions/GetSuitRate.sql
@@ -0,0 +1,36 @@
+﻿-- =============================================
+-- Author:		<Ray>
+-- Create date: <2016/07/05>
+-- Description:	<指定ComboPcs,ComboType取得套裝配比>
+-- =============================================
+CREATE FUNCTION [dbo].[GetSuitRate]
+	(
+	 @ComboPcs		numeric(1,0),
+	 @ComboType		varchar(1)
+	)
+RETURNS numeric(3,1)
+AS
+BEGIN
+Declare @Return numeric(3,1) = 1
+
+if @ComboPcs = 2  
+Begin 
+	Set @Return = iif(@ComboType= 'T',0.6,iif(@ComboType= 'B',0.4,@Return))
+end
+
+if @ComboPcs = 3  
+Begin 
+	Set @Return = iif(@ComboType= 'T',0.4,iif(@ComboType= 'B',0.3,iif(@ComboType= 'I',0.3,@Return)))
+end
+
+if @ComboPcs = 4  
+Begin 
+	Set @Return = iif(@ComboType= 'T',0.3,iif(@ComboType= 'B',0.3,iif(@ComboType= 'I',0.2,iif(@ComboType= '0',0.2,@Return))))
+end
+
+RETURN @Return
+END
+
+
+GO
+
