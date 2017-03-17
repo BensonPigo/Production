a8738cc3926ef983d7bf1dc2ef8cae1209d82de1
JIMMY <jimmy@sportscity.com.tw>
2017-03-17 06:44:20 +0000

    Delete 判斷狀態 CurrentMaintain["Status"].EqualString("CONFIRMED")

--- Sci.Production.Warehouse/P16.cs
+++ Sci.Production.Warehouse/P16.cs
@@ -93,9 +93,8 @@ namespace Sci.Production.Warehouse
 
         // delete前檢查
         protected override bool ClickDeleteBefore()
-        {
-            DataRow dr = grid.GetDataRow<DataRow>(grid.GetSelectedRowIndex());
-            if (dr["status"].ToString().ToUpper() == "CONFIRMED")
+        {            
+            if (CurrentMaintain["Status"].EqualString("CONFIRMED"))
             {
                 MyUtility.Msg.WarningBox("Data is confirmed, can't delete.", "Warning");
                 return false;
@@ -106,8 +105,7 @@ namespace Sci.Production.Warehouse
         // edit前檢查
         protected override bool ClickEditBefore()
         {
-            DataRow dr = grid.GetDataRow<DataRow>(grid.GetSelectedRowIndex());
-            if (dr["status"].ToString().ToUpper() == "CONFIRMED")
+            if (CurrentMaintain["Status"].EqualString("CONFIRMED"))
             {
                 MyUtility.Msg.WarningBox("Data is confirmed, can't modify.", "Warning");
                 return false;
