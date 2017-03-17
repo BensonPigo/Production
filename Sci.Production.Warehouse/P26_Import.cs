0333f6a820df1dbe368924c4f22b1c73b030654a
JIMMY <jimmy@sportscity.com.tw>
2017-03-17 08:59:13 +0000

    remove sp Validating

--- Sci.Production.Warehouse/P26_Import.cs
+++ Sci.Production.Warehouse/P26_Import.cs
@@ -307,30 +307,30 @@ and r1.id = '{0}' ", transid, dr_master["stocktype"].ToString(),Sci.Env.User.Key
 
         private void textBox1_Validating(object sender, CancelEventArgs e)
         {
-            string sp = textBox1.Text.TrimEnd();
+//            string sp = textBox1.Text.TrimEnd();
 
-            if (MyUtility.Check.Empty(sp)) return;
+//            if (MyUtility.Check.Empty(sp)) return;
 
-            if (txtSeq1.checkEmpty(showErrMsg: false))
-            {
-                if (!MyUtility.Check.Seek(string.Format("select 1 where exists(select * from po_supp_detail WITH (NOLOCK) where id ='{0}')"
-                    , sp), null))
-                {
-                    MyUtility.Msg.WarningBox("SP# is not found!!");
-                    e.Cancel = true;
-                    return;
-                }
-            }
-            else
-            {
-                if (!MyUtility.Check.Seek(string.Format(@"select 1 where exists(select * from po_supp_detail WITH (NOLOCK) where id ='{0}' 
-                        and seq1 = '{1}' and seq2 = '{2}')", sp, txtSeq1.seq1, txtSeq1.seq2), null))
-                {
-                    MyUtility.Msg.WarningBox("SP#-Seq is not found!!");
-                    e.Cancel = true;
-                    return;
-                }
-            }
+//            if (txtSeq1.checkEmpty(showErrMsg: false))
+//            {
+//                if (!MyUtility.Check.Seek(string.Format("select 1 where exists(select * from po_supp_detail WITH (NOLOCK) where id ='{0}')"
+//                    , sp), null))
+//                {
+//                    MyUtility.Msg.WarningBox("SP# is not found!!");
+//                    e.Cancel = true;
+//                    return;
+//                }
+//            }
+//            else
+//            {
+//                if (!MyUtility.Check.Seek(string.Format(@"select 1 where exists(select * from po_supp_detail WITH (NOLOCK) where id ='{0}' 
+//                        and seq1 = '{1}' and seq2 = '{2}')", sp, txtSeq1.seq1, txtSeq1.seq2), null))
+//                {
+//                    MyUtility.Msg.WarningBox("SP#-Seq is not found!!");
+//                    e.Cancel = true;
+//                    return;
+//                }
+//            }
 
         }
         
