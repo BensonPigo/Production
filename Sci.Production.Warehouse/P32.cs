601ae61f83b3e1ef455de0986f5448202b1c9e98
JIMMY <jimmy@sportscity.com.tw>
2017-03-18 10:31:24 +0000

    Add FactoryID

--- Sci.Production.Warehouse/P32.cs
+++ Sci.Production.Warehouse/P32.cs
@@ -66,6 +66,7 @@ namespace Sci.Production.Warehouse
         {
             base.ClickNewAfter();
             CurrentMaintain["MDivisionID"] = Sci.Env.User.Keyword;
+            CurrentMaintain["FactoryID"] = Sci.Env.User.Factory;
             CurrentMaintain["Status"] = "New";
             CurrentMaintain["Type"] = "B";
             CurrentMaintain["IssueDate"] = DateTime.Now;
