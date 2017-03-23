4fb91ae50a81869ec864bbc4806ac99ca30cc9f3
RICKY <ricky@sportscity.com.tw>
2017-03-23 03:25:20 +0000

    Fix "0003279: [Ehancement] Shipping R13 Factory CMT Forecast" Issue 1.新增Report "Shipping R13 Factory CMT Forecast"。

---
+++ Sci.Production.Shipping/R13.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Sci.Production.Shipping
+{
+    partial class R13
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.label7 = new Sci.Win.UI.Label();
+            this.label8 = new Sci.Win.UI.Label();
+            this.dateRange1 = new Sci.Win.UI.DateRange();
+            this.txtbrand1 = new Sci.Production.Class.txtbrand();
+            this.comboBox2 = new Sci.Win.UI.ComboBox();
+            this.txtdropdownlist1 = new Sci.Production.Class.txtdropdownlist();
+            this.label5 = new Sci.Win.UI.Label();
+            this.comboBox1 = new Sci.Win.UI.ComboBox();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(434, 12);
+            this.print.Visible = false;
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(434, 48);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(434, 84);
+            // 
+            // label1
+            // 
+            this.label1.Lines = 0;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 23);
+            this.label1.TabIndex = 94;
+            this.label1.Text = "Buyer Delivery";
+            // 
+            // label4
+            // 
+            this.label4.Lines = 0;
+            this.label4.Location = new System.Drawing.Point(13, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 23);
+            this.label4.TabIndex = 97;
+            this.label4.Text = "Brand";
+            // 
+            // label7
+            // 
+            this.label7.Lines = 0;
+            this.label7.Location = new System.Drawing.Point(13, 123);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 23);
+            this.label7.TabIndex = 100;
+            this.label7.Text = "Factory";
+            // 
+            // label8
+            // 
+            this.label8.Lines = 0;
+            this.label8.Location = new System.Drawing.Point(13, 159);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 23);
+            this.label8.TabIndex = 101;
+            this.label8.Text = "Category";
+            // 
+            // dateRange1
+            // 
+            this.dateRange1.IsRequired = false;
+            this.dateRange1.Location = new System.Drawing.Point(112, 12);
+            this.dateRange1.Name = "dateRange1";
+            this.dateRange1.Size = new System.Drawing.Size(280, 23);
+            this.dateRange1.TabIndex = 102;
+            // 
+            // txtbrand1
+            // 
+            this.txtbrand1.BackColor = System.Drawing.Color.White;
+            this.txtbrand1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtbrand1.Location = new System.Drawing.Point(112, 48);
+            this.txtbrand1.Name = "txtbrand1";
+            this.txtbrand1.Size = new System.Drawing.Size(84, 23);
+            this.txtbrand1.TabIndex = 105;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.Color.White;
+            this.comboBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.IsSupportUnselect = true;
+            this.comboBox2.Location = new System.Drawing.Point(112, 123);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(74, 24);
+            this.comboBox2.TabIndex = 108;
+            // 
+            // txtdropdownlist1
+            // 
+            this.txtdropdownlist1.BackColor = System.Drawing.Color.White;
+            this.txtdropdownlist1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtdropdownlist1.FormattingEnabled = true;
+            this.txtdropdownlist1.IsSupportUnselect = true;
+            this.txtdropdownlist1.Location = new System.Drawing.Point(112, 159);
+            this.txtdropdownlist1.Name = "txtdropdownlist1";
+            this.txtdropdownlist1.Size = new System.Drawing.Size(121, 24);
+            this.txtdropdownlist1.TabIndex = 111;
+            this.txtdropdownlist1.Type = "Fty_CMT_Forecast";
+            // 
+            // label5
+            // 
+            this.label5.Lines = 0;
+            this.label5.Location = new System.Drawing.Point(13, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 23);
+            this.label5.TabIndex = 98;
+            this.label5.Text = "Shipper";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.White;
+            this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IsSupportUnselect = true;
+            this.comboBox1.Location = new System.Drawing.Point(112, 83);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(74, 24);
+            this.comboBox1.TabIndex = 112;
+            // 
+            // R13
+            // 
+            this.ClientSize = new System.Drawing.Size(526, 225);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtdropdownlist1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.txtbrand1);
+            this.Controls.Add(this.dateRange1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.IsSupportToPrint = false;
+            this.Name = "R13";
+            this.Text = "R13. Factory CMT Forecast";
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.label5, 0);
+            this.Controls.SetChildIndex(this.label7, 0);
+            this.Controls.SetChildIndex(this.label8, 0);
+            this.Controls.SetChildIndex(this.dateRange1, 0);
+            this.Controls.SetChildIndex(this.txtbrand1, 0);
+            this.Controls.SetChildIndex(this.comboBox2, 0);
+            this.Controls.SetChildIndex(this.txtdropdownlist1, 0);
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.comboBox1, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.Label label1;
+        private Win.UI.Label label4;
+        private Win.UI.Label label7;
+        private Win.UI.Label label8;
+        private Win.UI.DateRange dateRange1;
+        private Class.txtbrand txtbrand1;
+        private Win.UI.ComboBox comboBox2;
+        private Class.txtdropdownlist txtdropdownlist1;
+        private Win.UI.Label label5;
+        private Win.UI.ComboBox comboBox1;
+    }
+}
---
+++ Sci.Production.Shipping/R13.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict.Win;
+using Ict;
+using Sci.Data;
+using Sci.Utility.Excel;
+using System.Runtime.InteropServices;
+
+namespace Sci.Production.Shipping
+{
+    public partial class R13 : Sci.Win.Tems.PrintForm
+    {
+        DateTime? buyerDlv1, buyerDlv2;
+        string brand, Shipper, factory,category;
+        DataTable printData;
+        public R13(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();           
+            DataTable ShipperID, factory;
+            DBProxy.Current.Select(null, "select '' as ShipperID union all select ShipperID from FSRCpuCost WITH (NOLOCK) ", out ShipperID);
+            MyUtility.Tool.SetupCombox(comboBox1, 1, ShipperID);
+            comboBox1.Text = Sci.Env.User.Keyword;
+            DBProxy.Current.Select(null, "select '' as ID union all select distinct FtyGroup from Factory WITH (NOLOCK) ", out factory);
+            MyUtility.Tool.SetupCombox(comboBox2, 1, factory);
+            dateRange1.Value1 = DateTime.Today;
+            //comboBox2.SelectedIndex = -1;
+        }
+
+        // 驗證輸入條件
+        protected override bool ValidateInput()
+        {
+            if (MyUtility.Check.Empty(dateRange1.Value1) && MyUtility.Check.Empty(dateRange1.Value2))
+            {
+                MyUtility.Msg.WarningBox("Buyer Delivery can't all empty!!");
+                return false;
+            }
+            if(MyUtility.Check.Empty(txtdropdownlist1.Text))
+            {
+                MyUtility.Msg.WarningBox("Category can't empty!!");
+                return false;
+            }
+            
+            buyerDlv1 = dateRange1.Value1;
+            buyerDlv2 = dateRange1.Value2;
+            brand = txtbrand1.Text;
+            Shipper = comboBox1.Text.ToString().Trim();
+            factory = comboBox2.Text;
+            category = txtdropdownlist1.SelectedValue.ToString();
+            return base.ValidateInput();
+        }
+
+        // 非同步取資料
+        protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            StringBuilder sqlCmd = new StringBuilder();
+            sqlCmd.Append(@"with cte as (
+Select 
+o.BuyerDelivery,o.OrigBuyerDelivery,o.ID
+,Category=IIF(o.Category = 'B', 'Bulk',IIF(o.Category = 'S','Sample','Forecast'))
+,o.CustPONo
+,o.Qty
+,o.CustCDID
+,o.StyleID
+,o.SeasonID
+,o.Dest
+,o.SCIDelivery
+,o.BrandID
+,o.FactoryID
+,fd.ShipperID
+,o.CPU
+,CPUCost= (select top 1 fcd.CpuCost from dbo.FSRCpuCost_Detail fcd where fcd.ShipperID=fd.ShipperID and o.OrigBuyerDelivery between fd.BeginDate and fd.EndDate and o.OrigBuyerDelivery between fcd.BeginDate and fcd.EndDate)
+,SubPSCost=(Select sum(ot.Price) 
+					from Order_TmsCost ot
+					inner join ArtworkType a on ot.ArtworkTypeID = a.ID
+					where ot.ID = o.ID and (a.Classify = 'A' or ( a.Classify = 'I' and a.IsTtlTMS = 0)))
+,LocalPSCost= IIF ((select LocalCMT from dbo.Factory where Factory.ID = o.FactoryID) = 1, 
+						(select sum(ot.Price) 
+						from Order_TmsCost ot
+						inner join ArtworkType a on ot.ArtworkTypeID = a.ID
+						where ot.ID = o.id and a.Classify = 'P')
+					,0)
+
+From Orders o
+Left join FtyShipper_Detail fd on o.BrandID = fd.BrandID and fd.FactoryID = o.FactoryID and o.OrigBuyerDelivery between fd.BeginDate and fd.EndDate
+Where o.LocalOrder = 0 ");
+
+            if (!MyUtility.Check.Empty(buyerDlv1))
+            {
+                sqlCmd.Append(string.Format(" and o.BuyerDelivery >= '{0}'", Convert.ToDateTime(buyerDlv1).ToString("d")));
+            }
+
+            if (!MyUtility.Check.Empty(buyerDlv2))
+            {
+                sqlCmd.Append(string.Format(" and o.BuyerDelivery <= '{0}'", Convert.ToDateTime(buyerDlv2).ToString("d")));
+            }
+            if (!MyUtility.Check.Empty(brand))
+            {
+                sqlCmd.Append(string.Format(" and o.BrandID = '{0}'", brand));
+            }
+
+            if (!MyUtility.Check.Empty(Shipper))
+            {
+                sqlCmd.Append(string.Format(" and fd.ShipperID = '{0}'", Shipper));
+            }
+
+            if (!MyUtility.Check.Empty(factory))
+            {
+                sqlCmd.Append(string.Format(" and o.FtyGroup = '{0}'", factory));
+            }
+
+            if (category == "B")
+            {
+                sqlCmd.Append(" and o.Category = 'B'");
+            }
+            else if (category == "S")
+            {
+                sqlCmd.Append(" and o.Category = 'S'");
+            }
+            else if (category == "BS")
+            {
+                sqlCmd.Append(" and (o.Category = 'B' or o.Category = 'S')");
+            }
+            else if (category == "BF")
+            {
+                sqlCmd.Append(" and (o.Category = 'B' or o.IsForecast = 1)");
+            }
+            else if (category == "SF")
+            {
+                sqlCmd.Append(" and (o.Category = 'S' or o.IsForecast = 1)");
+            }
+            else if (category == "BSF")
+            {
+                sqlCmd.Append(" and (o.Category = 'B' or o.Category = 'S' or o.IsForecast = 1)");
+            }
+
+            sqlCmd.Append(@" ) 
+                            select *
+                            ,FtyCMPCostUnit=ROUND(cte.CPU * cte.CPUCost + cte.SubPSCost + cte.LocalPSCost, 2)
+                            ,TotalCMPDeclaredtoCustomer=cte.Qty*ROUND(cte.CPU * cte.CPUCost + cte.SubPSCost + cte.LocalPSCost, 2)
+                            from cte");
+
+            DualResult result = DBProxy.Current.Select(null, sqlCmd.ToString(), out printData);
+            if (!result)
+            {
+                DualResult failResult = new DualResult(false, "Query data fail\r\n" + result.ToString());
+                return failResult;
+            }
+            return Result.True;
+        }
+
+        // 產生Excel
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            // 顯示筆數於PrintForm上Count欄位
+            SetCount(printData.Rows.Count);
+
+            if (printData.Rows.Count <= 0)
+            {
+                MyUtility.Msg.WarningBox("Data not found!");
+                return false;
+            }
+
+            this.ShowWaitMessage("Starting EXCEL...");
+            Microsoft.Office.Interop.Excel.Application objApp = MyUtility.Excel.ConnectExcel(Sci.Env.Cfg.XltPathDir + "\\Shipping_R13_FactoryCMTForecast.xltx"); //預先開啟excel app
+            MyUtility.Excel.CopyToXls(printData, "", "Shipping_R13_FactoryCMTForecast.xltx", 3, true, null, objApp);
+            Microsoft.Office.Interop.Excel.Worksheet objSheets = objApp.ActiveWorkbook.Worksheets[1];
+            string Buyer_Delivery=" ~ ";
+            if (!MyUtility.Check.Empty(buyerDlv1)) Buyer_Delivery = buyerDlv1.Value.ToShortDateString() + Buyer_Delivery;
+            if (!MyUtility.Check.Empty(buyerDlv2)) Buyer_Delivery = Buyer_Delivery + buyerDlv2.Value.ToShortDateString();
+            objSheets.Cells[2, 2] = MyUtility.Convert.GetString(Buyer_Delivery);
+            objSheets.Cells[2, 5] = MyUtility.Convert.GetString(brand);
+            objSheets.Cells[2, 7] = MyUtility.Convert.GetString(Shipper);
+            objSheets.Cells[2, 9] = MyUtility.Convert.GetString(factory);
+            objSheets.Cells[2, 11] = MyUtility.Convert.GetString(txtdropdownlist1.Text.ToString().Replace(category+"-",""));
+            if (objSheets != null) Marshal.FinalReleaseComObject(objSheets);    
+            if (objApp != null) Marshal.FinalReleaseComObject(objApp);
+            this.HideWaitMessage();
+            return true;
+        }
+    }
+}
