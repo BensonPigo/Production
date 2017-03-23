2f673f716c7991b04bd9066de3d6f0c668d56f18
WILLY <willy@sportscity.com.tw>
2017-03-23 02:30:05 +0000

    刪除 Update by hand 字樣 修改Auto Run pls use "( _DailyDataTransfer.bat)"

--- Production.Daily/Main.Designer.cs
+++ Production.Daily/Main.Designer.cs
@@ -61,7 +61,6 @@
             this.textMailserver = new Sci.Win.UI.TextBox();
             this.labelMailserver = new Sci.Win.UI.Label();
             this.panelPath = new Sci.Win.UI.Panel();
-            this.label1 = new System.Windows.Forms.Label();
             this.chk_import = new System.Windows.Forms.CheckBox();
             this.chk_export = new System.Windows.Forms.CheckBox();
             this.btnGetExportDataPath = new Sci.Win.UI.Button();
@@ -238,11 +237,11 @@
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.Lines = 0;
-            this.label4.Location = new System.Drawing.Point(689, 120);
+            this.label4.Location = new System.Drawing.Point(671, 120);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(103, 21);
+            this.label4.Size = new System.Drawing.Size(152, 21);
             this.label4.TabIndex = 17;
-            this.label4.Text = "( trsdate.exe \"1\")";
+            this.label4.Text = "( _DailyDataTransfer.bat)";
             this.label4.TextStyle.Color = System.Drawing.Color.Red;
             // 
             // checkSendBack
@@ -273,7 +272,7 @@
             // 
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Lines = 0;
-            this.label3.Location = new System.Drawing.Point(383, 120);
+            this.label3.Location = new System.Drawing.Point(377, 120);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(306, 21);
             this.label3.TabIndex = 16;
@@ -421,7 +420,6 @@
             // 
             this.panelPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.panelPath.Controls.Add(this.label1);
             this.panelPath.Controls.Add(this.chk_import);
             this.panelPath.Controls.Add(this.chk_export);
             this.panelPath.Controls.Add(this.btnGetExportDataPath);
@@ -436,16 +434,6 @@
             this.panelPath.Name = "panelPath";
             this.panelPath.Size = new System.Drawing.Size(712, 103);
             this.panelPath.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.Color.Blue;
-            this.label1.Location = new System.Drawing.Point(458, 76);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(251, 15);
-            this.label1.TabIndex = 9;
-            this.label1.Text = "Update by Hand .rar document must be in E:\\";
             // 
             // chk_import
             // 
@@ -703,6 +691,5 @@
         private System.Windows.Forms.ToolStripStatusLabel labelProgress;
         private System.Windows.Forms.CheckBox chk_import;
         private System.Windows.Forms.CheckBox chk_export;
-        private System.Windows.Forms.Label label1;
     }
 }
