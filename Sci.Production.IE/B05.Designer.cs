fedf13023f63c609751abdb6b261e8d75fe2b558
ALGER <alger@sportscity.com.tw>
2017-03-27 09:26:38 +0000

    EnableGridJunkColor設為true。

--- Sci.Production.IE/B05.Designer.cs
+++ Sci.Production.IE/B05.Designer.cs
@@ -272,6 +272,7 @@
             this.DefaultControl = "displayBox1";
             this.DefaultControlForEdit = "displayBox1";
             this.DefaultOrder = "ID";
+            this.EnableGridJunkColor = true;
             this.IsSupportCopy = false;
             this.IsSupportDelete = false;
             this.IsSupportEdit = false;
