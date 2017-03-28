314d250e1a2318b6c9a2e4e8c30c9ff451496f9a
JIMMY <jimmy@sportscity.com.tw>
2017-03-28 09:33:42 +0000

    ADD P04_LocalTransaction

---
+++ Sci.Production.Warehouse/P04_LocalTransaction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sci.Production.Warehouse
+{
+    public partial class P04_LocalTransaction : Sci.Win.Subs.Base
+    {
+        public P04_LocalTransaction()
+        {
+            InitializeComponent();
+        }
+    }
+}
