1b34a142a07f278aff6927145a5eb9b574512811
JIMMY <jimmy@sportscity.com.tw>
2017-03-29 02:55:46 +0000

    ADD R40 Local Item Inventory Report

---
+++ Sci.Production.Warehouse/Report/R40.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sci.Production.Warehouse.Report
+{
+    public partial class R40 : Sci.Win.Tems.PrintForm
+    {
+        public R40(ToolStripMenuItem menuitem) 
+            :base(menuitem)
+        {
+            InitializeComponent();
+        }
+
+        protected override bool ValidateInput()
+        {
+            return base.ValidateInput();
+        }
+
+        protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            return base.OnAsyncDataLoad(e);
+        }
+
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            return base.OnToExcel(report);
+        }
+    }
+}
