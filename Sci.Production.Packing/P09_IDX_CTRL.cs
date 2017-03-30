ace07b73c6aa308f050ba6da7b323a333e4cbba1
JIMMY <jimmy@sportscity.com.tw>
2017-03-30 12:08:57 +0000

    ADD P09_IDX_CTRL

---
+++ Sci.Production.Packing/P09_IDX_CTRL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Sci.Production.Packing
+{
+    class P09_IDX_CTRL
+    {       
+        [DllImport("I:\\MIS\\ERP 2014\\PMS\\Scan & Pack auto conveyer\\Dll\\IDX_CTRL.dll", EntryPoint = "IdxCallVB")]
+        static extern string IdxCallVB(int a, string b, Int32 c);
+        public string test()
+        {
+            return IdxCallVB(1, "8:?", 4);
+        }
+    }
+}
