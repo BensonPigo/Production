5a1b62ee25d3a7915e3f02f1a6b8e0ab0ccae926
JEFF <jeff@sportscity.com.tw>
2017-04-05 07:23:37 +0000

    up

--- Sci.Production.Cutting/R03.cs
+++ Sci.Production.Cutting/R03.cs
@@ -232,6 +232,8 @@ where 1=1
             Microsoft.Office.Interop.Excel.Application objApp = MyUtility.Excel.ConnectExcel(Sci.Env.Cfg.XltPathDir + "\\Cutting_R03_CuttingScheduleListReport.xltx"); //預先開啟excel app
             MyUtility.Excel.CopyToXls(printData, "", "Cutting_R03_CuttingScheduleListReport.xltx", 2, true, null, objApp);// 將datatable copy to excel
             Microsoft.Office.Interop.Excel.Worksheet objSheets = objApp.ActiveWorkbook.Worksheets[1];   // 取得工作表
+            objSheets.get_Range("P2").ColumnWidth = 15.38;
+            objSheets.get_Range("T2").ColumnWidth = 15.38;
             if (objSheets != null) Marshal.FinalReleaseComObject(objSheets);    //釋放sheet
             if (objApp != null) Marshal.FinalReleaseComObject(objApp);          //釋放objApp
             return true;
