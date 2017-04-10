fd598a27d9770a680ef28e1125cd25a1407f44bd
RICKY <ricky@sportscity.com.tw>
2017-04-10 01:24:35 +0000

    Fix "0003582: WAREHOUSE_P11_Issue Sewing Material by Transfer Guide" Issue 1.SCIRefNo VarChar 宣告長度 (26) → (30)

--- Sci.Production.Warehouse/P11_AutoPick.cs
+++ Sci.Production.Warehouse/P11_AutoPick.cs
@@ -200,7 +200,7 @@ delete from #tmp2 where qty = 0;
 
 	Create Table #Tmp_BoaExpend
 		(  ExpendUkey BigInt Identity(1,1) Not Null, ID Varchar(13), Order_BOAUkey BigInt
-		 , RefNo VarChar(20), SCIRefNo VarChar(26), Article VarChar(8), ColorID VarChar(6), SuppColor NVarChar(Max)
+		 , RefNo VarChar(20), SCIRefNo VarChar(30), Article VarChar(8), ColorID VarChar(6), SuppColor NVarChar(Max)
 		 , SizeCode VarChar(8), SizeSpec VarChar(15), SizeUnit VarChar(8), Remark NVarChar(Max)
 		 , OrderQty Numeric(6,0), Price Numeric(8,4), UsageQty Numeric(9,2), UsageUnit VarChar(8), SysUsageQty  Numeric(9,2)
 		 , BomFactory VarChar(8), BomCountry VarChar(2), BomStyle VarChar(15), BomCustCD VarChar(20)
