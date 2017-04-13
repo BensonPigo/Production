a9210b8c9ea7e97e2c68f8777bae3a0f94e5114a
JIMMY <jimmy@sportscity.com.tw>
2017-04-13 04:16:16 +0000

    ADD P01_Continuity_PrintData.cs

---
+++ Sci.Production.Quality/P01_Continuity_Printdata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sci.Production.Quality
+{
+    class P01_Continuity_PrintData
+    {
+        string FactoryNameEN { set; get; }
+        string FactoryID { set; get; }
+        string StyleID { set; get; }
+        string Color { set; get; }
+        string FabricDesc { set; get; }
+        string FabricSupplier { set; get; }
+        string InvNo { set; get; }
+        string ETA { set; get; }
+    }
+}
