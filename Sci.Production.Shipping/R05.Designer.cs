799d2b759b5ec5a654927090041764a7c9f4e4e3
DYSON <dyson@sportscity.com.tw>
2017-04-18 01:44:34 +0000

    edit IsIncludeJunk = true

--- Sci.Production.Shipping/R05.Designer.cs
+++ Sci.Production.Shipping/R05.Designer.cs
@@ -119,7 +119,7 @@
             // txtsubcon1
             // 
             this.txtsubcon1.DisplayBox1Binding = "";
-            this.txtsubcon1.IsIncludeJunk = false;
+            this.txtsubcon1.IsIncludeJunk = true;
             this.txtsubcon1.Location = new System.Drawing.Point(78, 84);
             this.txtsubcon1.Name = "txtsubcon1";
             this.txtsubcon1.Size = new System.Drawing.Size(170, 23);
