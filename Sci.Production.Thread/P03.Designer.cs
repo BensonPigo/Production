3b5bfceebfffa389a9cb37a9e60152ff701ea5e4
LEO <leo@sportscity.com.tw>
2017-04-19 04:00:13 +0000

    修正預設文字

--- Sci.Production.Thread/P03.Designer.cs
+++ Sci.Production.Thread/P03.Designer.cs
@@ -221,7 +221,7 @@
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(130, 23);
             this.label7.TabIndex = 38;
-            this.label7.Text = "label7";
+            this.label7.Text = "";
             this.label7.TextStyle.Color = System.Drawing.Color.Red;
             // 
             // P03
