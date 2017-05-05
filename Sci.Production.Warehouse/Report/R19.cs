f07a0f355a659573b8b7edec66b2cae4b8fba439
JIMMY <jimmy@sportscity.com.tw>
2017-05-05 05:44:47 +0000

    --- SQL --- Seq 增加判斷 有 Seq1 無 Seq2

--- Sci.Production.Warehouse/Report/R19.cs
+++ Sci.Production.Warehouse/Report/R19.cs
@@ -52,62 +52,102 @@ namespace Sci.Production.Warehouse
             DateTime? returnDate1 = dateEstReturnDate.Value1;
             DateTime? returnDate2 = dateEstReturnDate.Value2;
             String spno = txtBorrowSPNo.Text.TrimEnd();
-            string seq1 = txtSeq.seq1;
-            string seq2 = txtSeq.seq2;
 
             DualResult result = Result.True;
             StringBuilder sqlcmd = new StringBuilder();
             #region sql command
-            sqlcmd.Append(@"with cte 
-as
-(Select a.id, frompoid, fromseq1,FromSeq2 ,FromStockType,sum(qty) qty, issuedate, estbackdate, backdate
-from borrowback a WITH (NOLOCK) inner join borrowback_detail b WITH (NOLOCK) on b.id = a.id 
-Where a.type='A'
-and a.Status = 'Confirmed'");
+            sqlcmd.Append(@"
+with cte as (
+    Select  a.id
+            , frompoid
+            , fromseq1
+            , FromSeq2
+            , FromStockType
+            , sum(qty) qty
+            , issuedate
+            , estbackdate
+            , backdate
+    from borrowback a WITH (NOLOCK) 
+    inner join borrowback_detail b WITH (NOLOCK) on b.id = a.id 
+    Where   a.type='A'
+            and a.Status = 'Confirmed'");
             if (!MyUtility.Check.Empty(returnDate1) || !MyUtility.Check.Empty(returnDate2))
             {
                 if (!MyUtility.Check.Empty(returnDate1))
-                    sqlcmd.Append(string.Format(" and '{0}' <= a.estbackdate", Convert.ToDateTime(returnDate1).ToString("d")));
+                    sqlcmd.Append(string.Format(@" 
+            and '{0}' <= a.estbackdate", Convert.ToDateTime(returnDate1).ToString("d")));
                 if (!MyUtility.Check.Empty(returnDate2))
-                    sqlcmd.Append(string.Format(" and a.estbackdate <= '{0}'", Convert.ToDateTime(returnDate2).ToString("d")));
+                    sqlcmd.Append(string.Format(@" 
+            and a.estbackdate <= '{0}'", Convert.ToDateTime(returnDate2).ToString("d")));
             }
             if (!MyUtility.Check.Empty(spno))
             {
-                sqlcmd.Append(string.Format(" And b.frompoid = '{0}'", spno));
+                sqlcmd.Append(string.Format(@"
+            And b.frompoid = '{0}'", spno));
             }
-            if (!txtSeq.checkEmpty(showErrMsg: false))
+
+            if (!txtSeq.checkSeq1Empty())
             {
-                sqlcmd.Append(string.Format(" And b.fromseq1 = '{0}' and b.FromSeq2 ='{1}'", seq1, seq2));
+                sqlcmd.Append(string.Format(@"
+            and b.fromSeq1 = '{0}'", txtSeq.seq1));
+            }
+            if (!txtSeq.checkSeq2Empty())
+            {
+                sqlcmd.Append(string.Format(@" 
+            and b.FromSeq2 = '{0}'", txtSeq.seq2));
             }
 
             switch (selectindex)
             {
                 case 0:
-                    sqlcmd.Append(@" And a.BackDate is null ");
+                    sqlcmd.Append(@" 
+            And a.BackDate is null ");
                     break;
                 case 1:
-                    sqlcmd.Append(@" And a.BackDate is not null ");
+                    sqlcmd.Append(@" 
+            And a.BackDate is not null ");
                     break;
                 case 2:
                     break;
             }
 
-            sqlcmd.Append(string.Format(@" Group by a.id, frompoid, FromSeq1,FromSeq2 
-,FromStockType
-, a.IssueDate, estbackdate, backdate
+            sqlcmd.Append(string.Format(@" 
+    Group by a.id, frompoid, FromSeq1, FromSeq2, FromStockType
+             , a.IssueDate, estbackdate, backdate
 )
-select cte.id,cte.FromPOID,cte.FromSeq1,cte.FromSeq2
-,case cte.FromStockType when 'B' then 'Bulk' when 'I' then 'Inventory' else  cte.FromStockType end as stocktype
-, cte.qty 
-,isnull((select sum(qty) from BorrowBack x WITH (NOLOCK) inner join BorrowBack_Detail y WITH (NOLOCK) on y.id = x.id 
-where x.Status = 'Confirmed' and x.type='B' and y.ToPOID = cte.FromPOID and y.ToSeq1 = cte.FromSeq1
-and y.ToSeq2 = cte.FromSeq2 and y.ToStockType = cte.FromStockType),0.00) as backQty
-, cte.qty - isnull((select sum(qty) from BorrowBack x WITH (NOLOCK) inner join BorrowBack_Detail y WITH (NOLOCK) on y.id = x.id 
-where x.Status = 'Confirmed' and x.type='B' and y.ToPOID = cte.FromPOID and y.ToSeq1 = cte.FromSeq1
-and y.ToSeq2 = cte.FromSeq2 and y.ToStockType = cte.FromStockType),0.00) as balance
-,cte.IssueDate
-,cte.EstBackDate
-,cte.BackDate
+select  cte.id
+        , cte.FromPOID
+        , cte.FromSeq1
+        , cte.FromSeq2
+        , stocktype = case cte.FromStockType 
+                        when 'B' then 'Bulk' 
+                        when 'I' then 'Inventory' 
+                        else  cte.FromStockType 
+                      end 
+        , cte.qty 
+        , backQty = isnull((select sum(qty) 
+                            from BorrowBack x WITH (NOLOCK) 
+                            inner join BorrowBack_Detail y WITH (NOLOCK) on y.id = x.id 
+                            where   x.Status = 'Confirmed' 
+                                    and x.type='B' 
+                                    and y.ToPOID = cte.FromPOID 
+                                    and y.ToSeq1 = cte.FromSeq1
+                                    and y.ToSeq2 = cte.FromSeq2 
+                                    and y.ToStockType = cte.FromStockType
+                            ),0.00) 
+        , balance = cte.qty - isnull((  select sum(qty) 
+                                        from BorrowBack x WITH (NOLOCK) 
+                                        inner join BorrowBack_Detail y WITH (NOLOCK) on y.id = x.id 
+                                        where   x.Status = 'Confirmed' 
+                                                and x.type='B' 
+                                                and y.ToPOID = cte.FromPOID 
+                                                and y.ToSeq1 = cte.FromSeq1
+                                                and y.ToSeq2 = cte.FromSeq2 
+                                                and y.ToStockType = cte.FromStockType)
+                                    ,0.00)
+        , cte.IssueDate
+        , cte.EstBackDate
+        , cte.BackDate
 from cte"));
 
             #endregion
