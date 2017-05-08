6b1e0cc8b9cee7046742f880d8cebf370cb29a34
JIMMY <jimmy@sportscity.com.tw>
2017-05-08 02:55:01 +0000

    路徑測試 2 此方法 1. 找執行檔的目錄 2. 找 System 的目錄 3. 找 Path 環境變數的目錄

--- Sci.Production.Packing/P09_IDX_CTRL.cs
+++ Sci.Production.Packing/P09_IDX_CTRL.cs
@@ -9,10 +9,41 @@ using System.Configuration;
 
 namespace Sci.Production.Packing
 {
+    public class DllInvoke
+    {
+        [DllImport("Kernel32.dll")]
+        private extern static IntPtr LoadLibrary(String path);
+
+        [DllImport("Kernel32.dll")]
+        private extern static IntPtr GetProcAddress(IntPtr lib, String funcName);
+
+        [DllImport("Kernel32.dll")]
+        private extern static bool FreeLibrary(IntPtr lib);
+
+        private IntPtr hLib;
+        public DllInvoke(String DLLPath)
+        {
+            hLib = LoadLibrary(DLLPath);
+        }
+
+        ~DllInvoke()
+        {
+            FreeLibrary(hLib);
+        }
+
+        public Delegate Invoke(String APIName, Type t)
+        {
+            IntPtr api = GetProcAddress(hLib, APIName);
+            
+            return (Delegate)Marshal.GetDelegateForFunctionPointer(api, t);
+        }
+    }
+
     class P09_IDX_CTRL
     {
+#if false
         static string x = Sci.Env.Cfg.XltPathDir;//System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
-        [DllImport("\\\\PHL-NEWPMS\\System2017$\\Production\\PH1_Dummy\\Production_201705030001\\IDX_CTRL\\IDX_CTRL.dll", EntryPoint = "IdxCallVB")]
+        [DllImport("IDX_CTRL.dll", EntryPoint = "IdxCallVB")]
         static extern void IdxCallVB(int a, string b, Int32 c);
 
         [HandleProcessCorruptedStateExceptions]
@@ -26,6 +57,16 @@ namespace Sci.Production.Packing
             {
                 MyUtility.Msg.InfoBox(e.Message);
             }
+        }
+#endif
+        private delegate int IdxCallVB_func(int command, string Request, int RequestSize);
+
+        public void IdxCall(int command, string Request, int RequestSize)
+        {
+            DllInvoke dll = new DllInvoke(".\\IDX_CTRL.dll");
+            IdxCallVB_func func = (IdxCallVB_func)dll.Invoke("IdxCallVB", typeof(IdxCallVB_func));
+
+            func(command, Request, RequestSize);
         }
     }
 }
