8a6b00cf341eb0ddecb1cddb532bfcdcd749b9fd
WENNY <wenny@sportscity.com.tw>
2017-05-08 06:17:20 +0000

    修改屬性 DefaultControl & DefaultControlForEdit 元件名稱

--- Sci.Production.Tools/PasswordByUser.Designer.cs
+++ Sci.Production.Tools/PasswordByUser.Designer.cs
@@ -70,20 +70,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
-            // detail
-            // 
-            this.detail.Size = new System.Drawing.Size(993, 515);
-            // 
             // detailcont
             // 
             this.detailcont.Controls.Add(this.grid1);
             this.detailcont.Controls.Add(this.radioGroup1);
-            this.detailcont.Size = new System.Drawing.Size(993, 477);
-            // 
-            // detailbtm
-            // 
-            this.detailbtm.Location = new System.Drawing.Point(0, 477);
-            this.detailbtm.Size = new System.Drawing.Size(993, 38);
             // 
             // browse
             // 
@@ -139,7 +129,7 @@
             this.radioGroup1.Controls.Add(this.labelID);
             this.radioGroup1.Location = new System.Drawing.Point(10, 3);
             this.radioGroup1.Name = "radioGroup1";
-            this.radioGroup1.Size = new System.Drawing.Size(463, 598);
+            this.radioGroup1.Size = new System.Drawing.Size(463, 478);
             this.radioGroup1.TabIndex = 3;
             this.radioGroup1.TabStop = false;
             // 
@@ -431,14 +421,14 @@
             this.grid1.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
             this.grid1.RowTemplate.Height = 24;
             this.grid1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.grid1.Size = new System.Drawing.Size(514, 469);
+            this.grid1.Size = new System.Drawing.Size(418, 349);
             this.grid1.TabIndex = 15;
             this.grid1.TabStop = false;
             // 
             // PasswordByUser
             // 
             this.ClientSize = new System.Drawing.Size(1001, 577);
-            this.DefaultControl = "textBox8";
+            this.DefaultControl = "txtIDStart";
             this.GridAlias = "Pass2";
             this.IsSupportCopy = false;
             this.IsSupportPrint = false;
