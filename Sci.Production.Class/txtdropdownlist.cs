094321045bcab8e175a980d00e0b0a2ba26bec58
LEO <leo@sportscity.com.tw>
2017-05-08 01:58:44 +0000

    0004322: Planning -> R11. Prod. Efficiency record by Style Type屬性設定為Category_Report

--- Sci.Production.Class/txtdropdownlist.cs
+++ Sci.Production.Class/txtdropdownlist.cs
@@ -14,12 +14,12 @@ namespace Sci.Production.Class
     public partial class txtdropdownlist : Sci.Win.UI.ComboBox
     {
         private string type;
-        [Category("Custom Properties")]
+        //[Category("Custom Properties")]
         public string Type
         {
             set
             {
-                this.type = value;
+                this.type = "Category_Report";
                 if (!Env.DesignTime)
                 {
                     string selectCommand = string.Format("select ID, rtrim(ID)+'- '+rtrim(Name) as IDName from DropDownList WITH (NOLOCK) where Type = '{0}' order by Seq", this.Type);
