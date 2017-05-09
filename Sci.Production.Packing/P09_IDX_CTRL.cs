afac0ab4fc85731e2a571bfdf42b0b4e16cf4e9b
JIMMY <jimmy@sportscity.com.tw>
2017-05-09 03:19:58 +0000

    update

--- Sci.Production.Packing/P09_IDX_CTRL.cs
+++ Sci.Production.Packing/P09_IDX_CTRL.cs
@@ -34,7 +34,6 @@ namespace Sci.Production.Packing
         public Delegate Invoke(String APIName, Type t)
         {
             IntPtr api = GetProcAddress(hLib, APIName);
-            
             return (Delegate)Marshal.GetDelegateForFunctionPointer(api, t);
         }
     }
@@ -42,17 +41,16 @@ namespace Sci.Production.Packing
     class P09_IDX_CTRL
     {
         private delegate int IdxCallVB_func(int command, string Request, int RequestSize);
+        DllInvoke dll;
+        IdxCallVB_func func;
 
+        public P09_IDX_CTRL()
+        {
+            dll = new DllInvoke(".\\IDX_CTRL.dll");
+            func = (IdxCallVB_func)dll.Invoke("IdxCallVB", typeof(IdxCallVB_func));
+        }
         public void IdxCall(int command, string Request, int RequestSize)
         {
-            DllInvoke dll;
-           
-            if (Environment.Is64BitOperatingSystem)
-                dll = new DllInvoke(".\\IDX_CTRL.dll");
-            else
-                dll = new DllInvoke(".\\IDX_CTRL.dll");
-
-            IdxCallVB_func func = (IdxCallVB_func)dll.Invoke("IdxCallVB", typeof(IdxCallVB_func));
             func(command, Request, RequestSize);
         }
     }
