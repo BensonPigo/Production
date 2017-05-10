e4ce1b40e94fee82f293d5bc4d671d8fe80d50f4
JIMMY <jimmy@sportscity.com.tw>
2017-05-10 05:58:05 +0000

    P09_IDX_CTRL 宣告移至 Start to Scan

--- Sci.Production.Packing/P09.cs
+++ Sci.Production.Packing/P09.cs
@@ -12,8 +12,7 @@ using Sci.Data;
 namespace Sci.Production.Packing
 {
     public partial class P09 : Sci.Win.Tems.QueryForm
-    {
-        P09_IDX_CTRL IDX = new P09_IDX_CTRL();
+    {        
         protected bool canUnConfirm;
         protected DataTable PackID;
         public P09(ToolStripMenuItem menuitem)
@@ -322,6 +321,7 @@ order by ID,Seq", txtSP.Text);
                 MyUtility.Msg.WarningBox("This carton had been scanned, so can't scan again!!");
                 return;
             }
+            P09_IDX_CTRL IDX = new P09_IDX_CTRL();
             IDX.IdxCall(1, "8:?", 4);
             Sci.Production.Packing.P09_StartToScan callNextForm = new Sci.Production.Packing.P09_StartToScan(dr);
             DialogResult result = callNextForm.ShowDialog(this);
