97b823c191140d05f58823e819f095fd4c27f5ca
JEFF <jeff@sportscity.com.tw>
2017-05-15 01:43:21 +0000

    0004529: CUTTING_P11 orderid欄位拉長一點 1.按[QUERY]，會開窗，將其[orderid]欄位拉長一點，讓資料不要有...，[20170513_002.jpg]。 2.開窗不用那麼寬，目前寬度的一半即可。 3.下方TEXTBOX不用那麼寬，目前寬度的一半即可。 4.按[Batch Create]，執行成功後，會跳出警告視窗，怪怪的?? 請改成跳出成功視窗，[20170513_005.jpg]。

--- Sci.Production.Cutting/P11.cs
+++ Sci.Production.Cutting/P11.cs
@@ -570,7 +570,10 @@ inner join tmp b on  b.sizecode = a.sizecode and b.Ukey = c.Ukey");
             }
             if (ExcessTb.Rows.Count > 0)
             {
-                MyUtility.Msg.ShowMsgGrid(ExcessTb, "Those detail had <EXCESS> not yet distribute to SP#", "Warning");
+                var m = MyUtility.Msg.ShowMsgGrid(ExcessTb, "Those detail had <EXCESS> not yet distribute to SP#", "Warning");                
+                m.Width = 500;
+                m.grid1.Columns[1].Width = 140;
+                m.text_Find.Width = 140;
             }
             #endregion
             #region GarmentList Table
@@ -1390,7 +1393,7 @@ inner join tmp b on  b.sizecode = a.sizecode and b.Ukey = c.Ukey");
                     }
                     _transactionscope.Complete();
                     _transactionscope.Dispose();
-                    MyUtility.Msg.WarningBox("Successfully");
+                    MyUtility.Msg.InfoBox("Successfully");
                 }
                 catch (Exception ex)
                 {
