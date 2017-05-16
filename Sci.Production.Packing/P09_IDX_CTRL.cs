15d6e1f632bc626b5205451c3e652f4203f16eb0
JIMMY <jimmy@sportscity.com.tw>
2017-05-16 08:10:48 +0000

    Revision 6858

--- Sci.Production.Packing/P09_IDX_CTRL.cs
+++ Sci.Production.Packing/P09_IDX_CTRL.cs
@@ -5,25 +5,53 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using System.Runtime.ExceptionServices;
+using System.Configuration;
 
 namespace Sci.Production.Packing
 {
+    public class DllInvoke
+    {
+        [DllImport("Kernel32.dll")]
+        private extern static IntPtr LoadLibrary(String path);
+
+        [DllImport("Kernel32.dll")]
+        private extern static IntPtr GetProcAddress(IntPtr lib, String funcName);
+
+        [DllImport("Kernel32.dll")]
+        private extern static bool FreeLibrary(IntPtr lib);
+
+        private IntPtr hLib;
+        public DllInvoke(String DLLPath)
+        {
+            hLib = LoadLibrary(DLLPath);
+        }
+
+        ~DllInvoke()
+        {
+            FreeLibrary(hLib);
+        }
+
+        public Delegate Invoke(String APIName, Type t)
+        {
+            IntPtr api = GetProcAddress(hLib, APIName);
+            return (Delegate)Marshal.GetDelegateForFunctionPointer(api, t);
+        }
+    }
+
     class P09_IDX_CTRL
     {
-        [DllImport("C:\\windows\\system32\\IDX_CTRL.dll", EntryPoint = "IdxCallVB")]
-        static extern void IdxCallVB(int a, string b, Int32 c);
+        private delegate int IdxCallVB_func(int command, string Request, int RequestSize);
+        DllInvoke dll;
+        IdxCallVB_func func;
 
-        [HandleProcessCorruptedStateExceptions]
-        public void IdxCall(int Command, string Request, Int32 RequestSize)
+        public P09_IDX_CTRL()
         {
-            try
-            {
-                IdxCallVB(Command, Request, RequestSize);
-            }
-            catch (AccessViolationException e)
-            {
-                MyUtility.Msg.InfoBox(e.Message);
-            }
+            dll = new DllInvoke(".\\IDX_CTRL.dll");
+            func = (IdxCallVB_func)dll.Invoke("IdxCallVB", typeof(IdxCallVB_func));
+        }
+        public void IdxCall(int command, string Request, int RequestSize)
+        {
+            func(command, Request, RequestSize);
         }
     }
 }
