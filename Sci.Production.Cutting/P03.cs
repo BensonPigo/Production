1944faa11ac06f88ef51befd935b7af69c302091
JEFF <jeff@sportscity.com.tw>
2017-05-18 04:20:06 +0000

    fix .Text

--- Sci.Production.Cutting/P03.cs
+++ Sci.Production.Cutting/P03.cs
@@ -138,7 +138,7 @@ From
             if (!MyUtility.Check.Empty(seq)) where = where + string.Format(" and Seq1+SEQ2='{0}'", seq);
             if (!MyUtility.Check.Empty(dateEstCutDate.Value)) where = where + string.Format("and estcutdate='{0}'",estcutdate);
             if (!MyUtility.Check.Empty(cutref)) where = where + string.Format(" and cutref='{0}'", cutref);
-            if (!MyUtility.Check.Empty(txtfactoryByM)) where = where + string.Format(" and a.Factoryid='{0}'", txtfactoryByM);
+            if (!MyUtility.Check.Empty(txtfactoryByM.Text)) where = where + string.Format(" and a.Factoryid='{0}'", txtfactoryByM);
 
             sql = sql + where + " ) as #tmp ";
             where = " Where 1=1 and actcutdate =''";
