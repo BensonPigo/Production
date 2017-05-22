80779f2186524adcd88482628b9abc7bfc64cbb7
JIMMY <jimmy@sportscity.com.tw>
2017-05-22 02:43:36 +0000

    Fix Query 單字錯誤

--- Sci.Production.Warehouse/P07.cs
+++ Sci.Production.Warehouse/P07.cs
@@ -397,7 +397,7 @@ from dbo.Export_Detail e WITH (NOLOCK)
 left join dbo.PO_Supp_Detail p WITH (NOLOCK) on e.PoID = p.ID and e.Seq1 = p.SEQ1 and e.Seq2 = p.seq2
 INNER JOIN MDivisionPoDetail M WITH (NOLOCK) ON E.PoID = M.POID and e.Seq1 = M.SEQ1 and e.Seq2 = M.seq2 
 where e.PoID ='{0}' and e.id = '{1}'
-Ordery By e.Seq1, e.Seq2, e.Refno", CurrentDetailData["poid"], CurrentMaintain["exportid"]);
+Order By e.Seq1, e.Seq2, e.Refno", CurrentDetailData["poid"], CurrentMaintain["exportid"]);
 
                         DBProxy.Current.Select(null, sqlcmd, out poitems);
 
