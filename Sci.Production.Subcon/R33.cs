3e1ad4b0963148c5c30715b2c92c88e093cd7ce9
JIMMY <jimmy@sportscity.com.tw>
2017-05-24 03:42:00 +0000

    update

--- Sci.Production.Subcon/R33.cs
+++ Sci.Production.Subcon/R33.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Ict;
+using Sci.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,11 +14,49 @@ namespace Sci.Production.Subcon
 {
     public partial class R33 : Sci.Win.Tems.PrintForm
     {
+        string Factory, SewingStart, SewingEnd, SP;
         public R33(ToolStripMenuItem menuitem)
             : base(menuitem)
         {
             InitializeComponent();
             this.print.Visible = false;
+            
+            //set ComboFactory
+            DataTable dtFactory;
+            DBProxy.Current.Select(null, "Select ID from Factory", out dtFactory);
+            MyUtility.Tool.SetupCombox(comboFactory, 1, dtFactory);
+            comboFactory.Text = Sci.Env.User.Factory;
+        }
+
+        protected override bool ValidateInput()
+        {
+            #region set Data
+            Factory = comboFactory.Text;
+            SewingStart = ((DateTime)dateSewingDate.Value1).ToString("yyyy-MM-dd");
+            SewingEnd = ((DateTime)dateSewingDate.Value2).ToString("yyyy-MM-dd");
+            SP = txtSP.Text;
+            #endregion 
+
+            #region CheckSewingDate
+            if (SewingStart.Empty() && SewingEnd.Empty())
+            {
+                MyUtility.Msg.InfoBox("Sewing Date can't be empty.");
+                return false;
+            }
+            #endregion
+            return true;
+        }
+
+        protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+
+            return Result.True;
+        }
+
+        protected override bool ToExcel()
+        {
+            base.ToExcel();
+            return true;
         }
     }
 }
