680af1ec6e6271fffe94f3709c19f42f5dcc0f42
JIMMY <jimmy@sportscity.com.tw>
2017-06-01 04:29:47 +0000

    Update SQL & SetExcel

--- Sci.Production.Subcon/R33.Designer.cs
+++ Sci.Production.Subcon/R33.Designer.cs
@@ -29,29 +29,32 @@
         private void InitializeComponent()
         {
             this.radioPanel1 = new Sci.Win.UI.RadioPanel();
+            this.txtSP = new Sci.Win.UI.TextBox();
+            this.comboFactory = new Sci.Win.UI.ComboBox();
+            this.dateSewingDate = new Sci.Win.UI.DateRange();
+            this.labelFactory = new Sci.Win.UI.Label();
+            this.labelSP = new Sci.Win.UI.Label();
+            this.labelReportType = new Sci.Win.UI.Label();
+            this.labelSewingDate = new Sci.Win.UI.Label();
             this.radioBySP = new Sci.Win.UI.RadioButton();
             this.radioByFactory = new Sci.Win.UI.RadioButton();
-            this.labelSewingDate = new Sci.Win.UI.Label();
-            this.labelFactory = new Sci.Win.UI.Label();
-            this.labelReportType = new Sci.Win.UI.Label();
-            this.labelSP = new Sci.Win.UI.Label();
-            this.dateSewingDate = new Sci.Win.UI.DateRange();
-            this.comboFactory = new Sci.Win.UI.ComboBox();
-            this.txtSP = new Sci.Win.UI.TextBox();
             this.radioPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // print
             // 
             this.print.Location = new System.Drawing.Point(419, 12);
+            this.print.TabIndex = 1;
             // 
             // toexcel
             // 
             this.toexcel.Location = new System.Drawing.Point(419, 48);
+            this.toexcel.TabIndex = 2;
             // 
             // close
             // 
             this.close.Location = new System.Drawing.Point(419, 84);
+            this.close.TabIndex = 3;
             // 
             // radioPanel1
             // 
@@ -67,8 +70,68 @@
             this.radioPanel1.Location = new System.Drawing.Point(12, 12);
             this.radioPanel1.Name = "radioPanel1";
             this.radioPanel1.Size = new System.Drawing.Size(398, 165);
-            this.radioPanel1.TabIndex = 95;
+            this.radioPanel1.TabIndex = 0;
             this.radioPanel1.Value = "P/L Rcv Report";
+            // 
+            // txtSP
+            // 
+            this.txtSP.BackColor = System.Drawing.Color.White;
+            this.txtSP.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtSP.Location = new System.Drawing.Point(105, 130);
+            this.txtSP.Name = "txtSP";
+            this.txtSP.Size = new System.Drawing.Size(121, 23);
+            this.txtSP.TabIndex = 4;
+            // 
+            // comboFactory
+            // 
+            this.comboFactory.BackColor = System.Drawing.Color.White;
+            this.comboFactory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboFactory.FormattingEnabled = true;
+            this.comboFactory.IsSupportUnselect = true;
+            this.comboFactory.Location = new System.Drawing.Point(105, 7);
+            this.comboFactory.Name = "comboFactory";
+            this.comboFactory.Size = new System.Drawing.Size(121, 24);
+            this.comboFactory.TabIndex = 0;
+            // 
+            // dateSewingDate
+            // 
+            this.dateSewingDate.IsRequired = false;
+            this.dateSewingDate.Location = new System.Drawing.Point(105, 40);
+            this.dateSewingDate.Name = "dateSewingDate";
+            this.dateSewingDate.Size = new System.Drawing.Size(280, 23);
+            this.dateSewingDate.TabIndex = 1;
+            // 
+            // labelFactory
+            // 
+            this.labelFactory.Location = new System.Drawing.Point(8, 7);
+            this.labelFactory.Name = "labelFactory";
+            this.labelFactory.Size = new System.Drawing.Size(94, 23);
+            this.labelFactory.TabIndex = 11;
+            this.labelFactory.Text = "Factory";
+            // 
+            // labelSP
+            // 
+            this.labelSP.Location = new System.Drawing.Point(8, 130);
+            this.labelSP.Name = "labelSP";
+            this.labelSP.Size = new System.Drawing.Size(94, 23);
+            this.labelSP.TabIndex = 11;
+            this.labelSP.Text = "SP#";
+            // 
+            // labelReportType
+            // 
+            this.labelReportType.Location = new System.Drawing.Point(8, 72);
+            this.labelReportType.Name = "labelReportType";
+            this.labelReportType.Size = new System.Drawing.Size(94, 23);
+            this.labelReportType.TabIndex = 11;
+            this.labelReportType.Text = "Report Type";
+            // 
+            // labelSewingDate
+            // 
+            this.labelSewingDate.Location = new System.Drawing.Point(8, 40);
+            this.labelSewingDate.Name = "labelSewingDate";
+            this.labelSewingDate.Size = new System.Drawing.Size(94, 23);
+            this.labelSewingDate.TabIndex = 11;
+            this.labelSewingDate.Text = "Sewing Date";
             // 
             // radioBySP
             // 
@@ -77,7 +140,7 @@
             this.radioBySP.Location = new System.Drawing.Point(105, 99);
             this.radioBySP.Name = "radioBySP";
             this.radioBySP.Size = new System.Drawing.Size(72, 21);
-            this.radioBySP.TabIndex = 1;
+            this.radioBySP.TabIndex = 3;
             this.radioBySP.TabStop = true;
             this.radioBySP.Text = "By SP#";
             this.radioBySP.UseVisualStyleBackColor = true;
@@ -91,71 +154,11 @@
             this.radioByFactory.Location = new System.Drawing.Point(105, 72);
             this.radioByFactory.Name = "radioByFactory";
             this.radioByFactory.Size = new System.Drawing.Size(93, 21);
-            this.radioByFactory.TabIndex = 0;
+            this.radioByFactory.TabIndex = 2;
             this.radioByFactory.TabStop = true;
             this.radioByFactory.Text = "By Factory";
             this.radioByFactory.UseVisualStyleBackColor = true;
             this.radioByFactory.Value = "P/L Rcv Report";
-            // 
-            // labelSewingDate
-            // 
-            this.labelSewingDate.Location = new System.Drawing.Point(8, 40);
-            this.labelSewingDate.Name = "labelSewingDate";
-            this.labelSewingDate.Size = new System.Drawing.Size(94, 23);
-            this.labelSewingDate.TabIndex = 11;
-            this.labelSewingDate.Text = "Sewing Date";
-            // 
-            // labelFactory
-            // 
-            this.labelFactory.Location = new System.Drawing.Point(8, 7);
-            this.labelFactory.Name = "labelFactory";
-            this.labelFactory.Size = new System.Drawing.Size(94, 23);
-            this.labelFactory.TabIndex = 11;
-            this.labelFactory.Text = "Factory";
-            // 
-            // labelReportType
-            // 
-            this.labelReportType.Location = new System.Drawing.Point(8, 72);
-            this.labelReportType.Name = "labelReportType";
-            this.labelReportType.Size = new System.Drawing.Size(94, 23);
-            this.labelReportType.TabIndex = 11;
-            this.labelReportType.Text = "Report Type";
-            // 
-            // labelSP
-            // 
-            this.labelSP.Location = new System.Drawing.Point(8, 130);
-            this.labelSP.Name = "labelSP";
-            this.labelSP.Size = new System.Drawing.Size(94, 23);
-            this.labelSP.TabIndex = 11;
-            this.labelSP.Text = "SP#";
-            // 
-            // dateSewingDate
-            // 
-            this.dateSewingDate.IsRequired = false;
-            this.dateSewingDate.Location = new System.Drawing.Point(105, 40);
-            this.dateSewingDate.Name = "dateSewingDate";
-            this.dateSewingDate.Size = new System.Drawing.Size(280, 23);
-            this.dateSewingDate.TabIndex = 12;
-            // 
-            // comboFactory
-            // 
-            this.comboFactory.BackColor = System.Drawing.Color.White;
-            this.comboFactory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.comboFactory.FormattingEnabled = true;
-            this.comboFactory.IsSupportUnselect = true;
-            this.comboFactory.Location = new System.Drawing.Point(105, 7);
-            this.comboFactory.Name = "comboFactory";
-            this.comboFactory.Size = new System.Drawing.Size(121, 24);
-            this.comboFactory.TabIndex = 13;
-            // 
-            // txtSP
-            // 
-            this.txtSP.BackColor = System.Drawing.Color.White;
-            this.txtSP.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.txtSP.Location = new System.Drawing.Point(105, 130);
-            this.txtSP.Name = "txtSP";
-            this.txtSP.Size = new System.Drawing.Size(121, 23);
-            this.txtSP.TabIndex = 14;
             // 
             // R33
             // 
--- Sci.Production.Subcon/R33.cs
+++ Sci.Production.Subcon/R33.cs
@@ -47,13 +47,11 @@ namespace Sci.Production.Subcon
             SewingEnd = ((DateTime)dateSewingDate.Value2).ToString("yyyy-MM-dd");
             SP = txtSP.Text;
             #endregion 
-
             return true;
         }
 
         protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
         {
-            this.ShowWaitMessage("Data Loading...");
             #region SQLParameter
             List<SqlParameter> sqlParameter = new List<SqlParameter>();
             sqlParameter.Add(new SqlParameter("@Factory", Factory));
@@ -69,6 +67,7 @@ declare @StartDate Date = @SewingStart;
 declare @EndDate Date = @SewingEnd;
 declare @SP char(20) = @SPNO;
 declare @ByFactory Bit = @ToExcelBy;
+declare @FactoryID char(10) = @Factory;
 
 --取出符合條件的訂單
 ----日期條件 (1. InLine & OffLine 都在區間內 2. InLine 在區間內 OffLine 在 End 後 3. InLine 在 Start 前 & OffLine 在 End 後)
@@ -84,12 +83,15 @@ where	o.Junk != 1
 		{0}
 		and not ((o.SewOffLine < @StartDate and o.SewInLine < @EndDate) or (o.SewInLine > @StartDate and o.SewInLine > @EndDate))
 		and (o.SewInLine is not null or o.SewInLine != '')
-		and (o.SewOffLine is not null or o.SewOffLine != '')
-order by o.POID, o.ID, o.SewLine
+		and (o.SewOffLine is not null or o.SewOffLine != '')		
+        and f.ID = @FactoryID
+--order by o.POID, o.ID, o.SewLine
 
---取出的訂單，執行以下判斷流程
-----Step1 尋找符合的訂單
+--取出的訂單，執行以下判斷流程 (#CutComb 儲存 整件衣服 應該裁剪的部位)
+----Step1 找出同 Article、Comb 的 bundel card 有幾個不同的 artwork bundle
 ------Step2 組出 SP# 的剪裁 Comb
+----------排除Order_BOF.Kind=0 (表Other) and 3 (表Polyfill)的資料
+----------須排除外裁項 (Order_EachCons.CuttingPiece == 0) 【0 代表非外裁項】
 select	#tsp.masterID
 		, #tsp.orderID
 		, #cur_artwork.FabricCombo
@@ -113,16 +115,27 @@ inner join (
 							  ), 1, 1, '')
 	from Bundle b
 	inner join Bundle_Detail bd on b.id = bd.id
-	inner join WorkOrder wo on b.Orderid = wo.OrderID
+	inner join WorkOrder wo on b.POID = wo.id
+							   and b.CutRef = wo.CutRef
+	left join Order_BOF ob on ob.Id = wo.Id 
+							  and ob.FabricCode = wo.FabricCode
+	left join Order_EachCons oec on oec.ID = ob.ID 
+								    and oec.MarkerName = wo.Markername 
+									and oec.FabricCombo = wo.FabricCombo 
+									and oec.FabricPanelCode = wo.FabricPanelCode 
+									and oec.FabricCode = wo.FabricCode
 	inner join #tsp on b.Orderid = #tsp.orderID
+	where ob.Kind not in ('0','3')
+	      and oec.CuttingPiece = 0
 )#cur_artwork on #tsp.orderID = #cur_artwork.orderID
 
 ----Step3 取出被 Loader 收下的 Bundle Data
-----------須排除假日 1. Sunday 2. 有列在 Holiday 的日期
+--------排除Order_BOF.Kind=0 (表Other) and 3 (表Polyfill)的資料
+--------須排除外裁項 (Order_EachCons.CuttingPiece == 0) 【0 代表非外裁項】
 select	b.orderid
 		, bd.SizeCode
 		, cdate2 = cDate.value
-		, Article
+		, b.Article
 		, wo.FabricCombo
 		, artwork = ArtWork.value
 		, qty = sum(isnull(bd.qty, 0))
@@ -130,10 +143,16 @@ into #cur_bdltrack2
 from BundleInOut bio
 inner join Bundle_Detail bd on bio.BundleNo = bd.BundleNo
 inner join Bundle b on b.id = bd.id
-inner join WorkOrder wo on b.Orderid = wo.OrderID 
+inner join WorkOrder wo on b.POID = wo.id
+						   and b.CutRef = wo.CutRef
+left join Order_BOF ob on ob.Id = wo.Id 
+						  and ob.FabricCode = wo.FabricCode
+left join Order_EachCons oec on oec.ID = ob.ID 
+							    and oec.MarkerName = wo.Markername 
+							    and oec.FabricCombo = wo.FabricCombo 
+							    and oec.FabricPanelCode = wo.FabricPanelCode 
+							    and oec.FabricCode = wo.FabricCode
 inner join #tsp on b.Orderid = #tsp.orderID
-left join Holiday h	on h.HolidayDate = CONVERT(char(10), bio.InComing, 120)
-					and h.FactoryID = #tsp.FactoryID
 outer apply (
 	select value = stuff ((	select '+' + subprocessid
 							from (
@@ -147,16 +166,15 @@ outer apply (
 outer apply(
 	select value = CONVERT(char(10), bio.InComing, 120)
 ) cDate
-where	bio.SubProcessId = 'loading'	
+where	bio.SubProcessId = 'loading'
+		and ob.Kind not in ('0','3')
+	    and oec.CuttingPiece = 0	
 		{1}
-		and h.HolidayDate is null
-		and DATEPART(DW, cDate.value) != 1
 group by b.orderid, bd.SizeCode, cDate.value, b.Article, wo.FabricCombo, ArtWork.value
 
 ----Step4 已收的bundle資料中找出各article/size/部位/artwork/加總數量
 ------Step5 計算sp#上線日至下線日的產出
 --------Step6 依條件日期區間，繞sewing 取得stdqty加總以及抓取備妥的成衣件數
-------------依照【日期, SP#, Article, Size, CDomb, Artwork】取最小數量 (因為每個部位都要有，才能成為一件衣服)
 select	#cutcomb.FactoryID
 		, #cutcomb.orderid
 		, #cur_bdltrack2.cdate2
@@ -165,59 +183,75 @@ select	#cutcomb.FactoryID
 		, #cur_bdltrack2.SizeCode
 		, #cutcomb.FabricCombo
 		, #cutcomb.artwork
-		, #cur_bdltrack2.qty
-		, AccuLoadingQty = sum(#cur_bdltrack2.qty) over (partition by #cutcomb.orderid, #cutcomb.Article, #cur_bdltrack2.SizeCode, #cutcomb.FabricCombo, #cutcomb.artwork 
-														 order by #cur_bdltrack2.cdate2)
+		, qty = isnull(#cur_bdltrack2.qty, 0)
+		, AccuLoadingQty = sum(isnull(#cur_bdltrack2.qty, 0)) over (partition by #cutcomb.FactoryID, #cutcomb.orderid,  #cutcomb.Article, #cur_bdltrack2.SizeCode, #cutcomb.FabricCombo, #cutcomb.artwork
+																    order by #cur_bdltrack2.cdate2)
 into #Min_cut
 from #cutcomb
-inner join #cur_bdltrack2 on	#cutcomb.artwork = #cur_bdltrack2.artwork 
-								and #cutcomb.FabricCombo = #cur_bdltrack2.FabricCombo
-								and #cutcomb.orderID = #cur_bdltrack2.orderid
-								and #cutcomb.Article = #cur_bdltrack2.Article 
-where	#cur_bdltrack2.qty is not null 
-		and #cur_bdltrack2.qty != 0
-		and #cur_bdltrack2.cdate2 between @StartDate and @EndDate
-order by cdate2, orderid, Article, SizeCode, FabricCombo, artwork
+left join #cur_bdltrack2 on	#cutcomb.artwork = #cur_bdltrack2.artwork 
+							and #cutcomb.FabricCombo = #cur_bdltrack2.FabricCombo
+							and #cutcomb.orderID = #cur_bdltrack2.orderid
+							and #cutcomb.Article = #cur_bdltrack2.Article 
+where	#cur_bdltrack2.cdate2 between @StartDate and @EndDate
+--order by cdate2, orderid, Article, SizeCode, FabricCombo, artwork
 
+----準備好要印的資料
 select	FactoryID
-		, orderID
-		, cdate2
-		, SewLine
-		, Article, SizeCode
-		, AccuStdQty = (select sum(s.StdQ)
-						from dbo.getDailystdq(#Min_cut.orderid) s
-						where s.Date between @StartDate and @EndDate)
-		, MinLoadingQty = min(AccuLoadingQty)
+		, SP
+		, SewingDate
+		, Line
+		, AccuStd
+		, AccuLoad = sum(AccuLoading) over (partition by FactoryID, SP, Line 
+											order by SewingDate)
 into #print
-from #Min_cut 
-group by FactoryID, orderID, cdate2, SewLine, Article, SizeCode
-order by FactoryID, orderID, cdate2, SewLine
-
-----判斷 ToExcel 
-IF @ByFactory = 1
-	select	FactoryID
-			, SewingDate = cdate2
-			, AccuStd = sum (isnull (AccuStdQty, 0))
-			, AccuLoading = sum (isnull(MinLoadingQty, 0))
-			, BCS = iif (sum (isnull (AccuStdQty, 0)) = 0, 100
-														 , iif (ROUND (sum (isnull (MinLoadingQty, 0)) / sum (isnull (AccuStdQty, 0)) * 100, 2) >= 100, 100
-																																					  , ROUND (sum (isnull (MinLoadingQty, 0)) / sum (isnull (AccuStdQty, 0)) * 100, 2)))
-	from #print
-	group by FactoryID, cdate2
-	order by FactoryID, cdate2
-Else 
+from (
 	select	FactoryID
 			, SP = orderID
 			, SewingDate = cdate2
 			, Line = SewLine
-			, AccuStd = sum (isnull (AccuStdQty, 0))
+			, AccuStd = isnull(std.value, 0)
 			, AccuLoading = sum (isnull(MinLoadingQty, 0))
-			, BCS = iif (sum (isnull (AccuStdQty, 0)) = 0, 100
-														 , iif (ROUND (sum (isnull (MinLoadingQty, 0)) / sum (isnull (AccuStdQty, 0)) * 100, 2) >= 100, 100
-																																					  , ROUND (sum (isnull (MinLoadingQty, 0)) / sum (isnull (AccuStdQty, 0)) * 100, 2)))
+	from (
+	----依照【日期, SP#, Article, Size, CDomb, Artwork】取最小數量 (因為每個部位都要有，才能成為一件衣服)
+		select	FactoryID
+				, orderID
+				, cdate2
+				, SewLine
+				, Article, SizeCode
+				, MinLoadingQty = min(AccuLoadingQty)
+		from #Min_cut 
+		group by FactoryID, orderID, cdate2, SewLine, Article, SizeCode		
+	)x
+	outer apply(
+		select	value = isnull(( select sum(s.StdQ)
+									from dbo.getDailystdq(x.orderid) s
+									where s.Date between @StartDate and @EndDate)
+								, 0)
+	) std
+	group by FactoryID, orderID, cdate2, SewLine, std.value
+) a
+
+
+----判斷 ToExcel & 算出 BCS = Round(loading / std * 100, 2)
+--------分母不能 = 0
+IF @ByFactory = 1
+	select  FactoryID
+			, SewingDate
+			, Std = sum(AccuStd)
+			, Loading = sum(AccuLoad) 
+			, BCS = iif(ROUND(sum(AccuLoad) / iif(sum(AccuStd) = 0, 1, sum(AccuStd)) * 100, 2) >= 100, 100
+																									 , ROUND(sum(AccuLoad) / iif(sum(AccuStd) = 0, 1, sum(AccuStd)) * 100, 2))
 	from #print
-	group by FactoryID, orderID, cdate2, SewLine
-	order by FactoryID, orderID, cdate2, SewLine
+	group by FactoryID, SewingDate
+	order by FactoryID, SewingDate
+Else
+	select	#print.* 
+			, BCS = iif(BCS.value >= 100, 100, BCS.value)
+	from #print
+	outer apply (
+		select value = ROUND(AccuLoad / iif(AccuStd = 0, 1, AccuStd) * 100, 2)
+	) BCS
+	order by FactoryID, SP, SewingDate, Line
 
 drop table #tsp
 drop table #cutcomb
@@ -230,12 +264,9 @@ drop table #print"
 
             DualResult result = DBProxy.Current.Select(null, strSQL, sqlParameter, out printData);
             if (!result)
-            {
-                this.HideWaitMessage();
-                MyUtility.Msg.ErrorBox(strSQL + "\n\r" + result.Description);
+            {                
                 return result;
             }
-            this.HideWaitMessage();
             return Result.True;
         }
 
@@ -246,6 +277,7 @@ drop table #print"
                 MyUtility.Msg.InfoBox("Data not found.");
                 return false;
             }
+            this.SetCount(printData.Rows.Count);
             this.ShowWaitMessage("Excel Processing...");
 
             Excel.Application objApp = null;
@@ -257,6 +289,10 @@ drop table #print"
                 MyUtility.Excel.CopyToXls(printData, "", "Subcon_R33_ByFactory.xltx", 3, showExcel: true, excelApp: objApp);
                 worksheet = objApp.Sheets[1];
                 #endregion 
+                #region set CheckDate & Factory
+                worksheet.Cells[2, 2] = SewingStart + " - " + SewingEnd;
+                worksheet.Cells[2, 5] = Factory;
+                #endregion 
             }
             else
             {
@@ -264,6 +300,10 @@ drop table #print"
                 objApp = MyUtility.Excel.ConnectExcel(Sci.Env.Cfg.XltPathDir + "\\Subcon_R33_BySPNO.xltx");
                 MyUtility.Excel.CopyToXls(printData, "", "Subcon_R33_BySPNO.xltx", 3, showExcel: true, excelApp: objApp);
                 worksheet = objApp.Sheets[1];
+                #endregion 
+                #region set CheckDate & Factory
+                worksheet.Cells[2, 3] = SewingStart + " - " + SewingEnd;
+                worksheet.Cells[2, 6] = Factory;
                 #endregion 
             }
 
