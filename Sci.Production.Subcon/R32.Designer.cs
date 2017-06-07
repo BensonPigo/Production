14521abed10f30d91f5804c232baeec9556ae806
JIMMY <jimmy@sportscity.com.tw>
2017-06-07 06:37:49 +0000

    ADD Subcon R32

---
+++ Sci.Production.Subcon/R32.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Sci.Production.Subcon
+{
+    partial class R32
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateFarmOutDate = new Sci.Win.UI.DateRange();
+            this.labelFarmOutDate = new Sci.Win.UI.Label();
+            this.comboSubProcess = new Sci.Win.UI.ComboBox();
+            this.labelSubProcess = new Sci.Win.UI.Label();
+            this.comboFactory = new Sci.Win.UI.ComboBox();
+            this.labelFactory = new Sci.Win.UI.Label();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(431, 12);
+            this.print.Visible = false;
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(431, 48);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(431, 84);
+            // 
+            // dateFarmOutDate
+            // 
+            this.dateFarmOutDate.IsRequired = false;
+            this.dateFarmOutDate.Location = new System.Drawing.Point(119, 9);
+            this.dateFarmOutDate.Name = "dateFarmOutDate";
+            this.dateFarmOutDate.Size = new System.Drawing.Size(280, 23);
+            this.dateFarmOutDate.TabIndex = 94;
+            // 
+            // labelFarmOutDate
+            // 
+            this.labelFarmOutDate.Location = new System.Drawing.Point(9, 9);
+            this.labelFarmOutDate.Name = "labelFarmOutDate";
+            this.labelFarmOutDate.Size = new System.Drawing.Size(103, 23);
+            this.labelFarmOutDate.TabIndex = 95;
+            this.labelFarmOutDate.Text = "Farm Out Date";
+            // 
+            // comboSubProcess
+            // 
+            this.comboSubProcess.BackColor = System.Drawing.Color.White;
+            this.comboSubProcess.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboSubProcess.FormattingEnabled = true;
+            this.comboSubProcess.IsSupportUnselect = true;
+            this.comboSubProcess.Location = new System.Drawing.Point(119, 38);
+            this.comboSubProcess.Name = "comboSubProcess";
+            this.comboSubProcess.Size = new System.Drawing.Size(121, 24);
+            this.comboSubProcess.TabIndex = 96;
+            // 
+            // labelSubProcess
+            // 
+            this.labelSubProcess.Location = new System.Drawing.Point(9, 38);
+            this.labelSubProcess.Name = "labelSubProcess";
+            this.labelSubProcess.Size = new System.Drawing.Size(103, 23);
+            this.labelSubProcess.TabIndex = 97;
+            this.labelSubProcess.Text = "Sub Process";
+            // 
+            // comboFactory
+            // 
+            this.comboFactory.BackColor = System.Drawing.Color.White;
+            this.comboFactory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboFactory.FormattingEnabled = true;
+            this.comboFactory.IsSupportUnselect = true;
+            this.comboFactory.Location = new System.Drawing.Point(119, 68);
+            this.comboFactory.Name = "comboFactory";
+            this.comboFactory.Size = new System.Drawing.Size(121, 24);
+            this.comboFactory.TabIndex = 98;
+            // 
+            // labelFactory
+            // 
+            this.labelFactory.Location = new System.Drawing.Point(9, 68);
+            this.labelFactory.Name = "labelFactory";
+            this.labelFactory.Size = new System.Drawing.Size(103, 23);
+            this.labelFactory.TabIndex = 99;
+            this.labelFactory.Text = "Factory";
+            // 
+            // R32
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 144);
+            this.Controls.Add(this.comboFactory);
+            this.Controls.Add(this.labelFactory);
+            this.Controls.Add(this.comboSubProcess);
+            this.Controls.Add(this.labelSubProcess);
+            this.Controls.Add(this.dateFarmOutDate);
+            this.Controls.Add(this.labelFarmOutDate);
+            this.Name = "R32";
+            this.Text = "R32";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.labelFarmOutDate, 0);
+            this.Controls.SetChildIndex(this.dateFarmOutDate, 0);
+            this.Controls.SetChildIndex(this.labelSubProcess, 0);
+            this.Controls.SetChildIndex(this.comboSubProcess, 0);
+            this.Controls.SetChildIndex(this.labelFactory, 0);
+            this.Controls.SetChildIndex(this.comboFactory, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.DateRange dateFarmOutDate;
+        private Win.UI.Label labelFarmOutDate;
+        private Win.UI.ComboBox comboSubProcess;
+        private Win.UI.Label labelSubProcess;
+        private Win.UI.ComboBox comboFactory;
+        private Win.UI.Label labelFactory;
+    }
+}
---
+++ Sci.Production.Subcon/R32.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sci.Data;
+using System.Data.SqlClient;
+using Ict;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+
+namespace Sci.Production.Subcon
+{
+    public partial class R32 : Sci.Win.Tems.PrintForm
+    {
+        string StartDate, EndDate, SubPress, Factory;
+        DataTable printData;
+
+        public R32(ToolStripMenuItem menuitem)
+            :base(menuitem)
+        {
+            InitializeComponent();
+            #region set ComboSubPress
+            DataTable dtSubPress;
+            DBProxy.Current.Select(null, @"
+select Id = ''
+
+union all
+select Id 
+from SubProcess 
+where IsRFIDProcess=1
+", out dtSubPress);
+            MyUtility.Tool.SetupCombox(comboSubProcess, 1, dtSubPress);
+            #endregion
+            #region set ComboFactory
+            DataTable dtFactory;
+            DBProxy.Current.Select(null, @"
+select FtyGroup = ''
+
+union all
+select distinct FTYGroup 
+from Factory 
+where Junk != 1", out dtFactory);
+            MyUtility.Tool.SetupCombox(comboFactory, 1, dtFactory);
+            comboFactory.Text = Sci.Env.User.Factory;
+            #endregion 
+        }
+
+        protected override bool ValidateInput()
+        {
+            #region Set Value
+            StartDate = ((DateTime)dateFarmOutDate.Value1).ToString("yyyy-MM-dd");
+            EndDate = ((DateTime)dateFarmOutDate.Value2).ToString("yyyy-MM-dd");
+            SubPress = comboSubProcess.Text;
+            Factory = comboFactory.Text;
+            #endregion 
+            #region 判斷必輸條件
+            if (StartDate.Empty() || EndDate.Empty())
+            {
+                MyUtility.Msg.InfoBox("Farm Out Date Can't be empty.");
+                return false;
+            }
+            #endregion
+            return true;
+        }
+
+        protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            #region SQl Parameters
+            List<SqlParameter> listSqlPar = new List<SqlParameter>();
+            listSqlPar.Add(new SqlParameter("@StartDate", StartDate));
+            listSqlPar.Add(new SqlParameter("@EndDate", EndDate));
+            listSqlPar.Add(new SqlParameter("@SubPress", SubPress));
+            listSqlPar.Add(new SqlParameter("@Factory", Factory));
+            #endregion 
+            #region SQL Filte
+            List<string> filte = new List<string>();
+            if (!SubPress.Empty())
+                filte.Add("and BT.StartProcess = @SubProcess  --Sub Process");
+            if (!Factory.Empty())
+                filte.Add("and O.FactoryID = @Factory  --Factory"); 
+            #endregion
+            #region SQL CMD
+            string sqlCmd = string.Format(@"
+SELECT  O.FactoryID
+		, O.ID
+		, O.StyleID
+		, BD.SizeCode
+		, BD.BundleGroup
+		, BD.BundleNo
+		, BodyCutNo = Concat(B.FabricPanelCode, '', B.Cutno)
+		, BD.Qty
+		, Color = Concat(B.Article, '', B.Colorid)
+		, BT.IssueDate
+		, BTD_IN.ReceiveDate
+		, '' remark
+FROM BundleTrack BT
+left join BundleTrack_detail BTD on BT.Id=BTD.Id
+left join Orders O on O.ID=BTD.orderid
+left join Bundle_Detail BD on BD.BundleNo=BTD.BundleNo
+left join Bundle B on B.ID =BD.Id
+left join BundleTrack_detail BTD_IN on BTD_IN.Id like 'TC%' and BD.BundleNo=BTD_IN.BundleNo  --Farm In Date
+WHERE	BT.Id LIKE 'TB%'  --Farm out
+		and BT.IssueDate >= @StartDate  --Farm Out Date start
+		and BT.IssueDate <= @EndDate  --Farm Out Date End
+        {0}
+ORDER BY  BD.BundleNo", filte.JoinToString("\r\n"));
+            #endregion
+            #region Get Data
+            DualResult result;
+            if (result = DBProxy.Current.Select(null, sqlCmd, listSqlPar, out printData))
+            {
+                return result;
+            }            
+            #endregion
+            return Result.True;
+        }
+
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            #region check printData
+            if (printData == null || printData.Rows.Count == 0)
+            {
+                MyUtility.Msg.InfoBox("Data not found.");
+                return false;
+            }
+            #endregion
+            this.SetCount(printData.Rows.Count);
+            this.ShowWaitMessage("Excel Processing");
+            #region To Excel
+            Excel.Application objApp = MyUtility.Excel.ConnectExcel(Sci.Env.Cfg.XltPathDir + "\\Subcon_R32.xltx");
+            MyUtility.Excel.CopyToXls(printData, "", "Subcon_R32.xltx", 1, showExcel: true, excelApp: objApp);
+            Excel.Worksheet worksheet = objApp.Sheets[1];
+            worksheet.Columns.AutoFit();
+
+            if (objApp != null) Marshal.FinalReleaseComObject(objApp);
+            if (worksheet != null) Marshal.FinalReleaseComObject(worksheet);
+            #endregion 
+            this.HideWaitMessage();            
+            return true;
+        }
+    }
+}
