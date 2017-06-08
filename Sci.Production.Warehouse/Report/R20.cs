c1070b43b5207788943168a706d47633f6a940dd
JIMMY <jimmy@sportscity.com.tw>
2017-06-08 06:26:13 +0000

    ADD R20

---
+++ Sci.Production.Warehouse/Report/R20.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sci.Production.Warehouse.Report
+{
+    public partial class R20 : Sci.Win.Tems.PrintForm
+    {
+        public R20(ToolStripMenuItem menuitem)
+            :base(menuitem)
+        {
+            InitializeComponent();
+        }
+    }
+}
