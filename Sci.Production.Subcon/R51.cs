d42009bf589a71631d3706df355601101aaf5b2e
JIMMY <jimmy@sportscity.com.tw>
2017-06-09 07:04:58 +0000

    ADD R51 & 畫面配置

---
+++ Sci.Production.Subcon/R51.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sci.Production.Subcon
+{
+    public partial class R51 : Sci.Win.Tems.PrintForm
+    {
+        public R51(ToolStripMenuItem menuitem)
+            :base(menuitem)
+        {
+            InitializeComponent();
+        }
+    }
+}
