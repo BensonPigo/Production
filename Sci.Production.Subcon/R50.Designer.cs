2c220d5161895936d8572a42cfc3102e38848793
JEFF <jeff@sportscity.com.tw>
2017-06-09 02:37:48 +0000

    0005131: Subcon_R50.Production Bundle Transfer Printing System需要用到Subcon_R50.ProductionBundleTransfer功能 請參考附件PMS_Subcon_R50.docx 1.新增Report, 只需顯示To Excel & Cancel , 不需要Print/Preview button 2.Excel欄位格式請依照撈取資料格式做設定

---
+++ Sci.Production.Subcon/R50.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Sci.Production.Subcon
+{
+    partial class R50
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new Sci.Win.UI.Panel();
+            this.labelFactory = new Sci.Win.UI.Label();
+            this.txtBundleNo = new Sci.Win.UI.TextBox();
+            this.Date = new Sci.Win.UI.DateRange();
+            this.labelBundleNo = new Sci.Win.UI.Label();
+            this.labelDate = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.txtfactory = new Sci.Production.Class.txtfactory();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(444, 12);
+            this.print.Visible = false;
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(444, 48);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(444, 84);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtfactory);
+            this.panel1.Controls.Add(this.labelFactory);
+            this.panel1.Controls.Add(this.txtBundleNo);
+            this.panel1.Controls.Add(this.Date);
+            this.panel1.Controls.Add(this.labelBundleNo);
+            this.panel1.Controls.Add(this.labelDate);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(413, 113);
+            this.panel1.TabIndex = 94;
+            // 
+            // labelFactory
+            // 
+            this.labelFactory.Location = new System.Drawing.Point(12, 79);
+            this.labelFactory.Name = "labelFactory";
+            this.labelFactory.Size = new System.Drawing.Size(99, 23);
+            this.labelFactory.TabIndex = 110;
+            this.labelFactory.Text = "Factory";
+            // 
+            // txtBundleNo
+            // 
+            this.txtBundleNo.BackColor = System.Drawing.Color.White;
+            this.txtBundleNo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtBundleNo.Location = new System.Drawing.Point(123, 43);
+            this.txtBundleNo.MaxLength = 2;
+            this.txtBundleNo.Name = "txtBundleNo";
+            this.txtBundleNo.Size = new System.Drawing.Size(106, 23);
+            this.txtBundleNo.TabIndex = 1;
+            // 
+            // Date
+            // 
+            this.Date.IsRequired = false;
+            this.Date.Location = new System.Drawing.Point(123, 7);
+            this.Date.Name = "Date";
+            this.Date.Size = new System.Drawing.Size(280, 23);
+            this.Date.TabIndex = 0;
+            // 
+            // labelBundleNo
+            // 
+            this.labelBundleNo.Location = new System.Drawing.Point(12, 43);
+            this.labelBundleNo.Name = "labelBundleNo";
+            this.labelBundleNo.Size = new System.Drawing.Size(99, 23);
+            this.labelBundleNo.TabIndex = 98;
+            this.labelBundleNo.Text = "Bundle No";
+            // 
+            // labelDate
+            // 
+            this.labelDate.Location = new System.Drawing.Point(12, 7);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(99, 23);
+            this.labelDate.TabIndex = 97;
+            this.labelDate.Text = "Date";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(430, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 22);
+            this.label4.TabIndex = 112;
+            this.label4.Text = "Paper Size A4";
+            // 
+            // txtfactory
+            // 
+            this.txtfactory.BackColor = System.Drawing.Color.White;
+            this.txtfactory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtfactory.IssupportJunk = false;
+            this.txtfactory.Location = new System.Drawing.Point(123, 79);
+            this.txtfactory.Name = "txtfactory";
+            this.txtfactory.Size = new System.Drawing.Size(66, 23);
+            this.txtfactory.TabIndex = 112;
+            // 
+            // R50
+            // 
+            this.ClientSize = new System.Drawing.Size(536, 170);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Name = "R50";
+            this.Text = "R50_ProductionBundleTransfer";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.Panel panel1;
+        private Win.UI.Label labelFactory;
+        private Win.UI.TextBox txtBundleNo;
+        private Win.UI.DateRange Date;
+        private Win.UI.Label labelBundleNo;
+        private Win.UI.Label labelDate;
+        private Class.txtfactory txtfactory;
+        private Win.UI.Label label4;
+    }
+}
---
+++ Sci.Production.Subcon/R50.cs
@@ -0,0 +1,86 @@
+﻿using Ict;
+using Sci.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Subcon
+{
+    public partial class R50 : Sci.Win.Tems.PrintForm
+    {
+        public R50(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+        }
+
+        string Factory, BundleNO;
+        DateTime? Date1, Date2;
+        DataTable printTable;
+        // 驗證輸入條件
+        protected override bool ValidateInput()
+        {
+            Date1 = Date.Value1;
+            Date2 = Date.Value2;
+            BundleNO = txtBundleNo.Text;
+            Factory = txtfactory.Text;
+
+            return base.ValidateInput();
+        }
+
+        //非同步讀取資料
+        protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            StringBuilder sql = new StringBuilder();
+            sql.Append(@"
+Select 
+	o.FactoryID,BD.BundleNo,bd.BundleGroup,o.StyleID,o.SeasonID,o.brandid,bd.Patterncode,bd.PatternDesc,bd.SizeCode,bd.Qty,
+	b.poid,b.Colorid,b.Article,b.Cdate,b.Orderid,b.Item ,b.AddDate,B.AddName,B.EditDate,b.AddName
+From Bundle_Detail BD 
+Left join bundle b on (bd.Id = b.ID)
+Left join Orders O on(b.Orderid = O.id)
+Where 1=1
+");
+            if (!MyUtility.Check.Empty(Date1))
+                sql.Append(string.Format("And (Convert (date, b.AddDate)  >= '{0}' Or Convert (date, b.EditDate) >= '{0}')", Convert.ToDateTime(Date1).ToString("d")));
+            if (!MyUtility.Check.Empty(Date2))
+                sql.Append(string.Format("And (Convert (date, b.AddDate)  <= '{0}' Or Convert (date, b.EditDate) <= '{0}')", Convert.ToDateTime(Date2).ToString("d")));
+            if (!MyUtility.Check.Empty(BundleNO))
+                sql.Append(string.Format("And BD.BundleNo = '{0}'", BundleNO));
+            if (!MyUtility.Check.Empty(Factory))
+                sql.Append(string.Format("And O.FtyGroup = '{0}'", Factory));
+            DualResult result = DBProxy.Current.Select(null, sql.ToString(), out printTable);
+            if (!result)
+            {
+                DualResult failResult = new DualResult(false, "Query data fail\r\n" + result.ToString());
+                return failResult;
+            }
+            return Result.True;
+        }
+
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            // 顯示筆數於PrintForm上Count欄位
+            SetCount(printTable.Rows.Count);
+            if (printTable.Rows.Count <= 0)
+            {
+                MyUtility.Msg.WarningBox("Data not found!");
+                return false;
+            }
+            //預先開啟excel app
+            Microsoft.Office.Interop.Excel.Application objApp = MyUtility.Excel.ConnectExcel(Sci.Env.Cfg.XltPathDir + "\\Subcon_R50_ProductionBundleTransfer.xltx");
+            // 將datatable copy to excel
+            MyUtility.Excel.CopyToXls(printTable, "", "Subcon_R50_ProductionBundleTransfer.xltx", 1, true, null, objApp);
+            // 取得工作表
+            Microsoft.Office.Interop.Excel.Worksheet objSheets = objApp.ActiveWorkbook.Worksheets[1];
+            if (objSheets != null) Marshal.FinalReleaseComObject(objSheets);    //釋放sheet
+            if (objApp != null) Marshal.FinalReleaseComObject(objApp);          //釋放objApp 
+            return true;
+        }
+    }
+}
