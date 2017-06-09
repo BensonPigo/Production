7e4f9591a6975c10bbb664d9e5f866fa3551a7ff
LEO <leo@sportscity.com.tw>
2017-06-09 07:46:22 +0000

    0005101: Subcon_P23 add new program [Farm out(Barcode)] add 框架

---
+++ Sci.Production.Subcon/P23.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sci.Production.Subcon
+{
+    public partial class P23 : Sci.Win.Tems.Input6
+    {
+        public P23(ToolStripMenuItem menuitem)
+            :base(menuitem)
+        {
+            InitializeComponent();
+        }
+
+    }
+}
