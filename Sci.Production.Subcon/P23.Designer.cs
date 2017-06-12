fda34d243f620d7b1166562e8f8a1b5205000ccf
LEO <leo@sportscity.com.tw>
2017-06-12 08:51:36 +0000

    0005101: Subcon_P23 add new program [Farm out(Barcode)] 1.Form完成 2.Browser完成 3.表頭完成 4.表身完成 5.其他控制項 待完成事項 [Import from scanner]未完成 [Create] 建單 按[print]產生EXCEL 後面由jeff接手做

--- Sci.Production.Subcon/P23.Designer.cs
+++ Sci.Production.Subcon/P23.Designer.cs
@@ -28,11 +28,174 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txSubproc = new Sci.Win.UI.TextBox();
+            this.txtTransfer = new Sci.Win.UI.TextBox();
+            this.labelSubproc = new Sci.Win.UI.Label();
+            this.labelIssueDate = new Sci.Win.UI.Label();
+            this.labelTransferID = new Sci.Win.UI.Label();
+            this.dateIssueDate = new Sci.Win.UI.DateBox();
+            ((System.ComponentModel.ISupportInitialize)(this.detailgridbs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.detailgrid2bs)).BeginInit();
+            this.masterpanel.SuspendLayout();
+            this.detailpanel.SuspendLayout();
+            this.detail2.SuspendLayout();
+            this.detailpanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).BeginInit();
+            this.detail.SuspendLayout();
+            this.detailcont.SuspendLayout();
+            this.detailbtm.SuspendLayout();
+            this.tabs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // masterpanel
+            // 
+            this.masterpanel.Controls.Add(this.txSubproc);
+            this.masterpanel.Controls.Add(this.txtTransfer);
+            this.masterpanel.Controls.Add(this.labelSubproc);
+            this.masterpanel.Controls.Add(this.labelIssueDate);
+            this.masterpanel.Controls.Add(this.labelTransferID);
+            this.masterpanel.Controls.Add(this.dateIssueDate);
+            this.masterpanel.Size = new System.Drawing.Size(892, 45);
+            this.masterpanel.Controls.SetChildIndex(this.gridicon, 0);
+            this.masterpanel.Controls.SetChildIndex(this.dateIssueDate, 0);
+            this.masterpanel.Controls.SetChildIndex(this.labelTransferID, 0);
+            this.masterpanel.Controls.SetChildIndex(this.labelIssueDate, 0);
+            this.masterpanel.Controls.SetChildIndex(this.labelSubproc, 0);
+            this.masterpanel.Controls.SetChildIndex(this.txtTransfer, 0);
+            this.masterpanel.Controls.SetChildIndex(this.txSubproc, 0);
+            // 
+            // detailpanel
+            // 
+            this.detailpanel.Location = new System.Drawing.Point(0, 45);
+            this.detailpanel.Size = new System.Drawing.Size(892, 304);
+            // 
+            // gridicon
+            // 
+            this.gridicon.Location = new System.Drawing.Point(787, 7);
+            // 
+            // detailgridcont
+            // 
+            this.detailgridcont.Size = new System.Drawing.Size(892, 304);
+            // 
+            // detail2
+            // 
+            this.detail2.Size = new System.Drawing.Size(892, 387);
+            // 
+            // detailgridcont2
+            // 
+            this.detailgridcont2.Size = new System.Drawing.Size(886, 341);
+            // 
+            // detailpanel2
+            // 
+            this.detailpanel2.Size = new System.Drawing.Size(886, 40);
+            // 
+            // detail
+            // 
+            this.detail.Size = new System.Drawing.Size(892, 387);
+            // 
+            // detailcont
+            // 
+            this.detailcont.Size = new System.Drawing.Size(892, 349);
+            // 
+            // detailbtm
+            // 
+            this.detailbtm.Location = new System.Drawing.Point(0, 349);
+            this.detailbtm.Size = new System.Drawing.Size(892, 38);
+            // 
+            // txSubproc
+            // 
+            this.txSubproc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.txSubproc.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "StartProcess", true));
+            this.txSubproc.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.txSubproc.IsSupportEditMode = false;
+            this.txSubproc.Location = new System.Drawing.Point(588, 12);
+            this.txSubproc.Name = "txSubproc";
+            this.txSubproc.ReadOnly = true;
+            this.txSubproc.Size = new System.Drawing.Size(123, 23);
+            this.txSubproc.TabIndex = 16;
+            // 
+            // txtTransfer
+            // 
+            this.txtTransfer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.txtTransfer.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "ID", true));
+            this.txtTransfer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.txtTransfer.IsSupportEditMode = false;
+            this.txtTransfer.Location = new System.Drawing.Point(86, 12);
+            this.txtTransfer.Name = "txtTransfer";
+            this.txtTransfer.ReadOnly = true;
+            this.txtTransfer.Size = new System.Drawing.Size(123, 23);
+            this.txtTransfer.TabIndex = 15;
+            // 
+            // labelSubproc
+            // 
+            this.labelSubproc.Location = new System.Drawing.Point(507, 12);
+            this.labelSubproc.Name = "labelSubproc";
+            this.labelSubproc.Size = new System.Drawing.Size(78, 23);
+            this.labelSubproc.TabIndex = 14;
+            this.labelSubproc.Text = "Subprocess";
+            // 
+            // labelIssueDate
+            // 
+            this.labelIssueDate.Location = new System.Drawing.Point(259, 12);
+            this.labelIssueDate.Name = "labelIssueDate";
+            this.labelIssueDate.Size = new System.Drawing.Size(75, 23);
+            this.labelIssueDate.TabIndex = 13;
+            this.labelIssueDate.Text = "IssueDate";
+            // 
+            // labelTransferID
+            // 
+            this.labelTransferID.Location = new System.Drawing.Point(8, 12);
+            this.labelTransferID.Name = "labelTransferID";
+            this.labelTransferID.Size = new System.Drawing.Size(75, 23);
+            this.labelTransferID.TabIndex = 12;
+            this.labelTransferID.Text = "Transfer ID";
+            // 
+            // dateIssueDate
+            // 
+            this.dateIssueDate.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "IssueDate", true));
+            this.dateIssueDate.IsSupportEditMode = false;
+            this.dateIssueDate.Location = new System.Drawing.Point(337, 12);
+            this.dateIssueDate.Name = "dateIssueDate";
+            this.dateIssueDate.ReadOnly = true;
+            this.dateIssueDate.Size = new System.Drawing.Size(100, 23);
+            this.dateIssueDate.TabIndex = 17;
+            // 
+            // P23
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "P23";
+            this.ClientSize = new System.Drawing.Size(900, 449);
+            this.KeyField1 = "ID";
+            this.Name = "P23";
+            this.Text = "P23.Farm Out(Barcode)";
+            this.Controls.SetChildIndex(this.tabs, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.detailgridbs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.detailgrid2bs)).EndInit();
+            this.masterpanel.ResumeLayout(false);
+            this.masterpanel.PerformLayout();
+            this.detailpanel.ResumeLayout(false);
+            this.detail2.ResumeLayout(false);
+            this.detailpanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).EndInit();
+            this.detail.ResumeLayout(false);
+            this.detailcont.ResumeLayout(false);
+            this.detailbtm.ResumeLayout(false);
+            this.detailbtm.PerformLayout();
+            this.tabs.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Win.UI.TextBox txSubproc;
+        private Win.UI.TextBox txtTransfer;
+        private Win.UI.Label labelSubproc;
+        private Win.UI.Label labelIssueDate;
+        private Win.UI.Label labelTransferID;
+        private Win.UI.DateBox dateIssueDate;
     }
 }
--- Sci.Production.Subcon/P23.cs
+++ Sci.Production.Subcon/P23.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Ict;
+using Ict.Win;
+using Sci.Data;
 
 namespace Sci.Production.Subcon
 {
@@ -16,7 +19,182 @@ namespace Sci.Production.Subcon
             :base(menuitem)
         {
             InitializeComponent();
+            this.DefaultFilter = string.Format("Id LIKE 'TB%'");
+            lbl_queryfor.Text = "Sub Process";
+            this.WorkAlias = "BundleTrack";
+            this.GridAlias = "BundleTrack_detail";
+            this.GridUniqueKey = "ID,BundleNo";
+            this.DefaultOrder = "ID";
+            this.ExpressQuery = true;
         }
+        protected override void OnFormLoaded()
+        {
+            base.OnFormLoaded();
+            DataTable queryDT;
+            string querySql = @"select '' union select Id from SubProcess where IsRFIDProcess=1";
+            DBProxy.Current.Select(null, querySql, out queryDT);
+            MyUtility.Tool.SetupCombox(queryfors, 1, queryDT);
+            queryfors.SelectedIndex = 0;
+            queryfors.SelectedIndexChanged += (s, e) =>
+            {
+                switch (queryfors.SelectedIndex)
+                {
+                    case 0:
+                        this.DefaultWhere = "";
+                        break;
+                    default:
+                        this.DefaultWhere = string.Format("StartProcess = '{0}'", queryfors.SelectedValue);
+                        break;
+                }
+                this.ReloadDatas();
+            };
+        }
+        protected override bool ClickNew()
+        {
+            var frm = new Sci.Production.Subcon.P23_ImportBarcode();
+            frm.ShowDialog(this);
+            this.ReloadDatas();
+            return true;
+        }
+        protected override void ClickNewAfter()
+        {
+            base.ClickNewAfter();
+            CurrentMaintain["IssueDate"] = DateTime.Now;
+            CurrentMaintain["StartSite"] = Sci.Env.User.Factory;
+        }
+        protected override bool ClickSaveBefore()
+        {
+            //DataTable newDt = ((DataTable)detailgridbs.DataSource).Clone();
+            //foreach (DataRow dtr in ((DataTable)detailgridbs.DataSource).Rows)
+            //{
+            //    string[] dtrLocation = dtr["location"].ToString().Split(',');
+            //    dtrLocation = dtrLocation.Distinct().ToArray();
 
+            //    if (dtrLocation.Length == 1)
+            //    {
+            //        DataRow newDr = newDt.NewRow();
+            //        newDr.ItemArray = dtr.ItemArray;
+            //        newDt.Rows.Add(newDr);
+            //    }
+            //    else
+            //    {
+            //        foreach (string location in dtrLocation)
+            //        {
+            //            DataRow newDr = newDt.NewRow();
+            //            newDr.ItemArray = dtr.ItemArray;
+            //            newDr["location"] = location;
+            //            newDt.Rows.Add(newDr);
+            //        }
+            //    }
+            //}
+            //設定ID編碼
+            if (MyUtility.Check.Empty(CurrentMaintain["ID"]))
+            {
+                string getID = MyUtility.GetValue.GetID("TB", "BundleTrack", DateTime.Today, 5, "ID", null);
+                if (MyUtility.Check.Empty(getID))
+                {
+                    MyUtility.Msg.WarningBox("GetID fail, please try again!");
+                    return false;
+                }
+                CurrentMaintain["ID"] = getID;
+            }
+            //檢核BundleTrack_detail.BundleNo是否已存在
+            foreach (DataRow dr in DetailDatas)
+            {
+                if (MyUtility.Check.Seek(dr["BundleNo"].ToString(), "BundleTrack_detail", "BundleNo") == true)
+                {
+                    MyUtility.Msg.WarningBox(string.Format("Data is Duplicate!!!\r\n {0}", dr["BundleNo"].ToString()));
+                    return false;
+                }
+            }
+            return base.ClickSaveBefore();
+        }
+        protected override DualResult OnDetailSelectCommandPrepare(Win.Tems.InputMasterDetail.PrepareDetailSelectCommandEventArgs e)
+        {
+            string masterID = (e.Master == null) ? "" : e.Master["ID"].ToString();
+            this.DetailSelectCommand = string.Format(@"
+                Select 
+                 BundleTrack_detail.BundleNo
+                ,BundleTrack_detail.orderid
+                ,ss.SubprocessId
+                ,Bundle_Detail.Patterncode
+                ,Bundle_Detail.PatternDesc
+                FROM BundleTrack_detail 
+                INNER JOIN Bundle_Detail_Art ON BundleTrack_detail.BundleNo= Bundle_Detail_Art.Bundleno
+                INNER JOIN Bundle_Detail ON BundleTrack_detail.BundleNo = Bundle_Detail.BundleNo
+                OUTER APPLY(
+	                SELECT SubprocessId = STUFF((
+		                SELECT SubprocessId + '+' 
+                from Bundle_Detail_Art
+                where Bundleno=BundleTrack_detail.BundleNo
+                FOR XML PATH('')
+	                ),1,1,'')
+	                )ss
+                WHERE 1=1
+                  --AND BundleTrack_detail.Id LIKE 'TB%'
+                  AND BundleTrack_detail.ID = '{0}'
+                ", masterID);
+            return base.OnDetailSelectCommandPrepare(e);
+        }
+        protected override void OnDetailGridSetup()
+        {
+            base.OnDetailGridSetup();
+            Ict.Win.DataGridViewGeneratorTextColumnSettings ts1 = new DataGridViewGeneratorTextColumnSettings();
+            //DataRow dr;
+            ts1.CellValidating += (s, e) =>
+            {
+               
+                if (this.EditMode && e.FormattedValue.ToString() != "")
+                {
+                    if (MyUtility.Check.Seek(string.Format("select 1 where exists(select * from Bundle_Detail WITH (NOLOCK) where BundleNo = '{0}')", e.FormattedValue), null))
+                    {
+                       DataTable getdata ;
+                        string sqlcmd = string.Format(@"
+                        select B.Orderid , Artwork.value , BD.Patterncode , BD.PatternDesc
+                        from Bundle_Detail BD WITH (NOLOCK)
+                        left join Bundle B on B.ID=BD.Id
+                        outer apply (
+	                        select value = stuff ((	select '+' + subprocessid
+							                        from (
+								                        select distinct subprocessid
+								                        from Bundle_Detail_Art bda WITH (NOLOCK)
+								                        where bd.BundleNo = bda.Bundleno
+							                        ) k
+							                        for xml path('')
+							                        ), 1, 1, '')
+                        ) ArtWork
+                        where BundleNo='{0}'
+                        ", e.FormattedValue);
+                         DBProxy.Current.Select(null, sqlcmd, out getdata);
+
+                         CurrentDetailData["BundleNo"] = e.FormattedValue;
+                         CurrentDetailData["OrderID"] = getdata.Rows[0]["Orderid"].ToString();
+                         CurrentDetailData["SubprocessId"] = getdata.Rows[0]["value"].ToString();
+                         CurrentDetailData["Patterncode"] = getdata.Rows[0]["Patterncode"].ToString();
+                         CurrentDetailData["PatternDesc"] = getdata.Rows[0]["PatternDesc"].ToString();
+                       
+                    }
+                    else
+                    {
+                        e.Cancel = true;
+                        CurrentDetailData["BundleNo"] = "";
+                        CurrentDetailData["OrderID"] = "";
+                        CurrentDetailData["SubprocessId"] = "";
+                        CurrentDetailData["Patterncode"] = "";
+                        CurrentDetailData["PatternDesc"] = "";
+                        MyUtility.Msg.WarningBox("Bundle# is not exist!!", "Data not found");
+                        return;
+                    }
+
+                }
+            };
+            Helper.Controls.Grid.Generator(this.detailgrid)
+                .Text("BundleNo", header: "Bundle#", width: Widths.AnsiChars(12),settings: ts1, iseditingreadonly: false)
+                .Text("OrderID", header: "SP#", width: Widths.AnsiChars(13))
+                .Text("SubprocessId", header: "Artwork", width: Widths.AnsiChars(8), iseditingreadonly: false)
+                .Text("Patterncode", header: "PTN Code", width: Widths.AnsiChars(10), iseditingreadonly: true)
+                .Text("PatternDesc", header: "PTN Desc.", width: Widths.AnsiChars(20), iseditingreadonly: true);
+
+        }
     }
 }
