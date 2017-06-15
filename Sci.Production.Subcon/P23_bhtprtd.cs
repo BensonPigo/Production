673e07c8001764035c79d628f5a75629267b2a56
JEFF <jeff@sportscity.com.tw>
2017-06-15 05:53:30 +0000

    加入Bhtprtd.dll在Production下 加入subcon_P23_Bhtprtd.cs來使用Bhtprtd.dll

---
+++ Sci.Production.Subcon/P23_bhtprtd.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Runtime.ExceptionServices;
+using System.Configuration;
+
+namespace Sci.Production.Subcon
+{
+    public class DllInvoke
+    {
+        [DllImport("Kernel32.dll")]
+        private extern static IntPtr LoadLibrary(String path);
+
+        [DllImport("Kernel32.dll")]
+        private extern static IntPtr GetProcAddress(IntPtr lib, String funcName);
+
+        [DllImport("Kernel32.dll")]
+        private extern static bool FreeLibrary(IntPtr lib);
+
+        private IntPtr hLib;
+        public DllInvoke(String DLLPath)
+        {
+            hLib = LoadLibrary(DLLPath);
+        }
+
+        ~DllInvoke()
+        {
+            FreeLibrary(hLib);
+        }
+
+        public Delegate Invoke(String APIName, Type t)
+        {
+            IntPtr api = GetProcAddress(hLib, APIName);
+            try
+            {
+                return (Delegate)Marshal.GetDelegateForFunctionPointer(api, t);
+            }
+            catch (Exception e)
+            {
+                MyUtility.Msg.ErrorBox(e.Message);
+                return null;
+            }
+        }
+    }
+
+    class P23_bhtprtd
+    {
+        private delegate int IdxCallVB_func(int command, string Request, int RequestSize);
+        DllInvoke dll;
+        IdxCallVB_func func;
+
+        public P23_bhtprtd()
+        {
+            dll = new DllInvoke(".\\bhtprtd.dll");
+            func = (IdxCallVB_func)dll.Invoke("IdxCallVB", typeof(IdxCallVB_func));        
+        }
+        public bool IdxCall(int command, string Request, int RequestSize)
+        {
+            if (func != null)
+            {
+                func(command, Request, RequestSize);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
