d2f8e4d4356e2641196ef6147935950eb02a6070
Jimmy <jimmy.liao@sportscity.com.tw>
2017-06-19 17:34:24 +0800

    Rate 改使用 FinanceEN.dbo.Rate

--- Database/dbo/Functions/GetCurrencyRate.sql
+++ Database/dbo/Functions/GetCurrencyRate.sql
@@ -1,4 +1,11 @@
-﻿CREATE Function [dbo].[GetCurrencyRate]
+﻿USE [Production]
+GO
+/****** Object:  UserDefinedFunction [dbo].[GetCurrencyRate]    Script Date: 2017/6/19 下午 05:25:54 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER Function [dbo].[GetCurrencyRate]
 (
 	  @ExchangeTypeID	VarChar(2)			--
 	 ,@FromCurrency		VarChar(3)			--
@@ -15,14 +22,14 @@ RETURN
 			, cTo.Exact
     FROM dbo.Currency as cFrom WITH (NOLOCK)
     left join dbo.Currency as cTo WITH (NOLOCK) on cTo.ID = @toCurrency
-    left join dbo.exchange as ex WITH (NOLOCK) on	ex.ExchangeTypeID=@ExchangeTypeID 
-													and ex.CurrencyFrom= cFrom.ID 
-													and ex.CurrencyTo= cTo.ID
-													and @Date between ex.DateStart and ex.DateEnd
+    left join FinanceEN.dbo.Rate as ex WITH (NOLOCK) on	ex.RateTypeID=@ExchangeTypeID 
+													and ex.OriginalCurrency= cFrom.ID 
+													and ex.ExchangeCurrency= cTo.ID
+													and @Date between ex.BeginDate and ex.EndDate
 	outer apply(
 	-- 分開寫是因為 換算的結果會因為 inull 的欄位結構而改變
 	-- ex: 0.00000444444 會變成 0.0000
 		select value = isnull(ex.rate, 0)
 	) ExchangeRate
-    where cFrom.ID = @fromCurrency
+     where cFrom.ID = @fromCurrency
 );
