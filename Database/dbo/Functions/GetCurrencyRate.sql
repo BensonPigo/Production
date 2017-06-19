cbfe14ef0ac19647d9b2a6f9ba89e1ac543d605e
Jimmy <jimmy.liao@sportscity.com.tw>
2017-06-19 16:32:34 +0800

    修正 [GetCurrencyRate] 在轉換時 ExchangeRate 分開判斷 分開寫是因為 換算的結果會因為 inull 的欄位結構而改變 ex: 0.00000444444 會變成 0.0000

--- Database/dbo/Functions/GetCurrencyRate.sql
+++ Database/dbo/Functions/GetCurrencyRate.sql
@@ -9,10 +9,20 @@ Returns TABLE
 AS
 RETURN 
 (
-    SELECT isnull( ex.rate, iif(cto.StdRate = 0, 0, cfrom.StdRate/cto.StdRate)) as rate,cTo.Exact
+    SELECT  rate = iif(ExchangeRate.value != 0, ExchangeRate.value
+											  , iif(cto.StdRate = 0, 0
+												                   , cfrom.StdRate/cto.StdRate)) 
+			, cTo.Exact
     FROM dbo.Currency as cFrom WITH (NOLOCK)
-     left join dbo.Currency as cTo WITH (NOLOCK) on cTo.ID = @toCurrency
-     left join dbo.exchange as ex WITH (NOLOCK) on ex.ExchangeTypeID=@ExchangeTypeID and ex.CurrencyFrom= cFrom.ID and ex.CurrencyTo= cTo.ID
-              and @Date between ex.DateStart and ex.DateEnd
-     where cFrom.ID = @fromCurrency
+    left join dbo.Currency as cTo WITH (NOLOCK) on cTo.ID = @toCurrency
+    left join dbo.exchange as ex WITH (NOLOCK) on	ex.ExchangeTypeID=@ExchangeTypeID 
+													and ex.CurrencyFrom= cFrom.ID 
+													and ex.CurrencyTo= cTo.ID
+													and @Date between ex.DateStart and ex.DateEnd
+	outer apply(
+	-- 分開寫是因為 換算的結果會因為 inull 的欄位結構而改變
+	-- ex: 0.00000444444 會變成 0.0000
+		select value = isnull(ex.rate, 0)
+	) ExchangeRate
+    where cFrom.ID = @fromCurrency
 );
