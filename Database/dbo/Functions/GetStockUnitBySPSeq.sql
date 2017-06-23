7632df45db89cdbcd56f215e8c4b5caf2871a31d
Jimmy <jimmy.liao@sportscity.com.tw>
2017-06-23 10:22:18 +0800

    新增 DB Functinos GetStockUnitBySPSeq  參數  Poid, Seq1, Seq2

---
+++ Database/dbo/Functions/GetStockUnitBySPSeq.sql
@@ -0,0 +1,31 @@
+﻿-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date, ,>
+-- Description:	<Description, ,>
+-- =============================================
+CREATE FUNCTION [dbo].[GetStockUnitBySPSeq]
+(
+	@poid varchar(10)
+	, @seq1 varchar(3)
+	, @seq2 varchar(3)
+)
+RETURNS varchar(8)
+AS
+BEGIN
+	declare @Return varchar(10) = ''
+	select @Return = StockUnit.value
+	from PO_Supp_Detail p
+	inner join [dbo].[Fabric] ff WITH (NOLOCK) on p.SCIRefno= ff.SCIRefno
+	inner join [dbo].[MtlType] mm WITH (NOLOCK) on mm.ID = ff.MtlTypeID
+	inner join [dbo].[Unit] uu WITH (NOLOCK) on ff.UsageUnit = uu.ID
+	outer apply (
+		select value = iif(mm.IsExtensionUnit is null or uu.ExtensionUnit = '', ff.UsageUnit 
+																			  , iif(mm.IsExtensionUnit > 0 , iif(uu.ExtensionUnit is null or uu.ExtensionUnit = '', ff.UsageUnit  
+																																								  , uu.ExtensionUnit) 
+																										   , ff.UsageUnit))  
+	) StockUnit
+	where	p.id = @poid
+			and p.seq1 = @seq1
+			and p.seq2 = @seq2
+	return @Return
+END
