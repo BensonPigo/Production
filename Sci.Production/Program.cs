360b72545928d3f2493ae1ad77d3c8b9e22809da
ricky <ricky.chen@sportscity.com.tw>
2017-07-17 13:46:16 +0800

    若是傳送參數則排除,Tradedb_TSR防止使用者修改到資料

--- Sci.Production/Program.cs
+++ Sci.Production/Program.cs
@@ -33,6 +33,8 @@ namespace Sci.Production
                     if (arg.Contains("TaipeiServer:'true'"))
                     {
                         ClearTaipeiServer = false;
+                        //若是傳送參數則排除,Tradedb_TSR防止使用者修改到資料
+                        ConfigurationManager.AppSettings["TaipeiServer"] = ConfigurationManager.AppSettings["TaipeiServer"].ToString().Replace(",Tradedb_TSR", "");
                     }
                 }
             }
