21d9afef4e3b961cc8b675e82bcc7a69ec041278
Willy <willy.wei@sprotscity.com.tw>
2017-07-28 10:08:09 +0800

    0006035: Warehouse P44 new function (to do 2016060011) 調整如下: 1.新增P44 Adjust Scrap Qty (Local Inventory) 2. P44 Adjust Scrap Qty (Local Inventory)_Import

---
+++ Sci.Production.Warehouse/P44.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace Sci.Production.Warehouse
+{
+    partial class P44
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.displayStocktakingID = new Sci.Win.UI.DisplayBox();
+            this.labelStocktakingID = new Sci.Win.UI.Label();
+            this.editRemark = new Sci.Win.UI.EditBox();
+            this.labelRemark = new Sci.Win.UI.Label();
+            this.btnImport = new Sci.Win.UI.Button();
+            this.dateIssueDate = new Sci.Win.UI.DateBox();
+            this.displayID = new Sci.Win.UI.DisplayBox();
+            this.label25 = new Sci.Win.UI.Label();
+            this.labelIssueDate = new Sci.Win.UI.Label();
+            this.labelID = new Sci.Win.UI.Label();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.btnFind = new Sci.Win.UI.Button();
+            this.txtLocateForSP = new Sci.Win.UI.TextBox();
+            this.labelLocateForSP = new Sci.Win.UI.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.detailgridbs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.detailgrid2bs)).BeginInit();
+            this.masterpanel.SuspendLayout();
+            this.detailpanel.SuspendLayout();
+            this.detail2.SuspendLayout();
+            this.detailpanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).BeginInit();
+            this.detail.SuspendLayout();
+            this.detailcont.SuspendLayout();
+            this.detailbtm.SuspendLayout();
+            this.tabs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // masterpanel
+            // 
+            this.masterpanel.Controls.Add(this.btnFind);
+            this.masterpanel.Controls.Add(this.txtLocateForSP);
+            this.masterpanel.Controls.Add(this.labelLocateForSP);
+            this.masterpanel.Controls.Add(this.displayStocktakingID);
+            this.masterpanel.Controls.Add(this.labelStocktakingID);
+            this.masterpanel.Controls.Add(this.editRemark);
+            this.masterpanel.Controls.Add(this.labelRemark);
+            this.masterpanel.Controls.Add(this.btnImport);
+            this.masterpanel.Controls.Add(this.dateIssueDate);
+            this.masterpanel.Controls.Add(this.displayID);
+            this.masterpanel.Controls.Add(this.label25);
+            this.masterpanel.Controls.Add(this.labelIssueDate);
+            this.masterpanel.Controls.Add(this.labelID);
+            this.masterpanel.Controls.Add(this.shapeContainer1);
+            this.masterpanel.Size = new System.Drawing.Size(954, 186);
+            this.masterpanel.Controls.SetChildIndex(this.shapeContainer1, 0);
+            this.masterpanel.Controls.SetChildIndex(this.labelID, 0);
+            this.masterpanel.Controls.SetChildIndex(this.labelIssueDate, 0);
+            this.masterpanel.Controls.SetChildIndex(this.label25, 0);
+            this.masterpanel.Controls.SetChildIndex(this.displayID, 0);
+            this.masterpanel.Controls.SetChildIndex(this.dateIssueDate, 0);
+            this.masterpanel.Controls.SetChildIndex(this.btnImport, 0);
+            this.masterpanel.Controls.SetChildIndex(this.labelRemark, 0);
+            this.masterpanel.Controls.SetChildIndex(this.editRemark, 0);
+            this.masterpanel.Controls.SetChildIndex(this.labelStocktakingID, 0);
+            this.masterpanel.Controls.SetChildIndex(this.displayStocktakingID, 0);
+            this.masterpanel.Controls.SetChildIndex(this.labelLocateForSP, 0);
+            this.masterpanel.Controls.SetChildIndex(this.txtLocateForSP, 0);
+            this.masterpanel.Controls.SetChildIndex(this.btnFind, 0);
+            this.masterpanel.Controls.SetChildIndex(this.gridicon, 0);
+            // 
+            // detailpanel
+            // 
+            this.detailpanel.Location = new System.Drawing.Point(0, 186);
+            this.detailpanel.Size = new System.Drawing.Size(954, 163);
+            // 
+            // gridicon
+            // 
+            this.gridicon.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)));
+            this.gridicon.Location = new System.Drawing.Point(835, 137);
+            // 
+            // detailgridcont
+            // 
+            this.detailgridcont.Size = new System.Drawing.Size(954, 163);
+            // 
+            // detail2
+            // 
+            this.detail2.Size = new System.Drawing.Size(892, 387);
+            // 
+            // detailgridcont2
+            // 
+            this.detailgridcont2.Size = new System.Drawing.Size(886, 341);
+            // 
+            // detailpanel2
+            // 
+            this.detailpanel2.Size = new System.Drawing.Size(886, 40);
+            // 
+            // detail
+            // 
+            this.detail.Size = new System.Drawing.Size(954, 387);
+            // 
+            // detailcont
+            // 
+            this.detailcont.Size = new System.Drawing.Size(954, 349);
+            // 
+            // detailbtm
+            // 
+            this.detailbtm.Location = new System.Drawing.Point(0, 349);
+            this.detailbtm.Size = new System.Drawing.Size(954, 38);
+            // 
+            // browse
+            // 
+            this.browse.Size = new System.Drawing.Size(954, 387);
+            // 
+            // tabs
+            // 
+            this.tabs.Size = new System.Drawing.Size(962, 416);
+            // 
+            // displayStocktakingID
+            // 
+            this.displayStocktakingID.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.displayStocktakingID.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "StocktakingId", true));
+            this.displayStocktakingID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.displayStocktakingID.Location = new System.Drawing.Point(614, 9);
+            this.displayStocktakingID.Name = "displayStocktakingID";
+            this.displayStocktakingID.Size = new System.Drawing.Size(120, 23);
+            this.displayStocktakingID.TabIndex = 74;
+            // 
+            // labelStocktakingID
+            // 
+            this.labelStocktakingID.Location = new System.Drawing.Point(506, 9);
+            this.labelStocktakingID.Name = "labelStocktakingID";
+            this.labelStocktakingID.Size = new System.Drawing.Size(105, 23);
+            this.labelStocktakingID.TabIndex = 73;
+            this.labelStocktakingID.Text = "Stocktaking ID";
+            // 
+            // editRemark
+            // 
+            this.editRemark.BackColor = System.Drawing.Color.White;
+            this.editRemark.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "remark", true));
+            this.editRemark.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.editRemark.Location = new System.Drawing.Point(97, 38);
+            this.editRemark.Multiline = true;
+            this.editRemark.Name = "editRemark";
+            this.editRemark.Size = new System.Drawing.Size(658, 87);
+            this.editRemark.TabIndex = 68;
+            // 
+            // labelRemark
+            // 
+            this.labelRemark.Location = new System.Drawing.Point(16, 39);
+            this.labelRemark.Name = "labelRemark";
+            this.labelRemark.Size = new System.Drawing.Size(75, 23);
+            this.labelRemark.TabIndex = 72;
+            this.labelRemark.Text = "Remark";
+            // 
+            // btnImport
+            // 
+            this.btnImport.EditMode = Sci.Win.UI.AdvEditModes.EnableOnEdit;
+            this.btnImport.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnImport.Location = new System.Drawing.Point(846, 38);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(89, 31);
+            this.btnImport.TabIndex = 69;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // dateIssueDate
+            // 
+            this.dateIssueDate.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "IssueDate", true));
+            this.dateIssueDate.IsSupportEditMode = false;
+            this.dateIssueDate.Location = new System.Drawing.Point(352, 9);
+            this.dateIssueDate.Name = "dateIssueDate";
+            this.dateIssueDate.ReadOnly = true;
+            this.dateIssueDate.Size = new System.Drawing.Size(130, 23);
+            this.dateIssueDate.TabIndex = 66;
+            // 
+            // displayID
+            // 
+            this.displayID.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.displayID.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "id", true));
+            this.displayID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.displayID.Location = new System.Drawing.Point(98, 9);
+            this.displayID.Name = "displayID";
+            this.displayID.Size = new System.Drawing.Size(120, 23);
+            this.displayID.TabIndex = 65;
+            // 
+            // label25
+            // 
+            this.label25.BackColor = System.Drawing.Color.Transparent;
+            this.label25.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label25.Location = new System.Drawing.Point(820, 9);
+            this.label25.Name = "label25";
+            this.label25.Size = new System.Drawing.Size(115, 23);
+            this.label25.TabIndex = 71;
+            this.label25.Text = "Not Approve";
+            this.label25.TextStyle.Color = System.Drawing.Color.Red;
+            // 
+            // labelIssueDate
+            // 
+            this.labelIssueDate.Location = new System.Drawing.Point(241, 9);
+            this.labelIssueDate.Name = "labelIssueDate";
+            this.labelIssueDate.Size = new System.Drawing.Size(108, 23);
+            this.labelIssueDate.TabIndex = 70;
+            this.labelIssueDate.Text = "Issue Date";
+            // 
+            // labelID
+            // 
+            this.labelID.Location = new System.Drawing.Point(16, 9);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(75, 23);
+            this.labelID.TabIndex = 67;
+            this.labelID.Text = "ID";
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 17;
+            this.lineShape1.X2 = 938;
+            this.lineShape1.Y1 = 133;
+            this.lineShape1.Y2 = 133;
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(954, 186);
+            this.shapeContainer1.TabIndex = 75;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // btnFind
+            // 
+            this.btnFind.EditMode = Sci.Win.UI.AdvEditModes.DisableOnEdit;
+            this.btnFind.Location = new System.Drawing.Point(280, 142);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(70, 30);
+            this.btnFind.TabIndex = 77;
+            this.btnFind.Text = "Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // txtLocateForSP
+            // 
+            this.txtLocateForSP.BackColor = System.Drawing.Color.White;
+            this.txtLocateForSP.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtLocateForSP.IsSupportEditMode = false;
+            this.txtLocateForSP.Location = new System.Drawing.Point(129, 146);
+            this.txtLocateForSP.Name = "txtLocateForSP";
+            this.txtLocateForSP.Size = new System.Drawing.Size(145, 23);
+            this.txtLocateForSP.TabIndex = 76;
+            // 
+            // labelLocateForSP
+            // 
+            this.labelLocateForSP.Location = new System.Drawing.Point(17, 146);
+            this.labelLocateForSP.Name = "labelLocateForSP";
+            this.labelLocateForSP.Size = new System.Drawing.Size(109, 23);
+            this.labelLocateForSP.TabIndex = 78;
+            this.labelLocateForSP.Text = "Locate for SP#";
+            // 
+            // P44
+            // 
+            this.ApvChkValue = "New";
+            this.ClientSize = new System.Drawing.Size(962, 449);
+            this.DefaultControl = "editRemark";
+            this.DefaultControlForEdit = "editRemark";
+            this.DefaultOrder = "Issuedate,ID";
+            this.Grid2New = 0;
+            this.GridAlias = "AdjustLocal_detail";
+            this.GridNew = 0;
+            this.GridUniqueKey = "poid,refno,color";
+            this.IsSupportConfirm = true;
+            this.IsSupportCopy = false;
+            this.IsSupportPrint = false;
+            this.IsSupportUnconfirm = true;
+            this.KeyField1 = "ID";
+            this.Name = "P44";
+            this.Text = "P44. Adjust Scrap Qty (Local Inventory)";
+            this.UnApvChkValue = "Confirmed";
+            this.UniqueExpress = "id";
+            this.WorkAlias = "AdjustLocal";
+            this.Controls.SetChildIndex(this.tabs, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.detailgridbs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.detailgrid2bs)).EndInit();
+            this.masterpanel.ResumeLayout(false);
+            this.masterpanel.PerformLayout();
+            this.detailpanel.ResumeLayout(false);
+            this.detail2.ResumeLayout(false);
+            this.detailpanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).EndInit();
+            this.detail.ResumeLayout(false);
+            this.detailcont.ResumeLayout(false);
+            this.detailbtm.ResumeLayout(false);
+            this.detailbtm.PerformLayout();
+            this.tabs.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.DisplayBox displayStocktakingID;
+        private Win.UI.Label labelStocktakingID;
+        private Win.UI.EditBox editRemark;
+        private Win.UI.Label labelRemark;
+        private Win.UI.Button btnImport;
+        private Win.UI.DateBox dateIssueDate;
+        private Win.UI.DisplayBox displayID;
+        private Win.UI.Label label25;
+        private Win.UI.Label labelIssueDate;
+        private Win.UI.Label labelID;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
+        private Win.UI.Button btnFind;
+        private Win.UI.TextBox txtLocateForSP;
+        private Win.UI.Label labelLocateForSP;
+    }
+}
