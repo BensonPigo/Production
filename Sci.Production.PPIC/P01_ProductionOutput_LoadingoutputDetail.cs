ab1a6c31b0851c931b2636d196b2ad3c54644276
JEFF <jeff.yeh@sportscity.com.tw>
2017-07-31 17:12:42 +0800

    0006041: PPIC_P01_ProductionOutput add [Loading Qutput] sheet 新增[Loading Qutput]頁籤，格式參考[Sewing Qutput]頁籤，[ppic_p01_English.jpg]。 1.[Loading Q'ty]的計算邏輯參考[Subcon][R44][Accu. Loading Qty of Garment]算法取得成衣件數。
    2.double click sql sample:
    select BIO.InComing, BIO.OutGoing, BIO.BundleNo, B.FabricPanelCode, BD.Patterncode, BD.Qty
    from Bundle B
    left join Bundle_Detail BD on BD.Id=B.ID
    left join BundleInOut BIO on BIO.BundleNo=BD.BundleNo
    where Orderid='17042215MM' and BIO.SubProcessId='LOADING' and B.Article='BQ5485' and BD.Sizecode='32'
    order by BIO.InComing
    
    3.double click sql sample:
    select BIO.InComing, BIO.OutGoing, BIO.BundleNo, B.FabricPanelCode, BD.Patterncode, BD.Qty
    from Bundle B
    left join Bundle_Detail BD on BD.Id=B.ID
    left join BundleInOut BIO on BIO.BundleNo=BD.BundleNo
    where Orderid='17042215MM' and BIO.SubProcessId='LOADING'
    order by BIO.InComing
    1.加上頁面Loading Q'ty
    直接把SubconR44的SQL最後加上條件和兩個欄位 再和Color Size OrderQty Table組起來
    2.新增程式Loading Output Detail
    加上event
    如說明SQL

---
+++ Sci.Production.PPIC/P01_ProductionOutput_LoadingoutputDetail.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict;
+using Ict.Win;
+using Sci.Data;
+
+namespace Sci.Production.PPIC
+{
+    public partial class P01_ProductionOutput_LoadingoutputDetail : Sci.Win.Subs.Base
+    {
+        string orderID, type, article, sizeCode;
+        public P01_ProductionOutput_LoadingoutputDetail(string OrderID, string Type, string Article, string SizeCode)
+        {
+            InitializeComponent();
+            orderID = OrderID;
+            type = Type;
+            article = Article;
+            sizeCode = SizeCode;
+            Text = "Loading Output Output - " + orderID;
+        }
+
+        protected override void OnFormLoaded()
+        {
+            base.OnFormLoaded();
+            //設定Grid1的顯示欄位
+            this.gridSewingDailyOutput.IsEditingReadOnly = true;
+            this.gridSewingDailyOutput.DataSource = listControlBindingSource1;
+            Helper.Controls.Grid.Generator(this.gridSewingDailyOutput)
+                 .DateTime("InComing", header: "InComing", width: Widths.AnsiChars(20))
+                 .DateTime("OutGoing", header: "OutGoing", width: Widths.AnsiChars(20))
+                 .Text("BundleNo", header: "BundleNo", width: Widths.AnsiChars(16))
+                 .Text("FabricPanelCode", header: "FabricPanelCode", width: Widths.AnsiChars(2))
+                 .Text("Patterncode", header: "PanelCode", width: Widths.AnsiChars(2))
+                 .Numeric("Qty", header: "Qty", width: Widths.AnsiChars(4));
+
+            string sqlCmd;
+            if (type == "A")
+            {
+                sqlCmd = string.Format(@"select BIO.InComing, BIO.OutGoing, BIO.BundleNo, B.FabricPanelCode, BD.Patterncode, BD.Qty
+from Bundle B
+left join Bundle_Detail BD on BD.Id=B.ID
+left join BundleInOut BIO on BIO.BundleNo=BD.BundleNo 
+where Orderid='{0}' and BIO.SubProcessId='LOADING'
+order by BIO.InComing", orderID);
+            }
+            else
+            {
+                sqlCmd = string.Format(@"select BIO.InComing, BIO.OutGoing, BIO.BundleNo, B.FabricPanelCode, BD.Patterncode, BD.Qty
+from Bundle B
+left join Bundle_Detail BD on BD.Id=B.ID
+left join BundleInOut BIO on BIO.BundleNo=BD.BundleNo 
+where Orderid='{0}' and BIO.SubProcessId='LOADING' and B.Article='{1}' and BD.Sizecode='{2}'
+order by BIO.InComing", orderID, article, sizeCode);
+            }
+
+            DataTable gridData;
+            DualResult result = DBProxy.Current.Select(null, sqlCmd, out gridData);
+            listControlBindingSource1.DataSource = gridData;
+        }
+    }
+}
