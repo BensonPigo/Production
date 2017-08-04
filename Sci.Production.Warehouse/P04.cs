2844534ad1fe9f9350390a87110309bd2ff4bde5
Jimmy <jimmy.liao@sportscity.com.tw>
2017-08-04 14:33:02 +0800

    Mantis_6124 1. P60 Local收貨時, 需把Local寫入LocalInventory.ALocation 	同SP# 同Refno 同Color時, 若多筆則用逗號隔開 2. P04 畫面加上欄位Bulk Location = LocalInventory.ALocation

--- Sci.Production.Warehouse/P04.cs
+++ Sci.Production.Warehouse/P04.cs
@@ -53,7 +53,8 @@ namespace Sci.Production.Warehouse
                 .Text("threadColor", header: "Thread Color", iseditingreadonly: true, width: Widths.AnsiChars(8))
                 .Numeric("inQty", header: "InQty", decimal_places: 2, integer_places: 10, iseditingreadonly: true, width: Widths.AnsiChars(6))
                 .Numeric("outQty", header: "OutQty", decimal_places: 2, integer_places: 10, iseditingreadonly: true, width: Widths.AnsiChars(6))
-                .Numeric("balance", header: "Balance", decimal_places: 2, integer_places: 10, iseditingreadonly: true, width: Widths.AnsiChars(6), settings: setBalance);
+                .Numeric("balance", header: "Balance", decimal_places: 2, integer_places: 10, iseditingreadonly: true, width: Widths.AnsiChars(6), settings: setBalance)
+                .EditText("Alocation", header: "Bulk Location", iseditingreadonly: true, width: Widths.AnsiChars(10));
             #endregion
         }
 
@@ -97,6 +98,7 @@ select  [sp] = l.OrderID
         , [inQty] = iif (l.InQty = 0, '', Convert (varchar, l.InQty))
         , [outQty] = iif (l.OutQty = 0, '', Convert (varchar, l.OutQty))
         , [Balance] = iif (InQty - OutQty + AdjustQty = 0, '', Convert (varchar, InQty - OutQty + AdjustQty))
+        , [ALocation] = l.ALocation
 from LocalInventory l
 left join LocalItem b on l.Refno=b.RefNo
 left join LocalSupp c on b.LocalSuppid=c.ID
