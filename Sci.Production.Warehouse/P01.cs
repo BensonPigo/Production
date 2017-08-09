a5d2701c1730b546d5d31a60a4c8cb7bae0078f1
Willy <willy.wei@sprotscity.com.tw>
2017-08-09 17:23:11 +0800

    0006087: Warehouse P01 add buttons (user feedback) 調整如下: 1.開啟P03 or P04後,Button 不能無效化,P01點選下一筆資料再按Button才能刷新P03 and P04畫面

--- Sci.Production.Warehouse/P01.cs
+++ Sci.Production.Warehouse/P01.cs
@@ -516,52 +516,48 @@ where o.ID = '{0}'", MyUtility.Convert.GetString(CurrentMaintain["ID"]))) ? Colo
 
         private void btnMeterialStatus_Click(object sender, EventArgs e)
         {
-            P03FormOpen();
+            if (callP03 != null && callP03.Visible == true)
+            {
+                callP03.P03Data(CurrentMaintain["ID"].ToString());
+            }
+            else
+            {
+                P03FormOpen();
+            }  
         }
 
         private void btnMeterialStatus_Local_Click(object sender, EventArgs e)
         {
-            P04FormOpen();          
+            if (callP04 != null && callP04.Visible == true)
+            {
+                callP04.P04Data(CurrentMaintain["ID"].ToString());
+            }
+            else
+            {
+                P04FormOpen();
+            }       
         }        
+
         Sci.Production.Warehouse.P03 callP03 = null;
         private void P03FormOpen()
         {
             callP03 = new P03(CurrentMaintain["ID"].ToString());
-            callP03.MdiParent = MdiParent;
-            this.btnMeterialStatus.Enabled = false;
-            callP03.FormClosed += (s, e) =>
-            {
-                this.btnMeterialStatus.Enabled = true;
-            };
+            callP03.MdiParent = MdiParent;                      
             callP03.Show();
-            
+            #region BackUP
+            //callP03.FormClosed += (s, e) =>
+            //{
+            //btnMeterialStatus.Enabled = true;
+            //};
+            #endregion
         }
+
         Sci.Production.Warehouse.P04 callP04 = null;
         private void P04FormOpen()
         {
             callP04 = new P04(CurrentMaintain["ID"].ToString());
             callP04.MdiParent = MdiParent;
-            this.btnMeterialStatus_Local.Enabled = false;
-            callP04.FormClosed += (s, e) =>
-            {
-                this.btnMeterialStatus_Local.Enabled = true;
-            };
             callP04.Show();
-
-        }
-        protected override void ClickMove(int direction)
-        {
-            base.ClickMove(direction);
-            if (MyUtility.Check.Empty(CurrentMaintain)) return;
-            //設定在detail頁,如果P03 or P04 開啟,按上下筆資料會連帶變動
-            if (callP03 != null && callP03.Visible == true)
-            {
-                callP03.P03Data(CurrentMaintain["ID"].ToString());             
-            }
-            if (callP04 != null && callP04.Visible == true)
-            {
-                callP04.P04Data(CurrentMaintain["ID"].ToString());
-            }
         }
 
     }
