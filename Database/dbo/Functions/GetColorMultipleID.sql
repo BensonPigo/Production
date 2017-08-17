084079106d88e4a6eccffb32ecace0ecdb27c641
JEFF <jeff.yeh@sportscity.com.tw>
2017-08-17 09:47:52 +0800

    add GetColorMultipleID

---
+++ Database/dbo/Functions/GetColorMultipleID.sql
@@ -0,0 +1,22 @@
+﻿
+Create Function [dbo].[GetColorMultipleID]
+	(
+	  @BrandID		VarChar(8)
+	 ,@ColorID		VarChar(6)
+	)
+Returns VarChar(500)
+As
+Begin
+	--Set NoCount On;
+	RETURN (	
+		
+		select 
+			m.ColorID + ','
+		from dbo.Color as c
+		LEFT join dbo.Color_multiple as m on m.ID = c.ID and m.BrandID = c.BrandId
+		where c.ID = @ColorID and c.BrandId = @BrandID
+		order by m.Seqno
+		for xml path('')
+	)
+	
+End
