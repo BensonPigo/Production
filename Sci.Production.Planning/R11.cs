e8675f2f688a221ff1ba83a0317b2d575e7fe162
Willy <willy.wei@sprotscity.com.tw>
2017-08-28 11:33:50 +0800

    0006623: Planning_R11 add [Article] column 調整如下: 1.Planning_R11.xltx 設定自動換行 2.R11 調整Article 固定欄寬

--- Sci.Production.Planning/R11.cs
+++ Sci.Production.Planning/R11.cs
@@ -427,7 +427,7 @@ drop table #tmpo,#tmpol,#tmp_AR_Basic,#tmp_A,#tmp_R,#tmp_P,#cls
             objApp.Cells.EntireRow.AutoFit();       //自動欄高
             objApp.Cells.EntireColumn.AutoFit();    //自動欄寬，不知為何還要多跑一次才會讓格式變美，先讓他多跑一次
             objApp.Cells.EntireRow.AutoFit();       //自動欄高
-
+            objSheets.get_Range("C1:C1").ColumnWidth = 50;
             #region Save & Show Excel
             string strExcelName = Sci.Production.Class.MicrosoftFile.GetName("Planning_R11");
             Microsoft.Office.Interop.Excel.Workbook workbook = objApp.ActiveWorkbook;
