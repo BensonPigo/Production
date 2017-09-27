71bbf230d95e6260ba581e9773cd577c2702ec0e
Jimmy <jimmy.liao@sportscity.com.tw>
2017-09-27 10:52:34 +0800

    Mantis_7630 Warehouse P10 1. 輸入 Request 後 NetQty 	改前：依照 Poid, Seq1, Seq2 先找【正常項】再找【非正常項】 	改後：依照 Poid, SciRefno, ColorID 先找【正常項】再找【非正常項】

--- Sci.Production.Warehouse/P10.cs
+++ Sci.Production.Warehouse/P10.cs
@@ -502,27 +502,33 @@ with main as(
                                                      and t.seq1 = c.seq1 
                                                      and t.seq2 = c.Seq2
     outer apply (
-        select top 1 NetQty = isnull (psd.NetQty, 0)
+        select top 1 NetQty = isnull (psdAll.NetQty, 0)
         from Cutplan_Detail_Cons cdc WITH (NOLOCK) 
         inner join PO_Supp_Detail psd WITH (NOLOCK) on psd.id = cdc.Poid 
                                                        and psd.seq1 = cdc.seq1 
                                                        and psd.seq2 = cdc.Seq2
-        where psd.NetQty != 0
-              and psd.NetQty is not null
-              and psd.StockPoid = ''
+        inner join Po_Supp_Detail psdAll WITH (NOLOCK) on psd.ID = psdAll.ID
+                                                         and psd.SciRefno = psdAll.SciRefno
+                                                         and psd.ColorID = psdAll.ColorID
+        where psdAll.NetQty != 0
+              and psdAll.NetQty is not null
+              and psdAll.StockPoid = ''
 			  and cdc.ID = c.ID
 			  and t.SCIRefno = psd.SCIRefno
 			  and t.ColorID = psd.ColorID
     ) Normal
     outer apply (
-        select top 1 NetQty = isnull (psd.NetQty, 0)
+        select top 1 NetQty = isnull (psdAll.NetQty, 0)
         from Cutplan_Detail_Cons cdc WITH (NOLOCK) 
         inner join PO_Supp_Detail psd WITH (NOLOCK) on psd.id = cdc.Poid 
                                                        and psd.seq1 = cdc.seq1 
                                                        and psd.seq2 = cdc.Seq2
-        where psd.NetQty != 0
-              and psd.NetQty is not null
-              and psd.StockPoid != ''
+        inner join Po_Supp_Detail psdAll WITH (NOLOCK) on psd.ID = psdAll.ID
+                                                         and psd.SciRefno = psdAll.SciRefno
+                                                         and psd.ColorID = psdAll.ColorID
+        where psdAll.NetQty != 0
+              and psdAll.NetQty is not null
+              and psdAll.StockPoid != ''
 			  and cdc.ID = c.ID
 			  and t.SCIRefno = psd.SCIRefno
 			  and t.ColorID = psd.ColorID
