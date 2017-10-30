ea03feac9a3a2b6c3a176deb736b5890e274e4c7
Jimmy <jimmy.liao@sportscity.com.tw>
2017-10-30 16:13:49 +0800

    Mantis_8307 & 8310 1. SewingOutput 新增資料表（第四層）記錄子單數量是從哪一張母單來的    Table : SewingOutput_Detail_Detail_Garment    Columns : ID, SewingOutput_DetailUKey, OrderId, ComboType, Article, SizeCode, OrderIDfrom, QAQty 2. 每日資料交換    Order_Qty_Garment 異動    須同步更新 【SewingOutput_Detail, SewingOutput_Detail_Detail, SewingOutput_Detail_Detail_Garment】    ※ 若 Packing 數量超過準備移除的數量，則該筆不處理 3. Sewing P01    a. Save 檢查時，母單第三層與子單第四層做比較 4. Sewing P03    a. Find 確認子單是否已被分配完畢使用 SewingOutput_Detail_Detail_Garment    b. 檢查 Order_Qty_Garment 異動，計算分配至子單數量時，使用 SewingOutput_Detail_Detail_Garment    c. 數量分配至子單時，須同步更新 SewingOutput_Detail_Detail_Garment 5. 舊資料填補    補資料，必須先補 AutoCreate = 1，後補 AutoCreate = 0    a. 子單 AutoCreate = 1，補第四層資料    b. 子單 AutoCreate 補母單數量，補子單第四層資料

---
+++ Database/dbo/Tables/SewingOutput_Detail_Detail_Garment.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[SewingOutput_Detail_Detail_Garment] (
+    [ID]                      VARCHAR (13) NOT NULL,
+    [SewingOutput_DetailUKey] BIGINT       NOT NULL,
+    [OrderId]                 VARCHAR (13) NOT NULL,
+    [ComboType]               VARCHAR (1)  NOT NULL,
+    [Article]                 VARCHAR (8)  NOT NULL,
+    [SizeCode]                VARCHAR (8)  NOT NULL,
+    [OrderIDfrom]             VARCHAR (13) NOT NULL,
+    [QAQty]                   INT          NOT NULL,
+    CONSTRAINT [PK_SewingOutput_Detail_Detail_Garment_1] PRIMARY KEY CLUSTERED ([ID] ASC, [SewingOutput_DetailUKey] ASC, [OrderId] ASC, [ComboType] ASC, [Article] ASC, [SizeCode] ASC, [OrderIDfrom] ASC)
+);
+
+
+
