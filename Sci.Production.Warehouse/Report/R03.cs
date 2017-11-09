7d68c8cf3fc09ef5459181951c7610ac91215b85
JEFF <jeff.yeh@sportscity.com.tw>
2017-11-09 16:10:33 +0800

    0008553: Warehouse_R03.efficiency optimizatio 01.優化一下這個報表導出的速度

--- Sci.Production.Warehouse/Report/R03.cs
+++ Sci.Production.Warehouse/Report/R03.cs
@@ -108,9 +108,9 @@ namespace Sci.Production.Warehouse
 select  F.MDivisionID
         ,O.FactoryID
         ,PS.id
-        ,style = (select StyleID from dbo.orders WITH (NOLOCK) where id = PS.id) 
+        ,style = si.StyleID
         ,PSD.FinalETD
-        ,supp = PS.suppid+'-'+S.NameEN 
+        ,supp = concat(PS.suppid,'-',S.NameEN )
         ,S.CountryID
         ,PSD.Refno
         ,PSD.SEQ1
@@ -120,7 +120,7 @@ select  F.MDivisionID
                         when 'A' then 'Accessory'
                         when 'O' then 'Other'
                       end 
-        ,LTRIM(RTRIM(dbo.getMtlDesc(PSD.id,PSD.seq1,PSD.seq2,2,0)))
+        ,dbo.getMtlDesc(PSD.id,PSD.seq1,PSD.seq2,2,0)
         ,PSD.Qty
         ,PSD.NETQty
         ,PSD.NETQty+PSD.LossQty
@@ -132,11 +132,7 @@ select  F.MDivisionID
         ,iif(PSD.Complete=1,'Y','N')
         --,PSD.ETA
         ,PSD.FinalETA
-        ,orderlist = (select t.orderid+',' 
-                      from (select OrderID 
-                            from DBO.PO_Supp_Detail_OrderList WITH (NOLOCK) 
-                            where id=PSD.id and seq1=PSD.seq1 and seq2 = PSD.SEQ2) t 
-                      for xml path('')) 
+        ,orderlist = a.orderlist
         ,MDPD.InQty
         ,PSD.StockUnit
         ,MDPD.OutQty
@@ -162,6 +158,17 @@ join dbo.Supp S on S.id = PS.SuppID
 join dbo.Orders O on o.id = PSD.id
 join dbo.Factory F on f.id = o.FactoryId
 left join dbo.MDivisionPoDetail MDPD on MDPD.POID = PSD.ID and MDPD.Seq1 = PSD.Seq1 and MDPD.Seq2 = PSD.Seq2
+outer apply(select StyleID from dbo.orders WITH (NOLOCK) where id = PS.id) si
+outer apply
+(
+	select orderlist = 
+	stuff((
+		select concat(',',OrderID)
+        from DBO.PO_Supp_Detail_OrderList WITH (NOLOCK) 
+        where id=PSD.id and seq1=PSD.seq1 and seq2 = PSD.SEQ2
+        for xml path('')
+	 ),1,1,'')
+)a
 where 1=1  
 "));
 
@@ -318,23 +325,16 @@ where 1=1
                 MyUtility.Msg.WarningBox("Data not found!");
                 return false;
             }
-
-            //MyUtility.Excel.CopyToXls(printData, "", "Warehouse_R03.xltx", 1);
+            
             Excel.Application objApp = MyUtility.Excel.ConnectExcel(Sci.Env.Cfg.XltPathDir + "\\Warehouse_R03.xltx"); //預先開啟excel app
-            //MyUtility.Excel.CopyToXls(printData, "", "Warehouse_R03.xltx", 1, showExcel: false, showSaveMsg: true, excelApp: objApp);
             this.ShowWaitMessage("Excel Processing...");
             Sci.Utility.Report.ExcelCOM com = new Sci.Utility.Report.ExcelCOM(Sci.Env.Cfg.XltPathDir + "\\Warehouse_R03.xltx", objApp);
+
+            //com.TransferArray_Limit = 200000;
+            com.ColumnsAutoFit = false;
             com.WriteTable(printData,2);
-
-
-            
-            Excel.Worksheet worksheet = objApp.Sheets[1];
-            worksheet.Columns[6].ColumnWidth = 35;
-            worksheet.Columns[23].ColumnWidth = 35;
-            worksheet.Columns[29].ColumnWidth = 35;
-            worksheet.Columns[30].ColumnWidth = 35;
-            //objApp.Rows.AutoFit();
-            //objApp.Columns.AutoFit();
+                        
+            //Excel.Worksheet worksheet = objApp.Sheets[1];
 
             //for (int i = 1; i <= printData.Rows.Count; i++)
             //{
@@ -348,7 +348,7 @@ where 1=1
             objApp.ActiveWorkbook.SaveAs(strExcelName);
             objApp.Quit();
             Marshal.ReleaseComObject(objApp);
-            Marshal.ReleaseComObject(worksheet);
+            //Marshal.ReleaseComObject(worksheet);
 
             strExcelName.OpenFile();
             #endregion
