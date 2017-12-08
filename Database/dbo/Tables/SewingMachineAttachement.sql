4088d1cd8100c58086c3ae8b952efa2321a4b138
JEFF <jeff.yeh@sportscity.com.tw>
2017-12-08 17:51:55 +0800

    0008822: IE P03. Line Mapping 增加兩個Table

---
+++ Database/dbo/Tables/SewingMachineAttachement.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[SewingMachineAttachement] (
+    [ID]          VARCHAR (20)   NOT NULL,
+    [Description] NVARCHAR (100) NULL,
+    [Junk]        BIT            NULL,
+    [AddName]     VARCHAR (10)   NULL,
+    [AddDate]     DATETIME       NULL,
+    [EditName]    VARCHAR (10)   NULL,
+    [EditDate]    DATETIME       NULL,
+    CONSTRAINT [PK_SewingMachineAttachement] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
