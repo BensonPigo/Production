37794c59a987db3cde8a782ac1dcaf1dd28187cb
JEFF <jeff.yeh@sportscity.com.tw>
2017-12-11 10:24:50 +0800

    0008822: IE P03. Line Mapping 新增B10/B11

---
+++ Sci.Production.IE/B10.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.IE
+{
+    /// <summary>
+    /// IE_B10
+    /// </summary>
+    public partial class B10 : Sci.Win.Tems.Input1
+    {
+        /// <summary>
+        /// B10
+        /// </summary>
+        /// <param name="menuitem">ToolStripMenuItem</param>
+        public B10(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// ClickEditAfter
+        /// </summary>
+        protected override void ClickEditAfter()
+        {
+            base.ClickEditAfter();
+            this.txtID.ReadOnly = true;
+        }
+    }
+}
---
+++ Sci.Production.IE/B11.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.IE
+{
+    /// <summary>
+    /// IE_B11
+    /// </summary>
+    public partial class B11 : Sci.Win.Tems.Input1
+    {
+        /// <summary>
+        /// B11
+        /// </summary>
+        /// <param name="menuitem">ToolStripMenuItem</param>
+        public B11(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// ClickEditAfter
+        /// </summary>
+        protected override void ClickEditAfter()
+        {
+            base.ClickEditAfter();
+            this.txtID.ReadOnly = true;
+        }
+    }
+}
