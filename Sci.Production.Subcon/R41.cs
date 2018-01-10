4f2102e3dffd06e5dd895ffb88dbd949fedb043d
JEFF <jeff.yeh@sportscity.com.tw>
2018-01-10 09:27:21 +0800

    1. 變更前三欄位至少輸入一個 2. 變更CutRef條件用<=和>=

--- Sci.Production.Subcon/R41.cs
+++ Sci.Production.Subcon/R41.cs
@@ -50,11 +50,6 @@ namespace Sci.Production.Subcon
         // 驗證輸入條件
         protected override bool ValidateInput()
         {
-            if (MyUtility.Check.Empty(dateBundleCDate.Value1) && MyUtility.Check.Empty(dateBundleCDate.Value2))
-            {
-                MyUtility.Msg.WarningBox("Bundel CDate can't empty!!");
-                return false;
-            }
             SubProcess = this.comboSubProcess.Text;
             SP = this.txtSPNo.Text;
             M = this.comboM.Text;
@@ -63,99 +58,19 @@ namespace Sci.Production.Subcon
             CutRef2 = this.txtCutRefEnd.Text;
             dateBundle1 = this.dateBundleCDate.Value1;
             dateBundle2 = this.dateBundleCDate.Value2;
+            if (MyUtility.Check.Empty(CutRef1) && MyUtility.Check.Empty(CutRef2) &&
+                MyUtility.Check.Empty(SP) &&
+                MyUtility.Check.Empty(dateBundleCDate.Value1) && MyUtility.Check.Empty(dateBundleCDate.Value2))
+            {
+                MyUtility.Msg.WarningBox("[Cut Ref#][SP#][Bundle CDate] can not all empty !!");
+                return false;
+            }
             return base.ValidateInput();
         }
 
         //非同步讀取資料
         protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
         {
-            //            #region sqlcmd
-            //            StringBuilder sqlCmd = new StringBuilder();
-            //            sqlCmd.Append(@"
-            //Select DISTINCT
-            //    [Bundleno] = bd.BundleNo,
-            //    [Cut Ref#] = b.CutRef,
-            //    [SP#] = b.Orderid,
-            //    [Master SP#] = b.POID,
-            //    [M] = b.MDivisionid,
-            //    [Factory] = o.FtyGroup,
-            //    [Style] = o.StyleID,
-            //    [Season] = o.SeasonID,
-            //    [Brand] = o.BrandID,
-            //    [Comb] = b.PatternPanel,
-            //	[Fab_Panel Code] = b.FabricPanelCode,
-            //    [Article] = b.Article,
-            //    [Color] = b.ColorId,
-            //    [Line] = b.SewinglineId,
-            //    [Cell] = b.SewingCell,
-            //    [Pattern] = bd.PatternCode,
-            //    [PtnDesc] = bd.PatternDesc,
-            //    [Group] = bd.BundleGroup,
-            //    [Size] = bd.SizeCode,
-            //    [Artwork] = sub.sub,
-            //    [Qty] = bd.Qty,
-            //    [Sub-process] = s.Id,
-            //    [InComing] = bio.InComing,
-            //    [Out (Time)] = bio.OutGoing
-
-            //from Bundle b WITH (NOLOCK) 
-            //inner join Bundle_Detail bd WITH (NOLOCK) on bd.Id = b.Id
-            //left join Bundle_Detail_Art bda WITH (NOLOCK) on bda.Id = bd.Id and bda.Bundleno = bd.Bundleno
-            //inner join orders o WITH (NOLOCK) on o.Id = b.OrderId
-            //inner join SubProcess s WITH (NOLOCK) on (s.IsRFIDDefault = 1 or s.Id = bda.SubprocessId) 
-            //inner join BundleInOut bio WITH (NOLOCK) on bio.Bundleno=bd.Bundleno and bio.SubProcessId = s.Id
-            //outer apply(
-            //	    select sub= stuff((
-            //		    Select distinct concat('+', bda.SubprocessId)
-            //		    from Bundle_Detail_Art bda WITH (NOLOCK) 
-            //		    where bda.Id = bd.Id and bda.Bundleno = bd.Bundleno
-            //		    for xml path('')
-            //	    ),1,1,'')
-            //) as sub
-
-            //where 1=1
-            //            ");
-            //            #endregion
-            //            #region Append畫面上的條件
-            //            if (!MyUtility.Check.Empty(SubProcess))
-            //            {
-            //                sqlCmd.Append(string.Format(@" and (s.Id = '{0}' or '{0}' = 'ALL') ", SubProcess));
-            //            }
-            //            if (!MyUtility.Check.Empty(CutRef1) && (!MyUtility.Check.Empty(CutRef2)))
-            //            {
-            //                sqlCmd.Append(string.Format(@" and b.CutRef between '{0}' and '{1}'", CutRef1, CutRef2));
-            //            }
-            //            if (!MyUtility.Check.Empty(SP))
-            //            {
-            //                sqlCmd.Append(string.Format(@" and b.Orderid = '{0}'", SP));
-            //            }
-            //            if (!MyUtility.Check.Empty(dateBundle1))
-            //            {
-            //                sqlCmd.Append(string.Format(@" and b.Cdate >= '{0}'",Convert.ToDateTime(dateBundle1).ToString("d")));
-            //            }
-            //            if (!MyUtility.Check.Empty(dateBundle2))
-            //            {
-            //                sqlCmd.Append(string.Format(@" and b.Cdate <= '{0}'",Convert.ToDateTime(dateBundle2).ToString("d")));
-            //            }
-            //            if (!MyUtility.Check.Empty(M))
-            //            {
-            //                sqlCmd.Append(string.Format(@" and b.MDivisionid = '{0}'", M));
-            //            }
-            //            if (!MyUtility.Check.Empty(Factory))
-            //            {
-            //                sqlCmd.Append(string.Format(@" and o.FtyGroup = '{0}'", Factory));
-            //            }
-            //            #endregion
-
-            //            sqlCmd.Append(@"order by [Bundleno],[Cut Ref#],[SP#],[Style],[Season],[Brand],[Article],[Color],[Line],[Cell],[Pattern],[PtnDesc],[Group],[Size],[Out (Time)] desc,[InComing] desc");
-
-            //            DualResult result = DBProxy.Current.Select(null, sqlCmd.ToString(), out printData);
-
-            //            if (!result)
-            //            {
-            //                DualResult failResult = new DualResult(false, "Query data fail\r\n" + result.ToString());
-            //                return failResult;
-            //            }
             return Result.True;
         }
 
@@ -216,9 +131,13 @@ where 1=1
             {
                 sqlCmd.Append(string.Format(@" and (s.Id = '{0}' or '{0}' = 'ALL') ", SubProcess));
             }
-            if (!MyUtility.Check.Empty(CutRef1) && (!MyUtility.Check.Empty(CutRef2)))
+            if (!MyUtility.Check.Empty(CutRef1))
             {
-                sqlCmd.Append(string.Format(@" and b.CutRef between '{0}' and '{1}'", CutRef1, CutRef2));
+                sqlCmd.Append(string.Format(@" and b.CutRef >= '{0}' ", CutRef1));
+            }
+            if (!MyUtility.Check.Empty(CutRef2))
+            {
+                sqlCmd.Append(string.Format(@" and b.CutRef <= '{0}' ", CutRef2));
             }
             if (!MyUtility.Check.Empty(SP))
             {
