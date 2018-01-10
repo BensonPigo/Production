94cb39ae51fdeecc10d9ff0f3b97785ea8080a89
JEFF <jeff.yeh@sportscity.com.tw>
2018-01-10 10:10:49 +0800

    9651 完成B47

---
+++ Sci.Production.Shipping/B47.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+
+namespace Sci.Production.Shipping
+{
+    /// <summary>
+    /// Packing_B01
+    /// </summary>
+    public partial class B47 : Sci.Win.Tems.Input1
+    {
+        /// <summary>
+        /// B47
+        /// </summary>
+        /// <param name="menuitem">menuitem</param>
+        public B47(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            this.InitializeComponent();
+        }
+    }
+}
