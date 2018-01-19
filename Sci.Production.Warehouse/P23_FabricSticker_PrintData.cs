9d660bf4f1fc4e1bbad7545a51f3097ef0e51a4d
Jimmy <jimmy.liao@sportscity.com.tw>
2018-01-19 13:45:28 +0800

    Mantis 9468 Warehouse P23 	1. 新增 Btn Print Fabric Sticker
    新增 Warehoue
    	1. P23_FabricSticker
    	2. P23_FabricSticker_Print.rdlc

---
+++ Sci.Production.Warehouse/P23_FabricSticker_PrintData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sci.Production.Warehouse
+{
+    class P23_FabricSticker_PrintData
+    {
+        public int RowNo { get; set; }
+        public string InventorySPNo { get; set; }
+        public string InventorySeq { get; set; }
+        public string BulkSPNo { get; set; }
+        public string BulkSeq { get; set; }
+        public string Roll { get; set; }
+        public string Dyelot { get; set; }
+        public string ToFactory { get; set; }
+        public string RefNo { get; set; }
+        public string Color { get; set; }
+        public string StockUnit { get; set; }
+        public string Qty { get; set; }
+    }
+}
