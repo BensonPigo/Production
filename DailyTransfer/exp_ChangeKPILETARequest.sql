a6d4ba35cf0149d79e6e128911d9eb64886259e4
ALGER <alger.song@sportscity.com.tw>
2018-02-06 13:47:45 +0800

    應愛德華要求，將轉出日期區間加到Pms_To_Trade.dbo.DateInfo。 NAME='ChangeKPILETARequest'

--- DailyTransfer/exp_ChangeKPILETARequest.sql
+++ DailyTransfer/exp_ChangeKPILETARequest.sql
@@ -10,6 +10,26 @@ BEGIN
   DROP TABLE ChangeKPILETARequest
 END
 
+
+declare @DateStart date= CONVERT(DATE,DATEADD(day,-30,GETDATE()));
+declare @DateEnd date=CONVERT(date, GETDATE());
+declare @DateInfoName varchar(30) ='ChangeKPILETARequest';
+
+--新增區間資料到DateInfo name='ChangeKPILETARequest' DateStart=轉檔日期前30天 DateEnd=轉檔日期
+If Exists (Select 1 From Pms_To_Trade.dbo.DateInfo Where Name = @DateInfoName )
+Begin
+	update Pms_To_Trade.dbo.dateInfo
+	set DateStart=@DateStart,
+	DateEnd=@DateEnd
+	Where Name = @DateInfoName 
+end;
+Else
+Begin 
+	insert into Pms_To_Trade.dbo.dateInfo(Name,DateStart,DateEnd)
+	values (@DateInfoName,@DateStart,@DateEnd);
+end;
+
+
 SELECT *
 INTO ChangeKPILETARequest
 FROM Production.dbo.ChangeKPILETARequest
