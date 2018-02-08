96ac97f6ce3fa668dae7d9731a8bea7e3571280b
JEFF <jeff.yeh@sportscity.com.tw>
2018-02-08 15:44:37 +0800

    ISP20180056

--- Sci.Production.SubProcess/P10.cs
+++ Sci.Production.SubProcess/P10.cs
@@ -859,7 +859,7 @@ insert (
 values (
 	s.OrderID	, s.[Group]	, s.SubProcessLineID	, s.OutputQty				, s.TargetQty
 	, s.Feature	, s.SMV		, s.EarlyInline			, s.SubProcessLearnCurveID	, s.Inline
-	, s.Offline	, GETDATE()	, @UserName, @MDivisionID,s.Overload);
+	, s.Offline	, GETDATE()	, @UserName, @MDivisionID,isnull(s.Overload,0));
 select @@IDENTITY";
                         List<DataRow> listTmpTable = new List<DataRow>();
                         listTmpTable.Add(drPPASchedule);
