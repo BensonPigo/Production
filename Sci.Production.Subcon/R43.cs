c2a8d6d7921537f49b240b334bb9ad3b76d67069
DOMAIN\aaron.shie <onizuka865@gmail.com>
2018-03-02 11:30:52 +0800

    -ISP20180103 BCS公式請幫忙改成 Round ((sum (DetailData.[Release Qty]) / sum (DetailData.[Receive Qty]) * 100), 2)

--- Sci.Production.Subcon/R43.cs
+++ Sci.Production.Subcon/R43.cs
@@ -94,7 +94,7 @@ select DetailData.M
        , DetailData.[Sub-Process]
        , [Receive Qty] = sum (DetailData.[Receive Qty])
        , [Release Qty] = sum (DetailData.[Release Qty])
-       , BCS = Round ((sum (DetailData.[Receive Qty]) / sum (DetailData.[Release Qty]) * 100), 2)
+       , BCS = Round ((sum (DetailData.[Release Qty]) / sum (DetailData.[Receive Qty]) * 100), 2)
 from (
     select [M] = b.MDivisionid
            , [Factory] = o.FtyGroup
