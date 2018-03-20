2699e1cd47e9eb2a77758418279c089e39863ff8
Willy <willy.wei@sprotscity.com.tw>
2018-03-20 13:42:01 +0800

    ISP20180141 Create Table :TransferToCFA,ClogReceiveCFA,CFAReceive,CFAReturn add columns in PackingList: TransferCFADate,CFAReceiveDate,CFAReturnFtyDate,CFAReturnClogDate,ClogReceiveCFADate,CFANeedInsp add columns in orders :CfaCTN

---
+++ Database/dbo/Tables/TransferToCFA.sql
@@ -0,0 +1,84 @@
+﻿CREATE TABLE [dbo].[TransferToCFA]
+(
+	[ID] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    [TransferDate] DATE NULL, 
+    [MDivisionID] VARCHAR(8) NULL DEFAULT (''), 
+    [OrderID] VARCHAR(13) NULL DEFAULT (''), 
+    [PackingListID] VARCHAR(13) NULL DEFAULT (''), 
+    [CTNStartNo] VARCHAR(6) NULL DEFAULT (''), 
+    [AddName] VARCHAR(10) NULL DEFAULT (''), 
+    [AddDate] DATETIME NULL
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Identity',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'TransferToCFA',
+    @level2type = N'COLUMN',
+    @level2name = N'ID'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'移轉日期',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'TransferToCFA',
+    @level2type = N'COLUMN',
+    @level2name = N'TransferDate'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'M',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'TransferToCFA',
+    @level2type = N'COLUMN',
+    @level2name = N'MDivisionID'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'訂單號碼',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'TransferToCFA',
+    @level2type = N'COLUMN',
+    @level2name = N'OrderID'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'PackID',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'TransferToCFA',
+    @level2type = N'COLUMN',
+    @level2name = N'PackingListID'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'箱號',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'TransferToCFA',
+    @level2type = N'COLUMN',
+    @level2name = N'CTNStartNo'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'新增人員',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'TransferToCFA',
+    @level2type = N'COLUMN',
+    @level2name = N'AddName'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'新增時間',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'TransferToCFA',
+    @level2type = N'COLUMN',
+    @level2name = N'AddDate'
