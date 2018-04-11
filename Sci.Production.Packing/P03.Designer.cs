7e4941e97f3404182478f0118bf116de11fcd1c1
sinfy <sinfy.yeh@sportscity.com.tw>
2018-04-11 14:00:42 +0800

    Sci Div -> Sci Dlv

--- Sci.Production.Packing/P03.Designer.cs
+++ Sci.Production.Packing/P03.Designer.cs
@@ -793,7 +793,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(127, 22);
             this.label1.TabIndex = 64;
-            this.label1.Text = "SCI Div.";
+            this.label1.Text = "SCI Dlv.";
             // 
             // label2
             // 
