4ce834fe52cf123a1a27a8e5051a648c1c697e38
JEFF <jeff.yeh@sportscity.com.tw>
2018-04-16 16:12:39 +0800

    ISP20180190 完成QAP25

---
+++ Sci.Production.Quality/P25.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using Ict;
+using Ict.Win;
+using Sci.Data;
+using System.Runtime.InteropServices;
+
+namespace Sci.Production.Quality
+{
+    /// <summary>
+    /// Quality_P25
+    /// </summary>
+    public partial class P25 : Sci.Win.Tems.QueryForm
+    {
+        /// <summary>
+        /// P25
+        /// </summary>
+        /// <param name="menuitem">ToolStripMenuItem</param>
+        public P25(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            this.InitializeComponent();
+        }
+
+        private DataTable gridData;
+        private string selectDataTable_DefaultView_Sort = string.Empty;
+        private Ict.Win.UI.DataGridViewCheckBoxColumn col_chk;
+
+        /// <summary>
+        /// OnFormLoaded()
+        /// </summary>
+        protected override void OnFormLoaded()
+        {
+            base.OnFormLoaded();
+
+            // Grid設定
+            this.gridReceiveDate.IsEditingReadOnly = false;
+            this.gridReceiveDate.DataSource = this.listControlBindingSource1;
+            this.Helper.Controls.Grid.Generator(this.gridReceiveDate)
+            .CheckBox("Selected", header: string.Empty, width: Widths.AnsiChars(3), iseditable: true, trueValue: 1, falseValue: 0).Get(out this.col_chk)
+            .Date("ReceiveDate", header: "Receive Date", iseditable: false)
+            .Text("PackingListID", header: "Pack ID", width: Widths.Auto(), iseditable: false)
+            .Text("CTNStartNo", header: "CTN#", width: Widths.Auto(), iseditable: false)
+            .Text("OrderID", header: "SP#", width: Widths.Auto(), iseditable: false)
+            .Text("CustPONo", header: "PO No.", width: Widths.Auto(), iseditable: false)
+            .Text("StyleID", header: "Style#", width: Widths.Auto(), iseditable: false)
+            .Text("BrandID", header: "Brand", width: Widths.Auto(), iseditable: false)
+            .Text("Dest", header: "Destination", width: Widths.Auto(), iseditable: false)
+            .Date("BuyerDelivery", header: "Buyer Delivery", width: Widths.Auto(), iseditable: false)
+            .Date("SciDelivery", header: "SCI Delivery", width: Widths.Auto(), iseditable: false)
+            .Text("AddName", header: "Received By", width: Widths.Auto(), iseditable: false);
+
+            // 增加CTNStartNo 有中文字的情況之下 按照我們希望的順序排
+            int rowIndex = 0;
+            int columIndex = 0;
+            this.gridReceiveDate.CellClick += (s, e) =>
+            {
+                rowIndex = e.RowIndex;
+                columIndex = e.ColumnIndex;
+            };
+
+            this.gridReceiveDate.Sorted += (s, e) =>
+            {
+                #region 如果準備排序的欄位 = "CTNStartNo" 則用以下方法排序
+                if ((rowIndex == -1) && this.gridData.Columns[columIndex].ColumnName.ToString().EqualString("CTNStartNo"))
+                {
+                    this.listControlBindingSource1.DataSource = null;
+
+                    if (this.selectDataTable_DefaultView_Sort == "DESC")
+                    {
+                        this.gridData.DefaultView.Sort = "rn DESC";
+                        this.selectDataTable_DefaultView_Sort = string.Empty;
+                    }
+                    else
+                    {
+                        this.gridData.DefaultView.Sort = "rn ASC";
+                        this.selectDataTable_DefaultView_Sort = "DESC";
+                    }
+
+                    this.listControlBindingSource1.DataSource = this.gridData;
+                    return;
+                }
+                #endregion
+            };
+        }
+
+        // Query
+        private void BtnQuery_Click(object sender, EventArgs e)
+        {
+            this.ShowWaitMessage("Data Loading...");
+            StringBuilder sqlCmd = new StringBuilder();
+            sqlCmd.Append(string.Format(
+                @"
+select  1 as selected
+        , ReceiveDate
+        , PackingListID
+        , CTNStartNo
+        , OrderID
+        , CustPONo
+        , StyleID
+        , BrandID
+        , Dest
+        , BuyerDelivery
+        , SCIDelivery
+        , AddName
+        , rn = ROW_NUMBER() over(order by TRY_CONVERT(int, CTNStartNo) ,(RIGHT(REPLICATE('0', 6) + rtrim(ltrim(CTNStartNo)), 6)))
+from (
+    select  cr.ReceiveDate
+            , cr.PackingListID
+            , cr.CTNStartNo
+            , cr.OrderID
+            , isnull(o.CustPONo,'') as CustPONo
+            , isnull(o.StyleID,'') as StyleID
+            , isnull(o.BrandID,'') as BrandID
+            , isnull(c.Alias,'') as Dest
+            , o.BuyerDelivery
+            , o.SciDelivery
+            , AddName = dbo.getPass1(cr.AddName)
+    from CFAReceive cr WITH (NOLOCK) 
+    left join Orders o WITH (NOLOCK) on cr.OrderID =  o.ID
+    left join Country c WITH (NOLOCK) on o.Dest = c.ID
+    where   cr.MDivisionID = '{0}'
+", Sci.Env.User.Keyword));
+
+            if (!MyUtility.Check.Empty(this.dateReceiveDate.Value1))
+            {
+                sqlCmd.Append(string.Format(
+                    @" 
+            and cr.ReceiveDate >= '{0}'", Convert.ToDateTime(this.dateReceiveDate.Value1).ToString("d")));
+            }
+
+            if (!MyUtility.Check.Empty(this.dateReceiveDate.Value2))
+            {
+                sqlCmd.Append(string.Format(
+                    @" 
+            and cr.ReceiveDate <= '{0}'", Convert.ToDateTime(this.dateReceiveDate.Value2).ToString("d")));
+            }
+
+            if (!MyUtility.Check.Empty(this.txtPackID.Text))
+            {
+                sqlCmd.Append(string.Format(
+                    @" 
+            and cr.PackingListID = '{0}'", MyUtility.Convert.GetString(this.txtPackID.Text)));
+            }
+
+            if (!MyUtility.Check.Empty(this.txtSPNo.Text))
+            {
+                sqlCmd.Append(string.Format(
+                    @" 
+            and cr.OrderID = '{0}'", MyUtility.Convert.GetString(this.txtSPNo.Text)));
+            }
+
+            sqlCmd.Append(@"
+)a
+order by PackingListID,rn,ReceiveDate");
+
+            DualResult result = DBProxy.Current.Select(null, sqlCmd.ToString(), out this.gridData);
+            if (!result)
+            {
+                MyUtility.Msg.WarningBox("Query data fail.\r\n" + result.ToString());
+            }
+
+            this.listControlBindingSource1.DataSource = this.gridData;
+            this.gridReceiveDate.AutoResizeColumns();
+            this.HideWaitMessage();
+        }
+
+        // Close
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
