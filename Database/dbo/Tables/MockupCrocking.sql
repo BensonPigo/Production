40109cfa3a7cd05b4f566ebf39ad8ef5fd122976
DOMAIN\aaron.shie <onizuka865@gmail.com>
2018-04-19 17:58:29 +0800

    -ISP20180198 QA P11. Laboratory(Sample) - Mockup Crocking Test add New Program QA P11

---
+++ Database/dbo/Tables/MockupCrocking.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[MockupCrocking]
+(
+	[ID] VARCHAR(13) NOT NULL PRIMARY KEY, 
+    [BrandID] VARCHAR(8) NULL, 
+    [StyleID] VARCHAR(15) NULL, 
+    [SeasonID] VARCHAR(8) NULL, 
+    [Article] VARCHAR(8) NULL, 
+    [ReceivedDate] DATETIME NULL, 
+    [ReleasedDate] DATETIME NULL, 
+    [T1Subcon] VARCHAR(8) NULL, 
+    [Remark] VARCHAR(300) NULL DEFAULT (''), 
+    [AddDate] DATETIME NULL, 
+    [AddName] VARCHAR(10) NULL DEFAULT (''), 
+    [EditDate] DATETIME NULL, 
+    [EditName] VARCHAR(10) NULL DEFAULT ('')
+)
