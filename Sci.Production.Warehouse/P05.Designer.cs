4babce58a7b72059c25b05a9898a4a63a5e3a353
JEFF <jeff.yeh@sportscity.com.tw>
2018-05-15 14:55:11 +0800

    ISP20180263 新增 P05

---
+++ Sci.Production.Warehouse/P05.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Sci.Production.Warehouse
+{
+    partial class P05
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listControlBindingSource1 = new Sci.Win.UI.ListControlBindingSource(this.components);
+            this.panel1 = new Sci.Win.UI.Panel();
+            this.txtSPNo = new Sci.Win.UI.TextBox();
+            this.labelSPNo = new Sci.Win.UI.Label();
+            this.grid1 = new Sci.Win.UI.Grid();
+            this.label1 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.grid2 = new Sci.Win.UI.Grid();
+            this.checkBox1 = new Sci.Win.UI.CheckBox();
+            this.btnAutoCal = new System.Windows.Forms.Button();
+            this.listControlBindingSource2 = new Sci.Win.UI.ListControlBindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtSPNo);
+            this.panel1.Controls.Add(this.labelSPNo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1008, 49);
+            this.panel1.TabIndex = 3;
+            // 
+            // txtSPNo
+            // 
+            this.txtSPNo.BackColor = System.Drawing.Color.White;
+            this.txtSPNo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtSPNo.Location = new System.Drawing.Point(121, 12);
+            this.txtSPNo.Name = "txtSPNo";
+            this.txtSPNo.Size = new System.Drawing.Size(145, 23);
+            this.txtSPNo.TabIndex = 0;
+            this.txtSPNo.Validating += new System.ComponentModel.CancelEventHandler(this.txtSPNo_Validating);
+            // 
+            // labelSPNo
+            // 
+            this.labelSPNo.Location = new System.Drawing.Point(9, 12);
+            this.labelSPNo.Name = "labelSPNo";
+            this.labelSPNo.Size = new System.Drawing.Size(109, 23);
+            this.labelSPNo.TabIndex = 18;
+            this.labelSPNo.Text = "SP#";
+            // 
+            // grid1
+            // 
+            this.grid1.AllowUserToAddRows = false;
+            this.grid1.AllowUserToDeleteRows = false;
+            this.grid1.AllowUserToResizeRows = false;
+            this.grid1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grid1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.grid1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.grid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid1.DataSource = this.listControlBindingSource1;
+            this.grid1.EditingEnter = Ict.Win.UI.DataGridViewEditingEnter.NextCellOrNextRow;
+            this.grid1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.grid1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.grid1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(162)))), ((int)(((byte)(163)))));
+            this.grid1.Location = new System.Drawing.Point(0, 78);
+            this.grid1.Name = "grid1";
+            this.grid1.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.grid1.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.grid1.RowTemplate.Height = 24;
+            this.grid1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grid1.ShowCellToolTips = false;
+            this.grid1.Size = new System.Drawing.Size(1008, 287);
+            this.grid1.TabIndex = 4;
+            this.grid1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(9, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 23);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Order List";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(9, 391);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 23);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Material List";
+            // 
+            // grid2
+            // 
+            this.grid2.AllowUserToAddRows = false;
+            this.grid2.AllowUserToDeleteRows = false;
+            this.grid2.AllowUserToResizeRows = false;
+            this.grid2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grid2.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.grid2.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.grid2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid2.DataSource = this.listControlBindingSource2;
+            this.grid2.EditingEnter = Ict.Win.UI.DataGridViewEditingEnter.NextCellOrNextRow;
+            this.grid2.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.grid2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.grid2.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(162)))), ((int)(((byte)(163)))));
+            this.grid2.Location = new System.Drawing.Point(0, 417);
+            this.grid2.Name = "grid2";
+            this.grid2.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.grid2.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.grid2.RowTemplate.Height = 24;
+            this.grid2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grid2.ShowCellToolTips = false;
+            this.grid2.Size = new System.Drawing.Size(1008, 242);
+            this.grid2.TabIndex = 21;
+            this.grid2.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ForeColor = System.Drawing.Color.Black;
+            this.checkBox1.Location = new System.Drawing.Point(444, 391);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(302, 21);
+            this.checkBox1.TabIndex = 22;
+            this.checkBox1.Text = "Usable Qty include On Warehouse Qty only";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // btnAutoCal
+            // 
+            this.btnAutoCal.Location = new System.Drawing.Point(444, 52);
+            this.btnAutoCal.Name = "btnAutoCal";
+            this.btnAutoCal.Size = new System.Drawing.Size(131, 23);
+            this.btnAutoCal.TabIndex = 23;
+            this.btnAutoCal.Text = "Auto-Calculate";
+            this.btnAutoCal.UseVisualStyleBackColor = true;
+            this.btnAutoCal.Click += new System.EventHandler(this.btnAutoCal_Click);
+            // 
+            // P05
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 661);
+            this.Controls.Add(this.btnAutoCal);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.grid2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.grid1);
+            this.Controls.Add(this.panel1);
+            this.Name = "P05";
+            this.Text = "P05. Material Consumption Calculation";
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.Controls.SetChildIndex(this.grid1, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.grid2, 0);
+            this.Controls.SetChildIndex(this.checkBox1, 0);
+            this.Controls.SetChildIndex(this.btnAutoCal, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.ListControlBindingSource listControlBindingSource1;
+        private Win.UI.Panel panel1;
+        private Win.UI.TextBox txtSPNo;
+        private Win.UI.Label labelSPNo;
+        private Win.UI.Grid grid1;
+        private Win.UI.Label label1;
+        private Win.UI.Label label2;
+        private Win.UI.Grid grid2;
+        private Win.UI.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnAutoCal;
+        private Win.UI.ListControlBindingSource listControlBindingSource2;
+    }
+}
---
+++ Sci.Production.Warehouse/P05.cs
@@ -0,0 +1,358 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ict;
+using Ict.Win;
+using Sci;
+using System.Data.SqlClient;
+using Sci.Data;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+
+namespace Sci.Production.Warehouse
+{
+    public partial class P05 : Sci.Win.Tems.QueryForm
+    {
+        public P05(ToolStripMenuItem menuitem)
+            :base(menuitem)
+        {
+            this.EditMode = true;
+            InitializeComponent();
+            this.grid1.IsEditingReadOnly = false;
+        }
+
+        protected override void OnFormLoaded()
+        {
+            base.OnFormLoaded();
+            DataGridViewGeneratorCheckBoxColumnSettings col_chk = new DataGridViewGeneratorCheckBoxColumnSettings();
+            col_chk.CellValidating += (s, e) =>
+            {
+                DataRow dr = grid1.GetDataRow<DataRow>(e.RowIndex);
+                dr["selected"] = e.FormattedValue;
+                dr.EndEdit();
+                calEstUsageAndBalance();
+            };
+
+            Ict.Win.DataGridViewGeneratorNumericColumnSettings qty = new DataGridViewGeneratorNumericColumnSettings();
+            qty.CellMouseDoubleClick += (s, e) =>
+            {
+                DataRow dr = grid1.GetDataRow<DataRow>(e.RowIndex);
+                Sci.Production.PPIC.P01_QtyShip callNextForm = new Sci.Production.PPIC.P01_QtyShip(MyUtility.Convert.GetString(dr["ID"]),txtSPNo.Text);
+                callNextForm.ShowDialog(this);
+            };
+
+            Ict.Win.DataGridViewGeneratorNumericColumnSettings qty2 = new DataGridViewGeneratorNumericColumnSettings();
+            qty2.CellMouseDoubleClick += (s, e) =>
+            {
+
+                if (callP03 != null && callP03.Visible == true)
+                {
+                    callP03.P03Data(txtSPNo.Text);
+                    callP03.Activate();
+                }
+                else
+                {
+                    P03FormOpen();
+                }
+            };
+            #region Set Grid1
+            Helper.Controls.Grid.Generator(this.grid1)
+            .CheckBox("Selected", header: string.Empty, width: Widths.AnsiChars(3), iseditable: true, trueValue: 1, falseValue: 0,settings: col_chk)
+            .Text("ID", header: "SP# (Child SP)", iseditingreadonly: true, width: Widths.AnsiChars(13))
+            .Text("Article", header: "Article", iseditingreadonly: true, width: Widths.AnsiChars(13))
+            .Numeric("OrderQty", header: "Order Qty", iseditingreadonly: true, width: Widths.AnsiChars(13),settings:qty)
+            .Date("BuyerDelivery", header: "Buyer Delivery", iseditingreadonly: true, width: Widths.AnsiChars(13))
+            .Date("SciDelivery", header: "Sci Delivery", iseditingreadonly: true, width: Widths.AnsiChars(13))
+            .Text("SewInLine", header: "Sewing Inline", iseditingreadonly: true, width: Widths.AnsiChars(13))
+            .Text("SewLine", header: "Sewing Line#", iseditingreadonly: true, width: Widths.AnsiChars(13))
+            .Text("VasShas", header: "VAS/SHAS", iseditingreadonly: true, width: Widths.AnsiChars(13))
+            .Text("CustCDID", header: "CustCD", iseditingreadonly: true, width: Widths.AnsiChars(13))
+            .Text("Alias", header: "Destination", iseditingreadonly: true, width: Widths.AnsiChars(13))
+            ;
+            #endregion
+
+            #region Set Grid2
+            Helper.Controls.Grid.Generator(this.grid2)
+            .Text("Refno", header: "Refno", iseditingreadonly: true, width: Widths.AnsiChars(13))
+            .Text("Description", header: "Description", iseditingreadonly: true, width: Widths.AnsiChars(13))
+            .Text("ColorID", header: "Color", iseditingreadonly: true, width: Widths.AnsiChars(13))
+            .Text("ETA", header: "ETA", iseditingreadonly: true, width: Widths.AnsiChars(13))
+            .Numeric("Qty", header: "Purchase Qty", iseditingreadonly: true, width: Widths.AnsiChars(13),decimal_places:2, settings: qty2)
+            .Numeric("Wqty", header: "On Warehouse Qty", iseditingreadonly: true, width: Widths.AnsiChars(13), decimal_places: 2)
+            .Numeric("bqty", header: "On board Qty", iseditingreadonly: true, width: Widths.AnsiChars(13), decimal_places: 2)
+            .Numeric("Uqty", header: "Usable Qty", iseditingreadonly: true, width: Widths.AnsiChars(13), decimal_places: 2)
+            .Numeric("EstUsageQty", header: "Est. Usage Qty", iseditingreadonly: true, width: Widths.AnsiChars(13), decimal_places: 2)
+            .Numeric("BlanceQty", header: "Blance Qty", iseditingreadonly: true, width: Widths.AnsiChars(13), decimal_places: 2,minimum:-999999)
+            ;
+            #endregion
+        }
+
+        Sci.Production.Warehouse.P03 callP03 = null;
+        private void P03FormOpen()
+        {
+            foreach (Form form in Application.OpenForms)
+            {
+                if (form is Sci.Production.Warehouse.P03)
+                {
+                    form.Activate();
+                    Sci.Production.Warehouse.P03 activateForm = (Sci.Production.Warehouse.P03)form;
+                    activateForm.setTxtSPNo(txtSPNo.Text);
+                    activateForm.Query();
+                    return;
+                }
+            }
+
+            ToolStripMenuItem P03MenuItem = null;
+            foreach (ToolStripMenuItem toolMenuItem in Sci.Env.App.MainMenuStrip.Items)
+            {
+                if (toolMenuItem.Text.EqualString("Warehouse"))
+                {
+                    foreach (var subMenuItem in toolMenuItem.DropDown.Items)
+                    {
+                        if (subMenuItem.GetType().Equals(typeof(System.Windows.Forms.ToolStripMenuItem)))
+                        {
+                            if (((ToolStripMenuItem)subMenuItem).Text.EqualString("P03. Material Status"))
+                            {
+                                P03MenuItem = ((ToolStripMenuItem)subMenuItem);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            callP03 = new P03(txtSPNo.Text, P03MenuItem);
+            callP03.MdiParent = MdiParent;
+            callP03.Show();
+            callP03.P03Data(txtSPNo.Text);
+            callP03.ChangeDetailColor();
+        }
+
+        private void txtSPNo_Validating(object sender, CancelEventArgs e)
+        {
+            Query();
+            calBalance();
+        }
+
+        DataTable dt;
+        DataTable dt2;
+        DataTable[] dt3;
+
+        private void Query()
+        {
+            #region 1
+            string sqlcmd = $@"
+select 	selected = 0,	o.ID,	oq.Article,	OrderQty = sum(oq.Qty),	o.BuyerDelivery,	o.SciDelivery,	o.SewInLine,	o.SewLine,VasShas = iif(o.VasShas=1,'Y',''),o.CustCDID,	c.Alias,
+	dtkey = ROW_NUMBER() OVER(ORDER BY BuyerDelivery) -1
+from Orders o with(nolock)
+inner join Order_Qty oq with(nolock) on o.ID = oq.ID
+left join Country c with(nolock) on o.Dest = c.ID
+where o.poid = '{txtSPNo.Text}'
+group by o.ID,oq.Article,o.BuyerDelivery,o.SciDelivery,o.SewInLine,o.SewLine,o.VasShas,o.CustCDID,c.Alias
+order by BuyerDelivery
+";
+            DualResult result = DBProxy.Current.Select(null, sqlcmd, out dt);
+            if (!result)
+            {
+                this.ShowErr(result);
+                return;
+            }
+            this.listControlBindingSource1.DataSource = dt;
+            this.grid1.AutoResizeColumns();
+            #endregion
+            #region 2
+            string sqlcmd2 = $@"
+select Refno,ColorID,FabricType,id,Qty = Sum(Po_Supp_Detail.Qty)
+into #tmp 
+from Po_Supp_Detail with(nolock)
+where FabricType = 'F' and id = '{txtSPNo.Text}'
+group by Refno,ColorID,FabricType,id
+
+select 
+	t.Refno,
+	f.Description,	
+	t.ColorID,	a.ETA,	t.Qty,
+	Wqty = isnull(b.InQty,0)+isnull(c.InQty,0),
+	bqty = isnull(b.bqty,0),
+	Uqty = iif('{checkBox1.Checked}'='True',isnull(b.InQty,0)+isnull(c.InQty,0), isnull(b.InQty,0)+isnull(c.InQty,0)+isnull(b.bqty,0)),
+	Uqty2 = iif('{checkBox1.Checked}'='True',isnull(b.InQty,0)+isnull(c.InQty,0), isnull(b.InQty,0)+isnull(c.InQty,0)+isnull(b.bqty,0)),
+	EstUsageQty = 0.00,
+	BlanceQty = 0.00
+from #tmp t
+outer apply(
+	select ETA = stuff((
+		select concat(',',format( ETA,'yyyy/MM/dd'))
+		from(
+			select distinct ETA
+			from Po_Supp_Detail with(nolock) 
+			where FabricType = t.FabricType and id = t.ID and Refno = t.Refno and ColorID = t.ColorID and ETA is not null
+		)x
+		for xml path('')),
+	1,1,'')
+)a
+outer apply(
+	select Description = stuff((
+		select concat(',',Description)
+		from(
+			select distinct Description 
+			from Fabric f with(nolock) 
+			where f.SCIRefno in (
+				select distinct SCIRefno 
+				from Po_Supp_Detail with(nolock) 
+				where FabricType = t.FabricType and id = t.ID and Refno = t.Refno and ColorID = t.ColorID)
+		)a
+		for xml path('')),
+	1,1,'')
+)f
+outer apply(
+	select InQty = sum(b.InQty), bqty = sum(a.ShipQty + a.ShipFOC - b.InQty)
+	from Po_Supp_Detail a with(nolock) 
+	left join MDivisionPoDetail b with(nolock) on a.id = b.POID and a.SEQ1 = b.Seq1 and a.SEQ2 = b.Seq2
+	where FabricType = t.FabricType and id = t.ID and Refno = t.Refno and ColorID = t.ColorID and a.seq1 not like '7%'
+)b
+outer apply(
+	select InQty = sum(InQty)
+	from Po_Supp_Detail a with(nolock) 
+	inner join MDivisionPoDetail b with(nolock) on a.id = b.POID and a.SEQ1 = b.Seq1 and a.SEQ2 = b.Seq2
+	where FabricType = t.FabricType and id = t.ID and Refno = t.Refno and ColorID = t.ColorID and a.seq1 like '7%' and ShipETA <=GETDATE()
+)c
+order by  t.Refno
+drop table #tmp
+";
+            DualResult result2 = DBProxy.Current.Select(null, sqlcmd2, out dt2);
+            if (!result2)
+            {
+                this.ShowErr(result2);
+                return;
+            }
+            this.listControlBindingSource2.DataSource = dt2;
+            this.grid2.AutoResizeColumns();
+            this.grid2.Columns["Description"].Width = 200;
+            #endregion
+            #region 3
+            if (dt.Rows.Count == 0)
+            {
+                return;
+            }
+
+            string sqlcmd3 = string.Empty;
+            foreach (DataRow dr in dt.Rows)
+            {
+                sqlcmd3 += $@"
+select ob.refno,oec.ColorID,TtlConsPC = avg(oe.conspc)*{dr["OrderQty"]},dtkey={dr["dtkey"]}
+from Order_EachCons oe
+inner join dbo.Order_BOF ob on oe.Id = ob.Id and oe.FabricCode = ob.FabricCode
+inner join dbo.Order_EachCons_Color oec on oe.Id = oec.Id and oe.Ukey = oec.Order_EachConsUkey
+where oe.ID='{txtSPNo.Text}'
+and (0 = iif(exists (select 1 from Order_EachCons_Article where Order_EachConsUkey = oe.Ukey),1,0) --若Order_EachCons_Article有資料,要確認Article是否存在於Order_EachCons_Article
+	or '{dr["Article"]}' in(select Article from Order_EachCons_Article oea where oea.Order_EachConsUkey = oe.Ukey))
+group by oe.ID,ob.refno,oec.ColorID
+order by ob.refno,oec.ColorID
+";
+            }
+            DualResult result3 = DBProxy.Current.Select(null, sqlcmd3, out dt3);
+            if (!result3)
+            {
+                this.ShowErr(result3);
+                return;
+            }
+            #endregion
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            foreach (DataRow dr2 in dt2.Rows)
+            {
+                dr2["Uqty"] = checkBox1.Checked ? dr2["Wqty"] : MyUtility.Convert.GetDecimal(dr2["Wqty"]) + MyUtility.Convert.GetDecimal(dr2["bqty"]);
+                dr2["Uqty2"] = checkBox1.Checked ? dr2["Wqty"] : MyUtility.Convert.GetDecimal(dr2["Wqty"]) + MyUtility.Convert.GetDecimal(dr2["bqty"]);
+            }
+
+            calBalance();
+        }
+
+        private void btnAutoCal_Click(object sender, EventArgs e)
+        {
+            foreach (DataRow dr in dt.Rows)
+            {
+                dr["selected"] = false;
+            }
+
+            foreach (DataRow dr2 in dt2.Rows)
+            {
+                dr2["Uqty2"] = dr2["Uqty"];
+            }
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                DataTable dt3c = dt3[MyUtility.Convert.GetInt(dr["dtkey"])];
+                bool flag = true;
+                foreach (DataRow dr2 in dt2.Rows)
+                {
+                    DataRow[] dr3s = dt3c.Select($"Refno = '{dr2["Refno"]}' and ColorID = '{dr2["ColorID"]}'");
+                    if (dr3s.Length > 0)
+                    {
+                        if (MyUtility.Convert.GetDecimal(dr2["Uqty2"]) - MyUtility.Convert.GetDecimal(dr3s[0]["TtlConsPC"]) < 0)
+                        {
+                            flag = false;
+                        }
+                    }
+                }
+
+                if (flag)
+                {
+                    foreach (DataRow dr2 in dt2.Rows)
+                    {
+                        DataRow[] dr3s = dt3c.Select($"Refno = '{dr2["Refno"]}' and ColorID = '{dr2["ColorID"]}'");
+                        if (dr3s.Length > 0)
+                        {
+                            dr2["Uqty2"] = MyUtility.Convert.GetDecimal(dr2["Uqty2"]) - MyUtility.Convert.GetDecimal(dr3s[0]["TtlConsPC"]);
+                        }
+                    }
+                    dr["selected"] = true;
+                }
+            }
+            calEstUsageAndBalance();
+            this.grid1.ValidateControl();
+            this.grid2.ValidateControl();
+        }
+
+        private void calEstUsageAndBalance()
+        {
+            foreach (DataRow dr2 in dt2.Rows)
+            {
+                dr2["EstUsageQty"] = 0;
+            }
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                if (MyUtility.Convert.GetInt(dr["selected"])==1)
+                {
+                    DataTable dt3c = dt3[MyUtility.Convert.GetInt(dr["dtkey"])];
+                    foreach (DataRow dr2 in dt2.Rows)
+                    {
+                        DataRow[] dr3s = dt3c.Select($"Refno = '{dr2["Refno"]}' and ColorID = '{dr2["ColorID"]}'");
+                        if (dr3s.Length > 0)
+                        {
+                            dr2["EstUsageQty"] = MyUtility.Convert.GetDecimal(dr2["EstUsageQty"]) + MyUtility.Convert.GetDecimal(dr3s[0]["TtlConsPC"]);
+                        }
+                    }
+                }
+            }
+            calBalance();
+        }
+
+        private void calBalance()
+        {
+            foreach (DataRow dr2 in dt2.Rows)
+            {
+                dr2["BlanceQty"] = MyUtility.Convert.GetDecimal(dr2["Uqty"]) - MyUtility.Convert.GetDecimal(dr2["EstUsageQty"]);
+            }
+        }
+    }
+}
