1d994ad16973b6640123094e367f68f43894abf6
JEFF <jeff.yeh@sportscity.com.tw>
2018-05-24 15:01:31 +0800

    ISP20180323 Garment List撈資料規則，帶出Phase為Bulk、Status=Complete然後修改日最大的那筆資料，如果沒有Phase為Bulk的話就用現在的規則帶出資料

--- Sci.Production.PublicForm/GarmentList.cs
+++ Sci.Production.PublicForm/GarmentList.cs
@@ -35,25 +35,43 @@ namespace Sci.Production.PublicForm
             if (MyUtility.Check.Empty(_cutref))
             {
                 patidsql = String.Format(
-                            @"SELECT ukey
-                              FROM [Production].[dbo].[Pattern] WITH (NOLOCK) 
-                              WHERE STYLEUKEY = '{0}'  and Status = 'Completed' 
-                              AND EDITdATE = 
-                              (
-                                SELECT MAX(EditDate) 
-                                from pattern WITH (NOLOCK) 
-                                where styleukey = '{0}' and Status = 'Completed'
-                              )
+                            @"
+SELECT ukey
+FROM [Production].[dbo].[Pattern] a WITH (NOLOCK) 
+outer apply(
+	SELECT EditDate = MAX(p.EditDate)
+	from pattern p WITH (NOLOCK) 
+	left join smnotice_detail s WITH (NOLOCK) on s.id=p.id and (s.PhaseID is not null and Rtrim(s.phaseId)!='' ) 
+	where styleukey = '{0}' and Status = 'Completed' and s.PhaseID = 'Bulk'
+)b
+outer apply(
+	SELECT EditDate = MAX(p.EditDate)
+	from pattern p WITH (NOLOCK) 
+	where styleukey = '{0}' and Status = 'Completed' 
+)c
+WHERE STYLEUKEY = '{0}'  and Status = 'Completed' 
+AND a.EDITdATE = iif(b.EditDate is null,c.EditDate,b.EditDate)
              ", Styleyukey);
             }
             else
             {
                 patidsql = String.Format(
-                            @"select top 1 Ukey 
-                            from Pattern WITH (NOLOCK)
-                            where PatternNo = (select top 1 substring(MarkerNo,1,9)+'N' from WorkOrder WITH (NOLOCK) where CutRef = '{0}' and ID='{1}')
-                            and Status = 'Completed'
-                            order by ActFinDate Desc
+                            @"
+select Ukey = isnull((
+	select top 1 Ukey 
+	from Pattern p WITH (NOLOCK)
+	left join smnotice_detail s WITH (NOLOCK) on s.id=p.id and (s.PhaseID is not null and Rtrim(s.phaseId)!='' ) 
+	where PatternNo = (select top 1 substring(MarkerNo,1,9)+'N' from WorkOrder WITH (NOLOCK) where CutRef = '{0}' and ID='{1}')
+	and Status = 'Completed' and s.PhaseID = 'bluk'
+	order by ActFinDate Desc
+),
+(
+	select top 1 Ukey 
+	from Pattern p WITH (NOLOCK)
+	where PatternNo = (select top 1 substring(MarkerNo,1,9)+'N' from WorkOrder WITH (NOLOCK) where CutRef = '{0}' and ID='{1}')
+	and Status = 'Completed'
+	order by ActFinDate Desc
+))
                             ", _cutref, id);
             }
             
