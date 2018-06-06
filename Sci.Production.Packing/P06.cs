16e7d8d8add144f03d4b96eadf6b25f557047d02
JEFF <jeff.yeh@sportscity.com.tw>
2018-06-06 16:06:14 +0800

    ISP20180367 加上欄位(RefNo/Description)&右鍵開窗

--- Sci.Production.Packing/P06.cs
+++ Sci.Production.Packing/P06.cs
@@ -20,6 +20,7 @@ namespace Sci.Production.Packing
         private Ict.Win.UI.DataGridViewTextBoxColumn col_ctnno;
         private Ict.Win.UI.DataGridViewTextBoxColumn col_article;
         private Ict.Win.UI.DataGridViewTextBoxColumn col_size;
+        private Ict.Win.UI.DataGridViewTextBoxColumn col_refno;
         private Ict.Win.UI.DataGridViewNumericBoxColumn col_ctnqty;
         private Ict.Win.UI.DataGridViewNumericBoxColumn col_shipqty;
         private Ict.Win.DataGridViewGeneratorTextColumnSettings article = new Ict.Win.DataGridViewGeneratorTextColumnSettings();
@@ -264,9 +265,40 @@ order by os.Seq",
             };
             #endregion
 
+            this.detailgrid.CellValueChanged += (s, e) =>
+            {
+                #region 選完RefNo後，要自動帶出Description
+                if (this.detailgrid.Columns[e.ColumnIndex].DataPropertyName == this.col_refno.DataPropertyName)
+                {
+                    DataRow dr = this.detailgrid.GetDataRow<DataRow>(e.RowIndex);
+
+                    if (MyUtility.Check.Empty(dr["RefNo"]))
+                    {
+                        dr["Description"] = string.Empty;
+                    }
+                    else
+                    {
+                        string seekSql = string.Format("select Description,CtnWeight from LocalItem WITH (NOLOCK) where RefNo = '{0}'", dr["RefNo"].ToString());
+                        DataRow localItem;
+                        if (MyUtility.Check.Seek(seekSql, out localItem))
+                        {
+                            dr["Description"] = localItem["Description"].ToString();
+                        }
+                        else
+                        {
+                            dr["Description"] = string.Empty;
+                        }
+                    }
+
+                    dr.EndEdit();
+                }
+                #endregion
+            };
             this.Helper.Controls.Grid.Generator(this.detailgrid)
                 .Text("CTNStartNo", header: "CTN#", width: Widths.AnsiChars(6)).Get(out this.col_ctnno)
                 .Numeric("CTNQty", header: "# of CTN").Get(out this.col_ctnqty)
+                .CellCartonItem("RefNo", header: "Ref No.", width: Widths.AnsiChars(13)).Get(out this.col_refno)
+                .Text("Description", header: "Description", width: Widths.AnsiChars(20), iseditingreadonly: true)
                 .Text("Article", header: "ColorWay", width: Widths.AnsiChars(8), settings: this.article).Get(out this.col_article)
                 .Text("SizeCode", header: "Size", width: Widths.AnsiChars(8), settings: this.size).Get(out this.col_size)
                 .Numeric("ShipQty", header: "Qty").Get(out this.col_shipqty)
