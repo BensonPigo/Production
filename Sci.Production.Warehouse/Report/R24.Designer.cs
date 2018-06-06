b0954fa06f833e6966df76ec51dfb6e30d199cbf
Willy <willy.wei@sprotscity.com.tw>
2018-06-06 15:49:32 +0800

    ISP20180361 1.新增R24 程式, 及Excel R24 Report

---
+++ Sci.Production.Warehouse/Report/R24.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Sci.Production.Warehouse
+{
+    partial class R24
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelIssueDate = new Sci.Win.UI.Label();
+            this.txtfactory = new Sci.Production.Class.txtfactory();
+            this.labelFactory = new Sci.Win.UI.Label();
+            this.labelM = new Sci.Win.UI.Label();
+            this.txtMdivision = new Sci.Production.Class.txtMdivision();
+            this.dateIssueDate = new Sci.Win.UI.DateRange();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(372, 84);
+            this.print.Visible = false;
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(372, 12);
+            this.toexcel.TabIndex = 3;
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(372, 48);
+            this.close.TabIndex = 4;
+            // 
+            // labelIssueDate
+            // 
+            this.labelIssueDate.Location = new System.Drawing.Point(9, 12);
+            this.labelIssueDate.Name = "labelIssueDate";
+            this.labelIssueDate.RectStyle.BorderColor = System.Drawing.Color.Black;
+            this.labelIssueDate.RectStyle.Color = System.Drawing.Color.SkyBlue;
+            this.labelIssueDate.Size = new System.Drawing.Size(75, 23);
+            this.labelIssueDate.TabIndex = 5;
+            this.labelIssueDate.Text = "Issue Date";
+            this.labelIssueDate.TextStyle.BorderColor = System.Drawing.Color.Black;
+            this.labelIssueDate.TextStyle.Color = System.Drawing.Color.Black;
+            // 
+            // txtfactory
+            // 
+            this.txtfactory.BackColor = System.Drawing.Color.White;
+            this.txtfactory.FilteMDivision = false;
+            this.txtfactory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtfactory.IssupportJunk = true;
+            this.txtfactory.Location = new System.Drawing.Point(86, 70);
+            this.txtfactory.Name = "txtfactory";
+            this.txtfactory.Size = new System.Drawing.Size(66, 23);
+            this.txtfactory.TabIndex = 2;
+            // 
+            // labelFactory
+            // 
+            this.labelFactory.Location = new System.Drawing.Point(9, 71);
+            this.labelFactory.Name = "labelFactory";
+            this.labelFactory.Size = new System.Drawing.Size(75, 23);
+            this.labelFactory.TabIndex = 7;
+            this.labelFactory.Text = "Factory";
+            // 
+            // labelM
+            // 
+            this.labelM.Location = new System.Drawing.Point(9, 41);
+            this.labelM.Name = "labelM";
+            this.labelM.Size = new System.Drawing.Size(75, 23);
+            this.labelM.TabIndex = 6;
+            this.labelM.Text = "M";
+            // 
+            // txtMdivision
+            // 
+            this.txtMdivision.BackColor = System.Drawing.Color.White;
+            this.txtMdivision.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtMdivision.Location = new System.Drawing.Point(87, 41);
+            this.txtMdivision.Name = "txtMdivision";
+            this.txtMdivision.Size = new System.Drawing.Size(66, 23);
+            this.txtMdivision.TabIndex = 1;
+            // 
+            // dateIssueDate
+            // 
+            // 
+            // 
+            // 
+            this.dateIssueDate.DateBox1.Location = new System.Drawing.Point(0, 0);
+            this.dateIssueDate.DateBox1.Name = "";
+            this.dateIssueDate.DateBox1.Size = new System.Drawing.Size(129, 23);
+            this.dateIssueDate.DateBox1.TabIndex = 0;
+            // 
+            // 
+            // 
+            this.dateIssueDate.DateBox2.Location = new System.Drawing.Point(151, 0);
+            this.dateIssueDate.DateBox2.Name = "";
+            this.dateIssueDate.DateBox2.Size = new System.Drawing.Size(129, 23);
+            this.dateIssueDate.DateBox2.TabIndex = 1;
+            this.dateIssueDate.IsRequired = false;
+            this.dateIssueDate.Location = new System.Drawing.Point(87, 12);
+            this.dateIssueDate.Name = "dateIssueDate";
+            this.dateIssueDate.Size = new System.Drawing.Size(280, 23);
+            this.dateIssueDate.TabIndex = 0;
+            // 
+            // R24
+            // 
+            this.ClientSize = new System.Drawing.Size(464, 155);
+            this.Controls.Add(this.labelIssueDate);
+            this.Controls.Add(this.txtfactory);
+            this.Controls.Add(this.labelFactory);
+            this.Controls.Add(this.labelM);
+            this.Controls.Add(this.txtMdivision);
+            this.Controls.Add(this.dateIssueDate);
+            this.Name = "R24";
+            this.Text = "R24. Bulk to Inventory Transaction List";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.dateIssueDate, 0);
+            this.Controls.SetChildIndex(this.txtMdivision, 0);
+            this.Controls.SetChildIndex(this.labelM, 0);
+            this.Controls.SetChildIndex(this.labelFactory, 0);
+            this.Controls.SetChildIndex(this.txtfactory, 0);
+            this.Controls.SetChildIndex(this.labelIssueDate, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.Label labelIssueDate;
+        private Class.txtfactory txtfactory;
+        private Win.UI.Label labelFactory;
+        private Win.UI.Label labelM;
+        private Class.txtMdivision txtMdivision;
+        private Win.UI.DateRange dateIssueDate;
+    }
+}
