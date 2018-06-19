22d623d017dda2479cecc8c1e7d009ad2686cd25
DOMAIN\aaron.shie <onizuka865@gmail.com>
2018-06-19 11:40:07 +0800

    -ISP20180412 自訂Packing P18訊息視窗避免刷條碼時因enter造成視窗關閉

---
+++ Sci.Production.Packing/P18_Message.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Packing
+{
+    public partial class P18_Message : Win.Forms.Base
+    {
+        /// <summary>
+        /// P18_Message
+        /// </summary>
+        public P18_Message()
+        {
+            this.InitializeComponent();
+            this.txt_msg.BackColor = SystemColors.Control;
+            this.txt_msg.ForeColor = Color.Black;
+            this.ActiveControl = this.label1;
+        }
+
+        /// <summary>
+        /// Show
+        /// </summary>
+        /// <param name="msg">msg</param>
+        public void Show(string msg)
+        {
+            this.txt_msg.Text = msg;
+            this.ShowDialog();
+        }
+
+    }
+}
