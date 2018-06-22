1fe3b2962fd630feb9ddccda987d77f45bd12513
Willy <willy.wei@sprotscity.com.tw>
2018-06-22 19:02:57 +0800

    ISP20180383 backup

---
+++ Sci.Production.PPIC/R11.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict;
+using Sci.Win;
+
+namespace Sci.Production.PPIC
+{
+    public partial class R11 : Sci.Win.Tems.PrintForm
+    {
+        public R11(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+        }
+
+        protected override bool ValidateInput()
+        {
+            return base.ValidateInput();
+        }
+
+        protected override DualResult OnAsyncDataLoad(ReportEventArgs e)
+        {
+            return base.OnAsyncDataLoad(e);
+        }
+
+        protected override bool OnToExcel(ReportDefinition report)
+        {
+            return base.OnToExcel(report);
+        }
+    }
+}
