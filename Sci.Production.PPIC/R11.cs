0f2abd86aa3456a0331d21c405fa6594d3f3339a
Willy <willy.wei@sprotscity.com.tw>
2018-06-29 16:51:32 +0800

    ISP20180383 1.Total Sewing Output若沒有值請顯示0。 2.計算紙箱、CFA...等資料若沒有找到在指定時間送達Colog倉也要顯示出來，不能整筆消除。 3.Category 請顯示全名Bulk、Sample、Garment。 4.Summary頁面每日統記請加上日期，右邊總結若完全0%達標要顯示0%。 5.重新調整Summery 計算公式 : 紀錄單筆pass,fail數量, 而非ctn Qty

--- Sci.Production.PPIC/R11.cs
+++ Sci.Production.PPIC/R11.cs
@@ -24,6 +24,8 @@ namespace Sci.Production.PPIC
         private string dateRangeReadyOrl;
         private DataTable dtFty;
         private DataTable[] dtList;
+        private List<string> listSQLFilter;
+        private StringBuilder sbFtycode;
         private Color BackRed = Color.FromArgb(255, 155, 155);
         private Color FontRed = Color.FromArgb(255, 0, 0);
         private Color BackGreen = Color.FromArgb(167, 255, 190);
@@ -45,43 +47,39 @@ namespace Sci.Production.PPIC
             this.dateRangeReady1 = MyUtility.Check.Empty(this.dateRangeReadyDate.Value1) ? string.Empty : ((DateTime)this.dateRangeReadyDate.Value1).AddDays(-MyUtility.Convert.GetInt(this.Gap)).ToString("yyyy/MM/dd");
             this.dateRangeReady2 = MyUtility.Check.Empty(this.dateRangeReadyDate.Value2) ? string.Empty : ((DateTime)this.dateRangeReadyDate.Value2).AddDays(-MyUtility.Convert.GetInt(this.Gap)).ToString("yyyy/MM/dd");
             this.dateRangeReadyOrl = MyUtility.Check.Empty(this.dateRangeReadyDate.Value2) ? string.Empty : ((DateTime)this.dateRangeReadyDate.Value2).ToString("yyyy/MM/dd");
-            return true;
-        }
 
-        protected override DualResult OnAsyncDataLoad(ReportEventArgs e)
-        {
-            List<string> listSQLFilter = new List<string>();
+            this.listSQLFilter = new List<string>();
             #region Sql where Filter
             if (!this.dateRangeReady1.Empty())
             {
-                listSQLFilter.Add($"and CONVERT(date, s.Offline) >= '{this.dateRangeReady1}'");
+                this.listSQLFilter.Add($"and CONVERT(date, s.Offline) >= '{this.dateRangeReady1}'");
             }
 
             if (!this.dateRangeReady2.Empty())
             {
-                listSQLFilter.Add($"and CONVERT(date, s.Offline) <= '{this.dateRangeReady2}'");
+                this.listSQLFilter.Add($"and CONVERT(date, s.Offline) <= '{this.dateRangeReady2}'");
             }
 
             if (!this.F.Empty())
             {
-                listSQLFilter.Add($"and s.FactoryID = '{this.F}'");
+                this.listSQLFilter.Add($"and s.FactoryID = '{this.F}'");
             }
 
             if (!this.M.Empty())
             {
-                listSQLFilter.Add($"and s.MDivisionID = '{this.M}'");
+                this.listSQLFilter.Add($"and s.MDivisionID = '{this.M}'");
             }
             #endregion
 
             #region Summery
 
+            sbFtycode = new StringBuilder();
             string sqlFty = $@"
 select distinct FactoryID from sewingschedule s
 where 1=1
-{listSQLFilter.JoinToString($"{Environment.NewLine} ")}";
-            StringBuilder sbFtycode = new StringBuilder();
+{this.listSQLFilter.JoinToString($"{Environment.NewLine} ")}";
             DBProxy.Current.Select(string.Empty, sqlFty, out this.dtFty);
-            if (this.dtFty != null )
+            if (this.dtFty != null)
             {
                 for (int i = 0; i < this.dtFty.Rows.Count; i++)
                 {
@@ -89,19 +87,34 @@ where 1=1
                 }
             }
 
-            #endregion
+            if (sbFtycode.Length == 0)
+            {
+                MyUtility.Msg.WarningBox("Data not found!");
+                return false;
+            }
 
+            #endregion
+            return true;
+        }
+
+        protected override DualResult OnAsyncDataLoad(ReportEventArgs e)
+        {
             #region Sql Command
             string sqlCmd = $@"
-
-
 SELECT distinct
-[ReadyDate] = convert(date,s.Offline+{MyUtility.Convert.GetInt(this.Gap)})
+[ReadyDate] = convert(date,s.Offline+2)
 ,[M] = o.MDivisionID
 ,[Factory] = o.FactoryID
 ,[Delivery] = o.BuyerDelivery
 ,[SPNO] = s.OrderID
-,[Category] = o.Category
+,[Category] = 
+case	when o.Category ='B' then 'Bulk'
+		when o.Category ='S' then 'Sample'
+		when o.Category ='M' then 'Material'
+		when o.Category ='O' then 'Other'
+		when o.Category ='G' then 'Garment'
+		when o.Category ='T' then 'SMLT'
+end
 ,[Cancelled] = o.Junk
 ,[Dest] = (select alias from Country where  o.Dest=id)
 ,[Style] = o.StyleID
@@ -109,19 +122,19 @@ SELECT distinct
 ,[PoNo] = o.CustPONo
 ,[Brand] = o.BrandID
 ,[Qty] = os.Qty
-,[SewingOutputQty] = SewingOutput.Qty
+,[SewingOutputQty] = isnull(SewingOutput.Qty,0)
 ,[InLine] = ss.inline
 ,[OffLine] = convert(date, s.Offline)
 ,[FirstSewnDate] = SewDate.FirstDate
 ,[LastSewnDate] = SewDate.LastDate
-,[%]= iif(pdm.TotalCTN=0,0, (convert(float,pdm.ClogCTN) / convert(float,pdm.TotalCTN))*100)
-,[TtlCTN] = pdm.TotalCTN,[FtyCTN] = pdm.FtyCtn,[ClogCTN] = pdm.ClogCTN
-,[cLogRecDate] = pdm.ClogRcvDate
+,[%]= iif(pdm.TotalCTN=0,0, ( isnull(convert(float,Receive.ClogCTN),0) / convert(float,pdm.TotalCTN))*100)
+,[TtlCTN] = pdm.TotalCTN,[FtyCTN] = pdm.FtyCtn,[ClogCTN] = isnull(Receive.ClogCTN,0)
+,[cLogRecDate] = Receive.ClogRcvDate
 ,[FinalInspDate]=cfa.FinalInsDate,cfa.Result
 ,[CfaName] = cfa.CfaName
 ,[SewingLine] = SewingLine.Line
 ,[ShipMode] = os.ShipmodeID
-,[Remark]= IIF(iif(pdm.TotalCTN=0,0, (convert(float,pdm.ClogCTN) / convert(float,pdm.TotalCTN))*100)=100,'Pass Ready date','Failed Ready Date')
+,[Remark]= IIF(iif(pdm.TotalCTN=0,0, (isnull(convert(float,Receive.ClogCTN),0) / convert(float,pdm.TotalCTN))*100)=100,'Pass Ready date','Failed Ready Date')
 into #tmp
 FROM sewingschedule S
 left join Orders o on s.OrderID=o.ID
@@ -157,20 +170,22 @@ outer apply(
 	where OrderID=s.OrderID 
 )ss
 outer apply( 
-	select Sum( pd.CTNQty) PackingCTN ,
-	Sum( case when p.Type in ('B', 'L') then pd.CTNQty else 0 end) TotalCTN,
-	Sum( case when p.Type in ('B', 'L') and pd.TransferDate is null then pd.CTNQty else 0 end) FtyCtn,
-	Sum(case when p.Type in ('B', 'L') and pd.ReceiveDate is not null then pd.CTNQty else 0 end) ClogCTN ,
-	Sum(case when p.Type <> 'F' then pd.ShipQty else 0 end) PackingQty ,
-	Sum(case when p.Type = 'F' then pd.ShipQty else 0 end) PackingFOCQty ,
-	Sum(case when p.Type in ('B', 'L') and p.INVNo <> ''  then pd.ShipQty else 0 end) BookingQty ,
-	Max (ReceiveDate) ClogRcvDate,
-	MAX(p.PulloutDate)  ActPulloutDate
+	select Sum( case when p.Type in ('B', 'L') then pd.CTNQty else 0 end) TotalCTN,
+	Sum( case when p.Type in ('B', 'L') and pd.TransferDate is null then pd.CTNQty else 0 end) FtyCtn
 	from PackingList_Detail pd WITH (NOLOCK) 
     LEFT JOIN PackingList p on pd.ID = p.ID 
     where  pd.OrderID = os.ID 
-	having Max (ReceiveDate) <= CONVERT(date, DATEADD(DAY,{MyUtility.Convert.GetInt(this.Gap)}, s.Offline))
+	and pd.OrderShipmodeSeq = os.Seq
 )  pdm
+outer apply (
+	select Sum(case when p.Type in ('B', 'L') and pd.ReceiveDate is not null then pd.CTNQty else 0 end) ClogCTN ,
+	Max (ReceiveDate) ClogRcvDate
+	from PackingList_Detail pd WITH (NOLOCK) 
+    LEFT JOIN PackingList p on pd.ID = p.ID 
+    where  pd.OrderID = os.ID 
+	and pd.OrderShipmodeSeq = os.Seq
+	having Max (ReceiveDate) <= CONVERT(date, DATEADD(DAY,{MyUtility.Convert.GetInt(this.Gap)}, s.Offline))
+) Receive
 outer apply(
 	select Line = stuff((
 		select concat(',',SewingLineID)
@@ -183,8 +198,7 @@ outer apply(
 		),1,1,'')
 )SewingLine
 where 1=1
-and pdm.TotalCTN>0
-{listSQLFilter.JoinToString($"{Environment.NewLine} ")}
+{this.listSQLFilter.JoinToString($"{Environment.NewLine} ")}
 
 
 select [M],[Factory],[Delivery],[SPNO],[Category],[Cancelled],[Dest],[Style],[OrderType],[PoNo]
@@ -193,54 +207,107 @@ select [M],[Factory],[Delivery],[SPNO],[Category],[Cancelled],[Dest],[Style],[Or
 from #tmp
 order by ReadyDate,m,Factory
 
-
-select m,Fty,[Failed Ready Date],[Pass Ready date],[Grand Total],[Rating]= convert(varchar,Rating)+'%' from (
+select ReadyDate,m,Fty,[Failed Ready Date],[Pass Ready date],[Grand Total],[Rating]= convert(varchar,Rating)+'%' 
+from (
 SELECT [M] = M
 ,[Fty] = Factory
-,[Failed Ready Date] = sum(FtyCTN)
-,[Pass Ready date] = sum(ClogCTN)
-,[Grand Total] = sum(TtlCTN),
-[Rating] = ROUND( convert(float,sum(ClogCTN))/convert(float,sum(TtlCTN)) *100,2)
+,[Failed Ready Date] =fail.cnt
+,[Pass Ready date] = pass.cnt
+,[Grand Total] =count(*),
+[Rating] = iif(count(*)=0,0, ROUND((convert(float, pass.cnt)/convert(float,count(*))*100) ,2))
 ,ReadyDate
-FROM #tmp
-where category='B' and Cancelled=0
-group by M,Factory,ReadyDate
+FROM #tmp t
+outer apply(
+	select count(*) cnt
+	FROM #tmp
+	where Remark='Failed Ready Date' and Factory=t.Factory and m=t.M and t.ReadyDate=ReadyDate
+	and category='Bulk' and Cancelled=0
+)fail
+outer apply(
+	select count(*) cnt
+	FROM #tmp
+	where Remark='Pass Ready Date' and Factory=t.Factory and m=t.M and t.ReadyDate=ReadyDate
+	and category='Bulk' and Cancelled=0
+)pass
+where category='Bulk' and Cancelled=0
+group by ReadyDate,M,Factory ,fail.cnt,pass.cnt
 
 union all
 
-select m,'Total' Fty
-,[Failed Ready Date] = sum(FtyCTN)
-,[Pass Ready date] = sum(ClogCTN)
-,[Grand Total] = sum(TtlCTN) 
-,[Rating] = ROUND( convert(float,sum(ClogCTN))/convert(float,sum(TtlCTN)) *100,2)
+SELECT [M] = M
+,[Fty] = 'Total'
+,[Failed Ready Date] =fail.cnt
+,[Pass Ready date] = pass.cnt
+,[Grand Total] =count(*),
+[Rating] = iif(count(*)=0,0, ROUND((convert(float, pass.cnt)/convert(float,count(*))*100) ,2))
 ,ReadyDate
-from #tmp
-group by M,ReadyDate
+FROM #tmp t
+outer apply(
+	select count(*) cnt
+	FROM #tmp
+	where Remark='Failed Ready Date' and m=t.M and t.ReadyDate=ReadyDate
+	and category='Bulk' and Cancelled=0
+)fail
+outer apply(
+	select count(*) cnt
+	FROM #tmp
+	where Remark='Pass Ready Date' and m=t.M and t.ReadyDate=ReadyDate
+	and category='Bulk' and Cancelled=0
+)pass
+where category='Bulk' and Cancelled=0
+group by ReadyDate,M ,fail.cnt,pass.cnt
 
 union all
 
-select 'x' m ,'Grand Total' Fty
-,[Failed Ready Date] = sum(FtyCTN)
-,[Pass Ready date] = sum(ClogCTN)
-,[Grand Total] = sum(TtlCTN) 
-,[Rating] = ROUND( convert(float,sum(ClogCTN))/convert(float,sum(TtlCTN)) *100,2)
+SELECT [M] = 'x'
+,[Fty] = 'Grand Total'
+,[Failed Ready Date] =fail.cnt
+,[Pass Ready date] = pass.cnt
+,[Grand Total] =count(*),
+[Rating] = iif(count(*)=0,0, ROUND((convert(float, pass.cnt)/convert(float,count(*))*100) ,2))
 ,ReadyDate
-from #tmp
-group by ReadyDate
+FROM #tmp t
+outer apply(
+	select count(*) cnt
+	FROM #tmp
+	where Remark='Failed Ready Date' and t.ReadyDate=ReadyDate
+	and category='Bulk' and Cancelled=0
+)fail
+outer apply(
+	select count(*) cnt
+	FROM #tmp
+	where Remark='Pass Ready Date' and t.ReadyDate=ReadyDate
+	and category='Bulk' and Cancelled=0
+)pass
+where category='Bulk' and Cancelled=0
+group by ReadyDate,fail.cnt,pass.cnt
 ) a
 order by ReadyDate,M,Fty
 
-select * from (
+select * from 
+(
 	SELECT Factory, ReadyDate
-	,[Rating] = round(convert(float,sum(ClogCTN))/convert(float,sum(TtlCTN)) *100,2)
-	FROM #tmp
-	where category='B' and Cancelled=0
-	group by Factory,ReadyDate
-	) as a
-	pivot 
-	(
-		sum(rating) for factory in ({sbFtycode.ToString().Substring(0, sbFtycode.ToString().Length - 1)})
-	) AS PT
+	,[Rating] = iif(count(*)=0,0, ROUND((convert(float, pass.cnt)/convert(float,count(*))*100) ,2))
+	FROM #tmp t
+	outer apply(
+		select count(*) cnt
+		FROM #tmp
+		where Remark='Failed Ready Date' and t.ReadyDate=ReadyDate
+		and category='Bulk' and Cancelled=0 and Factory = t.Factory
+	)fail
+	outer apply(
+		select count(*) cnt
+		FROM #tmp
+		where Remark='Pass Ready Date' and t.ReadyDate=ReadyDate
+		and category='Bulk' and Cancelled=0 and Factory = t.Factory
+	)pass
+	where category='Bulk' and Cancelled=0
+	group by Factory,ReadyDate,fail.cnt,pass.cnt
+) as a
+pivot 
+(
+	sum(rating) for factory in ({sbFtycode.ToString().Substring(0, sbFtycode.ToString().Length - 1)})
+) AS PT
 
 drop table #tmp
 ";
@@ -260,7 +327,7 @@ drop table #tmp
         protected override bool OnToExcel(ReportDefinition report)
         {
             #region check resultDT
-            if (this.dtList[0] == null || this.dtList[0].Rows.Count == 0)
+            if (MyUtility.Check.Empty(this.dtList[0]) || this.dtList[0].Rows.Count == 0)
             {
                 MyUtility.Msg.InfoBox("Data not Found.");
                 return false;
@@ -298,20 +365,20 @@ drop table #tmp
             TimeSpan ts = ts1.Subtract(ts2).Duration();
             int range = ts.Days + 1;
             // Summery header
-            wkcolor.Cells[1, 8].Value = "Date";
-            wkcolor.Cells[1, 8].Interior.Color = this.BackGray;
-            wkcolor.Cells[1, 8].Font.Bold = true;
+            wkcolor.Cells[1, 9].Value = "Date";
+            wkcolor.Cells[1, 9].Interior.Color = this.BackGray;
+            wkcolor.Cells[1, 9].Font.Bold = true;
             for (int d = 0; d < range; d++)
             {
-                wkcolor.Cells[2 + d, 8].Value = date1.AddDays(d);
-                wkcolor.Cells[2 + d, 8].Font.Bold = true;
+                wkcolor.Cells[2 + d, 9].Value = date1.AddDays(d);
+                wkcolor.Cells[2 + d, 9].Font.Bold = true;
             }
 
             for (int f = 0; f < this.dtFty.Rows.Count; f++)
             {
-                wkcolor.Cells[1, 9 + f].Value = this.dtFty.Rows[f]["FactoryID"];
-                wkcolor.Cells[1, 9 + f].Interior.Color = this.BackGray;
-                wkcolor.Cells[1, 9 + f].Font.Bold = true;
+                wkcolor.Cells[1, 10 + f].Value = this.dtFty.Rows[f]["FactoryID"];
+                wkcolor.Cells[1, 10 + f].Interior.Color = this.BackGray;
+                wkcolor.Cells[1, 10 + f].Font.Bold = true;
                 string fty = this.dtFty.Rows[f]["FactoryID"].ToString();
 
                 int cnt = 0;
@@ -319,29 +386,25 @@ drop table #tmp
                 {
                     if ((this.dtList[2].Rows.Count > cnt) && (MyUtility.Convert.GetDate((DateTime)this.dtList[2].Rows[cnt]["ReadyDate"]) == MyUtility.Convert.GetDate(date1.AddDays(i))))
                     {
-                        wkcolor.Cells[2 + i, 9 + f].Value = MyUtility.Check.Empty(this.dtList[2].Rows[cnt][fty]) ? string.Empty : this.dtList[2].Rows[cnt][fty] + "%";
+                        wkcolor.Cells[2 + i, 10 + f].Value = MyUtility.Check.Empty(this.dtList[2].Rows[cnt][fty]) ? "0%" : this.dtList[2].Rows[cnt][fty] + "%";
                         if (MyUtility.Convert.GetDecimal(this.dtList[2].Rows[cnt][fty]) > 90)
                         {
-                            wkcolor.Cells[2 + i, 9 + f].Interior.Color = this.BackGreen;
-                            wkcolor.Cells[2 + i, 9 + f].Font.Color = this.FontGreen;
-                        }
-                        else if (MyUtility.Check.Empty(this.dtList[2].Rows[cnt][fty]))
-                        {
-                            wkcolor.Cells[2 + i, 9 + f].Interior.Color = Color.Red;
+                            wkcolor.Cells[2 + i, 10 + f].Interior.Color = this.BackGreen;
+                            wkcolor.Cells[2 + i, 10 + f].Font.Color = this.FontGreen;
                         }
                         else
                         {
-                            wkcolor.Cells[2 + i, 9 + f].Interior.Color = this.BackRed;
-                            wkcolor.Cells[2 + i, 9 + f].Font.Color = this.FontRed;
+                            wkcolor.Cells[2 + i, 10 + f].Interior.Color = this.BackRed;
+                            wkcolor.Cells[2 + i, 10 + f].Font.Color = this.FontRed;
                         }
 
                         cnt++;
                     }
                     else
                     {
-                        string rowformat1 = MyExcelPrg.GetExcelColumnName(8);
-                        string rowformat2 = MyExcelPrg.GetExcelColumnName(8 + this.dtFty.Rows.Count);
-                        string rowformat3 = MyExcelPrg.GetExcelColumnName(9);
+                        string rowformat1 = MyExcelPrg.GetExcelColumnName(9);
+                        string rowformat2 = MyExcelPrg.GetExcelColumnName(9 + this.dtFty.Rows.Count);
+                        string rowformat3 = MyExcelPrg.GetExcelColumnName(10);
                         wkcolor.Range[$"{rowformat1}{2 + i}", $"{rowformat2}{2 + i}"].Interior.Color = Color.Red;
                         wkcolor.Range[$"{rowformat3}{2 + i}", $"{rowformat2}{2 + i}"].Merge();
                     }
@@ -350,7 +413,7 @@ drop table #tmp
 
             //畫框線
             Microsoft.Office.Interop.Excel.Range rg1;
-            rg1 = wkcolor.Range["H1", $"{MyExcelPrg.GetExcelColumnName(8 + this.dtFty.Rows.Count)}{range + 1}"];
+            rg1 = wkcolor.Range["I1", $"{MyExcelPrg.GetExcelColumnName(9 + this.dtFty.Rows.Count)}{range + 1}"];
             rg1.Borders[Microsoft.Office.Interop.Excel.XlBordersIndex.xlEdgeBottom].LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlLineStyleNone;
             rg1.Borders[Microsoft.Office.Interop.Excel.XlBordersIndex.xlEdgeBottom].Weight = 2;
             rg1.Borders[Microsoft.Office.Interop.Excel.XlBordersIndex.xlEdgeTop].LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlLineStyleNone;
@@ -372,20 +435,19 @@ drop table #tmp
             {
                 if (this.dtList[1].Rows[i]["Fty"].ToString() == "Total")
                 {
-
-                    wkcolor.Range[$"B{i + 2}", $"E{i + 2}"].Font.Bold = true;
+                    wkcolor.Range[$"C{i + 2}", $"F{i + 2}"].Font.Bold = true;
                 }
 
                 if (this.dtList[1].Rows[i]["Fty"].ToString() == "Grand Total")
                 {
-                    wkcolor.Cells[i + 2, 1].Value = "";
-                    wkcolor.Range[$"B{i + 2}", $"F{i + 2}"].Font.Bold = true;
+                    wkcolor.Cells[i + 2, 2].Value = "";
+                    wkcolor.Range[$"C{i + 2}", $"G{i + 2}"].Font.Bold = true;
                 }
             }
 
             //畫框線
             Microsoft.Office.Interop.Excel.Range rg2;
-            rg2 = wkcolor.Range["A1", $"F{this.dtList[1].Rows.Count + 1}"];
+            rg2 = wkcolor.Range["A1", $"G{this.dtList[1].Rows.Count + 1}"];
             rg2.Borders[Microsoft.Office.Interop.Excel.XlBordersIndex.xlEdgeBottom].LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlLineStyleNone;
             rg2.Borders[Microsoft.Office.Interop.Excel.XlBordersIndex.xlEdgeBottom].Weight = 2;
             rg2.Borders[Microsoft.Office.Interop.Excel.XlBordersIndex.xlEdgeTop].LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlLineStyleNone;
