6898a060bfb7a353d8020d33efc6d5e10c583338
JEFF <jeff.yeh@sportscity.com.tw>
2018-08-09 18:01:36 +0800

    ISP20180657 加在Pullout_Detail

--- DailyTransfer/exp_Pullout.sql
+++ DailyTransfer/exp_Pullout.sql
@@ -40,7 +40,7 @@ WHERE (LockDate BETWEEN (select DATEADD(DAY,1,PullLock) from Production.dbo.Syst
 AND Status <> 'New'
 ORDER BY Id
 
-SELECT B.* 
+SELECT B.* ,ExpressID = (select ExpressID from Production.dbo.PackingList where id = b.PackingListID)
 INTO Pullout_Detail
 FROM #CUR_PULLOUT1  A, Production.dbo.Pullout_Detail  B 
 WHERE A.ID = B.ID 
@@ -68,7 +68,6 @@ SELECT distinct b.*
 	   , DataFrom = 'G'
 	   , HCID = '             '
 	   , c.Abb
-	   , ExpressID = (select ExpressID from Production.dbo.PackingList p where p.id = a.PackingListID)
 INTO  #tmpFtyBooking1
 FROM Pullout_Detail  a, Production.dbo.GMTBooking b 
 left join Production.dbo.LocalSupp c on b.Forwarder = c.id
@@ -92,7 +91,6 @@ select ID = a.PackingListID
 	   , HCID = p1. ExpressID
 	   , p1.AddDate
 	   , p1.EditDate
-	   , p1.ExpressID
 into #tmpFtyBooking2
 from (	
 	select distinct PackingListID 
@@ -150,7 +148,6 @@ from (
 		   , DataFrom
 		   , HCID
 		   , Abb
-		   , ExpressID
 	from #tmpFtyBooking1
 
 	union all
@@ -193,7 +190,6 @@ from (
 		   , DataFrom
 		   , HCID
 		   , Abb = null
-		   , ExpressID
 	from #tmpFtyBooking2
 ) a 
 
