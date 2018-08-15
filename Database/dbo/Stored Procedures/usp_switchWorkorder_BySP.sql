e70ee7ef9880db30544ae9e11cd6317fe1304572
JEFF <jeff.yeh@sportscity.com.tw>
2018-08-15 12:22:15 +0800

    ISP20180652 在分配加scirefno條件

--- Database/dbo/Stored Procedures/usp_switchWorkorder_BySP.sql
+++ Database/dbo/Stored Procedures/usp_switchWorkorder_BySP.sql
@@ -63,6 +63,7 @@ order by isnull(hasforArticle.A,1),MixedSizeMarker desc,MarkerName
 --準備inline和qty分配資料
 select Inline = Min(s.Inline),[orderid] = o.id,oq.Article,occ.ColorID,oq.SizeCode,occ.PatternPanel,[Size_orderqty] = oq.qty,IDENTITY(int,1,1) as identRowid
 ,InlineForOrderby = isnull(Min(s.Inline),'9999/12/31')
+,sr.SCIRefno
 into #_tmpdisQty
 from Orders o WITH (NOLOCK)
 inner join Order_Qty oq WITH (NOLOCK) on oq.id = o.id
@@ -75,8 +76,13 @@ outer apply
 	inner join SewingSchedule_Detail ssd WITH (NOLOCK) on ss.ID = ssd.ID 
 	where ss.OrderID = o.id and ss.MDivisionID = o.MDivisionID and ssd.Article = oq.Article and oq.SizeCode = ssd.SizeCode and occ.Article = ssd.Article
 )s
+outer apply(
+	select ob.scirefno
+	from Order_BoF ob WITH (NOLOCK) 
+	where ob.Id = oe.id and ob.FabricCode = occ.FabricCode
+)sr
 where o.CuttingSP = @Cuttingid and o.MDivisionID = @MDivisionID and o.junk = 0
-group by s.Inline,o.id,oq.Article,occ.ColorID,oq.SizeCode,occ.PatternPanel,oq.qty 
+group by s.Inline,o.id,oq.Article,occ.ColorID,oq.SizeCode,occ.PatternPanel,oq.qty ,sr.SCIRefno
 order by InlineForOrderby,o.id
 --主要資料參數
 Declare @MixedSizeMarker varchar(2),@id varchar(13),@SizeCode varchar(8),@FirstSizeCode varchar(8),@colorid varchar(6),
@@ -111,12 +117,12 @@ Begin
 		select [SizeRatio]=oes.Qty,	b.identRowid,a.id,b.orderid,b.Article,b.SizeCode,b.Size_orderqty,a.ThisMarkerColor_MaxLayer,Rowid = IDENTITY(int,1,1) 
 		into #DistributeSource
 		from #WorkOrderMix a
-		inner join #_tmpdisQty b on a.ColorID = b.ColorID and a.FabricCombo = b.PatternPanel
+		inner join #_tmpdisQty b on a.ColorID = b.ColorID and a.FabricCombo = b.PatternPanel and a.SCIRefno = b.SCIRefno
 		inner join Order_EachCons_SizeQty oes on  oes.id = a.id and oes.Order_EachConsUkey = a.Order_EachConsUkey and b.SizeCode = oes.SizeCode
 		outer apply(select top 1 A=0 from Order_EachCons_Article WITH (NOLOCK) where Order_EachConsUkey=a.Order_EachConsUkey)hasforArticle
 		outer apply(select Article from Order_EachCons_Article  WITH (NOLOCK) where Order_EachConsUkey=a.Order_EachConsUkey)forArticle
 		where (b.Article in (forArticle.Article) or hasforArticle.A is null)--有forArticle已forArticle為主
-		and a.MarkerName = @MarkerName and a.ColorID = @colorid and a.FabricCombo = @FabricCombo and b.SizeCode = @sizecode
+		and a.MarkerName = @MarkerName and a.ColorID = @colorid and a.FabricCombo = @FabricCombo and b.SizeCode = @sizecode  and a.SCIRefno = @SCIRefno
 		order by identRowid
 		if (select sum(Size_orderqty) from #DistributeSource) < @ThisTotalCutQty
 		begin
