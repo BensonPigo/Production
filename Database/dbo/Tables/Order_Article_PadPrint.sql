3ffad26b9a4cca69c8f5203fb30370c8b617fd45
ricky <ricky.chen@sportscity.com.tw>
2018-08-30 11:04:05 +0800

    新增[dbo].[Order_Article_PadPrint]、[dbo].[Style_Article_PadPrint]

---
+++ Database/dbo/Tables/Order_Article_PadPrint.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Order_Article_PadPrint](
+    [ID]          VARCHAR (13) CONSTRAINT [DF_Order_Article_PadPrint_ID] NOT NULL,
+    [Article]         VARCHAR (8)   CONSTRAINT [DF_Order_Article_PadPrint_Article]   NOT NULL,
+    [ColorID]     VARCHAR (6) CONSTRAINT [DF_Order_Article_PadPrint_ColorID]  NOT NULL,
+    [Qty]  INT           NULL,
+    CONSTRAINT [PK_Order_Article_PadPrint] PRIMARY KEY CLUSTERED ([ID] ASC, [Article] ASC,[ColorID]  ASC)
+);
