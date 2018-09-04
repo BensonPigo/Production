9aa40cfd0866e2d5179a87016053f7f31eaea1b1
Willy <willy.wei@sprotscity.com.tw>
2018-09-04 17:20:10 +0800

    ISP20180722 1.新增SewingR07 2.新增Excel Sewing_R07.xltx

---
+++ Sci.Production.Sewing/R07.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict;
+using Sci.Win;
+using Sci.Data;
+using System.Runtime.InteropServices;
+using static Sci.MyUtility;
+
+namespace Sci.Production.Sewing
+{
+    public partial class R07 : Sci.Win.Tems.PrintForm
+    {
+        private string Factory;
+        private string M;
+        private string strIssueDate1;
+        private string strIssueDate2;
+        private string strApvDate1;
+        private string strApvDate2;
+        private string SubCon;
+        private string ContractNumb;
+        private string SP;
+        private string Status;
+        private DataTable printData;
+
+        public R07(ToolStripMenuItem menuitem) : base(menuitem)
+        {
+            this.InitializeComponent();
+            MyUtility.Tool.SetupCombox(this.comboStatus, 2, 1, "New,New,Confirmed,Confirmed");
+            this.comboStatus.SelectedIndex = 0;
+        }
+
+        protected override bool ValidateInput()
+        {
+            /*  string strBuyerDateStart = this.dateRangeBuyerDelivery.Value1.Empty() ? string.Empty : ((DateTime)this.dateRangeBuyerDelivery.Value1).ToString("yyyy/MM/dd");*/
+            this.Factory = this.txtfactory.Text;
+            this.M = this.txtMdivisionM.Text;
+            this.strIssueDate1 = this.dateIssueDate.Value1.Empty() ? string.Empty : ((DateTime)this.dateIssueDate.Value1).ToString("yyyy/MM/dd");
+            this.strIssueDate2 = this.dateIssueDate.Value2.Empty() ? string.Empty : ((DateTime)this.dateIssueDate.Value2).ToString("yyyy/MM/dd");
+            this.strApvDate1 = this.dateRangeApvDate.Value1.Empty()? string.Empty : ((DateTime)this.dateRangeApvDate.Value1).ToString("yyyy/MM/dd");
+            this.strApvDate2 = this.dateRangeApvDate.Value2.Empty() ? string.Empty : ((DateTime)this.dateRangeApvDate.Value2).ToString("yyyy/MM/dd");
+            this.SubCon = this.txtSubCon.Text;
+            this.ContractNumb = this.txtContract.Text;
+            this.SP = this.txtSPNO.Text;
+            this.Status = this.comboStatus.SelectedValue.ToString();
+
+            // 必輸條件
+            if (true)
+            {
+
+            }
+            return base.ValidateInput();
+        }
+
+        protected override DualResult OnAsyncDataLoad(ReportEventArgs e)
+        {
+            List<string> listSQLFilter = new List<string>();
+            #region Filter
+
+            if (!MyUtility.Check.Empty(this.Factory))
+            {
+                listSQLFilter.Add($"and s.Factoryid = '{this.Factory}'");
+            }
+
+            if (!MyUtility.Check.Empty(this.M))
+            {
+                listSQLFilter.Add($"and s.MDivisionID = '{this.M}'");
+            }
+
+            if (!MyUtility.Check.Empty(this.SubCon))
+            {
+                listSQLFilter.Add($"and s.SubConOutFty = '{this.SubCon}'");
+            }
+
+            if (!MyUtility.Check.Empty(this.ContractNumb))
+            {
+                listSQLFilter.Add($"and s.ContractNumber = '{this.ContractNumb}'");
+            }
+
+            if (!MyUtility.Check.Empty(this.SP))
+            {
+                listSQLFilter.Add($"and sd.Orderid = '{this.SP}'");
+            }
+
+            if (!MyUtility.Check.Empty(this.Status))
+            {
+                listSQLFilter.Add($"and s.Status = '{this.Status}'");
+            }
+
+            if (!MyUtility.Check.Empty(this.strIssueDate1))
+            {
+                listSQLFilter.Add($"and s.IssueDate >= '{this.strIssueDate1}'");
+            }
+
+            if (!MyUtility.Check.Empty(this.strIssueDate2))
+            {
+                listSQLFilter.Add($"and s.IssueDate <= '{this.strIssueDate2}'");
+            }
+
+            if (!MyUtility.Check.Empty(this.strApvDate1))
+            {
+                listSQLFilter.Add($"and s.ApvDate >= '{this.strApvDate1}'");
+            }
+
+            if (!MyUtility.Check.Empty(this.strApvDate2))
+            {
+                listSQLFilter.Add($"and s.ApvDate <= '{this.strApvDate2}'");
+            }
+            #endregion
+
+            string sqlcmd = $@"
+select 
+[Subcon Name]  = sd.SubConOutFty
+,[Contract No] = sd.ContractNumber
+,[Style] = o.StyleID
+,[SP] = o.ID
+,[Qty] = Order_Qty.Qty
+,[Sewing_CPU] = tms.SewingCPU
+,[SubConPrice/CPU] = sd.UnitPrice
+,[Cut] = tms.CuttingCPU
+,[H.T] = tms.HeatTransfer
+,[Inspection] = tms.InspectionCPU
+,[OtherCpu] = tms.OtherCPU
+,[EMB] = tms.EMBPrice
+,[Print] = tms.PrintingPrice
+,[OtherAmt] = tms.OtherAmt
+,[Price/CPU] = iif((tms.CuttingCPU+tms.HeatTransfer+tms.InspectionCPU+tms.OtherCPU)=0,0, (sd.UnitPrice-tms.EMBPrice-tms.PrintingPrice-tms.OtherAmt) / (tms.CuttingCPU+tms.HeatTransfer+tms.InspectionCPU+tms.OtherCPU))
+,[Or] = ''
+,[Min Rate Cpu] = ''
+,[TTLCPU] = Order_Qty.Qty * tms.SewingCPU
+,[Contract Amt]=Order_Qty.Qty * sd.UnitPrice
+,[ExchangeRate]=''
+,[Contract Amt_usd]=''
+,[SR]=''
+,[Remark]=''
+from dbo.SubconOutContract_Detail sd with (nolock)
+left join Orders o with (nolock) on sd.Orderid = o.ID
+left join SubconOutContract s with (nolock) 
+on sd.SubConOutFty=s.SubConOutFty  and sd.ContractNumber=s.ContractNumber
+outer apply(
+	select isnull(sum(Qty),0) Qty
+	from Order_Qty with (nolock) 
+	where ID = sd.OrderID and Article = sd.Article 
+)Order_Qty
+outer apply (
+    select  
+    [SewingCPU] = sum(iif(ArtworkTypeID = 'SEWING',TMS/1400,0)),
+    [CuttingCPU]= sum(iif(ArtworkTypeID = 'CUTTING',TMS/1400,0)),
+    [InspectionCPU]= sum(iif(ArtworkTypeID = 'INSPECTION',TMS/1400,0)),
+    [OtherCPU]= sum(iif(ArtworkTypeID in ('INSPECTION','CUTTING','SEWING'),0,TMS/1400)),
+    [OtherAmt]= sum(iif(ArtworkTypeID in ('PRINTING','EMBROIDERY'),0,Price)) * sd.OutputQty,
+    [EMBAmt] = sum(iif(ArtworkTypeID = 'EMBROIDERY',Price,0)) * sd.OutputQty,
+    [PrintingAmt] = sum(iif(ArtworkTypeID = 'PRINTING',Price,0)) * sd.OutputQty,
+    [OtherPrice]= sum(iif(ArtworkTypeID in ('PRINTING','EMBROIDERY'),0,Price)),
+    [EMBPrice] = sum(iif(ArtworkTypeID = 'EMBROIDERY',Price,0)),
+    [PrintingPrice] = sum(iif(ArtworkTypeID = 'PRINTING',Price,0)),
+	[HeatTransfer] = sum(iif(ArtworkTypeID = 'HEAT TRANSFER',TMS/1400,0))
+    from Order_TmsCost with (nolock)
+    where ID = sd.OrderID
+) as tms
+where 1=1
+{listSQLFilter.JoinToString($"{Environment.NewLine} ")}
+";
+
+            DualResult result = DBProxy.Current.Select(null, sqlcmd.ToString(),  out this.printData);
+            if (!result)
+            {
+                DualResult failResult = new DualResult(false, "Query data fail\r\n" + result.ToString());
+                return failResult;
+            }
+
+            return Result.True;
+        }
+
+        protected override bool OnToExcel(ReportDefinition report)
+        {
+            // 顯示筆數於PrintForm上Count欄位
+            this.SetCount(this.printData.Rows.Count);
+
+            if (this.printData.Rows.Count <= 0)
+            {
+                MyUtility.Msg.WarningBox("Data not found!");
+                return false;
+            }
+
+            Microsoft.Office.Interop.Excel.Application objApp = MyUtility.Excel.ConnectExcel(Sci.Env.Cfg.XltPathDir + "\\Sewing_R07.xltx"); 
+            objApp.Visible = false;
+            MyUtility.Excel.CopyToXls(this.printData, "", "Sewing_R07.xltx", 3, false, null, objApp);
+            Microsoft.Office.Interop.Excel.Worksheet wks = objApp.ActiveWorkbook.Worksheets[1];
+            for (int c = 1; c <= this.printData.Rows.Count; c++)
+            {
+                wks.Cells[c + 3, 16] = $"=IF(O{c + 3}<Q{c + 3},\"<\",\">\")";
+                wks.Cells[c + 3, 21] = $"=S{c + 3}/T{c + 3}";
+            }
+
+            objApp.Cells.EntireColumn.AutoFit();    //自動欄寬
+            objApp.Cells.EntireRow.AutoFit();       ////自動欄高
+
+            // 畫框線
+            int rowcnt = this.printData.Rows.Count + 3;
+            Microsoft.Office.Interop.Excel.Range rg1;
+            rg1 = wks.get_Range("A4", $"W{this.printData.Rows.Count + 3}");
+            rg1.Borders[Microsoft.Office.Interop.Excel.XlBordersIndex.xlEdgeBottom].LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlLineStyleNone;
+            rg1.Borders[Microsoft.Office.Interop.Excel.XlBordersIndex.xlEdgeBottom].Weight = 2;
+            rg1.Borders[Microsoft.Office.Interop.Excel.XlBordersIndex.xlEdgeTop].LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlLineStyleNone;
+            rg1.Borders[Microsoft.Office.Interop.Excel.XlBordersIndex.xlEdgeTop].Weight = 2;
+            rg1.Borders[Microsoft.Office.Interop.Excel.XlBordersIndex.xlEdgeLeft].LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlLineStyleNone;
+            rg1.Borders[Microsoft.Office.Interop.Excel.XlBordersIndex.xlEdgeLeft].Weight = 2;
+            rg1.Borders[Microsoft.Office.Interop.Excel.XlBordersIndex.xlEdgeRight].LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlLineStyleNone;
+            rg1.Borders[Microsoft.Office.Interop.Excel.XlBordersIndex.xlEdgeRight].Weight = 2;
+            rg1.Borders[Microsoft.Office.Interop.Excel.XlBordersIndex.xlInsideHorizontal].LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlLineStyleNone;
+            rg1.Borders[Microsoft.Office.Interop.Excel.XlBordersIndex.xlInsideHorizontal].Weight = 2;
+            rg1.Borders[Microsoft.Office.Interop.Excel.XlBordersIndex.xlInsideVertical].LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlLineStyleNone;
+            rg1.Borders[Microsoft.Office.Interop.Excel.XlBordersIndex.xlInsideVertical].Weight = 2;
+
+            #region Save Excel
+            string excelFile = Sci.Production.Class.MicrosoftFile.GetName("Sewing_R07");
+            objApp.ActiveWorkbook.SaveAs(excelFile);
+            objApp.Quit();
+            Marshal.ReleaseComObject(objApp);
+            Marshal.ReleaseComObject(objApp);
+            excelFile.OpenFile();
+            #endregion
+
+            return true;
+        }
+    }
+}
