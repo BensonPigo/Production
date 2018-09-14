7bd3ddd87f0f26bdb1d2ebb3c64b6b828786f057
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2018-09-14 11:50:21 +0800

    ISP20180818 - 彈跳視窗改用WarningBox

--- Sci.Production.Shipping/P05_ExpenseData.cs
+++ Sci.Production.Shipping/P05_ExpenseData.cs
@@ -109,7 +109,7 @@ where se.WKNo = '{0}' and se.junk=0", this.id);
                 string DebitNote = this.gridExpenseData.Rows[0].Cells[4].Value.ToString();
                 if (DebitNote.Length != 13 || !DebitNote.StartsWith("SD"))
                 {
-                    MyUtility.Msg.ErrorBox("Debit Note is not correct, please check again!");
+                    MyUtility.Msg.WarningBox("Debit Note is not correct, please check again!");
                     return;
                 }
 
