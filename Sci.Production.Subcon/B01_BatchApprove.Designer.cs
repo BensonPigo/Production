51ce2b18488fca2f2630dc91d40bfd3c714917b6
JEFF <jeff.yeh@sportscity.com.tw>
2018-09-20 19:53:55 +0800

    ISP20180789 使用close來完成Lock 當資料 Lock 時Description, Unit, AccountNo 不可被編輯 Subcon B01 – Detail 畫面 Approve 按鈕 (將 Approve 按鈕移除， 使用 Toolbar Confirm 取代此功能) Miscellaneous B02 –刪除原本程式,新增一隻用input1完成 變更Table, Pkey為ukey 3個詢價都加入權限判斷 加入Status欄位直接給New 詢價預設選擇第一個supp Save時先檢查選擇的那筆,supp,currency,Price都必須輸入 Batch approve 檢查選擇的資料[NewSupp] , [NewCurrency], [NewPrice] 不能為空, 一次顯示全部訊息 若有單已經被其他使用者先approve則跳過, 加上status = 'New' 為更新條件

---
+++ Sci.Production.Subcon/B01_BatchApprove.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Sci.Production.Subcon
+{
+    partial class B01_BatchApprove
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.grid1 = new Sci.Win.UI.Grid();
+            this.grid2 = new Sci.Win.UI.Grid();
+            this.listControlBindingSource1 = new Sci.Win.UI.ListControlBindingSource(this.components);
+            this.listControlBindingSource2 = new Sci.Win.UI.ListControlBindingSource(this.components);
+            this.btnconfirm = new Sci.Win.UI.Button();
+            this.btnRefresh = new Sci.Win.UI.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.grid1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.grid2);
+            this.splitContainer1.Size = new System.Drawing.Size(1214, 441);
+            this.splitContainer1.SplitterDistance = 802;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // grid1
+            // 
+            this.grid1.AllowUserToAddRows = false;
+            this.grid1.AllowUserToDeleteRows = false;
+            this.grid1.AllowUserToResizeRows = false;
+            this.grid1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.grid1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.grid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid1.DataSource = this.listControlBindingSource1;
+            this.grid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid1.EditingEnter = Ict.Win.UI.DataGridViewEditingEnter.NextCellOrNextRow;
+            this.grid1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.grid1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.grid1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(162)))), ((int)(((byte)(163)))));
+            this.grid1.Location = new System.Drawing.Point(0, 0);
+            this.grid1.Name = "grid1";
+            this.grid1.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.grid1.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.grid1.RowTemplate.Height = 24;
+            this.grid1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grid1.ShowCellToolTips = false;
+            this.grid1.Size = new System.Drawing.Size(802, 441);
+            this.grid1.TabIndex = 0;
+            this.grid1.TabStop = false;
+            // 
+            // grid2
+            // 
+            this.grid2.AllowUserToAddRows = false;
+            this.grid2.AllowUserToDeleteRows = false;
+            this.grid2.AllowUserToResizeRows = false;
+            this.grid2.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.grid2.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.grid2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid2.DataSource = this.listControlBindingSource2;
+            this.grid2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid2.EditingEnter = Ict.Win.UI.DataGridViewEditingEnter.NextCellOrNextRow;
+            this.grid2.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.grid2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.grid2.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(162)))), ((int)(((byte)(163)))));
+            this.grid2.Location = new System.Drawing.Point(0, 0);
+            this.grid2.Name = "grid2";
+            this.grid2.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.grid2.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.grid2.RowTemplate.Height = 24;
+            this.grid2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grid2.ShowCellToolTips = false;
+            this.grid2.Size = new System.Drawing.Size(408, 441);
+            this.grid2.TabIndex = 0;
+            this.grid2.TabStop = false;
+            // 
+            // btnconfirm
+            // 
+            this.btnconfirm.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnconfirm.Location = new System.Drawing.Point(952, 448);
+            this.btnconfirm.Name = "btnconfirm";
+            this.btnconfirm.Size = new System.Drawing.Size(117, 30);
+            this.btnconfirm.TabIndex = 6;
+            this.btnconfirm.Text = "Approve";
+            this.btnconfirm.UseVisualStyleBackColor = true;
+            this.btnconfirm.Click += new System.EventHandler(this.btnconfirm_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRefresh.Location = new System.Drawing.Point(1075, 448);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(117, 30);
+            this.btnRefresh.TabIndex = 7;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // B01_BatchApprove
+            // 
+            this.ClientSize = new System.Drawing.Size(1214, 490);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnconfirm);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "B01_BatchApprove";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grid1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private Win.UI.Grid grid1;
+        private Win.UI.ListControlBindingSource listControlBindingSource1;
+        private Win.UI.Grid grid2;
+        private Win.UI.ListControlBindingSource listControlBindingSource2;
+        private Win.UI.Button btnconfirm;
+        private Win.UI.Button btnRefresh;
+    }
+}
---
+++ Sci.Production.Subcon/B01_BatchApprove.cs
@@ -0,0 +1,286 @@
+﻿using Ict;
+using Ict.Win;
+using Sci.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Transactions;
+using System.Windows.Forms;
+
+namespace Sci.Production.Subcon
+{
+    public partial class B01_BatchApprove : Sci.Win.Forms.Base
+    {
+        public B01_BatchApprove()
+        {
+            InitializeComponent();
+            this.EditMode = true;
+        }
+
+        Ict.Win.UI.DataGridViewCheckBoxColumn col_chk = new Ict.Win.UI.DataGridViewCheckBoxColumn();
+        Ict.Win.UI.DataGridViewCheckBoxColumn col_chk2 = new Ict.Win.UI.DataGridViewCheckBoxColumn();
+        DataTable master, detail;
+
+        protected override void OnFormLoaded()
+        {
+            base.OnFormLoaded();
+
+            this.grid1.IsEditingReadOnly = false;
+            Helper.Controls.Grid.Generator(this.grid1)
+                .CheckBox("Selected", header: "", width: Widths.AnsiChars(3), iseditable: true, trueValue: true, falseValue: false).Get(out col_chk)
+                .Text("Category", header: "Category", width: Widths.AnsiChars(6), iseditingreadonly: true)
+                .Text("Refno", header: "Refno", width: Widths.AnsiChars(6), iseditingreadonly: true)
+                .Text("Description", header: "Description", width: Widths.AnsiChars(6), iseditingreadonly: true)
+                .Text("UnitID", header: "Unit", width: Widths.AnsiChars(6), iseditingreadonly: true)
+                .Text("LocalSuppID", header: "Supp", width: Widths.AnsiChars(6), iseditingreadonly: true)
+                .Text("CurrencyID", header: "Currency", width: Widths.AnsiChars(6), iseditingreadonly: true)
+                .Numeric("Price", header: "Price", width: Widths.AnsiChars(6), decimal_places: 4, iseditingreadonly: true)
+                .Text("NewSupp", header: "New Supp", width: Widths.AnsiChars(6), iseditingreadonly: true)
+                .Text("NewCurrency", header: "New Currency", width: Widths.AnsiChars(6), iseditingreadonly: true)
+                .Numeric("NewPrice", header: "New Price", width: Widths.AnsiChars(6), decimal_places: 4, iseditingreadonly: true)
+                ;
+            
+            Helper.Controls.Grid.Generator(this.grid2)
+                .CheckBox("Selected", header: "", width: Widths.AnsiChars(3), iseditable: true, trueValue: true, falseValue: false).Get(out col_chk2)
+                .Text("LocalSuppID", header: "Supp", width: Widths.AnsiChars(6), iseditingreadonly: true)
+                .Text("SuppAbb", header: "Supp Abb", width: Widths.AnsiChars(6), iseditingreadonly: true)
+                .Text("CurrencyID", header: "Currency", width: Widths.AnsiChars(6), iseditingreadonly: true)
+                .Numeric("Price", header: "Price", width: Widths.AnsiChars(6), decimal_places: 4, iseditingreadonly: true)
+                ;
+
+            // 按Header沒有排序功能
+            for (int i = 0; i < this.grid1.ColumnCount; i++)
+            {
+                this.grid1.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+
+            // 按Header沒有排序功能
+            for (int i = 0; i < this.grid2.ColumnCount; i++)
+            {
+                this.grid2.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+            query();
+        }
+        
+        public string sqlcmd(string Refno = "")
+        {
+            string wheresql = string.Empty;
+            if (!MyUtility.Check.Empty(Refno))
+            {
+                wheresql = $" and l.RefNo = '{Refno}'";
+            }
+            string sqlcmd;
+            return sqlcmd = $@"
+select l.*,
+	NewSupp = case when ChooseSupp = 1 then localsuppid1
+				   when ChooseSupp = 2 then localsuppid2
+				   when ChooseSupp = 3 then localsuppid3
+				   when ChooseSupp = 4 then localsuppid4 end,
+	NewCurrency = case 
+				   when ChooseSupp = 1 then currencyid1
+				   when ChooseSupp = 2 then currencyid2
+				   when ChooseSupp = 3 then currencyid3
+				   when ChooseSupp = 4 then currencyid4 end,
+	NewPrice = case 
+				   when ChooseSupp = 1 then price1
+				   when ChooseSupp = 2 then price2
+				   when ChooseSupp = 3 then price3
+				   when ChooseSupp = 4 then price4 end,
+    Selected = 0,
+	lq.Ukey
+	
+from LocalItem l
+inner join LocalItem_Quot lq on l.RefNo = lq.RefNo
+where lq.status <> 'Approved'
+{wheresql}
+";
+        }
+
+        private void query()
+        {
+            DataSet datas = null;
+            #region
+            string sqlCmd = sqlcmd() +
+                $@"
+select	RefNo
+		, IssueDate
+		, Ukey
+		, ChooseSupp
+		, [Status]
+into #bas
+from LocalItem_Quot
+where [Status] != 'Approved'
+
+select	[Selected] = iif (tmp.ChooseSupp = tmp.seq, 1, 0)
+		, *
+from (
+	select *
+	from #bas bas
+	outer apply (
+		select	seq = 1
+				, LocalSuppID = liq.LocalSuppID1
+				, CurrencyID = liq.CurrencyID1
+				, Price = liq.Price1
+		from LocalItem_Quot liq
+		where bas.Ukey = liq.Ukey
+	) one
+
+	union all 
+	select *
+	from #bas bas
+	outer apply (
+		select	seq = 2
+				, LocalSuppID = liq.LocalSuppID2
+				, CurrencyID = liq.CurrencyID2
+				, Price = liq.Price2
+		from LocalItem_Quot liq
+		where bas.Ukey = liq.Ukey
+	) two
+
+	union all 
+	select *
+	from #bas bas
+	outer apply (
+		select	seq = 3
+				, LocalSuppID = liq.LocalSuppID3
+				, CurrencyID = liq.CurrencyID3
+				, Price = liq.Price3
+		from LocalItem_Quot liq
+		where bas.Ukey = liq.Ukey
+	) three
+
+	union all 
+	select *
+	from #bas bas
+	outer apply (
+		select	seq = 4
+				, LocalSuppID = liq.LocalSuppID4
+				, CurrencyID = liq.CurrencyID4
+				, Price = liq.Price4
+		from LocalItem_Quot liq
+		where bas.Ukey = liq.Ukey
+	) four
+) tmp
+outer apply(select SuppAbb = abb from LocalSupp l where l.id=LocalSuppID)abb
+order by Ukey, seq
+
+drop table #bas
+";
+            #endregion
+            if (!SQL.Selects("", sqlCmd, out datas))
+            {
+                MyUtility.Msg.WarningBox(sqlCmd, "DB error!!");
+                return;
+            }
+
+            if (listControlBindingSource1.DataSource != null)
+            {
+                listControlBindingSource1.DataSource = null;
+            }
+            if (listControlBindingSource2.DataSource != null)
+            {
+                listControlBindingSource2.DataSource = null;
+            }
+
+            if (datas.Tables[0].Rows.Count == 0)
+            {
+                return;
+            }
+
+            master = datas.Tables[0];
+            master.TableName = "Master";
+
+            detail = datas.Tables[1];
+            detail.TableName = "Detail";
+
+            DataRelation relation = new DataRelation("rel1"
+                    , new DataColumn[] { master.Columns["Refno"] }
+                    , new DataColumn[] { detail.Columns["Refno"] }
+                    );
+
+            datas.Relations.Add(relation);
+
+            listControlBindingSource1.DataSource = datas;
+            listControlBindingSource1.DataMember = "Master";
+            listControlBindingSource2.DataSource = listControlBindingSource1;
+            listControlBindingSource2.DataMember = "rel1";
+            this.grid1.AutoResizeColumns();
+            this.grid1.Columns["Description"].Width = 100;
+            this.grid2.AutoResizeColumns();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            query();
+        }
+
+        private void btnconfirm_Click(object sender, EventArgs e)
+        {
+            this.grid1.ValidateControl();
+            if (master.Select("Selected = 1").Length == 0)
+            {
+                MyUtility.Msg.WarningBox("Must select datas!");
+                return;
+            }
+            DataTable selectdt = master.Select("Selected = 1").CopyToDataTable();
+
+            List<string> chkmsg = new List<string>();
+            foreach (DataRow drow in selectdt.Rows)
+            {
+                if (MyUtility.Check.Empty(drow["NewSupp"]) || MyUtility.Check.Empty(drow["NewCurrency"]) || MyUtility.Check.Empty(drow["NewPrice"]))
+                {
+                    chkmsg.Add($"[{drow["Refno"]}]");
+                }
+            }
+
+            if (chkmsg.Count > 0)
+            {
+                MyUtility.Msg.WarningBox(@" [NewSupp] or [NewCurrency] or [NewPrice] can not empty!
+" + "Refno: " + string.Join(",", chkmsg));
+                return;
+            }
+
+            confirm(selectdt);
+            query();
+        }
+
+        public bool confirm(DataTable selectdt)
+        {
+            using (TransactionScope scope = new TransactionScope())
+            {
+                DualResult upResult;
+
+                // 若有單已經被其他使用者先approve則跳過, 加上status = 'New' 為更新條件
+                string mergeSql = $@"
+Update localitem_quot set Status = 'Approved' ,editname = '{Env.User.UserID}', editdate = GETDATE() where ukey in (select ukey from #tmp) and status = 'New'
+
+merge localitem t
+using #tmp s
+on t.Refno = s.Refno
+when matched and t.status = 'New' then update set
+    t.localsuppid = s.NewSupp,
+    t.currencyid = s.NewCurrency,
+    t.price = s.NewPrice,
+    t.quotdate = GETDATE(),
+    t.editname = '{Env.User.UserID}',
+    t.editdate = GETDATE()
+;
+";
+                DataTable dt;
+                if (!(upResult = MyUtility.Tool.ProcessWithDatatable(selectdt, "Refno,Ukey,NewSupp,NewCurrency,NewPrice", mergeSql, out dt)))
+                {
+                    this.ShowErr(upResult);
+                    return false;
+                }
+
+                scope.Complete();
+            }
+
+            MyUtility.Msg.InfoBox("Success!");
+            return true;
+        }
+    }
+}
