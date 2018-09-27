6ac1f4952c830c73eeb87cdc325f02e92cfd1935
ricky <ricky.chen@sportscity.com.tw>
2018-09-27 16:57:50 +0800

    Cutting P20 手動輸入修正

--- Sci.Production.Cutting/P20.cs
+++ Sci.Production.Cutting/P20.cs
@@ -189,7 +189,7 @@ and a.MDivisionId = '{Sci.Env.User.Keyword}'
                 {
                     SelectItem sele;
 
-                    sele = new SelectItem(dt, string.Empty, string.Empty, dr["cutref"].ToString(), false, ",");
+                    sele = new SelectItem(dt, "cutref", string.Empty, dr["cutref"].ToString(), false, ",");
                     DialogResult result = sele.ShowDialog();
                     if (result == DialogResult.Cancel) { return; }
                     e.FormattedValue = sele.GetSelectedString();
