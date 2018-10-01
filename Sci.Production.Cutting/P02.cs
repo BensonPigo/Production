f5444879d185211462ca217c7ec8954f38c739bc
ricky <ricky.chen@sportscity.com.tw>
2018-10-01 14:03:42 +0800

    Cutting P02 ActDate顯示僅排除非New的資料

--- Sci.Production.Cutting/P02.cs
+++ Sci.Production.Cutting/P02.cs
@@ -252,7 +252,7 @@ outer apply
 	Select actcutdate = iif(sum(cut_b.Layer) = a.Layer, Max(cut.cdate),null)
 	From cuttingoutput cut WITH (NOLOCK) 
 	inner join cuttingoutput_detail cut_b WITH (NOLOCK) on cut.id = cut_b.id
-	Where cut_b.workorderukey = a.Ukey and cut.Status = 'confirmed' 
+	Where cut_b.workorderukey = a.Ukey and cut.Status != 'New' 
 )  as actcutdate
 outer apply(Select adduser = Name From Pass1 ps WITH (NOLOCK) Where ps.id = a.addName) as adduser
 outer apply(Select edituser = Name From Pass1 ps WITH (NOLOCK) Where ps.id = a.editName) as edituser
