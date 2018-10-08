dc04333a16daf6e647ec97c56a174ca185da3944
Willy <willy.wei@sprotscity.com.tw>
2018-10-08 10:48:45 +0800

    ISP20180854 1. add PPIC_R14 Report

---
+++ Sci.Production.PPIC/R14.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Sci.Production.PPIC
+{
+    partial class R14
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labIssueDate = new Sci.Win.UI.Label();
+            this.dateIssueDate = new Sci.Win.UI.DateRange();
+            this.dateBuyerDelivery = new Sci.Win.UI.DateRange();
+            this.txtSpNo1 = new Sci.Win.UI.TextBox();
+            this.labSPNo = new Sci.Win.UI.Label();
+            this.labBrand = new Sci.Win.UI.Label();
+            this.labBuyerDelivery = new Sci.Win.UI.Label();
+            this.labStatus = new Sci.Win.UI.Label();
+            this.txtSpNo2 = new Sci.Win.UI.TextBox();
+            this.txtBrand = new Sci.Production.Class.txtbrand();
+            this.comboDropDownList = new Sci.Production.Class.comboDropDownList(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(432, 137);
+            this.print.Visible = false;
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(432, 17);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(432, 53);
+            // 
+            // labIssueDate
+            // 
+            this.labIssueDate.Location = new System.Drawing.Point(28, 17);
+            this.labIssueDate.Name = "labIssueDate";
+            this.labIssueDate.Size = new System.Drawing.Size(100, 23);
+            this.labIssueDate.TabIndex = 94;
+            this.labIssueDate.Text = "Issue Date";
+            // 
+            // dateIssueDate
+            // 
+            // 
+            // 
+            // 
+            this.dateIssueDate.DateBox1.Location = new System.Drawing.Point(0, 0);
+            this.dateIssueDate.DateBox1.Name = "";
+            this.dateIssueDate.DateBox1.Size = new System.Drawing.Size(129, 23);
+            this.dateIssueDate.DateBox1.TabIndex = 0;
+            // 
+            // 
+            // 
+            this.dateIssueDate.DateBox2.Location = new System.Drawing.Point(151, 0);
+            this.dateIssueDate.DateBox2.Name = "";
+            this.dateIssueDate.DateBox2.Size = new System.Drawing.Size(129, 23);
+            this.dateIssueDate.DateBox2.TabIndex = 1;
+            this.dateIssueDate.Location = new System.Drawing.Point(131, 17);
+            this.dateIssueDate.Name = "dateIssueDate";
+            this.dateIssueDate.Size = new System.Drawing.Size(280, 23);
+            this.dateIssueDate.TabIndex = 95;
+            // 
+            // dateBuyerDelivery
+            // 
+            // 
+            // 
+            // 
+            this.dateBuyerDelivery.DateBox1.Location = new System.Drawing.Point(0, 0);
+            this.dateBuyerDelivery.DateBox1.Name = "";
+            this.dateBuyerDelivery.DateBox1.Size = new System.Drawing.Size(129, 23);
+            this.dateBuyerDelivery.DateBox1.TabIndex = 0;
+            // 
+            // 
+            // 
+            this.dateBuyerDelivery.DateBox2.Location = new System.Drawing.Point(151, 0);
+            this.dateBuyerDelivery.DateBox2.Name = "";
+            this.dateBuyerDelivery.DateBox2.Size = new System.Drawing.Size(129, 23);
+            this.dateBuyerDelivery.DateBox2.TabIndex = 1;
+            this.dateBuyerDelivery.Location = new System.Drawing.Point(131, 107);
+            this.dateBuyerDelivery.Name = "dateBuyerDelivery";
+            this.dateBuyerDelivery.Size = new System.Drawing.Size(280, 23);
+            this.dateBuyerDelivery.TabIndex = 96;
+            // 
+            // txtSpNo1
+            // 
+            this.txtSpNo1.BackColor = System.Drawing.Color.White;
+            this.txtSpNo1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtSpNo1.Location = new System.Drawing.Point(131, 48);
+            this.txtSpNo1.Name = "txtSpNo1";
+            this.txtSpNo1.Size = new System.Drawing.Size(130, 23);
+            this.txtSpNo1.TabIndex = 97;
+            // 
+            // labSPNo
+            // 
+            this.labSPNo.Location = new System.Drawing.Point(28, 48);
+            this.labSPNo.Name = "labSPNo";
+            this.labSPNo.Size = new System.Drawing.Size(100, 23);
+            this.labSPNo.TabIndex = 98;
+            this.labSPNo.Text = "SP#";
+            // 
+            // labBrand
+            // 
+            this.labBrand.Location = new System.Drawing.Point(28, 78);
+            this.labBrand.Name = "labBrand";
+            this.labBrand.Size = new System.Drawing.Size(100, 23);
+            this.labBrand.TabIndex = 99;
+            this.labBrand.Text = "Brand";
+            // 
+            // labBuyerDelivery
+            // 
+            this.labBuyerDelivery.Location = new System.Drawing.Point(28, 107);
+            this.labBuyerDelivery.Name = "labBuyerDelivery";
+            this.labBuyerDelivery.Size = new System.Drawing.Size(100, 23);
+            this.labBuyerDelivery.TabIndex = 100;
+            this.labBuyerDelivery.Text = "Buyer Delivery";
+            // 
+            // labStatus
+            // 
+            this.labStatus.Location = new System.Drawing.Point(28, 137);
+            this.labStatus.Name = "labStatus";
+            this.labStatus.Size = new System.Drawing.Size(100, 23);
+            this.labStatus.TabIndex = 101;
+            this.labStatus.Text = "Status";
+            // 
+            // txtSpNo2
+            // 
+            this.txtSpNo2.BackColor = System.Drawing.Color.White;
+            this.txtSpNo2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtSpNo2.Location = new System.Drawing.Point(283, 48);
+            this.txtSpNo2.Name = "txtSpNo2";
+            this.txtSpNo2.Size = new System.Drawing.Size(130, 23);
+            this.txtSpNo2.TabIndex = 102;
+            // 
+            // txtBrand
+            // 
+            this.txtBrand.BackColor = System.Drawing.Color.White;
+            this.txtBrand.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtBrand.Location = new System.Drawing.Point(131, 78);
+            this.txtBrand.Name = "txtBrand";
+            this.txtBrand.Size = new System.Drawing.Size(130, 23);
+            this.txtBrand.TabIndex = 103;
+            // 
+            // comboDropDownList
+            // 
+            this.comboDropDownList.BackColor = System.Drawing.Color.White;
+            this.comboDropDownList.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboDropDownList.FormattingEnabled = true;
+            this.comboDropDownList.IsSupportUnselect = true;
+            this.comboDropDownList.Location = new System.Drawing.Point(131, 137);
+            this.comboDropDownList.Name = "comboDropDownList";
+            this.comboDropDownList.OldText = "";
+            this.comboDropDownList.Size = new System.Drawing.Size(130, 24);
+            this.comboDropDownList.TabIndex = 104;
+            this.comboDropDownList.Type = "Pms_AVOStatus";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(262, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 17);
+            this.label1.TabIndex = 105;
+            this.label1.Text = "～";
+            // 
+            // R14
+            // 
+            this.ClientSize = new System.Drawing.Size(524, 214);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboDropDownList);
+            this.Controls.Add(this.txtBrand);
+            this.Controls.Add(this.txtSpNo2);
+            this.Controls.Add(this.labStatus);
+            this.Controls.Add(this.labBuyerDelivery);
+            this.Controls.Add(this.labBrand);
+            this.Controls.Add(this.labSPNo);
+            this.Controls.Add(this.txtSpNo1);
+            this.Controls.Add(this.dateBuyerDelivery);
+            this.Controls.Add(this.dateIssueDate);
+            this.Controls.Add(this.labIssueDate);
+            this.Name = "R14";
+            this.Text = "R14. Avoid Verbal Orders Report";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.labIssueDate, 0);
+            this.Controls.SetChildIndex(this.dateIssueDate, 0);
+            this.Controls.SetChildIndex(this.dateBuyerDelivery, 0);
+            this.Controls.SetChildIndex(this.txtSpNo1, 0);
+            this.Controls.SetChildIndex(this.labSPNo, 0);
+            this.Controls.SetChildIndex(this.labBrand, 0);
+            this.Controls.SetChildIndex(this.labBuyerDelivery, 0);
+            this.Controls.SetChildIndex(this.labStatus, 0);
+            this.Controls.SetChildIndex(this.txtSpNo2, 0);
+            this.Controls.SetChildIndex(this.txtBrand, 0);
+            this.Controls.SetChildIndex(this.comboDropDownList, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.Label labIssueDate;
+        private Win.UI.DateRange dateIssueDate;
+        private Win.UI.DateRange dateBuyerDelivery;
+        private Win.UI.TextBox txtSpNo1;
+        private Win.UI.Label labSPNo;
+        private Win.UI.Label labBrand;
+        private Win.UI.Label labBuyerDelivery;
+        private Win.UI.Label labStatus;
+        private Win.UI.TextBox txtSpNo2;
+        private Class.txtbrand txtBrand;
+        private Class.comboDropDownList comboDropDownList;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Sci.Production.PPIC/R14.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict;
+using Sci.Win;
+using System.Data.SqlClient;
+using Sci.Data;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+
+namespace Sci.Production.PPIC
+{
+    public partial class R14 : Sci.Win.Tems.PrintForm
+    {
+        private List<SqlParameter> sqlpar = new List<SqlParameter>();
+        private DataTable dtPrint;
+        private DualResult result;
+        private string sqlcmd;
+
+        public R14(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnFormLoaded()
+        {
+            base.OnFormLoaded();
+            this.comboDropDownList.SelectedIndex = 4;
+        }
+
+        protected override bool ValidateInput()
+        {
+            this.sqlpar.Clear();
+
+           
+            string where = string.Empty;
+
+            #region 檢查必輸條件 & 加入SQL where 參數
+            if ((MyUtility.Check.Empty(this.dateIssueDate.Value1) || MyUtility.Check.Empty(this.dateIssueDate.Value2))
+                && (MyUtility.Check.Empty(this.txtSpNo1.Text) || MyUtility.Check.Empty(this.txtSpNo2.Text))
+                && MyUtility.Check.Empty(this.txtBrand.Text)
+                && (MyUtility.Check.Empty(dateBuyerDelivery.Value1) || MyUtility.Check.Empty(dateBuyerDelivery.Value2)))
+            {
+                MyUtility.Msg.WarningBox("Please key-in condition!");
+                return false;
+            }
+
+            if (!MyUtility.Check.Empty(this.dateIssueDate.Value1))
+            {
+                where += $" and a1.cdate >='{((DateTime)this.dateIssueDate.Value1).ToString("yyyy/MM/dd")}'";
+            }
+
+            if (!MyUtility.Check.Empty(this.dateIssueDate.Value2))
+            {
+                where += $" and a1.cdate <='{((DateTime)this.dateIssueDate.Value2).ToString("yyyy/MM/dd")}'";
+            }
+
+            if (!MyUtility.Check.Empty(this.txtBrand.Text))
+            {
+                where += $" and o.BrandID = '{this.txtBrand.Text}'";
+            }
+
+            if (!MyUtility.Check.Empty(this.txtSpNo1.Text))
+            {
+                where += $" and o.id >='{this.txtSpNo1.Text}'";
+            }
+
+            if (!MyUtility.Check.Empty(this.txtSpNo2.Text))
+            {
+                where += $" and o.id <='{this.txtSpNo2.Text}'";
+            }
+
+            if (!MyUtility.Check.Empty(this.dateBuyerDelivery.Value1))
+            {
+                where += $" and oq.BuyerDelivery >='{((DateTime)this.dateBuyerDelivery.Value1).ToString("yyyy/MM/dd")}'";
+            }
+
+            if (!MyUtility.Check.Empty(this.dateBuyerDelivery.Value2))
+            {
+                where += $" and oq.BuyerDelivery <='{((DateTime)this.dateBuyerDelivery.Value2).ToString("yyyy/MM/dd")}'";
+            }
+
+            if (!MyUtility.Check.Empty(this.comboDropDownList.Text))
+            {
+                where += $" and a1.status='{comboDropDownList.Text}'";
+            }
+
+            sqlcmd = $@"
+select a1.cDate
+,o.FtyGroup
+,o.BrandID
+,o.SewLine
+,a2.OrderID
+,o.StyleID
+,o.CustPONo
+,c.Alias
+,oq.Qty
+,a2.OrderShipmodeSeq
+,a2.ShipModeID
+,oq.BuyerDelivery
+,[TotalCrtns] = Packing.qty
+,a3.RefNo
+,a1.Handle
+from AVO a1
+left join AVO_Detail a2 on a1.ID=a2.ID
+left join orders o on o.ID=a2.OrderID
+left join Country c on c.ID=o.Dest
+left join Order_QtyShip oq on oq.Id=o.ID and oq.ShipmodeID=a2.ShipModeID
+	and oq.Seq=a2.OrderShipmodeSeq
+outer apply(
+	select SUM(PD.CTNQty) qty
+	from PackingList_Detail PD 
+	LEFT JOIN PackingList P ON PD.ID=P.ID
+	where PD.orderid= a2.OrderID AND P.ShipModeID= a2.ShipModeID 
+	AND PD.OrderShipmodeSeq = a2.OrderShipmodeSeq
+)Packing
+outer apply(
+	select RefNo = STUFF((
+		select concat(',',RefNo)
+		from(
+			select distinct Refno
+			from AVO_Detail_RefNo
+			where AVO_DetailUkey=a2.Ukey
+		) s
+	for xml path ('')
+	) ,1,1,'')
+)a3
+where 1=1
+ {where}
+";
+            #endregion
+            return base.ValidateInput();
+        }
+
+        protected override DualResult OnAsyncDataLoad(ReportEventArgs e)
+        {
+            return DBProxy.Current.Select(string.Empty, this.sqlcmd, sqlpar, out this.dtPrint);
+        }
+
+        protected override bool OnToExcel(ReportDefinition report)
+        {
+            if (dtPrint.Rows.Count == 0)
+            {
+                MyUtility.Msg.WarningBox("Data not found!");
+                return false;
+            }
+
+            this.ShowWaitMessage("Excel Processing...");
+            this.SetCount(dtPrint.Rows.Count); // 顯示筆數
+
+            Excel.Application objApp = new Excel.Application();
+            Sci.Utility.Report.ExcelCOM com = new Sci.Utility.Report.ExcelCOM(Sci.Env.Cfg.XltPathDir + "\\PPIC_R14.xltx", objApp);
+            Excel.Worksheet worksheet = objApp.Sheets[1];
+            com.WriteTable(dtPrint, 3);
+            worksheet.get_Range($"A3:O{MyUtility.Convert.GetString(2 + dtPrint.Rows.Count)}").Borders.LineStyle = Excel.XlLineStyle.xlContinuous; // 畫線
+            com.ExcelApp.ActiveWorkbook.Sheets[1].Select(Type.Missing);
+            objApp.Visible = true;
+            Marshal.ReleaseComObject(worksheet);
+            Marshal.ReleaseComObject(objApp);
+            return true;
+        }
+    }
+}
