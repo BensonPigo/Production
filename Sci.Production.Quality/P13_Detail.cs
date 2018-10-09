1a785b847715bbc2487ee04b8c845d7751c1bff9
JEFF <jeff.yeh@sportscity.com.tw>
2018-10-09 12:17:41 +0800

    ISP20180898 QA P12_Detail_Detail, QA P13_Detail_Detail 畫面加上Manufacturer Test Condition (For Heat Transfer) ToPDF 若Artwork其中有一筆為HEAT TRANSFER, 則要出現 若無, 則不出現在報表中 R10新增欄位

--- Sci.Production.Quality/P13_Detail.cs
+++ Sci.Production.Quality/P13_Detail.cs
@@ -22,6 +22,7 @@ namespace Sci.Production.Quality
     public partial class P13_Detail : Sci.Win.Subs.Input4
     {
         private DataRow masterDr;
+        private DataRow Detaildr;
         private string reportNo;
         private string id;
         private bool isSee = false;
@@ -105,7 +106,6 @@ namespace Sci.Production.Quality
         {
             #region 表頭設定
             MyUtility.Check.Seek($"select * from MockupWash WITH (NOLOCK) where ID = '{this.id}'", out this.masterDr);
-            DataRow Detaildr;
             MyUtility.Check.Seek($"select * from MockupWash_Detail WITH (NOLOCK) where ReportNo = '{this.reportNo}'", out Detaildr);
 
             this.displayStyleID.Text = this.masterDr["StyleID"].ToString();
@@ -123,6 +123,14 @@ namespace Sci.Production.Quality
                 this.displayResult.Text = "";
                 this.txtTechnician.Text = "";
                 this.txtMR.Text = "";
+                this.numAPT.Value = 0;
+                this.numAFT.Value = 0;
+                this.numCT.Value = 0;
+                this.numP.Value = 0;
+                this.numT.Value = 0;
+                this.num2Pr.Value = 0;
+                this.num2Pnr.Value = 0;
+                this.txtPOff.Text = "";
             }
             else
             {
@@ -135,6 +143,14 @@ namespace Sci.Production.Quality
                 this.displayResult.Text = Detaildr["Result"].ToString();
                 this.txtTechnician.textbox1_text = Detaildr["Technician"].ToString();
                 this.txtMR.textbox1_text = Detaildr["MR"].ToString();
+                this.numAPT.Value = MyUtility.Convert.GetDecimal(Detaildr["HTPlate"]);
+                this.numAFT.Value = MyUtility.Convert.GetDecimal(Detaildr["HTPlate"]);
+                this.numCT.Value = MyUtility.Convert.GetDecimal(Detaildr["HTCoolingTime"]);
+                this.numP.Value = MyUtility.Convert.GetDecimal(Detaildr["HTPressure"]);
+                this.numT.Value = MyUtility.Convert.GetDecimal(Detaildr["HTTime"]);
+                this.num2Pr.Value = MyUtility.Convert.GetDecimal(Detaildr["HT2ndPressreversed"]);
+                this.num2Pnr.Value = MyUtility.Convert.GetDecimal(Detaildr["HT2ndPressnoreverse"]);
+                this.txtPOff.Text = Detaildr["HTPellOff"].ToString();
                 string TestMethod = Detaildr["TestingMethod"].ToString();
                 if (TestMethod != @"a. 5 cycles continuous wash at 60 degree followed by 5 cycles continuous wash at 60 degree in standard domestic washing machine and tumble dry after the 10th cycle" &&
                     TestMethod != @"b. 5 cycles continuous wash at 40 degree followed by 5 cycles continuous wash at 60 degree in standard domestic washing machine and tumble dry after the 10th cycle" &&
@@ -384,6 +400,7 @@ namespace Sci.Production.Quality
             #endregion
             Helper.Controls.Grid.Generator(this.grid)
             .Text("ArtworkTypeID", "Artwork", width: Widths.AnsiChars(17),settings: ts_artwork)
+            .Text("Typeofprint", "Typeofprint", width: Widths.AnsiChars(17))
             .Text("Design", "Design", width: Widths.AnsiChars(15), iseditingreadonly: false)
             .Text("ArtworkColorName", "Artwork Color", width: Widths.AnsiChars(18),settings: ts_artworkColor)
             .Text("FabricRefNo", "Fabric Ref No.", width: Widths.AnsiChars(17))
@@ -531,6 +548,7 @@ namespace Sci.Production.Quality
         }
         private string CreatePDF()
         {
+            bool haveHT = ((DataTable)gridbs.DataSource).AsEnumerable().Any(r => MyUtility.Convert.GetString(r["ArtworkTypeID"]).EqualString("HEAT TRANSFER"));
             this.ShowWaitMessage("PDF Processing...");
             DataTable gridData = (DataTable)gridbs.DataSource;
             if (gridData.Rows.Count == 0)
@@ -538,8 +556,10 @@ namespace Sci.Production.Quality
                 return "1";
             }
 
+            string file = haveHT ? "Quality_P13_Detail_Report2" : "Quality_P13_Detail_Report";
+            int haveHTrow = haveHT ? 6 : 0;
             string sql_cmd = string.Empty;
-            Microsoft.Office.Interop.Excel.Application objApp = MyUtility.Excel.ConnectExcel(Sci.Env.Cfg.XltPathDir + "\\Quality_P13_Detail_Report.xltx");
+            Microsoft.Office.Interop.Excel.Application objApp = MyUtility.Excel.ConnectExcel(Sci.Env.Cfg.XltPathDir + "\\" + file + ".xltx");
             objApp.DisplayAlerts = false;//設定Excel的警告視窗是否彈出
             Microsoft.Office.Interop.Excel.Worksheet worksheet = objApp.ActiveWorkbook.Worksheets[1];   // 取得工作表
 
@@ -553,6 +573,17 @@ namespace Sci.Production.Quality
             worksheet.Cells[5,6] = MyUtility.Check.Empty(this.dateBoxSubmitDate.Value) ? string.Empty : this.dateBoxSubmitDate.Text;
             worksheet.Cells[6,6] = MyUtility.Check.Empty(this.dateBoxReceivedDate.Value) ? string.Empty : this.dateBoxReceivedDate.Text;
             worksheet.Cells[7,6] = this.masterDr["SeasonID"].ToString();
+            if (haveHT)
+            {
+                worksheet.Cells[10, 2] = this.Detaildr["HTPlate"].ToString();
+                worksheet.Cells[11, 2] = this.Detaildr["HTFlim"].ToString();
+                worksheet.Cells[12, 2] = this.Detaildr["HTTime"].ToString();
+                worksheet.Cells[13, 2] = this.Detaildr["HTPressure"].ToString();
+                worksheet.Cells[10, 6] = this.Detaildr["HTPellOff"].ToString();
+                worksheet.Cells[11, 6] = this.Detaildr["HT2ndPressnoreverse"].ToString();
+                worksheet.Cells[12, 6] = this.Detaildr["HT2ndPressreversed"].ToString();
+                worksheet.Cells[13, 6] = this.Detaildr["HTCoolingTime"].ToString();
+            }
 
             //插入圖片與Technician名字
             sql_cmd = $@"select p.name,[SignaturePic] = s.PicPath + t.SignaturePic
@@ -564,7 +595,7 @@ where t.ID = '{this.txtTechnician.TextBox1.Text}'";
             string technicianName = string.Empty;
             string picSource = string.Empty;
             Image img = null;
-            Microsoft.Office.Interop.Excel.Range cell = worksheet.Cells[12, 2];
+            Microsoft.Office.Interop.Excel.Range cell = worksheet.Cells[12+ haveHTrow, 2];
 
             if (MyUtility.Check.Seek(sql_cmd, out drTechnicianInfo))
             {
@@ -572,7 +603,7 @@ where t.ID = '{this.txtTechnician.TextBox1.Text}'";
                 picSource = drTechnicianInfo["SignaturePic"].ToString();
             }
 
-            worksheet.Cells[13, 2] = technicianName;
+            worksheet.Cells[13+ haveHTrow, 2] = technicianName;
 
             if (!MyUtility.Check.Empty(picSource))
             {
@@ -592,16 +623,16 @@ where t.ID = '{this.txtTechnician.TextBox1.Text}'";
             //插入多的row
             if (gridData.Rows.Count > 0)
             {
-                Microsoft.Office.Interop.Excel.Range rngToInsert = worksheet.get_Range("A10:G10", Type.Missing).EntireRow;
+                Microsoft.Office.Interop.Excel.Range rngToInsert = worksheet.get_Range($"A{10 + haveHTrow}:G{10 + haveHTrow}", Type.Missing).EntireRow;
                 for (int i = 1; i < gridData.Rows.Count; i++)
                 {
                     rngToInsert.Insert(Microsoft.Office.Interop.Excel.XlInsertShiftDirection.xlShiftDown);
-                    worksheet.get_Range(string.Format("E{0}:G{0}", MyUtility.Convert.GetString(10 + i - 1))).Merge(false);
+                    worksheet.get_Range(string.Format("E{0}:G{0}", MyUtility.Convert.GetString(10 + haveHTrow + i - 1))).Merge(false);
                 }
                 Marshal.ReleaseComObject(rngToInsert);
             }
             //塞進資料
-            int start_row = 10;
+            int start_row = 10+ haveHTrow;
             foreach (DataRow dr in gridData.Rows)
             {
                 string remark= dr["Remark"].ToString(); 
@@ -627,8 +658,8 @@ where t.ID = '{this.txtTechnician.TextBox1.Text}'";
 
             string strFileName = string.Empty;
             string strPDFFileName = string.Empty;
-            strFileName = Sci.Production.Class.MicrosoftFile.GetName("Quality_P13_Detail_Report");
-            strPDFFileName = Sci.Production.Class.MicrosoftFile.GetName("Quality_P13_Detail_Report", Sci.Production.Class.PDFFileNameExtension.PDF);
+            strFileName = Sci.Production.Class.MicrosoftFile.GetName(file);
+            strPDFFileName = Sci.Production.Class.MicrosoftFile.GetName(file, Sci.Production.Class.PDFFileNameExtension.PDF);
             objApp.ActiveWorkbook.SaveAs(strFileName);
             objApp.Quit();
             Marshal.ReleaseComObject(worksheet);
