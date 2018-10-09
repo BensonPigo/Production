e6488871e203d369b6de4f031d7ce6e5b3812cbb
JEFF <jeff.yeh@sportscity.com.tw>
2018-10-09 09:12:12 +0800

    ISP20180898.更新Table

--- Database/dbo/Tables/MockupOven_Detail_Detail.sql
+++ Database/dbo/Tables/MockupOven_Detail_Detail.sql
@@ -2,18 +2,21 @@
     [ID]            VARCHAR (13)  NOT NULL,
     [ReportNo]      VARCHAR (13)  NOT NULL,
     [Ukey]          BIGINT        IDENTITY (1, 1) NOT NULL,
-    [ArtworkTypeID] VARCHAR (20)  CONSTRAINT [DF__MockupOve__Artwo__23817F9A] DEFAULT ('') NULL,
-    [ArtworkColor]  VARCHAR (35)  CONSTRAINT [DF__MockupOve__Artwo__2475A3D3] DEFAULT ('') NULL,
-    [FabricRefNo]   VARCHAR (30)  CONSTRAINT [DF__MockupOve__Fabri__2569C80C] DEFAULT ('') NULL,
-    [FabricColor]   VARCHAR (35)  CONSTRAINT [DF__MockupOve__Fabri__265DEC45] DEFAULT ('') NULL,
-    [Design]        VARCHAR (100) NULL,
-    [Result]        VARCHAR (4)   CONSTRAINT [DF__MockupOve__Resul__2752107E] DEFAULT ('') NULL,
-    [Remark]        VARCHAR (300) CONSTRAINT [DF__MockupOve__Remar__284634B7] DEFAULT ('') NULL,
+    [ArtworkTypeID] VARCHAR (20)  DEFAULT ('') NULL,
+    [ArtworkColor]  VARCHAR (35)  DEFAULT ('') NULL,
+    [FabricRefNo]   VARCHAR (30)  DEFAULT ('') NULL,
+    [FabricColor]   VARCHAR (35)  DEFAULT ('') NULL,
+    [Result]        VARCHAR (4)   DEFAULT ('') NULL,
+    [Remark]        VARCHAR (300) DEFAULT ('') NULL,
     [AddDate]       DATETIME      NULL,
-    [AddName]       VARCHAR (10)  CONSTRAINT [DF__MockupOve__AddNa__293A58F0] DEFAULT ('') NULL,
+    [AddName]       VARCHAR (10)  DEFAULT ('') NULL,
     [EditDate]      DATETIME      NULL,
-    [EditName]      VARCHAR (10)  CONSTRAINT [DF__MockupOve__EditN__2A2E7D29] DEFAULT ('') NULL,
-    CONSTRAINT [PK__MockupOv__5F39671CA904D784] PRIMARY KEY CLUSTERED ([Ukey] ASC)
+    [EditName]      VARCHAR (10)  DEFAULT ('') NULL,
+    [Design]        VARCHAR (100) DEFAULT ('') NULL,
+    [Typeofprint]   VARCHAR (5)   NULL,
+    PRIMARY KEY CLUSTERED ([Ukey] ASC)
 );
+
+
 
 
--- Database/dbo/Tables/MockupWash_Detail_Detail.sql
+++ Database/dbo/Tables/MockupWash_Detail_Detail.sql
@@ -2,18 +2,21 @@
     [ID]            VARCHAR (13)  NOT NULL,
     [ReportNo]      VARCHAR (13)  NOT NULL,
     [Ukey]          BIGINT        IDENTITY (1, 1) NOT NULL,
-    [ArtworkTypeID] VARCHAR (20)  CONSTRAINT [DF__MockupWas__Artwo__3A64E4F2] DEFAULT ('') NULL,
-    [ArtworkColor]  VARCHAR (35)  CONSTRAINT [DF__MockupWas__Artwo__3B59092B] DEFAULT ('') NULL,
-    [FabricRefNo]   VARCHAR (30)  CONSTRAINT [DF__MockupWas__Fabri__3C4D2D64] DEFAULT ('') NULL,
-    [FabricColor]   VARCHAR (35)  CONSTRAINT [DF__MockupWas__Fabri__3D41519D] DEFAULT ('') NULL,
-    [Design]        VARCHAR (100) NULL,
-    [Result]        VARCHAR (4)   CONSTRAINT [DF__MockupWas__Resul__3E3575D6] DEFAULT ('') NULL,
-    [Remark]        VARCHAR (300) CONSTRAINT [DF__MockupWas__Remar__3F299A0F] DEFAULT ('') NULL,
+    [ArtworkTypeID] VARCHAR (20)  DEFAULT ('') NULL,
+    [ArtworkColor]  VARCHAR (35)  DEFAULT ('') NULL,
+    [FabricRefNo]   VARCHAR (30)  DEFAULT ('') NULL,
+    [FabricColor]   VARCHAR (35)  DEFAULT ('') NULL,
+    [Result]        VARCHAR (4)   DEFAULT ('') NULL,
+    [Remark]        VARCHAR (300) DEFAULT ('') NULL,
     [AddDate]       DATETIME      NULL,
-    [AddName]       VARCHAR (10)  CONSTRAINT [DF__MockupWas__AddNa__401DBE48] DEFAULT ('') NULL,
+    [AddName]       VARCHAR (10)  DEFAULT ('') NULL,
     [EditDate]      DATETIME      NULL,
-    [EditName]      VARCHAR (10)  CONSTRAINT [DF__MockupWas__EditN__4111E281] DEFAULT ('') NULL,
-    CONSTRAINT [PK__MockupWa__5F39671CD1DA547A] PRIMARY KEY CLUSTERED ([Ukey] ASC)
+    [EditName]      VARCHAR (10)  DEFAULT ('') NULL,
+    [Design]        VARCHAR (100) DEFAULT ('') NULL,
+    [Typeofprint]   VARCHAR (5)   NULL,
+    PRIMARY KEY CLUSTERED ([Ukey] ASC)
 );
+
+
 
 
