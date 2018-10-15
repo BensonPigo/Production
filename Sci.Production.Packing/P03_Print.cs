e477b357c1bc7a84e7044d8a464a17839f322afd
sinfy <sinfy.yeh@sportscity.com.tw>
2018-10-15 15:23:58 +0800

    ISP20180924-若Order_SizeSpec找不到要去Order_SizeSpec_OrderCombo找

--- Sci.Production.Packing/P03_Print.cs
+++ Sci.Production.Packing/P03_Print.cs
@@ -202,16 +202,18 @@ select * from(
     from PackingList_Detail pd
     inner join orders o on o.id = pd.orderid
     outer apply (
-	    select SizeCode=stuff((select concat('/',SizeSpec) 
+	    select SizeCode=stuff((select ('/'+isnull(x.SizeSpec,z.SizeSpec)) 
 	    from PackingList_Detail pd2 
-	    inner join Order_SizeSpec os on os.SizeCode = pd2.SizeCode
-	    where pd2.id = pd.id and pd2.CTNStartNo = pd.CTNStartNo and os.id = o.poid and os.SizeItem = 'S01' for xml path('')),1,1,'')
+	    outer apply(select SizeSpec from Order_SizeSpec os where os.SizeCode = pd2.SizeCode and os.id = o.poid and os.SizeItem = 'S01')x
+		outer apply(select SizeSpec from Order_SizeSpec_OrderCombo oso where oso.SizeCode = pd2.SizeCode and oso.id = o.poid and oso.OrderComboID = o.OrderComboID and SizeItem = 'S01')z
+	    where pd2.id = pd.id and pd2.CTNStartNo = pd.CTNStartNo for xml path('')),1,1,'')
     )a
     outer apply (select ct = count(SizeCode) from PackingList_Detail pd2 where pd2.id = pd.id and pd2.CTNStartNo = pd.CTNStartNo)b1
     outer apply (
-	    select qty=stuff((select concat('/',SizeSpec+'-',ShipQty) 
+	    select qty=stuff((select concat('/',isnull(x.SizeSpec,z.SizeSpec)+'-',ShipQty) 
 	    from PackingList_Detail pd2 
-	    left join Order_SizeSpec os on os.SizeCode = pd2.SizeCode and os.id = o.poid and os.SizeItem = 'S01'
+	    outer apply(select SizeSpec from Order_SizeSpec os where os.SizeCode = pd2.SizeCode and os.id = o.poid and os.SizeItem = 'S01')x
+		outer apply(select SizeSpec from Order_SizeSpec_OrderCombo oso where oso.SizeCode = pd2.SizeCode and oso.id = o.poid and oso.OrderComboID = o.OrderComboID and SizeItem = 'S01')z
 	    where pd2.id = pd.id and pd2.CTNStartNo = pd.CTNStartNo for xml path('')),1,1,'')
     )b
     where pd.id = '{this.masterData["ID"]}'
