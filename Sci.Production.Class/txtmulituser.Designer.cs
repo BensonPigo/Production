007a39e214c8f7a99b3426fefa686e2466b80520
Willy <willy.wei@sprotscity.com.tw>
2018-10-24 15:41:31 +0800

    ISP20180974 1.新增Summary 報表 2.新增共用Function txtmulituser

---
+++ Sci.Production.Class/txtmulituser.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Sci.Production.Class
+{
+    partial class txtmulituser
+    {
+        /// <summary> 
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 元件設計工具產生的程式碼
+
+        /// <summary> 
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new Sci.Win.UI.TextBox();
+            this.displayBox1 = new Sci.Win.UI.DisplayBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(78, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.PopUp += new System.EventHandler<Sci.Win.UI.TextBoxPopUpEventArgs>(this.textBox1_PopUp);
+            this.textBox1.Validating += new System.ComponentModel.CancelEventHandler(this.textBox1_Validating);
+            // 
+            // displayBox1
+            // 
+            this.displayBox1.Location = new System.Drawing.Point(79, 0);
+            this.displayBox1.Name = "displayBox1";
+            this.displayBox1.Size = new System.Drawing.Size(220, 22);
+            this.displayBox1.TabIndex = 1;
+            // 
+            // txtmulituser
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.Controls.Add(this.displayBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "txtmulituser";
+            this.Size = new System.Drawing.Size(300, 23);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.TextBox textBox1;
+        private Win.UI.DisplayBox displayBox1;
+    }
+}
