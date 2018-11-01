0ef6e767d5bdc81ad2d75cf8db4b7dceb91a0a29
DOMAIN\aaron.shie <onizuka865@gmail.com>
2018-11-01 13:57:51 +0800

    -ISP20181014 (New) 採購價格異常原因基本檔 - Centralized Subcon_B02, Subcon B02

---
+++ Sci.Production.Subcon/B02.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sci.Production.Subcon
+{
+    public partial class B02 : Sci.Win.Tems.Input1
+    {
+        public B02(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            this.InitializeComponent();
+        }
+    }
+}
