051a134d4242f54991a4908c83a32cec4c5b2059
DOMAIN\aaron.shie <onizuka865@gmail.com>
2018-11-05 13:35:36 +0800

    -ISP20181028 QA_P02. add batch encode function 01.加上batch encdoe功能 如果表身 AIR.Status 不等於Confirmed則該按扭不可按,否則可以按 02.將表身底色為白色且表身Result =''的資料帶至開窗grid資料 (此開窗請參考 grid右鍵修改功能) Inspected %:使用 numericUpDown 且只能輸入整數,後面顯示%  (如果user有打%,則不用在多顯示一個) 預設:100% 不可輸入大於100 小於等於0 Inspect Date: 預設帶入今日 Inspector :預設帶入登入人員 Result :預設為空 Close 關閉視窗
    Encode 時檢查
    Inspected Qty，Inspect Date，Inspector 不可為空
    
    Encode時請將表身打勾的項次呼叫 ,表身按右鍵 save+Encode功能
    Inspected Qty 請寫入 表身 Arrive Qty * Inspected %,
    其餘請寫入畫面上輸入的資料

---
+++ Sci.Production.Class/numericUpDownRate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sci.Production.Class
+{
+    public class numericUpDownRate : Sci.Win.UI.NumericUpDown
+    {
+        protected override void UpdateEditText()
+        {
+            this.Text = this.Value.ToString() + "%";
+        }
+    }
+}
