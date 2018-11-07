079fe6a2c6891e92edc353ad957628bc21596613
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2018-11-07 10:25:45 +0800

    ISP20181042 - PPIC、WareHouse P01增加ExpectionFormRemark按鈕

---
+++ Sci.Production.PPIC/P01_ExpectionFormRemark.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Sci.Production.PPIC
+{
+    partial class P01_ExpectionFormRemark
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new Sci.Win.UI.Panel();
+            this.button1 = new Sci.Win.UI.Button();
+            this.displayBox1 = new Sci.Win.UI.DisplayBox();
+            this.panel3 = new Sci.Win.UI.Panel();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 365);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(615, 37);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(524, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // displayBox1
+            // 
+            this.displayBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.displayBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.displayBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.displayBox1.Location = new System.Drawing.Point(0, 1);
+            this.displayBox1.Multiline = true;
+            this.displayBox1.Name = "displayBox1";
+            this.displayBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.displayBox1.Size = new System.Drawing.Size(615, 364);
+            this.displayBox1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(615, 1);
+            this.panel3.TabIndex = 1;
+            // 
+            // P01_ExpectionFormRemark
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 402);
+            this.Controls.Add(this.displayBox1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Name = "P01_ExpectionFormRemark";
+            this.Text = "ExpectionFormRemark";
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Win.UI.Panel panel2;
+        private Win.UI.Button button1;
+        private Win.UI.DisplayBox displayBox1;
+        private Win.UI.Panel panel3;
+    }
+}
