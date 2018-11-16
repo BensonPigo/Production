26dcea4b2d5184c45bb0780f98e4e2cdaa75a75b
sinfy <sinfy.yeh@sportscity.com.tw>
2018-11-16 11:42:40 +0800

    update table

--- Database/dbo/Tables/GMTBooking.sql
+++ Database/dbo/Tables/GMTBooking.sql
@@ -35,7 +35,7 @@
     [AddDate]                  DATETIME        NULL,
     [EditName]                 VARCHAR (10)    CONSTRAINT [DF_GMTBooking_EditName] DEFAULT ('') NULL,
     [EditDate]                 DATETIME        NULL,
-    [NoExportCharges]          BIT             DEFAULT ((0)) NULL,
+    [NoExportCharges]          BIT             CONSTRAINT [DF_GMTBooking_NoExportCharges] DEFAULT ((0)) NULL,
     [BIRID]                    INT             NULL,
     CONSTRAINT [PK_GMTBooking] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
