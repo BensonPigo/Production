fb908732cc81201fde11a4bff03a4bd5e07d2a6d
Willy <willy.wei@sprotscity.com.tw>
2018-11-20 17:58:51 +0800

    ISP20181098 1.元件txtSubProcess 加上default value : this.text

--- Sci.Production.Class/txtsubprocess.cs
+++ Sci.Production.Class/txtsubprocess.cs
@@ -18,11 +18,11 @@ namespace Sci.Production.Class
         {
             string sqlWhere = "Select ID From Subprocess WITH (NOLOCK) where junk=0";
 
-            Sci.Win.Tools.SelectItem2 item = new Sci.Win.Tools.SelectItem2(sqlWhere, headercaptions: "Subprocess ID", columnwidths: "30", defaults: null, defaultValueColumn: null);
+            Sci.Win.Tools.SelectItem2 item = new Sci.Win.Tools.SelectItem2(sqlWhere, headercaptions: "Subprocess ID", columnwidths: "30", defaults: this.Text, defaultValueColumn: "ID");
             DialogResult result = item.ShowDialog();
             if (result == DialogResult.Cancel) { return; }
             this.Text = item.GetSelectedString();
-
+           
             this.ValidateText();
         }
     }
