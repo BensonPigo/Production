2f7f386f6f9d4c3a7f5ce23023bc9d0b2f0c7195
JEFF <jeff.yeh@sportscity.com.tw>
2018-11-22 17:21:55 +0800

    ISP20181108 2. 新增PPIC R15 新兩兩個範本檔

---
+++ Sci.Production.PPIC/R15.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace Sci.Production.PPIC
+{
+    partial class R15
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbSP = new Sci.Win.UI.Label();
+            this.lbSCIDelivery = new Sci.Win.UI.Label();
+            this.lbReport = new Sci.Win.UI.Label();
+            this.label1 = new Sci.Win.UI.Label();
+            this.lbPOSMR = new Sci.Win.UI.Label();
+            this.lbBrand = new Sci.Win.UI.Label();
+            this.txtSP2 = new Sci.Win.UI.TextBox();
+            this.label7 = new Sci.Win.UI.Label();
+            this.txtSP1 = new Sci.Win.UI.TextBox();
+            this.dateSCIDelivery = new Sci.Win.UI.DateRange();
+            this.cboReportType = new Sci.Win.UI.ComboBox();
+            this.txtBrand = new Sci.Production.Class.txtbrand();
+            this.txtMR = new Sci.Production.Class.txtuser();
+            this.txtSMR = new Sci.Production.Class.txtuser();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(432, 137);
+            this.print.Visible = false;
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(432, 17);
+            this.toexcel.TabIndex = 7;
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(432, 53);
+            this.close.TabIndex = 8;
+            // 
+            // lbSP
+            // 
+            this.lbSP.Location = new System.Drawing.Point(9, 59);
+            this.lbSP.Name = "lbSP";
+            this.lbSP.Size = new System.Drawing.Size(100, 23);
+            this.lbSP.TabIndex = 537;
+            this.lbSP.Text = "SP#";
+            // 
+            // lbSCIDelivery
+            // 
+            this.lbSCIDelivery.Location = new System.Drawing.Point(9, 34);
+            this.lbSCIDelivery.Name = "lbSCIDelivery";
+            this.lbSCIDelivery.Size = new System.Drawing.Size(100, 23);
+            this.lbSCIDelivery.TabIndex = 536;
+            this.lbSCIDelivery.Text = "SCI Delivery";
+            // 
+            // lbReport
+            // 
+            this.lbReport.Location = new System.Drawing.Point(9, 9);
+            this.lbReport.Name = "lbReport";
+            this.lbReport.Size = new System.Drawing.Size(100, 23);
+            this.lbReport.TabIndex = 535;
+            this.lbReport.Text = "Report Type";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(9, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 548;
+            this.label1.Text = "MR";
+            // 
+            // lbPOSMR
+            // 
+            this.lbPOSMR.Location = new System.Drawing.Point(9, 134);
+            this.lbPOSMR.Name = "lbPOSMR";
+            this.lbPOSMR.Size = new System.Drawing.Size(100, 23);
+            this.lbPOSMR.TabIndex = 547;
+            this.lbPOSMR.Text = "SMR";
+            // 
+            // lbBrand
+            // 
+            this.lbBrand.Location = new System.Drawing.Point(9, 84);
+            this.lbBrand.Name = "lbBrand";
+            this.lbBrand.Size = new System.Drawing.Size(100, 23);
+            this.lbBrand.TabIndex = 546;
+            this.lbBrand.Text = "Brand";
+            // 
+            // txtSP2
+            // 
+            this.txtSP2.BackColor = System.Drawing.Color.White;
+            this.txtSP2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtSP2.Location = new System.Drawing.Point(265, 60);
+            this.txtSP2.Name = "txtSP2";
+            this.txtSP2.Size = new System.Drawing.Size(127, 23);
+            this.txtSP2.TabIndex = 553;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Location = new System.Drawing.Point(243, 60);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(19, 23);
+            this.label7.TabIndex = 552;
+            this.label7.Text = "～";
+            this.label7.TextStyle.Color = System.Drawing.SystemColors.WindowText;
+            // 
+            // txtSP1
+            // 
+            this.txtSP1.BackColor = System.Drawing.Color.White;
+            this.txtSP1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtSP1.Location = new System.Drawing.Point(113, 60);
+            this.txtSP1.Name = "txtSP1";
+            this.txtSP1.Size = new System.Drawing.Size(127, 23);
+            this.txtSP1.TabIndex = 551;
+            // 
+            // dateSCIDelivery
+            // 
+            // 
+            // 
+            // 
+            this.dateSCIDelivery.DateBox1.Location = new System.Drawing.Point(0, 0);
+            this.dateSCIDelivery.DateBox1.Name = "";
+            this.dateSCIDelivery.DateBox1.Size = new System.Drawing.Size(129, 23);
+            this.dateSCIDelivery.DateBox1.TabIndex = 0;
+            // 
+            // 
+            // 
+            this.dateSCIDelivery.DateBox2.Location = new System.Drawing.Point(151, 0);
+            this.dateSCIDelivery.DateBox2.Name = "";
+            this.dateSCIDelivery.DateBox2.Size = new System.Drawing.Size(129, 23);
+            this.dateSCIDelivery.DateBox2.TabIndex = 1;
+            this.dateSCIDelivery.Location = new System.Drawing.Point(112, 35);
+            this.dateSCIDelivery.Name = "dateSCIDelivery";
+            this.dateSCIDelivery.Size = new System.Drawing.Size(280, 23);
+            this.dateSCIDelivery.TabIndex = 550;
+            // 
+            // cboReportType
+            // 
+            this.cboReportType.BackColor = System.Drawing.Color.White;
+            this.cboReportType.EditMode = Sci.Win.UI.AdvEditModes.None;
+            this.cboReportType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.cboReportType.FormattingEnabled = true;
+            this.cboReportType.IsSupportUnselect = true;
+            this.cboReportType.Location = new System.Drawing.Point(112, 9);
+            this.cboReportType.Name = "cboReportType";
+            this.cboReportType.OldText = "";
+            this.cboReportType.Size = new System.Drawing.Size(128, 24);
+            this.cboReportType.TabIndex = 549;
+            // 
+            // txtBrand
+            // 
+            this.txtBrand.BackColor = System.Drawing.Color.White;
+            this.txtBrand.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtBrand.Location = new System.Drawing.Point(113, 84);
+            this.txtBrand.Name = "txtBrand";
+            this.txtBrand.Size = new System.Drawing.Size(66, 23);
+            this.txtBrand.TabIndex = 554;
+            // 
+            // txtMR
+            // 
+            this.txtMR.DisplayBox1Binding = "";
+            this.txtMR.Location = new System.Drawing.Point(112, 109);
+            this.txtMR.Name = "txtMR";
+            this.txtMR.Size = new System.Drawing.Size(300, 23);
+            this.txtMR.TabIndex = 555;
+            this.txtMR.TextBox1Binding = "";
+            // 
+            // txtSMR
+            // 
+            this.txtSMR.DisplayBox1Binding = "";
+            this.txtSMR.Location = new System.Drawing.Point(112, 135);
+            this.txtSMR.Name = "txtSMR";
+            this.txtSMR.Size = new System.Drawing.Size(300, 23);
+            this.txtSMR.TabIndex = 556;
+            this.txtSMR.TextBox1Binding = "";
+            // 
+            // R15
+            // 
+            this.ClientSize = new System.Drawing.Size(524, 197);
+            this.Controls.Add(this.txtSMR);
+            this.Controls.Add(this.txtMR);
+            this.Controls.Add(this.txtBrand);
+            this.Controls.Add(this.txtSP2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtSP1);
+            this.Controls.Add(this.dateSCIDelivery);
+            this.Controls.Add(this.cboReportType);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbPOSMR);
+            this.Controls.Add(this.lbBrand);
+            this.Controls.Add(this.lbSP);
+            this.Controls.Add(this.lbSCIDelivery);
+            this.Controls.Add(this.lbReport);
+            this.Name = "R15";
+            this.Text = "R15. M.Notice & Each Cons checking report";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.lbReport, 0);
+            this.Controls.SetChildIndex(this.lbSCIDelivery, 0);
+            this.Controls.SetChildIndex(this.lbSP, 0);
+            this.Controls.SetChildIndex(this.lbBrand, 0);
+            this.Controls.SetChildIndex(this.lbPOSMR, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.cboReportType, 0);
+            this.Controls.SetChildIndex(this.dateSCIDelivery, 0);
+            this.Controls.SetChildIndex(this.txtSP1, 0);
+            this.Controls.SetChildIndex(this.label7, 0);
+            this.Controls.SetChildIndex(this.txtSP2, 0);
+            this.Controls.SetChildIndex(this.txtBrand, 0);
+            this.Controls.SetChildIndex(this.txtMR, 0);
+            this.Controls.SetChildIndex(this.txtSMR, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.Label lbSP;
+        private Win.UI.Label lbSCIDelivery;
+        private Win.UI.Label lbReport;
+        private Win.UI.Label label1;
+        private Win.UI.Label lbPOSMR;
+        private Win.UI.Label lbBrand;
+        private Win.UI.TextBox txtSP2;
+        private Win.UI.Label label7;
+        private Win.UI.TextBox txtSP1;
+        private Win.UI.DateRange dateSCIDelivery;
+        private Win.UI.ComboBox cboReportType;
+        private Class.txtbrand txtBrand;
+        private Class.txtuser txtMR;
+        private Class.txtuser txtSMR;
+    }
+}
