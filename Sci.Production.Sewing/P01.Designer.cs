06c5388f82e1e5031ca0ec96321fe06bfc3d72be
JEFF <jeff.yeh@sportscity.com.tw>
2018-12-21 11:57:49 +0800

    ISP20181216 新增table SewingOutput_RequestUnlock 更新Production DB Sewing P01 • 新增Request Unlock按鈕，按鈕只有在Status not in ('Send','Locked') 才會顯示。 a. 按下去Unlock彈窗,使用Sci.Win.UI.SelectReason("Sewing_RVS") b. SelectReason視窗按下Save後發送信件 ISP20181216 開放Recall按鈕.sql RecallChkValue = Send',按下寫入SewingOutput_History，更新SewingOutput_DailyUnlock、 SewingOutput Locked有值且Status=Send[日結] 可以按下編輯 Save時若日結要檢查表頭QA Qty與Manhours數量不能改變
    新增Batch Recall
    新增 Batch Recall按鈕,按下先檢查權限,沒有跳訊息不開視窗
    按下Save 勾選資料寫入SewingOutput_History，更新SewingOutput_DailyUnlock、SewingOutpu

--- Sci.Production.Sewing/P01.Designer.cs
+++ Sci.Production.Sewing/P01.Designer.cs
@@ -60,6 +60,8 @@
             this.txtSubconOutFty = new Sci.Production.Class.txtLocalSupp();
             this.label1 = new Sci.Win.UI.Label();
             this.txtSubConOutContractNumber = new Sci.Win.UI.TextBox();
+            this.btnRequestUnlock = new Sci.Win.UI.Button();
+            this.btnBatchRecall = new Sci.Win.UI.Button();
             ((System.ComponentModel.ISupportInitialize)(this.detailgridbs)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.detailgrid2bs)).BeginInit();
             this.masterpanel.SuspendLayout();
@@ -76,6 +78,7 @@
             // 
             // masterpanel
             // 
+            this.masterpanel.Controls.Add(this.btnRequestUnlock);
             this.masterpanel.Controls.Add(this.txtSubConOutContractNumber);
             this.masterpanel.Controls.Add(this.label1);
             this.masterpanel.Controls.Add(this.txtSubconOutFty);
@@ -108,8 +111,7 @@
             this.masterpanel.Controls.Add(this.labelLine);
             this.masterpanel.Controls.Add(this.labelDate);
             this.masterpanel.Controls.Add(this.dateDate);
-            this.masterpanel.Size = new System.Drawing.Size(1000, 140);
-            this.masterpanel.Controls.SetChildIndex(this.gridicon, 0);
+            this.masterpanel.Size = new System.Drawing.Size(892, 140);
             this.masterpanel.Controls.SetChildIndex(this.dateDate, 0);
             this.masterpanel.Controls.SetChildIndex(this.labelDate, 0);
             this.masterpanel.Controls.SetChildIndex(this.labelLine, 0);
@@ -142,11 +144,13 @@
             this.masterpanel.Controls.SetChildIndex(this.txtSubconOutFty, 0);
             this.masterpanel.Controls.SetChildIndex(this.label1, 0);
             this.masterpanel.Controls.SetChildIndex(this.txtSubConOutContractNumber, 0);
+            this.masterpanel.Controls.SetChildIndex(this.btnRequestUnlock, 0);
+            this.masterpanel.Controls.SetChildIndex(this.gridicon, 0);
             // 
             // detailpanel
             // 
             this.detailpanel.Location = new System.Drawing.Point(0, 140);
-            this.detailpanel.Size = new System.Drawing.Size(1000, 285);
+            this.detailpanel.Size = new System.Drawing.Size(892, 209);
             // 
             // gridicon
             // 
@@ -159,20 +163,10 @@
             // 
             // detailgridcont
             // 
-            this.detailgridcont.Size = new System.Drawing.Size(1000, 285);
-            // 
-            // detail
-            // 
-            this.detail.Size = new System.Drawing.Size(1000, 463);
-            // 
-            // detailcont
-            // 
-            this.detailcont.Size = new System.Drawing.Size(1000, 425);
+            this.detailgridcont.Size = new System.Drawing.Size(892, 209);
             // 
             // detailbtm
             // 
-            this.detailbtm.Location = new System.Drawing.Point(0, 425);
-            this.detailbtm.Size = new System.Drawing.Size(1000, 38);
             this.detailbtm.TabIndex = 1;
             // 
             // browse
@@ -521,7 +515,7 @@
             // 
             // btnRevisedHistory
             // 
-            this.btnRevisedHistory.Location = new System.Drawing.Point(865, 5);
+            this.btnRevisedHistory.Location = new System.Drawing.Point(865, 38);
             this.btnRevisedHistory.Name = "btnRevisedHistory";
             this.btnRevisedHistory.Size = new System.Drawing.Size(122, 30);
             this.btnRevisedHistory.TabIndex = 6;
@@ -532,7 +526,7 @@
             // btnShareWorkingHoursToSP
             // 
             this.btnShareWorkingHoursToSP.EditMode = Sci.Win.UI.AdvEditModes.EnableOnEdit;
-            this.btnShareWorkingHoursToSP.Location = new System.Drawing.Point(753, 41);
+            this.btnShareWorkingHoursToSP.Location = new System.Drawing.Point(750, 72);
             this.btnShareWorkingHoursToSP.Name = "btnShareWorkingHoursToSP";
             this.btnShareWorkingHoursToSP.Size = new System.Drawing.Size(237, 30);
             this.btnShareWorkingHoursToSP.TabIndex = 7;
@@ -612,9 +606,30 @@
             this.txtSubConOutContractNumber.TabIndex = 33;
             this.txtSubConOutContractNumber.Validating += new System.ComponentModel.CancelEventHandler(this.TxtSubConOutContractNumber_Validating);
             // 
+            // btnRequestUnlock
+            // 
+            this.btnRequestUnlock.Location = new System.Drawing.Point(865, 4);
+            this.btnRequestUnlock.Name = "btnRequestUnlock";
+            this.btnRequestUnlock.Size = new System.Drawing.Size(122, 30);
+            this.btnRequestUnlock.TabIndex = 34;
+            this.btnRequestUnlock.Text = "Request Unlock";
+            this.btnRequestUnlock.UseVisualStyleBackColor = true;
+            this.btnRequestUnlock.Click += new System.EventHandler(this.BtnRequestUnlock_Click);
+            // 
+            // btnBatchRecall
+            // 
+            this.btnBatchRecall.Location = new System.Drawing.Point(869, 18);
+            this.btnBatchRecall.Name = "btnBatchRecall";
+            this.btnBatchRecall.Size = new System.Drawing.Size(122, 30);
+            this.btnBatchRecall.TabIndex = 35;
+            this.btnBatchRecall.Text = "Batch Recall";
+            this.btnBatchRecall.UseVisualStyleBackColor = true;
+            this.btnBatchRecall.Click += new System.EventHandler(this.BtnBatchRecall_Click);
+            // 
             // P01
             // 
             this.ClientSize = new System.Drawing.Size(1008, 525);
+            this.Controls.Add(this.btnBatchRecall);
             this.DefaultControl = "dateDate";
             this.DefaultControlForEdit = "txtdropdownlistShift";
             this.DefaultDetailOrder = "OrderID,ComboType,Article";
@@ -623,9 +638,12 @@
             this.GridUniqueKey = "ID,OrderID,ComboType,Article";
             this.IsSupportCopy = false;
             this.IsSupportPrint = false;
+            this.IsSupportRecall = true;
+            this.IsSupportSend = true;
             this.IsSupportUnconfirm = true;
             this.KeyField1 = "ID";
             this.Name = "P01";
+            this.RecallChkValue = "Send";
             this.SubDetailKeyField1 = "id,ukey";
             this.SubDetailKeyField2 = "id,SewingOutput_DetailUKey";
             this.SubGridAlias = "SewingOutput_Detail_Detail";
@@ -635,6 +653,7 @@
             this.UniqueExpress = "ID";
             this.WorkAlias = "SewingOutput";
             this.Controls.SetChildIndex(this.tabs, 0);
+            this.Controls.SetChildIndex(this.btnBatchRecall, 0);
             ((System.ComponentModel.ISupportInitialize)(this.detailgridbs)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.detailgrid2bs)).EndInit();
             this.masterpanel.ResumeLayout(false);
@@ -688,5 +707,7 @@
         private Win.UI.Label labelSubconOutFty;
         private Win.UI.Label label1;
         private Win.UI.TextBox txtSubConOutContractNumber;
+        private Win.UI.Button btnRequestUnlock;
+        private Win.UI.Button btnBatchRecall;
     }
 }
---
+++ Sci.Production.Sewing/P01_BatchRecall.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Ict.Win;
+using Ict;
+using Sci.Data;
+using System.Transactions;
+
+namespace Sci.Production.Sewing
+{
+    /// <summary>
+    /// P01_BatchRecall
+    /// </summary>
+    public partial class P01_BatchRecall : Sci.Win.Tems.QueryForm
+    {
+        /// <inheritdoc/>
+        public P01_BatchRecall()
+        {
+            this.InitializeComponent();
+        }
+
+        private Ict.Win.UI.DataGridViewCheckBoxColumn col_chk = new Ict.Win.UI.DataGridViewCheckBoxColumn();
+
+        /// <inheritdoc/>
+        protected override void OnFormLoaded()
+        {
+            base.OnFormLoaded();
+            this.grid1.IsEditingReadOnly = false;
+            this.Helper.Controls.Grid.Generator(this.grid1)
+            .CheckBox("Selected", header: string.Empty, width: Widths.AnsiChars(3), iseditable: true, trueValue: true, falseValue: false).Get(out this.col_chk)
+            .Date("OutputDate", header: "Date", width: Widths.AnsiChars(10), iseditingreadonly: true)
+            .Text("FactoryID", header: "Factory", width: Widths.AnsiChars(6), iseditingreadonly: true)
+            .Text("SewingLineID", header: "Line#", width: Widths.AnsiChars(6), iseditingreadonly: true)
+            .Text("Team", header: "Team", width: Widths.AnsiChars(6), iseditingreadonly: true)
+            .Text("Shift", header: "Shift", width: Widths.AnsiChars(6), iseditingreadonly: true)
+            .Numeric("QAQty", header: "QAQty", width: Widths.AnsiChars(6), iseditingreadonly: true)
+            .Text("request", header: "request", width: Widths.AnsiChars(15), iseditingreadonly: true)
+            .Text("Reason", header: "Reason ", width: Widths.AnsiChars(15), iseditingreadonly: true)
+            .Text("remark", header: "Remark", width: Widths.AnsiChars(8), iseditingreadonly: true)
+            ;
+            this.Query();
+        }
+
+        private void Query()
+        {
+            string dateOutput1 = string.Empty;
+            string dateOutput2 = string.Empty;
+            string line = string.Empty;
+            string where = string.Empty;
+            if (!MyUtility.Check.Empty(this.dateOutput.Value1))
+            {
+                dateOutput1 = ((DateTime)this.dateOutput.Value1).ToString("yyyy/MM/dd");
+                where += $"and so.OutputDate >= '{dateOutput1}' ";
+            }
+
+            if (!MyUtility.Check.Empty(this.dateOutput.Value2))
+            {
+                dateOutput2 = ((DateTime)this.dateOutput.Value2).ToString("yyyy/MM/dd");
+                where += $"and so.OutputDate <= '{dateOutput2}' ";
+            }
+
+            if (!MyUtility.Check.Empty(this.txtsewinglineLine.Text))
+            {
+                line = this.txtsewinglineLine.Text;
+                where += $"and so.SewingLineID = '{line}' ";
+            }
+
+            DataTable dt;
+            string sqlcmd = $@"
+select selected = cast(0 as bit)
+    , so.OutputDate
+    , so.FactoryID
+    , so.SewingLineID
+    , so.Team
+    , so.Shift
+    , so.QAQty
+    , request= concat((select p1.name from pass1 p1 where p1.id=sod.requestname) ,'-',sod.requestdate)
+    , reason=sod.reasonid + '-' + (select name from Reason where ReasonTypeID='Sewing_RVS' and id=sod.reasonid)
+    , sod.remark
+    , so.id
+    , sod.ukey
+from SewingOutput so
+inner join SewingOutput_DailyUnlock sod on so.id = sod.SewingOutputID
+where 1=1
+and so.Status = 'Send' 
+and so.FactoryID = '{Sci.Env.User.Factory}'
+{where}
+order by so.LockDate,so.SewingLineID,so.Team,so.Shift
+";
+            DualResult result = DBProxy.Current.Select("Production", sqlcmd, out dt);
+            if (!result)
+            {
+                this.ShowErr(result);
+                return;
+            }
+
+            this.listControlBindingSource1.DataSource = dt;
+        }
+
+        private void BtnQuery_Click(object sender, EventArgs e)
+        {
+            this.listControlBindingSource1.DataSource = null;
+            this.Query();
+            if (this.listControlBindingSource1.DataSource != null && ((DataTable)this.listControlBindingSource1.DataSource).Rows.Count == 0)
+            {
+                MyUtility.Msg.InfoBox("Data not found!");
+            }
+        }
+
+        private void Btnsave_Click(object sender, EventArgs e)
+        {
+            this.grid1.ValidateControl();
+            DataTable dt = ((DataTable)this.listControlBindingSource1.DataSource).Select("selected = 1").CopyToDataTable();
+            string sqlcmd = $@"
+insert into SewingOutput_History (ID,HisType,OldValue,NewValue,ReasonID,Remark,AddName,AddDate)
+select id,'Status','Send','New',sod.ReasonID,sod.Remark,'{Sci.Env.User.UserID}',GETDATE()
+from #tmp t
+inner join SewingOutput_DailyUnlock  sod on t.ukey = sod.ukey
+
+Update sod set
+    UnLockDate = getdate()
+    , UnLockName= '{Sci.Env.User.UserID}'
+from SewingOutput_DailyUnlock sod
+where sod.ukey in (select ukey from #tmp)
+
+update s set Status='New', LockDate = null from SewingOutput s where id in (select id from #tmp)
+";
+            DataTable dt2;
+            using (TransactionScope scope = new TransactionScope())
+            {
+                DualResult upResult;
+                if (!MyUtility.Check.Empty(sqlcmd))
+                {
+                    if (!(upResult = MyUtility.Tool.ProcessWithDatatable(dt,string.Empty,sqlcmd,out dt2)))
+                    {
+                        this.ShowErr(upResult);
+                        return;
+                    }
+                }
+
+                scope.Complete();
+            }
+
+            MyUtility.Msg.InfoBox("Successfully");
+
+            this.listControlBindingSource1.DataSource = null;
+            this.Query();
+            if (this.listControlBindingSource1.DataSource != null && ((DataTable)this.listControlBindingSource1.DataSource).Rows.Count == 0)
+            {
+                MyUtility.Msg.InfoBox("Data not found!");
+            }
+        }
+
+        private void Btnclose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
