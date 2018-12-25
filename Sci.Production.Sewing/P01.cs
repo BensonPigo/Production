ccc87a9ba5e4f925a37d0270af25743e8802c590
JEFF <jeff.yeh@sportscity.com.tw>
2018-12-25 10:21:55 +0800

    ISP20181216Mailto預設帶出登入者工廠的Manger信箱

--- Sci.Production.Sewing/P01.cs
+++ Sci.Production.Sewing/P01.cs
@@ -2243,7 +2243,7 @@ WHERE  sewqty < packqty ",
             DialogResult dResult = callReason.ShowDialog(this);
             if (dResult == System.Windows.Forms.DialogResult.OK)
             {
-                string toAddress = string.Empty;
+                string toAddress = MyUtility.GetValue.Lookup($@"select p.EMail from Factory f inner join pass1 p on p.id = f.Manager where f.id = '{Sci.Env.User.Factory}'");
                 string ccAddress = "PMSHelp@sportscity.com.tw";
                 string subject = "Request Unlock Sewing";
 
