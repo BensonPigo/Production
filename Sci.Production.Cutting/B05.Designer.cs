dc4e9f6bb7dcbbc910602f151ccb1192ed4f39e9
JEFF <jeff.yeh@sportscity.com.tw>
2019-01-21 17:59:08 +0800

    ISP20190008 在Cutting 新增B05 Spreading No. ToolBar只需要開新增/修改 修改時ID欄位不能被編輯 CutCellID 使用txtCell元件

---
+++ Sci.Production.Cutting/B05.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Sci.Production.Cutting
+{
+    partial class B05
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkJunk = new Sci.Win.UI.CheckBox();
+            this.txtDescription = new Sci.Win.UI.TextBox();
+            this.labelDescription = new Sci.Win.UI.Label();
+            this.txtID = new Sci.Win.UI.TextBox();
+            this.labelID = new Sci.Win.UI.Label();
+            this.txtMdivision1 = new Sci.Production.Class.txtMdivision();
+            this.label1 = new Sci.Win.UI.Label();
+            this.txtCell1 = new Sci.Production.Class.txtCell();
+            this.label2 = new Sci.Win.UI.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).BeginInit();
+            this.detail.SuspendLayout();
+            this.detailcont.SuspendLayout();
+            this.detailbtm.SuspendLayout();
+            this.tabs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // detail
+            // 
+            this.detail.Size = new System.Drawing.Size(828, 400);
+            // 
+            // detailcont
+            // 
+            this.detailcont.Controls.Add(this.label2);
+            this.detailcont.Controls.Add(this.txtCell1);
+            this.detailcont.Controls.Add(this.label1);
+            this.detailcont.Controls.Add(this.txtMdivision1);
+            this.detailcont.Controls.Add(this.checkJunk);
+            this.detailcont.Controls.Add(this.txtDescription);
+            this.detailcont.Controls.Add(this.labelDescription);
+            this.detailcont.Controls.Add(this.txtID);
+            this.detailcont.Controls.Add(this.labelID);
+            this.detailcont.Size = new System.Drawing.Size(828, 362);
+            // 
+            // detailbtm
+            // 
+            this.detailbtm.Location = new System.Drawing.Point(0, 362);
+            this.detailbtm.Size = new System.Drawing.Size(828, 38);
+            // 
+            // browse
+            // 
+            this.browse.Size = new System.Drawing.Size(828, 400);
+            // 
+            // tabs
+            // 
+            this.tabs.Size = new System.Drawing.Size(836, 429);
+            // 
+            // createby
+            // 
+            this.createby.Size = new System.Drawing.Size(350, 23);
+            // 
+            // editby
+            // 
+            this.editby.Location = new System.Drawing.Point(471, 7);
+            this.editby.Size = new System.Drawing.Size(350, 23);
+            // 
+            // lbleditby
+            // 
+            this.lbleditby.Location = new System.Drawing.Point(423, 13);
+            // 
+            // checkJunk
+            // 
+            this.checkJunk.AutoSize = true;
+            this.checkJunk.DataBindings.Add(new System.Windows.Forms.Binding("Checked", this.mtbs, "junk", true));
+            this.checkJunk.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.checkJunk.Location = new System.Drawing.Point(233, 18);
+            this.checkJunk.Name = "checkJunk";
+            this.checkJunk.Size = new System.Drawing.Size(57, 21);
+            this.checkJunk.TabIndex = 1;
+            this.checkJunk.Text = "Junk";
+            this.checkJunk.UseVisualStyleBackColor = true;
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.BackColor = System.Drawing.Color.White;
+            this.txtDescription.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "Description", true));
+            this.txtDescription.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtDescription.Location = new System.Drawing.Point(116, 94);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(379, 23);
+            this.txtDescription.TabIndex = 3;
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.Location = new System.Drawing.Point(20, 94);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(89, 23);
+            this.labelDescription.TabIndex = 10;
+            this.labelDescription.Text = "Description";
+            // 
+            // txtID
+            // 
+            this.txtID.BackColor = System.Drawing.Color.White;
+            this.txtID.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "id", true));
+            this.txtID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtID.Location = new System.Drawing.Point(116, 18);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 23);
+            this.txtID.TabIndex = 0;
+            // 
+            // labelID
+            // 
+            this.labelID.Location = new System.Drawing.Point(21, 18);
+            this.labelID.Name = "labelID";
+            this.labelID.Size = new System.Drawing.Size(88, 23);
+            this.labelID.TabIndex = 8;
+            this.labelID.Text = "ID";
+            // 
+            // txtMdivision1
+            // 
+            this.txtMdivision1.BackColor = System.Drawing.Color.White;
+            this.txtMdivision1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "MDivisionid", true));
+            this.txtMdivision1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtMdivision1.Location = new System.Drawing.Point(116, 56);
+            this.txtMdivision1.Name = "txtMdivision1";
+            this.txtMdivision1.Size = new System.Drawing.Size(66, 23);
+            this.txtMdivision1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(20, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 23);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "M";
+            // 
+            // txtCell1
+            // 
+            this.txtCell1.BackColor = System.Drawing.Color.White;
+            this.txtCell1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "CutCellID", true));
+            this.txtCell1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtCell1.Location = new System.Drawing.Point(116, 132);
+            this.txtCell1.MDivisionID = "";
+            this.txtCell1.Name = "txtCell1";
+            this.txtCell1.Size = new System.Drawing.Size(30, 23);
+            this.txtCell1.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(20, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 23);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Cut Cell";
+            // 
+            // B05
+            // 
+            this.ClientSize = new System.Drawing.Size(836, 462);
+            this.IsSupportClip = false;
+            this.IsSupportCopy = false;
+            this.IsSupportDelete = false;
+            this.IsSupportPrint = false;
+            this.Name = "B05";
+            this.Text = "B05 Spreading No.";
+            this.WorkAlias = "SpreadingNo";
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).EndInit();
+            this.detail.ResumeLayout(false);
+            this.detailcont.ResumeLayout(false);
+            this.detailcont.PerformLayout();
+            this.detailbtm.ResumeLayout(false);
+            this.detailbtm.PerformLayout();
+            this.tabs.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.CheckBox checkJunk;
+        private Win.UI.TextBox txtDescription;
+        private Win.UI.Label labelDescription;
+        private Win.UI.TextBox txtID;
+        private Win.UI.Label labelID;
+        private Win.UI.Label label2;
+        private Class.txtCell txtCell1;
+        private Win.UI.Label label1;
+        private Class.txtMdivision txtMdivision1;
+    }
+}
---
+++ Sci.Production.Cutting/B05.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Cutting
+{
+    public partial class B05 : Sci.Win.Tems.Input1
+    {
+        public B05(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+        }
+
+        protected override void ClickEditAfter()
+        {
+            base.ClickEditAfter();
+            txtID.ReadOnly = true;
+        }
+
+    }
+}
