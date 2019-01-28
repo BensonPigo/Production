86cd776df4f1a87bb57b67ab761e3f8faa76c90f
Willy <willy.wei@sprotscity.com.tw>
2019-01-28 15:56:47 +0800

    ISP20190091 1.新增欄位: ShippingAP.Reason , GMTBooking.BLNO,GMTBooking.Bl2No 2.imp_base 轉入資料GMTBooking.BLNO,GMTBooking.Bl2No 3.Shipping.P05 表頭增加Blno,Bl2no 檢視 4.Shipping.P08 type2 選擇other 跳出提示訊息 5.有相同的blno 要輸入reason 6.type=export, type2=garment 要自動帶入上層blno 7.shippingMode = 是空運, 且AccountNo 也是空運, 並且沒有air-prepaid 的紀錄, 要跳訊息

--- Sci.Production.Shipping/P08.Designer.cs
+++ Sci.Production.Shipping/P08.Designer.cs
@@ -70,6 +70,9 @@
             this.txtUserHandle = new Sci.Production.Class.txtuser();
             this.txtpayterm_ftyTerms = new Sci.Production.Class.txtpayterm_fty();
             this.txtSubconSupplier = new Sci.Production.Class.txtsubcon();
+            this.labReason = new Sci.Win.UI.Label();
+            this.txtReason = new Sci.Win.UI.TextBox();
+            this.txtReasonDesc = new Sci.Win.UI.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.detailgridbs)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.detailgrid2bs)).BeginInit();
             this.masterpanel.SuspendLayout();
@@ -86,6 +89,9 @@
             // 
             // masterpanel
             // 
+            this.masterpanel.Controls.Add(this.txtReasonDesc);
+            this.masterpanel.Controls.Add(this.txtReason);
+            this.masterpanel.Controls.Add(this.labReason);
             this.masterpanel.Controls.Add(this.comboFactory1);
             this.masterpanel.Controls.Add(this.label1);
             this.masterpanel.Controls.Add(this.btnAcctApprove);
@@ -127,8 +133,7 @@
             this.masterpanel.Controls.Add(this.labelNo);
             this.masterpanel.Controls.Add(this.dateApvDate);
             this.masterpanel.Controls.Add(this.dateDate);
-            this.masterpanel.Size = new System.Drawing.Size(1006, 183);
-            this.masterpanel.Paint += new System.Windows.Forms.PaintEventHandler(this.Masterpanel_Paint);
+            this.masterpanel.Size = new System.Drawing.Size(1006, 185);
             this.masterpanel.Controls.SetChildIndex(this.dateDate, 0);
             this.masterpanel.Controls.SetChildIndex(this.dateApvDate, 0);
             this.masterpanel.Controls.SetChildIndex(this.labelNo, 0);
@@ -171,15 +176,18 @@
             this.masterpanel.Controls.SetChildIndex(this.gridicon, 0);
             this.masterpanel.Controls.SetChildIndex(this.label1, 0);
             this.masterpanel.Controls.SetChildIndex(this.comboFactory1, 0);
+            this.masterpanel.Controls.SetChildIndex(this.labReason, 0);
+            this.masterpanel.Controls.SetChildIndex(this.txtReason, 0);
+            this.masterpanel.Controls.SetChildIndex(this.txtReasonDesc, 0);
             // 
             // detailpanel
             // 
-            this.detailpanel.Location = new System.Drawing.Point(0, 183);
-            this.detailpanel.Size = new System.Drawing.Size(1006, 246);
+            this.detailpanel.Location = new System.Drawing.Point(0, 185);
+            this.detailpanel.Size = new System.Drawing.Size(1006, 244);
             // 
             // gridicon
             // 
-            this.gridicon.Location = new System.Drawing.Point(770, 148);
+            this.gridicon.Location = new System.Drawing.Point(770, 150);
             this.gridicon.TabIndex = 11;
             // 
             // refresh
@@ -188,7 +196,7 @@
             // 
             // detailgridcont
             // 
-            this.detailgridcont.Size = new System.Drawing.Size(1006, 246);
+            this.detailgridcont.Size = new System.Drawing.Size(1006, 244);
             // 
             // detail2
             // 
@@ -217,7 +225,7 @@
             // 
             // browse
             // 
-            this.browse.Size = new System.Drawing.Size(990, 467);
+            this.browse.Size = new System.Drawing.Size(1006, 467);
             // 
             // tabs
             // 
@@ -319,6 +327,7 @@
             this.comboType.IsSupportUnselect = true;
             this.comboType.Location = new System.Drawing.Point(64, 33);
             this.comboType.Name = "comboType";
+            this.comboType.OldText = "";
             this.comboType.Size = new System.Drawing.Size(100, 24);
             this.comboType.TabIndex = 0;
             this.comboType.SelectedIndexChanged += new System.EventHandler(this.ComboType_SelectedIndexChanged);
@@ -331,8 +340,10 @@
             this.comboType2.IsSupportUnselect = true;
             this.comboType2.Location = new System.Drawing.Point(169, 33);
             this.comboType2.Name = "comboType2";
+            this.comboType2.OldText = "";
             this.comboType2.Size = new System.Drawing.Size(203, 24);
             this.comboType2.TabIndex = 1;
+            this.comboType2.SelectedIndexChanged += new System.EventHandler(this.comboType2_SelectedIndexChanged);
             // 
             // txtRemark
             // 
@@ -522,7 +533,7 @@
             // 
             // labelHandle
             // 
-            this.labelHandle.Location = new System.Drawing.Point(635, 6);
+            this.labelHandle.Location = new System.Drawing.Point(621, 6);
             this.labelHandle.Name = "labelHandle";
             this.labelHandle.Size = new System.Drawing.Size(80, 23);
             this.labelHandle.TabIndex = 30;
@@ -530,7 +541,7 @@
             // 
             // labelAccountant
             // 
-            this.labelAccountant.Location = new System.Drawing.Point(635, 35);
+            this.labelAccountant.Location = new System.Drawing.Point(621, 35);
             this.labelAccountant.Name = "labelAccountant";
             this.labelAccountant.Size = new System.Drawing.Size(80, 23);
             this.labelAccountant.TabIndex = 31;
@@ -538,7 +549,7 @@
             // 
             // labelApvDate
             // 
-            this.labelApvDate.Location = new System.Drawing.Point(635, 64);
+            this.labelApvDate.Location = new System.Drawing.Point(621, 64);
             this.labelApvDate.Name = "labelApvDate";
             this.labelApvDate.Size = new System.Drawing.Size(80, 23);
             this.labelApvDate.TabIndex = 32;
@@ -546,7 +557,7 @@
             // 
             // labelVoucherNo
             // 
-            this.labelVoucherNo.Location = new System.Drawing.Point(635, 93);
+            this.labelVoucherNo.Location = new System.Drawing.Point(621, 93);
             this.labelVoucherNo.Name = "labelVoucherNo";
             this.labelVoucherNo.Size = new System.Drawing.Size(80, 23);
             this.labelVoucherNo.TabIndex = 33;
@@ -556,7 +567,7 @@
             // 
             this.dateApvDate.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "ApvDate", true));
             this.dateApvDate.IsSupportEditMode = false;
-            this.dateApvDate.Location = new System.Drawing.Point(717, 64);
+            this.dateApvDate.Location = new System.Drawing.Point(703, 64);
             this.dateApvDate.Name = "dateApvDate";
             this.dateApvDate.ReadOnly = true;
             this.dateApvDate.Size = new System.Drawing.Size(130, 23);
@@ -567,9 +578,9 @@
             this.displayVoucherNo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
             this.displayVoucherNo.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "VoucherID", true));
             this.displayVoucherNo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
-            this.displayVoucherNo.Location = new System.Drawing.Point(717, 93);
+            this.displayVoucherNo.Location = new System.Drawing.Point(703, 93);
             this.displayVoucherNo.Name = "displayVoucherNo";
-            this.displayVoucherNo.Size = new System.Drawing.Size(148, 23);
+            this.displayVoucherNo.Size = new System.Drawing.Size(167, 23);
             this.displayVoucherNo.TabIndex = 37;
             // 
             // btnShareExpense
@@ -630,6 +641,7 @@
             this.comboFactory1.IsSupportUnselect = true;
             this.comboFactory1.Location = new System.Drawing.Point(242, 6);
             this.comboFactory1.Name = "comboFactory1";
+            this.comboFactory1.OldText = "";
             this.comboFactory1.Size = new System.Drawing.Size(66, 24);
             this.comboFactory1.TabIndex = 41;
             // 
@@ -637,9 +649,9 @@
             // 
             this.txtSserAccountant.DataBindings.Add(new System.Windows.Forms.Binding("TextBox1Binding", this.mtbs, "Accountant", true));
             this.txtSserAccountant.DisplayBox1Binding = "";
-            this.txtSserAccountant.Location = new System.Drawing.Point(717, 35);
+            this.txtSserAccountant.Location = new System.Drawing.Point(703, 35);
             this.txtSserAccountant.Name = "txtSserAccountant";
-            this.txtSserAccountant.Size = new System.Drawing.Size(285, 23);
+            this.txtSserAccountant.Size = new System.Drawing.Size(295, 23);
             this.txtSserAccountant.TabIndex = 10;
             this.txtSserAccountant.TextBox1Binding = "";
             // 
@@ -647,9 +659,9 @@
             // 
             this.txtUserHandle.DataBindings.Add(new System.Windows.Forms.Binding("TextBox1Binding", this.mtbs, "Handle", true));
             this.txtUserHandle.DisplayBox1Binding = "";
-            this.txtUserHandle.Location = new System.Drawing.Point(717, 6);
+            this.txtUserHandle.Location = new System.Drawing.Point(703, 6);
             this.txtUserHandle.Name = "txtUserHandle";
-            this.txtUserHandle.Size = new System.Drawing.Size(285, 23);
+            this.txtUserHandle.Size = new System.Drawing.Size(295, 23);
             this.txtUserHandle.TabIndex = 9;
             this.txtUserHandle.TextBox1Binding = "";
             // 
@@ -673,6 +685,37 @@
             this.txtSubconSupplier.Size = new System.Drawing.Size(170, 23);
             this.txtSubconSupplier.TabIndex = 2;
             this.txtSubconSupplier.TextBox1Binding = "";
+            // 
+            // labReason
+            // 
+            this.labReason.Location = new System.Drawing.Point(621, 122);
+            this.labReason.Name = "labReason";
+            this.labReason.Size = new System.Drawing.Size(53, 23);
+            this.labReason.TabIndex = 42;
+            this.labReason.Text = "Reason";
+            // 
+            // txtReason
+            // 
+            this.txtReason.BackColor = System.Drawing.Color.White;
+            this.txtReason.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "Reason", true));
+            this.txtReason.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtReason.Location = new System.Drawing.Point(675, 122);
+            this.txtReason.Name = "txtReason";
+            this.txtReason.Size = new System.Drawing.Size(42, 23);
+            this.txtReason.TabIndex = 43;
+            this.txtReason.PopUp += new System.EventHandler<Sci.Win.UI.TextBoxPopUpEventArgs>(this.txtReason_PopUp);
+            this.txtReason.Validating += new System.ComponentModel.CancelEventHandler(this.txtReason_Validating);
+            // 
+            // txtReasonDesc
+            // 
+            this.txtReasonDesc.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.txtReasonDesc.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.txtReasonDesc.IsSupportEditMode = false;
+            this.txtReasonDesc.Location = new System.Drawing.Point(718, 122);
+            this.txtReasonDesc.Name = "txtReasonDesc";
+            this.txtReasonDesc.ReadOnly = true;
+            this.txtReasonDesc.Size = new System.Drawing.Size(152, 23);
+            this.txtReasonDesc.TabIndex = 44;
             // 
             // P08
             // 
@@ -753,5 +796,8 @@
         private Win.UI.Label labelM;
         private Class.comboFactory comboFactory1;
         private Win.UI.Label label1;
+        private Win.UI.Label labReason;
+        private Win.UI.TextBox txtReasonDesc;
+        private Win.UI.TextBox txtReason;
     }
 }
