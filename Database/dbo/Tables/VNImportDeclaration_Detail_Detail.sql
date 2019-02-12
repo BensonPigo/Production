8e02737606474989673a078afeb615ebfbcc3d49
JEFF <jeff.yeh@sportscity.com.tw>
2019-02-12 17:29:45 +0800

    ISP20190124 2.新增Table [VNImportDeclaration_Detail_Detail]

---
+++ Database/dbo/Tables/VNImportDeclaration_Detail_Detail.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[VNImportDeclaration_Detail_Detail] (
+    [ID]         VARCHAR (13)    NOT NULL,
+    [Refno]      VARCHAR (20)    NOT NULL,
+    [FabricType] VARCHAR (1)     NOT NULL,
+    [NLCode]     VARCHAR (5)     NOT NULL,
+    [Qty]        NUMERIC (14, 3) NULL,
+    [Remark]     NVARCHAR (60)   NULL,
+    CONSTRAINT [PK_VNImportDeclaration_Detail_Detail] PRIMARY KEY CLUSTERED ([ID] ASC, [Refno] ASC)
+);
+
