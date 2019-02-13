9c66b71f6bb8fc479bb1bb501d11a658e30d29f1
JEFF <jeff.yeh@sportscity.com.tw>
2019-02-13 17:47:29 +0800

    ISP20190124 VNImportDeclaration_Detail_Detail Pkey: ID,Refno, FabricType,NLCode

--- Database/dbo/Tables/VNImportDeclaration_Detail_Detail.sql
+++ Database/dbo/Tables/VNImportDeclaration_Detail_Detail.sql
@@ -1,10 +1,16 @@
 ﻿CREATE TABLE [dbo].[VNImportDeclaration_Detail_Detail] (
     [ID]         VARCHAR (13)    NOT NULL,
-    [Refno]      VARCHAR (20)    NOT NULL,
-    [FabricType] VARCHAR (1)     NOT NULL,
+    [Refno]      VARCHAR (21)    NOT NULL,
+    [FabricType] VARCHAR (20)    NOT NULL,
     [NLCode]     VARCHAR (5)     NOT NULL,
     [Qty]        NUMERIC (14, 3) NULL,
     [Remark]     NVARCHAR (60)   NULL,
-    CONSTRAINT [PK_VNImportDeclaration_Detail_Detail] PRIMARY KEY CLUSTERED ([ID] ASC, [Refno] ASC)
+    CONSTRAINT [PK_VNImportDeclaration_Detail_Detail] PRIMARY KEY CLUSTERED ([ID] ASC, [Refno] ASC, [FabricType] ASC, [NLCode] ASC)
 );
+
+
+
+
+
+
 
