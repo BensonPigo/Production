cc0d90dd9ab704fc486c353e3ea970744fd09417
JEFF <jeff.yeh@sportscity.com.tw>
2019-02-14 22:14:01 +0800

    ISP20190095 R08未完成

---
+++ Sci.Production.Cutting/R08.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Sci.Production.Cutting
+{
+    partial class R08
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new Sci.Win.UI.Panel();
+            this.txtfactory1 = new Sci.Production.Class.txtfactory();
+            this.txtMdivision1 = new Sci.Production.Class.txtMdivision();
+            this.txtSpreadingNo2 = new Sci.Production.Class.txtSpreadingNo();
+            this.txtSpreadingNo1 = new Sci.Production.Class.txtSpreadingNo();
+            this.txtCell2 = new Sci.Production.Class.txtCell();
+            this.txtCell1 = new Sci.Production.Class.txtCell();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCuttingSp = new Sci.Win.UI.TextBox();
+            this.label3 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.label1 = new Sci.Win.UI.Label();
+            this.labelFactory = new Sci.Win.UI.Label();
+            this.labelM = new Sci.Win.UI.Label();
+            this.labelEstCutDate = new Sci.Win.UI.Label();
+            this.dateEstCutDate = new Sci.Win.UI.DateRange();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(442, 12);
+            this.print.Visible = false;
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(442, 48);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(442, 84);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtfactory1);
+            this.panel1.Controls.Add(this.txtMdivision1);
+            this.panel1.Controls.Add(this.txtSpreadingNo2);
+            this.panel1.Controls.Add(this.txtSpreadingNo1);
+            this.panel1.Controls.Add(this.txtCell2);
+            this.panel1.Controls.Add(this.txtCell1);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtCuttingSp);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.labelFactory);
+            this.panel1.Controls.Add(this.labelM);
+            this.panel1.Controls.Add(this.labelEstCutDate);
+            this.panel1.Controls.Add(this.dateEstCutDate);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(410, 252);
+            this.panel1.TabIndex = 94;
+            // 
+            // txtfactory1
+            // 
+            this.txtfactory1.BackColor = System.Drawing.Color.White;
+            this.txtfactory1.FilteMDivision = false;
+            this.txtfactory1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtfactory1.IssupportJunk = false;
+            this.txtfactory1.Location = new System.Drawing.Point(112, 48);
+            this.txtfactory1.Name = "txtfactory1";
+            this.txtfactory1.Size = new System.Drawing.Size(66, 23);
+            this.txtfactory1.TabIndex = 116;
+            // 
+            // txtMdivision1
+            // 
+            this.txtMdivision1.BackColor = System.Drawing.Color.White;
+            this.txtMdivision1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtMdivision1.Location = new System.Drawing.Point(112, 12);
+            this.txtMdivision1.Name = "txtMdivision1";
+            this.txtMdivision1.Size = new System.Drawing.Size(66, 23);
+            this.txtMdivision1.TabIndex = 115;
+            // 
+            // txtSpreadingNo2
+            // 
+            this.txtSpreadingNo2.BackColor = System.Drawing.Color.White;
+            this.txtSpreadingNo2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtSpreadingNo2.Location = new System.Drawing.Point(184, 125);
+            this.txtSpreadingNo2.MDivisionID = "";
+            this.txtSpreadingNo2.Name = "txtSpreadingNo2";
+            this.txtSpreadingNo2.Size = new System.Drawing.Size(45, 23);
+            this.txtSpreadingNo2.TabIndex = 114;
+            // 
+            // txtSpreadingNo1
+            // 
+            this.txtSpreadingNo1.BackColor = System.Drawing.Color.White;
+            this.txtSpreadingNo1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtSpreadingNo1.Location = new System.Drawing.Point(111, 125);
+            this.txtSpreadingNo1.MDivisionID = "";
+            this.txtSpreadingNo1.Name = "txtSpreadingNo1";
+            this.txtSpreadingNo1.Size = new System.Drawing.Size(45, 23);
+            this.txtSpreadingNo1.TabIndex = 113;
+            // 
+            // txtCell2
+            // 
+            this.txtCell2.BackColor = System.Drawing.Color.White;
+            this.txtCell2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtCell2.Location = new System.Drawing.Point(161, 163);
+            this.txtCell2.MDivisionID = "";
+            this.txtCell2.Name = "txtCell2";
+            this.txtCell2.Size = new System.Drawing.Size(30, 23);
+            this.txtCell2.TabIndex = 112;
+            // 
+            // txtCell1
+            // 
+            this.txtCell1.BackColor = System.Drawing.Color.White;
+            this.txtCell1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtCell1.Location = new System.Drawing.Point(111, 163);
+            this.txtCell1.MDivisionID = "";
+            this.txtCell1.Name = "txtCell1";
+            this.txtCell1.Size = new System.Drawing.Size(30, 23);
+            this.txtCell1.TabIndex = 111;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(144, 169);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 17);
+            this.label5.TabIndex = 108;
+            this.label5.Text = "~";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(162, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 17);
+            this.label4.TabIndex = 107;
+            this.label4.Text = "~";
+            // 
+            // txtCuttingSp
+            // 
+            this.txtCuttingSp.BackColor = System.Drawing.Color.White;
+            this.txtCuttingSp.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtCuttingSp.Location = new System.Drawing.Point(111, 200);
+            this.txtCuttingSp.Name = "txtCuttingSp";
+            this.txtCuttingSp.Size = new System.Drawing.Size(126, 23);
+            this.txtCuttingSp.TabIndex = 106;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(13, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 23);
+            this.label3.TabIndex = 101;
+            this.label3.Text = "Cutting SP#";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(13, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 23);
+            this.label2.TabIndex = 100;
+            this.label2.Text = "Cut Cell";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(13, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 23);
+            this.label1.TabIndex = 99;
+            this.label1.Text = "Spreading No";
+            // 
+            // labelFactory
+            // 
+            this.labelFactory.Location = new System.Drawing.Point(12, 48);
+            this.labelFactory.Name = "labelFactory";
+            this.labelFactory.Size = new System.Drawing.Size(95, 23);
+            this.labelFactory.TabIndex = 98;
+            this.labelFactory.Text = "Factory";
+            // 
+            // labelM
+            // 
+            this.labelM.Location = new System.Drawing.Point(12, 12);
+            this.labelM.Name = "labelM";
+            this.labelM.Size = new System.Drawing.Size(94, 23);
+            this.labelM.TabIndex = 97;
+            this.labelM.Text = "M";
+            // 
+            // labelEstCutDate
+            // 
+            this.labelEstCutDate.Location = new System.Drawing.Point(12, 89);
+            this.labelEstCutDate.Name = "labelEstCutDate";
+            this.labelEstCutDate.Size = new System.Drawing.Size(94, 23);
+            this.labelEstCutDate.TabIndex = 3;
+            this.labelEstCutDate.Text = "Est. Cut Date";
+            // 
+            // dateEstCutDate
+            // 
+            // 
+            // 
+            // 
+            this.dateEstCutDate.DateBox1.Location = new System.Drawing.Point(0, 0);
+            this.dateEstCutDate.DateBox1.Name = "";
+            this.dateEstCutDate.DateBox1.Size = new System.Drawing.Size(129, 23);
+            this.dateEstCutDate.DateBox1.TabIndex = 0;
+            // 
+            // 
+            // 
+            this.dateEstCutDate.DateBox2.Location = new System.Drawing.Point(151, 0);
+            this.dateEstCutDate.DateBox2.Name = "";
+            this.dateEstCutDate.DateBox2.Size = new System.Drawing.Size(129, 23);
+            this.dateEstCutDate.DateBox2.TabIndex = 1;
+            this.dateEstCutDate.Location = new System.Drawing.Point(111, 89);
+            this.dateEstCutDate.Name = "dateEstCutDate";
+            this.dateEstCutDate.Size = new System.Drawing.Size(280, 23);
+            this.dateEstCutDate.TabIndex = 0;
+            // 
+            // R08
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 303);
+            this.Controls.Add(this.panel1);
+            this.IsSupportPrint = false;
+            this.Name = "R08";
+            this.Text = "R08.Actual Cutting Output Report";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.Panel panel1;
+        private Win.UI.Label labelFactory;
+        private Win.UI.Label labelM;
+        private Win.UI.Label labelEstCutDate;
+        private Win.UI.DateRange dateEstCutDate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private Win.UI.TextBox txtCuttingSp;
+        private Win.UI.Label label3;
+        private Win.UI.Label label2;
+        private Win.UI.Label label1;
+        private Class.txtSpreadingNo txtSpreadingNo2;
+        private Class.txtSpreadingNo txtSpreadingNo1;
+        private Class.txtCell txtCell2;
+        private Class.txtCell txtCell1;
+        private Class.txtfactory txtfactory1;
+        private Class.txtMdivision txtMdivision1;
+    }
+}
---
+++ Sci.Production.Cutting/R08.cs
@@ -0,0 +1,278 @@
+﻿using Ict;
+using Microsoft.Office.Core;
+using Sci.Data;
+using Sci.Utility.Excel;
+using Sci.Win;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Sci.Production.Cutting
+{
+    public partial class R08 : Sci.Win.Tems.PrintForm
+    {
+        private DataTable printData;
+        private string Mdivision;
+        private string Factory;
+        private DateTime? EstCutDate1;
+        private DateTime? EstCutDate2;
+        private string SpreadingNo1;
+        private string SpreadingNo2;
+        private string CutCell1;
+        private string CutCell2;
+        private string CuttingSP;
+
+        public R08(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+        }
+
+        protected override bool ValidateInput()
+        {
+            this.Mdivision = txtMdivision1.Text;
+            this.Factory = txtfactory1.Text;
+            this.EstCutDate1 = dateEstCutDate.Value1;
+            this.EstCutDate2 = dateEstCutDate.Value2;
+            this.SpreadingNo1 = txtSpreadingNo1.Text;
+            this.SpreadingNo2 = txtSpreadingNo2.Text;
+            this.CutCell1 = txtCell1.Text;
+            this.CutCell2 = txtCell2.Text;
+            this.CuttingSP = txtCuttingSp.Text;
+            return base.ValidateInput();
+        }
+        
+        protected override DualResult OnAsyncDataLoad(ReportEventArgs e)
+        {
+            string sqlcmd = string.Empty;
+            string where = string.Empty;
+            if (!MyUtility.Check.Empty(this.Mdivision))
+            {
+                where += $" and w.MDivisionId = '{this.Mdivision}' ";
+            }
+            if (!MyUtility.Check.Empty(this.Factory))
+            {
+                where += $" and w.FactoryID = '{this.Factory}' ";
+            }
+            if (!MyUtility.Check.Empty(this.EstCutDate1))
+            {
+                where += $" and w.EstCutDate >= '{((DateTime)this.EstCutDate1).ToString("yyyy/MM/dd")}' ";
+            }
+            if (!MyUtility.Check.Empty(this.EstCutDate2))
+            {
+                where += $" and w.EstCutDate <=  '{((DateTime)this.EstCutDate2).ToString("yyyy/MM/dd")}' ";
+            }
+            if (!MyUtility.Check.Empty(this.SpreadingNo1))
+            {
+                where += $" and w.SpreadingNoID >= '{this.SpreadingNo1}' ";
+            }
+            if (!MyUtility.Check.Empty(this.SpreadingNo2))
+            {
+                where += $" and w.SpreadingNoID <= '{this.SpreadingNo2}' ";
+            }
+            if (!MyUtility.Check.Empty(this.CutCell1))
+            {
+                where += $" and w.CutCellid >= '{this.CutCell1}' ";
+            }
+            if (!MyUtility.Check.Empty(this.CutCell2))
+            {
+                where += $" and w.CutCellid <= '{this.CutCell2}' ";
+            }
+            if (!MyUtility.Check.Empty(this.CuttingSP))
+            {
+                where += $" and w.ID = '{this.CuttingSP}' ";
+            }
+
+            sqlcmd = $@"
+select
+	w.FactoryID,
+	w.EstCutDate,
+	w.CutCellid,
+	w.SpreadingNoID,
+	w.CutplanID,
+	w.CutRef,
+	w.ID,
+	subSp.SubSP,
+	o.StyleID,
+	size.Size,
+	sumWdQty=sumWdQty.qty,
+	f.Description,
+	f.MtlTypeID,
+	w.FabricCombo,
+	MarkerLength=iif(w.Layer=0,0,w.cons/w.Layer),
+	PerimeterM=iif(w.ActCuttingPerimeter not like '%yd%',w.ActCuttingPerimeter,dbo.GetActualPerimeter(w.ActCuttingPerimeter)),
+	w.Layer,
+	SizeCode.SizeCode,
+	w.Cons,
+	sumWdQtyE=sumWdQtyE.qty,
+	NoofRoll.NoofRoll,
+	DyeLot=isnull(DyeLot.DyeLot,0),
+	NoofWindow=w.Cons/w.Layer/1.4,
+	ActSpd.ActualSpeed,
+	st.PreparationTime,
+	[ChangeoverTime] = iif(fr.isRoll = 0,st.ChangeOverUnRollTime,st.ChangeOverRollTime),
+	SpreadingSetupTime=st.SetupTime,
+	st.SpreadingTime,
+	st.SeparatorTime,
+	st.ForwardTime,
+	CuttingSetUpTime=ct.SetUpTime,
+	ct.WindowTime,
+	f.WeaveTypeID,w.Refno,[nNoofRoll]=n.NoofRoll,[slLayer]=sl.Layer,[scCons]=sc.Cons,ct.WindowLength
+into #tmp
+from WorkOrder w with(nolock)
+inner join orders o with(nolock) on o.id = w.ID
+left join Fabric f with(nolock) on f.SCIRefno = w.SCIRefno
+left join SpreadingTime st with(nolock) on st.WeaveTypeID = f.WeaveTypeID
+left join ManufacturingExecution.dbo.RefnoRelaxtime rr WITH (NOLOCK) on rr.Refno = w.Refno
+left join ManufacturingExecution.dbo.FabricRelaxation fr WITH (NOLOCK) on rr.FabricRelaxationID = fr.ID
+left join CuttingTime ct WITH (NOLOCK) on ct.WeaveTypeID = f.WeaveTypeID
+--outer apply(select TotalCuttingPerimeter = dbo.GetActualPerimeter(w.ActCuttingPerimeter))pm
+outer apply(
+	select SubSP = stuff((
+		select distinct concat(',',wd.OrderID)
+		from WorkOrder_Distribute wd with(nolock)
+		where wd.WorkOrderUkey = w.Ukey
+		For XML path('')
+	),1,1,'')
+)subSp
+outer apply(
+	select Size = stuff((
+		select distinct concat(',',wd.SizeCode)
+		from WorkOrder_Distribute wd with(nolock)
+		where wd.WorkOrderUkey = w.Ukey
+		For XML path('')
+	),1,1,'')
+)size
+outer apply(
+	select qty=sum(wd.qty)
+	from WorkOrder_Distribute wd with(nolock)
+	where wd.WorkOrderUkey = w.Ukey
+	and wd.OrderID <> 'EXCESS'	
+)sumWdQty
+outer apply(
+	select qty=sum(wd.qty)
+	from WorkOrder_Distribute wd with(nolock)
+	where wd.WorkOrderUkey = w.Ukey
+	and wd.OrderID = 'EXCESS'	
+)sumWdQtyE
+outer apply
+(
+	select SizeCode = stuff(
+	(
+		Select concat(', ' , wd.sizecode, '/ ', wd.qty)
+		From WorkOrder_SizeRatio wd WITH (NOLOCK) 
+		Where wd.WorkOrderUkey = w.Ukey 
+		For XML path('')
+	),1,1,'')
+)SizeCode
+outer apply(
+	select NoofRoll = count(1)
+	from(
+		select distinct cr.Seq1,cr.seq2,cr.Roll,cr.Dyelot
+		from CuttingOutputFabricRecord cr WITH (NOLOCK) 
+		where cr.CutRef = w.CutRef and cr.MDivisionId = w.MDivisionId
+	)disC
+)NoofRoll
+outer apply(
+	select DyeLot = count(1)
+	from(
+		select distinct cr.Seq1,cr.seq2,cr.Dyelot
+		from CuttingOutputFabricRecord cr WITH (NOLOCK) 
+		where cr.CutRef = w.CutRef and cr.MDivisionId = w.MDivisionId
+	)disC
+)DyeLot
+outer apply(	
+	select  ActualSpeed
+	from CuttingMachine_detail cmd WITH (NOLOCK) 
+	inner join CutCell cc WITH (NOLOCK) on cc.CuttingMachineID = cmd.id
+	where cc.id = w.CutCellid 
+	and w.Layer between cmd.LayerLowerBound and cmd.LayerUpperBound
+	and cmd.WeaveTypeID = f.WeaveTypeID 
+)ActSpd
+outer apply(
+	select avgInQty = avg(fi.InQty)
+	from PO_Supp_Detail psd with(nolock)
+	left join FtyInventory fi with(nolock) on fi.POID = psd.ID and fi.Seq1 = psd.SEQ1 and fi.Seq2 = psd.SEQ2
+	where psd.ID = w.id and psd.SCIRefno = w.SCIRefno
+	and fi.InQty is not null
+) as fi
+outer apply(select Layer = sum(w.Layer)over(partition by w.CutRef,w.MDivisionId))sl
+outer apply(select Cons = sum(w.Cons)over(partition by w.CutRef,w.MDivisionId))sc
+outer apply(select NoofRoll = iif(isnull(round(sc.Cons/fi.avgInQty,0),0)=0,1,round(sc.Cons/fi.avgInQty,0)))n
+
+where 1=1
+{where}
+
+select FactoryID,EstCutDate,CutCellid,SpreadingNoID,CutplanID,CutRef,ID,SubSP,StyleID,Size,sumWdQty,Description,MtlTypeID,FabricCombo,
+	MarkerLength,PerimeterM,Layer,SizeCode,Cons,sumWdQtyE,NoofRoll,DyeLot,NoofWindow,ActualSpeed,
+	[PreparationTime_min] = PreparationTime/60.0,
+	[ChangeoverTime_min] = ChangeoverTime/60.0,
+	[SpreadingSetupTime_min] = SpreadingSetupTime/60.0,
+	[MachineSpreadingTime_min]  =SpreadingTime/60.0,
+	[Separatortime_min] = SeparatorTime/60.0,
+	[ForwardTime_min] = ForwardTime/60.0,
+	[CuttingSetupTime_min] = CuttingSetUpTime/60.0,
+	MachCuttingTime_min,
+	[WindowTime_min] = WindowTime/60.0,
+	[TotalSpreadingTime_min] = (isnull(PreparationTime * iif(slLayer=0,0,scCons/slLayer),0) + 
+						 isnull(Changeovertime * nNoofRoll,0) +
+						 isnull(SpreadingSetupTime,0) +
+						 isnull(SpreadingTime * scCons,0) +
+						 isnull(SeparatorTime * (1 -1),0) +
+						 isnull(ForwardTime,0))
+						 /60.0,
+	[TotalCuttingTime_min] = (isnull(CuttingSetUpTime,0) + 
+					   iif(isnull(ActualSpeed,0)=0,0,isnull(p.PerimeterM_num,0)/ActualSpeed)*60 + 
+					   isnull(Windowtime * iif(isnull(slLayer,0)=0,0,scCons/slLayer*0.9144)/WindowLength,0))
+					   /60.0
+from #tmp
+outer apply(select PerimeterM_num=isnumeric(PerimeterM))p
+outer apply(select MachCuttingTime_min = iif(isnull(ActualSpeed,0)=0,0,p.PerimeterM_num/ActualSpeed))a
+
+drop table #tmp
+
+";
+            DualResult result = DBProxy.Current.Select(null, sqlcmd, out printData);
+            if (!result)
+            {                
+                return Result.F(result.ToString());
+            }
+            return Result.True;
+        }
+
+        protected override bool OnToExcel(ReportDefinition report)
+        {
+            this.ShowWaitMessage("Excel Processing...");
+            SetCount(printData.Rows.Count);
+            if (printData.Rows.Count <= 0)
+            {
+                MyUtility.Msg.WarningBox("Data not found!");
+                return false;
+            }
+            string excelName = "Cutting_R08";
+            Excel.Application excelApp = MyUtility.Excel.ConnectExcel(Sci.Env.Cfg.XltPathDir + $"\\{excelName}.xltx");
+
+            MyUtility.Excel.CopyToXls(printData, "", $"{excelName}.xltx", 1, false, null, excelApp, wSheet: excelApp.Sheets[1]);// 將datatable copy to excel
+
+            #region 釋放上面開啟過excel物件
+            string strExcelName = Class.MicrosoftFile.GetName(excelName);
+            Excel.Workbook workbook = excelApp.ActiveWorkbook;
+            workbook.SaveAs(strExcelName);
+            workbook.Close();
+            excelApp.Quit();
+
+            if (excelApp != null) Marshal.FinalReleaseComObject(excelApp);
+            #endregion
+            
+            this.HideWaitMessage();
+            strExcelName.OpenFile();
+            return true;
+        }
+    }
+}
