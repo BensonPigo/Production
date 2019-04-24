71b04a29a51507c250339b9459fcd5f20fc1202c
Jimmy <jimmy.liao@sportscity.com.tw>
2019-04-24 15:38:17 +0800

    usp_PMSUploadDataToAPS 1. 	Sewing Output 取資料時新增規則 	第二層的 QaQty 不可為零（除法會出問題） 2.	更新資料庫結構 	BundleTransfer 	Bundle_Detail 	Bundle_Detail_AllPart 	SubProcess 	imp_BundleTransfer

--- Database/dbo/Tables/Bundle_Detail_Allpart.sql
+++ Database/dbo/Tables/Bundle_Detail_Allpart.sql
@@ -1,12 +1,15 @@
-﻿CREATE TABLE [dbo].[Bundle_Detail_Allpart] (
+CREATE TABLE [dbo].[Bundle_Detail_Allpart] (
     [ID]          VARCHAR (13)   CONSTRAINT [DF_Bundle_Detail_Allpart_ID] DEFAULT ((0)) NOT NULL,
     [Patterncode] VARCHAR (20)   CONSTRAINT [DF_Bundle_Detail_Allpart_Patterncode] DEFAULT ('') NOT NULL,
     [PatternDesc] NVARCHAR (100) CONSTRAINT [DF_Bundle_Detail_Allpart_PatternDesc] DEFAULT ('') NULL,
     [parts]       NUMERIC (5)    CONSTRAINT [DF_Bundle_Detail_Allpart_parts] DEFAULT ((0)) NOT NULL,
     [Ukey]        BIGINT         IDENTITY (1, 1) NOT NULL,
     [IsPair]      BIT            NULL,
+    [Location]    VARCHAR (1)    DEFAULT ('') NOT NULL,
     CONSTRAINT [PK_Bundle_Detail_Allpart_1] PRIMARY KEY CLUSTERED ([Ukey] ASC)
 );
+
+
 
 
 
@@ -38,5 +41,6 @@ EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'數量', @l
 
 
 GO
-
+CREATE NONCLUSTERED INDEX [BundleCardID]
+    ON [dbo].[Bundle_Detail_Allpart]([ID] ASC);
 
