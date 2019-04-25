e2301ecb8c7ac20afeda273d87c220e34172f005
Willy <willy.wei@sprotscity.com.tw>
2019-04-25 16:48:23 +0800

    ISP20190466 1. backup

---
+++ Sci.Production.Planning/P06.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Sci.Production.Planning
+{
+    partial class P06
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel3 = new Sci.Win.UI.Panel();
+            this.labSCIDlv = new Sci.Win.UI.Label();
+            this.comboColumnType = new Sci.Production.Class.comboDropDownList(this.components);
+            this.labColumnType = new Sci.Win.UI.Label();
+            this.txtstyle = new Sci.Production.Class.txtstyle();
+            this.labStyle = new Sci.Win.UI.Label();
+            this.txtbrand = new Sci.Production.Class.txtbrand();
+            this.labBrandID = new Sci.Win.UI.Label();
+            this.labSPNo = new Sci.Win.UI.Label();
+            this.txtSPNo = new Sci.Win.UI.TextBox();
+            this.btnQuery = new Sci.Win.UI.Button();
+            this.btnSave = new Sci.Win.UI.Button();
+            this.gridImport = new Sci.Win.UI.Grid();
+            this.dateSCIDlvDate = new Sci.Win.UI.DateRange();
+            this.chxQueryTargetDate = new Sci.Win.UI.CheckBox();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridImport)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.chxQueryTargetDate);
+            this.panel3.Controls.Add(this.dateSCIDlvDate);
+            this.panel3.Controls.Add(this.labSCIDlv);
+            this.panel3.Controls.Add(this.comboColumnType);
+            this.panel3.Controls.Add(this.labColumnType);
+            this.panel3.Controls.Add(this.txtstyle);
+            this.panel3.Controls.Add(this.labStyle);
+            this.panel3.Controls.Add(this.txtbrand);
+            this.panel3.Controls.Add(this.labBrandID);
+            this.panel3.Controls.Add(this.labSPNo);
+            this.panel3.Controls.Add(this.txtSPNo);
+            this.panel3.Controls.Add(this.btnQuery);
+            this.panel3.Controls.Add(this.btnSave);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(857, 107);
+            this.panel3.TabIndex = 20;
+            // 
+            // labSCIDlv
+            // 
+            this.labSCIDlv.Location = new System.Drawing.Point(295, 43);
+            this.labSCIDlv.Name = "labSCIDlv";
+            this.labSCIDlv.Size = new System.Drawing.Size(97, 23);
+            this.labSCIDlv.TabIndex = 54;
+            this.labSCIDlv.Text = "SCI Dlv.";
+            // 
+            // comboColumnType
+            // 
+            this.comboColumnType.BackColor = System.Drawing.Color.White;
+            this.comboColumnType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboColumnType.FormattingEnabled = true;
+            this.comboColumnType.IsSupportUnselect = true;
+            this.comboColumnType.Location = new System.Drawing.Point(118, 43);
+            this.comboColumnType.Name = "comboColumnType";
+            this.comboColumnType.OldText = "";
+            this.comboColumnType.Size = new System.Drawing.Size(157, 24);
+            this.comboColumnType.TabIndex = 53;
+            this.comboColumnType.Type = "PMS_CriticalActivity";
+            // 
+            // labColumnType
+            // 
+            this.labColumnType.Location = new System.Drawing.Point(18, 44);
+            this.labColumnType.Name = "labColumnType";
+            this.labColumnType.Size = new System.Drawing.Size(97, 23);
+            this.labColumnType.TabIndex = 52;
+            this.labColumnType.Text = "Column Type";
+            // 
+            // txtstyle
+            // 
+            this.txtstyle.BackColor = System.Drawing.Color.White;
+            this.txtstyle.BrandObjectName = null;
+            this.txtstyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtstyle.Location = new System.Drawing.Point(544, 12);
+            this.txtstyle.Name = "txtstyle";
+            this.txtstyle.Size = new System.Drawing.Size(130, 23);
+            this.txtstyle.TabIndex = 51;
+            this.txtstyle.tarBrand = null;
+            this.txtstyle.tarSeason = null;
+            // 
+            // labStyle
+            // 
+            this.labStyle.Location = new System.Drawing.Point(476, 12);
+            this.labStyle.Name = "labStyle";
+            this.labStyle.Size = new System.Drawing.Size(65, 23);
+            this.labStyle.TabIndex = 50;
+            this.labStyle.Text = "Style";
+            // 
+            // txtbrand
+            // 
+            this.txtbrand.BackColor = System.Drawing.Color.White;
+            this.txtbrand.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtbrand.Location = new System.Drawing.Point(363, 12);
+            this.txtbrand.Name = "txtbrand";
+            this.txtbrand.Size = new System.Drawing.Size(110, 23);
+            this.txtbrand.TabIndex = 49;
+            // 
+            // labBrandID
+            // 
+            this.labBrandID.Location = new System.Drawing.Point(295, 12);
+            this.labBrandID.Name = "labBrandID";
+            this.labBrandID.Size = new System.Drawing.Size(65, 23);
+            this.labBrandID.TabIndex = 48;
+            this.labBrandID.Text = "Brand";
+            // 
+            // labSPNo
+            // 
+            this.labSPNo.Location = new System.Drawing.Point(18, 12);
+            this.labSPNo.Name = "labSPNo";
+            this.labSPNo.Size = new System.Drawing.Size(97, 23);
+            this.labSPNo.TabIndex = 46;
+            this.labSPNo.Text = "SP#";
+            // 
+            // txtSPNo
+            // 
+            this.txtSPNo.BackColor = System.Drawing.Color.White;
+            this.txtSPNo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtSPNo.Location = new System.Drawing.Point(118, 12);
+            this.txtSPNo.Name = "txtSPNo";
+            this.txtSPNo.Size = new System.Drawing.Size(157, 23);
+            this.txtSPNo.TabIndex = 47;
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnQuery.Location = new System.Drawing.Point(719, 10);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(97, 30);
+            this.btnQuery.TabIndex = 6;
+            this.btnQuery.Text = "Query";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnSave.Location = new System.Drawing.Point(719, 44);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(97, 30);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // gridImport
+            // 
+            this.gridImport.AllowUserToAddRows = false;
+            this.gridImport.AllowUserToDeleteRows = false;
+            this.gridImport.AllowUserToResizeRows = false;
+            this.gridImport.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.gridImport.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.gridImport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridImport.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridImport.EditingEnter = Ict.Win.UI.DataGridViewEditingEnter.NextCellOrNextRow;
+            this.gridImport.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.gridImport.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.gridImport.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(162)))), ((int)(((byte)(163)))));
+            this.gridImport.Location = new System.Drawing.Point(0, 107);
+            this.gridImport.Name = "gridImport";
+            this.gridImport.RowHeadersVisible = false;
+            this.gridImport.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.gridImport.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.gridImport.RowTemplate.Height = 24;
+            this.gridImport.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridImport.ShowCellToolTips = false;
+            this.gridImport.Size = new System.Drawing.Size(857, 349);
+            this.gridImport.TabIndex = 21;
+            this.gridImport.TabStop = false;
+            // 
+            // dateSCIDlvDate
+            // 
+            // 
+            // 
+            // 
+            this.dateSCIDlvDate.DateBox1.Location = new System.Drawing.Point(0, 0);
+            this.dateSCIDlvDate.DateBox1.Name = "";
+            this.dateSCIDlvDate.DateBox1.Size = new System.Drawing.Size(129, 23);
+            this.dateSCIDlvDate.DateBox1.TabIndex = 0;
+            // 
+            // 
+            // 
+            this.dateSCIDlvDate.DateBox2.Location = new System.Drawing.Point(151, 0);
+            this.dateSCIDlvDate.DateBox2.Name = "";
+            this.dateSCIDlvDate.DateBox2.Size = new System.Drawing.Size(129, 23);
+            this.dateSCIDlvDate.DateBox2.TabIndex = 1;
+            this.dateSCIDlvDate.IsRequired = false;
+            this.dateSCIDlvDate.Location = new System.Drawing.Point(395, 43);
+            this.dateSCIDlvDate.Name = "dateSCIDlvDate";
+            this.dateSCIDlvDate.Size = new System.Drawing.Size(280, 23);
+            this.dateSCIDlvDate.TabIndex = 55;
+            // 
+            // chxQueryTargetDate
+            // 
+            this.chxQueryTargetDate.AutoSize = true;
+            this.chxQueryTargetDate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.chxQueryTargetDate.Location = new System.Drawing.Point(19, 80);
+            this.chxQueryTargetDate.Name = "chxQueryTargetDate";
+            this.chxQueryTargetDate.Size = new System.Drawing.Size(206, 21);
+            this.chxQueryTargetDate.TabIndex = 56;
+            this.chxQueryTargetDate.Text = "Query have target date data";
+            this.chxQueryTargetDate.UseVisualStyleBackColor = true;
+            // 
+            // P06
+            // 
+            this.ClientSize = new System.Drawing.Size(857, 456);
+            this.Controls.Add(this.gridImport);
+            this.Controls.Add(this.panel3);
+            this.Name = "P06";
+            this.OnLineHelpID = "Sci.Win.Tems.QueryForm";
+            this.Text = "P06. Critical Activity Target Adjust";
+            this.Controls.SetChildIndex(this.panel3, 0);
+            this.Controls.SetChildIndex(this.gridImport, 0);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridImport)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Win.UI.Panel panel3;
+        private Win.UI.Button btnQuery;
+        private Win.UI.Button btnSave;
+        private Win.UI.Label labSCIDlv;
+        private Class.comboDropDownList comboColumnType;
+        private Win.UI.Label labColumnType;
+        private Class.txtstyle txtstyle;
+        private Win.UI.Label labStyle;
+        private Class.txtbrand txtbrand;
+        private Win.UI.Label labBrandID;
+        private Win.UI.Label labSPNo;
+        private Win.UI.TextBox txtSPNo;
+        private Win.UI.Grid gridImport;
+        private Win.UI.DateRange dateSCIDlvDate;
+        private Win.UI.CheckBox chxQueryTargetDate;
+    }
+}
---
+++ Sci.Production.Planning/P06.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Planning
+{
+    public partial class P06 : Sci.Win.Tems.QueryForm
+    {
+        public P06()
+        {
+            InitializeComponent();
+        }
+    }
+}
