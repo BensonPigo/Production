ae674172d5dcc480288c52e854f9b5fdf2ee92c0
Willy <willy.wei@sprotscity.com.tw>
2019-05-03 16:03:11 +0800

    ISP20190479 1. Accumulated 主資料表改由Export_detail 帶出Poid,Seq1,Seq2 2. 加上CheckBox 排除RcvQty=0資料, 並且整行反灰

--- Sci.Production.Warehouse/P07_AccumulatedQty.cs
+++ Sci.Production.Warehouse/P07_AccumulatedQty.cs
@@ -25,100 +25,75 @@ namespace Sci.Production.Warehouse
         protected override void OnFormLoaded()
         {
             base.OnFormLoaded();
-            StringBuilder selectCommand1 = new StringBuilder();
-            selectCommand1.Append(string.Format(@"
-select poid,seq1,seq2,dbo.GetUnitQty(PoUnit,dbo.GetStockUnitBySPSeq(poid,seq1,seq2),sum(shipqty)) shipqty,sum(accu_rcv) received
-    , sum(rcv) receiving ,description,Foc = sum(Foc)
+            string strSqlCmd = string.Empty;
+            if (MyUtility.Check.Empty(dr["ExportID"]))
+            {
+                strSqlCmd = $@"
+select poid,seq1,seq2
+,[shipqty] = dbo.GetUnitQty(PoUnit,dbo.GetStockUnitBySPSeq(poid,seq1,seq2),sum(shipqty))
+,[received] = sum(accu_rcv) 
+,[receiving] = sum(rcv) 
+,description
+,Foc = sum(Foc)
 from (
     select a.PoId,a.Seq1,a.Seq2,0 as shipqty,0 as accu_rcv,sum(a.StockQty) as rcv
-    ,dbo.getmtldesc(a.poid,a.seq1,a.seq2,2,0) as [description] ,a.POUnit ,Foc = 0
-    from dbo.Receiving_Detail a WITH (NOLOCK) where id='{0}' group by a.PoId,a.Seq1,a.Seq2,a.POUnit
-    union all
-",
-                dr["id"].ToString(),dr["exportid"].ToString()));
-            if (MyUtility.Check.Empty(dr["exportid"].ToString()))
-            {
-                selectCommand1.Append(string.Format(@"
+        ,dbo.getmtldesc(a.poid,a.seq1,a.seq2,2,0) as [description] ,a.POUnit ,Foc = 0
+    from dbo.Receiving_Detail a WITH (NOLOCK) 
+    where id='{dr["id"]}' 
+    group by a.PoId,a.Seq1,a.Seq2,a.POUnit
+
+    union all              
+
     select a.id poid,a.Seq1,a.seq2,a.Qty as shipqty,0 as accu_rcv,0 as rcv
         ,dbo.getmtldesc(a.id,a.seq1,a.seq2,2,0) as [description],a.POUnit,a.Foc
     from dbo.PO_Supp_Detail a WITH (NOLOCK) 
-    ,(select distinct PoId,Seq1,Seq2 from dbo.Receiving_Detail WITH (NOLOCK) where id='{0}') c 
+    ,(select distinct PoId,Seq1,Seq2 from dbo.Receiving_Detail WITH (NOLOCK) where id='{dr["id"]}') c 
     where a.id = c.poid and a.seq1 = c.seq1 and a.seq2 = c.seq2
 ) tmp
-group by poid,seq1,seq2,description,POUnit", dr["id"].ToString()));
+group by poid,seq1,seq2,description,POUnit";
             }
             else
             {
-                selectCommand1.Append(string.Format(@"
-    select a.PoId,a.Seq1,a.Seq2,0 as shipqty,sum(a.StockQty) as accu_rcv ,0 as rcv
-        ,dbo.getmtldesc(a.poid,a.seq1,a.seq2,2,0) as [description],a.POUnit,Foc = 0
+                strSqlCmd = $@"
+
+select ed.poid,ed.seq1,ed.seq2
+,[shipqty] = dbo.GetUnitQty(PoUnit,dbo.GetStockUnitBySPSeq(ed.poid,ed.seq1,ed.seq2),sum(ed.Qty)) 
+,[received] = sum(accu_rcv) 
+,[receiving] = sum(rcv)
+,rd.description
+,Foc = sum(ed.Foc)
+from Export_Detail ed
+outer apply(
+	select a.PoId,a.Seq1,a.Seq2,0 as accu_rcv,sum(a.StockQty) as rcv
+		,dbo.getmtldesc(a.poid,a.seq1,a.seq2,2,0) as [description] ,a.POUnit 
     from dbo.Receiving_Detail a WITH (NOLOCK) 
-    ,dbo.Receiving b WITH (NOLOCK) 
-    ,(select distinct PoId,Seq1,Seq2 from dbo.Receiving_Detail WITH (NOLOCK) where id='{0}') c 
-    where b.id!='{0}' and b.Status='Confirmed' and a.id=b.id and b.ExportId = '{1}'
-    and a.PoId=c.poid and a.seq1 = c.seq1 and a.seq2 = c.seq2 
-    group by a.PoId,a.Seq1,a.Seq2,a.POUnit
+	where 1=1
+	and a.Id='{dr["id"]}' 
+	and a.PoId=ed.PoID and a.Seq1=ed.Seq1 and a.Seq2=ed.Seq2
+	group by a.PoId,a.Seq1,a.Seq2,a.POUnit
+
     union all
-",
-                    dr["id"].ToString(), dr["exportid"].ToString()));
 
-                selectCommand1.Append(string.Format(@"
-    select 
-	    final.poid, 
-	    final.SEQ1, 
-	    final.SEQ2,
-	    shipqty = isnull(sf.shipqty,sf2.shipqty), 
-	    0 as accu_rcv,
-	    0 as rcv
-	    ,dbo.getmtldesc(final.poid, final.SEQ1, final.SEQ2,2,0) as [description] ,final.PoUnit,
-	    foc = isnull(sf.foc,sf2.foc)
-    from( 	
-	    select distinct zz.poid , zz.seq1, zz.seq2 ,zz.PoUnit
-	    from (
-		    select e.PoID poid, e.seq1, e.seq2,c.PoUnit 
-            from (select distinct PoId,Seq1,Seq2,PoUnit from dbo.Receiving_Detail WITH (NOLOCK) where id='{0}') c
-            , (select distinct Poid, seq1, seq2 from dbo.Export_Detail d WITH (NOLOCK) where d.id = '{1}') e
-			where  (c.PoId = e.poid and c.seq1 = e.seq1 and c.seq2 = e.seq2)
+    select a.PoId,a.Seq1,a.Seq2,sum(a.StockQty) as accu_rcv ,0 as rcv
+        ,dbo.getmtldesc(a.poid,a.seq1,a.seq2,2,0) as [description],a.POUnit
+    from dbo.Receiving_Detail a WITH (NOLOCK) 
+    ,dbo.Receiving b WITH (NOLOCK)
+    where b.id!='{dr["id"]}' and b.Status='Confirmed' and a.id=b.id and b.ExportId = ed.ID
+    and a.PoId=ed.poid and a.seq1 = ed.seq1 and a.seq2 = ed.seq2 
+    group by a.PoId,a.Seq1,a.Seq2,a.POUnit
+)rd
 
-		    union all
-
-		    select a.id poid, a.seq1, a.seq2,a.POUnit 
-            from dbo.PO_Supp_Detail a WITH (NOLOCK) 
-            ,(select distinct PoId,Seq1,Seq2 from dbo.Receiving_Detail WITH (NOLOCK) where id='{0}') c
-			where (a.id = c.poid and a.seq1 = c.seq1 and a.seq2 = c.seq2)
-	    ) zz
-    )final
-    outer apply
-	(
-		select not3RD.shipqty ,Foc
-		from 
-		(
-			select e.PoID poid,e.Seq1,e.seq2,e.Qty as shipqty,e.Foc
-			from (select distinct PoId,Seq1,Seq2 from dbo.Receiving_Detail WITH (NOLOCK) where id='{0}') c
-			, (select distinct Poid, seq1, seq2, Qty, Foc from dbo.Export_Detail d WITH (NOLOCK) where d.id = '{1}') e
-			where  (c.PoId = e.poid and c.seq1 = e.seq1 and c.seq2 = e.seq2)
-		)not3RD 
-		where not3RD.poid = final.poid and not3RD.SEQ1 = final.SEQ1 and not3RD.SEQ2 = final.SEQ2
-	)sf
-	outer apply
-	(
-		select is3RD.shipqty ,Foc
-		from (
-				select a.id poid,a.Seq1,a.seq2,a.Qty as shipqty,a.Foc
-				from dbo.PO_Supp_Detail a WITH (NOLOCK) 
-				,(select distinct PoId,Seq1,Seq2 from dbo.Receiving_Detail WITH (NOLOCK) where id='{0}') c
-				where (a.id = c.poid and a.seq1 = c.seq1 and a.seq2 = c.seq2)
-		)is3RD where is3RD.poid = final.poid and is3RD.SEQ1 = final.SEQ1 and is3RD.SEQ2 = final.SEQ2
-	)sf2
-) tmp
-group by poid,seq1,seq2,description,PoUnit", dr["id"].ToString(), dr["exportid"].ToString()));
+where ed.id='{dr["ExportID"]}'
+group by ed.poid,ed.seq1,ed.seq2,rd.description,rd.PoUnit
+order by PoId,Seq1,Seq2
+";
 
             }
             DataTable selectDataTable1;
             P07.ShowWaitMessage("Data loading...");
             DBProxy.Current.DefaultTimeout = 1200;
-            DualResult selectResult1 = DBProxy.Current.Select(null, selectCommand1.ToString(), out selectDataTable1);
-            if (selectResult1 == false) { ShowErr(selectCommand1.ToString(), selectResult1); }
+            DualResult selectResult1 = DBProxy.Current.Select(null, strSqlCmd.ToString(), out selectDataTable1);
+            if (selectResult1 == false) { ShowErr(strSqlCmd.ToString(), selectResult1); }
             DBProxy.Current.DefaultTimeout = 0;
             P07.HideWaitMessage();
             selectDataTable1.ColumnsDecimalAdd("variance", 0m, "received+receiving-shipqty-foc");
@@ -138,11 +113,63 @@ group by poid,seq1,seq2,description,PoUnit", dr["id"].ToString(), dr["exportid"]
                  .Numeric("variance", header: "Variance", width: Widths.AnsiChars(8), integer_places: 10, decimal_places: 2)
                  .EditText("Description", header: "Description", width: Widths.AnsiChars(40))
                  ;
+            this.grid_Filter();
+            this.ChangeColor();
         }
 
         private void btnClose_Click(object sender, EventArgs e)
         {
             this.Dispose();
+        }
+
+        private void grid_Filter()
+        {
+            string filter = "";
+
+            switch (chkRcvdQty.Checked)
+            {
+                case true:
+                    if (MyUtility.Check.Empty(gridAccumulatedQty))
+                        break;
+                    filter = $@"receiving > 0";
+                    ((DataTable)bindingSource1.DataSource).DefaultView.RowFilter = filter;
+                    break;
+
+                case false:
+                    if (MyUtility.Check.Empty(gridAccumulatedQty))
+                        break;
+                    filter = "";
+                    ((DataTable)bindingSource1.DataSource).DefaultView.RowFilter = filter;
+                    break;
+            }
+        }
+
+        private void ChangeColor()
+        {
+            for (int index = 0; index < gridAccumulatedQty.Rows.Count; index++)
+            {
+                DataRow dr = gridAccumulatedQty.GetDataRow(index);
+                if (gridAccumulatedQty.Rows.Count <= index || index < 0)
+                    return;
+
+                int i = index;
+
+
+                if (MyUtility.Check.Empty(dr["receiving"]))
+                {
+                    gridAccumulatedQty.Rows[i].DefaultCellStyle.BackColor = Color.FromArgb(190, 190, 190);
+                }
+                else
+                {
+                    gridAccumulatedQty.Rows[i].DefaultCellStyle.BackColor = Color.White;
+                }
+            }
+        }
+
+        private void chkRcvdQty_CheckedChanged(object sender, EventArgs e)
+        {
+            this.grid_Filter();
+            this.ChangeColor();
         }
     }
 }
