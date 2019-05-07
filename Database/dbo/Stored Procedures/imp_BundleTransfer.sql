607482f91d14d3dac0e71177851eb2d8c2ff6545
JEFF <jeff.yeh@sportscity.com.tw>
2019-05-07 15:03:46 +0800

    ISP20190480 改用RFIDReader_SubProcess. ProcessId

--- Database/dbo/Stored Procedures/imp_BundleTransfer.sql
+++ Database/dbo/Stored Procedures/imp_BundleTransfer.sql
@@ -38,17 +38,17 @@ BEGIN
 
 		declare @MaxSid bigint
 		select @MaxSid = max(Sid) from  #tmp
-
+		
 			--add BundleTransfer	
 			INSERT INTO BundleTransfer(Sid,RFIDReaderId,Type,SubProcessId,TagId,BundleNo,TransferDate,AddDate,LocationID,RFIDProcessLocationID,PanelNo,CutCellID)
-			SELECT RB.sid, RB.readerid, RR.type, RR.ProcessId, RB.tagid, RB.epcid, RB.transdate, GETDATE()
+			SELECT RB.sid, RB.readerid, RR.type, RRS.ProcessID, RB.tagid, RB.epcid, RB.transdate, GETDATE()
 			,[LocationID]=isnull (RB.LocationID, ' + '''''' + '),isnull (RR.RFIDProcessLocationID, '''')
-			,RB.PanelNo
-			,RRp.CutCellID
+			,PanelNo = isnull(RB.PanelNo,'''')
+			,CutCellID=isnull(RRp.CutCellID,'''')
 			FROM  #tmp  RB
 			left join RFIDReader RR on RB.readerid  collate Chinese_Taiwan_Stroke_CI_AS  =RR.Id
-			left join RFIDReader_Panel RRP on RRP.RFIDReaderID = RR.ID 
-			and RB.PanelNo  collate Chinese_Taiwan_Stroke_CI_AS = RRP.PanelNo
+			left join RFIDReader_Panel RRP on RRP.RFIDReaderID = RR.ID 	and isnull(RB.PanelNo,'''')  collate Chinese_Taiwan_Stroke_CI_AS = isnull(RRP.PanelNo,'''')
+			left join RFIDReader_SubProcess RRS on RRS.RFIDReaderID = RR.ID
 			
 			-- 先確認有哪些捆包號 + 工段 + Type，再到紀錄中找最新的資料
 			select *
@@ -56,32 +56,33 @@ BEGIN
 			from (
 				select distinct
 						tmp.EpcId
-						, rd.processId
+						, RRS.processId
 						, rd.Type
 						, rd.RFIDProcessLocationID
 				from #tmp tmp				
 				inner join RFIDReader rd on tmp.ReaderId collate Chinese_Taiwan_Stroke_CI_AS = rd.Id
+				left join RFIDReader_SubProcess RRS on RRS.RFIDReaderID = rd.ID
 				where len(tmp.Epcid) <= 10
 			) disBundle
 			outer apply (
 				select top 1
 						BundleNo = tmp.EpcId
-						, SubProcessId = rd.processId
+						, SubProcessId = RRS.processId
 						, TransDate = tmp.TransDate
 						, AddDate = GetDate()
 						, rd.SewingLineID
 						, [LocationID] = isnull (tmp.LocationID, ' + '''''' + ')
-						, tmp.PanelNo
-						, RRP.CutCellID
+						, PanelNo=isnull(tmp.PanelNo,'''')
+						, CutCellID=isnull(RRP.CutCellID,'''')
 				from #tmp tmp
 				inner join RFIDReader rd on tmp.ReaderId collate Chinese_Taiwan_Stroke_CI_AS = rd.Id
-				left join RFIDReader_Panel RRP on RRP.RFIDReaderID = rd.ID 
-				and tmp.PanelNo  collate Chinese_Taiwan_Stroke_CI_AS = RRP.PanelNo
+				left join RFIDReader_Panel RRP on RRP.RFIDReaderID = rd.ID and isnull(tmp.PanelNo,'''')  collate Chinese_Taiwan_Stroke_CI_AS = isnull(RRP.PanelNo,'''')
+				left join RFIDReader_SubProcess RRS on RRS.RFIDReaderID = rd.ID
 				where 	disBundle.EpcId = tmp.EpcId
-						and disBundle.processId = rd.processId
+						and disBundle.processId = RRS.processId
 						and disBundle.Type = rd.Type
 				order by tmp.TransDate Desc
-			) getLastTrans'
+			) getLastTrans '
 
 			--add update BundleInOut			
 			-- RFIDReader.Type=1
