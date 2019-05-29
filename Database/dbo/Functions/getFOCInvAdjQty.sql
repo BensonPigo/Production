054e510071d76450d439c91f85719282af6fc99c
JEFF <jeff.yeh@sportscity.com.tw>
2019-05-29 16:50:36 +0800

    ISP20190626新建[getFOCInvAdjQty]

---
+++ Database/dbo/Functions/getFOCInvAdjQty.sql
@@ -0,0 +1,13 @@
+﻿
+Create FUNCTION [dbo].[getFOCInvAdjQty](@orderid varchar(13), @seq varchar(2))
+RETURNS int
+BEGIN
+	DECLARE @int int --要回傳的字串
+	select @int = isnull(sum(i.AdjustPulloutQty),0)
+	from InvAdjust i WITH (NOLOCK)
+	inner join PackingList pl WITH (NOLOCK) on pl.INVNo = i.GarmentInvoiceID
+	where i.OrderID = @orderid
+	and i.OrderShipmodeSeq = @seq
+	
+	RETURN @int
+END
