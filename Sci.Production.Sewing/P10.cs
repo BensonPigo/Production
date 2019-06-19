5f7d4bdc21b5095cea89f1b21239c1719b1e5266
DOMAIN\jack.hsu <jack.hsu@sportscity.com.tw>
2019-06-19 15:41:31 +0800

    ISP20190777 - Sewing P10 新增程式。

---
+++ Sci.Production.Sewing/P10.cs
@@ -0,0 +1,47 @@
+﻿using Ict;
+using Ict.Win;
+using Sci.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Sewing
+{
+    public partial class P10 : Sci.Win.Tems.QueryForm
+    {
+        public P10(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnFormLoaded()
+        {
+            base.OnFormLoaded();
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            if (MyUtility.Check.Empty(this.dateBoxUpdateDate.Value))
+            {
+                MyUtility.Msg.WarningBox("Update date cannot be empty.");
+                return;
+            }
+
+            string sqlcmd = $"exec SNPAutoTransferToSewingOutput '{this.dateBoxUpdateDate.Text}' ";
+
+            // 12分鐘  比照排程執行時間
+            DBProxy.Current.DefaultTimeout = 7200;
+            DualResult result = DBProxy.Current.Execute(null, sqlcmd);
+            if (!result)
+            {
+                MyUtility.Msg.WarningBox(result.Messages.ToString());
+                return;
+            }
+        }
+    }
+}
