c99f4f85f90ad1dbf1e7b7b96cfa778e994b0bd3
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2019-06-20 15:27:07 +0800

    ISP20190747

---
+++ Sci.Production.Subcon/P41.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace Sci.Production.Subcon
+{
+    partial class P41
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnQuery = new Sci.Win.UI.Button();
+            this.chkExtendAllParts = new Sci.Win.UI.CheckBox();
+            this.chkAllPartOnly = new Sci.Win.UI.CheckBox();
+            this.comboSubPorcess = new Sci.Win.UI.ComboBox();
+            this.comboComplete = new Sci.Win.UI.ComboBox();
+            this.txtLineNo = new Sci.Win.UI.TextBox();
+            this.txtCutCell = new Sci.Win.UI.TextBox();
+            this.txtFactory = new Sci.Production.Class.txtscifactory();
+            this.txtSPNo = new Sci.Win.UI.TextBox();
+            this.estCutDate = new Sci.Win.UI.DateBox();
+            this.label7 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.label5 = new Sci.Win.UI.Label();
+            this.label6 = new Sci.Win.UI.Label();
+            this.label3 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.label1 = new Sci.Win.UI.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.grid = new Sci.Win.UI.Grid();
+            this.listControlBindingSource1 = new Sci.Win.UI.ListControlBindingSource(this.components);
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnQuery);
+            this.panel1.Controls.Add(this.chkExtendAllParts);
+            this.panel1.Controls.Add(this.chkAllPartOnly);
+            this.panel1.Controls.Add(this.comboSubPorcess);
+            this.panel1.Controls.Add(this.comboComplete);
+            this.panel1.Controls.Add(this.txtLineNo);
+            this.panel1.Controls.Add(this.txtCutCell);
+            this.panel1.Controls.Add(this.txtFactory);
+            this.panel1.Controls.Add(this.txtSPNo);
+            this.panel1.Controls.Add(this.estCutDate);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(978, 111);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(886, 9);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(80, 30);
+            this.btnQuery.TabIndex = 10;
+            this.btnQuery.Text = "Query";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            // 
+            // chkExtendAllParts
+            // 
+            this.chkExtendAllParts.AutoSize = true;
+            this.chkExtendAllParts.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.chkExtendAllParts.Location = new System.Drawing.Point(485, 75);
+            this.chkExtendAllParts.Name = "chkExtendAllParts";
+            this.chkExtendAllParts.Size = new System.Drawing.Size(126, 21);
+            this.chkExtendAllParts.TabIndex = 9;
+            this.chkExtendAllParts.Text = "Extend All Parts";
+            this.chkExtendAllParts.UseVisualStyleBackColor = true;
+            // 
+            // chkAllPartOnly
+            // 
+            this.chkAllPartOnly.AutoSize = true;
+            this.chkAllPartOnly.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.chkAllPartOnly.Location = new System.Drawing.Point(485, 43);
+            this.chkAllPartOnly.Name = "chkAllPartOnly";
+            this.chkAllPartOnly.Size = new System.Drawing.Size(112, 21);
+            this.chkAllPartOnly.TabIndex = 8;
+            this.chkAllPartOnly.Text = "All Parts Only";
+            this.chkAllPartOnly.UseVisualStyleBackColor = true;
+            // 
+            // comboSubPorcess
+            // 
+            this.comboSubPorcess.BackColor = System.Drawing.Color.White;
+            this.comboSubPorcess.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboSubPorcess.FormattingEnabled = true;
+            this.comboSubPorcess.IsSupportUnselect = true;
+            this.comboSubPorcess.Location = new System.Drawing.Point(611, 8);
+            this.comboSubPorcess.Name = "comboSubPorcess";
+            this.comboSubPorcess.OldText = "";
+            this.comboSubPorcess.Size = new System.Drawing.Size(119, 24);
+            this.comboSubPorcess.TabIndex = 7;
+            // 
+            // comboComplete
+            // 
+            this.comboComplete.BackColor = System.Drawing.Color.White;
+            this.comboComplete.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboComplete.FormattingEnabled = true;
+            this.comboComplete.IsSupportUnselect = true;
+            this.comboComplete.Location = new System.Drawing.Point(388, 74);
+            this.comboComplete.Name = "comboComplete";
+            this.comboComplete.OldText = "";
+            this.comboComplete.Size = new System.Drawing.Size(76, 24);
+            this.comboComplete.TabIndex = 6;
+            // 
+            // txtLineNo
+            // 
+            this.txtLineNo.BackColor = System.Drawing.Color.White;
+            this.txtLineNo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtLineNo.Location = new System.Drawing.Point(388, 42);
+            this.txtLineNo.Name = "txtLineNo";
+            this.txtLineNo.Size = new System.Drawing.Size(76, 23);
+            this.txtLineNo.TabIndex = 5;
+            // 
+            // txtCutCell
+            // 
+            this.txtCutCell.BackColor = System.Drawing.Color.White;
+            this.txtCutCell.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtCutCell.Location = new System.Drawing.Point(388, 10);
+            this.txtCutCell.Name = "txtCutCell";
+            this.txtCutCell.Size = new System.Drawing.Size(76, 23);
+            this.txtCutCell.TabIndex = 4;
+            // 
+            // txtFactory
+            // 
+            this.txtFactory.BackColor = System.Drawing.Color.White;
+            this.txtFactory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtFactory.Location = new System.Drawing.Point(114, 75);
+            this.txtFactory.Name = "txtFactory";
+            this.txtFactory.Size = new System.Drawing.Size(66, 23);
+            this.txtFactory.TabIndex = 3;
+            // 
+            // txtSPNo
+            // 
+            this.txtSPNo.BackColor = System.Drawing.Color.White;
+            this.txtSPNo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtSPNo.Location = new System.Drawing.Point(113, 43);
+            this.txtSPNo.Name = "txtSPNo";
+            this.txtSPNo.Size = new System.Drawing.Size(100, 23);
+            this.txtSPNo.TabIndex = 2;
+            // 
+            // estCutDate
+            // 
+            this.estCutDate.Location = new System.Drawing.Point(113, 9);
+            this.estCutDate.Name = "estCutDate";
+            this.estCutDate.Size = new System.Drawing.Size(130, 23);
+            this.estCutDate.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label7.Location = new System.Drawing.Point(485, 8);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(123, 23);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "SubPorcess";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(262, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 23);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Loading Complete";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label5.Location = new System.Drawing.Point(262, 42);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 23);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Inline Line#";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label6.Location = new System.Drawing.Point(262, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 23);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Cut Cell";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(19, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Factory";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.SkyBlue;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(19, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SP#";
+            this.label2.TextStyle.Color = System.Drawing.Color.Black;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.SkyBlue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(19, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Est. Cut Date";
+            this.label1.TextStyle.Color = System.Drawing.Color.Black;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.grid);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 111);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(978, 474);
+            this.panel2.TabIndex = 1;
+            // 
+            // grid
+            // 
+            this.grid.AllowUserToAddRows = false;
+            this.grid.AllowUserToDeleteRows = false;
+            this.grid.AllowUserToResizeRows = false;
+            this.grid.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.grid.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid.EditingEnter = Ict.Win.UI.DataGridViewEditingEnter.NextCellOrNextRow;
+            this.grid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.grid.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.grid.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(162)))), ((int)(((byte)(163)))));
+            this.grid.Location = new System.Drawing.Point(0, 0);
+            this.grid.Name = "grid";
+            this.grid.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.grid.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.grid.RowTemplate.Height = 24;
+            this.grid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grid.ShowCellToolTips = false;
+            this.grid.Size = new System.Drawing.Size(978, 474);
+            this.grid.TabIndex = 0;
+            // 
+            // P41
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 585);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "P41";
+            this.OnLineHelpID = "Sci.Win.Subs.Base";
+            this.Text = "P41. Sorting and Loading Bundle Checklist";
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.Controls.SetChildIndex(this.panel2, 0);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private Win.UI.Grid grid;
+        private Win.UI.Label label4;
+        private Win.UI.Label label5;
+        private Win.UI.Label label6;
+        private Win.UI.Label label3;
+        private Win.UI.Label label2;
+        private Win.UI.Label label1;
+        private Win.UI.ListControlBindingSource listControlBindingSource1;
+        private Win.UI.Label label7;
+        private Win.UI.ComboBox comboSubPorcess;
+        private Win.UI.ComboBox comboComplete;
+        private Win.UI.TextBox txtLineNo;
+        private Win.UI.TextBox txtCutCell;
+        private Class.txtscifactory txtFactory;
+        private Win.UI.TextBox txtSPNo;
+        private Win.UI.DateBox estCutDate;
+        private Win.UI.CheckBox chkExtendAllParts;
+        private Win.UI.CheckBox chkAllPartOnly;
+        private Win.UI.Button btnQuery;
+    }
+}
---
+++ Sci.Production.Subcon/P41.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sci.Production.Subcon
+{
+    public partial class P41 : Sci.Win.Tems.QueryForm
+    {
+        public P41(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            InitializeComponent();
+            EditMode = true;
+
+            //this.comboSubPorcess.set
+        }
+    }
+}
