6b628413a1abf870f6464e5d140028c65d17da78
JEFF <jeff.yeh@sportscity.com.tw>
2019-06-26 11:07:52 +0800

    刪除在表身點選右鍵開窗P02_ImportFromFOCPackingList加入時寫入PulloutDate的部分

--- Sci.Production.Shipping/P02_ImportFromFOCPackingList.cs
+++ Sci.Production.Shipping/P02_ImportFromFOCPackingList.cs
@@ -217,17 +217,7 @@ from Express_Detail where ID = '{0}' and Seq2 = ''),'{2}','{3}','{4}',{5},{6},'{
                                             Sci.Env.User.UserID));
             }
 
-            string pulloutdate = "Null";
-            if (!MyUtility.Check.Empty(this.masterData["ShipDate"]))
-            {
-                pulloutdate = "'" + ((DateTime)this.masterData["ShipDate"]).ToString("d") + "'";
-            }
-            else
-            {
-                pulloutdate = "Null";
-            }
-
-            insertCmds.Add($"update PackingList set ExpressID = '{this.masterData["ID"]}',pulloutdate={pulloutdate} where ID = '{dt.Rows[0]["ID"]}'");
+            insertCmds.Add($"update PackingList set ExpressID = '{this.masterData["ID"]}' where ID = '{dt.Rows[0]["ID"]}'");
             DualResult result1, result2;
             using (TransactionScope transactionScope = new TransactionScope())
             {
