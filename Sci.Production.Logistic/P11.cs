8bd97215316b600daac1d95fca9d7181f0b11e1d
Dyson <Dyson.Chen@sportscity.com.tw>
2019-07-11 16:16:10 +0800

    modify width layout

--- Sci.Production.Logistic/P11.cs
+++ Sci.Production.Logistic/P11.cs
@@ -115,16 +115,16 @@ where cdd.ID = '{masterID}'
         {
             #region -- 欄位設定 --
             this.Helper.Controls.Grid.Generator(this.detailgrid)
-            .Text("PackingListID", header: "Pack ID", width: Widths.AnsiChars(15), iseditingreadonly: true)
-            .Text("CTNStartNO", header: "CTN#", width: Widths.AnsiChars(6), iseditingreadonly: true)
-            .Text("OrderID", header: "SP#", width: Widths.AnsiChars(15), iseditingreadonly: true)
-            .Text("CustPoNo", header: "PO#", width: Widths.AnsiChars(20), iseditingreadonly: true)
-            .Text("StyleID", header: "Style", width: Widths.AnsiChars(15), iseditingreadonly: true)
-            .Text("Article", header: "ColorWay", width: Widths.AnsiChars(10), iseditingreadonly: true)
-            .Text("Color", header: "Color", width: Widths.AnsiChars(10), iseditingreadonly: true)
-            .Text("Size", header: "Size", width: Widths.AnsiChars(18), iseditingreadonly: true)
-            .Text("QtyPerCTN", header: "Qty", width: Widths.AnsiChars(18), iseditingreadonly: true)
-            .CellClogLocation("ClogLocationID", header: "Location", width: Widths.AnsiChars(10), iseditingreadonly: true);
+            .Text("PackingListID", header: "Pack ID", width: Widths.AnsiChars(13), iseditingreadonly: true)
+            .Text("CTNStartNO", header: "CTN#", width: Widths.AnsiChars(5), iseditingreadonly: true)
+            .Text("OrderID", header: "SP#", width: Widths.AnsiChars(13), iseditingreadonly: true)
+            .Text("CustPoNo", header: "PO#", width: Widths.AnsiChars(13), iseditingreadonly: true)
+            .Text("StyleID", header: "Style", width: Widths.AnsiChars(13), iseditingreadonly: true)
+            .Text("Article", header: "ColorWay", width: Widths.AnsiChars(9), iseditingreadonly: true)
+            .Text("Color", header: "Color", width: Widths.AnsiChars(8), iseditingreadonly: true)
+            .Text("Size", header: "Size", width: Widths.AnsiChars(5), iseditingreadonly: true)
+            .Text("QtyPerCTN", header: "Qty", width: Widths.AnsiChars(5), iseditingreadonly: true)
+            .CellClogLocation("ClogLocationID", header: "Location", width: Widths.AnsiChars(8), iseditingreadonly: true);
 
             #endregion 欄位設定
         }
