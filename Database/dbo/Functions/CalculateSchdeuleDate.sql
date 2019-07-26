3871948b1a8cfd6290d13df85029b22f4fe98a40
DOMAIN\jack.hsu <jack.hsu@sportscity.com.tw>
2019-07-26 17:07:47 +0800

    ISP20191000 - IE P02 寫入ChgOver_Check同步新增ScheduleDate依據BaseOn取得SCIDeliver、ChangeOverDate並排除星期日與假日                          新增fn CalculateSchdeuleDate

---
+++ Database/dbo/Functions/CalculateSchdeuleDate.sql
@@ -0,0 +1,71 @@
+﻿CREATE FUNCTION [dbo].[CalculateSchdeuleDate]
+(
+    @EndDate as Date,
+    @DiffDate as int = 0,
+    @FactoryID as varchar(10),
+	@isHoliday as bit = 0
+)
+RETURNS Date
+AS
+BEGIN
+	if @EndDate is null
+	begin
+		return null
+	end 
+
+    DECLARE @StartDate as Date 
+	DECLARE @HolidayB as int = 0, @HolidayE as int = 1 
+
+	select @StartDate = dateadd(d, @DiffDate * -1, @EndDate) 
+
+	if (@FactoryID = '' or @FactoryID is null)
+	begin 
+		while @HolidayB <> @HolidayE
+		begin
+			set @HolidayB = @HolidayE
+
+			;WITH WorkDays
+			AS(
+				SELECT @startDate AS WorkDay, @DiffDate AS DiffDays
+				UNION ALL
+				SELECT DATEADD(dd, 1, WorkDay),DiffDays -1
+				FROM WorkDays wd
+				WHERE DiffDays > 0
+			),
+			cntHoliday as (
+				SELECT count(*) cntHoliday
+				FROM WorkDays wd
+				WHERE DATEPART(dw, WorkDay ) = 1
+				or exists (SELECT * FROM Holiday h WHERE h.HolidayDate = wd.WorkDay and @isHoliday = 1)
+			)
+			select @HolidayE =  cntHoliday
+			from cntHoliday  
+		end  
+	end
+	else
+	begin 
+		while @HolidayB <> @HolidayE
+		begin
+			set @HolidayB = @HolidayE
+
+			;WITH WorkDays
+			AS(
+				SELECT @startDate AS WorkDay, @DiffDate AS DiffDays
+				UNION ALL
+				SELECT DATEADD(dd, 1, WorkDay),DiffDays -1
+				FROM WorkDays wd
+				WHERE DiffDays > 0
+			),
+			cntHoliday as (
+				SELECT count(*) cntHoliday
+				FROM WorkDays wd
+				WHERE DATEPART(dw, WorkDay ) = 1
+				or exists (SELECT * FROM Holiday h WHERE h.HolidayDate = wd.WorkDay and h.FactoryID = @FactoryID and @isHoliday = 1)
+			)
+			select @HolidayE =  cntHoliday
+			from cntHoliday  
+		end  
+	end
+	
+	return dateadd(d, (@HolidayE + @DiffDate) * -1, @EndDate)
+END
