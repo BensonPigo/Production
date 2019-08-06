8fccb2bba645bddd4b8289501637493c885daae5
JEFF <jeff.yeh@sportscity.com.tw>
2019-08-06 11:14:09 +0800

    ISP20191039 更新Dll 調整PPIC R03

--- Database/dbo/Tables/CustomizedReport_Detail.sql
+++ Database/dbo/Tables/CustomizedReport_Detail.sql
@@ -1,9 +1,11 @@
 ﻿CREATE TABLE [dbo].[CustomizedReport_Detail] (
-    [CustomizedIden]    BIGINT        NULL,
-    [ExcelColumnIndex]  NVARCHAR (50) NULL,
-    [ExcelColumnHeader] NVARCHAR (50) NULL,
-    [ExcelColumnIden]   BIGINT        NULL,
-    [ID]                BIGINT        IDENTITY (1, 1) NOT NULL,
+    [CustomizedIden]    BIGINT         NULL,
+    [ExcelColumnIndex]  NVARCHAR (50)  NULL,
+    [ExcelColumnHeader] NVARCHAR (100) NULL,
+    [ExcelColumnIden]   BIGINT         NULL,
+    [ID]                BIGINT         IDENTITY (1, 1) NOT NULL,
     CONSTRAINT [PK_CustomizedReport_Detail] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
+
+
 
