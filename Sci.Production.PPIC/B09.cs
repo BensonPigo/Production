a71c2ddbc2f6240fbe05310f8690783278f49e82
Willy <willy.wei@sprotscity.com.tw>
2019-08-27 17:34:22 +0800

    ISP20191149 1. B09 ID 自動+1 2. FinishingProcess DM300 移除identity

--- Sci.Production.PPIC/B09.cs
+++ Sci.Production.PPIC/B09.cs
@@ -15,5 +15,19 @@ namespace Sci.Production.PPIC
         {
             InitializeComponent();
         }
+
+        protected override bool ClickSaveBefore()
+        {
+            // GetID
+            if (this.IsDetailInserting)
+            {
+                string id = MyUtility.GetValue.Lookup(@"
+select MAXID = case when max(DM300) >= 0 then max(DM300) +1 else 0 end
+from FinishingProcess");
+                this.CurrentMaintain["DM300"] = id;
+            }
+
+            return base.ClickSaveBefore();
+        }
     }
 }
