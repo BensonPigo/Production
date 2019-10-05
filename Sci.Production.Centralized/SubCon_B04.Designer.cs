8cbb6c93cfad8beb7871b7a4ab2616954226a7fa
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2019-10-05 18:16:04 +0800

    ISP20191401 -  Centralized、Subcon 新增 B04功能

---
+++ Sci.Production.Centralized/SubCon_B04.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Sci.Production.Centralized
+{
+    partial class SubCon_B04
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtReason = new Sci.Win.UI.TextBox();
+            this.displayID = new Sci.Win.UI.DisplayBox();
+            this.label3 = new Sci.Win.UI.Label();
+            this.label1 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).BeginInit();
+            this.detail.SuspendLayout();
+            this.detailcont.SuspendLayout();
+            this.detailbtm.SuspendLayout();
+            this.tabs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // detail
+            // 
+            this.detail.Size = new System.Drawing.Size(792, 388);
+            // 
+            // detailcont
+            // 
+            this.detailcont.Controls.Add(this.label4);
+            this.detailcont.Controls.Add(this.txtReason);
+            this.detailcont.Controls.Add(this.displayID);
+            this.detailcont.Controls.Add(this.label3);
+            this.detailcont.Controls.Add(this.label1);
+            this.detailcont.Size = new System.Drawing.Size(792, 350);
+            // 
+            // detailbtm
+            // 
+            this.detailbtm.Location = new System.Drawing.Point(0, 350);
+            this.detailbtm.Size = new System.Drawing.Size(792, 38);
+            // 
+            // browse
+            // 
+            this.browse.Size = new System.Drawing.Size(792, 388);
+            // 
+            // tabs
+            // 
+            this.tabs.Size = new System.Drawing.Size(800, 417);
+            // 
+            // txtReason
+            // 
+            this.txtReason.BackColor = System.Drawing.Color.White;
+            this.txtReason.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "Reason", true));
+            this.txtReason.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtReason.Location = new System.Drawing.Point(121, 75);
+            this.txtReason.Name = "txtReason";
+            this.txtReason.Size = new System.Drawing.Size(452, 23);
+            this.txtReason.TabIndex = 12;
+            // 
+            // displayID
+            // 
+            this.displayID.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
+            this.displayID.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "ID", true));
+            this.displayID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.displayID.Location = new System.Drawing.Point(121, 27);
+            this.displayID.Name = "displayID";
+            this.displayID.Size = new System.Drawing.Size(100, 23);
+            this.displayID.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(33, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 23);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Reason";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(33, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 23);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "ID";
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "Status", true));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label4.Location = new System.Drawing.Point(585, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 23);
+            this.label4.TabIndex = 24;
+            this.label4.TextStyle.Color = System.Drawing.Color.Red;
+            // 
+            // SubCon_B04
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ConnectionName = "ProductionTPE";
+            this.DefaultFilter = "Type=\'SQ\'";
+            this.IsDeleteOnBrowse = false;
+            this.IsSupportCopy = false;
+            this.IsSupportDelete = false;
+            this.IsSupportJunk = true;
+            this.IsSupportUnJunk = true;
+            this.JunkChkValue = "New";
+            this.Name = "SubCon_B04";
+            this.OnLineHelpID = "Sci.Win.Tems.Input1";
+            this.Text = "SubCon_B04. Irregular Qty Reason";
+            this.UnjunkChkValue = "Junked";
+            this.WorkAlias = "SubconReason";
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).EndInit();
+            this.detail.ResumeLayout(false);
+            this.detailcont.ResumeLayout(false);
+            this.detailcont.PerformLayout();
+            this.detailbtm.ResumeLayout(false);
+            this.detailbtm.PerformLayout();
+            this.tabs.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.Label label4;
+        private Win.UI.TextBox txtReason;
+        private Win.UI.DisplayBox displayID;
+        private Win.UI.Label label3;
+        private Win.UI.Label label1;
+    }
+}
