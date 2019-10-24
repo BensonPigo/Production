663a2cc691014167e06caa4c0e134336df02352c
Willy <willy.wei@sprotscity.com.tw>
2019-10-24 16:51:59 +0800

    ISP20191543 1. 倒回Branch_ISP20191233 移除WH.P10,P13,P16 RDLC Report Balance Qty 欄位

--- Sci.Production.Warehouse/P13_PrintData.cs
+++ Sci.Production.Warehouse/P13_PrintData.cs
@@ -16,7 +16,6 @@ namespace Sci.Production.Warehouse
         public string Roll { get; set; }
         public string DYELOT { get; set; }
         public string QTY { get; set; }
-        public string BalanceQty { get; set; }
         public string TotalQTY { get; set; }
     }
 }
