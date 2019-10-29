8ef4d00371269b7762d7d8f2e2ddf89ed8e3e41f
Willy <willy.wei@sprotscity.com.tw>
2019-10-29 14:23:36 +0800

    ISP20191570 1. 新增Table結構: CustomizedClipPath

---
+++ Database/dbo/Tables/CustomizedClipPath.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[CustomizedClipPath] (
+    [TableName] VARCHAR (50)  NOT NULL,
+    [Path]      VARCHAR (100) NULL,
+    [AddName]   VARCHAR (10)  NULL,
+    [AddDate]   DATETIME      NULL,
+    [EditName]  VARCHAR (10)  NULL,
+    [EditDate]  DATETIME      NULL,
+    CONSTRAINT [PK_CustomizedClipPath] PRIMARY KEY CLUSTERED ([TableName] ASC)
+);
+
