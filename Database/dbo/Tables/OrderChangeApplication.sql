da7b3def5f57b39564b79a469e7a8154463d9db7
JEFF <jeff.yeh@sportscity.com.tw>
2019-11-27 21:06:22 +0800

    ISP20191360新增Table [OrderChangeApplication] [OrderChangeApplication_Detail] [OrderChangeApplication_Seq] [OrderChangeApplication_History]

---
+++ Database/dbo/Tables/OrderChangeApplication.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[OrderChangeApplication] (
+    [ID]       VARCHAR (13) NOT NULL,
+    [ReasonID] VARCHAR (5)  NULL,
+    [OrderID]  VARCHAR (13) NULL,
+    [Status]   VARCHAR (15) NULL,
+    [AddName]  VARCHAR (10) NULL,
+    [AddDate]  DATETIME     NULL,
+    [EditName] VARCHAR (10) NULL,
+    [EditDate] DATETIME     NULL,
+    CONSTRAINT [PK_OrderChangeApplication] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
---
+++ Database/dbo/Tables/OrderChangeApplication_History.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[OrderChangeApplication_History] (
+    [Ukey]       BIGINT       IDENTITY (1, 1) NOT NULL,
+    [ID]         VARCHAR (13) NULL,
+    [Status]     VARCHAR (15) NULL,
+    [StatusUser] VARCHAR (10) NULL,
+    [StatusDate] DATETIME     NULL,
+    CONSTRAINT [PK_OrderChangeApplication_History] PRIMARY KEY CLUSTERED ([Ukey] ASC)
+);
+
