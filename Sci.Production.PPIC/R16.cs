b0d0c066c2d327f992694ad815ceb980875d9ba6
DOMAIN\aaron.shie <onizuka865@gmail.com>
2019-11-28 14:01:29 +0800

    -ISP20191773 R16-bad performance 調整sql寫法，減少out apply使用

--- Sci.Production.PPIC/R16.cs
+++ Sci.Production.PPIC/R16.cs
@@ -70,6 +70,40 @@ namespace Sci.Production.PPIC
         protected override Ict.DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
         {
             StringBuilder sqlcmd = new StringBuilder();
+            StringBuilder sqlWhere = new StringBuilder();
+            StringBuilder sqlWhereOutstanding = new StringBuilder();
+            #region WHERE條件
+            if (!MyUtility.Check.Empty(this.BuyerDev_S))
+            {
+                sqlWhere.Append($"AND oq.BuyerDelivery >= '{this.BuyerDev_S.Value.ToString("yyyy/MM/dd")}'" + Environment.NewLine);
+            }
+
+            if (!MyUtility.Check.Empty(this.BuyerDev_E))
+            {
+                sqlWhere.Append($"AND oq.BuyerDelivery <= '{this.BuyerDev_E.Value.ToString("yyyy/MM/dd")}'" + Environment.NewLine);
+            }
+
+            if (!MyUtility.Check.Empty(this.BrandID))
+            {
+                sqlWhere.Append($"AND o.BrandID = '{this.BrandID}'" + Environment.NewLine);
+            }
+
+            if (!MyUtility.Check.Empty(this.MDivisionID))
+            {
+                sqlWhere.Append($"AND o.MDivisionID = '{this.MDivisionID}'" + Environment.NewLine);
+            }
+
+            if (!MyUtility.Check.Empty(this.FactoryID))
+            {
+                sqlWhere.Append($"AND o.FtyGroup = '{this.FactoryID}'" + Environment.NewLine);
+            }
+
+            if (this.IsOutstanding)
+            {
+                sqlWhereOutstanding.Append($" where ( main.OrderQty > ISNULL(pd.PackingQty,0) OR (ISNULL(pd.PackingCarton,0) - ISNULL(pd.ClogReceivedCarton,0)) <> 0 ) " + Environment.NewLine);
+            }
+
+            #endregion
 
             #region 組SQL
 
@@ -88,168 +122,113 @@ SELECT
 				   END
 	,[PartialShipment]=IIF(PartialShipment.Count > 1 ,'Y','')
 	,[Cancelled]=IIF(o.Junk=1,'Y','N')
-	,[OrderQty]=OrderQty.Qty
-	,[PackingCarton] = PackingCarton.Value
-	,[PackingQty]=PackingQty.Value
-	,[ClogReceivedCarton]=ISNULL(ClogReceivedCarton.Value ,0)
-	,[LastCMPOutputDate]=LastCMPOutputDate.Value
-	,[LastDQSOutputDate]=LastDQSOutputDate.Value
-	,[OST Clog Carton]= ISNULL(PackingCarton.Value,0) - ISNULL(ClogReceivedCarton.Value,0)
-INTO #tmp
+	,[OrderQty]= isnull(oq.Qty,0)
+	into #tmpOrderMain
 FROM Orders o WITH(NOLOCK)
 LEFT JOIN Order_QtyShip oq WITH(NOLOCK) ON o.ID=oq.ID
 LEFT JOIN OrderType ot WITH(NOLOCK) ON o.OrderTypeID=ot.ID AND o.BrandID = ot.BrandID
 OUTER APPLY(
 	SELECT [Count] = COUNT(ID) FROM Order_QtyShip oqq WITH(NOLOCK) WHERE oqq.Id=o.ID
 )PartialShipment
-OUTER APPLY(
-	SELECT Qty FROM Order_QtyShip oqq WITH(NOLOCK) WHERE oqq.Id=o.ID AND oqq.Seq=oq.Seq
-)OrderQty
-OUTER APPLY(
-	SELECT [Value]=COUNT(Ukey) FROM PackingList_Detail pd WITH(NOLOCK)
-	WHERE pd.OrderID=o.ID
-	AND pd.OrderShipmodeSeq=oq.Seq
-	AND pd.CTNQty=1
-)PackingCarton
-OUTER APPLY(
-	SELECT [Value]=isnull(Sum(pd.ShipQty),0)
-	FROM PackingList_Detail pd WITH(NOLOCK)
-	WHERE pd.OrderID=o.ID
-	AND pd.OrderShipmodeSeq=oq.Seq
-)PackingQty
-OUTER APPLY(
-	SELECT [Value]=COUNT(Ukey) FROM PackingList_Detail pd WITH(NOLOCK)
-	WHERE pd.OrderID=o.ID
-	AND pd.OrderShipmodeSeq=oq.Seq
-	AND pd.CTNQty=1
-	AND ( pd.CFAReceiveDate IS NOT NULL OR pd.ReceiveDate IS NOT NULL)
-)ClogReceivedCarton
+where o.Category IN ('B','G') and
+      isnull(ot.IsGMTMaster,0) = 0
+      {sqlWhere.ToString()}
+
+select 
+pd.OrderID,
+pd.OrderShipmodeSeq,
+[PackingQty] = sum(isnull(pd.ShipQty,0)),
+[PackingCarton] = sum(iif(pd.CTNQty = 1,1,0)),
+[ClogReceivedCarton] = sum(iif(pd.CTNQty = 1 AND ( pd.CFAReceiveDate IS NOT NULL OR pd.ReceiveDate IS NOT NULL),1,0)),
+[ClogReceivedQty] = sum(iif(pd.CTNQty = 1 AND ( pd.CFAReceiveDate IS NOT NULL OR pd.ReceiveDate IS NOT NULL),pd.ShipQty,0))
+into #tmpPackingList_Detail
+from PackingList_Detail pd with (nolock)
+where exists(select 1 from #tmpOrderMain main where pd.OrderID = main.ID and 
+													pd.OrderShipmodeSeq = main.Seq)
+group by pd.OrderID,pd.OrderShipmodeSeq
+
+--資料庫拆分時要改成以下
+--select 
+--*
+--into #tmpInspection
+--from openquery([ExtendServer],'
+--select ins.OrderId,
+--ins.Location,
+--[DQSQty] = count(1),
+--[LastDQSOutputDate] = MAX(iif(ins.Status in ('Pass','Fixed'), ins.AddDate, null))
+--from [ManufacturingExecution].[dbo].[Inspection] ins WITH(NOLOCK)
+--where exists( select 1 
+--from 
+--Orders o WITH(NOLOCK)
+--LEFT JOIN Order_QtyShip oq WITH(NOLOCK) ON o.ID=oq.ID
+--where o.ID = ins.OrderID--and 同#tmpOrderMain條件
+--)
+--group by ins.OrderId,ins.Location
+--')
+select 
+ins.OrderId,
+ins.Location,
+[DQSQty] = count(1),
+[LastDQSOutputDate] = MAX(iif(ins.Status in ('Pass','Fixed'), ins.AddDate, null))
+into #tmpInspection_Step1
+from [ManufacturingExecution].[dbo].[Inspection] ins WITH(NOLOCK)
+where exists(select 1 from #tmpOrderMain main where main.ID = ins.OrderId)
+group by ins.OrderId,ins.Location
+--
+
+select OrderId,
+[DQSQty] = MIN(DQSQty),
+[LastDQSOutputDate] = MAX(LastDQSOutputDate)
+into #tmpInspection
+from #tmpInspection_Step1
+group by OrderId
+
+
+select 
+	main.FactoryID
+	,main.ID
+	,main.CustPONo
+	,main.StyleID
+	,main.BuyerDelivery
+	,main.Seq
+	,main.ShipmodeID
+	,main.Category
+	,main.PartialShipment
+	,main.Cancelled
+	,main.OrderQty
+	,[PackingCarton] = isnull(pd.PackingCarton,0)
+	,[PackingQty] = isnull(pd.PackingQty,0)
+	,[ClogReceivedCarton] = isnull(pd.ClogReceivedCarton,0)
+	,[ClogReceivedQty]=IIF(main.PartialShipment='Y' ,'NA' ,CAST( ISNULL( pd.ClogReceivedQty,0)  as varchar))
+	,[LastCMPOutputDate]=LastCMPOutputDate.Value
+    ,[CMPQty]=IIF(PartialShipment='Y' ,'NA', CAST(ISNULL( CMPQty.Value,0)  as varchar))
+	,ins.LastDQSOutputDate
+	,[DQSQty]=IIF(main.PartialShipment='Y' , 'NA' , CAST( ISNULL( ins.DQSQty,0)  as varchar))
+	,[OST Packing Qty]=IIF(main.PartialShipment='Y' , 'NA' , CAST(( ISNULL(main.OrderQty,0) -  ISNULL(pd.PackingQty,0)) as varchar))
+	,[OST CMP Qty]=IIF(main.PartialShipment='Y' , 'NA' , CAST((  ISNULL(main.OrderQty,0) -  ISNULL(CMPQty.Value,0))  as varchar))
+	,[OST DQS Qty]=IIF(main.PartialShipment='Y' , 'NA' ,  CAST(( ISNULL(main.OrderQty,0) -  ISNULL(ins.DQSQty,0))  as varchar))
+	,[OST Clog Qty]=IIF(main.PartialShipment='Y' , 'NA' , CAST((  ISNULL(main.OrderQty,0) -  ISNULL(pd.ClogReceivedQty,0))  as varchar))
+	,[OST Clog Carton]= ISNULL(pd.PackingCarton,0) - ISNULL(pd.ClogReceivedCarton,0)
+from #tmpOrderMain main
+left join #tmpPackingList_Detail pd on pd.OrderID = main.id and pd.OrderShipmodeSeq = main.Seq
+left join #tmpInspection ins on ins.OrderId = main.ID
 OUTER APPLY(
 	SELECT [Value]=MAX(s.OutputDate)
 	FROM SewingOutput s WITH(NOLOCK)
 	INNER JOIN SewingOutput_Detail sd WITH(NOLOCK) ON s.ID=sd.ID
-	WHERE sd.OrderId=o.ID AND sd.QAQty > 0
+	WHERE sd.OrderId=main.ID AND sd.QAQty > 0
 )LastCMPOutputDate
 OUTER APPLY(
-	SELECT [Value]=MAX(Date)
-	FROM(
-		SELECT [Date]=MAX(AddDate) 
-		FROM [ManufacturingExecution].[dbo].[Inspection] WITH(NOLOCK)
-		WHERE OrderId=o.ID AND Status='Pass'
-		UNION 
-		SELECT [Date]=MAX(EditDate)
-		FROM [ManufacturingExecution].[dbo].[Inspection] WITH(NOLOCK)
-		WHERE OrderId=o.ID AND Status='Fixed'
-	)Tmp
-)LastDQSOutputDate
-WHERE o.Category IN ('B','G') and
-      isnull(ot.IsGMTMaster,0) = 0
-");
-            #endregion
-
-            #region WHERE條件
-            if (!MyUtility.Check.Empty(this.BuyerDev_S))
-            {
-                sqlcmd.Append($"AND oq.BuyerDelivery >= '{this.BuyerDev_S.Value.ToString("yyyy/MM/dd")}'" + Environment.NewLine);
-            }
-
-            if (!MyUtility.Check.Empty(this.BuyerDev_E))
-            {
-                sqlcmd.Append($"AND oq.BuyerDelivery <= '{this.BuyerDev_E.Value.ToString("yyyy/MM/dd")}'" + Environment.NewLine);
-            }
-
-            if (!MyUtility.Check.Empty(this.BrandID))
-            {
-                sqlcmd.Append($"AND o.BrandID = '{this.BrandID}'" + Environment.NewLine);
-            }
-
-            if (!MyUtility.Check.Empty(this.MDivisionID))
-            {
-                sqlcmd.Append($"AND o.MDivisionID = '{this.MDivisionID}'" + Environment.NewLine);
-            }
-
-            if (!MyUtility.Check.Empty(this.FactoryID))
-            {
-                sqlcmd.Append($"AND o.FtyGroup = '{this.FactoryID}'" + Environment.NewLine);
-            }
-
-            if (this.IsOutstanding)
-            {
-                sqlcmd.Append($" AND( OrderQty.Qty > PackingQty.Value OR (ISNULL(PackingCarton.Value,0) - ISNULL(ClogReceivedCarton.Value,0)) <> 0 ) " + Environment.NewLine);
-            }
-
-            #endregion
-
-            #region SQL
-
-            sqlcmd.Append($@"
-
-SELECT 
-	 FactoryID
-	,ID
-	,CustPONo
-	,StyleID
-	,BuyerDelivery
-	,Seq
-	,ShipmodeID
-	,Category
-	,PartialShipment
-	,Cancelled
-	,[OrderQty]
-	,[PackingCarton]
-	,[PackingQty]
-	,[ClogReceivedCarton]	
-	,[ClogReceivedQty]=IIF(PartialShipment='Y' ,'NA' ,CAST( ISNULL( ClogReceivedQty.Value,0)  as varchar))
-	,[LastCMPOutputDate]
-	,[CMPQty]=IIF(PartialShipment='Y' ,'NA', CAST(ISNULL( CMPQty.Value,0)  as varchar))
-	,[LastDQSOutputDate]
-	,[DQSQty]=IIF(PartialShipment='Y' , 'NA' , CAST( ISNULL( DQSQty.Value,0)  as varchar))
-	,[OST Packing Qty]=IIF(PartialShipment='Y' , 'NA' , CAST(( ISNULL(OrderQty,0) -  ISNULL(PackingQty,0)) as varchar))
-	,[OST CMP Qty]=IIF(PartialShipment='Y' , 'NA' , CAST((  ISNULL(OrderQty,0) -  ISNULL(CMPQty.Value,0))  as varchar))
-	,[OST DQS Qty]=IIF(PartialShipment='Y' , 'NA' ,  CAST(( ISNULL(OrderQty,0) -  ISNULL(DQSQty.Value,0))  as varchar))
-	,[OST Clog Qty]=IIF(PartialShipment='Y' , 'NA' , CAST((  ISNULL(OrderQty,0) -  ISNULL(ClogReceivedQty.Value,0))  as varchar))
-	,[OST Clog Carton]
-FROM #tmp o
-OUTER APPLY(
-	SELECT [Value]=Sum(pd.ShipQty) 
-	FROM PackingList_Detail pd WITH(NOLOCK)
-	WHERE pd.OrderID=o.ID
-	AND pd.OrderShipmodeSeq=o.Seq
-	AND ( pd.CFAReceiveDate IS NOT NULL OR pd.ReceiveDate IS NOT NULL)
-)ClogReceivedQty
-OUTER APPLY(
-	 SELECT [Value]=[dbo].[getMinCompleteSewQty](o.ID,NULL,NULL) 
+	 SELECT [Value]=[dbo].[getMinCompleteSewQty](main.ID,NULL,NULL) 
 )CMPQty
-OUTER APPLY(
-	SELECT  [Value]=CASE WHEN (select StyleUnit from Orders WITH (NOLOCK) where ID = o.ID) LIKE '%SETS%' 
-				 THEN 
-					 (			 
-						SELECT [Value]=MIN(Value) FROM (
-							SELECT Location,[Value]=COUNT(iD)
-							FROM [ManufacturingExecution].[dbo].[Inspection]  WITH(NOLOCK)
-							WHERE OrderID =o.ID
-							GROUP BY Location
-						)tmp
-					 )
-				 ELSE 
-						(
-							SELECT COUNT(i.iD)
-							FROM [ManufacturingExecution].[dbo].[Inspection]  i WITH(NOLOCK)
-							WHERE i.OrderID=o.ID
-						)
-				 END
-)DQSQty
+{sqlWhereOutstanding.ToString()}
+order by main.ID
 
-ORDER BY O.ID
-
-DROP TABLE #tmp
-
+DROP TABLE #tmpOrderMain,#tmpPackingList_Detail,#tmpInspection,#tmpInspection_Step1
 ");
             #endregion
 
-            DBProxy.Current.Select(string.Empty, sqlcmd.ToString(), out this.printData);
-
-            return Result.True;
+            return DBProxy.Current.Select(string.Empty, sqlcmd.ToString(), out this.printData);
         }
 
         /// <inheritdoc/>
