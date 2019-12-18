3a12b9b8f3874122c472302fb72519ab7e925186
JEFF <jeff.yeh@sportscity.com.tw>
2019-12-18 16:24:51 +0800

    ISP20191878 Location 欄位CFAReceiveDate有值則顯示CFA

--- Sci.Production.Logistic/R02.cs
+++ Sci.Production.Logistic/R02.cs
@@ -137,7 +137,7 @@ p.MDivisionID
 ,pd.CTNStartNo
 ,pd.ReceiveDate
 ,o.CustPONo
-,ClogLocationId=iif(pd.CFAReceiveDate is not null,format(pd.CFAReceiveDate,'yyyy/MM/dd'),pd.ClogLocationId)
+,ClogLocationId=iif(pd.CFAReceiveDate is not null,'CFA',pd.ClogLocationId)
 ,p.BrandID
 ,Cancelled = iif(o.junk=1,'Y','N')
 ,[TTLQty] = TTL.Qty
@@ -199,7 +199,7 @@ p.MDivisionID
 ,pd.CTNStartNo
 ,pd.ReceiveDate
 ,o.CustPONo
-,ClogLocationId=iif(pd.CFAReceiveDate is not null,format(pd.CFAReceiveDate,'yyyy/MM/dd'),pd.ClogLocationId)
+,ClogLocationId=iif(pd.CFAReceiveDate is not null,'CFA',pd.ClogLocationId)
 ,p.BrandID
 ,Cancelled = iif(o.junk=1,'Y','N')
 ,[TTLQty] = TTL.Qty
