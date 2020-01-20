68ab8279cd4edc38a8478537d72bf1747295d9b3
JEFF <jeff.yeh@sportscity.com.tw>
2020-01-20 11:02:29 +0800

    ISP20200105 移除一段SQL

--- Sci.Production.Subcon/R26.cs
+++ Sci.Production.Subcon/R26.cs
@@ -175,15 +175,15 @@ namespace Sci.Production.Subcon
                 {
                     if (rdbtn_payment.Checked)
                     {
-                        sqlWhere = @" where a.Status != 'Closed' AND b.qty > b.APQty and " + sqlWhere;
+                        sqlWhere = @" where b.qty > b.APQty and " + sqlWhere;
                     }
                     else if (rdbtn_incoming.Checked)
                     {
-                        sqlWhere = @" where a.Status != 'Closed' AND b.qty > b.InQty and " + sqlWhere;
+                        sqlWhere = @" where b.qty > b.InQty and " + sqlWhere;
                     }
                     else if (rdbtn_PandI.Checked)
                     {
-                        sqlWhere = @" where a.Status != 'Closed' AND (b.qty > b.APQty or b.qty > b.InQty)and " + sqlWhere;
+                        sqlWhere = @" where (b.qty > b.APQty or b.qty > b.InQty)and " + sqlWhere;
                     }
                     else
                     {
@@ -194,17 +194,17 @@ namespace Sci.Production.Subcon
                 {
                     if (rdbtn_payment.Checked)
                     {
-                        sqlWhere = @" where a.Status != 'Closed' and " + sqlWhere;
+                        sqlWhere = @" where " + sqlWhere;
                         sqlByPoOrder = "having  sum(b.Qty) >  sum(b.APQty)";
                     }
                     else if (rdbtn_incoming.Checked)
                     {
-                        sqlWhere = @" where a.Status != 'Closed' and " + sqlWhere;
+                        sqlWhere = @" where " + sqlWhere;
                         sqlByPoOrder = "having  sum(b.Qty) >  sum(b.InQty)";
                     }
                     else if (rdbtn_PandI.Checked)
                     {
-                        sqlWhere = @" where a.Status != 'Closed' and " + sqlWhere;
+                        sqlWhere = @" where " + sqlWhere;
                         sqlByPoOrder = "having  sum(b.Qty) >  sum(b.APQty) or sum(b.Qty) >  sum(b.InQty) ";
                     }
                     else
