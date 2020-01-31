0692d0b4f379b7d4bd3f5302b59f9d3e59497ae2
JEFF <jeff.yeh@sportscity.com.tw>
2020-01-31 17:46:14 +0800

    ISP20200136 Shipping R09 1. 新增篩選 Delay Replacement 勾選時只篩選出包含 Delay Replacement 的資料 	並且勾選時 FtyWK 的資料全部排除 	新增欄位 : Delay/Repacement 	屬於 Delay 顯示 'Delay' 	屬於 Replacement 顯示 'Replacement' 	同時屬於 Delay 與 Replacement 顯示 'Delay Replacement' 2. 修正 FtyExport Join ShareExpense 的欄位 	Import 的資料會寫入 ShareExpense.WKNo

--- Sci.Production.Shipping/R09.Designer.cs
+++ Sci.Production.Shipping/R09.Designer.cs
@@ -47,66 +47,79 @@
             this.dateVoucherDate = new Sci.Win.UI.DateRange();
             this.lbRateType = new Sci.Win.UI.Label();
             this.comboRateType = new Sci.Win.UI.ComboBox();
+            this.chkReplacement = new Sci.Win.UI.CheckBox();
             this.radioPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // print
             // 
-            this.print.Location = new System.Drawing.Point(452, 12);
+            this.print.Location = new System.Drawing.Point(542, 12);
             // 
             // toexcel
             // 
-            this.toexcel.Location = new System.Drawing.Point(452, 48);
+            this.toexcel.Location = new System.Drawing.Point(542, 48);
             // 
             // close
             // 
-            this.close.Location = new System.Drawing.Point(452, 84);
+            this.close.Location = new System.Drawing.Point(542, 84);
+            // 
+            // buttonCustomized
+            // 
+            this.buttonCustomized.Location = new System.Drawing.Point(496, 120);
+            // 
+            // checkUseCustomized
+            // 
+            this.checkUseCustomized.Location = new System.Drawing.Point(522, 156);
+            // 
+            // txtVersion
+            // 
+            this.txtVersion.Location = new System.Drawing.Point(522, 183);
             // 
             // labelArrivePortDate
             // 
-            this.labelArrivePortDate.Location = new System.Drawing.Point(24, 12);
+            this.labelArrivePortDate.Location = new System.Drawing.Point(29, 12);
             this.labelArrivePortDate.Name = "labelArrivePortDate";
-            this.labelArrivePortDate.Size = new System.Drawing.Size(101, 23);
+            this.labelArrivePortDate.Size = new System.Drawing.Size(128, 23);
             this.labelArrivePortDate.TabIndex = 94;
             this.labelArrivePortDate.Text = "Arrive Port Date";
             // 
             // labelDoxRcvdDate
             // 
-            this.labelDoxRcvdDate.Location = new System.Drawing.Point(25, 38);
+            this.labelDoxRcvdDate.Location = new System.Drawing.Point(30, 38);
             this.labelDoxRcvdDate.Name = "labelDoxRcvdDate";
-            this.labelDoxRcvdDate.Size = new System.Drawing.Size(101, 23);
+            this.labelDoxRcvdDate.Size = new System.Drawing.Size(128, 23);
             this.labelDoxRcvdDate.TabIndex = 95;
             this.labelDoxRcvdDate.Text = "Dox Rcvd Date";
             // 
             // labelAPApvDate
             // 
-            this.labelAPApvDate.Location = new System.Drawing.Point(25, 63);
+            this.labelAPApvDate.Location = new System.Drawing.Point(30, 63);
             this.labelAPApvDate.Name = "labelAPApvDate";
-            this.labelAPApvDate.Size = new System.Drawing.Size(101, 23);
+            this.labelAPApvDate.Size = new System.Drawing.Size(128, 23);
             this.labelAPApvDate.TabIndex = 96;
             this.labelAPApvDate.Text = "AP Apv. Date";
             // 
             // labelShippingMode
             // 
-            this.labelShippingMode.Location = new System.Drawing.Point(24, 113);
+            this.labelShippingMode.Location = new System.Drawing.Point(29, 113);
             this.labelShippingMode.Name = "labelShippingMode";
-            this.labelShippingMode.Size = new System.Drawing.Size(101, 23);
+            this.labelShippingMode.Size = new System.Drawing.Size(128, 23);
             this.labelShippingMode.TabIndex = 97;
             this.labelShippingMode.Text = "Shipping Mode";
             // 
             // labelForwarder
             // 
-            this.labelForwarder.Location = new System.Drawing.Point(24, 138);
+            this.labelForwarder.Location = new System.Drawing.Point(29, 138);
             this.labelForwarder.Name = "labelForwarder";
-            this.labelForwarder.Size = new System.Drawing.Size(101, 23);
+            this.labelForwarder.Size = new System.Drawing.Size(128, 23);
             this.labelForwarder.TabIndex = 98;
             this.labelForwarder.Text = "Forwarder";
             // 
             // labelReportType
             // 
-            this.labelReportType.Location = new System.Drawing.Point(24, 188);
+            this.labelReportType.Location = new System.Drawing.Point(30, 191);
             this.labelReportType.Name = "labelReportType";
-            this.labelReportType.Size = new System.Drawing.Size(101, 23);
+            this.labelReportType.Size = new System.Drawing.Size(128, 23);
             this.labelReportType.TabIndex = 99;
             this.labelReportType.Text = "Report Type";
             // 
@@ -127,7 +140,7 @@
             this.dateArrivePortDate.DateBox2.Size = new System.Drawing.Size(129, 23);
             this.dateArrivePortDate.DateBox2.TabIndex = 1;
             this.dateArrivePortDate.IsRequired = false;
-            this.dateArrivePortDate.Location = new System.Drawing.Point(129, 12);
+            this.dateArrivePortDate.Location = new System.Drawing.Point(160, 12);
             this.dateArrivePortDate.Name = "dateArrivePortDate";
             this.dateArrivePortDate.Size = new System.Drawing.Size(280, 23);
             this.dateArrivePortDate.TabIndex = 0;
@@ -149,7 +162,7 @@
             this.dateDoxRcvdDate.DateBox2.Size = new System.Drawing.Size(129, 23);
             this.dateDoxRcvdDate.DateBox2.TabIndex = 1;
             this.dateDoxRcvdDate.IsRequired = false;
-            this.dateDoxRcvdDate.Location = new System.Drawing.Point(129, 37);
+            this.dateDoxRcvdDate.Location = new System.Drawing.Point(160, 37);
             this.dateDoxRcvdDate.Name = "dateDoxRcvdDate";
             this.dateDoxRcvdDate.Size = new System.Drawing.Size(280, 23);
             this.dateDoxRcvdDate.TabIndex = 1;
@@ -171,7 +184,7 @@
             this.dateAPApvDate.DateBox2.Size = new System.Drawing.Size(129, 23);
             this.dateAPApvDate.DateBox2.TabIndex = 1;
             this.dateAPApvDate.IsRequired = false;
-            this.dateAPApvDate.Location = new System.Drawing.Point(129, 66);
+            this.dateAPApvDate.Location = new System.Drawing.Point(160, 66);
             this.dateAPApvDate.Name = "dateAPApvDate";
             this.dateAPApvDate.Size = new System.Drawing.Size(280, 23);
             this.dateAPApvDate.TabIndex = 2;
@@ -180,7 +193,7 @@
             // 
             this.radioPanel1.Controls.Add(this.radioListByWKNoByFeeType);
             this.radioPanel1.Controls.Add(this.radioListbyWKNo);
-            this.radioPanel1.Location = new System.Drawing.Point(129, 188);
+            this.radioPanel1.Location = new System.Drawing.Point(161, 191);
             this.radioPanel1.Name = "radioPanel1";
             this.radioPanel1.Size = new System.Drawing.Size(200, 59);
             this.radioPanel1.TabIndex = 7;
@@ -193,13 +206,13 @@
             this.radioListByWKNoByFeeType.Name = "radioListByWKNoByFeeType";
             this.radioListByWKNoByFeeType.Size = new System.Drawing.Size(184, 21);
             this.radioListByWKNoByFeeType.TabIndex = 1;
-            this.radioListByWKNoByFeeType.TabStop = true;
             this.radioListByWKNoByFeeType.Text = "List by WK# by Fee Type";
             this.radioListByWKNoByFeeType.UseVisualStyleBackColor = true;
             // 
             // radioListbyWKNo
             // 
             this.radioListbyWKNo.AutoSize = true;
+            this.radioListbyWKNo.Checked = true;
             this.radioListbyWKNo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.radioListbyWKNo.Location = new System.Drawing.Point(3, 3);
             this.radioListbyWKNo.Name = "radioListbyWKNo";
@@ -215,7 +228,7 @@
             this.txtshipmode.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.txtshipmode.FormattingEnabled = true;
             this.txtshipmode.IsSupportUnselect = true;
-            this.txtshipmode.Location = new System.Drawing.Point(129, 113);
+            this.txtshipmode.Location = new System.Drawing.Point(160, 113);
             this.txtshipmode.Name = "txtshipmode";
             this.txtshipmode.OldText = "";
             this.txtshipmode.Size = new System.Drawing.Size(80, 24);
@@ -226,7 +239,7 @@
             // 
             this.txtForwarder.BackColor = System.Drawing.Color.White;
             this.txtForwarder.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.txtForwarder.Location = new System.Drawing.Point(129, 138);
+            this.txtForwarder.Location = new System.Drawing.Point(160, 138);
             this.txtForwarder.Name = "txtForwarder";
             this.txtForwarder.Size = new System.Drawing.Size(61, 23);
             this.txtForwarder.TabIndex = 5;
@@ -237,16 +250,16 @@
             // 
             this.displayBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
             this.displayBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
-            this.displayBox1.Location = new System.Drawing.Point(188, 138);
+            this.displayBox1.Location = new System.Drawing.Point(219, 138);
             this.displayBox1.Name = "displayBox1";
             this.displayBox1.Size = new System.Drawing.Size(100, 23);
             this.displayBox1.TabIndex = 108;
             // 
             // lbVoucherDate
             // 
-            this.lbVoucherDate.Location = new System.Drawing.Point(24, 88);
+            this.lbVoucherDate.Location = new System.Drawing.Point(29, 88);
             this.lbVoucherDate.Name = "lbVoucherDate";
-            this.lbVoucherDate.Size = new System.Drawing.Size(101, 23);
+            this.lbVoucherDate.Size = new System.Drawing.Size(128, 23);
             this.lbVoucherDate.TabIndex = 109;
             this.lbVoucherDate.Text = "Voucher Date";
             // 
@@ -267,16 +280,16 @@
             this.dateVoucherDate.DateBox2.Size = new System.Drawing.Size(129, 23);
             this.dateVoucherDate.DateBox2.TabIndex = 1;
             this.dateVoucherDate.IsRequired = false;
-            this.dateVoucherDate.Location = new System.Drawing.Point(129, 88);
+            this.dateVoucherDate.Location = new System.Drawing.Point(160, 88);
             this.dateVoucherDate.Name = "dateVoucherDate";
             this.dateVoucherDate.Size = new System.Drawing.Size(280, 23);
             this.dateVoucherDate.TabIndex = 3;
             // 
             // lbRateType
             // 
-            this.lbRateType.Location = new System.Drawing.Point(24, 163);
+            this.lbRateType.Location = new System.Drawing.Point(29, 163);
             this.lbRateType.Name = "lbRateType";
-            this.lbRateType.Size = new System.Drawing.Size(101, 23);
+            this.lbRateType.Size = new System.Drawing.Size(128, 23);
             this.lbRateType.TabIndex = 111;
             this.lbRateType.Text = "Rate Type";
             // 
@@ -286,15 +299,27 @@
             this.comboRateType.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
             this.comboRateType.FormattingEnabled = true;
             this.comboRateType.IsSupportUnselect = true;
-            this.comboRateType.Location = new System.Drawing.Point(129, 162);
+            this.comboRateType.Location = new System.Drawing.Point(160, 162);
             this.comboRateType.Name = "comboRateType";
             this.comboRateType.OldText = "";
             this.comboRateType.Size = new System.Drawing.Size(159, 24);
             this.comboRateType.TabIndex = 6;
             // 
+            // chkReplacement
+            // 
+            this.chkReplacement.AutoSize = true;
+            this.chkReplacement.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.chkReplacement.Location = new System.Drawing.Point(30, 256);
+            this.chkReplacement.Name = "chkReplacement";
+            this.chkReplacement.Size = new System.Drawing.Size(261, 21);
+            this.chkReplacement.TabIndex = 8;
+            this.chkReplacement.Text = "Only show Delay / Replacement WK#";
+            this.chkReplacement.UseVisualStyleBackColor = true;
+            // 
             // R09
             // 
-            this.ClientSize = new System.Drawing.Size(544, 280);
+            this.ClientSize = new System.Drawing.Size(634, 333);
+            this.Controls.Add(this.chkReplacement);
             this.Controls.Add(this.comboRateType);
             this.Controls.Add(this.lbRateType);
             this.Controls.Add(this.dateVoucherDate);
@@ -339,6 +364,7 @@
             this.Controls.SetChildIndex(this.dateVoucherDate, 0);
             this.Controls.SetChildIndex(this.lbRateType, 0);
             this.Controls.SetChildIndex(this.comboRateType, 0);
+            this.Controls.SetChildIndex(this.chkReplacement, 0);
             this.radioPanel1.ResumeLayout(false);
             this.radioPanel1.PerformLayout();
             this.ResumeLayout(false);
@@ -367,5 +393,6 @@
         private Win.UI.DateRange dateVoucherDate;
         private Win.UI.Label lbRateType;
         private Win.UI.ComboBox comboRateType;
+        private Win.UI.CheckBox chkReplacement;
     }
 }
