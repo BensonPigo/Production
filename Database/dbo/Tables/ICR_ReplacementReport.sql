ee4c531cdba82f6359cf6ad45caeb455c7c73b9e
Yinmei <yinmei.lin@sportscity.com.tw>
2020-02-21 10:13:28 +0800

    新增ICR相關table

---
+++ Database/dbo/Tables/ICR_ReplacementReport.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[ICR_ReplacementReport] (
+    [ID]            VARCHAR (13) DEFAULT ('') NOT NULL,
+    [ReplacementNo] VARCHAR (20) DEFAULT ('') NOT NULL,
+    CONSTRAINT [PK_ICR_ReplacementReport] PRIMARY KEY CLUSTERED ([ID] ASC, [ReplacementNo] ASC)
+);
+
---
+++ Database/dbo/Tables/ICR_ResponsibilityDept.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ICR_ResponsibilityDept] (
+    [ID]           VARCHAR (13)    DEFAULT ('') NOT NULL,
+    [FactoryID]    VARCHAR (8)     DEFAULT ('') NOT NULL,
+    [DepartmentID] VARCHAR (8)     DEFAULT ('') NOT NULL,
+    [Percentage]   NUMERIC (5, 2)  DEFAULT ((0)) NOT NULL,
+    [Amount]       NUMERIC (11, 2) DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_ICR_ResponsibilityDept] PRIMARY KEY CLUSTERED ([ID] ASC, [FactoryID] ASC, [DepartmentID] ASC)
+);
+
