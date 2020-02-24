d8aff152fea77b24dd85503ef252d371898e1637
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2020-02-24 17:42:46 +0800

    ISP20200253 - Measurement Translate資料交換

--- DailyTransfer/imp_MESbase.sql
+++ DailyTransfer/imp_MESbase.sql
@@ -54,4 +54,49 @@ BEGIN
 
 	
 	DROP TABLE #Trade_DQSReason
+
+
+	----------MeasurementTranslate----------
+	
+	SELECT *
+	INTO #Trade_MeasurementTranslate
+	FROM [Mainserver].Trade_To_Pms.dbo.MeasurementTranslate
+	;	
+	INSERT INTO ManufacturingExecution.dbo.MeasurementTranslate 
+			   (Ukey
+			   ,DescEN
+			   ,DescCHS
+			   ,DescVN
+			   ,DescKH
+			   ,[AddName]
+			   ,[AddDate]
+			   ,[EditName]
+			   ,[EditDate])
+	SELECT 
+				 s.Ukey 
+				,s.DescEN
+				,s.DescCHS
+				,s.DescVN
+				,s.DescKH
+				,s.AddName
+				,s.AddDate
+				,s.EditName
+				,s.EditDate
+	FROM #Trade_MeasurementTranslate s WITH (NOLOCK) 
+	WHERE NOT EXISTS(SELECT 1 FROM ManufacturingExecution.dbo.MeasurementTranslate t WHERE t.Ukey=s.Ukey)
+	;
+	UPDATE t
+	SET   t.Ukey = s.Ukey
+		, t.DescEN = s.DescEN
+		, t.DescCHS = s.DescCHS
+		, t.DescVN = s.DescVN
+		, t.DescKH = s.DescKH
+		, t.AddName = s.AddName
+		, t.AddDate = s.AddDate
+		, t.EditName = s.EditName
+		, t.EditDate = s.EditDate
+	FROM ManufacturingExecution.dbo.MeasurementTranslate t
+	INNER JOIN #Trade_MeasurementTranslate s ON t.Ukey=s.Ukey
+	;
+	DROP TABLE #Trade_MeasurementTranslate
 END
