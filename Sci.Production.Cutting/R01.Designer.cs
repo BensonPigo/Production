9ae4dd837ca8cd939d565a6dac512b7915f57ddd
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2020-03-17 17:27:30 +0800

    ISP20200394 - 加入R01

---
+++ Sci.Production.Cutting/R01.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Sci.Production.Cutting
+{
+    partial class R01
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // buttonCustomized
+            // 
+            this.buttonCustomized.Location = new System.Drawing.Point(515, 198);
+            this.buttonCustomized.Size = new System.Drawing.Size(100, 30);
+            // 
+            // checkUseCustomized
+            // 
+            this.checkUseCustomized.Location = new System.Drawing.Point(515, 171);
+            // 
+            // txtVersion
+            // 
+            this.txtVersion.Location = new System.Drawing.Point(515, 156);
+            // 
+            // R01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "R01";
+            this.OnLineHelpID = "Sci.Win.Tems.PrintForm";
+            this.Text = "R01. Planned Cutting WIP Report";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
---
+++ Sci.Production.Cutting/R01.cs
@@ -0,0 +1,24 @@
+﻿using Ict;
+using Sci.Data;
+using Sci.Win.Tools;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using System.Linq;
+using System.IO;
+
+namespace Sci.Production.Cutting
+{
+    public partial class R01 : Sci.Win.Tems.PrintForm
+    {
+        public R01()
+        {
+            InitializeComponent();
+        }
+    }
+}
