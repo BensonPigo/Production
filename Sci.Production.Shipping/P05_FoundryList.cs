6c129f861f6cbb364b7759c4aa4e886cf94e707e
Willy <willy.wei@sprotscity.com.tw>
2020-04-09 17:37:02 +0800

    ISP20200553 調整Foundry 開窗欄位顯示

--- Sci.Production.Shipping/P05_FoundryList.cs
+++ Sci.Production.Shipping/P05_FoundryList.cs
@@ -30,8 +30,8 @@ namespace Sci.Production.Shipping
             this.grid.DataSource = this.listControlBindingSource1;
             this.Helper.Controls.Grid.Generator(this.grid)
             .Text("FactoryGroup", header: "Fty. Group", width: Widths.AnsiChars(8), iseditingreadonly: true)
-            .Numeric("CBM", header: "CBM", width: Widths.AnsiChars(11), decimal_places: 3, iseditingreadonly: true)
-            .Numeric("Ratio", header: "Ratio", decimal_places: 0, width: Widths.AnsiChars(10), iseditingreadonly: true)
+            .Numeric("CBM", header: "CBM / GW", width: Widths.AnsiChars(11), decimal_places: 3, iseditingreadonly: true)
+            .Numeric("Ratio", header: "Ratio (%)", decimal_places: 0, width: Widths.AnsiChars(10), iseditingreadonly: true)
             ;
 
             this.Query();
