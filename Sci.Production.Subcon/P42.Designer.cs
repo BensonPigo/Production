05afff3775861a152ae5a015772be387da84014f
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2020-04-23 11:31:09 +0800

    ISP20200671 - Subcon P42 調整控制項

--- Sci.Production.Subcon/P42.Designer.cs
+++ Sci.Production.Subcon/P42.Designer.cs
@@ -31,27 +31,20 @@
             this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.listControlBindingSource1 = new Sci.Win.UI.ListControlBindingSource(this.components);
             this.btnQuery = new Sci.Win.UI.Button();
             this.dateInline = new Sci.Win.UI.DateRange();
             this.label1 = new Sci.Win.UI.Label();
             this.txtSp2 = new Sci.Win.UI.TextBox();
             this.txtSp1 = new Sci.Win.UI.TextBox();
-            this.labInlineDate = new Sci.Win.UI.Label();
-            this.labSpno = new Sci.Win.UI.Label();
             this.grid1 = new Sci.Win.UI.Grid();
             this.txtPO = new Sci.Win.UI.TextBox();
-            this.label2 = new Sci.Win.UI.Label();
             this.chkBulk = new Sci.Win.UI.CheckBox();
             this.chkSample = new Sci.Win.UI.CheckBox();
             this.label3 = new Sci.Win.UI.Label();
             this.label4 = new Sci.Win.UI.Label();
             this.label5 = new Sci.Win.UI.Label();
-            this.label6 = new Sci.Win.UI.Label();
             this.dateSCIDelivery = new Sci.Win.UI.DateRange();
-            this.label7 = new Sci.Win.UI.Label();
             this.dateBuyerDelivery = new Sci.Win.UI.DateRange();
             this.label8 = new Sci.Win.UI.Label();
             this.cmbSummaryBy = new Sci.Win.UI.ComboBox();
@@ -59,6 +52,11 @@
             this.txtMdivision1 = new Sci.Production.Class.txtMdivision();
             this.listControlBindingSource2 = new Sci.Win.UI.ListControlBindingSource(this.components);
             this.grid2 = new Sci.Win.UI.Grid();
+            this.label9 = new Sci.Win.UI.Label();
+            this.label10 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.label11 = new Sci.Win.UI.Label();
+            this.label12 = new Sci.Win.UI.Label();
             ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.grid1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource2)).BeginInit();
@@ -128,22 +126,6 @@
             this.txtSp1.Size = new System.Drawing.Size(127, 23);
             this.txtSp1.TabIndex = 0;
             // 
-            // labInlineDate
-            // 
-            this.labInlineDate.Location = new System.Drawing.Point(513, 38);
-            this.labInlineDate.Name = "labInlineDate";
-            this.labInlineDate.Size = new System.Drawing.Size(97, 23);
-            this.labInlineDate.TabIndex = 22;
-            this.labInlineDate.Text = "Inline Date";
-            // 
-            // labSpno
-            // 
-            this.labSpno.Location = new System.Drawing.Point(5, 9);
-            this.labSpno.Name = "labSpno";
-            this.labSpno.Size = new System.Drawing.Size(75, 23);
-            this.labSpno.TabIndex = 21;
-            this.labSpno.Text = "SP#";
-            // 
             // grid1
             // 
             this.grid1.AllowUserToAddRows = false;
@@ -188,14 +170,6 @@
             this.txtPO.PopUpMode = Sci.Win.UI.TextBoxPopUpMode.Any;
             this.txtPO.Size = new System.Drawing.Size(127, 23);
             this.txtPO.TabIndex = 2;
-            // 
-            // label2
-            // 
-            this.label2.Location = new System.Drawing.Point(5, 38);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 23);
-            this.label2.TabIndex = 44;
-            this.label2.Text = "P.O.";
             // 
             // chkBulk
             // 
@@ -245,14 +219,6 @@
             this.label5.TabIndex = 50;
             this.label5.Text = "Factory";
             // 
-            // label6
-            // 
-            this.label6.Location = new System.Drawing.Point(513, 9);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(97, 23);
-            this.label6.TabIndex = 52;
-            this.label6.Text = "SCI Delivery";
-            // 
             // dateSCIDelivery
             // 
             // 
@@ -274,14 +240,6 @@
             this.dateSCIDelivery.Name = "dateSCIDelivery";
             this.dateSCIDelivery.Size = new System.Drawing.Size(280, 23);
             this.dateSCIDelivery.TabIndex = 7;
-            // 
-            // label7
-            // 
-            this.label7.Location = new System.Drawing.Point(513, 66);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(97, 23);
-            this.label7.TabIndex = 54;
-            this.label7.Text = "Buyer Delivery";
             // 
             // dateBuyerDelivery
             // 
@@ -360,6 +318,14 @@
             this.grid2.AllowUserToResizeRows = false;
             this.grid2.BackgroundColor = System.Drawing.SystemColors.Control;
             this.grid2.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.grid2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid2.DataSource = this.listControlBindingSource2;
+            this.grid2.EditingEnter = Ict.Win.UI.DataGridViewEditingEnter.NextCellOrNextRow;
+            this.grid2.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.grid2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.grid2.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(162)))), ((int)(((byte)(163)))));
+            this.grid2.Location = new System.Drawing.Point(945, 85);
+            this.grid2.Name = "grid2";
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
             dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
@@ -367,31 +333,7 @@
             dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.grid2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
-            this.grid2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.grid2.DataSource = this.listControlBindingSource2;
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.grid2.DefaultCellStyle = dataGridViewCellStyle3;
-            this.grid2.EditingEnter = Ict.Win.UI.DataGridViewEditingEnter.NextCellOrNextRow;
-            this.grid2.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
-            this.grid2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.grid2.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(162)))), ((int)(((byte)(163)))));
-            this.grid2.Location = new System.Drawing.Point(945, 85);
-            this.grid2.Name = "grid2";
-            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.grid2.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.grid2.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.grid2.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
             this.grid2.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
             this.grid2.RowTemplate.Height = 24;
@@ -402,16 +344,69 @@
             this.grid2.Visible = false;
             this.grid2.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.grid2_CellFormatting);
             // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.label9.Location = new System.Drawing.Point(5, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(75, 23);
+            this.label9.TabIndex = 101;
+            this.label9.Text = "SP#";
+            this.label9.TextStyle.Color = System.Drawing.Color.Black;
+            // 
+            // label10
+            // 
+            this.label10.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.label10.Location = new System.Drawing.Point(5, 38);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(75, 23);
+            this.label10.TabIndex = 102;
+            this.label10.Text = "P.O.";
+            this.label10.TextStyle.Color = System.Drawing.Color.Black;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.label2.Location = new System.Drawing.Point(513, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 23);
+            this.label2.TabIndex = 103;
+            this.label2.Text = "SCI Delivery";
+            this.label2.TextStyle.Color = System.Drawing.Color.Black;
+            // 
+            // label11
+            // 
+            this.label11.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.label11.Location = new System.Drawing.Point(513, 38);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(97, 23);
+            this.label11.TabIndex = 104;
+            this.label11.Text = "Inline Date";
+            this.label11.TextStyle.Color = System.Drawing.Color.Black;
+            // 
+            // label12
+            // 
+            this.label12.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.label12.Location = new System.Drawing.Point(513, 67);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(97, 23);
+            this.label12.TabIndex = 105;
+            this.label12.Text = "Buyer Delivery";
+            this.label12.TextStyle.Color = System.Drawing.Color.Black;
+            // 
             // P42
             // 
             this.ClientSize = new System.Drawing.Size(1008, 562);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
             this.Controls.Add(this.grid2);
             this.Controls.Add(this.cmbSummaryBy);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.dateBuyerDelivery);
-            this.Controls.Add(this.label7);
             this.Controls.Add(this.dateSCIDelivery);
-            this.Controls.Add(this.label6);
             this.Controls.Add(this.txtfactory1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.txtMdivision1);
@@ -419,7 +414,6 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.chkSample);
             this.Controls.Add(this.chkBulk);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.txtPO);
             this.Controls.Add(this.grid1);
             this.Controls.Add(this.btnQuery);
@@ -427,13 +421,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtSp2);
             this.Controls.Add(this.txtSp1);
-            this.Controls.Add(this.labInlineDate);
-            this.Controls.Add(this.labSpno);
             this.Name = "P42";
             this.OnLineHelpID = "Sci.Win.Tems.QueryForm";
             this.Text = "P42. SubProcess Overview";
-            this.Controls.SetChildIndex(this.labSpno, 0);
-            this.Controls.SetChildIndex(this.labInlineDate, 0);
             this.Controls.SetChildIndex(this.txtSp1, 0);
             this.Controls.SetChildIndex(this.txtSp2, 0);
             this.Controls.SetChildIndex(this.label1, 0);
@@ -441,7 +431,6 @@
             this.Controls.SetChildIndex(this.btnQuery, 0);
             this.Controls.SetChildIndex(this.grid1, 0);
             this.Controls.SetChildIndex(this.txtPO, 0);
-            this.Controls.SetChildIndex(this.label2, 0);
             this.Controls.SetChildIndex(this.chkBulk, 0);
             this.Controls.SetChildIndex(this.chkSample, 0);
             this.Controls.SetChildIndex(this.label3, 0);
@@ -449,13 +438,16 @@
             this.Controls.SetChildIndex(this.txtMdivision1, 0);
             this.Controls.SetChildIndex(this.label5, 0);
             this.Controls.SetChildIndex(this.txtfactory1, 0);
-            this.Controls.SetChildIndex(this.label6, 0);
             this.Controls.SetChildIndex(this.dateSCIDelivery, 0);
-            this.Controls.SetChildIndex(this.label7, 0);
             this.Controls.SetChildIndex(this.dateBuyerDelivery, 0);
             this.Controls.SetChildIndex(this.label8, 0);
             this.Controls.SetChildIndex(this.cmbSummaryBy, 0);
             this.Controls.SetChildIndex(this.grid2, 0);
+            this.Controls.SetChildIndex(this.label9, 0);
+            this.Controls.SetChildIndex(this.label10, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label11, 0);
+            this.Controls.SetChildIndex(this.label12, 0);
             ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.grid1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.listControlBindingSource2)).EndInit();
@@ -472,11 +464,8 @@
         private Win.UI.Label label1;
         private Win.UI.TextBox txtSp2;
         private Win.UI.TextBox txtSp1;
-        private Win.UI.Label labInlineDate;
-        private Win.UI.Label labSpno;
         private Win.UI.Grid grid1;
         private Win.UI.TextBox txtPO;
-        private Win.UI.Label label2;
         private Win.UI.CheckBox chkBulk;
         private Win.UI.CheckBox chkSample;
         private Win.UI.Label label3;
@@ -484,13 +473,16 @@
         private Class.txtMdivision txtMdivision1;
         private Win.UI.Label label5;
         private Class.txtfactory txtfactory1;
-        private Win.UI.Label label6;
         private Win.UI.DateRange dateSCIDelivery;
-        private Win.UI.Label label7;
         private Win.UI.DateRange dateBuyerDelivery;
         private Win.UI.Label label8;
         private Win.UI.ComboBox cmbSummaryBy;
         private Win.UI.ListControlBindingSource listControlBindingSource2;
         private Win.UI.Grid grid2;
+        private Win.UI.Label label9;
+        private Win.UI.Label label10;
+        private Win.UI.Label label2;
+        private Win.UI.Label label11;
+        private Win.UI.Label label12;
     }
 }
