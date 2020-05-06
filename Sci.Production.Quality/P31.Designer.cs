bb62aab37ae260a8527b3d91ab8336eb8cc6b258
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2020-05-06 13:35:03 +0800

    ISP20200715 - 加入P31

---
+++ Sci.Production.Quality/P31.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Sci.Production.Quality
+{
+    partial class P31
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ((System.ComponentModel.ISupportInitialize)(this.detailgridbs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.detailgrid2bs)).BeginInit();
+            this.masterpanel.SuspendLayout();
+            this.detailpanel.SuspendLayout();
+            this.detail2.SuspendLayout();
+            this.detailpanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).BeginInit();
+            this.detail.SuspendLayout();
+            this.detailcont.SuspendLayout();
+            this.detailbtm.SuspendLayout();
+            this.tabs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // masterpanel
+            // 
+            this.masterpanel.Size = new System.Drawing.Size(792, 100);
+            // 
+            // detailpanel
+            // 
+            this.detailpanel.Size = new System.Drawing.Size(792, 250);
+            // 
+            // detailgridcont
+            // 
+            this.detailgridcont.Size = new System.Drawing.Size(792, 250);
+            // 
+            // detail2
+            // 
+            this.detail2.Size = new System.Drawing.Size(792, 388);
+            // 
+            // detailgridcont2
+            // 
+            this.detailgridcont2.Size = new System.Drawing.Size(786, 342);
+            // 
+            // detailpanel2
+            // 
+            this.detailpanel2.Size = new System.Drawing.Size(786, 40);
+            // 
+            // detail
+            // 
+            this.detail.Size = new System.Drawing.Size(792, 388);
+            // 
+            // detailcont
+            // 
+            this.detailcont.Size = new System.Drawing.Size(792, 350);
+            // 
+            // detailbtm
+            // 
+            this.detailbtm.Location = new System.Drawing.Point(0, 350);
+            this.detailbtm.Size = new System.Drawing.Size(792, 38);
+            // 
+            // browse
+            // 
+            this.browse.Size = new System.Drawing.Size(792, 388);
+            // 
+            // tabs
+            // 
+            this.tabs.Size = new System.Drawing.Size(800, 417);
+            // 
+            // P31
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "P31";
+            this.OnLineHelpID = "Sci.Win.Tems.Input6";
+            this.Text = "P31";
+            this.Controls.SetChildIndex(this.tabs, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.detailgridbs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.detailgrid2bs)).EndInit();
+            this.masterpanel.ResumeLayout(false);
+            this.detailpanel.ResumeLayout(false);
+            this.detail2.ResumeLayout(false);
+            this.detailpanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).EndInit();
+            this.detail.ResumeLayout(false);
+            this.detailcont.ResumeLayout(false);
+            this.detailbtm.ResumeLayout(false);
+            this.detailbtm.PerformLayout();
+            this.tabs.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
---
+++ Sci.Production.Quality/P31.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sci.Production.Quality
+{
+    public partial class P31 : Sci.Win.Tems.Input6
+    {
+        public P31(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            this.InitializeComponent();
+        }
+    }
+}
