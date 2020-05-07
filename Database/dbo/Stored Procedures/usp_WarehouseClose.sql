30443a82f042f23e1a389b677d3a1ca8778e3a3d
JEFF <jeff.yeh@sportscity.com.tw>
2020-05-07 16:15:57 +0800

    ISP20200780 準備SubTransfer_Detail的[ToLocation], 使用Getlocation會有多筆可能,取得ID之後用[SplitString], 條件再用IN 變更MDivisionPoDetail在FtyInventory_detail之後 加上MDivisionPoDetail.CLocation更新

--- Database/dbo/Stored Procedures/usp_WarehouseClose.sql
+++ Database/dbo/Stored Procedures/usp_WarehouseClose.sql
@@ -85,70 +85,54 @@ BEGIN
 		END
 
 		select @OrdersFactoryID=FactoryID from Orders where ID=@poid
+		
+		--寫入 [SubTransfer_Detail]
+		SELECT
+			[ID]=[POID]
+			,[FromFtyInventoryUkey]=Ukey
+			,[FromMDivisionID]=''
+			,[FromFactoryID]=@OrdersFactoryID  
+			,[FromPOID]=[POID]
+			,[FromSeq1]=[Seq1]
+			,[FromSeq2]=[Seq2]
+			,[FromRoll]=[Roll]
+			,[FromStockType]='B'
+			,[FromDyelot]=[Dyelot]
+			,[ToMDivisionID]=''
+			,[ToFactoryID]=@OrdersFactoryID
+			,[ToPOID]=[POID]
+			,[ToSeq1]=[Seq1]
+			,[ToSeq2]=[Seq2]
+			,[ToRoll]=[Roll]
+			,[ToStockType]='O'
+			,[ToDyelot]=[Dyelot]
+			,[Qty]=ISNULL(InQty,0.0) - ISNULL(OutQty,0.0) + ISNULL(AdjustQty,0.0)
+			,[ToLocation]=isnull(
+			(
+				SELECT TOP 1 ID 
+				FROM MtlLocation WITH (NOLOCK)
+				WHERE ID in (select data from [dbo].[SplitString](dbo.Getlocation(sd.Ukey),','))
+				AND StockType='O' AND Junk=0
+			),'')
+		into #insert_SubTransfer_Detail
+		FROM DBO.FtyInventory sd WITH (NOLOCK)
+		WHERE sd.poid = @poid
+		and stocktype='B'
+		and ISNULL(InQty,0.0) - ISNULL(OutQty,0.0) + ISNULL(AdjustQty,0.0) > 0 
+		and lock=0
 
 		INSERT INTO [dbo].[SubTransfer_Detail]
-           ([ID]				,[FromFtyInventoryUkey]           ,[FromMDivisionID]           ,[FromFactoryID]
-		   ,[FromPOID]
-           ,[FromSeq1]           ,[FromSeq2]						,[FromRoll]					,[FromStockType]
-           ,[FromDyelot]           
-		   ,[ToMDivisionID]    ,[ToFactoryID]						,[ToPOID]					,[ToSeq1]
-           ,[ToSeq2]           ,[ToRoll]							,[ToStockType]				,[ToDyelot]
-           ,[Qty]			   ,[ToLocation])
-		 SELECT [POID]
-			,Ukey
-			,'' [FromMDivisionID] 
-			,@OrdersFactoryID [FromFactoryID] 
-			,[POID]
-			,[Seq1]
-			,[Seq2]
-			,[Roll]
-			,'B' [FromStock]
-			,[Dyelot]
-			,'' [ToMDivisionID]
-			,@OrdersFactoryID [ToFactoryID] 
-			,[POID]
-			,[Seq1]
-			,[Seq2]
-			,[Roll]
-			,'O'	[ToStock]
-			,[Dyelot]
-			,(InQty - OutQty + AdjustQty) [Qty]
-			,[ToLocation]=( 
-							SELECT TOP 1 ID 
-							FROM MtlLocation 
-							WHERE ID = dbo.Getlocation(sd.Ukey) AND (StockType='B'  OR StockType='O' ) AND Junk=0
-							GROUP BY ID 
-							HAVING Count(StockType)=2
-						  )
-			FROM DBO.FtyInventory sd WITH (NOLOCK)
-			WHERE sd.poid = @poid and stocktype='B' and ISNULL(InQty,0.0) - ISNULL(OutQty,0.0) + ISNULL(AdjustQty,0.0) > 0 
-					and lock=0;
-
-		-- 更新庫存 MDivisionPoDetail & FtyInventory
-		Select sd.POID,sd.seq1,sd.Seq2,sum(ISNULL(InQty,0.0) - ISNULL(OutQty,0.0) + ISNULL(AdjustQty,0.0)) ScrapQty into #tmpScrap
-		FROM DBO.FtyInventory sd WITH (NOLOCK)
-		WHERE sd.POID = @poid and stocktype='B' and ISNULL(InQty,0.0) - ISNULL(OutQty,0.0) + ISNULL(AdjustQty,0.0) > 0 and lock=0
-		group by sd.POID,sd.seq1,sd.Seq2
-			
-		-- 更新 MdivisionPodetail
-		--merge dbo.MDivisionPoDetail as target 
-		--using #tmpScrap as src
-		--on src.MDivisionID = target.MDivisionID and src.poid = target.POID and src.seq1 = target.Seq1 and src.Seq2 = target.seq2
-		--when matched then
-		--update 
-		--set OutQty = isnull(OutQty,0) + isnull(src.ScrapQty, 0);
-
-		update  a
-		set a.OutQty = isnull(a.OutQty,0) + isnull(b.ScrapQty, 0)
-		   ,a.LObQty = isnull(a.LObQty,0) + isnull(b.ScrapQty, 0)
-		from dbo.MDivisionPoDetail a 
-		inner join #tmpScrap b on
-			a.poid = b.poid and
-			a.seq1 = b.seq1 and
-			a.seq2 = b.seq2;
-
-		drop table #tmpScrap;
+			([ID]             ,[FromFtyInventoryUkey]
+			,[FromMDivisionID],[FromFactoryID]
+			,[FromPOID]       ,[FromSeq1]          ,[FromSeq2]    ,[FromRoll]    ,[FromStockType]    ,[FromDyelot]           
+			,[ToMDivisionID]  ,[ToFactoryID]						
+			,[ToPOID]         ,[ToSeq1]            ,[ToSeq2]      ,[ToRoll]      ,[ToStockType]      ,[ToDyelot]
+			,[Qty]            ,[ToLocation])
+		select * 
+		from #insert_SubTransfer_Detail
 		
+		-- 更新庫存 MDivisionPoDetail & FtyInventory
+
 		-- Insert Update FtyInventory (Scrap) 要先做Scrap倉再更新Bulk！！
 		SELECT poid,seq1,seq2,roll,dyelot,qty=ISNULL(InQty,0.0) - ISNULL(OutQty,0.0) + ISNULL(AdjustQty,0.0),stocktype='O' ,sd.Ukey
 		into #tmpFtyInventory
@@ -184,21 +168,57 @@ BEGIN
 		(poid,seq1,seq2,roll,stocktype,dyelot,inqty,outqty,adjustqty,lock )
 		VALUES (s.poid,s.seq1,s.seq2,s.roll,s.stocktype,s.dyelot,s.qty,0,0,0)		
 		output inserted.Ukey,s.Ukey into @TB ;
+
 		--寫入Scrap的FtyInventory時一併寫入Location資訊進FtyInventory_Detail
 		Insert into FtyInventory_Detail(Ukey, MtlLocationID)
 		select f.Ukey,ToLocation=isnull(sd.ToLocation,'')
 		from @TB f
-		inner join dbo.SubTransfer_Detail sd 
+		inner join dbo.SubTransfer_Detail sd WITH (NOLOCK)
 		on sd.id = @poid  and  sd.FromFtyInventoryUkey = f.sourceUkey
-		where not exists(select 1 from FtyInventory_Detail fd where f.Ukey = fd.Ukey and sd.ToLocation = fd.MtlLocationID)
+		where not exists(select 1 from FtyInventory_Detail fd WITH (NOLOCK) where f.Ukey = fd.Ukey and sd.ToLocation = fd.MtlLocationID)
 		and isnull(sd.ToLocation,'') <> ''
-		drop table #tmpFtyInventory
+		and exists(select 1 from MtlLocation m WITH (NOLOCK) where m.StockType = 'O' and m.id = sd.ToLocation and m.Junk = 0) -- Location需存在MtlLocation
 
+		-- FtyInventory, FtyInventory_Detail先更新完, 再更新 MDivisionPoDetail
+		select
+			f.POID,f.seq1,f.seq2,
+			[Qty]=ISNULL(InQty,0.0) - ISNULL(OutQty,0.0) + ISNULL(AdjustQty,0.0)
+		into #afterInsertFtyInventory
+		from FtyInventory f with(nolock)
+		where f.poid = @poid and f.StockType = 'O'
+
+		select f.POID,f.seq1,f.seq2,[Qty],
+			CLocation = stuff((
+				select distinct  concat(',',fd.MtlLocationID)
+				from FtyInventory f2
+				inner join FtyInventory_Detail fd WITH (NOLOCK) on f2.Ukey = fd.Ukey
+				where f2.poid = f.poid and f2.seq1 = f.Seq1 and f2.seq2 = f.seq2
+				and f2.StockType = 'O'
+				and exists(select 1 from MtlLocation m WITH (NOLOCK) where m.StockType = 'O' and m.id = fd.MtlLocationID and m.Junk = 0)
+				for xml path('')
+			),1,1,'')
+		into #afterInsertFtyInventory_L
+		from #afterInsertFtyInventory f
+
+		select POID,seq1,seq2,ScrapQty=sum([Qty]),CLocation
+		into #tmpScrap
+		FROM #afterInsertFtyInventory_L  f
+		group by POID,seq1,seq2,CLocation
+
+		update  a set
+			a.OutQty = isnull(a.OutQty,0) + isnull(b.ScrapQty, 0)
+		   ,a.LObQty = isnull(a.LObQty,0) + isnull(b.ScrapQty, 0)
+		   ,a.CLocation = b.CLocation
+		from dbo.MDivisionPoDetail a WITH (NOLOCK)
+		inner join #tmpScrap b on a.poid = b.POID and a.seq1 = b.seq1 and a.seq2 = b.seq2;
+
+		drop table #tmpScrap
+		drop table #tmpFtyInventory
 
 		-- 更新 FtyInventory (Bulk)
 		update dbo.FtyInventory 
 		set OutQty = OutQty + sd.Qty
-		FROM dbo.FtyInventory 
+		FROM dbo.FtyInventory WITH (NOLOCK)
 		inner join dbo.SubTransfer_Detail sd 
 		on sd.id = @poid and  sd.FromFtyInventoryUkey = FtyInventory.Ukey;
 
@@ -241,12 +261,12 @@ BEGIN
            ,[ToLocation]
            ,[Qty])
 		select 
-			l.OrderID
-			,@MDivisionid
-			,l.OrderID
-			,l.Refno
-			,l.ThreadColorID
-			,l.ALocation
+			 isnull(l.OrderID,'')
+			,isnull(@MDivisionid,'')
+			,isnull(l.OrderID,'')
+			,isnull(l.Refno,'')
+			,isnull(l.ThreadColorID,'')
+			,isnull(l.ALocation,'')
 			,''
 			,l.InQty-l.OutQty+l.AdjustQty
 		from LocalInventory l where l.OrderID = @poid AND (l.InQty-l.OutQty+l.AdjustQty)!=0
