4ec88c47ae668bef3e641d9bf206e13875a4c6d6
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2020-05-18 09:53:45 +0800

    ISP20200847 - Orders 新增欄位

--- DailyTransfer/imp_order.sql
+++ DailyTransfer/imp_order.sql
@@ -538,7 +538,8 @@ BEGIN
 				t.ForecastCategory     = s.ForecastCategory,
 				t.OnSiteSample		   = s.OnSiteSample,
 				t.PulloutCmplDate	   = s.PulloutCmplDate,
-				t.NeedProduction	   = s.NeedProduction
+				t.NeedProduction	   = s.NeedProduction,
+				t.IsBuyBack	   = isnull (s.IsBuyBack, 0)
 		when not matched by target then
 		insert (
 			ID						, BrandID				, ProgramID				, StyleID				, SeasonID
@@ -568,7 +569,7 @@ BEGIN
 			, KPICmpq				, KPIMNotice			, GFR					, SDPDate				, PulloutComplete		
 			, SewINLINE				, FtyGroup				, ForecastSampleGroup	, DyeingLoss			, SubconInType
 			, LastProductionDate	, EstPODD				, AirFreightByBrand		, AllowanceComboID      , ChangeMemoDate
-			, ForecastCategory		, OnSiteSample			, PulloutCmplDate		, NeedProduction
+			, ForecastCategory		, OnSiteSample			, PulloutCmplDate		, NeedProduction		, IsBuyBack
 
 		) values (
 			s.ID					, s.BrandID				, s.ProgramID			, s.StyleID				, s.SeasonID 
@@ -598,7 +599,7 @@ BEGIN
 			, s.KPICmpq 			, s.KPIMNotice			, s.GFR					, s.SDPDate				, s.PulloutComplete		
 			, s.SewINLINE           , s.FTY_Group			, s.ForecastSampleGroup , s.DyeingLoss          , '0'
 			, s.LastProductionDate	, s.EstPODD				, s.AirFreightByBrand	, s.AllowanceComboID    , s.ChangeMemoDate
-			, s.ForecastCategory	, s.OnSiteSample		, s.PulloutCmplDate		, s.NeedProduction
+			, s.ForecastCategory	, s.OnSiteSample		, s.PulloutCmplDate		, s.NeedProduction		, isnull (s.IsBuyBack, 0)
 		)
 		output inserted.id, iif(deleted.id is null,1,0) into @OrderT; --將insert =1 , update =0 把改變過的id output;
 
