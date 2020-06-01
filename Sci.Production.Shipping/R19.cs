472c1ceaca76fd50ccb7df3e3a71db461333470e
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2020-06-01 11:08:04 +0800

    ISP20200915 - 加入R19 Form

---
+++ Sci.Production.Shipping/R19.cs
@@ -0,0 +1,24 @@
+﻿using Ict;
+using Sci.Data;
+using Sci.Win;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Sci.Production.Shipping
+{
+    public partial class R19 : Sci.Win.Tems.PrintForm
+    {
+        public R19(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            this.InitializeComponent();
+        }
+    }
+}
