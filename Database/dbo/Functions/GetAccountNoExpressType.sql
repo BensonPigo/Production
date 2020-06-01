b24996be436229e2d3ce905bc5d9f0611759250e
jack.hsu <jack.hsu@sportscity.com.tw>
2020-06-01 11:01:38 +0800

    ISP20200925 - GetAccountNoExpressType 先判斷統制科目，沒有設定在判斷子科目

--- Database/dbo/Functions/GetAccountNoExpressType.sql
+++ Database/dbo/Functions/GetAccountNoExpressType.sql
@@ -4,14 +4,12 @@ Author:		Jack
 Create date: 2020/01/17
 Description: 判斷會計科目 Vat, Import, Export, 姊妹廠代墊（SisFty）, 空運費(IsAPP), 代墊台北(AdvancePaymentTPE) 
 
-統治科目(AccountNo.ID 前四碼) 判斷
- - 姊妹廠代墊(SisFty) 
- - 代墊台北(AdvancePaymentTPE)
-
 Vat = AccountNo.IsShippingVAT
 空運費(IsAPP) = AccountNo.IsAPP
 代墊台北(AdvancePaymentTPE) = AccountNo.AdvancePaymentTPE
 Import, Export, 姊妹廠代墊（SisFty） = DropDownList.Name and DropDownList.Type = 'ExpressType'
+
+規則 : 先判斷統制科目，沒有設定在判斷子科目
 =======================================================================================================================================
 */
 CREATE FUNCTION [dbo].[GetAccountNoExpressType]
@@ -34,14 +32,11 @@ BEGIN
 
 	set @SplitStringData = LOWER(@SplitStringData)
 
-	--SisFty, AdvancePaymentTPE 判斷統治科目
-	if @SplitStringData = LOWER('SisFty') or @SplitStringData = LOWER('AdvancePaymentTPE')
-	begin
-		set @ID = substring(@ID,1,4)
-	end
-
+	-- 統制科目
+	declare @controlAccount varchar(4) = substring(@ID,1,4) 
 	declare @rtnVal as bit = 0
 
+	-- 先判斷統制科目
 	select @rtnVal = 
 				case 
 					when @SplitStringData = LOWER('vat') then an.IsShippingVAT
@@ -50,7 +45,7 @@ BEGIN
 				else splitString.value  
 				end
 	from FinanceEN.dbo.AccountNo an
-	LEFT JOIN FinanceEN.dbo.DropDownList ddl on an.ExpressTypeID = ddl.ID and ddl.Type = 'ExpressType'
+	left join FinanceEN.dbo.DropDownList ddl on an.ExpressTypeID = ddl.ID and ddl.Type = 'ExpressType'
 	outer apply (
 		select value = cast(isnull( 
 			(select 1 
@@ -60,7 +55,31 @@ BEGIN
 				where LOWER(Data) = @SplitStringData))
 		,0) as bit)
 	) splitString
-	where an.ID = @ID
+	where an.ID = @controlAccount
 
-	RETURN @rtnVal
+	-- 沒有設定在判斷子科目
+	if @rtnVal = 0
+	begin
+		select @rtnVal = 
+				case 
+					when @SplitStringData = LOWER('vat') then an.IsShippingVAT
+					when @SplitStringData = LOWER('AdvancePaymentTPE') then an.AdvancePaymentTPE 
+					when @SplitStringData = LOWER('IsAPP') then an.IsAPP
+				else splitString.value  
+				end
+		from FinanceEN.dbo.AccountNo an
+		left join FinanceEN.dbo.DropDownList ddl on an.ExpressTypeID = ddl.ID and ddl.Type = 'ExpressType'
+		outer apply (
+			select value = cast(isnull( 
+				(select 1 
+				where exists (
+					select Data
+					from FinanceEN.dbo.SplitString(ddl.Name, '+')
+					where LOWER(Data) = @SplitStringData))
+			,0) as bit)
+		) splitString
+		where an.ID = @ID
+	end
+
+	return @rtnVal
 END
