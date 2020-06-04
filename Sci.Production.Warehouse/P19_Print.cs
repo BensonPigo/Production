ad042eed4f8732e15a48ad98f20fc40a2a2551cc
DOMAIN\aaron.shie <onizuka865@gmail.com>
2020-06-04 11:57:57 +0800

    ISP20200948 [P19. Transfer Out] - Transfer Out Export Function 1.請將W/H P19 Print功能改成按下後跳出視窗讓使用者選擇，若選第一個選項請把To Excel 按鈕Disable，若選第二個選項請把Print按鈕Disable。
    2.W/H P19 Print功能選擇第一個選項在按下Print同原先Print的資料。
    
    3.W/H P19 Print功能選擇第二個選項並按下To Excel則使用Warehouse_P18_ExcelImport Excel並帶入資料，只需要帶入SP#、SEQ1、SEQ2、Roll、Dyelot、Qty，其他帶空讓使用者自己選。

---
+++ Sci.Production.Warehouse/P19_Print.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ict;
+using Ict.Win;
+using Sci.Data;
+using Sci.Win;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Sci.Production.Warehouse
+{
+    public partial class P19_Print : Sci.Win.Tems.PrintForm
+    {
+        DataRow mainCurrentMaintain;
+        DataTable dtResult;
+        public P19_Print(DataRow drMain)
+        {
+            this.InitializeComponent();
+            this.PrintButtonStatusChange();
+            this.mainCurrentMaintain = drMain;
+        }
+
+        private void radioTransferOutReport_CheckedChanged(object sender, EventArgs e)
+        {
+            this.PrintButtonStatusChange();
+        }
+
+        private void radioP18ExcelImport_CheckedChanged(object sender, EventArgs e)
+        {
+            this.PrintButtonStatusChange();
+        }
+
+        private void PrintButtonStatusChange()
+        {
+            if (this.radioTransferOutReport.Checked)
+            {
+                this.print.Enabled = true;
+                this.toexcel.Enabled = false;
+            }
+            else
+            {
+                this.print.Enabled = false;
+                this.toexcel.Enabled = true;
+            }
+        }
+
+        protected override DualResult OnAsyncDataLoad(ReportEventArgs e)
+        {
+            if (this.radioTransferOutReport.Checked)
+            {
+                string id = this.mainCurrentMaintain["ID"].ToString();
+
+                #region  抓表頭資料
+                List<SqlParameter> pars = new List<SqlParameter>();
+                pars.Add(new SqlParameter("@ID", id));
+                DataTable dt;
+                DualResult result = DBProxy.Current.Select("",
+                @"select  b.name 
+            from dbo.TransferOut a WITH (NOLOCK) 
+            inner join dbo.mdivision  b WITH (NOLOCK) 
+            on b.id = a.mdivisionid
+            where b.id = a.mdivisionid
+            and a.id = @ID", pars, out dt);
+                if (!result) { this.ShowErr(result); }
+
+                if (dt == null || dt.Rows.Count == 0)
+                {
+                    return new DualResult(false, "Data not found!!!");
+                }
+                #endregion
+                #region  抓表身資料
+                pars = new List<SqlParameter>();
+                pars.Add(new SqlParameter("@ID", id));
+                result = DBProxy.Current.Select("", @"
+select a.POID
+    ,a.Seq1+'-'+a.seq2 as SEQ
+	,a.Roll,a.Dyelot
+	,IIF((b.ID =   lag(b.ID,1,'') over (order by b.ID,b.seq1,b.seq2) 
+		AND(b.seq1 = lag(b.seq1,1,'')over (order by b.ID,b.seq1,b.seq2))
+		AND(b.seq2 = lag(b.seq2,1,'')over (order by b.ID,b.seq1,b.seq2))) 
+		,'',dbo.getMtlDesc(a.poid,a.seq1,a.seq2,2,0))[DESC]
+	,CASE a.stocktype
+			WHEN 'B' THEN 'Bulk'
+			WHEN 'I' THEN 'Inventory'
+			WHEN 'O' THEN 'Scrap'
+			ELSE a.stocktype
+			END
+			stocktype
+	,unit = b.StockUnit
+	,a.Qty
+	,[Location]=dbo.Getlocation(fi.ukey)
+    ,[Total]=sum(a.Qty) OVER (PARTITION BY a.POID ,a.Seq1,a.Seq2 ) 	        
+from dbo.TransferOut_Detail a WITH (NOLOCK) 
+LEFT join dbo.PO_Supp_Detail b WITH (NOLOCK) on  b.id=a.POID and b.SEQ1=a.Seq1 and b.SEQ2=a.seq2
+left join dbo.FtyInventory FI on a.poid = fi.poid and a.seq1 = fi.seq1 and a.seq2 = fi.seq2 and a.Dyelot = fi.Dyelot
+    and a.roll = fi.roll and a.stocktype = fi.stocktype
+where a.id= @ID", pars, out dtResult);
+                if (!result) { this.ShowErr(result); }
+
+                if (dtResult == null || dtResult.Rows.Count == 0)
+                {
+                    return new DualResult(false, "Data not found!!!");
+                }
+            }
+            else
+            {
+                string sql = $@"
+select
+POID,
+Seq1,
+Seq2,
+Roll,
+Dyelot,
+[GW] = '',
+Qty
+from TransferOut_Detail with (nolock)
+where ID = '{this.mainCurrentMaintain["ID"]}'
+
+";
+                DualResult result = DBProxy.Current.Select(null, sql, out this.dtResult);
+                if (!result)
+                {
+                    return result;
+                }
+            }
+
+            return new DualResult(true);
+        }
+
+        protected override bool OnToExcel(ReportDefinition report)
+        {
+
+            this.ShowWaitMessage("Excel Processing...");
+            this.SetCount(this.dtResult.Rows.Count); // 顯示筆數
+
+            if (dtResult.Rows.Count == 0)
+            {
+                MyUtility.Msg.WarningBox("Data not found!");
+                this.HideWaitMessage();
+                return false;
+            }
+
+            Excel.Application objApp = new Excel.Application();
+            Sci.Utility.Report.ExcelCOM com = new Sci.Utility.Report.ExcelCOM(Sci.Env.Cfg.XltPathDir + "\\Warehouse_P18_ExcelImport.xltx", objApp);
+            com.UseInnerFormating = false;
+            com.WriteTable(dtResult, 3);
+
+            com.ExcelApp.ActiveWorkbook.Sheets[1].Select(Type.Missing);
+            objApp.Visible = true;
+            Marshal.ReleaseComObject(objApp);
+            this.HideWaitMessage();
+            return true;
+        }
+
+        protected override bool OnToPrint(ReportDefinition report)
+        {
+            this.SetCount(dtResult.Rows.Count);
+
+            DataRow row = this.mainCurrentMaintain;
+            string id = row["ID"].ToString();
+            string Remark = row["Remark"].ToString().Trim().Replace("\r", " ").Replace("\n", " ");
+            string issuedate = ((DateTime)MyUtility.Convert.GetDate(row["issuedate"])).ToShortDateString();
+            //抓M的EN NAME
+            DataTable dtNAME;
+            DBProxy.Current.Select("",
+            string.Format(@"select NameEN from MDivision where ID='{0}'", Sci.Env.User.Keyword), out dtNAME);
+            //
+            string RptTitle = dtNAME.Rows[0]["NameEN"].ToString();
+
+            report.ReportParameters.Add(new Microsoft.Reporting.WinForms.ReportParameter("RptTitle", RptTitle));
+            report.ReportParameters.Add(new Microsoft.Reporting.WinForms.ReportParameter("ID", id));
+            report.ReportParameters.Add(new Microsoft.Reporting.WinForms.ReportParameter("Remark", Remark));
+            report.ReportParameters.Add(new Microsoft.Reporting.WinForms.ReportParameter("issuedate", issuedate));
+
+            // 傳 list 資料            
+            List<P19_PrintData> data = dtResult.AsEnumerable()
+                .Select(row1 => new P19_PrintData()
+                {
+                    POID = row1["POID"].ToString().Trim(),
+                    SEQ = row1["SEQ"].ToString().Trim(),
+                    Roll = row1["Roll"].ToString().Trim(),
+                    Dyelot = row1["Dyelot"].ToString().Trim(),
+                    DESC = row1["DESC"].ToString().Trim(),
+                    stocktype = row1["stocktype"].ToString().Trim(),
+                    unit = row1["unit"].ToString().Trim(),
+                    QTY = row1["QTY"].ToString().Trim(),
+                    Location = row1["Location"].ToString().Trim(),
+                    Total = row1["Total"].ToString().Trim()
+                }).ToList();
+
+            report.ReportDataSource = data;
+            #endregion
+            // 指定是哪個 RDLC
+            #region  指定是哪個 RDLC
+            //DualResult result;
+            Type ReportResourceNamespace = typeof(P19_PrintData);
+            Assembly ReportResourceAssembly = ReportResourceNamespace.Assembly;
+            string ReportResourceName = "P19_Print.rdlc";
+
+            IReportResource reportresource;
+            DualResult result = ReportResources.ByEmbeddedResource(ReportResourceAssembly, ReportResourceNamespace, ReportResourceName, out reportresource);
+            if (!result)
+            {
+                //this.ShowException(result);
+                return false;
+            }
+
+            report.ReportResource = reportresource;
+            #endregion
+            // 開啟 report view
+            var frm = new Sci.Win.Subs.ReportView(report);
+            frm.MdiParent = MdiParent;
+            frm.Show();
+
+            return true;
+        }
+    }
+}
