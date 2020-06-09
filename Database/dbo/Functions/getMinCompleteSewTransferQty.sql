c7636a73169a5962a25a6976a850d3428f52cb0a
JEFF <jeff.yeh@sportscity.com.tw>
2020-06-09 14:48:13 +0800

    ISP20200961 Sewing R09 在[ID]右邊加入[Reason]欄位
    新增 [getMinCompleteSewTransferQty] 傳入from sp,To sp,Article(null),size(null)計算成套
    
    PPIC R17
    1. [Qty]修改欄位名稱 [Order Qty] 來源變更為 Order_Qty.Qty
    2. [Sewing output Qty]修改欄位名稱 [Sewing Qty]
    3. [Cancel Still need Prod]插在[From SP#]右邊
    4. [BuyBack Qty]插在[Sewing Qty]右邊
    5. [From SP# Order Qty]插在[BuyBack Qty]右邊，其值為orders.qty
    6. [From SP# Sewing Qty]插在[From SP# Order Qty]右邊，其值為[dbo].[getMinCompleteSewQty]並傳入[From SP#]
    7. [Transferred Qty]dbo.[getMinCompleteSewTransferQty]並傳入from sp,To sp

---
+++ Database/dbo/Functions/getMinCompleteSewTransferQty.sql
@@ -0,0 +1,109 @@
+﻿
+
+CREATE FUNCTION [dbo].[getMinCompleteSewTransferQty](@fromOrderid  varchar(13), @ToOrderid varchar(13), @article varchar(8), @sizecode varchar(8))
+RETURNS int
+BEGIN
+	DECLARE @minSeqQty int
+	DECLARE @StyleUnit as varchar(5), @StyleUkey as varchar(20)
+	SET @minSeqQty = 0
+
+	select @StyleUkey = StyleUkey,@StyleUnit = StyleUnit from Orders WITH (NOLOCK) where ID = @ToOrderid
+
+	IF @StyleUnit = 'PCS'
+		BEGIN
+		IF @article is null and @sizecode is null
+			BEGIN
+				select @minSeqQty = ToQty
+				from (
+					select sum(isnull(std.ToQty,0)) as ToQty
+					from SewingOutputTransfer_Detail std WITH (NOLOCK) 
+					inner join SewingOutputTransfer st WITH (NOLOCK) on std.ID = st.id
+					where std.FromOrderID = @fromOrderid and std.ToOrderID = @ToOrderid
+					and st.status = 'Confirmed'
+				)a 
+					
+			END
+		Else
+			IF @sizecode is null
+				BEGIN
+					select @minSeqQty = ToQty
+					from (
+						select sum(isnull(std.ToQty,0)) as ToQty
+						from SewingOutputTransfer_Detail std WITH (NOLOCK) 
+						inner join SewingOutputTransfer st WITH (NOLOCK) on std.ID = st.id
+						where std.FromOrderID = @fromOrderid and std.ToOrderID = @ToOrderid
+						and std.ToArticle = @article
+						and st.status = 'Confirmed'
+					)a  
+				END
+			ELSE
+				BEGIN
+					select @minSeqQty = ToQty
+					from (
+						select sum(isnull(std.ToQty,0)) as ToQty
+						from SewingOutputTransfer_Detail std WITH (NOLOCK) 
+						inner join SewingOutputTransfer st WITH (NOLOCK) on std.ID = st.id
+						where std.FromOrderID = @fromOrderid and std.ToOrderID = @ToOrderid
+						and std.ToArticle = @article
+						and std.ToSizeCode = @sizecode
+						and st.status = 'Confirmed'
+					)a  
+				END 
+		END	
+	ELSE
+		BEGIN
+		IF @article is null and @sizecode is null
+			BEGIN
+				select @minSeqQty = MIN(a.ToQty)
+				from (
+					select sl.Location, sum(isnull(std.ToQty,0)) as ToQty
+					from Order_Location sl WITH (NOLOCK)
+					outer apply(
+						select std.*
+						from SewingOutputTransfer_Detail std WITH (NOLOCK) 
+						inner join SewingOutputTransfer st WITH (NOLOCK) on std.ID = st.id
+						where std.FromOrderID = @fromOrderid and std.ToOrderID = @ToOrderid and std.ToComboType = sl.Location
+						and st.status = 'Confirmed'
+					)std
+					where sl.OrderID = @ToOrderid
+					group by sl.Location
+				) a
+			END
+		ELSE
+			IF @sizecode is null
+				BEGIN
+					select @minSeqQty = MIN(a.ToQty)
+					from (
+						select sl.Location, sum(isnull(std.ToQty,0)) as ToQty
+						from Order_Location sl WITH (NOLOCK)
+						outer apply(
+							select std.*
+							from SewingOutputTransfer_Detail std WITH (NOLOCK) 
+							inner join SewingOutputTransfer st WITH (NOLOCK) on std.ID = st.id
+							where std.FromOrderID = @fromOrderid and std.ToOrderID = @ToOrderid and std.ToComboType = sl.Location
+							and st.status = 'Confirmed'
+						)std
+						where sl.OrderID = @ToOrderid
+						group by sl.Location
+					) a
+				END
+			ELSE
+				BEGIN
+					select @minSeqQty = MIN(a.ToQty)
+					from (
+						select sl.Location, sum(isnull(std.ToQty,0)) as ToQty
+						from Order_Location sl WITH (NOLOCK)
+						outer apply(
+							select std.*
+							from SewingOutputTransfer_Detail std WITH (NOLOCK) 
+							inner join SewingOutputTransfer st WITH (NOLOCK) on std.ID = st.id
+							where std.FromOrderID = @fromOrderid and std.ToOrderID = @ToOrderid and std.ToComboType = sl.Location
+							and st.status = 'Confirmed'
+						)std
+						where sl.OrderID = @ToOrderid
+						group by sl.Location
+					) a
+				END
+		END
+	RETURN @minSeqQty
+END
