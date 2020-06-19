5b2677f7246b2e722dcace0da81843db66cf8357
JEFF <jeff.yeh@sportscity.com.tw>
2020-06-19 16:27:41 +0800

    ISP20201080 再次按下Generate時左下Table只取其一BundleGroup不用再group by

--- Sci.Production.Cutting/P10_Generate.cs
+++ Sci.Production.Cutting/P10_Generate.cs
@@ -283,7 +283,6 @@ order by ArticleGroup", patternukey);
             MyUtility.Tool.ProcessWithDatatable(detailTb, "PatternCode,PatternDesc,parts,subProcessid,BundleGroup,isPair,Location,NoBundleCardAfterSubprocess_String,PostSewingSubProcess_String", $@"
 Select PatternCode,PatternDesc,Parts,subProcessid,BundleGroup ,isPair ,Location,NoBundleCardAfterSubprocess_String,PostSewingSubProcess_String
 from #tmp where BundleGroup='{BundleGroup}'
-group by PatternCode,PatternDesc,Parts,subProcessid,BundleGroup ,isPair ,Location,NoBundleCardAfterSubprocess_String,PostSewingSubProcess_String
 order by iif(PatternCode='AllParts','ZZZZZZZ',PatternCode)
 ", out tmp);
             //需要使用上一層表身的值,不可重DB撈不然新增的資料就不會存回DB
