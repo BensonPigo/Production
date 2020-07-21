ed912423b3f41b01b96aa10d2961e73962dd3a36
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2020-07-21 11:04:55 +0800

    ISP20201222 - 新增WH R08功能

---
+++ Sci.Production.Warehouse/Report/R08.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Sci.Production.Warehouse
+{
+    partial class R08
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelBuyerDelivery = new Sci.Win.UI.Label();
+            this.labelSPNo = new Sci.Win.UI.Label();
+            this.labelFactory = new Sci.Win.UI.Label();
+            this.labelM = new Sci.Win.UI.Label();
+            this.label1 = new Sci.Win.UI.Label();
+            this.dateArrive = new Sci.Win.UI.DateRange();
+            this.txtSPNoEnd = new Sci.Win.UI.TextBox();
+            this.txtSPNoStart = new Sci.Win.UI.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtfactory = new Sci.Production.Class.Txtfactory();
+            this.txtMdivision = new Sci.Production.Class.TxtMdivision();
+            this.txtBrand = new Sci.Production.Class.Txtbrand();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(456, 12);
+            this.print.Visible = false;
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(456, 48);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(456, 84);
+            // 
+            // buttonCustomized
+            // 
+            this.buttonCustomized.Location = new System.Drawing.Point(317, 138);
+            // 
+            // checkUseCustomized
+            // 
+            this.checkUseCustomized.Location = new System.Drawing.Point(343, 144);
+            // 
+            // txtVersion
+            // 
+            this.txtVersion.Location = new System.Drawing.Point(375, 142);
+            // 
+            // labelBuyerDelivery
+            // 
+            this.labelBuyerDelivery.Location = new System.Drawing.Point(32, 12);
+            this.labelBuyerDelivery.Name = "labelBuyerDelivery";
+            this.labelBuyerDelivery.RectStyle.BorderColor = System.Drawing.Color.Black;
+            this.labelBuyerDelivery.RectStyle.BorderWidth = 1F;
+            this.labelBuyerDelivery.RectStyle.Color = System.Drawing.Color.SkyBlue;
+            this.labelBuyerDelivery.RectStyle.ExtBorderWidth = 1F;
+            this.labelBuyerDelivery.Size = new System.Drawing.Size(111, 23);
+            this.labelBuyerDelivery.TabIndex = 106;
+            this.labelBuyerDelivery.Text = "Arrive W/H Date";
+            this.labelBuyerDelivery.TextStyle.BorderColor = System.Drawing.Color.Black;
+            this.labelBuyerDelivery.TextStyle.Color = System.Drawing.Color.Black;
+            // 
+            // labelSPNo
+            // 
+            this.labelSPNo.Location = new System.Drawing.Point(32, 48);
+            this.labelSPNo.Name = "labelSPNo";
+            this.labelSPNo.RectStyle.BorderColor = System.Drawing.Color.Black;
+            this.labelSPNo.RectStyle.Color = System.Drawing.Color.SkyBlue;
+            this.labelSPNo.Size = new System.Drawing.Size(111, 23);
+            this.labelSPNo.TabIndex = 115;
+            this.labelSPNo.Text = "SP#";
+            this.labelSPNo.TextStyle.BorderColor = System.Drawing.Color.Black;
+            this.labelSPNo.TextStyle.Color = System.Drawing.Color.Black;
+            // 
+            // labelFactory
+            // 
+            this.labelFactory.Location = new System.Drawing.Point(32, 121);
+            this.labelFactory.Name = "labelFactory";
+            this.labelFactory.Size = new System.Drawing.Size(111, 23);
+            this.labelFactory.TabIndex = 131;
+            this.labelFactory.Text = "Factory";
+            // 
+            // labelM
+            // 
+            this.labelM.Location = new System.Drawing.Point(32, 84);
+            this.labelM.Name = "labelM";
+            this.labelM.Size = new System.Drawing.Size(111, 23);
+            this.labelM.TabIndex = 130;
+            this.labelM.Text = "M";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(32, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 23);
+            this.label1.TabIndex = 132;
+            this.label1.Text = "Brand";
+            // 
+            // dateArrive
+            // 
+            // 
+            // 
+            // 
+            this.dateArrive.DateBox1.Location = new System.Drawing.Point(0, 0);
+            this.dateArrive.DateBox1.Name = "";
+            this.dateArrive.DateBox1.Size = new System.Drawing.Size(129, 23);
+            this.dateArrive.DateBox1.TabIndex = 0;
+            // 
+            // 
+            // 
+            this.dateArrive.DateBox2.Location = new System.Drawing.Point(151, 0);
+            this.dateArrive.DateBox2.Name = "";
+            this.dateArrive.DateBox2.Size = new System.Drawing.Size(129, 23);
+            this.dateArrive.DateBox2.TabIndex = 1;
+            this.dateArrive.Location = new System.Drawing.Point(146, 12);
+            this.dateArrive.Name = "dateArrive";
+            this.dateArrive.Size = new System.Drawing.Size(280, 23);
+            this.dateArrive.TabIndex = 133;
+            // 
+            // txtSPNoEnd
+            // 
+            this.txtSPNoEnd.BackColor = System.Drawing.Color.White;
+            this.txtSPNoEnd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtSPNoEnd.Location = new System.Drawing.Point(307, 48);
+            this.txtSPNoEnd.Name = "txtSPNoEnd";
+            this.txtSPNoEnd.Size = new System.Drawing.Size(128, 23);
+            this.txtSPNoEnd.TabIndex = 135;
+            // 
+            // txtSPNoStart
+            // 
+            this.txtSPNoStart.BackColor = System.Drawing.Color.White;
+            this.txtSPNoStart.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtSPNoStart.Location = new System.Drawing.Point(146, 48);
+            this.txtSPNoStart.Name = "txtSPNoStart";
+            this.txtSPNoStart.Size = new System.Drawing.Size(128, 23);
+            this.txtSPNoStart.TabIndex = 134;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(275, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 17);
+            this.label2.TabIndex = 137;
+            this.label2.Text = " ～";
+            // 
+            // txtfactory
+            // 
+            this.txtfactory.BackColor = System.Drawing.Color.White;
+            this.txtfactory.BoolFtyGroupList = true;
+            this.txtfactory.FilteMDivision = false;
+            this.txtfactory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtfactory.IsProduceFty = false;
+            this.txtfactory.IssupportJunk = true;
+            this.txtfactory.Location = new System.Drawing.Point(146, 121);
+            this.txtfactory.MDivision = null;
+            this.txtfactory.Name = "txtfactory";
+            this.txtfactory.Size = new System.Drawing.Size(66, 23);
+            this.txtfactory.TabIndex = 139;
+            // 
+            // txtMdivision
+            // 
+            this.txtMdivision.BackColor = System.Drawing.Color.White;
+            this.txtMdivision.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtMdivision.Location = new System.Drawing.Point(146, 84);
+            this.txtMdivision.Name = "txtMdivision";
+            this.txtMdivision.Size = new System.Drawing.Size(66, 23);
+            this.txtMdivision.TabIndex = 138;
+            // 
+            // txtBrand
+            // 
+            this.txtBrand.BackColor = System.Drawing.Color.White;
+            this.txtBrand.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtBrand.Location = new System.Drawing.Point(146, 157);
+            this.txtBrand.Name = "txtBrand";
+            this.txtBrand.Size = new System.Drawing.Size(66, 23);
+            this.txtBrand.TabIndex = 140;
+            // 
+            // R08
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 225);
+            this.Controls.Add(this.txtBrand);
+            this.Controls.Add(this.txtfactory);
+            this.Controls.Add(this.txtMdivision);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSPNoEnd);
+            this.Controls.Add(this.txtSPNoStart);
+            this.Controls.Add(this.dateArrive);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelFactory);
+            this.Controls.Add(this.labelM);
+            this.Controls.Add(this.labelSPNo);
+            this.Controls.Add(this.labelBuyerDelivery);
+            this.Name = "R08";
+            this.OnLineHelpID = "Sci.Win.Tems.PrintForm";
+            this.Text = "R08. Fabric Release Report";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.buttonCustomized, 0);
+            this.Controls.SetChildIndex(this.checkUseCustomized, 0);
+            this.Controls.SetChildIndex(this.txtVersion, 0);
+            this.Controls.SetChildIndex(this.labelBuyerDelivery, 0);
+            this.Controls.SetChildIndex(this.labelSPNo, 0);
+            this.Controls.SetChildIndex(this.labelM, 0);
+            this.Controls.SetChildIndex(this.labelFactory, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.dateArrive, 0);
+            this.Controls.SetChildIndex(this.txtSPNoStart, 0);
+            this.Controls.SetChildIndex(this.txtSPNoEnd, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.txtMdivision, 0);
+            this.Controls.SetChildIndex(this.txtfactory, 0);
+            this.Controls.SetChildIndex(this.txtBrand, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.Label labelBuyerDelivery;
+        private Win.UI.Label labelSPNo;
+        private Win.UI.Label labelFactory;
+        private Win.UI.Label labelM;
+        private Win.UI.Label label1;
+        private Win.UI.DateRange dateArrive;
+        private Win.UI.TextBox txtSPNoEnd;
+        private Win.UI.TextBox txtSPNoStart;
+        private System.Windows.Forms.Label label2;
+        private Class.Txtfactory txtfactory;
+        private Class.TxtMdivision txtMdivision;
+        private Class.Txtbrand txtBrand;
+    }
+}
---
+++ Sci.Production.Warehouse/Report/R08.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Ict;
+using Sci.Data;
+
+namespace Sci.Production.Warehouse
+{
+    public partial class R08 : Win.Tems.PrintForm
+    {
+        private DateTime? arrive_s;
+        private DateTime? arrive_e;
+        private string mdivision;
+        private string factory;
+        private string spno_s;
+        private string spno_e;
+        private string brand;
+        private DataTable printData;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="R08"/> class.
+        /// </summary>
+        /// <param name="menuitem">Q_Q</param>
+        public R08(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            this.InitializeComponent();
+        }
+
+        protected override bool ValidateInput()
+        {
+            if ((!this.dateArrive.HasValue1 || !this.dateArrive.HasValue2) &&
+                (MyUtility.Check.Empty(this.txtSPNoStart.Text) && MyUtility.Check.Empty(this.txtSPNoEnd.Text)))
+            {
+                MyUtility.Msg.WarningBox("< Arrive W/H Date > or < SP# > can't be empty!!");
+                return false;
+            }
+
+            this.arrive_s = this.dateArrive.Value1;
+            this.arrive_e = this.dateArrive.Value2;
+            this.spno_s = this.txtSPNoStart.Text;
+            this.spno_e = this.txtSPNoEnd.Text;
+            this.mdivision = this.txtMdivision.Text;
+            this.factory = this.txtfactory.Text;
+            this.brand = this.txtBrand.Text;
+
+            return base.ValidateInput();
+        }
+
+        // 非同步取資料
+        protected override DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            #region Where條件
+            string where = string.Empty;
+            List<string> whereList = new List<string>();
+            List<string> whereList_2 = new List<string>();
+            List<SqlParameter> paras = new List<SqlParameter>();
+
+            if (this.arrive_s.HasValue && this.arrive_e.HasValue)
+            {
+                whereList.Add("R.WhseArrival between @WhseArrival_s and @WhseArrival_e " + Environment.NewLine);
+                whereList_2.Add("t.IssueDate between @WhseArrival_s and @WhseArrival_e " + Environment.NewLine);
+
+                paras.Add(new SqlParameter("@WhseArrival_s", this.arrive_s.Value));
+                paras.Add(new SqlParameter("@WhseArrival_e", this.arrive_e.Value));
+            }
+
+            if (!MyUtility.Check.Empty(this.spno_s))
+            {
+                whereList.Add("f.POID >= @spno_s" + Environment.NewLine);
+                whereList_2.Add("f.POID >= @spno_s" + Environment.NewLine);
+                paras.Add(new SqlParameter("@spno_s", this.spno_s));
+            }
+
+            if (!MyUtility.Check.Empty(this.spno_e))
+            {
+                whereList.Add("f.POID <= @spno_e" + Environment.NewLine);
+                whereList_2.Add("f.POID <= @spno_e" + Environment.NewLine);
+                paras.Add(new SqlParameter("@spno_e", this.spno_e));
+            }
+
+            if (!MyUtility.Check.Empty(this.mdivision))
+            {
+                whereList.Add("o.MDivisionid = @mdivision" + Environment.NewLine);
+                whereList_2.Add("o.MDivisionid = @mdivision" + Environment.NewLine);
+                paras.Add(new SqlParameter("@mdivision", this.mdivision));
+            }
+
+            if (!MyUtility.Check.Empty(this.factory))
+            {
+                whereList.Add("o.FtyGroup = @factory" + Environment.NewLine);
+                whereList_2.Add("o.FtyGroup = @factory" + Environment.NewLine);
+                paras.Add(new SqlParameter("@factory", this.factory));
+            }
+
+            if (!MyUtility.Check.Empty(this.brand))
+            {
+                whereList.Add("o.BrandID = @brand" + Environment.NewLine);
+                whereList_2.Add("o.BrandID = @brand" + Environment.NewLine);
+                paras.Add(new SqlParameter("@brand", this.brand));
+            }
+
+            #endregion
+
+            string cmd = $@"
+
+SELECT
+	f.POID
+	,[Seq]=RD.Seq1 +RD.Seq2
+	,o.BrandID
+	,o.StyleID
+	,PSD.Refno
+	,PSD.ColorID
+	,FC.WeaveTypeID
+	,R.WhseArrival
+	,F.ArriveQty
+	,[ReleaseQty]=SUM(FIT.OutQty) 
+	,[LastReleaseDate]=LastReleaseDate.Date
+from FIR F 
+inner join Receiving_Detail RD on F.ReceivingID=RD.ID and F.POID=RD.PoId and RD.Seq1=F.Seq1 and RD.Seq2=F.Seq2 
+inner join Receiving R on RD.ID=R.ID
+Left join Orders O on F.POID=O.ID
+Left join PO_Supp_Detail PSD on PSD.ID=F.POID and PSD.Seq1=F.Seq1 and PSD.Seq2=F.Seq2
+Left join FtyInventory FIT on FIT.POID=RD.POID and FIT.Seq1=RD.Seq1 and FIT.Seq2=RD.Seq2 and FIT.Roll=RD.Roll and FIT.Dyelot=RD.Dyelot and FIT.StockType=RD.StockType
+Left join Fabric FC on PSD.SCIRefno=FC.SCIRefno
+OUTER APPLY(
+	SELECT [Date]=Max(EditDate)
+	FROM View_TransactionList
+	WHERE Status='Confirmed' AND POID=o.ID AND Seq1=f.Seq1 AND Seq2=f.Seq2 --AND Roll = rd.Roll AND Dyelot = rd.Dyelot
+	--AND StockType  = rd.StockType 
+	AND tbname  IN ('BorrowBack-From',' Issue',' IssueLack ',' SubTransfer-From','TransferOut')
+)LastReleaseDate
+
+WHERE {whereList.JoinToString(" AND ")}
+
+Group by F.POID,RD.Seq1,RD.Seq2,O.StyleID,PSD.Refno,PSD.ColorID,FC.WeaveTypeID,R.WhseArrival,F.ArriveQty,LastReleaseDate.Date,o.BrandID
+
+UNION---------------
+
+select 
+	F.POID
+	,[Seq]=TD.Seq1 + '-'+TD.Seq2
+	,o.BrandID
+	,O.StyleID
+	,PSD.Refno
+	,PSD.ColorID
+	,FC.WeaveTypeID
+	,t.IssueDate
+	,F.ArriveQty
+	,sum(FIT.OutQty) 
+	,[LastReleaseDate]=LastReleaseDate.Date
+from FIR F 
+inner join TransferIn_Detail TD on F.ReceivingID=TD.ID and F.POID=TD.PoId and TD.Seq1=F.Seq1 and TD.Seq2=F.Seq2 
+inner join TransferIn T on TD.ID=T.ID
+Left join Orders O on F.POID=O.ID
+Left join PO_Supp_Detail PSD on PSD.ID=F.POID and PSD.Seq1=F.Seq1 and PSD.Seq2=F.Seq2
+Left join FtyInventory FIT on FIT.POID=TD.POID and FIT.Seq1=TD.Seq1 and FIT.Seq2=TD.Seq2 and FIT.Roll=TD.Roll and FIT.Dyelot=TD.Dyelot and FIT.StockType=TD.StockType
+Left join Fabric FC on PSD.SCIRefno=FC.SCIRefno
+OUTER APPLY(
+	SELECT [Date]=Max(EditDate)
+	FROM View_TransactionList
+	WHERE Status='Confirmed' AND POID=o.ID AND Seq1=f.Seq1 AND Seq2=f.Seq2 --AND Roll = TD.Roll AND Dyelot = TD.Dyelot
+	--AND StockType  = rd.StockType 
+	AND tbname  IN ('BorrowBack-From',' Issue',' IssueLack ',' SubTransfer-From','TransferOut')
+)LastReleaseDate
+
+WHERE {whereList_2.JoinToString(" AND ")}
+
+Group by F.POID,TD.Seq1,TD.Seq2,o.BrandID,O.StyleID,PSD.Refno,PSD.ColorID,FC.WeaveTypeID,t.IssueDate,F.ArriveQty,LastReleaseDate.Date
+";
+
+            DualResult result = DBProxy.Current.Select(null, cmd.ToString(), paras, out this.printData);
+            if (!result)
+            {
+                DualResult failResult = new DualResult(false, "Query data fail\r\n" + result.ToString());
+                return failResult;
+            }
+
+            return Ict.Result.True;
+        }
+
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            // 顯示筆數於PrintForm上Count欄位
+            this.SetCount(this.printData.Rows.Count);
+
+            if (this.printData.Rows.Count <= 0)
+            {
+                MyUtility.Msg.WarningBox("Data not found!");
+                return false;
+            }
+
+            MyUtility.Excel.CopyToXls(this.printData, string.Empty, "Warehouse_R08.xltx", 1);
+            return true;
+        }
+    }
+}
