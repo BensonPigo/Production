6763247df29948de92e730d413e6e53029bb935f
JEFF <jeff.yeh@sportscity.com.tw>
2020-09-01 15:45:19 +0800

    ISP20201432 TransferInOutInformation 加上固定條件, 只能撈取登入工廠 數量空的顯示0

--- Sci.Production.Sewing/P11_TransferInOutInformation.cs
+++ Sci.Production.Sewing/P11_TransferInOutInformation.cs
@@ -1,16 +1,8 @@
-﻿using Ict;
-using Ict.Win;
-using Sci.Data;
+﻿using Ict.Win;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Sci.Production.Sewing
 {
@@ -74,21 +66,23 @@ Declare @SP varchar(13) = @OrderID
 
 select
 	o.ID
-	, O.Qty
+	, Qty = isnull(O.Qty, 0)
 	, IsSet = iif(o.StyleUnit = 'PCS', 'N', 'Y')
 	, IsCancelOrder = iif(o.Junk = 0, 'N', 'Y')
 	, IsBuyback = iif(o.IsBuyBack = 0, 'N', 'Y')
 from orders o with(nolock)
 where o.POID in (select o2.POID from orders o2 where o2.id=@SP)
-order by o.ID
+and o.FtyGroup = '{Env.User.Factory}'
+order by o.ID
+
 select
 	Transfer_Type = 'In' 
 	, FromToOrderID = sotd.FromOrderID
 	, OrderID = sotd.ToOrderID
-	, buybackQty_in.buybackQty
-	, CancelQty_in.CancelQty
-	, sotd.TransferQty
-	, TransferInQty = sotd.TransferQty
+	, buybackQty = isnull(buybackQty_in.buybackQty, 0)
+	, CancelQty = isnull(CancelQty_in.CancelQty, 0)
+	, TransferQty = isnull(sotd.TransferQty, 0)
+	, TransferInQty = isnull(sotd.TransferQty, 0)
 	, TransferOutQty = 0
 	, ComboType = sotd.ToComboType
 	, Article = sotd.ToArticle
@@ -115,6 +109,7 @@ outer apply (
 ) CancelQty_in
 where 1=1
 and sot.Status ='Confirmed'
+and sot.FactoryID = '{Env.User.Factory}'
 and sotd.ToOrderID in (select distinct o3.id from Orders o2 inner join Orders o3 on o3.POID = o2.POID where o2.id=@SP)
 
 union all
@@ -123,11 +118,11 @@ select
 	Transfer_Type = 'Out'
 	, sotd.ToOrderID
 	, sotd.FromOrderID
-	, buybackQty_Out.buybackQty
-	, CancelQty_Out.CancelQty
-	, sotd.TransferQty
+	, buybackQty = isnull(buybackQty_Out.buybackQty, 0)
+	, CancelQty = isnull(CancelQty_Out.CancelQty, 0)
+	, TransferQty = isnull(sotd.TransferQty, 0)
 	, TransferInQty = 0
-	, TransferOutQty = sotd.TransferQty
+	, TransferOutQty = isnull(sotd.TransferQty, 0)
 	, sotd.FromComboType
 	, sotd.Article
 	, sotd.SizeCode
@@ -153,10 +148,11 @@ outer apply (
 ) CancelQty_Out
 where 1=1
 and sot.Status ='Confirmed'
+and sot.FactoryID = '{Env.User.Factory}'
 and sotd.FromOrderID in (select distinct o3.id from Orders o2 inner join Orders o3 on o3.POID = o2.POID where o2.id=@SP)
 order by Transfer_Type, FromOrderID
 
-select d.*, g.QAQty, SewingQty = IIF(SewingQty.SewingQty < 0, 0, SewingQty.SewingQty)
+select d.*, QAQty = isnull(g.QAQty, 0), SewingQty = IIF(isnull(SewingQty.SewingQty, 0) < 0, 0, isnull(SewingQty.SewingQty, 0))
 from(
 	select t.OrderID, t.ComboType, t.Article, t.SizeCode, buybackQty = sum(t.buybackQty), CancelQty = sum(t.CancelQty), TransferInQty = SUM(TransferInQty) , TransferOutQty = SUM(TransferOutQty)
 	from #tmp t
@@ -167,6 +163,7 @@ left join(
 	from orders o with(nolock)
 	inner join SewingOutput_Detail_Detail sodd with(nolock) on o.id = sodd.OrderId
 	where o.POID in (select o2.POID from orders o2 where o2.id=@SP)
+    and o.FtyGroup = '{Env.User.Factory}'
 	group by sodd.OrderId, sodd.ComboType, sodd.Article, sodd.SizeCode, o.Junk
 )g on d.OrderID = g.OrderId and d.ComboType = g.ComboType and d.Article = g.Article and d.SizeCode = g.SizeCode
 outer apply(select SewingQty = isnull(g.QAQty, 0) + isnull(d.TransferInQty, 0) - isnull(d.TransferOutQty, 0))SewingQty
