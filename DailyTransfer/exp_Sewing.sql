7b2c7d58a332518b63b6a0ba9f1755d901e1c178
JEFF <jeff.yeh@sportscity.com.tw>
2020-09-17 14:51:25 +0800

    ISP20201530 資料轉出 exp_Sewing 新增轉出 SewingOutputTransfer 與 SewingOutputTransfer_Detail
    補回轉移至 Buyback 的產量

--- DailyTransfer/exp_Sewing.sql
+++ DailyTransfer/exp_Sewing.sql
@@ -56,7 +56,7 @@ else
 declare @DateInfoName2 varchar(30) ='SewingOutput2';
 declare @DateStart2 date= (select DateStart from Production.dbo.DateInfo where name = @DateInfoName2);
 declare @DateEnd2 date  = (select DateEnd   from Production.dbo.DateInfo where name = @DateInfoName2);
-set @Remark = (select Remark from Production.dbo.DateInfo where name = @DateInfoName);
+set @Remark = (select Remark from Production.dbo.DateInfo where name = @DateInfoName2);
 
 --2.取得預設值
 if @DateStart2 is Null
@@ -65,11 +65,11 @@ if @DateEnd2 is Null
 	set @DateEnd2 = CONVERT(DATE, GETDATE())	
 
 --3.更新Pms_To_Trade.dbo.dateInfo
-if exists(select 1 from Pms_To_Trade.dbo.dateInfo where Name = @DateInfoName )
-	update Pms_To_Trade.dbo.dateInfo  set DateStart = @DateStart2,DateEnd = @DateEnd2, Remark=@Remark where Name = @DateInfoName 
+if exists(select 1 from Pms_To_Trade.dbo.dateInfo where Name = @DateInfoName2 )
+	update Pms_To_Trade.dbo.dateInfo  set DateStart = @DateStart2,DateEnd = @DateEnd2, Remark=@Remark where Name = @DateInfoName2 
 else
 	Insert into Pms_To_Trade.dbo.dateInfo(Name,DateStart,DateEnd,Remark)
-	values (@DateInfoName,@DateStart2,@DateEnd2,@Remark);
+	values (@DateInfoName2,@DateStart2,@DateEnd2,@Remark);
 ------------------------------------------------------------------------------------------------------
 
 --SewingOutput
@@ -96,4 +96,38 @@ select ol.*
 into Order_Location
 from Production.dbo.Order_Location ol
 where exists(select 1 from Pms_To_Trade.dbo.SewingOutput_Detail where OrderId = ol.OrderId)
+
+------------------------------------------------------------------------------------------------------
+--***資料交換的條件限制***
+--1. 優先取得Production.dbo.DateInfo
+declare @DateInfoName3 varchar(30) ='SewingOutputTransfer';
+declare @DateStart3 date= (select DateStart from Production.dbo.DateInfo where name = @DateInfoName3);
+declare @DateEnd3 date  = (select DateEnd   from Production.dbo.DateInfo where name = @DateInfoName3);
+set @Remark = (select Remark from Production.dbo.DateInfo where name = @DateInfoName3);
+
+--2.取得預設值
+if @DateStart3 is Null
+	set @DateStart3= CONVERT(DATE,DATEADD(day,-7,GETDATE()))
+if @DateEnd3 is Null
+	set @DateEnd3 = CONVERT(DATE, GETDATE())
+	
+--3.更新Pms_To_Trade.dbo.dateInfo
+if exists(select 1 from Pms_To_Trade.dbo.dateInfo where Name = @DateInfoName3 )
+	update Pms_To_Trade.dbo.dateInfo  set DateStart = @DateStart3,DateEnd = @DateEnd3, Remark=@Remark where Name = @DateInfoName3 
+else
+	Insert into Pms_To_Trade.dbo.dateInfo(Name,DateStart,DateEnd,Remark)
+	values (@DateInfoName3,@DateStart3,@DateEnd3,@Remark);
+
+--SewingOutputTransfer
+select *
+into SewingOutputTransfer
+from Production.dbo.SewingOutputTransfer sot
+where sot.Status = 'Confirmed'
+and sot.EditDate between @DateStart3 and @DateEnd3
+
+--SewingOutputTransfer_Detail
+select sotd.*
+into SewingOutputTransfer_Detail
+from Production.dbo.SewingOutputTransfer_Detail sotd
+inner join Pms_To_Trade.dbo.SewingOutputTransfer sot on sot.ID = sotd.ID
 END
