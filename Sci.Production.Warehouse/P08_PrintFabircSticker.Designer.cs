af6459bbf088f7af117521d1fc680f8294192bf5
jack.hsu <jack.hsu@sportscity.com.tw>
2020-09-25 14:10:23 +0800

    ISP20201578 - WH P08 增加Remark欄位，WH P08_PrintFabircSticker 增加 RDLC

---
+++ Sci.Production.Warehouse/P08_PrintFabircSticker.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Sci.Production.Warehouse
+{
+    partial class P08_PrintFabircSticker
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnClose = new Sci.Win.UI.Button();
+            this.bindingSource1 = new Sci.Win.UI.BindingSource(this.components);
+            this.grid = new Sci.Win.UI.Grid();
+            this.panel1 = new Sci.Win.UI.Panel();
+            this.panel2 = new Sci.Win.UI.Panel();
+            this.btnPrint = new Sci.Win.UI.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnClose.Location = new System.Drawing.Point(701, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(80, 30);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // gridAccumulatedQty
+            // 
+            this.grid.AllowUserToAddRows = false;
+            this.grid.AllowUserToDeleteRows = false;
+            this.grid.AllowUserToResizeRows = false;
+            this.grid.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.grid.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid.EditingEnter = Ict.Win.UI.DataGridViewEditingEnter.NextCellOrNextRow;
+            this.grid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.grid.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.grid.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(162)))), ((int)(((byte)(163)))));
+            this.grid.Location = new System.Drawing.Point(0, 0);
+            this.grid.Name = "gridAccumulatedQty";
+            this.grid.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.grid.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.grid.RowTemplate.Height = 24;
+            this.grid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grid.ShowCellToolTips = false;
+            this.grid.Size = new System.Drawing.Size(784, 389);
+            this.grid.TabIndex = 1;
+            this.grid.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.grid);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(784, 389);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnPrint);
+            this.panel2.Controls.Add(this.btnClose);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 389);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(784, 48);
+            this.panel2.TabIndex = 0;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(615, 12);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(80, 30);
+            this.btnPrint.TabIndex = 1;
+            this.btnPrint.Text = "Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.BtnPrint_Click);
+            // 
+            // P08_PrintFabircSticker
+            // 
+            this.ClientSize = new System.Drawing.Size(784, 437);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "P08_PrintFabircSticker";
+            this.OnLineHelpID = "Sci.Win.Subs.Base";
+            this.Text = "P08. Accumulated Qty";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Win.UI.Button btnClose;
+        private Win.UI.BindingSource bindingSource1;
+        private Win.UI.Grid grid;
+        private Win.UI.Panel panel1;
+        private Win.UI.Panel panel2;
+        private Win.UI.Button btnPrint;
+    }
+}
---
+++ Sci.Production.Warehouse/P08_PrintFabircSticker_PrintData.cs
@@ -0,0 +1,66 @@
+﻿namespace Sci.Production.Warehouse
+{
+    /// <inheritdoc/>
+    public class P08_PrintFabircSticker_PrintData
+    {
+        /// <summary>
+        /// new index
+        /// </summary>
+        public int Idx { get; set; }
+
+        /// <summary>
+        /// SP
+        /// </summary>
+        public string SP { get; set; }
+
+        /// <summary>
+        /// Ref
+        /// </summary>
+        public string Ref { get; set; }
+
+        /// <summary>
+        /// Color
+        /// </summary>
+        public string Color { get; set; }
+
+        /// <summary>
+        /// Qty
+        /// </summary>
+        public int Qty { get; set; }
+
+        /// <summary>
+        /// Roll
+        /// </summary>
+        public string Roll { get; set; }
+
+        /// <summary>
+        /// Fabric Width
+        /// </summary>
+        public decimal FabWidth { get; set; }
+
+        /// <summary>
+        /// Dyelot
+        /// </summary>
+        public string Dyelot { get; set; }
+
+        /// <summary>
+        /// Po_Supp_Detail SizeSpec
+        /// </summary>
+        public string CutWidth { get; set; }
+
+        /// <summary>
+        /// Po_Supp_Detail Special
+        /// </summary>
+        public string CutType { get; set; }
+
+        /// <summary>
+        /// ExpSlice
+        /// </summary>
+        public string ExpSlice { get; set; }
+
+        /// <summary>
+        /// ActSlice
+        /// </summary>
+        public string ActSlice { get; set; }
+    }
+}
