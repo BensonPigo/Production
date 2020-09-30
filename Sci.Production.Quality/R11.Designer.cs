a2de25e5263a31ed0be46883a9a49badbd12452b
JEFF <jeff.yeh@sportscity.com.tw>
2020-09-30 15:40:44 +0800

    ISP20201579 新增QA R11

---
+++ Sci.Production.Quality/R11.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Sci.Production.Quality
+{
+    partial class R11
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateArriveWHDate = new Sci.Win.UI.DateRange();
+            this.label10 = new Sci.Win.UI.Label();
+            this.label4 = new Sci.Win.UI.Label();
+            this.label5 = new Sci.Win.UI.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtSP1 = new Sci.Win.UI.TextBox();
+            this.txtSP2 = new Sci.Win.UI.TextBox();
+            this.label1 = new Sci.Win.UI.Label();
+            this.txtBrand = new Sci.Production.Class.Txtbrand();
+            this.labelRefno = new Sci.Win.UI.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRefno1 = new Sci.Win.UI.TextBox();
+            this.txtRefno2 = new Sci.Win.UI.TextBox();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Enabled = false;
+            this.print.Location = new System.Drawing.Point(404, 106);
+            this.print.Visible = false;
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(404, 12);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(404, 48);
+            // 
+            // buttonCustomized
+            // 
+            this.buttonCustomized.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCustomized.Location = new System.Drawing.Point(361, 142);
+            // 
+            // checkUseCustomized
+            // 
+            this.checkUseCustomized.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkUseCustomized.Location = new System.Drawing.Point(387, 178);
+            // 
+            // txtVersion
+            // 
+            this.txtVersion.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtVersion.Location = new System.Drawing.Point(384, 205);
+            // 
+            // dateArriveWHDate
+            // 
+            // 
+            // 
+            // 
+            this.dateArriveWHDate.DateBox1.Location = new System.Drawing.Point(0, 0);
+            this.dateArriveWHDate.DateBox1.Name = "";
+            this.dateArriveWHDate.DateBox1.Size = new System.Drawing.Size(129, 23);
+            this.dateArriveWHDate.DateBox1.TabIndex = 0;
+            // 
+            // 
+            // 
+            this.dateArriveWHDate.DateBox2.Location = new System.Drawing.Point(151, 0);
+            this.dateArriveWHDate.DateBox2.Name = "";
+            this.dateArriveWHDate.DateBox2.Size = new System.Drawing.Size(129, 23);
+            this.dateArriveWHDate.DateBox2.TabIndex = 1;
+            this.dateArriveWHDate.IsRequired = false;
+            this.dateArriveWHDate.Location = new System.Drawing.Point(126, 12);
+            this.dateArriveWHDate.Name = "dateArriveWHDate";
+            this.dateArriveWHDate.Size = new System.Drawing.Size(280, 23);
+            this.dateArriveWHDate.TabIndex = 0;
+            // 
+            // label10
+            // 
+            this.label10.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label10.Location = new System.Drawing.Point(392, 81);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(92, 22);
+            this.label10.TabIndex = 97;
+            this.label10.Text = "Paper Size A4";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(18, 41);
+            this.label4.Name = "label4";
+            this.label4.RectStyle.Color = System.Drawing.Color.SkyBlue;
+            this.label4.Size = new System.Drawing.Size(105, 23);
+            this.label4.TabIndex = 125;
+            this.label4.Text = "SP#";
+            this.label4.TextStyle.Color = System.Drawing.Color.Black;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(18, 12);
+            this.label5.Name = "label5";
+            this.label5.RectStyle.Color = System.Drawing.Color.SkyBlue;
+            this.label5.Size = new System.Drawing.Size(105, 23);
+            this.label5.TabIndex = 126;
+            this.label5.Text = "Arrive W/H Date";
+            this.label5.TextStyle.Color = System.Drawing.Color.Black;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(232, 44);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(16, 17);
+            this.label11.TabIndex = 140;
+            this.label11.Text = "~";
+            // 
+            // txtSP1
+            // 
+            this.txtSP1.BackColor = System.Drawing.Color.White;
+            this.txtSP1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtSP1.Location = new System.Drawing.Point(126, 41);
+            this.txtSP1.Name = "txtSP1";
+            this.txtSP1.Size = new System.Drawing.Size(100, 23);
+            this.txtSP1.TabIndex = 138;
+            // 
+            // txtSP2
+            // 
+            this.txtSP2.BackColor = System.Drawing.Color.White;
+            this.txtSP2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtSP2.Location = new System.Drawing.Point(254, 41);
+            this.txtSP2.Name = "txtSP2";
+            this.txtSP2.Size = new System.Drawing.Size(100, 23);
+            this.txtSP2.TabIndex = 139;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(18, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 23);
+            this.label1.TabIndex = 142;
+            this.label1.Text = "Brand";
+            // 
+            // txtBrand
+            // 
+            this.txtBrand.BackColor = System.Drawing.Color.White;
+            this.txtBrand.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtBrand.Location = new System.Drawing.Point(126, 70);
+            this.txtBrand.Name = "txtBrand";
+            this.txtBrand.Size = new System.Drawing.Size(66, 23);
+            this.txtBrand.TabIndex = 141;
+            // 
+            // labelRefno
+            // 
+            this.labelRefno.Location = new System.Drawing.Point(18, 99);
+            this.labelRefno.Name = "labelRefno";
+            this.labelRefno.Size = new System.Drawing.Size(105, 23);
+            this.labelRefno.TabIndex = 143;
+            this.labelRefno.Text = "Refno";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(232, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 146;
+            this.label2.Text = "~";
+            // 
+            // txtRefno1
+            // 
+            this.txtRefno1.BackColor = System.Drawing.Color.White;
+            this.txtRefno1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtRefno1.Location = new System.Drawing.Point(126, 99);
+            this.txtRefno1.Name = "txtRefno1";
+            this.txtRefno1.Size = new System.Drawing.Size(100, 23);
+            this.txtRefno1.TabIndex = 144;
+            // 
+            // txtRefno2
+            // 
+            this.txtRefno2.BackColor = System.Drawing.Color.White;
+            this.txtRefno2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtRefno2.Location = new System.Drawing.Point(254, 99);
+            this.txtRefno2.Name = "txtRefno2";
+            this.txtRefno2.Size = new System.Drawing.Size(100, 23);
+            this.txtRefno2.TabIndex = 145;
+            // 
+            // R11
+            // 
+            this.ClientSize = new System.Drawing.Size(496, 257);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtRefno1);
+            this.Controls.Add(this.txtRefno2);
+            this.Controls.Add(this.labelRefno);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBrand);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtSP1);
+            this.Controls.Add(this.txtSP2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.dateArriveWHDate);
+            this.Name = "R11";
+            this.OnLineHelpID = "Sci.Win.Tems.PrintForm";
+            this.Text = "R11. Sub-Process Inspection Report";
+            this.Controls.SetChildIndex(this.dateArriveWHDate, 0);
+            this.Controls.SetChildIndex(this.label10, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.label5, 0);
+            this.Controls.SetChildIndex(this.buttonCustomized, 0);
+            this.Controls.SetChildIndex(this.checkUseCustomized, 0);
+            this.Controls.SetChildIndex(this.txtVersion, 0);
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.txtSP2, 0);
+            this.Controls.SetChildIndex(this.txtSP1, 0);
+            this.Controls.SetChildIndex(this.label11, 0);
+            this.Controls.SetChildIndex(this.txtBrand, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.labelRefno, 0);
+            this.Controls.SetChildIndex(this.txtRefno2, 0);
+            this.Controls.SetChildIndex(this.txtRefno1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Win.UI.DateRange dateArriveWHDate;
+        private Win.UI.Label label10;
+        private Win.UI.Label label4;
+        private Win.UI.Label label5;
+        private System.Windows.Forms.Label label11;
+        private Win.UI.TextBox txtSP1;
+        private Win.UI.TextBox txtSP2;
+        private Win.UI.Label label1;
+        private Class.Txtbrand txtBrand;
+        private Win.UI.Label labelRefno;
+        private System.Windows.Forms.Label label2;
+        private Win.UI.TextBox txtRefno1;
+        private Win.UI.TextBox txtRefno2;
+    }
+}
---
+++ Sci.Production.Quality/R11.cs
@@ -0,0 +1,387 @@
+﻿using Ict;
+using Sci.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Sci.Production.Quality
+{
+    /// <inheritdoc/>
+    public partial class R11 : Win.Tems.PrintForm
+    {
+        private readonly List<SqlParameter> parameters = new List<SqlParameter>();
+        private readonly StringBuilder Sqlcmd = new StringBuilder();
+        private DataTable[] PrintData;
+
+        /// <inheritdoc/>
+        public R11(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            this.InitializeComponent();
+        }
+
+        /// <inheritdoc/>
+        protected override bool ValidateInput()
+        {
+            if (this.dateArriveWHDate.Value1.Empty() || this.dateArriveWHDate.Value2.Empty() || this.txtSP1.Text.Empty() || this.txtSP2.Text.Empty())
+            {
+                MyUtility.Msg.WarningBox("Arrive W/H Date and SP can't empty!");
+                return false;
+            }
+
+            this.Sqlcmd.Clear();
+            this.parameters.Clear();
+
+            string where1 = string.Empty;
+            string where2 = string.Empty;
+            if (!this.dateArriveWHDate.Value1.Empty())
+            {
+                where1 += $"and R.WhseArrival between @Date1 and @Date2" + Environment.NewLine;
+                where2 += $"and T.IssueDate between @Date1 and @Date2" + Environment.NewLine;
+                this.parameters.Add(new SqlParameter("@wDate1", this.dateArriveWHDate.Value1));
+                this.parameters.Add(new SqlParameter("@wDate2", this.dateArriveWHDate.Value2));
+            }
+
+            if (!this.txtSP1.Text.Empty())
+            {
+                where1 += $"and F.POID between @SP1 and @SP2" + Environment.NewLine;
+                where2 += $"and F.POID between @SP1 and @SP2" + Environment.NewLine;
+                this.parameters.Add(new SqlParameter("@SP1", this.txtSP1.Text));
+                this.parameters.Add(new SqlParameter("@SP2", this.txtSP2.Text));
+            }
+
+            if (!this.txtBrand.Text.Empty())
+            {
+                where1 += $"and O.BrandID = @BrandID" + Environment.NewLine;
+                where2 += $"and O.BrandID = @BrandID" + Environment.NewLine;
+                this.parameters.Add(new SqlParameter("@BrandID", this.txtBrand.Text));
+            }
+
+            if (!this.txtRefno1.Text.Empty())
+            {
+                where1 += $"and PSD.Refno >= @Refno1" + Environment.NewLine;
+                where2 += $"and PSD.Refno >= @Refno1" + Environment.NewLine;
+                this.parameters.Add(new SqlParameter("@Refno1", this.txtRefno1.Text));
+            }
+
+            if (!this.txtRefno2.Text.Empty())
+            {
+                where1 += $"and PSD.Refno <= @Refno2" + Environment.NewLine;
+                where2 += $"and PSD.Refno <= @Refno2" + Environment.NewLine;
+                this.parameters.Add(new SqlParameter("@Refno2", this.txtRefno2.Text));
+            }
+
+            this.Sqlcmd.Append($@"
+declare @Date1 date = @wDate1
+declare @Date2 date = @wDate2
+
+select
+	Supplier = concat(PS.ID,'-'+ S.AbbEN),
+	PSD.Refno,
+	PSD.ColorID,
+	Fabric.WeaveTypeID,
+	Composition.Composition,
+	Fabric.width,
+	Weight = RD.Weight * 1000,
+	Fabric.ConstructionID,
+	Description = dbo.getmtldesc(F.POID, F.SEQ1, f.SEQ2, 2, 0),
+	RD.ShipQty,
+	Rate = (select RateValue from dbo.View_Unitrate v where v.FROM_U = RD.PoUnit and v.TO_U= RD.StockUnit),
+	RD.StockQty,
+	F.POID,
+	R.ExportId,
+	RD.Dyelot,
+	F.Physical,
+	SEQ = CONCAT(F.SEQ1, '-' + F.SEQ2),
+	FP.Roll,
+	FP.InspDate,
+	FP.Result,
+	FP.Grade,
+	O.BrandID,
+	TotalDefectyds = isnull((select SUM(Point) from FIR_Physical_Defect fpd where FPD.FIR_PhysicalDetailUKey = FP.DetailUkey), 0) * 0.25,
+	FP.TicketYds,
+	RD.ID,
+	FP.DetailUkey,
+	FP.ActualYds
+into #tmp1
+from FIR F
+Inner join Receiving R on F.ReceivingID=R.Id
+Inner join Receiving_Detail RD on R.Id=RD.Id and F.POID=RD.PoId and F.SEQ1=RD.Seq1 and F.SEQ2=RD.Seq2
+Inner join PO_Supp_Detail PSD on PSD.ID=RD.PoId and PSD.SEQ1=RD.Seq1 and PSD.SEQ2=RD.Seq2
+Inner join PO_Supp PS on PSD.ID=PS.ID and PSD.SEQ1=PS.SEQ1
+Inner join Supp S on PS.SuppID=S.ID
+Inner join Fabric on PSD.SCIRefno=Fabric.SCIRefno
+Inner join Orders O on F.POID=O.ID
+Left join FIR_Physical FP on F.ID=FP.ID and FP.Roll=RD.Roll and FP.Dyelot=RD.Dyelot
+outer apply(
+	select Composition = STUFF((
+		select CONCAT('+', fc.percentage, '%')
+		from Fabric_Content fc
+		where fc.SCIRefno = Fabric.SCIRefno
+		for xml path('')
+	),1,1,'')
+)Composition
+
+Where 1=1
+{where1}
+
+select
+	Supplier = concat(PS.ID,'-'+ S.AbbEN),
+	PSD.Refno,
+	PSD.ColorID,
+	Fabric.WeaveTypeID,
+	Composition.Composition,
+	Fabric.width,
+	Weight = TD.Weight * 1000,
+	Fabric.ConstructionID,
+	Description = dbo.getmtldesc(F.POID, F.SEQ1, f.SEQ2, 2, 0),
+	TD.Qty,
+	F.POID,
+	TD.Dyelot,
+	F.Physical,
+	SEQ = CONCAT(F.SEQ1, '-' + F.SEQ2),
+	FP.Roll,
+	FP.InspDate,
+	FP.Result,
+	FP.Grade,
+	O.BrandID,
+	TotalDefectyds = isnull((select SUM(Point) from FIR_Physical_Defect fpd where FPD.FIR_PhysicalDetailUKey = FP.DetailUkey), 0) * 0.25,
+	FP.TicketYds,
+	TD.ID,
+	FP.DetailUkey,
+	FP.ActualYds
+into #tmp2
+from FIR F
+Inner join TransferIn T on F.ReceivingID=T.Id
+Inner join TransferIn_Detail TD on T.Id=TD.Id and F.POID=TD.PoId and F.SEQ1=TD.Seq1 and F.SEQ2=TD.Seq2
+Inner join PO_Supp_Detail PSD on PSD.ID=TD.PoId and PSD.SEQ1=TD.Seq1 and PSD.SEQ2=TD.Seq2
+Inner join PO_Supp PS on PSD.ID=PS.ID and PSD.SEQ1=PS.SEQ1
+Inner join Supp S on PS.SuppID=S.ID
+Inner join Fabric on PSD.SCIRefno=Fabric.SCIRefno
+Inner join Orders O on F.POID=O.ID
+Left join FIR_Physical FP on F.ID=FP.ID and FP.Roll=TD.Roll and FP.Dyelot=TD.Dyelot
+outer apply(
+	select Composition = STUFF((
+		select CONCAT('+', fc.percentage, '%')
+		from Fabric_Content fc
+		where fc.SCIRefno = Fabric.SCIRefno
+		for xml path('')
+	),1,1,'')
+)Composition
+Where 1=1
+{where2}
+
+--Summary分頁
+select
+	t.Supplier,
+	t.Refno,
+	t.ColorID,
+	t.WeaveTypeID,
+	t.Composition,
+	t.Width,
+	Weight = SUM(t.Weight),
+	t.ConstructionID,
+	t.Description,
+	ShippedQty = SUM(t.ShipQty * isnull(t.Rate,1)),
+	ArrivedQty = SUM(t.StockQty),
+	t.POID,
+	WK = t.ExportId,
+	t.Dyelot,
+	TotalDefectyds = Sum(TotalDefectyds),
+	RFT = iif(SUM(t.StockQty) = 0, 0, isnull(SUM(TotalDefectyds), 0) / SUM(t.StockQty)),
+	Inspection  = iif(SUM(TicketYds) = 0, 0, isnull(SUM(TicketYds), 0) / SUM(t.StockQty)),
+	t.Physical
+from #tmp1 t
+Group by t.Supplier,t.Refno,t.ColorID,t.WeaveTypeID,t.Composition,t.Width,
+t.ConstructionID,t.Description,t.POID,t.ExportId,t.SEQ,t.Dyelot,t.Physical,ID
+
+union all
+select
+	t.Supplier,
+	t.Refno,
+	t.ColorID,
+	t.WeaveTypeID,
+	t.Composition,
+	t.Width,
+	Weight = SUM(t.Weight),
+	t.ConstructionID,
+	t.Description,
+	ShippedQty = SUM(t.Qty),
+	ArrivedQty = SUM(t.Qty),
+	t.POID,
+	WK = '',
+	t.Dyelot,
+	TotalDefectyds = Sum(TotalDefectyds),
+	RFT = iif(SUM(t.Qty) = 0, 0, isnull(SUM(TotalDefectyds), 0) / SUM(t.Qty)),
+	Inspection  = iif(SUM(TicketYds) = 0, 0, isnull(SUM(TicketYds), 0) / SUM(t.Qty)),
+	t.Physical
+from #tmp2 t
+Group by t.Supplier,t.Refno,t.ColorID,t.WeaveTypeID,t.Composition,t.Width,
+t.ConstructionID,t.Description,t.POID,t.SEQ,t.Dyelot,t.Physical,ID
+
+--Defect_detail 分頁
+select
+	t.Supplier,
+	t.Refno,
+	t.ColorID,
+	t.WeaveTypeID,
+	t.Composition,
+	t.Width,
+	Weight = t.Weight,
+	t.ConstructionID,
+	t.Description,
+	t.POID,
+	t.seq,
+	WK = t.ExportId,
+	t.Dyelot,
+	t.Roll,
+	t.InspDate,
+	t.Result,
+	t.Grade,
+	Defect.DefectRecord,
+	fd.Type,
+	fd.DescriptionEN,
+	Defectrate = case when isnull(t.TicketYds, 0) = 0 then 0
+		when t.BrandID = 'LLL' and ISNULL(t.width, 0) = 0 then 0
+		when t.BrandID = 'LLL' then isnull(Defect.point, 0) * 3600 / (t.width * t.TicketYds)
+		else isnull(Defect.point,  0) / t.TicketYds * 100
+		end
+from #tmp1 t
+outer apply(
+	select DefectRecord = SUBSTRING(x.Data,1,1), point = sum(cast(SUBSTRING(x.Data,2,1) as int))
+	from FIR_Physical_Defect
+	outer apply(select  * from SplitString(DefectRecord,'/'))x
+	where FIR_PhysicalDetailUKey = t.DetailUkey
+	group by SUBSTRING(x.Data,1,1)
+)Defect
+left join FabricDefect fd on fd.ID = Defect.DefectRecord
+where t.InspDate is not null
+
+union all
+select
+	t.Supplier,
+	t.Refno,
+	t.ColorID,
+	t.WeaveTypeID,
+	t.Composition,
+	t.Width,
+	Weight = t.Weight,
+	t.ConstructionID,
+	t.Description,
+	t.POID,
+	t.seq,
+	WK = '',
+	t.Dyelot,
+	t.Roll,
+	t.InspDate,
+	t.Result,
+	t.Grade,
+	Defect.DefectRecord,
+	fd.Type,
+	fd.DescriptionEN,
+	Defectrate = case when isnull(t.TicketYds, 0) = 0 then 0
+		when t.BrandID = 'LLL' and ISNULL(t.width, 0) = 0 then 0
+		when t.BrandID = 'LLL' then isnull(Defect.point, 0) * 3600 / (t.width * t.TicketYds)
+		else isnull(Defect.point,  0) / t.TicketYds * 100
+		end
+from #tmp2 t
+outer apply(
+	select DefectRecord = SUBSTRING(x.Data,1,1), point = sum(cast(SUBSTRING(x.Data,2,1) as int))
+	from FIR_Physical_Defect
+	outer apply(select  * from SplitString(DefectRecord,'/'))x
+	where FIR_PhysicalDetailUKey = t.DetailUkey
+	group by SUBSTRING(x.Data,1,1)
+)Defect
+left join FabricDefect fd on fd.ID = Defect.DefectRecord
+where t.InspDate is not null
+
+--Lacking yard分頁
+select
+	t.Supplier,
+	t.Refno,
+	t.ColorID,
+	t.WeaveTypeID,
+	t.Composition,
+	t.Width,
+	Weight = t.Weight,
+	ShippedQty = t.ShipQty * isnull(t.Rate,1),
+	ArrivedQty = t.StockQty,
+	t.POID,
+	WK = t.ExportId,
+	t.Dyelot,
+	t.Roll,
+	t.TicketYds,
+	t.ActualYds,
+	isnull(t.TicketYds, 0) - isnull(t.ActualYds, 0),
+	t.InspDate
+from #tmp1 t
+where t.InspDate is not null
+
+
+union all
+select
+	t.Supplier,
+	t.Refno,
+	t.ColorID,
+	t.WeaveTypeID,
+	t.Composition,
+	t.Width,
+	Weight = t.Weight,
+	ShippedQty = t.Qty,
+	ArrivedQty = t.qty,
+	t.POID,
+	WK = '',
+	t.Dyelot,
+	t.Roll,
+	t.TicketYds,
+	t.ActualYds,
+	isnull(t.TicketYds, 0) - isnull(t.ActualYds, 0),
+	t.InspDate
+from #tmp2 t
+where t.InspDate is not null
+
+
+drop table #tmp1,#tmp2
+");
+            return base.ValidateInput();
+        }
+
+        /// <inheritdoc/>
+        protected override DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            return DBProxy.Current.Select(null, this.Sqlcmd.ToString(), this.parameters, out this.PrintData);
+        }
+
+        /// <inheritdoc/>
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            // 顯示筆數於PrintForm上Count欄位
+            this.SetCount(this.PrintData[0].Rows.Count);
+
+            if (this.PrintData[0].Rows.Count == 0)
+            {
+                MyUtility.Msg.WarningBox("Data not found!");
+                return false;
+            }
+
+            string excelName = "Quality_R11";
+            Excel.Application excelApp = MyUtility.Excel.ConnectExcel(Env.Cfg.XltPathDir + $"\\{excelName}.xltx");
+            MyUtility.Excel.CopyToXls(this.PrintData[0], string.Empty, $"{excelName}.xltx", 1, false, null, excelApp, wSheet: excelApp.Sheets[1]);
+            MyUtility.Excel.CopyToXls(this.PrintData[1], string.Empty, $"{excelName}.xltx", 1, false, null, excelApp, wSheet: excelApp.Sheets[2]);
+            MyUtility.Excel.CopyToXls(this.PrintData[2], string.Empty, $"{excelName}.xltx", 1, false, null, excelApp, wSheet: excelApp.Sheets[3]);
+            excelApp.Visible = true;
+
+            string strExcelName = Class.MicrosoftFile.GetName(excelName);
+            Excel.Workbook workbook = excelApp.ActiveWorkbook;
+            workbook.SaveAs(strExcelName);
+            Marshal.FinalReleaseComObject(excelApp);
+
+            return true;
+        }
+    }
+}
