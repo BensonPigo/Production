a30c1b3221f66549b51fd4a211b92faa3a29c168
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2020-10-08 17:25:17 +0800

    ISP20201607 - 加入Procedure、資料交換時Call API

--- DailyTransfer/imp_Order_Automation.sql
+++ DailyTransfer/imp_Order_Automation.sql
@@ -44,6 +44,7 @@ else
 
 	declare @orderList nvarchar(max);
 
+	-- Sunrise FinishingProcesses
 	select @Url = [dbo].[GetWebApiURL]('3A0134', 'FinishingProcesses') 
 	--需刪除的訂單
 	--M 單
@@ -97,4 +98,59 @@ else
 		drop table #tmpDeleteOrder
 	end
 
+	
+	-- Gensong FinishingProcesses
+	select @Url = [dbo].[GetWebApiURL]('3A0174', 'FinishingProcesses') 
+	
+	--需刪除的訂單
+	--M 單
+	--Cancel 訂單並且是不需要在生產的訂單
+	--SNP 請其他間工廠代工的訂單（g. N2E）
+	--訂單已轉到其他工廠（此部分就是目前使用 OrderComparisonList 的部分）
+	if(isnull(@Url, '') <> '')
+	begin
+		select a.OrderID into #tmpDeleteOrder
+		from (select [OrderID] = a.ID
+				from orders a with (nolock)
+				inner join  Factory f on a.FactoryID = f.ID
+				where	((a.AddDate >= @DateStart and a.AddDate < @DateEnd) or
+						(a.EditDate >= @DateStart and a.EditDate < @DateEnd) or
+						(a.PulloutCmplDate >= @DateStart and a.PulloutCmplDate < @DateEnd)) 
+						and
+						(a.Category = 'M' or
+						(a.junk = 1 and a.NeedProduction = 0) or
+						(f.IsProduceFty = 0))
+				union
+				select distinct ocl.OrderID 
+				from OrderComparisonList ocl with (nolock)
+				where ocl.UpdateDate >= @DateStart and
+					  ocl.DeleteOrder = 1 and
+					  not exists(
+						select 1
+						from orders o with (nolock)
+						where ocl.OrderId =  o.ID)) a
+
+		SELECT @orderList =  Stuff((select concat( ',',ID) 
+								from Production.dbo.Orders with (nolock)
+								where	((AddDate >= @DateStart and AddDate < @DateEnd) or
+										(EditDate >= @DateStart and EditDate < @DateEnd) or
+										(PulloutCmplDate >= @DateStart and PulloutCmplDate < @DateEnd)) 
+										and 
+										not exists (select 1 from #tmpDeleteOrder t where t.OrderID = Orders.ID)
+								FOR XML PATH('')),1,1,'') 
+		if(@orderList is not null and @orderList <> '')
+		begin
+			exec dbo.SentOrdersToFinishingProcesses @orderList,'Orders,Order_QtyShip'
+		end
+		
+
+		SELECT @orderList =  Stuff((select concat( ',',OrderId) from #tmpDeleteOrder FOR XML PATH('')),1,1,'') 
+
+		if(@orderList is not null and @orderList <> '')
+		begin
+			exec dbo.SentOrdersToFinishingProcesses @orderList,'Order_Delete'
+		end
+
+		drop table #tmpDeleteOrder
+	end
 END
