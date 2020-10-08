03745b64271ea8ef181df8f3e54d7d60cdabf32f
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2020-10-08 11:01:27 +0800

    ISP20201606 -  修正存檔後，FGWT表格顯示的SQL

--- Sci.Production.Quality/P04_Detail.cs
+++ Sci.Production.Quality/P04_Detail.cs
@@ -1176,14 +1176,14 @@ select [LocationText]= CASE WHEN Location='B' THEN 'Bottom'
         ,[Result]=IIF(f.Scale IS NOT NULL
 	        ,IIF( f.Scale='4-5' OR f.Scale ='5','Pass',IIF(f.Scale='','','Fail'))
 	        ,IIF( f.BeforeWash IS NOT NULL AND f.AfterWash IS NOT NULL AND f.Criteria IS NOT NULL AND f.Shrinkage IS NOT NULL
-					,( IIF( TestDetail ='%'
+					,( IIF( TestDetail = '%' OR TestDetail = 'Range%'   -- % 為ISP20201331舊資料、Range% 為ISP20201606加上的新資料，兩者都視作百分比
 								---- 百分比 判斷方式
-								,IIF( ISNULL(f.Criteria,0) * -1 <= ISNULL(f.Shrinkage,0) AND ISNULL(f.Shrinkage,0) <= ISNULL(f.Criteria,0)
+								,IIF( ISNULL(f.Criteria,0)  <= ISNULL(f.Shrinkage,0) AND ISNULL(f.Shrinkage,0) <= ISNULL(f.Criteria2,0)
 									, 'Pass'
 									, 'Fail'
 								)
 								---- 非百分比 判斷方式
-								,IIF( ISNULL(f.AfterWash,0) - ISNULL(f.BeforeWash,0) < ISNULL(f.Criteria,0)
+								,IIF( ISNULL(f.AfterWash,0) - ISNULL(f.BeforeWash,0) <= ISNULL(f.Criteria,0)
 									,'Pass'
 									,'Fail'
 								)
@@ -1194,6 +1194,7 @@ select [LocationText]= CASE WHEN Location='B' THEN 'Bottom'
         )
 		,gd.MtlTypeID
 		,f.Criteria
+		,f.Criteria2
 from GarmentTest_Detail_FGWT f 
 LEFT JOIN GarmentTest_Detail gd ON f.ID = gd.ID AND f.No = gd.NO
 where f.id = {this.Deatilrow["ID"]} and f.No = {this.Deatilrow["No"]} 
