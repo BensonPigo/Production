9a9361ea4f4695df573356a21854e1573566594d
jack.hsu <jack.hsu@sportscity.com.tw>
2020-11-06 09:50:48 +0800

    ISP20201762 - Cen R07、Planning R07 依需求調整。

--- Database/dbo/Stored Procedures/GetAdidasEfficiencyReport.sql
+++ Database/dbo/Stored Procedures/GetAdidasEfficiencyReport.sql
@@ -4,7 +4,8 @@
 	@M as varchar(8) = '',
 	@FactoryID as varchar(8) = '',
 	@CDCode as varchar(6) = '',
-	@Shift as varchar(1) = ''
+	@Shift as varchar(1) = '',
+	@IsSintexEffReportCompare as bit = 0
 AS
 BEGIN
 	declare @sql as varchar(max) = ''
@@ -19,7 +20,14 @@ select s.OutputDate
 	, s.FactoryID
 	, s.SewingLineID
 	, s.Shift
-	, [Category] = ''Inline''
+	, [Category] = case o.Category when ''B'' then ''Bulk''
+							   when ''S'' then ''Sample''
+							   when ''M'' then ''Material''
+							   when ''O'' then ''Other'' 
+							   when ''G'' then ''Garment''
+				               when ''T'' then ''SMLT''
+				else o.Category
+				end	
 	, o.StyleID
 	, s.Manpower
 	, [ManHour] = sum(sd.WorkHour)
@@ -30,42 +38,79 @@ select s.OutputDate
 	, [Fabrication] = ''=IFERROR(VLOOKUP(LEFT(INDIRECT(ADDRESS(ROW(), 10)), 2),''''Adidas data ''''!$A$2:$G$116, 4, FALSE), "")''
 	, [ProductGroup] = ''=IFERROR(VLOOKUP(LEFT(INDIRECT(ADDRESS(ROW(), 10)), 2),''''Adidas data ''''!$A$2:$G$116, 7, FALSE), "")''
 	, [ProductFabrication] = ''=INDIRECT(ADDRESS(ROW(), 14))&INDIRECT(ADDRESS(ROW(), 13))''
-	, [GSD] = ''''
+	, [GSD] = ''=INDIRECT(ADDRESS(ROW(),21)) * 1.1''
 	, [Earnedhours] = ''=IF(INDIRECT(ADDRESS(ROW(), 9))="","",IFERROR((INDIRECT(ADDRESS(ROW(), 9))*INDIRECT(ADDRESS(ROW(), 16)))/60,""))''
 	, [TotalWorkingHours] = ''=INDIRECT(ADDRESS(ROW(), 8))*INDIRECT(ADDRESS(ROW(), 7))''
 	, [CumulateDaysofDaysinProduction] = (select cumulate from dbo.getSewingOutputCumulateOfDays(o.StyleID, s.SewingLineID, s.OutputDate, s.FactoryID))
-	, [EfficiencyLine] = ''=INDIRECT(ADDRESS(ROW(), 17))/INDIRECT(ADDRESS(ROW(), 18))''
-	, [NoofInlineDefects] = sum(isnull(InlineInspection.RejectWIP, 0))
-	, [NoofEndlineDefectiveGarments] = sum(isnull(Inspection.cnt, 0))
-	, [WFT] = ''=IFERROR((INDIRECT(ADDRESS(ROW(), 21))+INDIRECT(ADDRESS(ROW(), 22)))/INDIRECT(ADDRESS(ROW(), 9)),"")''
+	, [EfficiencyLine] = ''=INDIRECT(ADDRESS(ROW(), 17))/INDIRECT(ADDRESS(ROW(), 18))''	
+	, [GSDProsmv] = SUM(iif(isnull(sd.QAQty, 0) = 0, 0, (o.CPU * o.CPUFactor * isnull([dbo].[GetOrderLocation_Rate](o.id, sd.ComboType), 100)) / sd.QAQty))
+	, [Earnedhours2] = ''=IF(INDIRECT(ADDRESS(ROW(),9))="","",IFERROR(INDIRECT(ADDRESS(ROW(),9))*INDIRECT(ADDRESS(ROW(),21))/60,""))''
+	, [EfficiencyLine2] = ''=INDIRECT(ADDRESS(ROW(),22))/INDIRECT(ADDRESS(ROW(),18))'''
+
+	if @IsSintexEffReportCompare = 0
+	begin
+		set @sql = @sql+ N'
+		, [NoofInlineDefects] = sum(isnull(InlineInspection.RejectWIP, 0))
+		, [NoofEndlineDefectiveGarments] = sum(isnull(Inspection.cnt, 0))
+		, [WFT] = ''=IFERROR((INDIRECT(ADDRESS(ROW(), 24))+INDIRECT(ADDRESS(ROW(), 25)))/INDIRECT(ADDRESS(ROW(), 9)),"")'''
+	end
+	else
+	begin
+		set @sql = @sql+ N'
+		, [NoofInlineDefects] = 0
+		, [NoofEndlineDefectiveGarments] = 0
+		, [WFT] = '''''
+	end
+
+
+	set @sql = @sql+ N'
+	, [Country] = case f.CountryID when ''PH'' then ''Philippines''
+					   when ''VN'' then ''Vietnam''
+					   when ''KH'' then ''Cambodia''
+					   when ''CN'' then ''China''
+				else f.CountryID end
+	  ,[Month] = Left(DATENAME(m, s.OutputDate),3)
+	  ,[IsGSDPro] = ''V''
+	  ,[Orderseq] = case f.CountryID when ''PH'' then 1
+					   when ''VN'' then 3
+					   when ''KH'' then 2
+					   when ''CN'' then 4
+					else 5 end
 from [Production].[dbo].SewingOutput s with (nolock)
 Inner join [Production].[dbo].SewingOutput_Detail sd with (nolock) on s.ID = sd.ID
 Inner Join [Production].[dbo].Orders o with (nolock) on sd.OrderId = o.ID
-Inner join [Production].[dbo].Factory f with (nolock) on o.FactoryID=f.ID
-Outer apply (
-	select [RejectWIP] = Sum(i.RejectWIP)
-	from SciMES_InlineInspection i with (nolock)
-	where  s.OutputDate = i.InspectionDate 
-	and s.SewingLineID = i.Line 
-	and s.FactoryID = i.FactoryID 
-	and s.Shift = iif(i.Shift = ''Day'', ''D'', ''N'')
-	and s.Team = i.Team 
-	and sd.OrderId = i.OrderID 
-	and sd.ComboType = i.Location 
-)InlineInspection
-Outer apply (
-	select cnt = Count(*)
-	from SciMES_Inspection i with (nolock) 
-	where s.OutputDate = i.InspectionDate 
-	and s.SewingLineID = i.Line 
-	and s.FactoryID = i.FactoryID 
-	and s.Shift = iif(i.Shift = ''Day'', ''D'', ''N'')
-	and s.Team = i.Team 
-	and sd.OrderId = i.OrderID 
-	and sd.ComboType = i.Location 
-	and sd.Article = i.Article
-	and i.Status in (''Fixed'',''Dispose'',''Reject'')
-)Inspection 
+Inner join [Production].[dbo].Factory f with (nolock) on o.FactoryID=f.ID'
+
+	if @IsSintexEffReportCompare = 0
+	begin
+		set @sql = @sql+ N'
+			Outer apply (
+				select [RejectWIP] = Sum(i.RejectWIP)
+				from SciMES_InlineInspection i with (nolock)
+				where  s.OutputDate = i.InspectionDate 
+				and sd.OrderId = i.OrderID 
+				and s.SewingLineID = i.Line 
+				and s.Shift = iif(i.Shift = ''Day'', ''D'', ''N'')
+				and s.FactoryID = i.FactoryID 
+				and sd.ComboType = i.Location 
+				and s.Team = i.Team
+			)InlineInspection
+			Outer apply (
+				select cnt = Count(*)
+				from SciMES_Inspection i with (nolock) 
+				where s.OutputDate = i.InspectionDate 
+				and sd.OrderId = i.OrderID 
+				and sd.Article = i.Article
+				and s.SewingLineID = i.Line 
+				and s.FactoryID = i.FactoryID 
+				and i.Status in (''Fixed'',''Dispose'',''Reject'')
+				and sd.ComboType = i.Location 
+				and s.Shift = iif(i.Shift = ''Day'', ''D'', ''N'')
+				and s.Team = i.Team 
+			)Inspection'
+	end
+
+set @sql = @sql+ N'
 Where  s.Outputdate between ''' + FORMAT(@OutPutDateS, 'yyyyMMdd') + ''' and  ''' + FORMAT(@OutPutDateE, 'yyyyMMdd') + '''
 and o.BrandID in (''Adidas'', ''Reebok'') 
 and o.category in (''B'', ''S'') 
@@ -101,7 +146,7 @@ and s.SHIFT = ''' + @Shift + ''''
 
 
 			set @sql = @sql + '
-Group by s.OutputDate, s.FactoryID, s.SewingLineID, s.Shift, o.StyleID, s.Manpower, o.CdCodeID, sd.ComboType, o.SeasonID, o.BrandID
+Group by s.OutputDate, s.FactoryID, s.SewingLineID, s.Shift, o.StyleID, s.Manpower, o.CdCodeID, sd.ComboType, o.SeasonID, o.BrandID, o.Category, f.CountryID
 order by s.OutputDate, s.FactoryID, s.SewingLineID, s.Shift, o.StyleID, o.SeasonID, o.BrandID' 
 
 	--print @sql
--- Sci.Production.Centralized/R07.cs
+++ Sci.Production.Centralized/R07.cs
@@ -10,6 +10,7 @@ using System.Xml.Linq;
 using System.Configuration;
 using System.Linq;
 using System.Data.SqlClient;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Sci.Production.Centralized
 {
@@ -25,7 +26,9 @@ namespace Sci.Production.Centralized
         private DateTime? outputDate1;
         private DateTime? outputDate2;
         private DataTable printData;
+        private int bolSintexEffReportCompare = 0;
         private StringBuilder condition = new StringBuilder();
+        private DateTime currentTime = DateTime.Now;
 
         /// <summary>
         /// R07
@@ -40,27 +43,48 @@ namespace Sci.Production.Centralized
             this.comboFactory.SetDefalutIndex(string.Empty);
         }
 
+        /// <inheritdoc/>
+        protected override void OnFormLoaded()
+        {
+            base.OnFormLoaded();
+            if (this.IsFormClosed)
+            {
+                return;
+            }
+
+            this.numYear.Value = this.currentTime.Year;
+        }
+
         /// <summary>
         /// ValidateInput
         /// </summary>
         /// <returns>bool</returns>
         protected override bool ValidateInput()
         {
-            if (MyUtility.Check.Empty(this.dateOutputDate.Value1))
+            if (MyUtility.Check.Empty(this.dateOutputDate.Value1) && this.radioDetail.Checked)
             {
                 MyUtility.Msg.WarningBox(" < Output Date > can't be empty!!");
                 return false;
             }
 
             #region -- 擇一必輸的條件 --
-            this.outputDate1 = this.dateOutputDate.Value1;
-            this.outputDate2 = this.dateOutputDate.Value2;
+            if (this.radioDetail.Checked)
+            {
+                this.outputDate1 = this.dateOutputDate.Value1;
+                this.outputDate2 = this.dateOutputDate.Value2;
+            }
+            else
+            {
+                this.outputDate1 = MyUtility.Convert.GetDate(this.numYear.Value.ToString() + "/01/01");
+                this.outputDate2 = MyUtility.Convert.GetDate(this.numYear.Value.ToString() + "/12/31");
+            }
             #endregion
 
             this.mdivision = this.comboM.Text;
             this.factory = this.comboFactory.Text;
             this.cdCode = this.txtCDCode.Text;
             this.shift = this.comboShift.SelectedValue.ToString();
+            this.bolSintexEffReportCompare = this.radioSintexEffReportCompare.Checked ? 1 : 0;
             return base.ValidateInput();
         }
 
@@ -72,15 +96,16 @@ namespace Sci.Production.Centralized
         protected override DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
         {
             string sqlCmd = string.Format(
-                "exec dbo.GetAdidasEfficiencyReport '{0}', '{1}', '{2}', '{3}', '{4}', '{5}'",
+                "exec dbo.GetAdidasEfficiencyReport '{0}', '{1}', '{2}', '{3}', '{4}', '{5}', {6}",
                 this.outputDate1.Value.ToString("yyyy/MM/dd"),
                 this.outputDate2.Value.ToString("yyyy/MM/dd"),
                 this.mdivision,
                 this.factory,
                 this.cdCode,
-                this.shift);
+                this.shift,
+                this.bolSintexEffReportCompare);
 
-            DBProxy.Current.DefaultTimeout = 1800;  // timeout時間改為30分鐘
+            DBProxy.Current.DefaultTimeout = 2700;  // timeout時間改為45分鐘
 
             #region --由 appconfig 抓各個連線路徑
             this.SetLoadingText("Load connections... ");
@@ -148,14 +173,21 @@ select t.OutputDate
 	, t.Fabrication
 	, t.ProductGroup
 	, t.ProductFabrication
-	, [GSD] = iif(isnull(sl.Rate, 0) = 0 or isnull(sq.TMS, 0) = 0, 0, (sq.TMS / 60) * (sl.Rate / 100))
+	, [GSD] = iif(isnull(sl.Rate, 0) = 0 or isnull(sq.TMS, 0) = 0, t.[GSD], cast((sq.TMS / 60) * (sl.Rate / 100) as varchar(100)))
 	, t.Earnedhours
 	, t.TotalWorkingHours
 	, t.CumulateDaysofDaysinProduction
-	, t.EfficiencyLine
+    , t.EfficiencyLine
+	, t.GSDProsmv
+	, t.Earnedhours2
+	, t.EfficiencyLine2
 	, t.NoofInlineDefects
 	, t.NoofEndlineDefectiveGarments
 	, t.WFT
+	, t.Country
+	, t.[Month]
+	, [IsGSDPro] = iif(isnull(sl.Rate, 0) = 0 or isnull(sq.TMS, 0) = 0, t.IsGSDPro, '')
+	, t.Orderseq
 from #tmp t
 left join Style s on t.StyleID = s.Id and t.BrandID = s.BrandID and t.SeasonID = s.SeasonID
 left join Style_Location sl on s.Ukey = sl.StyleUkey and RIGHT(t.CD, 1) = sl.Location
@@ -163,7 +195,7 @@ outer apply (
 	select TMS = sum(sq.TMS)
 	from Style_Quotation sq
 	where s.Ukey = sq.StyleUkey and sq.Article = ''
-	and sq.ArtworkTypeID in ('SEWING', 'PRESSING', 'PACKING')
+	and sq.ArtworkTypeID in ('SEWING', 'PRESSING', 'PACKING', 'Seamseal', 'Ultrasonic')
 )sq
 ";
 
@@ -210,23 +242,229 @@ outer apply (
                 return false;
             }
 
-            Microsoft.Office.Interop.Excel.Application objApp = MyUtility.Excel.ConnectExcel(Env.Cfg.XltPathDir + "\\Planning_R07.xltx"); // 預先開啟excel app
-            MyUtility.Excel.CopyToXls(this.printData, string.Empty, "Planning_R07.xltx", 1, false, null, objApp);      // 將datatable copy to excel
-            objApp.Visible = false;
+            Excel.Application objApp = MyUtility.Excel.ConnectExcel(Env.Cfg.XltPathDir + "\\Planning_R07.xltx"); // 預先開啟excel app
+            Excel.Worksheet objSheets1 = objApp.ActiveWorkbook.Worksheets[1];   // 取得工作表
+            Excel.Worksheet objSheets2 = objApp.ActiveWorkbook.Worksheets[2];   // 取得工作表
+
+            this.SetExcelSheet1(objApp, objSheets1);
+            if (this.radioDetail.Checked)
+            {
+                objSheets2.Visible = Excel.XlSheetVisibility.xlSheetHidden;
+            }
+            else
+            {
+                this.SetExcelSheet2(objSheets2);
+                objSheets1.get_Range("X:Z").EntireColumn.Hidden = true;
+            }
+
+            objSheets2.Columns.AutoFit();
 
             #region Save & Show Excel
             string strExcelName = Class.MicrosoftFile.GetName("Planning_R07");
-            Microsoft.Office.Interop.Excel.Workbook workbook = objApp.ActiveWorkbook;
+            Excel.Workbook workbook = objApp.ActiveWorkbook;
             workbook.SaveAs(strExcelName);
             workbook.Close();
             objApp.Quit();
             Marshal.ReleaseComObject(objApp);
+            Marshal.ReleaseComObject(objSheets1);
+            Marshal.ReleaseComObject(objSheets2);
             Marshal.ReleaseComObject(workbook);
 
             strExcelName.OpenFile();
             #endregion
             this.HideWaitMessage();
             return true;
+        }
+
+        private void SetExcelSheet1(Excel.Application objApp, Excel.Worksheet objSheets)
+        {
+            int cMax = 100000;
+            for (int i = 0; i <= this.printData.Rows.Count / cMax; i++)
+            {
+                int cSkip = cMax * i;
+                int cTake = i == 0 ? i : cSkip;
+                MyUtility.Excel.CopyToXls(
+                    this.printData.AsEnumerable().Skip(cSkip).Take(cMax).CopyToDataTable(),
+                    null,
+                    "Planning_R07.xltx",
+                    headerRow: cTake + 1,
+                    excelApp: objApp,
+                    showExcel: false,
+                    showSaveMsg: false,
+                    wSheet: objSheets);
+            }
+
+            objApp.Visible = false;
+            objSheets.get_Range("AA:AD").EntireColumn.Hidden = true;
+        }
+
+        private void SetExcelSheet2(Excel.Worksheet objSheets)
+        {
+            List<string> countrys = this.printData.AsEnumerable()
+                            .GroupBy(x => new { Country = x.Field<string>("Country"), OrderBySeq = x.Field<int>("Orderseq") })
+                            .OrderBy(x => x.Key.OrderBySeq)
+                            .Select(x => x.Key.Country)
+                            .ToList();
+
+            List<string> sessions = this.printData.AsEnumerable()
+                            .GroupBy(x => new { SeasonID = x.Field<string>("SeasonID") })
+                            .OrderBy(x => x.Key.SeasonID)
+                            .Select(x => x.Key.SeasonID)
+                            .ToList();
+
+            var countryAndMonth = this.printData.AsEnumerable()
+                            .OrderBy(x => x.Field<int>("Orderseq")).ThenBy(x => x.Field<DateTime?>("OutputDate"))
+                            .GroupBy(x => new { Country = x.Field<string>("Country"), Month = x.Field<string>("Month") })
+                            .Select(x => new { x.Key.Country, x.Key.Month })
+                            .ToList();
+
+            #region 上半部
+
+            for (int i = 0; i <= sessions.Count - 1; i++)
+            {
+                if (i > 0)
+                {
+                    objSheets.get_Range(string.Format("B{0}", i + 2)).Copy();
+                    objSheets.get_Range(string.Format("B{0}", i + 3)).PasteSpecial(Excel.XlPasteType.xlPasteAll, Excel.XlPasteSpecialOperation.xlPasteSpecialOperationNone, false, false);
+                }
+
+                objSheets.Cells[i + 3, 2] = sessions[i];
+            }
+
+            for (int i = 0; i <= countrys.Count - 1; i++)
+            {
+                objSheets.Cells[1, (i + 1) * 3] = countrys[i];
+                objSheets.get_Range(string.Format("{0}1:{1}1", MyUtility.Excel.ConvertNumericToExcelColumn((i + 1) * 3), MyUtility.Excel.ConvertNumericToExcelColumn(((i + 1) * 3) + 2))).Merge();
+
+                object[,] objArrayTop = new object[1, 3];
+                objArrayTop[0, 0] = "PRO Eff.";
+                objArrayTop[0, 1] = "SIO Eff.";
+                objArrayTop[0, 2] = "PRO/SIO Gap";
+                objSheets.Range[string.Format("{0}2:{1}2", MyUtility.Excel.ConvertNumericToExcelColumn((i + 1) * 3), MyUtility.Excel.ConvertNumericToExcelColumn(((i + 1) * 3) + 2))].Value2 = objArrayTop;
+
+                objSheets.get_Range(string.Format(
+                        "{0}1:{1}{2}",
+                        MyUtility.Excel.ConvertNumericToExcelColumn((i + 1) * 3),
+                        MyUtility.Excel.ConvertNumericToExcelColumn(((i + 1) * 3) + 2),
+                        sessions.Count + 2)).Interior.ColorIndex = this.SetExcelColor(countrys[i]);
+
+                objSheets.get_Range(string.Format(
+                        "{0}1:{1}{2}",
+                        MyUtility.Excel.ConvertNumericToExcelColumn((i + 1) * 3),
+                        MyUtility.Excel.ConvertNumericToExcelColumn(((i + 1) * 3) + 2),
+                        sessions.Count + 2)).Font.Name = "Segoe UI";
+
+                objSheets.get_Range(string.Format(
+                        "{0}1:{1}{2}",
+                        MyUtility.Excel.ConvertNumericToExcelColumn((i + 1) * 3),
+                        MyUtility.Excel.ConvertNumericToExcelColumn(((i + 1) * 3) + 2),
+                        sessions.Count + 2)).Font.Bold = true;
+
+                objSheets.get_Range(string.Format(
+                        "{0}1:{1}{2}",
+                        MyUtility.Excel.ConvertNumericToExcelColumn((i + 1) * 3),
+                        MyUtility.Excel.ConvertNumericToExcelColumn(((i + 1) * 3) + 2),
+                        sessions.Count + 2)).Borders.LineStyle = 1;
+
+                for (int j = 0; j <= sessions.Count - 1; j++)
+                {
+                    objArrayTop = new object[1, 3];
+                    objArrayTop[0, 0] = string.Format("=IFERROR(SUMIFS(Detail!V:V, Detail!AA:AA, ${0}$1, Detail!K:K, B{1}) / SUMIFS(Detail!R:R, Detail!AA:AA,${0}$1, Detail!K:K, B{1}), \"\")", MyUtility.Excel.ConvertNumericToExcelColumn((i + 1) * 3), j + 3);
+                    objArrayTop[0, 1] = string.Format("=IFERROR(SUMIFS(Detail!Q:Q, Detail!AA:AA, ${0}$1, Detail!K:K, B{1}) / SUMIFS(Detail!R:R,Detail!AA:AA, ${0}$1, Detail!K:K, B{1}), \"\")", MyUtility.Excel.ConvertNumericToExcelColumn((i + 1) * 3), j + 3);
+                    objArrayTop[0, 2] = string.Format("=IFERROR(INDIRECT(ADDRESS(ROW(), {0}))-INDIRECT(ADDRESS(ROW(), {1})), \"\")", ((i + 1) * 3) + 1, (i + 1) * 3);
+                    objSheets.Range[string.Format("{0}{2}:{1}{2}", MyUtility.Excel.ConvertNumericToExcelColumn((i + 1) * 3), MyUtility.Excel.ConvertNumericToExcelColumn(((i + 1) * 3) + 2), j + 3)].Value2 = objArrayTop;
+                }
+            }
+
+            #endregion
+
+            #region 下半部
+            int initR = 13;
+            int initC = 2;
+            int initI = 0;
+            string initCountry = string.Empty;
+            object[,] objArray = new object[1, 10];
+            foreach (var item in countryAndMonth)
+            {
+                if (initCountry.Empty())
+                {
+                    initCountry = item.Country;
+                }
+
+                objArray[0, 0] = item.Country;
+                objArray[0, 1] = item.Month;
+                objArray[0, 2] = string.Format("=SUMIFS(Detail!V:V, Detail!AA:AA, B{0}, Detail!AB:AB, C{0})", initR);
+                objArray[0, 3] = string.Format("=SUMIFS(Detail!R:R, Detail!AA:AA, B{0}, Detail!AB:AB, C{0})", initR);
+                objArray[0, 4] = "=IFERROR(ROUND(INDIRECT(ADDRESS(ROW(), 4))/INDIRECT(ADDRESS(ROW(), 5)),2), \"\")";
+                objArray[0, 5] = string.Format("=SUMIFS(Detail!Q:Q, Detail!AA:AA, B{0}, Detail!AB:AB, C{0})", initR);
+                objArray[0, 6] = string.Format("=SUMIFS(Detail!R:R, Detail!AA:AA, B{0}, Detail!AB:AB, C{0})", initR);
+                objArray[0, 7] = "=IFERROR(ROUND(INDIRECT(ADDRESS(ROW(), 7))/INDIRECT(ADDRESS(ROW(), 8)),2), \"\")";
+                objArray[0, 8] = "=IFERROR(INDIRECT(ADDRESS(ROW(), 9))-INDIRECT(ADDRESS(ROW(), 6)), \"\")";
+                objArray[0, 9] = string.Format("=IFERROR((SUMIFS(Detail!I:I, Detail!AA:AA, B{0}, Detail!AB:AB, C{0}, Detail!AC:AC, \"V\") * SUMIFS(Detail!P:P, Detail!AA:AA, B{0}, Detail!AB:AB, C{0}, Detail!AC:AC, \"V\")) / SUMIFS(Detail!Q:Q, Detail!AA:AA, B{0}, Detail!AB:AB, C{0}, Detail!AC:AC, \"V\"), \"\")", initR);
+                objSheets.Range[string.Format("B{0}:K{0}", initR)].Value2 = objArray;
+                objSheets.get_Range(string.Format("B{0}:C{0}", initR)).Interior.ColorIndex = this.SetExcelColor(item.Country);
+                objSheets.get_Range(string.Format("B{0}:K{0}", initR)).Borders.LineStyle = 1;
+                initR++;
+                initI++;
+
+                if (initI >= countryAndMonth.Count || !initCountry.EqualString(countryAndMonth[initI].Country))
+                {
+                    int rcount = countryAndMonth.Where(x => x.Country.EqualString(item.Country)).Count();
+
+                    objArray[0, 0] = item.Country;
+                    objArray[0, 1] = "YTD";
+                    objArray[0, 2] = string.Format("=Sum({0}{1}:{0}{2})", MyUtility.Excel.ConvertNumericToExcelColumn(initC + 2), initR - rcount, initR - 1);
+                    objArray[0, 3] = string.Format("=Sum({0}{1}:{0}{2})", MyUtility.Excel.ConvertNumericToExcelColumn(initC + 3), initR - rcount, initR - 1);
+                    objArray[0, 4] = "=IFERROR(ROUND(INDIRECT(ADDRESS(ROW(), 4))/INDIRECT(ADDRESS(ROW(), 5)),2), \"\")";
+                    objArray[0, 5] = string.Format("=Sum({0}{1}:{0}{2})", MyUtility.Excel.ConvertNumericToExcelColumn(initC + 5), initR - rcount, initR - 1);
+                    objArray[0, 6] = string.Format("=Sum({0}{1}:{0}{2})", MyUtility.Excel.ConvertNumericToExcelColumn(initC + 6), initR - rcount, initR - 1);
+                    objArray[0, 7] = "=IFERROR(ROUND(INDIRECT(ADDRESS(ROW(), 7))/INDIRECT(ADDRESS(ROW(), 8)),2), \"\")";
+                    objArray[0, 8] = "=IFERROR(INDIRECT(ADDRESS(ROW(), 9))-INDIRECT(ADDRESS(ROW(), 6)), \"\")";
+                    objArray[0, 9] = string.Format("=Sum({0}{1}:{0}{2})", MyUtility.Excel.ConvertNumericToExcelColumn(initC + 9), initR - rcount, initR - 1);
+                    objSheets.Range[string.Format("B{0}:K{0}", initR)].Value2 = objArray;
+                    objSheets.get_Range(string.Format("B{0}:C{0}", initR)).Interior.ColorIndex = this.SetExcelColor(item.Country);
+                    objSheets.get_Range(string.Format("B{0}:C{0}", initR)).Font.Bold = true;
+                    objSheets.get_Range(string.Format("B{0}:K{0}", initR)).Borders.LineStyle = 1;
+                    initCountry = initI >= countryAndMonth.Count ? item.Country : countryAndMonth[initI].Country;
+                    initR++;
+                }
+            }
+
+            #endregion
+        }
+
+        private int SetExcelColor(string country)
+        {
+            int rtnVal = 1;
+            switch (country.ToUpper())
+            {
+                case "PHILIPPINES":
+                    rtnVal = 17;
+                    break;
+                case "VIETNAM":
+                    rtnVal = 16;
+                    break;
+                case "CAMBODIA":
+                    rtnVal = 35;
+                    break;
+                case "CHINA":
+                    rtnVal = 45;
+                    break;
+            }
+
+            return rtnVal;
+        }
+
+        private void RadioDetail_CheckedChanged(object sender, EventArgs e)
+        {
+            this.dateOutputDate.Visible = true;
+            this.numYear.Visible = false;
+        }
+
+        private void RadioSintexEffReportCompare_CheckedChanged(object sender, EventArgs e)
+        {
+            this.dateOutputDate.Visible = false;
+            this.numYear.Visible = true;
         }
     }
 }
