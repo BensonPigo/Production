f17f692c75fe37e0300572c9453829f6e2cf7474
JEFF <jeff.yeh@sportscity.com.tw>
2020-11-16 13:41:57 +0800

    ISP20201821 ADIDASComplainDefect_Detail 結構調整 移除欄位 : Responsibility, MtlTypeID, FabricType 新增ADIDASComplainDefect_FabricType 每日資料交換 imp_Adicomp 新增轉入ADIDASComplainDefect_FabricType P40 來源ADIDASComplainDefect_Detail 改成ADIDASComplainDefect_FabricType

---
+++ Database/dbo/Tables/ADIDASComplainDefect_FabricType.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ADIDASComplainDefect_FabricType] (
+    [ID]             VARCHAR (2)    CONSTRAINT [DF_ADIDASComplainDefect_FabricType_ID] DEFAULT ('') NOT NULL,
+    [SubID]          VARCHAR (1)    CONSTRAINT [DF_ADIDASComplainDefect_FabricType_SubID] DEFAULT ('') NOT NULL,
+    [FabricType]     VARCHAR (1)    CONSTRAINT [DF_ADIDASComplainDefect_FabricType_FabricType] DEFAULT ('') NOT NULL,
+    [Responsibility] VARCHAR (2)    CONSTRAINT [DF_ADIDASComplainDefect_FabricType_Responsibility] DEFAULT ('') NOT NULL,
+    [MtlTypeID]      VARCHAR (5000) CONSTRAINT [DF_ADIDASComplainDefect_FabricType_MtlTypeID] DEFAULT ('') NOT NULL,
+    CONSTRAINT [PK_ADIDASComplainDefect_FabricType] PRIMARY KEY CLUSTERED ([ID] ASC, [SubID] ASC, [FabricType] ASC)
+);
+
