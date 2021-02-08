5c345f1b82ff9ac7f921a12cfcae9fcae16baef9
DOMAIN\aaron.shie <onizuka865@gmail.com>
2021-02-08 17:09:41 +0800

    ISP20210203 Shipping_B44. remove NL Code 2021 columns

--- Sci.Production.Shipping/B44.Designer.cs
+++ Sci.Production.Shipping/B44.Designer.cs
@@ -40,8 +40,6 @@
             this.numericBox2 = new Sci.Win.UI.NumericBox();
             this.numericBox3 = new Sci.Win.UI.NumericBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new Sci.Win.UI.Label();
-            this.displayNLCode2 = new Sci.Win.UI.DisplayBox();
             ((System.ComponentModel.ISupportInitialize)(this.gridbs)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.mtbs)).BeginInit();
             this.detail.SuspendLayout();
@@ -52,12 +50,11 @@
             // 
             // detail
             // 
-            this.detail.Size = new System.Drawing.Size(829, 327);
+            this.detail.Location = new System.Drawing.Point(4, 27);
+            this.detail.Size = new System.Drawing.Size(829, 325);
             // 
             // detailcont
             // 
-            this.detailcont.Controls.Add(this.displayNLCode2);
-            this.detailcont.Controls.Add(this.label2);
             this.detailcont.Controls.Add(this.label3);
             this.detailcont.Controls.Add(this.numericBox3);
             this.detailcont.Controls.Add(this.numericBox2);
@@ -70,16 +67,17 @@
             this.detailcont.Controls.Add(this.labelVietnamDesc);
             this.detailcont.Controls.Add(this.labelEnglishDesc);
             this.detailcont.Controls.Add(this.labelNLCode);
-            this.detailcont.Size = new System.Drawing.Size(829, 289);
+            this.detailcont.Size = new System.Drawing.Size(829, 287);
             // 
             // detailbtm
             // 
-            this.detailbtm.Location = new System.Drawing.Point(0, 289);
+            this.detailbtm.Location = new System.Drawing.Point(0, 287);
             this.detailbtm.Size = new System.Drawing.Size(829, 38);
             // 
             // browse
             // 
-            this.browse.Size = new System.Drawing.Size(829, 327);
+            this.browse.Location = new System.Drawing.Point(4, 27);
+            this.browse.Size = new System.Drawing.Size(829, 325);
             // 
             // tabs
             // 
@@ -87,12 +85,12 @@
             // 
             // createby
             // 
-            this.createby.Size = new System.Drawing.Size(350, 23);
+            this.createby.Size = new System.Drawing.Size(350, 24);
             // 
             // editby
             // 
             this.editby.Location = new System.Drawing.Point(471, 7);
-            this.editby.Size = new System.Drawing.Size(350, 23);
+            this.editby.Size = new System.Drawing.Size(350, 24);
             // 
             // lbleditby
             // 
@@ -102,7 +100,7 @@
             // 
             this.labelNLCode.Location = new System.Drawing.Point(22, 14);
             this.labelNLCode.Name = "labelNLCode";
-            this.labelNLCode.Size = new System.Drawing.Size(95, 23);
+            this.labelNLCode.Size = new System.Drawing.Size(111, 23);
             this.labelNLCode.TabIndex = 2;
             this.labelNLCode.Text = "Customs Code";
             // 
@@ -110,7 +108,7 @@
             // 
             this.labelEnglishDesc.Location = new System.Drawing.Point(22, 50);
             this.labelEnglishDesc.Name = "labelEnglishDesc";
-            this.labelEnglishDesc.Size = new System.Drawing.Size(95, 23);
+            this.labelEnglishDesc.Size = new System.Drawing.Size(111, 23);
             this.labelEnglishDesc.TabIndex = 3;
             this.labelEnglishDesc.Text = "English Desc";
             // 
@@ -118,7 +116,7 @@
             // 
             this.labelVietnamDesc.Location = new System.Drawing.Point(22, 87);
             this.labelVietnamDesc.Name = "labelVietnamDesc";
-            this.labelVietnamDesc.Size = new System.Drawing.Size(95, 23);
+            this.labelVietnamDesc.Size = new System.Drawing.Size(111, 23);
             this.labelVietnamDesc.TabIndex = 4;
             this.labelVietnamDesc.Text = "Vietnam Desc";
             // 
@@ -126,7 +124,7 @@
             // 
             this.labelVietnamUnit.Location = new System.Drawing.Point(22, 125);
             this.labelVietnamUnit.Name = "labelVietnamUnit";
-            this.labelVietnamUnit.Size = new System.Drawing.Size(95, 23);
+            this.labelVietnamUnit.Size = new System.Drawing.Size(111, 23);
             this.labelVietnamUnit.TabIndex = 5;
             this.labelVietnamUnit.Text = "Vietnam Unit";
             // 
@@ -135,9 +133,9 @@
             this.txtNLCode.BackColor = System.Drawing.Color.White;
             this.txtNLCode.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "NLCode", true));
             this.txtNLCode.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.txtNLCode.Location = new System.Drawing.Point(120, 14);
+            this.txtNLCode.Location = new System.Drawing.Point(136, 14);
             this.txtNLCode.Name = "txtNLCode";
-            this.txtNLCode.Size = new System.Drawing.Size(100, 23);
+            this.txtNLCode.Size = new System.Drawing.Size(100, 24);
             this.txtNLCode.TabIndex = 0;
             this.txtNLCode.PopUp += new System.EventHandler<Sci.Win.UI.TextBoxPopUpEventArgs>(this.TxtNLCode_PopUp);
             this.txtNLCode.Validating += new System.ComponentModel.CancelEventHandler(this.TxtNLCode_Validating);
@@ -148,9 +146,9 @@
             this.txtEnglishDesc.CharacterCasing = System.Windows.Forms.CharacterCasing.Normal;
             this.txtEnglishDesc.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "DescEN", true));
             this.txtEnglishDesc.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.txtEnglishDesc.Location = new System.Drawing.Point(120, 50);
+            this.txtEnglishDesc.Location = new System.Drawing.Point(136, 50);
             this.txtEnglishDesc.Name = "txtEnglishDesc";
-            this.txtEnglishDesc.Size = new System.Drawing.Size(479, 23);
+            this.txtEnglishDesc.Size = new System.Drawing.Size(479, 24);
             this.txtEnglishDesc.TabIndex = 6;
             // 
             // txtVietnamDesc
@@ -159,9 +157,9 @@
             this.txtVietnamDesc.CharacterCasing = System.Windows.Forms.CharacterCasing.Normal;
             this.txtVietnamDesc.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "DescVI", true));
             this.txtVietnamDesc.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.txtVietnamDesc.Location = new System.Drawing.Point(120, 87);
+            this.txtVietnamDesc.Location = new System.Drawing.Point(136, 87);
             this.txtVietnamDesc.Name = "txtVietnamDesc";
-            this.txtVietnamDesc.Size = new System.Drawing.Size(479, 23);
+            this.txtVietnamDesc.Size = new System.Drawing.Size(479, 24);
             this.txtVietnamDesc.TabIndex = 7;
             // 
             // txtVietnamUnit
@@ -170,16 +168,16 @@
             this.txtVietnamUnit.CharacterCasing = System.Windows.Forms.CharacterCasing.Normal;
             this.txtVietnamUnit.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "UnitVI", true));
             this.txtVietnamUnit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.txtVietnamUnit.Location = new System.Drawing.Point(120, 125);
+            this.txtVietnamUnit.Location = new System.Drawing.Point(136, 125);
             this.txtVietnamUnit.Name = "txtVietnamUnit";
-            this.txtVietnamUnit.Size = new System.Drawing.Size(68, 23);
+            this.txtVietnamUnit.Size = new System.Drawing.Size(68, 24);
             this.txtVietnamUnit.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.Location = new System.Drawing.Point(22, 163);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 23);
+            this.label1.Size = new System.Drawing.Size(111, 23);
             this.label1.TabIndex = 8;
             this.label1.Text = "Waste";
             // 
@@ -188,14 +186,14 @@
             this.numericBox2.BackColor = System.Drawing.Color.White;
             this.numericBox2.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "WasteLower", true));
             this.numericBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.numericBox2.Location = new System.Drawing.Point(120, 163);
+            this.numericBox2.Location = new System.Drawing.Point(136, 163);
             this.numericBox2.Name = "numericBox2";
             this.numericBox2.NullValue = new decimal(new int[] {
             0,
             0,
             0,
             0});
-            this.numericBox2.Size = new System.Drawing.Size(68, 23);
+            this.numericBox2.Size = new System.Drawing.Size(68, 24);
             this.numericBox2.TabIndex = 10;
             this.numericBox2.Value = new decimal(new int[] {
             0,
@@ -208,14 +206,14 @@
             this.numericBox3.BackColor = System.Drawing.Color.White;
             this.numericBox3.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "WasteUpper", true));
             this.numericBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.numericBox3.Location = new System.Drawing.Point(216, 163);
+            this.numericBox3.Location = new System.Drawing.Point(232, 163);
             this.numericBox3.Name = "numericBox3";
             this.numericBox3.NullValue = new decimal(new int[] {
             0,
             0,
             0,
             0});
-            this.numericBox3.Size = new System.Drawing.Size(68, 23);
+            this.numericBox3.Size = new System.Drawing.Size(68, 24);
             this.numericBox3.TabIndex = 11;
             this.numericBox3.Value = new decimal(new int[] {
             0,
@@ -226,29 +224,11 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(194, 169);
+            this.label3.Location = new System.Drawing.Point(210, 169);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(16, 17);
+            this.label3.Size = new System.Drawing.Size(17, 18);
             this.label3.TabIndex = 13;
             this.label3.Text = "~";
-            // 
-            // label2
-            // 
-            this.label2.Location = new System.Drawing.Point(223, 14);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(141, 23);
-            this.label2.TabIndex = 14;
-            this.label2.Text = "Customs Code(2021)";
-            // 
-            // displayNLCode2
-            // 
-            this.displayNLCode2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(227)))), ((int)(((byte)(255)))));
-            this.displayNLCode2.DataBindings.Add(new System.Windows.Forms.Binding("Value", this.mtbs, "NLCode2", true));
-            this.displayNLCode2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
-            this.displayNLCode2.Location = new System.Drawing.Point(367, 14);
-            this.displayNLCode2.Name = "displayNLCode2";
-            this.displayNLCode2.Size = new System.Drawing.Size(122, 23);
-            this.displayNLCode2.TabIndex = 15;
             // 
             // B44
             // 
@@ -288,7 +268,5 @@
         private Win.UI.NumericBox numericBox3;
         private Win.UI.NumericBox numericBox2;
         private Win.UI.Label label1;
-        private Win.UI.DisplayBox displayNLCode2;
-        private Win.UI.Label label2;
     }
 }
