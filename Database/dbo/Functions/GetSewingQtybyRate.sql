3b410621e3d4243fc73df84de8ba54161c7cb156
DOMAIN\aaron.shie <onizuka865@gmail.com>
2021-04-22 16:12:52 +0800

    ISP20210551 [R10. Factory Capacity by Month Report] - 月報及半月報 output日期準則 follow IST20210541 將 Trade..Planning_Report_R02 的變更 套用到 Production..Planning_Report_R10

--- Database/dbo/Functions/GetSewingQtybyRate.sql
+++ Database/dbo/Functions/GetSewingQtybyRate.sql
@@ -10,7 +10,8 @@ BEGIN
 		select @ret =sum(isnull(sdd.QAQty,0) * isnull(ol.Rate,0) / 100)
 		from Order_Location ol WITH (NOLOCK)
 		left join SewingOutput_Detail_Detail sdd WITH (NOLOCK) on sdd.OrderId = @orderid and sdd.ComboType = ol.Location
-		where ol.OrderID = @orderid
+		inner join SewingOutput so on sdd.ID = so.ID
+		where ol.OrderID = @orderid and so.LockDate is not null
 	END
 	ELSE
 	IF @sizecode is null
@@ -18,14 +19,16 @@ BEGIN
 		select @ret = sum(isnull(sdd.QAQty,0) * isnull(ol.Rate,0) / 100)
 		from Order_Location ol WITH (NOLOCK)
 		left join SewingOutput_Detail_Detail sdd WITH (NOLOCK) on sdd.OrderId = @orderid and sdd.ComboType = ol.Location and sdd.Article = @article
-		where ol.OrderID = @orderid
+		inner join SewingOutput so on sdd.ID = so.ID
+		where ol.OrderID = @orderid and so.LockDate is not null
 	END
 	ELSE
 	BEGIN
 		select @ret = sum(isnull(sdd.QAQty,0) * isnull(ol.Rate,0) / 100)
 		from Order_Location ol WITH (NOLOCK)
 		left join SewingOutput_Detail_Detail sdd WITH (NOLOCK) on sdd.OrderId = @orderid and sdd.ComboType = ol.Location and sdd.Article = @article and sdd.SizeCode = @sizecode
-		where ol.OrderID = @orderid
+		inner join SewingOutput so on sdd.ID = so.ID
+		where ol.OrderID = @orderid and so.LockDate is not null
 	END
 	
 	RETURN isnull(@ret,0);
