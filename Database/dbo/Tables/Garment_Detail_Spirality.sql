dfa0074b5dfa58a91e1eefb86691dd586059c7dd
DOMAIN\jeff.yeh <Jeff.Yeh@sportscity.com.tw>
2021-05-03 09:53:11 +0800

    ISP20210578 結構調整 GarmentTest_Detail_FGPT / SampleGarmentTest_Detail_FGPT 新增欄位 TypeSelection_VersionID TypeSelection_Seq
    新增資料表
    TypeSelection
    Garment_Detail_Spirality

---
+++ Database/dbo/Tables/Garment_Detail_Spirality.sql
@@ -0,0 +1,49 @@
+﻿CREATE TABLE [dbo].[Garment_Detail_Spirality] (
+    [ID]       BIGINT          NOT NULL,
+    [No]       INT             NOT NULL,
+    [Location] VARCHAR (1)     NOT NULL,
+    [AAPrime]  NUMERIC (11, 2) CONSTRAINT [DF_Garment_Detail_Spirality_AAPrime] DEFAULT ((0)) NOT NULL,
+    [APrimeB]  NUMERIC (11, 2) CONSTRAINT [DF_Garment_Detail_Spirality_APrimeB] DEFAULT ((0)) NOT NULL,
+    [AB]       NUMERIC (11, 2) CONSTRAINT [DF_Garment_Detail_Spirality_AB] DEFAULT ((0)) NOT NULL,
+    [CM]       NUMERIC (11, 2) CONSTRAINT [DF_Garment_Detail_Spirality_CM] DEFAULT ((0)) NOT NULL,
+    [MethodA]  NUMERIC (11, 2) CONSTRAINT [DF_Garment_Detail_Spirality_MethodA] DEFAULT ((0)) NOT NULL,
+    [MethodB]  NUMERIC (11, 2) CONSTRAINT [DF_Garment_Detail_Spirality_MethodB] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_Garment_Detail_Spirality] PRIMARY KEY CLUSTERED ([ID] ASC, [No] ASC, [Location] ASC)
+);
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Side Seam (%) side-panel (Method B)', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Garment_Detail_Spirality', @level2type = N'COLUMN', @level2name = N'MethodB';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Body A (%) within-panel (Method A)', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Garment_Detail_Spirality', @level2type = N'COLUMN', @level2name = N'MethodA';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Movement of side seam/outseam in CM', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Garment_Detail_Spirality', @level2type = N'COLUMN', @level2name = N'CM';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'參數 AB', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Garment_Detail_Spirality', @level2type = N'COLUMN', @level2name = N'AB';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'參數 A’B', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Garment_Detail_Spirality', @level2type = N'COLUMN', @level2name = N'APrimeB';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'參數 AA’', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Garment_Detail_Spirality', @level2type = N'COLUMN', @level2name = N'AAPrime';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'部位 (Top, Bottom)', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Garment_Detail_Spirality', @level2type = N'COLUMN', @level2name = N'Location';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Garment_Detail.No', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Garment_Detail_Spirality', @level2type = N'COLUMN', @level2name = N'No';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Garment_Detail.ID', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Garment_Detail_Spirality', @level2type = N'COLUMN', @level2name = N'ID';
+
--- Database/dbo/Tables/SampleGarmentTest_Detail_FGPT.sql
+++ Database/dbo/Tables/SampleGarmentTest_Detail_FGPT.sql
@@ -1,13 +1,25 @@
 ﻿CREATE TABLE [dbo].[SampleGarmentTest_Detail_FGPT] (
-    [ID]         BIGINT        NOT NULL,
-    [No]         INT           NOT NULL,
-    [Location]   VARCHAR (1)   NOT NULL,
-    [Type]       VARCHAR (150) NOT NULL,
-    [TestName]   VARCHAR (30)  CONSTRAINT [DF_SampleGarmentTest_Detail_FGPT_TestName] DEFAULT ('') NOT NULL,
-    [TestDetail] VARCHAR (10)  CONSTRAINT [DF_SampleGarmentTest_Detail_FGPT_TestDetail] DEFAULT ('') NOT NULL,
-    [Criteria]   INT           NULL,
-    [TestResult] VARCHAR (10)  CONSTRAINT [DF_SampleGarmentTest_Detail_FGPT_3Test] DEFAULT ('') NOT NULL,
-    [TestUnit]   VARCHAR (10)  CONSTRAINT [DF_SampleGarmentTest_Detail_FGPT_TestUnit] DEFAULT ('') NOT NULL,
+    [ID]                      BIGINT        NOT NULL,
+    [No]                      INT           NOT NULL,
+    [Location]                VARCHAR (1)   NOT NULL,
+    [Type]                    VARCHAR (150) NOT NULL,
+    [TestName]                VARCHAR (30)  CONSTRAINT [DF_SampleGarmentTest_Detail_FGPT_TestName] DEFAULT ('') NOT NULL,
+    [TestDetail]              VARCHAR (10)  CONSTRAINT [DF_SampleGarmentTest_Detail_FGPT_TestDetail] DEFAULT ('') NOT NULL,
+    [Criteria]                INT           NULL,
+    [TestResult]              VARCHAR (10)  CONSTRAINT [DF_SampleGarmentTest_Detail_FGPT_3Test] DEFAULT ('') NOT NULL,
+    [TestUnit]                VARCHAR (10)  CONSTRAINT [DF_SampleGarmentTest_Detail_FGPT_TestUnit] DEFAULT ('') NOT NULL,
+    [TypeSelection_VersionID] INT           CONSTRAINT [DF_SampleGarmentTest_Detail_FGPT_TypeSelection_VersionID] DEFAULT ((0)) NOT NULL,
+    [TypeSelection_Seq]       INT           CONSTRAINT [DF_SampleGarmentTest_Detail_FGPT_TypeSelection_Seq] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_SampleGarmentTest_Detail_FGPT] PRIMARY KEY CLUSTERED ([ID] ASC, [No] ASC, [Location] ASC, [Type] ASC)
 );
+
+
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Type 選項版本號', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SampleGarmentTest_Detail_FGPT', @level2type = N'COLUMN', @level2name = N'TypeSelection_VersionID';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Type 同版本下選擇的項目', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'SampleGarmentTest_Detail_FGPT', @level2type = N'COLUMN', @level2name = N'TypeSelection_Seq';
 
