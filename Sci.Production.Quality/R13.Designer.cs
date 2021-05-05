25251374126ef3de7d609ed525b948d97931df51
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2021-05-05 15:42:43 +0800

    ISP20210583 - 新增QA R13

---
+++ Sci.Production.Quality/R13.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace Sci.Production.Quality
+{
+    partial class R13
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label8 = new Sci.Win.UI.Label();
+            this.label7 = new Sci.Win.UI.Label();
+            this.label1 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.txtWK = new Sci.Win.UI.TextBox();
+            this.txtReceivingID = new Sci.Win.UI.TextBox();
+            this.dateArrWH = new Sci.Win.UI.DateBox();
+            this.radio8Slot = new Sci.Win.UI.RadioButton();
+            this.radio4Slot = new Sci.Win.UI.RadioButton();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(395, 12);
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(395, 48);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(395, 84);
+            // 
+            // buttonCustomized
+            // 
+            this.buttonCustomized.Location = new System.Drawing.Point(263, 101);
+            // 
+            // checkUseCustomized
+            // 
+            this.checkUseCustomized.Location = new System.Drawing.Point(276, 101);
+            // 
+            // txtVersion
+            // 
+            this.txtVersion.Location = new System.Drawing.Point(289, 105);
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label8.Location = new System.Drawing.Point(26, 72);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(105, 23);
+            this.label8.TabIndex = 157;
+            this.label8.Text = "Arrive W/H Date";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label7.Location = new System.Drawing.Point(26, 42);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 23);
+            this.label7.TabIndex = 156;
+            this.label7.Text = "Receiving ID";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(26, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 23);
+            this.label1.TabIndex = 155;
+            this.label1.Text = "WK#";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(26, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 23);
+            this.label2.TabIndex = 158;
+            this.label2.Text = "Print Format";
+            // 
+            // txtWK
+            // 
+            this.txtWK.BackColor = System.Drawing.Color.White;
+            this.txtWK.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtWK.Location = new System.Drawing.Point(134, 12);
+            this.txtWK.Name = "txtWK";
+            this.txtWK.Size = new System.Drawing.Size(132, 23);
+            this.txtWK.TabIndex = 159;
+            // 
+            // txtReceivingID
+            // 
+            this.txtReceivingID.BackColor = System.Drawing.Color.White;
+            this.txtReceivingID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtReceivingID.Location = new System.Drawing.Point(134, 41);
+            this.txtReceivingID.Name = "txtReceivingID";
+            this.txtReceivingID.Size = new System.Drawing.Size(132, 23);
+            this.txtReceivingID.TabIndex = 160;
+            // 
+            // dateArrWH
+            // 
+            this.dateArrWH.Location = new System.Drawing.Point(134, 72);
+            this.dateArrWH.Name = "dateArrWH";
+            this.dateArrWH.Size = new System.Drawing.Size(130, 23);
+            this.dateArrWH.TabIndex = 161;
+            // 
+            // radio8Slot
+            // 
+            this.radio8Slot.AutoSize = true;
+            this.radio8Slot.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.radio8Slot.Location = new System.Drawing.Point(202, 105);
+            this.radio8Slot.Name = "radio8Slot";
+            this.radio8Slot.Size = new System.Drawing.Size(62, 21);
+            this.radio8Slot.TabIndex = 163;
+            this.radio8Slot.Text = "8 Slot";
+            this.radio8Slot.UseVisualStyleBackColor = true;
+            // 
+            // radio4Slot
+            // 
+            this.radio4Slot.AutoSize = true;
+            this.radio4Slot.Checked = true;
+            this.radio4Slot.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.radio4Slot.Location = new System.Drawing.Point(134, 105);
+            this.radio4Slot.Name = "radio4Slot";
+            this.radio4Slot.Size = new System.Drawing.Size(62, 21);
+            this.radio4Slot.TabIndex = 162;
+            this.radio4Slot.TabStop = true;
+            this.radio4Slot.Text = "4 Slot";
+            this.radio4Slot.UseVisualStyleBackColor = true;
+            // 
+            // R13
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 169);
+            this.Controls.Add(this.radio8Slot);
+            this.Controls.Add(this.radio4Slot);
+            this.Controls.Add(this.dateArrWH);
+            this.Controls.Add(this.txtReceivingID);
+            this.Controls.Add(this.txtWK);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label1);
+            this.IsSupportToExcel = false;
+            this.Name = "R13";
+            this.OnLineHelpID = "Sci.Win.Tems.PrintForm";
+            this.Text = "R13. Shade Band Report";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.buttonCustomized, 0);
+            this.Controls.SetChildIndex(this.checkUseCustomized, 0);
+            this.Controls.SetChildIndex(this.txtVersion, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.label7, 0);
+            this.Controls.SetChildIndex(this.label8, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.txtWK, 0);
+            this.Controls.SetChildIndex(this.txtReceivingID, 0);
+            this.Controls.SetChildIndex(this.dateArrWH, 0);
+            this.Controls.SetChildIndex(this.radio4Slot, 0);
+            this.Controls.SetChildIndex(this.radio8Slot, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.Label label8;
+        private Win.UI.Label label7;
+        private Win.UI.Label label1;
+        private Win.UI.Label label2;
+        private Win.UI.TextBox txtWK;
+        private Win.UI.TextBox txtReceivingID;
+        private Win.UI.DateBox dateArrWH;
+        private Win.UI.RadioButton radio8Slot;
+        private Win.UI.RadioButton radio4Slot;
+    }
+}
---
+++ Sci.Production.Quality/R13.cs
@@ -0,0 +1,186 @@
+﻿using Ict;
+using Sci.Data;
+using Sci.Win;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Sci.Production.Quality
+{
+    /// <inheritdoc/>
+    public partial class R13 : Win.Tems.PrintForm
+    {
+        private string wkNO;
+        private string receivingID;
+        private string arrDate;
+        private string report_Type;
+
+        /// <inheritdoc/>
+        public R13(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            this.InitializeComponent();
+        }
+
+        /// <inheritdoc/>
+        protected override bool ValidateInput()
+        {
+            if (MyUtility.Check.Empty(this.txtWK.Text) || MyUtility.Check.Empty(this.txtReceivingID.Text) || MyUtility.Check.Empty(this.dateArrWH.Text))
+            {
+                MyUtility.Msg.InfoBox("WK# , Receiving ID, Arrive W/H Date can't be empty!!");
+                return false;
+            }
+
+            this.wkNO = this.txtWK.Text;
+            this.receivingID = this.txtReceivingID.Text;
+            this.arrDate = this.dateArrWH.Text;
+
+            if (this.radio4Slot.Checked)
+            {
+                this.report_Type = "4Slot";
+            }
+            else if (this.radio8Slot.Checked)
+            {
+                this.report_Type = "8Slot";
+            }
+
+            return true;
+        }
+
+        /// <inheritdoc/>
+        protected override DualResult OnAsyncDataLoad(ReportEventArgs e)
+        {
+            DualResult r = new DualResult(true);
+            string cmd = string.Empty;
+
+            DataTable head;
+
+            cmd = $@"
+SELECT 
+    a.id,
+    a.poid,
+    a.SEQ1,
+    a.SEQ2,
+    Receivingid,
+    a.Refno,
+    a.SCIRefno,
+    Suppid,
+    ArriveQty,
+    InspDeadline,
+    Result,
+    PhysicalEncode,
+    WeightEncode,
+    ShadeBondEncode,
+    ContinuityEncode,
+    NonPhysical,
+    Physical,
+    TotalInspYds,
+    PhysicalDate,
+    Physical,
+    NonWeight, 
+    Weight,
+    WeightDate,
+    Weight,
+    NonShadebond,
+    Shadebond,
+    ShadebondDate,
+    shadebond,
+    NonContinuity,
+    Continuity,
+    ContinuityDate,
+    Continuity,
+    a.Status,
+    ReplacementReportID,
+    (a.seq1+a.seq2) as seq,
+    (Select weavetypeid from Fabric b WITH (NOLOCK) where b.SCIRefno =a.SCIrefno) as weavetypeid,
+    c.Exportid,
+    [whseArrival] = isnull(c.whseArrival, ti.IssueDate),
+    dbo.getPass1(a.Approve) as approve1,
+    approveDate,
+    approve,
+    d.ColorID,
+    (Select ID+' - '+ AbbEn From Supp WITH (NOLOCK) Where a.suppid = supp.id) as SuppEn,
+    c.ExportID as Wkno
+    ,cn.name
+    ,a.nonOdor
+    ,a.Odor
+    ,a.OdorEncode
+    ,a.OdorDate
+    ,a.nonMoisture
+    ,a.Moisture
+    ,a.MoistureDate
+    ,[PhysicalInspector] = (select name from pass1 where id = a.PhysicalInspector)
+    ,[WeightInspector] = (select name from pass1 where id = a.WeightInspector)
+    ,[ShadeboneInspector] = (select name from pass1 where id = a.ShadeboneInspector)
+    ,[ContinuityInspector] = (select name from pass1 where id = a.ContinuityInspector)
+    ,[OdorInspector] = (select name from pass1 where id = a.OdorInspector)
+	,Moisture,
+	MoistureDate ,
+	MaterialCompositionGrp,
+	MaterialCompositionItem,
+	MoistureStandardDesc,
+	MoistureStandard1,
+	MoistureStandard2,
+	MoistureStandard1_Comparison,
+	MoistureStandard2_Comparison
+FROM FIR a WITH (NOLOCK) 
+Left join Receiving c WITH (NOLOCK) on c.ID = a.ReceivingID
+Left join TransferIn ti WITH (NOLOCK) on ti.id = a.receivingid
+inner join PO_Supp_Detail d WITH (NOLOCK) on d.id = a.poid and d.seq1 = a.seq1 and d.seq2 = a.seq2
+outer apply(select name from color WITH (NOLOCK) where color.id = d.colorid and color.BrandId = d.BrandId)cn
+WHERE c.ExportID='{this.wkNO}'
+AND c.WhseArrival='{this.arrDate}'
+AND a.ReceivingID='{this.receivingID}'
+";
+
+            r = DBProxy.Current.Select(string.Empty, cmd, out head);
+            if (!r)
+            {
+                this.ShowErr(r);
+                return r;
+            }
+
+            foreach (DataRow mainDr in head.Rows)
+            {
+                string txtsupplier = string.Empty;
+                string displayRefno = string.Empty;
+
+
+            }
+
+            if (this.report_Type == "4Slot")
+            {
+
+                DualResult result = DBProxy.Current.Select(string.Empty, cmd, out DataTable dt_title);
+
+                // 抓Invo,ETA 資料
+                cmd = $"select id,Eta from Export where id='{this.wkNO}' ";
+                result = DBProxy.Current.Select(string.Empty, cmd, out DataTable dt_Exp);
+                if (!result)
+                {
+                    this.ShowErr(result);
+                    return result;
+                }
+
+
+                // 變數區
+                string title = dt_title.Rows.Count == 0 ? string.Empty : dt_title.Rows[0]["NameEN"].ToString();
+                string suppid = this.txtsupplier.TextBox1.Text + " - " + this.txtsupplier.DisplayBox1.Text;
+                string invno = dt_Exp.Rows.Count == 0 ? string.Empty : dt_Exp.Rows[0]["ID"].ToString();
+                string brandID = MyUtility.GetValue.Lookup($"SELECT BrandID FROM Orders WHERE ID = '{this.displaySP.Text}'");
+
+            }
+
+            if (this.report_Type == "8Slot")
+            {
+
+            }
+
+            return r;
+        }
+    }
+}
