ed7801c9b5071329cecf19d5b646bd6d3fc3ca96
DOMAIN\jeff.yeh <Jeff.Yeh@sportscity.com.tw>
2021-05-10 15:53:10 +0800

    ISP20210590 新增[Clog][R04.Carton Status By Generate Date] 大致與R02相同 條件GenerateDate撈出此日期存在clog且還沒出貨資料

---
+++ Sci.Production.Logistic/R04.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace Sci.Production.Logistic
+{
+    partial class R04
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dateBuyerDelivery = new Sci.Win.UI.DateRange();
+            this.labelBuyerDelivery = new Sci.Win.UI.Label();
+            this.label6 = new Sci.Win.UI.Label();
+            this.txtPONo2 = new Sci.Win.UI.TextBox();
+            this.txtPONo1 = new Sci.Win.UI.TextBox();
+            this.labelPONo = new Sci.Win.UI.Label();
+            this.label7 = new Sci.Win.UI.Label();
+            this.txtSPNo2 = new Sci.Win.UI.TextBox();
+            this.txtSPNo1 = new Sci.Win.UI.TextBox();
+            this.labelSPNo = new Sci.Win.UI.Label();
+            this.txtbrand = new Sci.Production.Class.Txtbrand();
+            this.labelM = new Sci.Win.UI.Label();
+            this.labelBrand = new Sci.Win.UI.Label();
+            this.comboM = new Sci.Production.Class.ComboMDivision(this.components);
+            this.label1 = new Sci.Win.UI.Label();
+            this.dateGenerate = new Sci.Win.UI.DateBox();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Enabled = false;
+            this.print.Location = new System.Drawing.Point(414, 12);
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(414, 48);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(414, 84);
+            // 
+            // buttonCustomized
+            // 
+            this.buttonCustomized.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonCustomized.Location = new System.Drawing.Point(368, 120);
+            // 
+            // checkUseCustomized
+            // 
+            this.checkUseCustomized.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkUseCustomized.Location = new System.Drawing.Point(394, 156);
+            // 
+            // txtVersion
+            // 
+            this.txtVersion.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtVersion.Location = new System.Drawing.Point(394, 183);
+            // 
+            // dateBuyerDelivery
+            // 
+            // 
+            // 
+            // 
+            this.dateBuyerDelivery.DateBox1.Location = new System.Drawing.Point(0, 0);
+            this.dateBuyerDelivery.DateBox1.Name = "";
+            this.dateBuyerDelivery.DateBox1.Size = new System.Drawing.Size(129, 23);
+            this.dateBuyerDelivery.DateBox1.TabIndex = 0;
+            // 
+            // 
+            // 
+            this.dateBuyerDelivery.DateBox2.Location = new System.Drawing.Point(151, 0);
+            this.dateBuyerDelivery.DateBox2.Name = "";
+            this.dateBuyerDelivery.DateBox2.Size = new System.Drawing.Size(129, 23);
+            this.dateBuyerDelivery.DateBox2.TabIndex = 1;
+            this.dateBuyerDelivery.Location = new System.Drawing.Point(109, 67);
+            this.dateBuyerDelivery.Name = "dateBuyerDelivery";
+            this.dateBuyerDelivery.Size = new System.Drawing.Size(280, 23);
+            this.dateBuyerDelivery.TabIndex = 4;
+            // 
+            // labelBuyerDelivery
+            // 
+            this.labelBuyerDelivery.Location = new System.Drawing.Point(9, 67);
+            this.labelBuyerDelivery.Name = "labelBuyerDelivery";
+            this.labelBuyerDelivery.RectStyle.Color = System.Drawing.Color.SkyBlue;
+            this.labelBuyerDelivery.Size = new System.Drawing.Size(97, 23);
+            this.labelBuyerDelivery.TabIndex = 112;
+            this.labelBuyerDelivery.Text = "Buyer Delivery";
+            this.labelBuyerDelivery.TextStyle.Color = System.Drawing.Color.Black;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Location = new System.Drawing.Point(243, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(19, 23);
+            this.label6.TabIndex = 119;
+            this.label6.Text = "～";
+            this.label6.TextStyle.BorderColor = System.Drawing.Color.Black;
+            this.label6.TextStyle.Color = System.Drawing.Color.Black;
+            this.label6.TextStyle.ExtBorderColor = System.Drawing.Color.Black;
+            this.label6.TextStyle.GradientColor = System.Drawing.Color.Black;
+            // 
+            // txtPONo2
+            // 
+            this.txtPONo2.BackColor = System.Drawing.Color.White;
+            this.txtPONo2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtPONo2.Location = new System.Drawing.Point(267, 9);
+            this.txtPONo2.Name = "txtPONo2";
+            this.txtPONo2.Size = new System.Drawing.Size(130, 23);
+            this.txtPONo2.TabIndex = 1;
+            // 
+            // txtPONo1
+            // 
+            this.txtPONo1.BackColor = System.Drawing.Color.White;
+            this.txtPONo1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtPONo1.Location = new System.Drawing.Point(109, 9);
+            this.txtPONo1.Name = "txtPONo1";
+            this.txtPONo1.Size = new System.Drawing.Size(130, 23);
+            this.txtPONo1.TabIndex = 0;
+            // 
+            // labelPONo
+            // 
+            this.labelPONo.Location = new System.Drawing.Point(9, 9);
+            this.labelPONo.Name = "labelPONo";
+            this.labelPONo.RectStyle.Color = System.Drawing.Color.SkyBlue;
+            this.labelPONo.Size = new System.Drawing.Size(97, 23);
+            this.labelPONo.TabIndex = 116;
+            this.labelPONo.Text = "PO#";
+            this.labelPONo.TextStyle.Color = System.Drawing.Color.Black;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Location = new System.Drawing.Point(243, 38);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(19, 23);
+            this.label7.TabIndex = 123;
+            this.label7.Text = "～";
+            this.label7.TextStyle.BorderColor = System.Drawing.Color.Black;
+            this.label7.TextStyle.Color = System.Drawing.Color.Black;
+            this.label7.TextStyle.ExtBorderColor = System.Drawing.Color.Black;
+            this.label7.TextStyle.GradientColor = System.Drawing.Color.Black;
+            // 
+            // txtSPNo2
+            // 
+            this.txtSPNo2.BackColor = System.Drawing.Color.White;
+            this.txtSPNo2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtSPNo2.Location = new System.Drawing.Point(267, 38);
+            this.txtSPNo2.Name = "txtSPNo2";
+            this.txtSPNo2.Size = new System.Drawing.Size(130, 23);
+            this.txtSPNo2.TabIndex = 3;
+            // 
+            // txtSPNo1
+            // 
+            this.txtSPNo1.BackColor = System.Drawing.Color.White;
+            this.txtSPNo1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtSPNo1.Location = new System.Drawing.Point(109, 38);
+            this.txtSPNo1.Name = "txtSPNo1";
+            this.txtSPNo1.Size = new System.Drawing.Size(130, 23);
+            this.txtSPNo1.TabIndex = 2;
+            // 
+            // labelSPNo
+            // 
+            this.labelSPNo.Location = new System.Drawing.Point(9, 38);
+            this.labelSPNo.Name = "labelSPNo";
+            this.labelSPNo.RectStyle.Color = System.Drawing.Color.SkyBlue;
+            this.labelSPNo.Size = new System.Drawing.Size(97, 23);
+            this.labelSPNo.TabIndex = 120;
+            this.labelSPNo.Text = "SP#";
+            this.labelSPNo.TextStyle.Color = System.Drawing.Color.Black;
+            // 
+            // txtbrand
+            // 
+            this.txtbrand.BackColor = System.Drawing.Color.White;
+            this.txtbrand.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtbrand.Location = new System.Drawing.Point(109, 125);
+            this.txtbrand.Name = "txtbrand";
+            this.txtbrand.Size = new System.Drawing.Size(100, 23);
+            this.txtbrand.TabIndex = 9;
+            // 
+            // labelM
+            // 
+            this.labelM.Location = new System.Drawing.Point(11, 154);
+            this.labelM.Name = "labelM";
+            this.labelM.Size = new System.Drawing.Size(95, 23);
+            this.labelM.TabIndex = 125;
+            this.labelM.Text = "M";
+            // 
+            // labelBrand
+            // 
+            this.labelBrand.Location = new System.Drawing.Point(11, 125);
+            this.labelBrand.Name = "labelBrand";
+            this.labelBrand.Size = new System.Drawing.Size(95, 23);
+            this.labelBrand.TabIndex = 124;
+            this.labelBrand.Text = "Brand";
+            // 
+            // comboM
+            // 
+            this.comboM.BackColor = System.Drawing.Color.White;
+            this.comboM.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.comboM.FormattingEnabled = true;
+            this.comboM.IsSupportUnselect = true;
+            this.comboM.Location = new System.Drawing.Point(110, 154);
+            this.comboM.Name = "comboM";
+            this.comboM.OldText = "";
+            this.comboM.Size = new System.Drawing.Size(80, 24);
+            this.comboM.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(11, 96);
+            this.label1.Name = "label1";
+            this.label1.RectStyle.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label1.Size = new System.Drawing.Size(95, 23);
+            this.label1.TabIndex = 133;
+            this.label1.Text = "Generate Date";
+            this.label1.TextStyle.Color = System.Drawing.Color.Black;
+            // 
+            // dateGenerate
+            // 
+            this.dateGenerate.Location = new System.Drawing.Point(110, 96);
+            this.dateGenerate.Name = "dateGenerate";
+            this.dateGenerate.Size = new System.Drawing.Size(130, 23);
+            this.dateGenerate.TabIndex = 5;
+            // 
+            // R04
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 237);
+            this.Controls.Add(this.dateGenerate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboM);
+            this.Controls.Add(this.txtbrand);
+            this.Controls.Add(this.labelM);
+            this.Controls.Add(this.labelBrand);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtSPNo2);
+            this.Controls.Add(this.txtSPNo1);
+            this.Controls.Add(this.labelSPNo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtPONo2);
+            this.Controls.Add(this.txtPONo1);
+            this.Controls.Add(this.labelPONo);
+            this.Controls.Add(this.dateBuyerDelivery);
+            this.Controls.Add(this.labelBuyerDelivery);
+            this.IsSupportPrint = false;
+            this.IsSupportToPrint = false;
+            this.Name = "R04";
+            this.OnLineHelpID = "Sci.Win.Tems.PrintForm";
+            this.Text = "R04. Carton Status By Generate Date";
+            this.Controls.SetChildIndex(this.txtVersion, 0);
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.buttonCustomized, 0);
+            this.Controls.SetChildIndex(this.checkUseCustomized, 0);
+            this.Controls.SetChildIndex(this.labelBuyerDelivery, 0);
+            this.Controls.SetChildIndex(this.dateBuyerDelivery, 0);
+            this.Controls.SetChildIndex(this.labelPONo, 0);
+            this.Controls.SetChildIndex(this.txtPONo1, 0);
+            this.Controls.SetChildIndex(this.txtPONo2, 0);
+            this.Controls.SetChildIndex(this.label6, 0);
+            this.Controls.SetChildIndex(this.labelSPNo, 0);
+            this.Controls.SetChildIndex(this.txtSPNo1, 0);
+            this.Controls.SetChildIndex(this.txtSPNo2, 0);
+            this.Controls.SetChildIndex(this.label7, 0);
+            this.Controls.SetChildIndex(this.labelBrand, 0);
+            this.Controls.SetChildIndex(this.labelM, 0);
+            this.Controls.SetChildIndex(this.txtbrand, 0);
+            this.Controls.SetChildIndex(this.comboM, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.dateGenerate, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.DateRange dateBuyerDelivery;
+        private Win.UI.Label labelBuyerDelivery;
+        private Win.UI.Label label6;
+        private Win.UI.TextBox txtPONo2;
+        private Win.UI.TextBox txtPONo1;
+        private Win.UI.Label labelPONo;
+        private Win.UI.Label label7;
+        private Win.UI.TextBox txtSPNo2;
+        private Win.UI.TextBox txtSPNo1;
+        private Win.UI.Label labelSPNo;
+        private Class.Txtbrand txtbrand;
+        private Win.UI.Label labelM;
+        private Win.UI.Label labelBrand;
+        private Class.ComboMDivision comboM;
+        private Win.UI.Label label1;
+        private Win.UI.DateBox dateGenerate;
+    }
+}
---
+++ Sci.Production.Logistic/R04.cs
@@ -0,0 +1,266 @@
+﻿using Ict;
+using Sci.Data;
+using System;
+using System.Data;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sci.Production.Logistic
+{
+    /// <inheritdoc/>
+    public partial class R04 : Win.Tems.PrintForm
+    {
+        private string po1;
+        private string po2;
+        private string sp1;
+        private string sp2;
+        private string brand;
+        private string mDivision;
+        private DateTime? gdate;
+        private DataTable printData;
+        private StringBuilder sqlcmd = new StringBuilder();
+
+        /// <inheritdoc/>
+        public R04(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            this.InitializeComponent();
+            this.comboM.SetDefalutIndex(true);
+        }
+
+        /// <inheritdoc/>
+        protected override bool ValidateInput()
+        {
+            this.sqlcmd.Clear();
+            if (MyUtility.Check.Empty(this.dateGenerate.Value))
+            {
+                MyUtility.Msg.WarningBox("<Generate Date> Cannot be empty!!");
+                return false;
+            }
+
+            if ((!MyUtility.Check.Empty(this.txtPONo1.Text) && MyUtility.Check.Empty(this.txtPONo2.Text)) ||
+                (MyUtility.Check.Empty(this.txtPONo1.Text) && !MyUtility.Check.Empty(this.txtPONo2.Text)) ||
+                (!MyUtility.Check.Empty(this.txtSPNo1.Text) && MyUtility.Check.Empty(this.txtSPNo2.Text)) ||
+                (MyUtility.Check.Empty(this.txtSPNo1.Text) && !MyUtility.Check.Empty(this.txtSPNo2.Text)) ||
+                (MyUtility.Check.Empty(this.txtPONo1.Text) && MyUtility.Check.Empty(this.txtSPNo1.Text) && MyUtility.Check.Empty(this.dateBuyerDelivery.Value1)))
+            {
+                MyUtility.Msg.WarningBox("Please fill <PO#>, <SP#> or <Buyer Delivery>");
+                return false;
+            }
+
+            this.po1 = this.txtPONo1.Text;
+            this.po2 = this.txtPONo2.Text;
+            this.sp1 = this.txtSPNo1.Text;
+            this.sp2 = this.txtSPNo2.Text;
+            this.brand = this.txtbrand.Text;
+            this.mDivision = this.comboM.Text;
+            this.gdate = this.dateGenerate.Value;
+
+            #region Where
+            StringBuilder where = new StringBuilder();
+
+            if (!MyUtility.Check.Empty(this.txtPONo1.Text))
+            {
+                where.Append(string.Format("\r\nand o.CustPONo >= '{0}'", this.txtPONo1.Text));
+            }
+
+            if (!MyUtility.Check.Empty(this.txtPONo2.Text))
+            {
+                where.Append(string.Format("\r\nand o.CustPONo <= '{0}'", this.txtPONo2.Text));
+            }
+
+            if (!MyUtility.Check.Empty(this.txtSPNo1.Text))
+            {
+                where.Append(string.Format("\r\nand pd.OrderID >= '{0}'", this.txtSPNo1.Text));
+            }
+
+            if (!MyUtility.Check.Empty(this.txtSPNo2.Text))
+            {
+                where.Append(string.Format("\r\nand pd.OrderID <= '{0}'", this.txtSPNo2.Text));
+            }
+
+            if (!MyUtility.Check.Empty(this.dateBuyerDelivery.Value1))
+            {
+                where.Append(string.Format("\r\nand o.BuyerDelivery between '{0}' and '{1}'", Convert.ToDateTime(this.dateBuyerDelivery.Value1).ToString("yyyy/MM/dd"), Convert.ToDateTime(this.dateBuyerDelivery.Value2).ToString("yyyy/MM/dd")));
+            }
+
+            if (!MyUtility.Check.Empty(this.txtbrand.Text))
+            {
+                where.Append(string.Format("\r\nand o.BrandID = '{0}'", this.txtbrand.Text));
+            }
+
+            if (!MyUtility.Check.Empty(this.comboM.Text))
+            {
+                where.Append(string.Format("\r\nand p.MDivisionID = '{0}'", this.comboM.Text));
+            }
+
+            #endregion
+
+            this.sqlcmd.Append($@"
+declare @GenerateDate date = '{((DateTime)this.gdate).ToString("yyyy/MM/dd")}'
+select
+	p.MDivisionID,
+	o.FactoryID,
+	pd.OrderID,
+	o.StyleID,
+	PackingID = p.id,
+	pd.CTNStartNo,
+	ReceiveDate = (select MAX(ReceiveDate) from ClogReceive c where c.PackingListID = p.id and c.CTNStartNo = pd.CTNStartNo and ReceiveDate <= @GenerateDate),
+	o.CustPONo,
+	ClogLocationId=iif(pd.CFAReceiveDate is not null, 'CFA' ,pd.ClogLocationId),
+	p.BrandID,
+	Cancelled = iif(o.Junk=1, 'Y', 'N'),
+	TTLQty.TTLQty,
+	[QtyPerSize] = SizeCombo.combo,
+	[PulloutComplete] = case when o.qty > isnull(s.ShipQty,0) then 'S'
+							 when o.qty <= isnull(s.ShipQty,0) then'Y' end,
+	o.ActPulloutDate,
+	rea.reason
+from PackingList p WITH (NOLOCK) 
+inner join PackingList_Detail pd WITH (NOLOCK) on p.ID = pd.ID
+inner join Orders o WITH (NOLOCK) on o.ID = pd.OrderID
+outer apply(
+	select TTLQty = sum(pp.QtyPerCTN * iif(pp.CTNQty = 0, 1, pp.CTNQty)) 
+	from PackingList_Detail pp
+	where pp.ID= pd.ID and pp.CTNStartNo = pd.CTNStartNo
+) TTLQty
+outer apply(
+	select combo = Stuff((
+	    select concat('/',SizeCode+':'+ convert(varchar(10),QtyPerCTN))
+	    from(
+		    select distinct pp.SizeCode,pp.QtyPerCTN
+		    from PackingList_Detail pp
+		    where pp.ID=pd.ID and pp.CTNStartNo=pd.CTNStartNo
+	    )s
+	    for xml path('')
+	),1,1,'')
+) SizeCombo
+outer apply(
+	select ShipQty = sum(podd.ShipQty) 
+	from Pullout_Detail_Detail podd WITH (NOLOCK) 
+	inner join Order_Qty oq WITH (NOLOCK) on oq.id=podd.OrderID 
+	and podd.Article= oq.Article and podd.SizeCode=oq.SizeCode
+	where podd.OrderID = o.ID
+)s
+outer apply(
+	select top 1 reason=concat(c.ClogReasonID,'-'+cr.Description)
+	from ClogGarmentDispose_Detail cd
+	inner join ClogGarmentDispose c on c.ID=cd.ID
+	left join ClogReason cr on cr.id = c.ClogReasonID
+	where cd.PackingListID = p.ID
+)rea
+where pd.CTNQty > 0
+and pd.DisposeFromClog= 0
+and o.PulloutComplete = 1
+{where.ToString()}
+and ((select MAX(AddDate) from ClogReceive c where c.PackingListID = p.id and c.CTNStartNo = pd.CTNStartNo and ReceiveDate <= @GenerateDate)
+		> (select MAX(AddDate) from ClogReturn c where c.PackingListID = p.id and c.CTNStartNo = pd.CTNStartNo and ReturnDate <= @GenerateDate)
+	or (exists (select 1 from ClogReceive c where c.PackingListID = p.id and c.CTNStartNo = pd.CTNStartNo and ReceiveDate <= @GenerateDate) 
+		and not exists (select 1 from ClogReturn c where c.PackingListID = p.id and c.CTNStartNo = pd.CTNStartNo and ReturnDate <= @GenerateDate)))
+
+union  all
+
+select
+	p.MDivisionID,
+	o.FactoryID,
+	pd.OrderID,
+	o.StyleID,
+	PackingID = p.id,
+	pd.CTNStartNo,
+	ReceiveDate = (select MAX(ReceiveDate) from ClogReceive c where c.PackingListID = p.id and c.CTNStartNo = pd.CTNStartNo and ReceiveDate <= @GenerateDate),
+	o.CustPONo,
+	ClogLocationId=iif(pd.CFAReceiveDate is not null,pd.CFALocationID,pd.ClogLocationId),
+	p.BrandID,
+	Cancelled = iif(o.Junk=1, 'Y', 'N'),
+	TTLQty.TTLQty,
+	[QtyPerSize] = SizeCombo.combo,
+	[PulloutComplete] = 'N',
+	o.ActPulloutDate,
+	rea.reason
+from PackingList p WITH (NOLOCK) 
+inner join PackingList_Detail pd WITH (NOLOCK) on p.ID = pd.ID
+inner join Orders o WITH (NOLOCK) on o.ID = pd.OrderID
+left join Pullout po WITH (NOLOCK) on p.PulloutID = po.ID
+outer apply(
+	select TTLQty = sum(pp.QtyPerCTN * iif(pp.CTNQty = 0, 1, pp.CTNQty)) 
+	from PackingList_Detail pp
+	where pp.ID= pd.ID and pp.CTNStartNo = pd.CTNStartNo
+) TTLQty
+outer apply(
+	select combo = Stuff((
+	    select concat('/',SizeCode+':'+ convert(varchar(10),QtyPerCTN))
+	    from(
+		    select distinct pp.SizeCode,pp.QtyPerCTN
+		    from PackingList_Detail pp
+		    where pp.ID=pd.ID and pp.CTNStartNo=pd.CTNStartNo
+	    )s
+	    for xml path('')
+	),1,1,'')
+) SizeCombo
+outer apply(
+	select top 1 reason=concat(c.ClogReasonID,'-'+cr.Description)
+	from ClogGarmentDispose_Detail cd
+	inner join ClogGarmentDispose c on c.ID=cd.ID
+	left join ClogReason cr on cr.id = c.ClogReasonID
+	where cd.PackingListID = p.ID
+)rea
+where pd.CTNQty > 0
+and pd.DisposeFromClog= 0
+and o.PulloutComplete = 0
+and (p.PulloutID = '' or po.Status = 'New')
+{where.ToString()}
+and ((select MAX(AddDate) from ClogReceive c where c.PackingListID = p.id and c.CTNStartNo = pd.CTNStartNo and ReceiveDate <= @GenerateDate)
+		> (select MAX(AddDate) from ClogReturn c where c.PackingListID = p.id and c.CTNStartNo = pd.CTNStartNo and ReturnDate <= @GenerateDate)
+	or (exists (select 1 from ClogReceive c where c.PackingListID = p.id and c.CTNStartNo = pd.CTNStartNo and ReceiveDate <= @GenerateDate) 
+		and not exists (select 1 from ClogReturn c where c.PackingListID = p.id and c.CTNStartNo = pd.CTNStartNo and ReturnDate <= @GenerateDate)))
+");
+
+            return base.ValidateInput();
+        }
+
+        /// <inheritdoc/>
+        protected override DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            return DBProxy.Current.Select(null, this.sqlcmd.ToString(), out this.printData);
+        }
+
+        /// <inheritdoc/>
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            // 顯示筆數於PrintForm上Count欄位
+            this.SetCount(this.printData.Rows.Count);
+
+            if (this.printData.Rows.Count <= 0)
+            {
+                MyUtility.Msg.WarningBox("Data not found!");
+                return false;
+            }
+
+            this.ShowWaitMessage("Starting EXCEL...");
+            string strXltName = Env.Cfg.XltPathDir + "\\Logistic_R04.xltx";
+            Microsoft.Office.Interop.Excel.Application excel = MyUtility.Excel.ConnectExcel(strXltName);
+            MyUtility.Excel.CopyToXls(this.printData, string.Empty, "Logistic_R04.xltx", 4, false, null, excel);
+
+            Microsoft.Office.Interop.Excel.Worksheet worksheet = excel.ActiveWorkbook.Worksheets[1];
+            worksheet.Cells[2, 8] = ((DateTime)this.gdate).ToString("yyyy/MM/dd");
+            worksheet.Cells[2, 2] = this.po1 + " ~ " + this.po2;
+            worksheet.Cells[3, 2] = this.sp1 + " ~ " + this.sp2;
+            worksheet.Cells[2, 6] = this.brand;
+            worksheet.Cells[3, 6] = this.mDivision;
+
+            #region Save & Show Excel
+            string strExcelName = Class.MicrosoftFile.GetName("Logistic_R04");
+            Microsoft.Office.Interop.Excel.Workbook workbook = excel.ActiveWorkbook;
+            workbook.SaveAs(strExcelName);
+            workbook.Close();
+            excel.Quit();
+            Marshal.ReleaseComObject(excel);
+            strExcelName.OpenFile();
+            #endregion
+
+            this.HideWaitMessage();
+            return true;
+        }
+    }
+}
