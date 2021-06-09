f2bdf9e8a01623b7ece70d0f5ae3f75b3b3e10ca
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2021-06-09 11:06:14 +0800

    ISP20210767 - 暫時commit

--- Sci.Production.PublicForm/IntendedDeliveryDate.cs
+++ Sci.Production.PublicForm/IntendedDeliveryDate.cs
@@ -2,10 +2,12 @@
 using Ict.Win;
 using Sci.Data;
 using Sci.Production.Prg;
+using Sci.Win.Tools;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -64,6 +66,96 @@ namespace Sci.Production.PublicForm
         protected override void OnFormLoaded()
         {
             base.OnFormLoaded();
+            DataGridViewGeneratorTextColumnSettings reason = new DataGridViewGeneratorTextColumnSettings();
+
+            reason.EditingMouseDown += (s, e) =>
+            {
+                if (e.Button == MouseButtons.Right)
+                {
+                    if (this.EditMode)
+                    {
+                        if (e.RowIndex != -1)
+                        {
+                            DataRow dr = this.gridIDD.GetDataRow<DataRow>(e.RowIndex);
+
+                            string sqlCmd = $@"
+SELECT ID ,Description
+FROM ClogReason
+WHERE Type ='ID' AND Junk = 0
+ORDER BY ID
+";
+
+                            SelectItem item = new SelectItem(sqlCmd, "8,8", MyUtility.Convert.GetString(dr["Reason"]), headercaptions: "Reason,Reason Name");
+                            DialogResult returnResult = item.ShowDialog();
+                            if (returnResult == DialogResult.Cancel)
+                            {
+                                return;
+                            }
+
+                            IList<DataRow> colorData = item.GetSelecteds();
+
+                            dr["Reason"] = colorData[0]["ID"];
+                            dr["ReasonName"] = colorData[0]["Description"];
+
+                            dr.EndEdit();
+                        }
+                    }
+                }
+            };
+
+            reason.CellValidating += (s, e) =>
+            {
+                if (this.EditMode)
+                {
+
+                    DataRow dr = this.gridIDD.GetDataRow<DataRow>(e.RowIndex);
+                    if (MyUtility.Convert.GetString(e.FormattedValue) != MyUtility.Convert.GetString(dr["Reason"]))
+                    {
+                        if (MyUtility.Check.Empty(e.FormattedValue))
+                        {
+                            dr["Reason"] = string.Empty;
+                            dr["ReasonName"] = string.Empty;
+                            dr.EndEdit();
+                            return;
+                        }
+
+                        // sql參數
+                        SqlParameter sp1 = new SqlParameter("@id", MyUtility.Convert.GetString(dr["Reason"]));
+
+                        IList<SqlParameter> cmds = new List<SqlParameter>
+                        {
+                            sp1
+                        };
+
+                        DataTable dt;
+                        string sqlCmd = "select * from ClogReason where Id = @id and Type = 'ID' AND Junk = 0";
+                        DualResult result = DBProxy.Current.Select(null, sqlCmd, cmds, out dt);
+                        if (!result || dt.Rows.Count <= 0)
+                        {
+                            if (!result)
+                            {
+                                MyUtility.Msg.WarningBox("Sql connection fail!\r\n" + result.ToString());
+                            }
+                            else
+                            {
+                                MyUtility.Msg.WarningBox("Data not found!!!");
+                            }
+
+                            dr["Reason"] = string.Empty;
+                            dr["ReasonName"] = string.Empty;
+                            dr.EndEdit();
+                            e.Cancel = true;
+                            return;
+                        }
+                        else
+                        {
+                            dr["Reason"] = MyUtility.Convert.GetString(e.FormattedValue);
+                            dr["ReasonName"] = dt.Rows[0]["Description"];
+                            dr.EndEdit();
+                        }
+                    }
+                }
+            };
 
             this.Helper.Controls.Grid.Generator(this.gridIDD)
                 .Text("Seq", header: "Seq", width: Widths.AnsiChars(4), iseditingreadonly: true)
@@ -73,6 +165,8 @@ namespace Sci.Production.PublicForm
                 .EditText("Invoice", header: "Invoice#", width: Widths.AnsiChars(20), iseditingreadonly: true)
                 .CheckBox("AutoLock", header: "Auto Lock", trueValue: 1, falseValue: 0, width: Widths.AnsiChars(4), iseditable: false)
                 .Date("IDD", header: "Intended" + Environment.NewLine + "Delivery", width: Widths.AnsiChars(10), iseditingreadonly: false)
+                .Text("Reason", header: "Reason", width: Widths.AnsiChars(10), iseditingreadonly: false, settings: reason)
+                .Text("ReasonName", header: "Reason Name", width: Widths.AnsiChars(15), iseditingreadonly: true)
                 .Text("IDDEditName", header: "Edit By", width: Widths.AnsiChars(10), iseditingreadonly: true)
                 .DateTime("IDDEditDate", header: "Edit Date", width: Widths.AnsiChars(20), iseditingreadonly: true);
             this.QueryData();
@@ -91,7 +185,10 @@ select  oqs.Id,
         oqs.IDD,
         oqs.IDDEditName,
         oqs.IDDEditDate
+        ,Reason = cr.ID
+        ,ReasonName = cr.Description
 from    Order_QtyShip oqs with (nolock)
+LEFT JOIN ClogReason cr  with (nolock) ON cr.ID = oqs.ClogReasonID AND cr.Type='ID' AND cr.Junk = 0
 outer apply (SELECT val =  Stuff((  select  concat( ',',pl.INVNo)   
                                     from PackingList pl with (nolock) 
                                     where   exists(   select 1 
