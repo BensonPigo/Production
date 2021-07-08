bf5f7b5b18b35fc77139d96f9dbb34bce39576eb
jack.hsu <jack.hsu@sportscity.com.tw>
2021-07-08 09:15:03 +0800

    ISP20210845 - 新增Bundle View 與SP

---
+++ Database/dbo/Views/View_Bundle.sql
@@ -0,0 +1,16 @@
+﻿CREATE view [dbo].[View_Bundle]
+as 
+
+SELECT [ID], [POID], [MDivisionid], [Sizecode], [Colorid], [Article], [PatternPanel], [Cutno], [Cdate]
+	, [Orderid], [Sewinglineid], [Item], [SewingCell], [Ratio], [Startno], [Qty], [PrintDate], [AllPart], [CutRef]
+	, [AddName], [AddDate], [EditName], [EditDate], [oldid], [FabricPanelCode], [IsEXCESS], [ByToneGenerate], [SubCutNo]
+FROM Bundle WITH(NOLOCK)
+
+union ALL
+
+SELECT [ID], [POID], [MDivisionid], [Sizecode], [Colorid], [Article], [PatternPanel], [Cutno], [Cdate]
+	, [Orderid], [Sewinglineid], [Item], [SewingCell], [Ratio], [Startno], [Qty], [PrintDate], [AllPart], [CutRef]
+	, [AddName], [AddDate], [EditName], [EditDate], [oldid], [FabricPanelCode], [IsEXCESS], [ByToneGenerate], [SubCutNo]
+FROM Bundle_History WITH(NOLOCK)
+
+GO
---
+++ Database/dbo/Views/View_Bundle_Detail.sql
@@ -0,0 +1,14 @@
+﻿CREATE view [dbo].[View_Bundle_Detail]
+as 
+
+SELECT [BundleNo], [Id], [BundleGroup], [Patterncode], [PatternDesc], [SizeCode], [Qty], [Parts]
+	, [Farmin], [FarmOut], [PrintDate], [IsPair], [Location], [RFUID], [Tone], [RFPrintDate], [PrintGroup], [RFIDScan]
+FROM Bundle_Detail WITH(NOLOCK)
+
+union ALL
+
+SELECT [BundleNo], [Id], [BundleGroup], [Patterncode], [PatternDesc], [SizeCode], [Qty], [Parts]
+	, [Farmin], [FarmOut], [PrintDate], [IsPair], [Location], [RFUID], [Tone], [RFPrintDate], [PrintGroup], [RFIDScan]
+FROM Bundle_Detail_History WITH(NOLOCK)
+
+GO
