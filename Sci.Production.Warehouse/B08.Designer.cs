3ad75cf988405ad304204d0df3e34316f546b215
Willy <willy.wei@sprotscity.com.tw>
2021-09-10 13:45:50 +0800

    ISP20211133 新增WH.B08 程式

---
+++ Sci.Production.Warehouse/B08.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Sci.Production.Warehouse
+{
+    partial class B08
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new Sci.Win.UI.Label();
+            this.label2 = new Sci.Win.UI.Label();
+            this.label3 = new Sci.Win.UI.Label();
+            this.txtfactory1 = new Sci.Production.Class.Txtfactory();
+            this.editBox1 = new Sci.Win.UI.EditBox();
+            this.editBox2 = new Sci.Win.UI.EditBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).BeginInit();
+            this.detail.SuspendLayout();
+            this.detailcont.SuspendLayout();
+            this.detailbtm.SuspendLayout();
+            this.tabs.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // detail
+            // 
+            this.detail.Size = new System.Drawing.Size(792, 388);
+            // 
+            // detailcont
+            // 
+            this.detailcont.Controls.Add(this.editBox2);
+            this.detailcont.Controls.Add(this.editBox1);
+            this.detailcont.Controls.Add(this.txtfactory1);
+            this.detailcont.Controls.Add(this.label3);
+            this.detailcont.Controls.Add(this.label2);
+            this.detailcont.Controls.Add(this.label1);
+            this.detailcont.Size = new System.Drawing.Size(792, 350);
+            // 
+            // detailbtm
+            // 
+            this.detailbtm.Location = new System.Drawing.Point(0, 350);
+            this.detailbtm.Size = new System.Drawing.Size(792, 38);
+            // 
+            // browse
+            // 
+            this.browse.Size = new System.Drawing.Size(792, 388);
+            // 
+            // tabs
+            // 
+            this.tabs.Size = new System.Drawing.Size(800, 417);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(70, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Factory";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(70, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mail To";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(70, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "C.C.";
+            // 
+            // txtfactory1
+            // 
+            this.txtfactory1.BackColor = System.Drawing.Color.White;
+            this.txtfactory1.BoolFtyGroupList = true;
+            this.txtfactory1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "FactoryID", true));
+            this.txtfactory1.FilteMDivision = false;
+            this.txtfactory1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtfactory1.IsProduceFty = false;
+            this.txtfactory1.IssupportJunk = false;
+            this.txtfactory1.Location = new System.Drawing.Point(148, 29);
+            this.txtfactory1.MDivision = null;
+            this.txtfactory1.Name = "txtfactory1";
+            this.txtfactory1.Size = new System.Drawing.Size(66, 23);
+            this.txtfactory1.TabIndex = 0;
+            // 
+            // editBox1
+            // 
+            this.editBox1.BackColor = System.Drawing.Color.White;
+            this.editBox1.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "ToAddress", true));
+            this.editBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.editBox1.Location = new System.Drawing.Point(148, 58);
+            this.editBox1.Multiline = true;
+            this.editBox1.Name = "editBox1";
+            this.editBox1.Size = new System.Drawing.Size(502, 102);
+            this.editBox1.TabIndex = 1;
+            // 
+            // editBox2
+            // 
+            this.editBox2.BackColor = System.Drawing.Color.White;
+            this.editBox2.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.mtbs, "CCAddress", true));
+            this.editBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.editBox2.Location = new System.Drawing.Point(148, 166);
+            this.editBox2.Multiline = true;
+            this.editBox2.Name = "editBox2";
+            this.editBox2.Size = new System.Drawing.Size(502, 102);
+            this.editBox2.TabIndex = 2;
+            // 
+            // B08
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.DefaultControl = "txtfactory1";
+            this.DefaultControlForEdit = "txtfactory1";
+            this.IsSupportClip = false;
+            this.IsSupportCopy = false;
+            this.IsSupportPrint = false;
+            this.Name = "B08";
+            this.OnLineHelpID = "Sci.Win.Tems.Input1";
+            this.Text = "B08. MD Fail Mail To ";
+            this.WorkAlias = "MailGroup";
+            ((System.ComponentModel.ISupportInitialize)(this.gridbs)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mtbs)).EndInit();
+            this.detail.ResumeLayout(false);
+            this.detailcont.ResumeLayout(false);
+            this.detailcont.PerformLayout();
+            this.detailbtm.ResumeLayout(false);
+            this.detailbtm.PerformLayout();
+            this.tabs.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Win.UI.EditBox editBox2;
+        private Win.UI.EditBox editBox1;
+        private Class.Txtfactory txtfactory1;
+        private Win.UI.Label label3;
+        private Win.UI.Label label2;
+        private Win.UI.Label label1;
+    }
+}
---
+++ Sci.Production.Warehouse/B08.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sci.Production.Warehouse
+{
+    /// <inheritdoc/>
+    public partial class B08 : Sci.Win.Tems.Input1
+    {
+        /// <inheritdoc/>
+        public B08(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            this.InitializeComponent();
+            this.DefaultFilter = "Code = '104'";
+        }
+
+        /// <inheritdoc/>
+        protected override bool ClickSaveBefore()
+        {
+            if (MyUtility.Check.Empty(this.CurrentMaintain["FactoryID"]))
+            {
+                MyUtility.Msg.WarningBox($"Factory can't empty!");
+                return false;
+            }
+
+            if (this.IsDetailInserting)
+            {
+                string sqlcmd = $"select 1 from MailGroup where Code = '104' and FactoryID = '{this.CurrentMaintain["FactoryID"]}'";
+                if (MyUtility.Check.Seek(sqlcmd))
+                {
+                    MyUtility.Msg.WarningBox($"Factory {this.CurrentMaintain["FactoryID"]} already exists.");
+                    return false;
+                }
+            }
+
+            return base.ClickSaveBefore();
+        }
+
+        /// <inheritdoc/>
+        protected override void ClickNewAfter()
+        {
+            base.ClickNewAfter();
+            this.CurrentMaintain["Code"] = "104";
+        }
+    }
+}
