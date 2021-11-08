dfcb7ddf3655bdb31ec745aee57d4e0eee9ac846
jeff.yeh <Jeff.Yeh@sportscity.com.tw>
2021-11-08 17:08:31 +0800

    ISP20211280 Artwork內容會有複數資料,再用+號拆解去重複一次

--- Database/dbo/Functions/GetArtWork.sql
+++ Database/dbo/Functions/GetArtWork.sql
@@ -1,5 +1,4 @@
-﻿
-Create FUNCTION GetArtwork
+﻿Create FUNCTION GetArtwork
 (
 	@XML xml --= '<row Article="001" PatternUkey="233209"/><row Article="100" PatternUkey="233209"/>'
 	,@SizeCode varchar(8) -- 這只能輸入一個 Size = '3030'
@@ -13,13 +12,13 @@ BEGIN
 	declare @PatternUkey bigint =(select top 1  PatternUkey=Tbl.Col.value('@PatternUkey', 'bigint') FROM @xml.nodes('/row') Tbl(Col))
 
 	declare @Article table (Article varchar(8))
+
 	insert into @Article
 	select Article=Tbl.Col.value('@Article', 'varchar(8)')
 	FROM @xml.nodes('/row') Tbl(Col)
 
 	--找出要篩選哪些 ArticleGroup
 	declare  @ArticleGroup table (ArticleGroup varchar(6))
-
 	insert into @ArticleGroup
 	Select distinct ArticleGroup
 	from Pattern_GL_Article WITH (NOLOCK)
@@ -47,24 +46,32 @@ BEGIN
 		Select distinct ArticleGroup from Pattern_GL_Article WITH (NOLOCK) where PatternUkey = @PatternUkey 
 
 	select @Artwork = stuff((
-		Select distinct concat('+', pg.Annotation)
-		from Pattern_GL pg
-		inner join Pattern_GL_LectraCode pgl on pgl.PatternUKEY = pg. PatternUKEY and pgl.ID = pg.ID and pgl.Version = pg.Version and pgl.SEQ = pg.SEQ
-		Where pg.PatternUkey = @PatternUkey
-		and Annotation <> ''
-		and FabricPanelCode = @FabricPanelCode
-		and ArticleGroup in (select ArticleGroup from @ArticleGroup)
+		Select concat('+', s.Data)
+		from(
+			Select distinct s.Data
+			from Pattern_GL pg
+			inner join Pattern_GL_LectraCode pgl on pgl.PatternUKEY = pg. PatternUKEY and pgl.ID = pg.ID and pgl.Version = pg.Version and pgl.SEQ = pg.SEQ
+			outer apply(select * from SplitString(pg.Annotation, '+') s)s
+			Where pg.PatternUkey = @PatternUkey
+			and Annotation <> ''
+			and FabricPanelCode = @FabricPanelCode
+			and ArticleGroup in (select ArticleGroup from @ArticleGroup)
+		)s
 		for xml path('')
 	),1,1,'')
 
 	if isnull(@Artwork, '') = ''
 	select @Artwork = stuff((
-		Select distinct concat('+', pg.Annotation)
-		from Pattern_GL pg
-		inner join Pattern_GL_LectraCode pgl on pgl.PatternUKEY = pg. PatternUKEY and pgl.ID = pg.ID and pgl.Version = pg.Version and pgl.SEQ = pg.SEQ
-		Where pg.PatternUkey = @PatternUkey
-		and Annotation <> ''
-		and FabricPanelCode = @FabricPanelCode
+		Select concat('+', s.Data)
+		from(
+			Select distinct s.Data
+			from Pattern_GL pg
+			inner join Pattern_GL_LectraCode pgl on pgl.PatternUKEY = pg. PatternUKEY and pgl.ID = pg.ID and pgl.Version = pg.Version and pgl.SEQ = pg.SEQ
+			outer apply(select * from SplitString(pg.Annotation, '+') s)s
+			Where pg.PatternUkey = @PatternUkey
+			and Annotation <> ''
+			and FabricPanelCode = @FabricPanelCode
+		)s
 		for xml path('')
 	),1,1,'')
 
