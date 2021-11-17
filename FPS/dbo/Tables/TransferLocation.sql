e56fec3974bfa22a2b9260ed2a72dd6114cf9ee5
jeff.yeh <Jeff.Yeh@sportscity.com.tw>
2021-11-17 16:18:48 +0800

    ISP20211348 Table TransferLocation 與 MiniToPallet 欄位Pallet 結構改為 Varchar (50)
    PMS
    Packinglist_detail
    欄位Pallet 結構改為 Varchar (50)

---
+++ FPS/dbo/Tables/TransferLocation.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[TransferLocation] (
+    [ID]             BIGINT         NOT NULL,
+    [SCICtnNo]       VARCHAR (15)   NOT NULL,
+    [CustCTN]        VARCHAR (30)   NOT NULL,
+    [GW]             NUMERIC (7, 3) NULL,
+    [ClogLocationId] VARCHAR (10)   NULL,
+    [Pallet]         VARCHAR (50)   NULL,
+    [Time]           DATETIME       NOT NULL,
+    [SCIUpdate]      BIT            DEFAULT ((0)) NOT NULL,
+    [Type]           VARCHAR (15)   DEFAULT ('') NOT NULL,
+    CONSTRAINT [PK_TransferLocation] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Fty2Clog : 入MiniLoad倉後 & 退回的箱子再次入倉
+CFA2Clog : Clog 入庫 - 從 CFA 收回紙箱
+UpdLocation : Clog 儲位調整', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'TransferLocation', @level2type = N'COLUMN', @level2name = N'Type';
+
