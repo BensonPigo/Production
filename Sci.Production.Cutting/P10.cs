d14fba99bc680dee8965204ccd96841430b9249a
DOMAIN\jeff.yeh <Jeff.Yeh@sportscity.com.tw>
2022-01-27 12:13:10 +0800

    ISP20220138 Fix SP驗證

--- Sci.Production.Cutting/P10.cs
+++ Sci.Production.Cutting/P10.cs
@@ -1059,7 +1059,8 @@ and o.id = '{this.CurrentMaintain["POID"]}'
             else
             {
                 string selectCommand = $@"
-select id,POID,Seasonid,Styleid,sewline,styleukey,Sewline = case when SewLine like '%/%' then substring(Sewline,1,charindex('/',Sewline,1) - 1) else SewLine end
+select id,POID,Seasonid,Styleid,sewline,styleukey,Factoryid
+    ,Sewline = case when SewLine like '%/%' then substring(Sewline,1,charindex('/',Sewline,1) - 1) else SewLine end
 from orders a WITH (NOLOCK)
 where id = '{newvalue}' and mDivisionid='{this.keyword}' ";
                 if (MyUtility.Check.Seek(selectCommand, out DataRow cutdr, null))
