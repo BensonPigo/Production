c67185f55922725e30481447d0af726f2e484ad1
Willy <willy.wei@sprotscity.com.tw>
2022-02-08 13:55:34 +0800

    ISP20220137 * P03新增欄位[VAS/SHAS] , 新增sql value function [GVASSHAS_bit]

---
+++ Database/dbo/Functions/GetVASSHAS_bit.sql
@@ -0,0 +1,34 @@
+﻿-- =============================================
+-- Description:	Get VAS/SHAS bool
+-- =============================================
+CREATE FUNCTION [dbo].[GetVASSHAS_bit]
+(
+	@POID varchar(13),
+	@Seq1 varchar(3),
+	@Seq2 varchar(2)
+)
+RETURNS Bit
+AS
+BEGIN
+
+declare @value bit;
+	
+if exists(
+	select top 1 old.*
+	from PO_Supp_Detail psd
+	inner join Order_BOA ob on psd.SCIRefno = ob.SCIRefno
+	and psd.ID = ob.Id
+	and psd.SEQ1 = ob.Seq1
+	inner join Order_Label_Detail old on ob.Ukey = old.Order_BOAUkey
+	where psd.ID = @POID and psd.SEQ1= @Seq1 and psd.SEQ2 = @Seq2
+)
+	begin
+		set @value = 1
+	end
+else
+	begin 
+		set @value = 0
+	end
+
+	return @value;
+END
