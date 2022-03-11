775c5b67d6ab8382dab0b5c8ae67834d88860f3d
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2022-03-11 09:58:43 +0800

    ISP20220311 - 欄位與Trade同步

--- Database/dbo/Tables/Clip.sql
+++ Database/dbo/Tables/Clip.sql
@@ -2,7 +2,7 @@
     [PKey]        VARCHAR (12)  CONSTRAINT [DF_Clip_PKey] DEFAULT ('') NOT NULL,
     [TableName]   VARCHAR (50)  CONSTRAINT [DF_Clip_TableName] DEFAULT ('') NULL,
     [UniqueKey]   VARCHAR (80)  CONSTRAINT [DF_Clip_UniqueKey] DEFAULT ('') NULL,
-    [SourceFile]  NVARCHAR (60) CONSTRAINT [DF_Clip_SourceFile] DEFAULT ('') NULL,
+    [SourceFile]  NVARCHAR (266) CONSTRAINT [DF_Clip_SourceFile] DEFAULT ('') NULL,
     [Description] NVARCHAR (60) CONSTRAINT [DF_Clip_Description] DEFAULT ('') NULL,
     [AddName]     VARCHAR (10)  CONSTRAINT [DF_Clip_AddName] DEFAULT ('') NULL,
     [AddDate]     DATETIME      NULL,
