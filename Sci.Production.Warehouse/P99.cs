052564228175144f6caba5bfb5a0ad6f4ef286ef
DOMAIN\jeff.yeh <Jeff.Yeh@sportscity.com.tw>
2022-04-22 17:58:05 +0800

    ISP20211578  Revise 不用 isP99 參數 = true, 用 Ukey 重新撈數量. Delete 才要因為刪除後會找不到

--- Sci.Production.Warehouse/P99.cs
+++ Sci.Production.Warehouse/P99.cs
@@ -3575,16 +3575,16 @@ inner join #tmp s on t.Ukey = s.Ukey
 
                 if (!MyUtility.Check.Empty(errMsg))
                 {
-                    Gensong_AutoWHFabric.Sent(true, tmpDetailTableF, this.strFunction, EnumStatus.UnLock, EnumStatus.Unconfirm, fromNewBarcode: fromNewBarcode, isP99: true);
-                    Vstrong_AutoWHAccessory.Sent(true, tmpDetailTableA, this.strFunction, EnumStatus.UnLock, EnumStatus.Unconfirm, isP99: true);
+                    Gensong_AutoWHFabric.Sent(true, tmpDetailTableF, this.strFunction, EnumStatus.UnLock, EnumStatus.Unconfirm, fromNewBarcode: fromNewBarcode);
+                    Vstrong_AutoWHAccessory.Sent(true, tmpDetailTableA, this.strFunction, EnumStatus.UnLock, EnumStatus.Unconfirm);
                     this.ShowErr(errMsg);
                     return;
                 }
 
-                // PMS 更新之後,才執行WMS. Gensong 那邊必須先 Unlock 才能 Revise
-                Gensong_AutoWHFabric.Sent(false, tmpDetailTableF, this.strFunction, EnumStatus.UnLock, EnumStatus.Unconfirm, fromNewBarcode: fromNewBarcode, isP99: true);
-                Gensong_AutoWHFabric.Sent(true, tmpDetailTableF, this.strFunction, EnumStatus.Revise, EnumStatus.Confirm, fromNewBarcode: fromNewBarcode, isP99: true);
-                Vstrong_AutoWHAccessory.Sent(true, tmpDetailTableA, this.strFunction, EnumStatus.Revise, EnumStatus.Confirm, isP99: true);
+                // PMS 更新之後,才執行WMS. Gensong 那邊必須先 Unlock 才能 Revise. Revise 不用 isP99 參數 = true, 用 Ukey 重新撈數量. Delete 才要因為刪除後會找不到
+                Gensong_AutoWHFabric.Sent(false, tmpDetailTableF, this.strFunction, EnumStatus.UnLock, EnumStatus.Unconfirm, fromNewBarcode: fromNewBarcode);
+                Gensong_AutoWHFabric.Sent(true, tmpDetailTableF, this.strFunction, EnumStatus.Revise, EnumStatus.Confirm, fromNewBarcode: fromNewBarcode);
+                Vstrong_AutoWHAccessory.Sent(true, tmpDetailTableA, this.strFunction, EnumStatus.Revise, EnumStatus.Confirm);
 
                 // 將修改的資料存入Log
                 this.WriteInLog("Revise");
