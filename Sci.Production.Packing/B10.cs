548e83782dd699c8b5e61083c490e37445d088b4
Willy <willy.wei@sprotscity.com.tw>
2022-06-06 17:55:42 +0800

    ISP20220643 新增Packing.B10 & Centralized_Packing_B10

---
+++ Sci.Production.Packing/B10.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using Sci.Data;
+
+namespace Sci.Production.Packing
+{
+    /// <summary>
+    /// Packing_B09
+    /// </summary>
+    public partial class B10 : Win.Tems.Input1
+    {
+        /// <summary>
+        /// B09
+        /// </summary>
+        /// <param name="menuitem">menuitem</param>
+        public B10(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            this.InitializeComponent();
+        }
+    }
+}
