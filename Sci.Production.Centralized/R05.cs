4c8bb218ec8a6f819084ac914031a6c6451dd314
DOMAIN\ryan.tsao <ryan.tsao@sportscity.com.tw>
2022-11-03 09:47:30 +0800

    ISP20221354  [R05. Loading & Production output Summary] - 報表內增加欄位Balance(exclude Cancelled(StillNeedProd))-update -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- PMS Centralized [R05. Loading & Production output Summary] PMS Planning [R05. Loading & Production output Summary] 請協助調整【Balance(exclude Cancelled(StillNeedProd))】欄位計算規則：【Balance】-【Balance(Irregular)】

--- Sci.Production.Centralized/R05.cs
+++ Sci.Production.Centralized/R05.cs
@@ -191,7 +191,7 @@ select Date
     , BalanceCPU = iif(TransFtyZone = '{ftyZone}', 0, OrderCPU-sum(SewingOutputCPU))
     , OrderShortageCPU = iif(TransFtyZone = '{ftyZone}', 0,  OrderShortageCPU)
     , TransFtyZone
-    , BalanceExcludeCancelledStillNeedProd = iif(IsCancelNeedProduction  = 'Y', 0, OrderCPU - sum(SewingOutputCPU))
+    , BalanceExcludeCancelledStillNeedProd = iif(IsCancelNeedProduction  = 'Y', 0, ABS(OrderCPU - sum(SewingOutputCPU)))
     ,IsCancelNeedProduction
 into #tmp2_0
 from #tmp
@@ -206,7 +206,7 @@ select  Date
     , BalanceIrregularCPU = sum(iif(BalanceCPU >= 0, 0, BalanceCPU))
     , [OrderShortageCPU] = sum(OrderShortageCPU)
     , [SubconOutCPU] = sum(iif(TransFtyZone = '{ftyZone}', OrderCPU, 0))
-    , BalanceExcludeCancelledStillNeedProd =sum(iif(IsCancelNeedProduction  = 'Y', 0, iif(BalanceCPU >= 0, BalanceCPU, 0)-(iif(BalanceCPU >= 0, 0, BalanceCPU))))
+    ,BalanceExcludeCancelledStillNeedProd = sum(BalanceExcludeCancelledStillNeedProd)
 into #tmp2
 from #tmp2_0
 group by Date
