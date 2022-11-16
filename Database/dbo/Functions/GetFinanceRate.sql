008b212733955bf62c30448fdf47b266d03140eb
DOMAIN\ryan.tsao <ryan.tsao@sportscity.com.tw>
2022-11-16 15:03:18 +0800

    ISP20221420  [P02. Part Requisition to Purchase Order] - 新增欄位 & 欄位名稱修改 (便於比對當地與台北採購價格差異)
    Scalar-valued Functions: GetFinanceRate
    將裡面dbo.SciFMS_Rate全部改成FinanceRate

--- Database/dbo/Functions/GetFinanceRate.sql
+++ Database/dbo/Functions/GetFinanceRate.sql
@@ -13,37 +13,37 @@ BEGIN
 		RETURN 1
 
 	--正向
-	SELECT @returnRate = Rate FROM dbo.SciFMS_Rate WHERE RateTypeID = @rateTypeID AND OriginalCurrency = @originalCurrency AND ExchangeCurrency = @exchangeCurrency AND BeginDate <= @rateDate AND EndDate >= @rateDate
+	SELECT @returnRate = Rate FROM FinanceRate WHERE RateTypeID = @rateTypeID AND OriginalCurrency = @originalCurrency AND ExchangeCurrency = @exchangeCurrency AND BeginDate <= @rateDate AND EndDate >= @rateDate
 	IF @returnRate <> 0
 		RETURN @returnRate
 	
 	--反向
-	SELECT @returnRate = Rate FROM dbo.SciFMS_Rate WHERE RateTypeID = @rateTypeID AND OriginalCurrency = @exchangeCurrency AND ExchangeCurrency = @originalCurrency AND BeginDate <= @rateDate AND EndDate >= @rateDate
+	SELECT @returnRate = Rate FROM FinanceRate WHERE RateTypeID = @rateTypeID AND OriginalCurrency = @exchangeCurrency AND ExchangeCurrency = @originalCurrency AND BeginDate <= @rateDate AND EndDate >= @rateDate
 	IF @returnRate <> 0
 		RETURN 1 / @returnRate
 
 	--間接正向
-	SELECT @immediateRate = Rate FROM dbo.SciFMS_Rate WHERE RateTypeID = @rateTypeID AND OriginalCurrency = @originalCurrency AND ExchangeCurrency = 'TWD' AND BeginDate <= @rateDate AND EndDate >= @rateDate
+	SELECT @immediateRate = Rate FROM FinanceRate WHERE RateTypeID = @rateTypeID AND OriginalCurrency = @originalCurrency AND ExchangeCurrency = 'TWD' AND BeginDate <= @rateDate AND EndDate >= @rateDate
 	IF @immediateRate <> 0
 	BEGIN
-		SELECT @returnRate = Rate FROM dbo.SciFMS_Rate WHERE RateTypeID = @rateTypeID AND OriginalCurrency = 'TWD' AND ExchangeCurrency = @exchangeCurrency AND BeginDate <= @rateDate AND EndDate >= @rateDate
+		SELECT @returnRate = Rate FROM FinanceRate WHERE RateTypeID = @rateTypeID AND OriginalCurrency = 'TWD' AND ExchangeCurrency = @exchangeCurrency AND BeginDate <= @rateDate AND EndDate >= @rateDate
 		IF @returnRate <> 0
 			RETURN ROUND(@immediateRate * @returnRate, 8)
 
-		SELECT @returnRate = Rate FROM dbo.SciFMS_Rate WHERE RateTypeID = @rateTypeID AND OriginalCurrency = @exchangeCurrency AND ExchangeCurrency = 'TWD' AND BeginDate <= @rateDate AND EndDate >= @rateDate
+		SELECT @returnRate = Rate FROM FinanceRate WHERE RateTypeID = @rateTypeID AND OriginalCurrency = @exchangeCurrency AND ExchangeCurrency = 'TWD' AND BeginDate <= @rateDate AND EndDate >= @rateDate
 		IF @returnRate <> 0
 			RETURN ROUND(@immediateRate / @returnRate, 8)
 	END
 
 	--間接反向
-	SELECT @immediateRate = Rate FROM dbo.SciFMS_Rate WHERE RateTypeID = @rateTypeID AND OriginalCurrency = @exchangeCurrency AND ExchangeCurrency = 'TWD' AND BeginDate <= @rateDate AND EndDate >= @rateDate
+	SELECT @immediateRate = Rate FROM FinanceRate WHERE RateTypeID = @rateTypeID AND OriginalCurrency = @exchangeCurrency AND ExchangeCurrency = 'TWD' AND BeginDate <= @rateDate AND EndDate >= @rateDate
 	IF @immediateRate <> 0
 	BEGIN
-		SELECT @returnRate = Rate FROM dbo.SciFMS_Rate WHERE RateTypeID = @rateTypeID AND OriginalCurrency = 'TWD' AND ExchangeCurrency = @originalCurrency AND BeginDate <= @rateDate AND EndDate >= @rateDate
+		SELECT @returnRate = Rate FROM FinanceRate WHERE RateTypeID = @rateTypeID AND OriginalCurrency = 'TWD' AND ExchangeCurrency = @originalCurrency AND BeginDate <= @rateDate AND EndDate >= @rateDate
 		IF @returnRate <> 0
 			RETURN rOUND(1/ROUND(@immediateRate * @returnRate, 8),8)
 
-		SELECT @returnRate = Rate FROM dbo.SciFMS_Rate WHERE RateTypeID = @rateTypeID AND OriginalCurrency = @originalCurrency AND ExchangeCurrency = 'TWD' AND BeginDate <= @rateDate AND EndDate >= @rateDate
+		SELECT @returnRate = Rate FROM FinanceRate WHERE RateTypeID = @rateTypeID AND OriginalCurrency = @originalCurrency AND ExchangeCurrency = 'TWD' AND BeginDate <= @rateDate AND EndDate >= @rateDate
 		IF @returnRate <> 0
 			RETURN rOUND(1/ROUND(@immediateRate / @returnRate, 8),8)
 	END
