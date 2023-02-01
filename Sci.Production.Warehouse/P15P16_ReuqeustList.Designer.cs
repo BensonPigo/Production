4a2a0d3dfb0394dcd4a65d59a60f3764d2003042
DOMAIN\ryan.tsao <ryan.tsao@sportscity.com.tw>
2023-02-01 13:42:35 +0800

    ISP20230048  [P15. Issue Accessory Lacking && Replacement] - PPIC 補料相關內容沒有傳到倉庫補料系統

---
+++ Sci.Production.Warehouse/P15P16_ReuqeustList.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Sci.Production.Warehouse
+{
+    partial class P15P16_ReuqeustList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel2 = new Sci.Win.UI.Panel();
+            this.btnClose = new Sci.Win.UI.Button();
+            this.bindingSource1 = new Sci.Win.UI.BindingSource(this.components);
+            this.gridAccumulatedQty = new Sci.Win.UI.Grid();
+            this.panel1 = new Sci.Win.UI.Panel();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridAccumulatedQty)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnClose);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 402);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 48);
+            this.panel2.TabIndex = 3;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnClose.Location = new System.Drawing.Point(717, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(80, 30);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // gridAccumulatedQty
+            // 
+            this.gridAccumulatedQty.AllowUserToAddRows = false;
+            this.gridAccumulatedQty.AllowUserToDeleteRows = false;
+            this.gridAccumulatedQty.AllowUserToResizeRows = false;
+            this.gridAccumulatedQty.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.gridAccumulatedQty.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            this.gridAccumulatedQty.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridAccumulatedQty.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridAccumulatedQty.EditingEnter = Ict.Win.UI.DataGridViewEditingEnter.NextCellOrNextRow;
+            this.gridAccumulatedQty.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.gridAccumulatedQty.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.gridAccumulatedQty.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(162)))), ((int)(((byte)(163)))));
+            this.gridAccumulatedQty.Location = new System.Drawing.Point(0, 0);
+            this.gridAccumulatedQty.Name = "gridAccumulatedQty";
+            this.gridAccumulatedQty.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.gridAccumulatedQty.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+            this.gridAccumulatedQty.RowTemplate.Height = 24;
+            this.gridAccumulatedQty.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridAccumulatedQty.ShowCellToolTips = false;
+            this.gridAccumulatedQty.Size = new System.Drawing.Size(800, 450);
+            this.gridAccumulatedQty.TabIndex = 1;
+            this.gridAccumulatedQty.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.gridAccumulatedQty);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 4;
+            // 
+            // P15P16_ReuqeustList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "P15P16_ReuqeustList";
+            this.OnLineHelpID = "Sci.Win.Subs.Base";
+            this.Text = "ReuqeustList";
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridAccumulatedQty)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Win.UI.Panel panel2;
+        private Win.UI.Button btnClose;
+        private Win.UI.BindingSource bindingSource1;
+        private Win.UI.Grid gridAccumulatedQty;
+        private Win.UI.Panel panel1;
+    }
+}
