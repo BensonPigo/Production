12b40649cec7fa57f5478ba78935680f1c33ce3c
DOMAIN\willy.wei <willy.wei@sportscity.com.tw>
2023-03-08 14:51:29 +0800

    ISP20230183 新增報表R46

---
+++ Sci.Production.Warehouse/Report/R46.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Sci.Production.Warehouse
+{
+    partial class R46
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtfactory = new Sci.Production.Class.Txtfactory();
+            this.txtMdivision = new Sci.Production.Class.TxtMdivision();
+            this.label6 = new Sci.Win.UI.Label();
+            this.txtSP2 = new Sci.Win.UI.TextBox();
+            this.txtSP1 = new Sci.Win.UI.TextBox();
+            this.label4 = new Sci.Win.UI.Label();
+            this.dateIssue = new Sci.Win.UI.DateRange();
+            this.lblSP = new Sci.Win.UI.Label();
+            this.lblMdivision = new Sci.Win.UI.Label();
+            this.lbFactory = new Sci.Win.UI.Label();
+            this.SuspendLayout();
+            // 
+            // print
+            // 
+            this.print.Location = new System.Drawing.Point(424, 81);
+            this.print.TabIndex = 7;
+            this.print.Visible = false;
+            // 
+            // toexcel
+            // 
+            this.toexcel.Location = new System.Drawing.Point(424, 9);
+            this.toexcel.TabIndex = 5;
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(424, 45);
+            this.close.TabIndex = 6;
+            // 
+            // buttonCustomized
+            // 
+            this.buttonCustomized.Location = new System.Drawing.Point(231, 101);
+            this.buttonCustomized.TabIndex = 11;
+            // 
+            // checkUseCustomized
+            // 
+            this.checkUseCustomized.Location = new System.Drawing.Point(398, 117);
+            this.checkUseCustomized.TabIndex = 8;
+            // 
+            // txtVersion
+            // 
+            this.txtVersion.Location = new System.Drawing.Point(270, 72);
+            this.txtVersion.TabIndex = 9;
+            // 
+            // txtfactory
+            // 
+            this.txtfactory.BackColor = System.Drawing.Color.White;
+            this.txtfactory.BoolFtyGroupList = true;
+            this.txtfactory.FilteMDivision = false;
+            this.txtfactory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtfactory.IsProduceFty = false;
+            this.txtfactory.IssupportJunk = true;
+            this.txtfactory.Location = new System.Drawing.Point(120, 101);
+            this.txtfactory.MDivision = null;
+            this.txtfactory.Name = "txtfactory";
+            this.txtfactory.Size = new System.Drawing.Size(66, 23);
+            this.txtfactory.TabIndex = 4;
+            // 
+            // txtMdivision
+            // 
+            this.txtMdivision.BackColor = System.Drawing.Color.White;
+            this.txtMdivision.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtMdivision.Location = new System.Drawing.Point(120, 72);
+            this.txtMdivision.Name = "txtMdivision";
+            this.txtMdivision.Size = new System.Drawing.Size(66, 23);
+            this.txtMdivision.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(253, 44);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 23);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "～";
+            this.label6.TextStyle.Color = System.Drawing.Color.White;
+            // 
+            // txtSP2
+            // 
+            this.txtSP2.BackColor = System.Drawing.Color.White;
+            this.txtSP2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtSP2.Location = new System.Drawing.Point(277, 44);
+            this.txtSP2.Name = "txtSP2";
+            this.txtSP2.Size = new System.Drawing.Size(123, 23);
+            this.txtSP2.TabIndex = 2;
+            // 
+            // txtSP1
+            // 
+            this.txtSP1.BackColor = System.Drawing.Color.White;
+            this.txtSP1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtSP1.Location = new System.Drawing.Point(120, 44);
+            this.txtSP1.Name = "txtSP1";
+            this.txtSP1.Size = new System.Drawing.Size(130, 23);
+            this.txtSP1.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(9, 16);
+            this.label4.Name = "label4";
+            this.label4.RectStyle.Color = System.Drawing.Color.SkyBlue;
+            this.label4.Size = new System.Drawing.Size(108, 23);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Issue Date";
+            this.label4.TextStyle.Color = System.Drawing.Color.Black;
+            // 
+            // dateIssue
+            // 
+            // 
+            // 
+            // 
+            this.dateIssue.DateBox1.Location = new System.Drawing.Point(0, 0);
+            this.dateIssue.DateBox1.Name = "";
+            this.dateIssue.DateBox1.Size = new System.Drawing.Size(129, 23);
+            this.dateIssue.DateBox1.TabIndex = 0;
+            // 
+            // 
+            // 
+            this.dateIssue.DateBox2.Location = new System.Drawing.Point(151, 0);
+            this.dateIssue.DateBox2.Name = "";
+            this.dateIssue.DateBox2.Size = new System.Drawing.Size(129, 23);
+            this.dateIssue.DateBox2.TabIndex = 1;
+            this.dateIssue.Location = new System.Drawing.Point(120, 16);
+            this.dateIssue.Name = "dateIssue";
+            this.dateIssue.Size = new System.Drawing.Size(280, 23);
+            this.dateIssue.TabIndex = 0;
+            // 
+            // lblSP
+            // 
+            this.lblSP.Location = new System.Drawing.Point(9, 44);
+            this.lblSP.Name = "lblSP";
+            this.lblSP.RectStyle.Color = System.Drawing.Color.SkyBlue;
+            this.lblSP.Size = new System.Drawing.Size(108, 23);
+            this.lblSP.TabIndex = 13;
+            this.lblSP.Text = "SP#";
+            this.lblSP.TextStyle.Color = System.Drawing.Color.Black;
+            // 
+            // lblMdivision
+            // 
+            this.lblMdivision.Location = new System.Drawing.Point(9, 72);
+            this.lblMdivision.Name = "lblMdivision";
+            this.lblMdivision.Size = new System.Drawing.Size(108, 23);
+            this.lblMdivision.TabIndex = 14;
+            this.lblMdivision.Text = "M";
+            // 
+            // lbFactory
+            // 
+            this.lbFactory.Location = new System.Drawing.Point(9, 101);
+            this.lbFactory.Name = "lbFactory";
+            this.lbFactory.Size = new System.Drawing.Size(108, 23);
+            this.lbFactory.TabIndex = 15;
+            this.lbFactory.Text = "Factory";
+            // 
+            // R46
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 177);
+            this.Controls.Add(this.txtfactory);
+            this.Controls.Add(this.txtMdivision);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtSP2);
+            this.Controls.Add(this.txtSP1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateIssue);
+            this.Controls.Add(this.lblSP);
+            this.Controls.Add(this.lblMdivision);
+            this.Controls.Add(this.lbFactory);
+            this.Name = "R46";
+            this.OnLineHelpID = "Sci.Win.Tems.PrintForm";
+            this.Text = "R46. Remove from Scrap Whse Transaction List";
+            this.Controls.SetChildIndex(this.print, 0);
+            this.Controls.SetChildIndex(this.toexcel, 0);
+            this.Controls.SetChildIndex(this.close, 0);
+            this.Controls.SetChildIndex(this.buttonCustomized, 0);
+            this.Controls.SetChildIndex(this.checkUseCustomized, 0);
+            this.Controls.SetChildIndex(this.txtVersion, 0);
+            this.Controls.SetChildIndex(this.lbFactory, 0);
+            this.Controls.SetChildIndex(this.lblMdivision, 0);
+            this.Controls.SetChildIndex(this.lblSP, 0);
+            this.Controls.SetChildIndex(this.dateIssue, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.txtSP1, 0);
+            this.Controls.SetChildIndex(this.txtSP2, 0);
+            this.Controls.SetChildIndex(this.label6, 0);
+            this.Controls.SetChildIndex(this.txtMdivision, 0);
+            this.Controls.SetChildIndex(this.txtfactory, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Class.Txtfactory txtfactory;
+        private Class.TxtMdivision txtMdivision;
+        private Win.UI.Label label6;
+        private Win.UI.TextBox txtSP2;
+        private Win.UI.TextBox txtSP1;
+        private Win.UI.Label label4;
+        private Win.UI.DateRange dateIssue;
+        private Win.UI.Label lblSP;
+        private Win.UI.Label lblMdivision;
+        private Win.UI.Label lbFactory;
+    }
+}
---
+++ Sci.Production.Warehouse/Report/R46.cs
@@ -0,0 +1,145 @@
+﻿using Ict;
+using Sci.Data;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Sci.Production.Warehouse
+{
+    /// <inheritdoc/>
+    public partial class R46 : Win.Tems.PrintForm
+    {
+        private List<string> sqlWherelist;
+        private List<SqlParameter> lisSqlParameter;
+        private string strSQLWhere = string.Empty;
+        private DataTable printTable;
+
+        /// <inheritdoc/>
+        public R46(ToolStripMenuItem menuitem)
+            : base(menuitem)
+        {
+            this.InitializeComponent();
+        }
+
+        /// <inheritdoc/>
+        protected override bool ValidateInput()
+        {
+            this.strSQLWhere = string.Empty;
+            this.sqlWherelist = new List<string>();
+            this.lisSqlParameter = new List<SqlParameter>();
+
+            if (MyUtility.Check.Empty(this.txtSP1.Text) &&
+                MyUtility.Check.Empty(this.txtSP2.Text) &&
+                MyUtility.Check.Empty(this.dateIssue.Value1) &&
+                MyUtility.Check.Empty(this.dateIssue.Value2))
+            {
+                MyUtility.Msg.WarningBox("Issue Date and SP# can’t all be empty.");
+                return false;
+            }
+
+            if (!MyUtility.Check.Empty(this.txtSP1.Text) &&
+                !MyUtility.Check.Empty(this.txtSP2.Text))
+            {
+                this.sqlWherelist.Add("ad.POID >= @SP1 and ad.POID <= @SP2");
+                this.lisSqlParameter.Add(new SqlParameter("@SP1", this.txtSP1.Text));
+                this.lisSqlParameter.Add(new SqlParameter("@SP2", this.txtSP2.Text));
+            }
+
+            if (!MyUtility.Check.Empty(this.dateIssue.Value1) &&
+                !MyUtility.Check.Empty(this.dateIssue.Value2))
+            {
+                this.sqlWherelist.Add("A.IssueDate between @dateTransfer1 and @dateTransfer2");
+                this.lisSqlParameter.Add(new SqlParameter("@dateTransfer1", this.dateIssue.Value1));
+                this.lisSqlParameter.Add(new SqlParameter("@dateTransfer2", this.dateIssue.Value2));
+            }
+
+            if (!MyUtility.Check.Empty(this.txtMdivision.Text))
+            {
+                this.sqlWherelist.Add("vo.MDivisionID = @MDivisionID");
+                this.lisSqlParameter.Add(new SqlParameter("@MDivisionID", this.txtMdivision.Text));
+            }
+
+            if (!MyUtility.Check.Empty(this.txtfactory.Text))
+            {
+                this.sqlWherelist.Add("vo.FtyGroup = @FtyGroup");
+                this.lisSqlParameter.Add(new SqlParameter("@FtyGroup", this.txtfactory.Text));
+            }
+
+            this.strSQLWhere = string.Join(" and ", this.sqlWherelist);
+
+            return base.ValidateInput();
+        }
+
+        /// <inheritdoc/>
+        protected override DualResult OnAsyncDataLoad(Win.ReportEventArgs e)
+        {
+            string sqlcmd = $@"
+select [Dispose ID] = a.ID
+,[Dispose Dae] = a.IssueDate
+,[M] = vo.MDivisionID
+,[Factory] = vo.FactoryID
+,[SP#] = ad.POID
+,[Seq] = CONCAT(ad.Seq1,' ',ad.Seq2)
+,[Roll] = ad.Roll
+,[Dyelot] = ad.Dyelot
+,[Ref#] = psd.Refno
+,[Color] = IIF(f.MtlTypeID = 'EMB THREAD' or f.MtlTypeID = 'SP THREAD' or f.MtlTypeID = 'THREAD', IIF(psd.SuppColor = '' or psd.SuppColor is null, dbo.GetColorMultipleID(vo.BrandID, psd.ColorID), psd.SuppColor), dbo.GetColorMultipleID(vo.BrandID, psd.ColorID))
+,[MaterialType] = concat(IIF(psd.FabricType = 'F', 'Fabric', IIF(psd.FabricType = 'A', 'Accessory', IIF(psd.FabricType = 'O', 'Other', psd.FabricType))), '-', f.MtlTypeID)
+,[WeaveType] = f.WeaveTypeID
+,[DisposeQty] = ad.QtyBefore-ad.QtyAfter
+,[Unit] = psd.StockUnit
+,[Location] = dbo.Getlocation(fty.Ukey)
+,[Reason] = ad.ReasonId + '-' + (select name from Reason where ReasonTypeID='Stock_Remove' and id = ad.ReasonId)
+from Adjust a
+Inner join Adjust_Detail ad on ad.ID = a.ID
+left join View_WH_Orders vo on vo.ID = ad.POID
+Left join PO_Supp_Detail psd on psd.ID = ad.POID and psd.Seq1 = ad.Seq1 and psd.Seq2 = ad.Seq2
+Left join Fabric f on f.SCIRefno = psd.SCIRefno
+Left join FtyInventory fty on fty.POID = ad.POID and fty.Seq1 = ad.Seq1 and fty.Seq2 = ad.Seq2 and fty.Roll = ad.Roll and fty.Dyelot = ad.Dyelot and fty.StockType = ad.StockType
+where a.Type = 'R' and
+{this.strSQLWhere}
+";
+
+            DualResult result = DBProxy.Current.Select(null, sqlcmd, this.lisSqlParameter, out this.printTable);
+            if (!result)
+            {
+                DualResult failResult = new DualResult(false, "Query data fail\r\n" + result.ToString());
+                return failResult;
+            }
+
+            return Ict.Result.True;
+        }
+
+        /// <inheritdoc/>
+        protected override bool OnToExcel(Win.ReportDefinition report)
+        {
+            string excelName = "Warehouse_R46";
+
+            this.SetCount(this.printTable.Rows.Count);
+            if (this.printTable == null || this.printTable.Rows.Count == 0)
+            {
+                MyUtility.Msg.ErrorBox("Data not found");
+                return false;
+            }
+
+            Excel.Application objApp = MyUtility.Excel.ConnectExcel(Env.Cfg.XltPathDir + "\\" + $"\\{excelName}.xltx"); // 預先開啟excel app
+            MyUtility.Excel.CopyToXls(this.printTable, string.Empty, showExcel: false, xltfile: $"{excelName}.xltx", headerRow: 1, excelApp: objApp);
+
+            #region Save & Show Excel
+            string strExcelName = Class.MicrosoftFile.GetName(excelName);
+
+            Microsoft.Office.Interop.Excel.Workbook workbook = objApp.ActiveWorkbook;
+
+            workbook.SaveAs(strExcelName);
+            objApp.Quit();
+            Marshal.ReleaseComObject(objApp);
+            strExcelName.OpenFile();
+            #endregion
+
+            return true;
+        }
+    }
+}
