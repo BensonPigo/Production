0989d65754edd75070f9d57df890c02e3c62cd85
DOMAIN\ryan.tsao <ryan.tsao@sportscity.com.tw>
2023-03-20 14:01:00 +0800

    ISP20230123 [] - New report for cutting

--- Sci.Production.Cutting/R13.cs
+++ Sci.Production.Cutting/R13.cs
@@ -121,9 +121,7 @@ namespace Sci.Production.Cutting
             [Est.Cutting Date]= wo.EstCutDate,
             [Act.Cutting Date] = MincDate.MincoDate,
             [Earliest Sewing Inline] = c.SewInLine,
-            [Sewing Inline(SP)] = o.SewInLine,
             [Master SP#] = wo.ID, 
-            [SP#] = wo.OrderID,
             [Brand]=o.BrandID,
             [Style#] = o.StyleID,
             [FabRef#] = wo.Refno,
@@ -143,30 +141,23 @@ namespace Sci.Production.Cutting
             [Marker No.] = wo.MarkerNo,
             [Marker Length] = wo.MarkerLength,
             [Cutting Perimeter] = wo.ActCuttingPerimeter,
-            [Cutting Perimeter(Decimal)] = dbo.GetActualPerimeterYd(wo.ActCuttingPerimeter),
             [Straight Length] = wo.StraightLength,
-            [Straight Length (Decimal)] = dbo.GetActualPerimeterYd(wo.StraightLength),
             [Curved Length] = wo.CurvedLength,
-            [Curved Length (Decimal)] = dbo.GetActualPerimeterYd(wo.CurvedLength),
             [Delay Reason] =dw.[Name],
             [Remark] = wo.Remark,
-            fi.avgInQty,
-            f.WeaveTypeID,
-            [workorderSeq1] =wo.seq1,
-            [workorderSeq2] =wo.seq2
+            f.WeaveTypeID
             into #tmp
             from WorkOrder wo WITH (NOLOCK) 
             left join Orders o WITH (NOLOCK) on o.id = wo.OrderID
             left join Cutting c WITH (NOLOCK) on c.ID = o.CuttingSP
             left join DropDownList dw with (nolock) on dw.Type = 'PMS_UnFinCutReason' and dw.ID = wo.UnfinishedCuttingReason
             left join fabric f WITH (NOLOCK) on f.SCIRefno = wo.SCIRefno
-            left join PO_Supp_Detail psd with(nolock) on psd.id = wo.id and psd.seq1 = wo.seq1 and psd.seq2 = wo.seq2
-            outer apply(select val = sum(aa.Layer) from cuttingoutput_Detail aa WITH (NOLOCK) where aa.WorkOrderUkey = wo.Ukey)acc
+            outer apply(select val = sum(aa.Layer) from cuttingoutput_Detail aa WITH (NOLOCK) where aa.CutRef = wo.CutRef)acc
             outer apply(
                 Select MincoDate = iif(sum(cod.Layer) = wo.Layer, Max(co.cdate),null)
 	            From cuttingoutput co WITH (NOLOCK) 
 	            inner join cuttingoutput_detail cod WITH (NOLOCK) on co.id = cod.id
-	            Where cod.workorderukey = wo.Ukey and co.Status != 'New' 
+	            Where cod.CutRef = wo.CutRef and co.Status != 'New' 
             )MincDate
             outer apply(
 	            select val = (
@@ -176,63 +167,12 @@ namespace Sci.Production.Cutting
 		            for xml path('')
 	            )
             )as SQty
-            outer apply(
-	            select avgInQty = avg(fi.InQty)
-	            from PO_Supp_Detail psd with(nolock)
-	            left join FtyInventory fi with(nolock) on fi.POID = psd.ID and fi.Seq1 = psd.SEQ1 and fi.Seq2 = psd.SEQ2
-	            where psd.ID = wo.id and psd.SCIRefno = wo.SCIRefno
-	            and fi.InQty is not null
-            )fi
 
             where 1=1
             {this.strWhere}
-            group by wo.MDivisionId,wo.FactoryID,f.WeaveTypeID,wo.EstCutDate,MincDate.MincoDate,c.SewInLine,o.SewInLine,wo.ID,wo.OrderID,
-		             o.BrandID,o.StyleID,wo.Refno,c.WorkType,wo.CutRef,wo.Cutno,wo.SpreadingNoID,wo.CutCellid,wo.FabricCombo,sqty.val,
-		             wo.Markername,wo.MarkerNo,wo.MarkerLength,wo.ActCuttingPerimeter,wo.StraightLength,wo.CurvedLength,dw.[Name],wo.Remark
-		            ,fi.avgInQty,wo.seq1,wo.seq2
-
-            select 
-            *, 
-            [Spreading Time (mins)] =  st.st,
-            [Cutting Time (mins)] =  ct.ct,
-            [Marker Width] =MW.MW
-            into #tmp1
-            from #tmp
-            outer apply
-            (
-	            select st =
-	            cast(round(isnull(dbo.GetSpreadingTime(
-						            Fabrication,
-						            FabRef#,
-						            iif(iif(isnull(avgInQty,0)=0,1,round(iif(isnull(Ref#,'')='',Consumption,sum(Consumption)over(partition by Ref#,M,[Master SP#]))/avgInQty,0))<1,1,
-							            iif(isnull(avgInQty,0)=0,1,round(iif(isnull(Ref#,'')='',Consumption,sum(Consumption)over(partition by Ref#,M,[Master SP#]))/avgInQty,0))
-							            ),
-						            iif(isnull(Ref#,'')='',Layers,sum(Layers)over(partition by Ref#,M,[Master SP#])),
-						            iif(isnull(Ref#,'')='',Consumption,sum(Consumption)over(partition by Ref#,M,[Master SP#])),
-						            1
-					            )/60.0,0),2)as float)
-            )st
-            outer apply
-            (
-	             select ct =
-	             cast(round(isnull(
-						            dbo.GetCuttingTime(
-						            round(dbo.GetActualPerimeter(iif([Cutting Perimeter] not like '%yd%','0',[Cutting Perimeter])),4),
-						            [Cut Cell],
-						            iif(isnull([Ref#],'')='',Layers,sum(Layers)over(partition by Ref#,M,[Master SP#])),
-						            WeaveTypeID,
-						            iif(isnull([Ref#],'')='',Consumption,sum(Consumption)over(partition by  Ref#,M,[Master SP#]))
-						            )/60.0,0),2)as float)
-            ) ct
-            outer apply(
-	            SELECT top 1  MW = OBE.Width
-	            FROM Order_BOF OB WITH (NOLOCK)
-	            INNER JOIN Order_BOF_Expend OBE WITH (NOLOCK) ON OBE.Order_BOFUkey = OB.Ukey
-	            INNER JOIN PO_Supp PS WITH (NOLOCK) ON PS.ID = OB.Id 
-	            INNER JOIN PO_Supp_Detail PSD WITH (NOLOCK) ON PSD.ID= OB.Id AND PSD.RefNo = OB.Refno AND PSD.ColorID = OBE.ColorId 
-	            WHERE PSD.ID = [Master SP#] AND PSD.SEQ1=workorderSeq1 AND PSD.SEQ2=workorderSeq2
-            )MW
-            drop table #tmp
+           group by wo.MDivisionId,wo.FactoryID,f.WeaveTypeID,wo.EstCutDate,MincDate.MincoDate,c.SewInLine,wo.ID,o.BrandID,o.StyleID,
+		    wo.Refno,c.WorkType,wo.CutRef,wo.Cutno,wo.SpreadingNoID,wo.CutCellid,wo.FabricCombo,sqty.val,
+		    wo.Markername,wo.MarkerNo,wo.MarkerLength,wo.ActCuttingPerimeter,wo.StraightLength,wo.CurvedLength,dw.[Name],wo.Remark
 
             select 
             [M],
@@ -241,9 +181,7 @@ namespace Sci.Production.Cutting
             [Est.Cutting Date],
             [Act.Cutting Date],
             [Earliest Sewing Inline],
-            [Sewing Inline(SP)],
             [Master SP#], 
-            [SP#],
             [Brand],
             [Style#],
             [FabRef#],
@@ -253,42 +191,36 @@ namespace Sci.Production.Cutting
             [SpreadingNoID],
             [Cut Cell],
             [Combination],
-			[Layer] = sum([Layers]),
+            [Layer] = sum([Layers]),
             [Layers Level]= case when sum([Layers]) between 1 and 5 then '1~5'
-						        when sum([Layers]) between 6 and 10 then '6~10'
-						        when sum([Layers]) between 11 and 15 then '11~15'
-						        when sum([Layers]) between 16 and 30 then '16~30'
-						        when sum([Layers]) between 31 and 50 then '31~50'
-						        else '50 above'
-						        end ,
+					            when sum([Layers]) between 6 and 10 then '6~10'
+					            when sum([Layers]) between 11 and 15 then '11~15'
+					            when sum([Layers]) between 16 and 30 then '16~30'
+					            when sum([Layers]) between 31 and 50 then '31~50'
+					            else '50 above'
+					            end ,
             [LackingLayers],
             [Ratio],
             sum([Consumption]),
             sum([Act. Cons. Output]),
             sum([Balance Cons.]),
-            sum([Spreading Time (mins)]),
-            sum([Cutting Time (mins)]),
             [Marker Name],
             [Marker No.],
-            [Marker Width],
             [Marker Length],
             [Cutting Perimeter],
-            [Cutting Perimeter(Decimal)],
             [Straight Length],
-            [Straight Length (Decimal)],
             [Curved Length],
-            [Curved Length (Decimal)],
             [Delay Reason],
             [Remark]
-            from #tmp1
+            from #tmp
 			
-			group by [M],[Factory],[Fabrication],[Est.Cutting Date],[Act.Cutting Date],[Earliest Sewing Inline],
-            [Sewing Inline(SP)],[Master SP#],[SP#],[Brand],[Style#],[FabRef#],[Switch to Workorder],[Ref#],
+            group by [M],[Factory],[Fabrication],[Est.Cutting Date],[Act.Cutting Date],[Earliest Sewing Inline],
+            [Master SP#],[Brand],[Style#],[FabRef#],[Switch to Workorder],[Ref#],
             [Cut#],[SpreadingNoID],[Cut Cell],[Combination],[LackingLayers],[Ratio],[Marker Name],
-            [Marker No.],[Marker Width], [Marker Length],[Cutting Perimeter],[Cutting Perimeter(Decimal)],
-            [Straight Length],[Straight Length (Decimal)],[Curved Length],
-            [Curved Length (Decimal)],[Delay Reason],[Remark]
-            drop table #tmp1";
+            [Marker No.], [Marker Length],[Cutting Perimeter],
+            [Straight Length],[Curved Length],
+            [Delay Reason],[Remark]
+            drop table #tmp";
 
             DualResult result = DBProxy.Current.Select(null, sqlCmd, out this.printData);
             if (!result)
