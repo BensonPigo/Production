7ba2e15b2a6fb8910141f0f1b22371c4cf70a68a
DOMAIN\jeff.yeh <jeff.yeh@sportscity.com.tw>
2023-03-21 11:14:13 +0800

    ISP20230232  加上 brancdid條件

--- Database/dbo/Functions/GetColorMultipleID.sql
+++ Database/dbo/Functions/GetColorMultipleID.sql
@@ -13,7 +13,7 @@ Begin
 			   from dbo.Color as c
 			   LEFT join dbo.Color_multiple as m on m.ID = c.ID 
 				  								    and m.BrandID = c.BrandId
-               left join color c2 on c2.id = m.ColorID
+               left join color c2 on c2.id = m.ColorID and c2.BrandID = m.BrandID
 			   where c.ID = @ColorID and c.BrandId = @BrandID
 			   order by m.Seqno
 			   for xml path(''))
