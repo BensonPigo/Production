bbfee7c10f782de90e17d9ebd229e354ca52d2f8
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2023-03-27 17:47:52 +0800

    ISP20230259 - 新增結構

---
+++ Database/dbo/Tables/LostFinalInspNotification.sql
@@ -0,0 +1,21 @@
+﻿
+CREATE TABLE [dbo].LostFinalInspNotification
+(   
+	FactoryID Varchar(8)  NOT NULL , 
+	ToAddress      nVarchar(500) NOT NULL CONSTRAINT [DF_LostFinalInspNotification_ToAddress] DEFAULT '',
+	CcAddress      nVarchar(500) NOT NULL CONSTRAINT [DF_LostFinalInspNotification_CcAddress] DEFAULT '',
+	StartTime    datetime   NULL ,
+	EndTime    datetime   NULL ,
+	Frequency   Varchar(20)    NOT NULL CONSTRAINT [DF_LostFinalInspNotification_Frequency] DEFAULT '',
+	[Description]     NVarchar(500) NOT NULL CONSTRAINT [DF_LostFinalInspNotification_Description] DEFAULT '',
+	AddDate datetime NULL,
+	AddName Varchar(10) NOT NULL CONSTRAINT [DF_LostFinalInspNotification_AddName] DEFAULT '',
+	EditDate datetime NULL,
+	EditName Varchar(10) NOT NULL CONSTRAINT [DF_LostFinalInspNotification_EditName] DEFAULT '',
+	LastExecuteTime datetime NULL,
+	CONSTRAINT [PK_LostFinalInspNotification] PRIMARY KEY CLUSTERED 
+	(
+		FactoryID ASC
+	)
+)
+GO
