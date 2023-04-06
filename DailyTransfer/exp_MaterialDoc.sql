5eafb52ee0812986d6993eee58d933924f9e9927
DOMAIN\willy.wei <willy.wei@sportscity.com.tw>
2023-04-06 09:54:51 +0800

    ISP20230220 新增每日資料交換檔案

--- DailyTransfer/exp_MaterialDoc.sql
+++ DailyTransfer/exp_MaterialDoc.sql
@@ -2,9 +2,20 @@
 AS
 BEGIN
 SET NOCOUNT ON;
+
 IF OBJECT_ID(N'dbo.UASentReport') IS NOT NULL
 BEGIN
   DROP TABLE UASentReport
+END
+
+IF OBJECT_ID(N'dbo.NewSentReport') IS NOT NULL
+BEGIN
+  DROP TABLE NewSentReport
+END
+
+IF OBJECT_ID(N'dbo.ExportRefnoSentReport') IS NOT NULL
+BEGIN
+  DROP TABLE ExportRefnoSentReport
 END
 
 ------------------------------------------------------------------------------------------------------
@@ -12,7 +23,9 @@ declare @DateStart date;
 --***資料交換的條件限制***
 --1.取得預設值
 if @DateStart is Null
+begin
 	set @DateStart= CONVERT(DATE,DATEADD(day,-30,GETDATE()))
+end
 ------------------------------------------------------------------------------------------------------
 
 SELECT
@@ -32,11 +45,52 @@ SELECT
 	,AddName
 	,EditDate
 	,Editname
-	into UASentReport
+into UASentReport
 FROM Production.dbo.UASentReport with (nolock)
+where isnull(EditDate,AddDate) BETWEEN @DateStart and GETDATE()
+
+--	NewSentReport
+select 
+	  [ExportID]
+      ,[PoID]
+      ,[Seq1]
+      ,[Seq2]
+      ,[DocumentName]
+      ,[BrandID]
+      ,[ReportDate]
+      ,[FTYReceivedReport]
+      ,[AWBno]
+      ,[T2InspYds]
+      ,[T2DefectPoint]
+      ,[T2Grade]
+      ,[TestReportTestDate]
+      ,[Ukey]
+      ,[AddDate]
+      ,[AddName]
+      ,[EditDate]
+      ,[Editname]
+into [NewSentReport]
+FROM [Production].[dbo].[NewSentReport]
+where isnull(EditDate,AddDate) BETWEEN  @DateStart and GETDATE()
+
+
+--	ExportRefnoSentReport
+SELECT [ExportID]
+      ,[BrandRefno]
+      ,[ColorID]
+      ,[DocumentName]
+      ,[BrandID]
+      ,[ReportDate]
+      ,[FTYReceivedReport]
+      ,[AWBno]
+      ,[UKEY]
+      ,[AddDate]
+      ,[AddName]
+      ,[EditDate]
+      ,[Editname]
+into [ExportRefnoSentReport]
+FROM [Production].[dbo].[ExportRefnoSentReport]
 where isnull(EditDate,AddDate) BETWEEN  @DateStart  and GETDATE()
+
+
 END
-
-
-
-
--- DailyTransfer/imp_ MaterialDoc.sql
+++ DailyTransfer/imp_ MaterialDoc.sql
@@ -62,4 +62,80 @@ BEGIN
 	from [Trade_To_Pms].[dbo].UASentReport as b WITH (NOLOCK)
 	where not exists(select 1 from [Production].[dbo].UASentReport as a WITH (NOLOCK) where a.BrandRefno = b.BrandRefno and a.ColorID = b.ColorID and a.SuppID = b.SuppID and a.DocumentName = b.DocumentName and a.BrandID = b.BrandID)
 
+
+	/*NewSentReport */
+	-------UPDATE
+	UPDATE a
+	SET 	
+	 a.ReportDate = b.ReportDate
+	,a.AWBno = b.AWBno
+	,a.EditDate = b.EditDate
+	,a.Editname = b.Editname
+	from [Production].[dbo].NewSentReport as a with(nolock)
+	inner join [Trade_To_Pms].[dbo].NewSentReport  as b with(nolock) on a.ExportID = b.ExportID 
+		and a.PoID = b.PoID 
+		and a.Seq1 = b.Seq1 
+		and a.Seq2 = b.Seq2
+		and a.DocumentName = b.DocumentName 
+		and a.BrandID = b.BrandID
+	where 
+	(
+		(
+			(a.EditDate is not null and b.EditDate is not null)
+			and			
+			a.EditDate < b.EditDate
+		)
+		or
+		(
+			a.EditDate is null and b.EditDate is not null
+		)
+		or
+		(
+			(a.EditDate is null and b.EditDate is null)
+			and			
+			a.AddDate < b.AddDate
+		)
+	)
+	
+	
+	--isnull(a.EditDate,a.AddDate) < isnull(b.EditDate,b.AddDate)
+	-------------------------- INSERT INTO 
+	INSERT INTO [Production].[dbo].UASentReport
+	 (
+		BrandRefno
+		,ColorID
+		,SuppID
+		,DocumentName
+		,BrandID
+		,TestSeasonID
+		,DueSeason
+		,DueDate
+		,TestReport
+		,FTYReceivedReport
+		,TestReportTestDate
+		,AddDate
+		,AddName
+		,EditDate
+		,Editname
+	)
+	SELECT
+		BrandRefno
+		,ColorID
+		,SuppID
+		,DocumentName
+		,BrandID
+		,TestSeasonID
+		,DueSeason
+		,DueDate
+		,TestReport
+		,FTYReceivedReport
+		,TestReportTestDate
+		,AddDate
+		,AddName
+		,EditDate
+		,Editname
+	from [Trade_To_Pms].[dbo].UASentReport as b WITH (NOLOCK)
+	where not exists(select 1 from [Production].[dbo].UASentReport as a WITH (NOLOCK) where a.BrandRefno = b.BrandRefno and a.ColorID = b.ColorID and a.SuppID = b.SuppID and a.DocumentName = b.DocumentName and a.BrandID = b.BrandID)
+
+
 end
