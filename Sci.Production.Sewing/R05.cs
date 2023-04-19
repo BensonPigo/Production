dba1ac2390ba6033ae6924d82b1de04b3dbb87b1
DOMAIN\jack.hsu <jack.hsu@sportscity.com.tw>
2023-04-19 14:10:50 +0800

    ISP20230332 - [Sewing] [R05] 調整報表速度、欄位。

--- Sci.Production.Sewing/R05.cs
+++ Sci.Production.Sewing/R05.cs
@@ -73,86 +73,63 @@ namespace Sci.Production.Sewing
         {
             StringBuilder sqlCmd = new StringBuilder();
             sqlCmd.Append(string.Format(@"
-                        Select  [Sp#] = OQG.ID
-                                ,[StyleID] = O.StyleID
-		                        , o.BuyerDelivery
-		                        , o.SciDelivery
-		                        , o.CPU
-		                        , [BtoG] = g.BtoG
-                                ,[Brand] = O.BrandID
-                                ,[Season] = O.SeasonID
-                                ,[*] = SL.Location
-                                ,[From SP#] = OQG.OrderIDFrom
-                                ,[Article] = OQG.Article
-                                ,[SizeCode] =OQG.SizeCode
-                                ,[ToSP_Cmf_PK_Qty] = PL.PackQty
-                                ,[ToSP_Qty] = IIF(OQG.Junk=0,OQG.Qty,0)
-                                ,[ToSp_allocate_qty] = SODDG.ToSp_allocate_qty
-                                ,[ToSp_Balance] =  IIF(OQG.Junk=0,OQG.Qty,0) - SODDG.ToSp_allocate_qty
-                                ,[ToSp_BuyerDelivery] = O.BuyerDelivery
-                                ,[FromSp_Sewing_qty] = SODD.FromSp_Sewing_qty
-                                ,[FromSp_Accu_Qty] = SODDG2.FromSp_Accu_Qty
-                                ,[FromSp_Avl_Qty] = SODD.FromSp_Sewing_qty  -  SODDG2.FromSp_Accu_Qty
-                                ,[Is_Trans_Qty] = IIF(SODDG.ToSp_allocate_qty >= IIF(OQG.Junk=0,OQG.Qty,0), 'N/A' , IIF((SODD.FromSp_Sewing_qty  -  SODDG2.FromSp_Accu_Qty) >= (IIF(OQG.Junk=0,OQG.Qty,0) - SODDG.ToSp_allocate_qty) , 'Y' , 'N' ))
-                                ,[Is_Trans_Qty_Excess] = IIF(SODDG.ToSp_allocate_qty > IIF(OQG.Junk=0,OQG.Qty,0) , 'Y' , 'N')
-                        From Order_Qty_Garment OQG WITH (NOLOCK)
-                        Inner join Orders O  WITH (NOLOCK) on OQG.id=O.id
-                        Inner join Factory F  WITH (NOLOCK) on O.FactoryID=F.ID
-                        Left join Style_Location  SL  WITH (NOLOCK) on O.styleukey=SL.Styleukey 
-                        outer apply(select isnull(Sum(b.shipqty),0) AS PackQty
-		                                from PackingList a  WITH (NOLOCK)
-		                                inner join PackingList_Detail b on a.ID=b.ID
-		                                where b.OrderID= OQG.ID
-		                                      and b.Article = OQG.Article 
-                                                and b.SizeCode = OQG.SizeCode
-		                                      and a.Status= 'Confirmed') as PL
-                        outer apply(select Isnull(Sum(SewingOutput_Detail_Detail_Garment.QAQty),0) as ToSp_allocate_qty
-                                        from SewingOutput_Detail_Detail_Garment  WITH (NOLOCK)
-                                        Where SewingOutput_Detail_Detail_Garment.OrderId = OQG.ID
-                                        		and SewingOutput_Detail_Detail_Garment.ComboType = SL.Location
-                                        		and SewingOutput_Detail_Detail_Garment.Article =  OQG.Article 
-                                        		and SewingOutput_Detail_Detail_Garment.SizeCode = OQG.SizeCode
-                                        		and SewingOutput_Detail_Detail_Garment.OrderIDfrom = OQG.OrderIDFrom
-                                        ) as SODDG
-                        outer apply(select Isnull(Sum(SewingOutput_Detail_Detail.QAQty),0) as FromSp_Sewing_qty
-                                    from SewingOutput_Detail_Detail  WITH (NOLOCK)
-                                    Where SewingOutput_Detail_Detail.OrderId = OQG.OrderIDFrom
-                                    		and SewingOutput_Detail_Detail.ComboType = SL.Location
-                                    		and SewingOutput_Detail_Detail.Article = OQG.Article
-                                    		and SewingOutput_Detail_Detail.SizeCode = OQG.SizeCode
-                                     ) as SODD
-                        outer apply(select Isnull(Sum(SewingOutput_Detail_Detail_Garment.QAQty),0) as FromSp_Accu_Qty
-                                    from SewingOutput_Detail_Detail_Garment  WITH (NOLOCK)
-                                    Where SewingOutput_Detail_Detail_Garment.ComboType = SL.Location
-                                    		and SewingOutput_Detail_Detail_Garment.Article = OQG.Article
-                                    		and SewingOutput_Detail_Detail_Garment.SizeCode =OQG.SizeCode
-                                    		and SewingOutput_Detail_Detail_Garment.OrderIDfrom = OQG.OrderIDFrom
-                                    ) as SODDG2
-                        outer apply (
-	                        SELECT [BtoG] = IIF(COUNT(*) > 1 , 'O' ,IIF(SUM(g.[gQTY]) = SUM(g.[bQTY]), 'A' ,'O'))
-	                        FROM (
-		                        SELECT [gPOID] = g3.POID,
-			                        [bPOID] = b3.POID,
-			                        [gQTY] = SUM(g3.Qty),
-			                        [bQTY] = SUM(b3.Qty)
-		                        FROM Order_Qty_Garment og3
-		                        INNER JOIN Orders g3 ON og3.ID = g3.ID
-		                        INNER JOIN Orders b3 ON og3.OrderIDFrom = b3.ID 
-		                        INNER JOIN (
-				                        SELECT DISTINCT [gPOID] = g2.POID, 
-					                        [bPOID] = b2.POID
-				                        FROM Order_Qty_Garment og2 
-				                        INNER JOIN Orders g2 ON og2.ID = g2.ID
-				                        INNER JOIN Orders b2 ON og2.OrderIDFrom = b2.ID 
-				                        INNER JOIN Order_Qty_Garment og1 ON og1.ID = OQG.ID AND og1.Junk = 0
-				                        INNER JOIN Orders g1 ON og1.ID = g1.ID AND g1.Category = 'G'
-				                        INNER JOIN Orders b1 ON og1.OrderIDFrom = b1.ID AND b1.Category = 'B'
-				                        WHERE (g1.POID = g2.POID OR b1.POID = b2.POID)
-			                        ) tmp ON (g3.POID = tmp.gPOID OR b3.POID = tmp.bPOID) AND og3.Junk = 0
-		                        GROUP BY g3.POID, b3.POID
-	                        )g 
-                        ) g
-                        where 1 = 1 
+Select  [Sp#] = OQG.ID
+        ,[StyleID] = O.StyleID
+        , o.BuyerDelivery
+        , o.SciDelivery
+        , o.CPU
+        ,[Brand] = O.BrandID
+        ,[Season] = O.SeasonID
+        ,[*] = SL.Location
+        ,[From SP#] = OQG.OrderIDFrom
+        ,[Article] = OQG.Article
+        ,[SizeCode] =OQG.SizeCode
+        ,[ToSP_Cmf_PK_Qty] = PL.PackQty
+        ,[ToSP_Qty] = IIF(OQG.Junk=0,OQG.Qty,0)
+        ,[ToSp_allocate_qty] = SODDG.ToSp_allocate_qty
+        ,[ToSp_Balance] =  IIF(OQG.Junk=0,OQG.Qty,0) - SODDG.ToSp_allocate_qty
+        ,[ToSp_BuyerDelivery] = O.BuyerDelivery
+        ,[FromSp_Sewing_qty] = SODD.FromSp_Sewing_qty
+        ,[FromSp_Accu_Qty] = SODDG2.FromSp_Accu_Qty
+        ,[FromSp_Avl_Qty] = SODD.FromSp_Sewing_qty  -  SODDG2.FromSp_Accu_Qty
+        ,[Is_Trans_Qty] = IIF(SODDG.ToSp_allocate_qty >= IIF(OQG.Junk=0,OQG.Qty,0), 'N/A' , IIF((SODD.FromSp_Sewing_qty  -  SODDG2.FromSp_Accu_Qty) >= (IIF(OQG.Junk=0,OQG.Qty,0) - SODDG.ToSp_allocate_qty) , 'Y' , 'N' ))
+         ,[Is_Trans_Qty_Excess] = IIF(SODDG.ToSp_allocate_qty > IIF(OQG.Junk=0,OQG.Qty,0) , 'Y' , 'N')
+         ,[Junk] = IIF(OQG.Junk = 1, 'Y', 'N')
+into #tmp
+From Order_Qty_Garment OQG WITH (NOLOCK)
+Inner join Orders O  WITH (NOLOCK) on OQG.id=O.id
+Inner join Factory F  WITH (NOLOCK) on O.FactoryID=F.ID
+Left join Style_Location  SL  WITH (NOLOCK) on O.styleukey=SL.Styleukey 
+outer apply(select isnull(Sum(b.shipqty),0) AS PackQty
+                from PackingList a  WITH (NOLOCK)
+                inner join PackingList_Detail b on a.ID=b.ID
+                where b.OrderID= OQG.ID
+                      and b.Article = OQG.Article 
+                        and b.SizeCode = OQG.SizeCode
+                      and a.Status= 'Confirmed') as PL
+outer apply(select Isnull(Sum(SewingOutput_Detail_Detail_Garment.QAQty),0) as ToSp_allocate_qty
+                from SewingOutput_Detail_Detail_Garment  WITH (NOLOCK)
+                Where SewingOutput_Detail_Detail_Garment.OrderId = OQG.ID
+                		and SewingOutput_Detail_Detail_Garment.ComboType = SL.Location
+                		and SewingOutput_Detail_Detail_Garment.Article =  OQG.Article 
+                		and SewingOutput_Detail_Detail_Garment.SizeCode = OQG.SizeCode
+                		and SewingOutput_Detail_Detail_Garment.OrderIDfrom = OQG.OrderIDFrom
+                ) as SODDG
+outer apply(select Isnull(Sum(SewingOutput_Detail_Detail.QAQty),0) as FromSp_Sewing_qty
+            from SewingOutput_Detail_Detail  WITH (NOLOCK)
+            Where SewingOutput_Detail_Detail.OrderId = OQG.OrderIDFrom
+            		and SewingOutput_Detail_Detail.ComboType = SL.Location
+            		and SewingOutput_Detail_Detail.Article = OQG.Article
+            		and SewingOutput_Detail_Detail.SizeCode = OQG.SizeCode
+             ) as SODD
+outer apply(select Isnull(Sum(SewingOutput_Detail_Detail_Garment.QAQty),0) as FromSp_Accu_Qty
+            from SewingOutput_Detail_Detail_Garment  WITH (NOLOCK)
+            Where SewingOutput_Detail_Detail_Garment.ComboType = SL.Location
+            		and SewingOutput_Detail_Detail_Garment.Article = OQG.Article
+            		and SewingOutput_Detail_Detail_Garment.SizeCode =OQG.SizeCode
+            		and SewingOutput_Detail_Detail_Garment.OrderIDfrom = OQG.OrderIDFrom
+            ) as SODDG2
+where 1 = 1 
                 "));
 
             if (!MyUtility.Check.Empty(this.buyerDate1))
@@ -198,22 +175,92 @@ namespace Sci.Production.Sewing
             string status_condistion = string.Empty;
             if (this.status.Equals("Unfinished"))
             {
-                status_condistion = " ToSp_allocate_qty < ToSP_Qty";
+                status_condistion = " where ToSp_allocate_qty < ToSP_Qty";
             }
             else if (this.status.Equals("Finished"))
             {
-                status_condistion = " ToSp_allocate_qty = ToSP_Qty";
+                status_condistion = " where ToSp_allocate_qty = ToSP_Qty";
             }
             else if (this.status.Equals("Excess"))
             {
-                status_condistion = " ToSp_allocate_qty > ToSP_Qty";
+                status_condistion = " where ToSp_allocate_qty > ToSP_Qty";
             }
 
-            if (status_condistion.Length > 0)
-            {
-                sqlCmd.Insert(0, @"select * from (");
-                sqlCmd.Append(@") as alldata where " + status_condistion);
-            }
+            sqlCmd.Append($@"
+select t.[Sp#]
+	, g.BtoG
+into #tmp_Order_Qty_Garment
+from ( select distinct [Sp#] from #tmp t {status_condistion}) t
+outer apply (
+	SELECT [BtoG] = CASE 
+		 	 WHEN COUNT(*) > 1 THEN 'O'
+		 	 WHEN g4.gQty = b4.bQty THEN 'A'
+			 WHEN g4.gQty <> b4.bQty THEN 'O'
+		 	 ELSE ''
+		  END
+	FROM (
+		  SELECT DISTINCT [gPOID] = g3.POID,
+			   [bPOID] = b3.POID
+		  FROM Order_Qty_Garment og3 WITH (NOLOCK)
+		  INNER JOIN Orders g3 WITH (NOLOCK) ON og3.ID = g3.ID AND g3.Category = 'G'
+		  INNER JOIN Orders b3 WITH (NOLOCK) ON og3.OrderIDFrom = b3.ID AND b3.Category = 'B'
+		  INNER JOIN (
+			    SELECT DISTINCT [gPOID] = g2.POID, 
+				     [bPOID] = b2.POID
+			    FROM Order_Qty_Garment og2 WITH (NOLOCK)
+			    INNER JOIN Orders g2 WITH (NOLOCK) ON og2.ID = g2.ID AND g2.Category = 'G'
+			    INNER JOIN Orders b2 WITH (NOLOCK) ON og2.OrderIDFrom = b2.ID  AND b2.Category = 'B'
+			    INNER JOIN Order_Qty_Garment og1 WITH (NOLOCK) ON og1.ID = t.[Sp#] AND og1.Junk = 0
+			    INNER JOIN Orders g1 WITH (NOLOCK) ON og1.ID = g1.ID AND g1.Category = 'G'
+			    INNER JOIN Orders b1 WITH (NOLOCK) ON og1.OrderIDFrom = b1.ID AND b1.Category = 'B'
+			    WHERE (g1.POID = g2.POID OR b1.POID = b2.POID)
+				and og2.Junk = 0
+		  ) tmp ON (g3.POID = tmp.gPOID OR b3.POID = tmp.bPOID) AND og3.Junk = 0
+	) tmp 
+	OUTER APPLY (
+		SELECT [gQty] = SUM(g4.Qty) 
+		FROM Orders g4 WITH (NOLOCK) 
+		WHERE tmp.gPOID = g4.POID 
+		and g4.Category = 'G'
+	) g4
+	OUTER APPLY (
+		SELECT [bQty] = SUM(b4.Qty) 
+		FROM Orders b4 WITH (NOLOCK) 
+		WHERE tmp.bPOID = b4.POID 
+		and b4.Category = 'B'
+	) b4 
+	GROUP BY g4.gQty, b4.bQty
+) g
+
+select t.[Sp#]
+     , t.[StyleID]
+	 , t.BuyerDelivery
+	 , t.SciDelivery
+	 , t.CPU
+     , t.[Brand]
+     , t.[Season]
+	 , g.[BtoG]
+     , t.[*]
+     , t.[From SP#]
+     , t.[Article]
+     , t.[SizeCode]
+     , t.[ToSP_Cmf_PK_Qty]
+     , t.[ToSP_Qty]
+     , t.[ToSp_allocate_qty]
+     , t.[ToSp_Balance]
+     , t.[ToSp_BuyerDelivery]
+     , t.[FromSp_Sewing_qty]
+     , t.[FromSp_Accu_Qty]
+     , t.[FromSp_Avl_Qty]
+     , t.[Is_Trans_Qty]
+     , t.[Is_Trans_Qty_Excess] 
+	 , t.[Junk]
+from #tmp t
+inner join #tmp_Order_Qty_Garment g on t.[Sp#] = g.[Sp#]
+{status_condistion}
+
+drop table #tmp, #tmp_Order_Qty_Garment
+");
 
             return sqlCmd.ToString();
         }
