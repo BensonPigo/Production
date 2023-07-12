c9872b73dff95b48c3533500d1bbc64d62a54942
DOMAIN\jeff.yeh <jeff.yeh@sportscity.com.tw>
2023-07-12 10:03:35 +0800

    ISP20230663 如說明

---
+++ Database/dbo/Functions/GetPo3Spec_Null.sql
@@ -0,0 +1,20 @@
+﻿CREATE FUNCTION [dbo].GetPo3Spec_Null
+(
+	@POID varchar(13)
+	, @Seq1 varchar(3)
+	, @Seq2 varchar(2)
+)
+RETURNS TABLE AS RETURN
+(
+	select *
+	from 
+	(
+		select BomTypeID = BomType.ID, SpecValue = po3s.SpecValue
+		from Production.dbo.BomType with (nolock)
+		left join PO_Supp_Detail_Spec po3s with (nolock) on po3s.ID = @POID and po3s.Seq1 = @Seq1 and po3s.Seq2 = @Seq2 and BomType.ID = po3s.SpecColumnID
+	)tmp
+	pivot
+	(
+		MAX(SpecValue) for BomTypeID in (Color, Size, SizeUnit, ZipperInsert, Article, COO, Gender, CustomerSize, DecLabelSize, BrandFactoryCode, Style, StyleLocation, Season, CareCode, CustomerPO)
+	) as p
+)
