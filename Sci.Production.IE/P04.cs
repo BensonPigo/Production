a3832f9fde76b8a90caa7bb2290adb2ca16795b9
DOMAIN\harry.lee <harry.lee@sportscity.com.tw>
2023-07-18 17:05:55 +0800

    [IST20230804]增加顯示欄位

--- Sci.Production.IE/P04.cs
+++ Sci.Production.IE/P04.cs
@@ -11,72 +11,80 @@ using System.Windows.Forms;
 
 namespace Sci.Production.IE
 {
-    public partial class P04 : Sci.Win.Tems.QueryForm
-    {
-        public P04(ToolStripMenuItem menuitem)
-            : base(menuitem)
-        {
-            InitializeComponent();
-            this.EditMode = true;
-        }
+	public partial class P04 : Sci.Win.Tems.QueryForm
+	{
+		public P04(ToolStripMenuItem menuitem)
+			: base(menuitem)
+		{
+			InitializeComponent();
+			this.EditMode = true;
+		}
 
-        protected override void OnFormLoaded()
-        {
-            base.OnFormLoaded();
+		protected override void OnFormLoaded()
+		{
+			base.OnFormLoaded();
 
-            #region 欄位設定
-            this.Helper.Controls.Grid.Generator(this.gridLineMappingStatus)
-                .Text("FtyGroup", header: "Factory", iseditingreadonly: true, width: Widths.AnsiChars(7))
-                .Text("StyleID", header: "Style#", iseditingreadonly: true, width: Widths.AnsiChars(14))
-                .Text("SeasonID", header: "Season", iseditingreadonly: true, width: Widths.AnsiChars(7))
-                .Text("BrandID", header: "Brand", iseditingreadonly: true, width: Widths.AnsiChars(7))
-                .Date("SewInLine", header: "Sewing Inline", iseditingreadonly: true, width: Widths.AnsiChars(11))
-                .Text("Version", header: "Version", iseditingreadonly: true, width: Widths.AnsiChars(7))
-                .Numeric("ttlTimeDiff", header: "Total % time diff", decimal_places: 2, integer_places: 8, width: Widths.AnsiChars(10), iseditingreadonly: true)
-                .Text("Status", header: "Status", iseditingreadonly: true, width: Widths.AnsiChars(9))
-                .Text("AddName", header: "Add Name", iseditingreadonly: true, width: Widths.AnsiChars(23))
-                .Text("EditName", header: "Edit Name", iseditingreadonly: true, width: Widths.AnsiChars(23))
-                .DateTime("AddDate", header: "Add Date", iseditingreadonly: true)
-                .DateTime("EditDate", header: "Edit Date", iseditingreadonly: true)
-                ;
-            #endregion
-        }
+			#region 欄位設定
+			this.Helper.Controls.Grid.Generator(this.gridLineMappingStatus)
+				.Text("FtyGroup", header: "Factory", iseditingreadonly: true, width: Widths.AnsiChars(7))
+				.Text("StyleID", header: "Style#", iseditingreadonly: true, width: Widths.AnsiChars(14))
+				.Text("SeasonID", header: "Season", iseditingreadonly: true, width: Widths.AnsiChars(7))
+				.Text("BrandID", header: "Brand", iseditingreadonly: true, width: Widths.AnsiChars(7))
+				.Date("SewInLine", header: "Sewing Inline", iseditingreadonly: true, width: Widths.AnsiChars(11))
+				.Text("Version", header: "Version", iseditingreadonly: true, width: Widths.AnsiChars(7))
+				.Text("Phase", header: "Phase", iseditingreadonly: true, width: Widths.AnsiChars(7))
+				.Text("SewingLineID", header: "Sewing Line", iseditingreadonly: true, width: Widths.AnsiChars(7))
+				.Text("Team", header: "Team", iseditingreadonly: true, width: Widths.AnsiChars(5))
+				.Numeric("ttlTimeDiff", header: "Total % time diff", decimal_places: 2, integer_places: 8, width: Widths.AnsiChars(10), iseditingreadonly: true)
+				.Numeric("LBR", header: "LBR", decimal_places: 2, width: Widths.AnsiChars(5), iseditingreadonly: true)
+				.Text("Status", header: "Status", iseditingreadonly: true, width: Widths.AnsiChars(9))
+				.Text("AddName", header: "Add Name", iseditingreadonly: true, width: Widths.AnsiChars(23))
+				.Text("EditName", header: "Edit Name", iseditingreadonly: true, width: Widths.AnsiChars(23))
+				.DateTime("AddDate", header: "Add Date", iseditingreadonly: true)
+				.DateTime("EditDate", header: "Edit Date", iseditingreadonly: true)
+				;
+			#endregion
+		}
 
-        private void BtnQuery_Click(object sender, EventArgs e)
-        {
-            if (MyUtility.Check.Empty(this.dateInlineDate.Value1) || MyUtility.Check.Empty(this.dateInlineDate.Value2))
-            {
-                MyUtility.Msg.WarningBox("Please fill < Sewing Inline >.");
-                this.dateInlineDate.Focus();
-                return;
-            }
+		private void BtnQuery_Click(object sender, EventArgs e)
+		{
+			if (MyUtility.Check.Empty(this.dateInlineDate.Value1) || MyUtility.Check.Empty(this.dateInlineDate.Value2))
+			{
+				MyUtility.Msg.WarningBox("Please fill < Sewing Inline >.");
+				this.dateInlineDate.Focus();
+				return;
+			}
 
-            this.Query();
-        }
+			this.Query();
+		}
 
-        private void Query()
-        {
-            DataTable dtData = new DataTable();
-            this.listControlBindingSource1.DataSource = null;
-            string strWhere = "";
-            if (!MyUtility.Check.Empty(this.dateInlineDate.Value1) && !MyUtility.Check.Empty(this.dateInlineDate.Value2))
-            {
-                strWhere += $@" and o.SewInLine between '{string.Format("{0:yyyy-MM-dd}", this.dateInlineDate.Value1)}' and '{string.Format("{0:yyyy-MM-dd}", this.dateInlineDate.Value2)}'" + Environment.NewLine;
-            }
+		private void Query()
+		{
+			DataTable dtData = new DataTable();
+			this.listControlBindingSource1.DataSource = null;
+			string strWhere = "";
+			if (!MyUtility.Check.Empty(this.dateInlineDate.Value1) && !MyUtility.Check.Empty(this.dateInlineDate.Value2))
+			{
+				strWhere += $@" and o.SewInLine between '{string.Format("{0:yyyy-MM-dd}", this.dateInlineDate.Value1)}' and '{string.Format("{0:yyyy-MM-dd}", this.dateInlineDate.Value2)}'" + Environment.NewLine;
+			}
 
             if (!MyUtility.Check.Empty(this.txtfty.Text))
-            {
-                strWhere += $@" and o.FtyGroup = '{this.txtfty.Text}'";
-            }
+			{
+				strWhere += $@" and o.FtyGroup = '{this.txtfty.Text}'";
+			}
 
-            string sqlcmd = $@"
+			string sqlcmd = $@"
 select o.FtyGroup
 ,o.StyleID
 ,o.SeasonID
 ,o.BrandID
 ,[SewInLine] = MIN(o.SewInLine)
 ,[Version] = isnull(lm.Version,'')
-,[ttlTimeDiff] = IIF(isnull(lm.TotalGSD,0) = 0, 0, cast( Round( (cast((isnull(lm.TotalGSD,0) - isnull(lm.TotalCycle,0)) as float) / cast(isnull(lm.TotalGSD,0) as float)) * 100 ,2)  as float) )
+,[Phase] = isnull(lm.Phase,'')
+,[SewingLineID] = isnull(lm.SewingLineID,'')
+,[Team] = isnull(lm.Team,'')
+,[ttlTimeDiff] = IIF(isnull(lm.TotalGSD,0) = 0, 0, cast( Round( (cast((isnull(lm.TotalGSD,0) - isnull(lm.TotalCycle,0)) as float) / cast(isnull(lm.TotalGSD,0) as float)) * 100, 2)  as float) )
+,[LBR] = IIF(isnull(lm.HighestCycle,0) * isnull(lm.CurrentOperators,0) = 0, 0,  cast( Round(isnull(lm.TotalCycle,0)/isnull(lm.HighestCycle,0)/isnull(lm.CurrentOperators,0) * 100, 2) as float))
 ,[Status] = isnull(lm.Status,'')
 ,[AddName] = isnull(AddName.IdAndNameAndExt,'')
 ,[EditName] = isnull(EditName.IdAndNameAndExt,'')
@@ -100,79 +108,80 @@ where o.Category = 'B' and f.IsProduceFty = 1
 {strWhere}
 group by o.FtyGroup,o.StyleID,o.SeasonID,o.BrandID,lm.Version,lm.Status,AddName.IdAndNameAndExt,EditName.IdAndNameAndExt,lm.AddDate
 ,lm.TotalGSD,lm.TotalCycle,lm.EditDate,lm.StyleID,lm.SeasonID,lm.BrandID,lm.StyleUKey,o.StyleUkey
+,lm.Phase,lm.SewingLineID,lm.Team,lm.HighestCycle,lm.CurrentOperators
 order by o.FtyGroup,o.StyleID,o.SeasonID,o.BrandID
 ";
-            this.ShowWaitMessage("Data Loading....");
+			this.ShowWaitMessage("Data Loading....");
 
-            DualResult result;
-            if (result = DBProxy.Current.Select(null, sqlcmd, out dtData))
-            {
-                if (dtData.Rows.Count == 0)
-                {
-                    MyUtility.Msg.WarningBox("Data not found!!");
-                }
+			DualResult result;
+			if (result = DBProxy.Current.Select(null, sqlcmd, out dtData))
+			{
+				if (dtData.Rows.Count == 0)
+				{
+					MyUtility.Msg.WarningBox("Data not found!!");
+				}
 
-                this.listControlBindingSource1.DataSource = dtData;
-                this.Grid_Filter();
-            }
-            else
-            {
-                this.ShowErr(result);
-            }
+				this.listControlBindingSource1.DataSource = dtData;
+				this.Grid_Filter();
+			}
+			else
+			{
+				this.ShowErr(result);
+			}
 
-            this.HideWaitMessage();
-        }
+			this.HideWaitMessage();
+		}
 
-        private void Grid_Filter()
-        {
-            string filter = string.Empty;
-            if (this.gridLineMappingStatus.RowCount > 0)
-            {
-                switch (this.chkAllStyle.Checked)
-                {
-                    case true:
-                        if (MyUtility.Check.Empty(this.gridLineMappingStatus))
-                        {
-                            break;
-                        }
+		private void Grid_Filter()
+		{
+			string filter = string.Empty;
+			if (this.gridLineMappingStatus.RowCount > 0)
+			{
+				switch (this.chkAllStyle.Checked)
+				{
+					case true:
+						if (MyUtility.Check.Empty(this.gridLineMappingStatus))
+						{
+							break;
+						}
 
-                        if (this.chkLaster.Checked)
-                        {
-                            filter = @" MaxVersion = 1";
-                        }
+						if (this.chkLaster.Checked)
+						{
+							filter = @" MaxVersion = 1";
+						}
 
-                        ((DataTable)this.listControlBindingSource1.DataSource).DefaultView.RowFilter = filter;
-                        break;
+						((DataTable)this.listControlBindingSource1.DataSource).DefaultView.RowFilter = filter;
+						break;
 
-                    case false:
-                        if (MyUtility.Check.Empty(this.gridLineMappingStatus))
-                        {
-                            break;
-                        }
+					case false:
+						if (MyUtility.Check.Empty(this.gridLineMappingStatus))
+						{
+							break;
+						}
 
-                        if (this.chkLaster.Checked)
-                        {
-                            filter = $@" StyleUkey is null and MaxVersion = 1";
-                        }
-                        else
-                        {
-                            filter = $@" StyleUkey is null";
-                        }
+						if (this.chkLaster.Checked)
+						{
+							filter = $@" StyleUkey is null and MaxVersion = 1";
+						}
+						else
+						{
+							filter = $@" StyleUkey is null";
+						}
 
-                        ((DataTable)this.listControlBindingSource1.DataSource).DefaultView.RowFilter = filter;
-                        break;
-                }
-            }
-        }
+						((DataTable)this.listControlBindingSource1.DataSource).DefaultView.RowFilter = filter;
+						break;
+				}
+			}
+		}
 
-        private void ChkAllStyle_CheckedChanged(object sender, EventArgs e)
-        {
-            this.Grid_Filter();
-        }
+		private void ChkAllStyle_CheckedChanged(object sender, EventArgs e)
+		{
+			this.Grid_Filter();
+		}
 
-        private void ChkLaster_CheckedChanged(object sender, EventArgs e)
-        {
-            this.Grid_Filter();
-        }
-    }
+		private void ChkLaster_CheckedChanged(object sender, EventArgs e)
+		{
+			this.Grid_Filter();
+		}
+	}
 }
