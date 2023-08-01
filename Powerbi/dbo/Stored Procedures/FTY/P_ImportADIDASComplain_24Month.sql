aa957c99b520cc74cda7b2cbbba51becf1f941ab
DOMAIN\ryan.tsao <ryan.tsao@sportscity.com.tw>
2023-08-01 14:04:56 +0800

    ISP20230742 新建BI資料表-P_ADIDASComplain_24Month

---
+++ Powerbi/dbo/Stored Procedures/FTY/P_ImportADIDASComplain_24Month.sql
@@ -0,0 +1,28 @@
+﻿CREATE PROCEDURE [dbo].[P_ImportADIDASComplain_24Month]
+AS
+BEGIN
+	DECLARE @sqlcmd NVARCHAR(MAX) = '';
+
+	set @sqlcmd = 
+	N'
+	Delete FROM dbo.P_ADIDASComplain_24Month
+
+	Insert into dbo.P_ADIDASComplain_24Month
+	Select 
+	[Year]
+	,[Month]
+	,[BrandFtyCode]
+	,[FactoryName]
+	,[KPILO]
+	,[PV_RAW_24Month]
+	,[SV_RAW_24Month]
+	,[WHC]
+	,[Defective_Return]
+	,isnull([AddName],'''''''')
+	,[AddDate]
+	,[EditName]
+	,[EditDate]
+	FROM MainServer.Production.dbo.ADIDASComplain_24Month
+	'
+	Exec(@sqlcmd)
+END
