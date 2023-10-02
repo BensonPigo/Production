ce4d527a4a307a1c86657dd6bb23e2f79df3b75b
DOMAIN\ryan.tsao <ryan.tsao@sportscity.com.tw>
2023-10-02 17:19:58 +0800

    ISP20230949 [P03. Line Mapping] - No employee data can be selected in P03

--- Sci.Production.IE/P03.cs
+++ Sci.Production.IE/P03.cs
@@ -492,24 +492,19 @@ and BrandID = '{this.CurrentMaintain["BrandID"]}'
                             DataRow dr = this.detailgrid.GetDataRow<DataRow>(e.RowIndex);
 
                             this.GetEmployee(null);
-
-                            Win.Tools.SelectItem item = new Win.Tools.SelectItem(this.EmployeeData, "ID,FirstName,LastName,Section,", "10,18,16,2,5", dr["EmployeeID"].ToString(), headercaptions: "ID,First Name,Last Name,Section")
-                            {
-                                Width = 700,
-                            };
-                            DialogResult returnResult = item.ShowDialog();
-                            if (returnResult == DialogResult.Cancel)
+                            P03_Operator callNextForm = new P03_Operator(this.EmployeeData, MyUtility.Convert.GetString(this.CurrentMaintain["SewingLineID"]));
+                            DialogResult result = callNextForm.ShowDialog(this);
+                            if (result == DialogResult.Cancel)
                             {
                                 return;
                             }
 
-                            IList<DataRow> selectedData = item.GetSelecteds();
                             DataTable dt = (DataTable)this.detailgridbs.DataSource;
 
-                            DataRow[] errorDataRow = dt.Select($"EmployeeID = '{MyUtility.Convert.GetString(selectedData[0]["ID"])}' and NO <> '{MyUtility.Convert.GetString(dr["No"])}'");
+                            DataRow[] errorDataRow = dt.Select($"EmployeeID = '{MyUtility.Convert.GetString(callNextForm.SelectOperator["ID"])}' and NO <> '{MyUtility.Convert.GetString(dr["No"])}'");
                             if (errorDataRow.Length > 0)
                             {
-                                MyUtility.Msg.WarningBox($"<{selectedData[0]["ID"]} {selectedData[0]["Name"]}> already been used in No.{MyUtility.Convert.GetString(errorDataRow[0]["No"])}!!");
+                                MyUtility.Msg.WarningBox($"<{callNextForm.SelectOperator["ID"]} {callNextForm.SelectOperator["Name"]}> already been used in No.{MyUtility.Convert.GetString(errorDataRow[0]["No"])}!!");
                                 return;
                             }
 
@@ -523,9 +518,9 @@ and BrandID = '{this.CurrentMaintain["BrandID"]}'
                                 {
                                     int dataIndex = sortDataTable.Rows.IndexOf(dataRow);
                                     DataRow row = this.detailgrid.GetDataRow<DataRow>(dataIndex);
-                                    row["EmployeeID"] = selectedData[0]["ID"];
-                                    row["EmployeeName"] = selectedData[0]["Name"];
-                                    row["EmployeeSkill"] = selectedData[0]["Skill"];
+                                    row["EmployeeID"] = callNextForm.SelectOperator["ID"];
+                                    row["EmployeeName"] = callNextForm.SelectOperator["Name"];
+                                    row["EmployeeSkill"] = callNextForm.SelectOperator["Skill"];
                                     row.EndEdit();
                                 }
                             }
@@ -577,23 +572,19 @@ and BrandID = '{this.CurrentMaintain["BrandID"]}'
 
                             this.GetEmployee(null);
 
-                            Win.Tools.SelectItem item = new Win.Tools.SelectItem(this.EmployeeData, "ID,FirstName,LastName,Section,", "10,18,16,2,5", dr["EmployeeID"].ToString(), headercaptions: "ID,First Name,Last Name,Section")
-                            {
-                                Width = 700,
-                            };
-                            DialogResult returnResult = item.ShowDialog();
-                            if (returnResult == DialogResult.Cancel)
+                            P03_Operator callNextForm = new P03_Operator(this.EmployeeData, MyUtility.Convert.GetString(this.CurrentMaintain["SewingLineID"]));
+                            DialogResult result = callNextForm.ShowDialog(this);
+                            if (result == DialogResult.Cancel)
                             {
                                 return;
                             }
 
-                            IList<DataRow> selectedData = item.GetSelecteds();
                             DataTable dt = (DataTable)this.detailgridbs.DataSource;
 
-                            DataRow[] errorDataRow = dt.Select($"EmployeeID = '{MyUtility.Convert.GetString(selectedData[0]["ID"])}' and NO <> '{MyUtility.Convert.GetString(dr["No"])}'");
+                            DataRow[] errorDataRow = dt.Select($"EmployeeID = '{MyUtility.Convert.GetString(callNextForm.SelectOperator["ID"])}' and NO <> '{MyUtility.Convert.GetString(dr["No"])}'");
                             if (errorDataRow.Length > 0)
                             {
-                                MyUtility.Msg.WarningBox($"<{selectedData[0]["ID"]} {selectedData[0]["Name"]}> already been used in No.{MyUtility.Convert.GetString(dr["No"])}!!");
+                                MyUtility.Msg.WarningBox($"<{callNextForm.SelectOperator["ID"]} {callNextForm.SelectOperator["Name"]}> already been used in No.{MyUtility.Convert.GetString(dr["No"])}!!");
                                 return;
                             }
 
@@ -607,9 +598,9 @@ and BrandID = '{this.CurrentMaintain["BrandID"]}'
                                 {
                                     int dataIndex = sortDataTable.Rows.IndexOf(dataRow);
                                     DataRow row = this.detailgrid.GetDataRow<DataRow>(dataIndex);
-                                    row["EmployeeID"] = selectedData[0]["ID"];
-                                    row["EmployeeName"] = selectedData[0]["Name"];
-                                    row["EmployeeSkill"] = selectedData[0]["Skill"];
+                                    row["EmployeeID"] = callNextForm.SelectOperator["ID"];
+                                    row["EmployeeName"] = callNextForm.SelectOperator["Name"];
+                                    row["EmployeeSkill"] = callNextForm.SelectOperator["Skill"];
                                     row.EndEdit();
                                 }
                             }
@@ -1319,11 +1310,11 @@ and Name = @PPA
 
             if (MyUtility.Check.Empty(this.CurrentMaintain["FactoryID"]))
             {
-                sqlCmd = "select ID,FirstName,LastName,Section,Skill , [Name] = iif(LastName+ ','+ FirstName <> ',' ,LastName+ ','+ FirstName,'') from Employee WITH (NOLOCK) where (ResignationDate > GETDATE() or ResignationDate is null) and Junk = 0 " + (iD == null ? string.Empty : " and ID = @id ") + (IsEmptySewingLine ? string.Empty : " and (SewingLineID = @SewingLine or Section = @SewingLine)");
+                sqlCmd = "select ID,FirstName,LastName,Section,Skill,SewingLineID , [Name] = iif(LastName+ ','+ FirstName <> ',' ,LastName+ ','+ FirstName,'') from Employee WITH (NOLOCK) where (ResignationDate > GETDATE() or ResignationDate is null) and Junk = 0 " + (iD == null ? string.Empty : " and ID = @id "); //+ (IsEmptySewingLine ? string.Empty : " and (SewingLineID = @SewingLine or Section = @SewingLine)");
             }
             else
             {
-                sqlCmd = "select ID,FirstName,LastName,Section,Skill , [Name] = iif(LastName+ ','+ FirstName <> ',' ,LastName+ ','+ FirstName,'')  from Employee WITH (NOLOCK) where (ResignationDate > GETDATE() or ResignationDate is null) and Junk = 0 and FactoryID = @factoryid " + (iD == null ? string.Empty : " and ID = @id ") + (IsEmptySewingLine ? string.Empty : " and (SewingLineID = @SewingLine or Section = @SewingLine)");
+                sqlCmd = "select ID,FirstName,LastName,Section,Skill,SewingLineID , [Name] = iif(LastName+ ','+ FirstName <> ',' ,LastName+ ','+ FirstName,'')  from Employee WITH (NOLOCK) where (ResignationDate > GETDATE() or ResignationDate is null) and Junk = 0 and FactoryID = @factoryid " + (iD == null ? string.Empty : " and ID = @id "); // + (IsEmptySewingLine ? string.Empty : " and (SewingLineID = @SewingLine or Section = @SewingLine)");
             }
 
             DualResult result = DBProxy.Current.Select(null, sqlCmd, cmds, out this.EmployeeData);
---
+++ Sci.Production.IE/P03_Operator.cs
@@ -0,0 +1,120 @@
+﻿using Ict.Win;
+using Microsoft.Office.Interop.Excel;
+using Sci.Production.Class.Command;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Collections.Specialized.BitVector32;
+using DataTable = System.Data.DataTable;
+
+namespace Sci.Production.IE
+{
+    /// <inheritdoc/>
+    public partial class P03_Operator : Win.Subs.Base
+    {
+        private DataRow _SelectOperator;
+        private DataTable dtDeful;
+        private DataTable dt;
+        private string strSewingLineID;
+
+        /// <inheritdoc/>
+        public P03_Operator(DataTable dataTable, string sewingLineID)
+        {
+            this.InitializeComponent();
+            this.dtDeful = dataTable;
+            this.strSewingLineID = sewingLineID;
+            bool isEmptySewingLine = MyUtility.Check.Empty(sewingLineID);
+            var strSewingWhere = isEmptySewingLine ? string.Empty : $"(SewingLineID = '{sewingLineID}' or Section = '{sewingLineID}')";
+            this.dt = dataTable.Select(strSewingWhere).TryCopyToDataTable(dataTable);
+        }
+
+        /// <inheritdoc/>
+        protected override void OnFormLoaded()
+        {
+            base.OnFormLoaded();
+            this.gridDetail.IsEditingReadOnly = false;
+            this.gridDetail.DataSource = this.listControlBindingSource1;
+            this.Helper.Controls.Grid.Generator(this.gridDetail)
+            .Text("ID", header: "ID", width: Widths.AnsiChars(25), iseditingreadonly: true)
+            .Text("FirstName", header: "FirstName", width: Widths.AnsiChars(35), iseditingreadonly: true)
+            .Text("LastName", header: "Last Name", width: Widths.AnsiChars(15), iseditingreadonly: true)
+            .Text("Section", header: "Section", width: Widths.AnsiChars(15), iseditingreadonly: true)
+            ;
+            this.listControlBindingSource1.DataSource = this.dt;
+        }
+
+        /// <inheritdoc/>
+        public DataRow SelectOperator
+        {
+            get => this._SelectOperator;
+            set => this._SelectOperator = value;
+        }
+
+        private void ChkDisplay_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.chkDisplay.Checked)
+            {
+                this.listControlBindingSource1.DataSource = this.dtDeful;
+            }
+            else
+            {
+                bool isEmptySewingLine = MyUtility.Check.Empty(this.strSewingLineID);
+                var strSewingWhere = isEmptySewingLine ? string.Empty : $"(SewingLineID = '{this.strSewingLineID}' or Section = '{this.strSewingLineID}')";
+                this.dt = this.dtDeful.Select(strSewingWhere).TryCopyToDataTable(this.dtDeful);
+                this.listControlBindingSource1.DataSource = this.dt;
+            }
+        }
+
+        // Find
+        private void BtnFind_Click(object sender, EventArgs e)
+        {
+            StringBuilder filterCondition = new StringBuilder();
+            List<SqlParameter> paras = new List<SqlParameter>();
+
+            List<string> descList = new List<string>();
+            if (!string.IsNullOrEmpty(this.txtDescription.Text))
+            {
+                string tmp = this.txtDescription.Text.Replace("'", "''");
+                descList = tmp.Split(';').Where(o => !string.IsNullOrEmpty(o)).ToList();
+            }
+ 
+            if (descList.Any())
+            {
+                filterCondition.Append($@" and (ID like '%{string.Join("%' and ID like '%", descList)}%' ) ");
+                filterCondition.Append($@" or (FirstName like '%{string.Join("%' and FirstName like '%", descList)}%' ) ");
+                filterCondition.Append($@" or (LastName like '%{string.Join("%' and LastName like '%", descList)}%' ) ");
+                filterCondition.Append($@" or (Section like '%{string.Join("%' and Section like '%", descList)}%' ) ");
+            }
+
+            if (filterCondition.Length > 0)
+            {
+                string filter = filterCondition.ToString();
+                (this.listControlBindingSource1.DataSource as DataTable).DefaultView.RowFilter = "1=1" + filter;
+            }
+            else
+            {
+                (this.listControlBindingSource1.DataSource as DataTable).DefaultView.RowFilter = string.Empty;
+            }
+
+            this.gridDetail.AutoResizeColumns();
+        }
+
+        private void BtnSelect_Click(object sender, EventArgs e)
+        {
+            DataGridViewSelectedRowCollection selectRows = this.gridDetail.SelectedRows;
+
+            foreach (DataGridViewRow datarow in selectRows)
+            {
+                this.SelectOperator = ((DataRowView)datarow.DataBoundItem).Row;
+            }
+            this.DialogResult = DialogResult.OK;
+        }
+    }
+}
