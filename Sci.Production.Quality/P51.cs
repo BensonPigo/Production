da90c829e8a1650981978fd4a6d50a665b143338
DOMAIN\willy.wei <willy.wei@sportscity.com.tw>
2023-11-06 17:04:28 +0800

    ISP20231084 P51 where 條件同Trade, P52 調整Po_supp_detail foc > 0 也要顯示

--- Sci.Production.Quality/P51.cs
+++ Sci.Production.Quality/P51.cs
@@ -568,6 +568,23 @@ Update ExportRefnoSentReport SET  AWBNO = @updateData, EditName = @UserID ,EditD
                 join = "LEFT JOIN dbo.ExportRefnoSentReport sr WITH (NOLOCK) on sr.ExportID = main.ExportID  and sr.BrandRefno = main.BrandRefno and sr.ColorID = main.ColorID and sr.BrandID = main.BrandID and sr.DocumentName = @DocumentName";
             }
 
+            // 預設關聯庫存項
+            var getpo3_sql = @"
+LEFT JOIN PO_Supp_Detail stockPO3 with (nolock)
+    on iif(po3.StockPOID >'', 1, 0) = 1
+    and stockPO3.ID =  po3.StockPOID
+	and stockPO3.Seq1 = po3.StockSeq1
+	and stockPO3.Seq2 = po3.StockSeq2	        
+Outer APPLY (
+	Select ID = COALESCE(stockPO3.ID, po3.ID)
+    , SCIRefno = COALESCE(stockPO3.SCIRefno, po3.SCIRefno)
+    , Seq1 = COALESCE(stockPO3.Seq1, po3.Seq1)
+    , Seq2 = COALESCE(stockPO3.Seq2, po3.Seq2)
+    , Junk = COALESCE(stockPO3.Junk, po3.Junk)
+    , Qty = COALESCE(stockPO3.Qty, po3.Qty)
+    , Foc = COALESCE(stockPO3.Foc, po3.Foc)
+) getpo3";
+
             #region 基本檔設定
             conditions.AppendLine($" and f.type='{this.drBasic["FabricType"]}'");
             conditions.AppendLine($" and f.BrandRefNo <> ''");
@@ -575,6 +592,22 @@ Update ExportRefnoSentReport SET  AWBNO = @updateData, EditName = @UserID ,EditD
             parmes.Add(new SqlParameter() { ParameterName = "@documentName", SqlDbType = SqlDbType.VarChar, Size = 100, Value = this.cboDocumentname.Text });
             parmes.Add(new SqlParameter() { ParameterName = "@brandID", SqlDbType = SqlDbType.VarChar, Size = 8, Value = this.txtBrand1.Text });
             parmes.Add(new SqlParameter("@FileRule", this.drBasic["FileRule"]));
+
+            // 排除庫存項
+            if (MyUtility.Convert.GetBool(this.drBasic["ExcludeStock"]))
+            {
+                conditions.AppendLine($" and (po3.seq1 < '70' or po3.seq1 > '79')");
+                getpo3_sql = @"
+Outer APPLY (
+	Select ID = po3.ID
+    , SCIRefno = po3.SCIRefno
+    , Seq1 = po3.Seq1
+    , Seq2 = po3.Seq2
+    , Junk = po3.Junk
+    , Qty = po3.Qty
+    , Foc = po3.Foc
+) getpo3";
+            }
             #endregion
 
             if (this.txtWKID.Text != string.Empty)
@@ -757,9 +790,7 @@ Update ExportRefnoSentReport SET  AWBNO = @updateData, EditName = @UserID ,EditD
                    ,canModify = CAST(iif((chkNoRes.value is null and '{this.drBasic["Responsibility"]}' = 'F') or chkNoRes.value = 'F', 1, 0) AS BIT)
                    ,o.FtyGroup
                  from  PO_Supp_Detail po3 WITH (NOLOCK)
-                 LEFT JOIN PO_Supp_Detail stockPO3 with (nolock) on iif(po3.StockPOID >'', 1, 0) = 0 AND stockPO3.ID =  po3.StockPOID
-			     and stockPO3.Seq1 = po3.StockSeq1
-			     and stockPO3.Seq2 = po3.StockSeq2	
+                 {getpo3_sql}
                  INNER JOIN Orders o with(nolock) on o.ID = IIF(IsNull(po3.StockPOID, '') = '' , po3.ID, stockPO3.ID)
                  inner join Fabric f WITH (NOLOCK) on f.SCIRefno = IIF(IsNull(po3.StockPOID, '') = '' , po3.SCIRefno, stockPO3.SCIRefno)
                  inner join PO_Supp po2 WITH (NOLOCK) on po2.ID = o.ID and po2.Seq1 = IIF(IsNull(po3.StockPOID, '') = '' , po3.Seq1, stockPO3.Seq1)
@@ -778,13 +809,12 @@ Update ExportRefnoSentReport SET  AWBNO = @updateData, EditName = @UserID ,EditD
                  Outer apply(
                     select top 1 value = Responsibility FROM MaterialDocument_Responsbility where DocumentName = @documentName and BrandID = @brandID and SuppID = s2.ID
                 )chkNoRes
-                where  o.Junk = 0
-	                and o.Qty > 0
-	                and po2.SuppID <> 'FTY'
-                    and IIF(IsNull(po3.StockPOID, '') = '' , po3.Junk, stockPO3.Junk)  = 0        
-	                and IIF(IsNull(po3.StockPOID, '') = '' , po3.Qty, stockPO3.Qty) > 0
+                where po2.SuppID <> 'FTY'
+                    and getpo3.Junk  = 0     
+                    and (getpo3.Qty > 0 or getpo3.Foc > 0)
                     and o.BrandID = @brandID
 	                and s.DevOption = 0
+                    and f.BrandRefNo <> ''
                     and {conditions}
                 GROUP BY ed.ID,e.Eta,iif({this.drBasic["FileRule"]} = 5, s2.ID, Supp.ID),iif({this.drBasic["FileRule"]} = 5, s2.AbbEN, Supp.AbbEN),f.BrandRefNo,Color.ID,Color.Name,o.BrandID,iif({this.drBasic["FileRule"]} = 5, '', ed.ExportIDOld),iif({this.drBasic["FileRule"]} = 5, '', e.InvNo),iif({this.drBasic["FileRule"]} = 5, '', ed.Pino),iif({this.drBasic["FileRule"]} = 5, '', ed.POID),iif({this.drBasic["FileRule"]} = 5, '', ed.Seq1),iif({this.drBasic["FileRule"]} = 5, '', ed.Seq2),o.FactoryID,chkNoRes.value,o.FtyGroup
         )main
