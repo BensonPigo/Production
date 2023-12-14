0cf70f1d0df9353ad1d5698b7a64a1ed0a1ed3c0
DOMAIN\jack.hsu <jack.hsu@sportscity.com.tw>
2023-12-14 15:48:22 +0800

    ISP20231233 - BI 新增P_ProdEfficiencyByFactorySewingLine

--- Powerbi/dbo/Stored Procedures/FTY/execAllPowerBISP.sql
+++ Powerbi/dbo/Stored Procedures/FTY/execAllPowerBISP.sql
@@ -1420,6 +1420,48 @@ SET @ErrorMessage = ''
 	SET @ErrDesc = ''
 
 /****************************************************************************************************************************/
+/***********************************P_Import_ProdEfficiencyByFactorySewingLine****************************************************************/
+BEGIN TRY
+	set @StartDate = cast(concat(cast(year(getdate())as varchar), cast(month(dateadd(month, -1, getdate())) as varchar), '01') as date) 
+	set @Stime = getdate()  
+	execute [dbo].[P_Import_ProdEfficiencyByFactorySewingLine] @StartDate
+	set @Etime = getdate()
+END TRY
+
+BEGIN CATCH
+
+SET @ErrorMessage = 
+'
+[33-P_Import_ProdEfficiencyByFactorySewingLine]' + CHAR(13) +
+',錯誤代碼: ' + CONVERT(VARCHAR, ERROR_NUMBER()) + CHAR(13) +
+',錯誤行數: ' + CONVERT(VARCHAR, ERROR_LINE()) + CHAR(13) +
+',錯誤訊息: ' + ERROR_MESSAGE()
+
+SET @ErrDesc = '錯誤代碼: ' + CONVERT(VARCHAR, ERROR_NUMBER()) +
+',錯誤行數: ' + CONVERT(VARCHAR, ERROR_LINE())  +
+',錯誤訊息: ' + ERROR_MESSAGE()
+
+SET @ErrorStatus = 0
+
+END CATCH;
+IF (@ErrorMessage IS NULL or @ErrorMessage='')
+BEGIN 
+	set @desc += CHAR(13) + '
+[33-P_Import_ProdEfficiencyByFactorySewingLine] is completed' + ' Time:' + FORMAT(@Stime, 'yyyy/MM/dd HH:mm:ss') + ' - ' + FORMAT(@Etime, 'yyyy/MM/dd HH:mm:ss')
+END
+ELSE
+BEGIN
+	set @desc += CHAR(13) + @ErrorMessage
+END
+SET @ErrorMessage = ''
+
+-- Write in P_TransLog
+	insert into P_TransLog(functionName,Description,StartTime,EndTime,TransCode) 
+	values('P_Import_ProdEfficiencyByFactorySewingLine',@ErrDesc,@Stime,@Etime,@TransCode)
+
+	SET @ErrDesc = ''
+
+/****************************************************************************************************************************/
 DECLARE @comboDesc nvarchar(4000);
 
 	set @comboDesc = '
