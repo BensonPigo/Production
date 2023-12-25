d58d8be7dd8b9a58623b8cbdb919448dd539f326
DOMAIN\jack.hsu <jack.hsu@sportscity.com.tw>
2023-12-25 15:59:47 +0800

    ISP20231268 - BI 新增 P_CartonStatusTrackingList

---
+++ Database/dbo/Stored Procedures/PPIC_R21_FORBI.sql
@@ -0,0 +1,247 @@
+﻿CREATE PROCEDURE [dbo].[PPIC_R21_FORBI]
+	@StartDate as date
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	if @StartDate is null
+	begin
+		set @StartDate = DATEADD(MONTH,-1,GETDATE())
+	end
+
+	select distinct [KPIGroup] = f.KPICode
+		, [Fty] = o.FactoryID
+		, [Line] = ISNULL(Reverse(stuff(Reverse(o.SewLine),1,1,'')), '')
+		, [SP] = o.ID
+		, [SeqNo] = oqs.Seq
+		, [Category] = ISNULL(d.Name, '')
+		, [Brand] = o.BrandID
+		, [Style] = o.StyleID
+		, [PONO] = o.CustPONo
+		, [Season] = o.SeasonID
+		, [Destination] = ISNULL(p.Dest, '')
+		, o.SciDelivery
+		, oqs.BuyerDelivery
+		, [PackingListID] = p.ID
+		, [CtnNo] = pld.CTNStartNo
+		, [Size] = ISNULL(Size.val, '')
+		, [CartonQty] = ISNULL(CartonQty.val, 0)
+		, [Status] = case 
+				when HaulingScanTime.val is null 
+						and PackingAuditScanTime.val is null 
+						and M360MDScanTime.val is null
+						and pld.ScanEditDate  is null 
+						and pld.TransferDate is null 
+						then 'Fty'
+				when pld.TransferDate is null and PackingAuditScanTime.val > isnull(HaulingScanTime.val, '19710101') and PackingAuditScanTime.val > isnull(M360MDScanTime.val, '19710101') and PackingAuditScanTime.val > isnull(pld.ScanEditDate, '19710101') 
+						then 'Packing Audit'
+				when pld.TransferDate is null and HaulingScanTime.val > isnull(PackingAuditScanTime.val, '19710101') and HaulingScanTime.val > isnull(M360MDScanTime.val, '19710101') and HaulingScanTime.val > isnull(pld.ScanEditDate, '19710101') 
+						then 'Hauiling'
+				when pld.TransferDate is null and M360MDScanTime.val > isnull(PackingAuditScanTime.val, '19710101') and M360MDScanTime.val > isnull(HaulingScanTime.val, '19710101') and M360MDScanTime.val > isnull(pld.ScanEditDate, '19710101') 
+						then 'M360 MD'
+				when pld.TransferDate is null and pld.ScanEditDate > isnull(PackingAuditScanTime.val, '19710101') and pld.ScanEditDate > isnull(HaulingScanTime.val, '19710101') and pld.ScanEditDate > isnull(M360MDScanTime.val, '19710101') 
+						then 'Scan & Pack'
+				when pld.TransferDate is not null and ClogReceiveTime.val is null 
+						then 'Transit to CLOG'
+				when CFAReceiveTime.val is not null and CFAReceiveTime.val > isnull(CFAReturnTime.val, '19710101') 
+						then 'CFA'
+				when ClogReceiveTime.val is not null and (CFAReceiveTime.val is null or ClogReceiveFromCFATime.val > isnull(CFAReturnTime.val, '19710101') )
+						then 'Clog'
+			else '' end 
+		, [HaulingScanTime] = HaulingScanTime.val
+		, [HauledQty] = ISNULL(HauledQty.val, 0)
+		, [DryRoomReceiveTime] = DryRoomReceiveTime.val
+		, [DryRoomTransferTime] = DryRoomTransferTime.val 
+		, [MDScanTime] = MDScan.val
+		, [MDFailQty] = ISNULL(MDFailQty.val, 0)
+		, [PackingAuditScanTime] = PackingAuditScanTime.val
+		, [PackingAuditFailQty] = PackingAuditFailQty.val
+		, [M360MDScanTime] = M360MDScanTime.val
+		, [M360MDFailQty] = M360MDFailQty.val
+		, [TransferToPackingErrorTime] = TransferToPackingErrorTime.val
+		, [ConfirmPackingErrorReviseTime] = ConfirmPackingErrorReviseTime.val
+		, [ScanAndPackTime] = pld.ScanEditDate
+		, [ScanQty] = ISNULL(pld.ScanQty, 0)
+		, [FtyTransferToClogTime] = FtyTransferToClogTime.val
+		, [ClogReceiveTime] = ClogReceiveTime.val
+		, [ClogLocation] = ISNULL(pld.ClogLocationId, '')
+		, [ClogReturnTime] = ClogReturnTime.val
+		, [ClogTransferToCFATime] = ClogTransferToCFATime.val
+		, [CFAReceiveTime] = CFAReceiveTime.val
+		, [CFAReturnTime] = CFAReturnTime.val
+		, [CFAReturnDestination] = ISNULL(CFAReturnDestination.val, '')
+		, [ClogReceiveFromCFATime] = ClogReceiveFromCFATime.val
+		, pld.DisposeDate
+		, [PulloutComplete] = IIF(o.PulloutComplete = 1, 'Y', 'N')
+		, p.PulloutDate
+	from Production.dbo.Orders o with (nolock)
+	inner join Production.dbo.Order_QtyShip oqs with (nolock) on o.ID = oqs.Id
+	inner join Production.dbo.Factory f with (nolock) on f.ID = o.FactoryID
+	inner join Production.dbo.PackingList_Detail pld with (nolock) on pld.OrderID = oqs.ID and pld.OrderShipmodeSeq = oqs.Seq and pld.CTNQty = 1
+	inner join Production.dbo.PackingList p with (nolock) on p.ID = pld.ID
+	left join Production.dbo.DropDownList d with (nolock) on d.Type = 'Category' AND d.ID = o.Category	
+	outer apply(
+		select [val] = Stuff((select distinct concat('/',SizeCode) 
+								from Production.dbo.PackingList_Detail pd with(nolock)
+								where	pd.ID = pld.ID and
+									pd.CtnStartNo = pld.CtnStartNo
+								FOR XML PATH('')), 1, 1, '')
+	) Size
+	outer apply(
+		select [val] = (select sum(pd.shipQty)
+						from Production.dbo.PackingList_Detail pd with(nolock)
+						where	pd.ID = pld.ID and
+								pd.CtnStartNo = pld.CtnStartNo and
+								pd.OrderID = pld.OrderID)
+	) CartonQty
+	outer apply(
+		select [val] = (select max(ch.AddDate)
+						from Production.dbo.CTNHauling ch with(nolock)
+						where	ch.PackingListID = pld.ID and
+								ch.CtnStartNo = pld.CtnStartNo and
+								ch.OrderID = pld.OrderID)
+	) HaulingScanTime
+	outer apply(
+		select [val] = (select isnull(sum(QtyPerCtn),0) 
+						from Production.dbo.PackingList_Detail pd with(nolock)
+						where pd.ID = pld.ID
+						and exists (select 1 from Production.dbo.CTNHauling ch with(nolock)
+									where ch.PackingListID = pld.ID
+									and ch.CTNStartNo = pld.CTNStartNo
+									and ch.OrderID = pld.OrderID
+									and ch.SCICtnNo = pd.SCICtnNo))
+	) HauledQty
+	outer apply(
+		select [val] = (select max(dr.AddDate)
+						from Production.dbo.DryReceive dr with(nolock)
+						where	dr.PackingListID = pld.ID and
+								dr.CtnStartNo = pld.CtnStartNo and
+								dr.OrderID = pld.OrderID)
+	) DryRoomReceiveTime
+	outer apply(
+		select [val] = (select max(dr.AddDate)
+						from Production.dbo.DryTransfer dr with(nolock)
+						where	dr.PackingListID = pld.ID and
+								dr.CtnStartNo = pld.CtnStartNo and
+								dr.OrderID = pld.OrderID)
+	) DryRoomTransferTime
+	outer apply( 
+		select [val] = (select max(AddDate)
+						from Production.dbo.MDScan md with (nolock) 
+						where	md.DataRemark = 'Create from PMS' and
+								md.PackingListID = pld.ID and 
+								md.CTNStartNo = pld.CTNStartNo and 
+								md.OrderID = pld.OrderID)
+	) MDScan
+	outer apply(
+		select [val] = (select  MAX(MDFailQty)
+				from Production.dbo.MDScan md with (nolock) 
+				where	md.PackingListID = pld.ID and 
+						md.CTNStartNo = pld.CTNStartNo and 
+						md.OrderID = pld.OrderID and
+						md.AddDate = MDScan.val)
+	) MDFailQty
+	outer apply(
+		select [val] = (select MAX(AddDate)
+				from Production.dbo.CTNPackingAudit pa with (nolock) 
+				where	pa.PackingListID = pld.ID and 
+						pa.SCICtnNo = pld.SCICtnNo)
+	) PackingAuditScanTime
+	outer apply(
+		select [val] = (select isnull(sum(pa.Qty), 0)
+				from Production.dbo.CTNPackingAudit pa with (nolock) 
+				where	pa.PackingListID = pld.ID and 
+						pa.SCICtnNo = pld.SCICtnNo and 
+						pa.AddDate = PackingAuditScanTime.val)
+	) PackingAuditFailQty
+	
+	outer apply(
+		select [val] = (select MAX(AddDate)
+				from Production.dbo.MDScan md with (nolock) 
+				where	md.DataRemark = 'Create from M360' and
+						md.PackingListID = pld.ID and 
+						md.SCICtnNo = pld.SCICtnNo)
+	) M360MDScanTime
+	outer apply(
+		select [val] = (select isnull(sum(md.MDFailQty), 0)
+				from Production.dbo.MDScan md with (nolock) 
+				where	md.DataRemark = 'Create from M360' and
+						md.PackingListID = pld.ID and 
+						md.SCICtnNo = pld.SCICtnNo and
+						md.AddDate = M360MDScanTime.val)
+	) M360MDFailQty
+	outer apply(
+		select [val] = (select MAX(AddDate)
+				from Production.dbo.PackErrTransfer pe with (nolock) 
+				where	pe.PackingListID = pld.ID and 
+						pe.CTNStartNo = pld.CTNStartNo and
+						pe.OrderID = pld.OrderID)
+	) TransferToPackingErrorTime
+	outer apply(
+		select [val] = (select MAX(AddDate)
+				from Production.dbo.PackErrCFM pe with (nolock) 
+				where	pe.PackingListID = pld.ID and 
+						pe.CTNStartNo = pld.CTNStartNo and
+						pe.OrderID = pld.OrderID)
+	) ConfirmPackingErrorReviseTime
+	outer apply(
+		select [val] = (select MAX(AddDate)
+				from Production.dbo.TransferToClog tc with (nolock) 
+				where	tc.PackingListID = pld.ID and 
+						tc.CTNStartNo = pld.CTNStartNo and
+						tc.OrderID = pld.OrderID)
+	) FtyTransferToClogTime
+	outer apply(
+		select [val] = (select MAX(AddDate)
+				from Production.dbo.ClogReceive cr with (nolock) 
+				where	cr.PackingListID = pld.ID and 
+						cr.CTNStartNo = pld.CTNStartNo and
+						cr.OrderID = pld.OrderID)
+	) ClogReceiveTime
+	outer apply(
+		select [val] = (select MAX(AddDate)
+				from Production.dbo.ClogReturn cr with (nolock) 
+				where	cr.PackingListID = pld.ID and 
+						cr.CTNStartNo = pld.CTNStartNo and
+						cr.OrderID = pld.OrderID)
+	) ClogReturnTime
+	outer apply(
+		select [val] = (select MAX(AddDate)
+				from Production.dbo.TransferToCFA tc with (nolock) 
+				where	tc.PackingListID = pld.ID and 
+						tc.CTNStartNo = pld.CTNStartNo and
+						tc.OrderID = pld.OrderID)
+	) ClogTransferToCFATime
+	outer apply(
+		select [val] = (select MAX(AddDate)
+				from Production.dbo.CFAReceive cr with (nolock) 
+				where	cr.PackingListID = pld.ID and 
+						cr.CTNStartNo = pld.CTNStartNo and
+						cr.OrderID = pld.OrderID)
+	) CFAReceiveTime
+	outer apply(
+		select [val] = (select MAX(AddDate)
+				from Production.dbo.CFAReturn cr with (nolock) 
+				where	cr.PackingListID = pld.ID and 
+						cr.CTNStartNo = pld.CTNStartNo and
+						cr.OrderID = pld.OrderID)
+	) CFAReturnTime
+	outer apply(
+		select [val] = (select MAX(AddDate)
+				from Production.dbo.CFAReturn cr with (nolock) 
+				where	cr.PackingListID = pld.ID and 
+						cr.CTNStartNo = pld.CTNStartNo and
+						cr.OrderID = pld.OrderID and 
+						cr.AddDate = CFAReturnTime.val)
+	) CFAReturnDestination
+	outer apply(
+		select [val] = (select MAX(AddDate)
+				from Production.dbo.ClogReceiveCFA cr with (nolock) 
+				where	cr.PackingListID = pld.ID and 
+						cr.CTNStartNo = pld.CTNStartNo and
+						cr.OrderID = pld.OrderID)
+	) ClogReceiveFromCFATime
+	where oqs.BuyerDelivery >= @StartDate
+	and o.Category in ('B', 'G')
+
+END
---
+++ Powerbi/dbo/Tables/P_CartonStatusTrackingList.sql
@@ -0,0 +1,255 @@
+﻿CREATE TABLE [dbo].[P_CartonStatusTrackingList](
+	[KPIGroup] [varchar](8) NOT NULL,
+	[Fty] [varchar](8) NOT NULL,
+	[Line] [varchar](180) NOT NULL,
+	[SP] [varchar](13) NOT NULL,
+	[SeqNo] [varchar](2) NOT NULL,
+	[Category] [varchar](7) NOT NULL,
+	[Brand] [varchar](8) NOT NULL,
+	[Style] [varchar](15) NOT NULL,
+	[PONO] [varchar](30) NOT NULL,
+	[Season] [varchar](10) NOT NULL,
+	[Destination] [varchar](2) NOT NULL,
+	[SCIDelivery] [date] NULL,
+	[BuyerDelivery] [date] NULL,
+	[PackingListID] [varchar](13) NOT NULL,
+	[CtnNo] [varchar](6) NOT NULL,
+	[Size] [varchar](180) NOT NULL,
+	[CartonQty] [int] NOT NULL,
+	[Status] [varchar](20) NOT NULL,
+	[HaulingScanTime] [datetime] NULL,
+	[HauledQty] [int] NOT NULL,
+	[DryRoomReceiveTime] [datetime] NULL,
+	[DryRoomTransferTime] [datetime] NULL,
+	[MDScanTime] [datetime] NULL,
+	[MDFailQty] [int] NOT NULL,
+	[PackingAuditScanTime] [datetime] NULL,
+	[PackingAuditFailQty] [int] NOT NULL,
+	[M360MDScanTime] [datetime] NULL,
+	[M360MDFailQty] [int] NOT NULL,
+	[TransferToPackingErrorTime] [datetime] NULL,
+	[ConfirmPackingErrorReviseTime] [datetime] NULL,
+	[ScanAndPackTime] [datetime] NULL,
+	[ScanQty] [int] NOT NULL,
+	[FtyTransferToClogTime] [datetime] NULL,
+	[ClogReceiveTime] [datetime] NULL,
+	[ClogLocation] [nvarchar](50) NOT NULL,
+	[ClogReturnTime] [datetime] NULL,
+	[ClogTransferToCFATime] [datetime] NULL,
+	[CFAReceiveTime] [datetime] NULL,
+	[CFAReturnTime] [datetime] NULL,
+	[CFAReturnDestination] [varchar](7) NOT NULL,
+	[ClogReceiveFromCFATime] [datetime] NULL,
+	[DisposeDate] [date] NULL,
+	[PulloutComplete] [varchar](1) NOT NULL,
+	[PulloutDate] [date] NULL,
+ CONSTRAINT [PK_P_CartonStatusTrackingList] PRIMARY KEY CLUSTERED 
+(
+	[SP] ASC,
+	[SeqNo] ASC,
+	[PackingListID] ASC,
+	[CtnNo] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_KPIGroup]  DEFAULT ('') FOR [KPIGroup]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_Fty]  DEFAULT ('') FOR [Fty]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_Line]  DEFAULT ('') FOR [Line]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_SP]  DEFAULT ('') FOR [SP]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_SeqNo]  DEFAULT ('') FOR [SeqNo]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_Category]  DEFAULT ('') FOR [Category]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_Brand]  DEFAULT ('') FOR [Brand]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_Style]  DEFAULT ('') FOR [Style]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_PONO]  DEFAULT ('') FOR [PONO]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_Season]  DEFAULT ('') FOR [Season]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_Destination]  DEFAULT ('') FOR [Destination]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_PackingListID]  DEFAULT ('') FOR [PackingListID]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_CtnNo]  DEFAULT ('') FOR [CtnNo]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_Size]  DEFAULT ('') FOR [Size]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_CartonQty]  DEFAULT ((0)) FOR [CartonQty]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_HauledQty]  DEFAULT ((0)) FOR [HauledQty]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_MDFailQty]  DEFAULT ((0)) FOR [MDFailQty]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_PackingAuditFailQty]  DEFAULT ((0)) FOR [PackingAuditFailQty]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_M360MDFailQty]  DEFAULT ((0)) FOR [M360MDFailQty]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_ScanQty]  DEFAULT ((0)) FOR [ScanQty]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_ClogLocation]  DEFAULT ('') FOR [ClogLocation]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_CFAReturnDestination]  DEFAULT ('') FOR [CFAReturnDestination]
+GO
+
+ALTER TABLE [dbo].[P_CartonStatusTrackingList] ADD  CONSTRAINT [DF_P_CartonStatusTrackingList_PulloutComplete]  DEFAULT ('') FOR [PulloutComplete]
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'SDP KPI Code' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'KPIGroup'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Order Factory' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'Fty'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Sewing Line' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'Line'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Order No' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'SP'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Shipment Seq' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'SeqNo'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Category' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'Category'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Brand' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'Brand'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Style' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'Style'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Cust PONo' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'PONO'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Season' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'Season'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Destination' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'Destination'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'SCI Delivery' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'SCIDelivery'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Buyer Delivery' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'BuyerDelivery'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Packing List #' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'PackingListID'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Carton#' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'CtnNo'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Ctn Size' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'Size'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Qty per Carton' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'CartonQty'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Carton Status' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'Status'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Hauling Scan Time' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'HaulingScanTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Hauled Qty' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'HauledQty'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Show the last scan time in Sewing/P06' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'DryRoomReceiveTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Show the last scan time in Sewing/P12' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'DryRoomTransferTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Show the last scan time in Sewing/P08' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'MDScanTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'MD Scan discrepancy qty' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'MDFailQty'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Show the last scan time in Packing/P29' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'PackingAuditScanTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Show the discrepancy qty in Packing/P29 with the last audit time' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'PackingAuditFailQty'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Show the scan time in Packing/P09 with data remark = ''Create from M360'' and the last scan time' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'M360MDScanTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Show the discrepancy qty  in Packing/P09 with data remark = ''Create from M360'' and the last scan time' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'M360MDFailQty'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Show the last scan time in Packing/P19' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'TransferToPackingErrorTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Show the last scan time in Packing/P20' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'ConfirmPackingErrorReviseTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Show the last scan time in Packing/P18' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'ScanAndPackTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Complete Scan & Pack qty' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'ScanQty'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Show the last scan time in Packing/P10' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'FtyTransferToClogTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Show the last scan time in Clog/P02' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'ClogReceiveTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'The current location in Clog' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'ClogLocation'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Show the last scan time in Clog/P03' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'ClogReturnTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Show the last scan time in Clog/P07' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'ClogTransferToCFATime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Show the last scan time in QA/P23' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'CFAReceiveTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Show the last scan time in QA/P24' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'CFAReturnTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'CFA can return to Clog or Fty' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'CFAReturnDestination'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Show the last scan time in Clog/P08' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'ClogReceiveFromCFATime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'This ctn dispose date' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'DisposeDate'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Y: Pullout; N: Not yet pullout' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'PulloutComplete'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'This packing list pullout date' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'P_CartonStatusTrackingList', @level2type=N'COLUMN',@level2name=N'PulloutDate'
+GO
