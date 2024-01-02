7a7b00bee083aeb8ef97f25faf188e326154ff95
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2024-01-02 10:45:59 +0800

    ISP20231140  - ISP20231298的修改，接續在ISP20231140 Branch裡面做

---
+++ Database/dbo/Tables/AcceptableQualityLevelsPro.sql
@@ -0,0 +1,57 @@
+﻿CREATE TABLE [dbo].[AcceptableQualityLevelsPro](
+	[ProUkey] [bigint] IDENTITY(1,1) NOT NULL,
+	[InspectionLevels] [varchar](5) NOT NULL,
+	[LotSize_Start] [int] NOT NULL,
+	[LotSize_End] [int] NOT NULL,
+	[SampleSize] [int] NOT NULL,
+	[AQLType] [numeric](2, 1) NOT NULL,
+	[BrandID] [varchar](8) NOT NULL,
+	[Category] [varchar](15) NOT NULL,
+	[Junk] [bit] NOT NULL,
+ CONSTRAINT [PK_AcceptableQualityLevelsPro] PRIMARY KEY CLUSTERED 
+(
+	[ProUkey] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[AcceptableQualityLevelsPro] ADD  CONSTRAINT [DF_AcceptableQualityLevelsPro_InspectionLevels]  DEFAULT ('') FOR [InspectionLevels]
+GO
+
+ALTER TABLE [dbo].[AcceptableQualityLevelsPro] ADD  CONSTRAINT [DF_AcceptableQualityLevelsPro_LotSize_Start]  DEFAULT ((0)) FOR [LotSize_Start]
+GO
+
+ALTER TABLE [dbo].[AcceptableQualityLevelsPro] ADD  CONSTRAINT [DF_AcceptableQualityLevelsPro_LotSize_End]  DEFAULT ((0)) FOR [LotSize_End]
+GO
+
+ALTER TABLE [dbo].[AcceptableQualityLevelsPro] ADD  CONSTRAINT [DF_AcceptableQualityLevelsPro_SampleSize]  DEFAULT ((0)) FOR [SampleSize]
+GO
+
+ALTER TABLE [dbo].[AcceptableQualityLevelsPro] ADD  CONSTRAINT [DF_AcceptableQualityLevelsPro_AQLType]  DEFAULT ((0)) FOR [AQLType]
+GO
+
+ALTER TABLE [dbo].[AcceptableQualityLevelsPro] ADD  CONSTRAINT [DF_AcceptableQualityLevelsPro_BrandID]  DEFAULT ('') FOR [BrandID]
+GO
+
+ALTER TABLE [dbo].[AcceptableQualityLevelsPro] ADD  CONSTRAINT [DF_AcceptableQualityLevelsPro_Category]  DEFAULT ('') FOR [Category]
+GO
+
+ALTER TABLE [dbo].[AcceptableQualityLevelsPro] ADD  CONSTRAINT [DF_AcceptableQualityLevelsPro_Junk]  DEFAULT ((0)) FOR [Junk]
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'抽樣計劃' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'AcceptableQualityLevelsPro', @level2type=N'COLUMN',@level2name=N'InspectionLevels'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'產品總數 - 判斷範圍起始值' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'AcceptableQualityLevelsPro', @level2type=N'COLUMN',@level2name=N'LotSize_Start'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'產品總數 - 判斷範圍結束值' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'AcceptableQualityLevelsPro', @level2type=N'COLUMN',@level2name=N'LotSize_End'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'抽樣數' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'AcceptableQualityLevelsPro', @level2type=N'COLUMN',@level2name=N'SampleSize'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'AQL類型' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'AcceptableQualityLevelsPro', @level2type=N'COLUMN',@level2name=N'AQLType'
+GO
+
+
---
+++ Database/dbo/Tables/AcceptableQualityLevelsPro_DefectCategory.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[AcceptableQualityLevelsPro_DefectCategory](
+	[Ukey] [bigint] IDENTITY(1,1) NOT NULL,
+	[Description] [varchar](20) NOT NULL,
+ CONSTRAINT [PK_AcceptableQualityLevelsPro_DefectCategory] PRIMARY KEY CLUSTERED 
+(
+	[Ukey] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[AcceptableQualityLevelsPro_DefectCategory] ADD  CONSTRAINT [DF_AcceptableQualityLevelsPro_DefectCategory_Description]  DEFAULT ('') FOR [Description]
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Defect種類Ukey' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'AcceptableQualityLevelsPro_DefectCategory', @level2type=N'COLUMN',@level2name=N'Ukey'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Defect種類描述' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'AcceptableQualityLevelsPro_DefectCategory', @level2type=N'COLUMN',@level2name=N'Description'
+GO
+
