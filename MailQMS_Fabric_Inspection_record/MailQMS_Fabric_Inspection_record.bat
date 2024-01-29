91e86bdbc199daaf1f099d504ef6bb1fa9f176aa
aaron95271234@gmail.com <aaron95271234@gmail.com>
2024-01-29 14:08:31 +0800

    ISP20231271  工廠windows job統整 刪除整併到FactoryServerTaskJob的Job專案

--- MailQMS_Fabric_Inspection_record/MailQMS_Fabric_Inspection_record.bat
+++
@@ -1,2 +0,0 @@
-path \\System2016\System2017\Production\SPT_Dummy\PMSUploadDataToAPS
-start /b \\System2016\System2017\Production\SPT_Dummy\PMSUploadDataToAPS\PMSUploadDataToAPS.Daily "DailyTransfer"
