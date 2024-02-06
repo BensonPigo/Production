6ce0374b245a86d15680c83cc133e5050c4fe9a3
DOMAIN\jeff.yeh <jeff.yeh@sportscity.com.tw>
2024-02-06 17:50:26 +0800

    ISP20240139

---
+++ Sci.Production.PublicPrg/PowerBI/Logic/PPIC_R03.cs
@@ -0,0 +1,48 @@
+﻿using Sci.Data;
+using Sci.Production.Prg.PowerBI.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Sci.Production.Prg.PowerBI.Logic
+{
+    /// <summary>
+    /// 有調整到需要一併更新至BI
+    /// </summary>
+    public class PPIC_R03
+    {
+        /// <inheritdoc/>
+        public PPIC_R03()
+        {
+            DBProxy.Current.DefaultTimeout = 1800;
+        }
+
+        /// <inheritdoc/>
+        public Base_ViewModel GetPPICMasterList(PPIC_R03_ViewModel model)
+        {
+            List<SqlParameter> listPar = new List<SqlParameter>
+            {
+                new SqlParameter("@SciDelivery1", SqlDbType.Date) { Value = (object)model.SciDelivery1 ?? DBNull.Value },
+                new SqlParameter("@SciDelivery2", SqlDbType.Date) { Value = (object)model.SciDelivery2 ?? DBNull.Value },
+            };
+
+            string sql = @"
+exec [dbo].[PPIC_R03_FORBI] @SciDelivery1, @SciDelivery2, 1
+exec [dbo].[PPIC_R03_FORBI] @SciDelivery1, @SciDelivery2, 2
+";
+            Base_ViewModel resultReport = new Base_ViewModel
+            {
+                Result = DBProxy.Current.Select("Production", sql, listPar, out DataTable[] dataTables),
+            };
+
+            if (!resultReport.Result)
+            {
+                return resultReport;
+            }
+
+            resultReport.DtArr = dataTables;
+            return resultReport;
+        }
+    }
+}
---
+++ Sci.Production.PublicPrg/PowerBI/Model/PPIC_R03_ViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Office.Interop.Word;
+using System;
+using System.Collections.Generic;
+
+namespace Sci.Production.Prg.PowerBI.Model
+{
+    /// <inheritdoc/>
+#pragma warning disable SA1600 // Elements should be documented
+#pragma warning disable SA1516 // Elements should be separated by blank line
+    public class PPIC_R03_ViewModel
+    {
+        public DateTime? BuyerDelivery1 { get; set; }
+        public DateTime? BuyerDelivery2 { get; set; }
+        public DateTime? SciDelivery1 { get; set; }
+        public DateTime? SciDelivery2 { get; set; }
+        public DateTime? CutOffDate1 { get; set; }
+        public DateTime? CutOffDate2 { get; set; }
+        public DateTime? CustRQSDate1 { get; set; }
+        public DateTime? CustRQSDate2 { get; set; }
+        public DateTime? PlanDate1 { get; set; }
+        public DateTime? PlanDate2 { get; set; }
+        public string SP1 { get; set; }
+        public string SP2 { get; set; }
+        public string Style { get; set; }
+        public string Article { get; set; }
+        public string Season { get; set; }
+        public string Brand { get; set; }
+        public string CustCD { get; set; }
+        public string Zone { get; set; }
+        public string M { get; set; }
+        public string Factory { get; set; }
+        public bool Bulk { get; set; }
+        public bool Sample { get; set; }
+        public bool Material { get; set; }
+        public bool Forecast { get; set; }
+        public bool Garment { get; set; }
+        public bool SMTL { get; set; }
+        public string SubProcess { get; set; }
+        public bool IncludeHistoryOrder { get; set; }
+        public bool IncludeArtworkData { get; set; }
+        public bool PrintingDetail { get; set; }
+        public bool ByCPU { get; set; }
+        public bool IncludeArtworkDataKindIsPAP { get; set; }
+        public bool SeparateByQtyBdownByShipmode { get; set; }
+        public bool ListPOCombo { get; set; }
+        public bool IncludeCancelOrder { get; set; }
+    }
+#pragma warning restore SA1516 // Elements should be separated by blank line
+#pragma warning restore SA1600 // Elements should be documented
+}
