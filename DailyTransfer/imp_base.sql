dae76394bd4e730b8b951782217b04cc151f94d4
DOMAIN\neil.lo <neil.lo@sportscity.com.tw>
2024-06-14 09:05:32 +0800

    [ISP20240551] B03 新增Manual Cutting Max Layer、Auto Cutting Max Layer欄位 資料交換Construction調整

--- DailyTransfer/imp_base.sql
+++ DailyTransfer/imp_base.sql
@@ -2291,6 +2291,8 @@ SET
        --a.Id		     =b.Id	
       a.Name		      = isnull(b.Name	               ,'')
       ,a.CuttingLayer		      = isnull(b.CuttingLayer	,0)
+	  ,a.ManualCutLayer   = iif(b.ManualCutLayer = 0, isnull(b.CuttingLayer, 0), b.ManualCutLayer)
+	  ,a.AutoCutLayer     = iif(b.AutoCutLayer = 0, isnull(b.CuttingLayer, 0), b.AutoCutLayer)
       ,a.Junk		      = isnull(b.Junk				   ,0)
       ,a.AddName		      = isnull(b.AddName		   ,'')
       ,a.AddDate		      = b.AddDate		  
@@ -2303,6 +2305,8 @@ INSERT INTO Production.dbo.Construction(
        Id
       ,Name
       ,CuttingLayer
+	  ,ManualCutLayer
+	  ,AutoCutLayer
       ,Junk
       ,AddName
       ,AddDate
@@ -2314,6 +2318,8 @@ select
        isnull(Id           ,'')
       ,isnull(Name		   ,'')
       ,isnull(CuttingLayer ,0)
+	  ,isnull(ManualCutLayer, 0)
+	  ,isnull(AutoCutLayer, 0)
       ,isnull(Junk		   ,0)
       ,isnull(AddName	   ,'')
       ,AddDate	 
