ae59bdd37363a69d0f0156b0950fc76a7dd188b7
DOMAIN\willy.wei <willy.wei@sportscity.com.tw>
2024-07-23 10:22:43 +0800

    ISP20240657 新增BI報表P_LiineBalancingRate

---
+++ Powerbi/dbo/Stored Procedures/P_Import_LineBalancingRate.sql
@@ -0,0 +1,219 @@
+﻿-- =============================================
+-- Description:	資料來源SewingSchedule + P_LineMapping
+-- =============================================
+CREATE PROCEDURE [dbo].[P_Import_LineBalancingRate]
+	@StartDate date
+AS
+
+BEGIN	
+	SET NOCOUNT ON;
+
+declare @SDate date = CONVERT(date, @StartDate)
+declare @EDate date = CONVERT(date,DATEADD(day,7, @StartDate)) 
+
+select distinct SewingDate = CONVERT(date,@SDate),FactoryID  
+into #tmpMain
+from  [MainServer].Production.dbo.SewingSchedule s
+where @SDate >=CONVERT(date, s.Inline) and @EDate <= CONVERT(date, s.Offline)
+
+create table #P_LineBalancingRate
+(
+	SewingDate date,
+	FactoryID varchar(8),
+	[Total SP Qty] int,
+	[Total LBR] numeric(7,2),
+	[Avg. LBR] numeric(7,2),
+	[Total SP Qty In7Days] int,
+	[Total LBR In7Days] numeric(7,2),
+	[Avg. LBR In7Days] numeric(7,2)
+)
+
+insert #P_LineBalancingRate(SewingDate,FactoryID,[Total SP Qty],[Total LBR],[Avg. LBR])
+select t.SewingDate,t.FactoryID
+,[Total SP Qty] = OrderCnt.value
+,[Total LBR] = LBR.value
+,[Avg. LBR] = ROUND(LBR.value / OrderCnt.value,2)
+from #tmpMain t
+left join (
+	select FactoryID, value = count(1) 
+	from 
+	(
+		select * from 
+		(
+			select rowno = ROW_NUMBER() over(partition by lm.ID order by lm.ID desc)
+			, s.OrderID,s.FactoryID,lm.StyleUKey
+			from [MainServer].Production.dbo.SewingSchedule s with(nolock)
+			left join [MainServer].Production.dbo.orders o with(nolock) on s.OrderId = o.id
+			left join P_LineMapping lm with(nolock) on lm.StyleUKey = o.StyleUkey and  s.FactoryID = lm.Factory	and s.ComboType = lm.ComboType and lm.Phase = 'Final'
+			where 1=1
+			and CONVERT(date,@SDate) between convert(date,Inline) and CONVERT(date, Offline)
+		) a
+		where rowno=1
+		and StyleUKey is not null
+	) a
+	group by FactoryID
+)OrderCnt on OrderCnt.FactoryID = t.FactoryID
+left join (
+	select Factory,value = round(convert(float,sum(value)),2)
+	from (
+		select Factory,value= AVG([LBR By Cycle Time(%)])
+		from (
+			select rowno = ROW_NUMBER() over(partition by Factory,StyleUKey,ComboType order by version desc),*
+			from P_LineMapping lm with(nolock)
+			where Phase='Final'
+			and exists(
+				select 1 
+				from [MainServer].Production.dbo.SewingSchedule s with(nolock)
+				left join [MainServer].Production.dbo.orders o with(nolock) on s.OrderId = o.id
+				where  lm.StyleUKey = o.StyleUkey and s.FactoryID = lm.Factory
+				and s.ComboType = lm.ComboType 
+				and CONVERT(date,@SDate) between CONVERT(date,s.Inline) and CONVERT(date,s.Offline)
+			)
+		) a
+		where rowno=1
+		group by Factory,StyleUkey,ComboType
+	) a
+	group by Factory
+)LBR on lbr.Factory = t.FactoryID
+option (recompile)
+
+/*
+跑迴圈一天一天加總塞資料
+避免用Range會無法算到相同訂單資料
+*/
+DECLARE @Day INT = 1;
+
+WHILE @Day <= 7
+BEGIN	
+	select   t.SewingDate,t.FactoryID,t.[Total SP Qty],t.[Total LBR],t.[Avg. LBR]
+	,[Total SP Qty In7Days] = OrderCnt.value
+	,[Total LBR In7Days] = LBR.value
+	into #tmpLoop
+	from #P_LineBalancingRate t
+	outer apply(
+		select FactoryID, value = count(1) 
+		from 
+		(
+			select * from 
+			(
+				select rowno = ROW_NUMBER() over(partition by lm.ID order by lm.ID desc)
+				, s.OrderID,s.FactoryID,lm.StyleUKey
+				from [MainServer].Production.dbo.SewingSchedule s with(nolock)
+				left join [MainServer].Production.dbo.orders o with(nolock) on s.OrderId = o.id
+				left join P_LineMapping lm with(nolock) on lm.StyleUKey = o.StyleUkey and  s.FactoryID = lm.Factory	and s.ComboType = lm.ComboType AND lm.Phase = 'Final'
+				where 1=1
+				and CONVERT(date,DATEADD(day,@Day, @SDate)) between convert(date,Inline) and CONVERT(date, Offline)
+			) a
+			where rowno=1
+			and StyleUKey is not null
+		) a
+		where FactoryID = t.FactoryID
+		group by FactoryID
+	)OrderCnt
+	outer apply(
+		select Factory,value = round(convert(float,sum(value)),2)
+		from (
+			select Factory,value= AVG([LBR By Cycle Time(%)])
+			from (
+				select rowno = ROW_NUMBER() over(partition by Factory,StyleUKey,ComboType order by version desc),*
+				from P_LineMapping lm with(nolock)
+				where Phase='Final'
+				and exists(
+					select 1 
+					from [MainServer].Production.dbo.SewingSchedule s with(nolock)
+					left join [MainServer].Production.dbo.orders o with(nolock) on s.OrderId = o.id
+					where  lm.StyleUKey = o.StyleUkey and s.FactoryID = lm.Factory
+					and s.ComboType = lm.ComboType 
+					and CONVERT(date,DATEADD(day,1, @SDate)) between convert(date,Inline) and CONVERT(date, Offline)
+				)
+			)a
+			where rowno=1
+			group by Factory,StyleUkey,ComboType
+		) a
+		where Factory = t.FactoryID
+		group by Factory
+	)LBR
+	option (recompile)
+
+	--select * from #tmpLoop
+
+	update t
+	set [Total SP Qty In7Days] = isnull(t.[Total SP Qty In7Days],0) + s.[Total SP Qty In7Days]
+	,[Total LBR In7Days] = isnull(t.[Total LBR In7Days],0) + s.[Total LBR In7Days]
+	from #P_LineBalancingRate t
+	inner join #tmpLoop s on t.FactoryID = s.FactoryID and t.SewingDate = s.SewingDate
+
+	drop table #tmpLoop
+
+    -- 增加日期和天數
+    SET @Day = @Day + 1;
+END
+
+-- 計算加總後未來七天的Avg
+
+update t
+set [Avg. LBR In7Days]  = ROUND(convert(float,[Total LBR In7Days]) / [Total SP Qty In7Days],2)
+from #P_LineBalancingRate t
+
+
+delete t
+from P_LineBalancingRate t
+where not exists(
+	select 1 from #P_LineBalancingRate s
+	where s.SewingDate = t.SewingDate
+	and s.FactoryID = t.FactoryID
+)
+and SewingDate = @SDate
+
+update t
+set t.[Total SP Qty] = s.[Total SP Qty] 
+,t.[Total LBR] = s.[Total LBR]
+,t.[Avg. LBR] = s.[Avg. LBR]
+,t.[Total SP Qty In7Days] = s.[Total SP Qty In7Days]
+,t.[Total LBR In7Days] = s.[Total LBR In7Days]
+,t.[Avg. LBR In7Days] = s.[Avg. LBR In7Days]
+from P_LineBalancingRate t
+inner join #P_LineBalancingRate s 
+	on s.SewingDate = t.SewingDate
+	and s.FactoryID = t.FactoryID
+
+insert into P_LineBalancingRate(
+[SewingDate]
+,[FactoryID]
+,[Total SP Qty]
+,[Total LBR]
+,[Avg. LBR]
+,[Total SP Qty In7Days]
+,[Total LBR In7Days]
+,[Avg. LBR In7Days]
+)
+select  
+[SewingDate]
+,[FactoryID]
+,[Total SP Qty]
+,[Total LBR]
+,[Avg. LBR]
+,[Total SP Qty In7Days]
+,[Total LBR In7Days]
+,[Avg. LBR In7Days]
+from #P_LineBalancingRate t
+where not exists(
+	select 1 from P_LineBalancingRate s
+	where s.SewingDate = t.SewingDate
+	and s.FactoryID = t.FactoryID
+)
+
+if exists(select 1 from BITableInfo where Id = 'P_LineBalancingRate')
+begin
+	update BITableInfo set TransferDate = @SDate
+	where Id = 'P_LineBalancingRate'
+end
+else
+begin
+	insert into BITableInfo(Id, TransferDate, IS_Trans) values('P_LineBalancingRate', @SDate, 0)
+end
+
+
+drop table #tmpMain,#P_LineBalancingRate
+
+end
