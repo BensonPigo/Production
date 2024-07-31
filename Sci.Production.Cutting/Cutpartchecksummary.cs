b913f82ea5df57d8281f6a7bee5d8ea6955660d8
dyson.chen <dyson.chen@sportscity.com.tw>
2024-07-31 11:23:31 +0800

    remove title text )

--- Sci.Production.Cutting/Cutpartchecksummary.cs
+++ Sci.Production.Cutting/Cutpartchecksummary.cs
@@ -35,7 +35,7 @@ namespace Sci.Production.Cutting
         {
             this.InitializeComponent();
             this.form = form;
-            this.Text = $"Cut Parts Check Summary<SP:{cuttingID}>)";
+            this.Text = $"Cut Parts Check Summary<SP:{cuttingID}>";
             this.CuttingID = cuttingID;
             this.DetailDatas = detailDatas;
             this.dtSizeRatio = dtSizeRatio?.AsEnumerable().Where(w => w.RowState != DataRowState.Deleted).TryCopyToDataTable(dtSizeRatio);
