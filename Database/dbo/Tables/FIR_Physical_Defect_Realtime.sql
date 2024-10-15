87c9586c149ed5e64345178c6e787e48c8b8a053
DOMAIN\jack.hsu <jack.hsu@sportscity.com.tw>
2024-10-15 20:51:28 +0800

    ISP20240974 - MachineIoT Inspection、Unroll、Spreading 初版

--- Database/dbo/Tables/FIR_Physical_Defect_Realtime.sql
+++ Database/dbo/Tables/FIR_Physical_Defect_Realtime.sql
@@ -1,35 +1,53 @@
-﻿CREATE TABLE [dbo].[FIR_Physical_Defect_Realtime] (
-    [FIR_PhysicalDetailUkey] BIGINT         CONSTRAINT [DF_FIR_Physical_Defect_Realtime_FIR_PhysicalDetailUkey] DEFAULT ((0)) NULL,
-    [Yards]                  NUMERIC (9, 3) CONSTRAINT [DF_FIR_Physical_Defect_Realtime_Yards] DEFAULT ((0)) NULL,
-    [FabricdefectID]         VARCHAR (2)    CONSTRAINT [DF_FIR_Physical_Defect_Realtime_FabricdefectID] DEFAULT ('') NULL,
-    [AddDate]                DATETIME       NULL,
-    [Id]                     BIGINT         IDENTITY (1, 1) NOT NULL,
-    CONSTRAINT [PK_FIR_Physical_Defect_Realtime] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
-
-
-
+﻿CREATE TABLE [dbo].[FIR_Physical_Defect_Realtime](
+	[FIR_PhysicalDetailUkey] [bigint] NULL,
+	[Yards] [numeric](9, 3) NULL,
+	[FabricdefectID] [varchar](2) NULL,
+	[AddDate] [datetime] NULL,
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[T2] [bit] NOT NULL,
+	[MachineIoTUkey] [bigint] NULL,
+ CONSTRAINT [PK_FIR_Physical_Defect_Realtime] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'FIR_Physical_Defect_Realtime';
 
-
+ALTER TABLE [dbo].[FIR_Physical_Defect_Realtime] ADD  CONSTRAINT [DF_FIR_Physical_Defect_Realtime_FIR_PhysicalDetailUkey]  DEFAULT ((0)) FOR [FIR_PhysicalDetailUkey]
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'DetailUkey', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'FIR_Physical_Defect_Realtime', @level2type = N'COLUMN', @level2name = N'FIR_PhysicalDetailUkey';
 
-
+ALTER TABLE [dbo].[FIR_Physical_Defect_Realtime] ADD  CONSTRAINT [DF_FIR_Physical_Defect_Realtime_Yards]  DEFAULT ((0)) FOR [Yards]
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'瑕疵碼數', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'FIR_Physical_Defect_Realtime', @level2type = N'COLUMN', @level2name = N'Yards';
 
-
+ALTER TABLE [dbo].[FIR_Physical_Defect_Realtime] ADD  CONSTRAINT [DF_FIR_Physical_Defect_Realtime_FabricdefectID]  DEFAULT ('') FOR [FabricdefectID]
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'瑕疵代號', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'FIR_Physical_Defect_Realtime', @level2type = N'COLUMN', @level2name = N'FabricdefectID';
 
-
+ALTER TABLE [dbo].[FIR_Physical_Defect_Realtime] ADD  DEFAULT ((0)) FOR [T2]
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'新增時間', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'FIR_Physical_Defect_Realtime', @level2type = N'COLUMN', @level2name = N'AddDate';
 
-
+ALTER TABLE [dbo].[FIR_Physical_Defect_Realtime] ADD  CONSTRAINT [DF_FIR_Physical_Defect_Realtime_MachineIoTUkey]  DEFAULT ((0)) FOR [MachineIoTUkey]
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Identity', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'FIR_Physical_Defect_Realtime', @level2type = N'COLUMN', @level2name = N'Id';
 
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'DetailUkey' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FIR_Physical_Defect_Realtime', @level2type=N'COLUMN',@level2name=N'FIR_PhysicalDetailUkey'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'瑕疵碼數' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FIR_Physical_Defect_Realtime', @level2type=N'COLUMN',@level2name=N'Yards'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'瑕疵代號' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FIR_Physical_Defect_Realtime', @level2type=N'COLUMN',@level2name=N'FabricdefectID'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'新增時間' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FIR_Physical_Defect_Realtime', @level2type=N'COLUMN',@level2name=N'AddDate'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Identity' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FIR_Physical_Defect_Realtime', @level2type=N'COLUMN',@level2name=N'Id'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'T2' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FIR_Physical_Defect_Realtime', @level2type=N'COLUMN',@level2name=N'T2'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'IoT 機台 Ukey' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FIR_Physical_Defect_Realtime', @level2type=N'COLUMN',@level2name=N'MachineIoTUkey'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'FIR_Physical_Defect_Realtime'
+GO
--- Database/dbo/Tables/Fabric_UnrollandRelax.sql
+++ Database/dbo/Tables/Fabric_UnrollandRelax.sql
@@ -1,90 +1,111 @@
-﻿CREATE TABLE [dbo].[Fabric_UnrollandRelax] (
-    [Barcode]             VARCHAR (255)   NOT NULL,
-    [POID]                VARCHAR (13)    DEFAULT ('') NULL,
-    [Seq1]                VARCHAR (3)     DEFAULT ('') NULL,
-    [Seq2]                VARCHAR (2)     DEFAULT ('') NULL,
-    [Roll]                VARCHAR (8)     DEFAULT ('') NULL,
-    [Dyelot]              VARCHAR (8)     DEFAULT ('') NULL,
-    [StockType]           CHAR (1)        DEFAULT ('') NULL,
-    [UnrollStatus]        VARCHAR (10)    DEFAULT ('') NULL,
-    [UnrollStartTime]     DATETIME        NULL,
-    [UnrollEndTime]       DATETIME        NULL,
-    [RelaxationStartTime] DATETIME        NULL,
-    [RelaxationEndTime]   DATETIME        NULL,
-    [UnrollScanner]       VARCHAR (10)    DEFAULT ('') NULL,
-    [UnrollActualQty]     NUMERIC (11, 2) DEFAULT ((0)) NULL,
-    [UnrollRemark]        NVARCHAR (100)  DEFAULT ('') NULL,
-    [IsAdvance] BIT NOT NULL DEFAULT ((0)), 
-    CONSTRAINT [PK_Fabric_UnrollandRelax] PRIMARY KEY CLUSTERED ([Barcode] ASC)
-);
-
-
+﻿CREATE TABLE [dbo].[Fabric_UnrollandRelax](
+	[Barcode] [varchar](255) NOT NULL,
+	[POID] [varchar](13) NULL,
+	[Seq1] [varchar](3) NULL,
+	[Seq2] [varchar](2) NULL,
+	[Roll] [varchar](8) NULL,
+	[Dyelot] [varchar](8) NULL,
+	[StockType] [char](1) NULL,
+	[UnrollStatus] [varchar](10) NULL,
+	[UnrollStartTime] [datetime] NULL,
+	[UnrollEndTime] [datetime] NULL,
+	[RelaxationStartTime] [datetime] NULL,
+	[RelaxationEndTime] [datetime] NULL,
+	[UnrollScanner] [varchar](10) NULL,
+	[UnrollActualQty] [numeric](11, 2) NULL,
+	[UnrollRemark] [nvarchar](100) NULL,
+	[IsAdvance] [bit] NOT NULL,
+	[MachineIoTUkey] [bigint] NULL,
+ CONSTRAINT [PK_Fabric_UnrollandRelax] PRIMARY KEY CLUSTERED 
+(
+	[Barcode] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'M360 Unroll 階段備註', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Fabric_UnrollandRelax', @level2type = N'COLUMN', @level2name = N'UnrollRemark';
 
-
+ALTER TABLE [dbo].[Fabric_UnrollandRelax] ADD  DEFAULT ('') FOR [POID]
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'M360 Unroll 階段實際收到的數量', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Fabric_UnrollandRelax', @level2type = N'COLUMN', @level2name = N'UnrollActualQty';
 
-
+ALTER TABLE [dbo].[Fabric_UnrollandRelax] ADD  DEFAULT ('') FOR [Seq1]
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'M360 Unroll 掃描 Unroll Location 的使用者', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Fabric_UnrollandRelax', @level2type = N'COLUMN', @level2name = N'UnrollScanner';
 
-
+ALTER TABLE [dbo].[Fabric_UnrollandRelax] ADD  DEFAULT ('') FOR [Seq2]
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'倉庫鬆布 完成時間', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Fabric_UnrollandRelax', @level2type = N'COLUMN', @level2name = N'RelaxationEndTime';
 
-
+ALTER TABLE [dbo].[Fabric_UnrollandRelax] ADD  DEFAULT ('') FOR [Roll]
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'倉庫鬆布 開始時間', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Fabric_UnrollandRelax', @level2type = N'COLUMN', @level2name = N'RelaxationStartTime';
 
-
+ALTER TABLE [dbo].[Fabric_UnrollandRelax] ADD  DEFAULT ('') FOR [Dyelot]
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'倉庫攤開布捲 完成時間', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Fabric_UnrollandRelax', @level2type = N'COLUMN', @level2name = N'UnrollEndTime';
 
-
+ALTER TABLE [dbo].[Fabric_UnrollandRelax] ADD  DEFAULT ('') FOR [StockType]
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'倉庫攤開布捲 開始時間', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Fabric_UnrollandRelax', @level2type = N'COLUMN', @level2name = N'UnrollStartTime';
 
-
+ALTER TABLE [dbo].[Fabric_UnrollandRelax] ADD  DEFAULT ('') FOR [UnrollStatus]
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'倉庫攤開布捲狀態，可能值包括 Ongoing 或 Done', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Fabric_UnrollandRelax', @level2type = N'COLUMN', @level2name = N'UnrollStatus';
 
-
+ALTER TABLE [dbo].[Fabric_UnrollandRelax] ADD  DEFAULT ('') FOR [UnrollScanner]
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'鬆布時對應的 StockType', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Fabric_UnrollandRelax', @level2type = N'COLUMN', @level2name = N'StockType';
 
-
+ALTER TABLE [dbo].[Fabric_UnrollandRelax] ADD  DEFAULT ((0)) FOR [UnrollActualQty]
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'鬆布時對應的 Dyelot', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Fabric_UnrollandRelax', @level2type = N'COLUMN', @level2name = N'Dyelot';
 
-
+ALTER TABLE [dbo].[Fabric_UnrollandRelax] ADD  DEFAULT ('') FOR [UnrollRemark]
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'鬆布時對應的 Roll', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Fabric_UnrollandRelax', @level2type = N'COLUMN', @level2name = N'Roll';
 
-
+ALTER TABLE [dbo].[Fabric_UnrollandRelax] ADD  CONSTRAINT [DF_Fabric_UnrollandRelax_IsAdvance]  DEFAULT ((0)) FOR [IsAdvance]
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'鬆布時對應的 Seq2', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Fabric_UnrollandRelax', @level2type = N'COLUMN', @level2name = N'Seq2';
 
-
+ALTER TABLE [dbo].[Fabric_UnrollandRelax] ADD  CONSTRAINT [DF_Fabric_UnrollandRelax_MachineIoTUkey]  DEFAULT ((0)) FOR [MachineIoTUkey]
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'鬆布時對應的 Seq1', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Fabric_UnrollandRelax', @level2type = N'COLUMN', @level2name = N'Seq1';
 
-
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'主料條碼' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Fabric_UnrollandRelax', @level2type=N'COLUMN',@level2name=N'Barcode'
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'鬆布時對應的 POID', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Fabric_UnrollandRelax', @level2type = N'COLUMN', @level2name = N'POID';
 
-
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'鬆布時對應的 POID' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Fabric_UnrollandRelax', @level2type=N'COLUMN',@level2name=N'POID'
 GO
-EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'主料條碼', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Fabric_UnrollandRelax', @level2type = N'COLUMN', @level2name = N'Barcode';
 
-
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'鬆布時對應的 Seq1' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Fabric_UnrollandRelax', @level2type=N'COLUMN',@level2name=N'Seq1'
 GO
-EXEC sp_addextendedproperty @name = N'MS_Description',
-    @value = N'判斷是否為提前鬆布',
-    @level0type = N'SCHEMA',
-    @level0name = N'dbo',
-    @level1type = N'TABLE',
-    @level1name = N'Fabric_UnrollandRelax',
-    @level2type = N'COLUMN',
-    @level2name = N'IsAdvance'
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'鬆布時對應的 Seq2' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Fabric_UnrollandRelax', @level2type=N'COLUMN',@level2name=N'Seq2'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'鬆布時對應的 Roll' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Fabric_UnrollandRelax', @level2type=N'COLUMN',@level2name=N'Roll'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'鬆布時對應的 Dyelot' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Fabric_UnrollandRelax', @level2type=N'COLUMN',@level2name=N'Dyelot'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'鬆布時對應的 StockType' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Fabric_UnrollandRelax', @level2type=N'COLUMN',@level2name=N'StockType'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'倉庫攤開布捲狀態，可能值包括 Ongoing 或 Done' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Fabric_UnrollandRelax', @level2type=N'COLUMN',@level2name=N'UnrollStatus'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'倉庫攤開布捲 開始時間' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Fabric_UnrollandRelax', @level2type=N'COLUMN',@level2name=N'UnrollStartTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'倉庫攤開布捲 完成時間' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Fabric_UnrollandRelax', @level2type=N'COLUMN',@level2name=N'UnrollEndTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'倉庫鬆布 開始時間' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Fabric_UnrollandRelax', @level2type=N'COLUMN',@level2name=N'RelaxationStartTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'倉庫鬆布 完成時間' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Fabric_UnrollandRelax', @level2type=N'COLUMN',@level2name=N'RelaxationEndTime'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'M360 Unroll 掃描 Unroll Location 的使用者' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Fabric_UnrollandRelax', @level2type=N'COLUMN',@level2name=N'UnrollScanner'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'M360 Unroll 階段實際收到的數量' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Fabric_UnrollandRelax', @level2type=N'COLUMN',@level2name=N'UnrollActualQty'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'M360 Unroll 階段備註' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Fabric_UnrollandRelax', @level2type=N'COLUMN',@level2name=N'UnrollRemark'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'判斷是否為提前鬆布' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Fabric_UnrollandRelax', @level2type=N'COLUMN',@level2name=N'IsAdvance'
+GO
+
+EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'布卷攤開的機台' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'Fabric_UnrollandRelax', @level2type=N'COLUMN',@level2name=N'MachineIoTUkey'
+GO
