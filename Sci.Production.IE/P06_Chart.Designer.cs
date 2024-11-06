79d931816bd2a10550ed1dc5944625ccd8b72bd4
DOMAIN\ryan.tsao <ryan.tsao@sportscity.com.tw>
2024-11-06 10:01:08 +0800

    ISP20240132  [P06. Line Mapping & Balancing] - Operator ID&Name not reflect to Excel

--- Sci.Production.IE/P06_Chart.Designer.cs
+++ Sci.Production.IE/P06_Chart.Designer.cs
@@ -57,7 +57,7 @@
             title2.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Left;
             title2.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             title2.Name = "Act GSD Time";
-            title2.Text = "Act GSD Time (in secs)";
+            title2.Text = "AVG. GSD Time (in secs)";
             title3.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Bottom;
             title3.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             title3.Name = "Operator No.";
