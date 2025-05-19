103f958a4d0acec766f38c51cdae9ac3a57c7ca4
DOMAIN\benson.chung <benson.chung@sportscity.com.tw>
2025-05-19 14:47:17 +0800

    ISP20250541 - 根據mix marker 專案，調整ecp_Cutting，但轉出去的欄位名稱不變，方便Trade轉入

--- DailyTransfer/exp_Cutting.sql
+++ DailyTransfer/exp_Cutting.sql
@@ -1,5 +1,5 @@
 
-Create PROCEDURE [dbo].[exp_Cutting] 
+CREATE PROCEDURE [dbo].[exp_Cutting] 
 
 AS
 BEGIN
@@ -55,32 +55,28 @@ WHERE CUT1. Status != 'New'
 AND  (CUT1.Lock  BETWEEN @DateStart AND  @DateEnd or  CUT1.Lock IS NULL)
 
 
-SELECT ID,CutRef,CuttingID,isnull(FabricCombo,'') as FabricCombo,Cutno,MarkerName,Markerlength,Layer,Cons,UKey,WorkOrderUkey,Colorid 
-INTO CuttingOutput_Detail
-FROM (
-SELECT 
-CUT1. ID,CUT2.CutRef,CUT2.CuttingID,
-(select isnull(FabricCombo,'') from Production.dbo.WorkOrder where UKey = CUT2.WorkOrderUKey) as FabricCombo,
-CUT2. Cutno,CUT2. MarkerName,CUT2. Markerlength,CUT2. Layer,CUT2. Cons,
-CUT2 .UKey, CUT2. WorkOrderUkey,CUT2. Colorid
-FROM Pms_To_Trade.dbo.CuttingOutput CUT1 , Production.dbo.CuttingOutput_Detail CUT2  
-WHERE CUT1. ID = CUT2.ID
-)as aaa
+SELECT
+    d.ID,
+    d.CutRef,
+    d.CuttingID,
+    ISNULL(w.FabricCombo, '') AS FabricCombo,
+    d.Cutno,
+    d.MarkerName,
+    d.Markerlength,
+    d.Layer,
+    d.Cons,
+    d.UKey,
+    WorkOrderUkey = d.WorkOrderForOutputUkey,
+    d.Colorid
+INTO dbo.CuttingOutput_Detail
+FROM Pms_To_Trade.dbo.CuttingOutput AS o
+INNER JOIN Production.dbo.CuttingOutput_Detail AS d¡@ON o.ID = d.ID
+LEFT JOIN Production.dbo.WorkOrderForOutput AS w¡@ON w.UKey = d.WorkOrderForOutputUkey;
 
 SELECT CUT1.ID,CUT3.CuttingOutput_detailUkey,CUT3.CuttingID, CUT3.SizeCode,CUT3.Qty
 INTO  CuttingOutput_Detail_Detail
 FROM Pms_To_Trade.dbo.CuttingOutput CUT1, Production.dbo.CuttingOutput_Detail_Detail CUT3 
 WHERE CUT1. ID = CUT3. ID
-
---Select distinct orderid = o.ID,wd.SizeCode,wd.article
---into #tmp1
---from Production.dbo.Orders o WITH (NOLOCK)
---inner join Production.dbo.WorkOrder_Distribute wd  on o.id = wd.OrderID
---inner join Production.dbo.Order_ColorCombo occ on o.poid = occ.id and occ.Article = wd.Article
---inner join Production.dbo.order_Eachcons cons on occ.id = cons.id and cons.FabricCombo = occ.PatternPanel and cons.CuttingPiece='0'
---inner join Production.dbo.CuttingOutput_Detail cud on cud.WorkOrderUkey = wd.WorkOrderUkey
---inner join Pms_To_Trade.dbo.CuttingOutput CUT1 on CUT1.ID = cud.ID
---where occ.FabricCode !='' and occ.FabricCode is not null 
 
 -- Only for CuttingOutput_WIP
 select WIP.OrderID,WIP.Article,WIP.Size,WIP.Qty
@@ -89,10 +85,10 @@ FROM Production.dbo.CuttingOutput_WIP WIP
 where WIP.EditDate between DATEADD(DAY,-7, GETDATE()) and GETDATE()
 
 
-select wd.WorkOrderUkey,wd.ID,wd.OrderID,wd.Article,wd.SizeCode,wd.Qty
+select WorkOrderUkey = wd.WorkorderForOutputUkey,wd.ID,wd.OrderID,wd.Article,wd.SizeCode,wd.Qty
 into Workorder_Distribute
-from Production.dbo.Workorder_Distribute wd
-inner join Pms_To_Trade.dbo.CuttingOutput_Detail cud on cud.WorkOrderUkey = wd.WorkOrderUkey
+from Production.dbo.WorkorderForOutput_Distribute wd
+inner join Pms_To_Trade.dbo.CuttingOutput_Detail cud on cud.WorkOrderUkey = wd.WorkorderForOutputUkey
 
 
 END
