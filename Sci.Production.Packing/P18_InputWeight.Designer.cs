c7832c74293fd38327c681ebd37b0ecf36f81501
shan.lin@sportscity.com.tw <shan.lin@sportscity.com.tw>
2025-08-04 11:14:23 +0800

    [ISP20250717]-因為不是每個工廠都有電子秤，但可能有多台的COM(不是每一台都是電子秤) 1.所有的COM都連不到:畫面不顯示 COM的訊息 2.如果有多台COM連得到並且其中一台能回傳重量的訊息:畫面顯示 此COM的訊息 3.如果有多台COM連得到，但每一台的訊息都不對:畫面顯示 此第一個可連並且有訊息，但同時要彈出錯誤訊息

--- Sci.Production.Packing/P18_InputWeight.Designer.cs
+++ Sci.Production.Packing/P18_InputWeight.Designer.cs
@@ -31,7 +31,7 @@
             this.numWeight = new Sci.Win.UI.NumericBox();
             this.lbWeight = new Sci.Win.UI.Label();
             this.btn_Save = new Sci.Win.UI.Button();
-            this.btnReadWeight = new Sci.Win.UI.Button();
+            this.labCom = new Sci.Win.UI.Label();
             this.SuspendLayout();
             // 
             // numWeight
@@ -39,14 +39,14 @@
             this.numWeight.BackColor = System.Drawing.Color.White;
             this.numWeight.DecimalPlaces = 3;
             this.numWeight.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.numWeight.Location = new System.Drawing.Point(157, 22);
+            this.numWeight.Location = new System.Drawing.Point(144, 18);
             this.numWeight.Name = "numWeight";
             this.numWeight.NullValue = new decimal(new int[] {
             0,
             0,
             0,
             0});
-            this.numWeight.Size = new System.Drawing.Size(146, 23);
+            this.numWeight.Size = new System.Drawing.Size(213, 23);
             this.numWeight.TabIndex = 113;
             this.numWeight.Value = new decimal(new int[] {
             0,
@@ -56,7 +56,7 @@
             // 
             // lbWeight
             // 
-            this.lbWeight.Location = new System.Drawing.Point(22, 22);
+            this.lbWeight.Location = new System.Drawing.Point(9, 18);
             this.lbWeight.Name = "lbWeight";
             this.lbWeight.Size = new System.Drawing.Size(131, 23);
             this.lbWeight.TabIndex = 112;
@@ -64,7 +64,7 @@
             // 
             // btn_Save
             // 
-            this.btn_Save.Location = new System.Drawing.Point(223, 51);
+            this.btn_Save.Location = new System.Drawing.Point(277, 47);
             this.btn_Save.Name = "btn_Save";
             this.btn_Save.Size = new System.Drawing.Size(80, 30);
             this.btn_Save.TabIndex = 114;
@@ -72,28 +72,29 @@
             this.btn_Save.UseVisualStyleBackColor = true;
             this.btn_Save.Click += new System.EventHandler(this.Btn_Save_Click);
             // 
-            // btnReadWeight
+            // labCom
             // 
-            this.btnReadWeight.Location = new System.Drawing.Point(106, 51);
-            this.btnReadWeight.Name = "btnReadWeight";
-            this.btnReadWeight.Size = new System.Drawing.Size(111, 30);
-            this.btnReadWeight.TabIndex = 115;
-            this.btnReadWeight.Text = "Read Weight";
-            this.btnReadWeight.UseVisualStyleBackColor = true;
-            this.btnReadWeight.Click += new System.EventHandler(this.BtnReadWeight_Click);
+            this.labCom.AutoHeight = true;
+            this.labCom.BackColor = System.Drawing.Color.Transparent;
+            this.labCom.Location = new System.Drawing.Point(9, 47);
+            this.labCom.Name = "labCom";
+            this.labCom.Size = new System.Drawing.Size(265, 33);
+            this.labCom.TabIndex = 116;
+            this.labCom.Text = "message";
+            this.labCom.TextStyle.Color = System.Drawing.Color.Blue;
             // 
             // P18_InputWeight
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(321, 93);
-            this.Controls.Add(this.btnReadWeight);
+            this.ClientSize = new System.Drawing.Size(384, 111);
+            this.Controls.Add(this.labCom);
             this.Controls.Add(this.btn_Save);
             this.Controls.Add(this.numWeight);
             this.Controls.Add(this.lbWeight);
             this.Name = "P18_InputWeight";
             this.OnLineHelpID = "Sci.Win.Forms.Base";
-            this.Text = "P18_InputWeight";
+            this.Text = "If the system cannot read the weight, please select a different COM port below. ";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,6 +105,6 @@
         private Win.UI.NumericBox numWeight;
         private Win.UI.Label lbWeight;
         private Win.UI.Button btn_Save;
-        private Win.UI.Button btnReadWeight;
+        private Win.UI.Label labCom;
     }
 }
--- Sci.Production.Packing/P18_InputWeight.cs
+++ Sci.Production.Packing/P18_InputWeight.cs
@@ -1,13 +1,21 @@
 ﻿using System;
+using System.Data;
 using System.IO;
 using System.IO.Ports;
 using System.Text.RegularExpressions;
+using System.Management;
 using System.Windows.Forms;
+using System.Threading.Tasks;
+using static sun.awt.SunHints;
 
 namespace Sci.Production.Packing
 {
     /// <summary>
     /// P18_InputWeight
+    /// [ISP20250717]-因為不是每個工廠都有電子秤，但可能有多台的COM(不是每一台都是電子秤)
+    /// 1.所有的COM都連不到:畫面不顯示 COM的訊息
+    /// 2.如果有多台COM連得到並且其中一台能回傳重量的訊息:畫面顯示 此COM的訊息
+    /// 3.如果有多台COM連得到，但每一台的訊息都不對:畫面顯示 此第一個可連並且有訊息，但同時要彈出錯誤訊息
     /// </summary>
     public partial class P18_InputWeight : Win.Forms.Base
     {
@@ -24,6 +32,29 @@ namespace Sci.Production.Packing
             this.InitializeComponent();
             this.DialogResult = DialogResult.Cancel;
             this.ActWeight = 0;
+
+            this.labCom.Text = string.Empty;
+
+            Task.Run(() =>
+            {
+                string firstErrMsg = string.Empty;
+                string firstComName = string.Empty;
+                foreach (string comName in SerialPort.GetPortNames())
+                {
+                    string errMsg = this.TryReadWeight(comName, comName);
+
+                    if (!string.IsNullOrEmpty(errMsg) && string.IsNullOrEmpty(firstErrMsg))
+                    {
+                        firstComName = comName;
+                        firstErrMsg = errMsg;
+                    }
+                }
+
+                if (string.IsNullOrEmpty(this.labCom.Text) && !string.IsNullOrEmpty(firstErrMsg))
+                {
+                    MessageBox.Show(firstErrMsg);
+                }
+            });
         }
 
         private void Btn_Save_Click(object sender, EventArgs e)
@@ -32,63 +63,73 @@ namespace Sci.Production.Packing
             this.DialogResult = DialogResult.OK;
         }
 
-        private void BtnReadWeight_Click(object sender, EventArgs e)
-        {
-            this.TryReadWeight("COM4", this.numWeight);
-        }
-
         /// <summary>
         /// 嘗試讀取機器上數值
         /// </summary>
-        /// <param name="comPort">port : COM3</param>
-        /// <param name="targetNumericbox">數值</param>
-        public void TryReadWeight(string comPort, Ict.Win.UI.NumericBox targetNumericbox)
+        /// <param name="comPort">comPort</param>
+        /// <param name="comName">comName</param>
+        /// <returns>錯誤訊息</returns>
+        public string TryReadWeight(string comPort, string comName)
         {
             SerialPort scalePort = new SerialPort(comPort, 9600, Parity.None, 8, StopBits.One);
-            scalePort.ReadTimeout = 1000;
-
+            scalePort.ReadTimeout = 500;
             try
             {
                 scalePort.Open();
                 System.Threading.Thread.Sleep(1000); // 等一下讓設備有機會回傳
+            }
+            catch (Exception ex)
+            {
+                return string.Empty;
+            }
 
-                // 讀取一筆資料
-                string raw = scalePort.ReadLine(); // 範例："ST. NET. 3.50 kg"
-                var match = Regex.Match(raw, @"(\d+(\.\d+)?)");
-                if (match.Success)
+            try
+            {
+                while (true)
                 {
-                    string value = match.Value;
-                    targetNumericbox.Value = decimal.Parse(value);
-                }
-                else
-                {
-                    // 若格式異常，視情況顯示錯誤或略過
-                    MessageBox.Show($"讀到無效資料：{raw}");
+                    string raw = scalePort.ReadLine(); // 讀取一筆資料, 範例："ST. NET. 3.50 kg"
+                    var match = Regex.Match(raw, @"(\d+(\.\d+)?)");
+                    if (match.Success)
+                    {
+                        string value = match.Value;
+
+                        // 切回 UI 執行緒設定數值
+                        this.Invoke(new Action(() =>
+                        {
+                            this.numWeight.Value = decimal.Parse(value);
+                            this.labCom.Text = comName;
+                        }));
+                    }
+                    else
+                    {
+                        return $"<{comPort}> Invalid data received: {raw}";
+                    }
                 }
             }
             catch (UnauthorizedAccessException)
             {
-                MessageBox.Show("Port 被其他程式用掉");
+                return $"<{comPort}> is being used by another program";
             }
             catch (TimeoutException)
             {
-                MessageBox.Show("沒回應");
+                // 非秤重機，還是有可能會連接成功，但沒有回應
+                return string.Empty;
             }
             catch (IOException ioEx)
             {
                 var ports = SerialPort.GetPortNames();
                 if (Array.Exists(ports, p => p == comPort))
                 {
-                    MessageBox.Show($"Port {comPort} 存在，但無法開啟，可能被佔用或硬體異常。\n錯誤訊息：{ioEx.Message}");
+                    return $"<{comPort}> exists but cannot be opened, possibly occupied or hardware malfunction.\nError message: {ioEx.Message}";
                 }
                 else
                 {
-                    MessageBox.Show($"Port {comPort} 根本不存在");
+                    return $"<{comPort}> No response";
                 }
             }
             catch (Exception ex)
             {
-                MessageBox.Show("秤重失敗：" + ex.Message);
+                return $"<{comPort}> Scale reading failed: " + ex.Message;
             }
             finally
             {
